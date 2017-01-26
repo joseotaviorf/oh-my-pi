53a0b5c8cb6a76fbfdd692f0d2eca2b88549f7aa
felipetancredo <felipetancredo@gmail.com>
2017-01-26 17:37:24 -0200

    Fix Marketing_Costs (Added all facebook accounts)

--- jobs/etl/marketing_costs.py
+++ jobs/etl/marketing_costs.py
@@ -29,10 +29,13 @@ def extract_facebook_marketing_campaigns(dt):
     api = get_facebook_api()
     social_account = AdAccount(social)
     supply_account = AdAccount(supply)
+    liq_account = AdAccount(liquidity)
+
     fields = [
         Insights.Field.campaign_name,
         Insights.Field.campaign_id,
-        Insights.Field.spend
+        Insights.Field.spend,
+        Insights.Field.account_name
     ]
 
     insights = []
@@ -42,28 +45,46 @@ def extract_facebook_marketing_campaigns(dt):
             'level': 'campaign',
             'limit': 1000
         }
+
+        liq_campaigns = liq_account.get_insights(fields=fields, params=params)
+        for c in liq_campaigns:
+            if dt < date(2016, 3, 1):  # regra para pegar ads na conta de social somente ate dez/2016
+                try:
+                    if c['campaign_name']:
+                        if c['campaign_name'].startswith('Publica') :
+                            c['account_name'] = 'Social'
+                except:
+                    pass
+            insights.append(c)
+
+        campaigns_supply_in_social_account = social_account.get_insights(fields=fields, params=params)
+        for c in campaigns_supply_in_social_account:
+            if dt <= date(2016, 12, 1):  # regra para pegar ads na conta de social somente ate dez/2016
+                try:
+                    if c['campaign_name']:
+                        if c['campaign_name'].startswith('Trazer pro') or c['campaign_name'].startswith('Lead ads'):
+                            c['account_name'] = 'Supply'
+                        else:
+                            cnv = datetime.strptime(c['campaign_name'][0:8], '%Y%m%d')
+                            if cnv:
+                                c['account_name'] = 'Supply'
+
+                        insights.append(c)
+                except:
+                    pass
+            else:
+                insights.append(c)
+
         supply_campaigns = supply_account.get_insights(fields=fields, params=params)
         for s in supply_campaigns:
             insights.append(s)
 
-        if dt <= date(2016,12,1): # regra para pegar ads na conta de social somente ate dez/2016
-            campaigns_supply_in_social_account = social_account.get_insights(fields=fields, params=params)
-            for c in campaigns_supply_in_social_account:
-                try:
-                    if c['campaign_name']:
-                        if c['campaign_name'].startswith('Trazer pro') or c['campaign_name'].startswith('Lead ads'):
-                            insights.append(c)
-                        else:
-                            cnv = datetime.strptime(c['campaign_name'][0:8], '%Y%m%d')
-                            if cnv:
-                                insights.append(c)
-                except:
-                    pass
         dt = dt + timedelta(days=1)
 
     table = petl.fromdicts(insights)
     table = table.rename('date_start', 'date')
-    table = table.cut('date', 'campaign_id', 'campaign_name', 'spend')
+    table = table.cut('date', 'campaign_id', 'campaign_name', 'spend', 'account_name')
+
     return list(table)
 
 
@@ -133,6 +154,7 @@ if __name__ == '__main__':
                 db_enum=EnumDb.BI_ODS,
                 data_table=facebook_table,
                 table_name='facebook_ads_campaigns',
+                encoding='UTF-8',
                 append=False
             )
         if args[1] == 'google':
