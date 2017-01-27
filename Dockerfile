bee07b3b5df3083f0a621329df731394575a21de
felipetancredo <felipetancredo@gmail.com>
2017-01-27 18:35:37 -0200

    Fix DAGs (force_pull)

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ENV PYTHONPATH $PYTHONPATH:/opt/current-app
 
 ADD . .
 
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 ENTRYPOINT ["python"]
--- jobs/etl/imovel_full_history.py
+++ jobs/etl/imovel_full_history.py
@@ -1,23 +1,23 @@
 from jobs.base.base_etl import BaseETL, EnumDb
 
 ids = BaseETL.from_db_query(
-    db_enum=EnumDb.QuintoAndar_ebdb,
-    query="select distinct id from Imovel_AUD order by 1 desc")[1:]
+    db_enum=EnumDb.BI_ODS,
+    query="select distinct id from imovel_status_history order by 1 desc")[1:]
 
 imoveis = []
 count_ids = 0
 erros = []
 append = False
-conn = BaseETL.get_connection(db_enum=EnumDb.QuintoAndar_ebdb)
+conn = BaseETL.get_connection(db_enum=EnumDb.BI_ODS, encoding='UTF8')
 while ids:
     id = ids[0]
     count_ids += 1
     imovel = BaseETL.from_db_query(
         db_enum=EnumDb.QuintoAndar_ebdb,
-        query="select * from v_ImovelStatusHistory where id  = {};".format(id[0]),
+        query="select * from f_list_imovel_status_full_history ({}) where final_status_date_position;".format(id[0]),
         conn=conn
     )
-    imovel = BaseETL.decode_table(imovel, 'latin-1')
+    # imovel = BaseETL.decode_table(imovel, 'utf-8')
     if len(imoveis) == 0:
         imoveis.extend(map(list, imovel)) # insere com header se for a primeira vez
     elif len(imovel) > 2:
@@ -32,7 +32,7 @@ while ids:
     if count_ids == 2000 or len(ids) == 0:
         BaseETL.bulk_insert(
             table=imoveis,
-            table_name='imovel_status_history',
+            table_name='imovel_status_full_history',
             db_enum=EnumDb.BI_ODS,
             encoding='UTF8',
             append=append,
