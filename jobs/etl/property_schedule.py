71872ffc018bc6fb227d7457b666ee7b27c5634d
felipetancredo <felipetancredo@gmail.com>
2017-02-03 18:01:34 -0200

    Addind ADWORDS_KEY

---
+++ jobs/etl/property_schedule.py
@@ -0,0 +1,34 @@
+from jobs.base.base_etl import BaseETL, EnumDb
+import sys
+from datetime import datetime
+
+
+args = sys.argv
+
+if len(args) > 1:
+    if args[1] == 'ODS':
+
+        print("Start query: {}".format(datetime.now()))
+
+        listODS = BaseETL.from_db_query(
+            db_enum=EnumDb.QuintoAndar_ebdb,
+            query="call ebdb.list_property_schedule();")
+
+        print("To ODS: {}".format(datetime.now()))
+
+        BaseETL.bulk_insert(
+            table=listODS,
+            table_name='property_schedule',
+            db_enum=EnumDb.BI_ODS,
+            append=False,
+            commit=True
+        )
+
+    elif args[1] == 'DW':
+        BaseETL.move_table(
+            table_name='vw_fact_liquidity_scheduled_properties',
+            table_name_dest='fact_liquidity_scheduled_properties',
+            enum_db_source=EnumDb.BI_ODS,
+            enum_db_dest=EnumDb.BI_DW,
+            append=False
+        )
