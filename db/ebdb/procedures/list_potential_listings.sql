e9392a8260e89d74e4870b24cce93d13aa128704
felipetancredo <felipetancredo@gmail.com>
2017-03-23 00:33:07 -0300

    DATABASE OBJECTS

--- db/ebdb/procedures/list_potential_listings.sql
+++ db/ebdb/procedures/list_potential_listings.sql
@@ -151,7 +151,10 @@ BEGIN
           u.tipoAdmin,
           lfu.firstUpdateDate as first_inside_sales_contact_date,
           null as prospect_date,
-          coalesce(cl.dataConversao, cl.criadoEm, from_unixtime(lu.timestamp/1000)) as qualified_date,
+          -- coalesce(cl.dataConversao, cl.criadoEm, from_unixtime(lu.timestamp/1000)) as qualified_date,
+          CASE WHEN cl.leadConvertido_id IS NOT NULL
+            THEN coalesce(cl.dataConversao, cl.criadoEm, from_unixtime(lu.timestamp/1000)) -- if there is a match with the table conversaolead, we can substitute the dataconversao by criadoEm in case dataconversao is missing
+          END as qualified_date,
           coalesce(l.criadoEm, l.anuncioCriadoEm, l.captadoEm) as created_date,
           l.atualizadoEm as updated_date
         FROM
@@ -239,7 +242,7 @@ BEGIN
                    
         LEFT JOIN ConversaoLead cl
           on cl.imovel_id = i.id
-          -- and cl.status = 'Concluido'
+          
         left join Lead l
           on l.id = cl.leadConvertido_id
         LEFT JOIN 
