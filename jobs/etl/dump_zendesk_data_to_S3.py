4003af02baa77164c39a23d1ea16fc95c1f13f03
Ribaldo <ribaldorafael@gmail.com>
2017-03-29 13:03:00 -0300

    Converting datetime to epoch int for zendesk api

--- jobs/etl/dump_zendesk_data_to_S3.py
+++ jobs/etl/dump_zendesk_data_to_S3.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 from cStringIO import StringIO
+from datetime import datetime
 
 import boto3
 from jobs.wrappers.Zendesk.zendesk_api import ZendeskAPI
@@ -10,7 +11,7 @@ from jobs.wrappers.Zendesk.zendesk_api import ZendeskAPI
 class ZendeskDataToS3(object):
     def __init__(self, args):
         self.object_type = args[1]
-        self.start_time = args[2]
+        self.start_time = int(datetime.strptime(args[2], '%Y-%m-%d').strftime('%s'))
         self.s3_bucket = args[3]
         self.s3 = boto3.client('s3')
 
