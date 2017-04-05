041be647791df7d7c22f8fff28bc06edd8e8020a
Ribaldo <ribaldorafael@gmail.com>
2017-04-05 15:52:24 -0300

    Encoding thumbnail_url

--- jobs/etl/load_zendesk_data_to_ods.py
+++ jobs/etl/load_zendesk_data_to_ods.py
@@ -183,7 +183,7 @@ class ZendeskDataToODS(object):
                     BaseETL.coalesce(user_photo['content_type']),
                     BaseETL.coalesce(user_photo['size']),
                     BaseETL.coalesce(user_photo['inline']),
-                    BaseETL.coalesce(thumbnail_url)
+                    BaseETL.coalesce(thumbnail_url.encode('utf-8') if thumbnail_url else None)
                 )
 
                 photo_id = self.__execute_command(command=upsert_user_photo_command, return_value=True)
