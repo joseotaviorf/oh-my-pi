fd539ce9b1d4bddbe29d61d4e3194d0ab0983783
felipetancredo <felipetancredo@gmail.com>
2017-04-05 18:45:37 -0300

    Change table name

--- jobs/etl/amplitude_events.py
+++ jobs/etl/amplitude_events.py
@@ -159,16 +159,16 @@ if __name__ == '__main__':
         )
 
     elif args[1] == 'load_schedule_visit':
-        table_name='amplitude_event_schedule_visit'
+        table_name=''
         # BaseETL.drop_table(db_enum=EnumDb.BI_ODS, table_name=table_name)
         vw = BaseETL.from_db_table(
             db_enum=EnumDb.BI_ODS,
-            table_name='vw_{}'.format(table_name),
+            table_name='vw_amplitude_event_booked_visit',
             server_cursor_postgres=table_name
         )
         BaseETL.bulk_insert(
             table=vw,
-            table_name=table_name,
+            table_name='amplitude.booked_visit',
             db_enum=EnumDb.BI_ODS,
             append=False,
             commit=True
