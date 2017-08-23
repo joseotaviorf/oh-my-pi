1dda6a5f2ee9bd595a0d63a3ca67db41a4527be1
Felipe Tancredo <felipetancredo@gmail.com>
2017-08-23 18:52:09 -0300

    adding CRM files

---
+++ db/2.datalake/queries/crm/list_tasks.sql
@@ -0,0 +1,20 @@
+select
+	_id,  
+  type,
+  datainicio as start_date,
+  realizadaem as performed_date,
+  imovelid as property_id,
+  authorid as author_id,
+  authorname as author_name,
+  assigneeid as assignee_id,
+  assigneename as assignee_name,
+  workgroupid as workgroup_id,
+  workgrouptitle as workgroup_title,
+  destinatarioid as recipient_id,
+  status,
+  descricao as description,
+  titulo as title
+from
+	datalake_raw.crm_tasks
+where
+	extracted_on = cast('{}' as date)
