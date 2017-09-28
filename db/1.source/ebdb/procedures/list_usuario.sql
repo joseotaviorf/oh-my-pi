3ca28cadc5d87fa5c39cafb2347f70f1ae642477
dgolhiardi <dgolhiardi@gmail.com>
2017-09-28 18:53:40 -0300

    fixed startDate for afiliados

--- db/1.source/ebdb/procedures/list_usuario.sql
+++ db/1.source/ebdb/procedures/list_usuario.sql
@@ -66,7 +66,7 @@ BEGIN
     daf.contadorPlanilhaDeLeads as dadosafiliado_contador_planilha_leads,
     daf.contratosFechados as dadosafiliado_contratos_fechados,
     daf.indicacaoShortUrl as dadosafiliado_indicacao_shorturl,
-    daf.inicioAtuacao as dadosafiliado_inicio_atuacao,
+    coalesce(da2.initioAtuacao, daf.inicioAtuacao) as dadosafiliado_inicio_atuacao,
     daf.ultimoCalculoComissaoIndicado as dadosafiliado_ultim_calculo_comissao_indicado,
     daf.verificado+0 as dadosafiliado_verificado,
     daf.tipoAfiliado as dadosafiliado_tipo,
@@ -123,6 +123,12 @@ BEGIN
   left join
     DadosAfiliado daf
     on daf.id = u.dadosAfiliado_id
+  left join
+	(select id, min(REV) as REV from DadosAfiliado_AUD where inicioAtuacao is not null group by id) da1
+	on da1.id = u.dadosAfiliado_id
+  left join
+    DadosAfiliado_AUD da2
+    on da1.id = da2.id and da1.REV = da2.REV
   left join
     DadosGerenteContas gc
     on gc.id = daf.gerenteContas_id
