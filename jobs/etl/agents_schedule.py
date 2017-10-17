8fc036718fd73580f8ede80bf247f0e24ffb7687
Rafael Ribaldo <ribaldorafael@gmail.com>
2017-10-17 13:29:03 -0200

    Update agents_schedule.py

--- jobs/etl/agents_schedule.py
+++ jobs/etl/agents_schedule.py
@@ -4,7 +4,6 @@ from jobs.base.base_etl import BaseETL, EnumDb
 from jobs.wrappers.GoogleDrive.google_drive_api import GoogleDriveApi
 import os
 
-
 bucket_datalake = os.environ['bi-datalake-s3-bucket']
 process_name = BaseETL.get_current_filename()
 
