d739b60181a6cbd457f7f126ab3d0f957b741613
dgolhiardi <dgolhiardi@gmail.com>
2017-10-19 18:47:03 -0200

    added owner mkt costs

---
+++ db/ODS/views/unit_economics/supply/marketing/vw_supply_mkt_owner_campaigns_costs.sql
@@ -0,0 +1,59 @@
+create or replace view vw_supply_mkt_owner_campaigns_costs as
+with google_monthly_supply_costs as (
+	select
+		date_part('month', "day"::date) as "month",
+		date_part('year', "day"::date) as "year",
+		sum((cost::DECIMAL(14,2)/1000000)::DECIMAL(14,2)) as cost
+	from
+		google_ads_campaigns
+	where
+		(
+			campaign like '%proprietarios%' or
+			campaign like '%lp_quanto_cobrar%'
+		)
+	group by
+		date_part('month', "day"::date),
+		date_part('year', "day"::date)
+),
+facebook_monthly_supply_costs as
+(
+	select
+		date_part('month', "date"::date) as "month",
+		date_part('year', "date"::date) as "year",
+		sum(spend::DECIMAL(14,2)) as cost
+	from
+		facebook_ads_campaigns
+	where
+		account_name = 'Supply'
+	group by
+		account_name,
+		date_part('month', "date"::date),
+		date_part('year', "date"::date)
+),
+supply_mkt_costs as
+(
+	select
+		coalesce(g."month", f."month") as month,
+		coalesce(g."year", f."year") as year,
+		(coalesce(g.cost, 0) + coalesce(f.cost, 0)) as cost
+	from
+		google_monthly_supply_costs g
+	full outer join
+		facebook_monthly_supply_costs f
+	on g."year" = f."year" and f."month" = g."month"
+)
+select
+sk_property,
+imovel_id as property_id,
+publication_date as sk_cash_flow_date,
+(coalesce(mkt.cost, 0)/count(1) over (
+	partition by
+	date_part('year', publication_date),
+	date_part('month', publication_date)
+))::decimal(14,4) as vl_owner_campaigns
+from
+	vw_base_property_costs base
+left join
+	supply_mkt_costs mkt
+	on date_part('year', publication_date) = mkt.year
+	and date_part('month', publication_date) = mkt.month
