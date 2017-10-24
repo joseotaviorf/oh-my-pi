20e63b8010d414b338ccb44891b15faec875b390
Rafael Ribaldo <ribaldorafael@gmail.com>
2017-10-24 19:45:19 -0200

    Update dim_property.sql

--- db/3.dw/public/DDL/dim_property.sql
+++ db/3.dw/public/DDL/dim_property.sql
@@ -165,9 +165,7 @@ CREATE TABLE public.dim_property (
   load_timestamp TIMESTAMP WITHOUT TIME ZONE,
   usuario_que_cadastrou_id INTEGER,
 
-  property_v3 INTEGER,
-  
-  lead_type VARCHAR(30)
+  property_v3 INTEGER
   
   CONSTRAINT dim_property_pkey PRIMARY KEY(sk_property)
 ) WITH OIDS;
