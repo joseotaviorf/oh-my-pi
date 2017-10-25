24f3c9b26aed39794884ba1e338bd54f3ea399d6
Ribaldo <ribaldorafael@gmail.com>
2017-10-25 11:31:28 -0200

    Refactoring supply ops costs

--- db/ODS/views/unit_economics/supply/ops/vw_supply_ops_costs.sql
+++ db/ODS/views/unit_economics/supply/ops/vw_supply_ops_costs.sql
@@ -1,13 +1,29 @@
 drop view if exists vw_supply_ops_costs;
 create or replace view vw_supply_ops_costs as
 select
-  coalesce(vsopc.sk_property, vsoisc.sk_property) as sk_property,
-  coalesce(vsopc.property_id, vsoisc.sk_property) as property_id,
-  coalesce(vsopc.dt_cash_flow, vsoisc.dt_cash_flow) as dt_cash_flow,
-  coalesce(vsopc.vl_photos, 0) as vl_photos,
-  coalesce(vsoisc.vl_inside_sales, 0) as vl_inside_sales
-from vw_supply_ops_photos_costs vsopc
-full outer join vw_supply_ops_inside_sales_costs vsoisc
-  on vsoisc.sk_property = vsopc.sk_property
-     and vsoisc.dt_cash_flow = vsopc.dt_cash_flow
+  sk_property,
+  property_id,
+  dt_cash_flow,
+  sum(vl_photos) as vl_photos,
+  sum(vl_inside_sales) as vl_inside_sales
+from (
+  select
+    sk_property,
+    property_id,
+    dt_cash_flow,
+    vl_photos,
+    0 as vl_inside_sales
+  from vw_supply_ops_photos_costs
+
+  union all
+
+  select
+    sk_property,
+    property_id,
+    dt_cash_flow,
+    0 as vl_photos,
+    vl_inside_sales
+  from vw_supply_ops_inside_sales_costs
+) supply_ops
+group by sk_property, property_id, dt_cash_flow
 ;
