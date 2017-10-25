96876d57c2f0f263dbf25b209fbdbd3c32698ae8
Ribaldo <ribaldorafael@gmail.com>
2017-10-25 10:45:21 -0200

    Adding ops to supply costs

--- db/ODS/views/unit_economics/supply/vw_supply_costs.sql
+++ db/ODS/views/unit_economics/supply/vw_supply_costs.sql
@@ -6,10 +6,16 @@ drop view vw_supply_costs;
 ---
 create or replace view vw_supply_costs as
 select
-	sk_property,
-	property_id,
-	dt_cash_flow,
-	vl_affiliate_campaigns,
-	vl_owner_campaigns
+	coalesce(vsmc.sk_property, vsoc.sk_property) as sk_property,
+	coalesce(vsmc.property_id, vsoc.property_id) as property_id,
+	coalesce(vsmc.dt_cash_flow, vsoc.dt_cash_flow) as dt_cash_flow,
+	coalesce(vsmc.vl_affiliate_campaigns, 0) as vl_affiliate_campaigns,
+	coalesce(vsmc.vl_owner_campaigns, 0) as vl_owner_campaigns,
+	coalesce(vsoc.vl_photos, 0) as vl_photos,
+	coalesce(vsoc.vl_inside_sales, 0) as vl_inside_sales
 from
-	vw_supply_mkt_costs
+	vw_supply_mkt_costs vsmc
+full outer join vw_supply_ops_costs vsoc
+  on vsmc.sk_property = vsoc.sk_property
+     and vsmc.dt_cash_flow = vsoc.dt_cash_flow
+;
