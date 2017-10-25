bb125baa2ea265ba305a1d2b7945b80e310e9d80
Ribaldo <ribaldorafael@gmail.com>
2017-10-25 10:26:13 -0200

    Renaming inside sales view column

--- db/ODS/views/unit_economics/supply/ops/vw_supply_ops_inside_sales_costs.sql
+++ db/ODS/views/unit_economics/supply/ops/vw_supply_ops_inside_sales_costs.sql
@@ -21,7 +21,7 @@ costs as (
       fp.property_id,
       fp.listing_date,
       cis.dre_date as dt_cash_flow,
-      cis."value" / (count(fp.property_id) over (partition by cis.dre_date))::double precision as vl_photos
+      cis."value" / (count(fp.property_id) over (partition by cis.dre_date))::double precision as vl_inside_sales
     from filtered_properties fp
     join cdre_inside_sales cis
       on cis.dre_date = date_trunc('month', fp.listing_date)
@@ -30,7 +30,7 @@ select
   vbpc.sk_property,
   c.property_id,
   c.dt_cash_flow,
-  c.vl_photos
+  c.vl_inside_sales
 from costs c
 join vw_base_property_costs vbpc
   on vbpc.property_id = c.property_id
