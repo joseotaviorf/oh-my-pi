38ca0b9eea82fa1116faaf5f806887d1cd6e72d0
Ribaldo <ribaldorafael@gmail.com>
2017-10-27 18:39:00 -0200

    Fixing missing data

--- db/ODS/views/unit_economics/liquidity/vw_liquidity_costs.sql
+++ db/ODS/views/unit_economics/liquidity/vw_liquidity_costs.sql
@@ -6,9 +6,9 @@ drop view vw_liquidity_costs;
 ---
 create or replace view vw_liquidity_costs as
 select
-  coalesce(mkt.sk_property, ops.sk_property) as sk_property,
-  coalesce(mkt.property_id, ops.property_id) as property_id,
-  coalesce(mkt.dt_cash_flow, ops.dt_cash_flow) as dt_cash_flow,
+  coalesce(mkt.sk_property, ops.sk_property, agent_hours.sk_property) as sk_property,
+  coalesce(mkt.property_id, ops.property_id, agent_hours.property_id) as property_id,
+  coalesce(mkt.dt_cash_flow, ops.dt_cash_flow, agent_hours.dt_cash_flow) as dt_cash_flow,
   coalesce(mkt.vl_tenant_campaigns, 0)::decimal(14,4) as vl_tenant_campaigns,
   coalesce(ops.vl_bo_pre_sale, 0) as vl_bo_pre_sale,
   coalesce(ops.vl_cs_pre_sale, 0) as vl_cs_pre_sale,
