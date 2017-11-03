168e3b996176e70288c1d23cff21a2da65f5773a
Ribaldo <ribaldorafael@gmail.com>
2017-11-03 17:39:23 -0200

    Adding contract to fact_property_economics

--- db/ODS/views/unit_economics/vw_fact_property_economics.sql
+++ db/ODS/views/unit_economics/vw_fact_property_economics.sql
@@ -1,169 +1,228 @@
---drop view if exists vw_fact_property_economics;
+drop view if exists vw_fact_property_economics;
 ---
 --- Returns the final view for Unit Economics
 --- Cost: All costs grouped by versioned property / cash flow date
 --- Cash Flow Date: Date of Payment
 --- Placeholders with random int will be kept while developing the remainder values
 ---
+
 create or replace view vw_fact_property_economics as
+with unit_economics as (
+    select
+        sk_property,
+        property_id,
+        sk_cash_flow_date,
+        dt_cash_flow,
+        -sum(vl_owner_campaigns) as vl_owner_campaigns,
+        -sum(vl_affiliate_campaigns) as vl_affiliate_campaigns,
+        sum(vl_inside_sales) as vl_inside_sales,
+        sum(vl_photos) as vl_photos,
+        -sum(vl_affiliate_bonus) as vl_affiliate_bonus,
+        -sum(vl_lockbox) as vl_lockbox,
+        -sum(vl_tenant_campaigns) as vl_tenant_campaigns,
+        sum(vl_cs_pre_sale) as vl_cs_pre_sale,
+        sum(vl_field_ops) as vl_field_ops,
+        sum(vl_bo_pre_sale) as vl_bo_pre_sale,
+        sum(vl_agent_hours) as vl_agent_hours,
+        -sum(vl_st_pis_cofins) as vl_st_pis_cofins,
+        -sum(vl_st_iss) as vl_st_iss,
+        -sum(vl_affiliate_commission) as vl_affiliate_commission,
+        -sum(vl_agent_commission) as vl_agent_commission,
+        sum(vl_delay_fine) as vl_delay_fine,
+        0 as vl_termination_fine,
+        sum(vl_brokerage_fee) as vl_brokerage_fee,
+        sum(vl_management_fee) as vl_management_fee,
+        sum(vl_cs_post_sale) as vl_cs_post_sale,
+        sum(vl_collection) as vl_collection,
+        sum(vl_bo_onboarding) as vl_bo_onboarding,
+        sum(vl_bo_ongoing) as vl_bo_ongoing,
+        sum(vl_bo_offboarding) as vl_bo_offboarding,
+        -sum(vl_insurance_fee) as vl_insurance_fee
+    from
+    (
+        select
+            sk_property,
+            property_id,
+            coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
+            dt_cash_flow,
+            0 as vl_owner_campaigns,
+            0 as vl_affiliate_campaigns,
+            0 as vl_inside_sales,
+            0 as vl_photos,
+            0 as vl_affiliate_bonus,
+            vl_lockbox as vl_lockbox,
+            vl_tenant_campaigns,
+            vl_cs_pre_sale as vl_cs_pre_sale,
+            vl_field_ops as vl_field_ops,
+            vl_bo_pre_sale as vl_bo_pre_sale,
+            vl_agent_hours as vl_agent_hours,
+            0 as vl_st_pis_cofins,
+            0 as vl_st_iss,
+            0 as vl_affiliate_commission,
+            0 as vl_agent_commission,
+            0 as vl_delay_fine,
+            0 as vl_termination_fine,
+            0 as vl_brokerage_fee,
+            0 as vl_management_fee,
+            0 as vl_cs_post_sale,
+            0 as vl_collection,
+            0 as vl_bo_onboarding,
+            0 as vl_bo_ongoing,
+            0 as vl_bo_offboarding,
+            0 as vl_insurance_fee
+        from
+            vw_liquidity_costs
+        union all
+        select
+            sk_property,
+            property_id,
+            coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
+            dt_cash_flow,
+            vl_owner_campaigns,
+            vl_affiliate_campaigns,
+            vl_inside_sales as vl_inside_sales,
+            vl_photos as vl_photos,
+            vl_affiliate_bonus,
+            0 as vl_lockbox,
+            0 as vl_tenant_campaigns,
+            0 as vl_cs_pre_sale,
+            0 as vl_field_ops,
+            0 as vl_bo_pre_sale,
+            0 as vl_agent_hours,
+            0 as vl_st_pis_cofins,
+            0 as vl_st_iss,
+            0 as vl_affiliate_commission,
+            0 as vl_agent_commission,
+            0 as vl_delay_fine,
+            0 as vl_termination_fine,
+            0 as vl_brokerage_fee,
+            0 as vl_management_fee,
+            0 as vl_cs_post_sale,
+            0 as vl_collection,
+            0 as vl_bo_onboarding,
+            0 as vl_bo_ongoing,
+            0 as vl_bo_offboarding,
+            0 as vl_insurance_fee
+        from
+            vw_supply_costs
+        union all
+        select
+            sk_property,
+            property_id,
+            coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
+            dt_cash_flow,
+            0 as vl_owner_campaigns,
+            0 as vl_affiliate_campaigns,
+            0 as vl_inside_sales,
+            0 as vl_photos,
+            0 as vl_affiliate_bonus,
+            0 as vl_lockbox,
+            0 as vl_tenant_campaigns,
+            0 as vl_cs_pre_sale,
+            0 as vl_field_ops,
+            0 as vl_bo_pre_sale,
+            0 as vl_agent_hours,
+            0 as vl_st_pis_cofins,
+            0 as vl_st_iss,
+            0 as vl_affiliate_commission,
+            0 as vl_agent_commission,
+            0 as vl_delay_fine,
+            0 as vl_termination_fine,
+            0 as vl_brokerage_fee,
+            0 as vl_management_fee,
+            vl_cs_post_sale as vl_cs_post_sale,
+            vl_collection as vl_collection,
+            vl_bo_onboarding as vl_bo_onboarding,
+            vl_bo_ongoing as vl_bo_ongoing,
+            vl_bo_offboarding as vl_bo_offboarding,
+            vl_insurance_fee as vl_insurance_fee
+        from
+            vw_mgmt_costs
+        union all
+        select
+            sk_property,
+            property_id,
+            coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
+            dt_cash_flow,
+            0 as vl_owner_campaigns,
+            0 as vl_affiliate_campaigns,
+            0 as vl_inside_sales,
+            0 as vl_photos,
+            0 as vl_affiliate_bonus,
+            0 as vl_lockbox,
+            0 as vl_tenant_campaigns,
+            0 as vl_cs_pre_sale,
+            0 as vl_field_ops,
+            0 as vl_bo_pre_sale,
+            0 as vl_agent_hours,
+            vl_st_pis_cofins as vl_st_pis_cofins,
+            vl_st_iss as vl_st_iss,
+            vl_affiliate_commission as vl_affiliate_commission,
+            vl_agent_commission as vl_agent_commission,
+            vl_delay_fine as vl_delay_fine,
+            0 as vl_termination_fine,
+            vl_brokerage_fee as vl_brokerage_fee,
+            vl_management_fee as vl_management_fee,
+            0 as vl_cs_post_sale,
+            0 as vl_collection,
+            0 as vl_bo_onboarding,
+            0 as vl_bo_ongoing,
+            0 as vl_bo_offboarding,
+            0 as vl_insurance_fee
+        from
+            vw_net_revenue_costs
+    ) tbl
+    group by sk_property, property_id, sk_cash_flow_date, dt_cash_flow
+),
+-- change to fact_liquidity when versioning is fixed
+contracts as (
+	select
+		id as sk_contract,
+		imovel_id as property_id,
+		"dataAssinado" as start_date,
+		case
+			when coalesce("dataRescisao", "dataFimContratoPrevisto")::date > now()::date
+				then now()::date
+			else coalesce("dataRescisao", "dataFimContratoPrevisto")::date
+		end as end_date
+	from contract
+	where tipo = 'FullService'
+	  and "dataAssinado" is not null
+	  and ("dataRescisao" is not null or "dataFimContratoPrevisto" is not null)
+)
 select
-	sk_property,
-	property_id,
-	sk_cash_flow_date,
-	-sum(vl_owner_campaigns) as vl_owner_campaigns,
-	-sum(vl_affiliate_campaigns) as vl_affiliate_campaigns,
-	sum(vl_inside_sales) as vl_inside_sales,
-	sum(vl_photos) as vl_photos,
-	-sum(vl_affiliate_bonus) as vl_affiliate_bonus,
-	-sum(vl_lockbox) as vl_lockbox,
-	-sum(vl_tenant_campaigns) as vl_tenant_campaigns,
-	sum(vl_cs_pre_sale) as vl_cs_pre_sale,
-	sum(vl_field_ops) as vl_field_ops,
-	sum(vl_bo_pre_sale) as vl_bo_pre_sale,
-	sum(vl_agent_hours) as vl_agent_hours,
-	-sum(vl_st_pis_cofins) as vl_st_pis_cofins,
-	-sum(vl_st_iss) as vl_st_iss,
-	-sum(vl_affiliate_commission) as vl_affiliate_commission,
-	-sum(vl_agent_commission) as vl_agent_commission,
-	sum(vl_delay_fine) as vl_delay_fine,
-	0 as vl_termination_fine,
-	sum(vl_brokerage_fee) as vl_brokerage_fee,
-	sum(vl_management_fee) as vl_management_fee,
-	sum(vl_cs_post_sale) as vl_cs_post_sale,
-	sum(vl_collection) as vl_collection,
-	sum(vl_bo_onboarding) as vl_bo_onboarding,
-	sum(vl_bo_ongoing) as vl_bo_ongoing,
-	sum(vl_bo_offboarding) as vl_bo_offboarding,
-	-sum(vl_insurance_fee) as vl_insurance_fee
-from
-(
-	select
-		sk_property,
-		property_id,
-		coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
-		0 as vl_owner_campaigns,
-		0 as vl_affiliate_campaigns,
-		0 as vl_inside_sales,
-		0 as vl_photos,
-		0 as vl_affiliate_bonus,
-		vl_lockbox as vl_lockbox,
-		vl_tenant_campaigns,
-		vl_cs_pre_sale as vl_cs_pre_sale,
-		vl_field_ops as vl_field_ops,
-		vl_bo_pre_sale as vl_bo_pre_sale,
-		vl_agent_hours as vl_agent_hours,
-		0 as vl_st_pis_cofins,
-		0 as vl_st_iss,
-		0 as vl_affiliate_commission,
-		0 as vl_agent_commission,
-		0 as vl_delay_fine,
-		0 as vl_termination_fine,
-		0 as vl_brokerage_fee,
-		0 as vl_management_fee,
-		0 as vl_cs_post_sale,
-		0 as vl_collection,
-		0 as vl_bo_onboarding,
-		0 as vl_bo_ongoing,
-		0 as vl_bo_offboarding,
-		0 as vl_insurance_fee
-	from
-		vw_liquidity_costs
-	union all
-	select
-		sk_property,
-		property_id,
-		coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
-		vl_owner_campaigns,
-		vl_affiliate_campaigns,
-		vl_inside_sales as vl_inside_sales,
-		vl_photos as vl_photos,
-		vl_affiliate_bonus,
-		0 as vl_lockbox,
-		0 as vl_tenant_campaigns,
-		0 as vl_cs_pre_sale,
-		0 as vl_field_ops,
-		0 as vl_bo_pre_sale,
-		0 as vl_agent_hours,
-		0 as vl_st_pis_cofins,
-		0 as vl_st_iss,
-		0 as vl_affiliate_commission,
-		0 as vl_agent_commission,
-		0 as vl_delay_fine,
-		0 as vl_termination_fine,
-		0 as vl_brokerage_fee,
-		0 as vl_management_fee,
-		0 as vl_cs_post_sale,
-		0 as vl_collection,
-		0 as vl_bo_onboarding,
-		0 as vl_bo_ongoing,
-		0 as vl_bo_offboarding,
-		0 as vl_insurance_fee
-	from
-		vw_supply_costs
-	union all
-	select
-		sk_property,
-		property_id,
-		coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
-		0 as vl_owner_campaigns,
-		0 as vl_affiliate_campaigns,
-		0 as vl_inside_sales,
-		0 as vl_photos,
-		0 as vl_affiliate_bonus,
-		0 as vl_lockbox,
-		0 as vl_tenant_campaigns,
-		0 as vl_cs_pre_sale,
-		0 as vl_field_ops,
-		0 as vl_bo_pre_sale,
-		0 as vl_agent_hours,
-		0 as vl_st_pis_cofins,
-		0 as vl_st_iss,
-		0 as vl_affiliate_commission,
-		0 as vl_agent_commission,
-		0 as vl_delay_fine,
-		0 as vl_termination_fine,
-		0 as vl_brokerage_fee,
-		0 as vl_management_fee,
-		vl_cs_post_sale as vl_cs_post_sale,
-		vl_collection as vl_collection,
-		vl_bo_onboarding as vl_bo_onboarding,
-		vl_bo_ongoing as vl_bo_ongoing,
-		vl_bo_offboarding as vl_bo_offboarding,
-		vl_insurance_fee as vl_insurance_fee
-	from
-		vw_mgmt_costs
-	union all
-	select
-		sk_property,
-		property_id,
-		coalesce(replace(dt_cash_flow::varchar, '-', '')::integer, -1) as sk_cash_flow_date,
-		0 as vl_owner_campaigns,
-		0 as vl_affiliate_campaigns,
-		0 as vl_inside_sales,
-		0 as vl_photos,
-		0 as vl_affiliate_bonus,
-		0 as vl_lockbox,
-		0 as vl_tenant_campaigns,
-		0 as vl_cs_pre_sale,
-		0 as vl_field_ops,
-		0 as vl_bo_pre_sale,
-		0 as vl_agent_hours,
-		vl_st_pis_cofins as vl_st_pis_cofins,
-		vl_st_iss as vl_st_iss,
-		vl_affiliate_commission as vl_affiliate_commission,
-		vl_agent_commission as vl_agent_commission,
-		vl_delay_fine as vl_delay_fine,
-		0 as vl_termination_fine,
-		vl_brokerage_fee as vl_brokerage_fee,
-		vl_management_fee as vl_management_fee,
-		0 as vl_cs_post_sale,
-		0 as vl_collection,
-		0 as vl_bo_onboarding,
-		0 as vl_bo_ongoing,
-		0 as vl_bo_offboarding,
-		0 as vl_insurance_fee
-	from
-		vw_net_revenue_costs
-) tbl
-group by sk_property, property_id, sk_cash_flow_date
+  ue.sk_property,
+  ue.property_id,
+  coalesce(c.sk_contract, -1) as sk_contract,
+  ue.sk_cash_flow_date,
+  ue.vl_owner_campaigns,
+  ue.vl_affiliate_campaigns,
+  ue.vl_inside_sales,
+  ue.vl_photos,
+  ue.vl_affiliate_bonus,
+  ue.vl_lockbox,
+  ue.vl_tenant_campaigns,
+  ue.vl_cs_pre_sale,
+  ue.vl_field_ops,
+  ue.vl_bo_pre_sale,
+  ue.vl_agent_hours,
+  ue.vl_st_pis_cofins,
+  ue.vl_st_iss,
+  ue.vl_affiliate_commission,
+  ue.vl_agent_commission,
+  ue.vl_delay_fine,
+  ue.vl_termination_fine,
+  ue.vl_brokerage_fee,
+  ue.vl_management_fee,
+  ue.vl_cs_post_sale,
+  ue.vl_collection,
+  ue.vl_bo_onboarding,
+  ue.vl_bo_ongoing,
+  ue.vl_bo_offboarding,
+  ue.vl_insurance_fee
+from unit_economics ue
+left join contracts c
+  on ue.property_id = c.property_id
+     and ue.dt_cash_flow between c.start_date and c.end_date
 ;
+
