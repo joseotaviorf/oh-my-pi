36229d06e2573a382c11567df5e6a2cf35beaddf
Ribaldo <ribaldorafael@gmail.com>
2017-11-08 18:51:09 -0200

    Fixing property status over period (multiple status)

--- db/ODS/views/vw_property_status_over_period.sql
+++ db/ODS/views/vw_property_status_over_period.sql
@@ -1,28 +1,38 @@
 drop view if exists vw_property_status_over_period;
-
-CREATE VIEW public.vw_property_status_over_period 
-as
-select 
-	 ((p.id || '00') || COALESCE(p.version, 1))::bigint AS sk_property,
-	 p.id,
-	 p.publication_date::date as pub_date,
-	 p."version",
-	 (i.date - p.publication_date::date)::varchar || ' days' as days,
-	 i.date as date,
-	 i.status_history as status
-from 
-	property_listing  p
-inner join
-	imovel_status_full_history i
-	on i.id = p.id
-	and i.date in (
-		p.publication_date::date + interval '7' day, 
-		p.publication_date::date + interval '14' day, 
-		p.publication_date::date + interval '21' day,
-		p.publication_date::date + interval '28' day,
-		p.publication_date::date + interval '35' day,
-		p.publication_date::date + interval '42' day,
-		p.publication_date::date + interval '49' day,
-		p.publication_date::date + interval '56' day
-	)
+create or replace view public.vw_property_status_over_period_ribs as
+with dup as (
+  select
+    ((p.id || '00') || COALESCE(p.version, 1))::bigint AS sk_property,
+    p.id,
+    p.min_version_time::date as pub_date,
+    p."version",
+    (i.date - p.min_version_time::date)::varchar || ' days' as days,
+    max(i.status_time) over (partition by p.id, (i.date - p.min_version_time::date)) as max_t,
+    i.status_time,
+    i.date as date,
+    i.status_history as status
+  from vw_property_listing_ribs  p
+  join imovel_status_full_history i
+    on i.id = p.id
+       and i.date in (
+            p.min_version_time::date + interval '7' day,
+            p.min_version_time::date + interval '14' day,
+            p.min_version_time::date + interval '21' day,
+            p.min_version_time::date + interval '28' day,
+            p.min_version_time::date + interval '35' day,
+            p.min_version_time::date + interval '42' day,
+            p.min_version_time::date + interval '49' day,
+            p.min_version_time::date + interval '56' day
+       )
+)
+select
+  sk_property,
+  id,
+  pub_date,
+  "version",
+  days,
+  "date",
+  status
+from dup
+where max_t = status_time
 ;
