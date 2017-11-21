b8be1dc8a213383a7945bf7d61c866fe7ebb07cf
Ribaldo <ribaldorafael@gmail.com>
2017-11-21 19:37:25 -0200

    Using pandas instead of petl and fixing base_etl-save_to_s3

--- jobs/etl/s3_files_to_ods.py
+++ jobs/etl/s3_files_to_ods.py
@@ -1,9 +1,10 @@
+import logging as log
+import os
 import sys
+
+import pandas as pd
 from jobs.base.base_etl import BaseETL, EnumDb
 from jobs.wrappers.S3.S3_file_reader import S3FileReader
-import os
-import logging as log
-
 
 if __name__ == '__main__':
     args = sys.argv
@@ -13,11 +14,12 @@ if __name__ == '__main__':
     s3 = S3FileReader()
     files = s3.get_files_from_bucket('bi-etl-ejuice-xls2ods')
     schema = 'files'
-    # files = list(files)
     for f in files:
         try:
-            table = s3.get_tables_from_files(f[0])
-            table_name=f[1]
+            table = pd.read_excel(f[0], skiprows=1)
+            table = table.where(pd.notnull(table), None)
+
+            table_name = f[1]
 
             exists = BaseETL.table_exists(db_enum=EnumDb.BI_ODS, table_name=table_name, schema=schema)
             if not exists:
@@ -29,12 +31,12 @@ if __name__ == '__main__':
                     sample=100000
                 )
 
-            BaseETL.bulk_insert(
-                db_enum=EnumDb.BI_ODS,
-                table=table,
+            BaseETL.dataframe_to_ods(
+                df=table,
                 table_name='{}."{}"'.format(schema, table_name),
                 append=False,
-                encoding='utf-8')
+                encoding='utf-8'
+            )
 
             BaseETL.to_s3(
                 filename=table_name,
