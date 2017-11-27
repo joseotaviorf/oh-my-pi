a695c509bc84e6fac38a6327500a7ecd5630c75b
dgolhiardi <dgolhiardi@gmail.com>
2017-11-27 15:41:59 -0200

    1 year cut off - cs pre sale

--- db/ODS/views/unit_economics/replace_all.sql
+++ db/ODS/views/unit_economics/replace_all.sql
@@ -1506,11 +1506,18 @@ with cdre_cs_pre_sale as (
 ),
 filtered_properties as (
     select distinct
-      sk_property,
-      property_id,
-      publication_date::date,
-      min_version_time::date,
-      max_version_time::date
+        sk_property,
+        property_id,
+        publication_date::date,
+        min_version_time::date,
+		case
+			when min_version_time + interval '1 year' >= max_version_time
+				then max_version_time
+			when min_version_time + interval '1 year' >= now()
+				then now()
+			else
+				min_version_time + interval '1 year'
+		end as max_liquidity_date
     from vw_base_property_costs
     where status = 'publicado'
 ),
@@ -1523,7 +1530,7 @@ costs as (
     from filtered_properties fp
     join cdre_cs_pre_sale cps
       on cps.dre_date between date_trunc('month', fp.min_version_time) + interval '1 month'
-         and date_trunc('month', fp.max_version_time) + interval '1 month'
+         and date_trunc('month', fp.max_liquidity_date) + interval '1 month'
 )
 select
   vbpc.sk_property,
