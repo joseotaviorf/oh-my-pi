f368c4f85b424c6150547c08ad23e5ba383eca62
Ribaldo <ribaldorafael@gmail.com>
2017-12-19 14:11:30 -0200

    Updating cost avg for ongoing (ue)

--- db/ODS/views/unit_economics/replace_all.sql
+++ db/ODS/views/unit_economics/replace_all.sql
@@ -1584,12 +1584,11 @@ create or replace view unit_economics.vw_mgmt_ops_bo_ongoing_costs as
 with cdre_ongoing as (
     select
       dre_value,
-      dre_date,
-      0 as flg_expected_bo_ongoing
+      dre_date
     from unit_economics.vw_base_dre_costs
     where dre_category = 'Back-Office (ongoing)'
 ),
-filtered_contracts as (
+filtered_contracts_prev as (
     select distinct
       property_id,
       init_date as start_date,
@@ -1599,54 +1598,82 @@ filtered_contracts as (
       and (termination_date is not null
             or expected_end_date is not null)
 ),
-series as (
-	select
-		null::double precision as dre_value,
-		generate_series((max(dre.dre_date) + interval '1 month')::date,
-		    (max(dre.dre_date) + interval '60 month')::date, interval '1 month') as dre_date,
-		1 as flg_expected_bo_ongoing
-	from cdre_ongoing dre
-
-	union
-
-	select
-	    dre_value,
-	    dre_date,
-	    flg_expected_bo_ongoing
-	from cdre_ongoing
-),
-cdre_ongoing_fc as (
-    select
-        gap_fill(dre_value) over (order by dre_date) as dre_value,
-        dre_date,
-        coalesce(flg_expected_bo_ongoing, 1) as flg_expected_bo_ongoing
-    from series
-),
-costs as (
-    select
+filtered_contract as (
+    select distinct
       fc.property_id,
       fc.start_date,
       fc.end_date,
-      co.dre_date::date as dt_cash_flow,
-      co.dre_value / (count(fc.property_id) over (partition by co.dre_date))::double precision as vl_bo_ongoing,
-      flg_expected_bo_ongoing
-    from filtered_contracts fc
-    join cdre_ongoing_fc co
-      on co.dre_date between date_trunc('month', fc.start_date)  + interval '1 month'
+      date_trunc('month', dd."date") as dt_cash_flow
+    from dim_date dd
+    join filtered_contracts_prev fc
+        on date_trunc('month', dd."date") between date_trunc('month', fc.start_date)  + interval '1 month'
                         and date_trunc('month', fc.end_date) + interval '1 month'
+),
+costs as (
+    select distinct
+      fc.property_id,
+      fc.start_date,
+      fc.end_date,
+      fc.dt_cash_flow,
+      co.dre_value,
+      (count(fc.property_id) over (partition by fc.dt_cash_flow)),
+      co.dre_value / (count(fc.property_id) over (partition by fc.dt_cash_flow))::double precision as vl_bo_ongoing,
+      (dre_value is null)::int as flg_expected_bo_ongoing
+    from filtered_contract fc
+    left join cdre_ongoing co
+      on co.dre_date = fc.dt_cash_flow
+),
+result as (
+    select
+      coalesce(vbpc.sk_property, (c.property_id || '001')::bigint) as sk_property,
+      c.property_id,
+      c.dt_cash_flow,
+      c.vl_bo_ongoing,
+      flg_expected_bo_ongoing
+    from costs c
+    left join unit_economics.vw_base_property_costs vbpc
+      on vbpc.property_id = c.property_id
+        and c.start_date >= vbpc.min_version_time
+        and c.end_date <= vbpc.max_version_time
+),
+m_avg_prev as (
+    select distinct
+        dt_cash_flow,
+        avg(vl_bo_ongoing) over (partition by dt_cash_flow order by dt_cash_flow) as _avg1
+    from result
+    order by dt_cash_flow asc
+),
+last_value as (
+    select
+        dt_cash_flow,
+        case
+            when _avg1 is null
+                and avg(_avg1) over (rows between 3 preceding and 1 preceding) is not null
+                and lag(_avg1) over () is not null
+                and lead(_avg1) over () is null
+              then avg(_avg1) over (rows between 3 preceding and 1 preceding)
+            else _avg1
+        end as m_avg
+    from m_avg_prev
+),
+last_value_gap_fill as (
+    select
+        dt_cash_flow,
+        coalesce(m_avg, gap_fill(m_avg) over ()) as new_value
+    from last_value
 )
 select
-  coalesce(vbpc.sk_property, (c.property_id || '001')::bigint) as sk_property,
-  c.property_id,
-  c.dt_cash_flow,
-  c.vl_bo_ongoing,
-  flg_expected_bo_ongoing
-from costs c
-left join unit_economics.vw_base_property_costs vbpc
-  on vbpc.property_id = c.property_id
-    and c.start_date >= vbpc.min_version_time
-    and c.end_date <= vbpc.max_version_time
+    r.sk_property,
+    r.property_id,
+    r.dt_cash_flow,
+    r.vl_bo_ongoing as old_vl_bo_ongoing,
+    r.flg_expected_bo_ongoing,
+    lv.new_value as vl_bo_ongoing
+from result r
+join last_value_gap_fill lv
+    on r.dt_cash_flow = lv.dt_cash_flow
 ;
+
 
 create or replace view unit_economics.vw_mgmt_ops_collection_costs as
 with cdre_collection as (
