20c987fc66c380a57f26dce5c39aa581044e4d36
Ribaldo <ribaldorafael@gmail.com>
2017-12-19 08:50:40 -0200

    Casting timestamp to date (insurance_pis_cofins - ue)

--- db/ODS/views/unit_economics/replace_all.sql
+++ db/ODS/views/unit_economics/replace_all.sql
@@ -2105,7 +2105,7 @@ select
 	property_id,
 	contract_id,
 	-(vl_insurance_fee * 0.0925) as vl_st_pis_cofins,
-	date_trunc('month', dt_cash_flow) + interval '19 day' as dt_cash_flow,
+	(date_trunc('month', dt_cash_flow) + interval '19 day')::date as dt_cash_flow,
 	flg_expected
 from
 	unit_economics.vw_mgmt_insurance_fee
