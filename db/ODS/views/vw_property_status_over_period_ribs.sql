a5eb33e8bb6fbee5177f8138a9301965221dd6cd
Ribaldo <ribaldorafael@gmail.com>
2018-01-02 14:23:11 -0200

    Adding new property_status_over_period

--- db/ODS/views/vw_property_status_over_period_ribs.sql
+++ db/ODS/views/vw_property_status_over_period_ribs.sql
@@ -1,5 +1,5 @@
-drop view if exists vw_property_status_over_period;
-create or replace view public.vw_property_status_over_period as
+drop view if exists vw_property_status_over_period_ribs;
+create or replace view public.vw_property_status_over_period_ribs as
 with dup as (
   select
     ((p.id || '00') || COALESCE(p.version, 1))::bigint AS sk_property,
@@ -11,7 +11,7 @@ with dup as (
     i.status_time,
     i.date as date,
     i.status_history as status
-  from vw_property_listing  p
+  from vw_property_listing_ribs  p
   join imovel_status_full_history i
     on i.id = p.id
        and i.date in (
