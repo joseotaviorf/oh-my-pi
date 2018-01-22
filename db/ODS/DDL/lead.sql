c594087b2b274c21d9fd2f1dcfe488569e7e0ba8
Ribaldo <ribaldorafael@gmail.com>
2018-01-22 09:55:37 -0200

    Altering utm_campaign column size (100 -> 255)

--- db/ODS/DDL/lead.sql
+++ db/ODS/DDL/lead.sql
@@ -63,7 +63,7 @@ CREATE TABLE public.lead
   criado_em timestamp without time zone,
   url_source character varying(100) DEFAULT NULL::character varying,
   utm_medium character varying(100) DEFAULT NULL::character varying,
-  utm_campaign character varying(100) DEFAULT NULL::character varying,
+  utm_campaign character varying(255) DEFAULT NULL::character varying,
   utm_source character varying(100) DEFAULT NULL::character varying,
   usuario_que_indicou_id character varying(100) DEFAULT NULL::character varying
 )
