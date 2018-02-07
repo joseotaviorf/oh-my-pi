4cca9d2b9efed97f9d1185f4529fa6c349e1d58f
dgolhiardi <dgolhiardi@gmail.com>
2018-02-07 14:09:34 -0200

    created ranking query and tables

---
+++ db/3.dw/growth/DDL/agents_performance_ranking.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS growth.agents_performance_ranking;
+CREATE TABLE growth.agents_performance_ranking (
+	agent_id int8,
+	agent_name varchar(255),
+	greater_region varchar(100),
+	bookings numeric(10,4),
+	contracts numeric(10,4),
+	conversion numeric(10,4),
+	ranking numeric(10,4),
+	average_conversion numeric(10,4),
+	top_percentile numeric(10,4),
+	yellow_flag int4,
+	commission numeric(10,4),
+	gold_threshold numeric(10,4),
+	silver_threshold numeric(10,4),
+	yellow_flag_threshold numeric(10,4),
+	dt_ranking date
+) ;
