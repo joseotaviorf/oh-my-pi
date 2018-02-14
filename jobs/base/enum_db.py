0f5eac39c7443d7e3dea159a2fb8f0e6c2cadbf4
Ribaldo <ribaldorafael@gmail.com>
2018-02-14 16:59:04 -0200

    Adding new enum db connection (sorting hat)

--- jobs/base/enum_db.py
+++ jobs/base/enum_db.py
@@ -2,6 +2,7 @@ class EnumDb():
     QuintoAndar_ebdb = 'ENV_EBDB'
     QuintoAndar_godfather = 'ENV_GODFATHER'
     QuintoAndar_darkrum = 'ENV_DARKRUM'
+    QuintoAndar_sortinghat = 'ENV_SORTINGHAT'
     BI_Staging = 'ENV_BI_STG'
     BI_ODS = 'ENV_BI_ODS'
     BI_DW = 'ENV_BI_DW'
