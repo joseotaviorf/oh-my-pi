60a894967651acee3645ce0683a8552d699a1fda
Ribaldo <ribaldorafael@gmail.com>
2018-02-14 17:11:19 -0200

    Creating new sorting hat job

---
+++ jobs/new_etl/sorting_hat.py
@@ -0,0 +1,22 @@
+from jobs.base.base_etl import BaseETL
+from jobs.base.enum_db import EnumDb
+from qa_python_utils.default_logger import _logger, logger
+
+
+class SortingHat(object):
+    @logger
+    def extract_table_from_db(self, table_name):
+        _logger.info('m=table_extraction_and_load, msg={} - from db'.format(table_name))
+        return BaseETL.from_db_query(
+            db_enum=EnumDb.QuintoAndar_sortinghat,
+            query='select * from "{}";'.format(table_name)
+        )
+
+    @logger
+    def load_table_to_s3(self, table_name, data_table, s3_bucket):
+        _logger.info('m=table_extraction_and_load, msg={} - to s3'.format(table_name))
+        BaseETL.to_s3(
+            filename='{}.csv'.format(table_name),
+            data_table=data_table,
+            bucket_folder_path='{}/raw/sorting_hat/{}'.format(s3_bucket, table_name)
+        )
