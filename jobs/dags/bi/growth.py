0ec9d3e488bfb574387bf3a4f26869a875ee2fd8
Ribaldo <ribaldorafael@gmail.com>
2018-02-20 11:57:03 -0300

    Removing day from employees queries (growth)

--- jobs/dags/bi/growth.py
+++ jobs/dags/bi/growth.py
@@ -184,7 +184,9 @@ def sub_dag_func_no_filters(main_dag_name, sub_dag_name, funnel, start_date, sch
                                              dag=local_dag,
                                              op_kwargs={'measure': sub_dag_name}
                                              )
-    consolidation_task.set_upstream([no_filter_tasks[0], no_filter_tasks[1], no_filter_tasks[2], no_filter_tasks[3]])
+
+    no_filter_list = [no_filter_tasks[i] for i in range(0, 4)]
+    consolidation_task.set_upstream(no_filter_list[1:] if sub_dag_name == 'employees' else no_filter_list)
 
     return local_dag
 
@@ -283,8 +285,8 @@ fact_task = get_python_operator('load_fact_growth', load_fact_growth, main_dag)
 amplitude_engaged_users_previous_task >> engaged_users_sub_dag
 
 leads_sub_dag >> new_listings_sub_dag >> opportunities_sub_dag >> prospects_sub_dag >> qualifieds_sub_dag >> \
-    ongoing_contracts_sub_dag >> engaged_users_sub_dag >> employees_sub_dag >> ticket_resolution_sub_dag >> \
-    tickets_sub_dag >> approved_by_insurer_sub_dag >> documentation_sent_sub_dag >> offerers_sub_dag >> \
-    offerers_approved_sub_dag >> offerers_sent_doc_sub_dag >> offers_approved_sub_dag >> \
-    offers_submitted_sub_dag >> tenant_prospects_sub_dag >> tenants_sub_dag >> visitors_sub_dag >> \
-    visits_booked_sub_dag >> visits_completed_sub_dag >> fact_task
+ongoing_contracts_sub_dag >> engaged_users_sub_dag >> employees_sub_dag >> ticket_resolution_sub_dag >> \
+tickets_sub_dag >> approved_by_insurer_sub_dag >> documentation_sent_sub_dag >> offerers_sub_dag >> \
+offerers_approved_sub_dag >> offerers_sent_doc_sub_dag >> offers_approved_sub_dag >> \
+offers_submitted_sub_dag >> tenant_prospects_sub_dag >> tenants_sub_dag >> visitors_sub_dag >> \
+visits_booked_sub_dag >> visits_completed_sub_dag >> fact_task
