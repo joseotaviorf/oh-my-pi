f111dd6891446e49640c6e565c96f561308239ba
Ribaldo <ribaldorafael@gmail.com>
2018-02-21 16:05:17 -0300

    Adding credit analysis init date to proposal (ODS) and vw_dim_proposal

--- db/ODS/views/vw_dim_proposal.sql
+++ db/ODS/views/vw_dim_proposal.sql
@@ -22,7 +22,8 @@ SELECT
   "criadoEm" as dt_created,
   "atualizadoEm" as dt_updated,
   now()::timestamp as dt_timestamp,
-  "primeiroEnvioDocInq" as dt_tenant_first_document_sent
+  "primeiroEnvioDocInq" as dt_tenant_first_document_sent,
+  credit_analysis_init_date as dt_credit_analysis_init
 FROM
   public.proposal ;
 
