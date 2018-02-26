bb2fd33b88bccc77d8703b88694096d1b3fa52f5
Ribaldo <ribaldorafael@gmail.com>
2018-02-26 12:24:21 -0300

    Adding table name - godfather

--- jobs/new_etl/godfather.py
+++ jobs/new_etl/godfather.py
@@ -12,7 +12,7 @@ class GodFather(object):
     def __get_table(table_name):
         return BaseETL.from_db_query(
             db_enum=EnumDb.QuintoAndar_godfather,
-            query='select * from {};'.format(GodFather.SCHEMA, table_name)
+            query='select * from {}.{};'.format(GodFather.SCHEMA, table_name)
         )
 
     @staticmethod
