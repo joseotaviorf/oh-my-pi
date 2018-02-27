c823d229ff05fffd3444e1251ee4960015d3d82e
Ribaldo <ribaldorafael@gmail.com>
2018-02-27 14:43:20 -0300

    Replacing approved for response (credit SLA)

--- db/ODS/views/vw_fact_liquidity_property_scheduling.sql
+++ db/ODS/views/vw_fact_liquidity_property_scheduling.sql
@@ -266,8 +266,8 @@ select
               date_part('hour', internal_analysis_date - offer_date)) / 24.0 as offer_to_internal_analyis,
   (date_part('day', credit_analysis_init_date - offer_approved_date) * 24 +
               date_part('hour', credit_analysis_init_date - offer_approved_date)) / 24.0 as offer_to_credit_analysis_init_date,
-  (date_part('day', coalesce(credit_analysis_end_date, credit_analysis_approved_date)  - credit_analysis_init_date) * 24 +
-              date_part('hour', coalesce(credit_analysis_end_date, credit_analysis_approved_date) - credit_analysis_init_date)) / 24.0 as credit_analysis_init_to_end,
+  (date_part('day', coalesce(credit_analysis_end_date, credit_analysis_date)  - credit_analysis_init_date) * 24 +
+              date_part('hour', coalesce(credit_analysis_end_date, credit_analysis_date) - credit_analysis_init_date)) / 24.0 as credit_analysis_init_to_end,
   (date_part('day', contract_date - credit_analysis_approved_date) * 24 +
               date_part('hour', contract_date - credit_analysis_approved_date)) / 24.0 as credit_analysis_to_contract
 from calculated_dates
