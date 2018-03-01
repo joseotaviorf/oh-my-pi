b0525173a82b54a6942ab55c75ac4d73fe3780c8
dgolhiardi <dgolhiardi@gmail.com>
2018-03-01 20:18:31 -0300

    removed task from subdag

--- jobs/dags/bi/demand_scheduled_properties.py
+++ jobs/dags/bi/demand_scheduled_properties.py
@@ -251,13 +251,6 @@ def dw_sub_dag(sub_dag_name):
         op_kwargs={'table_name': 'booking_media_sources', 'file_name': 'extract_booking_media_sources.sql'}
     )
 
-    BaseDAG.get_python_operator(
-        task_id='ODS_listing_views',
-        dag=local_dag,
-        func_command=load_athena_file_query_to_ods,
-        op_kwargs={'table_name': 'listing_views', 'file_name': 'listing_views.sql'}
-    )
-
     dim_booking_task = BaseDAG.get_python_operator(
         task_id='DW_dim_booking',
         dag=local_dag,
@@ -283,6 +276,13 @@ ods_property_scheduling_task = BaseDAG.get_python_operator(
     dag=main_dag,
     func_command=extract_query_dim_from_ebdb_to_ods,
     op_kwargs={'dim_name': 'property_scheduling', 'command': 'call ebdb.list_property_scheduling();'}
+)
+
+BaseDAG.get_python_operator(
+    task_id='ODS_listing_views',
+    dag=main_dag,
+    func_command=load_athena_file_query_to_ods,
+    op_kwargs={'table_name': 'listing_views', 'file_name': 'listing_views.sql'}
 )
 
 fact_property_scheduling_task = BaseDAG.get_python_operator(
