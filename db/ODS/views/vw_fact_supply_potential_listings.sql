4ef173b9fbaf4e3998205881b2332a9253212bf6
Ribaldo <ribaldorafael@gmail.com>
2018-03-02 16:40:18 -0300

    Refactoring supply fact

--- db/ODS/views/vw_fact_supply_potential_listings.sql
+++ db/ODS/views/vw_fact_supply_potential_listings.sql
@@ -462,14 +462,7 @@ result as (
       )
       as cac_marketing_per_published_listing,
 
-      now()::timestamp as load_timestamp,
-
-      (date_part('day', first_inside_sales_contact_date - contact_date) * 24 +
-                  date_part('hour', first_inside_sales_contact_date - contact_date)) / 24.0 as lead_to_first_contact,
-      (date_part('day', first_inside_sales_contact_date - prospect_date) * 24 +
-                  date_part('hour', first_inside_sales_contact_date - prospect_date)) / 24.0 as prospect_to_first_contact,
-      (date_part('day', first_publication_date - qualified_date) * 24 +
-                  date_part('hour', first_publication_date - qualified_date)) / 24.0 as qualified_to_listing
+      now()::timestamp as load_timestamp
 
 
     FROM
@@ -555,8 +548,12 @@ result as (
 )
 select
     *,
+    (date_part('day', dt_first_inside_sales_contact - dt_contact) * 24 +
+              date_part('hour', dt_first_inside_sales_contact - dt_contact)) / 24.0 as lead_to_first_contact,
     (date_part('day', dt_first_inside_sales_contact - dt_lead_and_prospect) * 24 +
-              date_part('hour', dt_first_inside_sales_contact - dt_lead_and_prospect)) / 24.0 as first_contact_to_prospect,
+              date_part('hour', dt_first_inside_sales_contact - dt_lead_and_prospect)) / 24.0 as prospect_to_first_contact,
+    (date_part('day', dt_first_publication - dt_qualified) * 24 +
+              date_part('hour', dt_first_publication - dt_qualified)) / 24.0 as qualified_to_listing,
     (date_part('day', dt_first_publication - dt_opportunity) * 24 +
               date_part('hour', dt_first_publication - dt_opportunity)) / 24.0 as opportunity_to_listing
 from result
