1053dd9000e62be37d0a2506344d1e3ff7ec0430
Ribaldo <ribaldorafael@gmail.com>
2018-03-09 09:25:25 -0300

    Adding partial to landing views table and adding ddl for growth_staging

--- db/2.datalake/queries/amplitude/owner_landing_views/suffix.sql
+++ db/2.datalake/queries/amplitude/owner_landing_views/suffix.sql
@@ -3,6 +3,7 @@ select distinct
   _month,
   _week,
   _day,
+  partial,
   dense_rank() over (partition by _year, _month, _week, _day  order by amplitude_id asc)
     + dense_rank() over (partition by _year, _month, _week, _day order by amplitude_id desc)
     - 1 as daily_count,
