d8ace749b4d1ef8215a222f74d2932a8c037cf63
Ribaldo <ribaldorafael@gmail.com>
2018-03-14 09:24:23 -0300

    Removing extra column from Offer table (godfather)

--- jobs/dags/bi/demand_scheduled_properties.py
+++ jobs/dags/bi/demand_scheduled_properties.py
@@ -115,16 +115,33 @@ def ods_sub_dag(sub_dag_name):
         func_command=load_athena_raw_query_to_ods,
         op_kwargs={'table_name': 'offer', 'query': """
                                                     select distinct
-                                                        eo.*,
-                                                        go.type,
-                                                        go.first_sent_at,
-                                                        go.last_sent_at,
-                                                        gt.type as topic_type
+                                                      eo.id,
+                                                      eo.atualizadoem,
+                                                      eo.criadoem,
+                                                      eo.firestoreid,
+                                                      eo.godfatherid,
+                                                      eo.originalcondo,
+                                                      eo.originalhomeinsurance,
+                                                      eo.originaliptu,
+                                                      eo.originalrent,
+                                                      eo.rent,
+                                                      eo.status,
+                                                      eo.turn,
+                                                      eo.client_id,
+                                                      eo.house_id,
+                                                      eo.rentflow_id,
+                                                      eo.rejectionreason,
+                                                      eo.iteration,
+                                                      eo.expirationdate,
+                                                      go.type,
+                                                      go.first_sent_at,
+                                                      go.last_sent_at,
+                                                      gt.type as topic_type
                                                     from datalake_raw.ebdb_offer eo
                                                     join datalake_raw.godfather_offer go
-                                                        on eo.godfatherid = go.id
+                                                      on eo.godfatherid = go.id
                                                     left join datalake_raw.godfather_topic gt
-                                                        on gt.offer_id = go.id
+                                                      on gt.offer_id = go.id
                                                     ;
                                                     """
                    }
