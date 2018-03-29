b046f8df56ac2b97ead90ad7e29393cce51b8833
Ribaldo <ribaldorafael@gmail.com>
2018-03-29 13:53:21 -0300

    Fixing unit economics test queries dir name

--- jobs/dags/bi/__init__.py
+++ jobs/dags/bi/__init__.py
@@ -2,6 +2,6 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DW_TEST_QUERIES_DIR = os.path.join(dir_path, '../../../db/3.dw/public/queries/tests')
-UNIT_ECONOMICS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../../db/ODS/unit_economics/test/queries')
+UNIT_ECONOMICS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../../db/ODS/unit_economics/tests/queries')
 
 DEFAULT_DAG_OWNER = 'Data Team'
