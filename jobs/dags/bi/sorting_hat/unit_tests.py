b5b1ce7c2b5459449414ca15ab7dd475f8e10bab
Ribaldo <ribaldorafael@gmail.com>
2018-04-06 16:01:25 -0300

    Fixing sorting hat test queries var

--- jobs/dags/bi/sorting_hat/unit_tests.py
+++ jobs/dags/bi/sorting_hat/unit_tests.py
@@ -2,7 +2,7 @@ from jobs.base.base_dag import BaseDAG
 from jobs.base.base_etl import BaseETL
 from jobs.base.base_test import BaseTest
 from jobs.base.enum_db import EnumDb
-from jobs.dags.bi.__init__ import SORTINGHAT_TEST_QUERIES_DIR
+from jobs.dags.bi.sorting_hat.__init__ import SORTINGHAT_TEST_QUERIES_DIR
 from qa_python_utils.default_logger import logger, _logger
 
 COUNT_CHECK_SQL_SUFFIX = 'count_check.sql'
