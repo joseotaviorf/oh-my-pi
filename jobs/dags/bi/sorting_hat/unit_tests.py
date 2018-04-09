d0c07dd56552b545f1e99e47fe1d058a00db6e3a
Ribaldo <ribaldorafael@gmail.com>
2018-04-09 16:07:01 -0300

    Indexing query return (sorting hat)

--- jobs/dags/bi/sorting_hat/unit_tests.py
+++ jobs/dags/bi/sorting_hat/unit_tests.py
@@ -31,25 +31,23 @@ def build(sub_dag_name, dag_name, schedule_interval, start_date, entity):
 
 def __test_count(**kwargs):
     sh_query = BaseETL.get_query_from_file_name(kwargs['sh_file_path'])
-
     dl_query = BaseETL.get_query_from_file_name(kwargs['dl_file_path'])
-
     ods_query = BaseETL.get_query_from_file_name(kwargs['ods_file_path'])
 
     sh_return = BaseTest.get_query_result_for_comparison(
         query=sh_query,
         enum_db=EnumDb.QuintoAndar_sortinghat
-    ) if sh_query != '' else None
+    )[0][1][0] if sh_query != '' else None
 
     dl_return = BaseTest.get_query_result_for_comparison(
         query=dl_query,
         from_athena=True
-    ) if dl_query != '' else None
+    )[0][1][0] if dl_query != '' else None
 
     ods_return = BaseTest.get_query_result_for_comparison(
         query=ods_query,
         enum_db=EnumDb.BI_ODS
-    ) if ods_query != '' else None
+    )[0][1][0] if ods_query != '' else None
 
     if (sh_return is not None and dl_return is not None and sh_return != dl_return) \
             or (sh_return is not None and ods_return is not None and sh_return != ods_return) \
