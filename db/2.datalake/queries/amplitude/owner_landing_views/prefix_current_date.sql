20bcd2b845cd822169ad05d30178c1cc22a10edf
Ribaldo <ribaldorafael@gmail.com>
2018-04-10 10:26:04 -0300

    Updating amplitude owner landing app

--- db/2.datalake/queries/amplitude/owner_landing_views/prefix_current_date.sql
+++ db/2.datalake/queries/amplitude/owner_landing_views/prefix_current_date.sql
@@ -10,9 +10,14 @@ with all_events as (
     'QuintoAndar' as city
   from datalake_clean.amplitude_events
   where ym >= '2017-01'
-    and ((trim(app) = '170698' and et = 'landing_page_viewed' and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-08-23' as date))
-      or (trim(app) = '160023' and et = 'Owner_Landing-Views_register_form' and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast('2017-08-23' as date)))
+    and et in (
+      'landing_page_viewed',
+      'Owner_Landing-Views_register_form'
+    )
+    and ((trim(app) = '183047' and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-08-23' as date))
+      or (trim(app) = '160023' and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast('2017-08-23' as date)))
     and extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(year from (now() - interval '1' month))
     and extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(month from (now() - interval '1' month))
     and extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) < extract(day from now())
+    and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-01-01' as date)
 )
