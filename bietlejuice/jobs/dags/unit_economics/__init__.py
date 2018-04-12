52d2a6e3519adc77e21a19e15ce53bb8ba464ac9
Ribaldo <ribaldorafael@gmail.com>
2018-04-12 10:25:47 -0300

    Fixing dir tree (unit economics tests)

--- bietlejuice/jobs/dags/unit_economics/__init__.py
+++ bietlejuice/jobs/dags/unit_economics/__init__.py
@@ -1,4 +1,4 @@
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-UNIT_ECONOMICS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/ODS/unit_economics/tests/queries')
+UNIT_ECONOMICS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../../db/ODS/unit_economics/tests/queries')
