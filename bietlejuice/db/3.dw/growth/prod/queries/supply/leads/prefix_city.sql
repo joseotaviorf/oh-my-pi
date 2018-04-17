0bfa1f90e9faa9576d5a405d9ee0bb47848c6a2a
Ribaldo <ribaldorafael@gmail.com>
2018-04-17 14:51:42 -0300

    Converting sk_lead_date to date

--- bietlejuice/db/3.dw/growth/prod/queries/supply/leads/prefix_city.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/supply/leads/prefix_city.sql
@@ -1,51 +1,51 @@
 with all_dates_prev as (
 	select distinct
-    date_part('year', f.dt_contact) as _year,
-    date_part('month', f.dt_contact) as _month,
-    date_part('week', f.dt_contact) as _week,
-    date_part('day', f.dt_contact) as _day,
+    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
+    date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _month,
+    date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _week,
+    date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _day,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_contact),
-    																date_part('month', f.dt_contact),
-    																date_part('week', f.dt_contact),
-    																date_part('day', f.dt_contact) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_contact),
-    																		date_part('month', f.dt_contact),
-    																		date_part('week', f.dt_contact),
-    																		date_part('day', f.dt_contact) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																		date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																		date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																		date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as daily_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_contact),
-    																date_part('week', f.dt_contact) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_contact),
-    																		date_part('week', f.dt_contact) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																		date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as weekly_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_contact),
-    																date_part('month', f.dt_contact) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_contact),
-    																		date_part('month', f.dt_contact) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    																		date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as monthly_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_contact) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_contact) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as yearly_count
 	from fact_supply f
 	join dim_lead dl
 		on f.sk_lead = dl.sk_lead
-		  and f.dt_contact >= '2017-01-01' and f.dt_contact < current_date
+		  and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') < current_date
 		  and f.sk_lead != -1
 	join dim_property dpr
 		on f.sk_property = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-  order by coalesce(dr.city_name, ''), date_part('year', f.dt_contact), date_part('month', f.dt_contact), date_part('week', f.dt_contact), date_part('day', f.dt_contact)
+  order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')), date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')), date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD'))
 ),
 all_dates as (
   select distinct
@@ -66,46 +66,46 @@ all_dates_last_week as (
 ),
 all_dates_last_month as (
 	select
-    date_part('year', f.dt_contact) as _year,
-    date_part('month', f.dt_contact) as _month,
+    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
+    date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _month,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
-    count(f.dt_contact) as monthly_count
+    count(to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as monthly_count
 	from fact_supply f
 	join dim_lead dl
 		on f.sk_lead = dl.sk_lead
-		  and f.dt_contact >= '2017-01-01' and f.dt_contact < current_date
+		  and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') < current_date
 		  and f.sk_lead != -1
 	join dim_property dpr
   	on f.sk_property = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-	where date_part('year', f.dt_contact) = date_part('year', add_months(current_date, -1))
-  		and date_part('month', f.dt_contact) = date_part('month', add_months(current_date, -1))
-  		and date_part('day', f.dt_contact) < date_part('day', current_date)
- 	group by coalesce(dr.city_name, ''), date_part('year', f.dt_contact), date_part('month', f.dt_contact)
-  order by coalesce(dr.city_name, ''), date_part('year', f.dt_contact), date_part('month', f.dt_contact)
+	where date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) = date_part('year', add_months(current_date, -1))
+  		and date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) = date_part('month', add_months(current_date, -1))
+  		and date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) < date_part('day', current_date)
+ 	group by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD'))
+  order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD'))
 ),
 all_dates_last_year as (
 	select
-		date_part('year', f.dt_contact) as _year,
+		date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
 		'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
-    count(f.dt_contact) as yearly_count
+    count(to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as yearly_count
   from fact_supply f
 	join dim_lead dl
 		on f.sk_lead = dl.sk_lead
-		  and f.dt_contact >= '2017-01-01' and f.dt_contact < current_date
+		  and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_lead_date::varchar, 'YYYYMMDD') < current_date
 		  and f.sk_lead != -1
   join dim_property dpr
   	on f.sk_property = dpr.sk_property
   left join dim_region dr
   	on dpr.regiao_id = dr.id
-  where date_part('year', f.dt_contact) = date_part('year', add_months(current_date, -12))
-  		and ((date_part('month', f.dt_contact) = date_part('month', add_months(current_date, -12))
-  		      and date_part('day', f.dt_contact) < date_part('day', add_months(current_date, -12)))
-  		  or date_part('month', f.dt_contact) < date_part('month', add_months(current_date, -12))
+  where date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) = date_part('year', add_months(current_date, -12))
+  		and ((date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) = date_part('month', add_months(current_date, -12))
+  		      and date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) < date_part('day', add_months(current_date, -12)))
+  		  or date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) < date_part('month', add_months(current_date, -12))
   		  )
-  group by coalesce(dr.city_name, ''), date_part('year', f.dt_contact)
-	order by coalesce(dr.city_name, ''), date_part('year', f.dt_contact)
+  group by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD'))
+	order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD'))
 ),
