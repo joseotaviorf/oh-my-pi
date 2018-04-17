fd69f3c604238dfa4e282e8268ebc9e1587dccc7
Ribaldo <ribaldorafael@gmail.com>
2018-04-17 16:43:04 -0300

    Updating prospect and qualified sks (growth)

--- bietlejuice/db/3.dw/growth/prod/queries/supply/prospects/prefix_city.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/supply/prospects/prefix_city.sql
@@ -1,40 +1,40 @@
 with all_dates_prev as (
 	select distinct
-    date_part('year', f.dt_lead_and_prospect) as _year,
-    date_part('month', f.dt_lead_and_prospect) as _month,
-    date_part('week', f.dt_lead_and_prospect) as _week,
-    date_part('day', f.dt_lead_and_prospect) as _day,
+    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _year,
+    date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _month,
+    date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _week,
+    date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _day,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_lead_and_prospect),
-    																date_part('month', f.dt_lead_and_prospect),
-    																date_part('week', f.dt_lead_and_prospect),
-    																date_part('day', f.dt_lead_and_prospect) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_lead_and_prospect),
-    																		date_part('month', f.dt_lead_and_prospect),
-    																		date_part('week', f.dt_lead_and_prospect),
-    																		date_part('day', f.dt_lead_and_prospect) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																		date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																		date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																		date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as daily_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_lead_and_prospect),
-    																date_part('week', f.dt_lead_and_prospect) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_lead_and_prospect),
-    																		date_part('week', f.dt_lead_and_prospect) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																		date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as weekly_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_lead_and_prospect),
-    																date_part('month', f.dt_lead_and_prospect) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_lead_and_prospect),
-    																		date_part('month', f.dt_lead_and_prospect) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')),
+    																		date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as monthly_count,
     rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', f.dt_lead_and_prospect) order by f.sk_lead asc)
+                                    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
     	+ rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', f.dt_lead_and_prospect) order by f.sk_lead desc)
+    	                                  date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
 			- 1 as yearly_count
 	from fact_supply f
 	join dim_contacts_and_prospects cp
@@ -45,13 +45,13 @@ with all_dates_prev as (
 	  		and f.dt_lead is not null)
 	  	or (cp.self_service is true and f.dt_prospect is not null)
 	  	)
-	  	and f.dt_lead_and_prospect >= '2017-01-01' and f.dt_lead_and_prospect < current_date
+	  	and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') < current_date
 	  	and f.sk_lead != -1
 	join dim_property dpr
 		on f.sk_property = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-  order by coalesce(dr.city_name, ''), date_part('year', f.dt_lead_and_prospect), date_part('month', f.dt_lead_and_prospect), date_part('week', f.dt_lead_and_prospect), date_part('day', f.dt_lead_and_prospect)
+  order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')), date_part('week', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')), date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD'))
 ),
 all_dates as (
   select distinct
@@ -72,11 +72,11 @@ all_dates_last_week as (
 ),
 all_dates_last_month as (
 	select
-    date_part('year', f.dt_lead_and_prospect) as _year,
-    date_part('month', f.dt_lead_and_prospect) as _month,
+    date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _year,
+    date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _month,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
-    count(f.dt_lead_and_prospect) as monthly_count
+    count(to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as monthly_count
 	from fact_supply f
 	join dim_contacts_and_prospects cp
 	  on f.sk_lead = cp.sk_cap_id
@@ -86,24 +86,24 @@ all_dates_last_month as (
 	  		and f.dt_lead is not null)
 	  	or (cp.self_service is true and f.dt_prospect is not null)
 	  	)
-	  	and f.dt_lead_and_prospect >= '2017-01-01' and f.dt_lead_and_prospect < current_date
+	  	and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') < current_date
 	  	and f.sk_lead != -1
 	join dim_property dpr
   	on f.sk_property = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-	where date_part('year', f.dt_lead_and_prospect) = date_part('year', add_months(current_date, -1))
-  		and date_part('month', f.dt_lead_and_prospect) = date_part('month', add_months(current_date, -1))
-  		and date_part('day', f.dt_lead_and_prospect) < date_part('day', current_date)
- 	group by coalesce(dr.city_name, ''), date_part('year', f.dt_lead_and_prospect), date_part('month', f.dt_lead_and_prospect)
-  order by coalesce(dr.city_name, ''), date_part('year', f.dt_lead_and_prospect), date_part('month', f.dt_lead_and_prospect)
+	where date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) = date_part('year', add_months(current_date, -1))
+  		and date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) = date_part('month', add_months(current_date, -1))
+  		and date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) < date_part('day', current_date)
+ 	group by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD'))
+  order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')), date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD'))
 ),
 all_dates_last_year as (
 	select
-		date_part('year', f.dt_lead_and_prospect) as _year,
+		date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as _year,
 		'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
-    count(f.dt_lead_and_prospect) as yearly_count
+    count(to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) as yearly_count
   from fact_supply f
 	join dim_contacts_and_prospects cp
 	  on f.sk_lead = cp.sk_cap_id
@@ -113,17 +113,17 @@ all_dates_last_year as (
 	  		and f.dt_lead is not null)
 	  	or (cp.self_service is true and f.dt_prospect is not null)
 	  	)
-	  	and f.dt_lead_and_prospect >= '2017-01-01' and f.dt_lead_and_prospect < current_date
+	  	and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') >= '2017-01-01' and to_date(f.sk_prospect_date::varchar, 'YYYYMMDD') < current_date
 	  	and f.sk_lead != -1
   join dim_property dpr
   	on f.sk_property = dpr.sk_property
   left join dim_region dr
   	on dpr.regiao_id = dr.id
-	where date_part('year', f.dt_lead_and_prospect) = date_part('year', add_months(current_date, -12))
-  		and ((date_part('month', f.dt_lead_and_prospect) = date_part('month', add_months(current_date, -12))
-  		      and date_part('day', f.dt_lead_and_prospect) < date_part('day', add_months(current_date, -12)))
-  		  or date_part('month', f.dt_lead_and_prospect) < date_part('month', add_months(current_date, -12))
+	where date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) = date_part('year', add_months(current_date, -12))
+  		and ((date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) = date_part('month', add_months(current_date, -12))
+  		      and date_part('day', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) < date_part('day', add_months(current_date, -12)))
+  		  or date_part('month', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD')) < date_part('month', add_months(current_date, -12))
   		  )
-	group by coalesce(dr.city_name, ''), date_part('year', f.dt_lead_and_prospect)
-	order by coalesce(dr.city_name, ''), date_part('year', f.dt_lead_and_prospect)
+	group by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD'))
+	order by coalesce(dr.city_name, ''), date_part('year', to_date(f.sk_prospect_date::varchar, 'YYYYMMDD'))
 ),
