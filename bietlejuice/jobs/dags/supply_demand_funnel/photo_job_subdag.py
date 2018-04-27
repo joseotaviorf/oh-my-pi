30e921f9d9e8e7ae69663460672a6f2aeea506f0
dgolhiardi <dgolhiardi@gmail.com>
2018-04-27 11:28:56 -0300

    added photo job subdag and tests to lead and photo job

--- bietlejuice/jobs/dags/supply_demand_funnel/photo_job_subdag.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/photo_job_subdag.py
@@ -1,8 +1,9 @@
 from bietlejuice.jobs.base.base_dag import BaseDAG
+from bietlejuice.jobs.base.enum_db import EnumDb
 from bietlejuice.jobs.base.base_test import BaseTest
 from bietlejuice.jobs.base.base_sub_dag import BaseSubDag
 import bietlejuice.jobs.base.new_base_etl as utils
-from qa_python_utils.default_logger import logger
+from qa_python_utils.default_logger import logger, _logger
 
 
 class PhotoJobSubDag(BaseSubDag):
@@ -19,7 +20,7 @@ class PhotoJobSubDag(BaseSubDag):
     def build(self):
         lead_dag = self.__build_local_dag()
 
-        photo_job, dim_photo_job = self.__build_data_tasks(lead_dag)
+        photo_job, dim_photo_job, load_photo_job = self.__build_data_tasks(lead_dag)
 
         test_photo_job = self.__build_tests_tasks(lead_dag)
 
@@ -49,23 +50,56 @@ class PhotoJobSubDag(BaseSubDag):
 
         dim_photo_job = BaseDAG.get_quintoandar_python_operator(
             dag=dag,
-            task_id='DW_dim_photo_job',
-            func_command=utils.load_dim_from_ods_to_dw,
+            task_id='STAGING_dim_photo_job',
+            func_command=utils.load_dim_from_ods_to_staging,
             op_kwargs={'dim_name': 'photo_job', 'bucket': self.bucket}
         )
 
-        return photo_job, dim_photo_job
+        load_photo_job = BaseDAG.get_quintoandar_python_operator(
+            dag=dag,
+            task_id='DW_dim_photo_job',
+            func_command=utils.load_dim_from_staging_to_dw,
+            op_kwargs={'dim_name': 'photo_job', 'bucket': self.bucket}
+        )
+
+        return photo_job, dim_photo_job, load_photo_job
 
     @logger
     def __build_tests_tasks(self, dag):
-        test_photo_job = BaseDAG.get_quintoandar_python_operator(
+
+        duplicate_lead = BaseDAG.get_quintoandar_python_operator(
             dag=dag,
-            task_id='TEST_dim_photo_job',
-            func_command=self.__test_lead_count
+            task_id='TEST_duplicates_dim_photo_job',
+            func_command=self.__test_duplicates
         )
 
-        return test_photo_job
+        empty_lead = BaseDAG.get_quintoandar_python_operator(
+            dag=dag,
+            task_id='TEST_emptiness_dim_photo_job',
+            func_command=self.__test_duplicates
+        )
+
+        return duplicate_lead, empty_lead
 
     @staticmethod
-    def __test_lead_count():
-        return True
+    def __test_duplicates():
+
+        if BaseTest.check_for_duplicates(
+                schema='staging',
+                table='dim_photo_job',
+                key='sk_photo_job',
+                enum_db=EnumDb.BI_ODS):
+            raise Exception
+
+        _logger.info('m=test_duplicates {} free from duplicates'.format('dim_photo_job'))
+
+    @staticmethod
+    def __test_emptiness():
+
+        if BaseTest.check_for_emptiness(
+                schema='staging',
+                table='dim_photo_job',
+                enum_db=EnumDb.BI_ODS):
+            raise Exception
+
+        _logger.info('m=test_emptiness {} not empty'.format('dim_photo_job'))
