78c41ce80eefb232134ce80f2328dc515fbded20
dgolhiardi <dgolhiardi@gmail.com>
2018-04-30 16:26:31 -0300

    force refresh

--- bietlejuice/jobs/dags/supply_demand_funnel.py
+++ bietlejuice/jobs/dags/supply_demand_funnel.py
@@ -15,7 +15,7 @@ bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 biz_etl = BusinessDimensionETL(bucket)
 
 MAIN_DAG_NAME = 'bi-supply-demand-etl'
-MAIN_START_DATE = datetime(2018, 3, 1, 0, 0, 0)
+MAIN_START_DATE = datetime(2018, 4, 29, 0, 0, 0)
 MAIN_SCHEDULE_INTERVAL = '0 2 * * *'
 
 # create main DAG definition
