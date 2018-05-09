16e5835654b0c47d7429dbd1bce654ca1048943d
Ribaldo <ribaldorafael@gmail.com>
2018-05-09 10:41:34 -0300

    Reducing the acceptable_diff from dim_subdag

--- bietlejuice/jobs/dags/supply_demand_funnel/dim_subdag.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/dim_subdag.py
@@ -57,7 +57,7 @@ class DimSubDag(BaseSubDag):
 
     def __test_counts_from_file_query(self):
         BaseTest.are_counts_equal({
-            'acceptable_diff': 1.0,
+            'acceptable_diff': .5,
             'sources': [
                 {
                     'schema': 'staging',
