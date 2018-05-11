a6107b378e169ebcc7f23e43ec6f906a67461c8e
Ribaldo <ribaldorafael@gmail.com>
2018-05-11 16:57:48 -0300

    Adding incremental processing to agents availability

--- bietlejuice/db/3.dw/staging/queries/agents_scheduling.sql
+++ bietlejuice/db/3.dw/staging/queries/agents_scheduling.sql
@@ -3,7 +3,7 @@ select
 	u.nome as agent_name,
 	t.planner_status,
 	t.history_status,
-	t.slot_dt,
+	date(t.slot_dt) as slot_dt,
 	case
 		when t.slot_dt >= current_date
 			then 'Open Schedule'
@@ -11,7 +11,8 @@ select
 	end as realized_schedule,
 	sum(cast(t.available_slot as integer)) as available_slots
 from staging.agents_slots t
-left join datalake_raw.ebdb_usuario u
+join datalake_raw.ebdb_usuario u
 	on u.dadosagente_id = t.agent_id
+where date(t.slot_dt) = date('{}')
 group by 1, 2, 3, 4, 5, 6
 ;
