eed7e7d19fbcbaa07a01dbc7f6148051c532bdde
dgolhiardi <dgolhiardi@gmail.com>
2018-05-29 10:58:22 -0300

    forgot the trims

--- bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views_bv/prefix_all_dates.sql
+++ bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views_bv/prefix_all_dates.sql
@@ -12,8 +12,8 @@ with all_events as (
   where ym >= '2018-01'
     and et = 'landing_page_viewed'
     and trim(app) = '183047'
-    and e_ub_page_variant = 'bv'
-    and e_ub_page_name = 'Proprietário'
+    and trim(e_ub_page_variant) = 'bv'
+    and trim(e_ub_page_name) = 'Proprietário'
     and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2018-05-07' as date)
     and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast(now() as date)
 )
--- bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
+++ bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
@@ -12,8 +12,8 @@ with all_events as (
   where ym >= '2018-01'
     and et = 'landing_page_viewed'
     and trim(app) = '183047'
-    and e_ub_page_variant = 'bv'
-    and e_ub_page_name = 'Proprietário'
+    and trim(e_ub_page_variant) = 'bv'
+    and trim(e_ub_page_name) = 'Proprietário'
     and extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(year from (now() - interval '1' month))
     and extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(month from (now() - interval '1' month))
     and extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) < extract(day from now())
