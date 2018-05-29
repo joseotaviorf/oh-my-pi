400a01b3d1ff7dcd515a11a6eddf1a8c77fac9e9
dgolhiardi <dgolhiardi@gmail.com>
2018-05-29 10:45:14 -0300

    added new landing view bv to growth

--- bietlejuice/jobs/new_etl/amplitude/owner_landing_views.py
+++ bietlejuice/jobs/new_etl/amplitude/owner_landing_views.py
@@ -28,3 +28,31 @@ class OwnerLandingViews(GrowthUsers):
     def __append(self, _filter, prefix):
         df = self.get_df(prefix=prefix, suffix=self.suffix_query)
         OwnerLandingViews.df_to_dw(df=df)
+
+
+class OwnerLandingViewsBV(GrowthUsers):
+    TABLE_NAME = 'amplitude_owner_landing_views_bv'
+
+    @logger
+    def __init__(self, s3_bucket):
+        super(OwnerLandingViewsBV, self).__init__(measure='owner_landing_views_bv', s3_bucket=s3_bucket)
+
+    @staticmethod
+    @logger(exclude='df')
+    def df_to_dw(df):
+        GrowthUsers._df_to_dw(df, OwnerLandingViews.TABLE_NAME)
+
+    @staticmethod
+    @logger
+    def truncate_table():
+        GrowthUsers._truncate_table(OwnerLandingViews.TABLE_NAME)
+
+    @logger
+    def append_to_table(self, _filter):
+        self.__append(_filter=_filter, prefix=self.all_dates_query)
+        self.__append(_filter=_filter, prefix=self.current_date_query)
+
+    @logger
+    def __append(self, _filter, prefix):
+        df = self.get_df(prefix=prefix, suffix=self.suffix_query)
+        OwnerLandingViews.df_to_dw(df=df)
