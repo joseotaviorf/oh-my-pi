ab63e93ba26d0afc81f0d8dba1d4d2d3b05bccfd
ihoelscher <igor.hoelscher@gmail.com>
2018-06-01 18:31:46 -0300

    Refactoring

--- bietlejuice/jobs/dags/crawl_cpfs.py
+++ bietlejuice/jobs/dags/crawl_cpfs.py
@@ -5,11 +5,12 @@ from datetime import datetime
 
 from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
+from qa_python_utils.default_logger import _logger, logger
+
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_etl import BaseETL
 from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.new_etl.crawlers.crawler_cpfs import CrawlerCPFs
-from qa_python_utils.default_logger import _logger, logger
 
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 data_google_api_key = env.get_airflow_env_var('DATA_GOOGLE_API_KEY')
@@ -21,41 +22,44 @@ NO_LOCATIONS_MSG = 'no locations to crawl'
 @logger
 def crawl_cpfs(**kwargs):
     ws = kwargs.get('ws', 'vivareal')
-    neighbourhood = kwargs.get('neighbourhood')
+    neighborhood = kwargs.get('neighborhood')
+    delta_days = kwargs.get('delta_days', 3)
+    limit = kwargs.get('limit', 500)
 
     crawler_cpfs = CrawlerCPFs(s3_bucket=s3_bucket, google_maps_api_key=data_google_api_key)
-    locations_raw = crawler_cpfs.get_locations(ws=ws, delta_days=kwargs.get('delta_days', 3))
+    locations_raw = crawler_cpfs.get_locations(ws=ws, delta_days=delta_days)
     if locations_raw.empty:
         _logger.info('m=get_locations, msg={}'.format(NO_LOCATIONS_MSG))
         return None
 
     _logger.info('m=crawl_cpfs, got {} locations from crawlers'.format(len(locations_raw)))
-    locations_cleaned = crawler_cpfs.cleaning(locations_raw)
 
-    locations_coverage = crawl_cpfs.check_locations_coverage(locations_cleaned)
+    locations_cleaned = crawler_cpfs.cleaning(locations_raw)
+    locations_neighbors = crawler_cpfs.check_neighborhoods(locations_cleaned, neighborhood)
+    locations_coverage = crawler_cpfs.check_locations_coverage(locations_neighbors)
     if locations_coverage.empty:
         _logger.info('m=crawl_cpfs, msg={}'.format(NO_LOCATIONS_MSG))
         return None
 
-    locations_regions = crawl_cpfs.check_neighborhoods(locations_coverage, neighbourhood)
-    if locations_regions.empty:
-        _logger.info('m=crawl_cpfs, msg={}'.format(NO_LOCATIONS_MSG))
-        return None
+    _logger.info('m=crawl_cpfs, {} locations after filtering regions'.format(len(locations_coverage)))
 
-    _logger.info('m=crawl_cpfs, {} locations after filtering regions'.format(len(locations_regions)))
+    locations = crawler_cpfs.fill_in(locations_coverage, limit, debug=True)
+    locations_unique = locations.groupby(['street_name', 'street_number']).size().reset_index()
+    locations_unique.columns = ['street_name', 'street_number', 'n_listings']
 
-    locations = crawl_cpfs.fill_in(locations_regions)
-    locations_unique = locations.drop_duplicates(subset=['street_name', 'street_number'])
-
-    last_date = datetime.strptime(crawler_cpfs.get_last_crawling_date(ws), '%Y-%m-%d')
     _logger.info('m=crawl_cpfs, saving seed with {} unique street_name and street_number'.format(len(locations_unique)))
-    suffix = '{}-{}-{}-{}.csv'.format(
-        ws, last_date.strftime('%Y-%m-%d'), kwargs.get('delta_days', 3), neighbourhood or 'all')
+
+    last_date = crawler_cpfs.get_last_crawling_date(ws)
+    suffix = '{}-{}-{}.csv'.format(ws, last_date, delta_days)
     filename = 'raw/crawled_cpfs/source/' + suffix
-    BaseETL.csv_to_s3(locations, s3_bucket, filename)
+
+    if len(locations) >= limit:
+        BaseETL.csv_to_s3(locations.sample(n=limit), s3_bucket, filename)
+    else:
+        BaseETL.csv_to_s3(locations, s3_bucket, filename)
 
     _logger.info('m=crawl_cpfs, starting job...')
-    crawl_cpfs.start_batch_job(
+    crawler_cpfs.start_batch_job(
         job_name='crawl_' + suffix.split('.')[0].replace('-', '_'),
         job_queue='crawling-cpfs',
         job_definition='crawling-cpfs:1',
--- bietlejuice/jobs/new_etl/crawlers/crawler_cpfs.py
+++ bietlejuice/jobs/new_etl/crawlers/crawler_cpfs.py
@@ -3,10 +3,12 @@
 import re
 from datetime import datetime, timedelta
 
+import pandas as pd
+from qa_python_utils.default_logger import logger, _logger
+
 from bietlejuice.jobs.base.base_etl import BaseETL
 from bietlejuice.jobs.new_etl import DATALAKE_QUERIES_DIR
 from bietlejuice.jobs.new_etl.crawlers.crawler_entity import CrawlerEntity
-from qa_python_utils.default_logger import logger, _logger
 
 
 class CrawlerCPFs(CrawlerEntity):
@@ -14,24 +16,50 @@ class CrawlerCPFs(CrawlerEntity):
     def __init__(self, s3_bucket, google_maps_api_key):
         super(CrawlerCPFs, self).__init__(s3_bucket=s3_bucket, google_maps_api_key=google_maps_api_key)
 
+    def parse_address(self, data):
+        pattern = re.compile(r'-(.*)-n-(\d+)|-(.*)')
+        data['street_name'] = data.street.apply(lambda x: self._search_pattern(x, pattern, 1))
+        data['street_name'] = data.street_name.combine_first(
+            data.street.apply(lambda x: self._search_pattern(x, pattern, 3)))
+        data['street_name'] = data.street_name.apply(lambda x: x.replace('-', ' ') if x is not None else None)
+        data['street_number'] = data.street.apply(lambda x: self._search_pattern(x, pattern, 2))
+
+        return data
+
     @logger(exclude='data')
-    def fill_in(self, data):
-        street_pattern = re.compile(r'-(.*)-n-(\d+)|-(.*)')
-        data['street_name'] = data.street.apply(lambda x: CrawlerEntity._search_pattern(x, street_pattern, 1))
-        data.street_name = data.street_name.combine_first(
-            data.street.apply(lambda x: CrawlerEntity._search_pattern(x, street_pattern, 3)))
-        data.street_name = data.street_name.apply(lambda x: x.replace('-', ' ') if x is not None else None)
-        data['street_number'] = data.street.apply(lambda x: CrawlerEntity._search_pattern(x, street_pattern, 2))
+    def fill_in(self, data, limit, debug=False):
+        data = self.parse_address(data)
 
-        for i, row in data.query("""street_name.isnull() or street_number.isnull()""").iterrows():
-            try:
-                street, number = self.reverse_geocode(row.lat, row.lng)
-                data.loc[i, 'street_name'] = ' '.join(street.split(' ')[1:])
-                data.loc[i, 'street_number'] = number
-            except Exception:
-                _logger.warn('m=fill_in, could not retrieve street and number.')
+        to_fill = """street_name.isnull() or street_number.isnull()"""
+        as_ref = """~street_name.isnull() and ~street_number.isnull()"""
 
-        return data.query("""~street_name.isnull() and ~street_number.isnull()""")
+        n_locations = len(data.query(as_ref).groupby(['street_name', 'street_number']).size())
+
+        if debug is False and n_locations <= limit:
+            df_to_fill = data.query(to_fill)
+            for i, row in df_to_fill.iterrows():
+                try:
+                    df_as_ref = data.query(as_ref)
+                    actual_address = df_as_ref.loc[
+                        (df_as_ref.lat == row.lat) & (df_as_ref.lng == row.lng), ['street_name', 'street_number']]
+
+                    if actual_address.empty:
+                        street, number = self.reverse_geocode(row.lat, row.lng)
+                        street = ' '.join(street.split(' ')[1:])
+                        n_locations += 1
+                    else:
+                        street = actual_address.iloc[0].street_name
+                        number = actual_address.iloc[0].street_number
+
+                    data.loc[i, 'street_name'] = street.lower()
+                    data.loc[i, 'street_number'] = number
+                except Exception:
+                    _logger.warn('m=fill_in, could not retrieve street and number.')
+
+                if n_locations >= limit:
+                    break
+
+        return data.query(as_ref)
 
     @logger
     def get_locations(self, ws, delta_days):
@@ -54,9 +82,12 @@ class CrawlerCPFs(CrawlerEntity):
                                         locations.regions.isin(self.house_allowed))]
 
     @logger(exclude='locations')
-    def check_neighborhoods(self, locations, neighbourhood):
-        if neighbourhood is None:
-            return None
+    def check_neighborhoods(self, locations, neighborhood):
+        if neighborhood is None:
+            return pd.DataFrame(columns=locations.columns)
 
-        neighbourhood = self.sanitize_text(neighbourhood)
-        return locations[locations.neighborhood == neighbourhood]
+        if not isinstance(neighborhood, list):
+            neighborhood = [neighborhood]
+
+        neighborhood = [self.sanitize_text(n) for n in neighborhood]
+        return locations[locations.neighborhood.isin(neighborhood)]
