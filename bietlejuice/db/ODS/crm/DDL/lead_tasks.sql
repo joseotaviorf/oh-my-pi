c30bab81db532f1f70001818ca32821ce840a209
dgolhiardi <dgolhiardi@gmail.com>
2018-06-05 11:37:34 -0300

    fixed wrong order and filter on CRM tasks

--- bietlejuice/db/ODS/crm/DDL/lead_tasks.sql
+++ bietlejuice/db/ODS/crm/DDL/lead_tasks.sql
@@ -4,9 +4,9 @@ CREATE TABLE crm.lead_tasks (
 	task_id varchar(255) NULL,
 	task_status varchar(255) NULL,
 	rep_id int4 NULL,
+	first_rep_id int4 NULL,
 	lead_id int4 NULL,
 	number_of_reschedules int4 NULL,
-	first_rep_id int4 NULL,
 	dt_created timestamp NULL,
 	dt_closed timestamp NULL
 )
@@ -15,3 +15,10 @@ WITH (
 ) ;
 CREATE INDEX dtx ON crm.lead_tasks USING btree (dt_created) ;
 CREATE INDEX lead_idx ON crm.lead_tasks USING btree (lead_id) ;
+
+
+
+
+
+        'task_id', 'task_status', 'rep_id', 'first_rep_id', 'lead_id', 'number_of_reschedules',
+        'dt_created', 'dt_closed'
