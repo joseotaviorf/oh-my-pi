8dae04e14052f1b647e9d6e3c4663916c12223d2
dgolhiardi <dgolhiardi@gmail.com>
2018-06-05 15:31:28 -0300

    fixed organics being considered

--- bietlejuice/db/3.dw/growth/prod/queries/supply/new_listings_landing_bv/prefix_all.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/supply/new_listings_landing_bv/prefix_all.sql
@@ -35,7 +35,7 @@ with all_dates as (
 		  and f.sk_property != -1
 	join dim_lead dl
 		on f.sk_lead = dl.sk_lead
-	where dl.origem = 'OwnerPWA'
+	where dl.origem = 'OwnerPWA' and dl.tipo <> 'Organic'
   order by date_part('year', dl.criado_em), date_part('month', dl.criado_em), date_part('week', dl.criado_em), date_part('day', dl.criado_em)
 ),
 all_dates_last_week as (
@@ -58,7 +58,7 @@ all_dates_last_month as (
   where date_part('year', dl.criado_em) = date_part('year', add_months(current_date, -1))
   		and date_part('month', dl.criado_em) = date_part('month', add_months(current_date, -1))
   		and date_part('day', dl.criado_em) < date_part('day', current_date)
-  		and dl.origem = 'OwnerPWA'
+  		and dl.origem = 'OwnerPWA' and dl.tipo <> 'Organic'
   group by date_part('year', dl.criado_em), date_part('month', dl.criado_em)
   order by date_part('year', dl.criado_em), date_part('month', dl.criado_em)
 ),
@@ -80,7 +80,7 @@ all_dates_last_year as (
   		      and date_part('day', dl.criado_em) < date_part('day', add_months(current_date, -12)))
   		  or date_part('month', dl.criado_em) < date_part('month', add_months(current_date, -12))
   		  )
-  		and dl.origem = 'OwnerPWA'
+  		and dl.origem = 'OwnerPWA' and dl.tipo <> 'Organic'
   group by date_part('year', dl.criado_em)
   order by date_part('year', dl.criado_em)
 ),
