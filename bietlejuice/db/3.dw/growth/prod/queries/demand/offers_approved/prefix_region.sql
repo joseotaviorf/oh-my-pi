c943b3fdee9edfb306762b97d57eddd10177977d
Ribaldo <ribaldorafael@gmail.com>
2018-06-19 17:27:27 -0300

    Replacing dt_approved for dt_analysis (offer_approved)

--- bietlejuice/db/3.dw/growth/prod/queries/demand/offers_approved/prefix_region.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/demand/offers_approved/prefix_region.sql
@@ -1,97 +1,100 @@
 with all_dates as (
 	select distinct
-    date_part('year', dof.dt_approved) as _year,
-    date_part('month', dof.dt_approved) as _month,
-    date_part('week', dof.dt_approved) as _week,
-    date_part('day', dof.dt_approved) as _day,
+    date_part('year', dof.dt_analysis) as _year,
+    date_part('month', dof.dt_analysis) as _month,
+    date_part('week', dof.dt_analysis) as _week,
+    date_part('day', dof.dt_analysis) as _day,
     coalesce(dr.long_region_name, '') as region,
     'QuintoAndar'::varchar as city,
     dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-                                    date_part('year', dof.dt_approved),
-    																date_part('month', dof.dt_approved),
-    																date_part('week', dof.dt_approved),
-    																date_part('day', dof.dt_approved) order by dof.sk_offer asc)
+                                    date_part('year', dof.dt_analysis),
+    																date_part('month', dof.dt_analysis),
+    																date_part('week', dof.dt_analysis),
+    																date_part('day', dof.dt_analysis) order by dof.sk_offer asc)
     	+ dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-    	                                  date_part('year', dof.dt_approved),
-    																		date_part('month', dof.dt_approved),
-    																		date_part('week', dof.dt_approved),
-    																		date_part('day', dof.dt_approved) order by dof.sk_offer desc)
+    	                                  date_part('year', dof.dt_analysis),
+    																		date_part('month', dof.dt_analysis),
+    																		date_part('week', dof.dt_analysis),
+    																		date_part('day', dof.dt_analysis) order by dof.sk_offer desc)
 			- 1 as daily_count,
     dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-                                    date_part('year', dof.dt_approved),
-    																date_part('week', dof.dt_approved) order by dof.sk_offer asc)
+                                    date_part('year', dof.dt_analysis),
+    																date_part('week', dof.dt_analysis) order by dof.sk_offer asc)
     	+ dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-    	                                  date_part('year', dof.dt_approved),
-    																		date_part('week', dof.dt_approved) order by dof.sk_offer desc)
+    	                                  date_part('year', dof.dt_analysis),
+    																		date_part('week', dof.dt_analysis) order by dof.sk_offer desc)
 			- 1 as weekly_count,
     dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-                                    date_part('year', dof.dt_approved),
-    																date_part('month', dof.dt_approved) order by dof.sk_offer asc)
+                                    date_part('year', dof.dt_analysis),
+    																date_part('month', dof.dt_analysis) order by dof.sk_offer asc)
     	+ dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-    	                                  date_part('year', dof.dt_approved),
-    																		date_part('month', dof.dt_approved) order by dof.sk_offer desc)
+    	                                  date_part('year', dof.dt_analysis),
+    																		date_part('month', dof.dt_analysis) order by dof.sk_offer desc)
 			- 1 as monthly_count,
     dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-                                    date_part('year', dof.dt_approved) order by dof.sk_offer asc)
+                                    date_part('year', dof.dt_analysis) order by dof.sk_offer asc)
     	+ dense_rank() over (partition by coalesce(dr.long_region_name, ''),
-    	                                  date_part('year', dof.dt_approved) order by dof.sk_offer desc)
+    	                                  date_part('year', dof.dt_analysis) order by dof.sk_offer desc)
 			- 1 as yearly_count
 	from fact_demand f
 	join dim_offer dof
 		on f.sk_offer = dof.sk_offer
-			and dof.dt_approved >= '2017-01-01' and dof.dt_approved < current_date
+			and dof.dt_analysis between '2017-01-01' and current_date - 1
+			and dof.status = 'Aprovada'
 			and f.sk_offer != -1
 	join dim_property dpr
 		on f.sk_house = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-  order by coalesce(dr.long_region_name, ''), date_part('year', dof.dt_approved), date_part('month', dof.dt_approved), date_part('week', dof.dt_approved), date_part('day', dof.dt_approved)
+  order by 5, 1, 2, 3, 4
 ),
 all_dates_last_week as (
 	select 1
 ),
 all_dates_last_month as (
 	select
-    date_part('year', dof.dt_approved) as _year,
-    date_part('month', dof.dt_approved) as _month,
+    date_part('year', dof.dt_analysis) as _year,
+    date_part('month', dof.dt_analysis) as _month,
     coalesce(dr.long_region_name, '') as region,
     'QuintoAndar'::varchar as city,
     count(distinct dof.sk_offer) as monthly_count
 	from fact_demand f
 	join dim_offer dof
 		on f.sk_offer = dof.sk_offer
-			and dof.dt_approved >= '2017-01-01' and dof.dt_approved < current_date
+			and dof.dt_analysis between '2017-01-01' and current_date - 1
+			and dof.status = 'Aprovada'
 			and f.sk_offer != -1
 	join dim_property dpr
   	on f.sk_house = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-	where date_part('year', dof.dt_approved) = date_part('year', add_months(current_date, -1))
-  		and date_part('month', dof.dt_approved) = date_part('month', add_months(current_date, -1))
-  		and date_part('day', dof.dt_approved) < date_part('day', current_date)
- 	group by coalesce(dr.long_region_name, ''), date_part('year', dof.dt_approved), date_part('month', dof.dt_approved)
-  order by coalesce(dr.long_region_name, ''), date_part('year', dof.dt_approved), date_part('month', dof.dt_approved)
+	where date_part('year', dof.dt_analysis) = date_part('year', add_months(current_date, -1))
+  		and date_part('month', dof.dt_analysis) = date_part('month', add_months(current_date, -1))
+  		and date_part('day', dof.dt_analysis) < date_part('day', current_date)
+ 	group by 3, 1, 2
+  order by 3, 1, 2
 ),
 all_dates_last_year as (
 	select
-		date_part('year', dof.dt_approved) as _year,
+		date_part('year', dof.dt_analysis) as _year,
     coalesce(dr.long_region_name, '') as region,
     'QuintoAndar'::varchar as city,
     count(distinct dof.sk_offer) as yearly_count
   from fact_demand f
 	join dim_offer dof
 		on f.sk_offer = dof.sk_offer
-			and dof.dt_approved >= '2017-01-01' and dof.dt_approved < current_date
+			and dof.dt_analysis between '2017-01-01' and current_date - 1
+			and dof.status = 'Aprovada'
 			and f.sk_offer != -1
   join dim_property dpr
   	on f.sk_house = dpr.sk_property
   left join dim_region dr
   	on dpr.regiao_id = dr.id
-	where date_part('year', dof.dt_approved) = date_part('year', add_months(current_date, -12))
-  		and ((date_part('month', dof.dt_approved) = date_part('month', add_months(current_date, -12))
-  		      and date_part('day', dof.dt_approved) < date_part('day', add_months(current_date, -12)))
-  		  or date_part('month', dof.dt_approved) < date_part('month', add_months(current_date, -12))
+	where date_part('year', dof.dt_analysis) = date_part('year', add_months(current_date, -12))
+  		and ((date_part('month', dof.dt_analysis) = date_part('month', add_months(current_date, -12))
+  		      and date_part('day', dof.dt_analysis) < date_part('day', add_months(current_date, -12)))
+  		  or date_part('month', dof.dt_analysis) < date_part('month', add_months(current_date, -12))
   		  )
-	group by coalesce(dr.long_region_name, ''), date_part('year', dof.dt_approved)
-	order by coalesce(dr.long_region_name, ''), date_part('year', dof.dt_approved)
+	group by 2, 1
+	order by 2, 1
 ),
