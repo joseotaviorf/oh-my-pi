4fc8579abc0a6075a57916b9500ac4c1c0b209da
Ribaldo <ribaldorafael@gmail.com>
2018-07-02 18:26:01 -0300

    Refactoring ongoing_contracts

--- bietlejuice/db/3.dw/growth/prod/queries/closing/ongoing_contracts/prefix_all.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/closing/ongoing_contracts/prefix_all.sql
@@ -1,52 +1,51 @@
 with all_dates as (
   select distinct
-    date_part('year', dd."date") as _year,
-    date_part('month', dd."date") as _month,
-    date_part('week', dd."date") as _week,
-    date_part('day', dd."date") as _day,
+    dd.year as _year,
+    dd.month as _month,
+    dd.calendar_week as _week,
+    dd.day as _day,
     'QuintoAndar'::varchar as region,
     'QuintoAndar'::varchar as city,
-    dense_rank() over (partition by date_part('year', dd."date"),
-    																date_part('month', dd."date"),
-    																date_part('week', dd."date"),
-    																date_part('day', dd."date") order by f.sk_contract asc)
-    	+ dense_rank() over (partition by date_part('year', dd."date"),
-    																		date_part('month', dd."date"),
-    																		date_part('week', dd."date"),
-    																		date_part('day', dd."date") order by f.sk_contract desc)
+    dense_rank() over (partition by dd.year,
+    																dd.month,
+    																dd.calendar_week,
+    																dd.day order by f.sk_contract asc)
+    	+ dense_rank() over (partition by dd.year,
+    																		dd.month,
+    																		dd.calendar_week,
+    																		dd.day order by f.sk_contract desc)
 			- 1 as daily_count,
-    dense_rank() over (partition by date_part('year', dd."date"),
-    																date_part('week', dd."date") order by f.sk_contract asc)
-    	+ dense_rank() over (partition by date_part('year', dd."date"),
-    																		date_part('week', dd."date") order by f.sk_contract desc)
+    dense_rank() over (partition by dd.year,
+    																dd.calendar_week order by f.sk_contract asc)
+    	+ dense_rank() over (partition by dd.year,
+    																		dd.calendar_week order by f.sk_contract desc)
 			- 1 as weekly_count,
-    dense_rank() over (partition by date_part('year', dd."date"),
-    																date_part('month', dd."date") order by f.sk_contract asc)
-    	+ dense_rank() over (partition by date_part('year', dd."date"),
-    																		date_part('month', dd."date") order by f.sk_contract desc)
+    dense_rank() over (partition by dd.year,
+    																dd.month order by f.sk_contract asc)
+    	+ dense_rank() over (partition by dd.year,
+    																		dd.month order by f.sk_contract desc)
 			- 1 as monthly_count,
-    dense_rank() over (partition by date_part('year', dd."date") order by f.sk_contract asc)
-    	+ dense_rank() over (partition by date_part('year', dd."date") order by f.sk_contract desc)
+    dense_rank() over (partition by dd.year order by f.sk_contract asc)
+    	+ dense_rank() over (partition by dd.year order by f.sk_contract desc)
 			- 1 as yearly_count
 	from fact_demand f
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
-	where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-  order by date_part('year', dd."date"), date_part('month', dd."date"), date_part('week', dd."date"), date_part('day', dd."date")
+	where dd."date" < current_date
+  order by 1, 2, 3, 4
 ),
 all_dates_last_week as (
 	select 1
 ),
 all_dates_last_month as (
 	select
-	 	date_part('year', dd."date") as _year,
-	  date_part('month', dd."date") as _month,
+	 	dd.year as _year,
+	  dd.month as _month,
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_contract) as monthly_count
@@ -54,21 +53,20 @@ all_dates_last_month as (
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
-  where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-		and date_part('year', dd."date") = date_part('year', add_months(current_date, -1))
-  	and date_part('month', dd."date") = date_part('month', add_months(current_date, -1))
-  	and date_part('day', dd."date") < date_part('day', current_date)
-  group by date_part('year', dd."date"), date_part('month', dd."date")
-  order by date_part('year', dd."date"), date_part('month', dd."date")
+  where dd."date" < current_date
+		and dd.year = date_part('year', add_months(current_date, -1))
+  	and dd.month = date_part('month', add_months(current_date, -1))
+  	and dd.day < date_part('day', current_date)
+  group by 1, 2
+  order by 1, 2
 ),
 all_dates_last_year as (
 	select
-	 	date_part('year', dd."date") as _year,
+	 	dd.year as _year,
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_contract) as yearly_count
@@ -76,17 +74,16 @@ all_dates_last_year as (
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
-	where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-	  and date_part('year', dd."date") = date_part('year', add_months(current_date, -12))
-  		and ((date_part('month', dd."date") = date_part('month', add_months(current_date, -12))
-  		      and date_part('day', dd."date") < date_part('day', add_months(current_date, -12)))
-  		  or date_part('month', dd."date") < date_part('month', add_months(current_date, -12))
+	where dd."date" < current_date
+	  and dd.year = date_part('year', add_months(current_date, -12))
+  		and ((dd.month = date_part('month', add_months(current_date, -12))
+  		      and dd.day < date_part('day', add_months(current_date, -12)))
+  		  or dd.month < date_part('month', add_months(current_date, -12))
   		  )
-  group by date_part('year', dd."date")
-  order by date_part('year', dd."date")
+  group by 1
+  order by 1
 ),
--- bietlejuice/db/3.dw/growth/prod/queries/closing/ongoing_contracts/prefix_city.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/closing/ongoing_contracts/prefix_city.sql
@@ -1,64 +1,63 @@
 with all_dates as (
 	select distinct
-    date_part('year', dd."date") as _year,
-    date_part('month', dd."date") as _month,
-    date_part('week', dd."date") as _week,
-    date_part('day', dd."date") as _day,
+    dd.year as _year,
+    dd.month as _month,
+    dd.calendar_week as _week,
+    dd.day as _day,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
     dense_rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', dd."date"),
-    																date_part('month', dd."date"),
-    																date_part('week', dd."date"),
-    																date_part('day', dd."date") order by f.sk_contract asc)
+                                    dd.year,
+    																dd.month,
+    																dd.calendar_week,
+    																dd.day order by f.sk_contract asc)
     	+ dense_rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', dd."date"),
-    																		date_part('month', dd."date"),
-    																		date_part('week', dd."date"),
-    																		date_part('day', dd."date") order by f.sk_contract desc)
+    	                                  dd.year,
+    																		dd.month,
+    																		dd.calendar_week,
+    																		dd.day order by f.sk_contract desc)
 			- 1 as daily_count,
     dense_rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', dd."date"),
-    																date_part('week', dd."date") order by f.sk_contract asc)
+                                    dd.year,
+    																dd.calendar_week order by f.sk_contract asc)
     	+ dense_rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', dd."date"),
-    																		date_part('week', dd."date") order by f.sk_contract desc)
+    	                                  dd.year,
+    																		dd.calendar_week order by f.sk_contract desc)
 			- 1 as weekly_count,
     dense_rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', dd."date"),
-    																date_part('month', dd."date") order by f.sk_contract asc)
+                                    dd.year,
+    																dd.month order by f.sk_contract asc)
     	+ dense_rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', dd."date"),
-    																		date_part('month', dd."date") order by f.sk_contract desc)
+    	                                  dd.year,
+    																		dd.month order by f.sk_contract desc)
 			- 1 as monthly_count,
     dense_rank() over (partition by coalesce(dr.city_name, ''),
-                                    date_part('year', dd."date") order by f.sk_contract asc)
+                                    dd.year order by f.sk_contract asc)
     	+ dense_rank() over (partition by coalesce(dr.city_name, ''),
-    	                                  date_part('year', dd."date") order by f.sk_contract desc)
+    	                                  dd.year order by f.sk_contract desc)
 			- 1 as yearly_count
 	from fact_demand f
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
 	join dim_property dpr
 		on f.sk_house = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-	where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-  order by coalesce(dr.city_name, ''), date_part('year', dd."date"), date_part('month', dd."date"), date_part('week', dd."date"), date_part('day', dd."date")
+	where dd."date" < current_date
+  order by 6, 1, 2, 3, 4
 ),
 all_dates_last_week as (
 	select 1
 ),
 all_dates_last_month as (
 	select
-    date_part('year', dd."date") as _year,
-    date_part('month', dd."date") as _month,
+    dd.year as _year,
+    dd.month as _month,
     'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
     count(distinct f.sk_contract) as monthly_count
@@ -66,25 +65,24 @@ all_dates_last_month as (
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
 	join dim_property dpr
   	on f.sk_house = dpr.sk_property
 	left join dim_region dr
 		on dpr.regiao_id = dr.id
-	where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-		and date_part('year', dd."date") = date_part('year', add_months(current_date, -1))
-    and date_part('month', dd."date") = date_part('month', add_months(current_date, -1))
-  	and date_part('day', dd."date") < date_part('day', current_date)
- 	group by coalesce(dr.city_name, ''), date_part('year', dd."date"), date_part('month', dd."date")
-  order by coalesce(dr.city_name, ''), date_part('year', dd."date"), date_part('month', dd."date")
+	where dd."date" < current_date
+		and dd.year = date_part('year', add_months(current_date, -1))
+    and dd.month = date_part('month', add_months(current_date, -1))
+  	and dd.day < date_part('day', current_date)
+ 	group by 4, 1, 2
+  order by 4, 1, 2
 ),
 all_dates_last_year as (
 	select
-		date_part('year', dd."date") as _year,
+		dd.year as _year,
 		'QuintoAndar'::varchar as region,
     coalesce(dr.city_name, '') as city,
     count(distinct f.sk_contract) as yearly_count
@@ -92,21 +90,20 @@ all_dates_last_year as (
 	left join dim_contract dc
 		on f.sk_contract = dc.sk_contract
 	right join dim_date dd
-		on dc.dt_contract_start <= dd."date"
+		on dc.contract_status != 'Cancelado'
+		  and dc.dt_signature is not null
+		  and dc.dt_contract_start <= dd."date"
 			and coalesce(dc.dt_contract_annulment, dc.dt_contract_intended_end) >= dd."date"
   join dim_property dpr
   	on f.sk_house = dpr.sk_property
   left join dim_region dr
   	on dpr.regiao_id = dr.id
-	where dc.dt_contract_start < current_date
-		and dc.contract_status != 'Cancelado'
-		and dc.dt_signature is not null
-		and dd."date" >= '2017-01-01' and dd."date" < current_date
-    and date_part('year', dd."date") = date_part('year', add_months(current_date, -12))
-  		and ((date_part('month', dd."date") = date_part('month', add_months(current_date, -12))
-  		      and date_part('day', dd."date") < date_part('day', add_months(current_date, -12)))
-  		  or date_part('month', dd."date") < date_part('month', add_months(current_date, -12))
+	where dd."date" < current_date
+    and dd.year = date_part('year', add_months(current_date, -12))
+  		and ((dd.month = date_part('month', add_months(current_date, -12))
+  		      and dd.day < date_part('day', add_months(current_date, -12)))
+  		  or dd.month < date_part('month', add_months(current_date, -12))
   		  )
-	group by coalesce(dr.city_name, ''), date_part('year', dd."date")
-	order by coalesce(dr.city_name, ''), date_part('year', dd."date")
+	group by 3, 1
+	order by 3, 1
 ),
