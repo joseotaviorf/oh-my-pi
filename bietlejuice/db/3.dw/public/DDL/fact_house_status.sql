84a9773d6b7259cf9af6d3d92c11c8a76d3f7b00
Ribaldo <ribaldorafael@gmail.com>
2018-07-02 14:02:24 -0300

    Removing unecessary id_house from fact

--- bietlejuice/db/3.dw/public/DDL/fact_house_status.sql
+++ bietlejuice/db/3.dw/public/DDL/fact_house_status.sql
@@ -1,7 +1,6 @@
 drop table if exists fact_house_status;
 create table if not exists fact_house_status (
 	sk_house bigint,
-	id_house bigint,
 	status_history varchar,
 	sk_min_status_date integer,
 	sk_max_status_date integer,
