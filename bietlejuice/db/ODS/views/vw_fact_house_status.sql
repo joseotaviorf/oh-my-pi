3bfc0ca2595babf92dcfd23dcfa53ac8a36c529d
Ribaldo <ribaldorafael@gmail.com>
2018-07-02 10:20:54 -0300

    Enhancing vw_fact_house_status

--- bietlejuice/db/ODS/views/vw_fact_house_status.sql
+++ bietlejuice/db/ODS/views/vw_fact_house_status.sql
@@ -4,35 +4,38 @@ with distinct_status as (
 	select distinct
 		id,
 		status_history,
-		status_time::date as min_status_date,
-		next_status_date as max_status_date
+		status_time::date as dt_min_status,
+		next_status_date as dt_max_status
 	from imovel_status_full_history
 ),
 min_max as (
 	select distinct
 		id,
 		status_history,
-		min_status_date,
+		dt_min_status,
 		case
-			when max_status_date = lead(min_status_date) over (partition by id order by min_status_date)
-						and status_history = lead(status_history) over (partition by id order by min_status_date)
-				then lead(max_status_date) over (partition by id order by min_status_date)
-			else max_status_date
-		end as max_status_date,
-		coalesce(not(min_status_date = lag(max_status_date) over (partition by id order by min_status_date)
-				and status_history = lag(status_history) over (partition by id order by min_status_date)), true) as valid
+			when dt_max_status = lead(dt_min_status) over (partition by id order by dt_min_status)
+						and status_history = lead(status_history) over (partition by id order by dt_min_status)
+				then lead(dt_max_status) over (partition by id order by dt_min_status)
+			else dt_max_status
+		end as dt_max_status,
+		coalesce(not(dt_min_status = lag(dt_max_status) over (partition by id order by dt_min_status)
+				and status_history = lag(status_history) over (partition by id order by dt_min_status)), true) as valid
 	from distinct_status
 )
 select
 	coalesce(sdp.sk_property, rpad(mm.id::varchar, 12, '0')::bigint) as sk_property,
-	mm.id,
+	mm.id as id_house,
 	mm.status_history,
-	mm.min_status_date,
-	mm.max_status_date
+	mm.dt_min_status,
+	mm.dt_max_status,
+	to_char(mm.dt_min_status, 'YYYYMMDD')::integer as sk_min_status_date,
+	to_char(mm.dt_max_status, 'YYYYMMDD')::integer as sk_max_status_date,
+	now() as dt_timestamp
 from min_max mm
 left join staging.dim_property sdp
 	on sdp.id = mm.id
-		and mm.min_status_date >= sdp.min_version_time::date
-		and coalesce(mm.max_status_date, now()) <= coalesce(sdp.max_version_time::date, now())
+		and mm.dt_min_status >= sdp.min_version_time::date
+		and coalesce(mm.dt_max_status, now()) <= coalesce(sdp.max_version_time::date, now())
 where mm.valid is true
 ;
