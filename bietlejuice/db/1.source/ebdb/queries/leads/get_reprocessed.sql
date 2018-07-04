986b9dc729ab98717be637e689d9a5abf6776989
ihoelscher <igor.hoelscher@gmail.com>
2018-07-04 16:08:35 -0300

    Optimize query

--- bietlejuice/db/1.source/ebdb/queries/leads/get_reprocessed.sql
+++ bietlejuice/db/1.source/ebdb/queries/leads/get_reprocessed.sql
@@ -1,15 +1,6 @@
-select
-  *
-from
-  (
-    select distinct
-      case
-        when SUBSTRING_INDEX(infosExtras,';',1) REGEXP '^-?[0-9]+$'
-          then CAST(SUBSTRING_INDEX(infosExtras,';',1) AS UNSIGNED INTEGER)
-        else NULL
-      end as id,
-      criadoEm as reprocessed_at
-    from Lead
-    where origem = 'Reprocessado'
-  ) t
-where id is not null
+select distinct
+  CAST(SUBSTRING_INDEX(infosExtras,';',1) AS UNSIGNED INTEGER) as id,
+  criadoEm as reprocessed_at
+from Lead
+where origem = 'Reprocessado'
+  and SUBSTRING_INDEX(infosExtras,';',1) REGEXP '^-?[0-9]+$'
