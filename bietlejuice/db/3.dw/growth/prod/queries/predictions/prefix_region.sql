fde5b3d22e467139c88413f74963d8149b713ff3
Ribaldo <ribaldorafael@gmail.com>
2018-07-06 09:59:02 -0300

    Getting region_code (growth - prediction)

--- bietlejuice/db/3.dw/growth/prod/queries/predictions/prefix_region.sql
+++ bietlejuice/db/3.dw/growth/prod/queries/predictions/prefix_region.sql
@@ -4,7 +4,7 @@ with all_dates as (
     date_part('month', cast(cast("date" as timestamp) as date))::int as _month,
     date_part('week', cast(cast("date" as timestamp) as date))::int as _week,
     date_part('day', cast(cast("date" as timestamp) as date))::int as _day,
-	  region,
+	  region_code as region,
     'QuintoAndar'::varchar as city,
     ({daily_count}::float)::int as daily_count,
     max({weekly_count}::float::int) over (partition by date_part('year', cast(cast("date" as timestamp) as date)), date_part('week', cast(cast("date" as timestamp) as date))) as weekly_count,
@@ -21,7 +21,7 @@ all_dates_last_month as (
 	select distinct
 	  date_part('year', cast(cast("date" as timestamp) as date))::int as _year,
     date_part('month', cast(cast("date" as timestamp) as date))::int as _month,
-	  region,
+	  region_code as region,
 	  'QuintoAndar'::varchar as city,
 	  max({monthly_count}::float::int) over (partition by date_part('year', cast(cast("date" as timestamp) as date)), date_part('month', cast(cast("date" as timestamp) as date))) as monthly_count
   from datalake_raw.growth_{funnel}_prediction
@@ -30,12 +30,12 @@ all_dates_last_month as (
     and date_part('year', cast(cast("date" as timestamp) as date))::int = date_part('year', add_months(current_date, -1))
   	and date_part('month', cast(cast("date" as timestamp) as date))::int = date_part('month', add_months(current_date, -1))
   	and date_part('day', cast(cast("date" as timestamp) as date))::int < date_part('day', current_date)
-  order by region, date_part('year', cast(cast("date" as timestamp) as date)), date_part('month', cast(cast("date" as timestamp) as date))
+  order by 3, 1, 2
 ),
 all_dates_last_year as (
 	select distinct
 	 	date_part('year', cast(cast("date" as timestamp) as date))::int as _year,
-	  region,
+	  region_code as region,
 	  'QuintoAndar'::varchar as city,
   	max({yearly_count}::float::int) over (partition by date_part('year', cast(cast("date" as timestamp) as date))) as yearly_count
 	from datalake_raw.growth_{funnel}_prediction
@@ -46,5 +46,5 @@ all_dates_last_year as (
   		     and date_part('day', cast(cast("date" as timestamp) as date))::int < date_part('day', add_months(current_date, -12)))
   		or date_part('month', cast(cast("date" as timestamp) as date))::int < date_part('month', add_months(current_date, -12))
     )
-  order by region, date_part('year', cast(cast("date" as timestamp) as date))
+  order by 2, 1
 ),
