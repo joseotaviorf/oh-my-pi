dc24298b76244a8a716392445ad26d42097feca8
ihoelscher <igor.hoelscher@gmail.com>
2018-07-11 19:11:30 -0300

    Refactor crawling-cpf job

--- bietlejuice/jobs/dags/crawl_cpfs.py
+++ bietlejuice/jobs/dags/crawl_cpfs.py
@@ -1,11 +1,9 @@
 # coding=utf-8
-
 import json
 from datetime import datetime
 
-from airflow.models import DAG
-from airflow.operators.python_operator import PythonOperator
-from qa_python_utils.default_logger import _logger, logger
+from qa_python_utils.aws.batch import BatchClient
+from qa_python_utils.default_logger import _logger
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_etl import BaseETL
@@ -13,56 +11,36 @@ from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.new_etl.crawlers.crawler_cpfs import CrawlerCPFs
 
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
-data_google_api_key = env.get_airflow_env_var('DATA_GOOGLE_API_KEY')
 crawl_cpfs_params = env.get_airflow_env_var('crawl_cpfs_params')
 
-NO_LOCATIONS_MSG = 'no locations to crawl'
+MAIN_DAG_NAME = 'crawling-cpfs'
+MAIN_START_DATE = datetime(2018, 7, 1)
+MAIN_SCHEDULE_INTERVAL = '0 16 * * 7'
 
 
-@logger
 def crawl_cpfs(**kwargs):
+    delta_days = kwargs.get('delta_days', 0)
     ws = kwargs.get('ws', 'vivareal')
-    neighborhood = kwargs.get('neighborhood')
-    delta_days = kwargs.get('delta_days', 3)
-    limit = kwargs.get('limit', 500)
+    limit = kwargs.get('limit', 0)
 
-    crawler_cpfs = CrawlerCPFs(s3_bucket=s3_bucket, google_maps_api_key=data_google_api_key)
+    crawler_cpfs = CrawlerCPFs(s3_bucket=s3_bucket, google_maps_api_key=None)
     locations_raw = crawler_cpfs.get_locations(ws=ws, delta_days=delta_days)
     if locations_raw.empty:
-        _logger.info('m=get_locations, msg={}'.format(NO_LOCATIONS_MSG))
+        _logger.info('m=get_locations, msg=no locations to crawl')
         return None
 
     _logger.info('m=crawl_cpfs, got {} locations from crawlers'.format(len(locations_raw)))
 
-    locations_cleaned = crawler_cpfs.cleaning(locations_raw)
-    locations_neighbors = crawler_cpfs.check_neighborhoods(locations_cleaned, neighborhood)
-    locations_coverage = crawler_cpfs.check_locations_coverage(locations_neighbors)
-    if locations_coverage.empty:
-        _logger.info('m=crawl_cpfs, msg={}'.format(NO_LOCATIONS_MSG))
-        return None
-
-    _logger.info('m=crawl_cpfs, {} locations after filtering regions'.format(len(locations_coverage)))
-
-    locations = crawler_cpfs.fill_in(locations_coverage, limit)
-    if locations.empty:
-        _logger.info('m=crawl_cpfs, msg={}'.format(NO_LOCATIONS_MSG))
-        return None
-
-    locations_unique = locations.groupby(['street_name', 'street_number']).size().reset_index()
-    locations_unique.columns = ['street_name', 'street_number', 'n_listings']
-    _logger.info('m=crawl_cpfs, saving seed with {} unique street_name and street_number'.format(len(locations_unique)))
-
     last_date = crawler_cpfs.get_last_crawling_date(ws)
-    suffix = '{}-{}-{}.csv'.format(ws, last_date, delta_days)
+    suffix = '{}-{}.csv'.format(last_date, delta_days)
     filename = 'raw/crawled_cpfs/source/' + suffix
-
-    if len(locations) >= limit:
-        BaseETL.csv_to_s3(locations.sample(n=limit), s3_bucket, filename)
+    if 0 < limit <= len(locations_raw):
+        BaseETL.csv_to_s3(locations_raw.sample(n=limit), s3_bucket, filename)
     else:
-        BaseETL.csv_to_s3(locations, s3_bucket, filename)
+        BaseETL.csv_to_s3(locations_raw, s3_bucket, filename)
 
     _logger.info('m=crawl_cpfs, starting job...')
-    crawler_cpfs.start_batch_job(
+    job = BatchClient().start_batch_job(
         job_name='crawl_' + suffix.split('.')[0].replace('-', '_'),
         job_queue='crawling-cpfs',
         job_definition='crawling-cpfs:2',
@@ -71,26 +49,21 @@ def crawl_cpfs(**kwargs):
                       '--max_crawl', '1000000', '--threads', '2']
     )
 
-    return locations
+    _logger.info('m=crawl_cpfs, job submitted with status {}. {}'.format(
+        job.get('status'), '-'.join([job.get('jobId'), job.get('jobName')])))
 
 
 # main dag
-dag = DAG(
-    dag_id='crawling-cpfs',
-    default_args={
-        'owner': BaseDAG.DEFAULT_OWNER,
-        'wait_for_downstream': False,
-        'depends_on_past': False
-    },
-    start_date=datetime(2018, 3, 25, 0, 0, 0),
-    schedule_interval='@once',
-    max_active_runs=1
+dag = BaseDAG.build_dag(
+    dag_id=MAIN_DAG_NAME,
+    start_date=MAIN_START_DATE,
+    schedule_interval=env.convert_to_utc_schedule(MAIN_SCHEDULE_INTERVAL)
 )
 
 # operators
-PythonOperator(
+BaseDAG.get_quintoandar_python_operator(
     dag=dag,
     task_id='crawl-cpfs',
-    python_callable=crawl_cpfs,
+    func_command=crawl_cpfs,
     op_kwargs=json.loads(crawl_cpfs_params)
 )
