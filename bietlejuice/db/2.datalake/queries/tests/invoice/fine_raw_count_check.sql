12fc950a09d0c1d2c43cfd5450d76a19c2b6e095
Ribaldo <ribaldorafael@gmail.com>
2018-07-12 17:28:31 -0300

    Removing api data duplication

--- bietlejuice/db/2.datalake/queries/tests/invoice/fine_raw_count_check.sql
+++ bietlejuice/db/2.datalake/queries/tests/invoice/fine_raw_count_check.sql
@@ -1,4 +1,4 @@
-select count(1)
+select count(*)
 from datalake_raw.seubarriga_invoice_fine
 where ym = '{year_month}'
 ;
--- bietlejuice/db/2.datalake/queries/tests/invoice/report_raw_count_check.sql
+++ bietlejuice/db/2.datalake/queries/tests/invoice/report_raw_count_check.sql
@@ -1,4 +1,9 @@
-select count(1)
-from datalake_raw.seubarriga_invoice
-where ym = '{year_month}'
+-- because the API from Seu Barriga returns duplicate data, the following distinct must be applied
+with _distinct as (
+  select distinct *
+  from datalake_raw.seubarriga_invoice
+  where ym = '{year_month}'
+)
+select count(*)
+from _distinct
 ;
