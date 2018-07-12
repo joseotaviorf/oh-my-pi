45b073063a8d31891ec8f7fd92426be0b09e6455
Ribaldo <ribaldorafael@gmail.com>
2018-07-12 17:31:07 -0300

    Refactoring unit-tests class

--- bietlejuice/jobs/dags/invoice/unit_tests.py
+++ bietlejuice/jobs/dags/invoice/unit_tests.py
@@ -4,9 +4,7 @@ from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_etl import BaseETL
 from bietlejuice.jobs.base.base_test import BaseTest
 from bietlejuice.jobs.base.enum_db import EnumDb
-from bietlejuice.jobs.dags import DATALAKE_RAW_TEST_QUERIES_DIR, DATALAKE_CLEAN_TEST_QUERIES_DIR, ODS_TEST_QUERIES_DIR
-
-COUNT_CHECK_SQL_SUFFIX = 'count_check.sql'
+from bietlejuice.jobs.dags import DATALAKE_TEST_QUERIES_DIR, ODS_TEST_QUERIES_DIR
 
 
 @logger
@@ -33,18 +31,19 @@ def __test_count(**kwargs):
     dl_clean_query = BaseETL.get_query_from_file_name(kwargs['dl_clean_file_path'])
     ods_query = BaseETL.get_query_from_file_name(kwargs['ods_file_path'])
 
+    year_month = '{}-{}'.format(kwargs['execution_date'].year, kwargs['execution_date'].strftime('%m'))
     dl_raw_return = BaseTest.get_query_result_for_comparison(
-        query=dl_raw_query,
+        query=dl_raw_query.format(year_month=year_month),
         from_athena=True
     ).values[0][0] if dl_raw_query != '' else None
 
     dl_clean_return = BaseTest.get_query_result_for_comparison(
-        query=dl_clean_query,
+        query=dl_clean_query.format(year_month=year_month),
         from_athena=True
     ).values[0][0] if dl_clean_query != '' else None
 
     ods_return = BaseTest.get_query_result_for_comparison(
-        query=ods_query,
+        query=ods_query.format(year_month=year_month),
         enum_db=EnumDb.BI_ODS
     )[1][0] if ods_query != '' else None
 
@@ -54,14 +53,18 @@ def __test_count(**kwargs):
 
 @logger
 def __build_test_tasks(local_dag, entity):
+    count_check_sql_suffix = 'count_check.sql'
     return BaseDAG.get_quintoandar_python_operator(
         dag=local_dag,
         task_id='TEST_{}_count'.format(entity),
         func_command=__test_count,
+        provide_context=True,
         op_kwargs={
-            'dl_raw_file_path': '{}/{}_{}'.format(DATALAKE_RAW_TEST_QUERIES_DIR, entity, COUNT_CHECK_SQL_SUFFIX),
-            'dl_clean_file_path': '{}/{}_{}'.format(DATALAKE_CLEAN_TEST_QUERIES_DIR, entity, COUNT_CHECK_SQL_SUFFIX),
-            'ods_file_path': '{}/{}_{}'.format(ODS_TEST_QUERIES_DIR, entity, COUNT_CHECK_SQL_SUFFIX),
+            'dl_raw_file_path': '{}/invoice/{}_raw_{}'.format(DATALAKE_TEST_QUERIES_DIR, entity,
+                                                              count_check_sql_suffix),
+            'dl_clean_file_path': '{}/invoice/{}_clean_{}'.format(DATALAKE_TEST_QUERIES_DIR, entity,
+                                                                  count_check_sql_suffix),
+            'ods_file_path': '{}/invoice/{}_{}'.format(ODS_TEST_QUERIES_DIR, entity, count_check_sql_suffix),
             'acceptable_diff': .0
         }
     )
