df88915ce1d51808c28e633b553e19f55b43e40c
Ribaldo <ribaldorafael@gmail.com>
2018-07-19 19:32:14 -0300

    Renaming counts

--- bietlejuice/db/2.datalake/queries/amplitude/listings_unique_page_views/suffix.sql
+++ bietlejuice/db/2.datalake/queries/amplitude/listings_unique_page_views/suffix.sql
@@ -18,7 +18,7 @@ select distinct
                                         _month,
                                         _week,
                                         _day order by house_id desc)
-			- 1 as _daily,
+			- 1 as daily_count,
     dense_rank() over (partition by region,
                                     city,
                                     _year,
@@ -27,7 +27,7 @@ select distinct
     	                                  city,
     	                                  _year,
     								                    _week order by house_id desc)
-			- 1 as _weekly,
+			- 1 as weekly_count,
     dense_rank() over (partition by region,
                                     city,
                                     _year,
@@ -36,13 +36,13 @@ select distinct
     	                                  city,
     	                                  _year,
     									                  _month order by house_id desc)
-			- 1 as _monthly,
+			- 1 as monthly_count,
     dense_rank() over (partition by region,
                                     city,
                                     _year order by house_id asc)
     	+ dense_rank() over (partition by region,
     	                                  city,
     	                                  _year order by house_id desc)
-			- 1 as _yearly
+			- 1 as yearly_count
 from listings
 ;
