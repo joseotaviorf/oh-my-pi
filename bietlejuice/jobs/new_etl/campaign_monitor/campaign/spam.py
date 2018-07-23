178ed5c74e4af092fe9b655671e430dce40ec83d
Ribaldo <ribaldorafael@gmail.com>
2018-07-23 14:10:10 -0300

    Renaming spam file

---
+++ bietlejuice/jobs/new_etl/campaign_monitor/campaign/spam.py
@@ -0,0 +1,23 @@
+from qa_python_utils.default_logger import logger
+
+from bietlejuice.jobs.new_etl.campaign_monitor.campaign.campaign_monitor import CampaignMonitorCampaign
+
+
+class CampaignSpam(CampaignMonitorCampaign):
+    @logger(exclude='cm_auth')
+    def __init__(self, s3_bucket, cm_auth, _type, execution_date):
+        super(CampaignSpam, self).__init__(s3_bucket, cm_auth, _type, execution_date)
+
+    @logger
+    def request_campaign_data(self):
+        self._delete_old_files()
+
+        result_gen = self._request_incremental_campaign_data()
+        self._build_objs_and_send_to_s3(
+            result_gen=result_gen,
+            json_fields=self.get_json_fields()
+        )
+
+    @logger
+    def get_json_fields(self):
+        return ['Date', 'EmailAddress', 'ListID']
