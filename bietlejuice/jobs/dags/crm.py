fdc1fb1ccccbb02c0d9a1473b7d22c5433a87ab6
Rafael Ribaldo <ribaldorafael@gmail.com>
2018-08-03 10:35:24 -0300

    Setting depends_on_past = True

--- bietlejuice/jobs/dags/crm.py
+++ bietlejuice/jobs/dags/crm.py
@@ -92,7 +92,7 @@ main_dag = DAG(
     default_args={
         'owner': BaseDAG.DEFAULT_OWNER,
         'wait_for_downstream': False,
-        'depends_on_past': False
+        'depends_on_past': True
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
