0431e264c61fc1c04c73c990d70c98e2625ca888
Ribaldo <ribaldorafael@gmail.com>
2018-08-10 11:32:57 -0300

    Refactoring crm dag

--- bietlejuice/jobs/dags/crm.py
+++ bietlejuice/jobs/dags/crm.py
@@ -7,20 +7,60 @@ from airflow.operators.python_operator import ShortCircuitOperator
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_sub_dag import BaseSubDag
 from bietlejuice.jobs.dags.util import environment as env
-from bietlejuice.jobs.new_etl.crm.tasks import CRMTasksFactory
+from bietlejuice.jobs.new_etl.crm.tasks import CRMTasks, CRMTasksFactory
 
 # env vars
 env.set_airflow_var_to_local_env('BI_DW')
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 mongo_client_uri = env.get_airflow_env_var('MONGODB_CRM_URI')
 
-MAIN_DAG_NAME = 'bi-crm-model'
+MAIN_DAG_ID = 'bi-crm-load'
 MAIN_START_DATE = datetime(2015, 1, 1)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 1 * * *')
 
 
 # functions
-def data_existence_check(_class, bucket_type, **kwargs):
+def data_existence_check(bucket_type, **kwargs):
+    crm_tasks = CRMTasks(
+        s3_bucket=s3_bucket,
+        mongo_client_uri=mongo_client_uri,
+        execution_date=kwargs['execution_date']
+    )
+
+    return crm_tasks._data_existence_check(bucket_type)
+
+
+def upsert_partition(bucket_type, method, **kwargs):
+    crm_tasks = CRMTasks(
+        s3_bucket=s3_bucket,
+        mongo_client_uri=mongo_client_uri,
+        execution_date=kwargs['execution_date']
+    )
+
+    getattr(crm_tasks, method)(bucket_type)
+
+
+def extract_and_load_data(**kwargs):
+    crm_tasks = CRMTasks(
+        s3_bucket=s3_bucket,
+        mongo_client_uri=mongo_client_uri,
+        execution_date=kwargs['execution_date']
+    )
+
+    crm_tasks._extract_and_load_data()
+
+
+def exec_crm_method(method, **kwargs):
+    crm_tasks = CRMTasks(
+        s3_bucket=s3_bucket,
+        mongo_client_uri=mongo_client_uri,
+        execution_date=kwargs['execution_date']
+    )
+
+    getattr(crm_tasks, method)()
+
+
+def exec_factory_method(_class, method, **kwargs):
     crm_tasks = CRMTasksFactory.factory(
         _class=_class,
         s3_bucket=s3_bucket,
@@ -28,67 +68,12 @@ def data_existence_check(_class, bucket_type, **kwargs):
         execution_date=kwargs['execution_date']
     )
 
-    crm_tasks._data_existence_check(bucket_type)
-
-
-def upsert_partition(_class, bucket_type, **kwargs):
-    crm_tasks = CRMTasksFactory.factory(
-        _class=_class,
-        s3_bucket=s3_bucket,
-        mongo_client_uri=mongo_client_uri,
-        execution_date=kwargs['execution_date']
-    )
-
-    crm_tasks._upsert_partition(bucket_type)
-
-
-def extract_and_load_data(_class, **kwargs):
-    crm_tasks = CRMTasksFactory.factory(
-        _class=_class,
-        s3_bucket=s3_bucket,
-        mongo_client_uri=mongo_client_uri,
-        execution_date=kwargs['execution_date']
-    )
-
-    crm_tasks.extract_and_load_data()
-
-
-def move_to_clean(_class, **kwargs):
-    crm_tasks = CRMTasksFactory.factory(
-        _class=_class,
-        s3_bucket=s3_bucket,
-        mongo_client_uri=mongo_client_uri,
-        execution_date=kwargs['execution_date']
-    )
-
-    crm_tasks.move_to_clean()
-
-
-def load_dim(_class, **kwargs):
-    crm_tasks = CRMTasksFactory.factory(
-        _class=_class,
-        s3_bucket=s3_bucket,
-        mongo_client_uri=mongo_client_uri,
-        execution_date=kwargs['execution_date']
-    )
-
-    crm_tasks._load_dim()
-
-
-def load_fact(_class, **kwargs):
-    crm_tasks = CRMTasksFactory.factory(
-        _class=_class,
-        s3_bucket=s3_bucket,
-        mongo_client_uri=mongo_client_uri,
-        execution_date=kwargs['execution_date']
-    )
-
-    crm_tasks._load_fact()
+    getattr(crm_tasks, method)()
 
 
 # dags
 main_dag = DAG(
-    dag_id=MAIN_DAG_NAME,
+    dag_id=MAIN_DAG_ID,
     default_args={
         'owner': BaseDAG.DEFAULT_OWNER,
         'wait_for_downstream': False,
@@ -104,102 +89,219 @@ def class_sub_dag(sub_dag_name, **kwargs):
     local_dag = BaseSubDag(
         bucket=s3_bucket,
         sub_dag_name=sub_dag_name,
-        dag_name=MAIN_DAG_NAME,
+        dag_name=MAIN_DAG_ID,
         schedule_interval=MAIN_SCHEDULE_INTERVAL,
         start_date=MAIN_START_DATE
     )._build_local_dag()
 
-    extract_and_load_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='extract_and_load',
-        func_command=extract_and_load_data,
-        dag=local_dag,
-        provide_context=True,
-        op_kwargs={
-            '_class': kwargs['_class']
-        }
-    )
-
-    data_existence_check_task = ShortCircuitOperator(
-        task_id='data_existence_check',
-        python_callable=data_existence_check,
+    move_dim_to_staging_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='move_dim_to_staging',
+        func_command=exec_factory_method,
         dag=local_dag,
         provide_context=True,
         op_kwargs={
             '_class': kwargs['_class'],
-            'bucket_type': 'raw'
+            'method': 'move_dim_to_staging'
         }
     )
 
-    upsert_raw_partition_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='upsert_raw_partition',
-        func_command=upsert_partition,
+    move_fact_to_staging_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='move_fact_to_staging',
+        func_command=exec_factory_method,
         dag=local_dag,
         provide_context=True,
         op_kwargs={
             '_class': kwargs['_class'],
-            'bucket_type': 'raw'
+            'method': 'move_fact_to_staging'
         }
     )
 
-    move_to_clean_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='move_to_clean',
-        func_command=move_to_clean,
-        dag=local_dag,
-        provide_context=True,
-        op_kwargs={
-            '_class': kwargs['_class']
-        }
-    )
-
-    upsert_clean_partition_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='upsert_clean_partition',
-        func_command=upsert_partition,
+    move_dim_to_dw_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='append_dim_to_dw',
+        func_command=exec_factory_method,
         dag=local_dag,
         provide_context=True,
         op_kwargs={
             '_class': kwargs['_class'],
-            'bucket_type': 'clean'
+            'method': 'append_dim_to_dw'
         }
     )
 
-    load_dim_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='load_dim',
-        func_command=load_dim,
+    move_fact_to_dw_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='append_fact_to_dw',
+        func_command=exec_factory_method,
         dag=local_dag,
         provide_context=True,
         op_kwargs={
-            '_class': kwargs['_class']
+            '_class': kwargs['_class'],
+            'method': 'append_fact_to_dw'
         }
     )
 
-    load_fact_task = BaseDAG.get_quintoandar_python_operator(
-        task_id='load_fact',
-        func_command=load_fact,
+    delete_staging_fact_entries_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='delete_staging_fact_entries',
+        func_command=exec_factory_method,
         dag=local_dag,
         provide_context=True,
         op_kwargs={
-            '_class': kwargs['_class']
+            '_class': kwargs['_class'],
+            'method': 'delete_staging_fact_entries'
+        }
+    )
+
+    delete_staging_dim_entries_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='delete_staging_dim_entries',
+        func_command=exec_factory_method,
+        dag=local_dag,
+        provide_context=True,
+        op_kwargs={
+            '_class': kwargs['_class'],
+            'method': 'delete_staging_dim_entries'
         }
     )
 
     airflow_helpers.chain(
-        extract_and_load_task,
-        data_existence_check_task,
-        upsert_raw_partition_task,
-        move_to_clean_task,
-        upsert_clean_partition_task
+        move_dim_to_staging_task,
+        move_dim_to_dw_task,
+        delete_staging_dim_entries_task
     )
-    upsert_clean_partition_task.set_downstream([load_dim_task, load_fact_task])
+
+    airflow_helpers.chain(
+        move_fact_to_staging_task,
+        move_fact_to_dw_task,
+        delete_staging_fact_entries_task
+    )
+
+    return local_dag
+
+
+def clean_tasks_sub_dag(sub_dag_name, **kwargs):
+    local_dag = BaseSubDag(
+        bucket=s3_bucket,
+        sub_dag_name=sub_dag_name,
+        dag_name=MAIN_DAG_ID,
+        schedule_interval=MAIN_SCHEDULE_INTERVAL,
+        start_date=MAIN_START_DATE
+    )._build_local_dag()
+
+    move_tasks_to_clean_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='move_tasks_to_clean',
+        func_command=exec_crm_method,
+        dag=local_dag,
+        provide_context=True,
+        op_kwargs={
+            'method': '_move_tasks_to_clean'
+        }
+    )
+
+    upsert_tasks_clean_partition_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='upsert_tasks_clean_partition',
+        func_command=upsert_partition,
+        dag=local_dag,
+        provide_context=True,
+        op_kwargs={
+            'bucket_type': 'clean',
+            'method': '_upsert_tasks_partition'
+        }
+    )
+
+    move_tasks_to_clean_task >> upsert_tasks_clean_partition_task
+
+    return local_dag
+
+
+def clean_task_resolution_sub_dag(sub_dag_name, **kwargs):
+    local_dag = BaseSubDag(
+        bucket=s3_bucket,
+        sub_dag_name=sub_dag_name,
+        dag_name=MAIN_DAG_ID,
+        schedule_interval=MAIN_SCHEDULE_INTERVAL,
+        start_date=MAIN_START_DATE
+    )._build_local_dag()
+
+    move_tasks_resolution_to_clean_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='move_tasks_resolution_to_clean',
+        func_command=exec_crm_method,
+        dag=local_dag,
+        provide_context=True,
+        op_kwargs={
+            'method': '_move_tasks_resolution_to_clean'
+        }
+    )
+
+    upsert_tasks_resolution_clean_partition_task = BaseDAG.get_quintoandar_python_operator(
+        task_id='upsert_tasks_resolution_clean_partition',
+        func_command=upsert_partition,
+        dag=local_dag,
+        provide_context=True,
+        op_kwargs={
+            'bucket_type': 'clean',
+            'method': '_upsert_tasks_resolution_partition'
+        }
+    )
+
+    move_tasks_resolution_to_clean_task >> upsert_tasks_resolution_clean_partition_task
 
     return local_dag
 
 
 # operators
+extract_and_load_task = BaseDAG.get_quintoandar_python_operator(
+    task_id='extract_and_load',
+    func_command=extract_and_load_data,
+    dag=main_dag,
+    provide_context=True
+)
+
+data_existence_check_task = ShortCircuitOperator(
+    task_id='data_existence_check',
+    python_callable=data_existence_check,
+    dag=main_dag,
+    provide_context=True,
+    op_kwargs={
+        'bucket_type': 'raw'
+    }
+)
+
+upsert_raw_partition_task = BaseDAG.get_quintoandar_python_operator(
+    task_id='upsert_raw_partition',
+    func_command=upsert_partition,
+    dag=main_dag,
+    provide_context=True,
+    op_kwargs={
+        'bucket_type': 'raw',
+        'method': '_upsert_tasks_partition'
+    }
+)
+
+clean_tasks_sub_dag_task = BaseSubDag.get_sub_dag_operator(
+    dag=main_dag,
+    sub_dag_name='create_clean_tasks_table',
+    sub_dag_func=clean_tasks_sub_dag,
+)
+
+clean_tasks_resolution_sub_dag_task = BaseSubDag.get_sub_dag_operator(
+    dag=main_dag,
+    sub_dag_name='create_clean_tasks_resolution_table',
+    sub_dag_func=clean_task_resolution_sub_dag,
+)
+
 tasks_credit_sub_dag = BaseSubDag.get_sub_dag_operator(
     dag=main_dag,
     sub_dag_name='tasks_credit',
     sub_dag_func=class_sub_dag,
     _class='credit'
 )
+
+# flow
+airflow_helpers.chain(
+    extract_and_load_task,
+    data_existence_check_task,
+    upsert_raw_partition_task,
+    clean_tasks_sub_dag_task,
+    clean_tasks_resolution_sub_dag_task
+)
+
+clean_tasks_resolution_sub_dag_task.set_downstream([tasks_credit_sub_dag])
 
 # TODO: add unit tests
