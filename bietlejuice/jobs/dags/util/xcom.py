fc325933b4c9d1cb05ec5d57ee528d9448f2d100
felipemiquelim <felipemiquelim@gmail.com>
2018-08-21 19:03:34 -0300

    Adjusting xcom parameters

--- bietlejuice/jobs/dags/util/xcom.py
+++ bietlejuice/jobs/dags/util/xcom.py
@@ -1,15 +1,13 @@
 import logging
 
 
-def xcom_push(task_instance, key):
-    k_value = True
-
+def xcom_push(task_instance, key, k_value=True):
     # Executes airflow interdag communication
     logging.info('Creating Xcom: key={} | value={}'.format(key, str(k_value)))
     task_instance.xcom_push(key=key, value=k_value)
 
 
-def xcom_pull(task_instance, key, task_id, dag_id, include_prior_dates=True):
+def xcom_pull(task_instance, key=None, task_id=None, dag_id=None, include_prior_dates=True):
     # Executes airflow interdag communication, gathering previous messages
     logging.info('Getting Xcom: key={} | task_id={} | dag_id={}'.format(key, task_id, dag_id))
     status = task_instance.xcom_pull(key=key, task_ids=task_id, dag_id=dag_id,
