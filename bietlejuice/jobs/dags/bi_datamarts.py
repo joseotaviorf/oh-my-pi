591dbbd90af1caa44a3067c640f55a5e2a552c7c
Fernando Paiva <fernandosjp@gmail.com>
2018-08-23 12:42:08 -0300

    Apply flake standard

--- bietlejuice/jobs/dags/bi_datamarts.py
+++ bietlejuice/jobs/dags/bi_datamarts.py
@@ -1,7 +1,6 @@
 import os
 from datetime import datetime
 
-import airflow.utils.helpers as airflow_helpers
 from airflow.models import DAG
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
@@ -22,28 +21,29 @@ MAIN_SCHEDULE_INTERVAL = None
 
 # functions
 
+
 @logger
 def create_datamart(table_name, **kwargs):
     """
     """
     schema = "datamarts"
-    query = BaseETL.get_query_from_file_name('{}/{}.sql'.format(DATAMART_QUERIES_DIR,table_name))
+    query = BaseETL.get_query_from_file_name('{}/{}.sql'.format(DATAMART_QUERIES_DIR, table_name))
 
     _logger.info("m=create_datamart, table_name={}, msg=Dropping table".format(table_name))
     BaseETL.execute_command(
-            command='drop table if exists {}.{}'.format(schema, table_name),
-            db_enum=EnumDb.BI_DW,
-            encoding='utf-8',
-            commit=True
-        )
+        command='drop table if exists {}.{}'.format(schema, table_name),
+        db_enum=EnumDb.BI_DW,
+        encoding='utf-8',
+        commit=True
+    )
 
     _logger.info("m=create_datamart, table_name={}, msg=Creating table".format(table_name))
     BaseETL.execute_command(
-            command='create table {}.{} as ({})'.format(schema, table_name, query),
-            db_enum=EnumDb.BI_DW,
-            encoding='utf-8',
-            commit=True
-        )
+        command='create table {}.{} as ({})'.format(schema, table_name, query),
+        db_enum=EnumDb.BI_DW,
+        encoding='utf-8',
+        commit=True
+    )
 
 
 # dags
@@ -66,7 +66,7 @@ for filename in os.listdir(DATAMART_QUERIES_DIR):
 
     filename_split = filename.split(".")
 
-    if len(filename_split)<1:
+    if len(filename_split) < 1:
         _logger.warn("m=dag_run, filename={}, msg=no file extension".format(filename))
         continue
 
@@ -77,7 +77,7 @@ for filename in os.listdir(DATAMART_QUERIES_DIR):
     table_name = filename_split[0]
 
     PythonOperator(
-        task_id= table_name,
+        task_id=table_name,
         provide_context=True,
         python_callable=create_datamart,
         dag=main_dag,
