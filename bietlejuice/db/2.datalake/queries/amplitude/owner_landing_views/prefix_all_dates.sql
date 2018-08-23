9cafedcddbfe4af1a0b2a8d53b9b66679d15b162
felipemiquelim <felipemiquelim@gmail.com>
2018-08-23 09:48:14 -0300

    Trim

--- bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views/prefix_all_dates.sql
+++ bietlejuice/db/2.datalake/queries/amplitude/owner_landing_views/prefix_all_dates.sql
@@ -18,5 +18,5 @@ with all_events as (
       or (trim(app) = '160023' and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast('2017-08-23' as date)))
     and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast(now() as date)
     and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-01-01' as date)
-    and not(trim(e_ub_page_variant) in ('bv','cj', 'co') and trim(e_ub_page_name) = 'Proprietário')
+    and not(trim(e_ub_page_variant) in ('bv','cj','co') and trim(e_ub_page_name) = 'Proprietário')
 )
