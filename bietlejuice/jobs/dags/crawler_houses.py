2aef676f97c072d21111d6d4fc905e5f58fc64fd
felipemiquelim <felipemiquelim@gmail.com>
2018-08-24 16:09:11 -0300

    Adjusting names and variables

--- bietlejuice/jobs/dags/crawler_houses.py
+++ bietlejuice/jobs/dags/crawler_houses.py
@@ -7,6 +7,7 @@ from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.dags.util import xcom as xcom
 from bietlejuice.jobs.sensors.aws_batch_sensor import QuintoAndarAWSBatchSensor
+from qa_python_utils.aws.batch import BatchClient
 from qa_python_utils.default_logger import _logger
 
 MAIN_DAG_NAME = 'crawler_houses'
@@ -14,6 +15,7 @@ MAIN_START_DATE = datetime(2018, 3, 20)
 MAIN_SCHEDULE_INTERVAL = '0 0 1/3 * *'
 MAX_TIMES_RUN = 20
 
+s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 crawler_params = env.get_airflow_env_var('CRAWLING_HOUSES_PARAMS')
 dict_params = json.loads(crawler_params)
 
@@ -23,25 +25,26 @@ def start_crawler(**kwargs):
     states = kwargs.get('states')
     source = kwargs.get('source')
     ti = kwargs.get('ti')
-    print(kwargs)
 
     assert isinstance(max_crawl, int)
     assert isinstance(states, list)
 
-    _logger.info('Starting job {0}...'.format(source))
-    # r = BatchClient().start_batch_job(
-    #     job_name='crawl-{0}'.format(source),
-    #     job_queue='crawling-houses',
-    #     job_definition='crawling-houses:10',
-    #     command=['./crawlers/{0}.py'.format(source), '--max_crawl', str(max_crawl), '--states'] + states
-    # )
-    # _logger.info('Finished with status {}. {}'.format(r.get('status'), '-'.join([r.get('jobId'), r.get('jobName')])))
+    _logger.info('m=start_crawler, source={0}, msg=starting job...'.format(source))
+    r = BatchClient().start_batch_job(
+        job_name='crawl-{0}'.format(source),
+        job_queue='crawling-houses',
+        job_definition='crawling-houses:10',
+        command=['./crawlers/{0}.py'.format(source), '--max_crawl', str(max_crawl), '--s3_bucket', s3_bucket,
+                 '--states'] + states
+    )
+    _logger.info('m=start_crawler, status={}, job={}, msg=finished.'.format(r.get('status'), '-'.join(
+        [r.get('jobId'), r.get('jobName')])))
 
-    if source == 'proxy':
+    if source == 'proxies':
         exec_date = str(datetime.date(kwargs.get('execution_date')))
         xcom.xcom_push(ti,
                        key='crawler_houses_proxy_{}'.format(exec_date),
-                       k_value='69624e04-7ae0-4dbe-ae6b-b2d5fce9cabb')  # r.get('jobId'))
+                       k_value=r.get('jobId'))
 
 
 dag = DAG(
@@ -62,7 +65,7 @@ crawl_proxies = BaseDAG.get_quintoandar_python_operator(
     task_id='crawl-proxies',
     func_command=start_crawler,
     provide_context=True,
-    op_kwargs=dict(dict_params.items() + ({'source': 'proxy'}).items())
+    op_kwargs=dict(dict_params.items() + ({'source': 'proxies'}).items())
 )
 
 crawl_imovelweb = BaseDAG.get_quintoandar_python_operator(
@@ -86,13 +89,13 @@ crawl_zap = BaseDAG.get_quintoandar_python_operator(
     task_id='crawl-zapimoveis',
     func_command=start_crawler,
     provide_context=True,
-    op_kwargs=dict(dict_params.items() + ({'source': 'zap'}).items())
+    op_kwargs=dict(dict_params.items() + ({'source': 'zapimoveis'}).items())
 )
 
 proxy_success_test = QuintoAndarAWSBatchSensor(
     task_id='proxy_success_test',
-    poke_interval=10,
-    timeout=100,
+    poke_interval=30,
+    timeout=3600,
     provide_context=True,
     xcom_task_id='crawl-proxies'
 )
