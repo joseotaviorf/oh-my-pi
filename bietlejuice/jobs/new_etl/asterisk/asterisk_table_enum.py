065e0e3f7ba441ebf7d5d8e38cb1b34464b049d2
Ribaldo <ribaldorafael@gmail.com>
2018-08-30 11:31:38 -0300

    Adding new asterisk class and enum

---
+++ bietlejuice/jobs/new_etl/asterisk/asterisk_table_enum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class AsteriskTableEnum(Enum):
+    IVR_DETAILS = 'ivr_details'
+    IVR_ENTRIES = 'ivr_entries'
+    USERS = 'users'
+    DEVICES = 'devices'
+    QUEUES_CONFIG = 'queues_config'
+    QUEUES_DETAILS = 'queues_details'
+    CXPANEL_QUEUES = 'cxpanel_queues'
+    CXPANEL_USERS = 'cxpanel_users'
+    CDR = 'cdr'
