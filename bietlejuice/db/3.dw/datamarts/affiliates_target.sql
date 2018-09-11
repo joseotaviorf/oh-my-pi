1f2ba3513b1f8257ffd402b0fb6fc7307bc2be62
felipemiquelim <felipemiquelim@gmail.com>
2018-09-11 15:04:36 -0300

    Adjusting types on query

--- bietlejuice/db/3.dw/datamarts/affiliates_target.sql
+++ bietlejuice/db/3.dw/datamarts/affiliates_target.sql
@@ -1,4 +1,8 @@
 SELECT
-    *
+    day::bigint,
+    indicaai_target_listings::bigint,
+    doorman_target_listings::bigint,
+    indicaai_target_leads::bigint,
+    doorman_target_leads::bigint
 FROM
     datalake_raw.affiliates_target
