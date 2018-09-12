e44b173ae4adc8ac8ff357d1dad0a9236e090878
Igor Mossinato <igormossinato@quintoanda.com.br>
2018-09-12 16:36:12 -0300

    fixing typo

--- bietlejuice/db/3.dw/public/queries/fact_agent.sql
+++ bietlejuice/db/3.dw/public/queries/fact_agent.sql
@@ -19,7 +19,7 @@ first_visits AS
 available_next_days AS
 (SELECT
 	t.agent_id AS sk_agent_id,
-	case when sum(case when t.available_slot = 1 AND coalesce(t.last_change_reason,'') <> 'day off' then cast(t.available_slot_24h AS INTEGER) end) > 0 then 1 else 0 end AS flg_available_next_daysgit
+	case when sum(case when t.available_slot = 1 AND coalesce(t.last_change_reason,'') <> 'day off' then cast(t.available_slot_24h AS INTEGER) end) > 0 then 1 else 0 end AS flg_available_next_days
  FROM staging.agents_slots t
  WHERE DATE(t.slot_dt) BETWEEN DATEADD(DAY, 1, DATE('{0}')) AND DATEADD(DAY, 4, DATE('{0}'))
 GROUP BY 1
