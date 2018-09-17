0b22933224a0edd07a1644dc922f22240b057d86
Ribaldo <ribaldorafael@gmail.com>
2018-09-17 14:13:31 -0300

    Renaming ODS to ods and .sql to .ddl in DDL folders

--- bietlejuice/jobs/dags/__init__.py
+++ bietlejuice/jobs/dags/__init__.py
@@ -3,10 +3,11 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DW_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/public/queries/tests')
 DW_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/public/queries/tests')
-ODS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/ODS/queries/tests')
+ODS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/ods/queries/tests')
 DATALAKE_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/datalake/queries/tests')
 DATALAKE_QUERIES_DIR = os.path.join(dir_path, '../../db/datalake/queries')
 SOURCE_QUERIES_TESTS_DIR = os.path.join(dir_path, '../../db/source/queries/tests')
 SOURCE_QUERIES_DIR = os.path.join(dir_path, '../../db/source/queries/')
 
 DEFAULT_DAG_OWNER = 'Data Team'
+''
