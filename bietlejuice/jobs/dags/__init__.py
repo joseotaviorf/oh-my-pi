f463040079cde7ff273c608b5b8aa1ee85bac62c
Ribaldo <ribaldorafael@gmail.com>
2018-09-17 15:49:03 -0300

    Removing public path from dw queries

--- bietlejuice/jobs/dags/__init__.py
+++ bietlejuice/jobs/dags/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-DW_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/public/queries/tests')
-DW_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/public/queries/tests')
+DW_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/queries/')
+DW_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/dw/queries/tests')
 ODS_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/ods/queries/tests')
 DATALAKE_TEST_QUERIES_DIR = os.path.join(dir_path, '../../db/datalake/queries/tests')
 DATALAKE_QUERIES_DIR = os.path.join(dir_path, '../../db/datalake/queries')
@@ -10,4 +10,3 @@ SOURCE_QUERIES_TESTS_DIR = os.path.join(dir_path, '../../db/source/queries/tests
 SOURCE_QUERIES_DIR = os.path.join(dir_path, '../../db/source/queries/')
 
 DEFAULT_DAG_OWNER = 'Data Team'
-''
