27bf622d0d64f84160db8f3093cae04f139048f6
Ribaldo <ribaldorafael@gmail.com>
2018-09-21 09:59:41 -0300

    Removing integer cast from asterisk queries

--- bietlejuice/db/datalake/queries/asterisk/create_cxpanel_users_table.sql
+++ bietlejuice/db/datalake/queries/asterisk/create_cxpanel_users_table.sql
@@ -1,15 +1,15 @@
 select
-  cast(cxpanel_user_id as integer) as cxpanel_user_id,
-  cast(user_id as integer) as user_id,
+  cxpanel_user_id,
+  user_id,
   display_name,
   peer,
-  cast(add_extension as integer) as add_extension,
-  cast("full" as integer) as "full",
-  cast(add_user as integer) as add_user,
+  add_extension,
+  "full",
+  add_user,
   hashed_password,
   initial_password,
-  cast(auto_answer as integer) as auto_answer,
-  cast(nullif(parent_user_id, '') as integer) as parent_user_id,
-  cast(password_dirty as integer) as password_dirty
+  auto_answer,
+  nullif(parent_user_id, '') as parent_user_id,
+  password_dirty
 from datalake_raw.asterisk_cxpanel_users
 ;
