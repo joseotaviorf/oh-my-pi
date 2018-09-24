153626a098e66c12d85f274549612632f75d8bac
Leonardo Santos <3fsleo@gmail.com>
2018-09-24 19:23:57 -0300

    Refactoring to StitchTransferRaw class

--- bietlejuice/jobs/new_etl/stitch/stitch_transfer_raw.py
+++ bietlejuice/jobs/new_etl/stitch/stitch_transfer_raw.py
@@ -1,46 +1,49 @@
-import time
 from datetime import datetime
 
+import s3fs
 from qa_python_utils.aws.athena import AthenaClient
+from qa_python_utils.default_logger import logger, _logger
 
 
 class StitchTransferRaw(object):
+    @logger
     def __init__(self, bucket, execution_date, integration, database, table, date_field):
         self.athena = AthenaClient(bucket)
+        self.bucket = bucket
         self.execution_date = execution_date
         self.integration = integration
         self.database = database
         self.table = table
         self.date_field = date_field
-
-    def fetch_data(self):
-        partition = datetime.strftime(self.execution_date, "%Y-%m-%d")
-        self.athena.add_partition(self.database, self.table, "dt='{}'".format(partition))
-
-        query = """
+        self.source_key = "raw/market_cost/{integration}/{table}/acc={account}/dt={date_partition}/{file_name}.jsonl"
+        self.base_query = """
             SELECT *, DATE(FROM_ISO8601_TIMESTAMP({date_field})) as created_at
             FROM {database}.{table}
             limit 10
-        """.format(date_field=self.date_field,
-                   integration=self.integration,
-                   table=self.table,
-                   database=self.database)
+        """
 
-        dataframe = self.athena.execute_query_and_return_dataframe(query)
-        date_group = dataframe['created_at'].unique()
+    def _build_query(self):
+        return self.base_query.format(
+            date_field=self.date_field,
+            database=self.database,
+            table=self.table
+        )
 
-        for item in date_group:
-            splitted_df = dataframe.query("created_at == '{}'".format(item))
-            path = "raw/market_cost/{integration}/{table}/acc={account}/dt={date_partititon}/{file_name}.parquet".format(
-                integration=self.integration,
-                table=self.table,
-                account=self.integration,
-                date_partititon=item,
-                file_name=int(time.mktime(datetime.now().timetuple())) * 1000
-            )
+    @logger
+    def _fetch_data(self, query):
+        _logger.info("m=_fetch_data, msg=building partition from execution_date")
+        partition = datetime.strftime(self.execution_date, "%Y-%m-%d")
 
+        self.athena.add_partition(self.database, self.table, "dt='{}'".format(partition))
 
-if __name__ == '__main__':
-    transfer = StitchTransferRaw("5a-datalake", datetime.now(), "facebook_ads_supply_landlords", "stitch_test",
-                                 "stitch_adsets", "created_time")
-    transfer.fetch_data()
+        _logger.info("m=_fetch_data, msg=getting dataframe from query")
+        return self.athena.execute_query_and_return_dataframe(query)
+
+    @logger(exclude='df')
+    def _move_files_to_raw(self, df, key):
+        fs = s3fs.S3FileSystem()
+        with fs.open("{}/{}".format(self.bucket, key), 'wb') as f:
+            _logger.info(
+                "m=_move_files_to_raw, msg=moving json files from dataframe to raw bucket, key=s3://{}/{}".format(
+                    self.bucket, key))
+            f.write(df.to_json(orient='records', lines=True).encode())
