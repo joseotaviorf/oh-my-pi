4f1302a9557f2904bd1040c8c2086efd36f8ee90
ihoelscher <igor.hoelscher@gmail.com>
2018-10-02 20:12:29 -0300

    replace old logger in bietlejuice/jobs/dags

--- bietlejuice/jobs/dags/supply_demand_funnel/dim_subdag.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/dim_subdag.py
@@ -1,9 +1,11 @@
-from qa_python_utils.default_logger import logger
+from qa_python_utils import QuintoAndarLogger
 
 from bietlejuice.jobs.base.base_sub_dag import BaseSubDag
 from bietlejuice.jobs.base.base_test import BaseTest
 from bietlejuice.jobs.base.enum_db import EnumDB
 from bietlejuice.jobs.dags import SOURCE_QUERIES_TESTS_DIR
+
+logger = QuintoAndarLogger('DimSubDag')
 
 
 class DimSubDag(BaseSubDag):
