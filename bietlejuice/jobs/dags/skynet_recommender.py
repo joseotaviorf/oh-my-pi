2f1bdb4c99ab37e06b07fbe437d6f10a7505ec89
ihoelscher <igor.hoelscher@gmail.com>
2018-10-08 19:15:55 -0300

    hide logs from sagemaker training jobs

--- bietlejuice/jobs/dags/skynet_recommender.py
+++ bietlejuice/jobs/dags/skynet_recommender.py
@@ -95,7 +95,8 @@ def train_model(**kwargs):
         's3://{}/{}'.format(
             SKYNET_BUCKET,
             INPUT_PATH.format(exec_date.strftime('%Y-%m-%d'))),
-        job_name=job_name)
+        job_name=job_name,
+        logs=False)
 
     xcom.xcom_push(kwargs.get('ti'), key='job_name', k_value=job_name)
 
