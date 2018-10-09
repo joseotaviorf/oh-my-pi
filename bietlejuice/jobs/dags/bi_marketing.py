ca96cb6f7d6b30441516733c6358d35e3961fe52
Leonardo Santos <3fsleo@gmail.com>
2018-10-09 18:30:43 -0300

    Added google ads dags to raw and clean, google ads accounts and fix dags name

--- bietlejuice/jobs/dags/bi_marketing.py
+++ bietlejuice/jobs/dags/bi_marketing.py
@@ -4,14 +4,38 @@ from airflow.models import DAG
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_sub_dag import BaseSubDag
-from bietlejuice.jobs.dags.marketing.adwords_subdag import AdWordsSubDag
-from bietlejuice.jobs.dags.marketing.facebook_ads_subdag import FacebookAdsSubDag
+from bietlejuice.jobs.dags.marketing.to_clean.googleads_clean_subdag import GoogleAdsCleanSubDag
+from bietlejuice.jobs.dags.marketing.to_raw.googleads_raw_subdag import GoogleAdsStitchSubDag
+from bietlejuice.jobs.dags.marketing.to_raw.facebook_ads_raw_subdag import FacebookAdsSubDag
 from bietlejuice.jobs.dags.util import environment as env
 
 MAIN_DAG_NAME = 'bi-marketing'
-MAIN_START_DATE = datetime(2018, 1, 1, 0, 0, 0)
+MAIN_START_DATE = datetime(2018, 10, 2, 0, 0, 0)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 1 * * *')
+
 FACEBOOK_ADS_ACCOUNTS = ['demand_acqui2sition', 'social', 'demand_retargeting', 'supply_affiliates', 'supply_landlords']
+GOOGLE_ADS_ACCOUNTS = [
+    'quintoandar_belo_horizonte',
+    'quintoandar_brasilia',
+    'quintoandar_display',
+    'quintoandar_dra',
+    'quintoandar_dsa',
+    'quintoandar_goiania',
+    'quintoandar_institucional',
+    'quintoandar_other_cities',
+    'quintoandar_rio_de_janeiro',
+    'quintoandar_sao_paulo',
+    'quintoandar_universal_app_campaigns',
+    'quintoandar_supply_belo_horizonte',
+    'quintoandar_supply_brasilia',
+    'quintoandar_supply_goiania',
+    'quintoandar_supply_jundiai',
+    'quintoandar_supply_niteroi',
+    'quintoandar_supply_other_cities',
+    'quintoandar_supply_rio_de_janeiro',
+    'quintoandar_supply_sao_paulo'
+]
+
 STITCH_DATABASE = 'stitch'
 
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
@@ -26,7 +50,7 @@ main_dag = DAG(
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    catchup=False,
+    catchup=True,
     max_active_runs=1
 )
 
@@ -46,8 +70,8 @@ def facebook_sub_dag(sub_dag_name):
     return sub_dag.build_facebook_tasks()
 
 
-def adwords_sub_dag(sub_dag_name):
-    sub_dag = AdWordsSubDag(
+def googleads_raw_sub_dag(sub_dag_name):
+    sub_dag = GoogleAdsStitchSubDag(
         s3_bucket,
         sub_dag_name,
         MAIN_DAG_NAME,
@@ -56,17 +80,38 @@ def adwords_sub_dag(sub_dag_name):
         STITCH_DATABASE
     )
 
-    return sub_dag.build_adwords_tasks()
+    return sub_dag.build_googleads_raw_tasks()
+
+
+def googleads_clean_sub_dag(sub_dag_name):
+    sub_dag = GoogleAdsCleanSubDag(
+        s3_bucket,
+        sub_dag_name,
+        MAIN_DAG_NAME,
+        MAIN_SCHEDULE_INTERVAL,
+        MAIN_START_DATE,
+        GOOGLE_ADS_ACCOUNTS
+    )
+
+    return sub_dag.build_googleads_clean_tasks()
 
 
 facebook_dag = BaseSubDag.get_sub_dag_operator(
     dag=main_dag,
     sub_dag_func=facebook_sub_dag,
-    sub_dag_name="FacebookAds"
+    sub_dag_name="stitch-facebookads-to-raw"
 )
 
-adwords_dag = BaseSubDag.get_sub_dag_operator(
+googleads_raw_dag = BaseSubDag.get_sub_dag_operator(
     dag=main_dag,
-    sub_dag_func=adwords_sub_dag,
-    sub_dag_name='AdWords'
+    sub_dag_func=googleads_raw_sub_dag,
+    sub_dag_name='stitch-googleads-to-raw'
 )
+
+googleads_clean_dag = BaseSubDag.get_sub_dag_operator(
+    dag=main_dag,
+    sub_dag_func=googleads_clean_sub_dag,
+    sub_dag_name='raw-googleads-to-clean'
+)
+
+googleads_raw_dag >> googleads_clean_dag
