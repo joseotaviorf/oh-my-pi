5f471bd01154aa18d78a2c5a13cc094a4d58e6bf
Ribaldo <ribaldorafael@gmail.com>
2018-10-25 12:26:49 -0300

    Adding inspection and lead reference to crm factory

--- bietlejuice/jobs/new_etl/crm/tasks/factory.py
+++ bietlejuice/jobs/new_etl/crm/tasks/factory.py
@@ -1,6 +1,8 @@
 from bietlejuice.jobs.new_etl.crm.tasks.closing import CRMTasksClosing
 from bietlejuice.jobs.new_etl.crm.tasks.credit import CRMTasksCredit
 from bietlejuice.jobs.new_etl.crm.tasks.crm_tasks_table_enum import CRMTasksTableEnum
+from bietlejuice.jobs.new_etl.crm.tasks.inspection import CRMTasksInspection
+from bietlejuice.jobs.new_etl.crm.tasks.lead import CRMTasksLead
 from bietlejuice.jobs.new_etl.crm.tasks.onboarding_tenant import CRMTasksOnboardingTenant
 from bietlejuice.jobs.new_etl.crm.tasks.payment import CRMTasksPayment
 from bietlejuice.jobs.new_etl.crm.tasks.visit import CRMTasksVisit
@@ -27,5 +29,7 @@ class CRMTasksFactory(object):
             CRMTasksTableEnum.VISIT: CRMTasksVisit,
             CRMTasksTableEnum.CLOSING: CRMTasksClosing,
             CRMTasksTableEnum.ONBOARDING_TENANT: CRMTasksOnboardingTenant,
-            CRMTasksTableEnum.PAYMENT: CRMTasksPayment
+            CRMTasksTableEnum.PAYMENT: CRMTasksPayment,
+            CRMTasksTableEnum.INSPECTION: CRMTasksInspection,
+            CRMTasksTableEnum.LEAD: CRMTasksLead
         }.get(_class)
