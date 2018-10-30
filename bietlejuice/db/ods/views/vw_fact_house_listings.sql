ffef0e7f163e4abf9541c94c72929b0a5a424202
Ribaldo <ribaldorafael@gmail.com>
2018-10-30 18:17:20 -0300

    Adding new fact_house_listings table

---
+++ bietlejuice/db/ods/views/vw_fact_house_listings.sql
@@ -0,0 +1,16 @@
+drop view if exists vw_fact_house_listings;
+create or replace view vw_fact_house_listings as
+select
+  ((i.id || '00') || coalesce(pl.version, 1))::bigint as sk_house_listing,
+  coalesce(i.usuario_que_cadastrou_id, -1) as sk_user_registration,
+  coalesce(c.id, -1) as sk_contract,
+  (date_part('epoch', c.ts_signature - pl.min_version_time) / 86400)::int8 as time_first_listing_to_contract_signed,
+  (date_part('epoch', pl.de_publication_date - pl.min_version_time) / 86400)::int8 as time_listing_to_depublication,
+  coalesce(pl.nr_renting::bigint, 0) as nr_renting,
+  now() as ts_load
+from imovel i
+left join house_listing pl
+  on pl.id = i.id
+left join contract c
+  on c.id = pl.contract_id
+;
