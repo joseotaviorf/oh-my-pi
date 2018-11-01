1232167ce72d60377275fed315b0705af752226a
Ribaldo <ribaldorafael@gmail.com>
2018-11-01 14:54:48 -0300

    Fixing fact_house_listings typo

--- bietlejuice/db/datalake/queries/amplitude/schedule_page_views/middle_region.sql
+++ bietlejuice/db/datalake/queries/amplitude/schedule_page_views/middle_region.sql
@@ -11,7 +11,7 @@ listings as (
   from all_events ae
   join datalake_clean.ods_dim_house_listing dhl
     on ae.house_id = cast(dhl.id_house as integer)
-  join datalake_clean.ods_fact_house_listing fhl
+  join datalake_clean.ods_fact_house_listings fhl
     on dhl.sk_house_listing = fhl.sk_house_listing
   left join datalake_clean.ods_dim_region dr
     on fhl.sk_region = cast(dr.sk_region as varchar)
