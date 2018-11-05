6d743b769edc6c970289d5b264722e537ed3fa49
felipemiquelim <felipemiquelim@gmail.com>
2018-11-05 18:34:29 -0200

    Parameterizing

--- bietlejuice/jobs/new_etl/autodialer/autodialer.py
+++ bietlejuice/jobs/new_etl/autodialer/autodialer.py
@@ -20,29 +20,40 @@ class Autodialer_ETL(object):
             raise Exception('m=,error={}, msg=Could not connect to mongo'.format(ex))
 
     # task references
-    def get_task_references_data(self, execution_date=None):
-        mongo_db = self.mongo_connect(document_type='taskReferences')
+    @logger
+    def get_db_data(self, document_type, execution_date=None):
+        mongo_db = self.mongo_connect(document_type=document_type)
         raw_data = self.get_data(mongo_db, execution_date)
         return raw_data
 
-    def dump_task_references_data(self, df, execution_date=None):
+    @logger(exclude='df')
+    def dump_data_into_datalake(self, df, document_type, execution_date=None):
         dt = 'dt={}'.format(dummy_dt if execution_date is None else execution_date.strftime('%Y-%m-%d'))
-        self.df_to_s3(df, path='raw/autodialer/task_references/{}/tr.csv'.format(dt))
+        self.df_to_s3(df, path='raw/autodialer/{0}/{1}/tr.csv'.format(document_type, dt))
+        logger.info("m=dump_data_into_datalake, document_type={0},"
+                    " execution_date={1},"
+                    " df_length={2}, "
+                    " msg=Df dumped into datalake!".format(document_type,
+                                                           str(execution_date) if execution_date else 'None',
+                                                           str(len(df))))
 
     # tools
+    @logger(exclude='df')
     def df_to_s3(self, df, path):
         BaseETL.csv_to_s3(data=df, bucket=self.bucket_datalake, filename=path)
 
+    @logger
     def mongo_connect(self, document_type):
-        if document_type == 'taskReferences':
+        if document_type == 'task_references':
             return self.db.taskReferences
-        if document_type == 'taskReferenceInboundEventHistories':
+        if document_type == 'task_reference_inbound_event_histories':
             return self.db.taskReferenceInboundEventHistories
-        if document_type == 'taskReferenceOutboundHistory':
+        if document_type == 'task_reference_outbound_history':
             return self.db.taskReferenceOutboundHistory
         else:
             raise ValueError('m=connect, document_type={}, msg=Invalid document type.'.format(document_type))
 
+    @logger
     def get_data(self, mongo_db, execution_date):
         filter = None if execution_date is not None else ''
 
@@ -55,5 +66,9 @@ class Autodialer_ETL(object):
 
 
 autodialer = Autodialer_ETL('5a-datalake')
-df = autodialer.get_task_references_data()
-autodialer.dump_task_references_data(df)
+df = autodialer.get_db_data('task_references')
+autodialer.dump_data_into_datalake(df, 'task_references')
+df = autodialer.get_db_data('task_reference_inbound_event_histories')
+autodialer.dump_data_into_datalake(df, 'task_reference_inbound_event_histories')
+df = autodialer.get_db_data('task_reference_outbound_history')
+autodialer.dump_data_into_datalake(df, 'task_reference_outbound_history')
