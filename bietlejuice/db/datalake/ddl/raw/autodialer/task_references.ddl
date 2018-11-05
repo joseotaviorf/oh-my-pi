537991fe80b02de110df21a47dd26c9da498bcf0
felipemiquelim <felipemiquelim@gmail.com>
2018-11-05 16:17:05 -0200

    DDLs

---
+++ bietlejuice/db/datalake/ddl/raw/autodialer/task_references.ddl
@@ -0,0 +1,29 @@
+CREATE EXTERNAL TABLE datalake_raw.task_references (
+  `_class` string,
+  `_id` string,
+  `active` boolean,
+  `agentid` string,
+  `assignee` string,
+  `autodialerresponse` string,
+  `contactinfo` string,
+  `createddate` string,
+  `dialstatus` string,
+  `score` string,
+  `snoozeduntil` string,
+  `taskid` string,
+  `tasklink` string,
+  `tasktype` string,
+  `updateddate` string)
+PARTITIONED BY (
+  `dt` string)
+row format serde 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+with serdeproperties (
+  'separatorChar' = ',',
+  'quoteChar' = '\"'
+)
+LOCATION
+  's3://5a-datalake/raw/autodialer/task_references/'
+TBLPROPERTIES (
+  'skip.header.line.count'='1')
+
+msck repair table datalake_raw.task_references
--- bietlejuice/jobs/new_etl/autodialer/autodialer.py
+++ bietlejuice/jobs/new_etl/autodialer/autodialer.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from bietlejuice.jobs.base.base_etl import BaseETL
 from bietlejuice.jobs.dags.util import environment as env
 from pymongo import MongoClient, DESCENDING
 from qa_python_utils import QuintoAndarLogger
@@ -16,26 +18,33 @@ class Autodialer_ETL(object):
 
         self.db = self.client.autodialer
 
+    # task references
     def get_task_references_data(self, execution_date=None):
         mongo_db = self.mongo_connect(document_type='taskReferences')
         raw_data = self.get_data(mongo_db, execution_date)
-        len(raw_data)
-        print(len(raw_data))
-        print(len(raw_data))
+        return raw_data
+
+    def dump_task_references_data(self, df, execution_date=None):
+        dt = 'dt={}'.format('2018-01-01' if execution_date is None else execution_date.strftime('%Y-%m-%d'))
+        self.df_to_s3(df, path='raw/autodialer/task_references/{}/tr.csv'.format(dt))
+
+    # tools
+    def df_to_s3(self, df, path):
+        BaseETL.csv_to_s3(data=df, bucket=self.bucket_datalake, filename=path)
 
     def mongo_connect(self, document_type):
         # db.collection_names()
         if document_type == 'taskReferences':
             return self.db.taskReferences
         else:
-            raise ValueError('m=connect, documen_type={}, msg=Invalid document type.'.format(document_type))
+            raise ValueError('m=connect, document_type={}, msg=Invalid document type.'.format(document_type))
 
     def get_data(self, mongo_db, execution_date):
         documents = []
         filter = None if execution_date is not None else ''
 
         documents = mongo_db.find().sort("_id", DESCENDING)
-        return documents
+        return pd.DataFrame(list(documents))
 
 
 # document = task_references.find().sort("_id", DESCENDING).limit(1)
@@ -43,4 +52,5 @@ class Autodialer_ETL(object):
 
 
 autodialer = Autodialer_ETL('5a-datalake')
-autodialer.get_task_references_data()
+df = autodialer.get_task_references_data()
+autodialer.dump_task_references_data(df)
