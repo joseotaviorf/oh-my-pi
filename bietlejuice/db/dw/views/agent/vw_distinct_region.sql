a2884d7533026e2167d89ee024fa2c1a7dfabd59
felipemiquelim <felipemiquelim@gmail.com>
2018-11-07 14:57:29 -0200

    Adjusting vw_distinct_region to consider old and new regions

--- bietlejuice/db/dw/views/agent/vw_distinct_region.sql
+++ bietlejuice/db/dw/views/agent/vw_distinct_region.sql
@@ -1,9 +1,15 @@
-DROP VIEW staging.vw_distinct_region
+DROP VIEW staging.vw_distinct_region;
+
 CREATE VIEW staging.vw_distinct_region AS (
-SELECT DISTINCT
-    dim_region.city_name,
-    dim_region.region_code,
-    dim_region.region_code_deprecated
+SELECT
+	dim_region.city_name,
+    dim_region.region_code
 FROM dim_region
 WHERE dim_region.region_code <> ''
-)
+UNION
+SELECT
+	dim_region.city_name,
+	dim_region.region_code_deprecated AS region_code
+FROM dim_region
+WHERE dim_region.region_code <> ''
+);
