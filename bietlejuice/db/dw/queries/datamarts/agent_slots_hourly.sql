82c31eb669409fec2c686b285ec3acecd6f3d3f0
felipemiquelim <felipemiquelim@gmail.com>
2018-11-13 16:36:45 -0200

    Formatting

--- bietlejuice/db/dw/queries/datamarts/agent_slots_hourly.sql
+++ bietlejuice/db/dw/queries/datamarts/agent_slots_hourly.sql
@@ -1,20 +1,10 @@
-WITH hourly_schedule as (
 SELECT
 	agent_id,
 	to_char(slot_dt, 'YYYYMMDD') as sk_date,
 	DATE_PART(HOUR, slot_dt)::integer as hour,
-	available_slot,
-	available_slot_24h
-FROM
-	agent.agents_slots
-WHERE date(slot_dt) >= date('2018-10-01')
-)
-SELECT
-	agent_id,
-	sk_date,
-	hour,
 	SUM(COALESCE(available_slot,0)) as available_slots,
 	SUM(COALESCE(available_slot_24h,0)) as available_slots_24h
 FROM
-	hourly_schedule
+	agent.agents_slots
+WHERE date(slot_dt) >= date('2018-10-01')
 GROUP BY 1,2,3
