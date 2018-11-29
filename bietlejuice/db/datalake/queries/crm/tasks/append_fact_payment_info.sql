01c63ceb9200802d608514a13d7f458c51a2cf84
Ribaldo <ribaldorafael@gmail.com>
2018-11-29 10:32:14 -0200

    Adding missing comma in CTE

--- bietlejuice/db/datalake/queries/crm/tasks/append_fact_payment_info.sql
+++ bietlejuice/db/datalake/queries/crm/tasks/append_fact_payment_info.sql
@@ -16,7 +16,7 @@ contract_house_listing as (
   select
     cast(sk_house_listing as bigint) as sk_house_listing,
     cast(sk_owner as bigint) as sk_house_owner,
-    cast(sk_contract as bigint) as sk_contract
+    cast(sk_contract as bigint) as sk_contract,
     cast(sk_client as bigint) as sk_tenant
   from datalake_clean.ods_fact_listing_rent_flows
   where sk_contract != '-1'
