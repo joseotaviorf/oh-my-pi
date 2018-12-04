f80db682dd078b0f21177105886c725139927093
felipemiquelim <felipemiquelim@gmail.com>
2018-12-04 16:35:31 -0200

    Including new column to consider in flg_doorman

--- bietlejuice/db/source/queries/ebdb/supply_demand_funnel/fact_supply.sql
+++ bietlejuice/db/source/queries/ebdb/supply_demand_funnel/fact_supply.sql
@@ -219,7 +219,10 @@ from
       end as acquisition_method,
       case
         when uda.id = 279289 and l.origem <> 'Reprocessado' then 'Doorman'
-        when (da.doormanAffiliateData_id is not null) and (dad.joinedProgramAt <= l.criadoEm) then 'Doorman'
+        when da.doormanAffiliateData_id is not null
+            and dad.joinedProgramAt <= l.criadoEm
+            and da.affiliateType = 'Doorman'
+            then 'Doorman'
         when l.tipo = 'Porteiro' then 'Doorman'
         when l.tipo = 'Afiliado' and l.origem = 'App' then 'Affiliate App'
         when l.tipo = 'Afiliado' and l.origem = 'Form' then 'Affiliate Form'
@@ -240,7 +243,10 @@ from
       end as acquisition_channel,
       case
         when uda.id = 279289 and l.origem <> 'Reprocessado' then 'Doorman'
-        when (da.doormanAffiliateData_id is not null) and (dad.joinedProgramAt <= l.criadoEm) then 'Doorman'
+        when da.doormanAffiliateData_id is not null
+            and dad.joinedProgramAt <= l.criadoEm
+            and da.affiliateType = 'Doorman'
+            then 'Doorman'
         when l.tipo = 'Porteiro' then 'Doorman'
         when l.origem = 'Reprocessado' then 'Reprocessed'
         when l.tipo = 'Afiliado' then 'Affiliate'
