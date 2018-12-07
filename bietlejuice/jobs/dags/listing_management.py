ead83e1bc7ca08182674b6115a1ec32ba87a565d
Antoine Hachez <antoine.hachez@quintoandar.com.br>
2018-12-07 16:29:32 -0200

    flake8

--- bietlejuice/jobs/dags/listing_management.py
+++ bietlejuice/jobs/dags/listing_management.py
@@ -34,7 +34,7 @@ SAGEMAKER_ROLE = env.get_airflow_env_var('SAGEMAKER_ROLE')
 SKYNET_KUBERNETES_TOKEN = env.get_airflow_env_var('SKYNET_KUBERNETES_TOKEN')
 KUBERNETES_API_ENDPOINT = env.get_airflow_env_var('KUBERNETES_API_ENDPOINT')
 
-# where on s3 to write the input data to give to fit 
+# where on s3 to write the input data to give to fit
 # (and copy inside the container to /opt/ml/input/data/training)
 INPUT_PATH = 'listing-mgmt/data/raw/dt={}'
 
