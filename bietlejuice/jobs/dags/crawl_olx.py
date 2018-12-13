2a08f2f23bb8a5c38ef91bb7257fb0282c102b48
Adan Echemendia <aemontero7@gmail.com>
2018-12-13 19:43:08 -0200

    integrating dags of crawling_olx and generating_olx_leads into one

--- bietlejuice/jobs/dags/crawl_olx.py
+++ bietlejuice/jobs/dags/crawl_olx.py
@@ -2,19 +2,84 @@ import json
 from datetime import datetime
 
 from airflow.models import DAG
-from qa_python_utils.aws.batch import BatchClient
 from qa_python_utils import QuintoAndarLogger
+from qa_python_utils.aws.batch import BatchClient
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
+from bietlejuice.jobs.new_etl.crawlers.crawler_leads import CrawlerLeads
 
-logger = QuintoAndarLogger('crawling-houses-olx')
+env.set_airflow_var_to_local_env('EBDB')
+logger = QuintoAndarLogger('crawling-leads-olx')
 
-MAIN_DAG_NAME = 'crawling-houses-olx'
+MAIN_DAG_NAME = 'crawling-leads-olx'
 MAIN_START_DATE = datetime(2018, 3, 20)
-MAIN_SCHEDULE_INTERVAL = '0 0 1/1 * *'
+MAIN_SCHEDULE_INTERVAL = '0 2 1/1 * *'
 
 crawler_params = env.get_airflow_env_var('CRAWLING_HOUSES_PARAMS')
+
+s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
+data_google_api_key = env.get_airflow_env_var('DATA_GOOGLE_API_KEY')
+insert_leads_params = env.get_airflow_env_var('insert_leads_params')
+
+NO_LEADS_MSG = 'There are no leads to insert.'
+
+
+def insert_leads(**kwargs):
+    ws = kwargs.get('ws')
+    states = kwargs.get('states')
+    delta_days = kwargs.get('since')
+
+    crawler_leads = CrawlerLeads(s3_bucket=s3_bucket, google_maps_api_key=data_google_api_key)
+    leads = crawler_leads.leads(ws=ws, states=states, delta_days=delta_days)
+
+    if leads.empty:
+        logger.info(NO_LEADS_MSG)
+        return None
+
+    logger.info('m=insert_leads, got {} leads from crawlers'.format(len(leads)))
+    logger.info('m=insert_leads, state_size={}'.format(leads.groupby('state').size()))
+
+    leads_cleaned = crawler_leads.cleaning(leads)
+    if leads_cleaned.empty:
+        logger.info(NO_LEADS_MSG)
+        return None
+
+    phones = crawler_leads.check_known_phone(90)
+    regex_phone = r'(?P<code>\+\d{2})?(?P<number>\d+)'
+    phones.phone_number = phones.phone_number.str.extract(regex_phone, expand=False).number
+
+    leads_cleaned['phone_number'] = leads_cleaned.phones.apply(lambda p: eval(p)[0]).astype(str)
+    leads_cleaned['known'] = leads_cleaned.phone_number.isin(phones.phone_number)
+    leads_filtered = leads_cleaned[~leads_cleaned.known].sort_values(by='updated_on')
+    if leads_filtered.empty:
+        logger.info(NO_LEADS_MSG)
+        return None
+
+    logger.info('m=insert_leads, {} leads with new phone numbers'.format(len(leads_filtered)))
+    logger.info('m=insert_leads, state_size={}'.format(leads_filtered.groupby('state').size()))
+
+    # enrich lat and lng with ceps
+    info = crawler_leads.enrich(leads_filtered.query("""lat.isnull() or lng.isnull()""").cep.unique())
+    leads_enriched = leads_filtered.merge(info, how='left', left_on='cep', right_on='location')
+    leads_enriched.lat = leads_enriched.lat.combine_first(leads_enriched.glat)
+    leads_enriched.lng = leads_enriched.lng.combine_first(leads_enriched.glng)
+    leads_enriched = leads_enriched.dropna(subset=['lat', 'lng'])
+
+    # get to which region each lead belongs
+    leads_enriched['regions'] = leads_enriched.apply(lambda row: crawler_leads.check_coverage(row.lat, row.lng), axis=1)
+
+    # filter out units outside our coverage area
+    leads_filtered = leads_enriched[
+        (leads_enriched.regions > -1) &
+        ((~leads_enriched.type.str.contains('casa')) | leads_enriched.regions.isin(crawler_leads.house_allowed))]
+    if leads_filtered.empty:
+        logger.info(NO_LEADS_MSG)
+        return None
+    logger.info('m=insert_leads, {} leads after filtering regions'.format(len(leads_filtered)))
+    logger.info('m=insert_leads, state_size={}'.format(leads_filtered.groupby('state').size()))
+
+    crawler_leads.send_leads(leads_filtered.iloc[:kwargs.get('max_leads')], ws=kwargs.get('ws'))
 
 
 def submit_olx(**kwargs):
@@ -47,9 +112,19 @@ dag = DAG(
     catchup=False
 )
 
-BaseDAG.build_quintoandar_python_operator(
+# operators
+crawl_olx = BaseDAG.build_quintoandar_python_operator(
     dag=dag,
     task_id='crawl-olx',
     python_callable=submit_olx,
     op_kwargs=json.loads(crawler_params)
 )
+
+insert_leads = BaseDAG.build_quintoandar_python_operator(
+    dag=dag,
+    task_id='insert-leads',
+    python_callable=insert_leads,
+    op_kwargs=json.loads(insert_leads_params)
+)
+
+crawl_olx >> insert_leads
