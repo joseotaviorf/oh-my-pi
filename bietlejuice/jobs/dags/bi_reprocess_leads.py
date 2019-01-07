43afad85365c4dbf1608eaac47b0ae2c77b9e64d
felipemiquelim <felipemiquelim@gmail.com>
2019-01-07 14:15:19 -0200

    Creating DAG template

---
+++ bietlejuice/jobs/dags/bi_reprocess_leads.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from airflow.models import DAG
+from bietlejuice.jobs.base.base_dag import BaseDAG
+from bietlejuice.jobs.dags.util import environment as env
+
+env.set_airflow_var_to_local_env('BI_ODS', 'EBDB')
+bucket_datalake = env.get_airflow_env_var('bi-datalake-s3-bucket')
+
+
+def reprocess_leads(**kwargs):
+    exec_date = kwargs['execution_date']
+
+
+dag = DAG(
+    dag_id='bi-reprocess-leads',
+    default_args={
+        'owner': BaseDAG.DEFAULT_OWNER,
+        'wait_for_downstream': False,
+        'depends_on_past': False
+    },
+    start_date=datetime(2019, 1, 1, 0, 0, 0),
+    schedule_interval='@once',
+    max_active_runs=1
+)
+
+# Get and treat leads to be reprocessed
+reprocess_leads_task = BaseDAG.build_quintoandar_python_operator(
+    dag=dag,
+    task_id='reprocess_leads_task',
+    provide_context=True,
+    python_callable=reprocess_leads,
+    op_kwargs=None
+)
