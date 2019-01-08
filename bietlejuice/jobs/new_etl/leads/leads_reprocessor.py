1918bed6ec1c77a46120f0b8b210c73bef50e2db
felipemiquelim <felipemiquelim@gmail.com>
2019-01-08 18:23:22 -0200

    Adding methods to query db

--- bietlejuice/jobs/new_etl/leads/leads_reprocessor.py
+++ bietlejuice/jobs/new_etl/leads/leads_reprocessor.py
@@ -1,7 +1,9 @@
 import decimal
 import json
 
-from bietlejuice.jobs.base.base_etl import BaseETL
+import pandas as pd
+from bietlejuice.jobs.base.base_etl import BaseETL, EnumDB
+from bietlejuice.jobs.new_etl import SOURCE_QUERIES_DIR
 from qa_python_utils import QuintoAndarLogger
 
 logger = QuintoAndarLogger('LeadsReprocessor')
@@ -15,7 +17,7 @@ def decimal_default(obj):
 
 class LeadsReprocessor(object):
     def __init__(self, config_json):
-        # Configuring required columns
+        # Configuring
         required_columns = ['query', 'defaultColumns', 'infosExtras']
 
         # Checks
@@ -35,11 +37,11 @@ class LeadsReprocessor(object):
 
     @logger
     def get_leads(self):
-        ids = self.__get_lead_ids_from_query()
-        leads = self.__get_lead_infos_from_ids(ids)
-        treated_leads = self.__process_leads(leads)
-
-        return treated_leads
+        df_ids = self.__get_lead_ids_from_query()
+        leads = self.__get_lead_infos_from_ids(df_leads=df_ids)
+        # treated_leads = self.__treat_leads(leads)
+        treated_leads = ''
+        return leads
 
     @logger(exclude='json_list')
     def send_leads(self, json_list, queue='CrawlerLeads'):
@@ -49,7 +51,7 @@ class LeadsReprocessor(object):
             queue_name=queue
         )
 
-    @logger(exclude='config_json')
+    @logger(exclude=['config_dict', 'required_columns'])
     def __check_required_params(self, config_dict, required_columns):
         for column in required_columns:
             if column not in config_dict:
@@ -63,12 +65,34 @@ class LeadsReprocessor(object):
 
     @logger
     def __get_lead_ids_from_query(self):
-        raise NotImplementedError
+        lead_ids = BaseETL.from_db_query(db_enum=EnumDB.QuintoAndar_ebdb, query=self.query)
+
+        # Remove header
+        del lead_ids[0]
+
+        # Convert to df
+        df_leads = pd.DataFrame({'id': lead_ids})
+        return df_leads
+
+    @logger(exclude='df_leads')
+    def __get_lead_infos_from_ids(self, df_leads):
+        # Treat columns
+        list_columns = self.defaultColumns
+        columns_comma_separated = ','.join(list_columns).encode('UTF8')
+
+        # Treat where clause
+        list_leads = [str(elem[0]) for elem in df_leads['id']]
+        leads_comma_separated = ','.join(list_leads).encode('UTF8')
+
+        base_query = BaseETL.get_query_from_file_name(
+            '{}/ebdb/leads/get_leads_to_reprocess.sql'.format(SOURCE_QUERIES_DIR))
+
+        leads = BaseETL.from_db_query(db_enum=EnumDB.QuintoAndar_ebdb,
+                                      query=base_query.format(columns=columns_comma_separated,
+                                                              where_clause=leads_comma_separated))
+
+        return leads
 
     @logger
-    def __get_lead_infos_from_ids(self):
-        raise NotImplementedError
-
-    @logger
-    def __process_leads(self):
+    def __treat_leads(self):
         raise NotImplementedError
