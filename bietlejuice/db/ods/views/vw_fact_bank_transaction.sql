9496dc49cd8c6f8ca858fc569dac915d9af09005
felipemiquelim <felipemiquelim@gmail.com>
2019-01-18 16:47:27 -0200

    Adjusting joins

--- bietlejuice/db/ods/views/vw_fact_bank_transaction.sql
+++ bietlejuice/db/ods/views/vw_fact_bank_transaction.sql
@@ -9,16 +9,13 @@ select
 	bt.valor as "value",
 	bt."contaCorrente_id" as sk_bank_account,
 	bt.imovel_id as house_id,
-	COALESCE(ba.usuario_id, -1) as sk_user,
+	COALESCE(u.id, -1) as sk_user,
 	COALESCE(b.id, -1) as sk_bank,
 	now() as ts_load
 from
 	bank_transaction bt
-join
-	bank_account ba on bt."contaCorrente_id" = ba.id
 left join
-	usuario u on ba.usuario_id = u.id
+	usuario u on bt."contaCorrente_id" = u.contaCorrente_id
 left join
 	bank b on b.codigo = u.dadosbancarios_banco_codigo
-where ba.usuario_id is null
 ;
