911645cd1eb31db4b44f8e8086374910aeb512c8
SabrinaSimao <sabrina_simao@hotmail.com>
2019-01-22 15:01:25 -0200

    refactor _class to class_

--- bietlejuice/jobs/etl/marketing/factory.py
+++ bietlejuice/jobs/etl/marketing/factory.py
@@ -11,23 +11,22 @@ logger = QuintoAndarLogger("MarketingFactory")
 class MarketingFactory(object):
 
     @staticmethod
-    def factory(_class, s3_bucket, execution_date, **kwargs):
-        __class = MarketingFactory.__dispatch_dict(_class)
-        if _class is None:
-            raise Exception('m=factory, _class={}, msg=class type not found'.format(_class))
+    def factory(class_, s3_bucket, execution_date, **kwargs):
+        class__ = MarketingFactory.__dispatch_dict(class_)
+        if class_ is None:
+            raise Exception('m=factory, _class={}, msg=class type not found'.format(class_))
 
-        return __class(
+        return class__(
             s3_bucket=s3_bucket,
-            account=kwargs['account'],
             execution_date=execution_date,
-            auth=kwargs['auth']
+            **kwargs
         )
 
     @staticmethod
     @logger
-    def __dispatch_dict(_class):
+    def __dispatch_dict(class_):
         return {
             MarketingEnum.GOOGLE_ADS: GoogleAds,
             MarketingEnum.FACEBOOK_ADS: FacebookAds,
             MarketingEnum.CRITEO: CriteoCampaigns
-        }.get(_class)
+        }.get(class_)
