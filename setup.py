b223a0306a9730a2e340888017630ea148bb6929
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-01-27 17:09:20 -0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='bietlejuice',
+   version='1.0',
+   description='bi-etl-ejuice module',
+   author='Data Team',
+   packages=['bietlejuice'],
+   install_requires=[]
+)
