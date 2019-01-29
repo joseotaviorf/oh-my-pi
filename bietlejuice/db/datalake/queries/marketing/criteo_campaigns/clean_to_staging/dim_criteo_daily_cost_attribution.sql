7f51e4e4d76277732550513da19455fc63ef9835
SabrinaSimao <sabrina_simao@hotmail.com>
2019-01-29 18:21:26 -0200

    querries weren't casting the clean to staging columns

--- bietlejuice/db/datalake/queries/marketing/criteo_campaigns/clean_to_staging/dim_criteo_daily_cost_attribution.sql
+++ bietlejuice/db/datalake/queries/marketing/criteo_campaigns/clean_to_staging/dim_criteo_daily_cost_attribution.sql
@@ -1,6 +1,6 @@
 SELECT (
-  campaign_id as sk_criteo_campaign,
-  campaign_id as id_campaign,
+  cast(campaign_id as integer) as sk_criteo_campaign,
+  cast(campaign_id as integer) as id_campaign,
   campaign_name,
 )
 FROM datalake_clean.marketing_criteo_campaigns
--- bietlejuice/db/datalake/queries/marketing/criteo_campaigns/clean_to_staging/fact_criteo_daily_cost_attributions.sql
+++ bietlejuice/db/datalake/queries/marketing/criteo_campaigns/clean_to_staging/fact_criteo_daily_cost_attributions.sql
@@ -1,16 +1,16 @@
 SELECT (
     advertiser_name,
-    campaign_id as sk_criteo_campaign,
+    cast(campaign_id as integer) as sk_criteo_campaign,
     cast(date_format(cast(cost_attribution_date as date), '%Y%m%d') as integer) as sk_cost_attribution_date,
     currency,
-    clicks,
-    impressions,
-    audience,
-    cost,
-    all_sales,
-    revenue,
-    composition_win,
-    cpc,
+    cast(clicks as integer) as clicks,
+    cast(impressions as double) as impressions,
+    cast(audience as double) as audience,
+    cast(cost as double) as cost,
+    cast(all_sales as integer) as all_sales,
+    cast(revenue as integer) as revenue,
+    cast(composition_win as double) as composition_win,
+    cast(cpc as double) as cpc,
 )
 FROM datalake_clean.marketing_criteo_campaigns
 WHERE dt_cost_attribution  = '{dt_cost_attribution}'
