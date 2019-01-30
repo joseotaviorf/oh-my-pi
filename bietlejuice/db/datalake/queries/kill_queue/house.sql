507c73510ce0a731d4c1cc1d8df55ccffbb6aa38
Adan Echemendia <aemontero7@gmail.com>
2019-01-30 15:05:41 -0200

    updated datalake/queries house.sql ignoring field owner_id cause of error

--- bietlejuice/db/datalake/queries/kill_queue/house.sql
+++ bietlejuice/db/datalake/queries/kill_queue/house.sql
@@ -1,2 +1,17 @@
-SELECT *
+SELECT
+  id,
+  created_at,
+  updated_at,
+  version,
+  main_id,
+  street_address,
+  house_number,
+  complement,
+  city,
+  state,
+  reservation_allowed,
+  rent_price,
+  floor,
+  reservation_fee,
+  region_id
 FROM datalake_raw.killqueue_house;
