d8a9d35ebfa647c05cd2e90fcce94717c6b17d76
Adan Echemendia <aemontero7@gmail.com>
2019-01-30 17:32:47 -0200

    first version of kill_queque dag

--- bietlejuice/jobs/dags/kill_queue.py
+++ bietlejuice/jobs/dags/kill_queue.py
@@ -6,7 +6,7 @@ from qa_python_utils import QuintoAndarLogger
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
-from bietlejuice.jobs.etl.kill_queue.kill_queue import KillQueue
+from bietlejuice.jobs.etl.kill_queue.killqueue import KillQueue
 
 env.set_airflow_var_to_local_env('KILLQUEUE')
 logger = QuintoAndarLogger('kill_queue')
@@ -18,12 +18,17 @@ MAIN_SCHEDULE_INTERVAL = '0 1 1/1 * *'
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 
 params = env.get_airflow_env_var('KILLQUEUE_PARAMS')
+kill_queue = KillQueue(s3_bucket)
 
 
 def extract_data_and_move_to_raw(**kwargs):
-    kill_queue = KillQueue(s3_bucket)
     table = kwargs.get('table')
     kill_queue.extract_data_and_move_to_raw(table)
+
+
+def move_data_from_raw_to_clean(**kwargs):
+    table = kwargs.get('table')
+    kill_queue.move_data_from_raw_to_clean(table)
 
 
 dag = DAG(
@@ -43,6 +48,7 @@ dag = DAG(
 tables = json.loads(params).get("tables")
 
 extract_data_ops = []
+move_data_to_clean_ops = []
 for table in tables:
     extract_data_op = BaseDAG.build_python_operator(
         dag=dag,
@@ -51,4 +57,14 @@ for table in tables:
         provide_context=True,
         op_kwargs={'table': table}
     )
-    extract_data_ops.append(extract_data_and_move_to_raw)
+    # extract_data_ops.append(extract_data_and_move_to_raw)
+
+    move_data_to_clean_op = BaseDAG.build_python_operator(
+        dag=dag,
+        task_id='move-data-to-clean-{}'.format(table),
+        python_callable=move_data_from_raw_to_clean,
+        provide_context=True,
+        op_kwargs={'table': table}
+    )
+    move_data_to_clean_op.set_upstream(extract_data_op)
+    move_data_to_clean_ops.append(move_data_to_clean_op)
