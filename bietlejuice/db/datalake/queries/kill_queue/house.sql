13400387d1d04a20494f45a0f53925dad7fba33f
Adan Echemendia <aemontero7@gmail.com>
2019-01-30 17:23:09 -0200

    modified house table extraction from datalake/queries

--- bietlejuice/db/datalake/queries/kill_queue/house.sql
+++ bietlejuice/db/datalake/queries/kill_queue/house.sql
@@ -1,17 +1,2 @@
-SELECT
-  id,
-  created_at,
-  updated_at,
-  version,
-  main_id,
-  street_address,
-  house_number,
-  complement,
-  city,
-  state,
-  reservation_allowed,
-  rent_price,
-  floor,
-  reservation_fee,
-  region_id
+SELECT *
 FROM datalake_raw.killqueue_house;
