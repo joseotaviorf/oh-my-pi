a492d01ac9c35ddbe2343ed468234b9f5a43c700
Adan Echemendia <aemontero7@gmail.com>
2019-01-31 20:48:39 -0200

    created reservation_clean query

--- bietlejuice/db/datalake/queries/kill_queue/reservation_clean.sql
+++ bietlejuice/db/datalake/queries/kill_queue/reservation_clean.sql
@@ -1,2 +1,13 @@
-SELECT *
-FROM datalake_raw.killqueue_reservation;
+SELECT
+  cast(id as bigint) as id,
+  cast(created_at as timestamp) as created_at,
+  cast(updated_at as timestamp) as updated_at,
+  cast(version as integer) as version,
+  cast(attempt as integer) as attempt,
+  cast(rent_flow_id as bigint) as rent_flow_id,
+  status,
+  cast(tenant_id as biging) as tenant_id,
+  cast(value as decimal(19,2)) as value,
+  mundipagg_token,
+  cast(is_ongoing as integer)
+FROM datalake_clean.killqueue_reservation;
