1f255df1665873af035fbddc76c9a53118afb8b4
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-02-07 09:32:21 -0200

    Update bank_transaction.py

--- bietlejuice/jobs/dags/supply_demand_funnel/bank_transaction.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/bank_transaction.py
@@ -15,7 +15,7 @@ class BankTransactionSubDag(DimSubDag):
             dag_name=dag_name,
             schedule_interval=schedule_interval,
             start_date=start_date,
-            ebdb_table_name='OperacaoContaCorrente',
+            table_name='OperacaoContaCorrente',
             ods_stg_table_name='bank_transaction'
         )
 
