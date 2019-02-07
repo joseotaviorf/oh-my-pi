eef2b14aa0b6bda1204e5884bc6c501b98ded92d
Leonardo Santos <3fsleo@gmail.com>
2019-02-07 12:01:56 -0200

    Apply regex fix into dim_ticket query

--- bietlejuice/db/datalake/queries/zendesk/dim_ticket.sql
+++ bietlejuice/db/datalake/queries/zendesk/dim_ticket.sql
@@ -1,8 +1,8 @@
 with parse_fields as (
     select zt.id,
         f1.field,
-        regexp_extract(f1.field, '.*{\\"id\\":(\d+)', 1) as field_id,
-        replace(regexp_extract(f1.field, '.*"value\\":(.+)}"}', 1), '\"') as value
+        regexp_extract(f1.field, '.*{\\?"id\\?":(\d+)', 1) as field_id,
+        nullif(regexp_extract(f1.field, '"value\\?":\\?"?([^\\?"|}]+)', 1), 'null') as value
     from datalake_clean.zendesk_tickets zt
     cross join unnest(regexp_extract_all(zt.custom_fields, '{[^}]+[^,]+[^{]+}')) as f1(field)
 ),
