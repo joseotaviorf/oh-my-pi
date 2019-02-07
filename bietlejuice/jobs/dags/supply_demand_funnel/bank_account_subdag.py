44f4936f5171f91a3ee91365df9543996e346f42
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-02-07 09:34:33 -0200

    Update bank_account_subdag.py

--- bietlejuice/jobs/dags/supply_demand_funnel/bank_account_subdag.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/bank_account_subdag.py
@@ -44,7 +44,7 @@ class BankAccountSubDag(DimSubDag):
             python_callable=utils.extract_table_dim_from_ebdb_to_ods,
             op_kwargs={
                 'dim_name': self.ods_stg_table_name,
-                'table_name': self.ebdb_table_name,
+                'table_name': self.table_name,
                 'copy_to_clean': False,
                 'bucket': DimSubDag.S3_BUCKET
             }
