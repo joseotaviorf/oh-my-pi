1e76337f1bb86a34f5abb19ab364149e96eeb493
Adan Echemendia <aemontero7@gmail.com>
2019-02-07 15:49:26 -0200

    refactoring ddl

--- bietlejuice/db/ods/views/vw_dim_reservation.sql
+++ bietlejuice/db/ods/views/vw_dim_reservation.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS vw_dim_reservation;
 CREATE OR REPLACE VIEW vw_dim_reservation as
 SELECT
   id as sk_reservation,
-  id as id,
+  id as id_reservation,
   created_at as ts_created,
   updated_at as ts_updated,
   version,
