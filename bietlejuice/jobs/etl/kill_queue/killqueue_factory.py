33b8785fade0ede06e3c1d723f1db480749af754
Adan Echemendia <aemontero7@gmail.com>
2019-02-10 19:00:01 -0200

    added tests of killqueue etl

--- bietlejuice/jobs/etl/kill_queue/killqueue_factory.py
+++ bietlejuice/jobs/etl/kill_queue/killqueue_factory.py
@@ -12,12 +12,13 @@ logger = QuintoAndarLogger('KillQueueFactory')
 class KillQueueFactory(object):
     @staticmethod
     def get_object(table, s3_bucket):
-        __class = KillQueueFactory.__dispatch_dict(table)
-        if not __class:
-            logger.error('m=get_object, _class={}, msg=class type not found'.format(table))
-            raise Exception
+        if not table:
+            raise ValueError('m=get_object, table={}, msg=table cannot be none'.format(table))
+        _class = KillQueueFactory.__dispatch_dict(table)
+        if not _class:
+            raise RuntimeError('m=get_object, table={}, msg=class type not found'.format(table))
 
-        return __class(
+        return _class(
             s3_bucket=s3_bucket,
         )
 
