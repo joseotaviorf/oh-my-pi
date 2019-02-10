4db2719dc8b7045e7ee5317e14f5242e1ccd74d7
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-02-10 21:42:04 -0200

    Adding requirements intallation in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+import os
 from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = [line for line in f.read().splitlines() if len(line) > 0]
 
 setup(
     name='bietlejuice',
@@ -7,5 +10,5 @@ setup(
     description='bi-etl-ejuice module',
     author='Data Team',
     packages=find_packages(),
-    install_requires=[]
+    install_requires=install_requires
 )
