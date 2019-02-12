f6f4954d108dd95ed453f55a7c1b04769c4f2a1a
felipemiquelim <felipemiquelim@gmail.com>
2019-02-12 14:13:07 -0200

    Adjusting conftest

--- tests/unit/jobs/etl/autodialer/conftest.py
+++ tests/unit/jobs/etl/autodialer/conftest.py
@@ -3,16 +3,12 @@ from datetime import datetime
 import pytest
 from bietlejuice.jobs.etl.autodialer import AutodialerETL, AutodialerEnum
 
-S3_BUCKET = 's3_bucket'
-MONGO_CLIENT_URI = 'mongo_client_uri'
-EXECUTION_DATE = datetime.today()
-
 
 @pytest.fixture(scope='session')
 def autodialer_etl():
     return AutodialerETL(
-        mongo_client_uri=MONGO_CLIENT_URI,
-        bucket_name=S3_BUCKET,
+        mongo_client_uri='mongo_client_uri',
+        bucket_name='s3_bucket',
         document_type_enum=AutodialerEnum.TASK_REFERENCES,
-        execution_date=EXECUTION_DATE
+        execution_date=datetime.today()
     )
