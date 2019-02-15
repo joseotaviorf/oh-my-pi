c4329122e6410197e0300672cb77403013357671
felipemiquelim <felipemiquelim@gmail.com>
2019-02-15 15:57:01 -0200

    Adding lead_score_factor query and ddl

---
+++ bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
@@ -0,0 +1,17 @@
+with t_dates as (
+	select
+		id_origin,
+		max(dt) as dt
+	from
+		datalake_clean.crm_tasks
+	where origin = 'Lead'
+	group by 1
+)
+select
+	cast(t.id_origin as bigint) as lead_id,
+	cast(t.score_factor as bigint) as score_factor
+from
+	t_dates td
+join
+	datalake_clean.crm_tasks t
+	on td.id_origin = t.id_origin and td.dt = t.dt and t.origin = 'Lead'
