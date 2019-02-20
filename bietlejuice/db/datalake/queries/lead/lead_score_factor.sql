f55cf19e78988a89aaada0a5e2b8b56a0e0bb050
felipemiquelim <felipemiquelim@gmail.com>
2019-02-20 18:57:37 -0300

    Remove duplication caused by multiples task in the same dt

--- bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
+++ bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
@@ -1,17 +1,19 @@
 with t_dates as (
 	select
 		id_origin,
-		min(dt) as dt
+		id as task_id,
+		dt,
+		row_number() over (partition by id_origin order by dt, ts_start) as rn
 	from
 		datalake_clean.crm_tasks
 	where origin = 'Lead'
-	group by 1
 )
 select
-	cast(t.id_origin as bigint) as lead_id,
+	cast(td.id_origin as bigint) as lead_id,
 	cast(t.score_factor as bigint) as score_factor
 from
 	t_dates td
 join
 	datalake_clean.crm_tasks t
-	on td.id_origin = t.id_origin and td.dt = t.dt and t.origin = 'Lead'
+	on td.task_id = t.id and t.origin = 'Lead' and t.dt = td.dt
+where score_factor is not null and rn=1
