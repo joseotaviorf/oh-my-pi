47b04844e0cb9d2de1a51929a72c1dd72eb61190
felipemiquelim <felipemiquelim@gmail.com>
2019-02-20 19:07:16 -0300

    Optimizing query

--- bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
+++ bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
@@ -1,19 +1,15 @@
 with t_dates as (
 	select
-		id_origin,
-		id as task_id,
-		dt,
+		cast(id_origin as bigint) as lead_id,
+		cast(score_factor as bigint) as score_factor,
 		row_number() over (partition by id_origin order by dt, ts_start) as rn
 	from
 		datalake_clean.crm_tasks
 	where origin = 'Lead'
 )
 select
-	cast(td.id_origin as bigint) as lead_id,
-	cast(t.score_factor as bigint) as score_factor
+	lead_id,
+	score_factor
 from
-	t_dates td
-join
-	datalake_clean.crm_tasks t
-	on td.task_id = t.id and t.origin = 'Lead' and t.dt = td.dt
-where score_factor is not null and rn=1
+	t_dates
+	where rn = 1
