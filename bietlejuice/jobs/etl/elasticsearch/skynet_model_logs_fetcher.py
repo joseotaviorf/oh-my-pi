5aa9ed9fc4d6f29f0b6c087c92de507c688912f3
ihoelscher <igor.hoelscher@gmail.com>
2019-02-27 17:35:57 -0300

    remove sample code

--- bietlejuice/jobs/etl/elasticsearch/skynet_model_logs_fetcher.py
+++ bietlejuice/jobs/etl/elasticsearch/skynet_model_logs_fetcher.py
@@ -1,6 +1,3 @@
-import json
-import logging
-from datetime import datetime
 from bietlejuice.jobs.etl.elasticsearch import ESLogsFetcher
 from qa_python_utils import QuintoAndarLogger
 
@@ -46,30 +43,3 @@ class SkynetModelLogsFetcher(ESLogsFetcher):
                 .build_query(message_level=message_level)
                 .fetch_all(step_size=step_size, max_size=max_size))
         return logs
-
-
-def sample():
-    ES_LOGS__HOSTNAME = ('https://search-logs-wluvttx6dvl2pq6vgdhhwej7le' +
-                         '.us-east-1.es.amazonaws.com/')
-    EXECUTION_DATE = datetime(2019, 2, 26)
-    APP_NAME = 'ClosingPredictor'
-
-    es_extractor = SkynetModelLogsFetcher(
-        es_logs__hostname=ES_LOGS__HOSTNAME,
-        app_logger_name=APP_NAME
-    )
-
-    logs = es_extractor.run(
-        date_=EXECUTION_DATE,
-        message_level=logging.getLevelName(logging.INFO),
-        step_size=10,
-        max_size=25
-    )
-    logs = map(json.dumps, logs)
-
-    with open('logs.out', 'w') as file:
-        file.write('\n'.join(logs))
-
-
-if __name__ == '__main__':
-    sample()
