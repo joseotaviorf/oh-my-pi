6330e89d48960714084713e53c0bfb77f3fbaa9d
ihoelscher <igor.hoelscher@gmail.com>
2019-02-28 19:20:03 -0300

    set start date as datetime object

--- bietlejuice/jobs/dags/elasticsearch_logs/closing_predictor_logs.py
+++ bietlejuice/jobs/dags/elasticsearch_logs/closing_predictor_logs.py
@@ -1,12 +1,13 @@
 import os
 from airflow.models import DAG
+from datetime import datetime
 
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.dags.elasticsearch_logs import skynet_logs_to_s3
 
 MAIN_DAG_NAME = 'closing-predictor-logs'
-MAIN_START_DATE = '2019-02-26'
+MAIN_START_DATE = datetime(2019, 2, 26)
 MAIN_SCHEDULE_INTERVAL = '0 3 * * *'  # 3am UTC every day
 
 env.set_airflow_var_to_local_env('ES_LOGS__HOSTNAME')
