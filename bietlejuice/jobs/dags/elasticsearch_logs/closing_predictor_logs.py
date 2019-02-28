e9c351c1174206aefd12d7f7492c70b7e346e5c6
ihoelscher <igor.hoelscher@gmail.com>
2019-02-28 18:49:18 -0300

    rename dag to keep standards

--- bietlejuice/jobs/dags/elasticsearch_logs/closing_predictor_logs.py
+++ bietlejuice/jobs/dags/elasticsearch_logs/closing_predictor_logs.py
@@ -5,7 +5,7 @@ from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.dags.elasticsearch_logs import skynet_logs_to_s3
 
-MAIN_DAG_NAME = 'skynet-model-logs-closing-predictor'
+MAIN_DAG_NAME = 'closing-predictor-logs'
 MAIN_START_DATE = '2019-02-26'
 MAIN_SCHEDULE_INTERVAL = '0 3 * * *'  # 3am UTC every day
 
