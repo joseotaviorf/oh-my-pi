9c55e6d972f5c8e7e16551a65f679ea3af375923
felipemiquelim <felipemiquelim@gmail.com>
2019-02-28 10:35:45 -0300

    Removing inconsistent house_ids that come from amplitude

--- bietlejuice/db/datalake/queries/listing_views.sql
+++ bietlejuice/db/datalake/queries/listing_views.sql
@@ -8,7 +8,7 @@ with views as (
 	where et = 'listing_page_viewed'
 	and trim(app) = '170698'
 	-- remove inconsistent house_ids that come from amplitude
-	and length(e_house_id) < 13
+	and length(e_house_id) <= 13
 ),
 unique_viewers as (
 	select
