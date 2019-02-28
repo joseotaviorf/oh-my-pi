f923d289e9434c4254326022da83fdd8ba600917
ihoelscher <igor.hoelscher@gmail.com>
2019-02-28 15:59:49 -0300

    implement closing predictor logs dag

---
+++ bietlejuice/jobs/dags/elasticsearch_logs/closing_predictor_dag.py
@@ -0,0 +1,38 @@
+import os
+from airflow.models import DAG
+
+from bietlejuice.jobs.base.base_dag import BaseDAG
+from bietlejuice.jobs.dags.util import environment as env
+from bietlejuice.jobs.dags.elasticsearch_logs import skynet_logs_to_s3
+
+MAIN_DAG_NAME = 'skynet-model-logs-closing-predictor'
+MAIN_START_DATE = '2019-02-26'
+MAIN_SCHEDULE_INTERVAL = '0 3 * * *'  # 3am every day
+
+env.set_airflow_var_to_local_env('ES_LOGS__HOSTNAME')
+
+config = {
+    'ES_LOGS__HOSTNAME': os.getenv('ES_LOGS__HOSTNAME'),
+    'MODEL_NAME': 'ClosingPredictor',
+}
+
+dag = DAG(
+    dag_id=MAIN_DAG_NAME,
+    default_args={
+        'owner': BaseDAG.DEFAULT_OWNER,
+        'wait_for_downstream': False,
+        'depends_on_past': False
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+    max_active_runs=1,
+    catchup=False
+)
+
+dump_logs_to_datalake_raw_op = BaseDAG.build_python_operator(
+    dag=dag,
+    task_id='dump_logs_to_datalake_raw',
+    provide_context=True,
+    python_callable=skynet_logs_to_s3,
+    op_kwargs=config
+)
