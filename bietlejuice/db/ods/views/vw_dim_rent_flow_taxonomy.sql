f6807ba190b10e79b21d67f45e5276a69ebe93d9
felipemiquelim <felipemiquelim@gmail.com>
2019-03-07 10:59:38 -0300

    Adjusting calls to taxonomy

--- bietlejuice/db/ods/views/vw_dim_rent_flow_taxonomy.sql
+++ bietlejuice/db/ods/views/vw_dim_rent_flow_taxonomy.sql
@@ -3,13 +3,13 @@ create view vw_dim_rent_flow_taxonomy as
 select
 	cast(id as bigint) as sk_rent_flow_taxonomy,
 	cast(id as bigint) as id_rent_flow_taxonomy,
-	"Category" as mkt_category,
-	"Flow" as mkt_flow,
-	"Completion" as mkt_completion,
-	"Channel" as mkt_channel,
-	"Medium" as mkt_medium,
-	"Source" as mkt_source,
-	"Platform" as mkt_platform,
+	Category as mkt_category,
+	Flow as mkt_flow,
+	Completion as mkt_completion,
+	Channel as mkt_channel,
+	Medium as mkt_medium,
+	Source as mkt_source,
+	Platform as mkt_platform,
 	now()::timestamp as ts_load
 from files.taxonomy_demand
 ;
