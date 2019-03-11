76896d6cfe42e34763c005c5eeb90b4689eceebb
Leonardo Santos <3fsleo@gmail.com>
2019-03-11 18:12:59 -0300

    Fix in factory class

--- bietlejuice/jobs/etl/marketing/factory.py
+++ bietlejuice/jobs/etl/marketing/factory.py
@@ -13,11 +13,12 @@ logger = QuintoAndarLogger("MarketingFactory")
 class MarketingFactory(object):
 
     @staticmethod
-    def factory(class_, s3_bucket, execution_date, account=None):
-        class__ = MarketingFactory.__dispatch_dict(class_)
+    def factory(class_, s3_bucket, execution_date, auth=None, account=None):
         if class_ is None:
             raise TypeError('m=factory, _class={}, msg=class type not found'.format(class_))
-            
+
+        class__ = MarketingFactory.__dispatch_dict(class_)
+
         return class__(
             s3_bucket=s3_bucket,
             execution_date=execution_date,
