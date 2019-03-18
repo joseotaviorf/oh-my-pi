dadee96459eac65ee27e0f58f4ac54d27e81ba49
felipemiquelim <felipemiquelim@gmail.com>
2019-03-18 17:43:42 -0300

    Adjust leads process to group by all tax

--- bietlejuice/db/dw/queries/growth/taxonomy/leads/prefix_region.sql
+++ bietlejuice/db/dw/queries/growth/taxonomy/leads/prefix_region.sql
@@ -1,4 +1,4 @@
-with all_dates_prev as (
+with all_dates as (
 	select
     date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
     date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _month,
@@ -15,36 +15,132 @@ with all_dates_prev as (
 	f.mkt_platform,
 	l.utm_campaign,
 	l.utm_content,
-	l.utm_term,
-    rank() over (partition by coalesce(dr.city_group, ''),
-                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                              date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
-    	+ rank() over (partition by coalesce(dr.city_group, ''),
-                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                                  date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                                  date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                                  date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+	l.utm_term, --coalesce(dr.city_group, ''),
+    rank() over (partition by
+                        coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead asc)
+    	+ rank() over (partition by
+    	                coalesce(dr.city_group, ''),
+    	                date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead desc)
 			- 1 as daily_count,
-    rank() over (partition by coalesce(dr.city_group, ''),
-                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
-    	+ rank() over (partition by coalesce(dr.city_group, ''),
-                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                                  date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+    rank() over (partition by
+                        coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead asc)
+    	+ rank() over (partition by
+    	                coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead desc)
 			- 1 as weekly_count,
-    rank() over (partition by coalesce(dr.city_group, ''),
-                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
-    	+ rank() over (partition by coalesce(dr.city_group, ''),
-                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
-                                  date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+    rank() over (partition by
+                        coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead asc)
+    	+ rank() over (partition by
+    	                coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead desc)
 			- 1 as monthly_count,
-    rank() over (partition by coalesce(dr.city_group, ''),
-                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
-    	+ rank() over (partition by coalesce(dr.city_group, ''),
-                                  date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+    rank() over (partition by
+                        coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                        f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+                        order by f.sk_lead asc)
+    	+ rank() over (partition by
+    	                coalesce(dr.city_group, ''),
+                        date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+    	                f.mkt_category,
+                        f.mkt_flow,
+                        f.mkt_completion,
+                        f.mkt_channel,
+                        f.mkt_medium,
+                        f.mkt_source,
+                        f.mkt_platform,
+                        l.utm_campaign,
+                        l.utm_content,
+                        l.utm_term
+    	                order by f.sk_lead desc)
 			- 1 as yearly_count
 	from fact_house_listing_flows f
 	join fact_house_listings fhl
@@ -55,28 +151,4 @@ with all_dates_prev as (
         on f.sk_lead = l.sk_lead
   where f.sk_lead_date between 20180101 and to_char(current_date - 1, 'YYYYMMDD')::integer
   order by 5, 1, 2, 3, 4
-),
-all_dates as (
-  select distinct
-    _year,
-    _month,
-    _week,
-    _day,
-    city_group,
-    city,
-    mkt_category,
-	mkt_flow,
-	mkt_completion,
-	mkt_channel,
-	mkt_medium,
-	mkt_source,
-	mkt_platform,
-	utm_campaign,
-	utm_content,
-	utm_term,
-    max(daily_count) over (partition by city_group, _year, _month, _week, _day) as daily_count,
-    max(weekly_count) over (partition by city_group, _year, _week) as weekly_count,
-    max(monthly_count) over (partition by city_group, _year, _month) as monthly_count,
-    max(yearly_count) over (partition by city_group, _year) as yearly_count
-  from all_dates_prev
 ),
