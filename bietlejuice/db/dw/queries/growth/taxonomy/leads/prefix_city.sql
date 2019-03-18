2b3abb05a9df1197798cc1ec4c3ae7f26367054d
felipemiquelim <felipemiquelim@gmail.com>
2019-03-18 15:34:08 -0300

    Implementing taxonomy_leads process

---
+++ bietlejuice/db/dw/queries/growth/taxonomy/leads/prefix_city.sql
@@ -0,0 +1,82 @@
+with all_dates_prev as (
+	select distinct
+    date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
+    date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _month,
+    date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _week,
+    date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _day,
+    'QuintoAndar'::varchar as city_group,
+    coalesce(dr.city_name, '') as city,
+    f.mkt_category,
+	f.mkt_flow,
+	f.mkt_completion,
+	f.mkt_channel,
+	f.mkt_medium,
+	f.mkt_source,
+	f.mkt_platform,
+	l.utm_campaign,
+	l.utm_content,
+	l.utm_term,
+    rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
+    	+ rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('day', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+			- 1 as daily_count,
+    rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
+    	+ rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('week', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+			- 1 as weekly_count,
+    rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
+    	+ rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')),
+                              date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+			- 1 as monthly_count,
+    rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead asc)
+    	+ rank() over (partition by coalesce(dr.city_name, ''),
+                              date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) order by f.sk_lead desc)
+			- 1 as yearly_count
+	from fact_house_listing_flows f
+	join fact_house_listings fhl
+	  on fhl.sk_house_listing = f.sk_house_listing
+	left join dim_region dr
+		on fhl.sk_region = dr.sk_region
+	join dim_lead l
+        on f.sk_lead = l.sk_lead
+	where f.sk_lead_date between 20180101 and to_char(current_date - 1, 'YYYYMMDD')::integer
+  order by 6, 1, 2, 3, 4
+),
+all_dates as (
+  select distinct
+    _year,
+    _month,
+    _week,
+    _day,
+    city_group,
+    city,
+    mkt_category,
+	mkt_flow,
+	mkt_completion,
+	mkt_channel,
+	mkt_medium,
+	mkt_source,
+	mkt_platform,
+	utm_campaign,
+	utm_content,
+	utm_term,
+    max(daily_count) over (partition by city, _year, _month, _week, _day) as daily_count,
+    max(weekly_count) over (partition by city, _year, _week) as weekly_count,
+    max(monthly_count) over (partition by city, _year, _month) as monthly_count,
+    max(yearly_count) over (partition by city, _year) as yearly_count
+  from all_dates_prev
+),
