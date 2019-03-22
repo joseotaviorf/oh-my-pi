2959c4a9190847bd1ecaa83eef221bf12c1b7e95
felipemiquelim <felipemiquelim@gmail.com>
2019-03-22 09:18:01 -0300

    Adding group by

--- bietlejuice/db/dw/queries/growth/taxonomy/leads/prefix_region.sql
+++ bietlejuice/db/dw/queries/growth/taxonomy/leads/prefix_region.sql
@@ -1,4 +1,4 @@
-with all_dates as (
+with all_dates_prev as (
 	select
     date_part('year', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _year,
     date_part('month', to_date(f.sk_lead_date::varchar, 'YYYYMMDD')) as _month,
@@ -151,4 +151,30 @@ with all_dates as (
         on f.sk_lead = l.sk_lead
   where f.sk_lead_date between 20180101 and to_char(current_date - 1, 'YYYYMMDD')::integer
   order by 5, 1, 2, 3, 4
+),
+all_dates as (
+    select
+        _year,
+        _month,
+        _week,
+        _day,
+        city_group,
+        city,
+        mkt_category,
+        mkt_flow,
+        mkt_completion,
+        mkt_channel,
+        mkt_medium,
+        mkt_source,
+        mkt_platform,
+        utm_campaign,
+        utm_content,
+        utm_term,
+        max(daily_count) as daily_count,
+        max(weekly_count) as weekly_count,
+        max(monthly_count) as monthly_count,
+        max(yearly_count) as yearly_count
+    from
+        all_dates_prev
+    group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
 ),
