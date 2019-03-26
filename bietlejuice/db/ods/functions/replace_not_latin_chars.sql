742bec00aa3b6c6056b470050216cb722aa02548
felipemiquelim <felipemiquelim@gmail.com>
2019-03-26 09:58:41 -0300

    Adding chars

--- bietlejuice/db/ods/functions/replace_not_latin_chars.sql
+++ bietlejuice/db/ods/functions/replace_not_latin_chars.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION replace_not_latin_chars(p_text text)
+ CREATE OR REPLACE FUNCTION replace_not_latin_chars(p_text text)
   RETURNS text AS
  $BODY$
  Select translate($1,
- 'ẐẑẒẓẔẕŹźŻżŽžȤȥⱫⱬƵƶẊẋẌẍẎẏỾỿỲỳỴỵỶỷỸỹŶŷƳƴŸÿ',
- 'ZzZzZzZzZzZzZzZzZzXxXxYyYyYyYyYyYyYyYyYy'
+ 'ẐẑẒẓẔẕŹźŻżŽžȤȥⱫⱬƵƶẊẋẌẍẎẏỾỿỲỳỴỵỶỷỸỹŶŷƳƴŸÿŞşṠṡṢṣŠšȘș',
+ 'ZzZzZzZzZzZzZzZzZzXxXxYyYyYyYyYyYyYyYyYySsSsSsSsSs'
   );
  $BODY$
  LANGUAGE sql
