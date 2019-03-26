c1ca9d54a7851a2de2b7124b2359f8d31a8a49a4
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-03-26 15:13:34 -0300

    adding time to attendance

--- bietlejuice/db/datalake/queries/asterisk/create_calls_table.sql
+++ bietlejuice/db/datalake/queries/asterisk/create_calls_table.sql
@@ -12,10 +12,10 @@ ids_calls as (
 ),
 events_ura as (
   SELECT
-        regexp_extract(content,'VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "(NOW|QUEUEJOINTIME)=(\d+)"', 1) as id_call,
-        regexp_extract(content,'VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "(NOW|QUEUEJOINTIME)=(\d+)"', 2) as id_ura,
-        regexp_extract(content,'VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "NOW=(\d+)"', 3) as ts_start_ura,
-        regexp_extract(content,'VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "QUEUEJOINTIME=(\d+)"', 3) as ts_end_ura
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "(NOW|QUEUEJOINTIME)=(\d+)"', 2) as id_call,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "(NOW|QUEUEJOINTIME)=(\d+)"', 3) as id_ura,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "NOW=(\d+)"', 1) as ts_start_ura,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/3001-(\w+)", "QUEUEJOINTIME=(\d+)"', 1) as ts_end_ura
   FROM asterisk_data
   WHERE content like '%[C-000063b1]%'
   GROUP BY 1,2,3,4
@@ -35,12 +35,13 @@ events_queue as (
 -- [2019-03-18 08:47:32] VERBOSE[29930][C-000063b1] app_dial.c: PJSIP/8667-00007b7d answered Local/8667@from-queue-00003d78;2
 events_attendance as (
 	select 
-	    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-.+ answered Local/\d+@from-queue-(\w+);\d',2) as id_call,
-	    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-.+ answered Local/\d+@from-queue-(\w+);\d',1) as ts_start_attendance,
-        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-.+ answered Local/\d+@from-queue-(\w+);\d',3) as id_caller,
-        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-.+ answered Local/\d+@from-queue-(\w+);\d',4) as id_queue	    
+	    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',2) as id_call,
+	    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',1) as ts_start_attendance,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',3) as id_caller,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',4) as id_attendance,
+        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',5) as id_queue	    
     from asterisk_data   
-    group by 1,2,3,4
+    group by 1,2,3,4,5
 ),
 
 events_queue_join_attendance as (
@@ -53,12 +54,24 @@ events_queue_join_attendance as (
     group by 1,2,3,4
 ),
 
+--[2019-03-18 08:48:53] VERBOSE[30040][C-000063b1] pbx.c: Executing [s@crm-hangup:2] NoOp("PJSIP/8667-00007b7d", "HANGUP CAUSE: 16") in new stack
+events_hangup as (
+    SELECT
+       regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("PJSIP/(\d+)-(\w+)".+HANGUP CAUSE: (\d+)"\) in new stack', 1) as ts_hangup,
+       regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("PJSIP/(\d+)-(\w+)".+HANGUP CAUSE: (\d+)"\) in new stack', 2) as id_call,
+       regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("PJSIP/(\d+)-(\w+)".+HANGUP CAUSE: (\d+)"\) in new stack', 3) as id_caller,
+       regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("PJSIP/(\d+)-(\w+)".+HANGUP CAUSE: (\d+)"\) in new stack', 4) as id_attendance,
+       regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("PJSIP/(\d+)-(\w+)".+HANGUP CAUSE: (\d+)"\) in new stack', 5) as cod_hangup_cause
+    FROM asterisk_data
+    group by 1,2,3,4,5
+),
+
 URA AS (
     SELECT 
        id_call,
        id_ura,
-       from_unixtime(max(coalesce(cast(ts_start_ura as integer),0))) as ts_start_ura,
-       from_unixtime(max(coalesce(cast(ts_end_ura as integer),0))) as ts_end_ura
+       max(ts_start_ura) as ts_start_ura,
+       max(ts_end_ura) as ts_end_ura
     FROM events_ura
     GROUP BY 1,2
 ),
@@ -66,10 +79,11 @@ URA AS (
 QUEUES as (
 	select 
 		q.id_call,
-		--q.id_queue,
+		q.id_queue,
 		q.id_caller,
+        a.id_attendance,
 		q.ts_start_queue,
-        j.ts_queue_join_time as ts_end_attendance,
+        h.ts_hangup as ts_end_attendance,
         coalesce(a.ts_start_attendance,j.ts_queue_join_time) as ts_end_queue
     from
        events_queue q
@@ -81,8 +95,12 @@ QUEUES as (
        events_queue_join_attendance j
     ON
        j.id_call = a.id_call and j.id_caller = a.id_caller and j.id_queue = a.id_queue
+    LEFT JOIN
+       events_hangup h
+    ON
+       h.id_call = a.id_call and h.id_caller = a.id_caller and h.id_attendance = a.id_attendance
     group by
-     	1,2,3,4,5
+     	1,2,3,4,5,6,7
 )
 
 SELECT
@@ -90,15 +108,16 @@ SELECT
     u.id_ura,
     u.ts_start_ura,
     u.ts_end_ura,
-   -- q.id_queue,
+    q.id_queue,
     q.id_caller,
     q.ts_start_queue,
     q.ts_end_queue,
     --q.ts_end_queue,
+    a.id_attendance,
     a.ts_start_attendance,
     q.ts_end_attendance,
     --q.ts_queue_join_time,
-    date_diff('second', u.ts_start_ura, u.ts_end_ura) as seconds_time_ura,
+    date_diff('second', cast(u.ts_start_ura as timestamp), cast(u.ts_end_ura as timestamp)) as seconds_time_ura,
     date_diff('second', cast(q.ts_start_queue as timestamp), cast(q.ts_end_queue as timestamp)) as seconds_time_queue,
     date_diff('second', cast(a.ts_start_attendance as timestamp), cast(q.ts_end_attendance as timestamp)) as seconds_time_attendance
 FROM
