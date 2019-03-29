bbcf077f3dc2c0636730e5aa7dbcf7ad8f5d1bd2
felipemiquelim <felipemiquelim@gmail.com>
2019-03-29 10:15:38 -0300

    Adding regex

--- bietlejuice/db/ods/functions/replace_not_latin_chars.sql
+++ bietlejuice/db/ods/functions/replace_not_latin_chars.sql
@@ -1,9 +1,7 @@
  CREATE OR REPLACE FUNCTION replace_not_latin_chars(p_text text)
   RETURNS text AS
  $BODY$
- Select translate($1,
- 'ÅåÄäĀāĄąȦȧÆæǢǣḂḃĆćĊċČčĘęËëȨȩḞḟĠġǦǧḦḧḨḩḰḱĹĺŁłḾḿṀṁØøÖöȮȯŔŕṘṙŚśŜŝẐẑẒẓẔẕŹźŻżŽžȤȥⱫⱬƵƶẊẋẌẍẎẏỾỿỲỳỴỵỶỷỸỹŶŷƳƴŸÿŞşṠṡṢṣŠšȘș',
- 'AaAaAaAaAaAaAaBbCcCcCcEeEeEeFfGgGgHhHhKkLlLlMmMmOoOoOoRrRrSsSsZzZzZzZzZzZzZzZzZzXxXxYyYyYyYyYyYyYyYyYySsSsSsSsSs'
+ Select regexp_replace($1,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '', 'g'
   );
  $BODY$
  LANGUAGE sql
