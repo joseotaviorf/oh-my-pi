12ad70f4f9814c824f5ce099cc4437558a3d5dc2
felipemiquelim <felipemiquelim@gmail.com>
2019-04-01 19:19:05 -0300

    Dynamically getting all files via gsheets

--- bietlejuice/jobs/wrappers/GoogleDrive/google_sheets.py
+++ bietlejuice/jobs/wrappers/GoogleDrive/google_sheets.py
@@ -14,26 +14,22 @@ class GoogleSheets(object):
         self.google_api_scope = google_api_scope
 
     @logger(exclude=['google_sheets_files'])
-    def move_sheets_data_to_datalake(self, google_sheets_files, list_filenames):
+    def move_sheets_data_to_datalake(self, google_sheets_files):
+        gsheets = GoogleSheetsClient(self.google_s_a_credentials, self.google_api_scope)
+        s3 = S3ToODS(s3_bucket=self.s3_bucket)
+        found = 0
 
-        for row in list_filenames:
-            gsheets = GoogleSheetsClient(self.google_s_a_credentials, self.google_api_scope)
-            s3 = S3ToODS(s3_bucket=self.s3_bucket)
-            found = 0
+        for item in google_sheets_files['files']:
+            found += 1
+            df_gsheets = gsheets.get_dataframe_from_sheet(sheet_name=item['sheetName'],
+                                                          sheet_id=item['sheetId'])
+            snake_case_columns = self._to_snake_case_columns(df_gsheets.columns)
+            df_gsheets.rename(columns=snake_case_columns, inplace=True)
+            s3.move_df_to_datalake(df=df_gsheets, tablename=item['s3_path'])
 
-            for item in google_sheets_files['files']:
-                if item['fileName'] == row:
-                    found += 1
-                    df_gsheets = gsheets.get_dataframe_from_sheet(sheet_name=item['sheetName'],
-                                                                  sheet_id=item['sheetId'])
-                    snake_case_columns = self._to_snake_case_columns(df_gsheets.columns)
-                    df_gsheets.rename(columns=snake_case_columns, inplace=True)
-                    s3.move_df_to_datalake(df=df_gsheets, tablename=item['s3_path'])
-
-            if found == 0:
-                raise ValueError(
-                    'm=move_sheets_data_to_datalake, filename={}, msg=no filename found in json google sheets schema.'.format(
-                        row))
+        if found == 0:
+            raise ValueError(
+                'm=move_sheets_data_to_datalake, msg=no filename found in json google sheets schema.')
 
     @staticmethod
     @logger(exclude='old_columns')
