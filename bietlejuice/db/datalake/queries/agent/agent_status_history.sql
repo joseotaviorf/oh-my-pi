f5566da5fb6dc9aad24d6f76fdd99d8e1ecf6c05
felipemiquelim <felipemiquelim@gmail.com>
2019-04-01 10:48:07 -0300

    Adding agent_status_history

---
+++ bietlejuice/db/datalake/queries/agent/agent_status_history.sql
@@ -0,0 +1,22 @@
+with agent_changes as (
+    select
+        da.id,
+        rev,
+        from_unixtime(cast(rv.timestamp as decimal)/1000) as ts_rev,
+        revtype,
+        ativo,
+        coalesce(ativo <> lag(ativo) over (partition by da.id order by rv.timestamp), true) has_changed -- Coalesce true to set rev = 0 as a change
+    from datalake_raw.ebdb_dadosagente_aud da
+    join datalake_raw.ebdb_usuario_revision_entity rv
+        on da.rev = rv.id
+)
+select
+    id as agent_id,
+    case
+        when ativo = '1' then 'Active'
+        else 'Suspended'
+        end as status,
+    date(ts_rev) as start_date,
+    coalesce(date(lead(ts_rev) over (partition by id order by ts_rev)), date('2099-12-31')) as end_date
+from agent_changes
+where has_changed = true
