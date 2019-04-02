03ce9d3d99c69c84deda768a1c5264efa6054b16
Leonardo Santos <3fsleo@gmail.com>
2019-04-02 18:28:45 -0300

    Fix append_fact_ungrouped_manual_info query

--- bietlejuice/db/datalake/queries/crm/tasks/append_fact_ungrouped_manual_info.sql
+++ bietlejuice/db/datalake/queries/crm/tasks/append_fact_ungrouped_manual_info.sql
@@ -1,8 +1,9 @@
 , contracts as (
   select
     t.*,
-    cast(coalesce(dc.sk_contract, ec.id, offer_contract.id, fhl.sk_contract, '-1') as bigint) as sk_contract,
-    cast(coalesce(eo.id, '-1') as bigint) as sk_rent_flow
+    dc.sk_contract as dc_sk_contract,
+    cast(coalesce(dc.sk_contract, ec.id, offer_contract.id) as bigint) as sk_contract,
+    cast(coalesce(eo.id) as bigint) as sk_rent_flow
   from tasks t
   join datalake_clean.crm_tasks ct
     on t.sk_task = trim(ct.id)
@@ -18,7 +19,7 @@
     on cast(ep.id as bigint) = try(cast(ec.proposta_id as bigint))
   left join datalake_raw.ebdb_offer eof
     on trim(ct.origin) = 'Offer'
-        and cast(ct.id_origin as bigint) = try(cast(eof.id as bigint))
+        and cast(ct.id_origin as bigint) = try(cast(eof.godfatherid as bigint))
   left join datalake_raw.ebdb_proposta offer_prop
     on offer_prop.offer_id = eof.id
   left join datalake_raw.ebdb_contrato offer_contract
@@ -26,27 +27,14 @@
   left join datalake_raw.ebdb_fluxolocacao eo
     on trim(ct.origin) = 'FluxoLocacao'
         and cast(ct.id_origin as bigint) = try(cast(eo.id as bigint))
-  left join datalake_clean.ods_dim_house_listing dhl
-    on trim(ct.origin) = 'Imovel'
-      and cast(ct.id_origin as bigint) = try(cast(dhl.id_house as bigint))
-      and cast(regexp_extract(ct.ts_start, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
-        between cast(regexp_extract(dhl.ts_listing_version_start, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
-          and (case
-                 when dhl.ts_listing_version_end = ''
-                   then now()
-                 else cast(regexp_extract(dhl.ts_listing_version_end, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
-               end)
-  left join datalake_clean.ods_fact_house_listings fhl
-    on fhl.sk_house_listing = dhl.sk_house_listing
 ),
 contract_house_listing as (
   select
-    cast(sk_house_listing as bigint) as sk_house_listing,
     cast(sk_contract as bigint) as sk_contract,
     cast(sk_rent_flow as bigint) as sk_rent_flow
   from datalake_clean.ods_fact_listing_rent_flows
   where sk_contract != '-1'
-  group by 1, 2, 3
+  group by 1, 2
 )
 select distinct
   c.sk_task,
@@ -66,13 +54,9 @@ select distinct
   c.ts_task_user_end as ts_task_action_end,
   c.task_user_type as task_action_type,
   c.task_user_resolve_hours,
-  coalesce(chl_contract.sk_contract, chl_rent_flow.sk_contract, -1) as sk_contract,
+  coalesce(chl_rent_flow.sk_contract, c.sk_contract, -1) as sk_contract,
   c.dt_partition
 from contracts c
-left join contract_house_listing chl_contract
- on c.sk_contract = chl_contract.sk_contract
-   and c.sk_contract != -1
 left join contract_house_listing chl_rent_flow
     on c.sk_rent_flow = chl_rent_flow.sk_rent_flow
-    and c.sk_rent_flow != -1
-;
+    and c.sk_contract is null
