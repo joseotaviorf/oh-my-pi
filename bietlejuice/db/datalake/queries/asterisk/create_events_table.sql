34ec173ca263b2fbd02ee6df504b81babc212f26
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-04-03 16:23:37 -0300

    renaming call values with better names and adding partition_date param

--- bietlejuice/db/datalake/queries/asterisk/create_events_table.sql
+++ bietlejuice/db/datalake/queries/asterisk/create_events_table.sql
@@ -1,13 +1,14 @@
 with asterisk_data as (
-    select dt, content from datalake_raw.asterisk_logs_full
-    where date(from_iso8601_timestamp(dt)) = date('2019-03-19')
+	select dt, content from datalake_raw.asterisk_logs_full
+	where date(from_iso8601_timestamp(dt)) = date('{partition_date}')
 ),
 ids_calls as (
-    select
-        min(regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 1)) as ts_time_ocurred,
-        regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 2) as id_call
-    from asterisk_data
-    group by 2
+	select
+		min(regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 1)) as ts_time_ocurred,
+		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 2) as id_call
+	from asterisk_data
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)')=true
+	group by 2
 ),
 event_start_call as (
 	select
@@ -16,14 +17,15 @@ event_start_call as (
 	    'call' as phase,
 	    'call_started' as name,
 	    case
-	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='SIP' then 'call_received'
-	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='PJSIP' then 'call_made'
-	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='Local' then 'call_direct_received'
+	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='SIP' then 'incoming_call'
+	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='PJSIP' then 'made_call'
+	    	when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 3)='Local' then 'incoming_call_directly'
 	    end as params,
 		min(regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)', 1)) as ts_time_ocurred    
 	from asterisk_data
 	right join ids_calls id_c
 	on (id_call=id_c.id_call and ts_time_ocurred=id_c.ts_time_ocurred)
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("(\w+)')=true		
 	group by 1,2,3,4,5
 ),
 event_end_call as (
@@ -40,7 +42,7 @@ event_end_call as (
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("(PJSIP|SIP|Local)/\d+(@from-queue)?-(\w+)", "(HANGUP CAUSE: \d+)"\)', 6) as params,
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("(PJSIP|SIP|Local)/\d+(@from-queue)?-(\w+)", "(HANGUP CAUSE: \d+)"\)', 1) as ts_time_ocurred
 	from asterisk_data
-	where content like '%HANGUP CAUSE:%'
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+NoOp\("(PJSIP|SIP|Local)/\d+(@from-queue)?-(\w+)", "(HANGUP CAUSE: \d+)"\)')=true
 	group by 1,2,3,4,5,6
 ),
 event_start_ura as (
@@ -52,7 +54,7 @@ event_start_ura as (
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/\d+-(\w+)", "(__CRM_SOURCE=\d+)"', 4) as params,
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/\d+-(\w+)", "(__CRM_SOURCE=\d+)"', 1) as ts_time_ocurred
 	from asterisk_data
-	where content like '%__CRM_SOURCE%' and content like '%SIP%'
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("SIP/\d+-(\w+)", "(__CRM_SOURCE=\d+)"')=true
 	group by 1,2,3,4,5,6
 ),
 event_start_queue as (
@@ -64,7 +66,7 @@ event_start_queue as (
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QAGENT=\d+)"', 4) as params,
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QAGENT=\d+)"', 1) as ts_time_ocurred
 	from asterisk_data
-	where content like '%QAGENT%'
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QAGENT=\d+)"')=true
 	group by 1,2,3,4,5,6
 ),
 event_set_queue_num as (
@@ -76,7 +78,7 @@ event_set_queue_num as (
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QUEUENUM=\d+)"', 4) as params,
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QUEUENUM=\d+)"', 1) as ts_time_ocurred
 	from asterisk_data
-	where content like '%QUEUENUM%'
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("Local/\d+@from-queue-(\w+);\d", "(QUEUENUM=\d+)"')=true	
 	group by 1,2,3,4,5,6
 ),
 event_set_destination as ( -- only to calls made by agents
@@ -88,6 +90,7 @@ event_set_destination as ( -- only to calls made by agents
 		regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("PJSIP/\d+-(\w+)", "(__CRM_DESTINATION=\d+)"\) in new stack', 4) as params,
 		regexp_extract(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("PJSIP/\d+-(\w+)", "(__CRM_DESTINATION=\d+)"\) in new stack', 1) as ts_time_ocurred
     from asterisk_data
+	where regexp_like(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+Set\("PJSIP/\d+-(\w+)", "(__CRM_DESTINATION=\d+)"\) in new stack')=true		 
     group by 1,2,3,4,5,6   
 ),
 events_attendance as (
@@ -98,6 +101,7 @@ events_attendance as (
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',5) as id_queue,
 		regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d',1) as ts_time_ocurred	    
     from asterisk_data   
+	where regexp_like(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\] app_dial.c: PJSIP/(\d+)-(\w+) answered Local/\d+@from-queue-(\w+);\d')=true
     group by 1,2,3,4,5
 ),
 event_start_attendance as (
@@ -123,7 +127,7 @@ event_answer_agent as (
 	group by 1,2,3,4,5,6
 ),
 event_key_typed as (
-  select 
+  	select 
         regexp_extract(content,'\[(.+)\] DTMF\[[0-9]+\]\[C-(\w+)\] channel.c: DTMF begin .(\d). received on (SIP|PJSIP|Local)/\d+(@from-queue)?-(\w+)', 2) as id_call,  
         regexp_extract(content,'\[(.+)\] DTMF\[[0-9]+\]\[C-(\w+)\] channel.c: DTMF begin .(\d). received on (SIP|PJSIP|Local)/\d+(@from-queue)?-(\w+)', 6) as id_phase, 
 		case 
@@ -134,31 +138,34 @@ event_key_typed as (
 		'key_typed'as name,          
         regexp_extract(content,'\[(.+)\] DTMF\[[0-9]+\]\[C-(\w+)\] channel.c: DTMF begin .(\d). received on (SIP|PJSIP|Local)/\d+(@from-queue)?-(\w+)', 3) as params,
         regexp_extract(content,'\[(.+)\] DTMF\[[0-9]+\]\[C-(\w+)\] channel.c: DTMF begin .(\d). received on (SIP|PJSIP|Local)/\d+(@from-queue)?-(\w+)', 1) as ts_time_ocurred
-  from asterisk_data
-  group by 1,2,3,4,5,6
+	from asterisk_data
+	where regexp_like(content, '\[(.+)\] DTMF\[[0-9]+\]\[C-(\w+)\] channel.c: DTMF begin .(\d). received on (SIP|PJSIP|Local)/\d+(@from-queue)?-(\w+)')=true
+	group by 1,2,3,4,5,6
 ),
 event_audio_message as (
-  select
-    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 2) as id_call,
-    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 4) as id_phase,
-	case 
-		when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 3)='SIP' then 'ura'
-		when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 3)='PJSIP' then 'attendance' 
-	end as phase,
-	'audio_message_started' as name, 
-    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 6) as params,
-    regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 1) as ts_time_ocurred
-  from asterisk_data
-  group by 1,2,3,4,5,6
+  	select
+    	regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 2) as id_call,
+    	regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 4) as id_phase,
+		case 
+			when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 3)='SIP' then 'ura'
+			when regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 3)='PJSIP' then 'attendance' 
+		end as phase,
+		'audio_message_started' as name, 
+    	regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 6) as params,
+    	regexp_extract(content,'\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*', 1) as ts_time_ocurred
+  	from asterisk_data
+	where regexp_like(content, '\[(.+)\] VERBOSE\[[0-9]+\]\[C-(\w+)\].+<(SIP|PJSIP)/\d+-(\w+)> Playing .?(.custom/)?(.+)\.(ulaw.?|gsm).*')=true
+  	group by 1,2,3,4,5,6
 )
-select * from event_start_call where id_call is not null 
-union select * from event_end_call where id_call is not null  
-union select * from event_start_ura where id_call is not null and id_phase is not null 
-union select * from event_start_queue where id_call is not null and id_phase is not null
-union select * from event_set_queue_num where id_call is not null and id_phase is not null and value_event is not null
-union select * from event_start_attendance where id_call is not null and id_phase is not null 
-union select * from event_answer_agent where id_call is not null and id_phase is not null
-union select * from event_key_typed where id_call is not null and id_phase is not null
-union select * from event_set_destination where id_call is not null and id_phase is not null and value_event is not null
-union select * from event_audio_message where id_call is not null and id_phase is not null and value_event is not null
+
+select * from event_start_call where id_call is not null
+union all select * from event_end_call where id_call is not null  
+union all select * from event_start_ura where id_call is not null and id_phase is not null 
+union all select * from event_start_queue where id_call is not null and id_phase is not null
+union all select * from event_set_queue_num where id_call is not null and id_phase is not null and params is not null
+union all select * from event_start_attendance where id_call is not null and id_phase is not null 
+union all select * from event_answer_agent where id_call is not null and id_phase is not null
+union all select * from event_key_typed where id_call is not null and id_phase is not null
+union all select * from event_set_destination where id_call is not null and id_phase is not null and params is not null
+union all select * from event_audio_message where id_call is not null and id_phase is not null and params is not null
 ;
