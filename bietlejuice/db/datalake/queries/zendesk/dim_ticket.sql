8e1054c2f6439cd59e2779f7e29a1903bcc0330b
Leonardo Santos <3fsleo@gmail.com>
2019-04-03 14:21:41 -0300

    Fix dim_ticket custom_fields regex to get field_id

--- bietlejuice/db/datalake/queries/zendesk/dim_ticket.sql
+++ bietlejuice/db/datalake/queries/zendesk/dim_ticket.sql
@@ -26,7 +26,7 @@ last_ticket_groups as (
 parse_fields as (
     select zt.id,
         f1.field,
-        regexp_extract(f1.field, '{\\?"id\\?":"(\d+)"', 1) as field_id,
+        regexp_extract(f1.field, '{\\?"id\\?":"?(\d+)"?', 1) as field_id,
         nullif(regexp_extract(f1.field, '"value\\?":\\?"?([^\\?"|}]+)', 1), 'null') as value
     from tickets_filter zt
     cross join unnest(regexp_extract_all(zt.custom_fields, '{[^}]+[^,]+[^{]+}')) as f1(field)
