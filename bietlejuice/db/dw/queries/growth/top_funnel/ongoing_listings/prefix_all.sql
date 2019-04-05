e57bf7d302be26ef93f88ae686404316d6a449a7
carol.cavalcante <carolinavalanic@gmail.com>
2019-04-05 17:53:51 -0300

    Adjusting ongoing listings on fact growth to apply current business rule

--- bietlejuice/db/dw/queries/growth/top_funnel/ongoing_listings/prefix_all.sql
+++ bietlejuice/db/dw/queries/growth/top_funnel/ongoing_listings/prefix_all.sql
@@ -1,38 +1,53 @@
-with all_dates as (
+with
+fact_house_status_filter as (
+-- new step so we can filter last version and also last status (in case there are two status in the same day)
+select
+    fhs.*,
+    rank() over(partition by dd.week_start, fhs.sk_house order by coalesce(fhs.sk_max_status_date, to_char(current_date -1, 'YYYYMMDD')::bigint) desc) as rk,
+    dh.is_last_version,
+    dd.date,
+    dd.year,
+    dd.month,
+    dd.calendar_week,
+    dd.day
+from fact_house_status fhs
+join dim_date dd
+		on dd.sk_date between fhs.sk_min_status_date and coalesce(fhs.sk_max_status_date, to_char(current_date - 1, 'YYYYMMDD')::bigint)
+left join dim_house_listing dh
+  on fhs.sk_house = dh.sk_house_listing
+),
+all_dates as (
   select distinct
-    dd.year as _year,
-    dd.month as _month,
-    dd.calendar_week as _week,
-    dd.day as _day,
+    f.year as _year,
+    f.month as _month,
+    f.calendar_week as _week,
+    f.day as _day,
     'QuintoAndar'::varchar as region,
     'QuintoAndar'::varchar as city,
-    dense_rank() over (partition by dd.year,
-    																dd.month,
-    																dd.calendar_week,
-    																dd.day order by f.sk_house asc)
-    	+ dense_rank() over (partition by dd.year,
-    																		dd.month,
-    																		dd.calendar_week,
-    																		dd.day order by f.sk_house desc)
+    dense_rank() over (partition by f.year,
+    																f.month,
+    																f.calendar_week,
+    																f.day order by f.sk_house asc)
+    	+ dense_rank() over (partition by f.year,
+    																		f.month,
+    																		f.calendar_week,
+    																		f.day order by f.sk_house desc)
 			- 1 as daily_count,
-    dense_rank() over (partition by dd.year,
-    																dd.calendar_week order by f.sk_house asc)
-    	+ dense_rank() over (partition by dd.year,
-    																		dd.calendar_week order by f.sk_house desc)
+    dense_rank() over (partition by f.year,
+    																f.calendar_week order by f.sk_house asc)
+    	+ dense_rank() over (partition by f.year,
+    																		f.calendar_week order by f.sk_house desc)
 			- 1 as weekly_count,
-    dense_rank() over (partition by dd.year,
-    																dd.month order by f.sk_house asc)
-    	+ dense_rank() over (partition by dd.year,
-    																		dd.month order by f.sk_house desc)
+    dense_rank() over (partition by f.year,
+    																f.month order by f.sk_house asc)
+    	+ dense_rank() over (partition by f.year,
+    																		f.month order by f.sk_house desc)
 			- 1 as monthly_count,
-    dense_rank() over (partition by dd.year order by f.sk_house asc)
-    	+ dense_rank() over (partition by dd.year order by f.sk_house desc)
+    dense_rank() over (partition by f.year order by f.sk_house asc)
+    	+ dense_rank() over (partition by f.year order by f.sk_house desc)
 			- 1 as yearly_count
-	from fact_house_status f
-	join dim_date dd
-		on dd.sk_date between f.sk_min_status_date and coalesce(f.sk_max_status_date, to_char(current_date - 1, 'YYYYMMDD')::bigint)
-			and f.status_history = 'publicado'
-	where dd."date" < current_date
+	from fact_house_status_filter f
+	where f."date" < current_date and f.is_last_version = true and f.rk = 1 and f.status_history = 'publicado'
   order by 1, 2, 3, 4
 ),
 all_dates_last_week as (
@@ -40,37 +55,31 @@ all_dates_last_week as (
 ),
 all_dates_last_month as (
 	select
-	 	dd.year as _year,
-	  dd.month as _month,
+	  year as _year,
+	  month as _month,
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_house) as monthly_count
-	from fact_house_status f
-	join dim_date dd
-		on dd.sk_date between f.sk_min_status_date and coalesce(f.sk_max_status_date, to_char(current_date - 1, 'YYYYMMDD')::bigint)
-			and f.status_history = 'publicado'
-	where dd."date" < current_date
-		and dd.year = date_part('year', add_months(current_date, -1))
-  	and dd.month = date_part('month', add_months(current_date, -1))
-  	and dd.day < date_part('day', current_date)
+	from fact_house_status_filter f
+	where f."date" < current_date and f.is_last_version = true and f.rk = 1 and f.status_history = 'publicado'
+		and f.year = date_part('year', add_months(current_date, -1))
+  	and f.month = date_part('month', add_months(current_date, -1))
+  	and f.day < date_part('day', current_date)
   group by 1, 2
   order by 1, 2
 ),
 all_dates_last_year as (
 	select
-	 	dd.year as _year,
+	   year as _year,
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_house) as yearly_count
-	from fact_house_status f
-	join dim_date dd
-		on dd.sk_date between f.sk_min_status_date and coalesce(f.sk_max_status_date, to_char(current_date - 1, 'YYYYMMDD')::bigint)
-			and f.status_history = 'publicado'
-	where dd."date" < current_date
-	  and dd.year = date_part('year', add_months(current_date, -12))
-  		and ((dd.month = date_part('month', add_months(current_date, -12))
-  		      and dd.day < date_part('day', add_months(current_date, -12)))
-  		  or dd.month < date_part('month', add_months(current_date, -12))
+	from fact_house_status_filter f
+	where f."date" < current_date and f.is_last_version = true and f.rk = 1 and f.status_history = 'publicado'
+	  and f.year = date_part('year', add_months(current_date, -12))
+  		and ((f.month = date_part('month', add_months(current_date, -12))
+  		      and f.day < date_part('day', add_months(current_date, -12)))
+  		  or f.month < date_part('month', add_months(current_date, -12))
   		  )
   group by 1
   order by 1
