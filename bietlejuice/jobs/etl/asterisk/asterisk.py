354a9c3479caf736f6b637d17d65bfd4f233d47d
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-04-18 15:13:26 -0300

    Update bietlejuice/jobs/etl/asterisk/asterisk.py
    Co-Authored-By: darambaris <jessika.darambaris@gmail.com>

--- bietlejuice/jobs/etl/asterisk/asterisk.py
+++ bietlejuice/jobs/etl/asterisk/asterisk.py
@@ -134,7 +134,7 @@ class Asterisk(object):
     @logger
     def _upsert_partitions(self, class_, bucket_type, partitions_list_dicts):
         if bucket_type not in ('raw', 'clean'):
-            raise ValueError('m=_upsert_partition, bucket_type={}, msg=invalid bucket type'.format(bucket_type))
+            raise ValueError('m=_upsert_partitions, bucket_type={}, msg=invalid bucket type'.format(bucket_type))
 
         self.athena_client.upsert_partitions(
             bucket_folder_path='{}/{}/asterisk/{}'.format(self.s3_bucket, bucket_type, class_.value),
