693c865eb79125b84ad554902dc303ec3d1427d2
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-04-23 19:17:41 -0300

    Fixing uncommitted changes

--- bietlejuice/jobs/dags/bi_agents_allocation_optimization.py
+++ bietlejuice/jobs/dags/bi_agents_allocation_optimization.py
@@ -19,6 +19,19 @@ MAIN_DAG_ID = 'bi-agents-allocation-optimization'
 MAIN_START_DATE = datetime(2019, 1, 1)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 8 * * *')
 
+CREATE_JOB_FLOW_TASK_ID = 'create_job_flow'
+MACROS = {
+    'xcom_job_flow_push': '{{ ' + "task_instance.xcom_pull('{job_flow_task_id}', key='return_value')".format(
+        job_flow_task_id=CREATE_JOB_FLOW_TASK_ID) + ' }}',
+    'xcom_job_flow_pull': '{{ ' + "task_instance.xcom_pull(dag_id='{dag_id}.{job_flow_subdag_id}', "
+                                  "task_ids='{job_flow_task_id}', key='return_value')".format(
+        dag_id=MAIN_DAG_ID,
+        job_flow_subdag_id=CREATE_JOB_FLOW_TASK_ID,
+        job_flow_task_id=CREATE_JOB_FLOW_TASK_ID
+    ) + ' }}',
+    'xcom_step_pull': "task_instance.xcom_pull('{step}_step', key='return_value')[0]"
+}
+
 # env vars
 s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
 bi_hekima_json = json.loads(env.get_airflow_env_var('bi-agents-allocation-optimization')
@@ -62,14 +75,14 @@ def create_job_flow_sub_dag(sub_dag_name, **kwargs):
     )._build_local_dag()
 
     create_job_flow_task = EmrCreateJobFlowOperator(
-        task_id='create_job_flow',
+        task_id=CREATE_JOB_FLOW_TASK_ID,
         job_flow_overrides=bi_hekima_json['job_flow_overrides'],
         dag=local_dag
     )
 
     create_job_flow_sensor = QuintoAndarEmrJobFlowSensor(
-        task_id='check_create_job_flow',
-        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        task_id='check_{}'.format(CREATE_JOB_FLOW_TASK_ID),
+        job_flow_id=MACROS['xcom_job_flow_push'],
         dag=local_dag
     )
 
@@ -89,15 +102,15 @@ def send_step_sub_dag(sub_dag_name, step, **kwargs):
 
     step_task = EmrAddStepsOperator(
         task_id='{}_step'.format(step),
-        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        job_flow_id=MACROS['xcom_job_flow_pull'],
         steps=bi_hekima_json['{}_params'.format(step)],
         dag=local_dag
     )
 
     step_sensor = EmrStepSensor(
         task_id='check_{}_step'.format(step),
-        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
-        step_id="{{ task_instance.xcom_pull('{}_step', key='return_value')[0] }}".format(step),
+        job_flow_id=MACROS['xcom_job_flow_pull'],
+        step_id='{{ ' + MACROS['xcom_step_pull'].format(step=step) + ' }}',
         dag=local_dag
     )
 
@@ -117,13 +130,13 @@ def terminate_job_flow_sub_dag(sub_dag_name, **kwargs):
 
     terminate_job_flow_task = EmrTerminateJobFlowOperator(
         task_id='terminate_job_flow',
-        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        job_flow_id=MACROS['xcom_job_flow_pull'],
         dag=local_dag
     )
 
     check_terminate_job_flow_sensor = QuintoAndarEmrJobFlowSensor(
         task_id='check_terminate_job_flow',
-        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        job_flow_id=MACROS['xcom_job_flow_pull'],
         dag=local_dag
     )
 
