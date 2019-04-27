3133539c676d2659da827ed02bca89f84fc85ee4
Ribaldo <ribaldorafael@gmail.com>
2019-04-27 09:28:02 -0300

    Adding trigger execution to bi-agents-allocation from bi-supply-demand

--- bietlejuice/jobs/dags/bi_agents_allocation_optimization.py
+++ bietlejuice/jobs/dags/bi_agents_allocation_optimization.py
@@ -17,7 +17,7 @@ from bietlejuice.jobs.sensors import QuintoAndarEmrJobFlowSensor
 # global vars
 MAIN_DAG_ID = 'bi-agents-allocation-optimization'
 MAIN_START_DATE = datetime(2019, 1, 1)
-MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 8 * * *')
+MAIN_SCHEDULE_INTERVAL = None
 
 CREATE_JOB_FLOW_TASK_ID = 'create_job_flow'
 MACROS = {
