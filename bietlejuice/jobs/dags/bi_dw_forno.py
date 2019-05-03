ed7ed4cbcdb30121c4ca1289883e96eae4e7504f
felipemiquelim <felipemiquelim@gmail.com>
2019-05-03 11:52:47 -0300

    Adjusting minor code issues and adding DE standard to loggers

--- bietlejuice/jobs/dags/bi_dw_forno.py
+++ bietlejuice/jobs/dags/bi_dw_forno.py
@@ -5,45 +5,43 @@ import airflow.utils.helpers as airflow_helpers
 from airflow.models import DAG
 from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.dags.util import environment as env
-from bietlejuice.jobs.wrappers.Redshift import Redshift
-from qa_python_utils import QuintoAndarLogger
+from bietlejuice.jobs.wrappers.redshift import RedshiftClient
 
 DW_PROD_ID = env.get_airflow_env_var('DW_PROD_ID')
 DW_FORNO_ID = env.get_airflow_env_var('DW_FORNO_ID')
 DW_FORNO_CONFIGS = json.loads(env.get_airflow_env_var('DW_FORNO_CONFIGS'))
 
 # global vars
-logger = QuintoAndarLogger('dw_forno')
-MAIN_DAG_ID = 'bi-dw_forno'
+MAIN_DAG_ID = 'bi-dw-forno'
 MAIN_START_DATE = datetime(2019, 1, 1)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 0 * * 1')
 
 
 def shutdown_cluster(target_cluster):
-    rs_client = Redshift()
+    rs_client = RedshiftClient()
     # if there is no cluster with given id, do not tries to shutdown
     if rs_client.check_if_cluster_exists(cluster_id=target_cluster):
         rs_client.shutdown_cluster(cluster_id=target_cluster)
 
 
 def check_cluster_shutdown(target_cluster):
-    rs_client = Redshift()
+    rs_client = RedshiftClient()
     rs_client.wait_for_cluster_shutdown(cluster_id=target_cluster)
 
 
 def create_cluster(source_cluster, target_cluster, config_json):
-    rs_client = Redshift()
+    rs_client = RedshiftClient()
     rs_client.create_cluster_from_snapshot(source_cluster_id=source_cluster, target_cluster_id=target_cluster,
                                            config_json=config_json)
 
 
 def check_cluster_availability(target_cluster):
-    rs_client = Redshift()
+    rs_client = RedshiftClient()
     rs_client.wait_for_cluster_availability(cluster_id=target_cluster)
 
 
 def scale_down_cluster(target_cluster):
-    rs_client = Redshift()
+    rs_client = RedshiftClient()
     rs_client.scale_down_cluster(cluster_id=target_cluster)
 
 
