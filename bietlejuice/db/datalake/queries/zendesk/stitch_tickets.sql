701ea5c2c2b86dc0acef1ad57de3b23def62ab77
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-05-08 14:31:41 -0300

    set timezone UTC

--- bietlejuice/db/datalake/queries/zendesk/stitch_tickets.sql
+++ bietlejuice/db/datalake/queries/zendesk/stitch_tickets.sql
@@ -6,21 +6,24 @@ with stitch_data as (
 	   		from_iso8601_timestamp(created_at) <= cast('2018-11-04 03:00:00 UTC' as timestamp)),
 			from_iso8601_timestamp(created_at) at time zone 'GMT-3',
 			from_iso8601_timestamp(created_at) at time zone 'Brazil/East') as ts_created_local,
+		regexp_extract(satisfaction_rating, '\{"score":"(\w+)".+', 1) as score,
+		regexp_extract(via, '\{"channel":"(\w+)".+', 1) as channel,
     	row_number() over (partition by id, dt order by updated_at desc) as last_updated
     from stitch.tickets
     where regexp_extract(via, '\{"channel":"(\w+)".+', 1) is not null
 		  and raw_subject != 'SCRUBBED'
-		  and dt = '{}'
+		  and subject not like '%WhatsApp - Comunicado%'
+		  and dt = '{execution_date}'
 )
 select 
     id as ticket_id, 
     satisfaction_rating,
     url as ticket_url,
     priority,
-    regexp_extract(via, '\{"score":"(\w+)".+', 1) as score,
+    score,
     raw_subject,
     subject,
-    regexp_extract(via, '\{"channel":"(\w+)".+', 1) as channel,
+    channel,
     tags,
     group_id,
     ticket_form_id, 
@@ -37,9 +40,9 @@ select
     description,
     recipient,
     is_public,
-    created_at as ts_created,
-    ts_created_local,
-    updated_at as ts_updated,
+    cast(from_iso8601_timestamp(created_at) as varchar) as ts_created,
+    cast(ts_created_local as varchar) as ts_created_local,
+    cast(from_iso8601_timestamp(updated_at) as varchar) as ts_updated,
     now() as ts_load
 from stitch_data t
-where last_updated = 1
+where last_updated = 1;
