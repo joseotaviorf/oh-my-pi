93a8a23f258188f2597d275a28ea7e3c7539e380
felipemiquelim <felipemiquelim@gmail.com>
2019-05-10 19:27:33 -0300

    Adding fact_lead_tasks query

---
+++ bietlejuice/db/dw/queries/crm/fact_lead_tasks.sql
@@ -0,0 +1,66 @@
+ with task_metrics as (
+ SELECT
+    sk_task,
+    sk_lead,
+	case when action_type = 'CREATE' then 
+ 		min(sk_action_date) over (partition by sk_task order by ts_action ROWS UNBOUNDED PRECEDING) 
+ 		end	as sk_created_date,
+	case when action_type = 'REALIZE' then 
+ 		min(sk_action_date) over (partition by sk_task order by ts_action ROWS UNBOUNDED PRECEDING) 
+ 		end as sk_first_realized_date,
+	case when action_type = 'RESOLVE' then 
+ 		min(sk_action_date) over (partition by sk_task order by ts_action ROWS UNBOUNDED PRECEDING) 
+ 		end as sk_first_resolved_date,
+	row_number() over(PARTITION BY sk_task ORDER BY ts_action) as rn,
+	count(1) over (PARTITION by sk_task) as total_actions,
+    action_type,
+    sk_assignee
+ FROM crm.fact_lead_tasks flta -- to be crm.fact_lead_task_actions
+),
+task_dates as (
+select 
+	tm.sk_task,
+	tm.sk_lead,
+	min(tm.sk_created_date) as sk_created_date,
+	min(tm.sk_first_realized_date) as sk_first_realized_date,
+	min(tm.sk_first_resolved_date) as sk_first_resolved_date
+from task_metrics tm
+group by 1,2
+)
+, first_assignee as (
+select 
+	sk_task,
+	min(rn) as min_rn		
+from task_metrics tm
+where sk_assignee <> -1
+group by 1
+)
+, first_resolver as (
+select 
+	sk_task,
+	min(rn) as min_rn		
+from task_metrics tm
+where action_type = 'REALIZE'
+group by 1
+)
+select
+	td.sk_task,
+	td.sk_lead,
+	coalesce(td.sk_first_realized_date, -1) + COALESCE(td.sk_first_resolved_date, -1) <> -2 as is_closed,
+	coalesce(td.sk_created_date, -1) as sk_created_date,
+	coalesce(td.sk_first_realized_date, -1) as sk_first_realized_date,
+	coalesce(td.sk_first_resolved_date, -1) as sk_first_resolved_date,
+	coalesce(tm.sk_assignee, -1) as sk_user_first_asignee,
+	coalesce(tm2.sk_assignee, -1) as sk_rank_user_first_resolver
+into crm.fact_lead_tasks_test
+from task_dates td
+join first_assignee fa
+	on td.sk_task = fa.sk_task
+join task_metrics tm 
+	on tm.sk_task = fa.sk_task
+	and tm.rn = fa.min_rn
+left join first_resolver fr
+	on td.sk_task = fr.sk_task
+left join task_metrics tm2 
+	on tm2.sk_task = fr.sk_task
+	and tm2.rn = fr.min_rn
