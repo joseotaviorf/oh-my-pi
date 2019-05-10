387cd30af1788556d26b064049278c112c60be45
felipemiquelim <felipemiquelim@gmail.com>
2019-05-10 14:56:27 -0300

    Adding parameters to move_file_query_data_to_db method

--- bietlejuice/jobs/base/base_etl.py
+++ bietlejuice/jobs/base/base_etl.py
@@ -648,8 +648,12 @@ class BaseETL(object):
         )
 
     @classmethod
-    def move_file_query_data_to_db(cls, schema, file_name, append, db_enum_source, db_enum_destination):
+    def move_file_query_data_to_db(cls, schema, file_name, table_name, append, db_enum_source, db_enum_destination,
+                                   query_params_dict=None):
         query = BaseETL.get_query_from_file_name(file_name=file_name)
+
+        if query_params_dict:
+            query = query.format(**query_params_dict)
 
         table = BaseETL.from_db_query(
             db_enum=db_enum_source,
@@ -657,7 +661,7 @@ class BaseETL(object):
 
         BaseETL.bulk_insert(
             table=table,
-            table_name='{}.{}'.format(schema, file_name),
+            table_name='{}.{}'.format(schema, table_name),
             db_enum=db_enum_destination,
             encoding='UTF-8',
             append=append
