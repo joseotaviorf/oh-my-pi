b89d60c77d20da857e5ae2ab792e4ccce53a7a53
felipemiquelim <felipemiquelim@gmail.com>
2019-05-14 10:59:24 -0300

    Raising timeout in truncate_table method

--- bietlejuice/jobs/base/base_etl.py
+++ bietlejuice/jobs/base/base_etl.py
@@ -316,7 +316,7 @@ class BaseETL(object):
         cls.execute_command(
             command='TRUNCATE TABLE "{}"."{}";'.format(schema, table_name),
             db_enum=db_enum,
-            timeout=30,
+            timeout=300,
             commit=True
         )
 
