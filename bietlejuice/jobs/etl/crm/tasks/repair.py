12db0fdab60aa39077f0993a6b8ac239dad0b439
Ribaldo <ribaldorafael@gmail.com>
2019-05-15 09:42:03 -0300

    Reformating crm repair tasks classes

--- bietlejuice/jobs/etl/crm/tasks/repair.py
+++ bietlejuice/jobs/etl/crm/tasks/repair.py
@@ -6,7 +6,11 @@ logger = QuintoAndarLogger('CRMTasksRepair')
 
 
 class CRMTasksRepair(CRMTasks):
-    MANUAL_TASK_WORKGROUP_IDS = ['DEP_MEDIACAO_POS_CONTRATO_ID', 'PROTECTION_CUSTOMERS', 'PROTECTION_PARTNERS']
+    MANUAL_TASK_WORKGROUP_IDS = [
+        'DEP_MEDIACAO_POS_CONTRATO_ID',
+        'PROTECTION_CUSTOMERS',
+        'PROTECTION_PARTNERS'
+    ]
 
     TABLE_NAMES = {
         'fact': 'fact_repair_tasks',
