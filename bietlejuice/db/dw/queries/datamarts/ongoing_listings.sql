57598dcb993c5b17b17079d22be8ecdd5adf67f2
fredericosouza <frederico.souza@quintoandar.com.br>
2019-05-15 17:11:37 -0300

    Creating datamart with ongoing and suspended listings.

---
+++ bietlejuice/db/dw/queries/datamarts/ongoing_listings.sql
@@ -0,0 +1,27 @@
+select
+    week_start,
+    week_since_publication,
+	sk_house
+from(
+select
+    fhs.*,
+    rank() over(partition by dd.week_start, fhs.sk_house order by coalesce(fhs.sk_min_status_date, to_char(current_date -1, 'YYYYMMDD')::bigint) desc) as rk,
+    dr.city_name as city,
+    dd.date,
+    date(dh.ts_publication) as ts_publication,
+    date_diff('week', dh.ts_publication, dd.week_start) as week_since_publication,
+    dd.weekday_name,
+    dd.week_start,
+    dh.is_last_version
+from fact_house_status fhs
+join dim_date dd
+  on dd.sk_date between fhs.sk_min_status_date and coalesce(fhs.sk_max_status_date, to_char(current_date -1, 'YYYYMMDD')::bigint)
+left join dim_region dr
+     using(sk_region)
+left join dim_house_listing dh
+  on fhs.sk_house = dh.sk_house_listing
+where dd.weekday_name = 'Sunday'
+)
+where rk = 1
+  and status_history = 'publicado'
+  and week_start = '2018-01-01'
