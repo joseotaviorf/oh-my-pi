3729b098c156eb1bb3887ba08d0d8b98784fb347
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-05-17 17:36:26 -0300

    missing import

--- bietlejuice/jobs/dags/zendesk/__init__.py
+++ bietlejuice/jobs/dags/zendesk/__init__.py
@@ -3,3 +3,4 @@ from bietlejuice.jobs.dags.zendesk.tickets import ZendeskTickets
 from bietlejuice.jobs.dags.zendesk.ticket_fields import ZendeskTicketFields
 from bietlejuice.jobs.dags.zendesk.groups import ZendeskGroups
 from bietlejuice.jobs.dags.zendesk.users import ZendeskUsers
+from bietlejuice.jobs.dags.zendesk.group_memberships import ZendeskGroupMemberships
