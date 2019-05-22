ce4cf7baacf5f26699ef108aa8d421807b2e9e44
felipemiquelim <felipemiquelim@gmail.com>
2019-05-22 10:11:26 -0300

    Increasing amplitude load event timeout

--- bietlejuice/jobs/dags/bi_amplitude_load_events.py
+++ bietlejuice/jobs/dags/bi_amplitude_load_events.py
@@ -98,7 +98,8 @@ xcom_amplitude_load_events_task = BaseDAG.build_python_operator(
     dag=dag,
     task_id='XCom_amplitude_load_events',
     python_callable=xcom_amplitude_load_events,
-    provide_context=True
+    provide_context=True,
+    execution_timeout=timedelta(hours=5)
 )
 
 load_active_user_sessions_raw_task = BaseDAG.build_python_operator(
