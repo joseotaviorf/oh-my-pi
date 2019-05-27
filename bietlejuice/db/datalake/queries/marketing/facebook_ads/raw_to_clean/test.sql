1f4bb4e8956e825df39839a41c361084c618ea49
felipemiquelim <felipemiquelim@gmail.com>
2019-05-27 15:26:51 -0300

    Adjusting facebook validation query

--- bietlejuice/db/datalake/queries/marketing/facebook_ads/raw_to_clean/test.sql
+++ bietlejuice/db/datalake/queries/marketing/facebook_ads/raw_to_clean/test.sql
@@ -1,6 +1,6 @@
 with t_rows as (
 SELECT count(1) as total_rows FROM datalake_raw.{table_name}
-WHERE dt_created = '{date}' AND acc = '{account}'
+WHERE dt = '{date}' AND acc = '{account}'
 UNION ALL
 SELECT count(1) * -1 as total_rows FROM datalake_clean.{table_name}
 WHERE dt_created = '{date}' AND acc = '{account}'
