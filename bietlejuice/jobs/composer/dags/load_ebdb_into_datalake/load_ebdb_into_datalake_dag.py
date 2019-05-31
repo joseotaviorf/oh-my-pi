07321bea791672a4ccb5312da627611309b3f8e1
Rafael Leinio <rafael.leper@gmail.com>
2019-05-31 14:40:38 -0300

    add BranchPythonOperator to ebdb dag

--- bietlejuice/jobs/composer/dags/load_ebdb_into_datalake/load_ebdb_into_datalake_dag.py
+++ bietlejuice/jobs/composer/dags/load_ebdb_into_datalake/load_ebdb_into_datalake_dag.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 
 from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from airflow.operators.python_operator import BranchPythonOperator
 from airflow.models import DAG
 
 from bietlejuice.jobs.base import BaseDAG
+from bietlejuice.jobs.composer.etl.load_ebdb_into_datalake import load_ebdb_into_datalake_etl
 
 DAG_ID = 'load-ebdb-into-datalake'
+ebdb_raw_file_path = 'dbfs:/FileStore/airflow/bla.py'
+create_athena_external_tables_file_path = 'dbfs:/FileStore/airflow/bla.py'
 
 new_cluster = {
     'cluster_name': DAG_ID,
@@ -32,14 +36,62 @@ dag = DAG(
     catchup=False
 )
 
-load_ebdb_into_datalake_raw_task = DatabricksSubmitRunOperator(
-    task_id='load_ebdb_into_datalake_raw_task',
+
+def check_if_ebdb_exists():
+    client = load_ebdb_into_datalake_etl.get_athena_client()
+    execution_id = load_ebdb_into_datalake_etl.execute_athena_query(client, 'show tables in ebdb', 'ebdb')
+    results = client.get_query_results(QueryExecutionId=execution_id, MaxResults=1000)
+    ebdb_exists = 'ebdb' in [row['Data'][0]['VarCharValue'] for row in results['ResultSet']['Rows']]
+    if ebdb_exists:
+        return 'ebdb_raw_daily'
+    else:
+        return 'ebdb_raw_first_time'
+
+
+check_if_ebdb_exists = BranchPythonOperator(task_id='check_if_ebdb_exists', python_callable=check_if_ebdb_exists)
+
+ebdb_raw_first_time = DatabricksSubmitRunOperator(
+    task_id='ebdb_raw_first_time',
     dag=dag,
     provide_context=True,
     json={
         'new_cluster': new_cluster,
         'spark_python_task': {
-            'python_file': 'dbfs:/FileStore/airflow/bla.py',
+            'python_file': ebdb_raw_file_path,
+            'parameters': [
+                'first_time'
+            ]
         }
     }
 )
+
+ebdb_raw_daily = DatabricksSubmitRunOperator(
+    task_id='ebdb_raw_daily',
+    dag=dag,
+    provide_context=True,
+    json={
+        'new_cluster': new_cluster,
+        'spark_python_task': {
+            'python_file': ebdb_raw_file_path,
+            'parameters': [
+                'daily'
+            ]
+        }
+    }
+)
+
+create_athena_external_tables = DatabricksSubmitRunOperator(
+    task_id='create_athena_external_tables',
+    dag=dag,
+    provide_context=True,
+    json={
+        'new_cluster': new_cluster,
+        'spark_python_task': {
+            'python_file': create_athena_external_tables_file_path
+        }
+    }
+)
+
+# dependencies
+check_if_ebdb_exists >> ebdb_raw_first_time >> create_athena_external_tables
+check_if_ebdb_exists >> ebdb_raw_daily >> create_athena_external_tables
