10089d1e4b79823686f9c398c526d660375d323f
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-05-31 19:07:48 -0300

    fix listing duplicated

--- bietlejuice/db/datalake/queries/zendesk/fact_tickets.sql
+++ bietlejuice/db/datalake/queries/zendesk/fact_tickets.sql
@@ -188,6 +188,6 @@ left join house dhl
 	on c.cols['Código do Imóvel'] = dhl.id_house
     and date_format(cast(t.ts_created as timestamp with time zone), '%Y-%m-%d')
     between dhl.dt_listing_version_start
-    and coalesce(dhl.dt_listing_version_end, date_format(now() - interval '1' day, '%Y-%m-%d'))         
+    and date_format(coalesce(cast(dhl.dt_listing_version_end as timestamp), now())  - interval '1' day,'%Y-%m-%d')       
 left join contract dc
     on c.cols['Código do Contrato'] = cast(dc.sk_contract as varchar);
