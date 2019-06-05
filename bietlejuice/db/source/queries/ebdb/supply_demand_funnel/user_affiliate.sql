1c3e7399da94e03d5bb2d0ff2959cee21e999b11
felipemiquelim <felipemiquelim@gmail.com>
2019-06-05 13:08:15 -0300

    Adjusting join in user_affiliate

--- bietlejuice/db/source/queries/ebdb/supply_demand_funnel/user_affiliate.sql
+++ bietlejuice/db/source/queries/ebdb/supply_demand_funnel/user_affiliate.sql
@@ -1,14 +1,15 @@
 select
-	id,
-	inicioAtuacao,
-	tipoAfiliado,
-	cidadeAtuacao,
-	coalesce(ativo,0) as ativo,
-	atualizadoEm,
-	criadoEm,
-	left(numeroCreci, 50) as numeroCreci,
-	origin,
-	affiliateType,
-    usuario_id
-from DadosAfiliado
-;
+	da.id,
+	da.inicioAtuacao,
+	da.tipoAfiliado,
+	da.cidadeAtuacao,
+	coalesce(da.ativo,0) as ativo,
+	da.atualizadoEm,
+	da.criadoEm,
+	left(da.numeroCreci, 50) as numeroCreci,
+	da.origin,
+	da.affiliateType,
+    u.id as user_id
+from DadosAfiliado da
+join Usuario u
+	on da.id = u.dadosAfiliado_id
