4df544bdcaaec5e84d25d5aeec49a3a8dc1429c6
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-06-07 13:51:35 -0300

    start unit tests for Stitch

--- tests/unit/jobs/etl/zendesk/conftest.py
+++ tests/unit/jobs/etl/zendesk/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from datetime import datetime
 
-from bietlejuice.jobs.etl.zendesk import ZendeskETL
+from bietlejuice.jobs.etl.zendesk import Zendesk
 
 S3_BUCKET = 's3_bucket'
 EXECUTION_DATE = datetime(2018, 1, 1)
@@ -9,7 +9,7 @@ EXECUTION_DATE = datetime(2018, 1, 1)
 
 @pytest.fixture(scope='session')
 def zendesk():
-    return ZendeskETL(
-        bucket=S3_BUCKET,
+    return Zendesk(
+        s3_bucket=S3_BUCKET,
         execution_date=EXECUTION_DATE
     )
