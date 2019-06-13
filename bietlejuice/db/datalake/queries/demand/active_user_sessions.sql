6b1959efecc2cb50bcf60f5f9b5188be1ae1786b
felipemiquelim <felipemiquelim@gmail.com>
2019-06-13 14:50:33 -0300

    Adding mkt_origin to column mapping in demand

--- bietlejuice/db/datalake/queries/demand/active_user_sessions.sql
+++ bietlejuice/db/datalake/queries/demand/active_user_sessions.sql
@@ -22,4 +22,4 @@ join
 	datalake_clean.ods_dim_date dd
 	on dd."date" = aaus.dt_event
 group by
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
