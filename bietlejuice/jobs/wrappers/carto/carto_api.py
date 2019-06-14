9b4d2914f1bf65d6d224fe554248439af6bff121
Bernardo Loureiro <lourb896@newschool.edu>
2019-06-14 13:39:51 -0300

    Improve error catching in carto api

--- bietlejuice/jobs/wrappers/carto/carto_api.py
+++ bietlejuice/jobs/wrappers/carto/carto_api.py
@@ -2,6 +2,7 @@ import urllib
 import requests
 import pandas as pd
 import gzip
+import json
 
 from qa_python_utils import QuintoAndarLogger
 
@@ -17,30 +18,42 @@ class CartoApi(object):
         self.BASE_URL_COPY = self.BASE_URL + 'sql/copyfrom'
 
     def run_sql(self, sql, data_format='JSON'):
+        url_params_encoded = urllib.pathname2url(sql)
+        url = '{}?q={}&api_key={}&format={}'.format(self.BASE_URL_SQL, url_params_encoded, self.CARTO_API_KEY, data_format)
+        logger.info('m=run_sql, msg=running SQL on CARTO, sql={}'.format(sql))
         try:
-            url_params_encoded = urllib.pathname2url(sql)
-            url = '{}?q={}&api_key={}&format={}'.format(self.BASE_URL_SQL, url_params_encoded, self.CARTO_API_KEY, data_format)
-            r = requests.get(url)
-            logger.info('m=run_sql, msg=running SQL on CARTO, sql={}, r={}'.format(sql, r.content))
+            response = requests.get(url)
         except Exception as e:
-            logger.error('m=run_sql, msg=error before running SQL on CARTO, sql={}, e={}'.format(sql, e))
+            logger.error('m=run_sql, msg=error running SQL on CARTO, sql={}, e={}'.format(sql, e))
+            raise ValueError(e)
+        logger.info('m=run_sql, msg=CARTO API responded, response={}'.format(response.content))
+        response_json = json.loads(response.content)
+        if u'error' in response_json:
+            logger.error('m=run_sql, msg=error running SQL on CARTO, e={}'.format(response_json[u'error']))
+            raise ValueError(response_json[u'error'])
 
     def upload_csv(self, csv_file_path, table_name):
+        df = pd.read_csv(csv_file_path)
+        cols = ','.join(df.columns.values.tolist())
+        gzip_file_path = csv_file_path + '.gz'
+        with open(csv_file_path, 'rb') as f_in, gzip.open(gzip_file_path, 'wb') as f_out:
+            f_out.writelines(f_in)
+        sql = 'COPY {} ({}) FROM stdin WITH (FORMAT csv, HEADER true)'.format(table_name, cols)
+        url_params_encoded = urllib.pathname2url(sql)
+        url = '{}?q={}&api_key={}'.format(self.BASE_URL_COPY, url_params_encoded, self.CARTO_API_KEY)
+        headers = {
+            'Content-Encoding': 'gzip',
+            'Content-Type': 'application/octet-stream'
+        }
+        data = open(gzip_file_path, 'rb').read()
+        logger.info('m=upload_csv, msg=uploading CSV to CARTO, table={}'.format(table_name))
         try:
-            df = pd.read_csv(csv_file_path)
-            cols = ','.join(df.columns.values.tolist())
-            gzip_file_path = csv_file_path + '.gz'
-            with open(csv_file_path, 'rb') as f_in, gzip.open(gzip_file_path, 'wb') as f_out:
-                f_out.writelines(f_in)
-            sql = 'COPY {} ({}) FROM stdin WITH (FORMAT csv, HEADER true)'.format(table_name, cols)
-            url_params_encoded = urllib.pathname2url(sql)
-            url = '{}?q={}&api_key={}'.format(self.BASE_URL_COPY, url_params_encoded, self.CARTO_API_KEY)
-            headers = {
-                'Content-Encoding': 'gzip',
-                'Content-Type': 'application/octet-stream'
-            }
-            data = open(gzip_file_path, 'rb').read()
-            r = requests.post(url, data=data, headers=headers)
-            logger.info('m=upload_csv, msg=uploading CSV to CARTO, table={}, r={}'.format(table_name, r.content))
+            response = requests.post(url, data=data, headers=headers)
         except Exception as e:
-            logger.error('m=run_sql, msg=error before uploding CSV to CARTO, table={}, e={}'.format(table_name, e))
+            logger.error('m=upload_csv, msg=error uploding CSV to CARTO, table={}, e={}'.format(table_name, e))
+            raise ValueError(e)
+        logger.info('m=upload_csv, msg=CARTO API responded, response={}'.format(response.content))
+        response_json = json.loads(response.content)
+        if u'error' in response_json:
+            logger.error('m=upload_csv, msg=error uploding CSV to CARTO, table={}, e={}'.format(table_name, response_json[u'error']))
+            raise ValueError(response_json[u'error'])
