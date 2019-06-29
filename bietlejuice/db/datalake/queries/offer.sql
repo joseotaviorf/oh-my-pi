a650dacddaf2c014deda18ca64440614277df99b
Ribaldo <ribaldorafael@gmail.com>
2019-06-29 11:20:09 -0300

    Renaming dim_offer columns

--- bietlejuice/db/datalake/queries/offer.sql
+++ bietlejuice/db/datalake/queries/offer.sql
@@ -1,33 +1,34 @@
 with analysis_date as (
-select
-oa.id,
-min(cast(from_unixtime(cast(ure.timestamp as double) / 1000) as timestamp)) as _date
-from datalake_raw.ebdb_offer_aud oa
-join datalake_raw.ebdb_usuariorevisionentity ure
-on oa.rev = ure.id
-where
-	oa.status_MOD = '1'
+  select
+    oa.id,
+    min(cast(from_unixtime(cast(ure.timestamp as double) / 1000) as timestamp)) as _date
+  from datalake_raw.ebdb_offer_aud oa
+  join datalake_raw.ebdb_usuariorevisionentity ure
+    on oa.rev = ure.id
+  where oa.status_MOD = '1'
 	and oa.status in ('Aprovada', 'Rejeitada')
-group by 1
+  group by 1
 ),
-rent_values as
-(
-select
-	godfatherid,
-	originalrent,
-	max(case when turn = 'Owner' then rent end) as last_rent_value_offered_by_tenant,
-	max(case when turn = 'Tenant' then rent end) as last_rent_value_offered_by_owner
-from
-(
-select
-	distinct
-	eoa.godfatherid,
-	eoa.originalrent,
-	first_value(rent) over (partition by eoa.godfatherid, eoa.turn) as rent,
-	eoa.turn
-from datalake_raw.ebdb_offer_aud eoa
-)
-group by 1, 2
+rent_value_offers as (
+  with max_date as (
+    select
+      godfatherid,
+      turn,
+      max(rev) as rev_
+    from datalake_raw.ebdb_offer_aud
+    where turn_mod = '1'
+      and type = 'Price'
+    group by 1, 2
+  )
+  select
+    o_aud.godfatherid,
+    max(case when md.turn = 'Owner' then o_aud.rent end) as last_rent_offered_by_tenant,
+	max(case when md.turn = 'Tenant' then o_aud.rent end) as last_rent_offered_by_owner
+  from datalake_raw.ebdb_offer_aud o_aud
+  join max_date md
+    on md.godfatherid = o_aud.godfatherid
+      and md.rev_ = o_aud.rev
+  group by 1
 )
 select distinct
   eo.id,
@@ -40,7 +41,7 @@ select distinct
   eo.originalhomeinsurance,
   eo.originaliptu,
   eo.originalrent,
-  eo.rent,
+  eo.rent as last_rent,
   eo.status,
   eo.turn,
   eo.client_id,
@@ -53,8 +54,8 @@ select distinct
   go.first_sent_at,
   go.last_sent_at,
   gt.type as topic_type,
-  rv.last_rent_value_offered_by_tenant,
-  rv.last_rent_value_offered_by_owner
+  rvo.last_rent_offered_by_tenant,
+  rvo.last_rent_offered_by_owner
 from datalake_raw.ebdb_offer eo
 join datalake_raw.godfather_offer go
   on eo.godfatherid = go.id
@@ -62,6 +63,6 @@ left join datalake_raw.godfather_topic gt
   on gt.offer_id = go.id
 left join analysis_date ad
   on ad.id = eo.id
-left join rent_values rv
-  on rv.godfatherid = eo.godfatherid
+left join rent_value_offers rvo
+  on rvo.godfatherid = eo.godfatherid
 ;
