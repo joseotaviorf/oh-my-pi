882ff8dc25e2a1092dd9fad7937c8429f3c6944e
Adan Echemendia Montero <aemontero7@gmail.com>
2019-07-03 18:37:28 -0300

    refactoring ebdb_to_datalake dag

---
+++ bietlejuice/jobs/composer/dags/ebdb_into_datalake/spark_jobs/create_raw_external_tables.py
@@ -0,0 +1,34 @@
+import logging
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.consumers import DatabricksConsumer
+from bietlejuice.jobs.composer.etl import DataSourceIntoDataLakeLoader
+from bietlejuice.jobs.composer.wrappers import AthenaClient
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger('create_raw_external_tables')
+
+ATHENA_DB = 'datalake_raw_spark'
+RAW_PATH = 's3://5a-datalake/raw_spark/ebdb'
+
+if __name__ == '__main__':
+    AthenaClient.execute_athena_query('CREATE DATABASE IF NOT EXISTS `{}`'.format(ATHENA_DB), 'default')
+    connection = {
+        'db': 'ebdb'
+    }
+    databricks_consumer = DatabricksConsumer(connection)
+    df = databricks_consumer.get_table_names_and_sizes()
+    tables = df.select('tableName').collect()
+
+    logger.info('m=__main__, msg=Creating raw external tables...')
+    for table in tables:
+        table_name = 'ebdb_{}'.format(table.tableName)
+        DataSourceIntoDataLakeLoader.create_athena_external_table(
+            consumer=databricks_consumer,
+            table=table_name,
+            raw_path=RAW_PATH,
+            athena_raw_schema=ATHENA_DB
+        )
+
+    logger.info('m=__main__, msg=All raw external tables were created successfully.')
