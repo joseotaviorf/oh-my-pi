4e87c441746ef06954dd3745cee523f0088c523c
Adan Echemendia Montero <aemontero7@gmail.com>
2019-07-04 17:47:31 -0300

    refactoring docx dag

--- bietlejuice/jobs/composer/dags/docx/spark_jobs/create_raw_external_tables.py
+++ bietlejuice/jobs/composer/dags/docx/spark_jobs/create_raw_external_tables.py
@@ -8,26 +8,26 @@ from bietlejuice.jobs.composer.etl import DataSourceIntoDataLakeLoader
 from bietlejuice.jobs.composer.wrappers import AthenaClient
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
-logger = QuintoAndarLogger('create_raw_external_tables')
+logger = QuintoAndarLogger("create_raw_external_tables")
 
-if __name__ == '__main__':
-    AthenaClient.execute_athena_query('CREATE DATABASE IF NOT EXISTS `{}`'.format(
-        DataSourceIntoDataLakeLoader.DATALAKE_RAW_DB), 'default')
-    connection = {
-        'db': 'datalake_raw_spark'
-    }
+if __name__ == "__main__":
+    AthenaClient.execute_athena_query(
+        "CREATE DATABASE IF NOT EXISTS `{}`".format(
+            DataSourceIntoDataLakeLoader.DATALAKE_RAW_DB
+        ),
+        "default",
+    )
+    connection = {"db": "datalake_raw_spark"}
     databricks_consumer = DatabricksConsumer(connection)
     df = databricks_consumer.get_table_names_and_sizes()
-    tables = df.select('table_name') \
-        .filter(col('table_name').rlike(r'^docx_')) \
-        .collect()
+    tables = (
+        df.select("table_name").filter(col("table_name").rlike(r"^docx_")).collect()
+    )
 
-    logger.info('m=__main__, msg=Creating raw external tables...')
+    logger.info("m=__main__, msg=Creating raw external tables...")
     for table in tables:
         DataSourceIntoDataLakeLoader.create_athena_external_table(
-            consumer=databricks_consumer,
-            table_name=table.table_name,
-            db_source='docx'
+            consumer=databricks_consumer, table_name=table.table_name, db_source="docx"
         )
 
-    logger.info('m=__main__, msg=All raw external tables were created successfully.')
+    logger.info("m=__main__, msg=All raw external tables were created successfully.")
