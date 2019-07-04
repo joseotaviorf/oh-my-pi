bbda7c3c6a0692ceb00be7df60a431e9c56576e8
Adan Echemendia Montero <aemontero7@gmail.com>
2019-07-04 13:42:04 -0300

    removed python3.6 sintaxis for strings in upload_spark_jobs script

--- scripts/upload_spark_jobs_to_s3.py
+++ scripts/upload_spark_jobs_to_s3.py
@@ -11,19 +11,21 @@ args = parser.parse_args()
 
 s3 = boto3.client('s3')
 s3_bucket = '5a-databricks'
-s3_folder_path = f'github-repos/bi-etl-ejuice/spark_jobs/{args.bucket_type}'
+s3_folder_path = 'github-repos/bi-etl-ejuice/spark_jobs/{}'.format(args.bucket_type)
 
 composer_dags_folder = 'bietlejuice/jobs/composer/dags/'
 abs_path = os.path.dirname(os.path.realpath(__file__))
 
-for root, _, files in os.walk(f'{abs_path}/../{composer_dags_folder}'):
+for root, _, files in os.walk('{}/../{}'.format(abs_path, composer_dags_folder)):
     if 'spark_jobs' not in root:
         continue
 
     dag = re.split('/dags/|/spark_jobs', root)[1]
-    print(f"Processing dag '{dag}'...")
+    print("Processing dag '{}'...".format(dag))
 
     for file_ in files:
-        print(f"Uploading file '{file_}' from dag '{dag}'...")
-        s3.upload_file(f'{root}/{file_}', s3_bucket, f'{s3_folder_path}/{dag}/{file_}')
-        print(f"File '{file_}' from dag '{dag}' successfully uploaded!\n")
+        print("Uploading file '{}' from dag '{}'...".format(file_, dag))
+        local_path = '{}/{}'.format(root, file_)
+        remote_path = '{}/{}/{}'.format(s3_folder_path, dag, file_)
+        s3.upload_file(local_path, s3_bucket, remote_path)
+        print("File '{}' from dag '{}' successfully uploaded!\n".format(file_, dag))
