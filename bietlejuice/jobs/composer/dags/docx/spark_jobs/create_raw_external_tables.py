dfd8938f617dd102e876b603f2782466094c58bd
Adan Echemendia Montero <aemontero7@gmail.com>
2019-07-04 16:52:46 -0300

    added param in method call

--- bietlejuice/jobs/composer/dags/docx/spark_jobs/create_raw_external_tables.py
+++ bietlejuice/jobs/composer/dags/docx/spark_jobs/create_raw_external_tables.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
         DataSourceIntoDataLakeLoader.create_athena_external_table(
             consumer=databricks_consumer,
             table_name=table.table_name,
+            db_source='docx'
         )
 
     logger.info('m=__main__, msg=All raw external tables were created successfully.')
