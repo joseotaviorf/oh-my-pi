3a7a24078f1ffd4644718ca5c674bbb2bfcdaa96
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-07-22 08:34:51 -0300

    Update lead_score_factor.sql

--- bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
+++ bietlejuice/db/datalake/queries/lead/lead_score_factor.sql
@@ -2,7 +2,7 @@ with t_dates as (
 	select
 		cast(cast(id_origin as decimal) as bigint) as lead_id,
 		cast(cast(score_factor as decimal) as bigint) as score_factor,
-		row_number() over (partition by id_origin order by dt, ts_start) as rn
+		row_number() over (partition by cast(cast(id_origin as decimal) as bigint) order by dt, ts_start) as rn
 	from
 		datalake_clean.crm_tasks
 	where origin = 'Lead'
