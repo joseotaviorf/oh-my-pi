814f975c8159ab58aa7f94d800735e9635299d39
Adan Echemendia Montero <aemontero7@gmail.com>
2019-08-02 15:45:11 -0300

    added cookiecutter docs, put cookiecutter requirement, and ignore dag file created by template in flake8

--- dag_template/hooks/post_gen_project.py
+++ dag_template/hooks/post_gen_project.py
@@ -7,16 +7,22 @@ CURR_FOLDER = os.path.realpath(os.path.curdir)
 
 
 def add_file(task_name):
-    dest_path = os.path.join(CURR_FOLDER, 'spark_jobs', '{}.py'.format(task_name))
-    source_path = os.path.join(CURR_FOLDER, 'spark_jobs/job_template.py')
+    dest_path = os.path.join(CURR_FOLDER, "spark_jobs", "{}.py".format(task_name))
+    source_path = os.path.join(CURR_FOLDER, "spark_jobs/job_template.py")
     copyfile(source_path, dest_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    task_names = "{{ cookiecutter.task_names }}".strip().replace(' ', '_').replace('-', '_').split(',') or []
+    task_names = (
+        "{{ cookiecutter.task_names }}".strip()
+        .replace(" ", "_")
+        .replace("-", "_")
+        .split(",")
+        or []
+    )
     for task_name in task_names:
-        if re.match(r'^[a-z]\w*', task_name):
+        if re.match(r"^[a-z]\w*", task_name):
             add_file(task_name)
 
-    os.remove(os.path.join(CURR_FOLDER, 'spark_jobs/job_template.py'))
+    os.remove(os.path.join(CURR_FOLDER, "spark_jobs/job_template.py"))
