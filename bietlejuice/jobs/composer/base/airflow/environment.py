edb13c543e3af2c13ce1321a59c4b915fd649879
Adan Echemendia Montero <aemontero7@gmail.com>
2019-08-07 13:11:51 -0300

    created methods in environment class

--- bietlejuice/jobs/composer/base/airflow/environment.py
+++ bietlejuice/jobs/composer/base/airflow/environment.py
@@ -1,3 +1,11 @@
 class Environment:
     FORNO = 'forno'
     PROD = 'prod'
+
+    @classmethod
+    def is_valid_environment(cls, environment):
+        return environment == cls.FORNO or environment == cls.PROD
+
+    @classmethod
+    def get_valid_environments(cls):
+        return [cls.FORNO, cls.PROD]
