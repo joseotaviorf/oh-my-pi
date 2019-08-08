507d50acf1250783d2d41789c6a5bbcd8ec26819
darambaris <jessika.darambaris@gmail.com>
2019-08-08 19:38:50 -0300

    specific Teravoz class to calls request

---
+++ bietlejuice/jobs/composer/loaders/teravoz/teravoz_calls_loader.py
@@ -0,0 +1,63 @@
+from bietlejuice.jobs.composer.loaders.teravoz import TeravozLoader
+from quintoandar_logger import QuintoAndarLogger
+from pyspark.sql.functions import lit
+from datetime import datetime
+from collections import OrderedDict
+
+
+logger = QuintoAndarLogger("TeravozCallsLoader")
+
+
+class TeravozCallsLoader(TeravozLoader):
+    
+    PARTITIONS = OrderedDict([
+        ("year", "{year}"),
+        ("month", "{month}"),
+        ("day", "{day}")
+    ])
+
+    PARAMS = {
+        "date": "{date}"
+    }
+    
+    def __init__(self, api_user, api_pwd, execution_date):
+        
+        self.PARTITIONS["year"] = self.PARTITIONS["year"].format(year=execution_date.year)
+        self.PARTITIONS["month"] = self.PARTITIONS["month"].format(month=execution_date.month)
+        self.PARTITIONS["day"] = self.PARTITIONS["day"].format(day=execution_date.day)
+        
+        strdate = datetime.strftime(execution_date, "%d-%m-%Y")
+        self.PARAMS["date"] = self.PARAMS["date"].format(date=strdate) 
+
+        super().__init__(api_user, api_pwd, execution_date)
+
+    @staticmethod
+    @logger
+    def _create_dataframe_columns_to_partition_table(df, partitions):
+        
+        for partition_name, partition_value in partitions.items():
+            df = df.withColumn(partition_name, lit(partition_value))
+
+        return df
+
+
+    @logger
+    def request_api_and_get_dataframe(self):
+
+        df = super()._request_api_and_get_dataframe(
+            endpoint="calls",
+            params=self.PARAMS
+        )
+        return df
+
+    @logger
+    def load_data_into_datalake(self, df, datalake_layer):
+        
+        df = self._create_dataframe_columns_to_partition_table(df, self.PARTITIONS)
+        
+        super()._load_data_into_datalake(
+            df=df, 
+            endpoint="calls", 
+            datalake_layer=datalake_layer, 
+            partitions=self.PARTITIONS
+        )
