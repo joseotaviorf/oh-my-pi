8c073ed36d3c58ab6647619a742c9162f91faeba
darambaris <jessika.darambaris@gmail.com>
2019-08-08 19:39:28 -0300

    add TeravozCallsLoader and TeravozFactory in init file

--- bietlejuice/jobs/composer/loaders/teravoz/__init__.py
+++ bietlejuice/jobs/composer/loaders/teravoz/__init__.py
@@ -1,1 +1,3 @@
 from bietlejuice.jobs.composer.loaders.teravoz.teravoz_loader import TeravozLoader
+from bietlejuice.jobs.composer.loaders.teravoz.factory import TeravozFactory
+from bietlejuice.jobs.composer.loaders.teravoz.calls_teravoz_loader import TeravozCallsLoader
