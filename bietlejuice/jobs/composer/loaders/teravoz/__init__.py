6475ad84e847c40f73c155888323f3ca25c6c0ba
darambaris <jessika.darambaris@gmail.com>
2019-08-09 14:13:08 -0300

    change package order

--- bietlejuice/jobs/composer/loaders/teravoz/__init__.py
+++ bietlejuice/jobs/composer/loaders/teravoz/__init__.py
@@ -1,5 +1,5 @@
 from bietlejuice.jobs.composer.loaders.teravoz.teravoz_loader import TeravozLoader
-from bietlejuice.jobs.composer.loaders.teravoz.factory import TeravozFactory
 from bietlejuice.jobs.composer.loaders.teravoz.teravoz_calls_loader import (
     TeravozCallsLoader,
 )
+from bietlejuice.jobs.composer.loaders.teravoz.factory import TeravozFactory
