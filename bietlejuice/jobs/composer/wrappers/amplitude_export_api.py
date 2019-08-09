6554c1b935a7af555b7cc392f78950c4c66ea41e
Adan Echemendia Montero <aemontero7@gmail.com>
2019-08-09 18:58:55 -0300

    reverted back changes in amplitude wrapper

--- bietlejuice/jobs/composer/wrappers/amplitude_export_api.py
+++ bietlejuice/jobs/composer/wrappers/amplitude_export_api.py
@@ -1,6 +1,6 @@
 import io
-
 import requests
+
 from quintoandar_logger import QuintoAndarLogger
 
 logger = QuintoAndarLogger("AmplitudeExportApi")
@@ -8,17 +8,17 @@ logger = QuintoAndarLogger("AmplitudeExportApi")
 
 class AmplitudeExportApi:
     def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
+        self.API_KEY = api_key
+        self.SECRET_KEY = secret_key
 
     def get_files_from_extract_api(self, start_time, end_time):
         url = "https://amplitude.com/api/2/export?start={}&end={}"
         url_full = url.format(start_time, end_time)
-        r = requests.get(url_full, auth=(self.api_key, self.secret_key))
+        r = requests.get(url_full, auth=(self.API_KEY, self.SECRET_KEY))
         file_stream = None
         logger.info(
             "Keys: {}, {} - URL: {} - STATUS_CODE: {}".format(
-                self.api_key, self.secret_key, url_full, r.status_code
+                self.API_KEY, self.SECRET_KEY, url_full, r.status_code
             )
         )
         if r.status_code == 200:
