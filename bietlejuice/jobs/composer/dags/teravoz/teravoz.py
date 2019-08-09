be2c88d347e91f2c01192fe34d53120099e59e4d
darambaris <jessika.darambaris@gmail.com>
2019-08-09 19:07:04 -0300

    add existing cluster id param

--- bietlejuice/jobs/composer/dags/teravoz/teravoz.py
+++ bietlejuice/jobs/composer/dags/teravoz/teravoz.py
@@ -72,7 +72,9 @@ def raw_sub_dag(sub_dag_name, **kwargs):
         task_id="request-api-load-to-raw",
         dag=local_dag,
         json={
-            "existing_cluster_id": '{{task_instance.xcom_pull(dag_id="teravoz", task_ids="create-cluster", key="cluster_id")}}',
+            "existing_cluster_id": '{{task_instance.xcom_pull(dag_id="'
+            + DAG_ID
+            + '", task_ids="create_cluster", key="cluster_id")}}',
             "spark_python_task": {
                 "python_file": "{}/load_teravoz_into_datalake.py".format(
                     SPARK_JOBS_PATH
