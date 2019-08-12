fd7ac06afec22d6518ce60d027735854a3915a40
Rafael Leinio <rafael.leper@gmail.com>
2019-08-12 19:35:04 -0300

    url decoding for partition names

--- bietlejuice/jobs/composer/base/spark/base_spark.py
+++ bietlejuice/jobs/composer/base/spark/base_spark.py
@@ -1,4 +1,5 @@
 import re
+from urllib.parse import unquote
 
 from pyspark.sql import session, context
 from pyspark import SparkContext
@@ -24,7 +25,7 @@ class BaseDBUtils:
     @logger(exclude_return=True)
     def discover_partition_values_in_path(self, path, dbutils):
         return [
-            file_info.name.split("=")[1][:-1]
+            unquote(file_info.name.split("=")[1][:-1])
             for file_info in dbutils.fs.ls(path)
             # filter only directories with names in partition format
             if file_info.isDir() and re.search(r".+\=.+", file_info.name)
