ed1ae5b8b528578ee5b74e59c3d90a3260b76196
darambaris <jessika.darambaris@gmail.com>
2019-08-13 16:03:01 -0300

    remove if datalake raw

--- bietlejuice/jobs/composer/dags/teravoz/spark_jobs/load_teravoz_into_datalake.py
+++ bietlejuice/jobs/composer/dags/teravoz/spark_jobs/load_teravoz_into_datalake.py
@@ -8,7 +8,6 @@ from bietlejuice.jobs.composer.loaders.teravoz.factory import TeravozFactory
 from quintoandar_logger import QuintoAndarLogger
 
 DATABRICKS_SCOPE = "quintoandar-prod"
-# NB_THREADS = 4
 
 
 JOB_NAME = "load_teravoz_into_datalake"
@@ -54,28 +53,27 @@ if __name__ == "__main__":
     execution_date = args.execution_date
     endpoint = args.endpoint
 
-    if datalake_layer == "raw":
 
-        # start Spark Session
-        base_dbutils = BaseDBUtils()
+    # start Spark Session
+    base_dbutils = BaseDBUtils()
 
-        if base_dbutils.get_dbutils() is not None:
-            dbutils = base_dbutils.get_dbutils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
 
-        # get Teravoz credentials stored in Databricks secrets
-        json_credentials = dbutils.secrets.get(
-            scope=DATABRICKS_SCOPE, key="ENV_TERAVOZ"
-        )
+    # get Teravoz credentials stored in Databricks secrets
+    json_credentials = dbutils.secrets.get(
+        scope=DATABRICKS_SCOPE, key="ENV_TERAVOZ"
+    )
 
-        credentials = json.loads(json_credentials)
+    credentials = json.loads(json_credentials)
 
-        teravoz = exec_factory_method(
-            endpoint=endpoint,
-            method="__init__",
-            api_user=credentials["teravoz_user"],
-            api_pwd=credentials["teravoz_password"],
-            execution_date=execution_date,
-        )
+    teravoz = exec_factory_method(
+        endpoint=endpoint,
+        method="__init__",
+        api_user=credentials["teravoz_user"],
+        api_pwd=credentials["teravoz_password"],
+        execution_date=execution_date,
+    )
 
-        df = teravoz.request_api_and_get_dataframe()
-        teravoz.load_data_into_datalake(df=df, datalake_layer=datalake_layer)
+    df = teravoz.request_api_and_get_dataframe()
+    teravoz.load_data_into_datalake(df=df, datalake_layer=datalake_layer)
