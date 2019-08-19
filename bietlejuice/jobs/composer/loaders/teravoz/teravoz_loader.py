bb47015f7cf853c5b23f4461e06f6b450c9453de
darambaris <jessika.darambaris@gmail.com>
2019-08-19 00:02:46 -0300

    fix ident create partition and refresh table into datalake method

--- bietlejuice/jobs/composer/loaders/teravoz/teravoz_loader.py
+++ bietlejuice/jobs/composer/loaders/teravoz/teravoz_loader.py
@@ -92,7 +92,6 @@ class TeravozLoader:
         )
 
         spark.sql(create_partition)
-        spark.sql("REFRESH TABLE {}.{}".format(db_name, table_name))
 
     @logger
     def _create_dataframe_columns_to_partition_table(df, partitions):
@@ -186,4 +185,6 @@ class TeravozLoader:
                         "{}={}".format(partition_name, partition_value)
                     )
 
-            self._create_partition_table(db_name, table_name, s3_path, list_partitions)
+                self._create_partition_table(db_name, table_name, s3_path, list_partitions)
+            
+            spark.sql("REFRESH TABLE {}.{}".format(db_name, table_name))
