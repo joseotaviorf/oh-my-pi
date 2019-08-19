667c9e57db0de0909a2e07a1de3f5fd912c2ac84
darambaris <jessika.darambaris@gmail.com>
2019-08-18 22:29:30 -0300

    fix py3

--- bietlejuice/jobs/composer/dags/teravoz/spark_jobs/load_teravoz_into_datalake.py
+++ bietlejuice/jobs/composer/dags/teravoz/spark_jobs/load_teravoz_into_datalake.py
@@ -17,7 +17,9 @@ logger = QuintoAndarLogger(JOB_NAME)
 
 
 @logger
-def exec_factory_method(table_name, method, api_user, api_pwd, environment, execution_date):
+def exec_factory_method(
+    table_name, method, api_user, api_pwd, environment, execution_date
+):
 
     teravoz = TeravozFactory.factory(
         table_name=table_name,
