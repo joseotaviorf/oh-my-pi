e58939a180c906981a3095e3e5c763446c867bef
darambaris <jessika.darambaris@gmail.com>
2019-08-18 23:43:04 -0300

    change to list in set_downstream

--- bietlejuice/jobs/composer/dags/teravoz/teravoz.py
+++ bietlejuice/jobs/composer/dags/teravoz/teravoz.py
@@ -126,8 +126,8 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 )
 
 create_cluster_task.set_downstream(
-    calls_sub_dag_task, queues_sub_dag_task, peers_sub_dag_task, ddrs_sub_dag_task
+    [calls_sub_dag_task, queues_sub_dag_task, peers_sub_dag_task, ddrs_sub_dag_task]
 )
 terminate_cluster_task.set_upstream(
-    calls_sub_dag_task, queues_sub_dag_task, peers_sub_dag_task, ddrs_sub_dag_task
+    [calls_sub_dag_task, queues_sub_dag_task, peers_sub_dag_task, ddrs_sub_dag_task]
 )
