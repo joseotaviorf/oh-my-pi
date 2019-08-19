b607fcd494a5f4c4a2a98ad7d04c6fd8308f3c4f
darambaris <jessika.darambaris@gmail.com>
2019-08-18 23:19:01 -0300

    creating queues, ddrs and peers flows

--- bietlejuice/jobs/composer/loaders/teravoz/teravoz_loader.py
+++ bietlejuice/jobs/composer/loaders/teravoz/teravoz_loader.py
@@ -21,13 +21,14 @@ class TeravozLoader:
     SOURCE = "teravoz"
 
     @logger
-    def __init__(self, api_user, api_pwd, environment):
+    def __init__(self, api_user, api_pwd, environment, execution_date=None):
         # api_instance (temporarily here)
         self.api_instance = TeravozClient(api_user=api_user, api_pwd=api_pwd)
         self.ENV = environment
+        self.execution_date = execution_date
 
     @logger
-    def _request_api_and_get_dataframe(self, endpoint, params):
+    def request_api_and_get_dataframe(self, endpoint, params={}):
         """
             endpoint: endpoint ..@teravoz.com.br/{endpoint}
             params: if exists is expected the format:
@@ -151,7 +152,7 @@ class TeravozLoader:
         ).save()
 
     @logger
-    def _load_data_into_datalake(self, df, table_name, datalake_layer, partitions=None):
+    def load_data_into_datalake(self, df, table_name, datalake_layer, partitions=None):
 
         """
           load request from endpoint to s3 without partitions and
