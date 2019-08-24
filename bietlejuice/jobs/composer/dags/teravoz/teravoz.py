67d2850030e28efc427d9980ab712d7fab381b69
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-08-24 10:16:48 -0300

    add report-agent-status subdag

--- bietlejuice/jobs/composer/dags/teravoz/teravoz.py
+++ bietlejuice/jobs/composer/dags/teravoz/teravoz.py
@@ -129,6 +129,9 @@ report_agent_performance_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
 report_queue_stats_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
     dag=dag, sub_dag_name="report-queue-stats", sub_dag_func=sub_dag
 )
+report_agent_status_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
+    dag=dag, sub_dag_name="report-agent-status", sub_dag_func=sub_dag
+)
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
@@ -137,13 +140,14 @@ create_cluster_task.set_downstream(
     [calls_sub_dag_task, queues_sub_dag_task, peers_sub_dag_task, ddrs_sub_dag_task]
 )
 queues_sub_dag_task.set_downstream(
-    [report_agent_performance_sub_dag_task, report_queue_stats_sub_dag_task]
+    [report_agent_performance_sub_dag_task, report_queue_stats_sub_dag_task, report_agent_status_sub_dag_task]
 )
 terminate_cluster_task.set_upstream(
     [
         calls_sub_dag_task,
         report_agent_performance_sub_dag_task,
         report_queue_stats_sub_dag_task,
+        report_agent_status_sub_dag_task,
         peers_sub_dag_task,
         ddrs_sub_dag_task,
     ]
