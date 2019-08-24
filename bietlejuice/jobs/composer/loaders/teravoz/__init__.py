c295b716e20cbe759fec3f20e76fd07d47992783
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-08-24 10:07:19 -0300

    create ReportAgentStatusLoader class and add in flow

--- bietlejuice/jobs/composer/loaders/teravoz/__init__.py
+++ bietlejuice/jobs/composer/loaders/teravoz/__init__.py
@@ -5,4 +5,6 @@ from bietlejuice.jobs.composer.loaders.teravoz.teravoz_calls_loader import (
 from bietlejuice.jobs.composer.loaders.teravoz.teravoz_reports_loader import (
     TeravozReportsLoader,
 )
+from bietlejuice.jobs.composer.loaders.teravoz.teravoz_report_agent_status_loader import TeravozReportAgentStatusLoader
 from bietlejuice.jobs.composer.loaders.teravoz.factory import TeravozFactory
+
