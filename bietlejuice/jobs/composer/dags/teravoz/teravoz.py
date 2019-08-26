df7247f02bf7f953c426516830ba7618371c0b1e
Jessika Darambaris <jessika.darambaris@gmail.com>
2019-08-25 23:58:39 -0300

    change Teravoz package verrsion to install on Databricks cluster

--- bietlejuice/jobs/composer/dags/teravoz/teravoz.py
+++ bietlejuice/jobs/composer/dags/teravoz/teravoz.py
@@ -43,7 +43,7 @@ CUSTOM_LIBRARIES = [
         "whl": "s3://5a-artifacts/tapioca-wrapper/tapioca_wrapper-quintoandar_1.5.1-py3-none-any.whl"
     },
     {
-        "whl": "s3://5a-artifacts/teravoz-client/quintoandar_teravoz_client-0.1.4-py3-none-any.whl"
+        "whl": "s3://5a-artifacts/teravoz-client/quintoandar_teravoz_client-0.1.5-py3-none-any.whl"
     },
 ]
 LIBRARIES_DESCRIPTION = DEFAULT_LIBRARIES + CUSTOM_LIBRARIES
