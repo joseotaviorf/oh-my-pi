49fd482c9e70cdc5c33410d889622e99b44b4483
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-08-31 11:18:41 -0300

    Removing agent_optimitzation trigger from supply-demand-etl

--- bietlejuice/jobs/dags/bi_supply_demand_etl.py
+++ bietlejuice/jobs/dags/bi_supply_demand_etl.py
@@ -588,13 +588,6 @@ trigger_bi_crm_load_dag_task = TriggerDagRunOperator(
     execution_date='{{ execution_date }}'
 )
 
-# trigger bi-agents-allocation-optimization dag after all tasks have been successfully completed
-trigger_bi_agents_allocation_optimization_dag_task = TriggerDagRunOperator(
-    dag=main_dag,
-    task_id='trigger_bi_agents_allocation_optimization_dag',
-    trigger_dag_id='bi-agents-allocation-optimization'
-)
-
 # TODO Recreate tasks flow after the data flow is fully fixed
 # xcom_amplitude_task.set_downstream([booking_dag, affiliate_dag])
 affiliate_dag.set_upstream([region_dag, user_dag])
