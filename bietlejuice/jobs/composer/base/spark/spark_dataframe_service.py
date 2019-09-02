ccef370cd3eb3014e2ece5609b9be269373d1533
Rafael Leinio <rafael.leper@gmail.com>
2019-09-02 19:54:41 -0300

    documentation of SparkDataframeService class

--- bietlejuice/jobs/composer/base/spark/spark_dataframe_service.py
+++ bietlejuice/jobs/composer/base/spark/spark_dataframe_service.py
@@ -12,6 +12,32 @@ logger = QuintoAndarLogger("DataFrameService")
 
 
 class SparkDataFrameService:
+    """
+    SparkDataFrameService is a class with the purpose of implement several useful operations over a Spark dataframe.
+    The methods of this class were implemented in a way for the users to create a pipeline of operations over
+    dataframes. So the methods doesn't return a new dataframe, rather they return a SparkDataFrameService object.
+    To get the result df after all operations the output method is needed at the end of the pipeline. Examples of use:
+    Ex1:
+    result_df = SparkDataFrameService(input_df)
+            .explode_json_column(json_column="user_properties", prefix="user_", format_column_names=True)
+            .explode_json_column(json_column="event_properties", prefix="event_", format_column_names=True)
+            .optimize_partition(records_by_partition=250000)
+            .output()
+    Ex2:
+    result_df = SparkDataFrameService().input(input_df)
+            .explode_json_column(json_column="user_properties", prefix="user_", format_column_names=True)
+            .explode_json_column(json_column="event_properties", prefix="event_", format_column_names=True)
+            .optimize_partition(records_by_partition=250000)
+            .output()
+    Ex3:
+    dataframe_service = SparkDataFrameService()
+    result_df = dataframe_service.input(input_df)
+                .explode_json_column(json_column="user_properties", prefix="user_", format_column_names=True)
+                .explode_json_column(json_column="event_properties", prefix="event_", format_column_names=True)
+                .optimize_partition(records_by_partition=250000)
+                .output()
+    """
+
     def __init__(self, df=None):
         self.df = df
 
@@ -23,6 +49,10 @@ class SparkDataFrameService:
 
     @staticmethod
     def format_column_name(column_name):
+        """
+        :param column_name: name of a column in your dataframe
+        :return: formatted name
+        """
         formatted_name = re.sub(
             r"\W", "", column_name.replace(" ", "_").replace(".", "_")
         )
@@ -31,6 +61,10 @@ class SparkDataFrameService:
         return formatted_name.lower()
 
     def format_column_names(self):
+        """
+        This operation format all names of the columns in the dataframe
+        :return: SparkDataFrameService object with the result df
+        """
         if not self.df:
             raise ValueError("m=format_column_names, msg=input df is None")
         existing_names = self.df.schema.fieldNames()
@@ -42,6 +76,10 @@ class SparkDataFrameService:
         return SparkDataFrameService(self.df)
 
     def convert_struct_type_to_json(self):
+        """
+        This operation cast all struct type columns in the dataframe to string/json type
+        :return: SparkDataFrameService object with the result df
+        """
         if not self.df:
             raise ValueError("m=convert_struct_type_to_json, msg=input df is None")
         for field in self.df.schema.fields:
@@ -55,6 +93,15 @@ class SparkDataFrameService:
         return SparkDataFrameService(self.df)
 
     def explode_json_column(self, json_column, prefix="", format_column_names=False):
+        """
+        This operation gets all fields of a json column and create one new column in the dataframe for each of those
+        fields. This operation will not work on struct type columns, so you can use convert_struct_type_to_json method
+        first in the pipeline.
+        :param json_column: name of the json column in the dataframe
+        :param prefix: a prefix to use in the name of the newly created columns
+        :param format_column_names: Optional operation to format all the names of the new columns that will be created
+        :return: SparkDataFrameService object with the result df
+        """
         if not self.df:
             raise ValueError("m=explode_json_column, msg=input df is None")
         if json_column not in self.df.schema.fieldNames():
@@ -92,6 +139,12 @@ class SparkDataFrameService:
         return SparkDataFrameService(spark.sql(query).drop(json_column))
 
     def create_year_month_day_columns(self, date_column_name):
+        """
+        Given a name of date type column in the dataframe this operation will create three new columns:
+        year, month, day extracted from the given column.
+        :param date_column_name: name of the column to extract the year, month and day values
+        :return: SparkDataFrameService object with the result df
+        """
         if not self.df:
             raise ValueError("m=create_year_month_day_columns, msg=input df is None")
         return SparkDataFrameService(
@@ -101,6 +154,12 @@ class SparkDataFrameService:
         )
 
     def optimize_partition(self, records_by_partition):
+        """
+        Given a value of records_by_partition this operation will perform a coalesce or  a repartition in the dataframe
+        to optimize the number of partitions of the dataframe based in the given number.
+        :param records_by_partition: number of records to be in each dataframe partition
+        :return: SparkDataFrameService object with the result df
+        """
         len_data = self.df.count()
         partitions = max(len_data // records_by_partition, 1)
         if partitions > self.df.rdd.getNumPartitions():
