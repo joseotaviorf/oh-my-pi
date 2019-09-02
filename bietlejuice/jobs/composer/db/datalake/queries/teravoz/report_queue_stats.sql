991799b2c471c7ea24bf028f919c1f578c1a7a5e
darambaris <jessika.darambaris@gmail.com>
2019-09-02 15:49:22 -0300

    report_queue_stats query

---
+++ bietlejuice/jobs/composer/db/datalake/queries/teravoz/report_queue_stats.sql
@@ -0,0 +1,13 @@
+select 
+    smallint(queue) as queue_number,
+    date(callDate) as dt_created,
+    smallint(abandonedCalls) as calls_abandoned,
+    smallint(answeredCalls) as calls_answered,
+    smallint(receivedCalls) as calls_received,
+    smallint(timedOutCalls) as calls_timed_out,
+    year,
+    month,
+    day
+from
+    (select queue, year, month, day, inline(days) from datalake_teravoz_raw.report_queue_stats
+    where year="{year}" and month="{month}" and day="{day}")
