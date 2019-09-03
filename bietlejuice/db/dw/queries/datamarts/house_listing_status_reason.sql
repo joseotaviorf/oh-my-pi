b562e448b817d4a4ec7c0e4565dcabc8a5a04faa
carolinavalanic <carolinavalanic@gmail.com>
2019-09-02 23:08:09 -0300

    Adjusting datamart to use new table

--- bietlejuice/db/dw/queries/datamarts/house_listing_status_reason.sql
+++ bietlejuice/db/dw/queries/datamarts/house_listing_status_reason.sql
@@ -20,37 +20,36 @@ where
 ),
 aux_query as
 (select
-	fh.sk_house,
+	fh.sk_house_listing,
 	fh.sk_region,
 	fh.status_history,
 	sr.status_reason,
 	sr.status_reason_2,
-	fh.sk_min_version_status_date,
-	fh.sk_min_status_date,
-	fh.sk_max_status_date,
-	fh.dt_timestamp,
-	row_number() over (partition by hl.id_house, fh.status_history, sr.status,	fh.sk_min_status_date order by hl.id_house) as rn
-from fact_house_status fh
+	fh.sk_status_start_date,
+	fh.sk_status_end_date,
+	fh.ts_load,
+	row_number() over (partition by hl.id_house, fh.status_history, sr.status, fh.sk_status_start_date order by hl.id_house) as rn
+from fact_house_listing_status fh
 left join dim_house_listing hl
-	on fh.sk_house = hl.sk_house_listing
+	on fh.sk_house_listing = hl.sk_house_listing
 left join status_reason as sr
 	on sr.id_house = hl.id_house
 	and fh.status_history = sr.status
 	and fh.status_history in ('despublicado', 'suspenso')
 	and sr.status in ('despublicado', 'suspenso')
-	and sr.date_change_suspension_reason = date(fh.sk_min_status_date)
+	and sr.date_change_suspension_reason = date(fh.sk_status_start_date)
 where
 	hl.ts_publication >= '2018-01-01'
 )
 select
-	sk_house,
+	sk_house_listing,
 	sk_region,
 	status_history,
 	status_reason,
 	status_reason_2 as status_reason_motive,
-	sk_min_version_status_date,
-	sk_min_status_date,
-	sk_max_status_date,
-	dt_timestamp
+	sk_status_start_date,
+	sk_status_end_date,
+	ts_load
 from aux_query
 where rn = 1
+;
