830e1fb2d299a68b9fbf2974ebaee17e40feb7eb
Ana Carolina Maia <ana.maia@quintoandar.com.br>
2019-09-10 16:25:54 -0300

    Fix duplicated tasks in CRM Closing and Photo Job

--- bietlejuice/db/datalake/queries/crm/tasks/append_fact_photo_job_info.sql
+++ bietlejuice/db/datalake/queries/crm/tasks/append_fact_photo_job_info.sql
@@ -11,15 +11,19 @@
     on t.sk_task = trim(ct.id)
   left join datalake_clean.ods_fact_photo_job fpj
     on trim(ct.origin) = 'JobFotografo'
-      and cast(cast(ct.id_origin as decimal) as bigint) = try(cast(fpj.id_photo_job as bigint))
+      and try_cast(try_cast(ct.id_origin as decimal) as bigint) = try_cast(fpj.id_photo_job as bigint)
   left join datalake_clean.ods_fact_house_listings fhl_photo
   	on fhl_photo.sk_house_listing = fpj.sk_house_listing
   	  and fhl_photo.sk_house_listing != '-1'
   left join datalake_clean.ods_dim_house_listing dhl
     on trim(ct.origin) = 'Imovel'
-      and cast(cast(ct.id_origin as decimal) as bigint) = try(cast(dhl.id_house as bigint))
+      and try_cast(try_cast(ct.id_origin as decimal) as bigint) = try_cast(dhl.id_house as bigint)
       and cast(regexp_extract(ct.ts_start, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
-        between cast(regexp_extract(dhl.ts_listing_version_start, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
+        between (case
+                 when dhl.ts_listing_version_start = ''
+                   then cast(regexp_extract(ct.ts_start, '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}') as timestamp)
+                 else now()
+               end)
           and (case
                  when dhl.ts_listing_version_end = ''
                    then now()
@@ -37,17 +41,11 @@
 ),
 photo_job_house_listing as (
   select
-    cast(dhl.sk_house_listing as bigint) as sk_house_listing,
-    cast(fhl.sk_owner as bigint) as sk_house_owner,
-    cast(coalesce(fpj.id_photo_job, '-1') as bigint) as sk_photo_job,
-    cast(coalesce(fpj.sk_user_rep, '-1') as bigint) as sk_user_sales_rep
-  from datalake_clean.ods_dim_house_listing dhl
-  join datalake_clean.ods_fact_photo_job fpj
-    on dhl.sk_house_listing = fpj.sk_house_listing
-  join datalake_clean.ods_fact_house_listings fhl
-    on fhl.sk_house_listing = dhl.sk_house_listing
-  where dhl.sk_house_listing != '-1'
-  group by 1, 2, 3, 4
+    cast(sk_house_listing as bigint) as sk_house_listing,
+    cast(sk_owner as bigint) as sk_house_owner
+  from datalake_clean.ods_fact_house_listings
+  where sk_house_listing != '-1'
+  group by 1, 2
 )
 select distinct
   pj.sk_task,
@@ -69,8 +67,8 @@ select distinct
   pj.task_user_resolve_hours,
   pj.sk_house_listing,
   coalesce(pjhl.sk_house_owner, pj.sk_house_owner) as sk_house_owner,
-  coalesce(pjhl.sk_photo_job, pj.sk_photo_job) as sk_photo_job,
-  coalesce(pjhl.sk_user_sales_rep, pj.sk_user_sales_rep) as sk_user_sales_rep,
+  pj.sk_photo_job as sk_photo_job,
+  pj.sk_user_sales_rep as sk_user_sales_rep,
   pj.dt_partition
 from photo_jobs pj
 left join photo_job_house_listing pjhl
