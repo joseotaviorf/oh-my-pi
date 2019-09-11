c7eacf69a23d9308a2f300d1f8ff3ac2ec5a0000
Bernardo Loureiro <lourb896@newschool.edu>
2019-09-11 14:47:48 -0300

    Change file names

--- bietlejuice/db/dw/queries/datamarts/crawled_listings_last_30_days.sql
+++ bietlejuice/db/dw/queries/datamarts/crawled_listings_last_30_days.sql
@@ -1,4 +1,4 @@
 SELECT
   *
 FROM datamarts.all_crawled_listings_last_120_days
-WHERE DATE(first_time_updated_on) >= CURRENT_DATE - INTERVAL '30' DAY  -- only listings posted or updated in the last 30 days
+WHERE DATE(first_time_updated_on) >= CURRENT_DATE - 30  -- only listings posted or updated in the last 30 days
