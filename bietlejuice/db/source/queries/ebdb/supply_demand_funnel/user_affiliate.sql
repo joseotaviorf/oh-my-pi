2476e48d3d8e6ef68ca45279d55722656d0b21b6
Ribaldo <ribaldorafael@gmail.com>
2019-09-12 19:55:19 -0300

    Removing numeroCreci and cidadeAtuacao from user_affiliate

--- bietlejuice/db/source/queries/ebdb/supply_demand_funnel/user_affiliate.sql
+++ bietlejuice/db/source/queries/ebdb/supply_demand_funnel/user_affiliate.sql
@@ -3,11 +3,9 @@ select
 	da.indicadoPor_id,
 	da.inicioAtuacao,
 	da.tipoAfiliado,
-	da.cidadeAtuacao,
 	coalesce(da.ativo,0) as ativo,
 	da.atualizadoEm,
 	da.criadoEm,
-	left(da.numeroCreci, 50) as numeroCreci,
 	da.origin,
 	da.affiliateType
 from DadosAfiliado da
