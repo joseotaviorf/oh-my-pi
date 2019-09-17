deabc820db0a59f548c97ad4c0ca62ec393d5ae3
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-09-17 16:51:42 -0300

    Update prefix_current_date.sql

--- bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
+++ bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
@@ -5,26 +5,6 @@ with all_events as (
         extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
         extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
         extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
-        trim(amplitude_id) as amplitude_id,
-        'QuintoAndar' as region,
-        'QuintoAndar' as city
-      from datalake_clean.amplitude_events
-      where ym >= '2018-01' and ym <= '2018-12'
-        and et = 'landing_page_viewed'
-        and trim(app) = '183047'
-        and trim(e_ub_page_variant) in ('bv','cj','co')
-        and trim(e_ub_page_name) = 'Proprietário'
-        and extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(year from (now() - interval '1' month))
-        and extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(month from (now() - interval '1' month))
-        and extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) < extract(day from now())
-        and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2018-05-07' as date)
-    union
-      select
-        true as partial,
-        extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _year,
-        extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
-        extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
-        extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
         coalesce(cast(amplitude_id as varchar), '') as amplitude_id,
         'QuintoAndar' as region,
         'QuintoAndar' as city
