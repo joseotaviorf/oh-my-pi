13118d6d52c156bac5f2a441e428dada5444227d
darambaris <jessika.darambaris@gmail.com>
2019-09-17 17:02:43 -0300

    revert changes from file_service file

--- bietlejuice/jobs/composer/base/db/__init__.py
+++ bietlejuice/jobs/composer/base/db/__init__.py
@@ -8,8 +8,9 @@ from bietlejuice.jobs.composer.base.db.database_enum import (
     DatabaseTypeEnum,
 )
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-DATALAKE_SQL_DIR = os.path.join(DIR_PATH, "../../db/datalake")
+DATALAKE_SQL_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "../../db/datalake"
+)
 
 QUERIES_DATALAKE_PATH = DATALAKE_SQL_DIR + "/queries/"
+DDL_DATALAKE_PATH = DATALAKE_SQL_DIR + "/ddl/"
