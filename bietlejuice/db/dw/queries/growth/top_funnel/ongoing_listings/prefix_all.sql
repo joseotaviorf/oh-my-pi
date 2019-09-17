6a7f9ad1c655de6629d0fc1e40ac4e7841d8b5d3
Ribaldo <ribaldorafael@gmail.com>
2019-09-17 16:12:12 -0300

    Removing remaining refs of fact_house_status

--- bietlejuice/db/dw/queries/growth/top_funnel/ongoing_listings/prefix_all.sql
+++ bietlejuice/db/dw/queries/growth/top_funnel/ongoing_listings/prefix_all.sql
@@ -1,5 +1,5 @@
 with
-fact_house_status_filter as (
+fact_house_listing_status_filter as (
 -- new step so we can filter last version and also last status (in case there are two status in the same day)
 select
     fhs.*,
@@ -8,11 +8,11 @@ select
     dd.month,
     dd.calendar_week,
     dd.day
-from fact_house_status fhs
+from fact_house_listing_status fhs
 join dim_date dd
-    on dd.sk_date between fhs.sk_min_status_date and coalesce(to_char(to_date(fhs.sk_max_status_date, 'YYYYMMDD') - 1, 'YYYYMMDD')::bigint, to_char(current_date - 1, 'YYYYMMDD')::bigint)
+    on dd.sk_date between fhs.sk_status_start_date and coalesce(to_char(to_date(fhs.sk_status_end_date, 'YYYYMMDD') - 1, 'YYYYMMDD')::bigint, to_char(current_date - 1, 'YYYYMMDD')::bigint)
 left join dim_house_listing dh
-  on fhs.sk_house = dh.sk_house_listing
+  on fhs.sk_house_listing = dh.sk_house_listing
 ),
 all_dates as (
   select distinct
@@ -25,26 +25,26 @@ all_dates as (
     dense_rank() over (partition by f.year,
     																f.month,
     																f.calendar_week,
-    																f.day order by f.sk_house asc)
+    																f.day order by f.sk_house_listing asc)
     	+ dense_rank() over (partition by f.year,
     																		f.month,
     																		f.calendar_week,
-    																		f.day order by f.sk_house desc)
+    																		f.day order by f.sk_house_listing desc)
 			- 1 as daily_count,
     dense_rank() over (partition by f.year,
-    																f.calendar_week order by f.sk_house asc)
+    																f.calendar_week order by f.sk_house_listing asc)
     	+ dense_rank() over (partition by f.year,
-    																		f.calendar_week order by f.sk_house desc)
+    																		f.calendar_week order by f.sk_house_listing desc)
 			- 1 as weekly_count,
     dense_rank() over (partition by f.year,
-    																f.month order by f.sk_house asc)
+    																f.month order by f.sk_house_listing asc)
     	+ dense_rank() over (partition by f.year,
-    																		f.month order by f.sk_house desc)
+    																		f.month order by f.sk_house_listing desc)
 			- 1 as monthly_count,
-    dense_rank() over (partition by f.year order by f.sk_house asc)
-    	+ dense_rank() over (partition by f.year order by f.sk_house desc)
+    dense_rank() over (partition by f.year order by f.sk_house_listing asc)
+    	+ dense_rank() over (partition by f.year order by f.sk_house_listing desc)
 			- 1 as yearly_count
-	from fact_house_status_filter f
+	from fact_house_listing_status_filter f
 	where f."date" < current_date
 	  and f.status_history = 'publicado'
   order by 1, 2, 3, 4
@@ -59,7 +59,7 @@ all_dates_last_month as (
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_house) as monthly_count
-	from fact_house_status_filter f
+	from fact_house_listing_status_filter f
 	where f."date" < current_date
 	  and f.status_history = 'publicado'
 	  and f.year = date_part('year', add_months(current_date, -1))
@@ -74,7 +74,7 @@ all_dates_last_year as (
 	  'QuintoAndar'::varchar as region,
 	  'QuintoAndar'::varchar as city,
   	count(distinct f.sk_house) as yearly_count
-	from fact_house_status_filter f
+	from fact_house_listing_status_filter f
 	where f."date" < current_date and f.status_history = 'publicado'
 	  and f.year = date_part('year', add_months(current_date, -12))
   		and ((f.month = date_part('month', add_months(current_date, -12))
