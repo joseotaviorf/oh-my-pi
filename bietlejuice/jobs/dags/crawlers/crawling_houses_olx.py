8f2b4067c675ee4e98764bc08f67aa3dbb0386a5
Adan Echemendia Montero <aemontero7@gmail.com>
2019-09-19 16:04:43 -0300

    augmented ram constraints to jobs submitted by scrappers

--- bietlejuice/jobs/dags/crawlers/crawling_houses_olx.py
+++ bietlejuice/jobs/dags/crawlers/crawling_houses_olx.py
@@ -35,7 +35,7 @@ def submit_olx(**kwargs):
         job_name='crawl-olx',
         job_queue='crawling-houses',
         job_definition='crawling-houses:10',
-        memory=8192,
+        memory=16384,
         command=['./crawlers/olx_crawler.py', '--listing_date', execution_date, '--states'] + states
     )
     logger.info('m=submit_olx, msg=Job {} with status {}'.format('-'.join([r.get('jobId'),
