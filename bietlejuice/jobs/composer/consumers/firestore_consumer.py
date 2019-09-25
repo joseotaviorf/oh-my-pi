87c997d5ec34f69c2ea405df692149e82cec534c
kenjihiraoka <willian.hiraoka@fatec.sp.gov.br>
2019-09-25 11:10:44 -0300

    DC-339 Fix code style

--- bietlejuice/jobs/composer/consumers/firestore_consumer.py
+++ bietlejuice/jobs/composer/consumers/firestore_consumer.py
@@ -1,7 +1,7 @@
 from quintoandar_logger import QuintoAndarLogger
 
 import firebase_admin
-from firebase_admin import (credentials, firestore)
+from firebase_admin import credentials, firestore
 from bietlejuice.jobs.composer.base.db import DatabaseTypeEnum
 from bietlejuice.jobs.composer.parsers.firestore_parser import FirestoreParser
 from bietlejuice.jobs.composer.consumers.database_consumer import DatabaseConsumer
@@ -16,13 +16,13 @@ class FirestoreConsumer(DatabaseConsumer):
     BATCH_SIZE = 5000
 
     def __init__(self, connection):
-        if connection['dbtype'].lower() != DatabaseTypeEnum.FIRESTORE:
+        if connection["dbtype"].lower() != DatabaseTypeEnum.FIRESTORE:
             raise RuntimeError(
                 f"m=__init__, con_type={connection.get('dbtype')}, "
                 f"msg=Connection is not a firestore connection"
             )
 
-        cred = credentials.Certificate(connection['credentials'])
+        cred = credentials.Certificate(connection["credentials"])
         firebase_admin.initialize_app(cred)
 
         self.db = firestore.client()
@@ -31,7 +31,9 @@ class FirestoreConsumer(DatabaseConsumer):
     @logger
     def get_table_names_and_sizes(self):
         all_collections = self.db.collections()
-        tables = [{'table_name': collection.id, 'size': 0} for collection in all_collections]
+        tables = [
+            {"table_name": collection.id, "size": 0} for collection in all_collections
+        ]
         return self._get_default_read_format_and_options(tables)
 
     @logger
@@ -43,17 +45,20 @@ class FirestoreConsumer(DatabaseConsumer):
         :param last_doc: Last value from query returned by this method
         :return: Tuple (spark dataframe, last_document)
         """
-        doc_ref = self.db.collection(f'{table}')
+        doc_ref = self.db.collection(f"{table}")
 
         if not last_doc:
-            first_query = doc_ref.order_by(f'{order_by_column}').limit(FirestoreConsumer.BATCH_SIZE)
+            first_query = doc_ref.order_by(f"{order_by_column}").limit(
+                FirestoreConsumer.BATCH_SIZE
+            )
             docs = first_query.stream()
             data_json, last_document = self.parse_file(docs)
             return self._get_default_read_format_and_options(data_json), last_document
 
-        last_pop = last_doc[f'{order_by_column}']
-        next_query = self.parser.parse_chunk_query(doc_ref, order_by_column, last_pop,
-                                                   FirestoreConsumer.BATCH_SIZE)
+        last_pop = last_doc[f"{order_by_column}"]
+        next_query = self.parser.parse_chunk_query(
+            doc_ref, order_by_column, last_pop, FirestoreConsumer.BATCH_SIZE
+        )
         docs = next_query.stream()
         data_json, last_document = self.parse_file(docs)
         return self._get_default_read_format_and_options(data_json), last_document
@@ -88,17 +93,17 @@ class FirestoreConsumer(DatabaseConsumer):
         :param table_name: string
         :return: spark dataframe
         """
-        doc_ref = self.db.collection(u'{}'.format(table_name))
+        doc_ref = self.db.collection("{}".format(table_name))
 
         doc_ref_parsed = self.parser.parse_query(doc_ref, query)
         docs = doc_ref_parsed.stream()
         data_json = self.parse_file(docs)
         return self._get_default_read_format_and_options(data_json[0])
 
-    @logger(exclude='json_parsed')
+    @logger(exclude="json_parsed")
     def _get_default_read_format_and_options(self, json_parsed):
         rdd = sc.parallelize([json_parsed], 3)
-        return spark.read.option('multiline', 'true').json(rdd)
+        return spark.read.option("multiline", "true").json(rdd)
 
     def parse_file(self, docs):
         return self.parser.parse_document_type(docs)
