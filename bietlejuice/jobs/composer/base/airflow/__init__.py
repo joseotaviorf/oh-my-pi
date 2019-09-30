6b15c79b107e7fea8194e8af5ac2f63b8ecfc16b
Adan Echemendia Montero <aemontero7@gmail.com>
2019-09-30 16:04:50 -0300

    Do changes after review of the PR by quintoandar-admin

--- bietlejuice/jobs/composer/base/airflow/__init__.py
+++ bietlejuice/jobs/composer/base/airflow/__init__.py
@@ -1,10 +1,7 @@
 import os
 
 from bietlejuice.jobs.composer.base.airflow.base_dag import BaseDAG
-from bietlejuice.jobs.composer.base.airflow.base_dag import BaseDAG
 from bietlejuice.jobs.composer.base.airflow.base_sub_dag import BaseSubDAG
-from bietlejuice.jobs.composer.base.airflow.base_sub_dag import BaseSubDAG
-from bietlejuice.jobs.composer.base.airflow.environment import Environment
 from bietlejuice.jobs.composer.base.airflow.environment import Environment
 
 DEPENDENCIES_FILE_PATH = os.path.join(
