d829751c28ff85816c51c44c1d74b02c787c3f96
Rafael Leinio <rafael.leper@gmail.com>
2019-09-30 11:36:47 -0300

    removing agent_region task

--- bietlejuice/jobs/composer/dags/ebdb/ebdb.py
+++ bietlejuice/jobs/composer/dags/ebdb/ebdb.py
@@ -122,7 +122,6 @@ def condo_sub_dag(sub_dag_name):
     create_clean_external_tables_task = create_external_tables_task(
         local_dag, ENV, "clean", "ebdb", ["condo"]
     )
-
     create_clean_condo_in_datalake_task >> [
         create_dim_condo_in_datalake_task,
         create_clean_external_tables_task,
@@ -144,17 +143,10 @@ def region_sub_dag(sub_dag_name):
     create_dim_condo_task = create_dw_table_in_datalake_task(
         local_dag, "dim_condo", "public", ENV, DAG_ID
     )
-    create_clean_agent_region_task = create_clean_table_in_datalake_task(
-        local_dag, "agent_region", "ebdb", ENV, DAG_ID
-    )
     create_clean_external_tables_task = create_external_tables_task(
-        local_dag, ENV, "clean", "ebdb", ["region", "agent_region"]
+        local_dag, ENV, "clean", "ebdb", ["region"]
     )
-
-    [
-        create_clean_region_task,
-        create_clean_agent_region_task,
-    ] >> create_dim_condo_task >> create_clean_external_tables_task
+    create_clean_region_task >> create_dim_condo_task >> create_clean_external_tables_task
 
     return local_dag
 
