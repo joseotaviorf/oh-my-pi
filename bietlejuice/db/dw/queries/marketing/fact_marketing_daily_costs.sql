b855c1a46618797cce4b3ce7c3c8ba674361a4d4
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2019-10-04 16:35:00 -0300

    [DTOF-500] Add LinkedIn into cost's cube (#1849)
    * Add LinkedIn costs to cube
    * add campaign name as utm_campaign
    * Add two executions per day

--- bietlejuice/db/dw/queries/marketing/fact_marketing_daily_costs.sql
+++ bietlejuice/db/dw/queries/marketing/fact_marketing_daily_costs.sql
@@ -261,6 +261,26 @@ UNION
       left join cities_share_by_ol csol on csol.sk_date = fcl.sk_cost_date
       -- filter with 'between' because there is future cost
       where fcl.sk_cost_date between 20180101 and cast(TO_CHAR(getdate() -1, 'YYYYMMDD') as integer)
+UNION
+    select
+        fli.sk_date,
+        'linkedin' as origin,
+        'fact_linkedin_daily_cost_attributions' as fact_cost,
+        dlc.campaign_name,
+        null as campaign_city,
+        dlc.account_name as account_name,
+        lower(dlc.campaign_name) as campaign_name_l,
+        lower(dlc.account_name) as account_name_l,
+        dlc.campaign_name as utm_campaign,
+        null as utm_term,
+        null as utm_content,
+        null as desktop_cost,
+        null as mobile_cost,
+        null as other_cost,
+        total_spent as total_cost
+    from marketing.fact_linkedin_daily_cost_attributions fli
+    join marketing.dim_linkedin_campaign dlc
+        on dlc.sk_campaign = fli.sk_campaign
 )
 ,demand_tax as (
 	select
@@ -328,14 +348,14 @@ UNION
 	    cf.utm_campaign,
 		cf.utm_term,
 		cf.utm_content,
-		case when total_cost is null then
-			case when tx.mkt_platform = 'Mobile' then mobile_cost
-				 when tx.mkt_platform = 'Desktop' then desktop_cost
-				 when tx.mkt_platform = 'Other' then other_cost
-			end   
-			else total_cost end
-			* cast(coalesce(fator_custo, '0') as numeric(3,2))
-			as cost,
+		case
+		    when total_cost is null then
+                case when tx.mkt_platform = 'Mobile' then mobile_cost
+                     when tx.mkt_platform = 'Desktop' then desktop_cost
+                     when tx.mkt_platform = 'Other' then other_cost
+                end
+            else total_cost
+        end * cast(coalesce(fator_custo, '0') as numeric(3,2)) as cost,
 		case when ((coalesce(cf.account_name_l,'') like '%supply%' or coalesce(cf.account_name_l,'') like '%display%')
 					and coalesce(cf.account_name_l,'') != 'supply_affiliates')
 					OR
@@ -360,22 +380,22 @@ UNION
 				and cf.origin = tx.origin
 )
 select 
-sk_date,
-side as funnel_side,
-account_name,
-campaign_name,
-city_group_final as city_group,
-mkt_category,
-mkt_flow,
-mkt_completion,
-mkt_origin,
-mkt_channel,
-mkt_medium,
-mkt_source,
-mkt_platform,
-utm_campaign,
-utm_term,
-utm_content,
-cost,
-getdate() as ts_load
+    sk_date,
+    side as funnel_side,
+    account_name,
+    campaign_name,
+    city_group_final as city_group,
+    mkt_category,
+    mkt_flow,
+    mkt_completion,
+    mkt_origin,
+    mkt_channel,
+    mkt_medium,
+    mkt_source,
+    mkt_platform,
+    utm_campaign,
+    utm_term,
+    utm_content,
+    cost,
+    getdate() as ts_load
 from demand_tax
