e75de55cb4883ef96f91a92b37851d58fded2f98
Rafael Ribaldo <ribaldorafael@gmail.com>
2019-10-07 15:47:22 -0300

    Updating start date name

--- bietlejuice/db/dw/queries/datamarts/house_available_hours.sql
+++ bietlejuice/db/dw/queries/datamarts/house_available_hours.sql
@@ -31,7 +31,7 @@ select
 	house_id,
 	to_char(available_start_date, 'YYYYMMDD')::bigint as sk_available_started_date,
 	to_char(available_end_date, 'YYYYMMDD')::bigint as sk_available_ended_date,
-	available_start_date,
+	available_started_date,
 	available_end_date,
 	day_of_week,
 	hours_available_08to09
