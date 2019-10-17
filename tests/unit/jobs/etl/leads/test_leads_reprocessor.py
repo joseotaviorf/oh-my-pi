d26ace9dcdd3a26417bd946117d9ab39a5300014
umluizlima <umluizlima@gmail.com>
2019-10-17 19:21:54 -0300

    Fix tests

--- tests/unit/jobs/etl/leads/test_leads_reprocessor.py
+++ tests/unit/jobs/etl/leads/test_leads_reprocessor.py
@@ -63,7 +63,7 @@ class TestLeadsReprocessor(object):
 
     @pytest.mark.parametrize('input, expected',
                              [(pd.DataFrame(data=[[1, 'test', '']], columns=['id', 'infosExtras', 'origem']),
-                               '[{"infosExtras":"infosExtras test; id_origin_lead=1","origem":"Reprocessado"}]'),
+                               '[{"id":1,"infosExtras":"infosExtras test; id_origin_lead=1","origem":"Reprocessado"}]'),
                               (pd.DataFrame(data=[[1, '']], columns=['id', 'origem']),
                                '[{"id":1,"origem":"Reprocessado","infosExtras":"infosExtras; id_origin_lead=1"}]')])
     def test__treat_leads_with_id(self, leads_reprocessor, input, expected):
