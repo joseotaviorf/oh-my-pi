445de3ecbd705cd18f1330e880dc12d8f3e196bf
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2019-10-31 17:17:31 -0300

    [DE-115] Removing ODS_condition from Offer subdag (#1953)
    * Removing ODS_condition from Offer subdag
    
    * Removing references from ODS_condition task

--- bietlejuice/jobs/dags/supply_demand_funnel/offer_subdag.py
+++ bietlejuice/jobs/dags/supply_demand_funnel/offer_subdag.py
@@ -30,7 +30,7 @@ class OfferSubDag(DimSubDag):
         offer_dag = self._build_local_dag()
 
         (offer_to_s3_task, topic_to_s3_task, message_to_s3_task, offer_to_ods_task, pre_proposal_task,
-         pre_proposta_aud_task, condicao_proposta_task, staging_dim_offer_task,
+         pre_proposta_aud_task, staging_dim_offer_task,
          dim_offer_task) = self.__build_data_tasks(offer_dag)
 
         tests_tasks = self.build_tests_tasks(
@@ -39,7 +39,7 @@ class OfferSubDag(DimSubDag):
         )
 
         offer_to_ods_task.set_upstream([offer_to_s3_task, topic_to_s3_task, message_to_s3_task])
-        pre_proposal_task >> pre_proposta_aud_task >> condicao_proposta_task
+        pre_proposal_task >> pre_proposta_aud_task
         offer_to_ods_task >> staging_dim_offer_task
         staging_dim_offer_task.set_downstream(tests_tasks)
         dim_offer_task.set_upstream(tests_tasks)
@@ -122,18 +122,6 @@ class OfferSubDag(DimSubDag):
             }
         )
 
-        condicao_proposta_task = BaseDAG.build_python_operator(
-            task_id='ODS_condition',
-            dag=dag,
-            python_callable=utils.extract_table_dim_from_ebdb_to_ods,
-            op_kwargs={
-                'dim_name': 'condition',
-                'table_name': 'CondicaoProposta',
-                'copy_to_clean': False,
-                'bucket': DimSubDag.S3_BUCKET
-            }
-        )
-
         staging_dim_offer_task = BaseDAG.build_python_operator(
             dag=dag,
             task_id='STAGING_dim_offer',
@@ -156,4 +144,4 @@ class OfferSubDag(DimSubDag):
         )
 
         return (offer_to_s3_task, topic_to_s3_task, message_to_s3_task, offer_to_ods_task, pre_proposal_task,
-                pre_proposta_aud_task, condicao_proposta_task, staging_dim_offer_task, dim_offer_task)
+                pre_proposta_aud_task, staging_dim_offer_task, dim_offer_task)
