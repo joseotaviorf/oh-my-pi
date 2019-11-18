e8c95d7600c76742038690acc5e8dd41240030c4
Jéssika Darambaris <jessika.darambaris@gmail.com>
2019-11-18 16:32:49 -0300

    [DE-267] Filter lonely events of the type 'service.command' and 'recording.available' (#2009)
    * filter lonely events of type 'service.command' and 'recording.available'
    
    * specify column nickname
    
    * add CTE events_filter
    
    * change tz from America/Sao_Paulo to Brazil/East

--- bietlejuice/jobs/composer/db/datalake/queries/bigfone_event/clean/events.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/bigfone_event/clean/events.sql
@@ -1,17 +1,34 @@
-select
-    bigint(id) as id,
-    event,
-    get_json_object(metadata, '$.call_id') as id_call,
-    metadata,
-    date_format(event_timestamp, 'y-MM-dd hh:mm:ss') as ts_created,
-    date_format(from_utc_timestamp(event_timestamp, 'America/Sao_Paulo'), 'y-MM-dd hh:mm:ss') as ts_created_local,
-    date_format(received_timestamp, 'y-MM-dd hh:mm:ss') as ts_received,
-    date_format(from_utc_timestamp(received_timestamp, 'America/Sao_Paulo'), 'y-MM-dd hh:mm:ss') as ts_received_local,
-    year,
-    month,
-    day
-from     
-    datalake_bigfone_raw.events
+with events_filter as (
+    select * from datalake_bigfone_raw.events
+    where provider='teravoz' and year={year} and month={month} and day={day}
+),
+-- we need to do this filter because there is a Teravoz bug that generates service.command 
+-- or/and recording.available events with wrong call id (Teravoz internal control call id).
+lonely_events as (
+    select 
+        count(get_json_object(metadata, '$.call_id')) as count_call_id, 
+        get_json_object(metadata, '$.call_id') as call_id 
+    from events_filter
+    where
+      event='service.command' or event='recording.available'
+    group by 2
+    -- Teravoz can generate 2 events with the same wrong call_id (recording and service) 
+    having count_call_id <= 2
+)
+select 
+    bigint(events.id) as id,
+    events.event as event,
+    get_json_object(events.metadata, '$.call_id') as id_call,
+    events.metadata as metadata,
+    date_format(events.event_timestamp, 'y-MM-dd hh:mm:ss') as ts_created,
+    date_format(from_utc_timestamp(events.event_timestamp, 'Brazil/East'), 'y-MM-dd hh:mm:ss') as ts_created_local,
+    date_format(events.received_timestamp, 'y-MM-dd hh:mm:ss') as ts_received,
+    date_format(from_utc_timestamp(events.received_timestamp, 'Brazil/East'), 'y-MM-dd hh:mm:ss') as ts_received_local,
+    events.year as year,
+    events.month as month,
+    events.day as day  
+from events_filter events
+left join lonely_events lonely
+on lonely.call_id=get_json_object(events.metadata, '$.call_id')
 where 
-    year={year} and month={month} and day={day}
-    and provider='teravoz'
+    lonely.call_id is null
