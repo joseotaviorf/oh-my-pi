c8a23c63b2779ae0ea66fb2ef17eb2137a31e60f
Jéssika Darambaris <jessika.darambaris@gmail.com>
2019-11-25 18:15:50 -0300

    [DE-268-agents] Create views of Bigfone actor events on Athena (#2035)
    * create view event_agent_left
    
    * change columns number to int
    
    * create view event_agent_entered
    
    * create view event_agent_ringing
    
    * remove parenthesis
    
    * create view event_agent_logged_out
    
    * create view event_agent_logged_in
    
    * create view event_agent_paused
    
    * create view event_agent_no_answer
    
    * standardizing collumn order
    
    * standardizing collumn order
    
    * change year/month/day to dt_event and rename table to agent_entered_events
    
    * change year/month/day to dt_event and rename table to agent_left_events
    
    * change year/month/day to dt_event and rename table to agent_left_events
    
    * change year/month/day to dt_event and rename table to agent_logged_in_events
    
    * change year/month/day to dt_event and rename table to agent_logged_in_events
    
    * change year/month/day to dt_event and rename table to agent_logged_out_events
    
    * fix rename and identation
    
    * change year/month/day to dt_event and rename table to agent_no_answer_events
    
    * change year/month/day to dt_event and rename table to agent_paused_events
    
    * change year/month/day to dt_event and rename table to agent_ringing_events

---
+++ bietlejuice/jobs/composer/db/datalake/views/bigfone_event/agents/agent_logged_out_events.sql
@@ -0,0 +1,16 @@
+drop view if exists datalake_bigfone_clean_prod.agent_logged_out_events;
+create or replace view datalake_bigfone_clean_prod.agent_logged_out_events as
+select
+    id,
+    json_extract_scalar(metadata, '$.actor') as agent_email,
+    cast(json_extract_scalar(metadata, '$.queue') as smallint) as queue_number,
+    cast(json_extract_scalar(metadata, '$.number') as integer) as extension_number,
+    ts_created,
+    ts_created_local,
+    ts_received,
+    ts_received_local,
+    date(concat(cast(year as varchar(4)), '-', cast(month as varchar(2)), '-', cast(day as varchar(2)))) as dt_event
+from     
+    datalake_bigfone_clean_prod.events
+where 
+    event='actor.logged-out';
---
+++ bietlejuice/jobs/composer/db/datalake/views/bigfone_event/agents/agent_no_answer_events.sql
@@ -0,0 +1,18 @@
+drop view if exists datalake_bigfone_clean_prod.agent_no_answer_events;
+create or replace view datalake_bigfone_clean_prod.agent_no_answer_events as
+select
+    id,
+    id_call,
+    json_extract_scalar(metadata, '$.actor') as agent_email,
+    cast(json_extract_scalar(metadata, '$.queue') as smallint) as queue_number,
+    cast(json_extract_scalar(metadata, '$.number') as integer) as extension_number,
+    cast(json_extract_scalar(metadata, '$.ringtime') as smallint) as seconds_ringing,
+    ts_created,
+    ts_created_local,
+    ts_received,
+    ts_received_local,
+    date(concat(cast(year as varchar(4)), '-', cast(month as varchar(2)), '-', cast(day as varchar(2)))) as dt_event
+from     
+    datalake_bigfone_clean_prod.events
+where 
+    event='actor.noanswer';
