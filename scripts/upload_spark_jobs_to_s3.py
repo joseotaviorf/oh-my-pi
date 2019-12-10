5b32bd8ba858070cbc0a678ad7a17823b29f2805
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2019-12-10 12:44:07 -0300

    add ExtraArgs to fix databrick access permissions to uploaded Spark Jobs on aws bucket (#2156)

--- scripts/upload_spark_jobs_to_s3.py
+++ scripts/upload_spark_jobs_to_s3.py
@@ -26,5 +26,6 @@ for root, _, files in os.walk('{}/../{}'.format(abs_path, composer_dags_folder))
         print("Uploading file '{}' from dag '{}'...".format(file_, dag))
         local_path = '{}/{}'.format(root, file_)
         remote_path = '{}/{}/{}'.format(s3_folder_path, dag, file_)
-        s3.upload_file(local_path, args.s3_bucket, remote_path)
+        s3.upload_file(local_path, args.s3_bucket, remote_path,
+                       ExtraArgs={'ACL': 'bucket-owner-full-control'})
         print("File '{}' from dag '{}' successfully uploaded!\n".format(file_, dag))
