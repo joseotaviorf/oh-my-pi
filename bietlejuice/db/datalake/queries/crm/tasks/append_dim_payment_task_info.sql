ec98bdcba49d540c97a87e598447aef367bf4196
kenjihiraoka <31676903+kenjihiraoka@users.noreply.github.com>
2019-12-12 16:03:04 -0300

    [DE-357] Remove duplicate sk_task in crm.dim_payment_task (#2170)

--- bietlejuice/db/datalake/queries/crm/tasks/append_dim_payment_task_info.sql
+++ bietlejuice/db/datalake/queries/crm/tasks/append_dim_payment_task_info.sql
@@ -1,4 +1,19 @@
-select
+,
+max_request_date as (
+    select
+      cast(id_external_contract as varchar) as id_external_contract,
+      max(json_extract_scalar(activity.metadata, '$.request_date')) as max_date
+    from datalake_heimdall_clean_prod.activity activity
+    group by 1
+),
+last_activity_refund as (
+  select m.id_external_contract, activity.status as status, m.max_date as max_date
+  from datalake_heimdall_clean_prod.activity activity
+  join max_request_date m
+    on cast(activity.id_external_contract as varchar) = m.id_external_contract
+      and m.max_date = json_extract_scalar(activity.metadata, '$.request_date')
+)
+select distinct
   ct.sk_task,
   ct.flg_solved,
   activity.status as tenant_refund_status,
@@ -19,5 +34,7 @@ select
 from tasks ct
 join datalake_clean.crm_tasks tasks
     on ct.sk_task = tasks.id
-left join datalake_heimdall_clean_prod.activity activity
+      and cast(ct.ts_partition as varchar) = tasks.dt
+left join last_activity_refund activity
   on cast(id_external_contract as varchar) = json_extract_scalar(tasks.metadata, '$.contract_id')
+     and activity.max_date = json_extract_scalar(tasks.metadata, '$.request_date')
