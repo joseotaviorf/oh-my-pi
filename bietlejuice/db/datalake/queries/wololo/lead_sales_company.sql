35f08655e0cb94aaf07989dcafe3caea4756cfc4
Isabela Sayuri Matsumoto <46712621+sayurimatsumoto5andar@users.noreply.github.com>
2019-12-20 15:32:44 -0300

    [DC-507] Solving bug: incorrect ts_sales_company_sent and sales_company (#2196)
    * [DC-507] Solving bug: change sales company and timestamp sale company change
    
    * Apply suggestions from code review
    
    Adjusting query
    
    Co-Authored-By: Rafael Ribaldo <ribaldorafael@gmail.com>
    
    Co-authored-by: felipemiquelim <felipemiquelim@gmail.com>
    Co-authored-by: Rafael Ribaldo <ribaldorafael@gmail.com>

--- bietlejuice/db/datalake/queries/wololo/lead_sales_company.sql
+++ bietlejuice/db/datalake/queries/wololo/lead_sales_company.sql
@@ -1,21 +1,30 @@
-with lead_sales_company as (
+with
+lead_revision as (
     select
-       p.referenceid as id_lead,
-       from_unixtime(cast(revtstmp as bigint)/1000) as ts_sales_company_sent,
-       salescompany as sales_company,
-       rank() over (partition by p.referenceid order by revtstmp desc) as rank_last_to_first
-    from datalake_wololo_raw_prod.prospect p
+      referenceid as id_lead,
+      dimensionentity_id,
+      salescompany as sales_company,
+      from_unixtime(info.revtstmp/1000) as rev_datetime,
+      min(from_unixtime(info.revtstmp/1000)) over (partition by referenceid) as insert_time,
+      case when lag (salescompany) over (partition by referenceid order by from_unixtime(info.revtstmp/1000) asc) <> salescompany then true else false end as flag_change_sales_company
+    from datalake_wololo_raw_prod.prospect_aud p
       join datalake_wololo_raw_prod.prospectdimension_aud pd_aud
           on p.dimensionentity_id =  pd_aud.id
       join datalake_wololo_raw_prod.revinfo info
          on pd_aud.rev = info.rev
+),
+last_change_company as (
+    select
+        id_lead,
+        max(case when flag_change_sales_company = true then rev_datetime else insert_time end) as last_change_company_time
+    from lead_revision
+    group by 1
 )
 select
-    id_lead,
+    lr.id_lead,
     sales_company,
-    ts_sales_company_sent
-from
-    lead_sales_company
-where
-    -- extracting the latest relation of lead - sales_company
-    rank_last_to_first = 1
+    last_change_company_time as ts_sales_company_sent
+from lead_revision lr
+     join last_change_company lcc
+        on lr.id_lead = lcc.id_lead
+        and lcc.last_change_company_time = lr.rev_datetime
