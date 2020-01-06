33aeed554933793e82a70ee1c7d244a90b0afa8a
kenjihiraoka <31676903+kenjihiraoka@users.noreply.github.com>
2020-01-06 13:17:21 -0300

    [DE-367] Updating Amplitude old references in SQL files  (#2201)

--- bietlejuice/db/datalake/queries/amplitude/owner_landing_views/prefix_current_date.sql
+++ bietlejuice/db/datalake/queries/amplitude/owner_landing_views/prefix_current_date.sql
@@ -1,24 +1,24 @@
 with all_events as (
-        select
-            true as partial,
-            extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _year,
-            extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
-            extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
-            extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
-            coalesce(cast(amplitude_id as varchar), '') as amplitude_id,
-            'QuintoAndar' as region,
-            'QuintoAndar' as city
-        from datalake_amplitude_clean_prod.events
-        where event_type in (
-              'landing_page_viewed',
-              'Owner_Landing-Views_register_form'
-            )
-            and ((app = 183047 and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-08-23' as date))
-              or (app = 160023 and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast('2017-08-23' as date)))
-            and extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(year from (now() - interval '1' month))
-            and extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(month from (now() - interval '1' month))
-            and extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) < extract(day from now())
-            and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-01-01' as date)
-            and not(cast(json_extract(event_properties, '$.ub_page_variant') as varchar) in ('bv','cj','co')
+    select
+        true as partial,
+        extract(year from ts_event) as _year,
+        extract(month from ts_event) as _month,
+        extract(week from ts_event) as _week,
+        extract(day from ts_event) as _day,
+        id_amplitude as amplitude_id,
+        'QuintoAndar' as region,
+        'QuintoAndar' as city
+    from datalake_amplitude_clean_prod.events_repartitioned
+    where event_type in (
+          'landing_page_viewed',
+          'Owner_Landing-Views_register_form'
+        )
+        and ((id_app = 183047 and date(ts_event) >= cast('2017-08-23' as date))
+            or (id_app = 160023 and date(ts_event) < cast('2017-08-23' as date)))
+        and extract(year from ts_event) = extract(year from (now() - interval '1' month))
+        and extract(month from ts_event) = extract(month from (now() - interval '1' month))
+        and extract(day from ts_event) < extract(day from now())
+        and date(ts_event) >= cast('2017-01-01' as date)
+        and not(cast(json_extract(event_properties, '$.ub_page_variant') as varchar) in ('bv','cj','co')
                 and cast(json_extract(event_properties, '$.ub_page_name') as varchar) = 'Proprietário')
 )
