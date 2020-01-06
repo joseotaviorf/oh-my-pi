7915d504d91296bf00d3ec89d5d9476c25bec930
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-01-06 11:27:13 -0300

    [DE-366] Updating Amplitude old references in SQL files (#2210)
    * add more event properties to house_id
    
    * remove inexistents events Id_imovel and imovel_id
    
    * update query amplitude/schedule_page_views/prefix_all_dates.sql
    
    * update datalake/queries/user_affiliate_origin.sql
    
    * update datalake/queries/extract_booking_media_sources.sql
    
    * remove unecessary cast

--- bietlejuice/db/datalake/queries/amplitude/schedule_page_views/prefix_all_dates.sql
+++ bietlejuice/db/datalake/queries/amplitude/schedule_page_views/prefix_all_dates.sql
@@ -1,34 +1,19 @@
 with all_events as (
-      select
-        false as partial,
-        extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _year,
-        extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
-        extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
-        extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
-        coalesce(uuid, '') as uuid,
-        case
-          when json_extract(event_properties, '$.house_id') is not null and regexp_extract(cast(json_extract(event_properties, '$.house_id') as varchar), '\d+') is not null
-            then if(length(regexp_extract(cast(json_extract(event_properties, '$.house_id') as varchar), '\d+')) < 9,
-                     892700000 + cast(cast(regexp_extract(cast(json_extract(event_properties, '$.house_id') as varchar), '\d+') as double) as integer),
-                     cast(cast(regexp_extract(cast(json_extract(event_properties, '$.house_id') as varchar), '\d+') AS double) AS integer))
-          when json_extract(event_properties, '$.Id_Imovel') is not null and regexp_extract(cast(json_extract(event_properties, '$.Id_Imovel') as varchar), '\d+') is not null
-            then if(length(regexp_extract(cast(json_extract(event_properties, '$.Id_Imovel') as varchar), '\d+')) < 9,
-                892700000 + cast(cast(regexp_extract(cast(json_extract(event_properties, '$.Id_Imovel') as varchar), '\d+') as double) as integer),
-                cast(cast(regexp_extract(cast(json_extract(event_properties, '$.Id_Imovel') as varchar), '\d+') as double) as integer))
-          when json_extract(event_properties, '$.imovel_id') is not null and regexp_extract(cast(json_extract(event_properties, '$.imovel_id') as varchar), '\d+') is not null
-            then if(length(regexp_extract(cast(json_extract(event_properties, '$.imovel_id') as varchar), '\d+')) < 9,
-                892700000 + cast(cast(regexp_extract(cast(json_extract(event_properties, '$.imovel_id') as varchar), '\d+') as double) as integer),
-                cast(cast(regexp_extract(cast(json_extract(event_properties, '$.imovel_id') as varchar), '\d+') as double) as integer))
-          when json_extract(event_properties, '$.Imovel_id') is not null and regexp_extract(cast(json_extract(event_properties, '$.Imovel_id') as varchar), '\d+') is not null
-            then if(length(regexp_extract(cast(json_extract(event_properties, '$.Imovel_id') as varchar), '\d+')) < 9,
-                892700000 + cast(cast(regexp_extract(cast(json_extract(event_properties, '$.Imovel_id') as varchar), '\d+') as double) as integer),
-                cast(cast(regexp_extract(cast(json_extract(event_properties, '$.Imovel_id') as varchar), '\d+') as double) as integer))
-        else -1
-        end as house_id
-      from datalake_amplitude_clean_prod.events
-      where event_type = 'schedule_page_viewed'
-            and app = 170698
-            and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2017-08-23' as date)
-            and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast(now() as date)
-
+  select
+    false as partial,
+    extract(year from ts_event) as _year,
+    extract(month from ts_event) as _month,
+    extract(week from ts_event) as _week,
+    extract(day from ts_event) as _day,
+    coalesce(uuid, '') as uuid,
+    case
+      when ep_house_id is not null and regexp_extract(ep_house_id, '\d+') is not null
+        then if(length(regexp_extract(ep_house_id, '\d+')) < 9,
+            892700000 + cast(regexp_extract(ep_house_id, '\d+') as integer),
+            cast(regexp_extract(ep_house_id, '\d+') as integer))
+      else -1
+    end as house_id
+  from datalake_amplitude_clean_prod."170698_schedule_page_viewed_events"
+  where cast(ts_event as date) >= cast('2017-08-23' as date)
+        and cast(ts_event as date) < cast(now() as date)
 ),
