b453636fb6a735f6cf98fa2e420c174f90181ede
Adan Echemendia Montero <aemontero7@gmail.com>
2020-01-06 15:45:38 -0300

    [DE-366] Adjust Amplitude old references in SQL queries (#2186)
    * Adjust some Amplitude queries
    
    * Refactor the queries

--- bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_all_dates.sql
+++ bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_all_dates.sql
@@ -1,17 +1,16 @@
 with all_events as (
       select
         false as partial,
-        extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _year,
-        extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
-        extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
-        extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
-        coalesce(cast(amplitude_id as varchar), '') as amplitude_id,
+        extract(year from ts_event) as _year,
+        extract(month from ts_event) as _month,
+        extract(week from ts_event) as _week,
+        extract(day from ts_event) as _day,
+        coalesce(cast(id_amplitude as varchar), '') as amplitude_id,
         'QuintoAndar' as region,
         'QuintoAndar' as city
-      from datalake_amplitude_clean_prod.landing_page_viewed_events
-      where app = 183047
-        and event_ub_page_variant in ('bv','cj','co')
-        and event_ub_page_name = 'Proprietário'
-        and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2018-05-07' as date)
-        and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) < cast(now() as date)
+      from datalake_amplitude_clean_prod."183047_landing_page_viewed_events"
+      where ep_ub_page_variant in ('bv', 'cj', 'co')
+        and ep_ub_page_name = 'Proprietário'
+        and date(ts_event) >= date('2018-05-07')
+        and date(ts_event) < date(now())
 )
--- bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
+++ bietlejuice/db/datalake/queries/amplitude/owner_landing_views_bv/prefix_current_date.sql
@@ -1,19 +1,18 @@
 with all_events as (
       select
         true as partial,
-        extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _year,
-        extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _month,
-        extract(week from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _week,
-        extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) as _day,
-        coalesce(cast(amplitude_id as varchar), '') as amplitude_id,
+        extract(year from ts_event) as _year,
+        extract(month from ts_event) as _month,
+        extract(week from ts_event) as _week,
+        extract(day from ts_event) as _day,
+        coalesce(cast(id_amplitude as varchar), '') as amplitude_id,
         'QuintoAndar' as region,
         'QuintoAndar' as city
-      from datalake_amplitude_clean_prod.landing_page_viewed_events
-      where app = 183047
-        and event_ub_page_variant in ('bv','cj','co')
-        and event_ub_page_name = 'Proprietário'
-		and extract(year from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(year from (now() - interval '1' month))
-        and extract(month from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) = extract(month from (now() - interval '1' month))
-        and extract(day from cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date)) < extract(day from now())
-        and cast(regexp_extract(trim(event_time), '\d{4}-\d{2}-\d{2}') as date) >= cast('2018-05-07' as date)
+      from datalake_amplitude_clean_prod."183047_landing_page_viewed_events"
+      where ep_ub_page_variant in ('bv','cj','co')
+        and ep_ub_page_name = 'Proprietário'
+		and extract(year from ts_event) = extract(year from (now() - interval '1' month))
+        and extract(month from ts_event) = extract(month from (now() - interval '1' month))
+        and extract(day from ts_event) < extract(day from now())
+        and date(ts_event) >= date('2018-05-07')
 )
