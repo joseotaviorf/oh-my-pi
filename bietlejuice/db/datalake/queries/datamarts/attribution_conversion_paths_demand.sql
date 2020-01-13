706dd61f4d3fd9375e97947da6b7ecccefb54ec0
Jéssika Darambaris <jessika.darambaris@gmail.com>
2020-01-13 16:27:24 -0300

    [DE-451] Fixing attribution_conversion_paths_demand datamart (#2239)
    * fixing attribution_conversion_paths_demand datamart
    
    * remove extra column into listing_page_viewed CTE
    
    * change rank() functions to min() functions

--- bietlejuice/db/datalake/queries/datamarts/attribution_conversion_paths_demand.sql
+++ bietlejuice/db/datalake/queries/datamarts/attribution_conversion_paths_demand.sql
@@ -1,37 +1,108 @@
-with events as (
-	-- retrieve events that reflect entry points (web only)
-	-- use entrance_uri user property to identify sessions UTM (more accurate than utm_ user props)
-	-- merge amplitude users to leading amplitude user (NEEDS MANUAL UPDATE) 
-	-- remove merge amplitude users because its columns aren't used.
-	select
-	  evt.ts_event as event_time,
-	  evt.id_amplitude,
-	  evt.id_session,
-	  evt.event_type,
-	  coalesce(split_part(split_part(json_extract_scalar(evt.user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
-		coalesce(split_part(split_part(json_extract_scalar(evt.user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
-		coalesce(split_part(split_part(json_extract_scalar(evt.user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
-		coalesce(split_part(split_part(json_extract_scalar(evt.user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
-		coalesce(split_part(split_part(json_extract_scalar(evt.user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term,
-		case when evt.event_type = 'visit_schedule_confirmed' then json_extract_scalar(evt.event_properties, '$.visit_code') end as visit_code,
-		rank() over (partition by evt.id_amplitude, evt.id_session order by evt.ts_event asc) as rnk_event_of_session
-	from datalake_amplitude_clean_prod.events_repartitioned evt
-	where evt.year = 2019
-	and evt.event_type in ('listing_page_viewed', 'home_page_viewed', 'search_results_page_viewed', 'schedule_page_viewed', 'visit_schedule_confirmed')
-	and evt.id_app = 170698
-	and evt.platform = 'Web'
+with listing_page_viewed as (
+    select 
+        min(ts_event) as ts_event, 
+        id_amplitude, 
+        id_session,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+        
+    from datalake_amplitude_clean_staging_prod."170698_listing_page_viewed_events"
+    where year = 2019 and platform = 'Web'
+    group by 2,3,4,5,6,7,8
+),
+home_page_viewed as (
+    select 
+        min(ts_event) as ts_event, 
+        id_amplitude, 
+        id_session,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+        
+    from datalake_amplitude_clean_staging_prod."170698_home_page_viewed_events"
+    where year = 2019 and platform = 'Web'
+    group by 2,3,4,5,6,7,8
+),
+search_results_page_viewed as (
+    select 
+        min(ts_event) as ts_event, 
+        id_amplitude, 
+        id_session,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+        
+    from datalake_amplitude_clean_staging_prod."170698_search_results_page_viewed_events"
+    where year = 2019 and platform = 'Web'
+    group by 2,3,4,5,6,7,8
+),
+schedule_page_viewed as (
+    select 
+        min(ts_event) as ts_event, 
+        id_amplitude, 
+        id_session,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+        
+    from datalake_amplitude_clean_staging_prod."170698_schedule_page_viewed_events"
+    where year = 2019 and platform = 'Web'
+    group by 2,3,4,5,6,7,8
+),
+visit_schedule_confirmed as (
+    select 
+        min(ts_event) as ts_event, 
+        id_amplitude, 
+        id_session,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+        coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term,
+        json_extract_scalar(event_properties, '$.visit_code') as visit_code
+    from datalake_amplitude_clean_staging_prod."170698_visit_schedule_confirmed_events"
+    where year = 2019 and platform = 'Web'
+    group by 2,3,4,5,6,7,8,9
+),
+pre_events as (
+    select *, null as visit_code from listing_page_viewed
+    union all select *, null as visit_code from home_page_viewed
+    union all select *, null as visit_code from search_results_page_viewed
+    union all select *, null as visit_code from schedule_page_viewed
+    union all select * from visit_schedule_confirmed
+),
+events_rank as (
+    select min(ts_event) as ts_event, id_amplitude, id_session
+    from pre_events group by 2,3
+),
+events as (
+    select 
+        p.* 
+    from pre_events p
+    inner join events_rank r
+    on p.ts_event=r.ts_event 
+       and p.id_amplitude=r.id_amplitude
+       and p.id_session=r.id_session
 ),
 conversions as (
 	-- retrieve conversion events per amplitude user
 	-- conversion def: booking that resulted in a visit with tenant prospect present onsite
 	select
-	    evt.event_time,
+	    evt.ts_event,
 	    evt.id_amplitude,
-	    rank() over (partition by id_amplitude order by event_time) as rnk_conversion
-	from events evt
+	    rank() over (partition by id_amplitude order by ts_event) as rnk_conversion
+	from visit_schedule_confirmed evt
 	join datalake_clean.ods_dim_visit dv on dv.cd_visit = evt.visit_code
 	join datalake_clean.ods_dim_booking db on db.id_visit = dv.id_visit and db.status = 'Realizado' and db.visitor_arrived = '1'
-	where event_type = 'visit_schedule_confirmed'
 ),
 touchpoints as (
 	-- aggregate events to attributed sessions (=touchpoints)
@@ -51,14 +122,13 @@ touchpoints as (
 		evt.utm_campaign,
 		evt.utm_content,
 		evt.utm_term,
-		evt.event_time as session_start_time,
-		min(conv.event_time) as conversion_time,
+		evt.ts_event as session_start_time,
+		min(conv.ts_event) as conversion_time,
 		min(conv.rnk_conversion) as nst_conversion
 	from events evt
-	join conversions conv on conv.id_amplitude = evt.id_amplitude and conv.event_time >= evt.event_time
-	where evt.rnk_event_of_session = 1
+	join conversions conv on conv.id_amplitude = evt.id_amplitude and conv.ts_event >= evt.ts_event
 	group by 1,2,3,4,5,6,7,8,9,10
-	order by 1,11,10
+    order by 1,11,10
 )
 -- aggregate touchpoints to unique conversions with their path concatenated in one column
 -- conversions July - September 2019
