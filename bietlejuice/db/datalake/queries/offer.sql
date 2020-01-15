784d8ce942dd38f148a762fcf11e8b6d9919b420
Giuseppe Giovanelli <50770057+giuseppecg@users.noreply.github.com>
2020-01-15 14:57:54 -0300

    [DREG-134] Changing script to get the first_rent_offered_by_tenant, first_rent_o… (#2241)
    * changing script to get the first_rent_offered_by_tenant, first_rent_offered_by_owner, last_rent_offered_by_tenant, last_rent_offered_by_owner even when there is not a change in rent value.
    
    * removing id filter used for testing
    
    * keeping new tables from clean datalake

--- bietlejuice/db/datalake/queries/offer.sql
+++ bietlejuice/db/datalake/queries/offer.sql
@@ -8,25 +8,23 @@ with analysis_date as (
   where oa.status_MOD = true
 	and oa.status in ('Aprovada', 'Rejeitada')
   group by 1
-),
-rent_value_offers as (
- with min_max_date as (
+), 
+min_max_date as (
     select
       id,
       turn,
       max(rev) as max_rev,
       min(rev) as min_rev
-    from datalake_ebdb_raw_prod.offer_aud
-    where
-    turn_mod = true and (type = 'Price' or (type = 'Custom' and originalrent != rent))
+    from datalake_ebdb_raw_prod.offer_aud as oa
     group by 1, 2
-  )
-  select
-    o_aud.id,
-    min(case when mnd.turn = 'Owner' and o_aud.iteration = 2 then o_aud.rent end) as first_rent_offered_by_tenant,
-  min(case when mnd.turn = 'Tenant' and o_aud.iteration = 3 then o_aud.rent end) as first_rent_offered_by_owner,
-    max(case when mxd.turn = 'Owner' then o_aud.rent end) as last_rent_offered_by_tenant,
-  max(case when mxd.turn = 'Tenant'then o_aud.rent end) as last_rent_offered_by_owner
+), 
+rent_value_offers as (
+  select 
+    o_aud.id, 
+    min(case when mnd.turn = 'Owner' and mnd.min_rev is not null then o_aud.rent end) as first_rent_offered_by_tenant,
+    min(case when mnd.turn = 'Tenant' and mnd.min_rev is  not null then o_aud.rent end) as first_rent_offered_by_owner,
+    max(case when mxd.turn = 'Owner' and mxd.max_rev is not null then o_aud.rent end) as last_rent_offered_by_tenant,
+    max(case when mxd.turn = 'Tenant' and mxd.max_rev is  not null then o_aud.rent end) as last_rent_offered_by_owner 
   from datalake_ebdb_raw_prod.offer_aud o_aud
   left join min_max_date mxd
     on mxd.id = o_aud.id
@@ -34,17 +32,14 @@ rent_value_offers as (
   left join min_max_date mnd
     on mnd.id = o_aud.id
       and mnd.min_rev = o_aud.rev
-  where o_aud.type != 'Express'
-  group by 1
-),
-max_topic_type as (
-  with max_topic_created as (
+  group by 1  
+), max_topic_created as (
     select
       id_offer,
       max(id) as id
     from datalake_godfather_clean_prod.business_topic
     group by 1
-  )
+), max_topic_type as (
   select gt.*
   from datalake_godfather_clean_prod.business_topic gt
   join max_topic_created mtc
