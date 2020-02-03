4728d13d4eff5912b81ff3915a2a112f75527212
Jéssika Darambaris <jessika.darambaris@gmail.com>
2020-02-03 16:59:03 -0300

    [DE-481] Creating MongoClient class (#2307)
    * creating MongoClient [WIP]
    
    * build connection string and Connection with Pymongo working
    
    * create MongoClient with __init__, conn, get_records and run methods
    
    * creating get_documents and adding except in get_records method
    
    * changing conn to conn_config param name

--- bietlejuice/jobs/composer/clients/db_clients/__init__.py
+++ bietlejuice/jobs/composer/clients/db_clients/__init__.py
@@ -1,3 +1,4 @@
 from bietlejuice.jobs.composer.clients.db_clients.athena_client import AthenaClient
 from bietlejuice.jobs.composer.clients.db_clients.postgres_client import PostgresClient
 from bietlejuice.jobs.composer.clients.db_clients.spark_client import SparkClient
+from bietlejuice.jobs.composer.clients.db_clients.mongo_client import MongoClient
