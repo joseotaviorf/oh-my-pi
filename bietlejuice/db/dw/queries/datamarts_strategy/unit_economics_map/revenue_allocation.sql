ab9e5273acfd2c8eb751ddb6d4aeaa775bcf69c6
Mariana Frasson Amaral Arruda <marianaamaralarruda@gmail.com>
2020-02-05 10:30:27 -0300

    Adjusting Revenue Allocation syntax and retsuko clean (#2333)

--- bietlejuice/db/dw/queries/datamarts_strategy/unit_economics_map/revenue_allocation.sql
+++ bietlejuice/db/dw/queries/datamarts_strategy/unit_economics_map/revenue_allocation.sql
@@ -1,26 +1,26 @@
 with
 invoice_ as (
 select
-  date_trunc('month', date(substring(i.due_date,1,10))) as due_month,
-  c.external_id as contract_id,
+  date_trunc('month', date(substring(i.ts_due,1,10))) as due_month,
+  c.id_external as id_contract,
   sum(i.due_amount) as due_amount,
   sum(case when i.status = 'paid' then i.paid_amount end) as paid_amount,
   sum(case when i.status = 'open' then i.due_amount end) as open_amount_month
-from datalake_retsuko_raw_prod.invoice i
-join datalake_retsuko_raw_prod.account a
-  on a.id = i.account_id
-inner join datalake_retsuko_raw_prod.contract c
-  on c.id = i.contract_id
+from datalake_retsuko_clean_prod.invoice i
+join datalake_retsuko_clean_prod.account a
+  on a.id = i.id_account
+inner join datalake_retsuko_clean_prod.contract c
+  on c.id = i.id_contract
 where a.type = 'tenant'
 group by 1,2
 ), revenues as (
 select
     due_month,
-    contract_id,
+    id_contract,
     due_amount,
     paid_amount,
     open_amount_month,
-    sum(open_amount_month) over(partition by contract_id order by due_month) as open_amount_cumsum
+    sum(open_amount_month) over(partition by id_contract order by due_month rows between 1 following and 1 following) as open_amount_cumsum
 from invoice_
 where due_month < current_date
 group by 1,2,3,4,5
@@ -37,7 +37,7 @@ select
     r.open_amount_cumsum
 from revenues r
 join datalake_clean.ods_fact_listing_rent_flows rf
-  on cast(r.contract_id as varchar) = rf.sk_contract
+  on cast(r.id_contract as varchar) = rf.sk_contract
 join datalake_clean.ods_dim_contract dc
-  on cast(r.contract_id as varchar) = dc.sk_contract
+  on cast(r.id_contract as varchar) = dc.sk_contract
 order by 2,5
