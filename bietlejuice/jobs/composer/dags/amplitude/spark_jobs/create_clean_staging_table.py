924fd47238454b3fdb9903b9f1627185a8f34624
Luc Somers <lucwsomers@gmail.com>
2020-02-10 11:12:30 -0300

    [DE-514] - Update Amplitude dag (#2341)
    * fix amplitude file
    
    * remove references of repart

--- bietlejuice/jobs/composer/dags/amplitude/spark_jobs/create_clean_staging_table.py
+++ bietlejuice/jobs/composer/dags/amplitude/spark_jobs/create_clean_staging_table.py
@@ -12,14 +12,13 @@ from bietlejuice.jobs.composer.services.metastore_services import SparkMetastore
 from bietlejuice.jobs.composer.clients.db_clients import SparkClient
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
-logger = QuintoAndarLogger("create_clean_staging_repartitioned_table")
+logger = QuintoAndarLogger("create_clean_staging_table")
 
-parser = ArgumentParser(description="create_clean_staging_repartitioned_table")
+parser = ArgumentParser(description="create_clean_staging_table")
 parser.add_argument("execution_date")
 parser.add_argument("env")
 parser.add_argument("source")
 parser.add_argument("source_table_name")
-parser.add_argument("target_table_name")
 parser.add_argument("--partition_by", nargs="+", dest="partition_by", required=False)
 
 if __name__ == "__main__":
@@ -29,13 +28,12 @@ if __name__ == "__main__":
     env = args.env
     source = args.source
     source_table_name = args.source_table_name
-    target_table_name = args.target_table_name
     partition_by = args.partition_by
 
     logger.info(
-        "m=__main__, date={}, source={}, target_table_name={}, "
-        "target_table_name{}, partition_by={}, msg=Job started".format(
-            execution_date, source, source_table_name, target_table_name, partition_by
+        "m=__main__, date={}, source={}, "
+        "source_table_name{}, partition_by={}, msg=Job started".format(
+            execution_date, source, source_table_name, partition_by
         )
     )
 
@@ -55,7 +53,7 @@ if __name__ == "__main__":
         "{}.{}".format(db_info["db_clean_databricks"], source_table_name)
     ).where("year = {} and month = {} and day = {}".format(year, month, day))
 
-    df_repartitioned = (
+    df_partitioned = (
         SparkDataFrameService(df)
         .optimize_partitions_by_partition_columns(partition_by)
         .output()
@@ -67,14 +65,14 @@ if __name__ == "__main__":
     #  only writes the files, but this needs to be discussed yet. For now it's
     #  not a big deal
     loader.load_incremental_table(
-        df=df_repartitioned,
+        df=df_partitioned,
         database_name=db_info["db_clean_staging_databricks"],
-        table_name=target_table_name,
+        table_name=source_table_name,
         format_options=SparkTableStorageFormat.DEFAULT_CLEAN,
         database_location=db_info["db_clean_staging_path"],
         partition_cols=partition_by,
     )
 
     metastore_service.refresh_table(
-        db_info["db_clean_staging_databricks"], target_table_name
+        db_info["db_clean_staging_databricks"], source_table_name
     )
