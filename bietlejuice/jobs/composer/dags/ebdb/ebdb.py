1916a111c17e7323928cbddd4f7925b8c91a563e
Luc Somers <lucwsomers@gmail.com>
2020-02-19 16:59:52 -0300

    add aud (#2424)

--- bietlejuice/jobs/composer/dags/ebdb/ebdb.py
+++ bietlejuice/jobs/composer/dags/ebdb/ebdb.py
@@ -842,12 +842,12 @@ agent_data_types_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
     clean_table="agent_data_types",
 )
 
-agent_specific_hour_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
+agent_specific_hour_aud_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
     dag=dag,
-    sub_dag_name="agent_specific_hour",
+    sub_dag_name="agent_specific_hour_aud",
     sub_dag_func=create_clean_tables_sub_dag,
     source=SOURCE,
-    clean_table="agent_specific_hour",
+    clean_table="agent_specific_hour_aud",
 )
 
 agent_weekly_hours_aud_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
@@ -999,7 +999,7 @@ ebdb_to_datalake_raw_task >> [
     info_condo_amenities_aud_sub_dag_task,
     agent_data_aud_sub_dag_task,
     agent_data_types_sub_dag_task,
-    agent_specific_hour_sub_dag_task,
+    agent_specific_hour_aud_sub_dag_task,
     agent_weekly_hours_aud_sub_dag_task,
     cep_sub_dag_task,
     contract_person_sub_dag_task,
