a6d82e7add54d205ed6371cec3d87c68310efd52
Luc Somers <lucwsomers@gmail.com>
2020-02-28 16:12:39 -0300

    escape brackets (#2453)

--- bietlejuice/db/datalake/queries/zendesk/custom_fields.sql
+++ bietlejuice/db/datalake/queries/zendesk/custom_fields.sql
@@ -17,7 +17,7 @@ parse_fields as (
     from tickets_filter tf
     inner join last_updated_ticket l
         on tf.id_ticket=l.id_ticket
-    cross join unnest(regexp_extract_all(tf.custom_fields, '{(.*?)}')) as f1(field)
+    cross join unnest(regexp_extract_all(tf.custom_fields, '{{(.*?)}}')) as f1(field)
     where regexp_like(json_format(json_extract(f1.field, '$.value')), '\"?null\"?') = false
 )
 select
