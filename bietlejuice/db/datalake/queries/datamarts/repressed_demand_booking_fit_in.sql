8c41758d52a0ab26dbe0a573105b3a7a0fa045e2
raphaelguia <39226703+raphaelguia@users.noreply.github.com>
2020-03-03 09:57:52 -0300

    expanding repressed demand booking fit in timeframe (#2467)

--- bietlejuice/db/datalake/queries/datamarts/repressed_demand_booking_fit_in.sql
+++ bietlejuice/db/datalake/queries/datamarts/repressed_demand_booking_fit_in.sql
@@ -62,7 +62,7 @@ date_series as (
   from
     datalake_clean.ods_dim_date dd
   where
-    date(week_start) between date_trunc('week',current_date - interval '2' month) and current_date
+	date(date) >= date('2019-01-01') and date(week_start) <= current_date - interval '1' day
     and date != ''
 ),
 regions as (
