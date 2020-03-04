7f70f435bc58fbb2ef1323fac2025ea4f0899046
Luc Somers <lucwsomers@gmail.com>
2020-03-04 19:14:24 -0300

    remove quotes (#2489)

--- bietlejuice/db/datalake/queries/zendesk/fact_ticket_tags.sql
+++ bietlejuice/db/datalake/queries/zendesk/fact_ticket_tags.sql
@@ -5,7 +5,7 @@ with tickets_filter as (
 )
 SELECT
   distinct tf.id_ticket as sk_ticket,
-  replace(replace(t.tag, '[', ''), ']', '') as ticket_tag,
+  regexp_extract(t.tag, '\w+') as ticket_tag,
   cast(tf.ts_updated as timestamp with time zone) as ts_updated,
   now() as ts_load
 from tickets_filter tf
