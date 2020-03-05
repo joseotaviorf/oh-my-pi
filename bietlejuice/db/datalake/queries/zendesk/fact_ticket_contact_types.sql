081865da0f103dcc6201fff675e4e91a5a17d6cb
Luc Somers <lucwsomers@gmail.com>
2020-03-05 13:20:36 -0300

    change replace to regex (#2491)

--- bietlejuice/db/datalake/queries/zendesk/fact_ticket_contact_types.sql
+++ bietlejuice/db/datalake/queries/zendesk/fact_ticket_contact_types.sql
@@ -4,7 +4,7 @@ with filtered_custom_fields AS (
     cast(zcf.dt_extracted as timestamp with time zone) as ts_updated,
     field,
     replace(regexp_extract(field, '^.*='), '=', '') as id_field,
-    replace(replace(replace(replace(regexp_extract(field, '\=(.*)'), '=', ''), '[', ''), ']', ''), '"', '') as contact_type_tag
+    regexp_extract(field, '^[^=]+=\[?"(.*)?"\]?$', 1) as contact_type_tag
   FROM datalake_clean.zendesk_custom_fields zcf
   CROSS JOIN UNNEST(
       split(zcf.custom_fields, ',')
