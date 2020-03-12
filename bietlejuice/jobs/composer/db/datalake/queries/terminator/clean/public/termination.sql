276055e4361405174585c6b43afe1e6ef9ffd9e6
Cássio Bastos <53241140+cassiolino@users.noreply.github.com>
2020-03-12 15:30:01 -0300

    Add four missing columns from termination table to datalake clean (#2529)

--- bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/public/termination.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/public/termination.sql
@@ -4,6 +4,10 @@ SELECT
     reason,
     date AS dt_termination,
     created_at AS ts_created,
-    updated_at AS ts_updated
+    updated_at AS ts_updated,
+    status,
+    canceled_at as ts_canceled,
+    vacancy_date as dt_vacancy,
+    source
 FROM
     datalake_terminator_raw.termination
