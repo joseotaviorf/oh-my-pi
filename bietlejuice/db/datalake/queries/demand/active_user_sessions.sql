cc81f007816fc7d1f1162070525208581b6b6aab
felipemiquelim <felipemiquelim@gmail.com>
2020-03-16 08:45:21 -0300

    Adding 2019 data filter (#2546)

--- bietlejuice/db/datalake/queries/demand/active_user_sessions.sql
+++ bietlejuice/db/datalake/queries/demand/active_user_sessions.sql
@@ -21,5 +21,6 @@ from
 join
 	datalake_clean.ods_dim_date dd
 	on dd."date" = aaus.dt_event
+	and cast(aaus.dt_event as date) >= date('2019-01-01')
 group by
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
