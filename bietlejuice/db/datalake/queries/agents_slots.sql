051b5925b746c8947be374eaaad70b7962c258da
felipemiquelim <felipemiquelim@gmail.com>
2020-03-19 09:28:42 -0300

    Adjusting data types (#2558)

--- bietlejuice/db/datalake/queries/agents_slots.sql
+++ bietlejuice/db/datalake/queries/agents_slots.sql
@@ -311,9 +311,9 @@ select
     vu.slot_dt,
     vu.dow,
     vu.slot_number,
-    cast(vu.ss_available_slot as integer) as ss_available_slot,
-    cast(vu.available_slot as integer) as available_slot,
-    cast(vu.available_slot_24h as integer) as available_slot_24h,
+    cast(cast(vu.ss_available_slot as boolean) as integer) as ss_available_slot,
+    cast(cast(vu.available_slot as boolean) as integer) as available_slot,
+    cast(cast(vu.available_slot_24h as boolean) as integer) as available_slot_24h,
     vu.specific_update,
     vu.time_window_update,
     vu.visit_update,
@@ -322,7 +322,7 @@ select
     vu.last_change_reason,
     cast(ah.status as integer) as history_status,
     case when pa._count > 0 then '1' else '0' end as planner_status,
-    cast(vu.specific_slot as integer) as specific_slot
+    cast(cast(vu.specific_slot as boolean) as integer) as specific_slot
 from
     visits_updates vu
     left join active_history ah
