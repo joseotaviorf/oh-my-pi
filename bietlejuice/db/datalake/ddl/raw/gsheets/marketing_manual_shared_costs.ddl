4692e92fb7d42dd02c1d562eebb53303087d5cfe
felipemiquelim <felipemiquelim@gmail.com>
2020-03-20 08:53:00 -0300

    [DE-936] Adding load of gsheets marketing_manual_shared_costs (#2560)
    Adding load of gsheets marketing_manual_shared_costs

---
+++ bietlejuice/db/datalake/ddl/raw/gsheets/marketing_manual_shared_costs.ddl
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS datalake_raw.marketing_manual_shared_costs;
+
+CREATE EXTERNAL TABLE datalake_raw.marketing_manual_shared_costs (
+  account_name string, 
+  ad_group_name string, 
+  campaign_name string, 
+  city_group string,
+  cost string,
+  dt string,
+  mkt_business string, 
+  mkt_channel string, 
+  mkt_medium string, 
+  mkt_origin string, 
+  mkt_source string, 
+  utm_content string, 
+  utm_term string
+)
+row format serde 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+with serdeproperties (
+  'separatorChar' = ',',
+  'quoteChar' = '\"'
+)
+location
+    's3://5a-datalake/raw/gsheets/marketing/cost_sharing/manual_shared_costs/'
+tblproperties (
+  'skip.header.line.count' = '1'
+)
+;
--- bietlejuice/jobs/dags/bi_marketing_daily_costs.py
+++ bietlejuice/jobs/dags/bi_marketing_daily_costs.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 import airflow.utils.helpers as airflow_helpers
@@ -7,15 +8,29 @@ from bietlejuice.jobs.base.base_dag import BaseDAG
 from bietlejuice.jobs.base.base_etl import BaseETL, EnumDB
 from bietlejuice.jobs.dags.util import environment as env
 from bietlejuice.jobs.etl import DW_QUERIES_DIR
+from bietlejuice.jobs.wrappers.GoogleDrive import GoogleSheets
 from qa_python_utils import QuintoAndarLogger
 
 env.set_airflow_var_to_local_env('BI_DW', 'DATA_ACC_AWS_ACCESS_KEY_ID', 'DATA_ACC_AWS_SECRET_ACCESS_KEY')
-
+s3_bucket = env.get_airflow_env_var('bi-datalake-s3-bucket')
+GOOGLE_S_A_CREDENTIALS = json.loads(env.get_airflow_env_var('GOOGLE_SERVICE_ACCOUNT_CREDENTIALS'))
+GOOGLE_API_SCOPE = env.get_airflow_env_var('GOOGLE_API_SCOPE')
+GOOGLE_SHEETS_FILES = json.loads(env.get_airflow_env_var('MARKETING_GOOGLE_SHEETS_FILES'))
 MAIN_DAG_NAME = 'bi-marketing-daily-costs'
 MAIN_START_DATE = datetime(2019, 1, 1)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 1,7,13,19 * * *')
 
 logger = QuintoAndarLogger(MAIN_DAG_NAME)
+
+
+def load_google_sheet_files_to_datalake(files):
+    gs = GoogleSheets(s3_bucket=s3_bucket, google_s_a_credentials=GOOGLE_S_A_CREDENTIALS,
+                      google_api_scope=GOOGLE_API_SCOPE)
+    for file in files:
+        gs.move_sheets_data_to_destination(google_sheets_file=file,
+                                           enumdb_destination=EnumDB.QuintoAndar_datalake,
+                                           csv=True)
+
 
 # create DAG definition
 main_dag = DAG(
@@ -49,6 +64,13 @@ def move_file_query_data_to_dw(schema, file_name):
     )
 
 
+load_shared_manual_costs_to_datalake_task = BaseDAG.build_python_operator(
+    dag=main_dag,
+    task_id='load_shared_manual_costs_to_datalake',
+    python_callable=load_google_sheet_files_to_datalake,
+    op_kwargs={'files': GOOGLE_SHEETS_FILES['files']}
+)
+
 load_fact_marketing_daily_costs_task = BaseDAG.build_python_operator(
     dag=main_dag,
     task_id='load_fact_marketing_daily_costs',
@@ -65,4 +87,7 @@ trigger_bi_marketing_funnels_conversions_task = TriggerDagRunOperator(
     execution_date='{{ execution_date }}'
 )
 
-airflow_helpers.chain(load_fact_marketing_daily_costs_task, trigger_bi_marketing_funnels_conversions_task)
+airflow_helpers.chain(
+    load_shared_manual_costs_to_datalake_task,
+    load_fact_marketing_daily_costs_task,
+    trigger_bi_marketing_funnels_conversions_task)
