e241c7b5166cb0e1cf5670b746ed0c5c19a737cd
helenafig <60613757+helenafig@users.noreply.github.com>
2020-03-25 11:55:12 -0300

    Update datamart_kpi_monthly_b2b.sql (#2569)

--- bietlejuice/db/dw/queries/datamarts/datamart_kpi_monthly_b2b.sql
+++ bietlejuice/db/dw/queries/datamarts/datamart_kpi_monthly_b2b.sql
@@ -45,7 +45,7 @@ select
 	date_trunc('month',coalesce(dc.dt_start, dc.dt_entrance)) as rental_month_start,
 	fhl.sk_partner,
 	fhl.sk_region,
-	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.dt_start, dc.dt_entrance)), fhl.sk_region) as new_rentals_monthly
+	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.dt_start, dc.dt_entrance)), fhl.sk_region, fhl.sk_partner) as new_rentals_monthly
 from dim_contract dc
 left join fact_house_listings fhl
   on fhl.sk_contract = dc.sk_contract
@@ -62,7 +62,7 @@ select
   date_trunc('month',coalesce(dc.ts_signature, dc.dt_start)) as contract_month_start,
   fhl.sk_partner,
   fhl.sk_region,
-  sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.ts_signature, dc.dt_start)), fhl.sk_region) as new_contracts_signed_monthly
+  sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.ts_signature, dc.dt_start)), fhl.sk_region, fhl.sk_partner) as new_contracts_signed_monthly
 from dim_contract dc
 left join fact_house_listings fhl
   on fhl.sk_contract = dc.sk_contract
@@ -77,7 +77,7 @@ select
     date_trunc('month', dd.date) as first_listing_month_start,
     fhl.sk_partner,
     lf.sk_region,
-    sum(count(distinct lf.sk_house_listing)) over(partition by date_trunc('month',dd.date), lf.sk_region) as new_first_listings_monthly
+    sum(count(distinct lf.sk_house_listing)) over(partition by date_trunc('month',dd.date), lf.sk_region, fhl.sk_partner) as new_first_listings_monthly
 from public.fact_house_listing_flows lf
 join dim_date dd
   on lf.sk_first_listing_date = dd.sk_date
@@ -114,7 +114,7 @@ re_rentals as (
 		ord.rental_month_start,
 		ord.sk_partner,
 		ord.sk_region,
-		sum(count(distinct ord.sk_contract)) over(partition by date_trunc('month',ord.rental_month_start), ord.sk_region) as re_rentals_monthly
+		sum(count(distinct ord.sk_contract)) over(partition by date_trunc('month',ord.rental_month_start), ord.sk_region, ord.sk_partner) as re_rentals_monthly
 	from ordered_rentals ord
 	where ord.row_number_renting > 1 and ord.status in ('Ativo', 'Finalizado') -- consider only contracts that are active or were active at a given period
 	group by 1, 2, 3
@@ -124,7 +124,7 @@ select
 	date_trunc('month',dc.dt_annulment) as month_date,
 	fhl.sk_partner,
 	fhl.sk_region,
-	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',dc.dt_annulment), fhl.sk_region) as ended_rentals_monthly
+	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',dc.dt_annulment), fhl.sk_region, fhl.sk_partner) as ended_rentals_monthly
 from dim_contract dc
 left join fact_house_listings fhl
   on fhl.sk_contract = dc.sk_contract
@@ -156,7 +156,7 @@ select
 	date_trunc('month',coalesce(dc.ts_analyst_annulment_input,dc.dt_annulment)) as month_date,
 	fhl.sk_partner,
 	fhl.sk_region,
-	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.ts_analyst_annulment_input,dc.dt_annulment)), fhl.sk_region) as ended_rentals_confirmed_monthly
+	sum(count(distinct dc.sk_contract)) over(partition by date_trunc('month',coalesce(dc.ts_analyst_annulment_input,dc.dt_annulment)), fhl.sk_region, fhl.sk_partner) as ended_rentals_confirmed_monthly
 from dim_contract dc
 left join fact_house_listings fhl
   on fhl.sk_contract = dc.sk_contract
@@ -273,7 +273,7 @@ full outer join new_rentals nrent
 full outer join new_contracts ncont
   on ocont.month_start = ncont.contract_month_start and ocont.sk_region = ncont.sk_region and ocont.sk_partner = ncont.sk_partner
 full outer join re_rentals rr
-  on rr.rental_month_start = ocont.month_start and rr.sk_region = ocont.sk_region and rr.sk_partner = orent.sk_partner
+  on rr.rental_month_start = ocont.month_start and rr.sk_region = ocont.sk_region and rr.sk_partner = ocont.sk_partner
 full outer join new_first_listings nfl
   on ocont.month_start = nfl.first_listing_month_start and ocont.sk_region = nfl.sk_region and ocont.sk_partner = nfl.sk_partner
 full outer join ended_rentals er
