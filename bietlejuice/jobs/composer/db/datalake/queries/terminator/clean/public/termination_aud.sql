5120fffed9bfe0951b289cab02133b2c3001778e
Cássio Bastos <53241140+cassiolino@users.noreply.github.com>
2020-03-25 19:33:07 -0300

    DS-970 (#2591)
    * Add four missing columns from termination table to datalake clean
    
    * Adding file termination_aud.sql to terminator on Datalake in order to check for modifiations on termination dates.
    
    * Adding file termination_aud.sql to terminator on Datalake in order to check for modifiations on termination dates.

---
+++ bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/public/termination_aud.sql
@@ -0,0 +1,16 @@
+SELECT
+    id,
+    rev,
+    revtype AS rev_type,
+    revend AS rev_end,
+    contract_id AS id_contract,
+    reason,
+    date AS dt_termination,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    status,
+    canceled_at as ts_canceled,
+    vacancy_date as dt_vacancy,
+    source
+FROM
+    datalake_terminator_raw.termination_aud
