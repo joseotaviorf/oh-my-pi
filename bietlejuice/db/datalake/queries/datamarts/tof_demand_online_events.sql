621f77d272af9eb5cef4d0eea269a2836c3cc199
Giuseppe Giovanelli <50770057+giuseppecg@users.noreply.github.com>
2020-03-30 15:06:55 -0300

    [DREG-257] Improving columns names (#2599)
    * escaping {}
    
    * improving columns names
    
    * changing listing_page__view column

--- bietlejuice/db/datalake/queries/datamarts/tof_demand_online_events.sql
+++ bietlejuice/db/datalake/queries/datamarts/tof_demand_online_events.sql
@@ -46,9 +46,9 @@ select
 	lvl.sk_house_listing,
 	ha.agent_id,
 	case when trim(event_type) = 'listing_page_viewed' then 1 else 0 end as listing_page_viewed,
-	case when trim(event_type) = 'pilot_cw_button_clicked' then 1 else 0 end as button_clicked,
-	case when trim(event_type) = 'piloto_cw_dialog_viewed' then 1 else 0 end as dialog_viewed, 
-	case when trim(event_type) = 'piloto_cw_message_sent' then 1 else 0 end as message_sent,
+	case when trim(event_type) = 'pilot_cw_button_clicked' then 1 else 0 end as talk_to_agent_button_clicked,
+	case when trim(event_type) = 'piloto_cw_dialog_viewed' then 1 else 0 end as talk_to_agent_dialog_viewed, 
+	case when trim(event_type) = 'piloto_cw_message_sent' then 1 else 0 end as talk_to_agent_message_sent,
     case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{{9}}))).*'),4) else null end as talk_to_agent_message_content
 from datalake_amplitude_clean_prod.events ev
 left join last_version_listings lvl
