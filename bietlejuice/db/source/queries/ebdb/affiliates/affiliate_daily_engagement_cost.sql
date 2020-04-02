52d1567c436bde852388133e336e9ddb8262f03c
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-04-02 14:16:34 -0300

    [DE-1019] Update MGM rule: removing affiliates that have been created for more than 6 months (#2619)
    * Reformat and indent file affiliate_daily_engagement_cost
    
    * Exclude from Member get Member costs the affiliates that were created more than six months: considering col inicioAtuacao

--- bietlejuice/db/source/queries/ebdb/affiliates/affiliate_daily_engagement_cost.sql
+++ bietlejuice/db/source/queries/ebdb/affiliates/affiliate_daily_engagement_cost.sql
@@ -7,7 +7,8 @@ select
 	da_aud.ativo as is_affiliate_active,
 	case when da_aud.affiliateType = 'Doorman' and u_aud.dadosagente_id is not null then 'Doorman & Agent'
 	     when u_aud.dadosagente_id is not null then 'Agent'
-		 else da_aud.affiliateType end as affiliate_type,
+       else da_aud.affiliateType
+  end as affiliate_type,
 	res.commission_type,
 	sum(res.value_brl) as value_brl
 from (
@@ -17,28 +18,38 @@ from (
             u.id as user_id,
             i.regiao_id as region_id,
             u.dadosAfiliado_id as dadosafiliado_id,
-            (select max(da_aud_int.REV)
-             from DadosAfiliado_AUD da_aud_int
-             join UsuarioRevisionEntity da_ure_int
-               on da_aud_int.REV = da_ure_int.id
-             where da_aud_int.id = u.dadosAfiliado_id
-               and from_unixtime(da_ure_int.timestamp/1000) <= occ.dataOperacao) as da_max_rev,
-            (select max(ua_aud_int.REV)
-             from Usuario_AUD ua_aud_int
-             join UsuarioRevisionEntity ua_ure_int
-               on ua_aud_int.REV = ua_ure_int.id
-             where ua_aud_int.id = u.id
-               and from_unixtime(ua_ure_int.timestamp/1000) <= occ.dataOperacao) as u_max_rev,
+            (
+              select max(da_aud_int.REV)
+              from DadosAfiliado_AUD da_aud_int
+              join UsuarioRevisionEntity da_ure_int
+                on da_aud_int.REV = da_ure_int.id
+              where da_aud_int.id = u.dadosAfiliado_id
+                and from_unixtime(da_ure_int.timestamp/1000) <= occ.dataOperacao
+            ) as da_max_rev,
+            (
+              select max(ua_aud_int.REV)
+              from Usuario_AUD ua_aud_int
+              join UsuarioRevisionEntity ua_ure_int
+                on ua_aud_int.REV = ua_ure_int.id
+              where ua_aud_int.id = u.id
+                and from_unixtime(ua_ure_int.timestamp/1000) <= occ.dataOperacao
+            ) as u_max_rev,
             sum(occ.valor) as value_brl
         from OperacaoContaCorrente occ
-        left join Usuario u on u.ContaCorrente_id = occ.ContaCorrente_id
-        join Imovel i on i.id = occ.imovel_id
+        left join Usuario u
+          on u.ContaCorrente_id = occ.ContaCorrente_id
+        join Imovel i
+          on i.id = occ.imovel_id
         where occ.tipo in ('valorFixoPorIndicacaoDeImovel', 'porcentagemPorIndicacaoDeImovel')
-        and occ.dataOperacao = date('{str_date}')
+          and occ.dataOperacao = date('{str_date}')
         group by 1,2,3,4,5,6,7
     ) res
-    join DadosAfiliado_AUD da_aud on da_aud.id = res.dadosAfiliado_id and da_aud.REV = res.da_max_rev
-    join Usuario_AUD u_aud on u_aud.id = res.user_id and u_aud.REV = res.u_max_rev
+    join DadosAfiliado_AUD da_aud
+      on da_aud.id = res.dadosAfiliado_id
+      and da_aud.REV = res.da_max_rev
+    join Usuario_AUD u_aud
+      on u_aud.id = res.user_id
+      and u_aud.REV = res.u_max_rev
     group by 1, 2, 3, 4, 5, 6, 7, 8
 union all
 select
@@ -50,53 +61,70 @@ select
 	da_aud.ativo as is_affiliate_active,
 	case when da_aud.affiliateType = 'Doorman' and u_aud.dadosagente_id is not null then 'Doorman & Agent'
 	     when u_aud.dadosagente_id is not null then 'Agent'
-		 else da_aud.affiliateType end as affiliate_type,
+       else da_aud.affiliateType
+   end as affiliate_type,
 	'comissaoSobreAfiliadoIndicado' as commission_type,
 	sum(res.value_brl * 0.1) as value_brl
 from (
 	select
-		resf.date_operation,
+    resf.date_operation,
 		uqi.id as "user_id",
 		resf.region_id,
 		uqi.dadosAfiliado_id as dadosafiliado_id,
-		(select max(da_aud_int.REV)
-		 from DadosAfiliado_AUD da_aud_int
-		 join UsuarioRevisionEntity da_ure_int
-		   on da_aud_int.REV = da_ure_int.id
-	     where da_aud_int.id = uqi.dadosAfiliado_id
-		   and from_unixtime(da_ure_int.timestamp/1000) <= resf.date_operation) as da_max_rev,
-	    (select max(ua_aud_int.REV)
-	     from Usuario_AUD ua_aud_int
-	     join UsuarioRevisionEntity ua_ure_int
-	       on ua_aud_int.REV = ua_ure_int.id
+		(
+      select max(da_aud_int.REV)
+		  from DadosAfiliado_AUD da_aud_int
+		  join UsuarioRevisionEntity da_ure_int
+        on da_aud_int.REV = da_ure_int.id
+      where da_aud_int.id = uqi.dadosAfiliado_id
+      and from_unixtime(da_ure_int.timestamp/1000) <= resf.date_operation
+    ) as da_max_rev,
+    (
+      select max(ua_aud_int.REV)
+      from Usuario_AUD ua_aud_int
+      join UsuarioRevisionEntity ua_ure_int
+        on ua_aud_int.REV = ua_ure_int.id
 		 where ua_aud_int.id = uqi.id
-		   and from_unixtime(ua_ure_int.timestamp/1000) <= resf.date_operation) as u_max_rev,
-		 sum(resf.value_brl) as value_brl
+     and from_unixtime(ua_ure_int.timestamp/1000) <= resf.date_operation
+    ) as u_max_rev,
+    sum(resf.value_brl) as value_brl
 	from (
-            select
-                occ.dataOperacao as date_operation,
-                u.id as user_id,
-                i.regiao_id as region_id,
-                u.dadosAfiliado_id as dadosafiliado_id,
-                u.dadosagente_id as dadosagente_id,
-                (select max(da_aud_int.REV)
-                 from DadosAfiliado_AUD da_aud_int
-                 join UsuarioRevisionEntity da_ure_int
-                   on da_aud_int.REV = da_ure_int.id
-                 where da_aud_int.id = u.dadosAfiliado_id
-                   and from_unixtime(da_ure_int.timestamp/1000) <= occ.dataOperacao) as da_max_rev,
-                sum(occ.valor) as value_brl
-            from OperacaoContaCorrente occ
-            left join Usuario u on u.ContaCorrente_id = occ.ContaCorrente_id
+          select
+            occ.dataOperacao as date_operation,
+            u.id as user_id,
+            i.regiao_id as region_id,
+            u.dadosAfiliado_id as dadosafiliado_id,
+            u.dadosagente_id as dadosagente_id,
+            (
+              select
+                max(da_aud_int.REV)
+              from DadosAfiliado_AUD da_aud_int
+              join UsuarioRevisionEntity da_ure_int
+                on da_aud_int.REV = da_ure_int.id
+             where da_aud_int.id = u.dadosAfiliado_id
+               and from_unixtime(da_ure_int.timestamp/1000) <= occ.dataOperacao
+            ) as da_max_rev,
+            sum(occ.valor) as value_brl
+          from OperacaoContaCorrente occ
+            left join Usuario u
+              on u.ContaCorrente_id = occ.ContaCorrente_id
             join Imovel i on i.id = occ.imovel_id
             where occ.tipo in ('valorFixoPorIndicacaoDeImovel', 'porcentagemPorIndicacaoDeImovel')
-            and occ.dataOperacao = date('{str_date}')
+              and occ.dataOperacao = date('{str_date}')
             group by 1,2,3,4,5,6
         ) resf
-        join DadosAfiliado_AUD da_aud on da_aud.id = resf.dadosAfiliado_id and da_aud.REV = resf.da_max_rev
-        join Usuario uqi on uqi.id = da_aud.indicadoPor_id
+        join DadosAfiliado_AUD da_aud
+          on da_aud.id = resf.dadosAfiliado_id
+          and da_aud.REV = resf.da_max_rev
+          and resf.date_operation <= DATE_ADD(da_aud.inicioAtuacao, INTERVAL 6 MONTH)
+        join Usuario uqi
+          on uqi.id = da_aud.indicadoPor_id
         group by 1,2,3,4,5,6
     ) res
-    join Usuario_AUD u_aud on u_aud.id = res.user_id and u_aud.REV = res.u_max_rev
-    join DadosAfiliado_AUD da_aud on da_aud.id = res.dadosafiliado_id and da_aud.REV = res.da_max_rev
+    join Usuario_AUD u_aud
+      on u_aud.id = res.user_id
+      and u_aud.REV = res.u_max_rev
+    join DadosAfiliado_AUD da_aud
+      on da_aud.id = res.dadosafiliado_id
+      and da_aud.REV = res.da_max_rev
     group by 1, 2, 3, 4, 5, 6, 7, 8
