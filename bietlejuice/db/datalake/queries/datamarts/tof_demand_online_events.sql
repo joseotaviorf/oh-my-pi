a880f8786cb8845d4259b3bf021819b6ed729695
Pedro Henrique Domingos <pedro.hndomingos@gmail.com>
2020-04-08 15:25:09 -0300

    [DREG-262] Add columns offer_submitted and utms (#2634)
    * Add offer_submitted and utms
    
    * Add column direct_offer and update where and order by
    
    * Change origin agent_id and reorder the columns

--- bietlejuice/db/datalake/queries/datamarts/tof_demand_online_events.sql
+++ bietlejuice/db/datalake/queries/datamarts/tof_demand_online_events.sql
@@ -7,34 +7,6 @@ select
 	cast(nullif(ts_listing_version_end,'') as timestamp) as ts_listing_version_end 
 from datalake_clean.ods_dim_house_listing 
 where cast(nullif(version,'') as bigint) > 0
-),
-houseagent as 
-(
-    with 
-    houseagent_aud as (
-                        select
-                            from_unixtime(cast(timestamp as bigint)/1000) as date_time,
-                            hou.*
-                        from datalake_ebdb_raw_prod.houseagent_aud hou
-                            join datalake_ebdb_raw_prod.usuariorevisionentity ure
-                                on hou.rev = ure.id
-    ),
-    houseagent_status as (
-                            select 
-                                ia.house_id as id_house,
-                                ia.date_time as started_timestamp,
-                                lead(date_time) over(partition by house_id order by rev) as ended_timestamp,
-                                agent_id
-                            from houseagent_aud ia
-    )
-        select 
-            cast(id_house as varchar) as id_house,
-            date_format(started_timestamp, '%Y%m%d') as sk_start_date,
-            date_format(ended_timestamp, '%Y%m%d') as sk_end_date,
-            started_timestamp as ts_allocation_start,
-            ended_timestamp as ts_allocation_end,
-            agent_id
-        from houseagent_status
 )
 select
     cast(date_trunc('week',date(ts_event)) as varchar) as event_week,
@@ -44,19 +16,20 @@ select
 	ev.id_amplitude,
 	trim(json_extract_scalar(event_properties, '$["house_id"]')) as house_id,
 	lvl.sk_house_listing,
-	ha.agent_id,
+	cast(json_extract_scalar(event_properties, '$.agent_id') as bigint) as agent_id,
 	case when trim(event_type) = 'listing_page_viewed' then 1 else 0 end as listing_page_viewed,
 	case when trim(event_type) = 'pilot_cw_button_clicked' then 1 else 0 end as talk_to_agent_button_clicked,
 	case when trim(event_type) = 'piloto_cw_dialog_viewed' then 1 else 0 end as talk_to_agent_dialog_viewed, 
 	case when trim(event_type) = 'piloto_cw_message_sent' then 1 else 0 end as talk_to_agent_message_sent,
-    case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{{9}}))).*'),4) else null end as talk_to_agent_message_content
+    case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{{9}}))).*'),4) else null end as talk_to_agent_message_content,
+   	case when trim(event_type) = 'offer_submitted' then 1 else 0 end as offer_submitted,
+    cast(json_extract_scalar(user_properties, '$.utm_source') as varchar) as utm_source,
+	cast(json_extract_scalar(user_properties, '$.utm_medium') as varchar) as utm_medium,
+	cast(json_extract_scalar(user_properties, '$.utm_campaign') as varchar) as utm_campaign,
+	cast(json_extract_scalar(user_properties, '$.utm_content') as varchar) as utm_content, 
+	cast(json_extract_scalar(user_properties, '$.utm_term') as varchar) as utm_term
 from datalake_amplitude_clean_prod.events ev
 left join last_version_listings lvl
   on trim(json_extract_scalar(ev.event_properties, '$["house_id"]'))  = lvl.id_house
   and ts_event between ts_listing_version_start and (coalesce(ts_listing_version_end,current_timestamp) - interval '1' second)
-left join houseagent ha
-  on trim(json_extract_scalar(ev.event_properties, '$["house_id"]'))  = ha.id_house
-  and ts_event between ha.ts_allocation_start and (coalesce(ha.ts_allocation_end,current_timestamp) - interval '1' second)
-  and trim(event_type) = 'piloto_cw_message_sent'
-where (event_type like '%cw%' or event_type = 'listing_page_viewed') and date(ts_event) >= date('2020-03-18')
-order by 1 desc, 2, 3, 4, 5, 6, 7, 8
+where date(ts_event) >= date('2020-03-18')
