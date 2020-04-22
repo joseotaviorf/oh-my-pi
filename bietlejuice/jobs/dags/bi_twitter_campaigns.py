05665ebb5b5558bccd841c7e29fa255c96bc2652
felipemiquelim <felipemiquelim@gmail.com>
2020-04-22 16:15:40 -0300

    Configuring new schedules to marketing-costs data flow (#2694)

--- bietlejuice/jobs/dags/bi_twitter_campaigns.py
+++ bietlejuice/jobs/dags/bi_twitter_campaigns.py
@@ -14,7 +14,7 @@ from bietlejuice.jobs.etl.marketing.marketing_enum import MarketingEnum
 
 BI_TWITTER_CAMPAIGNS_DAG_NAME = 'bi-twitter-campaigns'
 MAIN_START_DATE = datetime(2019, 7, 1, 0, 0, 0)
-MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('30 6,9 * * *')
+MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 5,8 * * *')
 
 env.set_airflow_var_to_local_env('BI_DW', 'DATA_ACC_AWS_ACCESS_KEY_ID', 'DATA_ACC_AWS_SECRET_ACCESS_KEY')
 S3_BUCKET = env.get_airflow_env_var('bi-datalake-s3-bucket')
