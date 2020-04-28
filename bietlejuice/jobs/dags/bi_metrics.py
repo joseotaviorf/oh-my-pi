33fe55627b809586ee3cd6ff424533767f215c8a
Bernardo Loureiro <bplmp@users.noreply.github.com>
2020-04-28 13:49:48 -0300

    [DATAOPS-108] Refactor bi-metrics DAG (#2718)
    * Create repo folder before tasks
    
    * Refactor bi-metrics DAG
    
    * Update bietlejuice/jobs/dags/bi_metrics.py
    
    Co-Authored-By: Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
    
    * Update bietlejuice/jobs/dags/bi_metrics.py
    
    Co-Authored-By: Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
    
    * Removing unused constants
    
    Co-authored-by: Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>

--- bietlejuice/jobs/dags/bi_metrics.py
+++ bietlejuice/jobs/dags/bi_metrics.py
@@ -1,7 +1,6 @@
 import os
 import boto3
 from datetime import datetime
-import shutil
 
 from airflow.models import DAG
 from qa_python_utils import QuintoAndarLogger
@@ -21,34 +20,27 @@ MAIN_START_DATE = datetime(2020, 4, 19)
 MAIN_SCHEDULE_INTERVAL = env.convert_to_utc_schedule('0 9 * * *')
 
 METRICS_SCHEMA = 'metrics'
-DATA_METRICS_REPO_NAME = 'data-metrics'
 DATA_METRICS_BUCKET_NAME = '5a-data-metrics'
-CLONE_TO_FOLDER = '/tmp'
-REPO_FOLDER = '{}/{}'.format(CLONE_TO_FOLDER, DATA_METRICS_REPO_NAME)
 
 logger = QuintoAndarLogger(MAIN_DAG_ID)
 
 
 # functions
 @logger
-def copy_data_metrics_repo_sqls(**kwargs):
-    logger.info('m=copy_data_metrics_repo_sqls, msg=listing s3 bucket contents and downloading SQL files')
+def read_data_metrics_repo_and_return_sqls():
+    logger.info('m=read_data_metrics_repo_and_return_sqls, msg=listing s3 bucket contents and downloading SQL files')
     s3_client = boto3.client('s3', aws_access_key_id=data_acc_aws_access_key_id, aws_secret_access_key=data_acc_aws_secret_access_key)
     objects = s3_client.list_objects_v2(Bucket=DATA_METRICS_BUCKET_NAME)['Contents']
-    if os.path.isdir(REPO_FOLDER):
-        # cleanup folder to remove old files
-        shutil.rmtree(REPO_FOLDER)
-    os.makedirs(REPO_FOLDER)
+    sqls = []
     for object in [x for x in objects if x['Key'].endswith('.sql')]:
-        file_name = os.path.split(object['Key'])[1]
-        # download sql files
-        s3_client.download_file(DATA_METRICS_BUCKET_NAME, object['Key'], '{}/{}'.format(REPO_FOLDER, file_name))
+        file_stem = os.path.split(object['Key'])[1].replace('.sql', '')
+        object_body = s3_client.get_object(Bucket=DATA_METRICS_BUCKET_NAME, Key=object['Key'])['Body'].read().decode('utf-8')
+        sqls.append({'table_name': file_stem, 'sql': object_body})
+    return sqls
 
 
 @logger
-def create_metric_from_dw(table_name, file_path, **kwargs):
-    query = BaseETL.get_query_from_file_name(file_path)
-
+def create_metric_from_dw(table_name, query, **kwargs):
     logger.info('m=create_metric_from_dw, table_name={}, msg=Dropping table'.format(table_name))
     BaseETL.execute_command(
         command='drop table if exists {}.{}'.format(METRICS_SCHEMA, table_name),
@@ -57,7 +49,7 @@ def create_metric_from_dw(table_name, file_path, **kwargs):
         commit=False
     )
 
-    logger.info('m=create_metric_from_dw, table_name={}, msg=Creating table'.format(table_name))
+    logger.info('m=create_metric_from_dw, table_name={}, query={}, msg=Creating table'.format(table_name, query))
     BaseETL.execute_command(
         command='create table {}.{} as ({})'.format(METRICS_SCHEMA, table_name, query.replace(';', '')),
         db_enum=EnumDB.BI_DW,
@@ -66,13 +58,13 @@ def create_metric_from_dw(table_name, file_path, **kwargs):
     )
 
 
-def create_task(dag, python_callable, db, file_path, table_name):
+def create_task(dag, python_callable, db, table_name, query):
     task = BaseDAG.build_python_operator(
         dag=dag,
         task_id='{}_{}'.format(db, table_name),
         provide_context=True,
         python_callable=python_callable,
-        op_kwargs={'table_name': table_name, 'file_path': file_path}
+        op_kwargs={'table_name': table_name, 'query': query}
     )
     return task
 
@@ -91,23 +83,14 @@ main_dag = DAG(
     catchup=False
 )
 
-copy_repo_sqls_task = BaseDAG.build_python_operator(
-    dag=main_dag,
-    task_id='copy_data_metrics_repo_sqls',
-    provide_context=True,
-    python_callable=copy_data_metrics_repo_sqls
-)
+sqls = read_data_metrics_repo_and_return_sqls()
 
 # create metrics tasks
-tasks = []
-for path in [p for p in os.listdir(REPO_FOLDER) if p.endswith('.sql')]:
-    task = create_task(
+for sql in sqls:
+    create_task(
         dag=main_dag,
         python_callable=create_metric_from_dw,
         db='dw',
-        file_path=path.as_posix(),
-        table_name=path.stem,
+        table_name=sql['table_name'],
+        query=sql['sql'],
     )
-    tasks.append(task)
-
-copy_repo_sqls_task.set_downstream(tasks)
