d02590b71a630fcbb1b1d0643d8c9fb4f71882ac
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2020-04-29 14:34:42 -0300

    [DE-1296] Teravoz - Create tasks and spark jobs for enriched tables (#2723)
    * adding enrich tasks
    
    * review suggestions
    
    * adding check for existing table

--- bietlejuice/jobs/composer/dags/bigfone_event/bigfone_event.py
+++ bietlejuice/jobs/composer/dags/bigfone_event/bigfone_event.py
@@ -10,10 +10,15 @@ from airflow.operators.quintoandar_databricks import (
 )
 import airflow.utils.helpers as airflow_helpers
 
-from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.bigfone_event import DAG_NAME
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, BaseSubDAG
+from bietlejuice.jobs.composer.dags.bigfone_event import (
+    DAG_NAME,
+    QUERIES_BIGFONE_EVENT_DATALAKE_PATH,
+)
 
 # dag params
+from bietlejuice.jobs.composer.services import FileService
+
 DAG_ID = "bietlejuice.{}".format(DAG_NAME)
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2019, 9, 9, 0, 0, 0, tzinfo=LOCAL_TZ)
@@ -53,6 +58,78 @@ dag = DAG(
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
 )
+
+
+def enrich_tasks(sub_dag_name, table_name, slugged_table_name):
+
+    sub_dag = BaseSubDAG(
+        sub_dag_name=sub_dag_name,
+        dag_name=DAG_ID,
+        schedule_interval=MAIN_SCHEDULE_INTERVAL,
+        start_date=MAIN_START_DATE,
+    )._build_local_dag()
+
+    load_enrich_table_task = QuintoAndarDatabricksSubmitRunOperator(
+        task_id=f"create-enrich-{slugged_table_name}-in-data-lake",
+        dag=sub_dag,
+        json={
+            "spark_python_task": {
+                "python_file": f"{SPARK_JOBS_PATH}/load_incremental_to_datalake_layer.py",
+                "parameters": [
+                    ENV,
+                    "{{ ds }}",
+                    DATALAKE_BUCKET,
+                    "clean",
+                    "enrich",
+                    table_name,
+                ],
+            }
+        },
+    )
+
+    create_external_table_task = QuintoAndarDatabricksSubmitRunOperator(
+        dag=sub_dag,
+        task_id=f"create-enrich-{slugged_table_name}-external-table",
+        json={
+            "spark_python_task": {
+                "python_file": f"{SPARK_JOBS_PATH}/create_incremental_external_table.py",
+                "parameters": [
+                    ENV,
+                    "{{ ds }}",
+                    DATALAKE_BUCKET,
+                    ATHENA_QUERY_RESULT_LOCATION,
+                    "enrich",
+                    table_name,
+                ],
+            }
+        },
+    )
+
+    load_enrich_table_task >> create_external_table_task
+
+    return sub_dag
+
+
+def build_subdags(layer):
+    file_list = FileService.list_files(f"{QUERIES_BIGFONE_EVENT_DATALAKE_PATH}/{layer}")
+    subdags = {}
+
+    for file_name in file_list:
+        file_name = FileService.remove_file_extension(file_name)
+        slugged_table_name = file_name.replace("_", "-")
+        table_sub_dag = BaseSubDAG.get_sub_dag_operator(
+            dag=dag,
+            sub_dag_name=f"load-{slugged_table_name}-to-{layer}",
+            sub_dag_func=eval(f"{layer}_tasks"),
+            table_name=file_name,
+            slugged_table_name=slugged_table_name,
+        )
+        subdags[file_name] = table_sub_dag
+
+    # subdags is a dict where the keys are table or file names
+    # and the values are corresponding subdag objects
+    return subdags
+
 
 # ------------------------------ TASKS ------------------------------------------------ #
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
@@ -110,10 +187,13 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
+enrich_sub_dags = build_subdags("enrich")
+
 airflow_helpers.chain(
     create_cluster_task,
     load_event_table_to_raw_task,
     load_event_table_to_clean_task,
     create_clean_partition_task,
-    terminate_cluster_task,
 )
+
+create_clean_partition_task >> list(enrich_sub_dags.values()) >> terminate_cluster_task
