c2384bcdd2eed66798c091cf892e9171290064ee
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-05-04 15:12:22 -0300

    [DS-1039] Add ts_created, ts_sent and due_amount to invoice model (#2740)
    * [DS-1039] Add ts_created, ts_sent and due_amount to invoice model
    
    * Update bietlejuice/jobs/composer/db/dw/ddl/payment/dim_invoice.ddl
    
    Co-authored-by: Jéssika Darambaris <jessika.darambaris@gmail.com>
    
    Co-authored-by: Jéssika Darambaris <jessika.darambaris@gmail.com>

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
@@ -2,6 +2,9 @@ select
     id_external as sk_invoice,
     replace(purpose, '-', ' ') as invoice_frequency,
     coalesce(replace(status, '-', ' '), 'not invoiceable') as payment_status,
+    due_amount as invoice_due_amount,
+    ts_created as ts_invoice_created,
+    date(ts_sent) as dt_invoice_sent,
     date(ts_due) as dt_invoice_due,
     date(ts_paid) as dt_invoice_paid,
     now() as ts_load
--- bietlejuice/jobs/composer/db/dw/ddl/payment/dim_invoice.ddl
+++ bietlejuice/jobs/composer/db/dw/ddl/payment/dim_invoice.ddl
@@ -3,6 +3,9 @@ create table if not exists payment.dim_invoice (
     sk_invoice bigint primary key,
     invoice_frequency varchar(50),
     payment_status varchar(50),
+    invoice_due_amount decimal(16,3),
+    ts_invoice_created timestamp,
+    dt_invoice_sent datetime,
     dt_invoice_due datetime,
     dt_invoice_paid datetime,
     ts_load timestamp
