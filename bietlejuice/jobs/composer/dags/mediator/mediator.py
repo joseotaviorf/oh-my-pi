ab70662233ffa8fb0e1e2212d64d8294480b94a9
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-05-05 16:13:07 -0300

    [DE-1257] Add a skip list to Mediator (#2753)
    * Upd doc and id
    
    * Read MEADIATOR_SKIP_LIST and send to sensor for these DAGs skipping

--- bietlejuice/jobs/composer/dags/mediator/mediator.py
+++ bietlejuice/jobs/composer/dags/mediator/mediator.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, date
 
-from airflow.models import DAG
+from airflow.models import DAG, Variable
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from airflow.operators.quintoandar_dag_mediator import (
     QuintoAndarShortCircuitExternalSensor,
@@ -19,14 +19,32 @@ def validate_dependencies(dependencies_list):
         )
 
 
-dependencies_file_path = COMPOSER_DAGS_PATH + "/dependencies.yaml"
-dependencies_dict = FileService.get_dict_from_yaml_file(dependencies_file_path)
+def extract_dependencies():
+    dependencies_file_path = COMPOSER_DAGS_PATH + "/dependencies.yaml"
+    dependencies_dict = FileService.get_dict_from_yaml_file(dependencies_file_path)
 
-validate_dependencies(dependencies_dict)
+    validate_dependencies(dependencies_dict)
+
+    return dependencies_dict
+
+
+def extract_skip_list():
+    mediator_skip_list = Variable.get(
+        "MEDIATOR_SKIP_LIST", deserialize_json=True, default_var={}
+    )
+    force_skip_list = []
+    if "dags" in mediator_skip_list and mediator_skip_list["dags"]:
+        for dag_id, skip_date in mediator_skip_list["dags"].items():
+            skip_date = datetime.strptime(skip_date, "%Y-%m-%d").date()
+            if date.today() == skip_date:
+                force_skip_list.append(dag_id)
+
+    return force_skip_list
+
 
 # Define tasks
 mediator_dag = DAG(
-    dag_id="bietlejuice.mediator",
+    dag_id="airflow.mediator",
     default_args={
         "owner": BaseDAG.DEFAULT_OWNER,
         "wait_for_downstream": False,
@@ -39,16 +57,20 @@ mediator_dag.doc_md = """
 #### Mediator DAG
 This DAG checks for dependencies completion and triggers the dependents DAG in
 the pipeline.
+To force some temporary DAG skipping, use this [variable](/admin/variable/?flt1_0=MEDIATOR_SKIP_LIST)
 
 Docs:
-- [short-circuit](https://github.com/quintoandar/airflow-plugins/blob/master/quintoandar_airflow_plugins/dag_mediator_plugin.md)
+
+- [short-circuit](https://github.com/quintoandar/airflow-plugins/blob/master/quintoandar_airflow_plugins/dag_mediator_plugin.md) sensor references
 - Mediator [directory](https://drive.google.com/drive/folders/1fIJBKVw4Jjojb9eLu8AHlzmFHBowRRGu) with docs and references
 """
 
+dependencies_dict = extract_dependencies()
 sensor_task = QuintoAndarShortCircuitExternalSensor(
     dag=mediator_dag,
     task_id="short-circuit",
     dependencies=dependencies_dict,
+    skip_list=extract_skip_list(),
     allow_rerun=False,
     retries=0,
 )
