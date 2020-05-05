2bef9dc593f60ecd0e2bba38222df7f71c9fc3c0
Ribaldo <ribaldorafael@gmail.com>
2020-05-05 08:55:39 -0300

    Converting date to string in dim_invoice

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
@@ -4,8 +4,8 @@ select
     coalesce(replace(status, '-', ' '), 'not invoiceable') as payment_status,
     due_amount as invoice_due_amount,
     ts_created as ts_invoice_created,
-    date(ts_sent) as dt_invoice_sent,
-    date(ts_due) as dt_invoice_due,
-    date(ts_paid) as dt_invoice_paid,
+    date_format(ts_sent, 'yyyy-MM-dd') as dt_invoice_sent,
+    date_format(ts_due, 'yyyy-MM-dd') as dt_invoice_due,
+    date_format(ts_paid, 'yyyy-MM-dd') as dt_invoice_paid,
     now() as ts_load
 from datalake_retsuko_clean.invoice
