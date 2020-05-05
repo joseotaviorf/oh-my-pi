57214f608a715d9cf72690998550c1f6053d51a1
Ribaldo <ribaldorafael@gmail.com>
2020-05-05 10:10:44 -0300

    Converting decimal to float in invoice_due_amount

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/dw/dim_invoice.sql
@@ -2,7 +2,7 @@ select
     id_external as sk_invoice,
     replace(purpose, '-', ' ') as invoice_frequency,
     coalesce(replace(status, '-', ' '), 'not invoiceable') as payment_status,
-    due_amount as invoice_due_amount,
+    cast(due_amount as float) as invoice_due_amount,
     ts_created as ts_invoice_created,
     date_format(ts_sent, 'yyyy-MM-dd') as dt_invoice_sent,
     date_format(ts_due, 'yyyy-MM-dd') as dt_invoice_due,
