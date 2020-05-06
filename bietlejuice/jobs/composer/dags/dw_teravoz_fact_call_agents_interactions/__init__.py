d6f0c0b58a4d0f81c4de229e87374c4552547498
Luc Somers <lucwsomers@gmail.com>
2020-05-06 11:14:32 -0300

    [DE-1368] - Materialize fact_call_agents_interactions (#2754)
    * fact call agents interactions
    
    * materialize
    
    * fix optional arg
    
    * remove str type
    
    * fix required arg
    
    * revert changes
    
    * add ddl, fix name
    
    * fix sql query
    
    * add ts_load
    
    * fix ddl

---
+++ bietlejuice/jobs/composer/dags/dw_teravoz_fact_call_agents_interactions/__init__.py
@@ -0,0 +1,5 @@
+from bietlejuice.jobs.composer.base.db import QUERIES_DATALAKE_PATH
+
+SOURCE = "dw_teravoz_fact_call_agents_interactions"
+DW_SCHEMA = "teravoz"
+QUERIES_DW_TERAVOZ_FACT_CALL_AGENTS_INTERACTIONS_PATH = QUERIES_DATALAKE_PATH + SOURCE
