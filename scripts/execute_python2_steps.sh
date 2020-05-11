e1926a3be115579bea52c5d9e30065be17a4bbd7
Rafael Ribaldo <ribaldorafael@gmail.com>
2020-05-11 09:43:51 -0300

    [DE-1401] Skipping python[2|3] drone steps for python[3|2] files (#2775)
    * Adding execute_python scripts
    
    * Reformating drone file
    
    * Fixing chmod command for python3
    
    * Adding marketing costs rules to file-validation step

---
+++ scripts/execute_python2_steps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CHANGED_PATHS=$(git diff-tree --no-commit-id --name-only -r HEAD..HEAD~1 | grep 'bietlejuice/db\|bietlejuice/jobs/base\|bietlejuice/jobs/dags\|bietlejuice/jobs/etl\|tests\|requirements\|setup')
+if [ ${#CHANGED_PATHS} -eq 0 ]; then
+  echo "===== No changes in Airflow EC2 files! Skipping! ===== "
+  exit 0
+else
+  echo "Changes in Airflow EC2 files detected! Executing step..."
+  $1 # command 1
+  $2 # command 2
+  $3 # command 3
+fi
---
+++ scripts/execute_python3_steps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# bietlejuice/db/dw/queries/marketing/costs/ and bietlejuice/jobs/dags/marketing/costs/sharing_rules.yml
+# temporary added until python2->3 full migration
+CHANGED_PATHS=$(git diff-tree --no-commit-id --name-only -r HEAD..HEAD~1 | grep 'bietlejuice/jobs/composer\|tests3\|requirements3\|setup3\|bietlejuice/db/dw/queries/marketing/costs/\|bietlejuice/jobs/dags/marketing/costs/sharing_rules.yml')
+if [ ${#CHANGED_PATHS} -eq 0 ]; then
+  echo "===== No changes in Composer files! Skipping! ===== "
+  exit 0
+else
+  echo "Changes in Composer files detected! Executing step..."
+  $1 # command 1
+  $2 # command 2
+fi
