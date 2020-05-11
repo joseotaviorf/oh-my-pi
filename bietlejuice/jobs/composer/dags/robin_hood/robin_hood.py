560501d8468c87582a44b01ab53a69f2e762b874
Isadora Gallerani <isadoragallerani@hotmail.com>
2020-05-11 14:12:54 -0300

    Moving cluster from minimum resource to r5.large (#2790)

--- bietlejuice/jobs/composer/dags/robin_hood/robin_hood.py
+++ bietlejuice/jobs/composer/dags/robin_hood/robin_hood.py
@@ -23,7 +23,7 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_minimum_resources_cluster", deserialize_json=True
+    "databricks_bietlejuice_robin_hood", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 LIBRARIES_DESCRIPTION = Variable.get(
