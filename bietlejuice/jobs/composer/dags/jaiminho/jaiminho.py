f7c4ab1eccc0e4e8b582ddfc520cc88f8f5f477d
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-05-12 10:33:47 -0300

    Moving cluster to minimum resources to r5.large on Jaiminho (#2799)
    * Revert "Change to minimum resources cluster (#2794)"
    
    This reverts commit b3351d60d40db4bc8c0be236e9d9fad32bc62977.
    
    * Use databricks_bietlejuice_jaiminho

--- bietlejuice/jobs/composer/dags/jaiminho/jaiminho.py
+++ bietlejuice/jobs/composer/dags/jaiminho/jaiminho.py
@@ -31,7 +31,7 @@ SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/"
 # cluster setup
 LOGS_OUTPUT_PATH = f"s3://{S3_BUCKET}/logs/jobs/{SOURCE}"
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_minimum_resources_cluster", deserialize_json=True
+    "databricks_bietlejuice_jaiminho", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 LIBRARIES_DESCRIPTION = Variable.get(
