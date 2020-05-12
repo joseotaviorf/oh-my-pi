390053460b5771ee586586fb8e471e829664c58b
Luc Somers <lucwsomers@gmail.com>
2020-05-12 08:31:23 -0300

    [DE-1265] - Change cluster to rlarge Chat Fup (#2796)
    * change  cluster
    
    * lint
    
    * fix cluster
    
    * remove code

--- bietlejuice/jobs/composer/dags/chat_fup/chat_fup.py
+++ bietlejuice/jobs/composer/dags/chat_fup/chat_fup.py
@@ -22,7 +22,7 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_minimum_resources_cluster", deserialize_json=True
+    "databricks_bietlejuice_chat_fup", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 LIBRARIES_DESCRIPTION = Variable.get(
