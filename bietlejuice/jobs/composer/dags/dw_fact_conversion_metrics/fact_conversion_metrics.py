293ff36513c072aff47f2811e3cc3a4cbdd5d5da
Luc Somers <lucwsomers@gmail.com>
2020-05-28 11:32:21 -0300

    [DE-1510] - Optimize Fact Conversion Metrics Dag (#2887)
    * add custom cluster
    
    * lint

--- bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/fact_conversion_metrics.py
+++ bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/fact_conversion_metrics.py
@@ -33,7 +33,9 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), SOURCE
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_conversion_metrics_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 LIBRARIES_DESCRIPTION = Variable.get(
