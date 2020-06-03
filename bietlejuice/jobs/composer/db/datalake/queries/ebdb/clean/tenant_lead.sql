7bf62328587991b40cf2f2f425302bb1077e581e
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2020-06-03 12:19:32 -0300

    Removing clean queries from ebdb tables that no longer exist (#2915)

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb/clean/tenant_lead.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    atualizadoEm as ts_updated,
-    criadoEm as ts_created,
-    email,
-    nome as name,
-    telefonePrincipal as main_phone
-FROM
-    datalake_ebdb_raw.tenantlead
