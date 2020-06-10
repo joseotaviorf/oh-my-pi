03e448ded3206d6bb5b009be8bf1892efa5b6edc
kenjihiraoka <31676903+kenjihiraoka@users.noreply.github.com>
2020-06-10 08:10:05 -0300

    remove miseed column and add new column (#2933)

--- bietlejuice/jobs/composer/db/datalake/queries/robin_hood/clean/accounting_entry.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/robin_hood/clean/accounting_entry.sql
@@ -6,10 +6,10 @@ select
     accrual_year_month,
     source_bill_item,
     payee_id as id_payee,
-    payment_request_id as id_payment_request,
     due_amount,
     timestamp(created_at) as ts_created,
     locale,
-    cost_center_code
+    cost_center_code,
+    date(occurrence_date) as dt_occurrence
 from
     datalake_robin_hood_raw.accounting_entry
