053126f4b9ad92d488ac8fba1275e75b088e4fdd
Larissa Tsuda <la_nimrod@hotmail.com>
2020-06-15 10:45:09 -0300

    [DRFS-45] Add new columns to credit_card_payment table in datalake_retsuko_clean_prod (#2941)
    * Adding new columns to existing table
    
    * fixing column name and alias
    
    * Revert "fixing column name and alias"
    
    This reverts commit 51d456032aace2fec77515f16e547fdf6f94c7e3.
    
    * fixing column name and alias

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/credit_card_payment.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/credit_card_payment.sql
@@ -9,5 +9,7 @@ select
     charged_amount,
     installments,
     status,
-    timestamp(created_at) as ts_created
+    timestamp(created_at) as ts_created,
+    acquirer_transaction_id as id_acquirer_transaction,
+    acquirer_nsu
 from datalake_retsuko_raw.credit_card_payment
