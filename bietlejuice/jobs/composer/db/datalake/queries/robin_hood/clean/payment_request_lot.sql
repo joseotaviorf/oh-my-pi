8c63e7542dac55973c76bdd98d8084fca674c7b1
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2020-06-24 11:29:29 -0300

    Removing dt_due column from query (#2987)

--- bietlejuice/jobs/composer/db/datalake/queries/robin_hood/clean/payment_request_lot.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/robin_hood/clean/payment_request_lot.sql
@@ -5,7 +5,6 @@ select
     name,
     payment_method,
     send_date as dt_sent,
-    date(due_date) as dt_due,
     timestamp(sent_at) as ts_sent,
     timestamp(created_at) as ts_created,
     timestamp(canceled_at) as ts_canceled
