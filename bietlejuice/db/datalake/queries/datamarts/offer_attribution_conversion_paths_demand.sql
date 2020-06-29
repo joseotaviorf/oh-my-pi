930d09838960006f6cb83f152a11abf06e035a6e
Luc Somers <lucwsomers@gmail.com>
2020-06-29 16:26:03 -0300

    Revert "[DEMKT-21] - Update Touchpoints for offer_attribution_conversion_paths_demand query (#2996)" (#3004)
    This reverts commit 1390edae75ca5897737cb0d1f90d24a509dea7ec.

--- bietlejuice/db/datalake/queries/datamarts/offer_attribution_conversion_paths_demand.sql
+++ bietlejuice/db/datalake/queries/datamarts/offer_attribution_conversion_paths_demand.sql
@@ -1,5 +1,98 @@
-with offer_submitted_conversions as (
-  select distinct
+with listing_page_viewed as (
+  select
+    min(ts_event) as ts_event,
+    -- Using colaesce to identify merged users first
+    coalesce(merged_amplitude_id,id_amplitude) as amplitude_id,
+    id_session,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+  from datalake_amplitude_clean_prod."170698_listing_page_viewed_events" damcs
+  -- Joining merge users table to identify cross-device conversions
+  left join datalake_raw.amplitude_merge_users_170698 amu
+    on damcs.id_amplitude = amu.amplitude_id
+  where year >= 2019 and platform = 'Web'
+  group by 2,3,4,5,6,7,8
+),
+home_page_viewed as (
+  select
+    min(ts_event) as ts_event,
+    -- Using colaesce to identify merged users first
+    coalesce(merged_amplitude_id,id_amplitude) as amplitude_id,
+    id_session,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+  from datalake_amplitude_clean_prod."170698_home_page_viewed_events" damcs
+  -- Joining merge users table to identify cross-device conversions
+  left join datalake_raw.amplitude_merge_users_170698 amu
+    on damcs.id_amplitude = amu.amplitude_id
+  where year >= 2019 and platform = 'Web'
+  group by 2,3,4,5,6,7,8
+),
+search_results_page_viewed as (
+  select
+    min(ts_event) as ts_event,
+    -- Using colaesce to identify merged users first
+    coalesce(merged_amplitude_id,id_amplitude) as amplitude_id,
+    id_session,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+  from datalake_amplitude_clean_prod."170698_search_results_page_viewed_events" damcs
+  -- Joining merge users table to identify cross-device conversions
+  left join datalake_raw.amplitude_merge_users_170698 amu
+    on damcs.id_amplitude = amu.amplitude_id
+  where year >= 2019 and platform = 'Web'
+  group by 2,3,4,5,6,7,8
+),
+schedule_page_viewed as (
+  select
+    min(ts_event) as ts_event,
+    -- Using colaesce to identify merged users first
+    coalesce(merged_amplitude_id,id_amplitude) as amplitude_id,
+    id_session,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term
+  from datalake_amplitude_clean_prod."170698_schedule_page_viewed_events" damcs
+  -- Joining merge users table to identify cross-device conversions
+  left join datalake_raw.amplitude_merge_users_170698 amu
+    on damcs.id_amplitude = amu.amplitude_id
+  where year >= 2019 and platform = 'Web'
+  group by 2,3,4,5,6,7,8
+),
+offer_submitted_aux as (
+  select
+    min(ts_event) as ts_event,
+    -- Using colaesce to identify merged users first
+    coalesce(merged_amplitude_id,id_amplitude) amplitude_id,
+    id_session,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_source=', 2), '&', 1), 'direct') as utm_source,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_medium=', 2), '&', 1), 'direct') as utm_medium,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_campaign=', 2), '&', 1), 'direct') as utm_campaign,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_content=', 2), '&', 1), 'direct') as utm_content,
+    coalesce(split_part(split_part(json_extract_scalar(user_properties, '$.entrance_uri'), 'utm_term=', 2), '&', 1), 'direct') as utm_term,
+    json_extract_scalar(event_properties, '$.visit_code') as visit_code,
+    json_extract_scalar(event_properties, '$.house_id') as house_id
+  from datalake_amplitude_clean_prod.events damcs
+  -- Joining merge users table to identify cross-device conversions
+  left join datalake_raw.amplitude_merge_users_170698 amu
+    on damcs.id_amplitude = amu.amplitude_id
+  where year >= 2019 and platform = 'Web'
+    and id_app = 170698 and event_type = 'offer_submitted'
+  group by 2,3,4,5,6,7,8,9,10
+),
+offer_submitted as (
+  select
     osa.ts_event,
     osa.amplitude_id,
     osa.id_session,
@@ -9,47 +102,40 @@ with offer_submitted_conversions as (
     osa.utm_content,
     osa.utm_term,
     osa.visit_code,
-    dcodr.city_group as city
-  from datamarts.offer_submitted_aux osa
+    dcodr.city_name as city
+  from offer_submitted_aux osa
   left join datalake_clean.ods_fact_house_listings clofhl
     on cast(osa.house_id as integer) = cast(clofhl.sk_house_listing as BIGINT)/1000
-  left join datalake_clean.ods_dim_region dcodr
+  left join datalake_clean.ods_dim_region dcodr 
     on cast(clofhl.sk_region as integer) = cast(dcodr.sk_region as integer)
-  where ts_event >= current_date - interval '6' month
-  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-),
-offer_submitted_sessions as (
-  select distinct
-    min(osc.ts_event) as ts_event,
-    osc.amplitude_id,
-    osc.id_session,
-    osc.utm_source,
-    osc.utm_medium,
-    osc.utm_campaign,
-    osc.utm_content,
-    osc.utm_term
-  from offer_submitted_conversions osc
-  group by 2, 3, 4, 5, 6, 7, 8
 ),
 pre_events as (
   select
-    *
-  from datamarts.listing_page_viewed_events
+    *,
+    null as visit_code,
+    null as city
+  from listing_page_viewed
   union all
     select
-      *
-    from datamarts.home_page_viewed_events
-  union all
-    select
-      *
-    from datamarts.search_results_page_viewed_events
-  union all
-    select
-      *
-    from datamarts.schedule_page_viewed_events
-  union all
-    select *
-    from offer_submitted_sessions
+      *,
+      null as visit_code,
+      null as city
+    from home_page_viewed
+    union all
+      select
+        *,
+        null as visit_code,
+        null as city
+      from search_results_page_viewed
+      union all
+        select
+          *,
+          null as visit_code,
+          null as city
+        from schedule_page_viewed
+        union all
+          select *
+          from offer_submitted
 ),
 events_rank as (
   select
@@ -69,30 +155,15 @@ events as (
     and p.amplitude_id=r.amplitude_id
     and p.id_session=r.id_session
 ),
-conversion_sessions_unique as (
-  -- retrieve conversion sessions, indicating last conversion session
+conversions as (
+  -- retrieve conversion events per amplitude user
   select
-    oss.id_session,
-    oss.amplitude_id,
-    er.ts_event as ts_session,
-    nullif(lag(er.ts_event) over (partition by oss.amplitude_id order by er.ts_event), er.ts_event) as ts_last_session
-  from offer_submitted_sessions oss
-  join events_rank er on er.amplitude_id = oss.amplitude_id and er.id_session = oss.id_session
-  group by 1, 2, 3
-),
-conversion_events as (
-  -- retrieve conversion events together with their session info
-  select
-    osc.ts_event,
-    osc.id_session,
-    osc.amplitude_id,
-    osc.visit_code,
-    osc.city,
-    csu.ts_session,
-    csu.ts_last_session,
-    rank() over (partition by osc.amplitude_id order by osc.ts_event) as rnk_conversion
-  from offer_submitted_conversions osc
-  join conversion_sessions_unique csu on csu.amplitude_id = osc.amplitude_id and csu.id_session = osc.id_session  
+    evt.ts_event,
+    evt.amplitude_id,
+    -- Adding the city
+    evt.city,
+    rank() over (partition by amplitude_id order by ts_event) as rnk_conversion
+  from offer_submitted evt
 ),
 touchpoints as (
   -- aggregate events to attributed sessions (=touchpoints)
@@ -115,15 +186,13 @@ touchpoints as (
     evt.utm_term,
     evt.ts_event as session_start_time,
     conv.city,
-    conv.ts_event as conversion_time,
-    conv.rnk_conversion as nst_conversion
-  from conversion_events conv
-  left join events evt
-  	on evt.amplitude_id = conv.amplitude_id
-  	and evt.ts_event > coalesce(conv.ts_last_session, date('2000-01-01')) -- join events that happened between last conversion and current conversion
-    and evt.ts_event <= conv.ts_session
-  where date_diff('month', evt.ts_event, conv.ts_event) <= 6
-  group by 1,2,3,4,5,6,7,8,9,10,11,12,13
+    min(conv.ts_event) as conversion_time,
+    min(conv.rnk_conversion) as nst_conversion
+    from events evt
+  join conversions conv
+    -- Adapting to join by the coalesced id
+    on conv.amplitude_id = evt.amplitude_id and conv.ts_event >= evt.ts_event
+  group by 1,2,3,4,5,6,7,8,9,10,11
   order by 1,12,10
 )
 -- aggregate touchpoints to unique conversions with their path concatenated in one column
@@ -141,5 +210,6 @@ select
   cast(array_join(array_agg(utm_content), '; ') as varchar(20000)) as path_utm_content,
   cast(array_join(array_agg(utm_term), '; ') as varchar(20000)) as path_utm_term
 from touchpoints
+where conversion_time >= date('2019-11-01')
+  and date_diff('month', session_start_time, conversion_time) <= 6
 group by 1, 2,3
-order by 3
