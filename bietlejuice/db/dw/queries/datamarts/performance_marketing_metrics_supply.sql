ddf29120e4868e331a8817986f472ab98bf0d9cc
Henrique Akira Miamoto <56843327+akiramiamoto@users.noreply.github.com>
2020-06-29 11:36:23 -0300

    Adding tracking dimensions (#2999)

--- bietlejuice/db/dw/queries/datamarts/performance_marketing_metrics_supply.sql
+++ bietlejuice/db/dw/queries/datamarts/performance_marketing_metrics_supply.sql
@@ -1,224 +1,252 @@
 with
 leads as (
 	select
-		sk_lead_date as dt,
-		coalesce(dr.city_group, '') as city_group,
-		f.mkt_origin,
-		f.mkt_channel,
-		count(1) as daily_leads
+		nullif(f.sk_lead_date, -1) sk_date,
+		coalesce(dr.city_group, '') city_group,
+		coalesce(f.mkt_origin,'') mkt_origin,
+		coalesce(f.mkt_channel,'') mkt_channel,
+		coalesce(f.mkt_medium,'') mkt_medium,
+		coalesce(f.mkt_source,'') mkt_source,
+		coalesce(dl.utm_campaign,'') utm_campaign,
+		coalesce(dl.utm_content,'') utm_content,
+		coalesce(dl.utm_term,'') utm_term,
+		count(1) as count_lead
 	from
 		fact_house_listing_flows f
+		join dim_lead dl
+			on dl.sk_lead = f.sk_lead
 		join dim_region dr
 			on f.sk_region = dr.sk_region
 	where
-		f.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and f.sk_lead_date>=20191001
-	group by 1, 2, 3, 4
-	),
+		f.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
 prospects as (
 	select
-		sk_prospect_date as dt,
-		coalesce(dr.city_group, '') as city_group,
-		f.mkt_origin,
-		f.mkt_channel,
-		count(1) as daily_prospects
+		nullif(f.sk_prospect_date, -1) sk_date,
+		coalesce(dr.city_group, '') city_group,
+		coalesce(f.mkt_origin,'') mkt_origin,
+		coalesce(f.mkt_channel,'') mkt_channel,
+		coalesce(f.mkt_medium,'') mkt_medium,
+		coalesce(f.mkt_source,'') mkt_source,
+		coalesce(dl.utm_campaign,'') utm_campaign,
+		coalesce(dl.utm_content,'') utm_content,
+		coalesce(dl.utm_term,'') utm_term,
+		count(1) as count_prospect
 	from
 		fact_house_listing_flows f
+		join dim_lead dl
+			on dl.sk_lead = f.sk_lead
 		join dim_region dr
 			on f.sk_region = dr.sk_region
 	where
-		f.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and f.sk_lead_date>=20191001
-	group by 1, 2, 3, 4
-	),
+		f.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
 qualifieds as (
 	select
-		sk_qualified_date as dt,
-		coalesce(dr.city_group, '') as city_group,
-		f.mkt_origin,
-		f.mkt_channel,
-		count(1) as daily_qualifieds
-	  from
-	    fact_house_listing_flows f
-	    join dim_region dr
-	    	on f.sk_region = dr.sk_region
+		nullif(f.sk_qualified_date, -1) sk_date,
+		coalesce(dr.city_group, '') city_group,
+		coalesce(f.mkt_origin,'') mkt_origin,
+		coalesce(f.mkt_channel,'') mkt_channel,
+		coalesce(f.mkt_medium,'') mkt_medium,
+		coalesce(f.mkt_source,'') mkt_source,
+		coalesce(dl.utm_campaign,'') utm_campaign,
+		coalesce(dl.utm_content,'') utm_content,
+		coalesce(dl.utm_term,'') utm_term,
+		count(1) as count_qualified
+	from
+		fact_house_listing_flows f
+		join dim_lead dl
+			on dl.sk_lead = f.sk_lead
+		join dim_region dr
+			on f.sk_region = dr.sk_region
 	where
-		f.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and f.sk_lead_date>=20191001
-	  group by 1, 2, 3, 4
-	),
+		f.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
 opportunities as (
 	select
-		sk_opportunity_date as dt,
-		coalesce(dr.city_group, '') as city_group,
-		f.mkt_origin,
-		f.mkt_channel,
-		count(1) as daily_opportunities
+		nullif(f.sk_opportunity_date, -1) sk_date,
+		coalesce(dr.city_group, '') city_group,
+		coalesce(f.mkt_origin,'') mkt_origin,
+		coalesce(f.mkt_channel,'') mkt_channel,
+		coalesce(f.mkt_medium,'') mkt_medium,
+		coalesce(f.mkt_source,'') mkt_source,
+		coalesce(dl.utm_campaign,'') utm_campaign,
+		coalesce(dl.utm_content,'') utm_content,
+		coalesce(dl.utm_term,'') utm_term,
+		count(1) as count_opportunity
 	from
 		fact_house_listing_flows f
+		join dim_lead dl
+			on dl.sk_lead = f.sk_lead
 		join dim_region dr
 			on f.sk_region = dr.sk_region
 	where
-		f.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and f.sk_lead_date>=20191001
-	group by 1, 2, 3, 4
-	),
+		f.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
 listings as (
 	select
-		sk_first_listing_date as dt,
-		coalesce(dr.city_group, '') as city_group,
-		f.mkt_origin,
-		f.mkt_channel,
-		count(1) as daily_listings
+		nullif(f.sk_first_listing_date, -1) sk_date,
+		coalesce(dr.city_group, '') city_group,
+		coalesce(f.mkt_origin,'') mkt_origin,
+		coalesce(f.mkt_channel,'') mkt_channel,
+		coalesce(f.mkt_medium,'') mkt_medium,
+		coalesce(f.mkt_source,'') mkt_source,
+		coalesce(dl.utm_campaign,'') utm_campaign,
+		coalesce(dl.utm_content,'') utm_content,
+		coalesce(dl.utm_term,'') utm_term,
+		count(1) as count_first_listing
 	from
 		fact_house_listing_flows f
+		join dim_lead dl
+			on dl.sk_lead = f.sk_lead
 		join dim_region dr
 			on f.sk_region = dr.sk_region
 	where
-		f.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and f.sk_lead_date>=20191001
-	group by 1, 2, 3, 4
-	),
-temp_res as (
+		f.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
+results as ( --coincident funnel
 	select
-		coalesce(l.dt,p.dt,q.dt,o.dt,t.dt) as dt,
-		coalesce(l.city_group,p.city_group,q.city_group,o.city_group,t.city_group) as city_group,
-		coalesce(l.mkt_origin,p.mkt_origin,q.mkt_origin,o.mkt_origin,t.mkt_origin) as mkt_origin,
-		coalesce(l.mkt_channel,p.mkt_channel,q.mkt_channel,o.mkt_channel,t.mkt_channel) as mkt_channel,
-		coalesce(l.daily_leads,0) as daily_leads,
-		coalesce(p.daily_prospects,0) as daily_prospects,
-		coalesce(q.daily_qualifieds,0) as daily_qualifieds,
-		coalesce(o.daily_opportunities,0) as daily_opportunities,
-		coalesce(t.daily_listings,0) as daily_listings
+		date(coalesce(l.sk_date,p.sk_date,q.sk_date,o.sk_date,fl.sk_date)) as date,
+		coalesce(l.city_group,p.city_group,q.city_group,o.city_group,fl.city_group) as city_group,
+		coalesce(l.mkt_origin,p.mkt_origin,q.mkt_origin,o.mkt_origin,fl.mkt_origin) as mkt_origin,
+		coalesce(l.mkt_channel,p.mkt_channel,q.mkt_channel,o.mkt_channel,fl.mkt_channel) as mkt_channel,
+		coalesce(l.mkt_medium,p.mkt_medium,q.mkt_medium,o.mkt_medium,fl.mkt_medium) as mkt_medium,
+		coalesce(l.mkt_source,p.mkt_source,q.mkt_source,o.mkt_source,fl.mkt_source) as mkt_source,
+		coalesce(l.utm_campaign,p.utm_campaign,q.utm_campaign,o.utm_campaign,fl.utm_campaign) as utm_campaign,
+		coalesce(l.utm_content,p.utm_content,q.utm_content,o.utm_content,fl.utm_content) as utm_content,
+		coalesce(l.utm_term,p.utm_term,q.utm_term,o.utm_term,fl.utm_term) as utm_term,
+		sum(coalesce(l.count_lead,0)) as leads,
+		sum(coalesce(p.count_prospect,0)) as prospects,
+		sum(coalesce(q.count_qualified,0)) as qualifieds,
+		sum(coalesce(o.count_opportunity,0)) as opportunities,
+		sum(coalesce(fl.count_first_listing,0)) as first_listings
 	from leads l
-	full outer join opportunities o
-		on  l.dt = o.dt
-		and l.city_group = o.city_group
-		and l.mkt_channel = o.mkt_channel
-		and l.mkt_origin = o.mkt_origin
-	full outer join qualifieds q
-		on  l.dt = q.dt
-		and l.city_group = q.city_group
-		and l.mkt_channel = q.mkt_channel
-		and l.mkt_origin = q.mkt_origin
 	full outer join prospects p
-		on  l.dt = p.dt
-		and l.city_group = p.city_group
-		and l.mkt_channel = p.mkt_channel
-		and l.mkt_origin = p.mkt_origin
-	full outer join listings t
-		on  l.dt = t.dt
-		and l.city_group = t.city_group
-		and l.mkt_channel = t.mkt_channel
-		and l.mkt_origin = t.mkt_origin
-	),
-dim_dt as (
-	select distinct
-		sk_date as dt
+		on  l.sk_date      = p.sk_date
+		and l.city_group   = p.city_group
+		and l.mkt_origin   = p.mkt_origin
+		and l.mkt_channel  = p.mkt_channel
+		and l.mkt_medium   = p.mkt_medium
+		and l.mkt_source   = p.mkt_source
+		and l.utm_campaign = p.utm_campaign
+		and l.utm_content  = p.utm_content
+		and l.utm_term     = p.utm_term
+	full outer join qualifieds q
+		on  l.sk_date      = q.sk_date
+		and l.city_group   = q.city_group
+		and l.mkt_origin   = q.mkt_origin
+		and l.mkt_channel  = q.mkt_channel
+		and l.mkt_medium   = q.mkt_medium
+		and l.mkt_source   = q.mkt_source
+		and l.utm_campaign = q.utm_campaign
+		and l.utm_content  = q.utm_content
+		and l.utm_term     = q.utm_term
+	full outer join opportunities o
+		on  l.sk_date      = o.sk_date
+		and l.city_group   = o.city_group
+		and l.mkt_origin   = o.mkt_origin
+		and l.mkt_channel  = o.mkt_channel
+		and l.mkt_medium   = o.mkt_medium
+		and l.mkt_source   = o.mkt_source
+		and l.utm_campaign = o.utm_campaign
+		and l.utm_content  = o.utm_content
+		and l.utm_term     = o.utm_term
+	full outer join listings fl
+		on  l.sk_date      = fl.sk_date
+		and l.city_group   = fl.city_group
+		and l.mkt_origin   = fl.mkt_origin
+		and l.mkt_channel  = fl.mkt_channel
+		and l.mkt_medium   = fl.mkt_medium
+		and l.mkt_source   = fl.mkt_source
+		and l.utm_campaign = fl.utm_campaign
+		and l.utm_content  = fl.utm_content
+		and l.utm_term     = fl.utm_term
+	group by 1,2,3,4,5,6,7,8,9
+),
+costs as (
+	select
+		date(nullif(mkt.sk_date, -1)) date,
+		coalesce(mkt.city_group, '') city_group,
+		coalesce(mkt.mkt_origin,'') mkt_origin,
+		coalesce(mkt.mkt_channel,'') mkt_channel,
+		coalesce(mkt.mkt_medium,'') mkt_medium,
+		coalesce(mkt.mkt_source,'') mkt_source,
+		coalesce(mkt.utm_campaign,'') utm_campaign,
+		coalesce(mkt.utm_content,'') utm_content,
+		coalesce(mkt.utm_term,'') utm_term,
+		sum(coalesce(mkt.cost,0)) as cost
 	from
-		dim_date
+		marketing.fact_marketing_daily_costs mkt
 	where
-		date between date('2019-10-01') and current_date
-	),
-dim_city_group as (
-	select distinct
-		coalesce(city_group, '') as city_group
-	from
-		dim_region
-	),
-dim_growth_taxonomy as (
-	select distinct
-		mkt_origin,
-		mkt_channel
-	from
-		temp_res
-	),
-dim_supply_distinct as (
+		mkt.mkt_origin in ('Owner PWA', 'Price Calculator')
+	group by 1,2,3,4,5,6,7,8,9
+),
+targets as (
 	select
-	dt,
-	city_group,
-	mkt_origin,
-	mkt_channel
-	from
-		dim_dt,
-		dim_city_group,
-		dim_growth_taxonomy
-	),
-supply_daily_funnel_counts as(
-	select
-		date(dst.dt) as date,
-		dst.city_group,
-		dst.mkt_origin,
-		dst.mkt_channel as mkt_channel,
-		sum(coalesce(tr.daily_leads, 0)) as leads,
-		sum(coalesce(tr.daily_prospects, 0)) as prospects,
-		sum(coalesce(tr.daily_qualifieds, 0)) as qualifieds,
-		sum(coalesce(tr.daily_opportunities, 0)) as opportunities,
-		sum(coalesce(tr.daily_listings, 0)) as first_listings
-	from
-		dim_supply_distinct dst
-		left join temp_res tr
-			on  dst.dt = tr.dt
-			and dst.city_group = tr.city_group
-			and dst.mkt_channel = tr.mkt_channel
-			and dst.mkt_origin = tr.mkt_origin
-	group by 1, 2, 3, 4
-	),
-supply_daily_targets as (
-	select
-		date(str.date) as date,
-		str.city_group,
-		str.mkt_origin,
-		str.mkt_channel,
-		sum(coalesce(budget::float,0)) as budget,
-		sum(coalesce(prospects_target::float,0)) as prospects_target,
-		sum(coalesce(qualifieds_target::float,0)) as qualifieds_target,
-		sum(coalesce(opportunities_target::float,0)) as opportunities_target,
-		sum(coalesce(first_listings_target::float,0)) as first_listings_target
+		date(nullif(str.date,'')) as date,
+		nullif(str.city_group,'')::varchar as city_group,
+		nullif(str.mkt_origin,'')::varchar as mkt_origin,
+		nullif(str.mkt_channel,'')::varchar as mkt_channel,
+		'' as mkt_medium,
+		'' as mkt_source,
+		'' as utm_campaign,
+		'' as utm_content,
+		'' as utm_term,
+		sum(nullif(budget,'')::float) as budget,
+		sum(nullif(prospects_target,'')::float) as prospects_target,
+		sum(nullif(qualifieds_target,'')::float) as qualifieds_target,
+		sum(nullif(opportunities_target,'')::float) as opportunities_target,
+		sum(nullif(first_listings_target,'')::float) as first_listings_target
 	from
 		datalake_raw.gsheets_supply_targets_replanning str
-	group by 1, 2, 3, 4
-	),
-supply_daily_spent as (
-	select
-		date(co.sk_date) date,
-		co.city_group,
-		co.mkt_origin,
-		co.mkt_channel,
-		sum(co.cost) as spent
-	from
-		marketing.fact_marketing_daily_costs co
-	where
-		co.mkt_origin in ('Price Calculator', 'Owner PWA')
-		and co.funnel_side = 'supply'
-	group by 1, 2, 3, 4
-	)
+	group by 1,2,3,4,5,6,7,8,9
+)
 select
-	coalesce(sdt.date, sdc.date, sdf.date) as date,
-	coalesce(sdt.city_group, sdc.city_group, sdf.city_group) as city_group,
-	coalesce(sdt.mkt_origin, sdc.mkt_origin, sdf.mkt_origin) as mkt_origin,
-	coalesce(sdt.mkt_channel, sdc.mkt_channel, sdf.mkt_channel) as mkt_channel,
-	sum(coalesce(sdc.spent, 0)) as spent,
-	sum(coalesce(sdf.leads, 0)) as leads,
-	sum(coalesce(sdf.prospects, 0)) as prospects,
-	sum(coalesce(sdf.qualifieds, 0)) as qualifieds,
-	sum(coalesce(sdf.opportunities, 0)) as opportunities,
-	sum(coalesce(sdf.first_listings, 0)) as listings,
-	sum(coalesce(sdt.budget, 0)) as budget,
-	sum(coalesce(sdt.prospects_target,0)) as prospects_target,
-	sum(coalesce(sdt.qualifieds_target,0))as qualifieds_target,
-	sum(coalesce(sdt.opportunities_target,0)) as opportunities_target,
-	sum(coalesce(sdt.first_listings_target,0)) as first_listings_target
+	coalesce(r.date, c.date,t.date) as date,
+	coalesce(r.city_group, c.city_group, t.city_group) as city_group,
+	coalesce(r.mkt_origin, c.mkt_origin, t.mkt_origin) as mkt_origin,
+	coalesce(r.mkt_channel, c.mkt_channel, t.mkt_channel) as mkt_channel,
+	coalesce(r.mkt_medium, c.mkt_medium, t.mkt_medium) as mkt_medium,
+	coalesce(r.mkt_source, c.mkt_source, t.mkt_source) as mkt_source,
+	coalesce(r.utm_campaign, c.utm_campaign, t.utm_campaign) as utm_campaign,
+	coalesce(r.utm_content, c.utm_content, t.utm_content) as utm_content,
+	coalesce(r.utm_term, c.utm_term, t.utm_term) as utm_term,
+	sum(coalesce(r.leads, 0)) as leads,
+	sum(coalesce(r.prospects, 0)) as prospects,
+	sum(coalesce(r.qualifieds, 0)) as qualifieds,
+	sum(coalesce(r.opportunities, 0)) as opportunities,
+	sum(coalesce(r.first_listings, 0)) as first_listings,
+	sum(coalesce(c.cost, 0)) as spent,
+	sum(coalesce(t.budget,0)) as budget,
+	sum(coalesce(t.prospects_target,0)) as prospects_target,
+	sum(coalesce(t.qualifieds_target,0)) as qualifieds_target,
+	sum(coalesce(t.opportunities_target,0)) as opportunities_target,
+	sum(coalesce(t.first_listings_target,0)) as first_listings_target
 from
-	supply_daily_targets sdt
-	full outer join supply_daily_spent sdc
-		on  sdt."date" = sdc."date"
-		and sdt.city_group = sdc.city_group
-		and sdt.mkt_channel = sdc.mkt_channel
-		and sdt.mkt_origin = sdc.mkt_origin
-	full outer join supply_daily_funnel_counts sdf
-		on  sdt."date" = sdf."date"
-		and sdt.city_group = sdf.city_group
-		and sdt.mkt_channel = sdf.mkt_channel
-		and sdt.mkt_origin = sdf.mkt_origin
-group by 1,2,3,4
+	results r
+	full outer join costs c
+		on  r.date         = c.date
+		and r.city_group   = c.city_group
+		and r.mkt_origin   = c.mkt_origin
+		and r.mkt_channel  = c.mkt_channel
+		and r.mkt_medium   = c.mkt_medium
+		and r.mkt_source   = c.mkt_source
+		and r.utm_campaign = c.utm_campaign
+		and r.utm_content  = c.utm_content
+		and r.utm_term     = c.utm_term
+	full outer join targets t
+		on  r.date         = t.date
+		and r.city_group   = t.city_group
+		and r.mkt_origin   = t.mkt_origin
+		and r.mkt_channel  = t.mkt_channel
+		and r.mkt_medium   = t.mkt_medium
+		and r.mkt_source   = t.mkt_source
+		and r.utm_campaign = t.utm_campaign
+		and r.utm_content  = t.utm_content
+		and r.utm_term     = t.utm_term
+	group by 1,2,3,4,5,6,7,8,9
