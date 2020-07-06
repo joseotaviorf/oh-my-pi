fb9f32ad26ff31f7c13e484191539092f85c2ead
felipemiquelim <felipe.miquelim@quintoandar.com.br>
2020-07-06 11:32:49 -0300

    [DF-52] Adding kill_queue reservation enrich process (#3029)

---
+++ bietlejuice/jobs/composer/db/datalake/queries/kill_queue/enrich/reservation.sql
@@ -0,0 +1,17 @@
+SELECT
+  cast(r.id as bigint) as id_reservation,
+  id_rent_flow,
+  r.id_tenant,
+  h.id_main as id_house,
+  cast(r.version as integer) as version,
+  cast(r.attempt as integer) as attempt,
+  r.status,
+  r.value,
+  r.mundipagg_token,
+  r.cancellation_reason,
+  cast(cast(r.is_ongoing as integer) as boolean) as is_ongoing,
+  r.ts_created,
+  r.ts_updated
+FROM datalake_kill_queue_clean.reservation r
+LEFT JOIN datalake_kill_queue_clean.house h
+    ON r.id_house = h.id
