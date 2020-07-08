dc0f94d3df4c540546cb8cfb1284443e3340eb90
Cássio Bastos <53241140+cassiolino@users.noreply.github.com>
2020-07-08 11:21:27 -0300

    [DS-1259] Adding tables negotiation and negotiation_aud on terminator (#3036)
    * Adding tables negotiation and negotiation_aud on terminator
    
    * Adding tables negotiation and negotiation_aud on terminator
    
    * Update negotiation.sql
    
    * Update negotiation_aud.sql

---
+++ bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/public/negotiation_aud.sql
@@ -0,0 +1,13 @@
+SELECT
+    id,
+    rev,
+    revtype AS rev_type,
+    revend AS rev_end,
+    termination_id AS id_termination,
+    has_landlord_comment,
+    needs_repair_by_tenant,
+    repair_resolution,
+    repair_cost,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM datalake_terminator_raw.negotiation_aud
