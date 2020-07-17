b38a031d81f88815ed87a1b81530ff0ede3324b3
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2020-07-17 14:33:18 -0300

    [DINT-89] Smart Price Model - dim_smart_price (#3066)
    * Creating dim_smart_price query and ddl
    
    * Adjusting order of columns in DDL
    
    * Adjusting column name
    
    * Adjusting column names and order

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_smart_price/dw/dim_smart_price.sql
@@ -0,0 +1,63 @@
+with cte_first_last_activation as (
+    select
+        id_smart_price,
+        min(case when status = 'ACTIVE' then ts_start_status end) as ts_first_activation,
+        max(case when status = 'INACTIVE' then ts_start_status end) as ts_last_deactivation
+    from
+        datalake_ebdb_smart_price.smart_price_versioning
+    group by 1
+),
+cte_status_is_enabled as(
+    select
+        smp.id_smart_price,
+        smp.id_house,
+        smp.id_dynamic_pricing_parameter,
+        smp.status,
+        dpa.is_enabled,
+        row_number() over(partition by smp.id_smart_price order by smp.ts_start_status desc) ranking
+    from
+        datalake_ebdb_smart_price.smart_price_versioning smp
+    join
+        datalake_ebdb_clean.dynamic_pricing_house_aud  dpa
+            on smp.id_dynamic_pricing = dpa.id
+            and smp.rev_dynamic_pricing_house_aud = dpa.rev
+),
+cte_last_status_is_enabled as (
+    select
+        id_smart_price,
+        id_house,
+        id_dynamic_pricing_parameter,
+        status,
+        is_enabled
+    from
+        cte_status_is_enabled
+    where
+        ranking = 1
+)
+select
+    fla.id_smart_price as sk_smart_price,
+    fla.id_smart_price,
+    lsie.is_enabled,
+    lsie.status as status,
+    dph.status as house_last_status,
+    dph.min_rent as house_min_rent,
+    dph.initial_rent as house_max_rent,
+    dpp.min_rent_percentile,
+    dpp.initial_rent_percentile as max_rent_percentile,
+    dpp.offers_threshold,
+    dpp.days_threshold,
+    dpp.visits_days_threshold,
+    fla.ts_first_activation,
+    fla.ts_last_deactivation,
+    now() as ts_load
+from
+    cte_first_last_activation fla
+join
+    cte_last_status_is_enabled lsie
+        on fla.id_smart_price = lsie.id_smart_price
+join
+    datalake_ebdb_clean.dynamic_pricing_house dph
+        on dph.id_house = lsie.id_house
+join
+    datalake_ebdb_clean.dynamic_pricing_parameters dpp
+        on lsie.id_dynamic_pricing_parameter = dpp.id
---
+++ bietlejuice/jobs/composer/db/dw/ddl/quintoandar/dim_smart_price.ddl
@@ -0,0 +1,18 @@
+drop table if exists quintoandar.dim_smart_price;
+create table quintoandar.dim_smart_price (
+    sk_smart_price bigint,
+    id_smart_price bigint,
+    is_enabled boolean,
+    status varchar,
+    house_last_status varchar,
+    house_min_rent integer,
+    house_max_rent integer,
+    min_rent_percentile integer,
+    max_rent_percentile integer,
+    offers_threshold integer,
+    days_threshold integer,
+    visits_days_threshold integer,
+    ts_first_activation timestamp,
+    ts_last_deactivation timestamp
+    ts_load timestamp
+);
