e1ef01290b42cb9f5864f7de40669756af3f981a
Luc Somers <lucwsomers@gmail.com>
2020-07-22 09:55:33 -0300

    rebase (#3063)

---
+++ bietlejuice/jobs/composer/dags/marketing_hub/marketing_hub.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+import pendulum
+
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.marketing_hub import SOURCE
+
+DAG_ID = "bietlejuice.marketing_hub"
+
+S3_PATH = Variable.get("datalake_old_bucket")
+S3_MARKETING_PATH = Variable.get("datalake_marketing_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/"
+
+LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
+    Variable.get("databricks_s3_bucket"), DAG_ID
+)
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+LIBRARIES_DESCRIPTION = Variable.get(
+    "bietlejuice_default_libraries", deserialize_json=True
+)
+
+
+local_tz = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2019, 5, 31, 0, 0, 0, tzinfo=local_tz)
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
+GOOGLE_ADS_SOURCE_PATH = f"s3://{S3_MARKETING_PATH}/google-reports"
+GOOGLE_ADS_TARGET_PATH = f"s3://{S3_PATH}/raw/marketing_hub/google_ads"
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=LIBRARIES_DESCRIPTION,
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+google_ads_load_to_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="google-ads-load-to-raw",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": SPARK_JOBS_PATH + "google_ads_load_to_raw.py",
+            "parameters": [GOOGLE_ADS_SOURCE_PATH, GOOGLE_ADS_TARGET_PATH, "{{ ds }}"],
+        }
+    },
+)
+
+create_cluster_task >> google_ads_load_to_raw_task >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/dags/marketing_hub/spark_jobs/google_ads_load_to_raw.py
@@ -0,0 +1,206 @@
+import boto3
+import datetime
+import logging
+import re
+
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.spark import SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.consumers.s3_consumer import S3Consumer
+from bietlejuice.jobs.composer.dags.marketing_hub import SOURCE
+from bietlejuice.jobs.composer.loaders import S3Loader
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+from bietlejuice.jobs.composer.services import S3Service
+
+JOB_NAME = "google_ads_load_to_raw"
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+REPORT_TYPES = {
+    "ad_performance_report": "ads_performance_report",
+    "campaign_performance_report": "campaigns_performance_report",
+    "keywords_performance_report": "keywords_performance_report",
+}
+
+REPORT_SCHEMAS = {
+    "ads_performance_report": [
+        "ExternalCustomerId",
+        "AdGroupId",
+        "AdGroupName",
+        "AdType",
+        "CampaignId",
+        "CampaignName",
+        "Clicks",
+        "Cost",
+        "Date",
+        "Description",
+        "Description1",
+        "Description2",
+        "Device",
+        "DisplayUrl",
+        "Id",
+        "Impressions",
+        "ImageCreativeName",
+        "AccountDescriptiveName",
+        "AbsoluteTopImpressionPercentage",
+    ],
+    "campaigns_performance_report": [
+        "ExternalCustomerId",
+        "CampaignId",
+        "CampaignName",
+        "Clicks",
+        "Cost",
+        "Date",
+        "Device",
+        "Impressions",
+        "AccountDescriptiveName",
+        "Month",
+        "Labels",
+        "Week",
+        "Year",
+        "AbsoluteTopImpressionPercentage",
+        "SearchImpressionShare",
+    ],
+    "keywords_performance_report": [
+        "ExternalCustomerId",
+        "AdGroupId",
+        "AdGroupName" "CampaignId",
+        "CampaignName",
+        "Clicks",
+        "Cost",
+        "Date",
+        "Device",
+        "Id" "Impressions",
+        "KeywordMatchType" "Labels",
+        "Criteria" "AccountDescriptiveName",
+        "AbsoluteTopImpressionPercentage",
+        "SearchImpressionShare",
+    ],
+}
+FULL_FILE_PATH_LENGTH = 8
+
+
+def format_account_name(account_name):
+    alphanumeric_account_name = re.sub(r"[^\w\s]", "", account_name)
+    snake_cased_account_name = re.sub(r"\s+", "_", alphanumeric_account_name)
+    return snake_cased_account_name.lower()
+
+
+def split_str(str, split_condition):
+    return [x for x in str.split(split_condition) if x != ""]
+
+
+def get_date(str):
+    return re.search("dt=(.*?)/", str).group(1)
+
+
+def format_date(date):
+    formatted_date = datetime.datetime.strptime(date, "%d-%m-%Y").strftime("%Y-%m-%d")
+    return formatted_date
+
+
+def filter_for_full_file_paths(file_paths):
+    filtered_file_paths = [
+        x for x in file_paths if len(split_str(x, "/")) >= FULL_FILE_PATH_LENGTH
+    ]
+    return filtered_file_paths
+
+
+def filter_for_execution_date(file_paths, execution_date):
+    filtered_file_paths = [
+        x for x in file_paths if format_date(get_date(x)) == execution_date
+    ]
+    return filtered_file_paths
+
+
+def build_target_file_path(s3_file, s3_source_file_path, report_type):
+    raw_account_name = s3_file.first().AccountDescriptiveName
+    account_name = format_account_name(raw_account_name)
+    dt = get_date(s3_source_file_path)
+    database_location = f"{s3_file_path_target}/{report_type}/acc={account_name}/"
+    table_name = f"dt={dt}"
+    return database_location, table_name
+
+
+def add_schema_to_csv(csv_file, s3_source_file_path, report_type):
+    report_schema = REPORT_SCHEMAS[report_type]
+    csv_file = csv_file.toDF(*report_schema)
+    return csv_file
+
+
+def convert_csv_to_json(csv_file):
+    json_file = csv_file.toJSON()
+    return json_file
+
+
+def get_report_type(s3_path):
+    raw_report_type = re.search("report=(.*?)/", s3_source_file_path).group(1).lower()
+    parsed_report_type = fix_report_type(raw_report_type)
+    return parsed_report_type
+
+
+def fix_report_type(report_type):
+    return REPORT_TYPES[report_type] if report_type in REPORT_TYPES else report_type
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument(
+        "source_path", type=str, help="source path where data will be taken from"
+    )
+    parser.add_argument(
+        "target_path", type=str, help="target path where data will be put"
+    )
+    parser.add_argument("execution_date")
+
+    args = parser.parse_args()
+
+    s3_file_path_source = args.source_path
+    s3_file_path_target = args.target_path
+    execution_date = args.execution_date
+
+    logger.info(
+        f"m={JOB_NAME}, target_path={s3_file_path_target}, source_path={s3_file_path_source}"
+        "msg=print args spark jobs params"
+    )
+
+
+s3_source_file_format = "csv"
+spark_client = SparkClient()
+spark_metastore_service = SparkMetastoreService(spark_client)
+
+s3_consumer = S3Consumer(spark_client)
+s3_service = S3Service(boto3.resource("s3"))
+s3_loader = S3Loader()
+
+s3_source_raw_file_paths = s3_service.list_objects(s3_file_path_source)
+s3_source_full_file_paths = filter_for_full_file_paths(s3_source_raw_file_paths)
+s3_source_file_paths = filter_for_execution_date(
+    s3_source_full_file_paths, execution_date
+)
+s3_target_files_with_path = list()
+
+for s3_source_file_path in s3_source_file_paths:
+    csv_s3_file = s3_consumer.get_data_from_file(
+        s3_source_file_path, s3_source_file_format
+    )
+    report_type = get_report_type(s3_source_file_path)
+    csv_s3_file_with_schema = add_schema_to_csv(
+        csv_s3_file, s3_source_file_path, report_type
+    )
+    s3_target_database_location, s3_target_table_name = build_target_file_path(
+        csv_s3_file_with_schema, s3_source_file_path, report_type
+    )
+
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    s3_loader.load_full_table(
+        df=csv_s3_file_with_schema,
+        database_name=SOURCE,
+        table_name=s3_target_table_name,
+        database_location=s3_target_database_location,
+        format_options=format_options,
+    )
