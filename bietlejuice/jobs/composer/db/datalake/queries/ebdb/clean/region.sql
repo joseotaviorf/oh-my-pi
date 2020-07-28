10397023d959567411e065e6e4ddf3cda6d417ca
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-07-28 12:16:47 -0300

    [DF-79] Enrich Ebdb Region (#3147)
    * Enrich ebdb region

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb/clean/region.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb/clean/region.sql
@@ -1,17 +1,9 @@
 SELECT
-    r.id AS id_region,
-    r.criadaEm AS ts_created,
-    r.atualizadoEm AS ts_updated,
-    r.nivel AS level,
-    r.nome AS name,
-    mr.id AS id_macro,
-    mr.nome AS macro_name,
-    c.id AS id_city,
-    c.nome AS city_name,
-    r.regiaoPai_id as id_parent_region
+    id,
+    nome AS name,
+    nivel AS level,
+    regiaoPai_id as id_parent_region,
+    criadaEm AS ts_created,
+    atualizadoEm AS ts_updated
 FROM
-    datalake_ebdb_raw.regiao r
-    LEFT JOIN datalake_ebdb_raw.regiao mr ON
-        mr.id = r.regiaoPai_id
-    LEFT JOIN datalake_ebdb_raw.regiao c ON
-        c.id = mr.regiaoPai_id
+    datalake_ebdb_raw.regiao
