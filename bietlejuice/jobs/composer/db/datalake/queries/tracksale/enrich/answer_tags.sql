7f9cb2bfdcf4febac32a3dc7c995c4f4cb9ab63e
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-08-05 12:44:19 -0300

    [DINT-186] Creating answer_tags query in NPS Enrich (#3186)
    * Creating answer_tags query in NPS Enrich
    
    * Adding ts_answer_sent
    
    * Fixing identation
    
    * Updating reference to enriched table
    
    * Fixing DAG broken by id column

---
+++ bietlejuice/jobs/composer/db/datalake/queries/tracksale/enrich/answer_tags.sql
@@ -0,0 +1,13 @@
+WITH unnested_tags AS (
+    SELECT
+        id AS id_answer,
+        ts_answer_sent,
+        EXPLODE(FROM_JSON(tags,'array<string>')) AS tag
+    FROM datalake_tracksale.answer
+)
+SELECT
+    id_answer,
+    ts_answer_sent,
+    GET_JSON_OBJECT(tag, '$.name') AS tag_name,
+    GET_JSON_OBJECT(tag, '$.value') AS tag_value
+FROM unnested_tags
