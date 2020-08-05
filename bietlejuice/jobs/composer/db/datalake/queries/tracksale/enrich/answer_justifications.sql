d52b3254c46965200040ead3a4561ed66e5af2ff
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-08-05 12:45:30 -0300

    [DINT-13] Add answer_justifications query in Enrich Tracksale (#3174)
    * Adding enriching query for nps answer justifications
    
    * Replacing regex to json parsing
    
    * Update table reference to enrich

---
+++ bietlejuice/jobs/composer/db/datalake/queries/tracksale/enrich/answer_justifications.sql
@@ -0,0 +1,40 @@
+WITH exploded_justifications AS (
+  SELECT
+      id AS id_answer,
+      justifications,
+      explode(FROM_json(justifications, 'array<string>')) AS just_json
+  FROM datalake_tracksale.answer
+  WHERE justifications != '[]'  
+),
+main_justifications AS (
+    SELECT
+        id_answer,
+        'main' AS level,
+        GET_JSON_OBJECT(just_json, '$.name') AS justification
+    FROM exploded_justifications
+),
+exploded_second_justifications AS (
+    SELECT
+        id_answer,
+        'second' AS level,
+        FROM_json(GET_JSON_OBJECT(just_json, '$.children'), 'array<string>') AS justification    
+    FROM exploded_justifications
+    where GET_JSON_OBJECT(just_json, '$.children') <> '[]'
+),
+second_justifications AS (
+    SELECT 
+        id_answer,
+        level,
+        explode(justification) AS justification
+    FROM exploded_second_justifications
+),
+justifications AS (
+    SELECT * FROM main_justifications
+    UNION ALL
+    SELECT * FROM second_justifications
+)
+SELECT 
+    id_answer,
+    level,
+    GET_JSON_OBJECT(justification, '$.pt-br') AS justification
+FROM justifications
