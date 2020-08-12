25d527f96f3ec6cb3a722e1fdbc0e6cca717d11e
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-08-12 15:04:21 -0300

    [DINT-112] Add fact_nps_customer_metrics query in DW Tracksale (#3205)
    * Adding sql and ddl file
    
    * Fixing uppercase in overall_nps column
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Including ts_load
    
    Co-authored-by: Jonas <42319039+vitojon@users.noreply.github.com>
    
    * Including ts_load in ddl file
    
    Co-authored-by: Jonas <42319039+vitojon@users.noreply.github.com>
    
    * Fixing uppercase in overall_nps column
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Fixing identation
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Fix data type of sk_nps_customer
    
    * fixing data types in ddl file
    
    * Excluding CTE of last shift score
    
    * Merging customer key CTEs
    
    * Fixing identation
    
    * Removing typo
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Remove uppercase
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    Co-authored-by: Jonas <42319039+vitojon@users.noreply.github.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/tracksale/dw/fact_nps_customer_metrics.sql
@@ -0,0 +1,131 @@
+WITH customer_conversions AS (
+	SELECT
+		 cc.id_customer,
+		 cc.id_dispatch,
+		 cc.id_answer,
+		 d.status,
+		 a.score_category,
+		 CASE
+			WHEN a.last_nps_answer <= 6 THEN 'detractor'
+			WHEN a.last_nps_answer <= 8 THEN 'passive'
+			WHEN a.last_nps_answer <= 10 THEN 'promoter'
+			END AS last_score_category,
+		 a.nps_answer,
+		 a.last_nps_answer,
+		 a.nps_comment,
+		 ROUND(a.seconds_spent_answering/60.0,2) AS minutes_spent_answering,
+		 d.ts_created,
+		 a.ts_answer_sent
+	FROM datalake_tracksale.customer_conversions cc
+	INNER JOIN datalake_tracksale.dispatch d
+		ON cc.id_dispatch_lot = d.id
+	LEFT JOIN datalake_tracksale.answer a
+		 ON cc.id_answer = a.id
+	WHERE cc.id_customer != '' -- filter out dispatches from unidentified customers
+),
+customer_metrics AS (
+	SELECT
+		id_customer,
+		MAX(id_answer) AS id_last_answer,
+		COUNT(id_dispatch) AS total_dispatches,
+		COUNT(id_answer) AS total_answers,
+		COUNT(CASE WHEN score_category = 'promoter' THEN id_answer end) AS promoters,
+		COUNT(CASE WHEN score_category = 'passive' THEN id_answer end) AS passives,
+		COUNT(CASE WHEN score_category = 'detractor' THEN id_answer end) AS detractors,
+		COUNT(CASE WHEN nps_comment IS NOT NULL THEN id_answer end) AS total_comments,
+		AVG(minutes_spent_answering) AS avg_minutes_response_time,
+		AVG(nps_answer) AS avg_score,
+		COUNT(CASE WHEN status != 'Finalizado' THEN id_dispatch END) > 0 AS has_pending_survey,
+		CAST(MAX(ts_created) AS date) AS dt_last_dispatched,
+		MIN(ts_created) AS ts_first_dispatched
+	FROM customer_conversions
+	GROUP BY 1
+),
+conversion_metrics AS (
+	SELECT
+		id_customer,
+		id_last_answer,
+		total_dispatches,
+		total_answers,
+		avg_minutes_response_time,
+		avg_score,
+		CASE WHEN total_answers > 0 THEN ROUND(100.0*(promoters - detractors)/total_answers,0)
+			END AS overall_nps,
+		CASE WHEN total_dispatches > 0 THEN ROUND(1.0*total_answers/total_dispatches,3)
+			END AS answer_rate,
+		CASE WHEN total_answers > 0 THEN ROUND(1.0*total_comments/total_answers,3)
+			END AS comment_rate,
+		has_pending_survey,
+		dt_last_dispatched,
+		ts_first_dispatched
+	FROM customer_metrics
+),
+answer_keys AS (
+	SELECT
+		id_answer,
+		MAX(CASE WHEN tag_name = 'User Id' THEN CAST(tag_value AS BIGINT)
+				END) AS id_user,
+		MAX(CASE WHEN tag_name = 'CPF' THEN tag_value
+				END) AS cpf
+	FROM datalake_tracksale.answer_tags
+	WHERE tag_name IN ('User Id','CPF')
+	GROUP BY 1
+),
+ebdb_user AS (
+	SELECT
+		ak.id_answer,
+		ak.id_user
+	FROM answer_keys ak
+	INNER JOIN datalake_ebdb_clean.user u
+		ON ak.id_user = u.id
+	GROUP BY 1,2
+),
+ebdb_cpf AS (
+	SELECT
+		ak.id_answer,
+		ak.cpf
+	FROM answer_keys ak
+	INNER JOIN datalake_ebdb_customer_contact_identification.customer_contact_identification cci
+		ON ak.cpf = cci.cpf
+	GROUP BY 1,2
+),
+customer_keys AS (
+    SELECT
+        cc.id_customer,
+        MAX(COALESCE(eu.id_user,cci_e.id_user, cci_p.id_user)) AS id_user,
+        MAX(COALESCE(ec.cpf,cci_e.cpf,cci_p.cpf)) AS cpf
+    FROM datalake_tracksale.customer_conversions cc
+    LEFT JOIN ebdb_user eu
+        ON eu.id_answer = cc.id_answer
+    LEFT JOIN ebdb_cpf ec
+        ON ec.id_answer = cc.id_answer
+    LEFT JOIN datalake_ebdb_customer_contact_identification.customer_contact_identification cci_p
+        ON cci_p.customer_contact = cc.customer_phone
+        AND cci_p.channel = 'phone'
+    LEFT JOIN datalake_ebdb_customer_contact_identification.customer_contact_identification cci_e
+		ON cci_e.customer_contact = cc.customer_email
+        AND cci_e.channel = 'email'
+    GROUP BY 1
+)
+SELECT
+	cm.id_customer AS sk_nps_customer,
+	COALESCE(ck.id_user,-1) AS sk_user,
+	ck.cpf AS sk_personal_document,
+	CONCAT(cc.last_score_category,':',cc.score_category) AS last_shift_type,
+	cm.total_dispatches,
+	cm.total_answers,
+	cm.answer_rate,
+	cm.comment_rate,
+	cm.avg_score,
+	cc.nps_answer AS last_score,
+	cm.overall_nps,
+	cm.avg_minutes_response_time,
+	cm.has_pending_survey,
+	cm.dt_last_dispatched,
+	current_timestamp as ts_load
+FROM conversion_metrics cm
+LEFT JOIN customer_keys ck
+	ON ck.id_customer = cm.id_customer
+LEFT JOIN customer_conversions cc
+    ON cc.id_customer = cm.id_customer
+    AND cc.id_answer = cm.id_last_answer
---
+++ bietlejuice/jobs/composer/db/dw/ddl/tracksale/fact_nps_customer_metrics.ddl
@@ -0,0 +1,18 @@
+drop table if exists tracksale.fact_nps_customer_metrics;
+create table if not exists tracksale.fact_nps_customer_metrics (
+    sk_nps_customer varchar(200) primary key,
+	sk_user bigint,
+	sk_personal_document varchar(25),
+	last_shift_type varchar(50),
+	total_dispatches bigint,
+	total_answers bigint,
+	answer_rate decimal(38,3),
+	comment_rate decimal(38,3),
+	avg_score float,
+	last_score bigint,
+	overall_nps decimal(38,0),
+	avg_minutes_response_time decimal(31,6),
+	has_pending_survey boolean,
+	dt_last_dispatched date,
+	ts_load timestamp
+)
