f9b72cafa0603e3899a2f003b7468c518c016af2
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-08-13 13:55:57 -0300

    [DINT-200] Fix broken DAG dw_tracksale by null dispatches (#3251)
    * Fix null id_dispatch in customer_conversions
    
    * Improving coalesce clause
    
    Co-authored-by: Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
    
    * Improving both coalesce clauses
    
    Co-authored-by: Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>

--- bietlejuice/jobs/composer/db/datalake/queries/tracksale/enrich/customer_conversions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/tracksale/enrich/customer_conversions.sql
@@ -8,7 +8,7 @@ clean_unnested_dispatches AS (
 	SELECT
 		id_dispatch,
 		LOWER(GET_JSON_OBJECT(customer,'$.name')) AS customer_name,
-		GET_JSON_OBJECT(customer,'$.email') AS customer_email,
+		LOWER(GET_JSON_OBJECT(customer,'$.email')) AS customer_email,
 		REGEXP_REPLACE(REGEXP_REPLACE(GET_JSON_OBJECT(customer,'$.phone'),'\\D+',''),'^55','') AS customer_phone
 	FROM unnested_dispatches
 ),
@@ -22,8 +22,12 @@ dispatch_customers AS (
 			COALESCE(customer_email,''),
 			COALESCE(customer_phone,''),
 			COALESCE(customer_name,'')
-            ) AS id_customer
+			) AS id_customer
 	FROM clean_unnested_dispatches
+	WHERE 
+		COALESCE(customer_email, '') != ''
+		AND COALESCE(customer_phone, '') != ''
+		AND COALESCE(customer_name, '') != ''
 	GROUP BY 1,2,3,4,5
 ),
 answers AS (
@@ -31,13 +35,17 @@ answers AS (
 		id AS id_answer,
 		lot_code AS id_dispatch,
 		CONCAT(
-			COALESCE(email,''),
+			COALESCE(LOWER(email),''),
 			COALESCE(REGEXP_REPLACE(phone,'\\D+',''),''),
 			COALESCE(LOWER(name),'')
 			) AS id_customer,
 		email,
 		phone
 	FROM datalake_tracksale.answer
+	WHERE 
+		COALESCE(email, '') != ''
+		AND COALESCE(phone, '') != ''
+		AND COALESCE(name, '') != ''
 ),
 -- consider only the last answer for each customer in a dispatch
 last_dispatch_answers AS (
@@ -76,3 +84,4 @@ LEFT JOIN datalake_tracksale.dispatch d -- complete dispatch information in answ
 	ON d.id = ac.id_dispatch
 	AND dc.id_dispatch IS NULL
 	AND dc.id_customer IS NULL
+WHERE dc.id_dispatch IS NOT NULL
