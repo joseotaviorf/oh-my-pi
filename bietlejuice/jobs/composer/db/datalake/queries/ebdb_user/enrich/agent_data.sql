5045dec06b714d361fb43b66e5e19ada56a5cec5
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-08-13 10:13:27 -0300

    [DF-110] Add table agent_data SQL (#3241)
    * Add table agent_data SQL and enrich_ebdb_user DAG dependencies
    
    * Set null values as false for is_sale_agent

---
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb_user/enrich/agent_data.sql
@@ -0,0 +1,13 @@
+select
+    ad.id,
+    ad.id_city,
+    ad.creci_number,
+    ad.profile,
+    ad.is_active,
+    max(coalesce(adbc.business_context = 'SALE', false)) as is_sale_agent,
+    -- non existent agents on businessContextsServed table are assumed as RENT
+    max(coalesce(adbc.business_context, 'RENT') = 'RENT') as is_rent_agent
+from datalake_ebdb_clean.agent_data as ad
+left join datalake_ebdb_clean.agent_data_business_contexts_served adbc
+    on adbc.id_agent_data = ad.id
+group by 1,2,3,4,5
