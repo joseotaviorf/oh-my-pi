1c90dfcccb7bd7502e7e7dbafb57fbae2bc9431c
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-08-14 16:14:28 -0300

    [DF-112] Add new columns to enriched table affiliate_data (#3252)
    * add new columns to enriched table affiliate_data

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb_user/enrich/affiliate_data.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb_user/enrich/affiliate_data.sql
@@ -1,15 +1,42 @@
+with first_operation_start (
+    with min_operation_start_rev (
+        select
+            id_affiliate_data,
+            min(REV) as REV
+        from datalake_ebdb_clean.affiliate_data_aud
+        where ts_operation_start is not null
+        group by 1
+	)
+	select
+	    ada.id_affiliate_data,
+	    ada.ts_operation_start
+	from min_operation_start_rev mosr
+    left join datalake_ebdb_clean.affiliate_data_aud ada
+        on mosr.id_affiliate_data = ada.id_affiliate_data
+        and mosr.REV = ada.REV
+)
 select
     ad.id,
     ad.id_indicated_by,
     ad.is_active,
+    (ad.id_doorman_affiliate_data is not null) as is_doorman_affiliate,
     ad.origin,
     case when ad.affiliate_type = 'Doorman' and u.id_agent is not null then 'Doorman & Agent'
          when u.id_affiliates is not null then 'Agent'
          else ad.affiliate_type
     end as affiliate_type,
+    ad.payment_preference,
+    ad.creci_number,
+    coalesce(ad.operation_city, dad.work_city) as work_city,
+    ad.last_week_balance_communication,
+    coalesce(fos.ts_operation_start, ad.ts_operation_start) as ts_first_operation_start,
     ad.ts_operation_start,
     ad.ts_created,
     ad.ts_updated
 from datalake_ebdb_clean.affiliate_data ad
 left join datalake_ebdb_clean.user u
     on u.id_affiliates = ad.id
+left join datalake_ebdb_clean.doorman_affiliate_data dad
+    on dad.id = ad.id_doorman_affiliate_data
+left join first_operation_start fos
+    on fos.id_affiliate_data = ad.id
