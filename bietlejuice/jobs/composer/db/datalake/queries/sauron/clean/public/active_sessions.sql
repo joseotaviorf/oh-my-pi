63e0672c1f5b45fdb9616025718de53cc53a0140
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-08-20 11:33:15 -0300

    FIX bug: col dialogflow_response renamed to bot_response (#3280)

--- bietlejuice/jobs/composer/db/datalake/queries/sauron/clean/public/active_sessions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/sauron/clean/public/active_sessions.sql
@@ -4,10 +4,10 @@ select
   status,
   user_data,
   user_phone,
-  last_message_at as ts_last_message,
-  first_message_at as ts_first_message,
   context,
   created_by,
+  last_message_at as ts_last_message,
+  first_message_at as ts_first_message,
   created_at as ts_created,
   updated_at as ts_updated
 from datalake_sauron_raw.activesessions
--- bietlejuice/jobs/composer/db/datalake/queries/sauron/clean/public/bot_outgoing_messages.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/sauron/clean/public/bot_outgoing_messages.sql
@@ -3,7 +3,7 @@ select
   session_id as id_session,
   message_id as id_message,
   message_payload,
-  dialogflow_response,
+  bot_response,
   user_phone,
   year_month,
   created_at as ts_created,
