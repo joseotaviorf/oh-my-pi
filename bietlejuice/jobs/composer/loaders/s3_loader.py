a6bfe9c652cd88599b43ee9bc1da726cae5d6883
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2020-08-25 09:05:32 -0300

    Revert "[DEMKT-117] - Modify DatalakeSubDAG to Accept Incremental Load (#3279)" (#3299)
    This reverts commit 4d562c5df7e9a1f9358c49932d56f08a6df874d9.

--- bietlejuice/jobs/composer/loaders/s3_loader.py
+++ bietlejuice/jobs/composer/loaders/s3_loader.py
@@ -25,8 +25,6 @@ class S3Loader:
         **options
     ):
         """
-        *This method will be deprecated soon, you shold use load_df.
-
         Loads the content of an Spark DataFrame as a table in S3.
 
         If the table already exits, this method overwrites the existing data while
@@ -79,8 +77,6 @@ class S3Loader:
         **options
     ):
         """
-        *This method will be deprecated soon, you shold use load_df.
-
         Loads the content of an Spark DataFrame into a table in S3 overwriting the
         corresponding partitions.
 
@@ -141,68 +137,4 @@ class S3Loader:
             "partitions={}, msg=loaded partitions successfully".format(
                 s3_path, partition_cols
             )
-        )
-
-    @logger(exclude="df")
-    def load_df(
-        self, df, s3_path, format_options, partitions, is_incremental, **options
-    ):
-        """
-        Loads the content of an Spark DataFrame into a table in S3 overwriting the
-        corresponding partitions.
-
-        This method will only overwrite the data of the partitions values contained in
-        the Spark DataFrame or overwrite the whole data, depending on load mode (is_incremental).
-        It doesn't refresh the table in the Spark Metastore, so you would need to do it
-        later in the caller side if this is your desire.
-
-        For incremental mode, the method is only allowed to execute if
-        spark.sql.sources.partitionOverwriteMode is set to 'dynamic',
-        if other behaviour is necessary when loading the data,
-        the data in S3 need to be deleted manually.
-
-        :param df: a dataframe
-        :type df: SparkDataFrame
-        :param s3_path: path where the table will be put or updated i.e. database_location + table_name
-        :type df: string
-        :param format_options: the file format used to save
-        :type format_options: str
-        :param partitions: names of partitioning columns
-        :type partitions: list
-        :param options: all other string options
-        :type options: keyworded, variable-length argument list
-        """
-
-        if not df:
-            raise ValueError("m=load_file, msg=Spark DataFrame is empty")
-
-        if is_incremental:
-            # check spark conf
-            spark = BaseSparkContext.spark
-            partition_overwrite_mode = spark.conf.get(
-                "spark.sql.sources.partitionOverwriteMode"
-            ).lower()
-            if partition_overwrite_mode != "dynamic":
-                raise RuntimeError(
-                    "m=load_file, spark.sql.sources.partitionOverwriteMode={}, "
-                    "msg=partitionOverwriteMode have to be configured to 'dynamic'".format(
-                        partition_overwrite_mode
-                    )
-                )
-
-        df_writer = (
-            df.write.mode("overwrite")
-            .format(format_options)
-            .option("maxRecordsPerFile", self.MAX_RECORDS_PER_FILE)
-        )
-        if partitions:
-            df_writer = df_writer.partitionBy(*partitions)
-
-        for op, val in options.items():
-            df_writer = df_writer.option(op, val)
-
-        df_writer.save(path=s3_path)
-
-        logger.info(
-            "m=load_df, s3_path={}, " "msg=loaded files into S3.".format(s3_path)
         )
