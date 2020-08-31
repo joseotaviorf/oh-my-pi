b315ecb0c866be33b0f9247abd4e054e9bb95b45
felipemiquelim <felipe.miquelim@quintoandar.com.br>
2020-08-31 11:10:56 -0300

    [DF-138] Adjusting data type in enrich User Revision Entity (#3358)

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb_user_revision_entity/enrich/user_revision_entity.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb_user_revision_entity/enrich/user_revision_entity.sql
@@ -1,9 +1,11 @@
 select
   id,
-  -- Adding milliseconds to ts_revision default timestamp format
-  from_unixtime(ts_revision/1000) 
-    + (ts_revision % 1000) * interval 1 milliseconds as ts_revision,
   id_user,
-reason
+  reason,
+    -- Adding milliseconds to ts_revision default timestamp format
+  cast(
+    from_unixtime(ts_revision/1000)
+        + (ts_revision % 1000) * interval 1 milliseconds
+  as timestamp) as ts_revision
 from 
   datalake_ebdb_clean.user_revision_entity
