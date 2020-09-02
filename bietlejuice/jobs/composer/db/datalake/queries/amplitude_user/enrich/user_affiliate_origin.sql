bdff3f4dc45f682cf8709f10ec8cf5829114b216
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-09-02 14:30:42 -0300

    [DF-120] Add new enriched column city_campaign in user_affiliate_origin (#3339)
    * Add new enriched column city_campaign in user_affiliate_origin
    
    * escape char for regex
    
    * Remove unecessary use of UDF (usign like instead) and get city name
    
    * Comment bug found
    
    * reformat comment

--- bietlejuice/jobs/composer/db/datalake/queries/amplitude_user/enrich/user_affiliate_origin.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/amplitude_user/enrich/user_affiliate_origin.sql
@@ -5,6 +5,7 @@ with amplitude_affiliate_tracking as(
         coalesce(get_json_object(user_properties, '$.utm_source'), '') as utm_source,
         coalesce(get_json_object(user_properties, '$.utm_medium'), '') as utm_medium,
         coalesce(get_json_object(user_properties, '$.utm_campaign'), '') as utm_campaign,
+        lower(regexp_replace(coalesce(get_json_object(user_properties, '$.utm_campaign'), ''), '[^\\w]+|_', '')) as utm_campaign_cleaned,
         coalesce(get_json_object(user_properties, '$.utm_content'), '') as utm_content,
         coalesce(get_json_object(user_properties, '$.utm_term'), '') as utm_term,
         coalesce(get_json_object(user_properties, '$.platform'), '') as platform,
@@ -29,6 +30,19 @@ select
 	utm_source,
 	utm_medium,
 	utm_campaign,
+	-- TODO [ODS]: we have a bug in this rule. I.g: Cases like 'sitelinkspoa' are considered like RMSP
+	case
+        when utm_campaign_cleaned like '%riodejaneiro%' or utm_campaign_cleaned like '%rj%' then 'Rio de Janeiro'
+        when utm_campaign_cleaned like '%belohorizonte%' THEN 'Belo Horizonte'
+        when utm_campaign_cleaned like '%florian_polis%' THEN 'Florianópolis'
+        when utm_campaign_cleaned like '%bras_lia%' THEN 'Brasília'
+        when utm_campaign_cleaned like '%goi_nia%' THEN 'Goiânia'
+        when utm_campaign_cleaned like '%portoalegre%' or utm_campaign_cleaned like 'rs%' then 'Porto Alegre'
+        when utm_campaign_cleaned like '%curitiba%' then 'Curitiba'
+        when utm_campaign_cleaned like '%campinas%' then 'Campinas'
+        when utm_campaign_cleaned like '%s_opaulo%' then 'RMSP'
+        when utm_campaign_cleaned like '%sp%' then 'RMSP'
+    end as city_campaign,
 	utm_content,
 	utm_term,
 	platform,
