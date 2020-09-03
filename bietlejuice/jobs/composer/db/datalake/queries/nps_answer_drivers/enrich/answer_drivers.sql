88c927124e20fe7826a3e159dabb10f34d7431ef
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-09-03 09:41:06 -0300

    [DINT-227-2] Update ts_answer_sent in NPS answer_drivers (#3385)
    * adding new queues and reprocess DAG in CRM closing
    
    * Revert "adding new queues and reprocess DAG in CRM closing"
    
    This reverts commit 4bc32df3b51347ec4b3af8ee3f349359981f5ba0.
    
    * updating ts_answer_sent_local in nps answer drivers

--- bietlejuice/jobs/composer/db/datalake/queries/nps_answer_drivers/enrich/answer_drivers.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/nps_answer_drivers/enrich/answer_drivers.sql
@@ -1,8 +1,8 @@
 WITH house_contract_tags AS (
 	SELECT
-                id_answer,
-                ts_answer_sent,
-                MAX(CASE WHEN tag_name <> 'Contrato' THEN CAST(tag_value AS BIGINT) END) AS id_house,
+		id_answer,
+		ts_answer_sent_local,
+		MAX(CASE WHEN tag_name <> 'Contrato' THEN CAST(tag_value AS BIGINT) END) AS id_house,
 		MAX(CASE WHEN tag_name = 'Contrato' THEN CAST(tag_value AS BIGINT) END) AS id_contract
 	FROM datalake_tracksale.answer_tags
 	WHERE tag_name IN ('Cu00f3d do imu00f3vel','Cód Imóvel','Cód do imóvel','Contrato')
@@ -11,7 +11,7 @@ WITH house_contract_tags AS (
 driver_tags AS (
 	SELECT
 		at1.id_answer,
-		at1.ts_answer_sent,
+		at1.ts_answer_sent_local,
 		CAST((CASE WHEN at1.tag_value = 'house_listing' THEN at2.tag_value END) AS BIGINT) AS id_house_listing,
 		CAST((CASE WHEN at1.tag_value = 'booking' THEN at2.tag_value END) AS BIGINT) AS id_booking,
 		CASE WHEN at1.tag_value = 'talk to agent' THEN at2.tag_value END AS id_tta,
@@ -32,7 +32,7 @@ combined_drivers AS (
 		dt.id_tta,
 		dt.id_offer_context,
 		COALESCE(dt.id_contract,hct.id_contract) AS id_contract,
-		COALESCE(dt.ts_answer_sent, hct.ts_answer_sent) AS ts_answer_sent
+		COALESCE(dt.ts_answer_sent_local, hct.ts_answer_sent_local) AS ts_answer_sent_local
 	FROM driver_tags dt
 	FULL JOIN house_contract_tags hct
 		ON hct.id_answer = dt.id_answer
@@ -40,7 +40,7 @@ combined_drivers AS (
 ebdb_house AS (
 	SELECT
 		cd.id_answer,
-		cd.ts_answer_sent,
+		cd.ts_answer_sent_local,
 		COALESCE(lh.id,sh.id) AS id_house
 	FROM combined_drivers cd
 	LEFT JOIN datalake_ebdb_clean.house lh
@@ -53,13 +53,13 @@ ebdb_house AS (
 ebdb_house_listing AS (
 	SELECT
 		eh.id_answer,
-		eh.ts_answer_sent,
-		MAX(hl.id_house_listing) AS id_house_listing -- preventing duplicates if ts_answer_sent is right at versioning transition
+		eh.ts_answer_sent_local,
+		MAX(hl.id_house_listing) AS id_house_listing -- preventing duplicates if ts_answer_sent_local is right at versioning transition
 	FROM ebdb_house eh
 	INNER JOIN datalake_ebdb_listing.house_listing hl
 		ON hl.id_house = eh.id_house
-	WHERE eh.ts_answer_sent >= hl.ts_listing_version_start
-		AND eh.ts_answer_sent <= hl.ts_listing_version_end
+	WHERE eh.ts_answer_sent_local >= hl.ts_listing_version_start
+		AND eh.ts_answer_sent_local <= hl.ts_listing_version_end
 	GROUP BY 1,2
 ),
 listing_drivers AS (
@@ -71,11 +71,11 @@ listing_drivers AS (
 		ON cd.id_answer = ehl.id_answer
 ),
 ebdb_listing AS (
-	SELECT 
+	SELECT
 		ld.id_answer,
 		ld.id_house_listing
-	FROM listing_drivers ld 
-	INNER JOIN datalake_ebdb_listing.house_listing hl 
+	FROM listing_drivers ld
+	INNER JOIN datalake_ebdb_listing.house_listing hl
 		ON hl.id_house_listing = ld.id_house_listing
 ),
 ebdb_booking AS (
@@ -83,16 +83,16 @@ ebdb_booking AS (
 		cd.id_answer,
 		cd.id_booking
 	FROM combined_drivers cd
-	INNER JOIN datalake_ebdb_clean.booking b 
+	INNER JOIN datalake_ebdb_clean.booking b
 		ON b.id = cd.id_booking
 ),
 unmounted_offers AS (
-	SELECT 
-        id_answer,
-        id_offer_context,
-        CASE WHEN SUBSTRING(CAST(id_offer_context AS STRING),-1,1) = '2' THEN (id_offer_context - 2)/100.0
-            END AS id_offer -- converting sk_offer to id_offer to validate values at EBDB
-    FROM driver_tags
+	SELECT
+		id_answer,
+		id_offer_context,
+		CASE WHEN SUBSTRING(CAST(id_offer_context AS STRING),-1,1) = '2' THEN (id_offer_context - 2)/100.0
+			END AS id_offer -- converting sk_offer to id_offer to validate values at EBDB
+	FROM driver_tags
 ),
 ebdb_offer AS (
 	SELECT
