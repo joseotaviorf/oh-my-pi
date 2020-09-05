493d87af20776a955a323dcb89d810d6c3aca51b
Amom Mendes <amommendes@hotmail.com>
2020-09-05 14:49:21 -0300

    [DIE-178] Change cluster configuration (#3397)
    * :zap: [DIE-178] Change cluster configuration
    
    * :rotating_light: Black fix

--- bietlejuice/jobs/composer/dags/enrich_tracksale/enrich_tracksale.py
+++ bietlejuice/jobs/composer/dags/enrich_tracksale/enrich_tracksale.py
@@ -26,7 +26,9 @@ SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/base"
 
 LOGS_OUTPUT_PATH = f"s3://{Variable.get('databricks_s3_bucket')}/logs/jobs/{DAG_ID}"
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_bietlejuice_enrich_tracksale", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 LIBRARIES_DESCRIPTION = Variable.get(
