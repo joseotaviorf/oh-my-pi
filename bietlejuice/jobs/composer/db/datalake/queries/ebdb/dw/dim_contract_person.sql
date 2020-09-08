404a66042d8e0107337dcc44dbb172ecc7c814f6
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-09-08 14:44:56 -0300

    [DINT-210] [DTSC-161] Adjust ongoing contracts aggregation by cpf in dim_contract_person (#3357)
    * adding new queues and reprocess DAG in CRM closing
    
    * Revert "adding new queues and reprocess DAG in CRM closing"
    
    This reverts commit 4bc32df3b51347ec4b3af8ee3f349359981f5ba0.
    
    * adjust aggregation on cpf in dim contract person

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb/dw/dim_contract_person.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb/dw/dim_contract_person.sql
@@ -1,84 +1,63 @@
-with contract_to_user as (
-	select 
-		c.id as sk_contract,
-		c.ts_signed,
-		rf.id_client as sk_client,
-		coalesce(h.id_user, pa_b2b_online.id_user,pa_b2b_prime.id_user, -1) as sk_owner,
-  		coalesce(pa_b2b_online.id_partner, pa_b2b_prime.id_partner, -1) as sk_partner
+with contract_users as (
+	select
+		id as id_contract,
+		id_user
+	from datalake_ebdb_clean.contract
+	union all
+	select
+		c.id as id_contract,
+		h.id_user as id_user
 	from datalake_ebdb_clean.contract c
-	join datalake_ebdb_clean.rent_flow rf
-		on c.id_house = rf.id_house
-		and c.id_user = rf.id_client
-	join datalake_ebdb_clean.house h
-		on c.id_house = h.id
-	left join datalake_ebdb_clean.partner_agent pa_b2b_prime
-		on h.id_user = pa_b2b_prime.id_user
-	left join datalake_ebdb_clean.conversion_lead lc
-		on lc.id_house = h.id
-	left join datalake_ebdb_clean.lead l
-		on l.id = lc.id_converted_lead
-	    and l.affiliate_type = 'B2BPartner'
-	left join datalake_ebdb_clean.partner_agent pa_b2b_online
-	  on pa_b2b_online.id_user = l.id_affiliate_has_indicated
+	inner join datalake_ebdb_clean.house h
+		on h.id = c.id_house
 ),
-contract_users as (
-	select 
-		sk_contract,
-		sk_owner as user
-	from contract_to_user 
-	union all
-	select 
-		sk_contract,
-		sk_client as user
-	from contract_to_user
-	union all
-	select 
-		sk_contract,
-		sk_partner as user
-	from contract_to_user
+contract_cpfs as (
+	select
+		id_contract,
+		cpf
+	from datalake_ebdb_clean.contract_person
 ),
-ongoing_contracts as ( 
-	select 
-		id,
-		case when status in ('Ativo','Finalizado') 
+ongoing_contracts as (
+	select
+		id as id_contract,
+		case when status in ('Ativo','Finalizado')
 			and type <> 'DealOnly'
 			and current_date >= coalesce(date(ts_signed),dt_started,dt_entered)
 			and (current_date < dt_termination or dt_termination is null)
 			then true
 		else false end as is_ongoing_contract
-from datalake_ebdb_clean.contract
-),
-user_contracts as (
-	select 
-		uc.*,
-		coalesce(oc.is_ongoing_contract,false) as is_ongoing_contract
-	from contract_users uc 
-	left join ongoing_contracts oc 
-		on uc.sk_contract = oc.id
-),
-ongoing_agg as (
-	select
-		user,
-		count(case when is_ongoing_contract = true then sk_contract end) as ongoing_contracts
-	from user_contracts
-	group by 1 
+	from datalake_ebdb_clean.contract
 ),
 users_ongoing as (
-	select 
-		user,
-		ongoing_contracts > 0 as has_ongoing_contract
-	from ongoing_agg
+	select
+		cu.id_user,
+		MAX(oc.id_contract) as id_max_ongoing_contract
+	from contract_users cu
+	inner join ongoing_contracts oc
+		on cu.id_contract = oc.id_contract
+	where oc.is_ongoing_contract = true
+	group by 1
+),
+cpfs_ongoing as (
+	select
+		cc.cpf,
+		MAX(oc.id_contract) as id_max_ongoing_contract
+	from contract_cpfs cc
+	inner join ongoing_contracts oc
+		on cc.id_contract = oc.id_contract
+	where oc.is_ongoing_contract = true
+	group by 1
 )
-select 
+select
 	cp.id as sk_contract_person,
 	cp.name as full_name,
 	cp.phone_number,
 	cp.email,
 	cp.cpf as personal_document,
-    case when cp.cpf rlike '([0-9]{3})(.)([0-9]{3})(.)([0-9]{3})(-)([0-9]{2})' then 'CPF'
-		when cp.cpf rlike '([0-9]{2})(.)([0-9]{3})(.)([0-9]{3})(\/)([0-9]{4})(-)([0-9]{2})' then 'CNPJ' 
+	case when cp.cpf rlike '([0-9]{3})(.)([0-9]{3})(.)([0-9]{3})(-)([0-9]{2})' then 'CPF'
+		when cp.cpf rlike '([0-9]{2})(.)([0-9]{3})(.)([0-9]{3})(\/)([0-9]{4})(-)([0-9]{2})' then 'CNPJ'
 	end as personal_document_type,
-	coalesce(uo.has_ongoing_contract,false) as has_ongoing_contract,
+	coalesce(uo.id_max_ongoing_contract,co.id_max_ongoing_contract) > 0 as has_ongoing_contract,
 	replace(lower(cp.gender), 'o', 'e') as gender,
 	case when cp.marital_status = 'Amasiado' then 'common-law marriage'
 		when cp.marital_status = 'Casado' then 'married'
@@ -89,10 +68,12 @@ select
 		when cp.marital_status = 'Viuvo' then 'widower'
 		end as marital_status,
 	cp.state_id as state_code,
-	cp.dt_birth,	
+	cp.dt_birth,
 	cp.ts_created,
 	cp.ts_updated,
 	current_timestamp as ts_load
 from datalake_ebdb_clean.contract_person cp
-left join users_ongoing uo 
-	on uo.user = cp.id_user
+left join users_ongoing uo
+	on uo.id_user = cp.id_user
+left join cpfs_ongoing co
+	on co.cpf = cp.cpf
