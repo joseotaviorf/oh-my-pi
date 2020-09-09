8a965551e8f6690cfe3c5615f6afde313be744a1
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2020-09-09 17:49:43 -0300

    fix cluster config (#3416)

--- bietlejuice/jobs/composer/dags/enrich_ebdb_listing_jobs/enrich_ebdb_listing_jobs.py
+++ bietlejuice/jobs/composer/dags/enrich_ebdb_listing_jobs/enrich_ebdb_listing_jobs.py
@@ -30,11 +30,13 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_minimum_resources_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 LIBRARIES_DESCRIPTION = Variable.get(
-    "databricks_minimum_resources_cluster", deserialize_json=True
+    "bietlejuice_default_libraries", deserialize_json=True
 )
 
 dag = DAG(
