9e354ec85f61704e77fd3255e6317b14fe63c56f
Luc Somers <lucwsomers@gmail.com>
2020-09-10 08:07:09 -0300

    [DEMKT-156] - Ingest ClassifiedsResponder DB Tables to Raw (#3409)
    * raw
    
    * add external tables
    
    * checkstyle
    
    * remove unnecessary code
    
    * fixes

---
+++ bietlejuice/jobs/composer/dags/classified_leads/classified_leads.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+import pendulum
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+
+
+SOURCE = "classified_leads"
+
+# airflow vars
+ENV = Variable.get("environment")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+
+# spark and databricks vars
+SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/"
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{SOURCE}"
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_minimum_resources_cluster", deserialize_json=True
+)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+LIBRARIES_DESCRIPTION = Variable.get(
+    "bietlejuice_default_libraries", deserialize_json=True
+)
+
+# DAG vars
+DAG_ID = f"bietlejuice.{SOURCE}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2019, 5, 31, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "0 6 * * *"
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=LIBRARIES_DESCRIPTION,
+)
+
+classified_leads_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="classified_leads-to-datalake-raw",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": f"{SPARK_JOB_PATH}/{SOURCE}/load_classified_leads_into_datalake.py",
+            "parameters": [ENV, DATALAKE_BUCKET],
+        }
+    },
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+create_cluster_task >> classified_leads_to_datalake_raw_task >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/dags/classified_leads/spark_jobs/load_classified_leads_into_datalake.py
@@ -0,0 +1,74 @@
+import json
+import logging
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.db import DatabaseEnum, DatalakeMetastoreService
+from bietlejuice.jobs.composer.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.consumers.db_consumers import PostgresConsumer
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+
+JOB_NAME = "load_classified_leads_into_datalake"
+BLOCK_LIST = [
+    "flyway_schema_history",
+    "change_owner_control",
+    "pooling_tracker",
+    "pg_stat_statements",
+]
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("env")
+    parser.add_argument("datalake_bucket")
+    args = parser.parse_args()
+    environment = args.env
+    datalake_bucket = args.datalake_bucket
+    source = "classified_leads"
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    conn_config_json = dbutils.secrets.get(
+        scope="quintoandar", key=DatabaseEnum.CLASSIFIED_LEADS
+    )
+    conn_config = json.loads(conn_config_json)
+    spark_client = SparkClient()
+    consumer = PostgresConsumer(conn_config, spark_client)
+
+    tables = consumer.get_table_names_and_sizes().collect()
+    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
+    metastore_service = SparkMetastoreService(spark_client)
+    s3_loader = S3Loader()
+    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+
+    # create database if not exists
+    database_name = db_info["db_raw_databricks"]
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = db_info["db_raw_path"]
+
+    metastore_service.create_database(database_name)
+
+    for table in tables:
+        if table.table_name not in BLOCK_LIST:
+            df = consumer.get_data_from_table(table.table_name)
+            s3_loader.load_full_table(
+                df=df,
+                database_name=database_name,
+                table_name=table.table_name.lower(),
+                format_options=format_options,
+                database_location=database_location,
+            )
+            spark_metastore_loader.update_metastore(
+                df,
+                database_name,
+                table.table_name.lower(),
+                format_options,
+                database_location,
+            )
