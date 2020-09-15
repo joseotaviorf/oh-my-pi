72392596e1e1839b696a92405fe03e1d21a37814
Isadora Gallerani <isadoragallerani@hotmail.com>
2020-09-15 10:17:03 -0300

    [DINT-244] Adding clean queries for Sale Offer and Monday from Firestore Audit DAG (#3435)
    * adding queries
    
    * casting

---
+++ bietlejuice/jobs/composer/db/datalake/queries/firestore_audit/clean/monday.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT
+    documentId AS id,
+    before AS original_message,
+    after AS updated_message,
+    eventType AS event_type,
+    CAST(timestamp AS TIMESTAMP) AS ts_updated,
+    year,
+    month, 
+    day
+FROM datalake_firestore_raw.monday
+WHERE year={year} AND month={month} AND day={day}
