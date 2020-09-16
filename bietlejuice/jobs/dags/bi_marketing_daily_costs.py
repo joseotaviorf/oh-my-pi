37d68dfaded303d2ba9eb36591a3b0b0fb73706c
felipemiquelim <felipe.miquelim@quintoandar.com.br>
2020-09-16 16:15:00 -0300

    Adjusting tasks dependencies (#3445)

--- bietlejuice/jobs/dags/bi_marketing_daily_costs.py
+++ bietlejuice/jobs/dags/bi_marketing_daily_costs.py
@@ -227,10 +227,11 @@ trigger_bi_marketing_funnels_conversions_task = TriggerDagRunOperator(
     execution_date="{{ execution_date }}",
 )
 
+load_aux_cost_share_files_into_datalake_task >> load_cost_share_rules_into_dw_task
+
 load_fact_marketing_daily_costs_task.set_upstream([
     load_cost_share_rules_into_dw_task,
-    load_shared_manual_costs_to_datalake_task,
-    load_aux_cost_share_files_into_datalake_task
+    load_shared_manual_costs_to_datalake_task
 ])
 
 load_fact_marketing_daily_costs_task >> trigger_bi_marketing_funnels_conversions_task
