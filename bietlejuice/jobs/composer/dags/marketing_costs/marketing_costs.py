46837b9c5ff0780793d6a9981e349e4bd59dca18
Victor Lúcio <victor.lucio10@gmail.com>
2020-09-17 12:41:01 -0300

    cluster config + name convention (#3455)

--- bietlejuice/jobs/composer/dags/marketing_costs/marketing_costs.py
+++ bietlejuice/jobs/composer/dags/marketing_costs/marketing_costs.py
@@ -15,7 +15,7 @@ from bietlejuice.jobs.composer.services import FileService
 
 SOURCE = "marketing_hub"
 TARGET = "marketing_costs"
-DAG_ID = f"bietlejuice.{TARGET}"
+DAG_ID = f"bietlejuice.enrich_{TARGET}"
 ENV = Variable.get("environment")
 
 DATALAKE_BUCKET = Variable.get("datalake_bucket")
@@ -27,7 +27,9 @@ ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
 LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_bietlejuice_marketing_costs_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 LIBRARIES_DESCRIPTION = Variable.get(
     "bietlejuice_default_libraries", deserialize_json=True
