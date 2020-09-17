0b60cb9839d0bd8afde58f75954e6d116cfa31c9
Pedro Henrique Domingos <pedro.domingos@quintoandar.com.br>
2020-09-17 14:35:28 -0300

    [DRFS-56] Add new Retsuko columns to clean (#3443)
    * Add new columns to clean
    
    * Reorder columns
    
    * Rename columns to keep pattern
    
    * Update columns type
    
    * Update column name
    
    * Update column name
    
    * Update column type

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/boleto.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/boleto.sql
@@ -3,12 +3,14 @@ select
     external_id as id_external,
     invoice_id as id_invoice,
     identifier,
-    timestamp(issue_date) as ts_issued,
     due_amount,
-    timestamp(due_date) as ts_due,
     our_number,
     our_number_digit,
     wallet_number,
+    barcode,
+    inputtable_line,
+    timestamp(due_date) as ts_due,
+    timestamp(issue_date) as ts_issued,
     timestamp(created_at) as ts_created
 from
     datalake_retsuko_raw.boleto
--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/contract.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/contract.sql
@@ -2,8 +2,10 @@ select
     id,
     external_id as id_external,
     version, 
-    timestamp(signature_date) as ts_signature,
+    locale as city,
     guarantee,
+    is_b2b,
+    timestamp(signature_date) as ts_signature,
     timestamp(guarantee_start_date) as ts_guaranteed_start,
     timestamp(start_period) as ts_period_started,
     timestamp(start_charge) as ts_charge_started
