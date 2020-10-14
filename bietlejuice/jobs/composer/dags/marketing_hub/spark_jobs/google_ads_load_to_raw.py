5b44a2490de9c5322ff96a46ec8a08795ff95c3f
Victor Lúcio <victor.lucio10@gmail.com>
2020-10-14 15:47:26 -0300

    [DTM-313] - Marketing Hub Missing Report Type (#3561)
    * removed campaing_name column
    
    * removed campaign_name from partition cols

--- bietlejuice/jobs/composer/dags/marketing_hub/spark_jobs/google_ads_load_to_raw.py
+++ bietlejuice/jobs/composer/dags/marketing_hub/spark_jobs/google_ads_load_to_raw.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     metastore_database_name = db_info["db_raw_databricks"]
     database_location = db_info["db_raw_path"]
     metastore_database_location = f"{database_location}{MEDIA}/"
-    partition_cols = ["acc", "campaign_name", "dt"]
+    partition_cols = ["acc", "dt"]
 
     for s3_source_file_path in s3_source_file_paths:
 
