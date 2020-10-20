50720de178aad0c94655653518478b7749c64e2b
Isadora Gallerani <isadoragallerani@hotmail.com>
2020-10-20 16:32:23 -0300

    [DINT-333] Creating Metabase DAG and loading full & incremental raw data  (#3560)
    * creating metabase dag
    
    * adding allow list
    
    * removing comment
    
    * reordering
    
    * raw load for full and incremental
    
    * adding init file
    
    * replacing _ for -
    
    * adding comments
    
    * lint
    
    * changing log
    
    * lower()
    
    * removing libraries and lower()
    
    * suggestion

---
+++ bietlejuice/jobs/composer/dags/metabase/metabase.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+import pendulum
+
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, BaseSubDAG
+
+SOURCE = "metabase"
+
+# airflow vars
+ENV = Variable.get("environment")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+
+# spark and databricks vars
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/"
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{SOURCE}"
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+# dag vars
+DAG_ID = f"bietlejuice.{SOURCE}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2020, 7, 27, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
+local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+
+# every table to be loaded must be here, with its db name and extraction type
+JOBS_EXTRACTION_TYPE = [
+    {"table_name": "core_user", "extraction_type": "incremental"},
+    {"table_name": "metabase_database", "extraction_type": "incremental"},
+    {"table_name": "metabase_field", "extraction_type": "incremental"},
+    {"table_name": "metabase_table", "extraction_type": "incremental"},
+    {"table_name": "pulse", "extraction_type": "incremental"},
+    {"table_name": "pulse_channel", "extraction_type": "incremental"},
+    {"table_name": "report_card", "extraction_type": "incremental"},
+    {"table_name": "report_dashboard", "extraction_type": "incremental"},
+    {"table_name": "report_dashboardcard", "extraction_type": "incremental"},
+    {"table_name": "collection", "extraction_type": "full"},
+    {"table_name": "pulse_card", "extraction_type": "full"},
+    {"table_name": "pulse_channel_recipient", "extraction_type": "full"},
+    {"table_name": "query", "extraction_type": "full"},
+    {"table_name": "query_execution", "extraction_type": "full"},
+    {"table_name": "revision", "extraction_type": "full"},
+    {"table_name": "view_log", "extraction_type": "full"},
+]
+
+# task builders
+
+
+def create_tables_sub_dag(sub_dag_name, table_name, extraction_type):
+    tables_sub_dag = BaseSubDAG(
+        sub_dag_name=sub_dag_name,
+        dag_name=DAG_ID,
+        schedule_interval=MAIN_SCHEDULE_INTERVAL,
+        start_date=MAIN_START_DATE,
+    )._build_local_dag()
+
+    slugged_table_name = table_name.replace("_", "-")
+
+    QuintoAndarDatabricksSubmitRunOperator(
+        task_id=f"load-{slugged_table_name}-to-raw",
+        dag=tables_sub_dag,
+        json={
+            "spark_python_task": {
+                "python_file": SPARK_JOBS_PATH
+                + f"load_{extraction_type}_data_into_datalake_raw.py",
+                "parameters": [ENV, SOURCE, DATALAKE_BUCKET, "{{ ds }}", table_name],
+            }
+        },
+    )
+
+    return tables_sub_dag
+
+
+# dag definition
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+# creating sub dags
+for job_extraction_type in JOBS_EXTRACTION_TYPE:
+    tables_sub_dag_task = BaseSubDAG.get_sub_dag_operator(
+        dag=dag,
+        sub_dag_name=job_extraction_type["table_name"],
+        sub_dag_func=create_tables_sub_dag,
+        table_name=job_extraction_type["table_name"],
+        extraction_type=job_extraction_type["extraction_type"],
+    )
+    create_cluster_task >> tables_sub_dag_task >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/dags/metabase/spark_jobs/load_incremental_data_into_datalake_raw.py
@@ -0,0 +1,121 @@
+import json
+import logging
+
+from argparse import ArgumentParser
+from datetime import datetime
+from collections import OrderedDict
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+
+from bietlejuice.jobs.composer.consumers.db_consumers import PostgresConsumer
+
+from bietlejuice.jobs.composer.base.db.database_enum import DatabaseEnum
+from bietlejuice.jobs.composer.base.db import DatalakeMetastoreService
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+from bietlejuice.jobs.composer.services import FileService
+
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+
+from bietlejuice.jobs.composer.base.db import QUERIES_DATALAKE_PATH
+
+JOB_NAME = "load_incremental_data_into_datalake_raw"
+
+QUERIES_METABASE_DATALAKE_PATH = QUERIES_DATALAKE_PATH + "metabase"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument("environment", help="forno/prod values")
+    parser.add_argument("source", help="name of the source")
+    parser.add_argument("datalake_bucket", help="bucket value in forno/prod")
+    parser.add_argument("execution_date", help="execution date in str format")
+    parser.add_argument("table_name", help="table name")
+
+    args = parser.parse_args()
+
+    environment = args.environment
+    source = args.source
+    datalake_bucket = args.datalake_bucket
+    execution_date = args.execution_date
+    table_name = args.table_name.lower()
+
+    logger.info(
+        f"m=__main__, environment={environment}, source={source}, datalake_bucket={datalake_bucket}, "
+        f"execution_date={execution_date}, table_name={table_name}, msg=Starting spark job..."
+    )
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    dt_execution = datetime.strptime(execution_date, "%Y-%m-%d")
+    partitions = OrderedDict(
+        [
+            ("year", (dt_execution.year)),
+            ("month", (dt_execution.month)),
+            ("day", (dt_execution.day)),
+        ]
+    )
+
+    partition_cols = list(partitions.keys())
+
+    # incremental load of a Postgres db type to our data lake with Spark
+    raw_query = FileService().get_query_from_file_name(
+        QUERIES_METABASE_DATALAKE_PATH + "/raw/" + table_name + ".sql"
+    )
+
+    conn_config = dbutils.secrets.get(scope="quintoandar", key=DatabaseEnum.METABASE)
+    conn_config_json = json.loads(conn_config)
+    spark_client = SparkClient()
+
+    consumer = PostgresConsumer(conn_config_json, spark_client)
+
+    df = consumer.get_data_from_query(raw_query.format(**partitions))
+
+    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
+    spark_metastore_service = SparkMetastoreService(spark_client)
+
+    logger.info("m=__main__, msg=Creating database in Spark Metastore if not exists...")
+    spark_metastore_service.create_database(db_info["db_raw_databricks"])
+
+    s3_loader = S3Loader()
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+
+    database_name = db_info["db_raw_databricks"]
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = db_info["db_raw_path"]
+
+    s3_loader.load_incremental_table(
+        df=df,
+        database_name=database_name,
+        table_name=table_name,
+        format_options=format_options,
+        database_location=database_location,
+        partition_cols=partition_cols,
+    )
+
+    spark_metastore_loader.update_metastore(
+        df,
+        database_name,
+        table_name,
+        format_options,
+        database_location,
+        partition_cols,
+        force_recreate=False,
+    )
+
+    spark_metastore_service.create_new_partitions_from_df(
+        database_name=database_name,
+        table_name=table_name,
+        df=df,
+        partition_cols=partition_cols,
+    )
+
+    spark_metastore_service.refresh_table(database_name, table_name)
