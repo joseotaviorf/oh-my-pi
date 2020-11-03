ff54bd41d7cbf68cad449065a7f09d55fa5e6a6a
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-11-03 18:33:43 -0300

    [DTSC-348] Add enriched query for inbound call locations (#3631)
    * adding query for inbound call locations
    
    * update enrich in call locations

---
+++ bietlejuice/jobs/composer/db/datalake/queries/bigfone_event/enrich/inbound_call_locations.sql
@@ -0,0 +1,16 @@
+WITH last_location_events AS (
+	SELECT
+		GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.call_sid') AS id_call,
+		MAX(id_event) AS id_last_event
+	FROM datalake_bigfone.call_flex_events
+	WHERE GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.from_city') IS NOT NULL
+	GROUP BY 1
+)
+SELECT
+	lle.id_call,
+	NULLIF(GET_JSON_OBJECT(cfe.metadata,'$.event_data.TaskAttributes.from_city'),'') AS from_city,
+	NULLIF(GET_JSON_OBJECT(cfe.metadata,'$.event_data.TaskAttributes.from_state'),'') AS from_state,
+	NULLIF(GET_JSON_OBJECT(cfe.metadata,'$.event_data.TaskAttributes.from_country'),'') AS from_country
+FROM datalake_bigfone.call_flex_events cfe
+INNER JOIN last_location_events lle
+	ON cfe.id_event = lle.id_last_event
