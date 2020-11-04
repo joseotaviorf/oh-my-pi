b2f7664c54e18ea86dea62c4593396ce4ffce8c3
Carol Maia <53832259+anacarolinamaia@users.noreply.github.com>
2020-11-04 09:05:14 -0300

    [DTSC-341] Add enriched queries for Bigfone Twilio IVR events (#3622)
    * adding new queues and reprocess DAG in CRM closing
    
    * Revert "adding new queues and reprocess DAG in CRM closing"
    
    This reverts commit 4bc32df3b51347ec4b3af8ee3f349359981f5ba0.
    
    * add enriching queries for bigfone twilio IVR events
    
    * including CSAT info
    
    * capitalize command
    
    * capitalize commands
    
    * fix incremental cleaning
    
    * fix incremental cleaning
    
    * adjusting names
    
    * fix indentation
    
    * update enrich in call ivr events
    
    * update enrich in ivr paths
    
    * fix table name
    
    * fix table name

---
+++ bietlejuice/jobs/composer/db/datalake/queries/bigfone_event/enrich/call_ivr_events.sql
@@ -0,0 +1,17 @@
+SELECT
+	id,
+	GET_JSON_OBJECT(metadata,'$.event_data.Sid') AS id_event,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.callSid') AS id_call,
+	NULLIF(GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.teravozCallId'),'') AS id_call_teravoz,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskSid') AS id_task,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskChannelSid') AS id_channel,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.from') AS from_number,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.to') AS to_number,
+	event AS event_type,
+	GET_JSON_OBJECT(metadata,'$.event_data.TaskCanceledReason') AS task_cancelation_reason,
+	CAST(GET_JSON_OBJECT(metadata,'$.event_data.TaskPriority') AS INT) AS task_priority,
+	CAST(GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.csat-1') AS INT) AS csat_1,
+	CAST(GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.csat-2') AS INT) AS csat_2,
+	ts_created_local,
+	ts_received_local
+FROM datalake_bigfone.twilio_ivr_events
---
+++ bietlejuice/jobs/composer/db/datalake/queries/bigfone_event/enrich/call_ivr_paths.sql
@@ -0,0 +1,15 @@
+WITH event_steps AS (
+	SELECT
+		id,
+		id_event,
+		EXPLODE(SPLIT(steps,'},')) as step
+	FROM datalake_bigfone.twilio_ivr_events
+)
+SELECT
+	id,
+	id_event,
+	REGEXP_EXTRACT(step,'([A-Za-z0-9_]+)',1) AS step_name,
+	REGEXP_EXTRACT(step,'("event":")(\\w+)',2) AS event_type,
+	CAST(REGEXP_EXTRACT(step,'("digits":")(\\d+)',2) AS INT) AS digits,
+	FROM_UNIXTIME(CAST(REGEXP_EXTRACT(step,'("timestamp":)(\\d+)',2) AS BIGINT)/1000) AS ts_created
+FROM event_steps
