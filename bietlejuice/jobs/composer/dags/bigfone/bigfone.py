52744bf684c90549a4d9e32abb1bae56fac4f8ac
Igor Gustavo Hoelscher <19557581+roelschr@users.noreply.github.com>
2020-11-05 15:30:05 -0300

    [MLOP-560] Load Bigfone Call and Queued to Datalake (#3629)
    * add bigfone etl files
    
    * remove database enum usage
    
    * loading to raw
    
    * clean queries
    
    * lint
    
    * Update bietlejuice/jobs/composer/dags/bigfone/spark_jobs/load_bigfone_into_datalake.py
    
    Co-authored-by: Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
    
    * lint
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    Co-authored-by: Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>

---
+++ bietlejuice/jobs/composer/dags/bigfone/bigfone.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+import pendulum
+
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
+from bietlejuice.jobs.composer.services import FileService
+
+SOURCE = "bigfone"
+
+# airflow vars
+ENV = Variable.get("environment")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+LOAD_BIGFONE_INTO_DATALAKE_RAW_FILE_PATH = (
+    S3_PREFIX + f"/spark_jobs/{SOURCE}/load_bigfone_into_datalake.py"
+)
+
+# spark and databricks vars
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{SOURCE}"
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+# dag vars
+DAG_ID = f"bietlejuice.{SOURCE}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2019, 9, 9, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
+local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+bigfone_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="bigfone-to-datalake-raw",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": LOAD_BIGFONE_INTO_DATALAKE_RAW_FILE_PATH,
+            "parameters": [ENV, DATALAKE_BUCKET],
+        }
+    },
+)
+
+clean_sub_dag = DatalakeSubDAG(
+    dag_id=DAG_ID,
+    start_date=MAIN_START_DATE,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    layer=LayerEnum.CLEAN,
+    database_base_name=SOURCE,
+    relative_query_path=SOURCE,
+    spark_job_paths=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+file_list = FileService.list_sql_files_without_extension_from_layer(
+    SOURCE, LayerEnum.CLEAN.value
+)
+
+clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(dag, file_list)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+create_cluster_task >> bigfone_to_datalake_raw_task >> list(
+    clean_sub_dags.values()
+) >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/dags/bigfone/spark_jobs/load_bigfone_into_datalake.py
@@ -0,0 +1,68 @@
+import json
+import logging
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+from bietlejuice.jobs.composer.base.db import DatabaseEnum, DatalakeMetastoreService
+from bietlejuice.jobs.composer.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.consumers.db_consumers import PostgresConsumer
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+
+SOURCE = "bigfone"
+JOB_NAME = "load_bigfone_into_datalake"
+ALLOW_LIST = ["Call", "Queued"]
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("env")
+    parser.add_argument("datalake_bucket")
+    args = parser.parse_args()
+    environment = args.env
+    datalake_bucket = args.datalake_bucket
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    conn_config_json = dbutils.secrets.get(
+        scope="quintoandar", key=DatabaseEnum.BIGFONE
+    )
+    conn_config = json.loads(conn_config_json)
+    spark_client = SparkClient()
+    postgres_consumer = PostgresConsumer(conn_config, spark_client)
+
+    tables = postgres_consumer.get_table_names_and_sizes().collect()
+    db_info = DatalakeMetastoreService.get_db_info(environment, SOURCE, datalake_bucket)
+    metastore_service = SparkMetastoreService(spark_client)
+    s3_loader = S3Loader()
+    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+
+    # create database if not exists
+    database_name = db_info["db_raw_databricks"]
+    metastore_service.create_database(database_name)
+
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = db_info["db_raw_path"]
+
+    for table in tables:
+        if table.table_name in ALLOW_LIST:
+            df = postgres_consumer.get_data_from_table(table.table_name)
+            s3_loader.load_full_table(
+                df=df,
+                database_name=database_name,
+                table_name=table.table_name.lower(),
+                format_options=format_options,
+                database_location=database_location,
+            )
+            spark_metastore_loader.update_metastore(
+                df,
+                database_name,
+                table.table_name.lower(),
+                format_options,
+                database_location,
+            )
