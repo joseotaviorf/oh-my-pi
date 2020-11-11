beb1ebe0070b354098e4fb4cd4e2bf9be13499e7
Isadora Gallerani <isadoragallerani@hotmail.com>
2020-11-11 15:29:21 -0300

    removing datamart (#3682)

--- bietlejuice/db/datalake/queries/datamarts/sales_company_outsourcing.sql
+++
@@ -1,5 +0,0 @@
-select
-    p.referenceid as sk_lead,
-    salescompany as sales_company
-from datalake_wololo_raw_prod.prospect p
-join datalake_wololo_raw_prod.prospectdimension pd on pd.id = p.dimensionentity_id
