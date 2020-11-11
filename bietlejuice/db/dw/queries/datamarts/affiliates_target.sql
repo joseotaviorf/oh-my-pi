c968641dfd6eb52457bd0f0f31efb1182b35e110
Danilo <danilo.oliveira@quintoandar.com.br>
2020-11-11 18:33:33 -0300

    Removing unused data marts (#3694)
    Also running:
    drop table datamarts.affiliates_target;
    drop table datamarts.agent_slots_hourly;
    drop table datamarts.agents_compensation;
    drop table datamarts.first_access_to_booked_listing_viewed;
    drop table datamarts.ongoing_listings_with_listing_page_views;
    drop table datamarts.task_pool;
    drop table datamarts.weekly_targets;

--- bietlejuice/db/dw/queries/datamarts/affiliates_target.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    day::bigint,
-    indicaai_target_listings::bigint,
-    doorman_target_listings::bigint,
-    indicaai_target_leads::bigint,
-    doorman_target_leads::bigint,
-    current_timestamp as ts_load
-FROM
-    datalake_raw.affiliates_target
