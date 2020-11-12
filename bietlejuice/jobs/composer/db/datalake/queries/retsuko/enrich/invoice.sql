890637799999b93b2881075ae6423acef0cb0655
Pedro Henrique Domingos <pedro.domingos@quintoandar.com.br>
2020-11-12 17:36:29 -0300

    [DRFS209-2]Create enrich invoice (#3693)
    * Create enrich invoice
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update invoice.sql
    
    * Edit indentation
    
    * Update for better running query
    
    * Update invoice query
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
@@ -0,0 +1,29 @@
+WITH invoice_replace AS (
+    SELECT
+        i.id_external AS id_invoice,
+        REPLACE(i.purpose, '-', ' ') AS invoice_frequency,
+        COALESCE(REPLACE(i.status, '-', ' '), 'not invoiceable') AS payment_status,
+        CASE WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') NOT LIKE '%contract%' THEN REPLACE(at.type, '-', ' ')
+                WHEN REPLACE(af.type, '-', ' ') NOT LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN REPLACE(af.type, '-', ' ')
+                WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN 'contract'
+            ELSE NULL END AS invoice_user,
+        i.ts_sent,
+        i.ts_due,
+        i.ts_paid
+    FROM datalake_retsuko_clean.invoice i
+    LEFT JOIN datalake_retsuko_clean.entry e
+            ON e.id_invoice = i.id
+    INNER JOIN datalake_retsuko_clean.account af 
+        ON e.id_from_account = af.id
+    INNER JOIN datalake_retsuko_clean.account at 
+        ON e.id_to_account = at.id
+)
+SELECT
+    ir.id_invoice,
+    ir.invoice_frequency,
+    ir.payment_status,
+    ir.invoice_user,   
+    ir.ts_sent,
+    ir.ts_due,
+    ir.ts_paid
+FROM invoice_replace ir
