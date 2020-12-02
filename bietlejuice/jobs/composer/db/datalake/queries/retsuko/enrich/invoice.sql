b78817bb337122c4a7a168e451d4cdbc82dfd919
Pedro Henrique Domingos <pedro.domingos@quintoandar.com.br>
2020-12-02 19:00:00 -0300

    [DRFS-241] Remove duplicated cases (#3788)
    * Remove duplicated cases
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/enrich/invoice.sql
@@ -3,10 +3,11 @@ WITH invoice_replace AS (
         i.id_external AS id_invoice,
         REPLACE(i.purpose, '-', ' ') AS invoice_frequency,
         COALESCE(REPLACE(i.status, '-', ' '), 'not invoiceable') AS payment_status,
-        CASE WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') NOT LIKE '%contract%' THEN REPLACE(at.type, '-', ' ')
+        MAX(CASE WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') NOT LIKE '%contract%' THEN REPLACE(at.type, '-', ' ')
                 WHEN REPLACE(af.type, '-', ' ') NOT LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN REPLACE(af.type, '-', ' ')
                 WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN 'contract'
-            ELSE NULL END AS invoice_user,
+            ELSE NULL
+        END) AS invoice_user,
         i.ts_sent,
         i.ts_due,
         i.ts_paid
@@ -17,6 +18,7 @@ WITH invoice_replace AS (
         ON e.id_from_account = af.id
     INNER JOIN datalake_retsuko_clean.account at 
         ON e.id_to_account = at.id
+    GROUP BY 1,2,3,5,6,7
 )
 SELECT
     ir.id_invoice,
