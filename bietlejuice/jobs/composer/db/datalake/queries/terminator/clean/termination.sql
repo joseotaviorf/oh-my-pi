14767387c23ff137008a2c64130ee2d85ff849a0
Danilo <danilo.oliveira@quintoandar.com.br>
2020-12-02 18:16:12 -0300

    [DTSC-456] Replacing Terminator column that do not exist anymore (#3795)
    * Replacing Terminator column that do not exist anymore by the new one
    
    * Reordering columns

--- bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/termination.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/termination.sql
@@ -1,16 +1,15 @@
 SELECT
     id,
     contract_id AS id_contract,
-    reason,
-    date AS dt_termination,
-    created_at AS ts_created,
-    updated_at AS ts_updated,
-    status,
-    canceled_at AS ts_canceled,
-    vacancy_date AS dt_vacancy,
-    source,
-    requested_by,
     exit_inspection_id AS id_exit_inspection,
-    feedback
+    feedback,
+    requested_by,
+    source,
+    status,
+    date AS dt_termination,
+    vacancy_date AS dt_vacancy,
+    canceled_at AS ts_canceled,
+    created_at AS ts_created,
+    updated_at AS ts_updated
 FROM
     datalake_terminator_raw.termination
