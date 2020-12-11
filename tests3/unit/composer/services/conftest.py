7839fdf74f5df8adb9ab631ae772d8c8e21f45c5
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2020-12-11 17:13:01 -0300

    [DF-312] Add SecretsService for Databricks (#3842)
    * Add SecretsService

--- tests3/unit/composer/services/conftest.py
+++ tests3/unit/composer/services/conftest.py
@@ -1,18 +1,8 @@
-from unittest.mock import Mock
-
 import pytest
 
-from bietlejuice.jobs.composer.services.metastore_services.hive_metastore_service import (
-    HiveMetastoreService,
-)
+from bietlejuice.jobs.composer.services.secrets_service import SecretsService
 
 
 @pytest.fixture()
-def hive_metastore_service():
-    mocked_open_conn = Mock()
-    mocked_open_conn.return_value = Mock()
-
-    mocked_client = Mock()
-    mocked_client.__enter__ = mocked_open_conn
-    mocked_client.__exit__ = Mock()
-    return HiveMetastoreService(client=mocked_client)
+def secrets_service():
+    return SecretsService()
