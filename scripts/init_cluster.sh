d27738646774359ab367a28de446e1a93e418629
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2021-01-04 14:09:25 -0300

    Versioning cluster init scripts (#3871)

--- scripts/init_cluster.sh
+++ scripts/init_cluster.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+
+# This script is used for initializing clusters dependencies in Databricks
+# and it is manually placed inside 5a-artifacts/bi-etl-ejuice
+
 echo "Downgrading pip"
 /databricks/python/bin/pip install pip==18.1
+/databricks/python/bin/pip install amqp==2.6.1
---
+++ scripts/libs_install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# This script is used for initializing clusters dependencies in Databricks
+# and it is manually placed inside 5a-artifacts/bi-etl-ejuice
+
+echo "Installing libs"
+/databricks/python/bin/pip install pip==18.1
+/databricks/python/bin/pip install amqp==2.6.1
+/databricks/python/bin/pip install awscli
+
+aws s3 cp s3://5a-artifacts/bi-etl-ejuice/bi_etl_ejuice-latest-py3-none-any.whl /bi_etl_ejuice-latest-py3-none-any.whl
+aws s3 cp s3://5a-artifacts/python-logger/quintoandar_logger-0.8.0-py3-none-any.whl /quintoandar_logger-0.8.0-py3-none-any.whl
+
+/databricks/python/bin/pip install /bi_etl_ejuice-latest-py3-none-any.whl
+/databricks/python/bin/pip install /quintoandar_logger-0.8.0-py3-none-any.whl
