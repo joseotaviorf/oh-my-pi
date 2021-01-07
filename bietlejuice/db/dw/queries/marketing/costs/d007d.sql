7f7a57dbfcc2973fd4799522e5f6b30dc96246dd
Gustavo Moura <54562374+gustsm@users.noreply.github.com>
2021-01-07 11:22:46 -0300

    Changing perf. mkt. Demand cost share rules to classificatory instead of eliminatory. (#3840)

--- bietlejuice/db/dw/queries/marketing/costs/d007d.sql
+++ bietlejuice/db/dw/queries/marketing/costs/d007d.sql
@@ -1,17 +1,25 @@
-select distinct
-        dd.sk_date::integer as sk_date,
-        dr.city_group::varchar as city_group,
-        (dense_rank() over (partition by dd.sk_date, dr.city_group order by fhs.sk_house_listing) +
-            dense_rank() over (partition by dd.sk_date, dr.city_group order by fhs.sk_house_listing desc) - 1) /
-        (dense_rank() over (partition by dd.sk_date order by fhs.sk_house_listing) +
-            dense_rank() over (partition by dd.sk_date order by fhs.sk_house_listing desc) - 1)::float as share
-    from dim_date dd
-    join fact_house_listing_status fhs
-        on dd.sk_date between fhs.sk_status_start_date and coalesce(nullif(fhs.sk_status_end_date, -1), to_char(current_date - 1, 'YYYYMMDD')::bigint)
-        and fhs.status_history = 'publicado'
-        and fhs.sk_status_start_date != -1
-    join dim_region dr
-        on dr.sk_region = fhs.sk_region
-        and city_group is not null
-    where dd.sk_date between 20180101 and cast(TO_CHAR(getdate() -1, 'YYYYMMDD') as integer)
-        and dr.city_group not in ('Goiânia', 'Belo Horizonte')
+SELECT DISTINCT
+    dd.sk_date::INTEGER AS  sk_date,
+    dr.city_group::VARCHAR AS city_group,
+    (DENSE_RANK() OVER(PARTITION BY dd.sk_date, dr.city_group ORDER BY fhs.sk_house_listing) +
+     DENSE_RANK() OVER(PARTITION BY dd.sk_date, dr.city_group ORDER BY fhs.sk_house_listing DESC) - 1) /
+         (DENSE_RANK() OVER(PARTITION BY dd.sk_date ORDER BY fhs.sk_house_listing) +
+         DENSE_RANK() OVER(PARTITION BY dd.sk_date ORDER BY fhs.sk_house_listing DESC) - 1)::FLOAT AS share
+FROM
+    dim_date AS dd
+    JOIN fact_house_listing_status AS fhs
+        ON dd.sk_date BETWEEN fhs.sk_status_start_date
+                                AND COALESCE(NULLIF(fhs.sk_status_end_date, -1), TO_CHAR(CURRENT_DATE - 1, 'YYYYMMDD')::BIGINT)
+        AND fhs.status_history = 'publicado'
+        AND fhs.sk_status_start_date != -1
+    JOIN dim_region AS dr
+        ON dr.sk_region = fhs.sk_region
+        AND city_group IN (
+            'Brasília',
+            'Campinas',
+            'Curitiba',
+            'Florianópolis',
+            'Porto Alegre',
+            'RMSP',
+            'Rio de Janeiro'
+        )
