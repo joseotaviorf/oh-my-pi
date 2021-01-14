654a57103de7d7c92844bee8a4f184c9b388af92
Luc Somers <lucwsomers@gmail.com>
2021-01-14 10:12:07 -0300

    [DTM-605] - RTB Raw + Clean (#3930)
    * rtb
    
    * fix versioning
    
    * Version 0.1.5
    
    * remove stats
    
    * fix misspell
    
    * fix version
    
    * add json
    
    * bump version
    
    * sql formatting
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/marketing_costs/rtb_campaigns/clean/rtb_campaigns.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/marketing_costs/rtb_campaigns/clean/rtb_campaigns.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/marketing_costs/rtb_campaigns/clean/rtb_campaigns.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/marketing_costs/rtb_campaigns/clean/rtb_campaigns.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>

---
+++ bietlejuice/jobs/composer/dags/rtb_campaigns/rtb_campaigns.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+import pendulum
+from airflow.models import DAG
+from airflow.models import Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.services import FileService
+
+# ENV setup
+ENV = Variable.get("environment")
+ACCOUNT = Variable.get("rtb_login")
+
+# DAG params setup
+SOURCE = "marketing_costs"
+MEDIA = "rtb_campaigns"
+DAG_ID = f"bietlejuice.{MEDIA}"
+local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2019, 6, 1, 0, 0, 0, tzinfo=local_tz)
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
+# s3 paths setup
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+LOAD_CRITEO_CAMPAIGNS_INTO_DATALAKE_RAW_FILE_PATH = (
+    S3_PREFIX + f"/spark_jobs/{MEDIA}/load_incremental_data_into_datalake_raw.py"
+)
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_ID}"
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
+
+# cluster setup
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+CUSTOM_LIBRARIES = [
+    {
+        "whl": f"{ARTIFACTS_S3_BUCKET}/rtb-api-client-python/"
+        f"quintoandar_rtb_api_client-0.1.6-py2.py3-none-any.whl"
+    }
+]
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=CUSTOM_LIBRARIES,
+)
+
+rtb_campaigns_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="rtb-campaigns-to-datalake-raw",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": LOAD_CRITEO_CAMPAIGNS_INTO_DATALAKE_RAW_FILE_PATH,
+            "parameters": [ENV, SOURCE, MEDIA, DATALAKE_BUCKET, ACCOUNT, "{{ ds }}"],
+        }
+    },
+)
+
+clean_sub_dag = DatalakeSubDAG(
+    dag_id=DAG_ID,
+    start_date=MAIN_START_DATE,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    layer=LayerEnum.CLEAN,
+    database_base_name=SOURCE,
+    relative_query_path=f"{SOURCE}/{MEDIA}",
+    spark_job_paths=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+file_list = FileService.list_sql_files_without_extension_from_layer(
+    f"{SOURCE}/{MEDIA}", LayerEnum.CLEAN.value
+)
+
+clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(
+    dag, file_list, is_incremental=True, partitions=["year", "month", "day"]
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+create_cluster_task >> rtb_campaigns_to_datalake_raw_task >> list(
+    clean_sub_dags.values()
+) >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/dags/rtb_campaigns/spark_jobs/load_incremental_data_into_datalake_raw.py
@@ -0,0 +1,144 @@
+import json
+import logging
+
+from argparse import ArgumentParser
+from datetime import datetime
+
+from quintoandar_logger import QuintoAndarLogger
+from quintoandar_rtb_api_client.clients import RTBClient
+
+from bietlejuice.jobs.composer.base.spark import SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+
+from bietlejuice.jobs.composer.base.spark import SparkDataFrameService
+from bietlejuice.jobs.composer.base.db import DatalakeMetastoreService
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+
+DATABRICKS_SCOPE = "quintoandar"
+JOB_NAME = "load_incremental_data_into_datalake_raw"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+
+schema = StructType(
+    [
+        StructField("subcampaign", StringType(), True),
+        StructField("impsCount", DoubleType(), True),
+        StructField("clicksCount", DoubleType(), True),
+        StructField("campaignCost", DoubleType(), True),
+        StructField("conversionsCount", DoubleType(), True),
+        StructField("conversionsValue", DoubleType(), True),
+        StructField("cr", DoubleType(), True),
+        StructField("ctr", DoubleType(), True),
+        StructField("ecc", DoubleType(), True),
+        StructField("cpc", DoubleType(), True),
+        StructField("roas", DoubleType(), True),
+        StructField("ecps", DoubleType(), True),
+        StructField("subcampaignHash", StringType(), True),
+        StructField("account_hash", StringType(), True),
+        StructField("account_name", StringType(), True),
+        StructField("account_currency", StringType(), True),
+        StructField("account_status", StringType(), True),
+        StructField("cost_attribution_date", StringType(), True),
+    ]
+)
+
+
+def get_api_response(account, execution_date):
+    client_id = account["client_id"]
+    client_secret = account["client_secret"]
+    rtb_client = RTBClient(
+        client_id=client_id, client_secret=client_secret, execution_date=execution_date
+    )
+    try:
+        api_response = rtb_client.get_data()
+        for data in api_response:
+            data["cost_attribution_date"] = data.pop("day")
+            data["conversionsCount"] = float(data["conversionsCount"])
+        return api_response
+    except Exception as e:
+        logger.error(f"m=get_api_response, msg={e}")
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument("environment", help="forno/prod values")
+    parser.add_argument("source", help="name of the source")
+    parser.add_argument("media", help="name of the media")
+    parser.add_argument("datalake_bucket", help="bucket value in forno/prod")
+    parser.add_argument("account", help="account w/ id and secret for api call")
+    parser.add_argument("execution_date", help="execution date in str format")
+
+    args = parser.parse_args()
+
+    logger.info(
+        f"""
+            m={JOB_NAME}, environment={args.environment}, source={args.source}, media={args.media},
+            execution_date={args.execution_date}, datalake_bucket={args.datalake_bucket}, msg=print spark jobs args"
+        """
+    )
+
+    environment = args.environment
+    source = args.source
+    media = args.media
+    datalake_bucket = args.datalake_bucket
+    account = json.loads(args.account)
+    execution_date = args.execution_date
+    partition_cols = ["year", "month", "day"]
+
+    spark_client = SparkClient()
+    api_response = get_api_response(account, execution_date)
+
+    if api_response:
+        df = spark_client.create_dataframe(api_response)
+        dt_execution = datetime.strptime(execution_date, "%Y-%m-%d")
+        df = df.coalesce(1)
+        df = (
+            SparkDataFrameService()
+            .input(df)
+            .create_year_month_day_columns_from_date(dt_execution)
+            .output()
+        )
+
+        datalake_info = DatalakeMetastoreService.get_db_info(
+            environment, source, datalake_bucket
+        )
+        spark_metastore_service = SparkMetastoreService(spark_client)
+        database_name = datalake_info["db_raw_databricks"]
+        spark_metastore_service.create_database(database_name)
+
+        database_location = datalake_info["db_raw_path"]
+        format_options = SparkTableStorageFormat.DEFAULT_RAW
+        table_name = media
+
+        # loaders
+        s3_loader = S3Loader()
+        spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+        s3_loader.load_df(
+            df=df,
+            s3_path=f"{database_location}{table_name}",
+            format_options=format_options,
+            partitions=partition_cols,
+        )
+        spark_metastore_loader.update_metastore(
+            df,
+            database_name,
+            table_name,
+            format_options,
+            database_location,
+            partition_cols,
+            force_recreate=False,
+        )
+        spark_metastore_service.create_new_partitions_from_df(
+            database_name=database_name,
+            table_name=table_name,
+            df=df,
+            partition_cols=partition_cols,
+        )
+        spark_metastore_service.refresh_table(database_name, table_name)
