662ec6d7f4d26e748619340c5d5e24a7fa350d32
Victor Lúcio <victor.lucio10@gmail.com>
2021-01-14 14:08:52 -0300

    [DTM-620] - Google API New Report Type Clean/Enrich (#3941)
    * clean sql
    
    * enrich sql
    
    * minor fixes
    
    * style
    
    * style
    
    * spacing
    
    * spacing
    
    * column reorder

---
+++ bietlejuice/jobs/composer/db/datalake/queries/marketing_costs/google/enrich/google_videos_performance_report.sql
@@ -0,0 +1,22 @@
+SELECT
+	CONCAT(id_external_customer, '-', id_video, '-', campaign_name) AS id,
+	id_video,
+	id_external_customer,
+	id_ad_group,
+	id_campaign,
+	ad_group_name,
+	campaign_name,
+	(campaign_name LIKE 'ZEBRA%') AS is_test_campaign,
+	clicks,
+	cost,
+	device,
+	impressions,
+	account_descriptive_name,
+	acc,
+	load_date,
+	dt_load,
+	dt_created
+FROM
+    datalake_marketing_hub_clean.videos_performance_report
+WHERE 
+    load_date = DATE('{year}-{month}-{day}')
---
+++ bietlejuice/jobs/composer/db/datalake/queries/marketing_hub/google/clean/google_videos_performance_report.sql
@@ -0,0 +1,20 @@
+SELECT
+    BIGINT(ExternalCustomerID) AS id_external_customer,
+    VideoID AS id_video,
+    BIGINT(AdGroupID) AS id_ad_group,
+    BIGINT(CampaignID) AS id_campaign,
+    AdGroup AS ad_group_name,
+    Campaign AS campaign_name,
+    Clicks AS clicks,
+    Cost AS cost,
+    Device AS device,
+    Impressions AS impressions,
+    Account AS account_descriptive_name,
+    acc,
+    Day AS load_date,
+    DATE(Day) AS dt_load,
+    FROM_UNIXTIME(TO_UNIX_TIMESTAMP(dt, 'dd-MM-yyyy'), 'yyyy-MM-dd') AS dt_created
+FROM
+    datalake_marketing_hub_raw.videos_performance_report
+WHERE 
+    FROM_UNIXTIME(TO_UNIX_TIMESTAMP(dt, 'dd-MM-yyyy'), 'yyyy-MM-dd') = DATE('{year}-{month}-{day}')
