e65da57f804dd741a8f9ac14801ef72edaab49d4
Lucas Mendes Mota da Fonseca <lucas.fonseca@quintoandar.com.br>
2021-01-28 17:47:52 -0300

    [DAE-79] Create views for Amplitude events and update offer_submitted_events query (#4006)
    * - add views for 170698_offer_submitted_events; 183049_offer_submitted_events; 170135_offer_submitted_events;
    - update query offer_submitted_events.sql to use views;
    
    * Add columns in query

--- bietlejuice/jobs/composer/db/datalake/queries/amplitude_offer/enrich/offer_submitted_events.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/amplitude_offer/enrich/offer_submitted_events.sql
@@ -1,22 +1,45 @@
-with extracted_utms as (
+with all_apps_events(
     select
         id_user,
         id_app,
-        cast(trim(get_json_object(event_properties, '$.house_id')) as string) as id_house,
-        cast(trim(get_json_object(event_properties, '$.offer_id')) as string) as id_firestore,
-        cast(get_json_object(user_properties , '$.platform') as string) as app_type,
-        cast(get_json_object(user_properties, '$.utm_source') as string) as utm_source,
-        cast(get_json_object(user_properties, '$.utm_medium') as string) as utm_medium,
-        cast(get_json_object(user_properties, '$.utm_campaign') as string) as utm_campaign,
-        cast(get_json_object(user_properties, '$.utm_content') as string) as utm_content,
-        cast(get_json_object(user_properties, '$.utm_term') as string) as utm_term,
-        date(ts_event) as dt_event,
-        ts_event
-    from
-        datalake_amplitude_clean.events
-    where
-        event_type = 'offer_submitted'
-        and id_app in (170698, 170135, 183049)
+        id_house,
+        id_firestore,
+        app_type,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        utm_content,
+        utm_term,
+        dt_event
+    from datalake_amplitude_clean.170698_offer_submitted_events
+    union all
+    select
+        id_user,
+        id_app,
+        id_house,
+        id_firestore,
+        app_type,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        utm_content,
+        utm_term,
+        dt_event
+    from datalake_amplitude_clean.170135_offer_submitted_events
+    union all
+    select
+        id_user,
+        id_app,
+        id_house,
+        id_firestore,
+        app_type,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        utm_content,
+        utm_term,
+        dt_event
+    from datalake_amplitude_clean.183049_offer_submitted_events
 )
 select
     id_user,
@@ -42,5 +65,5 @@ select
         ), false
     ) as is_branded,
     dt_event
-from extracted_utms
+from all_apps_events
 
---
+++ bietlejuice/jobs/composer/db/datalake/views/amplitude/spark/vw_170135_offer_submitted_events.sql
@@ -0,0 +1,18 @@
+DROP VIEW IF EXISTS datalake_amplitude_clean.170135_offer_submitted_events;
+CREATE OR REPLACE VIEW datalake_amplitude_clean.170135_offer_submitted_events
+AS
+    SELECT
+        id_user,
+        170135 as id_app, -- setting hardcoded because the id_app is wrongly set as null
+        cast(trim(get_json_object(event_properties, '$.house_id')) as string) as id_house,
+        cast(trim(get_json_object(event_properties, '$.offer_id')) as string) as id_firestore,
+        cast(get_json_object(user_properties , '$.platform') as string) as app_type,
+        cast(get_json_object(user_properties, '$.utm_source') as string) as utm_source,
+        cast(get_json_object(user_properties, '$.utm_medium') as string) as utm_medium,
+        cast(get_json_object(user_properties, '$.utm_campaign') as string) as utm_campaign,
+        cast(get_json_object(user_properties, '$.utm_content') as string) as utm_content,
+        cast(get_json_object(user_properties, '$.utm_term') as string) as utm_term,
+        date(ts_event) as dt_event,
+        ts_event
+    FROM
+        datalake_amplitude_clean_staging.170135_offer_submitted_events;
