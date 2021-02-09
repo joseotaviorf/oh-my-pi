6f678053911e3bfb5b9a04101d481a52fe813e48
Eduardo Trevisani <etgamba@gmail.com>
2021-02-09 17:47:01 -0300

    Adds braze enrich MD5 and fixes channel and action (#4080)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/datalake/queries/braze_events/enrich/events_owners.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_events/enrich/events_owners.sql
@@ -1,20 +1,29 @@
 SELECT
     GET_JSON_OBJECT(event_info,'$.id') AS id_event,
     GET_JSON_OBJECT(event_info,'$.dispatch_id') AS id_dispatch,
+    MD5(ENCODE(CONCAT(
+                COALESCE(
+                    GET_JSON_OBJECT(event_info,'$.dispatch_id'),
+                    CASE
+                        WHEN event_type = 'users.messages.webhook.Send'
+                        THEN CAST(FROM_UNIXTIME(CAST(GET_JSON_OBJECT(event_info,'$.time') AS INTEGER)) AS TIMESTAMP)
+                    END),
+                GET_JSON_OBJECT(event_info,'$.user_id')),
+        'utf-8')) AS id_user_dispatch,
     GET_JSON_OBJECT(event_info,'$.user_id') AS id_user_braze,
     GET_JSON_OBJECT(event_info,'$.external_user_id') AS id_user,
     GET_JSON_OBJECT(event_info,'$.campaign_id') AS id_campaign,
     GET_JSON_OBJECT(event_info,'$.message_variation_id') AS id_variant_campaign,
     GET_JSON_OBJECT(event_info,'$.canvas_id') AS id_canvas,
     GET_JSON_OBJECT(event_info,'$.canvas_variation_id') AS id_variant_canvas,
-    GET_JSON_OBJECT(event_info,'$.canvas_step_id') AS id_step_canvas,
-    SPLIT(event_type, '\\.')[2] AS event_channel,
-    LOWER(SPLIT(event_type, '\\.')[3]) AS event_action,
+    GET_JSON_OBJECT(event_info,'$.canvas_step_id') AS id_step_canvas,  
+    IF(SIZE(SPLIT(event_type, '\\.'))>3,LOWER(SPLIT(event_type, '\\.')[2]),NULL) AS event_channel,
+    LOWER(REVERSE(SPLIT(event_type, '\\.'))[0]) AS event_action,
     event_type,
     CAST(FROM_UNIXTIME(CAST(GET_JSON_OBJECT(event_info,'$.time') AS INTEGER)) AS TIMESTAMP) AS ts_event,
     year,
     month,
-    day      
+    day
 FROM
     datalake_braze_clean.events_owners
 WHERE
