68a6e0f57b6109b78c4175c94da1996710eeba52
helenafig <60613757+helenafig@users.noreply.github.com>
2021-02-23 10:28:36 -0300

    Fixing sk_buyer_booking_review in fact_visits (#4139)

--- bietlejuice/jobs/composer/db/datalake/queries/dw_sale_visits/dw/fact_visits.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_sale_visits/dw/fact_visits.sql
@@ -9,7 +9,7 @@ SELECT
     COALESCE(b.id_visit, -1) AS sk_visit,
     COALESCE(so.id_offer, -1) AS sk_offer,
     COALESCE(ar.id, -1) AS sk_agent_booking_review,
-    COALESCE(br.id, -1) AS sk_buyer_booking_review,
+    COALESCE(b.id, -1) AS sk_buyer_booking_review,
     COALESCE(BIGINT(DATE_FORMAT(b.ts_created, 'yyyyMMdd')), -1) AS sk_booking_created_date,
     COALESCE(BIGINT(DATE_FORMAT(b.ts_booking_utc, 'yyyyMMdd')), -1) AS sk_visit_date,
     COALESCE(BIGINT(DATE_FORMAT(b.ts_first_canceled, 'yyyyMMdd')), -1) AS sk_visit_canceled_date,
@@ -30,6 +30,5 @@ LEFT JOIN datalake_ebdb_clean.real_estate_agent_rating AS ar
     ON ar.id = b.id_real_estate_agent_rating
 LEFT JOIN datalake_insider_clean.review AS br 
     ON b.code = br.id_reviewed
-    AND br.status = 'DONE'
 WHERE
     b.visit_intent = 'SALE'
