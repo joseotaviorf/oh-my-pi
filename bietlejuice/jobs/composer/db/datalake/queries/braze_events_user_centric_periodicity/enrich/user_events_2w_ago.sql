c7ca56d1791fc171c990ddd1a222bc725ab072a7
Conrado Fabri <63262503+dataconra@users.noreply.github.com>
2021-02-23 10:29:51 -0300

    Creating tables for the third enrich layer of the user_centric table (#4128)
    * Creating tables for the third enrich layer of the user_centric table
    
    * moving files for the new directory for this specific enrich layer
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/braze_events_user_centric_periodicity/enrich/user_events_all_time.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/braze_events_user_centric_periodicity/enrich/user_events_all_time.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * Update user_events_2w_ago.sql
    
    * Update user_events_3w_ago.sql
    
    * Update user_events_all_time.sql
    
    * Update user_events_all_time.sql
    
    * Update user_events_last_week.sql
    
    * Update user_events_this_week.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_events_user_centric_periodicity/enrich/user_events_2w_ago.sql
@@ -0,0 +1,112 @@
+SELECT
+  id_user_braze,
+  SUM(
+    CASE
+      WHEN id_campaign IS NOT NULL
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_campaigns_sent_to,
+  SUM(
+    CASE
+      WHEN id_canvas IS NOT NULL
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_canvases_sent_to,
+  SUM(
+    CASE
+      WHEN user_type = 'tenant'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_tenant_communications_sent_to,
+  SUM(
+    CASE
+      WHEN user_type = 'owner'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_owner_communications_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_emails_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'delivery' THEN event_count
+      ELSE 0
+    END
+  ) AS total_emails_delivered_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'open' THEN event_count
+      ELSE 0
+    END
+  ) AS total_email_opens,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'click' THEN event_count
+      ELSE 0
+    END
+  ) AS total_email_clicks,
+  SUM(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_push_notifications_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'open' THEN event_count
+      ELSE 0
+    END
+  ) AS total_push_notification_opens,
+  SUM(
+    CASE
+      WHEN event_channel = 'inappmessage'
+        AND event_action = 'impression' THEN event_count
+      ELSE 0
+    END
+  ) AS total_inapp_message_views,
+  SUM(
+    CASE
+      WHEN event_channel = 'inappmessage'
+        AND event_action = 'click' THEN event_count
+      ELSE 0
+    END
+  ) AS total_inapp_message_clicks,
+  SUM(
+    CASE
+      WHEN event_channel = 'webhook'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_webhooks_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_sms_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'delivery' THEN event_count
+      ELSE 0
+    END
+  ) AS total_sms_delivered_to
+FROM
+    datalake_braze_user_centric.user_events_daily
+WHERE
+    DATE_TRUNC('week',DATE(dt_event)) = DATE_TRUNC('week',DATE(CURRENT_DATE) - INTERVAL '14' DAY)
+GROUP by 1
---
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_events_user_centric_periodicity/enrich/user_events_all_time.sql
@@ -0,0 +1,201 @@
+SELECT
+  id_user_braze,
+  SUM(
+    CASE
+      WHEN id_campaign IS NOT NULL
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_campaigns_sent_to,
+  SUM(
+    CASE
+      WHEN id_canvas IS NOT NULL
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_canvases_sent_to,
+  SUM(
+    CASE
+      WHEN user_type = 'tenant'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_tenant_communications_sent_to,
+  SUM(
+    CASE
+      WHEN user_type = 'owner'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_owner_communications_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_emails_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'delivery' THEN event_count
+      ELSE 0
+    END
+  ) AS total_emails_delivered_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'open' THEN event_count
+      ELSE 0
+    END
+  ) AS total_email_opens,
+  SUM(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'click' THEN event_count
+      ELSE 0
+    END
+  ) AS total_email_clicks,
+  SUM(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_push_notifications_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'open' THEN event_count
+      ELSE 0
+    END
+  ) AS total_push_notifications_opened,
+  SUM(
+    CASE
+      WHEN event_channel = 'inappmessage'
+      AND event_action = 'impression' THEN event_count
+      ELSE 0
+    END
+  ) AS total_inapp_messages_views,
+  SUM(
+    CASE
+      WHEN event_channel = 'inappmessage'
+        AND event_action = 'click' THEN event_count
+      ELSE 0
+    END
+  ) AS total_inapp_messages_clicks,
+  SUM(
+    CASE
+      WHEN event_channel = 'webhook'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_webhooks_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'send' THEN event_count
+      ELSE 0
+    END
+  ) AS total_sms_sent_to,
+  SUM(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'delivery' THEN event_count
+      ELSE 0
+    END
+  ) AS total_sms_delivered_to,
+  MAX(
+    CASE
+      WHEN id_campaign IS NOT NULL
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_campaign_sent_to,
+  MAX(
+    CASE
+      WHEN id_campaign IS NOT NULL
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_canvas_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_email_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'delivery' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_email_delivered_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'open' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_email_opened,
+  MAX(
+    CASE
+      WHEN event_channel = 'email'
+        AND event_action = 'click' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_email_clicked,
+  MAX(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_push_notification_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'pushnotification'
+        AND event_action = 'open' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_push_notification_opened,
+  MAX(
+    CASE
+      WHEN event_channel = 'inappmessage'
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_inapp_message_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'inappmessage'
+        AND event_action = 'open' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_inapp_message_clicked,
+  MAX(
+    CASE
+      WHEN event_channel = 'webhook'
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_webhook_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'send' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_sms_sent_to,
+  MAX(
+    CASE
+      WHEN event_channel = 'sms'
+        AND event_action = 'delivery' THEN dt_event
+      ELSE NULL
+    END
+  ) AS dt_last_sms_delivered_to
+FROM
+    datalake_braze_user_centric.user_events_daily
+GROUP BY 1
