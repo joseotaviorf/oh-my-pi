cd45f7b576314522e41dd1c4ba289efd634495ff
Mariana Frasson Amaral Arruda <mariana.frasson@quintoandar.com.br>
2021-02-26 11:45:13 -0300

    Adjusting lead processing operation rule (#4176)
    Co-authored-by: Mariana Frasson <marianafrasson@spf1lt-pj001326.ldap.quintoandar.com.br>

--- bietlejuice/db/dw/queries/datamarts/unique_supply_cohort_conversions.sql
+++ bietlejuice/db/dw/queries/datamarts/unique_supply_cohort_conversions.sql
@@ -204,8 +204,9 @@ SELECT
 	END AS lead_context,
   	sales_company,
     sourcing_ops,
-    CASE WHEN sourcing_ops IN ('IS Ext', 'IS Int', 'Other') AND lead_context = 'FSS'
-    	THEN 'IS'
+    CASE
+	    WHEN sourcing_ops IN ('IS Ext', 'IS Int', 'FSS IS PhotoJob', 'Other') AND lead_context = 'FSS' THEN 'IS'
+	    ELSE sourcing_ops
     END AS lead_processing_operation,
   	origin_table,
     context_origin,
