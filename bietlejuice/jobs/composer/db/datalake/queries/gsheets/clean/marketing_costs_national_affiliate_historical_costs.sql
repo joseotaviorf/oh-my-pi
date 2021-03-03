75e201a2646985054a7b4fd67e7df747767b9ff7
Luc Somers <lucwsomers@gmail.com>
2021-03-03 14:44:08 -0300

    [DTM-691] - Adjust National Affiliate Historical Costs Query (#4226)
    * fix clean
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_costs_national_affiliate_historical_costs.sql
    
    Co-authored-by: Eduardo Trevisani <etgamba@gmail.com>
    
    Co-authored-by: Eduardo Trevisani <etgamba@gmail.com>

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_costs_national_affiliate_historical_costs.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_costs_national_affiliate_historical_costs.sql
@@ -3,6 +3,6 @@ SELECT
     CAST(cost AS FLOAT) AS cost,
     city_group,
     mkt_source,
-    CAST(FROM_UNIXTIME(UNIX_TIMESTAMP(cost_date, 'dd/MM/yyyy'), 'yyyyMMdd') AS BIGINT) AS dt_cost
+    CAST(FROM_UNIXTIME(UNIX_TIMESTAMP(cost_date, 'MM/dd/yyyy'), 'yyyyMMdd') AS INTEGER) AS dt_cost
 FROM
     datalake_gsheets_raw.marketing_costs_national_affiliate_historical_costs
