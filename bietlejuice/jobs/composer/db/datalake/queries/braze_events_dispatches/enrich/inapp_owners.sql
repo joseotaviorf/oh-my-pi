a6924de6e5cbe27c99529f477aa2453b49a35457
marcussgomes <58474731+marcussgomes@users.noreply.github.com>
2021-03-04 17:10:00 -0300

    [DRB-268] Adjustment on inapp join at braze dispatch enrichment (#4223)
    * fix inapp
    
    * comment click events that should be used only in the future

--- bietlejuice/jobs/composer/db/datalake/queries/braze_events_dispatches/enrich/inapp_owners.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_events_dispatches/enrich/inapp_owners.sql
@@ -1,9 +1,9 @@
 SELECT
-	eo.id_dispatch,
+	eo.id_user_dispatch,
 	eo.id_user_braze,
 	eo.event_channel,
 	impression_owners.ts_event AS ts_inapp_impressioned,
-	click_owners.ts_event AS ts_inapp_clicked,
+-- 	click_owners.ts_event AS ts_inapp_clicked,
 	eo.year,
 	eo.month,
 	eo.day
@@ -11,16 +11,16 @@ FROM
 	datalake_braze.events_owners AS eo
 LEFT JOIN
 	datalake_braze.events_owners AS impression_owners
-		ON eo.id_user_braze = impression_owners.id_user_braze
+		ON eo.id_user_dispatch = impression_owners.id_user_dispatch
 		AND impression_owners.event_type = 'users.messages.inappmessage.Impression'
-LEFT JOIN
-	datalake_braze.events_owners AS click_owners
-		ON eo.id_user_braze = click_owners.id_user_braze
-		AND click_owners.event_type = 'users.messages.inappmessage.Click'
+-- LEFT JOIN
+-- 	datalake_braze.events_owners AS click_owners
+-- 		ON eo.id_user_dispatch = click_owners.id_user_dispatch
+-- 		AND click_owners.event_type = 'users.messages.inappmessage.Click'
 WHERE
 	eo.event_channel = 'inappmessage'
 	AND eo.year = {year}
 	AND eo.month = {month}
 	AND eo.day = {day}
 GROUP BY
-	1,2,3,4,5,6,7,8
+	1,2,3,4,5,6,7
--- bietlejuice/jobs/composer/db/datalake/queries/braze_events_dispatches/enrich/inapp_tenants.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_events_dispatches/enrich/inapp_tenants.sql
@@ -1,9 +1,9 @@
 SELECT
-	et.id_dispatch,
+	et.id_user_dispatch,
 	et.id_user_braze,
 	et.event_channel,
 	impression_tenants.ts_event AS ts_inapp_impressioned,
-	click_tenants.ts_event AS ts_inapp_clicked,
+-- 	click_tenants.ts_event AS ts_inapp_clicked,
 	et.year,
 	et.month,
 	et.day
@@ -11,16 +11,16 @@ FROM
 	datalake_braze.events_tenants AS et
 LEFT JOIN
 	datalake_braze.events_tenants AS impression_tenants
-		ON et.id_user_braze = impression_tenants.id_user_braze
+		ON et.id_user_dispatch = impression_tenants.id_user_dispatch
 		AND impression_tenants.event_type = 'users.messages.inappmessage.Impression'
-LEFT JOIN
-	datalake_braze.events_tenants AS click_tenants
-		ON et.id_user_braze = click_tenants.id_user_braze
-		AND click_tenants.event_type = 'users.messages.inappmessage.Click'
+-- LEFT JOIN
+-- 	datalake_braze.events_tenants AS click_tenants
+-- 		ON et.id_user_dispatch = click_tenants.id_user_dispatch
+-- 		AND click_tenants.event_type = 'users.messages.inappmessage.Click'
 WHERE
 	et.event_channel = 'inappmessage'
 	AND et.year = {year}
 	AND et.month = {month}
 	AND et.day = {day}
 GROUP BY
-	1,2,3,4,5,6,7,8
+	1,2,3,4,5,6,7
