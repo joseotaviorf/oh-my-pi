5f4c9d5fb5e7e674d53e008b996d4d28b1b79996
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2021-03-04 17:12:22 -0300

    change mediator interval to 15 minutes (#4241)

--- bietlejuice/jobs/composer/dags/mediator/mediator.py
+++ bietlejuice/jobs/composer/dags/mediator/mediator.py
@@ -53,7 +53,7 @@ mediator_dag = DAG(
         "depends_on_past": False,
     },
     start_date=datetime(2020, 5, 8, 0, 0, 0),
-    schedule_interval="0,15 * * * *",
+    schedule_interval="0,15,30,45 * * * *",
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
         chart_url=Variable.get("DOC_MD_BASE_URL"), dag_id=DAG_ID
     ),
