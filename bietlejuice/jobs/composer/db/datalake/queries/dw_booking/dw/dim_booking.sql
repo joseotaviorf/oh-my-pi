ed323ba3e76982b05a0a5a84dc3cfd7d1436aa77
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-03-08 13:08:33 -0300

    [DINT-631] Adding gsheet to dim_booking (#4230)
    * adding gsheets
    
    * updating
    
    * style guide
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/dw_booking/dw/dim_booking.sql
    
    Co-authored-by: Luc Somers <lucwsomers@gmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/dw_booking/dw/dim_booking.sql
    
    Co-authored-by: Luc Somers <lucwsomers@gmail.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/dw_booking/dw/dim_booking.sql
    
    Co-authored-by: Luc Somers <lucwsomers@gmail.com>
    
    * pr suggestion
    
    Co-authored-by: Luc Somers <lucwsomers@gmail.com>

--- bietlejuice/jobs/composer/db/datalake/queries/dw_booking/dw/dim_booking.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_booking/dw/dim_booking.sql
@@ -1,118 +1,124 @@
-with
--- removing duplicates rows due to case difference in the taxonomy
-taxonomy_demand as (
-    with taxonomy_min_ids as (
-        select
-            min(cast(id as bigint)) as id
-        from datalake_raw.gsheets_taxonomy_demand
-        group by
-            lower(app_type),
-            lower(utm_source),
-            lower(utm_medium),
-            lower(branded),
-            lower(first_update_source),
-            flg_via_reschedule
+-- removing duplicates rows due to CASE difference in the taxonomy
+WITH taxonomy_demand AS (
+  WITH taxonomy_min_ids AS (
+      SELECT
+          MIN(CAST(id AS BIGINT)) AS id
+      FROM
+        datalake_gsheets_clean.taxonomy_demand
+      GROUP BY
+          LOWER(app_type),
+          LOWER(utm_source),
+          LOWER(utm_medium),
+          LOWER(branded),
+          LOWER(first_update_source),
+          flg_via_reschedule
     )
-    select
-        cast(td.id as bigint) as id,
-        td.app_type,
-        td.utm_source,
-        td.utm_medium,
-        td.branded,
-        td.first_update_source,
-        cast(td.flg_via_reschedule as boolean) as flg_via_reschedule,
-        td.category as mkt_category,
-        td.flow as mkt_flow,
-        td.completion as mkt_completion,
-        td.channel as mkt_channel,
-        td.medium as mkt_medium,
-        td.origin as mkt_origin,
-        td.source as mkt_source,
-        td.platform as mkt_platform
-    from datalake_raw.gsheets_taxonomy_demand td
-    join taxonomy_min_ids td_min
-        on td.id = td_min.id
+    SELECT
+      CAST(td.id AS BIGINT) AS id,
+      td.app_type,
+      td.utm_source,
+      td.utm_medium,
+      td.branded,
+      td.first_update_source,
+      CAST(td.flg_via_reschedule AS BOOLEAN) AS flg_via_reschedule,
+      td.category AS mkt_category,
+      td.flow AS mkt_flow,
+      td.completion AS mkt_completion,
+      td.channel AS mkt_channel,
+      td.medium AS mkt_medium,
+      td.origin AS mkt_origin,
+      td.source AS mkt_source,
+      td.platform AS mkt_platform
+    FROM
+      datalake_gsheets_clean.taxonomy_demand AS td
+    JOIN
+      taxonomy_min_ids AS td_min
+        ON td.id = td_min.id
 )
-select
-    b.id as sk_booking,
-    coalesce(td.id, -1) as sk_rent_flow_taxonomy,
-    b.id as id_booking,
-    b.id_visitor,
-    b.id_visit,
-    b.id_house,
-    b.id_agent,
-    b.id_attendant,
-    b.id_rent_flow,
-    b.id_rescheduled_booking,
-    b.visit_intent,
-    b.buyer_intention,
-    b.type,
-    b.is_visit_completed,
-    b.is_visit_performed,
-    b.is_closed,
-    b.has_reschedule,
-    b.is_via_reschedule,
-    coalesce(src.is_branded, false) as is_branded,
-    b.has_tenant_attended,
-    b.has_agent_attended,
-    -- TODO [ODS] review this rule
-    coalesce(b.has_owner_arrived, true) as has_owner_arrived,
-    b.is_entrance_successful,
-    b.is_visit_created_from_app,
-    b.is_visit_last_updated_from_app,
-    b.visit_fup,
-    b.status,
-    b.slot_day,
-    substring(b.last_status_change_reason, 1, 200) as last_status_change_reason,
-    b.cancellation_reason,
-    b.cancellation_reason_category,
-    b.reason_category,
-    b.responsible,
-    b.last_update_source,
-    b.first_update_source,
-    b.tenant_absence_reason,
-    b.agent_absence_reason,
-    b.owner_missing_reason,
-    b.troublesome_entrance_problem,
-    b.checkin_status,
-    src.app_type,
-    -- TODO [ODS] review this rule
-    coalesce(src.media_source, "Unknown") as media_source,
-    src.adjust_network,
-    src.utm_source,
-    src.utm_medium,
-    src.utm_campaign,
-    src.utm_content,
-    src.utm_term,
-    case when td.id is null then 'Not Mapped' else td.mkt_category end as mkt_category,
-    case when td.id is null then 'Not Mapped' else td.mkt_flow end as mkt_flow,
-    case when td.id is null then 'Not Mapped' else td.mkt_completion end as mkt_completion,
-    case when td.id is null then 'Not Mapped' else td.mkt_origin end as mkt_origin,
-    case when td.id is null then 'Not Mapped' else td.mkt_channel end as mkt_channel,
-    case when td.id is null then 'Not Mapped' else td.mkt_medium end as mkt_medium,
-    case when td.id is null then 'Not Mapped' else td.mkt_source end as mkt_source,
-    case when td.id is null then 'Not Mapped' else td.mkt_platform end as mkt_platform,
-    b.ts_visit_fup,
-    b.ts_visit_follow_up_local_tz,
-    b.ts_booking_utc,
-    b.ts_booking_local_tz,
-    b.ts_first_canceled,
-    b.ts_first_canceled_local_tz,
-    b.ts_created,
-    b.ts_created_local_tz,
-    b.ts_updated,
-    now() as ts_load
-from datalake_booking.booking b
-left join datalake_ebdb_clean.visit v
-    on b.id_visit = v.id
-left join datalake_amplitude_visit.amplitude_visit src
-    on v.code = src.id_visit
-left join taxonomy_demand td
-    on lower(coalesce(td.app_type, '')) = lower(coalesce(src.app_type, ''))
-    and lower(coalesce(td.utm_source, '')) = lower(coalesce(src.utm_source, ''))
-    and lower(coalesce(td.utm_medium, '')) = lower(coalesce(src.utm_medium, ''))
-    -- TODO [ODS] use flag is_branded from datalake_amplitude_visit.amplitude_visit
-    and lower(coalesce(td.branded, '')) = lower(coalesce(src.branded, 'Outro'))
-    and lower(coalesce(td.first_update_source, '')) = lower(coalesce(b.first_update_source, ''))
-    and coalesce(td.flg_via_reschedule, false) = coalesce(b.is_via_reschedule, false)
+SELECT
+  b.id AS sk_booking,
+  COALESCE(td.id, -1) AS sk_rent_flow_taxonomy,
+  b.id AS id_booking,
+  b.id_visitor,
+  b.id_visit,
+  b.id_house,
+  b.id_agent,
+  b.id_attendant,
+  b.id_rent_flow,
+  b.id_rescheduled_booking,
+  b.visit_intent,
+  b.buyer_intention,
+  b.type,
+  b.is_visit_completed,
+  b.is_visit_performed,
+  b.is_closed,
+  b.has_reschedule,
+  b.is_via_reschedule,
+  COALESCE(src.is_branded, false) AS is_branded,
+  b.has_tenant_attended,
+  b.has_agent_attended,
+  -- TODO [ODS] review this rule
+  COALESCE(b.has_owner_arrived, true) AS has_owner_arrived,
+  b.is_entrance_successful,
+  b.is_visit_created_from_app,
+  b.is_visit_last_updated_from_app,
+  b.visit_fup,
+  b.status,
+  b.slot_day,
+  SUBSTRING(b.last_status_change_reason, 1, 200) AS last_status_change_reason,
+  b.cancellation_reason,
+  b.cancellation_reason_category,
+  b.reason_category,
+  b.responsible,
+  b.last_update_source,
+  b.first_update_source,
+  b.tenant_absence_reason,
+  b.agent_absence_reason,
+  b.owner_missing_reason,
+  b.troublesome_entrance_problem,
+  b.checkin_status,
+  src.app_type,
+  -- TODO [ODS] review this rule
+  COALESCE(src.media_source, "Unknown") AS media_source,
+  src.adjust_network,
+  src.utm_source,
+  src.utm_medium,
+  src.utm_campaign,
+  src.utm_content,
+  src.utm_term,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_category END AS mkt_category,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_flow END AS mkt_flow,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_completion END AS mkt_completion,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_origin END AS mkt_origin,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_channel END AS mkt_channel,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_medium END AS mkt_medium,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_source END AS mkt_source,
+  CASE WHEN td.id IS NULL THEN 'Not Mapped' ELSE td.mkt_platform END AS mkt_platform,
+  b.ts_visit_fup,
+  b.ts_visit_follow_up_local_tz,
+  b.ts_booking_utc,
+  b.ts_booking_local_tz,
+  b.ts_first_canceled,
+  b.ts_first_canceled_local_tz,
+  b.ts_created,
+  b.ts_created_local_tz,
+  b.ts_updated,
+  NOW() AS ts_load
+FROM
+  datalake_booking.booking AS b
+LEFT JOIN
+  datalake_ebdb_clean.visit AS v
+    ON b.id_visit = v.id
+LEFT JOIN
+  datalake_amplitude_visit.amplitude_visit AS src
+    ON v.code = src.id_visit
+LEFT JOIN
+  taxonomy_demand AS td
+    ON LOWER(COALESCE(td.app_type, '')) = LOWER(COALESCE(src.app_type, ''))
+    AND LOWER(COALESCE(td.utm_source, '')) = LOWER(COALESCE(src.utm_source, ''))
+    AND LOWER(COALESCE(td.utm_medium, '')) = LOWER(COALESCE(src.utm_medium, ''))
+    -- TODO [ODS] use flag is_branded FROM datalake_amplitude_visit.amplitude_visit
+    AND LOWER(COALESCE(td.branded, '')) = LOWER(COALESCE(src.branded, 'Outro'))
+    AND LOWER(COALESCE(td.first_update_source, '')) = LOWER(COALESCE(b.first_update_source, ''))
+    AND COALESCE(td.flg_via_reschedule, FALSE) = COALESCE(b.is_via_reschedule, FALSE)
     
