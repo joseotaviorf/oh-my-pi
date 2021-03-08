dba0e9fc1b89a9f6f84a012298cea2a312aae1f6
Danilo <danilo.oliveira@quintoandar.com.br>
2021-03-08 12:58:58 -0300

    [DECX] Change column that provides department name information (#4258)
    * Change column that provides department name info
    
    * Pull a new column from CTE

--- bietlejuice/jobs/composer/db/datalake/queries/dw_quinto_messenger/dw/dim_task.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_quinto_messenger/dw/dim_task.sql
@@ -1,15 +1,16 @@
 WITH completion_reason AS (
     SELECT
         id_task,
+        task_queue_name,
         task_completion_reason
     FROM datalake_quinto_messenger.task_event
     WHERE task_completion_reason IS NOT NULL
-    GROUP BY 1,2
+    GROUP BY 1, 2, 3
 )
 SELECT
     t.id_task AS sk_task,
     t.channel_type_internal AS channel,
-    t.ticket_group_name AS department,
+    cr.task_queue_name AS department,
     t.task_status AS status,
     cr.task_completion_reason AS completion_reason,
     t.customer_type_tag AS customer_type,
