43e7810fcb8c2968ca65708898425a314b900a18
Luc Somers <lucwsomers@gmail.com>
2021-03-10 09:36:02 -0300

    [DTM-721] - Rene Descartes DB Ingestion (#4275)
    * rene descartes
    
    * fixes
    
    * checkstyle
    
    * add to block list

---
+++ bietlejuice/jobs/composer/dags/rene_descartes/rene_descartes.md
@@ -0,0 +1,22 @@
+## Rene Descartes
+
+### Purpose
+
+This DAG handles the ingestion of our raw and clean data for our Rene Descartes Microservice, which is a Microservice for Owner Lead discard rules. For more information about the Database please refer to [the repository](https://github.com/quintoandar/rene-descartes).
+
+
+### Execution Interval
+
+This DAG is triggered daily.
+
+More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+1. In datalake raw:
+    - All tables which are available in the source's database.
+
+### Responsible Data Engineering Team
+
+For any questions or concerns about this DAG, please contact the Data Engineering Team responsible listed in the
+[DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
---
+++ bietlejuice/jobs/composer/dags/rene_descartes/rene_descartes.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+import pendulum
+
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+
+DAG_NAME = "rene_descartes"
+
+# airflow vars
+ENV = Variable.get("environment")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+
+LOAD_RENE_DESCARTES_INTO_DATALAKE_RAW_FILE_PATH = (
+    S3_PREFIX + f"/spark_jobs/{DAG_NAME}/load_rene_descartes_into_datalake.py"
+)
+
+# spark and databricks vars
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_NAME}"
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+# dag vars
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2020, 7, 27, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
+local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    ),
+)
+
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+rene_descartes_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="rene-descartes-to-datalake-raw",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": LOAD_RENE_DESCARTES_INTO_DATALAKE_RAW_FILE_PATH,
+            "parameters": [ENV, DATALAKE_BUCKET],
+        }
+    },
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+create_cluster_task >> rene_descartes_to_datalake_raw_task >> terminate_cluster_task
