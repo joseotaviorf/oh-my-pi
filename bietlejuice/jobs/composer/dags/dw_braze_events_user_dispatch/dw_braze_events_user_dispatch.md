d080c520fcc77396adfb2e6a84f1c733cdd080c7
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-03-12 10:05:48 -0300

    [DINT-653] Creating dw_braze_events_user_dispatch DAG (#4304)
    * adding dw dag
    
    * adding dependencies
    
    * adding doc
    
    * lint
    
    * removing unnecessary variables

---
+++ bietlejuice/jobs/composer/dags/dw_braze_events_user_dispatch/dw_braze_events_user_dispatch.md
@@ -0,0 +1,22 @@
+## DW Braze Events User Dispatch
+​
+### Purpose
+​
+This DAG creates the incremental modelings for Braze Events, context User Dispatch. 
+​
+### Execution​ Interval
+This DAG is triggered once per day via Mediator.
+
+More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+​
+This pipeline produces in DW, schema quintoandar, via incremental load:
+    - `fact_braze_campaign_user_dispatch`
+    - `fact_braze_canvas_user_dispatch`
+​
+### Responsible Data Teams
+​
+For any questions or concerns about this DAG, please contact the Data Engineering Team or Data Analytics Team
+responsibles listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+​
---
+++ bietlejuice/jobs/composer/dags/dw_braze_events_user_dispatch/dw_braze_events_user_dispatch.py
@@ -0,0 +1,92 @@
+from datetime import datetime
+import pendulum
+
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.incremental_dw_sub_dag import (
+    IncrementalDWSubDAG,
+)
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.services import FileService
+
+DW_SCHEMA = "quintoandar"
+CONTEXT = "braze_events_user_dispatch"
+DAG_NAME = f"dw_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+
+ENV = Variable.get("environment")
+DW_BUCKET = Variable.get("dw_bucket")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
+
+SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
+DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+
+LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
+    Variable.get("databricks_s3_bucket"), DAG_ID
+)
+
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+DAG_PARAMETERS = {
+    "partitions": ["year", "month", "day"],
+    "dw_query_filters": {"year": "{year}", "month": "{month}", "day": "{day}"},
+}
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+file_list = FileService.list_sql_files_without_extension_from_layer(
+    DAG_NAME, LayerEnum.DW.value
+)
+
+dw_sub_dag = IncrementalDWSubDAG(
+    dag_id=DAG_ID,
+    start_date=MAIN_START_DATE,
+    env=ENV,
+    dw_bucket=DW_BUCKET,
+    dw_schema=DW_SCHEMA,
+    relative_query_path=DAG_NAME,
+    spark_job_path=SPARK_JOBS_PATH,
+)
+
+dw_sub_dags = dw_sub_dag.build_subdags_from_sql_files(
+    dag,
+    file_list,
+    test_ods_migration=False,
+    partitions=DAG_PARAMETERS["partitions"],
+    dw_query_filters=DAG_PARAMETERS["dw_query_filters"],
+    spectrum_iam_role=SPECTRUM_IAM_ROLE,
+)
+
+create_cluster_task >> list(dw_sub_dags.values())
+list(dw_sub_dags.values()) >> terminate_cluster_task
