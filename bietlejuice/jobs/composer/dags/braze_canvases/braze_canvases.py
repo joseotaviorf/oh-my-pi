b1d3ee2c4e4037ff47e480cca055166fbd1034e1
Eduardo Trevisani <etgamba@gmail.com>
2021-03-16 18:52:43 -0300

    [DINT-654] Braze canvases DAG (#4336)
    * DINT-654 Created Braze Canvases DAG
    
    * DINT-655 Created Braze Canvases Doc MD
    
    * DINT-673 Created Braze Canvsaes clean queries
    
    * DINT-674 Created Braze Canvases Spark job
    
    * Fixed artifacts path and removed default libs
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

---
+++ bietlejuice/jobs/composer/dags/braze_canvases/braze_canvases.py
@@ -0,0 +1,116 @@
+from datetime import datetime
+
+import pendulum
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
+from bietlejuice.jobs.composer.services import FileService
+
+
+ENV = Variable.get("environment")
+
+SOURCE = "braze"
+DAG_NAME = f"{SOURCE}_canvases"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "30 0 * * *"
+
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+ARTIFACTS_S3_BUCKET = Variable.get("artifacts_default_bucket")
+DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+
+LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_NAME}"
+BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{DAG_NAME}"
+
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+CUSTOM_LIBRARIES = [
+    {
+        "whl": f"{ARTIFACTS_S3_BUCKET}/braze-api-client-python/"
+        f"quintoandar_braze_api_client-0.1.0-py2.py3-none-any.whl"
+    }
+]
+
+APP_GROUPS = ["owners", "tenants"]
+ENDPOINTS = ["details", "analytics"]
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=CUSTOM_LIBRARIES,
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+clean_sub_dag = DatalakeSubDAG(
+    dag_id=DAG_ID,
+    start_date=MAIN_START_DATE,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    layer=LayerEnum.CLEAN,
+    database_base_name=SOURCE,
+    relative_query_path=DAG_NAME,
+    spark_job_paths=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    execution_timeout_hours=0.5,
+)
+
+file_list = FileService.list_sql_files_without_extension_from_layer(
+    DAG_NAME, LayerEnum.CLEAN.value
+)
+
+clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(dag, file_list)
+
+# Creating sub dags
+for app_group in APP_GROUPS:
+    for endpoint in ENDPOINTS:
+        load_to_raw_task = QuintoAndarDatabricksSubmitRunOperator(
+            task_id=f"load-canvases-{endpoint}-{app_group}-to-raw",
+            dag=dag,
+            json={
+                "spark_python_task": {
+                    "python_file": f"{SPARK_JOBS_PATH}/load_braze_canvases_into_datalake.py",
+                    "parameters": [
+                        ENV,
+                        SOURCE,
+                        DATALAKE_BUCKET,
+                        app_group,
+                        endpoint,
+                        "{{ ts }}",
+                    ],
+                }
+            },
+        )
+
+        create_cluster_task >> load_to_raw_task >> clean_sub_dags.pop(
+            f"canvases_{endpoint}_{app_group}"
+        ) >> terminate_cluster_task
---
+++ bietlejuice/jobs/composer/db/datalake/queries/braze_canvases/clean/canvases_analytics_owners.sql
@@ -0,0 +1,16 @@
+SELECT
+    canvas_id AS id_canvas,
+    messages,
+    conversions_by_send_time,
+    conversions1_by_send_time,
+    conversions2_by_send_time,
+    conversions3_by_send_time,
+    conversions,
+    conversions1,
+    conversions2,
+    conversions3,
+    unique_recipients,
+    revenue,
+    TO_TIMESTAMP(`time`) AS ts_canvas_analytics_time
+FROM
+    datalake_braze_raw.canvases_analytics_owners
