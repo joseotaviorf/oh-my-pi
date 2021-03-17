0e8548baca824625786b8ebf07682a12be60e235
Victor Lúcio <victor.lucio10@gmail.com>
2021-03-17 18:39:23 -0300

    [DTM-746] - Refactor Google Marketing Costs Dims (#4345)
    * refactor dims
    
    * update fact to use dims

--- bietlejuice/jobs/composer/db/datalake/queries/dw_marketing_costs/google/dw/fact_google_daily_cost_attributions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_marketing_costs/google/dw/fact_google_daily_cost_attributions.sql
@@ -133,6 +133,7 @@ final_cte_ads as (
         and dim.ad_group_name = cte_ads.ad_group_name
         and dim.campaign_name = cte_ads.campaign_name
         and dim.ad_type = cte_ads.ad_type
+        and dim.load_date = cte_ads.load_date
 ),
 
 -- ADS END
@@ -269,6 +270,7 @@ final_cte_keywords as (
         and dim.ad_group_name = cte_keywords.ad_group_name
         and dim.campaign_name = cte_keywords.campaign_name
         and dim.match_type = cte_keywords.match_type
+        and dim.load_date = cte_keywords.load_date
 ),
 
 -- KEYWORDS END
@@ -388,6 +390,7 @@ final_cte_campaigns as (
     on dim.id_campaign = cte_campaigns.id_campaign
         and dim.account_name = cte_campaigns.acc
         and dim.campaign_name = cte_campaigns.campaign_name
+        and dim.load_date = cte_campaigns.load_date
 ),
 
 -- CAMPAIGNS END
@@ -524,6 +527,7 @@ ON dim.id_video = cte_videos.id_video
     AND dim.account_name = cte_videos.acc
     AND dim.ad_group_name = cte_videos.ad_group_name
     AND dim.campaign_name = cte_videos.campaign_name
+    and dim.load_date = cte_videos.load_date
 )
 
 -- VIDEOS END
