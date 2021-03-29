0e59b9f5eb8c5ed7230b0b7c654fed6282ae03e9
Eduardo Trevisani <etgamba@gmail.com>
2021-03-29 18:28:18 -0300

    Created CRM task_resolution_history enrich table (#4412)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/crm/enrich/incremental/task_resolution_history.sql
@@ -0,0 +1,103 @@
+SELECT DISTINCT
+    tsk.id AS id_task,
+    tsh.id AS id_action,
+    tsh.id_user_action,
+    tsk.id_assignee,
+    tsk.id_house,
+    tsk.id_rent_flow,
+    tsk.id_origin,
+    tsk.id_opened_by,
+    tsk.id_tenant,
+    tsk.id_negotiation,
+    tsk.id_manager,
+    tsk.id_owner,
+    tsk.id_receiver,
+    tsh.task_status,
+    tsh.action_reason,
+    tsh.action_user_name,
+    tsh.action_type,
+    tsk.type,
+    tsk.version,
+    tsk.tags,
+    tsk.score,
+    tsk.score_factor,
+    tsk.receiver_name,
+    tsk.receiver_type,
+    tsk.task_comment,
+    tsk.origin,
+    tsk.description,
+    tsk.phase,
+    tsk.subject,
+    tsk.visit_fup,
+    tsk.is_resolved,
+    tsh.ts_action,
+    tsk.ts_created,
+    tsk.ts_start,
+    tsk.ts_completed,
+    tsk.ts_visit,
+    tsk.ts_silenced_until,
+    tsk.ts_origin,
+    tsk.ts_fup,
+    tsh.year,
+    tsh.month,
+    tsh.day
+FROM
+    datalake_crm.tasks tsk
+INNER JOIN
+    datalake_crm.task_status_histories tsh
+        ON tsk.id = tsh.id_task
+        AND tsh.year={year}
+        AND tsh.month={month}
+        AND tsh.day={day}
+UNION ALL
+SELECT DISTINCT
+    tsk.id AS id_task,
+    tac.id AS id_action,
+    tac.id_user_analyst AS id_user_action,
+    tsk.id_assignee,
+    tsk.id_house,
+    tsk.id_rent_flow,
+    tsk.id_origin,
+    tsk.id_opened_by,
+    tsk.id_tenant,
+    tsk.id_negotiation,
+    tsk.id_manager,
+    tsk.id_owner,
+    tsk.id_receiver,
+    NULL AS task_status,
+    NULL AS action_reason,
+    tac.analyst_username AS action_user_name,
+    tac.action_type,
+    tsk.type,
+    tsk.version,
+    tsk.tags,
+    tsk.score,
+    tsk.score_factor,
+    tsk.receiver_name,
+    tsk.receiver_type,
+    tsk.task_comment,
+    tsk.origin,
+    tsk.description,
+    tsk.phase,
+    tsk.subject,
+    tsk.visit_fup,
+    tsk.is_resolved,
+    tac.ts_action,
+    tsk.ts_created,
+    tsk.ts_start,
+    tsk.ts_completed,
+    tsk.ts_visit,
+    tsk.ts_silenced_until,
+    tsk.ts_origin,
+    tsk.ts_fup,
+    tac.year,
+    tac.month,
+    tac.day
+FROM
+    datalake_crm.tasks tsk
+INNER JOIN
+    datalake_crm.tasks_actions tac
+        ON tsk.id=tac.id_task
+        AND tac.year={year}
+        AND tac.month={month}
+        AND tac.day={day}
