e8a39ef1047c5fccc65bb275465b6ff3b78ba090
Victor Lúcio <victor.lucio10@gmail.com>
2021-03-30 12:14:20 -0300

    [DTM-764] - Implement Multiple Runs per Day Gsheets (#4413)
    * added short circuit + reschedule
    
    * set all times to not labeled entries
    
    * added doc
    
    * added task id
    
    * update time to UTC
    
    * updated check_run_hour
    
    * lint

--- bietlejuice/jobs/composer/dags/gsheets/gsheets.py
+++ bietlejuice/jobs/composer/dags/gsheets/gsheets.py
@@ -10,11 +10,20 @@ from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksTerminateClusterOperator,
     QuintoAndarDatabricksSubmitRunOperator,
 )
+from airflow.operators.python_operator import ShortCircuitOperator
+import airflow.utils.helpers as airflow_helpers
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
 from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
 from bietlejuice.jobs.composer.base.pipeline import LayerEnum
 from bietlejuice.jobs.composer.services import FileService
+
+
+def check_run_hour(cron, local_tz):
+    current_time = datetime.now(local_tz)
+    current_hour = current_time.hour - 3
+    return str(current_hour) in cron.split(",")
+
 
 # ENV setup
 ENV = Variable.get("environment")
@@ -24,7 +33,7 @@ SOURCE = "gsheets"
 DAG_ID = f"bietlejuice.{SOURCE}"
 local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
 MAIN_START_DATE = datetime(2021, 1, 14, 0, 0, 0, tzinfo=local_tz)
-MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+MAIN_SCHEDULE_INTERVAL = "0 4,12,20 * * *"
 
 # s3 paths setup
 ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
@@ -99,6 +108,15 @@ for TABLE_NAME, SHEET_DETAILS in GOOGLE_FILES.items():
 
     slugged_table_name = TABLE_NAME.replace("_", "-")
 
+    if "cron" not in SHEET_DETAILS:
+        SHEET_DETAILS["cron"] = "1,9,17"
+
+    skip_run_task = ShortCircuitOperator(
+        task_id=f"check-hour-to-skip-{TABLE_NAME}",
+        python_callable=check_run_hour,
+        op_kwargs={"cron": SHEET_DETAILS["cron"], "local_tz": local_tz},
+    )
+
     gsheets_to_datalake_raw_tasks = QuintoAndarDatabricksSubmitRunOperator(
         task_id=f"gsheets-{slugged_table_name}-to-datalake-raw",
         dag=dag,
@@ -116,6 +134,10 @@ for TABLE_NAME, SHEET_DETAILS in GOOGLE_FILES.items():
         },
     )
 
-    create_cluster_task >> gsheets_to_datalake_raw_tasks >> clean_sub_dags.pop(
-        SHEET_DETAILS["clean_table_name"]
-    ) >> terminate_cluster_task
+    airflow_helpers.chain(
+        create_cluster_task,
+        skip_run_task,
+        gsheets_to_datalake_raw_tasks,
+        clean_sub_dags.pop(SHEET_DETAILS["clean_table_name"]),
+        terminate_cluster_task,
+    )
