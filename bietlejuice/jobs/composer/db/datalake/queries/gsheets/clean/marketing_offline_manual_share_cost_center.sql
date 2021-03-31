26ae761383d851f88143e76677888ed5851ea7bd
Victor Lúcio <victor.lucio10@gmail.com>
2021-03-31 15:17:55 -0300

    applied nullif (#4438)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_offline_manual_share_cost_center.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_offline_manual_share_cost_center.sql
@@ -1,6 +1,6 @@
 SELECT
-    id_rule_cost_center,
-    cost_center,
-    FLOAT(share) AS share
+    NULLIF(id_rule_cost_center, '') AS id_rule_cost_center,
+    NULLIF(cost_center, '') AS cost_center,
+    FLOAT(NULLIF(share, '')) AS share
 FROM
     datalake_gsheets_raw.marketing_offline_manual_share_cost_center
