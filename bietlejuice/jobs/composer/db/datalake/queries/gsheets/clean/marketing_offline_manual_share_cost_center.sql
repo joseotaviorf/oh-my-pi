7712160a3496854afc8e49402ad3641568bbcaf9
Victor Lúcio <victor.lucio10@gmail.com>
2021-03-31 19:00:43 -0300

    new col to clean (#4440)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_offline_manual_share_cost_center.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_offline_manual_share_cost_center.sql
@@ -1,6 +1,7 @@
 SELECT
     NULLIF(id_rule_cost_center, '') AS id_rule_cost_center,
     NULLIF(cost_center, '') AS cost_center,
+    NULLIF(cost_center_context, '') AS cost_center_context,
     FLOAT(NULLIF(share, '')) AS share
 FROM
     datalake_gsheets_raw.marketing_offline_manual_share_cost_center
