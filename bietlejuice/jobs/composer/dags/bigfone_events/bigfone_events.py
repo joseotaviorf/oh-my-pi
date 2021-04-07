913959a16d3a882a6251546f1a95b1c6a377e58b
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2021-04-07 13:25:32 -0300

    Remove Hive sync from bigfone dags due to pattern inconsistences in those DAGs. This should be added after they are refactored (#4486)

--- bietlejuice/jobs/composer/dags/bigfone_events/bigfone_events.py
+++ bietlejuice/jobs/composer/dags/bigfone_events/bigfone_events.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import pendulum
 
-import airflow.utils.helpers as airflow_helpers
 from airflow.models import DAG, Variable
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
@@ -72,33 +71,6 @@ bigfone_events_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
     },
 )
 
-sync_metastore_tables_task = QuintoAndarDatabricksSubmitRunOperator(
-    task_id="sync-hive-metastore-raw-tables",
-    dag=dag,
-    json={
-        "spark_python_task": {
-            "python_file": BASE_SPARK_JOB_PATH + "sync_metastore_tables.py",
-            "parameters": [
-                DATALAKE_BUCKET,
-                LayerEnum.RAW.value,
-                SOURCE,
-                "--all-tables",
-            ],
-        }
-    },
-)
-
-validate_sync_metastore_table_task = QuintoAndarDatabricksSubmitRunOperator(
-    dag=dag,
-    task_id="validate-sync-hive-metastore-table",
-    json={
-        "spark_python_task": {
-            "python_file": BASE_SPARK_JOB_PATH + "validate_sync_metastore_tables.py",
-            "parameters": [LayerEnum.RAW.value, SOURCE, "--all-tables"],
-        }
-    },
-)
-
 clean_sub_dag = DatalakeSubDAG(
     dag_id=DAG_ID,
     start_date=MAIN_START_DATE,
@@ -122,10 +94,3 @@ clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(
 create_cluster_task >> bigfone_events_to_datalake_raw_task >> list(
     clean_sub_dags.values()
 ) >> terminate_cluster_task
-
-airflow_helpers.chain(
-    bigfone_events_to_datalake_raw_task,
-    sync_metastore_tables_task,
-    validate_sync_metastore_table_task,
-    terminate_cluster_task,
-)
