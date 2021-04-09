55f6e3a996893818b441d5312554f2808afa1835
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2021-04-09 14:22:41 -0300

    change cluster (#4528)

--- bietlejuice/jobs/composer/dags/enrich_ebdb_listing/enrich_ebdb_listing.py
+++ bietlejuice/jobs/composer/dags/enrich_ebdb_listing/enrich_ebdb_listing.py
@@ -31,7 +31,9 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_bietlejuice_enrich_ebdb_listing_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 LIBRARIES_DESCRIPTION = Variable.get(
