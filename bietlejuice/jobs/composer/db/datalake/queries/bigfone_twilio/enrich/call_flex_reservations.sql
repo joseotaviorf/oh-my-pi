a6e0a6434ec5be960b2534e191da3d09b9956a0a
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-04-12 18:12:57 -0300

    [DECX] fixing call reservation enrich logic (#4535)
    * fixing queue_time, talk_time and duration logic
    
    * fixing ident

--- bietlejuice/jobs/composer/db/datalake/queries/bigfone_twilio/enrich/call_flex_reservations.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/bigfone_twilio/enrich/call_flex_reservations.sql
@@ -41,7 +41,7 @@ tasks_events AS (
   WHERE
       provider = 'twilio'
       AND GET_JSON_OBJECT(metadata,'$.event_data.WorkflowName') = 'Assign to Anyone'
-      AND event = 'task.created'
+      AND (event = 'task.created' OR event='task.wrapup')
       AND year = {year}
       AND month = {month}
       AND day = {day}
@@ -70,16 +70,13 @@ call_flex_reservations AS (
                 WHEN re1.event = 'reservation.rejected' THEN re1.id_reservation
             END
         ) > 0 AS is_rejected,
-        MIN(te.ts_event_unix) AS ts_created_unix,
+        MIN(re1.ts_event_unix) AS ts_created_unix,
         MAX(re1.ts_event_unix) AS ts_ended_unix,
         re1.year,
         re1.month,
         re1.day
     FROM
         reservations_events AS re1
-    INNER JOIN
-        tasks_events AS te
-            ON te.id_task = re1.id_task
     LEFT JOIN
         reservations_events AS re2
             ON re1.id_task = re2.id_task
@@ -87,27 +84,35 @@ call_flex_reservations AS (
             AND re2.queue_name IS NOT NULL
     GROUP BY 1,2,3,4,5,6,12,13,14
 ),
-answered_time_calculations AS (
-    SELECT
-        re.id_task,
-        re.id_reservation,
-        CASE
-            WHEN te.event = 'task.created' AND LEAD(re.event,1) OVER (PARTITION BY re.id_task ORDER BY re.ts_event_unix) = 'reservation.accepted' THEN LEAD(re.ts_event_unix,1) OVER (PARTITION BY re.id_task ORDER BY re.ts_event_unix) - te.ts_event_unix
-            WHEN te.event = 'task.created' AND LEAD(re.event,1) OVER (PARTITION BY re.id_task ORDER BY re.ts_event_unix) = 'reservation.completed' THEN LEAD(re.ts_event_unix,2) OVER (PARTITION BY re.id_task ORDER BY re.ts_event_unix) - re.ts_event_unix
-        END AS seconds_queue_time,
-        CASE
-            WHEN re.event = 'reservation.accepted' THEN LEAD(re.ts_event_unix,1) OVER (PARTITION BY re.id_task ORDER BY re.ts_event_unix) - re.ts_event_unix
-        END AS seconds_talk_time
-    FROM
+full_events AS (
+    SELECT 
+        re.* 
+    FROM 
         reservations_events re
-    JOIN
+    JOIN --filtering only answered reservations
         call_flex_reservations cr
             ON re.id_task = cr.id_task
             AND re.id_reservation = cr.id_reservation
             AND cr.is_answered
-    JOIN 
-        tasks_events te
-            ON te.id_task = re.id_task
+    UNION ALL
+    SELECT * FROM tasks_events
+),
+answered_time_calculations AS (
+    SELECT
+        fe.id_task,
+        fe.id_reservation,
+        CASE
+            WHEN 
+                event = 'reservation.created' 
+                AND LEAD(event,1) OVER (PARTITION BY fe.id_task ORDER BY ts_event) = 'reservation.accepted' 
+            THEN 
+                LEAD(ts_event_unix,1) OVER (PARTITION BY fe.id_task ORDER BY ts_event) - COALESCE(LAG(ts_event_unix,1) OVER (PARTITION BY fe.id_task ORDER BY ts_event),ts_event_unix)
+        END AS seconds_queue_time,
+        CASE
+            WHEN event = 'reservation.accepted' THEN LEAD(ts_event_unix,1) OVER (PARTITION BY fe.id_task ORDER BY ts_event) - ts_event_unix
+        END AS seconds_talk_time
+    FROM
+        full_events fe
 )
 SELECT
     cr.id_call,
@@ -119,7 +124,7 @@ SELECT
     is_answered,
     is_timeout,
     is_rejected,
-    ts_ended_unix - ts_created_unix AS seconds_duration,
+    SUM(seconds_queue_time) + SUM(seconds_talk_time) AS seconds_duration,
     SUM(seconds_queue_time) AS seconds_wait_time,
     SUM(seconds_talk_time) AS seconds_talk_time,
     CAST(FROM_UNIXTIME(ts_created_unix, 'yyyy-MM-dd HH:mm:ss') AS TIMESTAMP) AS ts_created,
@@ -136,4 +141,4 @@ LEFT JOIN
     answered_time_calculations AS atc
         ON cr.id_task = atc.id_task
         AND cr.id_reservation = atc.id_reservation
-GROUP BY 1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,18,19,20
+GROUP BY 1,2,3,4,5,6,7,8,9,13,14,15,16,17,18,19,20
