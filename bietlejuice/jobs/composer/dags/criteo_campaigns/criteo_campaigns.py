db2438aede8fbe1fe40cd2c592edb3901e9e358f
Eduardo Trevisani <etgamba@gmail.com>
2021-04-12 12:21:18 -0300

    Add Hive sync for criteo_campaigns DAG (#4530)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/criteo_campaigns/criteo_campaigns.py
+++ bietlejuice/jobs/composer/dags/criteo_campaigns/criteo_campaigns.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pendulum
-from airflow.models import DAG
-from airflow.models import Variable
+from airflow.models import DAG, Variable
+import airflow.utils.helpers as airflow_helpers
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -78,6 +78,33 @@ criteo_campaigns_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
     },
 )
 
+sync_metastore_tables_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id="sync-hive-metastore-raw-tables",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": SPARK_JOBS_PATH + "/sync_metastore_tables.py",
+            "parameters": [
+                DATALAKE_BUCKET,
+                LayerEnum.RAW.value,
+                SOURCE,
+                "--all-tables",
+            ],
+        }
+    },
+)
+
+validate_sync_metastore_table_task = QuintoAndarDatabricksSubmitRunOperator(
+    dag=dag,
+    task_id="validate-sync-hive-metastore-table",
+    json={
+        "spark_python_task": {
+            "python_file": SPARK_JOBS_PATH + "/validate_sync_metastore_tables.py",
+            "parameters": [LayerEnum.RAW.value, SOURCE, "--all-tables"],
+        }
+    },
+)
+
 clean_sub_dag = DatalakeSubDAG(
     dag_id=DAG_ID,
     start_date=MAIN_START_DATE,
@@ -101,7 +128,16 @@ clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
+airflow_helpers.chain(
+    criteo_campaigns_to_datalake_raw_task,
+    sync_metastore_tables_task,
+    validate_sync_metastore_table_task,
+    terminate_cluster_task,
+)
 
-create_cluster_task >> criteo_campaigns_to_datalake_raw_task >> list(
-    clean_sub_dags.values()
-) >> terminate_cluster_task
+airflow_helpers.chain(
+    create_cluster_task,
+    criteo_campaigns_to_datalake_raw_task,
+    list(clean_sub_dags.values()),
+    terminate_cluster_task,
+)
