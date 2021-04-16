a8472b83de6342fadd8077ba2aedcd0e216a995e
Danilo <danilo.oliveira@quintoandar.com.br>
2021-04-16 17:28:24 -0300

    Ajdjust alias (#4585)

--- bietlejuice/jobs/composer/db/datalake/queries/dw_call/dw/dim_call.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_call/dw/dim_call.sql
@@ -46,7 +46,7 @@ csat_events AS (
     GROUP BY 1
 )
 SELECT
-    COALESCE(ic.id_call, fe.id_call, fe.id_conversation, fe.id_task) AS sk_call, -- only inbound calls have id_call, but all calls have id_task
+    COALESCE(ie.id_call, fe.id_call, fe.id_conversation, fe.id_task) AS sk_call, -- only inbound calls have id_call, but all calls have id_task
     COALESCE(ie.from_number,fe.from_number) AS from_phone_number,
     COALESCE(ie.to_number,fe.to_number) AS to_phone_number,
     fe.direction,
