78ab53aadfbbb3e487650c056fe3db9a6cb9f5dd
Isabela Sayuri Matsumoto <46712621+sayurimatsumoto5andar@users.noreply.github.com>
2021-04-22 11:22:45 -0300

    [DRO-72] Including gsheets of hotjar photos repressed demand (#4577)
    * [DRO-72] Including gsheets of hotjar photos repressed demand
    
    * Adding extra line in the end of file
    
    * Changing ts_answer_submitted position and removing 'has' from find_the_time
    
    * Moving  for alphabetical order
    
    * Fixing ts_answer casting
    
    * Fixing select from case sensitive

---
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/hotjar_photos_repressed_demand.sql
@@ -0,0 +1,9 @@
+SELECT
+    CAST(id_house AS BIGINT) AS id_house,
+    id_user_hotjar,
+    find_the_time,
+    reason,
+    comment,
+    CAST(ts_answer_submitted AS TIMESTAMP) AS ts_answer_submitted
+FROM
+    datalake_gsheets_raw.hotjar_photos_repressed_demand
