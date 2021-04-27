d843c5d45718a9f410bd7f228d86bb4d39ce5fc1
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-04-27 14:08:26 -0300

    micro refactor on sauron DAG to consider partitioned tables to increase performance (#4658)

--- bietlejuice/jobs/composer/dags/sauron/spark_jobs/load_sauron_into_datalake.py
+++ bietlejuice/jobs/composer/dags/sauron/spark_jobs/load_sauron_into_datalake.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from argparse import ArgumentParser
+from datetime import datetime
 
 from quintoandar_logger import QuintoAndarLogger
 from bietlejuice.jobs.composer.base.db import DatabaseEnum, DatalakeMetastoreService
@@ -12,14 +13,23 @@ from bietlejuice.jobs.composer.services.metastore_services import SparkMetastore
 
 SOURCE = "sauron"
 JOB_NAME = "load_sauron_into_datalake"
-ALLOW_LIST = [
-    "activesessions",
-    "botoutgoingmessages",
-    "expiredsessions",
-    "incomingmessagestatus",
-    "incomingmessages",
-    "session",
-]
+TABLES_DICT = {
+    "activesessions": {"table_name": "ActiveSessions"},
+    "botoutgoingmessages": {
+        "table_name": "BotOutgoingMessages_{year}{month}",
+        "partitioned_table": True,
+    },
+    "expiredsessions": {"table_name": "ExpiredSessions"},
+    "incomingmessagestatus": {
+        "table_name": "IncomingMessageStatus_{year}{month}",
+        "partitioned_table": True,
+    },
+    "incomingmessages": {
+        "table_name": "IncomingMessages_{year}{month}",
+        "partitioned_table": True,
+    },
+    "session": {"table_name": "Session"},
+}
 
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
@@ -34,6 +44,7 @@ if __name__ == "__main__":
     environment = args.env
     datalake_bucket = args.datalake_bucket
     execution_date = args.execution_date
+    dt_execution = datetime.strptime(execution_date, "%Y-%m-%d")
     partition_cols = ["year", "month", "day"]
 
     logger.info(
@@ -50,7 +61,6 @@ if __name__ == "__main__":
     spark_client = SparkClient()
     postgres_consumer = PostgresConsumer(conn_config, spark_client)
 
-    tables = postgres_consumer.get_table_names_and_sizes().collect()
     db_info = DatalakeMetastoreService.get_db_info(environment, SOURCE, datalake_bucket)
     metastore_service = SparkMetastoreService(spark_client)
     s3_loader = S3Loader()
@@ -62,20 +72,24 @@ if __name__ == "__main__":
     database_location = db_info["db_raw_path"]
     metastore_service.create_database(database_name)
 
-    for table in tables:
-        if table.table_name.lower() in ALLOW_LIST:
-            df = postgres_consumer.get_incremental_data_from_table(
-                table.table_name, "updated_at", execution_date
+    for datalake_table_name, table_info in TABLES_DICT.items():
+        sauron_table_name = table_info.get("table_name")
+        if table_info.get("partitioned_table"):
+            sauron_table_name = sauron_table_name.format(
+                year=dt_execution.year, month="{:02d}".format(dt_execution.month)
             )
-            # the table names in the datalake must be lowercase
-            s3_loader.load_incremental_table(
-                df=df,
-                database_name=database_name,
-                table_name=table.table_name.lower(),
-                format_options=format_options,
-                database_location=database_location,
-                partition_cols=partition_cols,
-            )
-            metastore_service.create_new_partitions_from_df(
-                database_name, table.table_name.lower(), df, partition_cols
-            )
+
+        df = postgres_consumer.get_incremental_data_from_table(
+            sauron_table_name, "updated_at", execution_date
+        )
+        s3_loader.load_incremental_table(
+            df=df,
+            database_name=database_name,
+            table_name=datalake_table_name,
+            format_options=format_options,
+            database_location=database_location,
+            partition_cols=partition_cols,
+        )
+        metastore_service.create_new_partitions_from_df(
+            database_name, datalake_table_name, df, partition_cols
+        )
