6732439050738666833a3100ac4a1adf146921e6
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2021-05-11 13:52:33 -0300

    [DAE-294] Refactor EBDB DAG declaration code (#4733)
    * Refactor EBDB DAG declaration code to make it more readable
    
    * Refactor DW functions; Remove no longer used functions
    
    * Update task group reference to task list
    
    * Rename var

--- bietlejuice/jobs/composer/dags/ebdb/ebdb.py
+++ bietlejuice/jobs/composer/dags/ebdb/ebdb.py
@@ -7,6 +7,7 @@ from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksSubmitRunOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
+from airflow.utils.helpers import cross_downstream
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
 from bietlejuice.jobs.composer.services.file_service import FileService
@@ -30,23 +31,22 @@ DATALAKE_BUCKET = Variable.get("datalake_bucket")
 
 # s3 path setup
 S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = S3_PREFIX + "/spark_jobs/{}/".format(DAG_ID)
+EBDB_SPARK_JOBS_PATH = S3_PREFIX + "/spark_jobs/{}/".format(DAG_ID)
 LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
 CREATE_CLEAN_TABLE_IN_DATA_LAKE_PATH = (
-    SPARK_JOBS_PATH + "create_clean_table_in_datalake.py"
+    EBDB_SPARK_JOBS_PATH + "create_clean_table_in_datalake.py"
 )
-CREATE_EXTERNAL_TABLES_FILE_PATH = SPARK_JOBS_PATH + "create_external_tables.py"
+CREATE_EXTERNAL_TABLES_FILE_PATH = EBDB_SPARK_JOBS_PATH + "create_external_tables.py"
 LOAD_DB_SCHEMA_INTO_DATALAKE_RAW_FILE_PATH = (
-    SPARK_JOBS_PATH + "load_ebdb_into_datalake.py"
+    EBDB_SPARK_JOBS_PATH + "load_ebdb_into_datalake.py"
 )
 
 # cluster params
 CLUSTER_DESCRIPTION = Variable.get("databricks_ebdb_cluster", deserialize_json=True)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
-
 
 # cluster libraries
 DEFAULT_LIBRARIES = Variable.get("bietlejuice_default_libraries", deserialize_json=True)
@@ -77,76 +77,41 @@ dag = DAG(
 )
 
 
-# methods to create tasks and subdags
-def create_clean_table_in_datalake_task(local_dag, table_name, source, env, dag_name):
-    return QuintoAndarDatabricksSubmitRunOperator(
-        task_id="create-clean-{}-in-datalake".format(table_name.replace("_", "-")),
-        dag=local_dag,
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_clean_table_in_datalake.py",
-                "parameters": [table_name, source, env, DATALAKE_BUCKET, dag_name],
-            }
-        },
-    )
-
-
-def create_dw_table_in_datalake_task(table_name, schema, env, dag_name):
-    return QuintoAndarDatabricksSubmitRunOperator(
-        task_id="create-dw-{}-{}-in-datalake".format(
-            schema, table_name.replace("_", "-")
-        ),
-        dag=dag,
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_dw_table_in_datalake.py",
-                "parameters": [table_name, DW_BUCKET, schema, env, dag_name],
-            }
-        },
-    )
-
-
-def load_dw_table_into_redshift_task(table_name, schema, env):
-    return QuintoAndarDatabricksSubmitRunOperator(
-        task_id="load-dw-{}-{}-in-datalake".format(
-            schema, table_name.replace("_", "-")
-        ),
-        dag=dag,
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "load_dw_table_into_redshift.py",
-                "parameters": [SPECTRUM_IAM_ROLE, table_name, DW_BUCKET, schema, env],
-            }
-        },
-    )
-
-
-def create_external_tables_task(env, datalake_layer, source, tables):
-    return QuintoAndarDatabricksSubmitRunOperator(
-        task_id="create-{}-external-tables".format(datalake_layer),
-        dag=dag,
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_external_tables.py",
-                "parameters": [
-                    env,
-                    ATHENA_QUERY_RESULT_LOCATION,
-                    datalake_layer,
-                    DATALAKE_BUCKET,
-                    source,
-                    "--tables",
-                ]
-                + tables,
-            }
-        },
-    )
-
-
 def dw_tasks(table_name):
-    dim_table_task = create_dw_table_in_datalake_task(
-        table_name, DW_SCHEMA, ENV, DAG_ID
+    """
+    Mounts the DW workflow for each table.
+
+    :return: the first and last tasks from the clean workflow slice.
+    :rtype: List[str, List[str]]
+    """
+    slugged_table_name = table_name.replace("_", "-")
+    dim_table_task = QuintoAndarDatabricksSubmitRunOperator(
+        task_id=f"create-dw-{DW_SCHEMA}-{slugged_table_name}-in-datalake",
+        dag=dag,
+        json={
+            "spark_python_task": {
+                "python_file": EBDB_SPARK_JOBS_PATH + "create_dw_table_in_datalake.py",
+                "parameters": [table_name, DW_BUCKET, DW_SCHEMA, ENV, DAG_ID],
+            }
+        },
     )
-    load_dim_table_task = load_dw_table_into_redshift_task(table_name, DW_SCHEMA, ENV)
+
+    load_dim_table_task = QuintoAndarDatabricksSubmitRunOperator(
+        task_id=f"load-dw-{DW_SCHEMA}-{slugged_table_name}-in-datalake",
+        dag=dag,
+        json={
+            "spark_python_task": {
+                "python_file": EBDB_SPARK_JOBS_PATH + "load_dw_table_into_redshift.py",
+                "parameters": [
+                    SPECTRUM_IAM_ROLE,
+                    table_name,
+                    DW_BUCKET,
+                    DW_SCHEMA,
+                    ENV,
+                ],
+            }
+        },
+    )
 
     dim_table_task >> load_dim_table_task
 
@@ -154,8 +119,13 @@ def dw_tasks(table_name):
 
 
 def clean_tasks(table_name):
-    slugged_table_name = table_name.replace("_", "-")
+    """
+    Mounts the clean workflow for each table.
 
+    :return: the first and last tasks from the clean workflow slice.
+    :rtype: List[str, List[str]]
+    """
+    slugged_table_name = table_name.replace("_", "-")
     clean_table_task = QuintoAndarDatabricksSubmitRunOperator(
         dag=dag,
         task_id=f"create-clean-{slugged_table_name}-in-data-lake",
@@ -193,12 +163,23 @@ def clean_tasks(table_name):
             }
         },
     )
+
     clean_table_task >> create_clean_external_tables_task
+
     return [clean_table_task, create_clean_external_tables_task]
 
 
-def create_raw_tables_sub_dag_tasks(source):
+def build_raw_task_list():
+    """
+    Since we're creating a bunch of tasks now instead of using subdags, we need to make sure the same behavior occurs in
+     relationships.
+    Hence, zero indexing represents the first task (load-to-raw/load-to-clean/etc), which must be the single first
+     relationship with upstream tasks.
+    Moreover, the -1 indexing represents the last task (or tasks) from the task group, which will continue being the last
+     task(s) for the initial of a downstream flow.
 
+    :rtype: List[str]
+    """
     load_tables_into_datalake_task = QuintoAndarDatabricksSubmitRunOperator(
         task_id="load-tables-to-datalake-raw",
         dag=dag,
@@ -211,7 +192,7 @@ def create_raw_tables_sub_dag_tasks(source):
         execution_timeout=timedelta(hours=RAW_EXECUTION_TIMEOUT_HOURS),
     )
 
-    create_external_tables_task = QuintoAndarDatabricksSubmitRunOperator(
+    create_raw_external_tables_task = QuintoAndarDatabricksSubmitRunOperator(
         task_id="create-all-raw-external-tables",
         dag=dag,
         pool="athena",
@@ -223,34 +204,66 @@ def create_raw_tables_sub_dag_tasks(source):
                     ATHENA_QUERY_RESULT_LOCATION,
                     "raw",
                     DATALAKE_BUCKET,
-                    source,
+                    SOURCE,
                     "--all",
                 ],
             }
         },
     )
 
-    load_tables_into_datalake_task >> create_external_tables_task
+    load_tables_into_datalake_task >> create_raw_external_tables_task
 
-    return [load_tables_into_datalake_task, create_external_tables_task]
+    return [load_tables_into_datalake_task, create_raw_external_tables_task]
 
 
-def build_subdags(stage):
-    # create subdag for each table
-    file_list = FileService.list_layer_sql_files(SOURCE, stage)
-    subdags = {}
+def build_layer_task_list(layer):
+    """
+    Creates a task group for each table, according to given layer.
+
+    Since we're creating a bunch of tasks now instead of using subdags, we need to make sure the same behavior occurs in
+     relationships.
+    Hence, zero indexing represents the first task (load-to-raw/load-to-clean/etc), which must be the single first
+     relationship with upstream tasks.
+    Moreover, the -1 indexing represents the last task (or tasks) from the task group, which will continue being the last
+     task(s) for the initial of a downstream flow.
+
+    :return: A dictionary where the key is the table name and the value is the
+     task group's last and first tasks.
+    """
+    file_list = FileService.list_layer_sql_files(SOURCE, layer)
+    task_list = {}
 
     for file_name in file_list:
-        file_name = FileService.remove_file_extension(file_name)
+        table_name = FileService.remove_file_extension(file_name)
+        task_list[table_name] = eval(f"{layer}_tasks('{table_name}')")
 
-        subdags[file_name] = eval(f"{stage}_tasks('{file_name}')")
-
-    # subdags is a dict where the keys are table or file names
-    # and the values are corresponding subdag objects
-    return subdags
+    return task_list
 
 
-# tasks and subdags definitions
+def task_list_first_task(task_list_tasks):
+    """
+    Gets the first task of the tasks list.
+
+    :rtype: airflow.models.BaseOperator
+    """
+    return task_list_tasks[0]
+
+
+def task_list_last_tasks(task_list_tasks):
+    """
+    Gets the last tasks list of the tasks list.
+    If it is a single task, then encapsulates into a list.
+
+    :rtype: List[airflow.models.BaseOperator]
+    """
+    last_tasks = task_list_tasks[-1]
+    if not isinstance(last_tasks, list):
+        last_tasks = [last_tasks]
+
+    return last_tasks
+
+
+# Tasks definitions
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag,
     task_id="create-cluster",
@@ -258,89 +271,89 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     libraries=LIBRARIES_DESCRIPTION,
 )
 
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
 polygon_region_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
     dag=dag,
     task_id="polygon_region_to_datalake_raw",
     json={
         "spark_python_task": {
-            "python_file": SPARK_JOBS_PATH + "load_query_table_in_datalake.py",
+            "python_file": EBDB_SPARK_JOBS_PATH + "load_query_table_in_datalake.py",
             "parameters": [ENV, DATALAKE_BUCKET, "poligonoregiao", SOURCE],
         }
     },
 )
 
-terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
-    dag=dag, task_id="terminate-cluster"
-)
-
-load_raw_sub_dag = create_raw_tables_sub_dag_tasks(source=SOURCE)
-
-
-def cross_downstream(from_tasks, to_tasks):
-    for task in from_tasks:
-        task.set_downstream(to_tasks)
-
-
-def subdag_first_task(subdag_task):
-    return subdag_task[0]
-
-
-def subdag_last_task(subdag_task):
-    return subdag_task[-1]
-
-
-clean_sub_dags = build_subdags("clean")
-dw_sub_dags = build_subdags("dw")
-
-# Note 1: since we're creating task lists now, we need to make sure the same behavior occurs in relationships
-# hence, zero indexing represents the first task (load-to-raw/load-to-clean/etc), which must be the single first
-# relationship with upstream tasks
-# moreover, the -1 indexing represents the last task (create-external-table), which will continue being the last
-# task for the initial of a downstream flow.
-
-# Note 2: the task lists created here were done by such as to maintain an approximate structure for SubDags representation
-# and also to make sure Airflow 2.0's TaskGroup can reuse it with little change.
+raw_task_list = build_raw_task_list()
+clean_task_list = build_layer_task_list("clean")
+dw_sub_dags = build_layer_task_list("dw")
 
 create_cluster_task >> [
-    subdag_first_task(load_raw_sub_dag),
+    task_list_first_task(raw_task_list),
     polygon_region_to_datalake_raw_task,
-]
-subdag_last_task(load_raw_sub_dag) >> [
-    subdag_first_task(c) for c in clean_sub_dags.values()
-]
-
-polygon_region_to_datalake_raw_task >> subdag_first_task(
-    clean_sub_dags["polygon_region"]
-)
-subdag_last_task(clean_sub_dags.pop("proponent_proposal")) >> [
-    subdag_first_task(dw_sub_dags["dim_proposal_person"]),
-    subdag_first_task(dw_sub_dags["fact_proposal_people"]),
-]
-
-subdag_last_task(clean_sub_dags.pop("proposal")) >> subdag_first_task(
-    dw_sub_dags["fact_proposal_people"]
-)
-
-subdag_last_task(clean_sub_dags.pop("user")) >> [
-    subdag_first_task(dw_sub_dags["fact_contract_people"]),
-    subdag_first_task(dw_sub_dags["fact_proposal_people"]),
 ]
 
 cross_downstream(
+    task_list_last_tasks(raw_task_list),
+    [task_list_first_task(c) for c in clean_task_list.values()],
+)
+
+polygon_region_to_datalake_raw_task >> task_list_first_task(
+    clean_task_list["polygon_region"]
+)
+
+cross_downstream(
+    task_list_last_tasks(clean_task_list.pop("proponent_proposal")),
     [
-        subdag_last_task(clean_sub_dags.pop("contract")),
-        subdag_last_task(clean_sub_dags.pop("contract_person")),
-        subdag_last_task(clean_sub_dags.pop("rent_flow")),
-        subdag_last_task(clean_sub_dags.pop("house")),
-        subdag_last_task(clean_sub_dags.pop("partner_agent")),
-        subdag_last_task(clean_sub_dags.pop("conversion_lead")),
-        subdag_last_task(clean_sub_dags.pop("lead")),
-    ],
-    [
-        subdag_first_task(dw_sub_dags["dim_contract_person"]),
-        subdag_first_task(dw_sub_dags["fact_contract_people"]),
+        task_list_first_task(dw_sub_dags["dim_proposal_person"]),
+        task_list_first_task(dw_sub_dags["fact_proposal_people"]),
     ],
 )
 
-[subdag_last_task(c) for c in clean_sub_dags.values()] >> terminate_cluster_task
-[subdag_last_task(d) for d in dw_sub_dags.values()] >> terminate_cluster_task
+cross_downstream(
+    task_list_last_tasks(clean_task_list.pop("proposal")),
+    task_list_first_task(dw_sub_dags["fact_proposal_people"]),
+)
+
+cross_downstream(
+    task_list_last_tasks(clean_task_list.pop("user")),
+    [
+        task_list_first_task(dw_sub_dags["fact_contract_people"]),
+        task_list_first_task(dw_sub_dags["fact_proposal_people"]),
+    ],
+)
+
+contract_model_dependencies = []
+contract_model_dependencies.extend(
+    task_list_last_tasks(clean_task_list.pop("contract"))
+)
+contract_model_dependencies.extend(
+    task_list_last_tasks(clean_task_list.pop("contract_person"))
+)
+contract_model_dependencies.extend(
+    task_list_last_tasks(clean_task_list.pop("rent_flow"))
+)
+contract_model_dependencies.extend(task_list_last_tasks(clean_task_list.pop("house")))
+contract_model_dependencies.extend(
+    task_list_last_tasks(clean_task_list.pop("partner_agent"))
+)
+contract_model_dependencies.extend(
+    task_list_last_tasks(clean_task_list.pop("conversion_lead"))
+)
+contract_model_dependencies.extend(task_list_last_tasks(clean_task_list.pop("lead")))
+
+cross_downstream(
+    contract_model_dependencies,
+    [
+        task_list_first_task(dw_sub_dags["dim_contract_person"]),
+        task_list_first_task(dw_sub_dags["fact_contract_people"]),
+    ],
+)
+
+for task_list in clean_task_list.values():
+    cross_downstream(task_list_last_tasks(task_list), terminate_cluster_task)
+
+for task_list in dw_sub_dags.values():
+    cross_downstream(task_list_last_tasks(task_list), terminate_cluster_task)
