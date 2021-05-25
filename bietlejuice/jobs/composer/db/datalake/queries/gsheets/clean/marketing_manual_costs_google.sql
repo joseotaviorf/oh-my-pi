f0d6d73fa36fa538b94c78ffb189a617d6d45d7e
Victor Lúcio <victor.lucio10@gmail.com>
2021-05-25 17:05:42 -0300

    [DTM-833] FMDC Migration Manual Costs (#4830)
    * removed unused tables
    
    * doc
    
    * dag
    
    * yaml mapping
    
    * clean_queries
    
    * env uppercase
    
    * query database fix
    
    * PR suggestions

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_manual_costs_google.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    account_name,
-    campaign_name,
-    CAST(desktop_cost AS DOUBLE) AS desktop_cost,
-    CAST(mobile_cost AS DOUBLE) AS mobile_cost,
-    CAST(tablet_cost AS DOUBLE) AS tablet_cost,
-    DATE(date) AS dt_cost
-FROM 
-    datalake_gsheets_raw.marketing_manual_costs_google
---
+++ bietlejuice/jobs/composer/db/datalake/queries/marketing_manual_daily_costs/clean/marketing_costs_name_convention_shared_costs.sql
@@ -0,0 +1,20 @@
+SELECT
+    NULLIF(rule_id, '') AS id_rule,
+    NULLIF(side, '') AS side,
+    NULLIF(account_name, '') AS account_name,
+    NULLIF(campaign_name, '') AS campaign_name,
+    NULLIF(ad_group_name, '') AS ad_group_name,
+    NULLIF(utm_term, '') AS utm_term,
+    NULLIF(utm_content, '') AS utm_content,
+    NULLIF(mkt_business, '') AS mkt_business,
+    NULLIF(mkt_origin, '') AS mkt_origin,
+    NULLIF(mkt_channel, '') AS mkt_channel,
+    NULLIF(mkt_medium, '') AS mkt_medium,
+    NULLIF(mkt_source, '') AS mkt_source,
+    CAST(NULLIF(cost, '') AS FLOAT) AS cost,
+    CAST(NULLIF(cost_share_mobile, '') AS FLOAT) AS cost_share_mobile,
+    CAST(NULLIF(cost_share_desktop, '') AS FLOAT) AS cost_share_desktop,
+    CAST(NULLIF(cost_share_other, '') AS FLOAT) AS cost_share_other,
+    CAST(NULLIF(dt, '') AS DATE) AS dt_cost
+FROM
+    datalake_marketing_manual_daily_costs_raw.marketing_costs_name_convention_shared_costs
