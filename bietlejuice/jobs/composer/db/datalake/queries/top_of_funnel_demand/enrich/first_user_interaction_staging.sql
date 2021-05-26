563a747d6f763c351abd6f51626cfea10b33955b
Victor Lúcio <victor.lucio10@gmail.com>
2021-05-26 16:59:12 -0300

    new partition to first interaction (#4850)

--- bietlejuice/jobs/composer/db/datalake/queries/top_of_funnel_demand/enrich/first_user_interaction_staging.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/top_of_funnel_demand/enrich/first_user_interaction_staging.sql
@@ -1,6 +1,7 @@
 SELECT
     ui.id,
     ui.id_tof_user,
+    ui.business_context,
     ui.year,
     ui.month,
     ui.day
@@ -8,6 +9,7 @@ FROM
     datalake_top_of_funnel_demand.user_interactions ui
     LEFT JOIN datalake_top_of_funnel_demand.first_user_interaction fui
         ON ui.id_tof_user = fui.id_tof_user
+        AND ui.business_context = fui.business_context
 WHERE
     ui.year = {year}
     AND ui.month = {month}
