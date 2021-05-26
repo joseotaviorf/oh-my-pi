31194d3bff6e3bef6db4ff065e635f7b52da4f24
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-05-26 17:58:09 -0300

    Adding full queries (#4848)

---
+++ bietlejuice/jobs/composer/db/datalake/queries/credit_evers/enrich/full/credit_evers_original_due_date.sql
@@ -0,0 +1,25 @@
+WITH filtering_invoices AS (
+    SELECT
+        *,
+        RANK() OVER (PARTITION BY id_contract_ebdb ORDER BY dt_contract_updated DESC) AS listing_rank
+    FROM
+        datalake_credit_evers.credit_evers_original_due_date_audit
+)
+SELECT 
+    id_contract_ebdb,
+    id_contract_retsuko,
+    id_proposal,
+    months_of_contract,
+    contract_mob_number,
+    contract_ever_number,
+    is_ever,
+    num_overs_in_mob_window,
+    total_due_amount_in_mob_window,
+    num_total_overs,
+    total_due_amount,
+    ts_signature,
+    dt_contract_updated
+FROM 
+    filtering_invoices
+WHERE
+    listing_rank = 1
