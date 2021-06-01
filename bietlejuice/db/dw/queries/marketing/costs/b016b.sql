5e42a3200b1d3422aaabf4a6f5bd4b920fef3ff3
Henrique Akira Miamoto <56843327+akiramiamoto@users.noreply.github.com>
2021-06-01 18:59:33 -0300

    [DTM] Adding branding cost share rules (#4892)
    * adding rule b016b sql
    
    * adding rule b017b sql
    
    * adding rule b018b sql
    
    * adding rules at yml file
    
    * fixing dates
    
    * Update b016b.sql
    
    * Update b017b.sql
    
    * Update b018b.sql

---
+++ bietlejuice/db/dw/queries/marketing/costs/b016b.sql
@@ -0,0 +1,16 @@
+--Branding cost share rule group 1 (https://docs.google.com/spreadsheets/d/11ExDZsnQ1IwiqgSy-13bVV5LGNET-ajUBmNoUryQtZE)
+SELECT
+	dd.sk_date,
+	dr.city_group,
+	CASE
+		WHEN dr.city_group='Campinas' THEN 0.2863
+		WHEN dr.city_group='Florianópolis' THEN 0.116
+        	WHEN dr.city_group='Belo Horizonte' THEN 0.5977
+		ELSE 0
+	END AS share
+FROM
+	dim_date dd
+	JOIN dim_region dr
+		ON dd.date BETWEEN '2021-05-01' AND CURRENT_DATE
+		AND dr.city_group IN ('Campinas','Florianópolis','Belo Horizonte')
+GROUP BY 1,2,3
