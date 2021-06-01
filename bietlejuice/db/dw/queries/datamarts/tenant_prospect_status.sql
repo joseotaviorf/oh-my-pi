07e6bf3ebb7761728571f059496a03c073aa6903
Gustavo Moura <54562374+gustsm@users.noreply.github.com>
2021-06-01 19:19:03 -0300

    [DTM-843] Modifying TP status explore to get all sk_users with contracts (#4882)
    * modifying TP status explore
    
    * standardizing categories

--- bietlejuice/db/dw/queries/datamarts/tenant_prospect_status.sql
+++ bietlejuice/db/dw/queries/datamarts/tenant_prospect_status.sql
@@ -1,7 +1,23 @@
 WITH
+tps_contracts AS (
+    SELECT DISTINCT
+        flrf.sk_client,
+        dr.city_group,
+        dc.ts_signature,
+        dc.sk_contract,
+        'tenant prospect' AS contract_role,
+        CAST(dt_annulment AS TIMESTAMP) AS ts_annulment,
+        NULL::INT AS cs_order
+    FROM
+        dim_contract AS dc
+        JOIN fact_listing_rent_flows AS flrf
+            ON dc.sk_contract = flrf.sk_contract
+        JOIN dim_region AS dr
+            ON flrf.sk_region = dr.sk_region
+),
 contract_person AS (
     SELECT DISTINCT
-        COALESCE(NULLIF(fcp.sk_user, -1), flrf.sk_client) AS sk_client,
+        fcp.sk_user AS sk_client,
         dr.city_group,
         dc.ts_signature,
         dc.sk_contract,
@@ -20,9 +36,27 @@ contract_person AS (
             ON flrf.sk_region = dr.sk_region
         JOIN quintoandar.fact_contract_people AS fcp
             ON dc.sk_contract = fcp.sk_contract
+        FULL OUTER JOIN tps_contracts AS tc -- Anti Join with tps_contracts to get only aditional users
+            ON tc.sk_client = fcp.sk_user
+            AND tc.ts_signature = dc.ts_signature
     WHERE
-        ts_signature IS NOT NULL
+        tc.sk_client IS NULL                -- Anti Join with tps_contracts to get only aditional users
+        AND dc.ts_signature IS NOT NULL
+        AND fcp.sk_user != -1
         AND fcp.contract_role IN ('tenant', 'dweller')
+),
+contract_flows AS (
+    SELECT
+        *
+    FROM
+        tps_contracts
+    UNION
+    SELECT
+        *
+    FROM
+        contract_person
+    WHERE
+        cs_order = 1
 ),
 events_base AS (
     -- Rent Flows
@@ -48,9 +82,7 @@ events_base AS (
         ts_signature AS ts_event,
         'contract signed as ' ||  contract_role AS event_type
     FROM
-        contract_person
-    WHERE
-        cs_order = 1
+        contract_flows
 
     UNION ALL
 
@@ -61,7 +93,7 @@ events_base AS (
         ts_annulment AS ts_event,
         'contract ended' AS event_type
     FROM
-        contract_person
+        contract_flows
     WHERE
         ts_annulment IS NOT NULL
 ),
@@ -105,6 +137,8 @@ churned_periods AS (
             ELSE 'CHURNED'
         END AS status,
         CASE
+            WHEN cd.event_type = 'contract signed as tenant prospect'
+                THEN 'Inactive by renting (tp)'
             WHEN cd.event_type = 'contract signed as tenant'
                 THEN 'Inactive by renting (tenant)'
             WHEN cd.event_type = 'contract signed as dweller'
@@ -113,7 +147,8 @@ churned_periods AS (
                 THEN 'Contract ended'
             WHEN cd.event_type = 'rent_flow'
                 THEN 'Inactivity'
-        END AS status_detail
+        END AS status_detail,
+        NULL::INT AS activation_order
     FROM
         aux_churn_dates AS cd
     WHERE
@@ -126,7 +161,8 @@ active_periods AS (
         MIN(cd.ts_event) AS ts_start,
         cd.ts_next_churn AS ts_end,
         'ACTIVE' AS status,
-        NULL::TEXT AS status_detail
+        NULL::TEXT AS status_detail,
+        ROW_NUMBER() OVER(PARTITION BY cd.sk_client, cd.city_group ORDER BY ts_start) AS activation_order -- Get first activetion by client and city group
     FROM
         aux_churn_dates AS cd
     WHERE
@@ -134,7 +170,8 @@ active_periods AS (
     GROUP BY 1,2,4,5
 ),
 base AS (
-    SELECT *
+    SELECT
+        *
     FROM
         churned_periods
 
@@ -153,13 +190,14 @@ SELECT
     TO_CHAR(ts_end, 'YYYYMMDD') AS sk_end_date,
     status,
     CASE
-        WHEN LAG(status) OVER(PARTITION BY sk_client, city_group ORDER BY ts_start) IS NULL AND status = 'ACTIVE'
+        WHEN activation_order = 1
             THEN 'First activation'
         WHEN LAG(status) OVER(PARTITION BY sk_client, city_group ORDER BY ts_start) = 'CHURNED'
             THEN COALESCE(status_detail, 'Recovered after churn')
         WHEN LAG(status) OVER(PARTITION BY sk_client, city_group ORDER BY ts_start) = 'RENTED'
             THEN COALESCE(status_detail, 'Recovered after renting')
         ELSE status_detail
-    END AS status_detail
+    END AS status_detail,
+	COALESCE(LEAD(status) OVER(PARTITION BY sk_client, city_group ORDER BY ts_start), 'STILL ACTIVE') AS next_status
 FROM
     base
