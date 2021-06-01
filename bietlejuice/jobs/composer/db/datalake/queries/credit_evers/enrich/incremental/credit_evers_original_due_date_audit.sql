5a2e59fecf70d8d6e7a52035e4724ea2799dda96
Giulio Cesare Mastrocinque Santo <32403782+GiulioCMSanto@users.noreply.github.com>
2021-06-01 13:47:04 -0300

    Increased mob array until 40 (#4885)

--- bietlejuice/jobs/composer/db/datalake/queries/credit_evers/enrich/incremental/credit_evers_original_due_date_audit.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/credit_evers/enrich/incremental/credit_evers_original_due_date_audit.sql
@@ -49,7 +49,9 @@ WITH debtors_all_time AS (
 mob_array AS (
   SELECT
     EXPLODE(
-      ARRAY(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+      ARRAY(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+            38, 39, 40)
     ) AS contract_mob_number
 ),
 ever_array AS (
