55880d9fdf23f4b91408285a83812cb2ccfd9434
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-06-09 12:50:46 -0300

    change dot to comma in tenant_entrance query (#4958)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/tenant_entrance_inspection_csat.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/tenant_entrance_inspection_csat.sql
@@ -1,6 +1,6 @@
 SELECT 
     INT(id_do_contrato) AS id_contract,
-    NULLIF(email.'') AS email,
+    NULLIF(email,'') AS email,
     NULLIF(pontos_de_melhoria,'') AS improvement_tags,
     NULLIF(campo_aberto,'') AS comments,
     INT(avaliacao_csat) AS general_satisfaction_evaluation,
