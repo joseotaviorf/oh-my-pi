8795ee79b3f36b8498871c37f5dd54d012ce3f60
Eduardo Trevisani <etgamba@gmail.com>
2021-06-10 16:54:53 -0300

    [DINT-866] Added Sirena Spark jobs (#4947)
    * Added Sirena Spark jobs
    
    * Added response schema into dataframe creation
    
    * Fixed lint
    
    * Removed trailing spaces
    
    * Added status column into interactions schema
    
    * Added countryCode into groups dataframe schema
    
    * Removed unused SparkDataFrameService
    
    * Removed unused imports
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

---
+++ bietlejuice/jobs/composer/dags/sirena/spark_jobs/load_sirena_groups_into_datalake.py
@@ -0,0 +1,92 @@
+import logging
+from argparse import ArgumentParser
+
+from bietlejuice.jobs.composer.base.api.api_enum import APIEnum
+from bietlejuice.jobs.composer.base.db import DatalakeMetastoreService
+from bietlejuice.jobs.composer.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+from quintoandar_logger import QuintoAndarLogger
+from quintoandar_sirena_api_client.consumers import CONSUMERS
+from quintoandar_sirena_api_client.clients import SirenaClient
+
+JOB_NAME = "load_sirena_into_datalake"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+SCHEMA = """
+    id STRING NOT NULL COMMENT 'Enum: "whatsapp" "facebook"',
+    displayName STRING COMMENT 'Public name of the group',
+    name STRING COMMENT 'Internal name of the group.',
+    parent STRING COMMENT 'Parent Group Id',
+    ancestors ARRAY<STRING> COMMENT 'Ancestors of the group',
+    descendants ARRAY<STRING> COMMENT 'Descendants of the group',
+    type ARRAY<STRING> COMMENT 'Type of group (store, leadProvider, group, OEM)',
+    timezone STRING COMMENT 'The timezone configured for the group',
+    countryCode STRING
+"""
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("environment")
+    parser.add_argument("source")
+    parser.add_argument("datalake_bucket")
+    parser.add_argument("table_name")
+    args = parser.parse_args()
+
+    environment = args.environment
+    source = args.source
+    datalake_bucket = args.datalake_bucket
+    table_name = args.table_name
+
+    logger.info(
+        f"m={JOB_NAME}, "
+        f"environment={environment}, "
+        f"source={source}, "
+        f"datalake_bucket={datalake_bucket}, "
+        f"msg=Spark job arguments"
+    )
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    s3_loader = S3Loader()
+    spark_client = SparkClient()
+    spark_metastore_service = SparkMetastoreService(spark_client)
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+    datalake_info = DatalakeMetastoreService.get_db_info(
+        environment, source, datalake_bucket
+    )
+
+    file_type = SparkTableStorageFormat.DEFAULT_RAW
+    filesystem_path = datalake_info["db_raw_path"]
+    database_name = datalake_info["db_raw_databricks"]
+    spark_metastore_service.create_database(database_name)
+
+    api_token = dbutils.secrets.get(scope="quintoandar", key=APIEnum.SIRENA)
+
+    sirena_client = SirenaClient(api_token=api_token)
+    consumer_groups = CONSUMERS["groups"](client=sirena_client)
+
+    results = consumer_groups.sync()
+
+    if results:
+        df = spark_client.create_dataframe(results, schema=SCHEMA)
+        s3_loader.load_df(
+            df=df, s3_path=f"{filesystem_path}{table_name}", format_options=file_type
+        )
+
+        spark_metastore_loader.update_metastore(
+            df=df,
+            database_name=database_name,
+            table_name=table_name,
+            format_options=file_type,
+            database_location=filesystem_path,
+            force_recreate=True,
+        )
+
+        spark_metastore_service.refresh_table(database_name, table_name)
