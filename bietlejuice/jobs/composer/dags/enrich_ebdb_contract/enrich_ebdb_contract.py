94caca990a11df9e49a7805a4acbcc44526df901
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2021-06-11 10:51:26 -0300

    [DAE-376] Implement TaskGroup builder in dw_booking (#4963)
    * Implement TaskGroup builder in dw_booking
    
    * Add dependencies setting
    
    * remove old code

--- bietlejuice/jobs/composer/dags/enrich_ebdb_contract/enrich_ebdb_contract.py
+++ bietlejuice/jobs/composer/dags/enrich_ebdb_contract/enrich_ebdb_contract.py
@@ -18,8 +18,8 @@ MAIN_START_DATE = datetime(2020, 7, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
 CONTEXT = "ebdb_contract"
 DAG_NAME = f"enrich_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
+
 ENV = os.environ.get("ENVIRONMENT")
-SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
 DATALAKE_BUCKET = Variable.get("datalake_bucket")
 ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
 DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
