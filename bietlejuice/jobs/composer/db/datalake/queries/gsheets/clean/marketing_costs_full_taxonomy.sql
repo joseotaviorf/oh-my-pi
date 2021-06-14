0cacbe82c36e3f5bf90a581d61858f7141c96ac1
Victor Lúcio <victor.lucio10@gmail.com>
2021-06-14 14:55:25 -0300

    New Simplified Taxonomy Table (#5003)
    * changed maketing costs taxonomy table
    
    * reduced number of columns

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_costs_full_taxonomy.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/marketing_costs_full_taxonomy.sql
@@ -1,13 +1,9 @@
 SELECT
-    NULLIF(fact_cost, '') AS media_fact_table_name,
-    NULLIF(schema, '') AS schema,
-    NULLIF(table_dim, '') AS media_dim_table_name,
     NULLIF(account_name, '') AS account_name,
     NULLIF(report_type, '') AS report_type,
     NULLIF(ad_type, '') AS ad_type,
     NULLIF(campaign_origin_aquisition, '') AS campaign_origin_acquisition,
     NULLIF(origin, '') AS origin,
-    NULLIF(split_into_cities, '') AS split_into_cities,
     NULLIF(side, '') AS side,
     NULLIF(mkt_category, '') AS mkt_category,
     NULLIF(mkt_flow, '') AS mkt_flow,
