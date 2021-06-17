558f7f1c3129bcfa156ddc8c039f4035257c57d0
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-06-17 16:19:44 -0300

    [DINT-900] Adding TaskGroups to enrich_amplitude_talk_to_agent (#5043)
    * Adding TaskGroups to enrich_amplitude_talk_to_agent
    
    * fix
    
    * updating doc
    
    * moving cluster
    
    * updating cluster

--- bietlejuice/jobs/composer/dags/enrich_amplitude_talk_to_agent/enrich_amplitude_talk_to_agent.md
+++ bietlejuice/jobs/composer/dags/enrich_amplitude_talk_to_agent/enrich_amplitude_talk_to_agent.md
@@ -2,7 +2,10 @@
 ​
 ### Purpose
 ​
-This DAG creates the enriched table talk_to_agent_events, which is used on Sale modelling for fact_sale_flows. This table contains events data from feature "Fale com Corretor", active during the start of the Covid-19 pandemic, as an alternative for blocking presential visits. 
+This DAG brings data from Talk To Agent events, which is used on Sale modelling for `fact_sale_flows`. This table contains events data from feature "Fale com Corretor", active during the start of the Covid-19 pandemic, as an alternative for blocking presential visits. 
+
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
 
 ### Execution​ Interval
 This DAG is no longer running since we don't have any updated data about TTA events. Anyway, it is still part of the funnel, so we must keep it.
@@ -11,19 +14,13 @@ More information about run time [here]({chart_url}{dag_id})
 
 ### Outputs
 ​
-This pipeline produces the following output table: 
+This pipeline produces the following output table, via full load: 
 ​
 - `talk_to_agent_events` – Contains information about TTA events.
 ​
-### Responsible Data Engineering Team
+### Responsible Data Teams
 
-For any questions or concerns about this DAG, please contact the Data Engineering Team responsible listed in the 
+For any questions or concerns about this DAG, please contact the Data Engineering Team or 
+the Data Analytics Team responsible listed in the 
 [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
-​
-### Additional Information
-​
-If you need additional information about TTA events data, please contact ForSale Data Analytics Team.  
-​
-### Major Changes (JIRA Tasks)
-​
-[DINT-440](https://quintoandar.atlassian.net/jira/software/projects/DINT/boards/384?selectedIssue=DINT-440)
+</details>
--- bietlejuice/jobs/composer/dags/enrich_amplitude_talk_to_agent/enrich_amplitude_talk_to_agent.py
+++ bietlejuice/jobs/composer/dags/enrich_amplitude_talk_to_agent/enrich_amplitude_talk_to_agent.py
@@ -1,16 +1,16 @@
-from datetime import datetime
-import pendulum
 import os
+import pendulum
+from datetime import datetime
 
 from airflow.models import DAG, Variable
+from airflow.utils.helpers import chain
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
-from bietlejuice.jobs.composer.services import FileService
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
 
 # This DAG had been turned off since TTA flow does not exist anymore, so we no longer receive updated data.
@@ -22,9 +22,9 @@ MAIN_START_DATE = datetime(2020, 7, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
 CONTEXT = "amplitude_talk_to_agent"
 DAG_NAME = f"enrich_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
+
 ENV = os.environ.get("ENVIRONMENT")
 DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
-
 SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
 DATALAKE_BUCKET = Variable.get("datalake_bucket")
 ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
@@ -37,7 +37,7 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
 )
 
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_bietlejuice_amplitude_talk_to_agent_cluster", deserialize_json=True
+    "databricks_bietlejuice_enrich_amplitude", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
@@ -59,26 +59,24 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
 )
 
-enrich_sub_dag = DatalakeSubDAG(
-    dag_id=DAG_ID,
-    start_date=MAIN_START_DATE,
-    env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    database_base_name=CONTEXT,
-    relative_query_path=CONTEXT,
-    spark_job_paths=SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
-    layer=LayerEnum.ENRICH,
-)
-
-file_list = FileService.list_sql_files_without_extension_from_layer(
-    CONTEXT, LayerEnum.ENRICH.value
-)
-
-enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(dag, file_list)
-
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-create_cluster_task >> list(enrich_sub_dags.values()) >> terminate_cluster_task
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
