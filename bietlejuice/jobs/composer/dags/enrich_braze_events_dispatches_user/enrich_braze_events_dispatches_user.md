000d19a022f71006ab666edba3c16aceae9e936a
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2021-06-21 11:48:15 -0300

    [DG-212] TaskGroup Refactoring - enrich_braze_events_dispatches_user (#5070)
    * Refactoring DAG
    
    * adjusting dependencies
    
    * adjusting doc
    
    * adjusting doc

--- bietlejuice/jobs/composer/dags/enrich_braze_events_dispatches_user/enrich_braze_events_dispatches_user.md
+++ bietlejuice/jobs/composer/dags/enrich_braze_events_dispatches_user/enrich_braze_events_dispatches_user.md
@@ -1,23 +1,24 @@
 ## Enrich Braze Events Dispatches User
-​
+
 ### Purpose
-​
 This DAG creates the incremental enriched tables for dispatches for both tenants and owners users from Braze.
-​
-### Execution​ Interval
+
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
 This DAG is triggered once per day via Mediator.
 
 More information about run time [here]({chart_url}{dag_id}).
 
 ### Outputs
-​
 This pipeline produces the following output tables, on enrich layer: 
-​
+
     - `owners_events`
     - `tenants_events` 
-​
-### Responsible Data Engineering Team
-​
-For any questions or concerns about this DAG, please contact the Data Engineering Team responsible listed in the 
+
+### Responsible Data Teams
+For any questions or concerns about this DAG, please contact the Data Engineering Team or 
+the Data Analytics Team responsible listed in the  
 [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
-​
+</details>
--- bietlejuice/jobs/composer/dags/enrich_braze_events_dispatches_user/enrich_braze_events_dispatches_user.py
+++ bietlejuice/jobs/composer/dags/enrich_braze_events_dispatches_user/enrich_braze_events_dispatches_user.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pendulum
 import os
 
+from airflow.utils.helpers import chain
 from airflow.models import DAG, Variable
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
@@ -9,9 +10,8 @@ from airflow.operators.quintoandar_databricks import (
 )
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
-from bietlejuice.jobs.composer.services import FileService
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
@@ -54,28 +54,26 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
 )
 
-enrich_sub_dag = DatalakeSubDAG(
-    dag_id=DAG_ID,
-    start_date=MAIN_START_DATE,
-    env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    database_base_name=f"{SOURCE}_dispatches_user",
-    relative_query_path=CONTEXT,
-    spark_job_paths=SPARK_JOB_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
-    layer=LayerEnum.ENRICH,
-)
-
-file_list = FileService.list_sql_files_without_extension_from_layer(
-    CONTEXT, LayerEnum.ENRICH.value
-)
-
-enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(
-    dag, file_list, is_incremental=True, partitions=PARTITION_COLS
-)
-
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-create_cluster_task >> list(enrich_sub_dags.values()) >> terminate_cluster_task
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=SPARK_JOB_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=f"{SOURCE}_dispatches_user",
+    target_database_base_name=f"{SOURCE}_dispatches_user",
+    is_incremental=True,
+    partitions=PARTITION_COLS,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
