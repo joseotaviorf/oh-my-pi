abf4182eff84f89e867d29d3d929061e69fb3c1a
Juliana Freire <5251778+jufreire@users.noreply.github.com>
2021-06-21 15:07:23 -0300

    refactoring enrich_firestore to use task groups (#5077)

--- bietlejuice/jobs/composer/dags/enrich_firestore/enrich_firestore.md
+++ bietlejuice/jobs/composer/dags/enrich_firestore/enrich_firestore.md
@@ -2,6 +2,8 @@
 
 ### Purpose
 This DAG creates the enriched tables from Firestore data, for ForSale. 
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
 
 ### Execution Interval
 This DAG is triggered daily, via Mediator, after `firestore` DAG. 
@@ -10,10 +12,13 @@ More information about run time [here]({chart_url}{dag_id}).
 
 ### Outputs
 This pipeline produces the following tables in enrich layer, via full load:
-    - `monday`
-    - `rent_offer`
-    - `sale_offer`
+- `monday`
+- `rent_offer`
+- `sale_offer`
 
-### Responsible Data Engineering Team
-For any questions or concerns about this DAG, please contact the Data Engineering Team responsible listed in the 
+### Responsible Data Teams
+​
+For any questions or concerns about this DAG, please contact the Data Engineering Team or 
+the Data Analytics Team responsible listed in the 
 [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+​</details>
--- bietlejuice/jobs/composer/dags/enrich_firestore/enrich_firestore.py
+++ bietlejuice/jobs/composer/dags/enrich_firestore/enrich_firestore.py
@@ -7,10 +7,10 @@ from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
+from airflow.utils.helpers import chain
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
-from bietlejuice.jobs.composer.services import FileService
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
@@ -25,7 +25,7 @@ ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
 DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
 
 S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
 LOGS_OUTPUT_PATH = f"s3://{Variable.get('databricks_s3_bucket')}/logs/jobs/{DAG_ID}"
 
 CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
@@ -56,26 +56,24 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     libraries=LIBRARIES_DESCRIPTION,
 )
 
-enrich_sub_dag = DatalakeSubDAG(
-    dag_id=DAG_ID,
-    start_date=MAIN_START_DATE,
-    env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    database_base_name=CONTEXT,
-    relative_query_path=CONTEXT,
-    spark_job_paths=BASE_SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
-    layer=LayerEnum.ENRICH,
-)
-
-file_list = FileService.list_sql_files_without_extension_from_layer(
-    CONTEXT, LayerEnum.ENRICH.value
-)
-
-enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(dag, file_list)
-
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-create_cluster_task >> list(enrich_sub_dags.values()) >> terminate_cluster_task
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
