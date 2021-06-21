8afe072cbeb72d16ce273773f5c2d2e323aa25e6
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-06-21 17:07:02 -0300

    Revert "[DECX] task force enrich crm tasks resolution (#5087)" (#5102)
    This reverts commit f32eb86606580a44671da171d0f3fa8e86020972.

--- bietlejuice/jobs/composer/dags/enrich_crm_tasks_resolution/enrich_crm_tasks_resolution.md
+++ bietlejuice/jobs/composer/dags/enrich_crm_tasks_resolution/enrich_crm_tasks_resolution.md
@@ -2,9 +2,6 @@
 
 Creates CRM (Customer Relationship Management) tasks resolution tables which union actions and status histories from CRM tasks in enrich `datalake_crm` layer and loads them into datalake enrich `datalake_crm_tasks_resolution` layer.
 
-<details>
-  <summary><strong> > DAG details (click to expand)</strong></summary>
-
 ### Execution Interval
 
 Daily. More information about run time [here]({chart_url}{dag_id}).
@@ -18,5 +15,3 @@ Daily. More information about run time [here]({chart_url}{dag_id}).
 ### Responsible Data Team
 ​
 For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
-
-</details>
--- bietlejuice/jobs/composer/dags/enrich_crm_tasks_resolution/enrich_crm_tasks_resolution.py
+++ bietlejuice/jobs/composer/dags/enrich_crm_tasks_resolution/enrich_crm_tasks_resolution.py
@@ -3,15 +3,15 @@ import pendulum
 import os
 
 from airflow.models import DAG, Variable
-from airflow.utils.helpers import chain
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
+from bietlejuice.jobs.composer.services import FileService
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
-from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2015, 10, 29, 0, 0, 0, tzinfo=LOCAL_TZ)
@@ -25,7 +25,7 @@ ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
 DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
 
 S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
+BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
 LOGS_OUTPUT_PATH = f"s3://{Variable.get('databricks_s3_bucket')}/logs/jobs/{DAG_ID}"
 
 CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
@@ -49,25 +49,27 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
 )
 
+enrich_sub_dag = DatalakeSubDAG(
+    dag_id=DAG_ID,
+    start_date=MAIN_START_DATE,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    database_base_name=CONTEXT,
+    relative_query_path=CONTEXT,
+    spark_job_paths=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    layer=LayerEnum.ENRICH,
+)
+
+full_file_list = FileService.list_sql_files_without_extension_from_layer(
+    CONTEXT, LayerEnum.ENRICH.value, schema="full"
+)
+full_enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(
+    dag, full_file_list, schema="full", partitions=["year", "month", "day"]
+)
+
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-datalake_task_group = DatalakeTaskGroup(
-    dag=dag,
-    env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    relative_query_path=CONTEXT,
-    spark_jobs_path=SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
-)
-
-enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
-    layer=LayerEnum.ENRICH,
-    source_database_base_name=CONTEXT,
-    target_database_base_name=CONTEXT,
-    partitions=["year", "month", "day"],
-)
-
-chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
-chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
+create_cluster_task >> list(full_enrich_sub_dags.values()) >> terminate_cluster_task
