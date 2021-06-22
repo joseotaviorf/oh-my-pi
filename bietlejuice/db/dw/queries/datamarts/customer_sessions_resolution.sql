0007440e58070b2022f6eb816f063594617e4687
Giovani Baroni R. Miguel <giovanibrmiguel@gmail.com>
2021-06-22 10:47:52 -0300

    Migrating Customer Sessions Resolution datamart to new front tickets schema (#5105)
    * Changed resolution datamart to reflect new modeling.
    
    * Deleted unnecessary yaml file.
    
    * Added requested changes.
    
    * Added fix to only consider closed tickets and corrected a typo.

--- bietlejuice/db/dw/queries/datamarts/customer_sessions_resolution.sql
+++ bietlejuice/db/dw/queries/datamarts/customer_sessions_resolution.sql
@@ -1,50 +1,38 @@
 WITH front_tickets AS (
-    SELECT DISTINCT
-        ticket_summary.sk_ticket,
-        ticket_summary.sk_user,
-        ticket_summary.sk_contract_ticket,
-        ticket_summary.channel,
-        ticket_summary.group_name,
-        ticket_summary.ticket_area,
-        ticket_summary.rental_process_step,
-        CASE
-            WHEN ticket_summary.rental_process_step IN ('Onboarding','Ongoing','Offboarding','Relacionamento') THEN 'POS'
-            WHEN ticket_summary.rental_process_step LIKE 'Pré-contrato' THEN 'PRE'
-        END AS rental_process_group,
-        ticket_summary.subject,
-        ticket_summary.customer_type_tag,
-        ticket_summary.contact_motivation_tag,
-        ticket_summary.contact_theme_tag,
-        ticket_summary.ts_created_local,
-        ticket_summary.ts_solved_local,
-        ticket_summary.ts_closed_local,
-        MAX(ticket_summary.back_ticket) AS back_ticket,
-        ticket_summary.has_department_transfers,
-        ticket_summary.has_analyst_transfers,
-        ticket_summary.resolution_survey,
-        ticket_summary.call_direction
+    SELECT
+        ft.sk_ticket,
+        ft.sk_contract,
+        MIN(ft.sk_user) AS sk_user,
+        ft.channel,
+        MAX(ft.back_ticket) AS back_ticket,
+        direction,
+        status,        
+        ft.has_transfers,
+        ft.total_departments,
+        ft.is_solved,
+        ts_started,
+        ts_closed
     FROM
-        datamarts.ticket_summary
+        front_tickets.fact_ticket as ft
+    LEFT JOIN
+        front_tickets.dim_channel
+            using(sk_channel)
     WHERE
-        sk_user > -1 --we are only including identified users
-        AND ((ticket_summary.channel = 'call' AND is_answered = 1) OR (ticket_summary.channel = 'chat') OR (ticket_summary.channel = 'email'))
-        AND is_automatic_email = 0
-        AND is_bot = 0
-        AND is_closed_by_merge = 0
-    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20
+        sk_user > -1
+    GROUP BY 1,2,4,6,7,8,9,10,11,12
 ),
 user_recontacts AS (
     SELECT DISTINCT
         sk_ticket,
-        COALESCE(telefone_principal, cpf, sk_contract_ticket::TEXT, sk_user::TEXT) AS ticket_user,
+        COALESCE(sk_user::TEXT,telefone_principal, cpf, sk_contract::TEXT) AS ticket_user,
         CASE
-            WHEN DATEDIFF(hour, LAG(ts_created_local, 1) OVER(PARTITION BY ticket_user ORDER BY ts_created_local), ts_created_local) < 168 THEN 1
+            WHEN DATEDIFF(hour, LAG(ts_started, 1) OVER(PARTITION BY ticket_user ORDER BY ts_started), ts_started) < 168 THEN 1
             ELSE 0
         END AS is_recontact,
         CASE
             WHEN is_recontact = 0 THEN sk_ticket
         END AS main_ticket,
-        ts_created_local
+        ts_started
     FROM
         front_tickets
     LEFT JOIN 
@@ -54,10 +42,10 @@ user_recontacts AS (
 ticket_sessions AS (
     SELECT
         sk_ticket,
-        MAX(main_ticket) OVER(PARTITION BY ticket_user ORDER BY ts_created_local ASC ROWS UNBOUNDED PRECEDING) AS ticket_session,
+        MAX(main_ticket) OVER(PARTITION BY ticket_user ORDER BY ts_started ASC ROWS UNBOUNDED PRECEDING) AS ticket_session,
         ticket_user,
         is_recontact,
-        ts_created_local
+        ts_started
     FROM
         user_recontacts
 ),
@@ -66,53 +54,42 @@ tickets_by_ticket_session AS (
         ticket_session,
         ticket_user,
         COUNT(DISTINCT sk_ticket) AS tickets,
-        MIN(ts_created_local) AS ts_created_local
+        MIN(ts_started) AS ts_started
     FROM
         ticket_sessions
     GROUP BY 1, 2
 )
 SELECT
-    tts.ticket_session AS sk_ticket_session,
-    ft.sk_user,
-    sk_contract_ticket,
-    tts.ticket_user AS session_user,
+    tts.ticket_session AS sk_ticket,
+    tts.ticket_user AS sk_user,
+    sk_contract,
     ft.channel,
-    group_name,
-    ticket_area,
-    rental_process_step,
-    rental_process_group,
-    subject,
-    customer_type_tag,
-    contact_motivation_tag,
-    contact_theme_tag,
     ft.back_ticket,
-    has_department_transfers,
-    has_analyst_transfers,
-    ft.resolution_survey,
+    has_transfers,
+    ft.total_departments,
+    ft.is_solved,
     tts.tickets AS ticket_count,
     CASE
         WHEN ft.channel <> 'chat'
             AND ((tickets > 1)
             OR ft.back_ticket IS NOT NULL
-            OR has_department_transfers > 0
-            OR has_analyst_transfers > 0
-            OR ft.resolution_survey = 0)
+            OR has_transfers = True
+            OR is_solved = False)
         THEN 0
         WHEN ft.channel = 'chat'
             AND ((tickets > 1)
             OR ft.back_ticket IS NOT NULL
-            OR has_department_transfers > 0
-            OR ft.resolution_survey = 0)
+            OR total_departments > 1)
         THEN 0
         ELSE 1
     END AS is_fcr,
-    tts.ts_created_local,
-    ts_solved_local,
-    ts_closed_local
+    ft.ts_started,
+    ft.ts_closed
 FROM
     tickets_by_ticket_session AS tts
 LEFT JOIN
     front_tickets AS ft
         ON ft.sk_ticket = tts.ticket_session
 WHERE
-    (call_direction = 'inbound' OR call_direction is null)
+    (direction = 'inbound' OR direction IS NULL)
+    AND status = 'closed'
