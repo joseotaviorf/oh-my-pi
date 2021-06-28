32dc16041bf786b6581e32b77d42e9fd000292ca
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-06-28 17:21:45 -0300

    [DINT-932] Creating enrich_buyer_prospect DAG (#5161)
    * Creating enrich_buyer_prospect DAG
    
    * dependencies

---
+++ bietlejuice/jobs/composer/dags/enrich_buyer_prospect/enrich_buyer_prospect.md
@@ -0,0 +1,26 @@
+## Enrich Buyer Prospect
+
+### Purpose
+
+This DAG creates the enriched table for buyer prospect context. 
+
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+
+This DAG is triggered daily via Mediator.
+
+More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+This pipeline produces the following output tables in the enrich layer, via full load: 
+    - `buyer_prospect`
+
+### Responsible Data Teams
+
+For any questions or concerns about this DAG, please contact the Data Engineering Team or 
+the Data Analytics Team responsible listed in the 
+[DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+</details>
---
+++ bietlejuice/jobs/composer/dags/enrich_buyer_prospect/enrich_buyer_prospect.py
@@ -0,0 +1,76 @@
+import os
+import pendulum
+from datetime import datetime
+
+from airflow.models import DAG, Variable
+from airflow.utils.helpers import chain
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2020, 7, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+CONTEXT = "buyer_prospect"
+DAG_NAME = f"enrich_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+
+ENV = os.environ.get("ENVIRONMENT")
+SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
+DATALAKE_BUCKET = Variable.get("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
+S3_BUCKET = Variable.get("databricks_s3_bucket")
+DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+
+SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
+
+LOGS_OUTPUT_PATH = f"s3://{S3_BUCKET}/logs/jobs/{DAG_ID}"
+
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
