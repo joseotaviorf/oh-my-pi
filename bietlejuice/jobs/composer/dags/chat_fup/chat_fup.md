90e24fe598ef76a6412d8dd2d18b27bd0274062c
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-06-29 16:19:06 -0300

    [DECX] task force chat fup (#5158)
    * refactor chat_fup dag to new standards
    
    * fixing lint
    
    * updating doc
    
    * updatind dependencies.yaml

--- bietlejuice/jobs/composer/dags/chat_fup/chat_fup.md
+++ bietlejuice/jobs/composer/dags/chat_fup/chat_fup.md
@@ -4,6 +4,9 @@
 
 Extraction of [Chat FUP](https://github.com/quintoandar/chatfup) tables into data lake. Chat FUP is a service to manage followups for whatsapp chats.
 
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
 ### Execution Interval
 
 Daily. More information about run time [here]({chart_url}{dag_id}).
@@ -23,3 +26,5 @@ Produces the following output tables in the clean layer:
 
 For any questions or concerns about this DAG, please contact the Data Engineering Team responsible listed in the 
 [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+
+</details>
--- bietlejuice/jobs/composer/dags/chat_fup/chat_fup.py
+++ bietlejuice/jobs/composer/dags/chat_fup/chat_fup.py
@@ -3,19 +3,20 @@ import pendulum
 import os
 
 from airflow.models import DAG, Variable
-import airflow.utils.helpers as airflow_helpers
+from airflow.utils.helpers import chain, cross_downstream
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
-    QuintoAndarDatabricksSubmitRunOperator,
 )
 
-from bietlejuice.jobs.composer.base.airflow import BaseDAG, BaseSubDAG
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.jobs.composer.base.pipeline import LayerEnum
-from bietlejuice.jobs.composer.services import FileService
 
-DAG_NAME = "chat_fup"
-DAG_ID = f"bietlejuice.{DAG_NAME}"
+SOURCE = "chat_fup"
+CONTEXT = SOURCE
+
+DAG_ID = f"bietlejuice.{SOURCE}"
 ENV = os.environ.get("ENVIRONMENT")
 DATALAKE_BUCKET = Variable.get("datalake_bucket")
 ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
@@ -29,13 +30,10 @@ CLUSTER_DESCRIPTION = Variable.get(
     "databricks_bietlejuice_chat_fup", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
-LIBRARIES_DESCRIPTION = Variable.get(
-    "bietlejuice_default_libraries", deserialize_json=True
-)
 
 # bietlejuice paths
 S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{DAG_NAME}/"
+RAW_SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/load_chat_fup_into_datalake.py"
 BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
 
 # dag params
@@ -52,133 +50,43 @@ dag = DAG(
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
-        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
-    ),
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID),
 )
-
-
-def clean_tasks(sub_dag_name, table_name, slugged_table_name):
-    sub_dag = BaseSubDAG(
-        sub_dag_name=sub_dag_name,
-        dag_name=DAG_ID,
-        schedule_interval=MAIN_SCHEDULE_INTERVAL,
-        start_date=MAIN_START_DATE,
-    )._build_local_dag()
-
-    load_clean_table_task = QuintoAndarDatabricksSubmitRunOperator(
-        dag=sub_dag,
-        task_id=f"create-clean-{slugged_table_name}-in-data-lake",
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_clean_table_in_datalake.py",
-                "parameters": [table_name, ENV, DATALAKE_BUCKET],
-            }
-        },
-    )
-
-    create_external_table_task = QuintoAndarDatabricksSubmitRunOperator(
-        dag=sub_dag,
-        task_id=f"create-clean-external-{slugged_table_name}-table",
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_clean_external_tables.py",
-                "parameters": [
-                    table_name,
-                    ENV,
-                    DATALAKE_BUCKET,
-                    ATHENA_QUERY_RESULT_LOCATION,
-                ],
-            }
-        },
-    )
-
-    sync_metastore_table_task = QuintoAndarDatabricksSubmitRunOperator(
-        task_id=f"sync-hive-metastore-table",
-        dag=sub_dag,
-        json={
-            "spark_python_task": {
-                "python_file": BASE_SPARK_JOBS_PATH + "sync_metastore_tables.py",
-                "parameters": [
-                    DATALAKE_BUCKET,
-                    LayerEnum.CLEAN.value,
-                    DAG_NAME,
-                    "--table-name",
-                    table_name,
-                ],
-            }
-        },
-    )
-
-    load_clean_table_task >> create_external_table_task
-    load_clean_table_task >> sync_metastore_table_task
-
-    return sub_dag
-
-
-def build_clean_subdags(prev_task, next_task):
-    # create subdag for each clean table
-    file_list = FileService.list_layer_sql_files(DAG_NAME, "clean")
-
-    if file_list:
-        for file_name in file_list:
-            file_name = FileService.remove_file_extension(file_name)
-            slugged_table_name = file_name.replace("_", "-")
-            table_sub_dag = BaseSubDAG.get_sub_dag_operator(
-                dag=dag,
-                sub_dag_name=f"load-{slugged_table_name}-to-datalake-clean",
-                sub_dag_func=clean_tasks,
-                table_name=file_name,
-                slugged_table_name=slugged_table_name,
-            )
-            prev_task >> table_sub_dag >> next_task
-    else:
-        prev_task >> next_task
-
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag,
-    task_id="create-cluster",
-    cluster_configuration=CLUSTER_DESCRIPTION,
-    libraries=LIBRARIES_DESCRIPTION,
-)
-
-chat_fup_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
-    task_id="chat-fup-to-datalake-raw",
-    dag=dag,
-    json={
-        "spark_python_task": {
-            "python_file": SPARK_JOBS_PATH + "load_chat_fup_into_datalake.py",
-            "parameters": [ENV, DATALAKE_BUCKET],
-        }
-    },
-)
-
-sync_metastore_raw_tables_task = QuintoAndarDatabricksSubmitRunOperator(
-    task_id="sync-hive-metastore-raw-tables",
-    dag=dag,
-    json={
-        "spark_python_task": {
-            "python_file": BASE_SPARK_JOBS_PATH + "sync_metastore_tables.py",
-            "parameters": [
-                DATALAKE_BUCKET,
-                LayerEnum.RAW.value,
-                DAG_NAME,
-                "--all-tables",
-            ],
-        }
-    },
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-create_cluster_task >> chat_fup_to_datalake_raw_task
-build_clean_subdags(chat_fup_to_datalake_raw_task, terminate_cluster_task)
-
-airflow_helpers.chain(
-    chat_fup_to_datalake_raw_task,
-    sync_metastore_raw_tables_task,
-    terminate_cluster_task,
+task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
 )
+
+raw_task_groups = task_group.build_raw_task_group_for_all_tables(
+    source=SOURCE,
+    target_database_base_name=SOURCE,
+    extraction_spark_job_file=RAW_SPARK_JOB_PATH,
+)
+
+clean_task_groups = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.CLEAN,
+    source_database_base_name=SOURCE,
+    target_database_base_name=SOURCE,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_groups))
+
+cross_downstream(
+    DatalakeTaskGroup.last_tasks(raw_task_groups),
+    DatalakeTaskGroup.all_first_tasks(clean_task_groups),
+)
+
+terminate_cluster_task.set_upstream(DatalakeTaskGroup.all_last_tasks(clean_task_groups))
