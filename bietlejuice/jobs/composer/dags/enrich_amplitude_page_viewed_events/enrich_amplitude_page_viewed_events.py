a000064fa223ee6545500192894d778c42d86b7a
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2021-07-15 14:09:30 -0300

    [DAE-452] Refactor Amplitude enrichment DAGs (#5282)
    * Refactor SparkSQLs folder name for enrich_amplitude_offer
    
    * Refactor SparkSQLs folder name for enrich_amplitude_page_viewed_events
    
    * Refactor SparkSQLs folder name for enrich_amplitude_talk_to_agent
    
    * Refactor SparkSQLs folder name for enrich_amplitude_user
    
    * Refactor SparkSQLs folder name for enrich_amplitude_visit
    
    * Fix to non yet refactored DAGs (enrich_amplitude_page_viewed_events and enrich_amplitude_talk_to_agent)

--- bietlejuice/jobs/composer/dags/enrich_amplitude_page_viewed_events/enrich_amplitude_page_viewed_events.py
+++ bietlejuice/jobs/composer/dags/enrich_amplitude_page_viewed_events/enrich_amplitude_page_viewed_events.py
@@ -61,14 +61,14 @@ enrich_sub_dag = DatalakeSubDAG(
     env=ENV,
     datalake_bucket=DATALAKE_BUCKET,
     database_base_name=CONTEXT,
-    relative_query_path=CONTEXT,
+    relative_query_path=DAG_NAME,
     spark_job_paths=SPARK_JOBS_PATH,
     athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
     layer=LayerEnum.ENRICH,
 )
 
 file_list = FileService.list_sql_files_without_extension_from_layer(
-    CONTEXT, LayerEnum.ENRICH.value
+    DAG_NAME, LayerEnum.ENRICH.value
 )
 
 enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(dag, file_list)
--- bietlejuice/jobs/composer/dags/enrich_amplitude_user/enrich_amplitude_user.py
+++ bietlejuice/jobs/composer/dags/enrich_amplitude_user/enrich_amplitude_user.py
@@ -65,7 +65,7 @@ datalake_task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
     datalake_bucket=datalake_bucket,
-    relative_query_path=CONTEXT,
+    relative_query_path=DAG_NAME,
     spark_jobs_path=BASE_SPARK_JOBS_PATH,
     athena_query_result_location=athena_query_results_bucket,
 )
