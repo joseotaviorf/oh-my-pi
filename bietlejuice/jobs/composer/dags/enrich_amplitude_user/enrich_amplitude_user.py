01518f6801d13fe77902aa6f9e1fc39b35e84718
Eduardo Trevisani <etgamba@gmail.com>
2021-07-15 17:37:26 -0300

    Changed cluster for enrich_amplitude_user (#5292)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/enrich_amplitude_user/enrich_amplitude_user.py
+++ bietlejuice/jobs/composer/dags/enrich_amplitude_user/enrich_amplitude_user.py
@@ -34,7 +34,9 @@ doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
 BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_bietlejuice_enrich_amplitude", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
     "destination"
 ] = f"{spark_jobs_logs_path}{DAG_ID}"
