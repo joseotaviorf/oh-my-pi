b3d87f67de55e0519c35d3a51bfdd686e57cb5fb
marcussgomes <58474731+marcussgomes@users.noreply.github.com>
2021-07-16 15:22:24 -0300

    [DRO-359] Adding workflows dates & status on Workflows-Transition from CRM (#5307)
    * Adding info about workflow dates and status
    
    * identation

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_crm/enrich/incremental/workflows_transitions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_crm/enrich/incremental/workflows_transitions.sql
@@ -1,6 +1,10 @@
 WITH exploded_transitions AS (
     SELECT 
         id,
+        status,
+        start_date_object,
+        updated_date_object,
+        end_date_object,
         EXPLODE(
             FROM_JSON(REPLACE(transitions,'$',''), 
             'ARRAY<
@@ -29,11 +33,15 @@ SELECT
     GET_JSON_OBJECT(REPLACE(id,'$',''),'$.oid') AS id_workflow,
     transition.from.taskId.oid AS id_task_from,
     transition.to.taskId.oid AS id_task_to,
+    status,
     transition.assignmentMethod AS assignment_method,
     transition.from.taskDefinition AS definition_task_from,
     transition.to.taskDefinition AS definition_task_to,
     transition.context AS context,
     COALESCE(CAST(transition.to.endWorkflow AS BOOLEAN), false) AS is_end_of_workflow,
+    CAST(GET_JSON_OBJECT(REPLACE(start_date_object,'$',''),'$.date') AS TIMESTAMP) AS ts_workflow_started,
+    CAST(GET_JSON_OBJECT(REPLACE(updated_date_object,'$',''),'$.date') AS TIMESTAMP) AS ts_workflow_updated,
+    CAST(GET_JSON_OBJECT(REPLACE(end_date_object,'$',''),'$.date') AS TIMESTAMP) AS ts_workflow_ended,
     transition.date.date AS ts_transitioned,
     year,
     month,
