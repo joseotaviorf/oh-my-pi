8b48c903238002c0daf1b992437136c3d88937d6
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-07-19 11:36:01 -0300

    Removing enrich_user_phone DAG (#5311)

--- bietlejuice/jobs/composer/dags/enrich_user_phone/__init__.py
+++
@@ -1,5 +0,0 @@
-from bietlejuice.jobs.composer.base.db import QUERIES_DATALAKE_PATH
-
-DAG_NAME = "enrich_user_phone"
-
-QUERIES_ENRICH_USER_PHONE_DATALAKE_PATH = QUERIES_DATALAKE_PATH + DAG_NAME
--- bietlejuice/jobs/composer/dags/enrich_user_phone/spark_jobs/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-SOURCE = "user_phone"
-
-logging.getLogger("py4j").setLevel(logging.ERROR)
