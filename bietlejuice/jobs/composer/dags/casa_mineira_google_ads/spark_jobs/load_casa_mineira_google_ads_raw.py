30e03b979217a864cb1bc8947b9671063aa61405
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-07-20 19:10:44 -0300

    [Dint-956] Filter google ads spark job for CM (#5237)
    * spark job for google ads cm (same used in DTM-931 except for the filter)
    
    * set rlike filter
    
    * passing source as arg, converted partition_cols to config args and used standard log msg
    
    * added a new line at the end of config file (No more tears, Sonar)
    
    * choose source bucket through env arg
    
    * fix flake
    
    * convert lists to dicts in config file
    
    * remove commas from report_folde_mapping
    
    * create database
    
    * fix udf to slugify
    
    * remove blank line between yaml items and convert log to two lines
    
    * removing trailing whitespace
    
    * splitting config files in spark job to be in the ConfigurationService standard
    
    * fix raw file path
    
    * renaming the dag to something near of other dags of casa mineira
    
    * renaming raw job name

---
+++ bietlejuice/jobs/composer/dags/casa_mineira_google_ads/spark_jobs/load_casa_mineira_google_ads_raw.py
@@ -0,0 +1,98 @@
+from argparse import ArgumentParser
+from pyspark.sql.functions import col, lit, udf
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.db import DatalakeMetastoreService
+from bietlejuice.jobs.composer.base.spark import SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from bietlejuice.jobs.composer.formatters import StringFormatter
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+JOB_NAME = "casa_mineira_google_ads_load_to_raw"
+
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument("env")
+    parser.add_argument("datalake_bucket", type=str, help="target bucket")
+    parser.add_argument("source", type=str, help="name of the source")
+    parser.add_argument("report_type")
+    parser.add_argument("execution_date")
+
+    args = parser.parse_args()
+
+    env = args.env
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+    report_type = args.report_type
+    execution_date = args.execution_date
+    config_service = ConfigurationService(source)
+
+    ads_performance_bucket = config_service.get_config("ads_performance_bucket")[env]
+    raw_partition_columns = config_service.get_config("raw_partition_columns")
+    report_schemas = config_service.get_config("report_schemas")
+    report_folder = config_service.get_config("report_folder_mapping")[report_type]
+    report_account_regex_filter = config_service.get_config(
+        "report_account_regex_filter"
+    )
+
+    logger.info(
+        f"""m=__main__, environment={env}, source={source}, datalake_bucket={datalake_bucket},
+        report_type={report_type}, execution_date={execution_date}, msg=Starting spark job..."""
+    )
+
+    spark_client = SparkClient()
+    s3_loader = S3Loader()
+    spark_metastore_service = SparkMetastoreService(spark_client)
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+
+    db_info = DatalakeMetastoreService.get_db_info(env, source, datalake_bucket)
+    database_name = db_info["db_raw_databricks"]
+    database_location = db_info["db_raw_path"]
+    spark_metastore_service.create_database(database_name)
+
+    df = (
+        spark_client.conn.read.option("header", "true")
+        .option(
+            "basePath",
+            f"s3://{ads_performance_bucket}/google-reports/report={report_folder}",
+        )
+        .csv(
+            f"s3://{ads_performance_bucket}/google-reports/report={report_folder}/*/dt={execution_date}"
+        )
+        .filter(col("Account").rlike(report_account_regex_filter))
+        .withColumn("acc", udf(StringFormatter.slugify)(col("Account")))
+        .withColumn("raw_report_type", lit(report_folder))
+        .toDF(*report_schemas[report_type])
+    )
+
+    s3_loader.load_df(
+        df=df,
+        format_options=SparkTableStorageFormat.DEFAULT_RAW,
+        s3_path=f"{database_location}{report_type}",
+        partitions=raw_partition_columns,
+    )
+
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+    spark_metastore_loader.update_metastore(
+        df=df,
+        database_name=database_name,
+        table_name=report_type,
+        format_options=SparkTableStorageFormat.DEFAULT_RAW,
+        database_location=database_location,
+        partitions=raw_partition_columns,
+    )
+
+    spark_metastore_service.create_new_partitions_from_df(
+        df=df,
+        database_name=database_name,
+        table_name=report_type,
+        partition_cols=raw_partition_columns,
+    )
