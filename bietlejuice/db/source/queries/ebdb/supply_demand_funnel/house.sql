8025b7063b4cd0258b0ae265fd602a7349310acb
Pedro Contart <68445581+pedro-contart@users.noreply.github.com>
2021-07-20 11:33:04 -0300

    Revert "filtered not unique id from query (#4376)" (#5327)
    This reverts commit 385ba1cd2842a960b0a29be10e598ca1b225d64a.

--- bietlejuice/db/source/queries/ebdb/supply_demand_funnel/house.sql
+++ bietlejuice/db/source/queries/ebdb/supply_demand_funnel/house.sql
@@ -272,7 +272,5 @@ left join HouseRegistrationStatus hrs
 left join InstantOffer io
   on io.house_id = i.id
 
-WHERE 
-  DATE(coalesce(i.dataCriacao, '1900-01-01 00:00:00')) <= DATE('{}')
-  AND i.id <> 892955945
+where DATE(coalesce(i.dataCriacao, '1900-01-01 00:00:00')) <= DATE('{}')
 ;
