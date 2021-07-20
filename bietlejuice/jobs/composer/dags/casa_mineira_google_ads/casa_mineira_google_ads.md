4b8de0bce4d13ef813eecf26cda856d49c0c35ee
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-07-20 19:29:29 -0300

    [DINT-957] Casa Mineira Google Ads Dag (#5247)
    * creating dag for google_ads_cm
    
    * dag for google_ads_cm
    
    * remove spark job from dag branch
    
    * add macros namespace before call ds_format
    
    * passing source as an arg to raw spark job and  put partition_cols into config file
    
    * using global config file to retrieve former airflow variables
    
    * Update bietlejuice/jobs/composer/dags/google_ads_cm/google_ads_cm_conf.yml
    
    Taking advice about the refactor done in DTM-936, the **acc** and **load_date** columns were renamed.
    
    Co-authored-by: Eduardo Trevisani <etgamba@gmail.com>
    
    * fix ds_format
    
    * splitting config file in forno and prod versions to use ConfigurationService standard
    
    * update clean_partition_columns to match with changes in clean queries
    
    * added ENV variable in cluster_description
    
    * renaming dag to something near of other dags of casa mineira
    
    Co-authored-by: Eduardo Trevisani <etgamba@gmail.com>

---
+++ bietlejuice/jobs/composer/dags/casa_mineira_google_ads/casa_mineira_google_ads.md
@@ -0,0 +1,27 @@
+## Casa Mineira Google Ads
+
+### Purpose
+
+Casa Mineira Google Ads DAG retrieves Casa Mineira's marketing campaigns data from Google Ads platform. It uses data extracted from [ads-performance](https://github.com/quintoandar/ads-performance) extraction tool, developed by Marketing Tools team.
+
+​<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+
+Daily. More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+Currently, there is the following output table for both our raw and clean layers:
+
+- `google_ads_performance_report`
+- `google_keywords_performance_report`
+- `google_campaigns_performance_report`
+- `google_videos_performance_report`
+
+### Responsible Data Team
+
+For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+
+</details>
---
+++ bietlejuice/jobs/composer/dags/casa_mineira_google_ads/casa_mineira_google_ads.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import pendulum
+import os
+
+from airflow.utils.helpers import chain, cross_downstream
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.formatters import StringFormatter
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+SOURCE = "casa_mineira_google_ads"
+CONTEXT = SOURCE
+DAG_ID = f"bietlejuice.{SOURCE}"
+ENV = os.environ.get("ENVIRONMENT")
+
+config_service = ConfigurationService(SOURCE)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+
+reports_list = config_service.get_config("reports_list")
+clean_partition_columns = config_service.get_config("clean_partition_columns")
+
+RAW_SPARK_JOB_PATH = (
+    f"{databricks_bietlejuice_repo_path}/spark_jobs/{CONTEXT}/load_{CONTEXT}_raw.py"
+)
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = spark_jobs_logs_path
+
+MAIN_START_DATE = datetime(2019, 5, 31, tzinfo=pendulum.timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = "15 3 * * *"
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=datalake_bucket,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=athena_query_results_bucket,
+)
+
+for report_type in reports_list:
+    slugged_table_name = StringFormatter.slugify(report_type)
+
+    raw_task_group = task_group.build_raw_task_group_for_single_table(
+        source=SOURCE,
+        target_database_base_name=CONTEXT,
+        table_name=report_type,
+        extraction_spark_job_file=RAW_SPARK_JOB_PATH,
+        raw_spark_job_extra_args=[
+            SOURCE,
+            report_type,
+            '{{ macros.ds_format(ds, "%Y-%m-%d", "%d-%m-%Y") }}',
+        ],
+    )
+
+    clean_task_group = task_group.build_clean_task_group(
+        source_database_base_name=CONTEXT,
+        target_database_base_name=CONTEXT,
+        table_name=report_type,
+        is_incremental=True,
+        partitions=clean_partition_columns,
+    )
+
+    chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_group))
+
+    cross_downstream(
+        DatalakeTaskGroup.last_tasks(raw_task_group),
+        DatalakeTaskGroup.first_tasks(clean_task_group),
+    )
+
+    terminate_cluster_task.set_upstream(DatalakeTaskGroup.last_tasks(clean_task_group))
