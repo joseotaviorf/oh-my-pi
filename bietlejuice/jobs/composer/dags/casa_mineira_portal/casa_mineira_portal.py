e83362c435110d1aaf0e9c2f9e348689d91e5ba7
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-07-21 17:47:32 -0300

    [DINT-964] Casa Mineira Portal Dag And Spark job  (#5274)
    * casa mineira portal Dag
    
    * added casa mneira portal spark job
    
    * added a folder in forno for cm
    
    * Using configureService and rename config files to the standard nomenclature
    
    * changing dag config to spark job config, updating tables and max_records
    
    * remove early config file
    
    * fix config file name
    
    * putting a bit of context in md file, translate clean tables and add parameters to log
    
    * fixing imovel_ponto_interesse name in config files
    
    * add extract type in md file
    
    * Update casa_mineira_portal.md
    
    added ":"

---
+++ bietlejuice/jobs/composer/dags/casa_mineira_portal/casa_mineira_portal.py
@@ -0,0 +1,99 @@
+from datetime import datetime
+
+import pendulum
+import os
+
+from airflow.models import DAG, Variable
+from airflow.utils.helpers import chain, cross_downstream
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+SOURCE = "casa_mineira_portal"
+CONTEXT = SOURCE
+ENV = os.environ.get("ENVIRONMENT")
+
+config_service = ConfigurationService(SOURCE)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+
+DAG_ID = f"bietlejuice.{SOURCE}"
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
+MAIN_SCHEDULE_INTERVAL = "30 6 * * *"
+
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+RAW_SPARK_JOB_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/{CONTEXT}/load_{CONTEXT}_into_datalake_raw.py"
+
+CLUSTER_DESCRIPTION = Variable.get(f"databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = spark_jobs_logs_path
+
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=datalake_bucket,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=athena_query_results_bucket,
+)
+
+raw_task_groups = task_group.build_raw_task_group_for_all_tables(
+    source=SOURCE,
+    target_database_base_name=CONTEXT,
+    extraction_spark_job_file=RAW_SPARK_JOB_PATH,
+    raw_spark_job_extra_args=[CONTEXT],
+)
+
+
+clean_task_groups = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.CLEAN,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+
+chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_groups))
+
+cross_downstream(
+    DatalakeTaskGroup.last_tasks(raw_task_groups),
+    DatalakeTaskGroup.all_first_tasks(clean_task_groups),
+)
+
+terminate_cluster_task.set_upstream(DatalakeTaskGroup.all_last_tasks(clean_task_groups))
---
+++ bietlejuice/jobs/composer/dags/casa_mineira_portal/spark_jobs/load_casa_mineira_portal_into_datalake_raw.py
@@ -0,0 +1,91 @@
+import logging
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.spark import SparkTableStorageFormat, BaseDBUtils
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.consumers.s3_consumer import S3Consumer
+from bietlejuice.jobs.composer.base.db import DatalakeMetastoreService
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+from bietlejuice.jobs.composer.loaders import S3Loader, SparkMetastoreLoader
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+
+JOB_NAME = "load_casa_mineira_portal_into_datalake_raw"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("environment", help="forno/prod values")
+    parser.add_argument("datalake_bucket", help="bucket value in forno/prod")
+    parser.add_argument("source", help="name of the source")
+
+    args = parser.parse_args()
+
+    environment = args.environment
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+
+    config_service = ConfigurationService(source)
+    source_root_path = config_service.get_config("source_root_path")
+    job_extra_args = config_service.get_config("job_extra_args")
+    consumer_extra_args = job_extra_args.get("consumer")
+    custom_records_per_file = job_extra_args.get("custom_records_per_file")
+
+    logger.info(
+        f"""
+                m=__main__, environment={environment}, source={source}, datalake_bucket={datalake_bucket},
+                source_root_path={source_root_path}, msg=Starting spark job...
+        """
+    )
+
+    # Initializing clients
+    spark_client = SparkClient()
+    s3_consumer = S3Consumer(spark_client)
+
+    # Retrieving folders/tables from root location.
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    tables = dbutils.fs.ls(source_root_path)
+
+    for table in tables:
+        table_name = table.name.replace("/", "")
+        source_path = f"{source_root_path}{table_name}/*.csv"
+        df = s3_consumer.get_data_from_file(path=source_path, **consumer_extra_args)
+
+        db_info = DatalakeMetastoreService.get_db_info(
+            environment, source, datalake_bucket
+        )
+        spark_metastore_service = SparkMetastoreService(spark_client)
+        spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+
+        logger.info(
+            "m=__main__, msg=Creating database in Spark Metastore if not exists..."
+        )
+        database_name = db_info["db_raw_databricks"]
+        format_options = SparkTableStorageFormat.DEFAULT_RAW
+        database_location = db_info["db_raw_path"]
+        spark_metastore_service.create_database(database_name)
+
+        s3_loader = S3Loader()
+
+        s3_loader.load_df(
+            df=df,
+            s3_path=f"{database_location}{table_name}",
+            format_options=format_options,
+            max_records_per_file=custom_records_per_file.get(
+                table_name, s3_loader.MAX_RECORDS_PER_FILE
+            ),
+        )
+
+        spark_metastore_loader.update_metastore(
+            df, database_name, table_name, format_options, database_location
+        )
