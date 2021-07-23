880d1e97e5161ee6a72ffe3400c00eeb16ebc156
Paulo de Castro <83730407+paulobevilaquadecastro@users.noreply.github.com>
2021-07-23 11:21:29 -0300

    Getting only first survey response to DW layer (#5360)
    * Getting only first survey response to DW layer
    
    On DW layer we do need only the first survey response.
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/dw_customer_support/dw/fact_csat.sql
    
    Co-authored-by: Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
    
    Co-authored-by: Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>

--- bietlejuice/jobs/composer/db/datalake/queries/dw_customer_support/dw/fact_csat.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_customer_support/dw/fact_csat.sql
@@ -1,5 +1,19 @@
-SELECT
-  CAST(id_ticket AS BIGINT) AS sk_ticket,
+WITH total_csat AS (
+  SELECT
+    CAST(id_ticket AS BIGINT) AS sk_ticket,
+    channel,
+    csat_comment,
+    source,
+    resolution_survey,
+    csat_score,
+    ts_survey,
+    ts_response,
+    row_number() OVER(PARTITION BY id_ticket ORDER BY ts_response) AS rowcount
+  FROM
+    datalake_customer_support.csat
+)
+SELECT 
+  sk_ticket,
   channel,
   csat_comment,
   source,
@@ -8,4 +22,6 @@ SELECT
   ts_survey,
   ts_response
 FROM
-  datalake_customer_support.csat
+  total_csat
+WHERE
+  rowcount = 1
