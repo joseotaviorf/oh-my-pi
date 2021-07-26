a15fcca37dd46eb6a19417c2fa23d89da0a63d59
Ana Alves <ana.sevlaraujo@gmail.com>
2021-07-26 17:07:03 -0300

    [DTM-875] taskgroup enrich marketing offline costs (#5350)
    * replacing subdags
    
    * fix relative_query_path error
    
    * changed to use config files
    
    * fix database name
    
    * commented lines removed
    
    * testing inner_dag_dependencies
    
    * added set_inner_dag_dependencies to correct inner dependencies
    
    * added offline-costs-and-budget table and tag details to doc.md

--- bietlejuice/jobs/composer/dags/enrich_marketing_costs_offline/enrich_marketing_costs_offline.md
+++ bietlejuice/jobs/composer/dags/enrich_marketing_costs_offline/enrich_marketing_costs_offline.md
@@ -4,6 +4,8 @@
 
 This DAG enriches offline costs manually defined and ingested via gsheets.
 
+<details><summary><string> > DAG details (click to expand)</strong></summary>
+
 ### Execution Interval
 
 Daily, after gsheets tables. More information about run time [here]({chart_url}{dag_id}).
@@ -13,7 +15,9 @@ Daily, after gsheets tables. More information about run time [here]({chart_url}{
 Currently, there are the following output tables in our enrich layer:
 
 - offline_manual_costs
+- offline-costs-and-budget
 
 ### Responsible Data Team
 
 For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+</details>
--- bietlejuice/jobs/composer/dags/enrich_marketing_costs_offline/enrich_marketing_costs_offline.py
+++ bietlejuice/jobs/composer/dags/enrich_marketing_costs_offline/enrich_marketing_costs_offline.py
@@ -1,45 +1,49 @@
+from datetime import datetime
 import pendulum
 import os
-from datetime import datetime
 
-from airflow.models import DAG
-from airflow.models import Variable
+from airflow.models import DAG, Variable
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
-import airflow.utils.helpers as airflow_helpers
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.jobs.composer.base.pipeline import LayerEnum
-from bietlejuice.jobs.composer.services import FileService
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
+from airflow.utils.helpers import chain
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
 
 # ENV setup
 ENV = os.environ.get("ENVIRONMENT")
 
 # DAG params setup
-SOURCE = "marketing_costs"
-DAG_NAME = f"enrich_{SOURCE}_offline"
+CONTEXT = "marketing_costs"
+DAG_NAME = f"enrich_{CONTEXT}_offline"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
-local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
-MAIN_START_DATE = datetime(2021, 3, 31, 0, 0, 0, tzinfo=local_tz)
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+MAIN_START_DATE = datetime(2021, 3, 31, 0, 0, 0, tzinfo=LOCAL_TZ)
 MAIN_SCHEDULE_INTERVAL = None
 
-# s3 paths setup
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-ARTIFACTS_S3_BUCKET = Variable.get("artifacts_default_bucket")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/"
-DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
-LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_ID}"
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
+config_service = ConfigurationService(CONTEXT)
+ATHENA_QUERY_RESULT_BUCKET = config_service.get_config("athena_query_results_bucket")
+DATALAKE_BUCKET = config_service.get_config("datalake_bucket")
+DATABRICKS_BIETLEJUICE_REPO_PATH = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+BASE_SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
+SPARK_JOBS_LOGS_PATH = config_service.get_config("spark_jobs_logs_path")
 DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
 
 # cluster setup
 CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
+    "destination"
+] = f"{SPARK_JOBS_LOGS_PATH}{DAG_ID}"
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -55,33 +59,58 @@ dag = DAG(
     ),
 )
 
-enrich_sub_dag = DatalakeSubDAG(
-    dag_id=DAG_ID,
-    start_date=MAIN_START_DATE,
-    env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    layer=LayerEnum.ENRICH,
-    database_base_name=SOURCE,
-    relative_query_path=f"{SOURCE}/offline",
-    spark_job_paths=f"{SPARK_JOB_PATH}/base",
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
-)
-file_list = FileService.list_sql_files_without_extension_from_layer(
-    f"{SOURCE}/offline", LayerEnum.ENRICH.value
-)
-enrich_sub_dags = enrich_sub_dag.build_subdags_from_sql_files(dag, file_list)
-
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-airflow_helpers.chain(
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=f"{CONTEXT}/offline",
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_BUCKET,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+INNER_DEPENDENCIES = {
+    "offline_costs_and_budget": list(
+        set(enrich_task_groups.keys()).difference(set(["offline_costs_and_budget"]))
+    )
+}
+
+(
+    task_groups_boundaries_without_inner_dependencies,
+    inner_dependencies_task_groups_boundaries,
+) = datalake_task_group.set_inner_dag_dependencies(
+    task_flow_helper=TaskFlowHelper(),
+    task_groups_boundaries=enrich_task_groups,
+    dag_inner_dependencies=INNER_DEPENDENCIES,
+)
+
+chain(
     create_cluster_task,
-    enrich_sub_dags.pop("offline_manual_costs"),
-    enrich_sub_dags.values(),
+    datalake_task_group.all_first_tasks(
+        task_groups_boundaries_without_inner_dependencies
+    )
+    + datalake_task_group.first_tasks(inner_dependencies_task_groups_boundaries),
+)
+
+chain(
+    datalake_task_group.all_last_tasks(
+        task_groups_boundaries_without_inner_dependencies
+    )
+    + datalake_task_group.last_tasks(inner_dependencies_task_groups_boundaries),
     terminate_cluster_task,
 )
