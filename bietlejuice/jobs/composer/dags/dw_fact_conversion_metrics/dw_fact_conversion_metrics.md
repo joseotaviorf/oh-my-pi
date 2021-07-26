ae5cd30285d201b066dff49619fc725e8d7ccb94
Eduardo Trevisani <etgamba@gmail.com>
2021-07-26 13:56:13 -0300

    [DTM-890] Refactoring dw_fact_conversion_metrics DAG with TaskGroups (#5365)
    * Added colapsible section to Doc MD
    
    * Added dependencies
    
    * Replaced SubDAGs by TaskGroups in DAG
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/dw_fact_conversion_metrics.md
+++ bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/dw_fact_conversion_metrics.md
@@ -1,19 +1,22 @@
 ## DW Fact Conversion Metrics
 ### Purpose
 
-This dag loads the models that we use to support our client analysis. There is one for both tenants and owners; The point is to provide some metrics about our clients and how it relates to their interactions on our platform.
+This DAG loads the models that we use to support our client analysis. There is one for both tenants and owners; The point is to provide some metrics about our clients and how they relate to their interactions on our platform.
+
+<details>
+    <summary><strong> > DAG details (click to expand)</strong></summary>
 
 ### Execution Interval
 
 Daily. More information about run time [here]({chart_url}{dag_id}).
 
 ### Outputs
+This pipeline produces the following output tables in DW layer, created in `quintoandar` schema:
 
-Currently, there are the following output tables in our DW layer:
-
-- `fact_owner_conversion_metrics`
-- `fact_tenant_conversion_metrics`
+- `quintoandar.fact_owner_conversion_metrics`
+- `quintoandar.fact_tenant_conversion_metrics`
 
 ### Responsible Data Team
 
 For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+</details>
--- bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/dw_fact_conversion_metrics.py
+++ bietlejuice/jobs/composer/dags/dw_fact_conversion_metrics/dw_fact_conversion_metrics.py
@@ -1,50 +1,47 @@
 from datetime import datetime
-import pendulum
+from pendulum import timezone
 import os
 
 from airflow.models import DAG, Variable
+from airflow.utils.helpers import chain
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
-    QuintoAndarDatabricksSubmitRunOperator,
 )
 
-from bietlejuice.jobs.composer.dags.dw_fact_conversion_metrics import (
-    SOURCE,
-    DW_SCHEMA,
-    QUERIES_DW_FACT_CONVERSION_METRICS_PATH,
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
+from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
+from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
 )
 
-from bietlejuice.jobs.composer.base.airflow import BaseDAG, BaseSubDAG
-from bietlejuice.jobs.composer.base.pipeline import LayerEnum
-from bietlejuice.jobs.composer.services import FileService
 
-LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
-MAIN_START_DATE = datetime(2020, 2, 20, 0, 0, 0, tzinfo=LOCAL_TZ)
-MAIN_SCHEDULE_INTERVAL = "0 5 * * *"
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+DW_SCHEMA = "quintoandar"
+CONTEXT = "fact_conversion_metrics"
+DAG_NAME = f"dw_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
 
-DAG_ID = f"bietlejuice.{SOURCE}"
 ENV = os.environ.get("ENVIRONMENT")
-SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
-DW_BUCKET = Variable.get("dw_bucket")
+MAIN_START_DATE = datetime(2019, 5, 31, tzinfo=timezone("America/Sao_Paulo"))
 
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/"
-BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
-
-LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
-    Variable.get("databricks_s3_bucket"), SOURCE
+config_service = ConfigurationService(DAG_NAME)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+dw_bucket = config_service.get_config("dw_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
 )
+spectrum_iam_role = config_service.get_config("spectrum_iam_role")
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
 
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_conversion_metrics_cluster", deserialize_json=True
+    "databricks_default_cluster", deserialize_json=True
 )
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
-
-LIBRARIES_DESCRIPTION = Variable.get(
-    "bietlejuice_default_libraries", deserialize_json=True
-)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = spark_jobs_logs_path
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -54,112 +51,40 @@ dag = DAG(
         "depends_on_past": False,
     },
     start_date=MAIN_START_DATE,
-    schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    doc_md=BaseDAG.get_dag_doc(SOURCE).format(chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID),
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(chart_url=doc_md_chart_url, dag_id=DAG_ID),
 )
-
-
-def dw_tasks(sub_dag_name, table_name, slugged_table_name):
-
-    sub_dag = BaseSubDAG(
-        sub_dag_name=sub_dag_name,
-        dag_name=DAG_ID,
-        schedule_interval=MAIN_SCHEDULE_INTERVAL,
-        start_date=MAIN_START_DATE,
-    )._build_local_dag()
-
-    create_table_in_dw_staging = QuintoAndarDatabricksSubmitRunOperator(
-        dag=sub_dag,
-        task_id=f"load-{slugged_table_name}-into-dw-{DW_SCHEMA}-staging",
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_table_in_dw_staging.py",
-                "parameters": [DW_BUCKET, table_name, ENV],
-            }
-        },
-    )
-
-    create_table_in_dw = QuintoAndarDatabricksSubmitRunOperator(
-        dag=sub_dag,
-        task_id=f"load-{slugged_table_name}-into-dw-{DW_SCHEMA}",
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "create_table_in_dw.py",
-                "parameters": [DW_BUCKET, table_name, ENV],
-            }
-        },
-    )
-
-    load_dw_table_into_redshift = QuintoAndarDatabricksSubmitRunOperator(
-        dag=sub_dag,
-        task_id=f"load-{slugged_table_name}-into-redshift",
-        json={
-            "spark_python_task": {
-                "python_file": SPARK_JOBS_PATH + "load_dw_table_into_redshift.py",
-                "parameters": [DW_BUCKET, table_name, ENV, SPECTRUM_IAM_ROLE],
-            }
-        },
-    )
-
-    sync_metastore_table_task = QuintoAndarDatabricksSubmitRunOperator(
-        task_id="sync-hive-metastore-table",
-        dag=sub_dag,
-        json={
-            "spark_python_task": {
-                "python_file": BASE_SPARK_JOBS_PATH + "sync_metastore_tables.py",
-                "parameters": [
-                    DW_BUCKET,
-                    LayerEnum.DW.value,
-                    DW_SCHEMA,
-                    "--table-name",
-                    table_name,
-                ],
-            }
-        },
-    )
-
-    create_table_in_dw_staging >> create_table_in_dw >> load_dw_table_into_redshift
-    create_table_in_dw >> sync_metastore_table_task
-
-    return sub_dag
-
-
-def build_subdags(stage):
-    # create subdag for each table
-    file_list = FileService.list_files(
-        f"{QUERIES_DW_FACT_CONVERSION_METRICS_PATH}/{stage}"
-    )
-    subdags = {}
-
-    for file_name in file_list:
-        file_name = FileService.remove_file_extension(file_name)
-        slugged_table_name = file_name.replace("_", "-")
-        table_sub_dag = BaseSubDAG.get_sub_dag_operator(
-            dag=dag,
-            sub_dag_name=f"load-{slugged_table_name}-to-{stage}",
-            sub_dag_func=eval(f"{stage}_tasks"),
-            table_name=file_name,
-            slugged_table_name=slugged_table_name,
-        )
-        subdags[file_name] = table_sub_dag
-
-    # subdags is a dict where the keys are table or file names
-    # and the values are corresponding subdag objects
-    return subdags
-
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag,
     task_id="create-cluster",
     cluster_configuration=CLUSTER_DESCRIPTION,
-    libraries=LIBRARIES_DESCRIPTION,
 )
-
-
-dw_sub_dags = build_subdags("dw")
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-create_cluster_task >> list(dw_sub_dags.values()) >> terminate_cluster_task
+task_group = DWTaskGroup(
+    dag=dag,
+    env=ENV,
+    dw_bucket=dw_bucket,
+    dw_schema=DW_SCHEMA,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+)
+
+dw_staging_task_group = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW_STAGING,
+)
+
+dw_task_group = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW,
+    spectrum_iam_role=spectrum_iam_role,
+)
+
+chain(create_cluster_task, DWTaskGroup.all_first_tasks(dw_staging_task_group))
+
+TaskFlowHelper.chain_task_groups_via_common_table(dw_staging_task_group, dw_task_group)
+
+chain(DWTaskGroup.all_last_tasks(dw_task_group), terminate_cluster_task)
