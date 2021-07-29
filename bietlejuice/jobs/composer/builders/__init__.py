906e8f37730a6ef6478ae29172031dcc1f107c5b
Felipe Miquelim <felipe.miquelim@quintoandar.com.br>
2021-07-29 09:01:03 -0300

    Adjusting imports (#5403)

--- bietlejuice/jobs/composer/builders/__init__.py
+++ bietlejuice/jobs/composer/builders/__init__.py
@@ -1,4 +1,1 @@
-from bietlejuice.jobs.composer.builders.google_ads_raw_builder import (
-    GoogleAdsRawBuilder,
-)
 from bietlejuice.jobs.composer.builders.cypress_raw_builder import CypressRawBuilder
