494484786b0cc4a7cdb2c74b8893275d2e9bab3b
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-07-30 08:58:36 -0300

    deduplicate reservation kill queue (#5416)

--- bietlejuice/db/datalake/queries/kill_queue/reservation_clean.sql
+++ bietlejuice/db/datalake/queries/kill_queue/reservation_clean.sql
@@ -1,17 +1,39 @@
+WITH reservation AS (
+  SELECT
+    cast(r.id as bigint) as id,
+    r.ts_created as created_at,
+    r.ts_updated as updated_at,
+    cast(r.version as integer) as version,
+    cast(r.attempt as integer) as attempt,
+    id_rent_flow as rent_flow_id,
+    r.status,
+    id_tenant as tenant_id,
+    r.value,
+    id_main as house_id,
+    r.mundipagg_token,
+    cast(r.is_ongoing as integer) as is_ongoing,
+    r.cancellation_reason,
+    r.installments,
+    ROW_NUMBER() OVER(PARTITION BY r.id ORDER BY h.ts_updated DESC) AS row_n
+  FROM datalake_kill_queue_clean_prod.reservation r
+  left join datalake_kill_queue_clean_prod.house h on r.id_house = h.id
+)
 SELECT
-  cast(r.id as bigint) as id,
-  r.ts_created as created_at,
-  r.ts_updated as updated_at,
-  cast(r.version as integer) as version,
-  cast(r.attempt as integer) as attempt,
-  id_rent_flow as rent_flow_id,
-  r.status,
-  id_tenant as tenant_id,
-  r.value,
-  id_main as house_id,
-  r.mundipagg_token,
-  cast(r.is_ongoing as integer) as is_ongoing,
-  r.cancellation_reason,
-  r.installments
-FROM datalake_kill_queue_clean_prod.reservation r
-left join datalake_kill_queue_clean_prod.house h on r.id_house = h.id;
+  id,
+  created_at,
+  updated_at,
+  version,
+  attempt,
+  rent_flow_id,
+  status,
+  tenant_id,
+  value,
+  house_id,
+  mundipagg_token,
+  is_ongoing,
+  cancellation_reason,
+  installments
+FROM
+  reservation
+WHERE
+  row_n = 1
--- bietlejuice/jobs/composer/db/datalake/queries/enrich_kill_queue/enrich/reservation.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_kill_queue/enrich/reservation.sql
@@ -1,18 +1,40 @@
-SELECT
-  cast(r.id as bigint) as id_reservation,
+WITH reservation AS (
+  SELECT
+    cast(r.id as bigint) as id_reservation,
+    id_rent_flow,
+    r.id_tenant,
+    h.id_main as id_house,
+    cast(r.version as integer) as version,
+    cast(r.attempt as integer) as attempt,
+    r.status,
+    r.value,
+    r.mundipagg_token,
+    r.cancellation_reason,
+    r.installments,
+    cast(cast(r.is_ongoing as integer) as boolean) as is_ongoing,
+    r.ts_created,
+    r.ts_updated,
+    ROW_NUMBER() OVER(PARTITION BY r.id ORDER BY h.ts_updated DESC) AS row_n
+  FROM datalake_kill_queue_clean.reservation r
+  LEFT JOIN datalake_kill_queue_clean.house h
+      ON r.id_house = h.id
+)
+SELECT 
+  id_reservation,
   id_rent_flow,
-  r.id_tenant,
-  h.id_main as id_house,
-  cast(r.version as integer) as version,
-  cast(r.attempt as integer) as attempt,
-  r.status,
-  r.value,
-  r.mundipagg_token,
-  r.cancellation_reason,
-  r.installments,
-  cast(cast(r.is_ongoing as integer) as boolean) as is_ongoing,
-  r.ts_created,
-  r.ts_updated
-FROM datalake_kill_queue_clean.reservation r
-LEFT JOIN datalake_kill_queue_clean.house h
-    ON r.id_house = h.id
+  id_tenant,
+  id_house,
+  version,
+  attempt,
+  status,
+  value,
+  mundipagg_token,
+  cancellation_reason,
+  installments,
+  is_ongoing,
+  ts_created,
+  ts_updated
+FROM
+  reservation
+WHERE
+  row_n = 1
