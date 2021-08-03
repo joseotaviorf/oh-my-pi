0c905efb9b7c9e0006dbcd1fd98ed6619fa375b3
Rafael Augusto Monteiro <rafaelmonteiro95@gmail.com>
2021-08-03 14:11:29 -0300

    [DG-266] Create script to upload Atlas metadata files to s3(#5406)
    * [DG-266] Create update metatada files script and add drone steps
    
    * [DG-266] Add log message
    
    * [DG-266] Update example in docstring
    
    * [DG-266] Rename function in upload_metadata
    
    * [DG-266] Rename drone step, support yaml extension, use recursive glob

---
+++ scripts/upload_atlas_metadata_files_to_s3.py
@@ -0,0 +1,79 @@
+import argparse
+import glob
+
+from yaml import safe_load
+import boto3
+
+
+def get_first_key(input_dict: dict) -> str:
+    """
+    returns the first key of a dict
+    it is expected that the key is a string
+    """
+    return next(iter(input_dict))
+
+
+def get_remote_path_from_yml_file(yml_path: str) -> str:
+    """
+    given a path to a yml file containing Atlas metadata info,
+    reads the file and generates a path where it should be stored in S3
+
+    Args:
+        yml_path: path where the metadata file is stored locally
+        e.g:
+            /../bi-etl-ejuice/jobs/composer/db/datalake/metadata/monopoly/clean/person_sale.yml
+            /../bi-etl-ejuice/jobs/composer/db/datalake/metadata/metabase/raw/metabase_table.yml
+
+    Returns:
+        a string representing the path where this file should be stored in S3
+        e.g:
+            lineage/atlas/datalake_monopoly_clean/person_sale.yml
+            tags/atlas/datalake_metabase_raw/metabase_table.yml
+    """
+    with open(yml_path, "r") as fp:
+        data = safe_load(fp)
+        db_name = data["database_name"]
+        table_name = data["table_name"]
+        first_column_key = get_first_key(data["columns"])
+        file_type = get_first_key(data["columns"][first_column_key])
+        if file_type not in ["lineage", "tags"]:
+            raise ValueError(f"File {yml_path} has invalid type: {file_type}")
+
+    return f"{file_type}/atlas/{db_name}/{table_name}.yml"
+
+
+def main():
+    """
+    This script reads Atlas metadata files and uploads them to the data-documentation S3 bucket.
+    It is expected that each file is stored in the following format:
+
+    bietlejuice/jobs/composer/db/datalake/metadata/{source}/{layer}/{table-name}.yml
+
+    And each file should have lineage OR tags definitions for the table columns
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest="metadata_s3_bucket")
+    args = parser.parse_args()
+
+    metadata_s3_bucket = args.metadata_s3_bucket
+    s3 = boto3.client("s3")
+
+    for extension in ("*.yml", "*.yaml"):
+        for file_path in glob.iglob(
+            f"../bietlejuice/jobs/composer/db/datalake/metadata/**/{extension}",
+            recursive=True,
+        ):
+            remote_path = get_remote_path_from_yml_file(file_path)
+            s3.upload_file(
+                file_path,
+                metadata_s3_bucket,
+                remote_path,
+                ExtraArgs={"ACL": "bucket-owner-full-control"},
+            )
+            print(
+                f"local_path={file_path}, remote_path={remote_path}, msg=Metadata file saved to S3"
+            )
+
+
+if __name__ == "__main__":
+    main()
