984cf2c67b90efe5a6076b742b68d7dbecae2066
Eduardo Trevisani <etgamba@gmail.com>
2021-08-03 10:25:02 -0300

    Removed cookiecutter DAG template (#5429)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- databricks_dag_template/{{ cookiecutter.dag_slug }}/spark_jobs/job_template.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from quintoandar_logger import QuintoAndarLogger
-
-logging.getLogger("py4j").setLevel(logging.ERROR)
-logger = QuintoAndarLogger("JOB_NAME")
-
-if __name__ == "__main__":
-    logger.info("m=__main__, msg=File created by cookiecutter. Please implement me!")
