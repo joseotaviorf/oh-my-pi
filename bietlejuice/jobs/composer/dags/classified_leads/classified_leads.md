d2d05b38899cc40ebb96e710d578699847f3e0b9
Ana Alves <ana.sevlaraujo@gmail.com>
2021-08-04 16:58:47 -0300

    [DTM-894] taskgroup classified leads (#5399)
    * replaced subdags
    
    * added tag details to doc.md

--- bietlejuice/jobs/composer/dags/classified_leads/classified_leads.md
+++ bietlejuice/jobs/composer/dags/classified_leads/classified_leads.md
@@ -3,6 +3,9 @@
 
 ClassifiedsResponder is a Database we use to store our contract history we have when dealing with Classifieds Leads (A lead which comes from one of our Classifieds like Criteo). This DAG ingests this database into our raw flow (until clean).
 
+<details>
+    <summary><strong> > DAG details (click to expand)</strong></summary>
+
 ### Execution Interval
 
 Daily. More information about run time [here]({chart_url}{dag_id}).
@@ -19,3 +22,4 @@ Currently, there are the following output tables for both our raw and clean laye
 ### Responsible Data Team
 
 For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+</details>
--- bietlejuice/jobs/composer/dags/classified_leads/classified_leads.py
+++ bietlejuice/jobs/composer/dags/classified_leads/classified_leads.py
@@ -1,43 +1,49 @@
 from datetime import datetime
-
 import pendulum
 import os
+
 from airflow.models import DAG, Variable
-import airflow.utils.helpers as airflow_helpers
+from airflow.utils.helpers import chain, cross_downstream
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
-    QuintoAndarDatabricksSubmitRunOperator,
 )
 from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.dags.base.datalake_sub_dag import DatalakeSubDAG
-from bietlejuice.jobs.composer.services import FileService
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
-
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
 
 SOURCE = "classified_leads"
-
-# airflow vars
+DAG_ID = f"bietlejuice.{SOURCE}"
 ENV = os.environ.get("ENVIRONMENT")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
-DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
 
-# spark and databricks vars
-SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/"
-LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{SOURCE}"
+config_service = ConfigurationService(SOURCE)
+
+DATALAKE_BUCKET = config_service.get_config("datalake_bucket")
+ATHENA_QUERY_RESULT_BUCKET = config_service.get_config("athena_query_results_bucket")
+DATABRICKS_BIETLEJUICE_REPO_PATH = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+BASE_SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
+DOC_MD_CHART_URL = config_service.get_config("doc_md_chart_url")
+SPARK_JOBS_LOGS_PATH = config_service.get_config("spark_jobs_logs_path")
+
+LOAD_CLASSIFIED_LEADS_INTO_DATALAKE_RAW_FILE_PATH = (
+    f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/{SOURCE}/load_classified_leads_into_datalake.py"
+)
+
 CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
+    "destination"
+] = f"{SPARK_JOBS_LOGS_PATH}{DAG_ID}"
+
 LIBRARIES_DESCRIPTION = Variable.get(
     "bietlejuice_default_libraries", deserialize_json=True
 )
 
-# DAG vars
-DAG_ID = f"bietlejuice.{SOURCE}"
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
 MAIN_START_DATE = datetime(2019, 5, 31, 0, 0, 0, tzinfo=LOCAL_TZ)
 MAIN_SCHEDULE_INTERVAL = "0 6 * * *"
@@ -51,7 +57,9 @@ dag = DAG(
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    doc_md=BaseDAG.get_dag_doc(SOURCE).format(chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID),
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(
+        chart_url=DOC_MD_CHART_URL, dag_id=DAG_ID
+    ),
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
@@ -61,63 +69,36 @@ create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     libraries=LIBRARIES_DESCRIPTION,
 )
 
-classified_leads_to_datalake_raw_task = QuintoAndarDatabricksSubmitRunOperator(
-    task_id="classified_leads-to-datalake-raw",
-    dag=dag,
-    json={
-        "spark_python_task": {
-            "python_file": f"{SPARK_JOB_PATH}/{SOURCE}/load_classified_leads_into_datalake.py",
-            "parameters": [ENV, DATALAKE_BUCKET],
-        }
-    },
-)
-
-sync_metastore_tables_task = QuintoAndarDatabricksSubmitRunOperator(
-    task_id="sync-hive-metastore-raw-tables",
-    dag=dag,
-    json={
-        "spark_python_task": {
-            "python_file": SPARK_JOBS_PATH + "/sync_metastore_tables.py",
-            "parameters": [
-                DATALAKE_BUCKET,
-                LayerEnum.RAW.value,
-                SOURCE,
-                "--all-tables",
-            ],
-        }
-    },
-)
-
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-clean_sub_dag = DatalakeSubDAG(
-    dag_id=DAG_ID,
-    start_date=MAIN_START_DATE,
+task_group = DatalakeTaskGroup(
+    dag=dag,
     env=ENV,
     datalake_bucket=DATALAKE_BUCKET,
-    database_base_name=SOURCE,
     relative_query_path=SOURCE,
-    spark_job_paths=SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_BUCKET,
+)
+
+raw_task_group = task_group.build_raw_task_group_for_all_tables(
+    source=SOURCE,
+    target_database_base_name=SOURCE,
+    extraction_spark_job_file=LOAD_CLASSIFIED_LEADS_INTO_DATALAKE_RAW_FILE_PATH,
+)
+
+clean_task_groups = task_group.build_task_group_from_sql_files(
     layer=LayerEnum.CLEAN,
+    source_database_base_name=SOURCE,
+    target_database_base_name=SOURCE,
 )
 
-file_list = FileService.list_sql_files_without_extension_from_layer(
-    SOURCE, LayerEnum.CLEAN.value
+chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_group))
+
+cross_downstream(
+    DatalakeTaskGroup.last_tasks(raw_task_group),
+    DatalakeTaskGroup.all_first_tasks(clean_task_groups),
 )
 
-clean_sub_dags = clean_sub_dag.build_subdags_from_sql_files(dag, file_list)
-
-airflow_helpers.chain(
-    create_cluster_task,
-    classified_leads_to_datalake_raw_task,
-    list(clean_sub_dags.values()),
-    terminate_cluster_task,
-)
-airflow_helpers.chain(
-    classified_leads_to_datalake_raw_task,
-    sync_metastore_tables_task,
-    terminate_cluster_task,
-)
+terminate_cluster_task.set_upstream(DatalakeTaskGroup.all_last_tasks(clean_task_groups))
