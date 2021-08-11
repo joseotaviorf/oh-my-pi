d19d83472c55ebd9764021707cb62a96f0b6512d
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-08-11 14:05:20 -0300

    removed columns that doesnt exist anymore (#5494)

--- bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_portal/clean/real_estate_agency_acquisition.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_portal/clean/real_estate_agency_acquisition.sql
@@ -1,8 +1,6 @@
 SELECT
     id,
     email,
-    total_venda AS total_sales_range,
-    total_aluguel AS total_rent_range,
     CAST(criado_em AS TIMESTAMP) AS ts_created
 FROM
     datalake_casa_mineira_portal_raw.captacao_imobiliaria
