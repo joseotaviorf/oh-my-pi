6395ae00ac6691a8ec2a7d9ec11401094ff78b01
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-08-16 16:45:39 -0300

    rollback call_flex_reservations (#5533)

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_bigfone_twilio/enrich/call_flex_reservations.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_bigfone_twilio/enrich/call_flex_reservations.sql
@@ -3,6 +3,16 @@ WITH reservations_events AS (
         GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.call_sid') AS id_call,
         GET_JSON_OBJECT(metadata,'$.event_data.TaskSid') AS id_task,
         GET_JSON_OBJECT(metadata,'$.event_data.ReservationSid') AS id_reservation,
+        COALESCE(
+            GET_JSON_OBJECT(metadata,'$.event_data.WorkerSid'),
+            GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.worker_sid')
+        ) AS id_agent,
+        CASE
+            WHEN event = 'reservation.created' THEN GET_JSON_OBJECT(metadata,'$.event_data.TaskQueueSid')
+        END AS id_queue,
+        CASE
+            WHEN event = 'reservation.created' THEN GET_JSON_OBJECT(metadata,'$.event_data.TaskQueueName')
+        END AS queue_name,
         event,
         GET_JSON_OBJECT(metadata,'$.created_at') as ts_event,
         GET_JSON_OBJECT(metadata, '$.event_data.Timestamp') AS ts_event_unix,
@@ -22,38 +32,16 @@ WITH reservations_events AS (
         AND year = {year}
         AND month = {month}
         AND day = {day}
-    GROUP BY 1,2,3,4,5,6,7,8,9
-),
-reservations_attributes AS (
-    SELECT DISTINCT
-        GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.call_sid') AS id_call,
-        GET_JSON_OBJECT(metadata,'$.event_data.TaskSid') AS id_task,
-        GET_JSON_OBJECT(metadata,'$.event_data.ReservationSid') AS id_reservation,
-        COALESCE(
-            GET_JSON_OBJECT(metadata,'$.event_data.WorkerSid'),
-            GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.worker_sid')
-        ) AS id_agent,
-        GET_JSON_OBJECT(metadata,'$.event_data.TaskQueueSid') AS id_queue,
-        GET_JSON_OBJECT(metadata,'$.event_data.TaskQueueName') AS queue_name
-    FROM
-        datalake_bigfone_events.events
-    WHERE
-        provider = 'twilio'
-        AND (
-            GET_JSON_OBJECT(metadata,'$.event_data.WorkflowName') = 'Assign to Anyone'
-            OR GET_JSON_OBJECT(metadata,'$.event_data.WorkflowName') = 'Assign to Flex'
-        )
-        AND event IN ('reservation.accepted', 'reservation.rejected', 'reservation.timeout')
-        AND GET_JSON_OBJECT(metadata,'$.event_data.ReservationSid') IS NOT NULL
-        AND year = {year}
-        AND month = {month}
-        AND day = {day}
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12
 ),
 tasks_events AS (
   SELECT
         GET_JSON_OBJECT(metadata,'$.event_data.TaskAttributes.call_sid') AS id_call,
         GET_JSON_OBJECT(metadata,'$.event_data.TaskSid') AS id_task,
         NULL AS id_reservation,
+        NULL AS id_agent,
+        NULL AS id_queue,
+        NULL AS queue_name,
         event,
         GET_JSON_OBJECT(metadata,'$.created_at') as ts_event,
         GET_JSON_OBJECT(metadata, '$.event_data.Timestamp') AS ts_event_unix,
@@ -72,13 +60,16 @@ tasks_events AS (
       AND year = {year}
       AND month = {month}
       AND day = {day}
-  GROUP BY 1,2,3,4,5,6,7,8,9
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12
 ),
 call_flex_reservations AS (
     SELECT
         re1.id_call,
         re1.id_task,
         re1.id_reservation,
+        re1.id_agent,
+        re2.id_queue,
+        re2.queue_name,
         COUNT(
             CASE
                 WHEN re1.event = 'reservation.accepted' THEN re1.id_reservation
@@ -96,12 +87,17 @@ call_flex_reservations AS (
         ) > 0 AS is_rejected,
         MIN(re1.ts_event_unix) AS ts_created_unix,
         MAX(re1.ts_event_unix) AS ts_ended_unix,
-        MAX(year) AS year,
-        MAX(month) AS month,
-        MAX(day) AS day
+        re1.year,
+        re1.month,
+        re1.day
     FROM
         reservations_events AS re1
-    GROUP BY 1,2,3
+    LEFT JOIN
+        reservations_events AS re2
+            ON re1.id_task = re2.id_task
+            AND re1.id_reservation = re2.id_reservation
+            AND re2.queue_name IS NOT NULL
+    GROUP BY 1,2,3,4,5,6,12,13,14
 ),
 full_events AS (
     SELECT 
@@ -145,9 +141,9 @@ SELECT
     cr.id_call,
     cr.id_task,
     cr.id_reservation,
-    reservations_attributes.id_agent,
-    reservations_attributes.id_queue,
-    reservations_attributes.queue_name,
+    cr.id_agent,
+    cr.id_queue,
+    cr.queue_name,
     is_answered,
     is_timeout,
     is_rejected,
@@ -159,17 +155,13 @@ SELECT
     CAST(FROM_UNIXTIME(ts_ended_unix, 'yyyy-MM-dd HH:mm:ss') AS TIMESTAMP) AS ts_ended,
     FROM_UTC_TIMESTAMP(CAST(FROM_UNIXTIME(ts_ended_unix, 'yyyy-MM-dd HH:mm:ss') AS TIMESTAMP), 'Brazil/East') AS ts_ended_local,
     ts_created_unix,
-    cr.year,
-    cr.month,
-    cr.day
+    year,
+    month,
+    day
 FROM
     call_flex_reservations AS cr
 LEFT JOIN
     answered_time_calculations AS atc
         ON cr.id_task = atc.id_task
         AND cr.id_reservation = atc.id_reservation
-LEFT JOIN
-    reservations_attributes
-        ON cr.id_task = reservations_attributes.id_task
-        AND cr.id_reservation = reservations_attributes.id_reservation
 GROUP BY 1,2,3,4,5,6,7,8,9,13,14,15,16,17,18,19,20
