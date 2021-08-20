7f3432c8e9c1c5c727cb539336040976c520e828
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-08-20 11:07:55 -0300

    [DFR-1015] Casa mineira crm lineage (#5573)
    * add data lineage for cm crm
    
    * big sonar dont cry
    
    * fix misspelling
    
    * added phone_for_sale, fixed evaluation_sale_price reference and query
    
    * fixing mapping sale_brok
    
    * fixing misspelling and missing columns

--- bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_crm/clean/user_house_evaluation.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_crm/clean/user_house_evaluation.sql
@@ -3,7 +3,7 @@ SELECT
     imovel_id AS id_house,
     avaliado_por AS evaluated_by,
     criado_por AS created_by,
-    CAST(preco_avaliacao AS FLOAT) AS evalutation_sale_price,
+    CAST(preco_avaliacao AS FLOAT) AS evaluation_sale_price,
     CAST(criado_em AS TIMESTAMP) AS ts_created
 FROM
     datalake_casa_mineira_crm_raw.usuario_avaliacao_imovel
