cdae13d3054768933bd3664090227be825c86266
Eduardo Trevisani <etgamba@gmail.com>
2021-08-20 15:50:18 -0300

    [AEMKT-270][GCH] Moved Braze events variables into config files (#5526)
    * Moved Braze events variables into config files
    
    * Lowercasing variables in DAG file
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/braze_events/braze_events.py
+++ bietlejuice/jobs/composer/dags/braze_events/braze_events.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-
-import pendulum
+from pendulum import timezone
 import os
 
 from airflow.models import DAG, Variable
@@ -14,36 +13,42 @@ from bietlejuice.jobs.composer.base.airflow import BaseDAG
 from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
 from bietlejuice.jobs.composer.base.pipeline import LayerEnum
 from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
 
 ENV = os.environ.get("ENVIRONMENT")
 
-CONTEXT = "braze_events"
+SOURCE = "braze_events"
+CONTEXT = SOURCE
 DAG_ID = f"bietlejuice.{CONTEXT}"
-LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
-MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+MAIN_START_DATE = datetime(2020, 11, 1, tzinfo=timezone("America/Sao_Paulo"))
 MAIN_SCHEDULE_INTERVAL = "30 0 * * *"
 
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-BRAZE_BUCKET = Variable.get("braze_bucket")
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+config_service = ConfigurationService(SOURCE)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 
-LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{CONTEXT}"
-BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
-RAW_SPARK_JOB_PATH = (
-    f"{S3_PREFIX}/spark_jobs/{CONTEXT}/load_braze_events_into_datalake.py"
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
 )
-
-CLUSTER_DESCRIPTION = Variable.get(
-    f"databricks_bietlejuice_{CONTEXT}_cluster", deserialize_json=True
+raw_spark_job_file = (
+    f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/load_braze_events_raw.py"
 )
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 
-# Names of app groups must match the directories created in Braze's S3 bucket
-APP_GROUPS = ["owners", "tenants"]
+clean_partition_cols = config_service.get_config("clean_partition_cols")
+braze_events_bucket = config_service.get_config("braze_events_bucket")
+app_groups_list = config_service.get_config("app_groups_list")
 
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
+    "destination"
+] = f"{spark_jobs_logs_path}{DAG_ID}"
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -55,7 +60,7 @@ dag = DAG(
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(CONTEXT).format(
-        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
 
@@ -70,21 +75,21 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
+    datalake_bucket=datalake_bucket,
     relative_query_path=CONTEXT,
-    spark_jobs_path=BASE_SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    spark_jobs_path=base_spark_jobs_path,
+    athena_query_result_location=athena_query_results_bucket,
 )
 
 raw_task_groups = {}
-for app_group in APP_GROUPS:
+for app_group in app_groups_list:
     table_name = f"events_{app_group}"
     raw_task_group = task_group.build_raw_task_group_for_single_table(
         source=CONTEXT,
         table_name=table_name,
         target_database_base_name=CONTEXT,
-        extraction_spark_job_file=RAW_SPARK_JOB_PATH,
-        raw_spark_job_extra_args=[CONTEXT, BRAZE_BUCKET, app_group, "{{ ds }}"],
+        extraction_spark_job_file=raw_spark_job_file,
+        raw_spark_job_extra_args=[CONTEXT, braze_events_bucket, app_group, "{{ ds }}"],
     )
     raw_task_groups[table_name] = raw_task_group
 
@@ -93,7 +98,7 @@ clean_task_groups = task_group.build_task_group_from_sql_files(
     source_database_base_name=CONTEXT,
     target_database_base_name=CONTEXT,
     is_incremental=True,
-    partitions=["event_type", "year", "month", "day"],
+    partitions=clean_partition_cols,
 )
 
 chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(raw_task_groups))
