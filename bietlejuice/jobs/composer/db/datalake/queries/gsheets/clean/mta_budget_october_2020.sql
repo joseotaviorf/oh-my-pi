7dc6285b0de6a98780ef9092d612009098fafe96
Gustavo Moura <54562374+gustsm@users.noreply.github.com>
2021-09-02 11:59:11 -0300

    Adding full taxonomy to mta sheet import (#5815)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/mta_budget_october_2020.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/mta_budget_october_2020.sql
@@ -1,10 +1,12 @@
 SELECT
-    budget,
+    CAST(budget AS DOUBLE) AS budget,
     city_group,
+    mkt_origin,
+    mkt_channel,
     mkt_medium,
     mkt_source,
-    is_blocked_budget,
-    week_start,
-    month
+    CAST(is_blocked_budget AS INTEGER) AS is_blocked_budget,
+    CAST(week_start AS DATE) AS week_start,
+    CAST(month AS DATE) AS month
 FROM
     datalake_gsheets_raw.mta_budget
