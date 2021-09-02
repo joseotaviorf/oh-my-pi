e2a1a1ceaf31e48dc4c661de2cd47ac14ab1b24a
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-09-02 17:48:40 -0300

    [DFR] Updating tasks_action with analyst_started information from CRM (#5818)
    * Updating tasks_action with analyst_started information
    
    * indentation

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_crm/enrich/incremental/tasks_actions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_crm/enrich/incremental/tasks_actions.sql
@@ -1,7 +1,9 @@
 WITH exploded_actions AS (
   SELECT
     GET_JSON_OBJECT(REPLACE(id, '$', ''),"$.oid") AS id,
-    EXPLODE(FROM_JSON(REPLACE(actions,'$',''), 
+    EXPLODE(
+        FROM_JSON(
+            REPLACE(actions,'$',''), 
             'ARRAY<
                 STRUCT<
                     _id: STRUCT<oid: STRING>,
@@ -25,21 +27,53 @@ WITH exploded_actions AS (
     AND year = {year}
     AND month = {month}
     AND day = {day}
+),
+exploded_analyst_started AS (
+    SELECT
+        GET_JSON_OBJECT(REPLACE(id, '$', ''),"$.oid") AS id, 
+        EXPLODE(
+            FROM_JSON(
+                REPLACE(analyst_started, '$', ''),
+                'ARRAY<
+                        STRUCT<
+                            _id: STRUCT<oid: STRING>,
+                            date: STRUCT<date: TIMESTAMP>,
+                            userId: INTEGER,
+                            userName: STRING
+                        >
+                    >'
+        )) AS analyst_started,
+        year,
+        month,
+        day
+    FROM
+        datalake_crm_clean.tasks
+    WHERE
+        year = {year}
+        AND month = {month}
+        AND day = {day}
 )
 SELECT
-    action._id.oid AS id,
-    id AS id_task,
-    action.userId AS id_user_action,
-    REPLACE(action.userName,'"') AS action_user_name,
-    REPLACE(action.type,'"') AS action_type,
-    GET_JSON_OBJECT(action.metadata, '$.key') AS metadata_key,
-    GET_JSON_OBJECT(action.metadata, '$.oldValue') AS metadata_old_value,
-    action.notificationSource AS notification_source,
-    action.date.date AS ts_action,
-    year,
-    month,
-    day
+    ea.action._id.oid AS id,
+    ea.id AS id_task,
+    es.analyst_started._id.oid AS id_analyst_started,
+    ea.action.userId AS id_user_action,
+    es.analyst_started.userId AS id_user_analyst_started,
+    REPLACE(ea.action.userName,'"') AS action_user_name,
+    es.analyst_started.userName AS analyst_started_user_name,
+    REPLACE(ea.action.type,'"') AS action_type,
+    GET_JSON_OBJECT(ea.action.metadata, '$.key') AS metadata_key,
+    GET_JSON_OBJECT(ea.action.metadata, '$.oldValue') AS metadata_old_value,
+    ea.action.notificationSource AS notification_source,
+    ea.action.date.date AS ts_action,
+    es.analyst_started.date.date AS ts_analyst_started,
+    ea.year,
+    ea.month,
+    ea.day
 FROM
-    exploded_actions
+    exploded_actions AS ea
+LEFT JOIN
+    exploded_analyst_started AS es
+        ON ea.id = es.id
 WHERE
-    DATE(action.date.date) = DATE('{year}-{month}-{day}')
+    DATE(ea.action.date.date) = DATE('{year}-{month}-{day}')
