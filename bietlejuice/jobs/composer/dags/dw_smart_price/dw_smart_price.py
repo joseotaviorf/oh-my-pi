b6d86e28eb64b916ef3012c52e1bbeece4823023
Marcelo Alves Baratela <56609583+marcelo-b@users.noreply.github.com>
2021-09-06 13:58:56 -0300

    [DEI-678] Changing dw_smart_price cluster (#5840)
    * Changing cluster to databricks_bietlejuice_dw_smart_price_cluster
    
    * minor adjustment
    
    * lint

--- bietlejuice/jobs/composer/dags/dw_smart_price/dw_smart_price.py
+++ bietlejuice/jobs/composer/dags/dw_smart_price/dw_smart_price.py
@@ -34,7 +34,9 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_bietlejuice_dw_smart_price_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 
