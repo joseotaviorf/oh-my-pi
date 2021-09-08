f06910fbc24f895d4c893f352d5a4825676ae1f7
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2021-09-08 13:33:14 -0300

    Escape backslash on regex (#5887)

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_lead_tracking/enrich/lead_first_event_tracking.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_lead_tracking/enrich/lead_first_event_tracking.sql
@@ -31,15 +31,15 @@ t_rn AS (
 )
 SELECT
 	id_from_lead AS id_lead,
-	REGEXP_REPLACE(utm_campaign,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_campaign,
-	REGEXP_REPLACE(utm_medium,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_medium,
-	REGEXP_REPLACE(utm_source,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_source,
-	REGEXP_REPLACE(utm_content,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_content,
-	REGEXP_REPLACE(utm_term,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_term,
-	REGEXP_REPLACE(platform,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_platform,
-	REGEXP_REPLACE(referring_domain,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_referring_domain,
-	REGEXP_REPLACE(region,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_region,
-	REGEXP_REPLACE(city,'[^\x00-\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_city
+	REGEXP_REPLACE(utm_campaign,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_campaign,
+	REGEXP_REPLACE(utm_medium,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_medium,
+	REGEXP_REPLACE(utm_source,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_source,
+	REGEXP_REPLACE(utm_content,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_content,
+	REGEXP_REPLACE(utm_term,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_term,
+	REGEXP_REPLACE(platform,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_platform,
+	REGEXP_REPLACE(referring_domain,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_referring_domain,
+	REGEXP_REPLACE(region,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_region,
+	REGEXP_REPLACE(city,'[^\\x00-\\x7F^ÁáÂâÃãÉéÊêÍíÓóÔôÕõÚúÇçÀà]+', '') AS tracking_city
 FROM t_rn
 WHERE id_from_lead IS NOT NULL
       AND lead_rn = 1
