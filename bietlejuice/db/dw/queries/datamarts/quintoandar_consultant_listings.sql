2f94ddb37a97c13a81385cc0ffc787ab05e10fc9
daniloamaral5A <72883439+daniloamaral5A@users.noreply.github.com>
2021-09-14 08:54:17 -0300

    Adding CIQ assigned date and new check to ForSale houses  (#5630)
    * Adding CIQ assigned date and new check to ForSale houses that were already ForRent
    
    * Update quintoandar_consultant_listings.sql
    
    * Update quintoandar_consultant_listings.sql
    
    Making some changes according to previous comments and identified adjustments.
    
    * fix conflicts
    
    * fix conflicts
    
    * fix conflicts
    
    Co-authored-by: Igor Santo <62711339+igor-santo@users.noreply.github.com>
    Co-authored-by: guilhermeCunha <guilherme.cunha@quintoandar.com.br>

--- bietlejuice/db/dw/queries/datamarts/quintoandar_consultant_listings.sql
+++ bietlejuice/db/dw/queries/datamarts/quintoandar_consultant_listings.sql
@@ -37,13 +37,19 @@ quintoandar_consultant_listings_rent as
             ELSE program.name
         END AS type_big_agent,
         'RENT' AS businesscontext,
-        NULL as businesscontext_detail
+        NULL as businesscontext_detail,
+        Agency.dt_since AS dt_ciq_started,
+        dp.id_partner
     FROM
         datalake_big_agent_prod.House
     LEFT JOIN
         last_enrollment le
-            ON le.id_house =House.id
+            ON le.id_house=House.id
     LEFT JOIN
+        datalake_big_agent_prod.Agency
+            ON Agency.id_house=le.id_house
+            AND Agency.id_enrollment=le.id_enrollment
+    LEFT JOIN 
         datalake_big_agent_prod.enrollment
             ON enrollment.id=le.id_enrollment
     LEFT JOIN
@@ -61,6 +67,12 @@ quintoandar_consultant_listings_rent as
     LEFT JOIN
         check_ciq_full ccf
             ON ccf.id_house_external=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
+    LEFT JOIN 
+        dim_partner_agent dpa
+            on dpa.id_user=CAST(JSON_EXTRACT_PATH_TEXT(Agent.details, 'userExternalId') AS BIGINT)
+    LEFT JOIN 
+        dim_partner dp
+            on dp.id_partner=dpa.id_partner
     ),
 quintoandar_consultant_listings_sale AS
 (
@@ -72,7 +84,8 @@ quintoandar_consultant_listings_sale AS
     lf.mkt_origin = 'CIQ' AS is_ciq_origin,
     CASE WHEN lf.mkt_origin = 'CIQ'  THEN TRUE ELSE false END AS is_account_manager,
     'CIQ_FULL'AS type_big_agent,
-    'SALE' AS businesscontext
+    'SALE' AS businesscontext,
+    dp.id_partner
 FROM
     datalake_ebdb_clean_prod.house i
 LEFT JOIN
@@ -91,10 +104,11 @@ LEFT JOIN
     sale.fact_listing_flows lf
         ON LEFT(lf.sk_house_listing,9)=i.id
 
+
 WHERE
     businesscontext='SALE'
     AND dp.type='AUTONOMOUS_AGENT'
-    AND lf.sk_first_listing_date>=20210601
+    AND (lf.sk_lead_date>=20210601 or lf.sk_first_listing_date>=20210601)
 
 ORDER BY 2,1
 ),
@@ -109,7 +123,7 @@ SELECT distinct
     dhl.house_number,
     dhl.house_zipcode,
     dhl.house_city,
-    dhl.house_complement,
+    REGEXP_REPLACE(dhl.house_complement,'([^0-9])','') as complement,
     fhl.sk_owner,
     du.nome,
     du.email,
@@ -161,22 +175,34 @@ INNER JOIN
 ),
 
 last_change_user AS (
+
 SELECT
-    ia.id_house,
-    MIN(rev) as rev_user
+    CAST(JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId') AS BIGINT) AS id_house,
+    Agency.dt_since AS dt_ciq_started
 FROM
-    datalake_ebdb_clean_prod.house_aud ia
-WHERE mod_user_registrant=TRUE
-GROUP BY  1
+    datalake_big_agent_prod.House
+LEFT JOIN
+    last_enrollment le
+        ON le.id_house=House.id
+LEFT JOIN
+    datalake_big_agent_prod.Agency
+        ON Agency.id_house=le.id_house
+        AND Agency.id_enrollment=le.id_enrollment
+
 ),
 
 last_change_sale AS (
 SELECT
     ia.id_house,
-    MAX(rev) as rev_sale
-FROM
+    MAX(FROM_UNIXTIME(u.ts_revision/ 1000)) as ts_sale
+ FROM
     datalake_ebdb_clean_prod.house_aud ia
-WHERE mod_is_for_sale=TRUE
+LEFT JOIN 
+    datalake_ebdb_clean_prod.user_revision_entity u
+        ON u.id=ia.rev
+WHERE 
+    mod_is_for_sale=TRUE  
+    AND is_for_sale=TRUE
 GROUP BY  1
 ),
 quintoandar_consultant_listings AS (
@@ -192,11 +218,13 @@ SELECT
     'SALE' AS businesscontext,
     CASE
         WHEN dc.is_for_rent IS TRUE AND dc.is_for_sale IS TRUE THEN 'cadastrado como rent e sale'
-        WHEN dc.is_for_rent IS TRUE AND (lcs.rev_sale>=lcu.rev_user or lcu.rev_user IS NULL OR qclr.type_big_agent='CIQ_FULL') THEN 'cadastrado como rent e virou sale'
+        WHEN dc.is_for_rent IS TRUE AND (lcs.ts_sale>=lcu.dt_ciq_started OR qclr.type_big_agent='CIQ_FULL') THEN 'cadastrado como rent e virou sale'
         WHEN dc.is_for_sale IS TRUE AND bf.id_house IS NOT NULL AND qcls.id_house<>bf.id_house AND dc.is_for_rent IS FALSE  THEN 'Cadastro de novo Id imovel para ForSale de imovel que já existe em ForRent'
         WHEN dc.is_for_rent IS FALSE AND dc.is_for_sale IS TRUE THEN 'cadastrado somente como sale'
         ELSE 'check'
-    END AS businesscontext_detail
+    END AS businesscontext_detail,
+    null as dt_ciq_started,
+    qcls.id_partner
 FROM
     quintoandar_consultant_listings_sale qcls
 LEFT JOIN
@@ -213,6 +241,7 @@ LEFT JOIN
         ON i.address=bf.house_address
         AND i.number=bf.house_number
         AND du.email=bf.email
+        and REGEXP_REPLACE(i.complement,'([^0-9])','')=bf.complement
         AND du.email<>'lisboagabrielysantos@gmail.com'
 LEFT JOIN sale.dim_listing dl
         ON dl.sk_house=qcls.id_house
@@ -229,9 +258,8 @@ LEFT JOIN
     quintoandar_consultant_listings_rent qclr
         ON qclr.id_house=qcls.id_house
 WHERE
-   lcs.rev_sale>=lcu.rev_user
+   lcs.ts_sale>=lcu.dt_ciq_started
    OR dc.is_for_sale IS TRUE
-   OR lcu.rev_user IS NULL
    OR qclr.type_big_agent='CIQ_FULL'
 )
 UNION
@@ -244,11 +272,9 @@ FROM
 )
 )
 
-
 SELECT
     *
 FROM
     quintoandar_consultant_listings
 ORDER BY
     id_house
-
