4b9b1d188ed892166311597d1ea5196ae2019070
Rafael Ribaldo <ribaldorafael@gmail.com>
2021-09-22 10:49:37 -0300

    [DFR-1098] Adding new columns to sorting_hat.early_credit_analysis clean table (#5981)
    * Adding new columns to early-credit-analysis clean table
    
    * Adding new columns to early-credit-analysis lineage
    
    * Rearranging columns for defined standard
    
    * Removing new line from file
    
    * Fixing yaml version
    
    * Fixing yaml identation

--- bietlejuice/jobs/composer/db/datalake/queries/sorting_hat/clean/early_credit_analysis.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/sorting_hat/clean/early_credit_analysis.sql
@@ -3,7 +3,10 @@ SELECT
     credit_evaluation_id AS id_credit_evaluation,
     user_id AS id_user,
     house_id AS id_house,
+    variant_id AS id_variant,
     result,
+    city,
+    version,
     created_at AS ts_created,
     expiration_date AS ts_expired
 FROM
