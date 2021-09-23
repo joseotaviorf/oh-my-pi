01a690641aa8f704c8f1e6b148af77697e0b01b6
Milena Khouri <mikhouri@users.noreply.github.com>
2021-09-23 11:34:51 -0300

    Adding Terminator utility bills tables to clean (#6016)
    Co-authored-by: Milena Khouri <milenakhouri@brsppn-hv347z2.ldap.quintoandar.com.br>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/terminator/clean/utility_bill_aud.sql
@@ -0,0 +1,16 @@
+SELECT
+    id,
+    termination_id AS id_termination,
+    external_id AS id_external,
+    rev,
+    revtype AS rev_type,
+    revend AS rev_end,
+    type,
+    status,
+    attachments_last_invoice,
+    attachments_payment_voucher,
+    is_included_condominium,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM 
+    datalake_terminator_raw.utility_bill_aud
