8dd9456b84cbb0799a5e679e9f4c1b3ddf1ea4e1
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-09-27 18:06:09 -0300

    [DFR ]updating coalesce and cast in tasks_users_resolutions_flow (#6033)
    * coalesce -1 in id_workgroup and cast hours to float
    
    * adding id_contract and ts_start
    
    * change alias to match table

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_crm_tasks_flows/enrich/tasks_users_resolutions_flow.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_crm_tasks_flows/enrich/tasks_users_resolutions_flow.sql
@@ -1,33 +1,46 @@
-WITH prev_tasks_full AS (
+WITH tasks AS (
+    SELECT
+        id AS id_task,
+        MAX(id_contract) AS id_contract
+    FROM
+        datalake_crm.tasks
+    GROUP BY 1
+),
+prev_tasks_full AS (
     SELECT DISTINCT
-        ct.id_action AS id_action,
-        COALESCE(DATE_FORMAT(ct.ts_action, 'YMMdd'), -1) AS id_action_date,
-        COALESCE(ct.id_assignee, -1) AS id_assignee,
-        COALESCE(DATE_FORMAT(ct.ts_completed, 'YMMdd'), -1) AS id_completed_date, 
-        COALESCE(CAST(ct.id_origin AS BIGINT), -1) AS id_origin,
-        COALESCE(CAST(ct.id_receiver AS BIGINT), -1) AS id_receiver,
-        COALESCE(DATE_FORMAT(ct.ts_start, 'YMMdd'), -1) AS id_start_date,
-        ct.id_task AS id_task,
-        COALESCE(DATE_FORMAT(ct.ts_action, 'YMMdd'), -1) AS id_task_user_end_date,
-        COALESCE(DATE_FORMAT(LAG(ts_action) OVER(PARTITION BY ct.id_task ORDER BY ts_action DESC), 'YMMdd'), -1) AS id_task_user_start_date,
-        COALESCE(ct.id_user_action, -1) AS id_user_action,
-        ct.id_workgroup,
-        ct.action_type,
-        ct.action_user_name,
-        ct.action_type AS task_user_type,
-        ct.origin,
-        ROUND((TO_UNIX_TIMESTAMP(ct.ts_action, 'yyyy-MM-dd hh:mm:ss')
-               - TO_UNIX_TIMESTAMP(LAG(ct.ts_action) OVER (PARTITION BY ct.id_task ORDER BY ct.ts_action), 'yyyy-MM-dd hh:mm:ss')) / 3600.0 , 1) AS task_user_resolve_hours,
-        ct.type,
-        DATE(ct.ts_action) AS dt_partition,
-        ct.ts_action,
-        ct.ts_action AS ts_task_user_end,
-        LAG(ts_action) OVER(PARTITION BY ct.id_task ORDER BY ts_action) AS ts_task_user_start,
-        year,
-        month,
-        day
+        tr.id_action AS id_action,
+        COALESCE(DATE_FORMAT(tr.ts_action, 'YMMdd'), -1) AS id_action_date,
+        COALESCE(tr.id_assignee, -1) AS id_assignee,
+        COALESCE(DATE_FORMAT(tr.ts_completed, 'YMMdd'), -1) AS id_completed_date,
+        t.id_contract,
+        COALESCE(CAST(tr.id_origin AS BIGINT), -1) AS id_origin,
+        COALESCE(CAST(tr.id_receiver AS BIGINT), -1) AS id_receiver,
+        COALESCE(DATE_FORMAT(tr.ts_start, 'YMMdd'), -1) AS id_start_date,
+        tr.id_task AS id_task,
+        COALESCE(DATE_FORMAT(tr.ts_action, 'YMMdd'), -1) AS id_task_user_end_date,
+        COALESCE(DATE_FORMAT(LAG(tr.ts_action) OVER(PARTITION BY tr.id_task ORDER BY tr.ts_action DESC), 'YMMdd'), -1) AS id_task_user_start_date,
+        COALESCE(tr.id_user_action, -1) AS id_user_action,
+        COALESCE(tr.id_workgroup, -1) AS id_workgroup,
+        tr.action_type,
+        tr.action_user_name,
+        tr.action_type AS task_user_type,
+        tr.origin,
+        ROUND((TO_UNIX_TIMESTAMP(tr.ts_action, 'yyyy-MM-dd hh:mm:ss')
+               - TO_UNIX_TIMESTAMP(LAG(tr.ts_action) OVER (PARTITION BY tr.id_task ORDER BY tr.ts_action), 'yyyy-MM-dd hh:mm:ss')) / 3600.0 , 1) AS task_user_resolve_hours,
+        tr.type,
+        DATE(tr.ts_action) AS dt_partition,
+        tr.ts_action,
+        tr.ts_start,
+        tr.ts_action AS ts_task_user_end,
+        LAG(tr.ts_action) OVER(PARTITION BY tr.id_task ORDER BY tr.ts_action) AS ts_task_user_start,
+        tr.year,
+        tr.month,
+        tr.day
     FROM 
-        datalake_crm_tasks_resolution.tasks_resolution ct
+        datalake_crm_tasks_resolution.tasks_resolution tr
+    JOIN
+        tasks t
+          USING(id_task)
 ),
 prev_tasks AS (
     SELECT *
@@ -45,7 +58,8 @@ prev_max_realized_by_user AS (
         MAX(ts_action) AS ts_max_action
     FROM 
         prev_tasks
-    WHERE action_type = 'REALIZE'
+    WHERE 
+        action_type = 'REALIZE'
         AND id_user_action != -1
     GROUP BY 1, 2
 ),
@@ -59,6 +73,7 @@ max_realized_by_user AS (
         t.action_type,
         t.task_user_resolve_hours,
         t.ts_action,
+        t.ts_start,
         t.ts_task_user_start,
         t.ts_task_user_end
     FROM 
@@ -75,6 +90,7 @@ SELECT DISTINCT
     CAST(COALESCE(mrbu.id_action_date, t.id_action_date) AS BIGINT) AS id_action_date,
     CAST(t.id_assignee AS BIGINT) AS id_assignee,
     CAST(t.id_completed_date AS BIGINT) AS id_completed_date,
+    t.id_contract,
     t.id_origin AS id_origin,
     CAST(t.id_receiver AS BIGINT) AS id_receiver,
     CAST(t.id_start_date AS BIGINT) AS id_start_date,
@@ -86,11 +102,12 @@ SELECT DISTINCT
     t.action_type,
     t.action_user_name,
     t.origin,
-    COALESCE(mrbu.task_user_resolve_hours, t.task_user_resolve_hours) AS task_user_resolve_hours,
+    CAST(COALESCE(mrbu.task_user_resolve_hours, t.task_user_resolve_hours) AS FLOAT) AS task_user_resolve_hours,
     t.type, 
     t.action_type AS task_user_type,
     t.dt_partition,
     COALESCE(mrbu.ts_action, t.ts_action) AS ts_action,
+    COALESCE(mrbu.ts_start, t.ts_start) AS ts_start,
     COALESCE(mrbu.ts_task_user_start, t.ts_task_user_start) AS ts_task_user_start,
     COALESCE(mrbu.ts_task_user_end, t.ts_task_user_end) AS ts_task_user_end,
     year,
@@ -103,5 +120,6 @@ LEFT JOIN
         ON t.id_task = mrbu.id_task
             AND t.id_user_action = mrbu.id_user_action
             AND t.action_type = mrbu.action_type
-WHERE NOT(t.id_user_action = -1
+WHERE 
+    NOT(t.id_user_action = -1
           AND t.action_type = 'REALIZE')
