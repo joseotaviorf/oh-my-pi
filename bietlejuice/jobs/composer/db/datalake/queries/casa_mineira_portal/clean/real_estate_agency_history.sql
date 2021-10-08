dbc043b9fe7258658acaeb83517a2900df4254ed
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2021-10-08 11:42:12 -0300

    [DFR] added column modified_value in real_estate_agency_history (#6151)
    * added column modified_value in real_estate_agency_history
    
    * update lineage

--- bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_portal/clean/real_estate_agency_history.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_portal/clean/real_estate_agency_history.sql
@@ -3,6 +3,7 @@ SELECT
     imobiliaria_id AS id_real_estate_agency,
     usuario_id AS id_user,
     campo AS modified_column,
+    valor AS modified_value,
     CAST(criado_em AS TIMESTAMP) AS ts_created
 FROM
     datalake_casa_mineira_portal_raw.imobiliaria_historico
