ab2fc594b1ae456871615f9f23befa689465796b
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-10-13 13:33:31 -0300

    [DECX] migrating crm.fact_onboarding_tenant_tasks (#6009)
    * migrating fact_onboarding_tenant_tasks
    
    * fixing dependencies.yaml
    
    * fixing dependencies.yaml
    
    * Update bietlejuice/jobs/composer/db/dw/ddl/crm_migration/fact_onboaring_tenant_tasks.ddl
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    * changing code review suggestions
    
    * updating ddl
    
    * fixing ddl
    
    * changing task_user_action_resolve_hours data type
    
    * removing cast to float on sql file
    
    * fixing lineage
    
    * update onboarding_tenant sk_origin data type
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/fact_onboarding_tenant_tasks.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update fact_onboaring_tenant_tasks.ddl
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/fact_onboarding_tenant_tasks.sql
@@ -0,0 +1,138 @@
+WITH offers AS (
+  SELECT
+     o.id_firestore,
+     MAX(oe.id) AS id
+  FROM
+    datalake_firestore.rent_offer o
+  LEFT JOIN
+    datalake_ebdb_clean.offer oe
+      ON o.id_firestore = oe.id_firestore
+  GROUP BY 1
+),
+contracts AS (
+  SELECT
+    turf.id_task AS sk_task,
+    turf.id_receiver AS sk_receiver,
+    turf.id_start_date AS sk_start_date,
+    turf.id_completed_date AS sk_completed_date,
+    turf.id_origin AS sk_origin,
+    turf.id_assignee AS sk_assignee,
+    turf.id_user_action AS sk_user_action,
+    turf.id_action_date AS sk_action_date,
+    CAST(COALESCE(dc.sk_contract, CAST(eo.id_contrato AS STRING), CAST(ev.id_contract AS STRING), CAST(co.id AS STRING), '-1') as BIGINT) AS sk_contract,
+    turf.id_task_user_start_date AS sk_task_user_start_date,
+    turf.id_task_user_end_date AS sk_task_user_end_date,    
+    turf.action_user_name,
+    turf.ts_action,
+    turf.action_type,
+    turf.ts_task_user_start,
+    turf.ts_task_user_end,
+    turf.task_user_type,
+    turf.task_user_resolve_hours,
+    turf.year,
+    turf.month,
+    turf.day
+  FROM
+    datalake_crm_tasks_flows.tasks_users_resolutions_flow turf
+  LEFT JOIN 
+    dw_janus.dim_contract dc
+      ON turf.origin = 'Contrato'
+      AND turf.id_origin = dc.sk_contract
+  LEFT JOIN 
+    datalake_ebdb_clean.onboarding eo
+      ON turf.origin = 'Onboarding'
+      AND turf.id_origin  = eo.id
+  LEFT JOIN 
+    datalake_ebdb_clean.inspection ev
+      ON turf.origin = 'Vistoria'
+      AND turf.id_origin = ev.id
+  LEFT JOIN 
+    offers o
+      ON turf.origin = 'Offer'
+      AND o.id_firestore = turf.id_origin
+  LEFT JOIN 
+    datalake_ebdb_clean.proposal p
+      ON o.id = p.id_offer
+  LEFT JOIN 
+    datalake_ebdb_clean.contract co
+      ON p.id = co.id
+  WHERE
+    (
+      turf.type IN (
+        'AgendarBuscaEEntregaDeChaves',
+        'AtualizaInfoChavesProp',
+        'AtualizaInfoChavesRecebidas',
+        'BuscarChaveProprietario',
+        'ConfirmarComprovantesContas',
+        'ConfirmarDados',
+        'ConfirmarLocalChaves',
+        'ContatarInquilinoInfoEntregaChaves',
+        'DevolverChavesParaProprietarios',
+        'EntregaChavesParaInquilino',
+        'InquilinoNaoRecebeuTodasChaves',
+        'PagarContasConsumo',
+        'SendLongTermEmailOwner',
+        'SendLongTermEmailTenant',
+        'SendShortTerm',
+        'TransferenciaContasConsumoAgua',
+        'TransferenciaContasConsumoGas',
+        'TransferenciaContasConsumoLuz',
+        'VerificarContrato'
+      )
+    OR (
+      turf.type = 'Manual' 
+      AND turf.id_workgroup IN (
+        'DEP_ONBOARDING_INQUILINO',
+        'DEP_KEY_TRAVEL_AFTER_EXIT'
+      )
+    )
+  )
+  AND turf.year = {year}
+  AND turf.month = {motnh}
+  AND turf.day = {day}
+),
+contract_house_listing AS (
+  SELECT
+    CAST(sk_house_listing AS BIGINT) as sk_house_listing,
+    CAST(sk_owner AS BIGINT) AS sk_house_owner,
+    CAST(sk_contract AS BIGINT) AS sk_contract,
+    CAST(sk_client AS BIGINT) AS sk_tenant,
+    CAST(sk_rent_flow AS BIGINT) AS sk_rent_flow
+  FROM
+    testing_map_ods_from_s3.ods_fact_listing_rent_flows
+  WHERE
+    sk_contract != '-1'
+  GROUP BY 1, 2, 3, 4, 5
+)
+SELECT DISTINCT
+  c.sk_task,
+  c.sk_receiver,
+  c.sk_start_date,
+  c.sk_completed_date,
+  COALESCE(CAST(c.sk_origin AS BIGINT), -1) AS sk_origin,
+  c.sk_assignee,
+  c.sk_user_action,
+  c.sk_action_date,
+  c.sk_contract,
+  COALESCE(chl.sk_house_listing, -1) AS sk_house_listing,
+  COALESCE(chl.sk_house_owner, -1) AS sk_house_owner,
+  COALESCE(chl.sk_tenant, -1) AS sk_tenant,
+  c.sk_task_user_start_date AS sk_task_action_start_date,
+  c.sk_task_user_end_date AS sk_task_action_end_date,
+  c.action_user_name,
+  c.action_type,
+  c.task_user_type AS task_action_type,
+  c.task_user_resolve_hours AS task_user_action_resolve_hours,
+  c.ts_task_user_start AS ts_task_action_start,
+  c.ts_task_user_end AS ts_task_action_end,
+  c.ts_action,
+  NOW() AS ts_load,
+  c.year,
+  c.month,
+  c.day
+FROM
+  contracts c
+LEFT JOIN
+  contract_house_listing chl
+    ON c.sk_contract = chl.sk_contract
+    AND c.sk_contract != -1
