0d31bf834b463493af99c26b8d50e9f7718d807e
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-10-13 14:34:51 -0300

    [DFR-1070] Migrating dim_inspection_task from CRM (#5875)
    * Migrating dim_inspection_task from CRM
    
    * removing extra comma
    
    * updating conditions and lineage
    
    * reordering
    
    * updating
    
    * comment
    
    * Updating DDL
    
    * updating float to decimal

--- bietlejuice/jobs/composer/dags/dw_crm/dw_crm.md
+++ bietlejuice/jobs/composer/dags/dw_crm/dw_crm.md
@@ -16,6 +16,7 @@ This pipeline produces the following output tables in DW layer, via incremental 
 
 - `crm_migration.dim_closing_task_transition`
 - `crm_migration.dim_closing_task_workflow`
+- `crm_migration.dim_inspection_task`
 - `crm_migration.dim_offboarding_task`
 - `crm_migration.dim_onboarding_tenant_task`
 - `crm_migration.dim_ungrouped_manual_task`
---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_inspection_task.sql
@@ -0,0 +1,55 @@
+-- TO DO: After finishing CRM migration, it is necessary to rename columns following our Naming Conventions
+SELECT
+    id_task AS sk_task,
+    score_factor,
+    version,
+    origin,
+    type,
+    description,
+    subject,
+    CAST(titles AS STRING) AS titles,
+    CAST(workgroups AS STRING) AS workgroups,
+    hours_task_started_to_completed AS hours_task_start_to_completed,
+    is_resolved AS flg_solved,
+    is_task_auto_completed,
+    ts_started AS ts_start,
+    ts_completed,
+    ts_silenced_until,
+    NOW() AS ts_load,
+    year,
+    month,
+    day
+FROM
+    datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+WHERE
+    (type IN (
+        'AgendarVistoria',
+        'AgendarVistoriaPreSaida',
+        'AgendarVistoriaSaida',
+        'AnalisarPreVistoria',
+        'AnaliseVistoriaSaida',
+        'CancelarVistoriaPreSaida',
+        'ConfirmarVistoriaEntrada',
+        'ConfirmarVistoriaPreSaida',
+        'ConfirmarVistoriaSaida',
+        'EnviarPrimeiroResultadoSaida',
+        'EnviarSegundoResultadoSaida',
+        'EnviarVistoria',
+        'FollowUpVistoriaEntrada',
+        'FollowUpVistoriaPreSaida',
+        'FollowUpVistoriaSaida',
+        'PrimeiraAnaliseVistoriaSaida',
+        'SegundaAnaliseVistoriaSaida'
+        )
+        OR (id_workgroup IN (
+                'DEP_VISTORIA_ID',
+                'DEP_VISTORIA_LAUDO',
+                'DEP_VISTORIA_OFFBOARDING',
+                'EXIT_INSPECTION_TEAM',
+                'REVISIT_POSTCONTRACT_TEAM'
+            )
+            AND type = 'Manual')
+    )
+    AND year = {year}
+    AND month = {month}
+    AND day = {day}
