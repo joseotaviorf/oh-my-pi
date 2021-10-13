35ec03e93d82cd74532a76f483e47a13c33fdbe5
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-10-13 13:33:46 -0300

    [DFR-1063] Migrating dim_onboarding_tenant_task from CRM (#5873)
    * Migrating dim_onboarding_tenant_task from CRM
    
    * conditions and lineage
    
    * reordering
    
    * reordering
    
    * updating
    
    * Updating DDLs
    
    * removing ts_partition column from dim_onboarding_tenant_task
    
    * Update bietlejuice/jobs/composer/db/dw/ddl/crm_migration/dim_onboarding_tenant_task.ddl
    
    Co-authored-by: paulocarneiro99 <paulo.carneiro@quintoandar.com.br>
    Co-authored-by: Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_onboarding_tenant_task.sql
@@ -0,0 +1,52 @@
+-- TO DO: After finishing CRM migration, it is necessary to rename columns following our Naming Conventions
+SELECT
+    id_task AS sk_task,
+    score_factor,
+    version,
+    origin,
+    type,
+    description,
+    subject,
+    CAST(titles AS STRING) AS titles,
+    CAST(workgroups AS STRING) AS workgroups,
+    hours_task_started_to_completed AS hours_task_start_to_completed,
+    is_resolved AS flg_solved,
+    is_task_auto_completed,
+    ts_started AS ts_start,
+    ts_completed,
+    ts_silenced_until,
+    NOW() AS ts_load,
+    year,
+    month,
+    day
+FROM
+    datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+WHERE
+    (type IN (
+        'AgendarBuscaEEntregaDeChaves',
+        'AtualizaInfoChavesProp',
+        'AtualizaInfoChavesRecebidas',
+        'BuscarChaveProprietario',
+        'ConfirmarComprovantesContas',
+        'ConfirmarDados',
+        'ConfirmarLocalChaves',
+        'ContatarInquilinoInfoEntregaChaves',
+        'DevolverChavesParaProprietarios',
+        'EntregaChavesParaInquilino',
+        'InquilinoNaoRecebeuTodasChaves',
+        'PagarContasConsumo',
+        'SendLongTermEmailOwner',
+        'SendLongTermEmailTenant',
+        'SendShortTerm',
+        'TransferenciaContasConsumoAgua',
+        'TransferenciaContasConsumoGas',
+        'TransferenciaContasConsumoLuz',
+        'VerificarContrato'
+        )
+        OR (id_workgroup IN ('DEP_KEY_TRAVEL_AFTER_EXIT', 'DEP_ONBOARDING_INQUILINO')
+                AND type = 'Manual'
+        )
+    )
+    AND year = {year}
+    AND month = {month}
+    AND day = {day}
