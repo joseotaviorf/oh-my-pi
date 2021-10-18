c53895b7ec450db201c108e8f9e081fc4bd04094
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-10-18 11:39:43 -0300

    [DFR-1100] Migrating crm.fact_credit_tasks (#5997)
    * Migrating crm.fact_credit_tasks
    
    * Adding lineage
    
    * indentation on yml
    
    * Updating query
    
    * Removing dt_parition, updating lineage, updating dependencies
    
    * Updating lineage
    
    * updating DDL, query
    
    * id_origin DDL
    
    * float
    
    * updating identation
    
    * updating data types
    
    * removing changes on doc.md and dependencies.yml to avoid conflicts, this will be added in a different PR
    
    Co-authored-by: paulocarneiro99 <paulo.carneiro@quintoandar.com.br>
    Co-authored-by: Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/fact_credit_tasks.sql
@@ -0,0 +1,85 @@
+WITH proposals AS (
+    SELECT
+        turf.id_task AS sk_task,
+        turf.id_workgroup AS sk_workgroup,
+        turf.id_receiver AS sk_receiver,
+        turf.id_start_date AS sk_start_date,
+        turf.id_completed_date AS sk_completed_date,
+        turf.id_origin AS sk_origin,
+        turf.id_assignee AS sk_assignee,
+        CAST(COALESCE(epi.id_user, -1) AS BIGINT) AS sk_house_owner,
+        CAST(COALESCE(ep.id_proponent, -1) AS BIGINT) AS sk_proponent,
+        CAST(COALESCE(ep.id, -1) AS BIGINT) AS sk_proposal,
+        turf.id_user_action AS sk_user_action,
+        turf.id_action_date AS sk_action_date,
+        turf.id_task_user_start_date AS sk_task_user_start_date,
+        turf.id_task_user_end_date AS sk_task_user_end_date,
+        turf.action_user_name,
+        turf.action_type,
+        turf.origin,
+        turf.task_user_type,
+        turf.task_user_resolve_hours,
+        turf.ts_action,
+        turf.ts_task_user_start,
+        turf.ts_task_user_end,
+        turf.year,
+        turf.month,
+        turf.day
+    FROM
+        datalake_crm_tasks_flows.tasks_users_resolutions_flow AS turf
+    LEFT JOIN
+        datalake_ebdb_clean.proposal AS ep
+            ON turf.origin = 'Proposta'
+            AND turf.id_origin = ep.id
+    LEFT JOIN
+        datalake_ebdb_clean.house AS epi
+            ON epi.id = ep.id_house
+    WHERE
+        turf.type IN ('EnviarCardiff')
+        AND turf.year = {year}
+        AND turf.month = {month}
+        AND turf.day = {day}
+),
+proposal_house_listing AS (
+    SELECT
+        CAST(sk_house_listing AS BIGINT) AS sk_house_listing,
+        CAST(sk_proposal AS BIGINT) AS sk_proposal,
+        CAST(sk_client AS BIGINT) AS sk_proponent
+    FROM
+        testing_map_ods_from_s3.ods_fact_listing_rent_flows
+    WHERE
+        sk_proposal != '-1'
+    GROUP BY 1, 2, 3
+)
+SELECT DISTINCT
+    p.sk_task,
+    p.sk_receiver,
+    COALESCE(CAST(p.sk_origin AS BIGINT), -1) AS sk_origin,
+    p.sk_assignee,
+    p.sk_user_action,
+    p.sk_proposal,
+    COALESCE(phl.sk_house_listing, -1) AS sk_house_listing,
+    p.sk_house_owner,
+    COALESCE(phl.sk_proponent, p.sk_proponent) AS sk_proponent,
+    p.sk_start_date,
+    p.sk_completed_date,
+    p.sk_action_date,
+    p.sk_task_user_start_date,
+    p.sk_task_user_end_date,
+    p.action_user_name,
+    p.action_type,
+    p.task_user_type,
+    p.task_user_resolve_hours,
+    p.ts_action,
+    p.ts_task_user_start,
+    p.ts_task_user_end,
+    NOW() AS ts_load,
+    p.year,
+    p.month,
+    p.day
+FROM
+    proposals AS p
+LEFT JOIN
+    proposal_house_listing AS phl
+        ON p.sk_proposal = phl.sk_proposal
+        AND p.sk_proposal != -1
---
+++ bietlejuice/jobs/composer/db/dw/ddl/crm_migration/fact_credit_tasks.ddl
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS crm_migration.fact_credit_tasks;
+CREATE TABLE crm_migration.fact_credit_tasks (
+    sk_task VARCHAR,
+    sk_receiver BIGINT,
+    sk_origin BIGINT,
+    sk_assignee BIGINT,
+    sk_user_action BIGINT,
+    sk_proposal BIGINT,
+    sk_house_listing BIGINT,
+    sk_house_owner BIGINT,
+    sk_proponent BIGINT,
+    sk_start_date BIGINT,
+    sk_completed_date BIGINT,
+    sk_action_date BIGINT,
+    sk_task_user_start_date BIGINT,
+    sk_task_user_end_date BIGINT,
+    action_user_name VARCHAR,
+    action_type VARCHAR,
+    task_user_type VARCHAR,
+    task_user_resolve_hours DECIMAL(11,1),
+    ts_action TIMESTAMP,
+    ts_task_user_start TIMESTAMP,
+    ts_task_user_end TIMESTAMP,
+    ts_load TIMESTAMP
+);
+
+ALTER TABLE crm_migration.fact_credit_tasks OWNER TO airflow;
+CALL grant_all_permissions_on_schema('crm_migration');
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA crm_migration TO GROUP etl;
+GRANT ALL ON SCHEMA crm_migration TO GROUP ETL;
