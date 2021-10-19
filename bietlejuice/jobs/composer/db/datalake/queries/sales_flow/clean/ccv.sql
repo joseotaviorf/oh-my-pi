22fbb1c62bcdfd28cfade6206d617bbac9e1c94f
Thiago Beppe <thiagoabeppe@gmail.com>
2021-10-19 17:19:18 -0300

    [DFSBB-59] Adding columns on sales_flow tables (#6200)

--- bietlejuice/jobs/composer/db/datalake/queries/sales_flow/clean/ccv.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/sales_flow/clean/ccv.sql
@@ -5,6 +5,7 @@ SELECT
     signed_document_token, 
     unsigned_document_token,
     sale_value AS sale_price,
+    ccv_version,
     status,
     signed_at AS ts_signed,
     created_at AS ts_created,
