d36745d731232a8ac19f7f2a398576439f076dca
Letícia Lima <59769848+lcdlima@users.noreply.github.com>
2021-10-20 16:31:27 -0300

    [DTM-1034] Adding new columns to rental and sale source targets tables (#6218)
    * Adding new columns to rental and sale source targets tables
    
    * Altering order of columns according to code review
    
    Co-authored-by: Leticia Lima <leticia.lima@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/rental_ntp_source_targets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/rental_ntp_source_targets.sql
@@ -1,13 +1,14 @@
 SELECT
-    ntp_target,
     tier,
     city_group,
     mkt_channel,
     mkt_medium,
     mkt_source,
+    ntp_target,
+    rtp_target,
     week_start,
     month,
     year,
-    date
+    date AS dt_target
 FROM
     datalake_gsheets_raw.rental_ntp_source_targets
--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/sale_nbp_source_targets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/sale_nbp_source_targets.sql
@@ -5,9 +5,10 @@ SELECT
     mkt_medium,
     mkt_source,
     nbp_target,
+    rbp_target,
     week_start,
     month,
     year,
-    date
+    date AS dt_target
 FROM
     datalake_gsheets_raw.sale_nbp_source_targets
