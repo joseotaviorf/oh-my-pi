40a621ec69087bc3815ec90fbc0391f91af0faf0
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-10-26 15:44:17 -0300

    updating CRM dims to full load (#6235)

--- bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_onboarding_tenant_task.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_onboarding_tenant_task.sql
@@ -1,6 +1,14 @@
 -- TO DO: After finishing CRM migration, it is necessary to rename columns following our Naming Conventions
+WITH last_updated_task AS (
+    SELECT
+        id_task,
+        MAX(DATE(CONCAT(year,'-',month,'-',day))) AS dt_last_updated
+    FROM
+        datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+    GROUP BY 1
+)
 SELECT
-    id_task AS sk_task,
+    tarf.id_task AS sk_task,
     score_factor,
     version,
     origin,
@@ -15,12 +23,13 @@ SELECT
     ts_started AS ts_start,
     ts_completed,
     ts_silenced_until,
-    NOW() AS ts_load,
-    year,
-    month,
-    day
+    NOW() AS ts_load
 FROM
-    datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+    datalake_crm_tasks_flows.tasks_actions_resolutions_flow tarf
+JOIN
+    last_updated_task lut
+        ON tarf.id_task = lut.id_task
+        AND DATE(CONCAT(tarf.year,'-',tarf.month,'-',tarf.day)) = lut.dt_last_updated
 WHERE
     (type IN (
         'AgendarBuscaEEntregaDeChaves',
@@ -47,6 +56,3 @@ WHERE
                 AND type = 'Manual'
         )
     )
-    AND year = {year}
-    AND month = {month}
-    AND day = {day}
--- bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_ungrouped_manual_task.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_ungrouped_manual_task.sql
@@ -1,5 +1,13 @@
+WITH last_updated_task AS (
+    SELECT
+        id_task,
+        MAX(DATE(CONCAT(year,'-',month,'-',day))) AS dt_last_updated
+    FROM
+        datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+    GROUP BY 1
+)
 SELECT
-  id_task AS sk_task,
+  tarf.id_task AS sk_task,
   score_factor,
   version,
   origin,
@@ -14,15 +22,13 @@ SELECT
   ts_started AS ts_start,
   ts_completed,
   ts_silenced_until,
-  NOW() AS ts_load,
-  year,
-  month,
-  day
+  NOW() AS ts_load
 FROM
-  datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+  datalake_crm_tasks_flows.tasks_actions_resolutions_flow tarf
+JOIN
+    last_updated_task lut
+        ON tarf.id_task = lut.id_task
+        AND DATE(CONCAT(tarf.year,'-',tarf.month,'-',tarf.day)) = lut.dt_last_updated
 WHERE
   type = 'Manual'
   AND id_workgroup IS NULL
-  AND year = {year}
-  AND month = {month}
-  AND day = {month}
