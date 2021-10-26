3c4d13076e2d430609007a85546f26500d97c77b
Isadora Gallerani <isadoragallerani@hotmail.com>
2021-10-26 19:04:50 -0300

    [DFR-1062] Migrating dim_collection_task from CRM (#5872)
    * Migrating dim_collection_task from CRM
    
    * updating lineage and conditions
    
    * partitions
    
    * updating
    
    * Updating DDLs
    
    * updating DDL
    
    * full load

--- bietlejuice/jobs/composer/dags/dw_crm/dw_crm.md
+++ bietlejuice/jobs/composer/dags/dw_crm/dw_crm.md
@@ -16,6 +16,7 @@ This pipeline produces the following output tables in DW layer, via incremental 
 
 - `crm_migration.dim_closing_task_transition`
 - `crm_migration.dim_closing_task_workflow`
+- `crm_migration.dim_collection_task`
 - `crm_migration.dim_credit_task`
 - `crm_migration.dim_inspection_task`
 - `crm_migration.dim_lead_task`
---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_crm/dw/dim_collection_task.sql
@@ -0,0 +1,34 @@
+WITH last_updated_task AS (
+    SELECT
+        id_task,
+        MAX(DATE(CONCAT(year, '-', month, '-', day))) AS dt_last_updated
+    FROM
+        datalake_crm_tasks_flows.tasks_actions_resolutions_flow
+    GROUP BY 1
+)
+SELECT
+    tarf.id_task AS sk_task,
+    score_factor,
+    version,
+    origin,
+    type,
+    description,
+    subject,
+    CAST(titles AS STRING) AS titles,
+    CAST(workgroups AS STRING) AS workgroups,
+    hours_task_started_to_completed AS hours_task_start_to_completed,
+    is_resolved AS flg_solved,
+    is_task_auto_completed,
+    ts_started AS ts_start,
+    ts_completed,
+    ts_silenced_until,
+    NOW() AS ts_load
+FROM
+    datalake_crm_tasks_flows.tasks_actions_resolutions_flow AS tarf
+JOIN
+    last_updated_task AS lut
+        ON tarf.id_task = lut.id_task
+        AND DATE(CONCAT(tarf.year, '-', tarf.month, '-', tarf.day)) = lut.dt_last_updated
+WHERE
+    id_workgroup IN ('DEP_COLLECTIONS_ID')
+    AND type = 'Manual'
