a490adde04a8172bc9c020ddefc987fa5bc106b0
Felipe Miquelim <felipe.miquelim@quintoandar.com.br>
2021-10-28 11:43:02 -0300

    Changing Cluster (#6271)

--- bietlejuice/jobs/composer/dags/enrich_listing_flow/enrich_listing_flow.py
+++ bietlejuice/jobs/composer/dags/enrich_listing_flow/enrich_listing_flow.py
@@ -34,7 +34,7 @@ doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
 SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get("databricks_mid_resources_cluster", deserialize_json=True)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
     "destination"
 ] = f"{spark_jobs_logs_path}{DAG_ID}"
