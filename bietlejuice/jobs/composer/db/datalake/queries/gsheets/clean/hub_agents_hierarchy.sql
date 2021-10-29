1e25291408eb328c23cb921709f2968cf10b13a6
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2021-10-29 09:00:48 -0300

    feat: change querie and lineage of hub_agents_hierarchy (#6276)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/hub_agents_hierarchy.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/hub_agents_hierarchy.sql
@@ -1,17 +1,13 @@
 SELECT
-  cpf,
-  cnpj,
-  creci,
-  full_name,
-  quintoandar_email,
-  personal_number,
-  status,
-  register_status,
-  function_type,
-  hub_region,
-  manager,
-  CAST(has_email_quintoandar AS BOOLEAN) AS has_email_quintoandar,
-  TO_DATE(dt_entrance, 'dd/mm/yyyy') AS dt_entrance,
-  TO_DATE(dt_ended, 'dd/mm/yyyy') AS dt_ended
+    CAST(id_agent AS BIGINT) AS id_agent,
+    cpf,
+    creci,
+    full_name,
+    quintoandar_email,
+    personal_number,
+    status,
+    function_type,
+    hub_region,
+    manager
 FROM
-  datalake_gsheets_raw.hub_agents_hierarchy
+    datalake_gsheets_raw.hub_agents_hierarchy
