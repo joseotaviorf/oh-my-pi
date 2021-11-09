c2d12609799873bb8023f841bf0ccdb8cca8578c
Letícia Lima <59769848+lcdlima@users.noreply.github.com>
2021-11-09 09:14:35 -0300

    Adding territory planning and city group in households_per_city_ibge.sql (#6322)
    Co-authored-by: Leticia Lima <leticia.lima@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/households_per_city_ibge.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/households_per_city_ibge.sql
@@ -3,6 +3,8 @@ SELECT
     NULLIF(state_UF, '') AS state_UF,
     NULLIF(state_name, '') AS state_name,
     NULLIF(microregion_name, '') AS microregion_name,
+    NULLIF(territory_planning, '') AS territory_planning,
+    NULLIF(city_group, '') AS city_group,
     CAST(households AS INTEGER) AS households
 FROM
     datalake_gsheets_raw.households_per_city_ibge
