5e5d5c3f16457d77b8465af6fdc3d8eb857b9fdf
Ana Alves <ana.araujo@quintoandar.com.br>
2021-11-11 15:10:04 -0300

    added enrich dag for portal casa mineira tof users (#6351)
    * added enrich dag for portal casa mineira tof users
    
    * added dag dependencies
    
    * added review suggestions

---
+++ bietlejuice/jobs/composer/dags/enrich_top_of_funnel_portal_cm/enrich_top_of_funnel_portal_cm.md
@@ -0,0 +1,23 @@
+## Enrich Top of Funnel Portal Casa Mineira
+
+### Purpose
+
+As a derivation of Amplitude enrich layer, this DAG generates a table with information about Casa Mineira Portal's top of the funnel users.
+
+​<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+
+Daily. More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+Currently, there is the following output table in our enrich layer:
+
+- `top_of_funnel_portal_cm.top_of_funnel_users_portal_casa_mineira`
+
+### Responsible Data Team
+
+For any questions or concerns about this DAG, please contact the Data Engineering or Data Analytics team responsible listed in the [DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+</details>
---
+++ bietlejuice/jobs/composer/dags/enrich_top_of_funnel_portal_cm/enrich_top_of_funnel_portal_cm.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from pendulum import timezone
+import os
+
+from airflow.utils.helpers import chain
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+MAIN_START_DATE = datetime(2021, 6, 17, tzinfo=timezone("America/Sao_Paulo"))
+
+CONTEXT = "top_of_funnel_portal_cm"
+DAG_NAME = f"enrich_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+ENV = os.environ.get("ENVIRONMENT")
+
+config_service = ConfigurationService(DAG_NAME)
+ATHENA_QUERY_RESULTS_BUCKET = config_service.get_config("athena_query_results_bucket")
+DATALAKE_BUCKET = config_service.get_config("datalake_bucket")
+DATABRICKS_BIETLEJUICE_REPO_PATH = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+SPARK_JOBS_LOGS_PATH = config_service.get_config("spark_jobs_logs_path")
+DOC_MD_CHART_URL = config_service.get_config("doc_md_chart_url")
+
+PARTITION_COLS = config_service.get_config("partition_cols")
+
+BASE_SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
+    "destination"
+] = f"{SPARK_JOBS_LOGS_PATH}{DAG_ID}"
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_CHART_URL, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULTS_BUCKET,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+    is_incremental=True,
+    partitions=PARTITION_COLS,
+)
+
+create_cluster_task.set_downstream(
+    DatalakeTaskGroup.all_first_tasks(enrich_task_groups)
+)
+terminate_cluster_task.set_upstream(
+    DatalakeTaskGroup.all_last_tasks(enrich_task_groups)
+)
