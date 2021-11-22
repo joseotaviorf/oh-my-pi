b0f4b6733d619a0e72c26c9ce9339b938c993609
luisarasera <44903017+luisarasera@users.noreply.github.com>
2021-11-22 10:45:21 -0300

    Update greenseer_sessions.sql (#6407)
    * Update greenseer_sessions.sql
    
    Adicionei as colunas response_key e is_menu available e alterei o filtro para considerar também os casos do novo menu de respostas.
    
    * correcoes update greenseer_sessions.sql
    
    Correções no padrão do SQL
    
    * correcao da identacao
    
    Correcao da identação nas condicionais da tabela greenseer_session.sql
    
    * correcao das condicionais
    
    correção de identação e do "then" de acordo com o SQL style guide
    
    * correcao_final(3)
    
    Ajuste de indentacao do END

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_greenseer/enrich/greenseer_session.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_greenseer/enrich/greenseer_session.sql
@@ -1,27 +1,36 @@
 WITH greenseer_sessions AS (
   SELECT
     id_session,
-    COALESCE(GET_JSON_OBJECT(memory, '$.legacy.user_data.user.id'),get_json_object(memory, '$.basic.user.id')) as id_user,
+    COALESCE(GET_JSON_OBJECT(memory, '$.legacy.user_data.user.id'),get_json_object(memory, '$.basic.user.id')) AS id_user,
     id_pipeline,
-    GET_JSON_OBJECT(memory, '$.predictions.intents.before_reception.intent') as before_reception,
-    GET_JSON_OBJECT(memory, '$.predictions.intents.after_reception.intent') as after_reception,
-    GET_JSON_OBJECT(memory, '$.basic.session.context_message') as context_message,
-    GET_JSON_OBJECT(memory, '$.basic.session.created_by_hsm') as created_by_hsm,
-    GET_JSON_OBJECT(memory, '$.business_rules.tags.added') as tags_added,
-    GET_JSON_OBJECT(memory, '$.basic.flags') as flags,
-    GET_JSON_OBJECT(memory, '$.basic.user.more_help_required') as more_help_required,
+    GET_JSON_OBJECT(memory, '$.predictions.intents.before_reception.intent') AS before_reception,
+    GET_JSON_OBJECT(memory, '$.predictions.intents.after_reception.intent') AS after_reception,
+    GET_JSON_OBJECT(memory, '$.basic.session.context_message') AS context_message,
+    GET_JSON_OBJECT(memory, '$.basic.session.created_by_hsm') AS created_by_hsm,
+    GET_JSON_OBJECT(memory, '$.business_rules.tags.added') AS tags_added,
+    GET_JSON_OBJECT(memory, '$.basic.flags') AS flags,
+    GET_JSON_OBJECT(memory, '$.basic.user.more_help_required') AS more_help_required,
+    GET_JSON_OBJECT(memory, '$.business_rules.menu_taxonomies.response_key') AS response_key,
+    CASE 
+	      WHEN
+	            GET_JSON_OBJECT(memory, '$.business_rules.menu_taxonomies.message') IS NULL
+	            OR GET_JSON_OBJECT(memory, '$.business_rules.menu_taxonomies.message') = '' THEN FALSE 
+	      ELSE TRUE
+    END AS is_menu_available,
     current_state,
     ts_started,
     ts_ended
   FROM
     datalake_greenseer_clean.session
 ),
-greenseer_retentions as (
+greenseer_retentions AS (
   SELECT 
     id_session,
     CASE 
-      WHEN session.agent = 'QuintoAndar' or session.agent is null THEN false 
-      ELSE true
+        WHEN 
+             session.agent = 'QuintoAndar' 
+             OR session.agent is null THEN false 
+        ELSE true
     END AS is_retention
   FROM
     greenseer_sessions
@@ -33,7 +42,7 @@ greenseer_retentions as (
     AND session.source = 'whatsapp'
     AND session.source_environment = 'default' 
     AND session.status = 'expired'
-    AND ((before_reception <> '' AND before_reception <> 'fallback' AND before_reception IS NOT NULL) OR (after_reception <> '' AND after_reception <> 'fallback' AND after_reception IS NOT NULL))
+    AND ((before_reception <> '' AND before_reception <> 'fallback' AND before_reception IS NOT NULL) OR (after_reception <> '' AND after_reception <> 'fallback' AND after_reception IS NOT NULL) OR (response_key IS NOT NULL AND response_key <> ''))
 ),
 sessions_and_tickets AS (
   SELECT DISTINCT
@@ -52,6 +61,8 @@ SELECT
   before_reception,
   after_reception,
   is_retention,
+  response_key,
+  is_menu_available,
   context_message,
   created_by_hsm,
   tags_added,
