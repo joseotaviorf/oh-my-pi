7acdec0aaaf5e12eb6113f00b9c5d0e4d110da76
daniloamaral5A <72883439+daniloamaral5A@users.noreply.github.com>
2021-11-22 13:44:36 -0300

    Adjusting BbigAgent's agency enrich table (#6419)
    * Adjusting BbigAgent's agency enrich table
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
    
    Co-authored-by: GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_big_agent/enrich/agency.sql
@@ -1,10 +1,28 @@
-WITH agency AS (
+WITH count_agency AS (
     SELECT
-        *,
-        ROW_NUMBER() OVER(PARTITION BY id_house ORDER BY ts_updated DESC) AS row_n
+        id_house,
+        COUNT(1) AS total_houses
     FROM
         datalake_big_agent_clean.agency
+    WHERE 
+        ts_deleted IS NULL
+    GROUP BY 1
+),
+
+agency AS (
+    SELECT
+        a.*,
+        ROW_NUMBER() OVER(PARTITION BY a.id_house ORDER BY a.ts_updated DESC) AS row_n
+    FROM
+        datalake_big_agent_clean.agency AS a
+    LEFT JOIN 
+        count_agency AS ca
+            ON a.id_house=ca.id_house
+    WHERE 
+        ts_deleted IS NULL 
+        OR ca.id_house IS NULL
 )
+
 SELECT
     id,
     id_house,
