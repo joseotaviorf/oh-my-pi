843df54840aa0d1d9ba182fe2693e864b056c5db
Eduardo Trevisani <etgamba@gmail.com>
2021-11-29 18:40:02 -0300

    [AEMKT-415] Added Casa Mineira marketing manual daily costs DAG (#6471)
    * Added SQL and lineage
    
    * Added DAG
    
    * Fixed table name
    
    * Fixed admin whinning
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_marketing_manual_daily_costs/clean/casa_mineira_marketing_manual_shared_costs.sql
    
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>
    Co-authored-by: Isadora Gallerani <isadoragallerani@hotmail.com>

---
+++ bietlejuice/jobs/composer/dags/casa_mineira_marketing_manual_daily_costs/casa_mineira_marketing_manual_daily_costs.py
@@ -0,0 +1,111 @@
+import os
+import json
+from datetime import datetime
+from pendulum import timezone
+
+from airflow.models import DAG, Variable
+from airflow.utils.helpers import chain
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+# DAG inputs
+SOURCE = "casa_mineira_marketing_manual_daily_costs"
+CONTEXT = "casa_mineira_marketing_manual_daily_costs"
+DAG_ID = f"bietlejuice.{SOURCE}"
+MAIN_START_DATE = datetime(2021, 5, 1, 0, 0, 0, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = "0 11 * * *"
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_min_general_cluster", deserialize_json=True
+)
+
+config_service = ConfigurationService(CONTEXT)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+artifacts_bucket = config_service.get_config("artifacts_bucket")
+
+sheets_list = config_service.get_config("sheets_list")
+
+ENV = os.environ["ENVIRONMENT"]
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+GSHEETS_RAW_FILE_PATH = f"{BASE_SPARK_JOBS_PATH}load_gsheets_into_datalake_raw.py"
+
+CUSTOM_LIBRARIES = [
+    {
+        "whl": f"{artifacts_bucket}/gsheets-api-client-python/"
+        f"quintoandar_gsheets_api_client-0.2.1-py2.py3-none-any.whl"
+    }
+]
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": BaseDAG.DEFAULT_OWNER,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
+    doc_md=BaseDAG.get_dag_doc(CONTEXT).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=CUSTOM_LIBRARIES,
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=datalake_bucket,
+    relative_query_path=CONTEXT,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    athena_query_result_location=athena_query_results_bucket,
+)
+
+raw_task_groups = {}
+for table_name, sheet_details in sheets_list.items():
+
+    raw_task_group = task_group.build_raw_task_group_for_single_table(
+        source=SOURCE,
+        target_database_base_name=SOURCE,
+        table_name=table_name,
+        extraction_spark_job_file=GSHEETS_RAW_FILE_PATH,
+        raw_spark_job_extra_args=[SOURCE, table_name, json.dumps(sheet_details)],
+    )
+    raw_task_groups[table_name] = raw_task_group
+
+
+clean_task_groups = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.CLEAN,
+    source_database_base_name=SOURCE,
+    target_database_base_name=SOURCE,
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(raw_task_groups))
+
+TaskFlowHelper.chain_task_groups_via_common_table(raw_task_groups, clean_task_groups)
+
+terminate_cluster_task.set_upstream(DatalakeTaskGroup.all_last_tasks(clean_task_groups))
---
+++ bietlejuice/jobs/composer/db/datalake/queries/casa_mineira_marketing_manual_daily_costs/clean/casa_mineira_marketing_manual_shared_costs.sql
@@ -0,0 +1,20 @@
+SELECT
+    NULLIF(side, '') AS side,
+    NULLIF(city_group, '') AS city_group,
+    NULLIF(account_name, '') AS account_name,
+    NULLIF(campaign_name, '') AS campaign_name,
+    NULLIF(ad_group_name, '') AS ad_group_name,
+    NULLIF(utm_term, '') AS utm_term,
+    NULLIF(utm_content, '') AS utm_content,
+    NULLIF(mkt_business, '') AS mkt_business,
+    NULLIF(mkt_origin, '') AS mkt_origin,
+    NULLIF(mkt_channel, '') AS mkt_channel,
+    NULLIF(mkt_medium, '') AS mkt_medium,
+    NULLIF(mkt_source, '') AS mkt_source,
+    CAST(NULLIF(cost, '') AS DOUBLE) AS cost,
+    CAST(NULLIF(cost_share_mobile, '') AS DOUBLE) AS cost_share_mobile,
+    CAST(NULLIF(cost_share_desktop, '') AS DOUBLE) AS cost_share_desktop,
+    CAST(NULLIF(cost_share_other, '') AS DOUBLE) AS cost_share_other,
+    DATE(NULLIF(dt, '')) AS dt_cost
+FROM
+    datalake_casa_mineira_marketing_manual_daily_costs_raw.casa_mineira_marketing_manual_shared_costs
