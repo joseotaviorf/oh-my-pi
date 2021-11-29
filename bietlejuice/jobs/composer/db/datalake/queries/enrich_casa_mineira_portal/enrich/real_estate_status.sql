5bccd3e9daedd6b0d13b1743157927814af8eb85
Gustavo Miller Santos <91481595+GustavoMSDextra@users.noreply.github.com>
2021-11-29 09:25:52 -0300

    [DFB-54] Portal Casa Mineira - Add status change dates to enrich real_estate_status (#6475)
    * fix: remove incorrect lineage
    
    * feat: add date trunc logic to enrich
    
    * docs: add examples to comment
    
    * fix: rename status change columns

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_casa_mineira_portal/enrich/real_estate_status.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_casa_mineira_portal/enrich/real_estate_status.sql
@@ -52,16 +52,44 @@ WITH real_estate_agency AS (
     SELECT *
     FROM 
         agency_reactivated
+),
+real_estate_status AS (
+    SELECT *,
+    LEAD (ts_created) 
+    OVER (
+        PARTITION BY 
+            id_real_estate_agency
+        ORDER BY 
+            ts_created
+    ) AS ts_status_ended
+FROM 
+    real_estate_agency
 )
-SELECT 
+
+/*
+    ts_created and ts_status_ended are the timestamps in which the status changes in the system, however
+    they aren't really the dates in which the changes are actually applied for billing and budget. Those
+    are actually dt_consider_status_started and dt_consider_status_ended.
+
+    If an agency changes status until the 15th, dt_consider_status_started will be the current month. However
+    after the 15th, it will be the next month. 
+    As for dt_consider_status_ended: if the status ends until the 15th, it will be the previous month. Otherwise,
+    it will be the current month.
+*/
+SELECT
     id_real_estate_agency,
     real_estate_agency_name,
     real_estate_agency_full_name,
     status,
+    CASE 
+        WHEN EXTRACT(day FROM ts_created) > 15 THEN ADD_MONTHS(DATE_TRUNC('month', ts_created), 1)
+        ELSE DATE_TRUNC('month', ts_created)
+    END AS dt_consider_status_started,
+    CASE
+        WHEN EXTRACT(day FROM ts_status_ended) > 15 THEN DATE_TRUNC('month', ts_status_ended)
+        ELSE ADD_MONTHS(DATE_TRUNC('month', ts_status_ended), -1)
+    END AS dt_consider_status_ended,
     ts_created,
-    LEAD (ts_created)
-        OVER (
-        PARTITION BY id_real_estate_agency
-        ORDER BY ts_created) AS ts_status_ended
+    ts_status_ended
 FROM 
-    real_estate_agency
+    real_estate_status
