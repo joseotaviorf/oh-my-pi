9de3376885695a372b0b751ca4e0d209c458cc40
Rafael Caran <rafael.fernandes@quintoandar.com.br>
2021-12-01 15:28:35 -0300

    [DTM-1083] Adjusting datamarts mkt_origin taxonomy (#6503)
    Co-authored-by: Rafael Fernandes <rafaelfernandes@BRSPPM-C02FJ3N0MD6M.local>

--- bietlejuice/db/dw/queries/datamarts/sale_performance_marketing_metrics_supply_coincident.sql
+++ bietlejuice/db/dw/queries/datamarts/sale_performance_marketing_metrics_supply_coincident.sql
@@ -913,7 +913,6 @@ SELECT
     city_group,
     CASE
         WHEN mkt_origin = 'PWA - Paid' THEN 'Owner PWA'
-        WHEN mkt_origin LIKE '% - Sale' THEN replace(mkt_origin,' - Sale','')
         ELSE mkt_origin
     END AS mkt_origin,
     mkt_channel,
