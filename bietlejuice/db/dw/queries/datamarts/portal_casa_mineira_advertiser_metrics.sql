b51385937f3c3cac748f6308762cfeeeabd5cc61
monicalafaria <87429592+monicalafaria@users.noreply.github.com>
2021-12-13 17:21:56 -0300

    Enhancing query for advertiser metrics CM (#6568)

--- bietlejuice/db/dw/queries/datamarts/portal_casa_mineira_advertiser_metrics.sql
+++ bietlejuice/db/dw/queries/datamarts/portal_casa_mineira_advertiser_metrics.sql
@@ -30,13 +30,12 @@ status as (
     SELECT
         date,
         week_start,
+        month_start,
         id_real_estate_agency as advertiser_id,
         r.real_estate_agency_name as advertiser_name,
         uf.uf_initials as advertiser_uf,
         ct.city_name as advertiser_city,
-        status,
-        NULL::FLOAT AS daily_budget,
-        NULL::INT AS published_listings
+        status
     FROM status_ts
     JOIN dim_date
         ON date BETWEEN ts_start::DATE AND COALESCE(ts_end::DATE, CURRENT_DATE) - 1
@@ -56,29 +55,16 @@ info_budget AS (
         b.ts_created,
         r.ts_disabled,
         b.budget_value,
-        r.real_estate_agency_name,
-        uf.uf_initials,
-        ct.city_name,
         LEAD(b.ts_created) OVER(PARTITION BY b.id_real_estate_agency ORDER BY b.ts_created) AS ts_next_change
     FROM datalake_casa_mineira_portal_clean_prod.real_estate_agency_budget b
         JOIN datalake_casa_mineira_portal_clean_prod.real_estate_agency r
             ON r.id=b.id_real_estate_agency
-        LEFT JOIN datalake_casa_mineira_portal_clean_prod.city AS ct
-            ON r.id_city = ct.id
-        LEFT JOIN datalake_casa_mineira_portal_clean_prod.uf AS uf
-            ON ct.id_uf = uf.id
 ),
 budget as (
     SELECT
         date,
-        week_start,
         id_real_estate_agency as advertiser_id,
-        real_estate_agency_name as advertiser_name,
-        uf_initials as advertiser_uf,
-        city_name as advertiser_city,
-        NULL::TEXT AS status,
-        budget_value / (DATE_DIFF('DAY', month_start, month_end) + 1)::FLOAT as daily_budget,
-        NULL::INT AS published_listings
+        budget_value / (DATE_DIFF('DAY', month_start, month_end) + 1)::FLOAT as daily_budget
     FROM info_budget b
         JOIN dim_date
             ON date BETWEEN ts_created::DATE AND COALESCE(ts_next_change::DATE, ts_disabled::DATE, CURRENT_DATE) - 1
@@ -89,31 +75,31 @@ budget as (
 listings as (
 SELECT
     date,
-    week_start,
     h.id_real_estate_agency::INT as advertiser_id,
-    r.real_estate_agency_name as advertiser_name,
-    uf.uf_initials as advertiser_uf,
-    ct.city_name as advertiser_city,
-    NULL::TEXT AS status,
-    NULL::FLOAT AS daily_budget,
     COUNT(DISTINCT h.id) AS published_listings
 FROM datalake_casa_mineira_portal_clean_prod.house h
     JOIN dim_date
         ON date BETWEEN ts_created::DATE AND COALESCE(ts_disabled::DATE, CURRENT_DATE) - 1
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.real_estate_agency AS r
-        ON h.id_real_estate_agency = r.id
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.city AS ct
-        ON r.id_city = ct.id
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.uf AS uf
-        ON ct.id_uf = uf.id
 WHERE date BETWEEN DATE_ADD('YEAR', -1, CURRENT_DATE) AND DATE_ADD('DAY', -1, CURRENT_DATE)
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2
 )
---------------------------------
------------UNION ALL------------
---------------------------------
-SELECT * FROM status 
-UNION ALL
-SELECT * FROM budget 
-UNION ALL
-SELECT * FROM listings
+----------------------------------
+-----------FINAL TABLE------------
+----------------------------------
+SELECT
+    s.date,
+    s.week_start,
+    s.month_start,
+    s.advertiser_id,
+    s.advertiser_name,
+    s.advertiser_uf,
+    s.advertiser_city,
+    s.status,
+    SUM(b.daily_budget) AS daily_budget,
+    SUM(l.published_listings) AS published_listings
+FROM status s
+LEFT JOIN budget b
+    ON s.date=b.date AND s.advertiser_id=b.advertiser_id
+LEFT JOIN listings l
+    ON s.date=l.date AND s.advertiser_id=l.advertiser_id
+GROUP BY 1,2,3,4,5,6,7,8
