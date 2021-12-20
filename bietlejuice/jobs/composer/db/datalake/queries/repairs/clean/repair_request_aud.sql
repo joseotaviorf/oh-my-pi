b0efbdf6d7b19c41ad4ca47e385e4f980abee615
Caio Cid Santiago <caiocid@gmail.com>
2021-12-20 18:07:30 -0300

    [DESS] Fix repair_request_aud (#6616)

--- bietlejuice/jobs/composer/db/datalake/queries/repairs/clean/repair_request_aud.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/repairs/clean/repair_request_aud.sql
@@ -6,13 +6,15 @@ SELECT
     third_party_crm_ticket_external_id AS id_third_party_crm_ticket_external,
     rev, 
     revend AS rev_end, 
-    CAST(revtype AS INTEGER) AS rev_type, 
+    CAST(revtype AS INTEGER) AS rev_type,
     allow_personal_info_sharing,
+    status, 
     third_party_crm,
     contract_id_mod AS mod_id_contract, 
     house_id_mod AS mod_id_house, 
     requester_user_id_mod AS mod_id_requester_user,
     third_party_crm_ticket_external_id_mod AS mod_id_third_party_crm_ticket_external,
+    status_mod AS mod_status,
     third_party_crm_mod AS mod_third_party_crm 
 FROM
     datalake_repairs_raw.repair_request_aud 
