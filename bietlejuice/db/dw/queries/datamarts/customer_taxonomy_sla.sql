221deb6f2a0f88611bed345aa236573e8cdfd10b
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2021-12-20 15:19:24 -0300

    changing join with team to journey_step (#6614)

--- bietlejuice/db/dw/queries/datamarts/customer_taxonomy_sla.sql
+++ bietlejuice/db/dw/queries/datamarts/customer_taxonomy_sla.sql
@@ -1,6 +1,8 @@
 SELECT 
     ft.sk_ticket ,
-    (( ft.full_resolution_time ) / (60*24.0)) <= sla_in_days AS is_sla
+    (( ft.full_resolution_time ) / (60*24.0)) <= sla_in_days AS is_sla,
+    ts.contact_theme_tag,
+    ts.contact_theme_detail_tag
 FROM 
     customer_support.fact_ticket ft 
 JOIN
@@ -12,7 +14,7 @@ JOIN
 JOIN 
     datalake_gsheets_clean_prod.taxonomy_sla ts
         ON dt.theme_detail = ts.contact_theme_detail_tag 
-        AND dd.team = ts.de_para_team 
+        AND dd.journey_step = ts.journey_step 
 WHERE
     ft.front_or_back = 'back'
     AND dt.theme_detail IS NOT NULL
