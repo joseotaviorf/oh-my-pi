45c7740a002315cb38c37b40c71583e51aa3bea7
Eduardo Trevisani <etgamba@gmail.com>
2021-12-20 16:30:01 -0300

    Added default_libraries into the DAG (#6615)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/enrich_casa_mineira_marketing_automatic_daily_costs/enrich_casa_mineira_marketing_automatic_daily_costs.py
+++ bietlejuice/jobs/composer/dags/enrich_casa_mineira_marketing_automatic_daily_costs/enrich_casa_mineira_marketing_automatic_daily_costs.py
@@ -34,6 +34,7 @@ databricks_bietlejuice_repo_path = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+default_libraries = config_service.get_config("default_libraries")
 
 ENV = os.environ.get("ENVIRONMENT")
 BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
@@ -53,7 +54,10 @@ dag = DAG(
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=default_libraries,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
