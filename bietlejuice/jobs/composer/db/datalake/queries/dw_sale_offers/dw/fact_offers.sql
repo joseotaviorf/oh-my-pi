e5a2a607f0d3d5296e5eed657c259e0e7362b58b
samuel-favarin <91481588+samuel-favarin@users.noreply.github.com>
2021-12-20 22:43:15 -0300

    Dforsale fix fact offers (#6618)
    * fix: update sk_consultant type
    
    * fix: sk_consultant

--- bietlejuice/jobs/composer/db/datalake/queries/dw_sale_offers/dw/fact_offers.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_sale_offers/dw/fact_offers.sql
@@ -7,7 +7,7 @@ SELECT
 	COALESCE(h.id_region,-1) AS sk_region,
   	COALESCE(eso.id_booking,-1) AS sk_booking,
   	COALESCE(eso.id_agent,-1) AS sk_agent,
-  	COALESCE(eso.id_consultant, -1) AS sk_consultant,
+  	eso.id_consultant AS sk_consultant,
   	COALESCE(eso.id_closing_specialist, -1) AS sk_closing_specialist,
 	COALESCE(CAST(REPLACE(substring(eso.ts_offer_submitted,1, 10),'-','') AS BIGINT), -1) AS sk_offer_submitted_date,
 	COALESCE(CAST(REPLACE(substring(eso.dt_offer_accepted,1, 10),'-','') AS BIGINT), -1) AS sk_offer_accepted_date,
