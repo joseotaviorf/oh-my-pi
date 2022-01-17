c3a922ebc528400ffd86d1571ac28b874e598c95
Giuseppe Giovanelli <50770057+giuseppecg@users.noreply.github.com>
2022-01-17 10:45:02 -0300

    [DAFIN] Adding columns to risk category clean (#6692)
    * adding more columns
    
    * sorting alphabetically columns

--- bietlejuice/jobs/composer/db/datalake/queries/rental_guarantee/clean/risk_category.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/rental_guarantee/clean/risk_category.sql
@@ -1,7 +1,10 @@
 SELECT 
     id,  
+    category_description,
     category_level, 
-    factor,
+    deposit_factor,
+    factor AS guarantee_factor,
+    allow_deposit AS is_deposit_allowed,
     allow_guarantee AS is_guarantee_allowed,
     created_at AS ts_created,
     updated_at AS ts_updated
