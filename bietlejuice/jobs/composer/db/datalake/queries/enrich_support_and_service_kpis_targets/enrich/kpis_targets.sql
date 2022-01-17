a3262967c823ee79dc3f8bb887cad98040756bc5
Paulo Andre de Oliveira Carneiro <pandrecarneiro@gmail.com>
2022-01-17 17:27:38 -0300

    changing dt_target to get all the dates not only the truncated (#6749)

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_support_and_service_kpis_targets/enrich/kpis_targets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_support_and_service_kpis_targets/enrich/kpis_targets.sql
@@ -2,23 +2,23 @@ WITH exploded_metric_vality AS (
   SELECT
     *,
     'Support' AS vertical,
-    EXPLODE(SEQUENCE(dt_start, dt_end)) AS dt_metric_valid
+    EXPLODE(SEQUENCE(dt_start, dt_end)) AS dt_target
   FROM
     datalake_gsheets_clean.target_support_kpis
   UNION ALL
   SELECT
     *,
     'Service' AS vertical,
-    EXPLODE(SEQUENCE(dt_start, dt_end)) AS dt_metric_valid
+    EXPLODE(SEQUENCE(dt_start, dt_end)) AS dt_target
   FROM
     datalake_gsheets_clean.target_service_kpis
 )
-SELECT DISTINCT
+SELECT
   metric_name,
   team,
   granularity,
   vertical,
   target,
-  DATE_TRUNC(granularity, dt_metric_valid) AS dt_target
+  dt_target
 FROM
   exploded_metric_vality
