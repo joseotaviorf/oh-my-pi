92e45fc7795901b7b8b69dd855f6112ed6747253
Isadora Gallerani <isadoragallerani@hotmail.com>
2022-01-20 18:13:44 -0300

    [DFR] Updating scheduling interval on Viva Real DAG (#6795)
    * Updating scheduling interval on Viva Real DAG
    
    * fix on import
    
    * wednesday

--- bietlejuice/jobs/composer/dags/crawlers/listings/viva_real.py
+++ bietlejuice/jobs/composer/dags/crawlers/listings/viva_real.py
@@ -27,7 +27,7 @@ CONFIG_NAME = 'crawlers_listings'
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 ENV = os.environ.get("ENVIRONMENT")
 MAIN_START_DATE = datetime(2021, 8, 23, 0, 0, 0, tzinfo=LOCAL_TZ)
-MAIN_SCHEDULE_INTERVAL = "0 3 * * 1"
+MAIN_SCHEDULE_INTERVAL = "0 3 * * 3"
 
 config_service = ConfigurationService(dag_name = CONFIG_NAME, intermediate_path=INTERMEDIATE_PATH)
 
