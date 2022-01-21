a33fde023d5a116e55727893707fb0242047eb2e
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-01-21 15:02:40 -0300

    [AES&S] Fixes and improvements in workday_window query (#6806)
    * fixing count_workdays
    
    * remove cte

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_date/enrich/workday_window.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_date/enrich/workday_window.sql
@@ -1,47 +1,23 @@
-WITH cities AS (
+WITH cities_calendar AS (
     SELECT 
-        id AS id_city,
-        name AS city
-    FROM 
-        datalake_region.region 
-    WHERE 
-        level = 'Cidade'
-),
-calendar_brz_holidays AS (
-    SELECT
-        id_date,
-        weekend, 
-        is_brz_holiday, 
+        r.id AS id_city,
+        r.name AS city,
+        ad.id_date,
         CASE 
-            WHEN weekend = 'Weekend' OR is_brz_holiday = 'Holiday' THEN FALSE 
+            WHEN ad.weekend = 'Weekend' OR ad.is_brz_holiday = 'Holiday' OR sch.dt_holiday IS NOT NULL THEN FALSE 
             ELSE TRUE
         END is_workday,
         date AS dt_reference
-    FROM
-        datalake_quintoandar.aux_date
-),
-cities_calendar AS (
-    SELECT 
-        * 
     FROM 
-        cities
+        datalake_region.region r
     CROSS JOIN
-        calendar_brz_holidays
-),
-workdays AS (
-    SELECT 
-        cc.id_city, 
-        cc.dt_reference, 
-        CASE 
-            WHEN cc.is_workday = FALSE OR sch.dt_holiday IS NOT NULL THEN FALSE
-            ELSE TRUE
-        END is_workday
-    FROM 
-        cities_calendar cc
+        datalake_quintoandar.aux_date ad
     LEFT JOIN
         datalake_gsheets_clean.service_city_holidays sch
-            ON cc.id_city = sch.id_city 
-            AND cc.dt_reference = sch.dt_holiday
+            ON r.id_city = sch.id_city 
+            AND ad.date = sch.dt_holiday
+    WHERE 
+        r.level = 'Cidade'
 ),
 workday_calendar AS (
     SELECT 
@@ -55,7 +31,7 @@ workday_calendar AS (
     FROM 
         cities_calendar cc
     LEFT JOIN 
-        workdays w 
+        cities_calendar w 
             ON cc.id_city = w.id_city
             AND w.dt_reference BETWEEN cc.dt_reference AND cc.dt_reference + interval '25' DAY	
     WHERE
@@ -67,17 +43,17 @@ SELECT
     city,
     dt_reference AS dt_ref,
     MAX(CASE WHEN count_workdays = 1 THEN dt_workday ELSE NULL END) AS dt_start,
-    MAX(CASE WHEN count_workdays = 1 THEN dt_workday ELSE NULL END) AS dt_end_1,
-    MAX(CASE WHEN count_workdays = 2 THEN dt_workday ELSE NULL END) AS dt_end_2,
-    MAX(CASE WHEN count_workdays = 3 THEN dt_workday ELSE NULL END) AS dt_end_3,
-    MAX(CASE WHEN count_workdays = 4 THEN dt_workday ELSE NULL END) AS dt_end_4,
-    MAX(CASE WHEN count_workdays = 5 THEN dt_workday ELSE NULL END) AS dt_end_5,
-    MAX(CASE WHEN count_workdays = 6 THEN dt_workday ELSE NULL END) AS dt_end_6,
-    MAX(CASE WHEN count_workdays = 7 THEN dt_workday ELSE NULL END) AS dt_end_7,
-    MAX(CASE WHEN count_workdays = 15 THEN dt_workday ELSE NULL END) AS dt_end_15
+    MAX(CASE WHEN count_workdays = 2 THEN dt_workday ELSE NULL END) AS dt_end_1,
+    MAX(CASE WHEN count_workdays = 3 THEN dt_workday ELSE NULL END) AS dt_end_2,
+    MAX(CASE WHEN count_workdays = 4 THEN dt_workday ELSE NULL END) AS dt_end_3,
+    MAX(CASE WHEN count_workdays = 5 THEN dt_workday ELSE NULL END) AS dt_end_4,
+    MAX(CASE WHEN count_workdays = 6 THEN dt_workday ELSE NULL END) AS dt_end_5,
+    MAX(CASE WHEN count_workdays = 7 THEN dt_workday ELSE NULL END) AS dt_end_6,
+    MAX(CASE WHEN count_workdays = 8 THEN dt_workday ELSE NULL END) AS dt_end_7,
+    MAX(CASE WHEN count_workdays = 16 THEN dt_workday ELSE NULL END) AS dt_end_15
 FROM 
     workday_calendar
 WHERE 
-    count_workdays IN (1, 2, 3, 4, 5, 6, 7, 15)
+    count_workdays IN (1, 2, 3, 4, 5, 6, 7, 8, 16)
 GROUP BY 
     1, 2, 3, 4
