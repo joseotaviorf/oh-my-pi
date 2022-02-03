f8dce6e823db790e0b95f3c8954ac7695f4d29ce
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-02-03 13:43:45 -0300

    [DAE-735] Add DAG `dw_agent` and table dim_agent_review (#6928)
    * Add dim_agent_review table and DAG

---
+++ bietlejuice/jobs/composer/dags/dw_agent/dw_agent.md
@@ -0,0 +1,17 @@
+## DW Agent
+
+### Purpose
+
+This DAG creates tables that summarize the agent context models.
+
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+This dag is triggered once per day via Mediator. More information about run time [here]({chart_url}{dag_id})
+
+### Outputs
+
+This pipeline produces the following output tables: 
+
+- `public.dim_agent_review`
---
+++ bietlejuice/jobs/composer/dags/dw_agent/dw_agent.py
@@ -0,0 +1,83 @@
+import os
+from datetime import datetime
+
+import pendulum
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+from airflow.utils.helpers import chain
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, DAGOwnerEnum
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
+from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
+from bietlejuice.jobs.composer.services import ConfigurationService
+
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2020, 2, 20, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+DW_SCHEMA = "public"
+CONTEXT = "agent"
+DAG_NAME = f"dw_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+ENV = os.environ.get("ENVIRONMENT")
+
+config_service = ConfigurationService(DAG_NAME)
+spectrum_iam_role = config_service.get_config("spectrum_iam_role")
+dw_bucket = config_service.get_config("dw_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
+
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_med_general_cluster", deserialize_json=True
+)
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": DAGOwnerEnum.DATA_FOR_RENT,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+dw_task_group = DWTaskGroup(
+    dag=dag,
+    env=ENV,
+    dw_bucket=dw_bucket,
+    dw_schema=DW_SCHEMA,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=SPARK_JOBS_PATH,
+)
+
+dw_staging_task_group = dw_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW_STAGING
+)
+
+dw_task_group = dw_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW, spectrum_iam_role=spectrum_iam_role
+)
+
+chain(create_cluster_task, DWTaskGroup.all_first_tasks(dw_staging_task_group))
+
+TaskFlowHelper.chain_task_groups_via_common_table(dw_staging_task_group, dw_task_group)
+
+chain(DWTaskGroup.all_last_tasks(dw_task_group), terminate_cluster_task)
