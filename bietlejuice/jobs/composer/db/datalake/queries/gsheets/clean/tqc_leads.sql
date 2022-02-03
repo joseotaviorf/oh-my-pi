13e763b93f4db8ab29271bcea6b82623db55502d
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-02-03 10:28:01 -0300

    [DFORSALE-397]: add new column in gsheet tqc_leads (#6907)
    * feat: adding new column in gsheet tqc_leads
    
    * test: test cast column is_valid_lead to int
    
    * fix: add null if in is_valid_lead column
    
    * fix: change cast to boolean

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/tqc_leads.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/tqc_leads.sql
@@ -4,6 +4,7 @@ SELECT
     lead_phone,
     lead_email,
     program_phase,
+    BOOLEAN(NULLIF(is_valid_lead, '')) AS is_valid_lead,
     CAST(appointment_date AS TIMESTAMP) AS ts_appointment,
     CAST(new_lead_date AS TIMESTAMP) AS ts_new_lead
 FROM
