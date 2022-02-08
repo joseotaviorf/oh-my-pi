12afb2d9d4eccf70ca487a4462d28fb2654219d8
Thiago Beppe <thiagoabeppe@gmail.com>
2022-02-08 18:25:30 -0300

    changing dag name (#6973)

--- bietlejuice/jobs/composer/dags/crawlers/listings/em_casa.py
+++ bietlejuice/jobs/composer/dags/crawlers/listings/em_casa.py
@@ -13,6 +13,7 @@ from bietlejuice.jobs.composer.base.airflow import BaseDAG
 from bietlejuice.jobs.composer.base.airflow.dag_owner_enum import DAGOwnerEnum
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
 from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.dags.crawlers.listings.loft import ORIGIN
 from bietlejuice.jobs.composer.services.configuration_service import (
     ConfigurationService,
 )
@@ -22,7 +23,7 @@ CRAWLER_ORIGIN = f"em_casa"
 
 SOURCE = "crawlers"
 CONTEXT = SOURCE
-DAG_NAME = f"{CRAWLER_CONTEXT}.{SOURCE}"
+DAG_NAME = f"{CRAWLER_CONTEXT}.{ORIGIN}"
 
 SOURCE_WITH_CONTEXT = f"{SOURCE}_{CRAWLER_CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
