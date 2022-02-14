5b83d404c9606c174c42c8751f2a641218984170
Thiago Beppe <thiagoabeppe@gmail.com>
2022-02-14 09:38:26 -0300

    [DFS] SalesFlow Start time (#7008)

--- bietlejuice/jobs/composer/dags/sales_flow/sales_flow.py
+++ bietlejuice/jobs/composer/dags/sales_flow/sales_flow.py
@@ -23,7 +23,7 @@ CONTEXT = SOURCE
 DAG_ID = f"bietlejuice.{CONTEXT}"
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2021, 3, 2, 0, 0, 0, tzinfo=LOCAL_TZ)
-MAIN_SCHEDULE_INTERVAL = "0 4 * * *"
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
 
 # airflow vars
 ENV = os.environ.get("ENVIRONMENT")
