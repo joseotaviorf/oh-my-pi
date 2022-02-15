440ad8067210e63f2bc4bf398b95b942c76b7cd0
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-02-15 15:38:04 -0300

    [AEBED] fix dw agent dag (#7058)
    * Fix columns type and order

--- bietlejuice/jobs/composer/db/datalake/queries/dw_agent/dw/dim_agent_region.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_agent/dw/dim_agent_region.sql
@@ -2,7 +2,7 @@ WITH region AS (
 SELECT
 	COALESCE(CAST(REPLACE(CAST(DATE(r.dt) AS string), '-', '') AS integer), -1) AS sk_regions_date,
 	dadosagente_id AS sk_agent,
-	regions AS regioes,
+	regions,
 	area,
 	secondary_area,
 	area_deprecated,
@@ -10,7 +10,13 @@ SELECT
 FROM datalake_ebdb_agents.agent_region_group r
 )
 SELECT
-	CONCAT(sk_regions_date, sk_agent)::BIGINT AS sk_agent_region,
-	*,
+	CAST(CONCAT(sk_regions_date, sk_agent) AS BIGINT) AS sk_agent_region,
+	CAST(sk_regions_date AS INTEGER) AS sk_regions_date,
+	CAST(sk_agent AS INTEGER) AS sk_agent,
+	CAST(regions AS VARCHAR(3076)) AS regions,
+	CAST(area AS VARCHAR(10)) AS area,
+	CAST(secondary_area AS VARCHAR(10)) AS secondary_area,
+	CAST(area_deprecated AS VARCHAR(10)) AS area_deprecated,
+	CAST(secondary_area_deprecated AS VARCHAR(10)) AS secondary_area_deprecated,
 	CURRENT_TIMESTAMP AS dt_timestamp
 FROM region
