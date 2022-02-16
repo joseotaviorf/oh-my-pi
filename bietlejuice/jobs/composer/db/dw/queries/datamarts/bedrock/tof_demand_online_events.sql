6c33244afbde5a514b2e9210259413efdabfd9db
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-02-16 08:52:26 -0300

    [AEBED] datamarts migration tof demand online events (#7065)
    * Datamart migration tof_demand_online_events

--- bietlejuice/jobs/composer/db/dw/queries/datamarts/bedrock/tof_demand_online_events.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/bedrock/tof_demand_online_events.sql
@@ -1,16 +1,16 @@
 with
 last_version_listings as (
-select
-	id_house,
-	sk_house_listing,
-	cast(nullif(cast(ts_listing_version_start as varchar), '') as timestamp) as ts_listing_version_start,
-	cast(nullif(cast(ts_listing_version_end as varchar), '') as timestamp) as ts_listing_version_end
-from datalake_clean.ods_dim_house_listing
-where cast(nullif(cast(version as varchar), '') as bigint) > 0
+	select
+		id_house,
+		sk_house_listing,
+		cast(nullif(cast(ts_listing_version_start as varchar), '') as timestamp) as ts_listing_version_start,
+		cast(nullif(cast(ts_listing_version_end as varchar), '') as timestamp) as ts_listing_version_end
+	from public.dim_house_listing
+	where cast(nullif(cast(version as varchar), '') as bigint) > 0
 )
 select
-  date_trunc('week', date(ts_event)) as event_week,
-  date(ts_event) as event_date,
+	date_trunc('week', date(ts_event)) as event_week,
+	date(ts_event) as event_date,
 	ts_event as event_timestamp,
 	ev.id_user,
 	ev.id_amplitude,
@@ -21,13 +21,14 @@ select
 	case when trim(event_type) = 'pilot_cw_button_clicked' then 1 else 0 end as talk_to_agent_button_clicked,
 	case when trim(event_type) = 'piloto_cw_dialog_viewed' then 1 else 0 end as talk_to_agent_dialog_viewed,
 	case when trim(event_type) = 'piloto_cw_message_sent' then 1 else 0 end as talk_to_agent_message_sent,
-  case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{9}))).*'),4) else null end as talk_to_agent_message_content,
-  case when trim(event_type) = 'offer_submitted' then 1 else 0 end as offer_submitted,
-  cast(json_extract_scalar(user_properties, '$.utm_source') as varchar) as utm_source,
+	case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{9}))).*'),4) else null end as talk_to_agent_message_content,
+	case when trim(event_type) = 'offer_submitted' then 1 else 0 end as offer_submitted,
+	cast(json_extract_scalar(user_properties, '$.utm_source') as varchar) as utm_source,
 	cast(json_extract_scalar(user_properties, '$.utm_medium') as varchar) as utm_medium,
 	cast(json_extract_scalar(user_properties, '$.utm_campaign') as varchar) as utm_campaign,
 	cast(json_extract_scalar(user_properties, '$.utm_content') as varchar) as utm_content,
-	cast(json_extract_scalar(user_properties, '$.utm_term') as varchar) as utm_term
+	cast(json_extract_scalar(user_properties, '$.utm_term') as varchar) as utm_term,
+	NOW() AS ts_load
 from datalake_amplitude_clean_prod.events ev
 left join last_version_listings lvl
   on trim(json_extract_scalar(ev.event_properties, '$["house_id"]'))  = cast(lvl.id_house as varchar)
