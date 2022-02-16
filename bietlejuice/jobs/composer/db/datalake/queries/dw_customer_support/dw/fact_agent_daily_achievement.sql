cc0d31e04a861152fd21a6c114b9f5a0117580e7
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-02-16 17:09:44 -0300

    Fixing fact_agent_daily_achievement table (#7086)

--- bietlejuice/jobs/composer/db/datalake/queries/dw_customer_support/dw/fact_agent_daily_achievement.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_customer_support/dw/fact_agent_daily_achievement.sql
@@ -8,7 +8,7 @@ WITH achievement AS (
         (ap.total_csat_satisfied_score/ap.total_tickets_with_csat_score)/rt.target_csat AS csat_satisfied_target_achievement,
         (ap.total_tickets_resolution/ap.total_tickets_answered_resolution)/rt.target_resolution AS resolution_rate_target_achievement,
         (ap.total_tickets+COALESCE(ap.total_crm_tasks_solved,0))/rt.target_productivity AS closed_tickets_target_achievement,
-        ((ap.total_minutes_resolution_time/ap.total_tickets)/1440.0)/rt.target_frt AS avg_days_resolution_target_achievement,
+        rt.target_frt/((ap.total_minutes_resolution_time/ap.total_tickets)/1440.0) AS avg_days_resolution_target_achievement,
         ap.dt,
         ap.year,
         ap.month,
