84b0b5268fec5896e59cb66cb4afec7feb755197
Samuel Favarin <91481588+samuel-favarin@users.noreply.github.com>
2022-02-17 11:52:53 -0300

    [DFORSALE-288] Enrich Leads Classified  (#7106)
    Create Enrich Leads Classified
    
    Co-authored-by: Gustavo Miller <gustavo.santos.dxt@ext.quintoandar.com.br>
    Co-authored-by: Samuel Brati Favarin <samuel.favarin@conectanuvem.com.br>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_sale_lead_flows/enrich/classified_lead_flow.sql
@@ -0,0 +1,79 @@
+WITH all_leads_with_replies AS (
+    SELECT 
+        id_lead
+    FROM
+        datalake_classified_leads_clean.lead_reply_whatsapp
+    UNION
+    SELECT
+        id_lead
+    FROM
+        datalake_classified_leads_clean.lead_reply_email
+),
+leads AS (
+    SELECT
+        lc.id,
+        CAST(lc.id_property AS BIGINT) AS id_house,
+        h.id_region,
+        lc.user_name,
+        REGEXP_EXTRACT(lc.user_phone_number,'([0-9]+)', 1) AS user_phone_number,
+        REGEXP_EXTRACT(lc.user_ddd,'([0-9]+)', 1) AS user_ddd,
+        lc.user_email,
+        lc.origin_partner,
+        lc.ts_received
+    FROM 
+        datalake_classified_leads_clean.lead_contact AS lc
+    INNER JOIN
+        all_leads_with_replies AS lwr
+            ON lc.id = lwr.id_lead
+    LEFT JOIN 
+        datalake_ebdb_clean.house AS h
+            ON h.id = lc.id_property
+    WHERE
+        lc.business_context = 'SALE'
+        AND CAST(lc.ts_received AS DATE) || lc.id_property != '2021-07-30893359741'
+),
+clean_leads AS (
+    SELECT *,
+        CASE
+            WHEN LENGTH(user_phone_number) IN (11,10) AND SUBSTRING(user_phone_number,1,2) = '55' 
+                THEN '+55' || COALESCE(NULLIF(user_ddd,''),'11') || SUBSTRING(user_phone_number,3)
+            WHEN LENGTH(user_phone_number) IN (11,10) AND SUBSTRING(user_phone_number,1,2) != '55' 
+                THEN '+55' || user_phone_number
+            WHEN LENGTH(user_phone_number) IN (8,9)
+                THEN '+55' || COALESCE(NULLIF(user_ddd,''),'11') || user_phone_number
+            WHEN LENGTH(user_phone_number) < 8 
+                THEN NULL
+            ELSE '+' || user_phone_number
+        END AS clean_phone_number
+    FROM
+        leads AS l
+    WHERE
+        RIGHT(user_phone_number, 7) 
+            NOT IN (
+                '1111111',
+                '9999999',
+                '0000000',
+                '1212121',
+                '3456789',
+                '1234567',
+                '2222222',
+                '3333333',
+                '4444444',
+                '5555555',
+                '6666666',
+                '7777777',
+                '8888888',
+                '9999991'
+            )
+)
+
+SELECT
+    id AS id_classified_lead,
+    id_region,
+    id_house,
+    user_email AS email,
+    clean_phone_number AS phone_number,
+    origin_partner,
+    ts_received AS ts_intent
+FROM
+    clean_leads
