3aea864a112297dd143d67f57438524dfc4c5e74
Thiago Beppe <thiagoabeppe@gmail.com>
2022-02-21 14:27:04 -0300

    core:Adding quintoandarconsultantlists dependency (#7181)

--- bietlejuice/jobs/composer/db/dw/queries/datamarts/for_sale_cross/sale_ongoing_listings.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/for_sale_cross/sale_ongoing_listings.sql
@@ -1,5 +1,5 @@
-with daily_published_listings as (
-select
+WITH daily_published_listings AS (
+SELECT
         f.sk_sale_listing,
         CASE WHEN hp.sk_house IS NOT NULL THEN 1 ELSE 0 END AS is_3p_supply,
         hp.partner AS supply_3p_partner,
@@ -10,36 +10,61 @@ select
         f.sk_status_start_date,
         f.sk_status_end_date,
         f.sk_region,
-        row_number() over(partition by f.sk_sale_listing, d.date order by f.ts_status_started desc) as order_status -- daily order status
-from sale.fact_listing_status f
-join dim_date d
-    on d.sk_date BETWEEN NULLIF(f.sk_status_start_date,-1) 
-    and coalesce(NULLIF(sk_status_end_date, -1), CAST(REPLACE(CAST(current_date AS VARCHAR), '-', '') AS BIGINT) - 1)
-LEFT JOIN datamarts.houses_3p AS hp
-    ON f.sk_sale_listing / 1000 = hp.sk_house
-where f.status_history = 'PUBLISHED'
-)
-, daily_published_listings_with_region as (
-select
+        ROW_NUMBER() OVER(PARTITION BY f.sk_sale_listing, d.date ORDER BY f.ts_status_started DESC) AS order_status -- daily order status
+FROM 
+    sale.fact_listing_status f
+JOIN 
+    dim_date d
+        ON d.sk_date BETWEEN NULLIF(f.sk_status_start_date,-1) 
+        AND COALESCE(NULLIF(sk_status_end_date, -1), CAST(REPLACE(CAST(current_date AS VARCHAR), '-', '') AS BIGINT) - 1)
+LEFT JOIN 
+    datamarts.houses_3p AS hp
+        ON f.sk_sale_listing / 1000 = hp.sk_house
+WHERE 
+    f.status_history = 'PUBLISHED'
+), 
+
+ciq AS (
+SELECT
+    sk_house_listing, 
+    sk_quintoandar_consultant,
+    type_big_agent,
+    businesscontext
+FROM 
+    datamarts.quintoandar_consultant_listings 
+WHERE 
+    businesscontext = 'SALE'
+),
+
+daily_published_listings_with_region AS (
+SELECT
     fhs.sk_sale_listing,
-    fhs.is_3p_supply,
-    fhs.supply_3p_partner,
+    ciq.sk_house_listing AS ciq_assignment,
     fhs.date,
     fhs.week_start,
     fhs.weekday_name,
     fhs.order_status,
     fhs.status_history,
     dr.sk_region,
+    fhs.supply_3p_partner,
+    fhs.is_3p_supply,
     dr.name AS region,
     dr.city_name,
     dr.city_group
-from daily_published_listings fhs
-join dim_region dr
-    on dr.sk_region = fhs.sk_region
-where fhs.order_status = 1
+FROM 
+    daily_published_listings fhs
+JOIN 
+    dim_region dr
+        ON dr.sk_region = fhs.sk_region
+LEFT JOIN 
+    ciq
+        ON ciq.sk_house_listing = fhs.sk_sale_listing
+WHERE 
+    fhs.order_status = 1
 )
-select
-    date,
+
+SELECT
+    DATE,
     weekday_name,
     week_start,
     sk_region,
@@ -48,6 +73,19 @@ select
     city_group,
     supply_3p_partner,
     is_3p_supply,
-    count(distinct sk_sale_listing) as ongoing_listings
-from daily_published_listings_with_region
-group by 1, 2, 3, 4 ,5, 6, 7, 8, 9
+    COUNT(DISTINCT sk_sale_listing) AS ongoing_listings,
+    COUNT(DISTINCT ciq_assignment) AS ciq_listing
+FROM 
+    daily_published_listings_with_region
+GROUP BY 
+    1, 
+    2, 
+    3, 
+    4,
+    5, 
+    6, 
+    7,
+    8,
+    9
+ORDER BY 
+    1
