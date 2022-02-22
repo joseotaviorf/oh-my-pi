c7efeab57567ccfb615a0daebcd03a04e524d9ae
Paulo Andre de Oliveira Carneiro <paulo.carneiro@quintoandar.com.br>
2022-02-22 09:54:33 -0300

    [AES&S]  removing invalid sla targets (#7184)
    * adding new column to taxonomy_sla table and using this column to filter on base_tasks
    * adding logic to dedup taxonomy sla target removing team column and applying distinct

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/taxonomy_sla.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/taxonomy_sla.sql
@@ -6,6 +6,7 @@ SELECT
     codigo_detalhe_do_assunto AS contact_theme_detail_tag,
     CAST(sla_visao_operacao_em_dias_uteis AS FLOAT) AS sla_in_days,
     TO_DATE(data_inicio, 'dd/MM/yyyy') AS dt_start,
-    TO_DATE(data_fim, 'dd/MM/yyyy') AS dt_end
+    TO_DATE(data_fim, 'dd/MM/yyyy') AS dt_end,
+    TO_DATE(invalidado,'dd/MM/yyyy') AS dt_target_invalidated
 FROM
     datalake_gsheets_raw.taxonomy_sla
