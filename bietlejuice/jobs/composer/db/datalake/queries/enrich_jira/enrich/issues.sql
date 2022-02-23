5117f85a9ad06d4a2bb4a1c23232acc5b7db3970
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-02-23 14:55:04 -0300

    [AEBED] fix enrich jira issues (#7210)
    * Fix data cast

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_jira/enrich/issues.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_jira/enrich/issues.sql
@@ -5,7 +5,7 @@ SELECT
     GET_JSON_OBJECT(fields,'$.description') AS issue_description,
     GET_JSON_OBJECT(fields,'$.issuetype.name') AS issue_type,
     GET_JSON_OBJECT(fields,'$.status.name') AS current_status,
-    CAST(GET_JSON_OBJECT(fields,'$.created') AS TIMESTAMP) AS ts_created,
-    CAST(GET_JSON_OBJECT(fields,'$.resolutiondate') AS TIMESTAMP) AS ts_resolved
+    CAST(REPLACE(GET_JSON_OBJECT(fields,'$.created'), '-0300', '') AS TIMESTAMP) AS ts_created,
+    CAST(REPLACE(GET_JSON_OBJECT(fields,'$.resolutiondate'), '-0300', '') AS TIMESTAMP) AS ts_resolved
 FROM 
     datalake_jira_clean.issues
