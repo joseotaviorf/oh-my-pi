bb763ca3c69651d78cf614f7f49d76f5663a7bf4
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-02-23 14:10:06 -0300

    [DFR-1323] Adding some more columns into contract_partnership_data table from EBDB (#7206)
    * Adding some more columns into contract_partnership_data table from EBDB
    
    * uppercase

--- bietlejuice/jobs/composer/db/datalake/queries/ebdb/clean/contract_partnership_data.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/ebdb/clean/contract_partnership_data.sql
@@ -1,11 +1,13 @@
-select
+SELECT
     id,
-    atualizadoEm as ts_updated,
-    criadoEm as ts_created,
-    administrationSplitPercentage as administration_split_percentage,
-    brokerageSplitPercentage as brokerage_split_percentage,
-    contractPlan as contract_plan,
-    contract_id as id_contract,
-    partner_id as id_partner
-from
+    contract_id AS id_contract,
+    partner_id AS id_partner,
+    administrationSplitPercentage AS administration_split_percentage,
+    brokerageSplitPercentage AS brokerage_split_percentage,
+    contractPlan AS contract_plan,
+    partnerType AS partner_type,
+    tradeName AS trade_name,
+    atualizadoEm AS ts_updated,
+    criadoEm AS ts_created
+FROM
     datalake_ebdb_raw.contractpartnershipdata
