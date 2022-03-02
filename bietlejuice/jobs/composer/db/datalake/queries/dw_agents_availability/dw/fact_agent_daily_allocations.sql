14651a26385a1ee74191dae667d085c335144d37
André Rugere Oliveira Jorge <andrerugere@gmail.com>
2022-03-02 16:34:16 -0300

    [AEBED] Migrating fact_agent_daily_allocations from ODS to Composer (#7012)
    * Create DDL for migrated fact_agent_daily_allocations
    
    * Create query for migrated fact_agent_daily_allocations
    
    * Create lineage for migrated fact_agent_daily_allocations

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_agents_availability/dw/fact_agent_daily_allocations.sql
@@ -0,0 +1,55 @@
+WITH agents_slots_daily AS ( -- TODO [ODS] we are following ODS current structure. The table structure should be updated later
+    SELECT
+        sk_agent,
+        sk_slot_date,
+        sk_agent_region,
+        sk_slot_date_agent,
+        id_work_contract,
+        DAYOFWEEK(ts_slot_hour) - 1 AS day_of_week,
+        area,
+        ts_first_visit,
+        SUM(allocated_slots) AS allocated_slots,
+        SUM(allocated_slots_0) AS allocated_slots_0,
+        year,
+        month,
+        day
+    FROM
+        dw_agent.fact_agent_hourly_allocations
+    WHERE
+        DATE(ts_slot_hour) BETWEEN DATE('{year}-{month}-{day}') AND (DATE('{year}-{month}-{day}') + INTERVAL 21 DAYS)
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13
+)
+SELECT
+	asd.sk_agent,
+	asd.sk_slot_date,
+	asd.sk_agent_region,
+	asd.sk_slot_date_agent,
+	asd.id_work_contract,
+	CAST(asd.allocated_slots AS INT) AS allocated_slots,
+	CAST(asd.allocated_slots_0 AS INT) AS allocated_slots_0,
+    (
+        COALESCE(CAST(mwh.has_hours_between_08_and_09_available AS INTEGER), 0)
+        + COALESCE(CAST(mwh.has_hours_between_09_and_10_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_10_and_11_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_11_and_12_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_12_and_13_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_13_and_14_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_14_and_15_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_15_and_16_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_16_and_17_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_17_and_18_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_18_and_19_available AS INTEGER),0)
+        + COALESCE(CAST(mwh.has_hours_between_19_and_20_available AS INTEGER),0)
+    ) * 4 AS max_slots_allocation_available,
+	asd.area,
+	ts_first_visit,
+    asd.year,
+    asd.month,
+    asd.day,
+	NOW() AS ts_load
+FROM 
+    agents_slots_daily asd
+LEFT JOIN 
+    datalake_ebdb_clean.mask_weekly_hour AS mwh
+        ON mwh.id_work_contract  = asd.id_work_contract 
+        AND MOD(mwh.day_of_week, 7) = MOD(asd.day_of_week, 7)
---
+++ bietlejuice/jobs/composer/db/dw/ddl/agent/fact_agent_daily_allocations.ddl
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS agent.fact_agent_daily_allocations;
+CREATE TABLE agent.fact_agent_daily_allocations (
+    sk_agent BIGINT NOT NULL,
+    sk_slot_date BIGINT,
+    sk_agent_region VARCHAR(50),
+    sk_slot_date_agent BIGINT,
+    id_work_contract INTEGER,
+    allocated_slots INTEGER,
+    allocated_slots_0 INTEGER,
+    max_slots_allocation_available INTEGER,
+    area VARCHAR(50),
+    ts_first_visit TIMESTAMP,
+    ts_load TIMESTAMP,
+    CONSTRAINT fact_agent_daily_allocations PRIMARY KEY(sk_slot_date_agent)
+);
+
+ALTER TABLE agent.fact_agent_daily_allocations OWNER TO databricks;
