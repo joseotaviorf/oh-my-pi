7f46e34a58411919086ca2f13fce42323c8981eb
Paulo Andre de Oliveira Carneiro <paulo.carneiro@quintoandar.com.br>
2022-03-07 13:26:38 -0300

    changing heimdall base tasks id and changing cluster to spark3 (#7301)

--- bietlejuice/jobs/composer/dags/enrich_customer_demand/enrich_customer_demand.py
+++ bietlejuice/jobs/composer/dags/enrich_customer_demand/enrich_customer_demand.py
@@ -37,15 +37,11 @@ ATHENA_QUERY_RESULT_LOCATION = config_service.get_config("athena_query_results_b
 
 S3_PREFIX = config_service.get_config("databricks_bietlejuice_repo_path")
 SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
-LOGS_OUTPUT_PATH = config_service.get_config("spark_jobs_logs_path")
 DOC_MD_BASE_URL = config_service.get_config("doc_md_chart_url")
 
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_memory_optimized_cluster", deserialize_json=True
+    "databricks_9_1_med_memory_cluster", deserialize_json=True
 )
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{LOGS_OUTPUT_PATH}{DAG_ID}"
 
 INNER_DEPENDENCIES = {
     "demand_metrics": ["base_tasks"],
