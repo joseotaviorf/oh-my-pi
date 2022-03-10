840a7aff9499e39a969ebf0061a83ad7646e44ab
Samuel Favarin <91481588+samuel-favarin@users.noreply.github.com>
2022-03-10 11:50:27 -0300

    feat: update schedule time (#7345)
    Co-authored-by: Samuel Brati Favarin <samuel.favarin@conectanuvem.com.br>

--- bietlejuice/jobs/composer/dags/classified_leads/classified_leads.py
+++ bietlejuice/jobs/composer/dags/classified_leads/classified_leads.py
@@ -44,7 +44,7 @@ LIBRARIES_DESCRIPTION = Variable.get(
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
 MAIN_START_DATE = datetime(2019, 5, 31, 0, 0, 0, tzinfo=LOCAL_TZ)
-MAIN_SCHEDULE_INTERVAL = "0 6 * * *"
+MAIN_SCHEDULE_INTERVAL = "0 3 * * *"
 
 dag = DAG(
     dag_id=DAG_ID,
