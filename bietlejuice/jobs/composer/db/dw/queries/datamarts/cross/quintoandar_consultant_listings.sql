167685907664785009990f6985702f2792205ce6
carolina-calegare <98416737+carolina-calegare@users.noreply.github.com>
2022-03-17 20:51:17 -0300

    DRB 1061 - update datamart of CIQ - new FS rule (#7370)
    * Update datamart of CIQ - new FS rull
    
    * Create lineage for quintoandar_consultant_listings
    
    Co-authored-by: daniloamaral5A <72883439+daniloamaral5A@users.noreply.github.com>
    Co-authored-by: guilhermeCunha <guilherme.cunha@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/dw/queries/datamarts/cross/quintoandar_consultant_listings.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/cross/quintoandar_consultant_listings.sql
@@ -1,4 +1,155 @@
-WITH check_ciq_full AS (
+WITH first_change_sale AS (
+    SELECT
+        ia.id_house,
+        DATE(MIN(FROM_UNIXTIME(u.ts_revision/ 1000))) AS dt_sale
+    FROM
+        datalake_ebdb_clean_prod.house_aud ia
+    LEFT JOIN 
+        datalake_ebdb_clean_prod.user_revision_entity u
+            ON u.id=ia.rev
+    WHERE 
+        is_for_sale=TRUE
+    GROUP BY 1
+),
+
+first_change_rent AS (
+    SELECT
+        ia.id_house,
+        DATE(MIN(FROM_UNIXTIME(u.ts_revision/ 1000))) AS dt_rent
+    FROM
+        datalake_ebdb_clean_prod.house_aud ia
+    LEFT JOIN 
+        datalake_ebdb_clean_prod.user_revision_entity u
+            ON u.id=ia.rev
+    WHERE 
+        is_for_rent=TRUE
+    GROUP BY 1
+),
+
+base_tempo_rent AS (
+    SELECT dhl.sk_house_listing,
+        ts_status_start,
+        ts_status_end,
+        ts_publication,
+        CASE 
+            WHEN ts_status_start<=DATEADD('day',30,ts_publication) THEN DATEDIFF('day', ts_status_start, 
+                CASE 
+                    WHEN ts_status_end<= DATEADD('day',30,ts_publication) THEN 
+                        CASE 
+                            WHEN sk_status_end_date=-1 THEN GETDATE() 
+                            ELSE ts_status_end 
+                        END 
+                    ELSE DATEADD('day',30,ts_publication) 
+                END ) 
+            ELSE 0 
+        END AS tempo
+    FROM fact_house_listing_status ls
+    LEFT JOIN dim_house_listing dhl
+        ON dhl.sk_house_listing=ls.sk_house_listing
+    WHERE status_history='publicado' or status_history='alugado'
+), 
+
+base_tempo_publicado_rent AS (
+    SELECT 
+        sk_house_listing,
+        SUM(tempo) AS tempo_publicado_for_rent
+    FROM base_tempo_rent
+    GROUP BY 1
+), 
+
+base_tempo_sale AS (
+    SELECT ls.sk_sale_listing,
+        ts_status_started,
+        ts_status_ended,
+        dd.date AS first_publication,
+        CASE 
+            WHEN ts_status_started<=DATEADD('day',30,dd.date) THEN DATEDIFF('day', ts_status_started, 
+                CASE 
+                    WHEN ts_status_ended<= DATEADD('day',30,dd.date) THEN 
+                        CASE 
+                            WHEN sk_status_end_date=-1 THEN GETDATE() 
+                            ELSE ts_status_ended 
+                        END 
+                    ELSE DATEADD('day',30,dd.date) 
+                END ) 
+            ELSE 0 
+        END AS tempo 
+    FROM sale.fact_listing_status ls
+    LEFT JOIN dim_date dd
+        ON dd.sk_date=sk_first_publication_date
+    WHERE status_history in ('PUBLISHED','CCV_SIGNED','SALE_COMPLETED')
+), 
+
+base_tempo_publicado_sale AS (
+    SELECT 
+        sk_sale_listing,
+        SUM(tempo) AS tempo_publicado_for_sale
+    FROM base_tempo_sale
+    GROUP BY 1
+),
+supply_sale AS (
+    SELECT 
+        lf.sk_house_listing, 
+        lf.sk_lead_date, 
+        lf.sk_first_listing_date,
+        lf.mkt_origin
+    FROM 
+        sale.fact_listing_flows lf
+    GROUP BY 1,2,3,4
+
+),
+quintoandar_consultant_listings_sale AS (
+
+    SELECT
+        dl.sk_sale_listing,
+        CAST(JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId') AS BIGINT) AS id_house,
+        CAST(JSON_EXTRACT_PATH_TEXT(Agent.details, 'userExternalId') AS BIGINT) AS sk_quintoandar_consultant,
+        lf.mkt_origin = 'CIQ' AS mkt_origin_ciq,
+        lf.mkt_origin = 'CIQ' AND  program.name='CIQ_FULL' AS is_ciq_origin,
+        CASE
+            WHEN program.name='CIQ_MANAGER' THEN TRUE
+            ELSE FALSE
+        END AS is_account_manager,
+        program.name AS type_big_agent,
+        lbc.business_context AS businesscontext,
+        NULL as businesscontext_detail,
+        Agency.dt_since AS dt_ciq_started,
+        dp.id_partner
+    FROM
+        datalake_big_agent_prod.House
+    LEFT JOIN
+        datalake_big_agent_prod.Agency
+            ON Agency.id_house=House.id
+    LEFT JOIN 
+        datalake_big_agent_prod.enrollment
+            ON enrollment.id=Agency.id_enrollment
+    LEFT JOIN
+        datalake_big_agent_prod.Agent
+            ON agent.id=enrollment.id_agent
+    LEFT JOIN
+        datalake_big_agent_prod.program
+            ON program.id=enrollment.id_program
+    LEFT JOIN
+        sale.dim_listing dl
+            ON dl.sk_house=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
+    LEFT JOIN
+        supply_sale lf
+            ON dl.sk_house = lf.sk_house_listing/1000
+            AND (lf.sk_lead_date>=20210601 or lf.sk_first_listing_date>=20210601)
+    LEFT JOIN 
+        dim_partner_agent dpa
+            ON dpa.id_user=CAST(JSON_EXTRACT_PATH_TEXT(Agent.details, 'userExternalId') AS BIGINT)
+    LEFT JOIN 
+        dim_partner dp
+            ON dp.id_partner=dpa.id_partner
+    INNER JOIN
+        datalake_ebdb_clean_prod.listing_business_context lbc
+            ON lbc.id_house=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
+            AND lbc.business_context='SALE'
+    WHERE (lf.sk_lead_date>=20210601 OR lf.sk_first_listing_date>=20210601)
+    ),
+    
+check_ciq_full AS (
     SELECT
         JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId') AS id_house_external
     FROM
@@ -11,6 +162,7 @@ WITH check_ciq_full AS (
     HAVING
         COUNT(JSON_EXTRACT_PATH_TEXT( House.details, 'houseExternalId'))  > 1
 ),
+
 last_enrollment AS (
     SELECT
         id_house,
@@ -20,8 +172,19 @@ last_enrollment AS (
     GROUP BY 1
 ),
 
-quintoandar_consultant_listings_rent as
-(
+supply_rent AS (
+
+    SELECT
+        mkt_origin,
+        sk_house_listing
+    FROM 
+        fact_house_listing_flows
+    GROUP BY 
+        1,2
+
+),
+
+quintoandar_consultant_listings_rent AS (
     SELECT
         dhl.sk_house_listing,
         CAST(JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId') AS BIGINT) AS id_house,
@@ -29,8 +192,8 @@ quintoandar_consultant_listings_rent as
         lf.mkt_origin = 'CIQ' AS mkt_origin_ciq,
         lf.mkt_origin = 'CIQ' AND  program.name='CIQ_FULL' AS is_ciq_origin,
         CASE
-            WHEN lf.mkt_origin = 'CIQ'  THEN TRUE
-            ELSE program.name='CIQ_MANAGER'
+            WHEN program.name='CIQ_MANAGER' THEN TRUE
+            ELSE FALSE
         END AS is_account_manager,
         CASE
             WHEN ccf.id_house_external IS NOT NULL THEN 'CIQ_FULL'
@@ -62,233 +225,67 @@ quintoandar_consultant_listings_rent as
         dim_house_listing dhl
             ON dhl.id_house=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
     LEFT JOIN
-        fact_house_listing_flows lf
+        supply_rent lf
             ON dhl.id_house = lf.sk_house_listing/1000
     LEFT JOIN
         check_ciq_full ccf
             ON ccf.id_house_external=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
     LEFT JOIN 
         dim_partner_agent dpa
-            on dpa.id_user=CAST(JSON_EXTRACT_PATH_TEXT(Agent.details, 'userExternalId') AS BIGINT)
+            ON dpa.id_user=CAST(JSON_EXTRACT_PATH_TEXT(Agent.details, 'userExternalId') AS BIGINT)
     LEFT JOIN 
         dim_partner dp
-            on dp.id_partner=dpa.id_partner
+            ON dp.id_partner=dpa.id_partner
     INNER JOIN
         datalake_ebdb_clean_prod.listing_business_context lbc
             ON lbc.id_house=JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId')
             AND lbc.business_context='RENT'
     ),
-quintoandar_consultant_listings_sale AS
-(
-    SELECT DISTINCT
-    dl.sk_sale_listing AS sk_house_listing,
-    i.id AS id_house,
-    pa.id_user AS sk_quintoandar_consultant,
-    lf.mkt_origin = 'CIQ' AS mkt_origin_ciq,
-    lf.mkt_origin = 'CIQ' AS is_ciq_origin,
-    CASE WHEN lf.mkt_origin = 'CIQ'  THEN TRUE ELSE false END AS is_account_manager,
-    'CIQ_FULL'AS type_big_agent,
-    'SALE' AS businesscontext,
-    dp.id_partner
-FROM
-    datalake_ebdb_clean_prod.house i
-LEFT JOIN
-    datalake_ebdb_clean_prod.listing_business_context lbc
-        ON i.id=lbc.id_house
-INNER JOIN
-    dim_partner_agent pa
-        ON pa.id_user=i.id_user_registrant
-LEFT JOIN
-    dim_partner dp
-        ON dp.id_partner=pa.id_partner
-LEFT JOIN
-    sale.dim_listing dl
-        ON dl.sk_house=i.id
-LEFT JOIN
-    sale.fact_listing_flows lf
-        ON LEFT(lf.sk_house_listing,9)=i.id
 
-
-WHERE
-    businesscontext='SALE'
-    AND dp.type='AUTONOMOUS_AGENT'
-    AND (lf.sk_lead_date>=20210601 or lf.sk_first_listing_date>=20210601)
-
-ORDER BY 2,1
+quintoandar_consultant_listings_uniao AS (
+    SELECT * FROM quintoandar_consultant_listings_rent
+    UNION ALL
+    SELECT * FROM quintoandar_consultant_listings_sale
 ),
 
-base_forrent AS (
-
-SELECT distinct
-    dhl.id_house,
-    dhl.short_id_house,
-    dhl.house_neighborhood,
-    dhl.house_address,
-    dhl.house_number,
-    dhl.house_zipcode,
-    dhl.house_city,
-    REGEXP_REPLACE(dhl.house_complement,'([^0-9])','') as complement,
-    fhl.sk_owner,
-    du.nome,
-    du.email,
-    du.telefone_principal
-
-FROM
-    dim_house_listing dhl
-LEFT JOIN
-    fact_house_listings fhl
-        ON LEFT(fhl.sk_house_listing,9)=dhl.id_house
-LEFT JOIN
-    dim_user du
-        ON du.id=fhl.sk_owner
-LEFT JOIN
-    dim_region dr
-        ON dr.sk_region=fhl.sk_region
-WHERE
-    city_group in ('RMSP','Rio de Janeiro')
-    AND dhl.status='publicado'
-    AND is_for_sale=FALSE
-    AND is_for_rent=TRUE
-ORDER BY 1,2),
-
-first_rev AS (
-
-SELECT
-    ia.id_house,
-    MIN(rev) AS first_rev
-FROM
-    datalake_ebdb_clean_prod.house_aud ia
-INNER JOIN
-    quintoandar_consultant_listings_sale qcls
-        ON ia.id_house=qcls.id_house
-GROUP BY 1),
-
-dados_cadastro AS (
-SELECT
-    ia.id_house,
-    mod_is_for_rent,
-    mod_is_for_sale,
-    is_for_rent,
-    is_for_sale
-FROM
-    datalake_ebdb_clean_prod.house_aud ia
-INNER JOIN
-    first_rev fr
-        ON fr.id_house=ia.id_house
-        AND ia.rev=fr.first_rev
-),
-
-last_change_user AS (
-
-SELECT
-    CAST(JSON_EXTRACT_PATH_TEXT(House.details, 'houseExternalId') AS BIGINT) AS id_house,
-    Agency.dt_since AS dt_ciq_started
-FROM
-    datalake_big_agent_prod.House
-LEFT JOIN
-    last_enrollment le
-        ON le.id_house=House.id
-LEFT JOIN
-    datalake_big_agent_prod.Agency
-        ON Agency.id_house=le.id_house
-        AND Agency.id_enrollment=le.id_enrollment
-
-),
-
-last_change_sale AS (
-SELECT
-    ia.id_house,
-    MAX(FROM_UNIXTIME(u.ts_revision/ 1000)) as ts_sale
- FROM
-    datalake_ebdb_clean_prod.house_aud ia
-LEFT JOIN 
-    datalake_ebdb_clean_prod.user_revision_entity u
-        ON u.id=ia.rev
-WHERE 
-    mod_is_for_sale=TRUE  
-    AND is_for_sale=TRUE
-GROUP BY  1
-),
 quintoandar_consultant_listings AS (
-(
-SELECT
-   dl.sk_sale_listing  AS sk_house_listing,
-    qcls.id_house,
-    qcls.sk_quintoandar_consultant,
-    lf.mkt_origin = 'CIQ' AS mkt_origin_ciq,
-    lf.mkt_origin = 'CIQ' AS is_ciq_origin,
-    CASE WHEN lf.mkt_origin = 'CIQ'  THEN TRUE ELSE FALSE  END AS is_account_manager,
-    'CIQ_FULL'AS type_big_agent,
-    'SALE' AS businesscontext,
-    CASE
-        WHEN dc.is_for_rent IS TRUE AND dc.is_for_sale IS TRUE THEN 'cadastrado como rent e sale'
-        WHEN dc.is_for_rent IS TRUE AND (lcs.ts_sale>=lcu.dt_ciq_started OR qclr.type_big_agent='CIQ_FULL') THEN 'cadastrado como rent e virou sale'
-        WHEN dc.is_for_sale IS TRUE AND bf.id_house IS NOT NULL AND qcls.id_house<>bf.id_house AND dc.is_for_rent IS FALSE  THEN 'Cadastro de novo Id imovel para ForSale de imovel que já existe em ForRent'
-        WHEN dc.is_for_rent IS FALSE AND dc.is_for_sale IS TRUE THEN 'cadastrado somente como sale'
-        ELSE 'check'
-    END AS businesscontext_detail,
-    null as dt_ciq_started,
-    qcls.id_partner
-FROM
-    quintoandar_consultant_listings_sale qcls
-LEFT JOIN
-    dados_cadastro dc
-        ON dc.id_house=qcls.id_house
-LEFT JOIN
-    datalake_ebdb_clean_prod.house i
-        ON i.id=qcls.id_house
-LEFT JOIN
-    dim_user du
-        ON i.id_user=du.id
-LEFT JOIN
-    base_forrent bf
-        ON i.address=bf.house_address
-        AND i.number=bf.house_number
-        AND du.email=bf.email
-        and REGEXP_REPLACE(i.complement,'([^0-9])','')=bf.complement
-        AND du.email<>'lisboagabrielysantos@gmail.com'
-LEFT JOIN sale.dim_listing dl
-        ON dl.sk_house=qcls.id_house
-LEFT JOIN
-    sale.fact_listing_flows lf
-        ON LEFT(lf.sk_house_listing,9)=qcls.id_house
-LEFT JOIN
-    last_change_user lcu
-        ON lcu.id_house=qcls.id_house
-LEFT JOIN
-    last_change_sale lcs
-        ON lcs.id_house=qcls.id_house
-LEFT JOIN
-    quintoandar_consultant_listings_rent qclr
-        ON qclr.id_house=qcls.id_house
-WHERE
-   lcs.ts_sale>=lcu.dt_ciq_started
-   OR dc.is_for_sale IS TRUE
-   OR qclr.type_big_agent='CIQ_FULL'
-)
-UNION
+    SELECT
+        qclu.sk_house_listing,
+        qclu.id_house,
+        qclu.sk_quintoandar_consultant,
+        qclu.mkt_origin_ciq,
+        qclu.is_ciq_origin,
+        qclu.is_account_manager,
+        qclu.type_big_agent,
+        qclu.businesscontext,
+        CASE 
+            WHEN btpr.tempo_publicado_for_rent>=15 AND btps.tempo_publicado_for_sale>=15 THEN 'hibrido'
+            WHEN btpr.tempo_publicado_for_rent>=15 AND (btps.tempo_publicado_for_sale<15 OR btps.tempo_publicado_for_sale IS NULL) THEN 'rent only'
+            WHEN (btpr.tempo_publicado_for_rent<15 OR btpr.tempo_publicado_for_rent IS NULL) AND btps.tempo_publicado_for_sale>=15 THEN 'sale only'
+            WHEN btpr.tempo_publicado_for_rent<15 AND btps.tempo_publicado_for_sale<15 THEN 'hibrido - menos de 15 dias publicado'
+            WHEN btpr.tempo_publicado_for_rent IS NULL AND btps.tempo_publicado_for_sale IS NULL THEN 'aguardando publicacao'
+            WHEN btpr.tempo_publicado_for_rent<15 AND btps.tempo_publicado_for_sale IS NULL THEN 'rent only - menos que 15 dias'
+            WHEN btpr.tempo_publicado_for_rent IS NULL AND btps.tempo_publicado_for_sale<15 THEN 'sale only - menos que 15 dias'
+            ELSE 'check'
+        END AS businesscontext_detail,
+        qclu.dt_ciq_started,
+        qclu.id_partner,
+        lcs.dt_sale,
+        lcr.dt_rent
+    FROM quintoandar_consultant_listings_uniao qclu
+    LEFT JOIN 
+        first_change_sale lcs
+            ON lcs.id_house=qclu.id_house
+    LEFT JOIN 
+        first_change_rent lcr
+            ON lcr.id_house=qclu.id_house
+    LEFT JOIN 
+        base_tempo_publicado_rent btpr
+            ON btpr.sk_house_listing=qclu.sk_house_listing
+    LEFT JOIN 
+        base_tempo_publicado_sale btps
+            ON btps.sk_sale_listing=qclu.sk_house_listing
 
-(
-SELECT
-    *
-FROM
-    quintoandar_consultant_listings_rent
-)
 )
 
-SELECT
-    sk_house_listing,
-    id_house,
-    sk_quintoandar_consultant,
-    mkt_origin_ciq,
-    is_ciq_origin,
-    is_account_manager,
-    type_big_agent,
-    businesscontext,
-    businesscontext_detail,
-    CAST(dt_ciq_started AS TIMESTAMP) AS dt_ciq_started,
-    id_partner
-FROM
-    quintoandar_consultant_listings
-ORDER BY
-    id_house
+SELECT * FROM quintoandar_consultant_listings
