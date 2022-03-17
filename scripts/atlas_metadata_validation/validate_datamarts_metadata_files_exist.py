4cfd3d01fadebcebfd68774b99e22efce3bff882
Rafael Augusto Monteiro <rafael.augusto@quintoandar.com.br>
2022-03-17 14:02:51 -0300

    [DG] Add a skip list to the datamarts metadata validation script (#7385)
    * Remove metadata validation from forno
    
    * Use correct file path when checking lineage files

--- scripts/atlas_metadata_validation/validate_datamarts_metadata_files_exist.py
+++ scripts/atlas_metadata_validation/validate_datamarts_metadata_files_exist.py
@@ -15,6 +15,135 @@ DATAMARTS_YAML_REGEX = re.compile(
     r"/dags/dw_datamarts/(?P<context>\w+)/(?P<dagname>\w+)_(:?forno|prod)_conf\.(:?yml|yaml)"
 )
 
+# These datamarts were created without lineage due to the
+# datamarts migration.
+SKIP_LIST = {
+    "dw_datamarts/cross": {
+        "talk_to_agent",
+        "lead_listing_flows",
+        "buyer_prospect_status",
+        "quintoandar_consultant_listings",
+    },
+    "dw_datamarts/growth": {
+        "marketplace_intelligence_metrics",
+        "performance_marketing_metrics_supply",
+        "listings_online_metrics",
+        "sale_performance_marketing_metrics_supply_cohort",
+        "plaquinhas_houses",
+        "tof_demand_online_events",
+        "casa_mineira_top_of_funnel_volumes_monthly",
+        "users_online_metrics",
+        "performance_marketing_metrics_imobiliaria_casa_mineira",
+        "market_share_by_sk_region",
+        "performance_marketing_metrics_portal_casa_mineira",
+        "attribution_conversion_paths_demand",
+        "portal_casa_mineira_advertiser_metrics",
+        "buyer_activation_events_combo",
+        "unique_owners",
+        "casa_mineira_top_of_funnel_volumes_weekly",
+        "casa_mineira_top_of_funnel_volumes_daily",
+    },
+    "dw_datamarts/for_rent": {
+        "house_available_hours",
+        "house_weekly_available_hours",
+        "datamart_kpi_weekly",
+        "datamart_cohort_rentals",
+        "house_weekly_entrance_info",
+        "repressed_demand",
+        "ongoing_listed_suspended_listings",
+        "contract_termination",
+        "weekly_demand_metrics",
+        "datamart_opportunity",
+    },
+    "dw_datamarts/for_sale_cross": {
+        "sale_ongoing_listings",
+        "houses_3p",
+        "sale_events_funnel",
+        "temp_supply_flows",
+        "sale_cohort_conversions",
+        "agents_3p",
+    },
+    "dw_datamarts/growth_dep_manual_costs": {
+        "performance_marketing_metrics_demand",
+        "performance_marketing_metrics_supply",
+        "marketing_demand_supply_branding_costs",
+        "sale_performance_marketing_metrics_demand",
+        "marketing_allocated_costs",
+        "sale_performance_marketing_metrics_supply_cohort",
+        "sale_performance_marketing_metrics_supply_coincident",
+        "indicaai_costs",
+        "marketing_campaigns_costs_and_volumes",
+        "unique_performance_marketing_metrics_supply_coincident",
+        "affiliate_acquisition_metrics",
+        "rental_performance_marketing_metrics_supply_coincident",
+    },
+    "dw_datamarts/growth_cross": {
+        "rental_cohort_conversions",
+        "sale_performance_marketing_metrics_demand",
+        "inbound_attendance_leads_flows",
+        "unique_performance_marketing_metrics_supply_coincident",
+        "top_of_funnel_volumes_weekly",
+        "rental_marketplace_flows",
+        "top_of_funnel_volumes_monthly",
+        "tenant_prospect_status",
+        "marketing_allocated_costs",
+        "affiliates_clusters",
+        "efficiency_monitor_sale",
+        "marketing_campaigns_costs_and_volumes",
+        "plaquinhas_users_demand",
+        "affiliate_acquisition_metrics",
+        "plaquinhas_metrics",
+        "unique_supply_events_funnel",
+        "marketing_demand_supply_branding_costs",
+        "house_demand_trends",
+        "top_of_funnel_volumes_daily",
+        "performance_marketing_cluster_promotional_bonus_costs",
+        "tenant_prospects_activations",
+        "rent_flow_interactions",
+        "efficiency_monitor_rental",
+        "rental_performance_marketing_metrics_supply_coincident",
+        "pricing_rent_categorization",
+        "demand_housing_cluster_changes",
+        "performance_marketing_metrics_demand",
+        "unique_supply_cohort_conversions",
+        "performance_marketing_cluster_promotional_bonus",
+        "daily_target_volumes_supply",
+        "user_funnel",
+        "sale_performance_marketing_metrics_supply_coincident",
+        "indicaai_costs",
+    },
+    "dw_datamarts/support_and_service": {
+        "house_media",
+        "customer_sessions_resolution",
+        "repressed_demand_booking_fit_in",
+        "ongoing_refund_requests",
+    },
+    "dw_datamarts/for_sale": {
+        "repressed_demand_sale",
+        "neighborhood_competition",
+        "temp_sale_offers",
+        "temp_autonomous_agents_listings",
+        "sale_agents_weekly_performance",
+        "dim_house_listing_amenities",
+        "sale_ongoing_listings_casa_mineira",
+        "deal_making_funnel",
+        "loft_house_listing",
+        "loft_status_listing_flows",
+        "em_casa_house_listing",
+        "buyer_first_visit_intent",
+        "repressed_demand_sale_booking_fit_in",
+        "buyer_visit_lead_flows",
+        "em_casa_house_listing_flows",
+    },
+    "dw_datamarts/for_rent_cross": {
+        "rental_events_funnel_partners",
+        "user_cohort_rental_demand_conversion",
+        "rental_events_funnel",
+        "credit_proposal_attribute",
+        "rental_demand_events_funnel_flows",
+    },
+}
+
 
 def get_all_datamarts_files():
     return {
@@ -32,6 +161,16 @@ def get_files_from_diff(branch):
 
     git_service = GitService()
     return git_service.get_modified_files_from_diff(from_branch, "HEAD")
+
+
+def table_in_skip_list(intermediate_path, table_name):
+    try:
+        if table_name in SKIP_LIST[intermediate_path]:
+            return True
+        else:
+            return False
+    except KeyError:
+        return False
 
 
 def main():
@@ -74,16 +213,22 @@ def main():
         context = match_dict.get("context")
         dag_name = match_dict.get("dagname")
         if context and dag_name:
-            intermediate_path = f"dw_datamarts/{context}"
+            dag_intermediate_path = f"dw_datamarts/{context}"
+            lineage_intermediate_path = f"dw_datamarts_{context}"
             configs = ConfigurationService(
-                dag_name, intermediate_path=intermediate_path, env="prod"
+                dag_name, intermediate_path=dag_intermediate_path, env="prod"
             )
             for pipeline, pipe_configs in configs.get_config("pipeline").items():
                 table_name = pipe_configs["dw"]["table"]
+                if table_in_skip_list(dag_intermediate_path, table_name):
+                    logger.info(
+                        f"dag={dag_intermediate_path}, table_name={table_name}, msg=Table in skip list"
+                    )
+                    continue
                 if not FileService.metadata_file_exists(
-                    intermediate_path, "dw", table_name
+                    lineage_intermediate_path, "dw", table_name
                 ):
-                    failed.append((intermediate_path, table_name))
+                    failed.append((dag_intermediate_path, table_name))
 
     if failed:
         print("Datamart tables without metadata files:")
