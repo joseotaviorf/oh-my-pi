e7492ccd50cf366a0b952c6321a7861b27424325
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-03-21 17:28:10 -0300

    [AEBED] fix enrich issues records (#7399)
    Fix record multiplicity from datalake_jira.issues

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_jira/enrich/issues.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_jira/enrich/issues.sql
@@ -1,11 +1,29 @@
+WITH record_selection AS (
+    SELECT 
+        key AS id_issue,
+        GET_JSON_OBJECT(fields,'$.parent.key') AS id_parent_issue,
+        GET_JSON_OBJECT(fields,'$.summary') AS summary,
+        GET_JSON_OBJECT(fields,'$.description') AS issue_description,
+        GET_JSON_OBJECT(fields,'$.issuetype.name') AS issue_type,
+        GET_JSON_OBJECT(fields,'$.status.name') AS current_status,
+        CAST(REPLACE(GET_JSON_OBJECT(fields,'$.created'), '-0300', '') AS TIMESTAMP) AS ts_created,
+        GET_JSON_OBJECT(fields,'$.updated') AS ts_updated,
+        CAST(REPLACE(GET_JSON_OBJECT(fields,'$.resolutiondate'), '-0300', '') AS TIMESTAMP) AS ts_resolved,
+        ROW_NUMBER() OVER (PARTITION BY key ORDER BY GET_JSON_OBJECT(fields,'$.updated') DESC) AS row_num
+    FROM 
+        datalake_jira_clean.issues
+)
 SELECT 
-    key AS id_issue,
-    GET_JSON_OBJECT(fields,'$.parent.key') AS id_parent_issue,
-    GET_JSON_OBJECT(fields,'$.summary') AS summary,
-    GET_JSON_OBJECT(fields,'$.description') AS issue_description,
-    GET_JSON_OBJECT(fields,'$.issuetype.name') AS issue_type,
-    GET_JSON_OBJECT(fields,'$.status.name') AS current_status,
-    CAST(REPLACE(GET_JSON_OBJECT(fields,'$.created'), '-0300', '') AS TIMESTAMP) AS ts_created,
-    CAST(REPLACE(GET_JSON_OBJECT(fields,'$.resolutiondate'), '-0300', '') AS TIMESTAMP) AS ts_resolved
-FROM 
-    datalake_jira_clean.issues
+    id_issue,
+    id_parent_issue,
+    summary,
+    issue_description,
+    issue_type,
+    current_status,
+    ts_created,
+    ts_updated,
+    ts_resolved
+FROM
+    record_selection
+WHERE
+    row_num = 1
