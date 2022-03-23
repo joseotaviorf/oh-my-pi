00bf0243ad5c137d0ab5777ef9c02bd722a507c7
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-03-23 14:33:34 -0300

    [AEBED] Adding lineage portal_casa_mineira_advertiser_metrics (#7410)
    * Fixing sql query style guide
    
    * Adding lineage yml for portal_casa_mineira_advertiser_metrics
    
    * Fixing lineage file by removing unnecessary columns

--- bietlejuice/jobs/composer/db/dw/queries/datamarts/growth/portal_casa_mineira_advertiser_metrics.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/growth/portal_casa_mineira_advertiser_metrics.sql
@@ -2,48 +2,57 @@ WITH
 --------------------------------
 ----------STATUS DAILY----------
 --------------------------------
-info_status as (
+info_status AS (
     SELECT
         id_real_estate_agency::INT,
         ts_created,
-        CASE WHEN row_number() OVER (PARTITION BY id_real_estate_agency ORDER BY ts_created) % 2 = 0 THEN 'Active' ELSE 'Inactive' END AS status
-    FROM datalake_casa_mineira_portal_clean_prod.real_estate_agency_history
-    WHERE modified_column = 'desativado_em'
+        CASE WHEN ROW_NUMBER() OVER (PARTITION BY id_real_estate_agency ORDER BY ts_created) % 2 = 0 THEN 'Active' ELSE 'Inactive' END AS status
+    FROM
+        datalake_casa_mineira_portal_clean_prod.real_estate_agency_history
+    WHERE
+        modified_column = 'desativado_em'
     
     UNION ALL
     
     SELECT
         id::INT id_real_estate_agency,
         ts_created,
-        'Active' as status
-    FROM datalake_casa_mineira_portal_clean_prod.real_estate_agency
+        'Active' AS status
+    FROM
+        datalake_casa_mineira_portal_clean_prod.real_estate_agency
 ),
-status_ts as (    
+status_ts AS (    
         SELECT
             id_real_estate_agency,
             status,
             ts_created AS ts_start,
             LEAD(ts_created) OVER(PARTITION BY id_real_estate_agency ORDER BY ts_created) AS ts_end
-        FROM info_status
+        FROM
+            info_status
 ),
-status as (
+status AS (
     SELECT
         date,
         week_start,
         month_start,
-        id_real_estate_agency as advertiser_id,
-        r.real_estate_agency_name as advertiser_name,
-        uf.uf_initials as advertiser_uf,
-        ct.city_name as advertiser_city,
+        id_real_estate_agency AS advertiser_id,
+        r.real_estate_agency_name AS advertiser_name,
+        uf.uf_initials AS advertiser_uf,
+        ct.city_name AS advertiser_city,
         status
-    FROM status_ts
+    FROM
+        status_ts
     JOIN dim_date
-        ON date BETWEEN ts_start::DATE AND COALESCE(ts_end::DATE, CURRENT_DATE) - 1
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.real_estate_agency r
+        ON date BETWEEN ts_start::DATE
+        AND COALESCE(ts_end::DATE, CURRENT_DATE) - 1
+    LEFT JOIN
+        datalake_casa_mineira_portal_clean_prod.real_estate_agency AS r
         ON r.id=status_ts.id_real_estate_agency
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.city AS ct
+    LEFT JOIN
+        datalake_casa_mineira_portal_clean_prod.city AS ct
         ON r.id_city = ct.id
-    LEFT JOIN datalake_casa_mineira_portal_clean_prod.uf AS uf
+    LEFT JOIN
+        datalake_casa_mineira_portal_clean_prod.uf AS uf
         ON ct.id_uf = uf.id
 ),
 --------------------------------
@@ -56,32 +65,39 @@ info_budget AS (
         r.ts_disabled,
         b.budget_value,
         LEAD(b.ts_created) OVER(PARTITION BY b.id_real_estate_agency ORDER BY b.ts_created) AS ts_next_change
-    FROM datalake_casa_mineira_portal_clean_prod.real_estate_agency_budget b
-        JOIN datalake_casa_mineira_portal_clean_prod.real_estate_agency r
-            ON r.id=b.id_real_estate_agency
+    FROM
+        datalake_casa_mineira_portal_clean_prod.real_estate_agency_budget AS b
+    JOIN
+        datalake_casa_mineira_portal_clean_prod.real_estate_agency AS r
+        ON r.id=b.id_real_estate_agency
 ),
-budget as (
+budget AS (
     SELECT
         date,
-        id_real_estate_agency as advertiser_id,
-        budget_value / (DATE_DIFF('DAY', month_start, month_end) + 1)::FLOAT as daily_budget
-    FROM info_budget b
-        JOIN dim_date
-            ON date BETWEEN ts_created::DATE AND COALESCE(ts_next_change::DATE, ts_disabled::DATE, CURRENT_DATE) - 1
+        id_real_estate_agency AS advertiser_id,
+        budget_value / (DATE_DIFF('DAY', month_start, month_end) + 1)::FLOAT AS daily_budget
+    FROM
+        info_budget AS b
+    JOIN dim_date
+    ON date BETWEEN ts_created::DATE AND COALESCE(ts_next_change::DATE, ts_disabled::DATE, CURRENT_DATE) - 1
 ),
 --------------------------------
 ----------LISTINGS DAILY--------
 --------------------------------
-listings as (
-SELECT
-    date,
-    h.id_real_estate_agency::INT as advertiser_id,
-    COUNT(DISTINCT h.id) AS published_listings
-FROM datalake_casa_mineira_portal_clean_prod.house h
-    JOIN dim_date
-        ON date BETWEEN ts_created::DATE AND COALESCE(ts_disabled::DATE, CURRENT_DATE) - 1
-WHERE date BETWEEN DATE_ADD('YEAR', -1, CURRENT_DATE) AND DATE_ADD('DAY', -1, CURRENT_DATE)
-GROUP BY 1,2
+listings AS (
+    SELECT
+        date,
+        h.id_real_estate_agency::INT AS advertiser_id,
+        COUNT(DISTINCT h.id) AS published_listings
+    FROM
+        datalake_casa_mineira_portal_clean_prod.house AS h
+    JOIN
+        dim_date
+        ON date BETWEEN ts_created::DATE
+        AND COALESCE(ts_disabled::DATE, CURRENT_DATE) - 1
+    WHERE
+        date BETWEEN DATE_ADD('YEAR', -1, CURRENT_DATE) AND DATE_ADD('DAY', -1, CURRENT_DATE)
+    GROUP BY 1,2
 )
 ----------------------------------
 -----------FINAL TABLE------------
@@ -97,9 +113,14 @@ SELECT
     s.status,
     SUM(b.daily_budget) AS daily_budget,
     SUM(l.published_listings) AS published_listings
-FROM status s
-LEFT JOIN budget b
-    ON s.date=b.date AND s.advertiser_id=b.advertiser_id
-LEFT JOIN listings l
-    ON s.date=l.date AND s.advertiser_id=l.advertiser_id
+FROM
+    status AS s
+LEFT JOIN
+    budget AS b
+    ON s.date=b.date
+    AND s.advertiser_id=b.advertiser_id
+LEFT JOIN
+    listings AS l
+    ON s.date=l.date
+    AND s.advertiser_id=l.advertiser_id
 GROUP BY 1,2,3,4,5,6,7,8
