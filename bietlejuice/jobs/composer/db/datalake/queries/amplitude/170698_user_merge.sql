8fbac50cd8acb6f6b5eb9abe85d109fadcdc210c
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2022-03-24 16:04:03 -0300

    [AEMKT-540] Create 170698_user_merge clean (#7459)
    * adding tasks to create user_merge clean table

--- bietlejuice/jobs/composer/db/datalake/queries/amplitude/170698_user_merge.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/amplitude/170698_user_merge.sql
@@ -2,14 +2,16 @@ SELECT
     amplitude_id AS id_amplitude,
     merged_amplitude_id AS id_amplitude_merged,
     scope AS id_app,
-    timestamp(merge_time) AS ts_merge,
-    timestamp(merge_server_time) AS ts_server_merge,
-    YEAR(timestamp(merge_time)) AS year,
-    MONTH(timestamp(merge_time)) AS month,
-    DAY(timestamp(merge_time)) AS day
+    timestamp(merge_time/1000) AS ts_merge,
+    timestamp(merge_server_time/1000) AS ts_server_merge,
+    YEAR(timestamp(merge_time/1000)) AS year,
+    MONTH(timestamp(merge_time/1000)) AS month,
+    DAY(timestamp(merge_time/1000)) AS day
 FROM
     datalake_amplitude_raw.170698_user_merge
 WHERE
-    YEAR(TIMESTAMP(merge_time)) = {year}
-    AND MONTH(TIMESTAMP(merge_time)) = {month}
-    AND DAY(TIMESTAMP(merge_time)) = {day}
+    amplitude_id IS NOT NULL
+    AND merge_time IS NOT NULL
+    AND YEAR(timestamp(merge_time/1000)) = {year}
+    AND MONTH(timestamp(merge_time/1000)) = {month}
+    AND DAY(timestamp(merge_time/1000)) = {day}
