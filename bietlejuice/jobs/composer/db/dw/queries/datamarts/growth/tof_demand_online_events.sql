542667f9f164842850a92f5925538b64d88f9f7d
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-03-25 11:42:56 -0300

    [AEBED] Adding lineage tof_demand_online_events (#7414)
    * Fixing sql query style guide
    
    * Adding lineage yml for tof_demand_online_events
    
    * Fixing lineage by removing unnecessary columns
    
    * Update tof_demand_online_events.yml
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/dw/queries/datamarts/growth/tof_demand_online_events.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/growth/tof_demand_online_events.sql
@@ -1,41 +1,46 @@
-with
-last_version_listings as (
-	select
+WITH
+last_version_listings AS (
+	SELECT
 		id_house,
 		sk_house_listing,
-		cast(nullif(cast(ts_listing_version_start as varchar), '') as timestamp) as ts_listing_version_start,
-		cast(nullif(cast(ts_listing_version_end as varchar), '') as timestamp) as ts_listing_version_end
-	from datalake_clean.ods_dim_house_listing
-	where cast(nullif(cast(version as varchar), '') as bigint) > 0
+		CAST(NULLIF(CAST(ts_listing_version_start as varchar), '') AS timestamp) AS ts_listing_version_start,
+		CAST(NULLIF(CAST(ts_listing_version_end as varchar), '') AS timestamp) AS ts_listing_version_end
+	FROM
+		datalake_clean.ods_dim_house_listing
+	WHERE
+		CAST(NULLIF(CAST(version AS varchar), '') AS bigint) > 0
 )
-select
-	date_trunc('week', date(ts_event)) as event_week,
-	date(ts_event) as event_date,
-	ts_event as event_timestamp,
+SELECT
+	DATE_TRUNC('week', DATE(ts_event)) AS event_week,
+	DATE(ts_event) AS event_date,
+	ts_event AS event_timestamp,
 	ev.id_user,
 	ev.id_amplitude,
-	trim(json_extract_scalar(event_properties, '$["house_id"]')) as house_id,
+	TRIM(JSON_EXTRACT_SCALAR(event_properties, '$["house_id"]')) AS house_id,
 	lvl.sk_house_listing,
-	json_extract_scalar(event_properties, '$.agent_id') as agent_id,
-	case when trim(event_type) = 'listing_page_viewed' then 1 else 0 end as listing_page_viewed,
-	case when trim(event_type) = 'pilot_cw_button_clicked' then 1 else 0 end as talk_to_agent_button_clicked,
-	case when trim(event_type) = 'piloto_cw_dialog_viewed' then 1 else 0 end as talk_to_agent_dialog_viewed,
-	case when trim(event_type) = 'piloto_cw_message_sent' then 1 else 0 end as talk_to_agent_message_sent,
-	case when trim(event_type) = 'piloto_cw_message_sent' then substr(regexp_extract(replace(regexp_replace(json_extract_scalar(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{9}))).*'),4) else null end as talk_to_agent_message_content,
-	case when trim(event_type) = 'offer_submitted' then 1 else 0 end as offer_submitted,
-	cast(json_extract_scalar(user_properties, '$.utm_source') as varchar) as utm_source,
-	cast(json_extract_scalar(user_properties, '$.utm_medium') as varchar) as utm_medium,
-	cast(json_extract_scalar(user_properties, '$.utm_campaign') as varchar) as utm_campaign,
-	cast(json_extract_scalar(user_properties, '$.utm_content') as varchar) as utm_content,
-	cast(json_extract_scalar(user_properties, '$.utm_term') as varchar) as utm_term,
+	JSON_EXTRACT_SCALAR(event_properties, '$.agent_id') AS agent_id,
+	CASE WHEN TRIM(event_type) = 'listing_page_viewed' THEN 1 ELSE 0 END AS listing_page_viewed,
+	CASE WHEN TRIM(event_type) = 'pilot_cw_button_clicked' THEN 1 ELSE 0 END AS talk_to_agent_button_clicked,
+	CASE WHEN TRIM(event_type) = 'piloto_cw_dialog_viewed' THEN 1 ELSE 0 END AS talk_to_agent_dialog_viewed,
+	CASE WHEN TRIM(event_type) = 'piloto_cw_message_sent' THEN 1 ELSE 0 END AS talk_to_agent_message_sent,
+	CASE WHEN TRIM(event_type) = 'piloto_cw_message_sent' THEN SUBSTR(REGEXP_EXTRACT(REPLACE(REGEXP_REPLACE(JSON_EXTRACT_SCALAR(event_properties, '$["message_content"]'),'\n',' '),'''',' '),'(?<=(([0-9]{9}))).*'),4) ELSE NULL END AS talk_to_agent_message_content,
+	CASE WHEN TRIM(event_type) = 'offer_submitted' THEN 1 ELSE 0 END AS offer_submitted,
+	CAST(JSON_EXTRACT_SCALAR(user_properties, '$.utm_source') AS varchar) AS utm_source,
+	CAST(JSON_EXTRACT_SCALAR(user_properties, '$.utm_medium') AS varchar) AS utm_medium,
+	CAST(JSON_EXTRACT_SCALAR(user_properties, '$.utm_campaign') AS varchar) AS utm_campaign,
+	CAST(JSON_EXTRACT_SCALAR(user_properties, '$.utm_content') AS varchar) AS utm_content,
+	CAST(JSON_EXTRACT_SCALAR(user_properties, '$.utm_term') AS varchar) AS utm_term,
 	NOW() AS ts_load
-from datalake_amplitude_clean_prod.events ev
-left join last_version_listings lvl
-  on trim(json_extract_scalar(ev.event_properties, '$["house_id"]'))  = cast(lvl.id_house as varchar)
-  and ts_event between ts_listing_version_start and (coalesce(ts_listing_version_end, current_timestamp) - interval '1' second)
-where
-	date(ts_event) >= current_date - interval '45' day
-	and trim(event_type) in (
+FROM
+	datalake_amplitude_clean_prod.events AS ev
+LEFT JOIN
+	last_version_listings AS lvl
+	ON TRIM(JSON_EXTRACT_SCALAR(ev.event_properties, '$["house_id"]'))  = CAST(lvl.id_house AS varchar)
+	AND ts_event BETWEEN ts_listing_version_start
+	AND (COALESCE(ts_listing_version_end, CURRENT_TIMESTAMP) - INTERVAL '1' SECOND)
+WHERE
+	DATE(ts_event) >= CURRENT_DATE - INTERVAL '45' DAY
+	AND TRIM(event_type) IN (
 		'listing_page_viewed',
 		'pilot_cw_button_clicked',
 		'piloto_cw_dialog_viewed',
