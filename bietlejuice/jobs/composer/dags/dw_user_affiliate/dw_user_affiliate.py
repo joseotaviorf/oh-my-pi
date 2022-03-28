f5a2262124695627a788dd53543860b76b086173
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-03-28 16:52:55 -0300

    Updating dw_user_affiliate DAG Owner (#7487)

--- bietlejuice/jobs/composer/dags/dw_user_affiliate/dw_user_affiliate.py
+++ bietlejuice/jobs/composer/dags/dw_user_affiliate/dw_user_affiliate.py
@@ -45,7 +45,7 @@ CLUSTER_DESCRIPTION = Variable.get(
 dag = DAG(
     dag_id=DAG_ID,
     default_args={
-        "owner": DAGOwnerEnum.DATA_FOR_RENT,
+        "owner": DAGOwnerEnum.DATA_GROWTH,
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
