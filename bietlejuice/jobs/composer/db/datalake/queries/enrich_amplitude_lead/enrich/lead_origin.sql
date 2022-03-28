b9a594e162b73ecdd0ab0289f6fa7e9373473b7e
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-03-28 15:20:07 -0300

    Revert "columns in datalake_amplitude_clean were renamed to match their versions in Athena/ec2, so we need to remap them in this query (#7479)" (#7489)
    This reverts commit 2fc1fe8a8ea26c463410b6375710a8056fa914d8.
    
    Co-authored-by: Lucas Fonseca <lucas.fonseca@quintoandar.com.br>

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_amplitude_lead/enrich/lead_origin.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_amplitude_lead/enrich/lead_origin.sql
@@ -41,61 +41,61 @@ WITH app_205027 AS (
 ),
 app_183047 AS (
     SELECT
-        ep_formfield_lead_uuid AS formfield_lead_uuid,
+        formfield_lead_uuid,
         event_properties,
         ts_event,
-        up_utm_campaign AS utm_campaign,
-        up_utm_medium AS utm_medium,
-        up_utm_source AS utm_source,
-        up_utm_content AS utm_content,
-        up_utm_term AS utm_term,
-        up_platform AS app_type,
-        up_referring_domain AS referring_domain,
+        utm_campaign,
+        utm_medium,
+        utm_source,
+        utm_content,
+        utm_term,
+        app_type,
+        referring_domain,
         region,
         city,
         uuid
     FROM
         datalake_amplitude_clean.183047_lead_form_submitted_events
     WHERE
-        ep_formfield_lead_uuid IS NOT NULL
+        formfield_lead_uuid IS NOT NULL
     UNION
     SELECT
-        ep_formfield_lead_uuid AS formfield_lead_uuid,
+        formfield_lead_uuid,
         event_properties,
         ts_event,
-        up_utm_campaign AS utm_campaign,
-        up_utm_medium AS utm_medium,
-        up_utm_source AS utm_source,
-        up_utm_content AS utm_content,
-        up_utm_term AS utm_term,
-        up_platform AS app_type,
-        up_referring_domain AS referring_domain,
+        utm_campaign,
+        utm_medium,
+        utm_source,
+        utm_content,
+        utm_term,
+        app_type,
+        referring_domain,
         region,
         city,
         uuid
     FROM
         datalake_amplitude_clean.183047_price_suggestion_form_submitted_events
     WHERE
-        ep_formfield_lead_uuid IS NOT NULL
+        formfield_lead_uuid IS NOT NULL
     UNION
     SELECT
-        ep_formfield_lead_uuid AS formfield_lead_uuid,
+        formfield_lead_uuid,
         event_properties,
         ts_event,
-        up_utm_campaign AS utm_campaign,
-        up_utm_medium AS utm_medium,
-        up_utm_source AS utm_source,
-        up_utm_content AS utm_content,
-        up_utm_term AS utm_term,
-        up_platform AS app_type,
-        up_referring_domain AS referring_domain,
+        utm_campaign,
+        utm_medium,
+        utm_source,
+        utm_content,
+        utm_term,
+        app_type,
+        referring_domain,
         region,
         city,
         uuid
     FROM
         datalake_amplitude_clean.183047_price_suggestion_sale_form_submitted_events
     WHERE
-        ep_formfield_lead_uuid IS NOT NULL
+        formfield_lead_uuid IS NOT NULL
 ),
 prep_ref AS (
     select
