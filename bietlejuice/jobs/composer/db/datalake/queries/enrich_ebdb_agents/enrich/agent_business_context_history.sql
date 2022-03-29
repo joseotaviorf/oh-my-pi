a6856430847780f8f50c9f9f81c476c2a6b17e2f
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-03-29 12:52:48 -0300

    [DFR-1383] reviewing our logic to assign context duration to an agent (#7512)
    * Enhance our logic to assign agent context duration using both rev_types

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_agents/enrich/agent_business_context_history.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_agents/enrich/agent_business_context_history.sql
@@ -1,28 +1,24 @@
 WITH agent_business_context_aud AS (
     SELECT
-        id_agent_data,
+        aud.id_agent_data,
         aud.rev_type,
         aud.business_context,
-        FROM_UNIXTIME(CAST(ure.ts_revision AS BIGINT)/1000) AS ts_updated,
-        TRUNC(FROM_UNIXTIME(CAST(ure.ts_revision AS BIGINT)/1000), 'day') AS dt_updated
+        LEAD(FROM_UNIXTIME(ure.ts_revision/1000)) OVER (PARTITION BY aud.id_agent_data, aud.business_context ORDER BY rev) AS ts_agent_business_context_ended,
+        FROM_UNIXTIME(ure.ts_revision/1000) AS ts_updated
     FROM
         datalake_ebdb_clean.agent_data_business_contexts_served_aud AS aud
     LEFT JOIN
         datalake_ebdb_clean.user_revision_entity AS ure
             ON ure.id = aud.rev
-),
-agent_business_context_history AS (
-    SELECT
-        id_agent_data,
-        business_context AS agent_business_context,
-        ts_updated AS ts_agent_business_context_started,
-        LEAD(ts_updated) OVER (PARTITION BY id_agent_data ORDER BY ts_updated) AS ts_agent_business_context_ended
-    FROM
-        agent_business_context_aud
     WHERE
-        rev_type <> 2
+        aud.rev_type IN (0,2)
 )
 SELECT
-    *
+    id_agent_data,
+    business_context AS agent_business_context,
+    ts_updated AS ts_agent_business_context_started,
+    ts_agent_business_context_ended
 FROM
-    agent_business_context_history
+    agent_business_context_aud
+WHERE
+    rev_type <> 2
