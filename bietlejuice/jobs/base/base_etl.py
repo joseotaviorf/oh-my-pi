ae836ab625e7c583d76077a489275460e3678ff8
Rafael Ribaldo <ribaldorafael@gmail.com>
2022-04-13 09:26:42 -0300

    Update base_etl.py

--- bietlejuice/jobs/base/base_etl.py
+++ bietlejuice/jobs/base/base_etl.py
@@ -24,6 +24,8 @@ import yaml
 data_acc_aws_access_key_id = os.environ.get('DATA_AWS_ACCESS_KEY_ID')
 data_acc_aws_secret_access_key = os.environ.get('DATA_AWS_SECRET_ACCESS_KEY')
 
+print("base_etl: using {} as aws key id".format(data_acc_aws_access_key_id[14:])
+
 
 class BaseETL(object):
     def __init__(self, *args, **kwargs):
