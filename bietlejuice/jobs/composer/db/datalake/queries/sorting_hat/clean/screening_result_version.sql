949fe25370bb43fb07c4ff97e532d0965e144f00
Fernando Gomes <101211208+ferctos-5a@users.noreply.github.com>
2022-04-13 12:00:03 -0300

    change screening_result_version cleaning table source (#7637)

--- bietlejuice/jobs/composer/db/datalake/queries/sorting_hat/clean/screening_result_version.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/sorting_hat/clean/screening_result_version.sql
@@ -1,9 +1,15 @@
-select
-    id,
-    screening_result_id as id_screening_result,
-    score,
-    risk_category,
-    liquidity,
-    proposal_id as id_proposal,
-    timestamp(versioned_at) as ts_versioned
-from datalake_sorting_hat_raw.screeningresultversion
+SELECT
+  id AS id_screening_result,
+  end_transaction_id AS id_end_transaction,
+  transaction_id AS id_transaction,
+  proposal_id AS id_proposal,
+  liquidity,
+  operation_type,
+  risk_category,
+  score,
+  proposal_id_mod AS mod_id_proposal,
+  liquidity_mod AS mod_liquidity,
+  risk_category_mod AS mod_risk_category,
+  score_mod AS mod_score
+FROM 
+  datalake_sorting_hat_raw.screeningresult_version
