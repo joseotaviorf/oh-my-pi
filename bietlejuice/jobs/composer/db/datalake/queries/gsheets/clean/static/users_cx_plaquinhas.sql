1aed91a34c9e058708aa9ebd6fcc3d9781e10975
André Rugere Oliveira Jorge <andrerugere@gmail.com>
2022-04-20 08:55:45 -0300

    [AEBED] Hotfix - Changing back users_cx_plaquinhas Gsheets column names(#7678)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/users_cx_plaquinhas.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/users_cx_plaquinhas.sql
@@ -2,13 +2,13 @@ SELECT
     id_house,
     id_user,
     atendimento_plaquinha,
-    bairro AS neighborhood,
-    canal AS channel,
-    conhecia_5a AS knew_5a_previously,
-    conhecia_5a_2 AS knew_5a_previously_2,
+    bairro,
+    canal,
+    conhecia_5a,
+    conhecia_5a_2,
     email,
-    motivo AS reason,
+    motivo,
     has_visit_booked,
-    TO_TIMESTAMP(data_hora, 'MM/dd/yyyy HH:mm:ss') AS ts_input
+    TO_TIMESTAMP(data_hora, 'MM/dd/yyyy HH:mm:ss') AS data_hora
 FROM
     datalake_gsheets_raw.users_cx_plaquinhas
