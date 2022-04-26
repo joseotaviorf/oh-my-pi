80b8a5407c7ddaf3fea99a0f9802bd073a5fed11
Fernando Gomes <101211208+ferctos-5a@users.noreply.github.com>
2022-04-26 17:39:58 -0300

    update cleaning process for rental guarantee risk_category table (#7733)
    * Add new source columns on risk_category cleaning query
    * Add new source columns on risk_category cleaning lineage

--- bietlejuice/jobs/composer/db/datalake/queries/rental_guarantee/clean/risk_category.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/rental_guarantee/clean/risk_category.sql
@@ -1,12 +1,17 @@
 SELECT 
-    id,  
-    category_description,
-    category_level, 
-    deposit_factor,
-    factor AS guarantee_factor,
-    allow_deposit AS is_deposit_allowed,
-    allow_guarantee AS is_guarantee_allowed,
-    created_at AS ts_created,
-    updated_at AS ts_updated
+  id,  
+  category_description,
+  category_level, 
+  deposit_factor,
+  factor AS guarantee_factor,
+  standalone_factor,
+  allow_deposit AS is_deposit_allowed,
+  allow_guarantee AS is_guarantee_allowed,
+  allow_pro_guarantor AS is_pro_guarantor_allowed,
+  allow_standalone AS is_standalone_allowed,
+  created_at AS ts_created,
+  disabled_at AS ts_disabled,
+  enabled_at AS ts_enabled,
+  updated_at AS ts_updated
 FROM 
-    datalake_rental_guarantee_raw.risk_category
+  datalake_rental_guarantee_raw.risk_category
