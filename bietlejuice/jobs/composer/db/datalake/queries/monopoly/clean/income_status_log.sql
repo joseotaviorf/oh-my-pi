4cf09f77c3c12dedeea5caf9037b8f72f2d8f3f9
André Rugere Oliveira Jorge <andrerugere@gmail.com>
2022-04-26 09:21:18 -0300

    [AEBED] Adding new columns to income_status_log (#7722)
    We need to add two more columns to income_status_log table on the Monopoly’s clean layer.

--- bietlejuice/jobs/composer/db/datalake/queries/monopoly/clean/income_status_log.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/monopoly/clean/income_status_log.sql
@@ -4,7 +4,9 @@ SELECT
     status,
     gateway_response,
     amount,
+    expiration_date AS dt_expiration,
     due_date AS dt_due,
+    occurrence_date AS ts_occurrence,
     created_at AS ts_created
 FROM
     datalake_monopoly_raw.income_status_log
