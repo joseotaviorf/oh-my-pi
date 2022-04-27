ff51e58f8e599fa62b7a75ea60a54a446866ae92
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2022-04-27 16:29:55 -0300

    Add enrich online_attribution to dependencies file

--- bietlejuice/jobs/composer/dags/enrich_online_attribution/enrich_online_attribution.py
+++ bietlejuice/jobs/composer/dags/enrich_online_attribution/enrich_online_attribution.py
@@ -39,7 +39,7 @@ PARTITION_COLS = config_service.get_config("partition_cols")
 
 BASE_SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_memory_optimized_cluster", deserialize_json=True
+    "databricks_9_1_med_memory_cluster", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
     "destination"
