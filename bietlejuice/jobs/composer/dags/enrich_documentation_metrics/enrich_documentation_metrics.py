1028d146bbe2c149fec9caccbc2349af90f36468
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2022-05-05 18:58:20 -0300

    pause dag enrich_metadata_propagator. (#7816)

--- bietlejuice/jobs/composer/dags/enrich_documentation_metrics/enrich_documentation_metrics.py
+++ bietlejuice/jobs/composer/dags/enrich_documentation_metrics/enrich_documentation_metrics.py
@@ -53,7 +53,6 @@ dag = DAG(
         "depends_on_past": False,
     },
     start_date=MAIN_START_DATE,
-    schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
         chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
