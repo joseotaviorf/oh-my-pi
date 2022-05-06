1f08715e381bdfa7f3da4a8783e4abb42ae7f2d8
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-05-06 13:23:24 -0300

    Fixed spark_jobs_path (#7822)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/enrich_casa_mineira_marketing_manual_daily_costs/enrich_casa_mineira_marketing_manual_daily_costs.py
+++ bietlejuice/jobs/composer/dags/enrich_casa_mineira_marketing_manual_daily_costs/enrich_casa_mineira_marketing_manual_daily_costs.py
@@ -35,6 +35,8 @@ databricks_bietlejuice_repo_path = config_service.get_config(
 spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
+
 # cluster setup
 CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = spark_jobs_logs_path
@@ -68,7 +70,7 @@ datalake_task_group = DatalakeTaskGroup(
     env=ENV,
     datalake_bucket=datalake_bucket,
     relative_query_path=DAG_NAME,
-    spark_jobs_path=spark_jobs_logs_path,
+    spark_jobs_path=BASE_SPARK_JOBS_PATH,
     athena_query_result_location=athena_query_results_bucket,
 )
 
