03a76e596615f570df3fac58a93873b60c9d0698
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-05-09 14:59:29 -0300

    [AEFR-71] Updating incremental condition in agents_activations enriched table (#7804)
    * Updating incremental load in agents_activations
    
    * Updating casts
    
    * Updating query

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_agents/enrich/agents_activations_suspensions_contracts_changes.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_agents/enrich/agents_activations_suspensions_contracts_changes.sql
@@ -7,30 +7,15 @@ WITH agent_work_contract_revision AS (
         rev,
         rev_type,
         is_active AS is_agent_active,
-        LAG(is_active) OVER (PARTITION BY awc.id ORDER BY rev ASC) AS is_previous_active,
-        CASE
-            WHEN id_work_contract != LAG(id_work_contract) OVER (PARTITION BY awc.id ORDER BY rev ASC) THEN TRUE
-            ELSE FALSE -- check if it's okay to attribute false for nulls
-        END AS has_changed_work_contract,
-        CASE
-            WHEN is_active <>
-                CASE
-                    WHEN LAG(is_active) OVER (PARTITION BY awc.id ORDER BY rev ASC) IS NULL THEN FALSE
-                    ELSE LAG(is_active) OVER (PARTITION BY awc.id ORDER BY rev ASC)
-                END
-            THEN TRUE
-        END AS has_changed_activated,
-        FROM_UNIXTIME(revision.ts_revision/1000) AS ts_revision,
-        YEAR(FROM_UNIXTIME(revision.ts_revision/1000)) AS year,
-        MONTH(FROM_UNIXTIME(revision.ts_revision/1000)) AS month,
-        DAY(FROM_UNIXTIME(revision.ts_revision/1000)) AS day
+        COALESCE(LAG(is_active) OVER (PARTITION BY awc.id ORDER BY rev ASC), FALSE) AS is_previous_active,
+        COALESCE(id_work_contract != LAG(id_work_contract) OVER (PARTITION BY awc.id ORDER BY rev ASC), FALSE) AS has_changed_work_contract,
+        is_active != COALESCE(LAG(is_active) OVER (PARTITION BY awc.id ORDER BY rev ASC), FALSE) AS has_changed_activated,
+        TIMESTAMP(FROM_UNIXTIME(revision.ts_revision/1000)) AS ts_revision
     FROM
         datalake_ebdb_clean.agent_data_aud AS awc
     INNER JOIN
         datalake_ebdb_clean.user_revision_entity AS revision 
             ON awc.rev = revision.id
-    WHERE
-        DATE(FROM_UNIXTIME(revision.ts_revision/1000)) = DATE('{year}-{month}-{day}')
 ),
 agent_work_contract_info AS (
     SELECT
@@ -57,10 +42,7 @@ agent_work_contract_info AS (
         awc.is_previous_active,
         awc.has_changed_work_contract,
         awc.has_changed_activated,
-        awc.ts_revision,
-        awc.year,
-        awc.month,
-        awc.day
+        awc.ts_revision
     FROM
         agent_work_contract_revision AS awc
     LEFT JOIN
@@ -80,33 +62,42 @@ agent_work_contract_info AS (
             ON at.id_agent_data = agent.id_agent
     WHERE
         has_changed_work_contract = TRUE OR has_changed_activated = TRUE
+),
+agent_action AS (
+    SELECT
+        id AS id_agent,
+        id_work_contract,
+        id_previous_work_contract,
+        id_user_change,
+        rev,
+        rev_type,
+        work_contract_name,
+        previous_work_contract_name,
+        agent_name,
+        agent_type,
+        user_change_name,
+        user_change_email,
+        CASE
+            WHEN action = 'ACTIVATED' THEN
+                CASE
+                    WHEN LAG(rev) OVER (PARTITION BY id, action ORDER BY rev ASC) IS NOT NULL THEN 'REACTIVATED'
+                    ELSE 'ACTIVATED' END
+            ELSE action
+        END AS action,
+        is_agent_active,
+        is_previous_active,
+        has_changed_work_contract,
+        has_changed_activated,
+        ts_revision
+    FROM
+        agent_work_contract_info
 )
 SELECT
-    id AS id_agent,
-    id_work_contract,
-    id_previous_work_contract,
-    id_user_change,
-    rev,
-    rev_type,
-    work_contract_name,
-    previous_work_contract_name,
-    agent_name,
-    agent_type,
-    user_change_name,
-    user_change_email,
-    CASE
-        WHEN action = 'ACTIVATED' THEN
-            CASE
-                WHEN LAG(rev) OVER (PARTITION BY id, action ORDER BY rev ASC) IS NOT NULL THEN 'REACTIVATED'
-                ELSE 'ACTIVATED' END
-        ELSE action
-    END AS action,
-    is_agent_active,
-    has_changed_work_contract,
-    has_changed_activated,
-    ts_revision,
-    year,
-    month,
-    day
+    *,
+    YEAR(ts_revision) AS year,
+    MONTH(ts_revision) AS month,
+    DAY(ts_revision) AS day
 FROM
-    agent_work_contract_info
+    agent_action
+WHERE
+    DATE(ts_revision) = DATE('{year}-{month}-{day}')
