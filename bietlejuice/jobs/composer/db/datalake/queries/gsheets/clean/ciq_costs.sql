f4faee45686d02d42af34ad330d7882e3fca14d9
Henrique Akira Miamoto <56843327+akiramiamoto@users.noreply.github.com>
2022-05-10 15:08:01 -0300

    [GROW-234] Adjusting CIQ Costs (#7845)
    * adjusting ciq costs
    
    * casting float
    
    * removing float cast at datamart file
    
    * removind replace
    
    * removing nullifs

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/ciq_costs.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/ciq_costs.sql
@@ -1,18 +1,18 @@
 SELECT
     NULLIF(city, '') AS city,
-    NULLIF(comission_cs_ciq_full, '') AS comission_cs_ciq_full,
-    NULLIF(comission_cs_ciq_manager, '') AS comission_cs_ciq_manager,
-    NULLIF(comission_listing, '') AS comission_listing,
-    NULLIF(comission_listing_fs, '') AS comission_listing_fs,
-    NULLIF(data, '')::DATE AS data,
-    NULLIF(impostos, '') AS impostos,
-    NULLIF(month, '') AS month,
-    NULLIF(other, '') AS other,
-    NULLIF(revshare_cs_ciq_full, '') AS revshare_cs_ciq_full,
-    NULLIF(revshare_cs_ciq_manager, '') AS revshare_cs_ciq_manager,
-    NULLIF(total_costs_ciq_full_for_rent, '') AS total_costs_ciq_full_for_rent,
-    NULLIF(total_costs_ciq_full_for_sale, '') AS total_costs_ciq_full_for_sale,
-    NULLIF(week_start, '') AS week_start,
-    NULLIF(year, '') AS year
+    FLOAT(NULLIF(comission_cs_ciq_full, '')) AS comission_cs_ciq_full,
+    FLOAT(NULLIF(comission_cs_ciq_manager, '')) AS comission_cs_ciq_manager,
+    FLOAT(NULLIF(comission_listing, '')) AS comission_listing,
+    FLOAT(NULLIF(comission_listing_fs, '')) AS comission_listing_fs,
+    FLOAT(NULLIF(impostos, '')) AS impostos,
+    FLOAT(NULLIF(other, '')) AS other,
+    FLOAT(NULLIF(revshare_cs_ciq_full, '')) AS revshare_cs_ciq_full,
+    FLOAT(NULLIF(revshare_cs_ciq_manager, '')) AS revshare_cs_ciq_manager,
+    FLOAT(NULLIF(total_costs_ciq_full_for_rent, '')) AS total_costs_ciq_full_for_rent,
+    FLOAT(NULLIF(total_costs_ciq_full_for_sale, '')) AS total_costs_ciq_full_for_sale,
+    DATE(data) AS data,
+    DATE(week_start) AS week_start,
+    NULLIF(year, '') AS year,
+    NULLIF(month, '') AS month
 FROM
     datalake_gsheets_raw.ciq_costs
--- bietlejuice/jobs/composer/db/dw/queries/datamarts/growth_cross/marketing_demand_supply_branding_costs.sql
+++ bietlejuice/jobs/composer/db/dw/queries/datamarts/growth_cross/marketing_demand_supply_branding_costs.sql
@@ -805,26 +805,26 @@ supply_ciq_cost AS (
 ),
 supply_ciq_cost_comission AS (
     SELECT
-        NULLIF(data, '')::DATE AS dt_cost,
+        data AS dt_cost,
         co.city AS city_group,
         'Rental' AS business,
         'Supply' AS planning_mkt_level1,
         'Affiliates' AS planning_mkt_level2,
         'CIQ' AS planning_mkt_level3,
-        SUM(REPLACE(NULLIF(co.total_costs_ciq_full_for_rent, ''), ',', '')::FLOAT) AS costs
+        SUM(co.total_costs_ciq_full_for_rent) AS costs
     FROM
         datalake_gsheets_clean_prod.ciq_costs AS co
     GROUP BY 1,2,3,4,5,6
 ),
 supply_ciq_cost_comission_sale AS (
     SELECT
-        NULLIF(data, '')::DATE AS dt_cost,
+        data AS dt_cost,
         co.city AS city_group,
         'Sale' AS business,
         'Supply' AS planning_mkt_level1,
         'Affiliates' AS planning_mkt_level2,
         'CIQ' AS planning_mkt_level3,
-        SUM(REPLACE(NULLIF(co.total_costs_ciq_full_for_sale, ''), ',', '')::FLOAT) AS costs
+        SUM(co.total_costs_ciq_full_for_sale) AS costs
     FROM
         datalake_gsheets_clean_prod.ciq_costs AS co
     WHERE
