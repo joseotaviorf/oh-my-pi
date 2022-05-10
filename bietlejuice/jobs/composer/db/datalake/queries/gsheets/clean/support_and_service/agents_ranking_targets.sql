5bac8f585ba3c363d5bae4f692667eca507b1153
Paulo Andre de Oliveira Carneiro <paulo.carneiro@quintoandar.com.br>
2022-05-10 13:14:28 -0300

    [AES&S] agents ranking targets sheet (#7842)
    updating agents ranking targets sheet

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/support_and_service/agents_ranking_targets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/support_and_service/agents_ranking_targets.sql
@@ -3,10 +3,11 @@ SELECT
     team,
     CAST(target_resolution AS FLOAT) AS target_resolution,
     CAST(target_csat AS FLOAT) AS target_csat,
-    CAST(target_frt AS FLOAT) AS target_frt,
+    CAST(target_sla AS FLOAT) AS target_sla,
     CAST(productivity_target AS INT) AS productivity_target,
-    CAST(ra_would_back_make_business_target AS FLOAT) AS ra_would_back_make_business_target,
+    CAST(ra_would_do_business_again_target AS FLOAT) AS ra_would_do_business_again_target,
     CAST(ra_target_score AS INT) AS ra_target_score,
+    CAST(ra_target_solution_rate AS INT) AS ra_target_solution_rate,
     DATE(start_date) AS dt_start,
     DATE(end_date) AS dt_end
 FROM
