5e9b561965a3c43c3a268bc512b4a372aa971fcc
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2022-05-10 13:59:30 -0300

    [AES&S] Enrich Customer Resolution - FCR Simulation by Team (#7837)
    * Updated enrich customer_resolution.sql with new FCR metrics
    
    * Updated customer_resolution.yml adding historical tables and ticket_recontact_list column
    
    * Updated CASE WHEN for CTE user_recontacts
    
    * Removed historical tables from front_tickets CTE and fixed SQL sintaxe
    
    * Removed historical tables from customer_reoslution.yml file
    
    * Replaced ts_ended with ts_closed in the customer_resolution.yml file
    
    * Updated filter SQL style
    
    * Removed timestamp convertion in user_recontacts CTE
    
    * Updated enrich_customer_resolution
    
    * Updated customer_resolution enrich SQL
    
    * Added gsheets clean department_control to dependencies config file
    
    * Updated SQL enrich customer resolutuion with FCR by Team
    
    * Updated dependency for enrich_customer_resolution
    
    * Updated lineage file
    
    * Adopted Attendance view to consider only inbound tickets in recontact analysis

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_customer_resolution/enrich/customer_resolution.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_customer_resolution/enrich/customer_resolution.sql
@@ -2,143 +2,153 @@ WITH front_tickets AS (
   SELECT
     id_ticket,
     MAX(id_user) AS id_user,
-    last_back_ticket AS back_ticket,
+    "call" AS ticket_channel,
+    dc.team AS team,
     direction,
     status,
-    CASE
-      WHEN is_solved = TRUE
-          AND (back_ticket_list IS NULL OR is_open_back_ticket = FALSE)
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN TRUE
-      WHEN is_solved IS NOT NULL
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN FALSE
-      ELSE NULL
-    END AS is_solved,
-    ts_ticket_started,
-    ts_ticket_ended
-  FROM 
-    datalake_customer_support.call 
-  WHERE 
-    front_or_back = 'front'
-    AND id_user > -1
+    ts_ticket_started AS ts_started,
+    ts_ticket_ended AS ts_closed
+  FROM
+    datalake_customer_support.call AS cl
+  INNER JOIN
+    datalake_gsheets_clean.department_control AS dc
+      ON cl.last_department = dc.department
+  WHERE
+    id_user > -1
+    AND ts_ticket_started IS NOT NULL
+    AND dc.team IS NOT NULL
+    AND cl.tags NOT LIKE '%atendimento_escalado%'
+    AND (
+      cl.front_or_back = 'front'
+      OR cl.front_or_back IS NULL
+    )
+    AND (
+      direction = 'inbound'
+      OR direction IS NULL
+    )
   GROUP BY 1,3,4,5,6,7,8
-  UNION ALL 
+  UNION ALL
   SELECT
     id_ticket,
     MAX(id_user) AS id_user,
-    last_back_ticket AS back_ticket,
+    "chat" AS ticket_channel,
+    dc.team AS team,
     "inbound" AS direction,
     status,
-    CASE
-      WHEN is_solved = TRUE
-          AND (back_ticket_list IS NULL OR is_open_back_ticket = FALSE)
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN TRUE
-      WHEN is_solved IS NOT NULL
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN FALSE
-      ELSE NULL
-    END AS is_solved,
-    ts_ticket_started,
-    ts_ticket_ended
-  FROM 
-    datalake_customer_support.chat 
-  WHERE 
-    front_or_back = 'front'
-    AND id_user > -1
+    ts_ticket_started AS ts_started,
+    ts_ticket_ended AS ts_closed
+  FROM
+    datalake_customer_support.chat AS ch
+  INNER JOIN
+    datalake_gsheets_clean.department_control AS dc
+      ON ch.last_department = dc.department
+  WHERE
+    id_user > -1
+    AND ts_ticket_started IS NOT NULL
+    AND dc.team IS NOT NULL
+    AND ch.tags NOT LIKE '%atendimento_escalado%'
+    AND (
+      ch.front_or_back = 'front'
+      OR ch.front_or_back IS NULL
+    )
   GROUP BY 1,3,4,5,6,7,8
-  UNION ALL 
+  UNION ALL
   SELECT
     id_ticket,
     MAX(id_user) AS id_user,
-    last_back_ticket AS back_ticket,
+    "email" AS ticket_channel,
+    dc.team AS team,
     direction,
     status,
-    CASE
-      WHEN is_solved = TRUE
-          AND (back_ticket_list IS NULL OR is_open_back_ticket = FALSE)
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN TRUE
-      WHEN is_solved IS NOT NULL
-          AND is_bot = FALSE
-          AND is_closed_by_merge = FALSE
-          AND (front_or_back = 'front' OR front_or_back IS NULL)
-      THEN FALSE
-      ELSE NULL
-    END AS is_solved,
-    ts_ticket_started,
-    ts_ticket_ended
-  FROM 
-    datalake_customer_support.email 
-  WHERE 
-    front_or_back = 'front'
-    AND id_user > -1
+    ts_ticket_started AS ts_started,
+    ts_ticket_ended AS ts_closed
+  FROM
+    datalake_customer_support.email AS em
+  INNER JOIN
+    datalake_gsheets_clean.department_control AS dc
+      ON em.department = dc.department
+  WHERE
+    id_user > -1
+    AND ts_ticket_started IS NOT NULL
+    AND dc.team IS NOT NULL
+    AND em.tags NOT LIKE '%atendimento_escalado%'
+    AND (
+      em.front_or_back = 'front'
+      OR em.front_or_back IS NULL
+    )
+    AND (
+      direction = 'inbound'
+      OR direction IS NULL
+    )
   GROUP BY 1,3,4,5,6,7,8
 ),
 user_recontacts AS (
-  SELECT DISTINCT 
-    id_ticket,
-    id_user,
+  SELECT DISTINCT
+    ft.id_ticket,
+    ft.id_user,
+    ft.team,
     CASE
-      WHEN ((BIGINT(TO_TIMESTAMP(ts_ticket_started)) - BIGINT(TO_TIMESTAMP(LAG(ts_ticket_started, 1) OVER(PARTITION BY id_user ORDER BY ts_ticket_started))))/(3600)) < 168 THEN 1
+      WHEN ((BIGINT(ts_started) - BIGINT(LAG(ts_started, 1) OVER(PARTITION BY id_user, team ORDER BY ts_started)))/(3600)) < 168 THEN 1
       ELSE 0
     END AS is_recontact,
     CASE
-      WHEN ((BIGINT(TO_TIMESTAMP(ts_ticket_started)) - BIGINT(TO_TIMESTAMP(LAG(ts_ticket_started, 1) OVER(PARTITION BY id_user ORDER BY ts_ticket_started))))/(3600)) < 168 THEN NULL
+      WHEN ((BIGINT(ts_started) - BIGINT(LAG(ts_started, 1) OVER(PARTITION BY id_user, team ORDER BY ts_started)))/(3600)) < 168 THEN NULL
       ELSE id_ticket
     END AS main_ticket,
-    ts_ticket_started
+    ft.ts_started
   FROM
-    front_tickets 
+    front_tickets AS ft
 ),
 ticket_sessions AS (
   SELECT
     id_ticket,
-    MAX(main_ticket) OVER(PARTITION BY id_user ORDER BY ts_ticket_started ASC ROWS UNBOUNDED PRECEDING) AS main_ticket,
+    MAX(main_ticket) OVER(PARTITION BY id_user, team ORDER BY ts_started ASC ROWS UNBOUNDED PRECEDING) AS ticket_session,
     id_user,
-    ts_ticket_started
+    is_recontact,
+    ts_started
   FROM
     user_recontacts
 ),
 tickets_by_ticket_session AS (
   SELECT
-    main_ticket,
+    ticket_session,
     id_user,
     COUNT(DISTINCT id_ticket) AS total_tickets,
-    MIN(ts_ticket_started) AS ts_started
+    MIN(ts_started) AS ts_started
+  FROM
+    ticket_sessions
+  GROUP BY 1,2
+),
+ticket_recontact_list AS (
+  SELECT
+    ticket_session,
+    id_user,
+    COLLECT_LIST(id_ticket) AS ticket_recontact_list
   FROM
     ticket_sessions
   GROUP BY 1,2
 )
 SELECT
-    tts.main_ticket AS id_ticket,
+    tts.ticket_session AS id_ticket,
     tts.id_user,
+    ft.ticket_channel,
     tts.total_tickets,
+    team,
     CASE
-        WHEN tts.total_tickets > 1
-            OR ft.back_ticket IS NOT NULL
-            OR is_solved = False
-        THEN FALSE
-        ELSE TRUE
+      WHEN tts.total_tickets > 1 THEN FALSE
+      ELSE TRUE
     END AS is_fcr,
-    ft.ts_ticket_started,
-    ft.ts_ticket_ended
-FROM 
+    trl.ticket_recontact_list,
+    ft.ts_started,
+    ft.ts_closed
+FROM
     tickets_by_ticket_session AS tts
-LEFT JOIN 
+LEFT JOIN
+    ticket_recontact_list AS trl
+      ON tts.ticket_session = trl.ticket_session
+      AND tts.id_user = trl.id_user
+LEFT JOIN
     front_tickets AS ft
-        ON ft.id_ticket = tts.main_ticket
+      ON ft.id_ticket = tts.ticket_session
 WHERE
-    (direction = 'inbound' OR direction IS NULL)
-    AND status = 'closed'
+    status = 'closed'
