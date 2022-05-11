02dec899cf120ad1d9c3d5ea424a99cdf502571e
Fernando Gomes <101211208+ferctos-5a@users.noreply.github.com>
2022-05-11 15:13:37 -0300

    [AEFIN-65] implement max_records_per_file strategy on arquivo_confidencial dag (#7860)
    * implement max_records_per_file strategy on arquivo_confidencial dag full loads

--- bietlejuice/jobs/composer/dags/arquivo_confidencial/spark_jobs/load_full_arquivo_confidencial_into_datalake.py
+++ bietlejuice/jobs/composer/dags/arquivo_confidencial/spark_jobs/load_full_arquivo_confidencial_into_datalake.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
             s3_path=f"{database_location}{table_name}",
             format_options=format_options,
             database_location=database_location,
+            max_records_per_file=100000,
         )
         spark_metastore_loader.update_metastore(
             df, database_name, table_name, format_options, database_location
