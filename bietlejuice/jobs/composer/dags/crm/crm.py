a0184a54bac8ab578231883d94bfce4f522620ce
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2022-05-12 18:47:48 -0300

    Refactor: Updated DAG Owner for S&S Dags (#7881)

--- bietlejuice/jobs/composer/dags/crm/crm.py
+++ bietlejuice/jobs/composer/dags/crm/crm.py
@@ -8,7 +8,7 @@ from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
-from bietlejuice.jobs.composer.base.airflow import BaseDAG
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, DAGOwnerEnum
 from bietlejuice.jobs.composer.base.pipeline import LayerEnum
 from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
 from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
@@ -45,7 +45,7 @@ CONFIGS_FILE_PATH = f"{os.path.dirname(os.path.realpath(__file__))}/crm.config"
 dag = DAG(
     dag_id=DAG_ID,
     default_args={
-        "owner": BaseDAG.DEFAULT_OWNER,
+        "owner": DAGOwnerEnum.DATA_SS,
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
