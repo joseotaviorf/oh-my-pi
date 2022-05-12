03ef104cab68fa9c44b79085dd55463e3a7db6d7
Rodrigo Moroni <32406306+RMoroni@users.noreply.github.com>
2022-05-11 22:39:17 -0300

    [DFORSALE-1541] Source Validation Suite (#7861)
    Create a validate suite for (three) "For Sale" sources inside the PostgresValidationSuite

--- bietlejuice/jobs/composer/validation_suites/databases/postgres_validation_suite.py
+++ bietlejuice/jobs/composer/validation_suites/databases/postgres_validation_suite.py
@@ -14,3 +14,21 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
             self.auth[DatabaseEnum.DW], SparkClient()
         )
         self._validate_connection(db_consumer)
+
+    def validate_sales_flow(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.SALES_FLOW], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_classified_leads(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.CLASSIFIED_LEADS], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_hub_services(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.HUB_SERVICES], SparkClient()
+        )
+        self._validate_connection(db_consumer)
