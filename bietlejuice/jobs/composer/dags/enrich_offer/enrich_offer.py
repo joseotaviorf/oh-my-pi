a6bc7148442fa762c303eefed7a396ee99fb5478
Samuel Favarin <91481588+samuel-favarin@users.noreply.github.com>
2022-05-16 16:29:09 -0300

    [DFORSALE 385] Update Enrich Offer Cluster (#7911)
    * Perf: change cluster
    
    * fix: resolve lint
    
    Co-authored-by: GabriellRodrigues <gabriel.firmino.dxt@ext.quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/enrich_offer/enrich_offer.py
+++ bietlejuice/jobs/composer/dags/enrich_offer/enrich_offer.py
@@ -32,7 +32,9 @@ LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
     Variable.get("databricks_s3_bucket"), DAG_ID
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_med_general_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 LIBRARIES_DESCRIPTION = Variable.get(
