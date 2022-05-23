edf0d86292315d91c7efcb0393b5fd7fcd72ea68
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-05-23 14:15:12 -0300

    [AEFR-144] Adding rental datasources validations (Postgres) (#7957)
    * adding suite validation to postgres consumer of renal context

--- bietlejuice/jobs/composer/validation_suites/databases/postgres_validation_suite.py
+++ bietlejuice/jobs/composer/validation_suites/databases/postgres_validation_suite.py
@@ -9,9 +9,27 @@ from bietlejuice.jobs.composer.consumers.db_consumers import PostgresConsumer
 class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
     REPOSITORY_CONSUMER_CLASS = PostgresConsumer
 
+    def validate_big_agent(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.BIG_AGENT], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_bob(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.BOB], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
     def validate_classified_leads(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.CLASSIFIED_LEADS], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_docx(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.DOCX], SparkClient()
         )
         self._validate_connection(db_consumer)
 
@@ -21,15 +39,33 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
         )
         self._validate_connection(db_consumer)
 
+    def validate_godfather(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.GODFATHER], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
     def validate_hub_services(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.HUB_SERVICES], SparkClient()
         )
         self._validate_connection(db_consumer)
 
+    def validate_metabase(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.METABASE], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
     def validate_monopoly(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.MONOPOLY], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_oscar(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.OSCAR], SparkClient()
         )
         self._validate_connection(db_consumer)
 
@@ -42,6 +78,12 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
     def validate_redshift(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.DW], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_rental_guarantee(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.RENTAL_GUARANTEE], SparkClient()
         )
         self._validate_connection(db_consumer)
 
@@ -63,8 +105,32 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
         )
         self._validate_connection(db_consumer)
 
+    def validate_signatures(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.SIGNATURES], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_sorting_hat(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.SORTING_HAT], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_terminator(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.TERMINATOR], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
     def validate_trato_feito(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.TRATO_FEITO], SparkClient()
+        )
+        self._validate_connection(db_consumer)
+
+    def validate_wololo(self):
+        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
+            self.auth[DatabaseEnum.WOLOLO], SparkClient()
         )
         self._validate_connection(db_consumer)
