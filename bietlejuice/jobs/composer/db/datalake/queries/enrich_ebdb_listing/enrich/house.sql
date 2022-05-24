134b113be1aa5cf9d92fb8bf4d65f8f49ec779b2
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-05-24 09:44:49 -0300

    [AEFR-140] Adding country info to House table from enrich_ebdb_listing (#7952)
    * Adding country info to enrich_ebdb_listing.house table
    
    * id_country column
    
    * Removing join with region
    
    * Updating enrich_ebdb_listing.house query and lineages
    
    * missing comma

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_listing/enrich/house.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_listing/enrich/house.sql
@@ -33,27 +33,37 @@ house_aud as (
     mod_status = 1
   group by 1
 )
-select
+SELECT
   h.id,
-  h.id % 892700000 as house_short_id,
+  COALESCE(ct.id, region.id_country) AS id_country,
+  h.id_state,
+  h.id_region,
+  h.id_user,
+  h.id_user_registrant,
+  h.id_external,
+  h.id_condo_parent,
+  h.id % 892700000 AS house_short_id,
   h.rent,
+  COALESCE(ct.code, region.country_code) AS country_code,
+  state.abbreviation AS state_abbreviation,
+  state.name AS state_name,
+  m_region.city_name AS region_city_name,
+  m_region.macro_name AS region_macro_name,
+  m_region.name AS region_name,
+  h.lat,
+  h.lng,
   h.neighborhood,
   h.zipcode,
   h.city,
+  h.address,
   h.complement,
   h.condo,
-  h.id_condo_parent,
-  h.dt_built,
-  h.has_elevator,
+  condo.name AS condo_name,
+  local.name AS closest_station_name,
   h.contact_email,
   h.photo_session_email,
-  h.address,
   h.conservation_status,
-  h.does_photographer_fill_information,
   h.iptu,
-  h.lat,
-  h.lng,
-  h.is_furnished,
   h.finishing_level,
   h.number,
   h.bathrooms,
@@ -61,7 +71,6 @@ select
   h.suites,
   h.parking_slots,
   h.register_percent_complete,
-  h.has_session_photos_email_preference,
   h.status,
   h.contact_phone_1,
   h.contact_phone_2,
@@ -69,34 +78,19 @@ select
   h.type,
   h.doorman_type,
   h.parking_slot_type,
-  h.is_verified,
-  h.id_user,
   h.promotion_code,
-  h.is_quote_insurance_needed,
   h.total_value,
-  h.dt_expiration,
-  h.dt_first_publication,
-  h.has_owner_incomplete_listing_notification,
-  h.has_admin_incomplete_listing_notification,
   h.short_url,
   h.insurance_value,
   h.booking_instructions,
   h.plan_accepted,
-  h.has_price_adjustment_suggestion,
-  h.has_rent_sign_permission,
   h.lead_description,
   h.listing_highlight_priority,
-  h.corretorAmigo as corretor_amigo, -- partners program name
-  h.porteiroAmigo as porteiro_amigo, -- partners program name
+  h.corretorAmigo AS corretor_amigo, -- partners program name
+  h.porteiroAmigo AS porteiro_amigo, -- partners program name
   h.condo_management_information,
   h.references,
-  h.is_condo_price_included,
-  h.has_manual_coordinates,
-  h.is_iptu_included,
-  h.is_follow_up_owner_upload_photos,
-  h.ts_first_verified,
   h.lead_collision_type,
-  h.ts_agent_lead_payment_calculated,
   h.cover_image_name,
   h.geo_hash,
   h.listing_type,
@@ -104,158 +98,165 @@ select
   h.internal_admin_info,
   NULLIF(REGEXP_EXTRACT(h.internal_admin_info, '(?<=\\[3P\\-)(.+?)(?=\\])'), '') AS partner_3p_supply,
   h.photo_booking_historic,
-  h.ts_affiliate_lead_payment_calculated,
-  h.has_service_bathroom,
-  h.has_service_bedroom,
   h.default_neighborhood,
   h.condo_type,
   h.iptu_type,
-  h.dt_availability_end,
   h.compare_price_with_average,
-  h.is_warn_owner_of_visits_needed,
-  h.is_listing_notifying_owner_email_sent,
   h.house_conversion_status,
   h.registration,
   h.last_confirmation_email_sent,
   h.lat_lng,
-  h.ts_last_index_update,
   h.title,
-  h.ts_suspension_end,
-  h.id_external,
-  h.is_in_negotiation,
-  h.is_in_external_negotiation,
-  h.ts_external_negotiation_start,
   h.cardiff_rejection_reason,
-  h.ts_recapture,
-  h.is_photographer_job_pending,
-  h.has_requested_professional_photos,
-  h.ts_last_confirmation_availability,
   h.registry,
   h.penalization_score,
-  h.is_receive_default_contract_copy_needed,
   h.rank_score,
-  h.ts_last_publication,
-  h.is_visit_information_confirmed,
-  h.id_region,
-  h.dt_creation,
-  h.ts_updated,
-  h.id_user_registrant,
   h.total_area,
   h.land_area,
   h.predicted_price,
   h.sale_price,
+  o_type.name AS occupant_type,
+  k_type.name AS key_type,
+  a_a_type.name AS key_location,
+  r_type.name AS visit_restriction,
+  hrs.registration_abandoned_reason AS registration_abandoned_reason,
+  CASE
+    WHEN h.status <> 'despublicado' THEN NULL
+    WHEN h.unpublished_reason IS NOT NULL THEN h.unpublished_reason
+    WHEN ure.reason = 'Imóvel indisponível' THEN 'HOUSE_NOT_AVAILABLE'
+    WHEN ure.reason = '[Auto] Proprietario confirmou indisponibilidade' THEN 'AUTO_OWNER_CONFIRMED_UNAVAILABILITY'
+    WHEN ure.reason LIKE '[AUTO]%[RESCISAO]%' THEN 'AUTO_CONTRACT_END_DEPUBLICATION'
+    WHEN LOWER(ure.reason) LIKE '%n%o concord%' THEN 'OWNER_DOESNT_AGREE'
+    WHEN ure.reason LIKE '%Desisti%' THEN 'OWNER_GAVE_UP_RENTING'
+    WHEN ure.reason LIKE '%MissedNegotiations%' THEN 'AUTO_OWNER_MISSED_NEGOTIATIONS_LIMIT_REACHED'
+    WHEN ure.reason LIKE 'Imóvel alugado direto%'
+      OR ure.reason LIKE '%Fechei com outro%' THEN 'OWNER_RENTING_DIRECT_WITH_TENANT'
+    WHEN ure.reason LIKE 'Aluguei por curta%' THEN 'OWNER_RENTING_FOR_SHORT_PERIOD'
+    WHEN ure.reason = 'Imóvel alugado com imobiliária tradicional'
+      OR ure.reason = 'Já aluguei o imóvel' THEN 'OWNER_RENTING_WITH_OTHER_COMPANY'
+    WHEN ure.reason = 'Vou vender o imóvel'
+      OR ure.reason LIKE '%vendeu o %' THEN 'OWNER_SELLING_HOUSE'
+    WHEN LOWER(ure.reason) LIKE '%duplicado%' THEN 'DUPLICATED_HOUSE'
+    WHEN ure.reason LIKE '%falta de confirmação%'
+      OR ure.reason LIKE '%falta de contato%'
+      OR ure.reason LIKE 'PP não responde%' THEN 'HOUSE_NOT_REACHABLE'
+    WHEN ure.reason = 'Despublicado pelo proprietário via App: Estou reformando' THEN 'APP_HOME_RENOVATION'
+    WHEN ure.reason = 'Despublicado pelo proprietário via App: Outro motivo' THEN 'APP_OTHER_REASON'
+    WHEN ure.reason = 'Despublicado pelo proprietário via App: Já aluguei' THEN 'APP_OWNER_RENTING_WITH_OTHER_COMPANY'
+    WHEN ure.reason = 'Usuário despublicou pelo app.' THEN 'APP_USER_DEPUBLISHED'
+    WHEN ure.reason LIKE 'Usuário rejeitou%' THEN 'USER_REJECTED_TERMS'
+    WHEN ure.reason IS NULL THEN 'UNKNOWN_NULL_VALUE'
+    ELSE 'OTHER'
+  END AS unpublished_reason,
+  h.has_elevator,
+  h.is_furnished,
+  h.has_session_photos_email_preference,
+  h.does_photographer_fill_information,
+  COALESCE(special_condition.is_exclusive, FALSE) AS is_exclusive,
+  COALESCE(visit_info.is_quintoandar_authorized, FALSE) AS is_quintoandar_authorized,
+  h.is_quote_insurance_needed,
+  h.is_verified,
+  h.is_in_negotiation,
+  h.is_in_external_negotiation,
+  h.is_condo_price_included,
+  h.has_manual_coordinates,
+  h.is_iptu_included,
+  h.is_follow_up_owner_upload_photos,
+  h.is_warn_owner_of_visits_needed,
+  h.is_listing_notifying_owner_email_sent,
+  h.is_photographer_job_pending,
+  h.is_receive_default_contract_copy_needed,
+  h.is_visit_information_confirmed,
   h.is_for_rent,
   h.is_for_sale,
   COALESCE(h.internal_admin_info LIKE '%[3P-%]%', FALSE) AS is_3p_supply,
-  (coalesce(h.announced_by, h.id_announced_by)
-    is not null) as is_imovel_v3,
-  state.abbreviation as state_abbreviation,
-  state.name as state_name,
-  m_region.city_name as region_city_name,
-  m_region.macro_name as region_macro_name,
-  m_region.name as region_name,
-  condo.name as condo_name,
-  local.name as closest_station_name,
-  o_type.name as occupant_type,
-  k_type.name as key_type,
-  a_a_type.name as key_location,
-  r_type.name as visit_restriction,
-  coalesce(r_type.name = 'Restriction', false) as has_visit_restriction,
-  hrs.registration_abandoned_reason as registration_abandoned_reason,
-  case
-    when h.status <> 'despublicado'
-      then NULL
-    when h.unpublished_reason is not null
-      then h.unpublished_reason
-    when ure.reason = 'Imóvel indisponível'
-      then 'HOUSE_NOT_AVAILABLE'
-    when ure.reason = '[Auto] Proprietario confirmou indisponibilidade'
-      then 'AUTO_OWNER_CONFIRMED_UNAVAILABILITY'
-    when ure.reason like '[AUTO]%[RESCISAO]%'
-      then 'AUTO_CONTRACT_END_DEPUBLICATION'
-    when lower(ure.reason) like '%n%o concord%'
-      then 'OWNER_DOESNT_AGREE'
-    when ure.reason like '%Desisti%'
-      then 'OWNER_GAVE_UP_RENTING'
-    when ure.reason like '%MissedNegotiations%'
-      then 'AUTO_OWNER_MISSED_NEGOTIATIONS_LIMIT_REACHED'
-    when ure.reason like 'Imóvel alugado direto%'
-      or ure.reason like '%Fechei com outro%'
-      then 'OWNER_RENTING_DIRECT_WITH_TENANT'
-    when ure.reason like 'Aluguei por curta%'
-      then 'OWNER_RENTING_FOR_SHORT_PERIOD'
-    when ure.reason = 'Imóvel alugado com imobiliária tradicional'
-      or ure.reason = 'Já aluguei o imóvel'
-      then 'OWNER_RENTING_WITH_OTHER_COMPANY'
-    when ure.reason = 'Vou vender o imóvel'
-      or ure.reason like '%vendeu o %'
-      then 'OWNER_SELLING_HOUSE'
-    when lower(ure.reason) like '%duplicado%'
-      then 'DUPLICATED_HOUSE'
-    when ure.reason like '%falta de confirmação%'
-      or ure.reason like '%falta de contato%'
-      or ure.reason like 'PP não responde%'
-      then 'HOUSE_NOT_REACHABLE'
-    when ure.reason = 'Despublicado pelo proprietário via App: Estou reformando'
-      then 'APP_HOME_RENOVATION'
-    when ure.reason = 'Despublicado pelo proprietário via App: Outro motivo'
-      then 'APP_OTHER_REASON'
-    when ure.reason = 'Despublicado pelo proprietário via App: Já aluguei'
-      then 'APP_OWNER_RENTING_WITH_OTHER_COMPANY'
-    when ure.reason = 'Usuário despublicou pelo app.'
-      then 'APP_USER_DEPUBLISHED'
-    when ure.reason like 'Usuário rejeitou%'
-      then 'USER_REJECTED_TERMS'
-    when ure.reason is null
-      then 'UNKNOWN_NULL_VALUE'
-    else 'OTHER'
-  end as unpublished_reason,
-  coalesce(special_condition.is_exclusive, false) as is_exclusive,
-  coalesce(visit_info.has_entry_authorization, false) as has_entry_authorization,
-  coalesce(visit_info.is_owner_joinning, false) as is_owner_joinning,
-  coalesce(visit_info.is_quintoandar_authorized, false) as is_quintoandar_authorized,
-  coalesce(visit_info.need_owner_authorization, false) as need_owner_authorization,
-  coalesce(visit_info.has_key_box, false) as has_key_box,
-  coalesce(io.is_enabled, false) as has_instant_offer_enabled
-from
-  datalake_ebdb_clean.house h
-left join
+  (COALESCE(h.announced_by, h.id_announced_by) IS NOT NULL) AS is_imovel_v3,
+  COALESCE(r_type.name = 'Restriction', FALSE) AS has_visit_restriction,
+  h.has_requested_professional_photos,
+  h.has_owner_incomplete_listing_notification,
+  h.has_admin_incomplete_listing_notification,
+  h.has_price_adjustment_suggestion,
+  h.has_rent_sign_permission,
+  h.has_service_bathroom,
+  h.has_service_bedroom,
+  COALESCE(visit_info.has_entry_authorization, FALSE) AS has_entry_authorization,
+  COALESCE(visit_info.is_owner_joinning, FALSE) AS is_owner_joinning,
+  COALESCE(visit_info.need_owner_authorization, FALSE) AS need_owner_authorization,
+  COALESCE(visit_info.has_key_box, FALSE) AS has_key_box,
+  COALESCE(io.is_enabled, FALSE) AS has_instant_offer_enabled,
+  h.dt_built,
+  h.dt_expiration,
+  h.dt_first_publication,
+  h.dt_availability_end,
+  h.dt_creation,
+  h.ts_first_verified,
+  h.ts_suspension_end,
+  h.ts_last_index_update,
+  h.ts_last_confirmation_availability,
+  h.ts_agent_lead_payment_calculated,
+  h.ts_affiliate_lead_payment_calculated,
+  h.ts_external_negotiation_start,
+  h.ts_recapture,
+  h.ts_last_publication,
+  h.ts_updated
+FROM
+  datalake_ebdb_clean.house AS h
+LEFT JOIN
   datalake_ebdb_clean.state
-    on state.id = h.id_state
-left join
-  datalake_ebdb_clean.map_region m_region
-    on h.id_region = coalesce(m_region.id,
+    ON state.id = h.id_state
+LEFT JOIN
+  datalake_ebdb_clean.country AS ct
+    ON ct.id = state.id_country
+LEFT JOIN
+  datalake_ebdb_clean.map_region AS m_region
+    ON h.id_region = COALESCE(m_region.id,
                               m_region.id_macro,
                               m_region.id_city)
-left join
+-- As we have several ids on House that has id_state null and would end up with no country info,
+-- this join between map_region and region attributes a country using the respective id_region
+LEFT JOIN
+  datalake_region.region
+    ON region.id = m_region.id
+LEFT JOIN
   datalake_ebdb_clean.condo
-    on condo.id = h.id_condo_parent
-left join
+    ON condo.id = h.id_condo_parent
+LEFT JOIN
   datalake_ebdb_clean.local
-    on local.id = h.id_closest_station
-left join datalake_ebdb_clean.access_type a_type
-  on a_type.id_house = h.id
+    ON local.id = h.id_closest_station
+LEFT JOIN
+  datalake_ebdb_clean.access_type AS a_type
+    ON a_type.id_house = h.id
 -- who lives in the house
-left join datalake_ebdb_clean.occupant_type o_type
-  on a_type.id_occupant = o_type.id
+LEFT JOIN
+  datalake_ebdb_clean.occupant_type AS o_type
+    ON a_type.id_occupant = o_type.id
 -- key types (e.g., password, biometric, etc.)
-left join datalake_ebdb_clean.key_type k_type
-  on a_type.id_type = k_type.id
+LEFT JOIN
+  datalake_ebdb_clean.key_type AS k_type
+    ON a_type.id_type = k_type.id
 -- where is the key (e.g., owner, lockbox, etc.)
-left join datalake_ebdb_clean.access_authorization_type a_a_type
-  on a_type.id_authorization = a_a_type.id
-left join datalake_ebdb_clean.restriction_type r_type
-  on a_type.id_restriction = r_type.id
+LEFT JOIN
+  datalake_ebdb_clean.access_authorization_type AS a_a_type
+    ON a_type.id_authorization = a_a_type.id
+LEFT JOIN
+  datalake_ebdb_clean.restriction_type AS r_type
+    ON a_type.id_restriction = r_type.id
 -- abandoned reason
-left join datalake_ebdb_clean.house_registration_status hrs
-  on h.id = hrs.id_house
-left join special_condition
-  on special_condition.id_house = h.id
-left join visit_info
-  on visit_info.id_house = h.id
-left join house_aud
-	on house_aud.id_house = h.id
-left join datalake_ebdb_clean.user_revision_entity ure
-	on house_aud.rev = ure.id
-left join datalake_ebdb_clean.instant_offer io
-    on h.id = io.id_house
+LEFT JOIN
+  datalake_ebdb_clean.house_registration_status AS hrs
+    ON h.id = hrs.id_house
+LEFT JOIN
+  special_condition
+    ON special_condition.id_house = h.id
+LEFT JOIN
+  visit_info
+    ON visit_info.id_house = h.id
+LEFT JOIN
+  house_aud
+	ON house_aud.id_house = h.id
+LEFT JOIN
+  datalake_ebdb_clean.user_revision_entity AS ure
+	ON house_aud.rev = ure.id
+LEFT JOIN
+  datalake_ebdb_clean.instant_offer AS io
+    ON h.id = io.id_house
