9c3836e757882fc7fb04bec8de2c059b9fd85c0a
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-05-30 09:08:19 -0300

    [AEBED] Fixing reverse_tracksale DAG (#8021)

--- bietlejuice/jobs/composer/dags/reverse_tracksale/spark_jobs/load_incremental_data_into_datalake_reverse.py
+++ bietlejuice/jobs/composer/dags/reverse_tracksale/spark_jobs/load_incremental_data_into_datalake_reverse.py
@@ -1,6 +1,7 @@
 import logging
 from argparse import ArgumentParser
-from pyspark.sql.functions import when, lit, col
+from pyspark.sql.functions import when, count, lit, col
+from pyspark.sql.window import Window as w
 from datetime import datetime, timedelta
 
 from quintoandar_logger import QuintoAndarLogger
@@ -57,66 +58,65 @@ if __name__ == "__main__":
     )
     query = FileService.get_query_from_file_name(query_path)
 
-    dfq = spark_client.get_records(query)
+    df = spark_client.get_records(query)
 
     execution_date = datetime.strptime(execution_date, "%Y-%m-%d") + timedelta(days=1)
 
     path = f"{datalake_bucket}/{campaign_query}/year={execution_date.year}/month={execution_date.month}/day={execution_date.day}/"
 
-    registers_found = True
     try:
-        dfs3 = s3_consumer.get_data_from_file(path=path, format="parquet")
+        df = df.unionByName(
+            s3_consumer.get_data_from_file(path=path, format="parquet"),
+            allowMissingColumns=True,
+        )
     except Exception as e:
         logger.error("m=There's no data here yet, message_error={}".format(e))
-        registers_found = False
 
-    if registers_found:
-        df = (
-            dfq.join(dfs3, (dfq.customer_email == dfs3.customer_email), how="left")
-            .withColumn(
-                "is_dispatched",
-                when(dfq.customer_email == dfs3.customer_email, True).otherwise(False),
+    df = df.withColumn(
+        "is_dispatched",
+        when(
+            count("customer_email").over(
+                w.partitionBy("customer_name", "customer_email")
             )
-            .select(dfq["*"], "is_dispatched")
-            .where(col("customer_email").isNotNull())
-        )
+            > 1,
+            True,
+        ).otherwise(False),
+    ).dropDuplicates(["customer_name", "customer_email"])
 
-        df = df.withColumn("column_create_date", lit(execution_date))
+    df = (
+        df.select(df["*"])
+        .where(col("customer_email").isNotNull())
+        .withColumn("column_create_date", lit(execution_date))
+    )
 
-        df = (
-            SparkDataFrameService()
-            .input(df)
-            .create_year_month_day_columns_from_dataframe_column("column_create_date")
-            .output()
-        )
+    df = (
+        SparkDataFrameService()
+        .input(df)
+        .create_year_month_day_columns_from_dataframe_column("column_create_date")
+        .output()
+    )
 
-        s3_data_path = datalake_bucket
-        db_info = DatalakeMetastoreService.get_db_info(
-            environment, source, s3_data_path
-        )
-        metastore_service = SparkMetastoreService(spark_client)
-        spark_metastore_loader = SparkMetastoreLoader(metastore_service)
-        s3_loader = S3Loader()
+    s3_data_path = datalake_bucket
+    db_info = DatalakeMetastoreService.get_db_info(environment, source, s3_data_path)
+    metastore_service = SparkMetastoreService(spark_client)
+    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+    s3_loader = S3Loader()
 
-        table_name = campaign_query
-        database_name = f"datalake_{source}_reverse"
-        format_options = SparkTableStorageFormat.DEFAULT_DW
-        database_location = s3_data_path
+    table_name = campaign_query
+    database_name = f"datalake_{source}_reverse"
+    format_options = SparkTableStorageFormat.DEFAULT_DW
+    database_location = s3_data_path
 
-        logger.info(
-            "m=__main__, msg=Creating database in Spark Metastore if not exists..."
-        )
-        metastore_service.create_database(database_name)
+    logger.info("m=__main__, msg=Creating database in Spark Metastore if not exists...")
+    metastore_service.create_database(database_name)
 
-        s3_loader.load_df(
-            df=df,
-            s3_path=f"{database_location}{table_name}",
-            format_options=format_options,
-            partitions=partition_cols,
-        )
+    s3_loader.load_df(
+        df=df,
+        s3_path=f"{database_location}{table_name}",
+        format_options=format_options,
+        partitions=partition_cols,
+    )
 
-        spark_metastore_loader.update_metastore(
-            df, database_name, table_name, format_options, database_location
-        )
-    else:
-        pass
+    spark_metastore_loader.update_metastore(
+        df, database_name, table_name, format_options, database_location
+    )
