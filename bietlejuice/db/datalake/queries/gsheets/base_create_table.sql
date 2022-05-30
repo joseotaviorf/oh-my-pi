3711c14dc489e8c2bd03aaa6ca40ff6994a62931
Caio Cid Santiago <caiocid@gmail.com>
2022-05-30 14:32:52 -0300

    [AES&S] Deleting GSheets EC2 DAG (#7767)

--- bietlejuice/db/datalake/queries/gsheets/base_create_table.sql
+++
@@ -1,9 +0,0 @@
-create external table {schema_name}.gsheets_{table_name} (
-  {columns}
-)
-row format serde 'org.openx.data.jsonserde.JsonSerDe'
-with serdeproperties (
-  'ignore.malformed.json'='true'
-)
-location 's3://{bucket}/{schema_folder}/gsheets/{table_name}/'
-;
