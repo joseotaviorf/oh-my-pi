b08445da44abf7b915542c649f23d0282dd7e8cf
Paulo Andre de Oliveira Carneiro <paulo.carneiro@quintoandar.com.br>
2022-05-31 15:23:46 -0300

    Revert "removing contract_house from zendesk enrich and adding it directly in the dw sql (#8026)" (#8038)
    This reverts commit 1c1b6e2753065e45a8925678ecb48837ac41cc60.

--- bietlejuice/jobs/composer/db/datalake/queries/dw_tickets/dw/fact_tickets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_tickets/dw/fact_tickets.sql
@@ -1,41 +1,11 @@
-WITH tenant AS (
-    SELECT DISTINCT
-        tmc_t.id_ticket,
-        ch_t.id_contract AS id_contract,
-        -- tickets will only have a valid client key according to its corresponding client type
-        CASE
-            WHEN tmc_t.client_type = 'inquilino' THEN ch_t.id_client
-        END AS id_client
-    FROM
-        datalake_zendesk_tickets.ticket_measurements tmc_t
-    LEFT JOIN
-        datalake_ebdb_contract.contract_house ch_t
-            ON tmc_t.id_contract = ch_t.id_contract
-),
-house_owner AS (
-    SELECT
-        tmc_o.id_ticket,
-        MAX(ch_o.id_house_listing) AS id_house_listing,
-        CASE
-            WHEN tmc_o.client_type IN ('proprietário','imobiliária_b2b') THEN ch_o.id_owner -- COALESCE(dc.id_owner, dhl.id_owner)
-        END AS id_owner
-    FROM
-        datalake_zendesk_tickets.ticket_measurements tmc_o
-    LEFT JOIN
-        datalake_ebdb_contract.contract_house ch_o
-            ON tmc_o.id_house = ch_o.id_house
-    WHERE
-        ch_o.id_owner > 0
-    GROUP BY 1, 3
-)
 SELECT
     CAST(t.id_ticket AS BIGINT) AS sk_ticket,
-    COALESCE(own.id_house_listing, -1) AS sk_house_listing,
-    COALESCE(ten.id_contract, -1)  AS sk_contract,
+    COALESCE(t.id_house_listing, -1) AS sk_house_listing,
+    COALESCE(t.id_contract, -1)  AS sk_contract,
     COALESCE(t.id_user, -1) AS sk_user,
     t.id_personal_document AS sk_personal_document,
-    COALESCE(ten.id_client, -1) AS sk_client,
-    COALESCE(own.id_owner, -1) AS sk_owner,
+    COALESCE(t.id_client, -1) AS sk_client,
+    COALESCE(t.id_owner, -1) AS sk_owner,
     t.id_zendesk_requester_user AS sk_zendesk_requester_user,
     t.id_zendesk_submitter_user AS sk_zendesk_submitter_user,
     t.id_zendesk_assignee_user AS sk_zendesk_assignee_user,
@@ -80,9 +50,3 @@ SELECT
     NOW() AS ts_load
 FROM
     datalake_zendesk_ticket_funnels.tickets_funnel_metrics AS t
-LEFT JOIN
-    tenant ten
-        ON t.id_ticket = ten.id_ticket
-LEFT JOIN
-    house_owner own
-        ON t.id_ticket = own.id_ticket
--- bietlejuice/jobs/composer/db/datalake/queries/enrich_zendesk_ticket_funnels/enrich/tickets_funnel_metrics.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_zendesk_ticket_funnels/enrich/tickets_funnel_metrics.sql
@@ -1,7 +1,41 @@
+WITH tenant AS (
+    SELECT DISTINCT
+        tmc_t.id_ticket,
+        ch_t.id_contract AS id_contract,
+        -- tickets will only have a valid client key according to its corresponding client type
+        CASE
+            WHEN tmc_t.client_type = 'inquilino' THEN ch_t.id_client
+        END AS id_client
+    FROM
+        datalake_zendesk_tickets.ticket_measurements tmc_t
+    LEFT JOIN
+        datalake_ebdb_contract.contract_house ch_t
+            ON tmc_t.id_contract = ch_t.id_contract
+),
+house_owner AS (
+    SELECT
+        tmc_o.id_ticket,
+        MAX(ch_o.id_house_listing) AS id_house_listing,
+        CASE
+            WHEN tmc_o.client_type IN ('proprietário','imobiliária_b2b') THEN ch_o.id_owner -- COALESCE(dc.id_owner, dhl.id_owner)
+        END AS id_owner
+    FROM
+        datalake_zendesk_tickets.ticket_measurements tmc_o
+    LEFT JOIN
+        datalake_ebdb_contract.contract_house ch_o
+            ON tmc_o.id_house = ch_o.id_house
+    WHERE
+        ch_o.id_owner > 0
+    GROUP BY 1, 3
+)
 SELECT DISTINCT
     t.id_ticket,
+    own.id_house_listing,
+    ten.id_contract,
     zuc.id_user,
     zuc.id_personal_document,
+    ten.id_client,
+    own.id_owner,
     t.id_zendesk_requester_user,
     t.id_zendesk_submitter_user,
     t.id_zendesk_assignee_user,
@@ -41,6 +75,12 @@ SELECT DISTINCT
     DAY(t.ts_updated) AS day
 FROM
     datalake_zendesk_tickets.ticket_measurements t
+LEFT JOIN
+    tenant ten
+        ON t.id_ticket = ten.id_ticket
+LEFT JOIN
+    house_owner own
+        ON t.id_ticket = own.id_ticket
 LEFT JOIN
     datalake_zendesk_tickets.zendesk_users_contact zuc
         ON zuc.id_zendesk_user = t.id_zendesk_requester_user
