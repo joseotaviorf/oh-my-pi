a46fe24869222925a9a6220c49e9ff4f53228817
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2022-05-31 10:39:12 -0300

    [AES&S] Updated enrich_heimdall cluster to 9.1 and refactored dag .py file (#7884)
    * Upgraded cluster to Databricks Runtime 9.1 Version
    
    * Refactored DAG .py file
    
    * Updated cluster to min_general instead of med_memory
    
    * Updated cluster to med_general

--- bietlejuice/jobs/composer/dags/enrich_heimdall/enrich_heimdall.py
+++ bietlejuice/jobs/composer/dags/enrich_heimdall/enrich_heimdall.py
@@ -26,23 +26,24 @@ CONTEXT = "heimdall"
 DAG_NAME = f"enrich_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
 
-ENV = os.environ.get("ENVIRONMENT")
-
 config_service = ConfigurationService(DAG_NAME)
-ATHENA_QUERY_RESULT_BUCKET = config_service.get_config("athena_query_results_bucket")
-DATALAKE_BUCKET = config_service.get_config("datalake_bucket")
-DATABRICKS_BIETLEJUICE_REPO_PATH = config_service.get_config(
+ENV = os.environ.get("ENVIRONMENT")
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
-SPARK_JOBS_LOGS_PATH = config_service.get_config("spark_jobs_logs_path")
-BASE_SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
-DOC_MD_CHART_URL = config_service.get_config("doc_md_chart_url")
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+LOGS_OUTPUT_PATH = f"s3://{databricks_bietlejuice_repo_path}/logs/jobs/{DAG_ID}"
+
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_med_general_cluster", deserialize_json=True
+)
 CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{SPARK_JOBS_LOGS_PATH}{DAG_ID}"
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 
 inner_dependencies = config_service.get_config("inner_dependencies")
 
@@ -56,7 +57,7 @@ dag = DAG(
     start_date=MAIN_START_DATE,
     schedule_interval=None,
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
-        chart_url=DOC_MD_CHART_URL, dag_id=DAG_ID
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
 
@@ -71,10 +72,10 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 datalake_task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
+    datalake_bucket=datalake_bucket,
     relative_query_path=DAG_NAME,
     spark_jobs_path=BASE_SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_BUCKET,
+    athena_query_result_location=athena_query_results_bucket,
 )
 
 enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
