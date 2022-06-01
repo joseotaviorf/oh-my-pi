9e56a076bd34ba9eab35c976e2cbbb49e0ab4d26
dmarcolongo <4944228+dmarcolongo@users.noreply.github.com>
2022-06-01 16:32:53 -0300

    DFS_ASP_20220531_AddNewGSheets (#8048)
    * Add sale_asp_portfolio_2022q2
    
    * Add sale_asp_portfolio_2022q2 files
    
    * Change column name
    
    * Fix Style
    
    * Fix Style yml
    
    * Update bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/for_sale/sale_asp_portfolio_2022q2.sql
    
    Co-authored-by: Gustavo Miller Santos <91481595+GustavoMSDextra@users.noreply.github.com>
    
    Co-authored-by: Gustavo Miller Santos <91481595+GustavoMSDextra@users.noreply.github.com>

---
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/for_sale/sale_asp_portfolio_2022q2.sql
@@ -0,0 +1,8 @@
+SELECT
+    INT(NULLIF(id_house, '')) AS id_house,
+    INT(NULLIF(id_owner, '')) AS id_owner,
+    asp_email,
+    asp_list,
+    DATE(NULLIF(dt_association, '')) AS dt_association
+FROM
+    datalake_gsheets_raw.sale_asp_portfolio_2022q2
