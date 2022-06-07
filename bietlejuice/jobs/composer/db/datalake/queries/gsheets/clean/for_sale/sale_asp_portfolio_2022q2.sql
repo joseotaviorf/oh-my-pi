a00bb534f209f8ca8ce924bed5bb51957ab7e67c
dmarcolongo <4944228+dmarcolongo@users.noreply.github.com>
2022-06-07 17:00:04 -0300

    Add asp_name and change source (#8098)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/for_sale/sale_asp_portfolio_2022q2.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/for_sale/sale_asp_portfolio_2022q2.sql
@@ -2,6 +2,7 @@ SELECT
     INT(NULLIF(id_house, '')) AS id_house,
     INT(NULLIF(id_owner, '')) AS id_owner,
     asp_email,
+    asp_name,
     asp_list,
     DATE(NULLIF(dt_association, '')) AS dt_association
 FROM
