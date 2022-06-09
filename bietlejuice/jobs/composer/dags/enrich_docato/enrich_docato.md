5242128681e3ad7cc4b4c970a4ed65ebcfd46e79
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-06-09 15:32:25 -0300

    [AEBED] create DAG enrich_docato and judicial_process_request_status table (#8126)
    * Create DAG enrich_docato and judicial_process_status table

---
+++ bietlejuice/jobs/composer/dags/enrich_docato/enrich_docato.md
@@ -0,0 +1,21 @@
+## Enrich Docato
+
+### Purpose
+This DAG creates the enriched tables from Docato data, for Bedrock. 
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+Daily. More information about run time [here]({chart_url}{dag_id}).
+
+
+### Outputs
+This pipeline produces the following tables in enrich layer, via full load:
+- `judicial_process_request_status`
+
+### Responsible Data Teams
+​
+For any questions or concerns about this DAG, please contact the Data Engineering Team or 
+the Data Analytics Team responsible listed in the 
+[DAG owners](https://www.notion.so/productquintoandar/DAG-Owners-01810df413074722b014ac1cf033b7bd).
+​</details>
---
+++ bietlejuice/jobs/composer/dags/enrich_docato/enrich_docato.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+import pendulum
+import os
+
+from airflow.utils.helpers import chain
+from airflow.models import DAG, Variable
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, BaseTaskGroup
+from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.base.airflow.dag_owner_enum import DAGOwnerEnum
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
+
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2021, 2, 4, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+CONTEXT = "docato"
+DAG_NAME = f"enrich_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+ENV = os.environ.get("ENVIRONMENT")
+
+
+config_service = ConfigurationService(DAG_NAME)
+
+SPECTRUM_IAM_ROLE = config_service.get_config("spectrum_iam_role")
+DATALAKE_BUCKET = config_service.get_config("datalake_bucket")
+ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
+DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+
+REPO_PATH = config_service.get_config("databricks_bietlejuice_repo_path")
+SPARK_JOBS_PATH = f"{REPO_PATH}/spark_jobs/base"
+
+LOGS_OUTPUT_PATH = f"s3://{Variable.get('databricks_s3_bucket')}/logs/jobs/{DAG_ID}"
+
+CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
+CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": DAGOwnerEnum.DATA_BEDROCK,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=DATALAKE_BUCKET,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=SPARK_JOBS_PATH,
+    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+)
+
+
+chain(create_cluster_task, BaseTaskGroup.all_first_tasks(enrich_task_groups))
+chain(BaseTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
