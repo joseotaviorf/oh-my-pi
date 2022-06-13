e1abd94ac8e79848c2a6518d0fc1d4adfd205ea7
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-06-13 12:37:26 -0300

    Changed cluster and libs install (#8137)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/dw_affiliate_costs/dw_affiliate_costs.py
+++ bietlejuice/jobs/composer/dags/dw_affiliate_costs/dw_affiliate_costs.py
@@ -1,6 +1,6 @@
-from datetime import datetime
-import pendulum
 import os
+from datetime import datetime
+from pendulum import timezone
 
 from airflow.models import DAG, Variable
 from airflow.utils.helpers import chain
@@ -10,34 +10,35 @@ from airflow.operators.quintoandar_databricks import (
 )
 
 from bietlejuice.jobs.composer.base.airflow import BaseDAG, DAGOwnerEnum
-from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
-from bietlejuice.jobs.composer.base.airflow.helpers.task_flow_helper import (
-    TaskFlowHelper,
-)
+from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
 
-
-LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
-MAIN_START_DATE = datetime(2020, 8, 29, 0, 0, 0, tzinfo=LOCAL_TZ)
-
+# Pipeline inputs
 DW_SCHEMA = "quintoandar"
 CONTEXT = "affiliate_costs"
 DAG_NAME = f"dw_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
-ENV = os.environ.get("ENVIRONMENT")
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
-SPECTRUM_IAM_ROLE = Variable.get("spectrum_iam_role")
-DW_BUCKET = Variable.get("dw_bucket")
-
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base"
-
-LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
-    Variable.get("databricks_s3_bucket"), DAG_ID
+MAIN_START_DATE = datetime(2020, 8, 29, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = None
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_med_general_cluster", deserialize_json=True
 )
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+config_service = ConfigurationService(DAG_NAME)
+dw_bucket = config_service.get_config("dw_bucket")
+spectrum_iam_role = config_service.get_config("spectrum_iam_role")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+default_libraries = config_service.get_config("default_libraries")
+
+ENV = os.environ.get("ENVIRONMENT")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -47,23 +48,26 @@ dag = DAG(
         "depends_on_past": False,
     },
     start_date=MAIN_START_DATE,
-    schedule_interval=None,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
-        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=default_libraries,
 )
 
 dw_task_group = DWTaskGroup(
     dag=dag,
     env=ENV,
-    dw_bucket=DW_BUCKET,
+    dw_bucket=dw_bucket,
     dw_schema=DW_SCHEMA,
     relative_query_path=DAG_NAME,
-    spark_jobs_path=SPARK_JOBS_PATH,
+    spark_jobs_path=base_spark_jobs_path,
 )
 
 dw_staging_task_group = dw_task_group.build_task_group_from_sql_files(
@@ -71,7 +75,7 @@ dw_staging_task_group = dw_task_group.build_task_group_from_sql_files(
 )
 
 dw_task_group = dw_task_group.build_task_group_from_sql_files(
-    layer=LayerEnum.DW, spectrum_iam_role=SPECTRUM_IAM_ROLE
+    layer=LayerEnum.DW, spectrum_iam_role=spectrum_iam_role
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
--- bietlejuice/jobs/composer/dags/dw_user_affiliate/dw_user_affiliate.py
+++ bietlejuice/jobs/composer/dags/dw_user_affiliate/dw_user_affiliate.py
@@ -1,6 +1,6 @@
+import os
 from datetime import datetime
 from pendulum import timezone
-import os
 
 from airflow.models import DAG, Variable
 from airflow.utils.helpers import chain
@@ -9,38 +9,36 @@ from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksTerminateClusterOperator,
 )
 
-from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.base.airflow.dag_owner_enum import DAGOwnerEnum
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, DAGOwnerEnum
 from bietlejuice.jobs.composer.base.airflow.helpers import TaskFlowHelper
-from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
 from bietlejuice.jobs.composer.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.jobs.composer.dags.base.dw_task_group import DWTaskGroup
 from bietlejuice.jobs.composer.services.configuration_service import (
     ConfigurationService,
 )
 
-
+# Pipeline inputs
 DW_SCHEMA = "public"
 CONTEXT = "user_affiliate"
 DAG_NAME = f"dw_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
-
-ENV = os.environ.get("ENVIRONMENT")
 MAIN_START_DATE = datetime(2020, 2, 20, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = None
+CLUSTER_DESCRIPTION = Variable.get(
+    "databricks_9_1_med_general_cluster", deserialize_json=True
+)
 
 config_service = ConfigurationService(DAG_NAME)
-athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
 dw_bucket = config_service.get_config("dw_bucket")
+spectrum_iam_role = config_service.get_config("spectrum_iam_role")
 databricks_bietlejuice_repo_path = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
-spectrum_iam_role = config_service.get_config("spectrum_iam_role")
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+default_libraries = config_service.get_config("default_libraries")
 
-BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
-
-CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_9_1_min_general_cluster", deserialize_json=True
-)
+ENV = os.environ.get("ENVIRONMENT")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -50,14 +48,17 @@ dag = DAG(
         "depends_on_past": False,
     },
     start_date=MAIN_START_DATE,
-    schedule_interval=None,
+    schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
         chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=CLUSTER_DESCRIPTION,
+    libraries=default_libraries,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
@@ -70,7 +71,7 @@ dw_task_group = DWTaskGroup(
     dw_bucket=dw_bucket,
     dw_schema=DW_SCHEMA,
     relative_query_path=DAG_NAME,
-    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    spark_jobs_path=base_spark_jobs_path,
 )
 
 dw_staging_task_group = dw_task_group.build_task_group_from_sql_files(
