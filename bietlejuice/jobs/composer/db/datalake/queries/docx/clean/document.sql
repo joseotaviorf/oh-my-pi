6f216814f0aec525a05b9303c82c208532617ea1
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-06-14 18:28:23 -0300

    [AEFR-190] Adding country info to DOCx's Document tables (#8154)
    * [AEFR-190] Adding country info to DOCx's Document table
    
    * Adding columns to aud table

--- bietlejuice/jobs/composer/db/datalake/queries/docx/clean/document.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/docx/clean/document.sql
@@ -4,8 +4,10 @@ SELECT
     document_context_id AS id_document_context,
     context_external_id AS id_context_external,
     document_type_id AS id_document_type,
+    country_code,
     attributes,
     attachments,
     created_at AS ts_created,
     updated_at AS ts_updated
-FROM datalake_docx_raw.document
+FROM
+    datalake_docx_raw.document
--- bietlejuice/jobs/composer/db/datalake/queries/docx/clean/document_aud.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/docx/clean/document_aud.sql
@@ -4,9 +4,12 @@ SELECT
     document_context_id AS id_document_context,
     context_external_id AS id_context_external,
     document_type_id AS id_document_type,
+    country_code,
     rev,
     revtype AS rev_type,
     revend AS rev_end,
     attributes,
-    attachments
-FROM datalake_docx_raw.document_aud
+    attachments,
+    country_code_mod AS mod_country_code
+FROM
+    datalake_docx_raw.document_aud
