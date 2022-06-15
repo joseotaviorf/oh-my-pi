cea93171e4ac696d74daaa5b1268ffd66ebd9f95
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-06-15 09:04:54 -0300

    [AEFR-181] Adding is_recurrent column to clean layer (Retsuko) (#8177)
    * adding is_recurrent column to clean

--- bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/credit_card_payment.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/retsuko/clean/credit_card_payment.sql
@@ -1,15 +1,17 @@
-select
+SELECT
     id,
-    external_id as id_external,
-    invoice_id as id_invoice,
+    external_id AS id_external,
+    invoice_id AS id_invoice,
+    acquirer_transaction_id AS id_acquirer_transaction,
     acquirer_auth_code,
+    acquirer_nsu,
+    brand_name,
+    status,
+    installments,
     acquirer_fee_amount,
     advance_fee_amount,
-    brand_name,
     charged_amount,
-    installments,
-    status,
-    timestamp(created_at) as ts_created,
-    acquirer_transaction_id as id_acquirer_transaction,
-    acquirer_nsu
-from datalake_retsuko_raw.credit_card_payment
+    is_recurrent,
+    created_at AS ts_created
+FROM
+    datalake_retsuko_raw.credit_card_payment
