1af1a44a923d6c3a3e4e82a503432ad6fa702a86
Henrique Akira Miamoto <56843327+akiramiamoto@users.noreply.github.com>
2022-06-23 08:58:02 -0300

    Adding field at LPV clean table (#8244)
    * adding event prop
    
    * lineage fix

--- bietlejuice/jobs/composer/db/datalake/queries/amplitude/clean/170698_listing_page_viewed_events.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/amplitude/clean/170698_listing_page_viewed_events.sql
@@ -6,6 +6,7 @@ SELECT
     COALESCE(NULLIF(REGEXP_EXTRACT(GET_JSON_OBJECT(user_properties, '$.entrance_uri'), 'utm_campaign=([^&|$]+)', 1), ''), 'direct') AS up_utm_campaign,
     COALESCE(NULLIF(REGEXP_EXTRACT(GET_JSON_OBJECT(user_properties, '$.entrance_uri'), 'utm_content=([^&|$]+)', 1), ''), 'direct') AS up_utm_content,
     COALESCE(NULLIF(REGEXP_EXTRACT(GET_JSON_OBJECT(user_properties, '$.entrance_uri'), 'utm_term=([^&|$]+)', 1), ''), 'direct') AS up_utm_term,
+    GET_JSON_OBJECT(event_properties, "$['listing_rent_model.rentalAdministrator']") AS ep_listing_rent_model_rentaladministrator,
     GET_JSON_OBJECT(event_properties, '$.business_context') AS business_context,
     GET_JSON_OBJECT(user_properties, '$.utm_source') AS utm_source,
     GET_JSON_OBJECT(user_properties, '$.utm_medium') AS utm_medium,
