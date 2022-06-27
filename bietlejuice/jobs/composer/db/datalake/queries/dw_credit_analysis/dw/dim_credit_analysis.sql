ae403c081fa75837d1d1d2fdee0de76e0d0ac76c
Fernando Gomes <101211208+ferctos-5a@users.noreply.github.com>
2022-06-27 16:20:07 -0300

    [AEFIN-118/121] refactor dim_credit_analysis and fact_proposal_credit_flows (#8257)
    * refactor dim_credit_analysis and fact_proposal_credit_flows

--- bietlejuice/jobs/composer/db/datalake/queries/dw_credit_analysis/dw/dim_credit_analysis.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_credit_analysis/dw/dim_credit_analysis.sql
@@ -1,28 +1,26 @@
-SELECT   
-    ca.id_credit_analysis,
-    ca.id_analyst,
-    ca.type AS documentation_policy,
-    ca.bypass,
-    cr.risk_category,
-    cr.score AS internal_score,
-    cr.liquidity,
-    ca.reason,
-    ca.result,
-    ca.level,
-    CASE
-        WHEN ca.id_proposal IS NULL THEN 'Error'
-        WHEN ca.category IS NULL THEN 'Clear-No'
-        WHEN ca.category IN (1,2,3,4,5,6) THEN 'Insurance'
-        WHEN ca.category IN (7,8,9,10,11,12,13,14,15,16,17,18,19) THEN 'Insurance or Deposit'
-        WHEN ca.category IN (20,21,22,23,24) THEN 'Deposit'
-        WHEN (ca.category = 0 OR bypass IS NOT NULL) THEN 'Free'
-          ELSE 'Error' 
-    END AS credit_decision_cluster,
-    IF(id_analyst IS NULL, FALSE, TRUE) AS is_manual_analysis,
-    ca.ts_created AS ts_credit_analysis_created,
-    NOW() AS ts_load
+SELECT
+  id_credit_analysis,
+  id_proposal,
+  id_analyst,
+  bypass,
+  category,
+  category_within_ca,
+  category_within_proposal,
+  documentation_policy,
+  guarantee_category,
+  level,
+  liquidity,
+  paid_guarantee_type,
+  standalone_factor,
+  reason,
+  risk_category,
+  internal_score,
+  max_bypass,
+  result,
+  credit_decision_cluster,
+  is_manual_analysis,
+  is_reprocessed,
+  ts_credit_analysis_created,
+  NOW() AS ts_load
 FROM
-    datalake_sorting_hat_clean.credit_analysis AS ca 
-LEFT JOIN
-    datalake_sorting_hat_clean.screening_result AS cr 
-        ON cr.id_proposal = ca.id_proposal
+  datalake_credit_analysis.credit_analysis
--- bietlejuice/jobs/composer/db/datalake/queries/dw_credit_analysis/dw/fact_proposal_credit_flows.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_credit_analysis/dw/fact_proposal_credit_flows.sql
@@ -1,56 +1,53 @@
- WITH credit_analysis_limits AS (
-    SELECT
-        ca.id_proposal,
-        ca.id_credit_analysis,
-        FIRST_VALUE(ca.id_credit_analysis) OVER (PARTITION BY ca.id_proposal ORDER BY ca.ts_updated) AS id_first_credit_analysis,
-        FIRST_VALUE(ca.id_variant) OVER (PARTITION BY ca.id_proposal ORDER BY ca.ts_updated) AS id_first_variant,
-        FIRST_VALUE(ca.id_credit_analysis) OVER (PARTITION BY ca.id_proposal ORDER BY ca.ts_updated DESC) AS id_last_credit_analysis,
-        ca.category,
-        p.guarantee
-    FROM
-        datalake_sorting_hat_clean.credit_analysis AS ca
-    JOIN
-        datalake_ebdb_clean.proposal AS p
-            ON ca.id_proposal = p.id
-    WHERE
-        p.id <> -1
-        AND p.id IS NOT NULL
+WITH credit_analysis AS (
+  SELECT
+    id_credit_analysis,
+    id_proposal,
+    category,
+    max_ca_category
+  FROM
+    datalake_credit_analysis.credit_analysis
 )
-SELECT 
-    cal.id_proposal AS sk_proposal,
-    CAST(COALESCE(cal.id_credit_analysis,-1) AS INT) AS sk_credit_analysis,
-    sk_client,
-    CAST(COALESCE(cal.id_first_credit_analysis,-1) AS INT) AS sk_first_credit_analysis,
-    CAST(COALESCE(cal.id_first_variant,-1) AS INT) AS sk_first_variant,
-    COALESCE(cal.category,-1) AS sk_guarantee_category,
-    CAST(COALESCE(cal.id_last_credit_analysis,-1) AS INT) AS sk_last_credit_analysis,
-    flrf.sk_offer,
-    flrf.sk_region,
-    flrf.sk_offer_submitted_date,
-    flrf.sk_offer_approved_date,
-    flrf.sk_last_credit_evaluation_init, 
-    flrf.sk_last_credit_evaluation_positive,
-    flrf.sk_last_credit_evaluation_negative,
-    COALESCE(NULLIF(flrf.sk_last_credit_evaluation_positive, -1),
-              CASE 
-                  WHEN flrf.sk_last_credit_evaluation_positive < 0 
-                  AND flrf.sk_last_credit_evaluation_negative > 0 
-                  AND cal.category IS NOT NULL
-                  AND cal.guarantee = 'RentalGuarantee' THEN flrf.sk_last_credit_evaluation_negative 
-              END, -1) sk_credit_evaluation_approved_date,
-    flrf.sk_tenant_first_doc_sent_date,
-    flrf.sk_tenant_doc_complete_date,
-    flrf.sk_credit_analysis_approved_date,
-    flrf.sk_guarantee_paid_date,        
-    flrf.funnel_step,
-    IF(cal.id_first_credit_analysis = cal.id_credit_analysis, True, False) AS is_first_credit_evaluation,
-    IF(cal.id_last_credit_analysis = cal.id_credit_analysis, True, False) AS is_last_credit_evaluation,
-    NOW() AS ts_load
+
+SELECT
+  flrf.sk_client,
+  flrf.sk_contract_signed_date,
+  CAST(COALESCE(ca.id_credit_analysis, -1) AS INTEGER) AS sk_credit_analysis,
+  flrf.sk_credit_analysis_approved_date,
+  COALESCE(
+    NULLIF(flrf.sk_last_credit_evaluation_positive, -1),
+    CASE
+      WHEN flrf.sk_last_credit_evaluation_positive < 0
+      AND flrf.sk_last_credit_evaluation_negative > 0 
+      AND ca.category IS NOT NULL THEN flrf.sk_last_credit_evaluation_negative 
+    END, -1
+  ) AS sk_credit_evaluation_approved_date,
+  CAST(COALESCE(cap.id_first_credit_analysis, -1) AS INTEGER) AS sk_first_credit_analysis,
+  CAST(COALESCE(cap.id_first_variant, -1) AS INTEGER) AS sk_first_variant,
+  COALESCE(ca.category, -1) AS sk_guarantee_category,
+  flrf.sk_guarantee_paid_date,
+  CAST(COALESCE(cap.id_last_credit_analysis, -1) AS INTEGER) AS sk_last_credit_analysis,
+  flrf.sk_last_credit_evaluation_init,
+  flrf.sk_last_credit_evaluation_negative,
+  flrf.sk_last_credit_evaluation_positive,
+  flrf.sk_offer,
+  flrf.sk_offer_approved_date,
+  flrf.sk_offer_submitted_date,
+  cap.id_proposal AS sk_proposal,
+  flrf.sk_region,
+  flrf.sk_tenant_doc_complete_date,
+  flrf.sk_tenant_first_doc_sent_date,
+  flrf.funnel_step,
+  IF(cap.id_first_credit_analysis = ca.id_credit_analysis, TRUE, FALSE) AS is_first_credit_evaluation,
+  IF(cap.id_last_credit_analysis = ca.id_credit_analysis, TRUE, FALSE) AS is_last_credit_evaluation,
+  NOW() AS ts_load
 FROM
-    dw_public.fact_listing_rent_flows flrf
-INNER JOIN 
-    credit_analysis_limits cal
-        ON cal.id_proposal = flrf.sk_proposal
+  dw_public.fact_listing_rent_flows AS flrf
+JOIN
+  datalake_credit_analysis.credit_analysis_proposals AS cap
+    ON cap.id_proposal = flrf.sk_proposal
+JOIN
+  credit_analysis AS ca
+    ON cap.id_proposal = ca.id_proposal
 WHERE
-    sk_last_credit_evaluation_init > 0
-    AND flrf.sk_proposal > 0 
+  sk_last_credit_evaluation_init > 0
+  AND flrf.sk_proposal > 0
