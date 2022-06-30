dc8fe8ebdc4fbd49a1bed2206e60266f00e17404
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-06-30 14:42:52 -0300

    [AEBED] Remove column and add cleanse logic on CAP data (#8311)
    * Removed column and added logic to only cleanse column when needed
    
    * Forcing string conversion
    
    * Removed col from lineage
    
    * Adding more understanding cleanse function

--- bietlejuice/jobs/composer/db/datalake/queries/payable_accounts_transactions/clean/accounts_payable.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/payable_accounts_transactions/clean/accounts_payable.sql
@@ -1,13 +1,15 @@
 SELECT
     `_doc_nf` AS nf_file_description,
-    TRIM(fornecedor) AS supplier_description,
+    CASE 
+        WHEN RLIKE(TRIM(fornecedor),"^[0-9]+\.0+$") THEN REGEXP_REPLACE(TRIM(fornecedor), "\.0+$", "")
+        ELSE TRIM(fornecedor)
+    END AS supplier_description,
     NULLIF(TRIM(cost_and_profit_center), '') AS payment_source,
     NULLIF(TRIM(revenue_and_cost_elements), '') AS payment_reason,
     banco AS bank_description,
     CAST(REPLACE(valor_bruto, ',', '') AS NUMERIC(38, 2)) AS paid_amount,
-    CAST(REPLACE(saldo_total, ',', '') AS NUMERIC(38, 2)) AS account_balance,
     DATE_FORMAT(TO_DATE(competencia, 'yyyy-MM-dd HH:mm:ss'), 'yyyyMM') AS accrual_year_month,
     TO_DATE(pagamento, 'yyyy-MM-dd HH:mm:ss') AS dt_paid,
     ts_load
 FROM
-    datalake_payable_accounts_transactions_raw.accounts_payable;
+    datalake_payable_accounts_transactions_raw.accounts_payable
