b5f80b96cdf02b58017bb02d277b605ac6745cc1
Fernando Gomes <101211208+ferctos-5a@users.noreply.github.com>
2022-06-30 08:48:02 -0300

    [AEFIN-141] add atta db ingestions on raw (#8286)
    * add the first ingestions from the external atta database

---
+++ bietlejuice/jobs/composer/dags/atta/atta.md
@@ -0,0 +1,59 @@
+## Atta
+
+### Purpose
+
+This DAG imports data from Atta which is one of 5A's acquisitions. Its main product is called iSolve which main focus is to manage the fluxes regarding internal financing options in ForSale contracts.
+
+
+<details>
+  <summary><strong> > DAG details (click to expand)</strong></summary>
+
+### Execution Interval
+This DAG is triggered daily, via Mediator. More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+In datalake raw, via full load:
+
+- `analiseparametrizacoes`
+- `analisescredito`
+- `analisesenriquecimento`
+- `cadastro_cliente`
+- `cadastro_imovel`
+- `cadastro_rating`
+- `canal`
+- `cliente`
+- `cliente_vendedor`
+- `cms_fechamento_detalhe`
+- `consulta_score`
+- `consulta_score_caixa`
+- `consulta_score_cliente_ctrl`
+- `consulta_score_itau`
+- `documento_produto`
+- `documento_sub_categoria`
+- `documento_tipo`
+- `documento_tpcontratacao`
+- `entidade_log`
+- `lead_dados_captura`
+- `lead_detalhe_acompanhamento`
+- `lead_detalhe_categoria`
+- `log_contrato_assinado_parceiro`
+- `log_proposta_esteira`
+- `log_proposta_reemissao`
+- `log_vinculo_analista_proposta`
+- `max_usuarios`
+- `meta_consultor`
+- `parceiro`
+- `pre_analise_observacao`
+- `produto`
+- `produto_esteira`
+- `proposta`
+- `proposta_credimob_confval`
+- `proposta_esteira`
+- `proposta_itau`
+- `simulacao_fornecedor`
+- `simulador_credito_imobiliario`
+- `simulador_credito_imobiliario_fornecedor`
+- `sla_fornecedor`
+- `spcrepositorio`
+- `usuariospilotomultibancos`
---
+++ bietlejuice/jobs/composer/dags/atta/spark_jobs/load_incremental_atta_into_datalake.py
@@ -0,0 +1,98 @@
+import json
+import logging
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.jobs.composer.base.db import DatabaseEnum, DatalakeMetastoreService
+from bietlejuice.jobs.composer.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.jobs.composer.clients.db_clients import SparkClient
+from bietlejuice.jobs.composer.consumers.db_consumers import MySqlConsumer
+from bietlejuice.jobs.composer.loaders import SparkMetastoreLoader
+from bietlejuice.jobs.composer.loaders.s3_loader import S3Loader
+from bietlejuice.jobs.composer.services.metastore_services import SparkMetastoreService
+
+JOB_NAME = "load_incremental_atta_into_datalake"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("env")
+    parser.add_argument("datalake_bucket")
+    parser.add_argument("source")
+    parser.add_argument("table_name")
+    parser.add_argument("date_filter_column", help="Date filter column")
+    parser.add_argument(
+        "unixtime_measure", type=str, help="Column unixtime unit", nargs="?"
+    )
+    parser.add_argument("execution_date", type=str, help="DAG execution date")
+
+    args = parser.parse_args()
+
+    environment = args.env
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+    table_name = args.table_name
+    date_filter_column = args.date_filter_column
+    unixtime_measure = args.unixtime_measure
+    execution_date = args.execution_date
+
+    logger.info(
+        f"""
+                m=__main__, environment={environment}, source={source}, datalake_bucket={datalake_bucket},
+                table_name={table_name}, date_filter_column={date_filter_column}, unixtime_measure={unixtime_measure},
+                execution_date={execution_date}, msg=Starting spark job...
+        """
+    )
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    conn_config_json = dbutils.secrets.get(scope="quintoandar", key=DatabaseEnum.ATTA)
+    conn_config = json.loads(conn_config_json)
+    spark_client = SparkClient()
+    mysql_consumer = MySqlConsumer(conn_config, spark_client)
+
+    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
+    metastore_service = SparkMetastoreService(spark_client)
+
+    # create database if it doesn't exists
+    database_name = db_info["db_raw_databricks"]
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = db_info["db_raw_path"]
+    metastore_service.create_database(database_name)
+
+    s3_loader = S3Loader()
+    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+
+    df = mysql_consumer.get_incremental_data_from_table(
+        table_name=table_name,
+        date_filter_column=date_filter_column,
+        date_filter_value=execution_date,
+        unixtime_measure=unixtime_measure,
+    )
+
+    if df:
+        s3_loader.load_df(
+            df=df,
+            s3_path=f"{database_location}{table_name}",
+            format_options=format_options,
+            database_location=database_location,
+            partitions=["year", "month", "day"],
+        )
+        spark_metastore_loader.update_metastore(
+            df,
+            database_name,
+            table_name,
+            format_options,
+            database_location,
+            partitions=["year", "month", "day"],
+        )
+    else:
+        logger.warning(
+            f"""m=__main__, table_name={table_name}, execution_date={execution_date},
+            msg=No data returned from Production database."""
+        )
