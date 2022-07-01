07032c29cf7edac51989511c4529900dc6c88d68
TamirisRodrigues <72048076+TamirisRodrigues@users.noreply.github.com>
2022-07-01 12:02:14 -0300

    Migrating unique_supply_cohort_conversions datamart (#8176)

---
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_datamarts_spark/dw/growth_cross/unique_supply_cohort_conversions.sql
@@ -0,0 +1,319 @@
+WITH
+l2p_pre AS (
+SELECT
+    lf.sk_lead_date,
+	dd.date,
+	dd.sk_date,
+	dr.city_group,
+	lf.mkt_origin AS supply_mkt_origin,
+	lf.mkt_channel AS supply_mkt_channel,
+	lf.mkt_completion AS supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_lead AS context_origin,
+	context_prospect AS context_conversion,
+    CAST(DATEDIFF(DATE_TRUNC('week',TO_DATE(lf.sk_prospect_date::STRING,"yyyyMMdd")),DATE_TRUNC('week',TO_DATE(lf.sk_lead_date::STRING,"yyyyMMdd")))/7 AS INTEGER) AS quantity_weeks_conversion
+FROM
+    dw_public.dim_date dd
+JOIN
+    dw_datamarts_cross.lead_listing_flows lf
+        ON dd.sk_date = lf.sk_lead_date
+        AND lf.sk_lead_date > 0
+LEFT JOIN
+    dw_public.dim_region dr
+        ON dr.sk_region = lf.sk_region
+WHERE
+    dd.date BETWEEN DATE_TRUNC('YEAR',CURRENT_DATE) - INTERVAL 4 year AND CURRENT_DATE  
+),
+l2p AS (
+SELECT
+	date,
+	sk_date,
+	city_group,
+	supply_mkt_origin,
+	supply_mkt_channel,
+	supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_origin,
+	context_conversion, 
+	CASE
+	    WHEN quantity_weeks_conversion < 0
+	        THEN 'W5+'
+        WHEN quantity_weeks_conversion BETWEEN 0 AND 4 
+            THEN 'W'||quantity_weeks_conversion
+        WHEN quantity_weeks_conversion >=5
+	        THEN 'W5+'
+    END AS weeks_conversion,
+  	COUNT(sk_lead_date) AS l2p,
+	NULL::BIGINT AS p2q,
+	NULL::BIGINT AS q2opp,
+	NULL::BIGINT AS opp2fl
+FROM
+    l2p_pre
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+),
+p2q_pre AS (
+SELECT
+	dd.date,
+	dd.sk_date,
+	lf.sk_prospect_date,
+	dr.city_group,
+	lf.mkt_origin AS supply_mkt_origin,
+	lf.mkt_channel AS supply_mkt_channel,
+	lf.mkt_completion AS supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_prospect AS context_origin,
+	context_qualified AS context_conversion,
+    CAST(DATEDIFF(DATE_TRUNC('week',TO_DATE(lf.sk_qualified_date::STRING,"yyyyMMdd")),DATE_TRUNC('week',TO_DATE(lf.sk_prospect_date::STRING,"yyyyMMdd")))/7 AS INTEGER) AS quantity_weeks_conversion
+FROM
+    dw_public.dim_date dd
+JOIN
+    dw_datamarts_cross.lead_listing_flows lf
+        ON dd.sk_date = lf.sk_prospect_date
+        AND lf.sk_prospect_date > 0
+LEFT JOIN
+    dw_public.dim_region dr
+        ON dr.sk_region = lf.sk_region
+WHERE
+    dd.date BETWEEN DATE_TRUNC('YEAR',CURRENT_DATE) - INTERVAL 4 year AND CURRENT_DATE
+),
+p2q AS (
+SELECT
+	date,
+	sk_date,
+	city_group,
+	supply_mkt_origin,
+	supply_mkt_channel,
+	supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_origin,
+	context_conversion,
+	CASE
+	    WHEN quantity_weeks_conversion < 0
+	        THEN 'W5+'
+        WHEN quantity_weeks_conversion BETWEEN 0 AND 4 
+            THEN 'W'||quantity_weeks_conversion
+        WHEN quantity_weeks_conversion >=5
+	        THEN 'W5+'
+    END AS weeks_conversion,
+  	NULL::BIGINT AS l2p,
+	COUNT(sk_prospect_date) AS p2q, -- this count is done on the prospect date because not all listings come FROM a lead, and maybe one lead brings multiple house listings
+	NULL::BIGINT AS q2opp,
+	NULL::BIGINT AS opp2fl
+FROM
+	p2q_pre
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+),
+q2opp_pre AS (
+SELECT
+	dd.date,
+	dd.sk_date,
+    lf.sk_qualified_date,
+	dr.city_group,
+	lf.mkt_origin AS supply_mkt_origin,
+	lf.mkt_channel AS supply_mkt_channel,
+	lf.mkt_completion AS supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_qualified AS context_origin,
+	context_opportunity AS context_conversion,
+    CAST(DATEDIFF(DATE_TRUNC('week',TO_DATE(lf.sk_opportunity_date::STRING,"yyyyMMdd")),DATE_TRUNC('week',TO_DATE(lf.sk_qualified_date::STRING,"yyyyMMdd")))/7 AS INTEGER) AS quantity_weeks_conversion
+FROM
+    dw_public.dim_date dd
+JOIN
+    dw_datamarts_cross.lead_listing_flows lf
+        ON dd.sk_date = lf.sk_qualified_date
+        AND lf.sk_qualified_date > 0
+LEFT JOIN
+    dw_public.dim_region dr
+        ON dr.sk_region = lf.sk_region
+WHERE
+    dd.date BETWEEN DATE_TRUNC('YEAR',CURRENT_DATE) - INTERVAL 4 year AND CURRENT_DATE -- filter data from 4 years ago
+
+),
+q2opp AS (
+SELECT
+	date,
+	sk_date,
+	city_group,
+	supply_mkt_origin,
+	supply_mkt_channel,
+	supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_origin,
+	context_conversion,
+	CASE
+	    WHEN quantity_weeks_conversion < 0
+	        THEN 'W5+'
+        WHEN quantity_weeks_conversion BETWEEN 0 AND 4 
+            THEN 'W'||quantity_weeks_conversion
+        WHEN quantity_weeks_conversion >=5
+	        THEN 'W5+'
+    END AS weeks_conversion,
+  	NULL::BIGINT AS l2p,
+	NULL::BIGINT AS p2q,
+	COUNT(sk_qualified_date) AS q2opp, -- this count is done on the qualified date because not all listings come FROM a lead, and maybe one lead brings multiple house listings
+	NULL::BIGINT AS opp2fl
+FROM
+	q2opp_pre
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+),
+opp2fl_pre AS (
+SELECT
+	dd.date,
+	dd.sk_date,
+    lf.sk_house_listing,
+	dr.city_group,
+	lf.mkt_origin AS supply_mkt_origin,
+	lf.mkt_channel AS supply_mkt_channel,
+	lf.mkt_completion AS supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_opportunity AS context_origin,
+	context_first_listing AS context_conversion,
+    CAST(DATEDIFF(DATE_TRUNC('week',TO_DATE(lf.sk_first_listing_date::STRING,"yyyyMMdd")),DATE_TRUNC('week',TO_DATE(lf.sk_opportunity_date::STRING,"yyyyMMdd")))/7 AS INTEGER) AS quantity_weeks_conversion
+FROM
+    dw_public.dim_date dd
+JOIN
+    dw_datamarts_cross.lead_listing_flows lf
+        ON dd.sk_date = lf.sk_opportunity_date
+        AND lf.sk_opportunity_date > 0
+LEFT JOIN
+    dw_public.dim_region dr
+        ON dr.sk_region = lf.sk_region
+WHERE
+    dd.date BETWEEN DATE_TRUNC('YEAR',CURRENT_DATE) - INTERVAL 4 year AND CURRENT_DATE -- filter data from 4 years ago
+),
+opp2fl AS (
+SELECT
+	date,
+	sk_date,
+	city_group,
+	supply_mkt_origin,
+	supply_mkt_channel,
+	supply_mkt_completion,
+	sales_company,
+	sourcing_ops,
+	origin_table,
+	lead_origin,
+	funnel_drop_reason,
+	context_origin,
+	context_conversion,
+	CASE
+	    WHEN quantity_weeks_conversion < 0
+	        THEN 'W5+'
+        WHEN quantity_weeks_conversion BETWEEN 0 AND 4 
+            THEN 'W'||quantity_weeks_conversion
+        WHEN quantity_weeks_conversion >=5
+	        THEN 'W5+'
+    END AS weeks_conversion,
+  	NULL::BIGINT AS l2p,
+	NULL::BIGINT AS p2q,
+	NULL::BIGINT AS q2opp,
+	COUNT(DISTINCT sk_house_listing) AS opp2fl
+FROM
+	opp2fl_pre
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+),
+union_all AS (
+    SELECT * FROM l2p
+	UNION ALL
+	SELECT * FROM p2q
+	UNION ALL
+	SELECT * FROM q2opp
+	UNION ALL
+	SELECT * FROM opp2fl
+),
+union_all_date AS (
+SELECT
+  dd.date,
+  ua.city_group,
+  ua.supply_mkt_origin,
+  ua.supply_mkt_channel,
+  ua.supply_mkt_completion,
+  ua.sales_company,
+  ua.sourcing_ops,
+  ua.origin_table,
+  ua.lead_origin,
+  ua.funnel_drop_reason,
+  ua.context_origin,
+  ua.context_conversion,
+  ua.weeks_conversion,
+  ua.l2p,
+  ua.p2q,
+  ua.q2opp,
+  ua.opp2fl
+FROM
+    union_all ua
+RIGHT JOIN
+    dw_public.dim_date dd
+        ON ua.sk_date = dd.sk_date
+WHERE
+    dd.date BETWEEN DATE_TRUNC('YEAR',CURRENT_DATE) - INTERVAL 4 year AND CURRENT_DATE
+)
+
+SELECT
+ date,
+  city_group,
+  supply_mkt_origin,
+  CASE
+      WHEN supply_mkt_origin = 'Owner PWA'
+          THEN supply_mkt_channel
+      WHEN supply_mkt_origin != 'Owner PWA'
+          THEN supply_mkt_origin
+  END AS supply_mkt_origin_detailed,
+  CASE
+      WHEN supply_mkt_origin = 'B2B' OR supply_mkt_origin = 'CIQ'
+          THEN supply_mkt_origin
+      WHEN supply_mkt_completion = 'Full Self-Service'
+          THEN 'FSS'
+      ELSE 'IS'
+  END AS lead_context,
+  sales_company,
+  sourcing_ops,
+  CASE
+      WHEN sourcing_ops IN ('IS Ext', 'IS Int', 'FSS IS PhotoJob', 'Other') 
+          AND (supply_mkt_completion = 'Full Self-Service' 
+                  OR (supply_mkt_origin <> 'B2B' AND supply_mkt_origin <> 'CIQ' )) 
+          THEN 'IS'
+      ELSE sourcing_ops
+  END AS lead_processing_operation,
+  origin_table,
+  lead_origin,
+  funnel_drop_reason,
+  context_origin,
+  context_conversion,
+  weeks_conversion AS weeks_conversion,
+  SUM(COALESCE(l2p,0)) AS l2p,
+  SUM(COALESCE(p2q,0)) AS p2q,
+  SUM(COALESCE(q2opp,0)) AS q2opp,
+  SUM(COALESCE(opp2fl,0)) AS opp2fl,
+  current_timestamp AS ts_load
+FROM
+  union_all_date
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
