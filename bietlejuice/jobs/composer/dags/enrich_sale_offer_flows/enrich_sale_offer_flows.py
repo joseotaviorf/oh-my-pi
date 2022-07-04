2ae344da68dd3a7a39e015cd40e38c42974a3961
Samuel Favarin <91481588+samuel-favarin@users.noreply.github.com>
2022-07-04 13:32:54 -0300

    [DFORSALE-1974] Data quality of sales (#8342)
    * feat: quality check of sale_offer and sale_offer_flows
    
    * feat: table level validation
    
    * fix: sale offer check
    
    * fix: data quality folder
    
    * fix: lint issues
    
    * feat: update enrush sale offer flow DAG
    
    * fix: lint issues
    
    Co-authored-by: samuel-favarin <samuel.favarin.dxt@ext.quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/enrich_sale_offer_flows/enrich_sale_offer_flows.py
+++ bietlejuice/jobs/composer/dags/enrich_sale_offer_flows/enrich_sale_offer_flows.py
@@ -117,3 +117,8 @@ chain(
     + datalake_task_group.last_tasks(inner_dependencies_task_groups_boundaries),
     terminate_cluster_task,
 )
+
+# Set data quality tasks if exists
+independent_tasks = DatalakeTaskGroup.all_independent_tasks(enrich_task_groups)
+if independent_tasks:
+    terminate_cluster_task.set_upstream(independent_tasks)
