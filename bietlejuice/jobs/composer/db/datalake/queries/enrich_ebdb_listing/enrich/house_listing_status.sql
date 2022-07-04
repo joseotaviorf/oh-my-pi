d930a54314744d2fc2e91feaab834452f17d9726
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-07-04 15:50:31 -0300

    [AEFR-253] set is_last_status_of_day depending on rev instead of timestamp (#8352)
    * set is_last_status_of_day depending on rev instead of timestamp, because we can have repeated timestamps

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_listing/enrich/house_listing_status.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_ebdb_listing/enrich/house_listing_status.sql
@@ -1,17 +1,18 @@
-select
-    cast(cast(id_house as string)||'00'||cast(order_version as string) as bigint) as id_house_listing,
+SELECT
+    CAST(CAST(id_house AS STRING)||'00'||CAST(order_version AS STRING) AS BIGINT) AS id_house_listing,
     id_house,
     id_region,
-    order_version as version,
-    status_history_new as status_history,
-    regexp_replace(reason, '\n', '') as status_change_reason,
-    coalesce(
-      -- get max ts per id_house_listings per day
-      max(ts_status_changed_new) over(
-	    partition by id_house, order_version, cast(ts_status_changed_new as date)
-      ) = ts_status_changed_new,
-    false) as is_last_status_of_day,
+    order_version AS version,
+    status_history_new AS status_history,
+    REGEXP_REPLACE(reason, '\n', '') AS status_change_reason,
+    COALESCE(
+      -- get MAX ts per id_house_listings per day
+      MAX(rev) OVER(
+      PARTITION BY id_house, order_version, CAST(ts_status_changed_new AS DATE)
+                    ) = rev,
+    false) AS is_last_status_of_day,
     ts_first_publication,
-    ts_status_changed_new as ts_status_started,
-    ts_status_changed_next as ts_status_ended
-from datalake_ebdb_listing.house_status_version_order
+    ts_status_changed_new AS ts_status_started,
+    ts_status_changed_next AS ts_status_ended
+FROM 
+    datalake_ebdb_listing.house_status_version_order
