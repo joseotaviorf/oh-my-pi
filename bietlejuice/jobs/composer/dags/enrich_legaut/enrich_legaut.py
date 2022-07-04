ec62cfe21a43df95b800ff15017129d24e1de5d0
heri-macedo <61527726+heri-macedo@users.noreply.github.com>
2022-07-04 18:03:25 -0300

    Fix spark_job_path (#8355)

--- bietlejuice/jobs/composer/dags/enrich_legaut/enrich_legaut.py
+++ bietlejuice/jobs/composer/dags/enrich_legaut/enrich_legaut.py
@@ -39,7 +39,7 @@ DOC_MD_BASE_URL = config_service.get_config("doc_md_chart_url")
 DATABRICKS_BIETLEJUICE_REPO_PATH = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
-SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base/"
+SPARK_JOBS_PATH = f"{DATABRICKS_BIETLEJUICE_REPO_PATH}/spark_jobs/base"
 SPARK_JOBS_LOGS_PATH = config_service.get_config("spark_jobs_logs_path")
 
 CLUSTER_DESCRIPTION = Variable.get(
