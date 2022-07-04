1d2915954bed201fec8b021bd6910fabc34815a0
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-07-04 10:33:13 -0300

    [AES&S] Updated LabelStudio enrich (#8076)
    * Update sessions.sql
    
    * Update sessions.yml
    
    * updated table

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_label_studio/enrich/sessions.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_label_studio/enrich/sessions.sql
@@ -1,43 +1,38 @@
 WITH explode_results AS (
     SELECT
         id_annotation,
-        task['project'] AS id_project,
-        task['id_session_data'] AS id_session,
-        completed_by['id'] AS id_agent,
+        id_project,
+        GET_JSON_OBJECT(task.data, "$.id_session") AS id_session,
+        completed_by.id AS id_agent,
         EXPLODE(result) AS result
     FROM
         datalake_label_studio_clean.annotations
 )
 SELECT
-    MD5(CONCAT(a.id_annotation, er.id_project, er.id_session, er.id_agent, er.result['id'])) AS id_task,
+    MD5(CONCAT(a.id_annotation, a.id_project, er.id_session, er.id_agent, er.result.id)) AS id_task,
     a.id_annotation,
-    er.id_project,
+    a.id_project,
     er.id_session,
     er.id_agent,
-    er.result['id'] AS id_result,
+    er.result.id AS id_result,
+    a.project_name,
     CASE
-        WHEN a.task['email'] IS NOT NULL THEN a.task['email']
+        WHEN a.task.email IS NOT NULL THEN a.task.email
         ELSE SPLIT(a.created_username, '[,]')[0]
     END AS agent_email,
-    a.task['user_info_data'] AS session_user,
     CASE
-        WHEN a.task['message_data'] IS NOT NULL THEN a.task['message_data']
-        ELSE a.task['text_message_data']
+        WHEN GET_JSON_OBJECT(a.task.data, "$.message") IS NOT NULL THEN REGEXP_REPLACE(GET_JSON_OBJECT(a.task.data, "$.message"), '<.+?>', '')
+        ELSE REGEXP_REPLACE(GET_JSON_OBJECT(a.task.data, "$.text_message"), '<.+?>', '')
     END AS session_message,
-    a.task['overlap'] AS session_overlap,
-    a.task['hsm_content_data'] AS hsm_content,
-    a.task['owner_contract_info_data'] AS owner_contract_info, 
-    a.task['tenant_contract_info_data'] AS tenant_contract_info, 
-    a.task['house_info_data'] AS house_info, 
-    er.result['value'] AS result_choice,
-    a.lead_time AS task_lead_time,
-    a.task['is_labeled'] AS is_labeled,
+    a.task.overlap AS session_overlap,
+    GET_JSON_OBJECT(er.result.value, "$.choices") AS result_choice,
+    a.task.is_labeled AS is_labeled,
     CASE
         WHEN project_name LIKE "%consistencia%" THEN TRUE
         ELSE FALSE
     END is_consistency,
-    CAST(a.task['created_at'] AS TIMESTAMP) AS ts_session_created,
-    CAST(a.task['updated_at'] AS TIMESTAMP) AS ts_session_updated,
+    CAST(a.task.created_at AS TIMESTAMP) AS ts_session_created,
+    CAST(a.task.updated_at AS TIMESTAMP) AS ts_session_updated,
     a.ts_created,
     a.ts_updated
 FROM
@@ -45,6 +40,6 @@ FROM
 LEFT JOIN
     explode_results er
         ON a.id_annotation = er.id_annotation
-        AND a.task['project'] = er.id_project
-        AND a.task['id_session_data'] = er.id_session
-        AND a.completed_by['id'] = er.id_agent
+        AND a.id_project = er.id_project
+        AND GET_JSON_OBJECT(a.task.data, "$.id_session") = er.id_session
+        AND a.completed_by.id = er.id_agent
