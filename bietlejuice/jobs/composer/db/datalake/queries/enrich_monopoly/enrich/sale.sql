d57cd8d3f148ade9245485c7ae0e0d95f7b6139c
heri-macedo <61527726+heri-macedo@users.noreply.github.com>
2022-07-04 15:14:18 -0300

    [DFORSALE-2045]: Change enrich_monopoly to return dt_occurrence from sale transactions table (#8310)

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_monopoly/enrich/sale.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_monopoly/enrich/sale.sql
@@ -1,11 +1,16 @@
 SELECT
     s.id AS id_sale,
     s.id_external_offer,
-    o.id_outcome,
-    o.dt_occurence,
+    st.id_income_reference,
+    MAX(ir.dt_income) AS dt_occurence,
     s.ts_created
 FROM
-    datalake_monopoly_clean.sale s
-LEFT JOIN
-    datalake_monopoly_clean.outcome_status_log o
-        ON o.id = s.id
+    datalake_monopoly_clean.sale AS s
+LEFT JOIN datalake_monopoly_clean.sale_transaction AS st
+    ON st.id_sale = s.id
+LEFT JOIN datalake_monopoly_clean.income_reference AS ir
+    ON st.id_income_reference = ir.id
+WHERE
+  st.event = 'income-down-payment'
+  AND st.status = 'created'
+GROUP BY 1,2,3,5
