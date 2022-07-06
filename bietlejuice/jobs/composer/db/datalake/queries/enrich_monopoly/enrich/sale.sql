8676ff365569511db50b4ae7e69531f9adc87280
heri-macedo <61527726+heri-macedo@users.noreply.github.com>
2022-07-06 10:36:17 -0300

    [DFORSALE-2255]: Fix duplicated offers on enrich monopoly (#8375)

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_monopoly/enrich/sale.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_monopoly/enrich/sale.sql
@@ -1,3 +1,13 @@
+WITH last_sale_transaction AS (
+  SELECT
+      *,
+      ROW_NUMBER() OVER (PARTITION BY id_sale ORDER BY ts_created DESC) AS rw_sale_transaction
+  FROM
+      datalake_monopoly_clean.sale_transaction AS st
+  WHERE
+      st.event = 'income-down-payment'
+      AND st.status = 'created'
+)
 SELECT
     s.id AS id_sale,
     s.id_external_offer,
@@ -6,11 +16,11 @@ SELECT
     s.ts_created
 FROM
     datalake_monopoly_clean.sale AS s
-LEFT JOIN datalake_monopoly_clean.sale_transaction AS st
-    ON st.id_sale = s.id
-LEFT JOIN datalake_monopoly_clean.income_reference AS ir
-    ON st.id_income_reference = ir.id
-WHERE
-  st.event = 'income-down-payment'
-  AND st.status = 'created'
+LEFT JOIN
+    last_sale_transaction AS st
+        ON st.id_sale = s.id
+        AND rw_sale_transaction = 1
+LEFT JOIN
+    datalake_monopoly_clean.income_reference AS ir
+        ON st.id_income_reference = ir.id
 GROUP BY 1,2,3,5
