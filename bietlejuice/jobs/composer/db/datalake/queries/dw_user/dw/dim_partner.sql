0fa9e5c1197b6432e1d60ac0083caa9f9ee86db5
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-07-06 17:43:27 -0300

    [AEFR-256] Fixing country_code on dim_partner (#8387)
    * [AEFR-256] Fixing country_code on dim_partner
    
    * updating dependencies
    
    * updating lineage

--- bietlejuice/jobs/composer/db/datalake/queries/dw_user/dw/dim_partner.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/dw_user/dw/dim_partner.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT -- [ODS] This table was migrated from ODS flow and needs a futur
   p.id as sk_partner,
   p.id as id_partner,
   p.id_amplitude_device,
-  u.country_code,
+  c.code AS country_code,
   p.name,
   p.trade_name,
   p.phone,
@@ -17,13 +17,16 @@ SELECT DISTINCT -- [ODS] This table was migrated from ODS flow and needs a futur
   p.ts_partnership_started AS ts_joined_partnership,
   p.ts_created,
   p.ts_updated,
-  now() as ts_load
+  NOW() AS ts_load
 FROM
   datalake_ebdb_clean.partner AS p
 LEFT JOIN
   datalake_amplitude_partner_taxonomy.amplitude_partner_taxonomy AS apt
     ON p.id_amplitude_device = apt.id_device
 LEFT JOIN
-  datalake_ebdb_user.user AS u
-    ON u.id = p.id
+  datalake_ebdb_clean.state AS s
+    ON s.id = p.id_state
+LEFT JOIN
+  datalake_ebdb_clean.country AS c
+    ON c.id = s.id_country
 WINDOW w AS (PARTITION BY apt.id_device ORDER BY apt.year, apt.month, apt.DAY ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
