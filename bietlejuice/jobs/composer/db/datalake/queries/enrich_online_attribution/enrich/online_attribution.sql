4016fc5aaad93bde0e3802466ba44d56da9fd560
Henrique Akira Miamoto <56843327+akiramiamoto@users.noreply.github.com>
2022-07-07 08:38:32 -0300

    [GROW-433] Adjusting online_attribution (#8382)
    * online_attribution
    
    * lineage and query
    
    * clause
    
    * some adjusts
    
    * minor
    
    * space

--- bietlejuice/jobs/composer/db/datalake/queries/enrich_online_attribution/enrich/online_attribution.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/enrich_online_attribution/enrich/online_attribution.sql
@@ -89,49 +89,29 @@ ids_mapped_from_conversions AS (
     GROUP BY 1,2,3
 ),
 events_filtered AS (
-    SELECT DISTINCT
-        event_type_sanitized,
-        event_type,
+    SELECT
         ts_event,
+        event_type_sanitized,
         COALESCE(imc.id_user, evt.id_user) AS id_user,
         COALESCE(imc.merged_amplitude_id, evt.id_amplitude) AS id_amplitude,
         id_session,
         id_house,
         id_firestore,
-        amplitude_platform,
         visit_code,
-        entrance_uri,
-        uri_event_property,
+        amplitude_platform,
+        gclid,
         web_medium,
         web_source,
         web_campaign,
         web_content,
         web_term,
-        web_last_medium,
-        web_last_source,
-        web_last_campaign,
-        web_last_content,
-        web_last_term,
-        adjust_app_version,
+        web_platform,
         adjust_adgroup,
         adjust_campaign,
-        adjust_city,
-        adjust_country,
         adjust_creative,
-        adjust_impression_based,
         adjust_reattributed_at,
-        adjust_installed_at,
-        adjust_is_organic,
-        adjust_tracking_enabled,
-        adjust_tracking_limited,
         adjust_network,
-        web_platform,
-        app_platform,
-        gclid,
-        referrer,
-        {year} as year,
-        {month} as month,
-        {day} as day
+        app_platform
     FROM
         datalake_online_attribution.events_exploded AS evt
     JOIN
@@ -139,114 +119,84 @@ events_filtered AS (
             ON imc.id_amplitude = evt.id_amplitude
     WHERE
         DATE(CONCAT_WS("-",evt.year, evt.month, evt.day)) > DATE('{year}-{month}-{day}') - INTERVAL '4' month
+    GROUP BY 
+        1,2,3,4,5,6,7,8,9,10,11,12,
+        13,14,15,16,17,18,19,20,21,22
 ),
-events_mapped AS (
+events_app AS (
     SELECT
         event_type_sanitized,
-        event_type,
         ts_event,
         id_user,
         id_amplitude,
         id_session,
         id_house,
         id_firestore,
-        amplitude_platform,
         visit_code,
-        entrance_uri,
-        uri_event_property,
+        amplitude_platform,
+        adjust_adgroup,
+        adjust_campaign,
+        adjust_creative,
+        adjust_reattributed_at,
+        adjust_network,
+        app_platform,
+        IF((UPPER(adjust_campaign) LIKE '%BRANDED%' OR UPPER(adjust_campaign) LIKE '%INSTITUCIONAL%') AND UPPER(adjust_campaign) NOT LIKE 'NON-BRANDED', 'Branded', 'Outro' ) AS app_branded,
+        FIRST_VALUE(ts_event) OVER (PARTITION BY id_amplitude, adjust_adgroup, adjust_campaign, adjust_creative, adjust_network, adjust_reattributed_at ORDER BY ts_event) AS ts_app_attribution
+    FROM 
+        events_filtered
+    WHERE
+        (adjust_network <> 'Organic')
+        OR (adjust_network = 'Organic' AND adjust_campaign IS NOT NULL)
+),
+events_web AS (
+    SELECT
+        event_type_sanitized,
+        ts_event,
+        id_user,
+        id_amplitude,
+        id_session,
+        id_house,
+        id_firestore,
+        visit_code,
+        amplitude_platform,
+        gclid AS web_gclid,
         web_medium,
         web_source,
         web_campaign,
         web_content,
         web_term,
-        web_last_medium,
-        web_last_source,
-        web_last_campaign,
-        web_last_content,
-        web_last_term,
-        adjust_app_version,
-        adjust_adgroup,
-        adjust_campaign,
-        adjust_city,
-        adjust_country,
-        adjust_creative,
-        adjust_impression_based,
-        adjust_reattributed_at,
-        adjust_installed_at,
-        adjust_is_organic,
-        adjust_tracking_enabled,
-        adjust_tracking_limited,
-        adjust_network,
         web_platform,
-        app_platform,
-        referrer,
-        gclid,
-        referrer,
         IF((UPPER(web_campaign) LIKE '%BRANDED%' OR UPPER(web_campaign) LIKE '%INSTITUCIONAL%') AND UPPER(web_campaign) NOT LIKE 'NON-BRANDED', 'Branded', 'Outro' ) AS web_branded,
-        adjust_network AS app_source,
-        adjust_campaign AS app_medium,
-        COALESCE(
-            NULLIF(
-                CASE
-                    WHEN web_platform in ("web_desktop", "web_mobile") THEN web_source
-                    ELSE adjust_network
-                END,
-                "Organic"
-            ),
-            "organic"
-        ) AS media_source,
-        IF((UPPER(adjust_campaign) LIKE '%BRANDED%' OR UPPER(adjust_campaign) LIKE '%INSTITUCIONAL%') AND UPPER(adjust_campaign) NOT LIKE 'NON-BRANDED', 'Branded', 'Outro' ) AS app_branded,
-        FIRST_VALUE(ts_event) OVER (PARTITION BY id_amplitude, web_medium, web_source, web_campaign, web_term, web_content, gclid ORDER BY ts_event) AS ts_web_attribution,
-        FIRST_VALUE(ts_event) OVER (PARTITION BY id_amplitude, adjust_adgroup, adjust_campaign, adjust_creative, adjust_network, adjust_reattributed_at ORDER BY ts_event) AS ts_app_attribution,
-        year,
-        month,
-        day
+        FIRST_VALUE(ts_event) OVER (PARTITION BY id_amplitude, web_medium, web_source, web_campaign, web_term, web_content, gclid ORDER BY ts_event) AS ts_web_attribution
     FROM 
         events_filtered
 )
 SELECT
-    event_type_sanitized,
-    event_type,
-    id_user,
-    id_amplitude,
-    id_session,
-    id_house,
-    id_firestore,
-    visit_code,
-    amplitude_platform,
-    entrance_uri,
-    uri_event_property,
+    COALESCE(web.id_user, app.id_user) AS id_user,
+    COALESCE(web.id_amplitude, app.id_amplitude) AS id_amplitude,
+    COALESCE(web.id_session, app.id_session) AS id_session,
+    COALESCE(web.id_house, app.id_house) AS id_house,
+    COALESCE(web.id_firestore, app.id_firestore) AS id_firestore,
+    COALESCE(web.visit_code, app.visit_code) AS visit_code,
+    COALESCE(web.amplitude_platform, app.amplitude_platform) AS amplitude_platform,
+    COALESCE(web.event_type_sanitized, app.event_type_sanitized) AS event_type_sanitized,
+    web_gclid,
     web_medium,
     web_source,
     web_campaign,
     web_content,
     web_term,
-    web_last_medium,
-    web_last_source,
-    web_last_campaign,
-    web_last_content,
-    web_last_term,
-    adjust_app_version,
+    web_platform,
+    web_branded,
     adjust_adgroup,
     adjust_campaign,
-    adjust_city,
-    adjust_country,
     adjust_creative,
-    adjust_impression_based,
     adjust_reattributed_at,
-    adjust_installed_at,
-    adjust_is_organic,
-    adjust_tracking_enabled,
-    adjust_tracking_limited,
     adjust_network,
-    referrer,
-    gclid,
-    web_branded,
-    app_source,
-    app_medium,
+    app_platform,
     app_branded,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN app_platform
+        WHEN ts_app_attribution>ts_web_attribution THEN app_platform
         WHEN ts_web_attribution>ts_app_attribution THEN web_platform
         WHEN (
             web_source IS NULL 
@@ -258,7 +208,7 @@ SELECT
         ELSE web_platform
     END AS platform,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN app_source
+        WHEN ts_app_attribution>ts_web_attribution THEN adjust_network
         WHEN ts_web_attribution>ts_app_attribution THEN web_source
         WHEN (
             web_source IS NULL 
@@ -266,11 +216,11 @@ SELECT
             AND web_campaign IS NULL 
             AND web_term IS NULL 
             AND web_content IS NULL
-        ) THEN app_source
+        ) THEN adjust_network
         ELSE web_source
     END AS utm_source,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN app_medium
+        WHEN ts_app_attribution>ts_web_attribution THEN adjust_campaign
         WHEN ts_web_attribution>ts_app_attribution THEN web_medium
         WHEN (
             web_source IS NULL 
@@ -278,11 +228,11 @@ SELECT
             AND web_campaign IS NULL 
             AND web_term IS NULL 
             AND web_content IS NULL
-        ) THEN app_medium
+        ) THEN adjust_campaign
         ELSE web_medium
     END AS utm_medium,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN adjust_campaign
+        WHEN ts_app_attribution>ts_web_attribution THEN adjust_campaign
         WHEN ts_web_attribution>ts_app_attribution THEN web_campaign
         WHEN (
             web_source IS NULL 
@@ -294,7 +244,7 @@ SELECT
         ELSE web_campaign
     END AS utm_campaign,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN adjust_creative
+        WHEN ts_app_attribution>ts_web_attribution THEN adjust_creative
         WHEN ts_web_attribution>ts_app_attribution THEN web_term
         WHEN (
             web_source IS NULL 
@@ -306,7 +256,7 @@ SELECT
         ELSE web_term
     END AS utm_term,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN adjust_adgroup
+        WHEN ts_app_attribution>ts_web_attribution THEN adjust_adgroup
         WHEN ts_web_attribution>ts_app_attribution THEN web_content
         WHEN (
             web_source IS NULL 
@@ -318,7 +268,7 @@ SELECT
         ELSE web_content
     END AS utm_content,
     CASE
-        WHEN ts_app_attribution>ts_web_attribution AND adjust_network != 'Organic' THEN app_branded
+        WHEN ts_app_attribution>ts_web_attribution THEN app_branded
         WHEN ts_web_attribution>ts_app_attribution THEN web_branded
         WHEN (
             web_source IS NULL 
@@ -329,12 +279,22 @@ SELECT
          ) THEN app_branded
         ELSE web_branded
     END AS branded,
-    media_source,
-    ts_event,
+    COALESCE(LOWER(IF(web_platform IN ("web_desktop", "web_mobile"), web_source, adjust_network)), 'organic') AS media_source,
+    COALESCE(web.ts_event, app.ts_event) AS ts_event,
     ts_web_attribution,
     ts_app_attribution,
-    year,
-    month,
-    day
+    {year} AS year,
+    {month} AS month,
+    {day} AS day
 FROM
-    events_mapped
+    events_web web
+    FULL OUTER JOIN events_app app
+        ON web.event_type_sanitized = app.event_type_sanitized
+        AND web.ts_event = app.ts_event
+        AND web.id_user = app.id_user
+        AND web.id_amplitude = app.id_amplitude
+        AND web.id_session = app.id_session 
+        AND web.id_house = app.id_house
+        AND web.id_firestore = app.id_firestore
+        AND web.visit_code = app.visit_code
+        AND web.amplitude_platform = app.amplitude_platform
