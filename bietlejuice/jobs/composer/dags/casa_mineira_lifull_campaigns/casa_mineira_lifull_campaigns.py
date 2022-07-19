0925a13a8460ce1c9792dd7625aa0771ad0d6ecd
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-07-19 19:08:08 -0300

    [GROW-505] Changed cluster and libs install for Lifull Campaigns (#8473)
    * Changed cluster config and DAG structure
    
    * Fixed first tasks list
    
    * Changed connection name
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/casa_mineira_lifull_campaigns/casa_mineira_lifull_campaigns.py
+++ bietlejuice/jobs/composer/dags/casa_mineira_lifull_campaigns/casa_mineira_lifull_campaigns.py
@@ -1,11 +1,10 @@
+import os
 from datetime import datetime
 from pendulum import timezone
-import os
+
+from airflow.models import DAG
+from airflow.utils.helpers import cross_downstream
 from airflow.contrib.operators.awsbatch_operator import AWSBatchOperator
-
-
-from airflow.utils.helpers import chain, cross_downstream
-from airflow.models import DAG, Variable
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -15,38 +14,48 @@ from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTask
 from bietlejuice.jobs.composer.services.configuration_service import (
     ConfigurationService,
 )
+from bietlejuice.jobs.composer.base.databricks import (
+    DatabricksGroupNameEnum,
+    ClusterPermissionEnum,
+)
 
+
+# Pipeline inputs
 SOURCE = "casa_mineira_lifull_campaigns"
 DAG_ID = f"bietlejuice.{SOURCE}"
-ENV = os.environ.get("ENVIRONMENT")
+MAIN_START_DATE = datetime(2021, 8, 26, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = "0 2 * * *"
+CLUSTER_DESCRIPTION = "databricks_10_4_min_general_cluster"
 
 config_service = ConfigurationService(SOURCE)
-athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
-datalake_bucket = config_service.get_config("datalake_bucket")
-databricks_bietlejuice_repo_path = config_service.get_config(
-    "databricks_bietlejuice_repo_path"
-)
-spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
-doc_md_chart_url = config_service.get_config("doc_md_chart_url")
-
 accounts = config_service.get_config("accounts")
 raw_table_name = config_service.get_config("raw_table_name")
+aws_conn_id = config_service.get_config("aws_conn_id")
 raw_output_path = config_service.get_config("raw_output_path")
 clean_tables_list = config_service.get_config("clean_tables_list")
 partition_cols = config_service.get_config("partition_cols")
 
-MAIN_START_DATE = datetime(2021, 8, 26, tzinfo=timezone("America/Sao_Paulo"))
-MAIN_SCHEDULE_INTERVAL = "0 2 * * *"
-BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
-RAW_SPARK_JOB_FILE = (
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+artifacts_bucket = config_service.get_config("artifacts_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+raw_spark_job_file = (
     f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/create_{SOURCE}_raw.py"
 )
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+cluster_configuration = config_service.get_config(CLUSTER_DESCRIPTION)
+default_libraries = config_service.get_config("default_libraries")
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{spark_jobs_logs_path}{DAG_ID}"
+DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
+    {
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+ENV = os.environ.get("ENVIRONMENT")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -55,7 +64,6 @@ dag = DAG(
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
-    catchup=False,
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(SOURCE).format(
@@ -68,12 +76,16 @@ task_group = DatalakeTaskGroup(
     env=ENV,
     datalake_bucket=datalake_bucket,
     relative_query_path=SOURCE,
-    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    spark_jobs_path=base_spark_jobs_path,
     athena_query_result_location=athena_query_results_bucket,
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=cluster_configuration,
+    libraries=default_libraries,
+    access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
@@ -84,11 +96,11 @@ raw_task_group = task_group.build_raw_task_group_for_single_table(
     source=SOURCE,
     target_database_base_name=SOURCE,
     table_name="campaigns_overview_report",
-    extraction_spark_job_file=RAW_SPARK_JOB_FILE,
+    extraction_spark_job_file=raw_spark_job_file,
     raw_spark_job_extra_args=[SOURCE],
 )
 
-chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_group))
+create_cluster_task.set_downstream(DatalakeTaskGroup.first_tasks(raw_task_group))
 
 for account_type, account_info in accounts.items():
     account_id = account_info["account_id"]
@@ -115,11 +127,11 @@ for account_type, account_info in accounts.items():
             "output_path": output_path,
         },
         overrides={},
-        aws_conn_id="aws_prod_data",
+        aws_conn_id=aws_conn_id,
         region_name="us-east-1",
     )
 
-    chain(aws_batch_start_job_task, create_cluster_task)
+    aws_batch_start_job_task.set_downstream(create_cluster_task)
 
 for clean_table_name in clean_tables_list:
     clean_task_group = task_group.build_clean_task_group(
