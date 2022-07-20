1ddbedb79a10cc32e341ec9a6000623fa6a10d87
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-07-20 17:13:49 -0300

    [GROW-414] Changed cluster and libs install for Rene Descartes (#8471)
    * Renamed Spark job
    
    * Changed Doc MD
    
    * Changed cluster type and DAG structure
    
    * Removed unused lib
    
    * Removed unused lib
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/jobs/composer/dags/rene_descartes/rene_descartes.py
+++ bietlejuice/jobs/composer/dags/rene_descartes/rene_descartes.py
@@ -1,58 +1,53 @@
-from datetime import datetime
-import pendulum
 import os
+from datetime import datetime
+from pendulum import timezone
 
-from airflow.models import DAG, Variable
+from airflow.models import DAG
+from airflow.utils.helpers import cross_downstream
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
-from airflow.utils.helpers import chain, cross_downstream
-
-from bietlejuice.jobs.composer.base.airflow import BaseDAG
-from bietlejuice.jobs.composer.base.airflow.dag_owner_enum import DAGOwnerEnum
+from bietlejuice.jobs.composer.base.airflow import BaseDAG, DAGOwnerEnum
+from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.jobs.composer.base.pipeline import LayerEnum
+from bietlejuice.jobs.composer.services.configuration_service import (
+    ConfigurationService,
+)
 from bietlejuice.jobs.composer.base.databricks import (
     DatabricksGroupNameEnum,
     ClusterPermissionEnum,
 )
-from bietlejuice.jobs.composer.base.pipeline import LayerEnum
-from bietlejuice.jobs.composer.dags.base.datalake_task_group import DatalakeTaskGroup
 
-CONTEXT = "rene_descartes"
-DAG_NAME = CONTEXT
+# Pipeline inputs
+SOURCE = "rene_descartes"
+DAG_ID = f"bietlejuice.{SOURCE}"
+MAIN_START_DATE = datetime(2020, 7, 27, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = "0 0 * * *"
+CLUSTER_DESCRIPTION = "databricks_10_4_med_general_cluster"
 
-# airflow vars
-ENV = os.environ.get("ENVIRONMENT")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
-DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
-
-RAW_SPARK_JOB_PATH = (
-    S3_PREFIX + f"/spark_jobs/{DAG_NAME}/load_rene_descartes_into_datalake.py"
+config_service = ConfigurationService(SOURCE)
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+datalake_bucket = config_service.get_config("datalake_bucket")
+artifacts_bucket = config_service.get_config("artifacts_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
 )
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+raw_spark_job_file = (
+    f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/load_{SOURCE}_raw.py"
+)
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+cluster_configuration = config_service.get_config(CLUSTER_DESCRIPTION)
+default_libraries = config_service.get_config("default_libraries")
 
-# spark and databricks vars
-BASE_SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/base/"
-LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_NAME}"
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
         "permission_level": ClusterPermissionEnum.MANAGE,
     }
 ]
-
-# dag vars
-DAG_ID = f"bietlejuice.{DAG_NAME}"
-LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
-MAIN_START_DATE = datetime(2020, 7, 27, 0, 0, 0, tzinfo=LOCAL_TZ)
-MAIN_SCHEDULE_INTERVAL = "0 0 * * *"
-
-local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
+ENV = os.environ.get("ENVIRONMENT")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -63,16 +58,16 @@ dag = DAG(
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
-        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
-
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag,
     task_id="create-cluster",
-    cluster_configuration=CLUSTER_DESCRIPTION,
+    cluster_configuration=cluster_configuration,
+    libraries=default_libraries,
     access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
 )
 
@@ -80,29 +75,28 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-task_group = DatalakeTaskGroup(
+datalake_task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
-    relative_query_path=CONTEXT,
-    spark_jobs_path=BASE_SPARK_JOB_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    datalake_bucket=datalake_bucket,
+    relative_query_path=SOURCE,
+    spark_jobs_path=base_spark_jobs_path,
+    athena_query_result_location=athena_query_results_bucket,
 )
 
-
-raw_task_group = task_group.build_raw_task_group_for_all_tables(
-    source=CONTEXT,
-    target_database_base_name=CONTEXT,
-    extraction_spark_job_file=RAW_SPARK_JOB_PATH,
+raw_task_group = datalake_task_group.build_raw_task_group_for_all_tables(
+    source=SOURCE,
+    target_database_base_name=SOURCE,
+    extraction_spark_job_file=raw_spark_job_file,
 )
 
-clean_task_groups = task_group.build_task_group_from_sql_files(
+clean_task_groups = datalake_task_group.build_task_group_from_sql_files(
     layer=LayerEnum.CLEAN,
-    source_database_base_name=CONTEXT,
-    target_database_base_name=CONTEXT,
+    source_database_base_name=SOURCE,
+    target_database_base_name=SOURCE,
 )
 
-chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_group))
+create_cluster_task.set_downstream(DatalakeTaskGroup.first_tasks(raw_task_group))
 
 cross_downstream(
     DatalakeTaskGroup.last_tasks(raw_task_group),
