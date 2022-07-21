109396914aa6f018d7ea166658cb8693e3601602
Ana Alves <ana.sevlaraujo@gmail.com>
2022-07-21 10:52:49 -0300

    added cast to float (#8498)

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/rental_tof_monthly_targets.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/rental_tof_monthly_targets.sql
@@ -3,7 +3,7 @@ SELECT
     mkt_channel,
     mkt_medium,
     mkt_source,
-    tof_users_target,
+    CAST(replace(tof_users_target, ',', '') AS FLOAT) AS tof_users_target,
     TO_DATE(month_start, 'yyyy-MM-dd') AS dt_month_started
 FROM
     datalake_gsheets_raw.rental_tof_monthly_targets
