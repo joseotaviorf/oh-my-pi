4f96e294f89521a5f0f9f09d61e6580a9b1fe0c6
Pedro Henrique Domingos <pedro.domingos@quintoandar.com.br>
2022-07-21 08:45:38 -0300

    [DAS&S] Add historic regional inspection q2-2022 (#8477)
    * Add historic regional inspection q2-2022
    
    * Add line at the end of file

--- bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/regional_inspection_q1_2022.sql
+++ bietlejuice/jobs/composer/db/datalake/queries/gsheets/clean/static/regional_inspection_q1_2022.sql
@@ -1,6 +1,8 @@
 SELECT
     CAST(id AS BIGINT) AS id,
     neighbourhood,
-    regional_inspection
+    regional_inspection_q1_2022,
+    regional_inspection_q2_2022
 FROM
     datalake_gsheets_raw.regional_inspection_q1_2022
+    
