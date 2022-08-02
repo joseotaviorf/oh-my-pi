0df1970bb97b18d08f01cdad3da499dabe27b32c
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-08-02 12:13:56 -0300

    [AEFR-354] Adding new id column to Rene's house_lead_aud table (#8585)

--- bietlejuice/db/datalake/queries/rene_descartes/clean/house_lead_aud.sql
+++ bietlejuice/db/datalake/queries/rene_descartes/clean/house_lead_aud.sql
@@ -5,6 +5,7 @@ SELECT
     address_id AS id_address,
     referred_by AS id_referred_by,
     external_reference_id AS id_external_reference,
+    new_id AS id_lead_ebdb,
     external_reference_name,
     reason,
     status,
