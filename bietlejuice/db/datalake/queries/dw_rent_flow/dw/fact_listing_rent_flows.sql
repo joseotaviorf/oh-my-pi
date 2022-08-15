7e56b862077ae519750c29a48c1f90ef7efc6a22
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-08-15 18:28:25 -0300

    [AEFR-268] Mask to remove wrong association between contracts and listings (#8656)
    * Applying a mask to force listing to be associated with a contract as priority instead of rent_flow

--- bietlejuice/db/datalake/queries/dw_rent_flow/dw/fact_listing_rent_flows.sql
+++ bietlejuice/db/datalake/queries/dw_rent_flow/dw/fact_listing_rent_flows.sql
@@ -18,7 +18,7 @@ WITH listing_rent_flows AS (
                     rent_flow.id_house ||
                     LPAD(
                         COALESCE(
-                            CAST(dim_house_listing.version AS VARCHAR(3)),
+                            CAST(COALESCE(dhl_contract.version, dim_house_listing.version) AS VARCHAR(3)),
                             '1'
                         ),
                         3,
@@ -36,7 +36,7 @@ WITH listing_rent_flows AS (
             COALESCE(rent_flow.id_visit, -1) AS sk_visit,
             COALESCE(dim_offer.sk_offer, -1) AS sk_offer,
             COALESCE(rent_flow.id_proposal, -1) AS sk_proposal,
-            COALESCE(rent_flow.id_contract, -1) AS sk_contract,
+            COALESCE(dim_contract.sk_contract, -1) AS sk_contract,
             COALESCE(reservation.id_reservation, -1) AS sk_reservation,
             dim_booking.sk_rent_flow_taxonomy,
             dim_proposal.status AS proposal_status,
@@ -118,9 +118,9 @@ WITH listing_rent_flows AS (
             COALESCE(CAST(DATE_FORMAT(rent_flow.dt_proposal_approved, "yyyyMMdd") AS BIGINT), -1) AS sk_proposal_approved_date,
             COALESCE(CAST(DATE_FORMAT(dim_proposal.dt_tenant_first_document_sent, "yyyyMMdd") AS BIGINT), -1) AS sk_tenant_manual_first_doc_sent_date,
             COALESCE(CAST(DATE_FORMAT(dim_proposal.dt_tenant_auto_first_doc_sent, "yyyyMMdd") AS BIGINT), -1) AS sk_tenant_auto_first_doc_sent_date,
-            COALESCE(CAST(DATE_FORMAT(rent_flow.dt_contract_created, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_created_date,
-            COALESCE(CAST(DATE_FORMAT(rent_flow.dt_contract_signed, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_signed_date,
-            COALESCE(CAST(DATE_FORMAT(rent_flow.dt_contract_annulment, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_annulment_date,
+            COALESCE(CAST(DATE_FORMAT(dim_contract.ts_created, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_created_date,
+            COALESCE(CAST(DATE_FORMAT(dim_contract.ts_signature, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_signed_date,
+            COALESCE(CAST(DATE_FORMAT(dim_contract.dt_annulment, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_annulment_date,
             COALESCE(CAST(DATE_FORMAT(dim_contract.ts_canceled, "yyyyMMdd") AS BIGINT), -1) AS sk_contract_canceled_date,
             COALESCE(CAST(DATE_FORMAT(dim_proposal.dt_credit_analysis_first_init, "yyyyMMdd") AS BIGINT), -1) AS sk_credit_analysis_first_init_date,
             COALESCE(CAST(DATE_FORMAT(dim_proposal.dt_credit_analysis_last_init, "yyyyMMdd") AS BIGINT), -1) AS sk_credit_analysis_last_init_date,
@@ -149,8 +149,8 @@ WITH listing_rent_flows AS (
             COALESCE(CAST(DATE_FORMAT(reservation.ts_created, "yyyyMMdd") AS BIGINT), -1) AS sk_reservation_created_date,
             CAST(reservation.reservation_attempts AS SMALLINT) AS reservation_attempts,
             CAST(NOW() AS TIMESTAMP) AS ts_load
-        FROM datalake_ebdb_rent_flow.rent_flow
-        JOIN dw_public.dim_house_listing
+        FROM datalake_ebdb_rent_flow.rent_flow 
+        JOIN dw_public.dim_house_listing -- 1:M (M rent_flow x 1 listing)
             ON dim_house_listing.id_house = rent_flow.id_house
             AND COALESCE(rent_flow.dt_rent_flow_created, '1900-01-01') BETWEEN
                 COALESCE(dim_house_listing.ts_listing_version_start, '1900-01-01')
@@ -162,8 +162,25 @@ WITH listing_rent_flows AS (
             AND dim_offer.sk_offer != -1
         LEFT JOIN dw_public.dim_proposal
             ON rent_flow.id_proposal = dim_proposal.id_proposal
-        LEFT JOIN dw_public.dim_contract
-            ON rent_flow.id_contract = dim_contract.id_contract
+        LEFT JOIN
+            datalake_ebdb_clean.contract AS con
+                ON con.id_house = rent_flow.id_house
+                AND con.id = rent_flow.id_contract
+        LEFT JOIN 
+            dw_public.dim_contract
+                ON dim_contract.sk_contract = COALESCE(con.id, -1)
+        LEFT JOIN
+        /*
+        dim_house_listing can be associated with the rent_flow 
+        or with the contract period. As we know nowdays, a rent_flow can
+        starts before the listing starts and it is generating 
+        an erronous relationship with listing. To solve this while
+        we align with SWE a permanent solution, we are giving priority
+        to contract association with listing, instead of rent_flow.
+        */
+            dw_public.dim_house_listing AS dhl_contract
+                ON con.id_house = dhl_contract.id_house 
+                AND dim_contract.ts_created BETWEEN COALESCE(dhl_contract.ts_listing_version_start, '2000-01-01 00:00:00') AND COALESCE(dhl_contract.ts_listing_version_end, CURRENT_DATE)
         LEFT JOIN datalake_ebdb_agents.agents_review ar
             ON rent_flow.id_booking = ar.id_booking
         LEFT JOIN reservation
