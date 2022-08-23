4d6281842b6fdd911a927e883c50f91c4a96d91c
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2022-08-23 13:57:57 -0300

    updating datamart clusters resources (#8757)

--- bietlejuice/dags/dw_datamarts/cross/dw_datamarts_cross.py
+++ bietlejuice/dags/dw_datamarts/cross/dw_datamarts_cross.py
@@ -45,7 +45,7 @@ BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 
 # cluster setup
 CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_9_1_med_general_cluster", deserialize_json=True
+    "databricks_10_4_med_io-memory_cluster", deserialize_json=True
 )
 CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
     "destination"
