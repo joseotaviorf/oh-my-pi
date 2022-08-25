399f96cfa8aab633b99b253b4757aa28a7a406ea
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-08-25 13:28:21 -0300

    [AES&S] Delete assessment_aud table and update assessment table (#8667)
    * Updated assessment
    
    * Delete assessment_aud
    
    * Moved sql and yml files for full folder
    
    * fix: incremental table
    
    * remove incremental folder
    
    * fix: date incremental column
    
    * fix: merge conflicts
    
    * add new column: house_supplies

--- bietlejuice/db/datalake/queries/inspections/clean/assessment.sql
+++ bietlejuice/db/datalake/queries/inspections/clean/assessment.sql
@@ -1,13 +1,22 @@
 SELECT
-    id,
+    id AS id_assessment,
     inspection_id AS id_inspection,
     previous_assessment_id AS id_previous_assessment,
     key_location,
     key_location_details,
+    source,
+    house_supplies,
     limit_revision_date AS dt_revision_limit,
     created_at AS ts_created,
     finished_at AS ts_finished,
     started_at AS ts_started,
-    updated_at AS ts_updated
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
 FROM
     datalake_inspections_raw.assessment
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
