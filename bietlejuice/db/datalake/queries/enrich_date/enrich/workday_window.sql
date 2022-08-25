b0234f837cabe89da29d9e89a318b92e964cbf79
William Angelo <39243586+williamangelo@users.noreply.github.com>
2022-08-25 15:43:41 -0300

    [AESS-438] New column on workday_window table (#8775)
    * feat: add extra column to workday_window table
    
    Also lints .sql file removing trailing spaces
    
    * fix: missing value on where clause
    
    * fix: remove test file
    
    * fix: update lineage file

--- bietlejuice/db/datalake/queries/enrich_date/enrich/workday_window.sql
+++ bietlejuice/db/datalake/queries/enrich_date/enrich/workday_window.sql
@@ -1,26 +1,26 @@
 WITH cities_calendar AS (
-    SELECT 
+    SELECT
         r.id AS id_city,
         r.name AS city,
         ad.id_date,
-        CASE 
-            WHEN ad.weekend = 'Weekend' OR ad.is_brz_holiday = 'Holiday' OR sch.dt_holiday IS NOT NULL THEN FALSE 
+        CASE
+            WHEN ad.weekend = 'Weekend' OR ad.is_brz_holiday = 'Holiday' OR sch.dt_holiday IS NOT NULL THEN FALSE
             ELSE TRUE
         END is_workday,
         date AS dt_reference
-    FROM 
+    FROM
         datalake_region.region r
     CROSS JOIN
         datalake_quintoandar.aux_date ad
     LEFT JOIN
         datalake_gsheets_clean.service_city_holidays sch
-            ON ad.date = sch.dt_holiday 
+            ON ad.date = sch.dt_holiday
             AND (r.id = sch.id_city OR sch.category = 'Nacional')
-    WHERE 
+    WHERE
         r.level = 'Cidade'
 ),
 workday_calendar AS (
-    SELECT 
+    SELECT
         cc.id_date,
         cc.id_city,
         cc.city,
@@ -28,16 +28,16 @@ workday_calendar AS (
         cc.is_workday,
         DATE(cc.dt_reference) AS dt_reference,
         DATE(w.dt_reference) AS dt_workday
-    FROM 
+    FROM
         cities_calendar cc
-    LEFT JOIN 
-        cities_calendar w 
+    LEFT JOIN
+        cities_calendar w
             ON cc.id_city = w.id_city
-            AND w.dt_reference BETWEEN cc.dt_reference AND cc.dt_reference + interval '25' DAY	
+            AND w.dt_reference BETWEEN cc.dt_reference AND cc.dt_reference + interval '25' DAY
     WHERE
         w.is_workday = TRUE
 )
-SELECT 
+SELECT
     CAST(id_date AS BIGINT) AS id_date,
     CAST(id_city AS BIGINT) AS id_city,
     city,
@@ -50,10 +50,11 @@ SELECT
     MAX(CASE WHEN count_workdays = 6 THEN dt_workday ELSE NULL END) AS dt_end_5,
     MAX(CASE WHEN count_workdays = 7 THEN dt_workday ELSE NULL END) AS dt_end_6,
     MAX(CASE WHEN count_workdays = 8 THEN dt_workday ELSE NULL END) AS dt_end_7,
+    MAX(CASE WHEN count_workdays = 9 THEN dt_workday ELSE NULL END) AS dt_end_8,
     MAX(CASE WHEN count_workdays = 16 THEN dt_workday ELSE NULL END) AS dt_end_15
-FROM 
+FROM
     workday_calendar
-WHERE 
-    count_workdays IN (1, 2, 3, 4, 5, 6, 7, 8, 16)
-GROUP BY 
+WHERE
+    count_workdays IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 16)
+GROUP BY
     1, 2, 3, 4
