6d4ac736b5c7e5cf8eaaf9d0416ec0ca817de9bd
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-08-25 13:12:30 -0300

    [AES&S] Update item_type and item_group_type tables (#8669)
    * Updated item_type
    
    * Updated item_group_type
    
    * Moved sql and yml files for incremental folder
    
    * remove incremental folder
    
    * updated lineages
    
    * fix: date incremental column

--- bietlejuice/db/datalake/queries/inspections/clean/item_group_type.sql
+++ bietlejuice/db/datalake/queries/inspections/clean/item_group_type.sql
@@ -1,7 +1,16 @@
 SELECT
-    id,
+    id AS id_item_group_type,
     type,
+    deletable AS is_deletable,
+    deleted AS is_deleted,
     created_at AS ts_created,
-    updated_at AS ts_updated
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
 FROM
     datalake_inspections_raw.item_group_type
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
--- bietlejuice/db/datalake/queries/inspections/clean/item_type.sql
+++ bietlejuice/db/datalake/queries/inspections/clean/item_type.sql
@@ -1,5 +1,5 @@
 SELECT
-    id,
+    id AS id_item_type,
     item_group_type_id AS id_item_group_type,
     display_type,
     media_type,
@@ -7,6 +7,13 @@ SELECT
     allow_comments,
     allow_media,
     created_at AS ts_created,
-    updated_at AS ts_updated
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
 FROM
     datalake_inspections_raw.item_type
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
