c9417a71e1d21131eb9aadd102953a6bf19a6bba
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-08-29 10:38:41 -0300

    [AEFF-446] Fix nazaré typo (#8805)
    * Fix name typo

--- bietlejuice/db/datalake/queries/reverse_nazare/reverse/offers.sql
+++ bietlejuice/db/datalake/queries/reverse_nazare/reverse/offers.sql
@@ -1,5 +1,4 @@
-WITH
-cte_demand AS (
+WITH cte_demand AS (
     WITH agents_3p AS (
         SELECT
             ac.id_agent,
@@ -9,56 +8,44 @@ cte_demand AS (
             wc.is_3p_contract
         FROM
             datalake_ebdb_agents.agent_contract AS ac
-        JOIN
-            datalake_ebdb_work_contract.work_contract AS wc
-                ON ac.id_work_contract = wc.id
+            JOIN datalake_ebdb_work_contract.work_contract AS wc ON ac.id_work_contract = wc.id
         WHERE
             is_3p_contract
     ),
     demand_3p AS (
-        SELECT DISTINCT
-            db.sk_booking,
+        SELECT
+            DISTINCT db.sk_booking,
             ap.demand_3p_partner,
             ap.is_3p_contract AS is_3p_demand
         FROM
             agents_3p AS ap
-        JOIN
-            dw_public.dim_booking AS db
-                ON ap.id_agent = db.id_agent
-                AND db.dt_created BETWEEN ap.ts_work_contract_started AND COALESCE(ts_work_contract_ended, db.ts_load)
+            JOIN dw_public.dim_booking AS db ON ap.id_agent = db.id_agent
+            AND db.dt_created BETWEEN ap.ts_work_contract_started
+            AND COALESCE(ts_work_contract_ended, db.ts_load)
     )
     SELECT
         fo.sk_offer,
         COALESCE(dp.is_3p_demand, False) AS is_3p_demand
     FROM
         dw_sale.fact_offers AS fo
-    LEFT JOIN
-        demand_3p AS dp
-            ON fo.sk_booking = dp.sk_booking
-    LEFT JOIN
-        datalake_gsheets_clean.forbrokers_3p_partner_conditions pc
-            ON UPPER(dp.demand_3p_partner) = UPPER(pc.partner_short_name)
-    WHERE TRIM(is_3p_demand) = 'true'
+        LEFT JOIN demand_3p AS dp ON fo.sk_booking = dp.sk_booking
+        LEFT JOIN datalake_gsheets_clean.forbrokers_3p_partner_conditions pc ON UPPER(dp.demand_3p_partner) = UPPER(pc.partner_short_name)
+    WHERE
+        TRIM(is_3p_demand) = 'true'
 ),
-
 cte_categoria AS (
     SELECT
         fo.sk_offer,
         CASE
-            WHEN da.is_3p_demand = 'true' AND da.is_3p_supply = 'true'
-                THEN '6P'
             WHEN da.is_3p_demand = 'true'
-                THEN '3P - Demand'
-            WHEN da.is_3p_supply = 'true'
-                THEN '3P - Supply'
+            AND da.is_3p_supply = 'true' THEN '6P'
+            WHEN da.is_3p_demand = 'true' THEN '3P - Demand'
+            WHEN da.is_3p_supply = 'true' THEN '3P - Supply'
         END AS tipo
     FROM
         dw_sale.fact_offers fo
-    LEFT JOIN
-        dw_sale.dim_sale_agreement da
-            ON da.sk_offer = fo.sk_offer
+        LEFT JOIN dw_sale.dim_sale_agreement da ON da.sk_offer = fo.sk_offer
 ),
-
 cte_categoria_filtro AS (
     SELECT
         cc.sk_offer,
@@ -67,20 +54,25 @@ cte_categoria_filtro AS (
             WHEN cc.tipo IS NOT NULL THEN cc.tipo
             ELSE NULL
         END AS partner_types
-        FROM
+    FROM
         cte_categoria cc
-    LEFT JOIN
-        cte_demand demand
-            ON demand.sk_offer = cc.sk_offer
-
+        LEFT JOIN cte_demand demand ON demand.sk_offer = cc.sk_offer
 )
-
 SELECT
     fo.sk_offer AS external_id,
     fo.sk_house AS house_id,
     CASE
-        WHEN dof.business_unit LIKE '%[3P%' and dr.city_group IN ('Rio de Janeiro')                                                                               THEN '22'
-        WHEN dof.business_unit LIKE '%[3P%' and dr.city_group IN ('RMSP', 'Ribeirão Preto', 'Santos', 'Sorocaba', 'São José do Rio Preto', 'São José dos Campos') THEN '2'
+        WHEN dof.business_unit LIKE '%[3P%'
+        AND dr.city_group IN ('Rio de Janeiro') THEN '22'
+        WHEN dof.business_unit LIKE '%[3P%'
+        AND dr.city_group IN (
+            'RMSP',
+            'Ribeirão Preto',
+            'Santos',
+            'Sorocaba',
+            'São José do Rio Preto',
+            'São José dos Campos'
+        ) THEN '2'
         ELSE fo.sk_business_unit
     END AS business_unit_id,
     cc.partner_types AS category,
@@ -91,33 +83,21 @@ SELECT
     dh.number AS house_number,
     dh.zipcode AS house_zipcode,
     ds.sale_price_agreed AS price_agreed,
-    ds.brokerage_fee AS brokage,
+    ds.brokerage_fee AS brokerage,
     ds.sale_agreement_cancellation_reason AS cancellation_reason,
     ds.ts_sale_agreement_cancelled AS cancellation_date,
     TO_DATE(STRING(fc.sk_payment_allowed_date), 'yyyyMMdd') AS payment_allowed_date,
     DATE(ds.ts_sale_agreement_signed) AS signature_date,
     YEAR(CURRENT_DATE) AS year,
-    MONTH(CURRENT_DATE) AS month,
-    DAY(CURRENT_DATE) AS day
+    MONTH(CURRENT_DATE) AS MONTH,
+    DAY(CURRENT_DATE) AS DAY
 FROM
     dw_sale.dim_offer dof
-LEFT JOIN
-    dw_sale.fact_offers fo
-        ON fo.sk_offer = dof.sk_offer
-LEFT JOIN
-    dw_sale.fact_closing_flows fc
-        ON fc.sk_offer = dof.sk_offer
-LEFT JOIN
-    dw_quintoandar.dim_house dh
-        ON fo.sk_house = dh.sk_house
-LEFT JOIN
-    dw_sale.dim_sale_agreement ds
-        ON ds.sk_offer = dof.sk_offer
-LEFT JOIN
-    cte_categoria_filtro cc
-        ON cc.sk_offer = dof.sk_offer
-LEFT JOIN
-    dw_public.dim_region dr
-        ON dr.sk_region = fo.sk_region
+    LEFT JOIN dw_sale.fact_offers fo ON fo.sk_offer = dof.sk_offer
+    LEFT JOIN dw_sale.fact_closing_flows fc ON fc.sk_offer = dof.sk_offer
+    LEFT JOIN dw_quintoandar.dim_house dh ON fo.sk_house = dh.sk_house
+    LEFT JOIN dw_sale.dim_sale_agreement ds ON ds.sk_offer = dof.sk_offer
+    LEFT JOIN cte_categoria_filtro cc ON cc.sk_offer = dof.sk_offer
+    LEFT JOIN dw_public.dim_region dr ON dr.sk_region = fo.sk_region
 WHERE
     ds.ts_sale_agreement_signed IS NOT NULL
