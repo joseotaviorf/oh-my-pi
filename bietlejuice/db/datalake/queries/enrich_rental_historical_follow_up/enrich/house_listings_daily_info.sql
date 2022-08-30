4860480071a518b0f2f56dd2b986a41a12acbeea
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-08-30 14:58:11 -0300

    [AEFR-433] Reviewing house_listings_daily_info logic (#8799)
    * reviewing house_listings_daily_info logic

--- bietlejuice/db/datalake/queries/enrich_rental_historical_follow_up/enrich/house_listings_daily_info.sql
+++ bietlejuice/db/datalake/queries/enrich_rental_historical_follow_up/enrich/house_listings_daily_info.sql
@@ -23,8 +23,37 @@ lbc AS (
     FROM 
         datalake_ebdb_listing.listing_business_context
     GROUP BY 1
+),
+last_ciq_of_day AS (
+    /*
+    One house can have multiples agencies assigned in the same day, so we could not guarantee
+    one status per day. To do so, we are assuming that only the last status/agency
+    of this house in a day will be considered. We aligned with SWE
+    and the rule is programs are excludent, so there is only
+    one program:house per time. 
+    For the SELECT program, we are not sure yet, but for CIQ/ASP this is valid.
+    
+    In addition, there are periods around June - 2021 (In our auds) when two programs exists at
+    the same time. It's an error, and we are treating it in this CTE.
+    
+    */
+    SELECT /*+ RANGE_JOIN(hch, 340) */
+        id_house,
+        consultant_type,
+        MAX(rev) OVER(PARTITION BY id_house, dbase.dt_day) = rev AS is_last_status_house_of_day,
+        dbase.dt_day,
+        ts_enrollment_started,
+        ts_enrollment_ended
+    FROM
+        datalake_big_agent.house_consultant_history AS hch
+    JOIN
+        daily_base AS dbase
+            ON dbase.dt_day >= DATE(hch.ts_enrollment_started)
+            AND dbase.dt_day < COALESCE(DATE(hch.ts_enrollment_ended), '2100-01-01')
+    WHERE
+        hch.is_last_status_of_day = True
 )
-SELECT /*+ RANGE_JOIN(hch, 340), RANGE_JOIN(heh, 1180) */
+SELECT /*+ RANGE_JOIN(heh, 1180) */
     CONCAT(hl.id_house_listing, DATE_FORMAT(dbase.dt_day, 'yMMdd')) AS id_house_listing_day,
     hl.id_house_listing,
     hl.id_house,
@@ -32,7 +61,7 @@ SELECT /*+ RANGE_JOIN(hch, 340), RANGE_JOIN(heh, 1180) */
     hl.id_contract,
     heh.id_occupant,
     h.id_region,
-    hch.consultant_type,
+    lcod.consultant_type,
     heh.doorman_type,
     awk.first_key_location,
     hls.status_history,
@@ -55,29 +84,29 @@ SELECT /*+ RANGE_JOIN(hch, 340), RANGE_JOIN(heh, 1180) */
     dbase.month,
     dbase.day
 FROM
-    datalake_ebdb_listing.house_listing AS hl
+    datalake_ebdb_listing.house_listing AS hl -- id_house_listing is PK
+JOIN
+    datalake_ebdb_listing.house_listing_status AS hls -- 1:M => 1 listing has M statuses 
+        ON hl.id_house_listing = hls.id_house_listing
+        AND hls.is_last_status_of_day = True
 JOIN
     daily_base AS dbase
-        ON dbase.dt_day >= DATE(hl.ts_listing_version_start)
-        AND dbase.dt_day < COALESCE(DATE(hl.ts_listing_version_end), '2100-01-01')
-LEFT JOIN 
+        ON ((dbase.dt_day >= DATE(hls.ts_status_started)
+              AND dbase.dt_day < COALESCE(DATE(hls.ts_status_ended), '2100-01-01'))
+              OR (dbase.dt_day = DATE(hls.ts_status_started)
+                  AND dbase.dt_day = DATE(hls.ts_status_ended))
+             )
+LEFT JOIN
     lbc
         ON lbc.id_house = hl.id_house
 LEFT JOIN
     datalake_ebdb_listing.house AS h
         ON hl.id_house = h.id
 LEFT JOIN
-    datalake_ebdb_listing.house_listing_status AS hls
-        ON hl.id_house_listing = hls.id_house_listing
-        AND dbase.dt_day >= DATE(hls.ts_status_started)
-        AND dbase.dt_day < COALESCE(DATE(hls.ts_status_ended), '2100-01-01')
-        AND hls.is_last_status_of_day = True
-LEFT JOIN
-    datalake_big_agent.house_consultant_history AS hch
-        ON hl.id_house = hch.id_house
-        AND dbase.dt_day >= DATE(hch.ts_enrollment_started)
-        AND dbase.dt_day < COALESCE(DATE(hch.ts_enrollment_ended), '2100-01-01')
-        AND hch.is_last_status_of_day = True
+    last_ciq_of_day AS lcod
+        ON hl.id_house = lcod.id_house
+        AND dbase.dt_day = lcod.dt_day
+        AND lcod.is_last_status_house_of_day = True
 LEFT JOIN
     datalake_ebdb_listing.house_entrance_history AS heh
         ON hl.id_house = heh.id_house
