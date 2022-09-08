6ac6bb351388b981af5829a8a1d9c04765b49e05
Thiago Felipe Nogueira De Jesus <thiago.nogueira@ufba.br>
2022-09-08 09:32:37 -0300

    [DFORSALE-2798] Update dw_tracksale and sale_potential_listings clusters configs (#8864)
    * Update dw_tracksale and enrich_sale_potential_listings cluster configs

--- bietlejuice/dags/enrich_sale_potential_listing/enrich_sale_potential_listing.py
+++ bietlejuice/dags/enrich_sale_potential_listing/enrich_sale_potential_listing.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 import pendulum
-from airflow.models import DAG, Variable
+from airflow.models import DAG
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -30,17 +30,11 @@ datalake_bucket = config_service.get_config("datalake_bucket")
 databricks_bietlejuice_repo_path = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
-spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
 SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
 
-CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_mid_resources_cluster", deserialize_json=True
-)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{spark_jobs_logs_path}{DAG_ID}"
+CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_general_cluster")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
