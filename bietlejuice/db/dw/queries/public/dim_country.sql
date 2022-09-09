7f92ca48f42ed72015dc9da345ddd8a34970bda0
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2022-09-09 11:05:56 -0300

    [AEFR] Adding null line for dim_country (#8906)

--- bietlejuice/db/dw/queries/public/dim_country.sql
+++ bietlejuice/db/dw/queries/public/dim_country.sql
@@ -1,12 +1,23 @@
 SELECT
-    COALESCE(CAST(id AS INTEGER), -1) AS sk_country,
+    CAST(id AS INTEGER) AS sk_country,
     CAST(id AS INTEGER) AS id_country,
     code AS country_code,
     name AS country_name,
     default_locale,
-    COALESCE(default_timezone, 'UTC') AS default_timezone,
+    default_timezone,
     ts_created,
     ts_updated,
     NOW() AS ts_load
 FROM
     datalake_ebdb_clean.country
+UNION ALL
+SELECT
+    -1 AS sk_country,
+    NULL AS id_country,
+    NULL AS country_code,
+    NULL AS country_name,
+    NULL AS default_locale,
+    'UTC' AS default_timezone,
+    NULL AS ts_created,
+    NULL AS ts_updated,
+    NOW() AS ts_load
