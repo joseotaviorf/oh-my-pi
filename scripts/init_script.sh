e01d27404edcf11fdb9455fc72f98c63f3156f09
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-09-12 13:55:19 -0300

    [DIN-40] Added Spark plugins for cluster monitoring (#8912)
    * Added new init_script with Maven and Spark plugins
    
    * Added test configs into forno_conf
    
    * Added metrics configs into prod_conf
    
    * Replaced forno paths by prod paths into init_script
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

---
+++ scripts/init_script.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# This script is used for initializing clusters dependencies in Databricks
+# and it is manually placed inside 5a-artifacts/bi-etl-ejuice
+
+echo "BEGIN: Modify Spark config settings"
+apt-get install maven -yqq
+
+spark_jars_path="/databricks/jars"
+spark_driver_config_file="$DB_HOME/driver/conf/00-spark-plugin-driver-defaults.conf"
+spark_driver_config_content="[driver] {
+    \"spark.plugins\" = \"ch.cern.CloudFSMetrics,ch.cern.CgroupMetrics\",
+    \"spark.cernSparkPlugin.cloudFsName\" = \"s3a\",
+    \"spark.extraListeners\" = \"ch.cern.sparkmeasure.FlightRecorderStageMetrics\"
+}
+"
+jars=(\
+    ch.cern.sparkmeasure:spark-plugins_2.12:0.2\
+    ch.cern.sparkmeasure:spark-measure_2.12:0.21\
+    com.amazon.deequ:deequ:1.2.2-spark-3.0
+)
+for artifact in "${jars[@]}"
+    do
+        mvn dependency:get -q -Dartifact="$artifact"
+        mvn dependency:copy -q -Dartifact="$artifact" -DoutputDirectory="$spark_jars_path"
+    done
+
+echo "$spark_driver_config_content" | tee $spark_driver_config_file 1>/dev/null
+echo "END: Modify Spark config settings"
+
+echo "BEGIN: Installing QuintoAndar internal libs"
+/databricks/python/bin/pip install -q awscli
+
+aws s3 cp s3://artifacts.s3.data.quintoandar.com.br/bi-etl-ejuice/bi_etl_ejuice-latest-py3-none-any.whl /bi_etl_ejuice-latest-py3-none-any.whl
+aws s3 cp s3://artifacts.s3.data.quintoandar.com.br/python-logger/quintoandar_logger-0.8.0-py3-none-any.whl /quintoandar_logger-0.8.0-py3-none-any.whl
+aws s3 cp s3://artifacts.s3.data.quintoandar.com.br/inmetro/inmetro-2.2.5-py3-none-any.whl /inmetro-2.2.5-py3-none-any.whl
+
+/databricks/python/bin/pip install -q /bi_etl_ejuice-latest-py3-none-any.whl
+/databricks/python/bin/pip install -q /quintoandar_logger-0.8.0-py3-none-any.whl
+/databricks/python/bin/pip install -q '/inmetro-2.2.5-py3-none-any.whl[pydeequ]'
+echo "END: Installing QuintoAndar internal libs"
