53df6b386d6cd7b8114a66a722ee54c8291b3964
André Rugere Oliveira Jorge <andrerugere@gmail.com>
2022-09-12 08:19:22 -0300

    [DEI-1275] Fixing specialist clean query (#8913)

--- bietlejuice/db/datalake/queries/sales_flow/clean/specialist.sql
+++ bietlejuice/db/datalake/queries/sales_flow/clean/specialist.sql
@@ -1,11 +1,8 @@
 SELECT
     id AS id_specialist,
     sales_flow_id AS id_sales_flow,
-    main_user_id AS id_main_user,
     user_id AS id_user,
     kind,
-    email,
-    name AS specialist_name,
     created_at AS ts_created,
     updated_at AS ts_updated
 FROM
