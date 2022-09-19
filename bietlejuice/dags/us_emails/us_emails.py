5159f35879f7e9aa3ec7c359cd2868254999a233
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-09-19 09:37:04 -0300

    [AEFR-440] Updating us_email cluster (#8962)
    * updating us_email cluster

--- bietlejuice/dags/us_emails/us_emails.py
+++ bietlejuice/dags/us_emails/us_emails.py
@@ -40,7 +40,7 @@ RAW_FULL_LOAD_SPARK_JOB_PATH = (
 )
 
 default_libraries = config_service.get_config("default_libraries")
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("custom_cluster")
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
