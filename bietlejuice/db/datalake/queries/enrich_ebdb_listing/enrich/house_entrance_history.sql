280a7a83aaacc86bf2c7ce29e6f45d5422fdcf95
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-09-20 16:25:10 -0300

    updating lineage (#8993)
    * Tracking key_location as part of entrance attributes

--- bietlejuice/db/datalake/queries/enrich_ebdb_listing/enrich/house_entrance_history.sql
+++ bietlejuice/db/datalake/queries/enrich_ebdb_listing/enrich/house_entrance_history.sql
@@ -52,6 +52,34 @@ scd_doorman AS (
     WHERE
         doorman_type <> COALESCE(previous_doorman_type, -1)
 ),
+key_location_aud AS (
+    SELECT
+        ata.id_house,
+        ata.rev,
+        aat.name AS key_location,
+        LAG(aat.name) OVER(PARTITION BY ata.id_house ORDER BY ata.rev) AS previous_key_location,
+        FROM_UNIXTIME(ure.ts_revision/1000) AS ts_updated
+    FROM
+        datalake_ebdb_clean.access_type_aud AS ata
+    JOIN
+        datalake_ebdb_clean.user_revision_entity AS ure
+            ON ata.rev = ure.id
+    LEFT JOIN
+        datalake_ebdb_clean.access_authorization_type AS aat
+            ON ata.id_authorization = aat.id
+),
+scd_key_location AS (
+    SELECT
+        id_house,
+        rev,
+        key_location,
+        ts_updated AS ts_key_location_started,
+        LEAD(ts_updated) OVER(PARTITION BY id_house ORDER BY rev) AS ts_key_location_ended
+    FROM
+        key_location_aud
+    WHERE
+        key_location <> COALESCE(previous_key_location, -1)
+),
 event_bus AS (
     SELECT
         id_house,
@@ -66,12 +94,20 @@ event_bus AS (
         ts_doorman_type_started AS ts_event
     FROM
         scd_doorman
+    UNION
+    SELECT
+        id_house,
+        rev,
+        ts_key_location_started AS ts_event
+    FROM
+        scd_key_location
 )
 SELECT
     eb.id_house,
     so.id_occupant,
     eb.rev,
     sd.doorman_type,
+    skl.key_location,
     COALESCE(MAX(eb.rev) OVER(PARTITION BY eb.id_house, DATE(eb.ts_event)) = eb.rev, False) AS is_last_status_of_day,
     eb.ts_event AS ts_entrance_started,
     LEAD(eb.ts_event) OVER(PARTITION BY eb.id_house ORDER BY eb.rev) AS ts_entrance_ended
@@ -87,3 +123,8 @@ LEFT JOIN
         ON eb.ts_event >= sd.ts_doorman_type_started
         AND eb.ts_event < COALESCE(sd.ts_doorman_type_ended, '2100-01-01')
         AND eb.id_house = sd.id_house
+LEFT JOIN
+    scd_key_location AS skl
+        ON eb.ts_event >= skl.ts_key_location_started
+        AND eb.ts_event < COALESCE(skl.ts_key_location_ended, '2100-01-01')
+        AND eb.id_house = skl.id_house
