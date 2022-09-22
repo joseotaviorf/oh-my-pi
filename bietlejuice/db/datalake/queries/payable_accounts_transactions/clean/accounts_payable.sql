f41ab34016a9467caa04b2590260b4ee4f3c552a
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2022-09-22 20:14:22 -0300

    Fix regex getting modified by python (#9030)

--- bietlejuice/db/datalake/queries/payable_accounts_transactions/clean/accounts_payable.sql
+++ bietlejuice/db/datalake/queries/payable_accounts_transactions/clean/accounts_payable.sql
@@ -1,15 +1,15 @@
 SELECT
     `_doc_nf` AS nf_file_description,
     CASE
-        WHEN RLIKE(TRIM(REGEXP_REPLACE(fornecedor,'\\p{Z}', '' )),"^[0-9]+\\.0+$") THEN REGEXP_REPLACE(TRIM(REGEXP_REPLACE(fornecedor,'\\p{Z}', '' )), "\\.0+$", "")
-        ELSE TRIM(REGEXP_REPLACE(fornecedor,'\\p{Z}', ' ' ))
+        WHEN RLIKE(TRIM(REGEXP_REPLACE(fornecedor,'\\p{{Z}}', '' )),"^[0-9]+\\.0+$") THEN REGEXP_REPLACE(TRIM(REGEXP_REPLACE(fornecedor,'\\p{{Z}}', '' )), "\\.0+$", "")
+        ELSE TRIM(REGEXP_REPLACE(fornecedor,'\\p{{Z}}', ' ' ))
     END AS supplier_description,
-    NULLIF(TRIM(REGEXP_REPLACE(cost_and_profit_center,'\\p{Z}', ' ' )), '') AS payment_source,
-    NULLIF(TRIM(REGEXP_REPLACE(revenue_and_cost_elements,'\\p{Z}', ' ' )), '') AS payment_reason,
-    REGEXP_REPLACE(banco,'\\p{Z}', '' ) AS bank_description,
-    CAST(REPLACE(REGEXP_REPLACE(valor_bruto,'\\p{Z}', '' ), ',', '') AS NUMERIC(38, 2)) AS paid_amount,
-    DATE_FORMAT(TO_DATE(REGEXP_REPLACE(competencia,'\\p{Z}', '' ), 'yyyy-MM-dd HH:mm:ss'), 'yyyyMM') AS accrual_year_month,
-    TO_DATE(REGEXP_REPLACE(pagamento,'\\p{Z}', '' ), 'yyyy-MM-dd HH:mm:ss') AS dt_paid,
+    NULLIF(TRIM(REGEXP_REPLACE(cost_and_profit_center,'\\p{{Z}}', ' ' )), '') AS payment_source,
+    NULLIF(TRIM(REGEXP_REPLACE(revenue_and_cost_elements,'\\p{{Z}}', ' ' )), '') AS payment_reason,
+    REGEXP_REPLACE(banco,'\\p{{Z}}', '' ) AS bank_description,
+    CAST(REPLACE(REGEXP_REPLACE(valor_bruto,'\\p{{Z}}', '' ), ',', '') AS NUMERIC(38, 2)) AS paid_amount,
+    DATE_FORMAT(TO_DATE(REGEXP_REPLACE(competencia,'\\p{{Z}}', '' ), 'yyyy-MM-dd HH:mm:ss'), 'yyyyMM') AS accrual_year_month,
+    TO_DATE(REGEXP_REPLACE(pagamento,'\\p{{Z}}', '' ), 'yyyy-MM-dd HH:mm:ss') AS dt_paid,
     ts_load
 FROM
     datalake_payable_accounts_transactions_raw.accounts_payable
