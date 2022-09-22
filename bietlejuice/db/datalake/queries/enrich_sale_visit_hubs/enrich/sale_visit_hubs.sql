be996ea3ccfe86968ef832dae9ce1c5848badef0
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-09-22 17:16:19 -0300

    [DFORSALE-2931]: adjust enrich sale visit hub (#9022)
    * fix: adjust work contract source

--- bietlejuice/db/datalake/queries/enrich_sale_visit_hubs/enrich/sale_visit_hubs.sql
+++ bietlejuice/db/datalake/queries/enrich_sale_visit_hubs/enrich/sale_visit_hubs.sql
@@ -92,18 +92,16 @@ teams_relations AS (
 
 work_contract AS (
     SELECT
-        aud.id_agent,
-        aud.id_user_change,
-        aud.work_contract_name,
-        aud.ts_revision AS ts_started,
-        COALESCE(LEAD(aud.ts_revision) OVER(PARTITION BY aud.id_agent ORDER BY aud.rev), CURRENT_DATE) AS ts_ended
+        ac.id_user_agent,
+        ac.work_contract_name,
+        ac.ts_work_contract_started AS ts_started,
+        COALESCE(ac.ts_work_contract_ended, CURRENT_DATE) AS ts_ended
     FROM 
-        datalake_ebdb_agents.agents_activations_suspensions_contracts_changes AS aud
+        datalake_ebdb_agents.agent_contract AS ac
 ),
 wc_hubs_padronization AS (
     SELECT
-        wc.id_agent,
-        wc.id_user_change AS id_user_agent,
+        wc.id_user_agent,
         IF (wc.work_contract_name = bus.hub_name_wc, bus.id_business_unit_teams, NULL) AS id_hub,
         IF (wc.work_contract_name = bus.hub_name_wc, bus.hub_name_teams, NULL) AS hub_name,
         wc.ts_started,
@@ -114,20 +112,6 @@ wc_hubs_padronization AS (
         datalake_gsheets_clean.sale_business_unit_standardization AS bus
             ON bus.hub_name_wc = wc.work_contract_name
 ),
-work_contract_relations AS (
-    SELECT
-        ua.id_agent,
-        ua.id AS id_user_agent,
-        wc.id_hub,
-        wc.hub_name,
-        wc.ts_started,
-        wc.ts_ended
-    FROM
-        wc_hubs_padronization AS wc
-    LEFT JOIN
-        datalake_ebdb_user.user AS ua
-            ON wc.id_agent = ua.id_agent
-),
 
 business_unit_region_relations AS (
     SELECT
@@ -135,7 +119,7 @@ business_unit_region_relations AS (
         bur.id_region,
         bur.business_unit AS hub_name,
         bur.dt_start,
-        bur.dt_end
+        COALESCE(bur.dt_end, CURRENT_DATE) AS dt_end
     FROM 
         datalake_gsheets_clean.business_unit_region AS bur
     LEFT JOIN 
@@ -146,8 +130,8 @@ business_unit_region_relations AS (
 visit_relation AS (
     SELECT
         b.id AS id_booking,
-        COALESCE(tr.id_business_unit, wcr.id_hub, bur.id_hub) AS id_business_unit,
-        COALESCE(tr.hub_name, wcr.hub_name, bur.hub_name) AS business_unit,
+        COALESCE(tr.id_business_unit, whp.id_hub, bur.id_hub) AS id_business_unit,
+        COALESCE(tr.hub_name, whp.hub_name, bur.hub_name) AS business_unit,
         b.id_user_sale_agent AS id_user_agent,
         tr.id_user_en,
         b.ts_created AS ts_visit_intent
@@ -161,10 +145,10 @@ visit_relation AS (
           ON tr.id_user_agent = b.id_user_sale_agent
           AND b.ts_created BETWEEN tr.ts_started AND tr.ts_ended
     LEFT JOIN 
-        work_contract_relations AS wcr
-          ON wcr.id_user_agent = b.id_user_sale_agent
-          AND b.ts_created BETWEEN wcr.ts_started AND wcr.ts_ended
-          AND wcr.id_hub IS NOT NULL
+        wc_hubs_padronization AS whp
+          ON whp.id_user_agent = b.id_user_sale_agent
+          AND b.ts_created BETWEEN whp.ts_started AND whp.ts_ended
+          AND whp.id_hub IS NOT NULL
     LEFT JOIN
         business_unit_region_relations AS bur
           ON bur.id_region = h.id_region
