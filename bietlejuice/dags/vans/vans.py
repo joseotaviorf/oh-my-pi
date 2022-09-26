a4b1e65ce7d988f4970c2dee7c1c82218da8fffb
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-09-26 09:03:22 -0300

    changing vans cluster (#9040)

--- bietlejuice/dags/vans/vans.py
+++ bietlejuice/dags/vans/vans.py
@@ -36,7 +36,7 @@ spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
 
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("databricks_10_4_med_memory_cluster")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
