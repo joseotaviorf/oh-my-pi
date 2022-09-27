b0e488c1fa436150eafffd7d6a1999ddcafdf1b2
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-09-27 14:01:28 -0300

    [AES&S] Updating and standardizing the Repairs DAG (#9053)
    * update DAG
    
    * fix: F401 'airflow.models.Variable' imported but unused
    
    * fix: date_filter_column -> date_column

--- bietlejuice/dags/repairs/repairs.py
+++ bietlejuice/dags/repairs/repairs.py
@@ -1,24 +1,32 @@
-from datetime import datetime
+import json
 import os
-import pendulum
+from datetime import datetime
 
-from airflow.utils.helpers import cross_downstream
-from airflow.models import DAG, Variable
+import pendulum
+from airflow.models import DAG
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
+from airflow.utils.helpers import cross_downstream
+
 from bietlejuice.base.airflow import BaseDAG, DAGOwnerEnum
+from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
 from bietlejuice.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.services.configuration_service import ConfigurationService
 
-
-SOURCE = "repairs"
-CONTEXT = SOURCE
-DAG_ID = f"bietlejuice.{SOURCE}"
+# ENV setup
 ENV = os.environ.get("ENVIRONMENT")
 
+# DAG and Jobs params setup
+SOURCE = "repairs"
+DAG_ID = f"bietlejuice.{SOURCE}"
+MAIN_START_DATE = datetime(2019, 5, 31, tzinfo=pendulum.timezone("America/Sao_Paulo"))
+MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+
 config_service = ConfigurationService(SOURCE)
+
+# s3 paths setup
 athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
 datalake_bucket = config_service.get_config("datalake_bucket")
 databricks_bietlejuice_repo_path = config_service.get_config(
@@ -27,21 +35,21 @@ databricks_bietlejuice_repo_path = config_service.get_config(
 spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
+base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+raw_spark_job_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/load_{SOURCE}_into_datalake.py"
+
+cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+
+default_libraries = config_service.get_config("default_libraries")
+DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
+    {
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+
 tables = config_service.get_config("tables")
-clean_partition_columns = config_service.get_config("clean_partition_columns")
-
-RAW_SPARK_JOB_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/{CONTEXT}/load_{{extraction_type}}_tables_into_datalake_raw.py"
-
-BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
-
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{spark_jobs_logs_path}{DAG_ID}"
-
-MAIN_START_DATE = datetime(2019, 5, 31, tzinfo=pendulum.timezone("America/Sao_Paulo"))
-MAIN_SCHEDULE_INTERVAL = "0 1 * * *"
+partition_cols = config_service.get_config("partition_cols")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -58,7 +66,11 @@ dag = DAG(
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=cluster_description,
+    access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
+    libraries=default_libraries,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
@@ -69,40 +81,33 @@ task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
     datalake_bucket=datalake_bucket,
-    relative_query_path=CONTEXT,
-    spark_jobs_path=BASE_SPARK_JOBS_PATH,
+    relative_query_path=SOURCE,
+    spark_jobs_path=base_spark_jobs_path,
     athena_query_result_location=athena_query_results_bucket,
 )
 
-for table in tables:
-    table_name = table["table_name"]
-    extraction_type = table["extraction_type"]
-    clean_table_name = table.get("clean_table_name", table_name)
-    is_incremental = extraction_type == "incremental"
-
-    parameters = [SOURCE, table_name]
-    if is_incremental:
-        parameters.append(table["date_filter_column"])
-        parameters.append(table.get("unixtime_measure", "date"))
-        parameters.append("{{ ds }}")
-
+for raw_table_name, table_details in tables.items():
     raw_task_group = task_group.build_raw_task_group_for_single_table(
         source=SOURCE,
-        target_database_base_name=CONTEXT,
-        table_name=table_name,
-        extraction_spark_job_file=RAW_SPARK_JOB_PATH.format(
-            extraction_type=extraction_type
-        ),
-        raw_spark_job_extra_args=parameters,
+        target_database_base_name=SOURCE,
+        table_name=raw_table_name,
+        extraction_spark_job_file=raw_spark_job_path,
+        raw_spark_job_extra_args=[
+            SOURCE,
+            json.dumps(table_details),
+            raw_table_name,
+            json.dumps(partition_cols),
+            "{{ ds }}",
+        ],
     )
 
-    partitions = clean_partition_columns if is_incremental else None
     clean_task_group = task_group.build_clean_task_group(
-        source_database_base_name=CONTEXT,
-        target_database_base_name=CONTEXT,
-        table_name=clean_table_name,
-        is_incremental=is_incremental,
-        partitions=partitions,
+        source_database_base_name=SOURCE,
+        target_database_base_name=SOURCE,
+        has_create_external_table_task=False,
+        table_name=table_details["clean_table_name"],
+        partitions=partition_cols if table_details.get("is_incremental") else None,
+        is_incremental=True if table_details.get("is_incremental") else False,
     )
 
     create_cluster_task.set_downstream(DatalakeTaskGroup.first_tasks(raw_task_group))
