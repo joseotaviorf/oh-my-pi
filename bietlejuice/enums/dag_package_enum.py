4d88584dd171f25ec38e79f22c8aae5e9c87995b
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-09-28 14:31:54 -0300

    Refactor DAGPackagesPathService methods (#9071)

--- bietlejuice/enums/dag_package_enum.py
+++ bietlejuice/enums/dag_package_enum.py
@@ -1,4 +1,3 @@
 class DAGPackagesEnum:
-    S3_DAG_PACKAGES_FILES_BUCKET = "databricks.s3.data.quintoandar.com.br"
-    S3_DAG_PACKAGES_FILES_BUCKET_OLD = "5a-databricks-forno"
+    DATABRICKS_FILES_BUCKET = "databricks.s3.data.quintoandar.com.br"
     S3_DAG_PACKAGES_FILES_PATH = "github-repos/bi-etl-ejuice/"
