024a4dd620cf297a45694a56ae228b40ed3fdabc
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2022-09-29 15:54:08 -0300

    recurrent_credit_card_id collumn added in datalake_retsuko_clean (#9097)
    * recurrent_credit_card collumn added in datalake_retsuko_clean
    
    * fixed column alias and lineage

--- bietlejuice/db/datalake/queries/retsuko/clean/credit_card_payment.sql
+++ bietlejuice/db/datalake/queries/retsuko/clean/credit_card_payment.sql
@@ -3,6 +3,7 @@ SELECT
     external_id AS id_external,
     invoice_id AS id_invoice,
     acquirer_transaction_id AS id_acquirer_transaction,
+    recurrent_credit_card_id AS id_recurrent_credit_card,
     acquirer_auth_code,
     acquirer_nsu,
     brand_name,
