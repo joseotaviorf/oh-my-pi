cac5dc5a155d119c1bc11ef222cecf941d5fec39
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-09-29 15:54:53 -0300

    Add arg as optional (#9103)

--- bietlejuice/dags/base/spark_jobs/data_quality_tests.py
+++ bietlejuice/dags/base/spark_jobs/data_quality_tests.py
@@ -113,6 +113,8 @@ def parse_args():
         "intermediate_path",
         type=str,
         help="partial path used in some DAGs off of our pattern",
+        required=False,
+        default="",
     )
 
     args = parser.parse_args()
