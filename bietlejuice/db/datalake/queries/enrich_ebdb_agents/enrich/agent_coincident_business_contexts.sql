41132a9e1742de26374bd8da1f1339982db81fa3
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2022-10-03 08:59:58 -0300

    [DFORSALE-3027] Create agent_coincident_business_contexts (#9093)
    Create agent_coincident_business_contexts table, which is similar to agent_business_context_history, but merges both contexts into a single row, with flags to tell if they are active in ForSale or ForRent (or both).

---
+++ bietlejuice/db/datalake/queries/enrich_ebdb_agents/enrich/agent_coincident_business_contexts.sql
@@ -0,0 +1,69 @@
+WITH business_context_changes_aux AS (
+    SELECT
+        id_agent_data,
+        CASE
+            WHEN agent_business_context = 'SALE' THEN TRUE
+            ELSE NULL
+        END AS is_agent_for_sale,
+        CASE
+            WHEN agent_business_context = 'RENT' THEN TRUE
+            ELSE NULL
+        END AS is_agent_for_rent,
+        ts_agent_business_context_started AS ts_business_context_changed
+    FROM
+        datalake_ebdb_agents.agent_business_context_history
+    UNION ALL
+    SELECT
+        id_agent_data,
+        CASE
+            WHEN agent_business_context = 'SALE' THEN FALSE
+            ELSE NULL
+        END AS is_agent_for_sale,
+        CASE
+            WHEN agent_business_context = 'RENT' THEN FALSE
+            ELSE NULL
+        END AS is_agent_for_rent,
+        ts_agent_business_context_ended AS ts_business_context_changed
+    FROM
+        datalake_ebdb_agents.agent_business_context_history
+    WHERE
+        ts_agent_business_context_ended IS NOT NULL
+)
+SELECT
+    id_agent_data,
+    COALESCE(
+        LAST( -- If there was not any non-null result in the group by, replicate the value from the previous line
+            FIRST(is_agent_for_sale, TRUE), -- Gets the non-null result in the group by (if it exists)
+            TRUE 
+        ) OVER(
+            PARTITION BY 
+                id_agent_data
+            ORDER BY
+                ts_business_context_changed
+        ), 
+        FALSE
+    ) AS is_agent_for_sale,
+    COALESCE(
+        LAST(
+            FIRST(is_agent_for_rent, TRUE),
+            TRUE
+        ) OVER(
+            PARTITION BY
+                id_agent_data
+            ORDER BY ts_business_context_changed
+        ),
+        FALSE
+    ) AS is_agent_for_rent,
+    TIMESTAMP(ts_business_context_changed) AS ts_agent_business_context_started,
+    TIMESTAMP(
+        LEAD(ts_business_context_changed) OVER (
+            PARTITION BY
+                id_agent_data
+            ORDER BY
+                ts_business_context_changed
+        )
+    ) AS ts_agent_business_context_ended
+FROM
+    business_context_changes_aux
+GROUP BY
+    id_agent_data, ts_business_context_changed
