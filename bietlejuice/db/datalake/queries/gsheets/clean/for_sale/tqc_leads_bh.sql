d2e8c017cae6211f418833da8028a468f205cfb1
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-10-03 08:58:02 -0300

    Fix: change cast from ts_appointment in tqc_leads_bh (#9113)

--- bietlejuice/db/datalake/queries/gsheets/clean/for_sale/tqc_leads_bh.sql
+++ bietlejuice/db/datalake/queries/gsheets/clean/for_sale/tqc_leads_bh.sql
@@ -8,6 +8,6 @@ SELECT
     lead_phone,
     lead_email,
     CAST(is_valid_lead AS BOOLEAN) AS is_valid_lead,
-    TO_TIMESTAMP(appointment_date, 'M/d/y H:m:s') AS ts_appointment
+    CAST(appointment_date AS TIMESTAMP) AS ts_appointment
 FROM 
     datalake_gsheets_raw.tqc_leads_bh
