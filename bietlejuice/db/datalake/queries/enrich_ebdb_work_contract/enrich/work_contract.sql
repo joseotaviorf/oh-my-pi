6a03fce5ee82b450cfed7e5592df19a03fd7f9e0
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-10-03 08:52:58 -0300

    [DFORSALE-2992]: apply padronization for work_contract hubs (#9084)
    * apply padronization rules

--- bietlejuice/db/datalake/queries/enrich_ebdb_work_contract/enrich/work_contract.sql
+++ bietlejuice/db/datalake/queries/enrich_ebdb_work_contract/enrich/work_contract.sql
@@ -1,9 +1,15 @@
 SELECT
-    id,
-    contract_name,
-    REGEXP_EXTRACT(contract_name, '(?<=\\[3P\\-)(.+?)(?=\\])') AS 3p_partner,
-    contract_name LIKE '%[3P-%]%' AS is_3p_contract,
-    ts_created,
-    ts_updated
+    wc.id,
+    IF (wc.contract_name = bus.hub_name_wc, bus.id_business_unit_teams, NULL) AS id_hub_teams,
+    IF (wc.contract_name = bus.hub_name_wc, bus.hub_name_teams, NULL) AS hub_name_teams,
+    wc.contract_name,
+    REGEXP_EXTRACT(wc.contract_name, '(?<=\\[3P\\-)(.+?)(?=\\])') AS 3p_partner,
+    wc.contract_name LIKE '%[3P-%]%' AS is_3p_contract,
+    wc.ts_created,
+    wc.ts_updated
 FROM
-    datalake_ebdb_clean.work_contract
+    datalake_ebdb_clean.work_contract AS wc
+LEFT JOIN
+    datalake_gsheets_clean.sale_business_unit_standardization AS bus
+        ON bus.hub_name_wc = wc.contract_name
+ 
