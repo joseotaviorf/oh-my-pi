4219b012bef08f69bb43a6f5e4c6ecf303e6ee75
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-10-03 11:19:56 -0300

    [AEFR-534] Changing kill_queue runtime to 10.4 (#9044)
    * updating kill_queue dag

--- bietlejuice/dags/kill_queue/kill_queue.py
+++ bietlejuice/dags/kill_queue/kill_queue.py
@@ -4,7 +4,7 @@ import pendulum
 import os
 
 from airflow.utils.helpers import chain, cross_downstream
-from airflow.models import DAG, Variable
+from airflow.models import DAG
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -20,24 +20,26 @@ CONTEXT = SOURCE
 
 DAG_ID = f"bietlejuice.{SOURCE}"
 ENV = os.environ.get("ENVIRONMENT")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-ARTIFACTS_S3_BUCKET = Variable.get("artifacts_s3_bucket")
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+config_service = ConfigurationService(SOURCE)
+spectrum_iam_role = config_service.get_config("spectrum_iam_role")
+datalake_bucket = config_service.get_config("datalake_bucket")
+athena_query_result_location = config_service.get_config("athena_query_results_bucket")
+artifacts_s3_bucket = config_service.get_config("artifacts_bucket")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+default_libraries = config_service.get_config("default_libraries")
 
-# databricks config
-LOGS_OUTPUT_PATH = "s3://{}/logs/jobs/{}".format(
-    Variable.get("databricks_s3_bucket"), DAG_ID
-)
-
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
+s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
+SPARK_JOBS_PATH = f"{s3_prefix}/spark_jobs/base"
+spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
+LOGS_OUTPUT_PATH = f"{spark_jobs_logs_path}{DAG_ID}"
+cluster_description = config_service.get_config("databricks_10_4_min_general_cluster")
 CUSTOM_LIBRARIES = [
     {
-        "jar": f"{ARTIFACTS_S3_BUCKET}/mysql-connector-java/mysql-connector-java-5.1"
+        "jar": f"{artifacts_s3_bucket}/mysql-connector-java/mysql-connector-java-5.1"
         f".47.jar"
     }
 ]
+LIBRARIES_DESCRIPTION = default_libraries + CUSTOM_LIBRARIES
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
@@ -46,9 +48,8 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 ]
 
 # bietlejuice paths
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-BASE_SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
-RAW_SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/{SOURCE}/load_{SOURCE}_into_datalake.py"
+BASE_SPARK_JOBS_PATH = f"{s3_prefix}/spark_jobs/base/"
+RAW_SPARK_JOB_PATH = f"{s3_prefix}/spark_jobs/{SOURCE}/load_{SOURCE}_into_datalake.py"
 
 # dag params
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
@@ -66,14 +67,16 @@ dag = DAG(
     },
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
-    doc_md=BaseDAG.get_dag_doc(SOURCE).format(chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID),
+    doc_md=BaseDAG.get_dag_doc(SOURCE).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag,
     task_id="create-cluster",
-    cluster_configuration=CLUSTER_DESCRIPTION,
-    libraries=CUSTOM_LIBRARIES,
+    cluster_configuration=cluster_description,
+    libraries=LIBRARIES_DESCRIPTION,
     access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
 )
 
@@ -84,10 +87,10 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
+    datalake_bucket=datalake_bucket,
     relative_query_path=CONTEXT,
     spark_jobs_path=BASE_SPARK_JOBS_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    athena_query_result_location=athena_query_result_location,
 )
 
 tables = config_service.get_config("tables")
@@ -108,7 +111,7 @@ for table in tables:
         extended_parameters = list(filter(None, extended_parameters))
         parameters.extend(extended_parameters)
 
-    raw_spark_job_path = f"{S3_PREFIX}/spark_jobs/{CONTEXT}/load_{extraction_type}_kill_queue_into_datalake.py"
+    raw_spark_job_path = f"{s3_prefix}/spark_jobs/{CONTEXT}/load_{extraction_type}_kill_queue_into_datalake.py"
     raw_task_group = task_group.build_raw_task_group_for_single_table(
         source=SOURCE,
         table_name=table_name,
