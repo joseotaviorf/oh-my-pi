ba9b3bdeb62541f9eb28ceaa58a8e7b886a8cffd
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-10-03 10:53:18 -0300

    [AEFR] Updating retsuko account table (#9121)
    * updating retsuko.account

--- bietlejuice/db/datalake/queries/retsuko/clean/account.sql
+++ bietlejuice/db/datalake/queries/retsuko/clean/account.sql
@@ -1,7 +1,11 @@
 select
     id,
-    external_id as id_external,
+    external_id AS id_external,
+    contract_id AS id_contract,
+    external_main_user_id AS id_main_user,
+    bank_code,
     type,
-    contract_id as id_contract
+    is_savings,
+    bank_info_updated_at AS ts_bank_info_updated
 from
     datalake_retsuko_raw.account
