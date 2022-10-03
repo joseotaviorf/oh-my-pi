d81a3ec497435d3d44fc863d3c0b1f034707ac49
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-10-03 15:57:25 -0300

    Adjust enrich early_credit_analysis (#9099)

--- bietlejuice/db/datalake/queries/enrich_credit_analysis/enrich/early_credit_analysis.sql
+++ bietlejuice/db/datalake/queries/enrich_credit_analysis/enrich/early_credit_analysis.sql
@@ -6,8 +6,22 @@ WITH evaluation AS (
         ts_created,
         RANK() OVER (PARTITION BY id_user, id_house ORDER BY ts_created DESC) AS rank
     FROM datalake_sorting_hat_clean.early_credit_analysis
+),
+rent_flow AS (
+  SELECT
+    rf.id_rent_flow,
+    rf.id_visit,
+    rf.id_booking,
+    rf.id_offer,
+    rf.id_client,
+    rf.id_house,
+    rf.dt_visit,
+    rf.dt_booking_created,
+    offer.ts_created AS ts_offer_created
+  FROM datalake_ebdb_rent_flow.rent_flow AS rf
+  LEFT JOIN datalake_ebdb_clean.offer AS offer
+      ON offer.id = rf.id_offer
 )
-
 SELECT
     eca.id_house, 
     rf.id_visit, 
@@ -16,18 +30,21 @@ SELECT
     rf.id_rent_flow, 
     eca.id_credit_evaluation,
     eca.result,
-    IF(rf.id_offer IS NOT NULL, DATEDIFF(offer.ts_created, eca.ts_created), NULL) AS days_offer_after_early_credit_evaluation,
+    IF(rf.id_offer IS NOT NULL, DATEDIFF(rf.ts_offer_created, eca.ts_created), NULL) AS days_offer_after_early_credit_evaluation,
+    IF(rf.id_booking IS NOT NULL, DATEDIFF(rf.dt_booking_created, eca.ts_created), NULL) AS days_booking_after_early_credit_evaluation,
     IF(rf.id_visit IS NOT NULL, DATEDIFF(rf.dt_visit, eca.ts_created), NULL) AS days_visit_after_early_credit_evaluation,
     IF(eval.rank = 1, TRUE, FALSE) AS is_most_recent_evaluation,
     rf.dt_visit,
-    offer.ts_created AS ts_offer_created,
+    rf.dt_booking_created,
+    rf.ts_offer_created,
     eca.ts_created AS ts_early_credit_analysis_created
 FROM datalake_sorting_hat_clean.early_credit_analysis AS eca
 JOIN evaluation AS eval
     ON eval.id_credit_evaluation = eca.id_credit_evaluation
-LEFT JOIN datalake_ebdb_rent_flow.rent_flow AS rf
+LEFT JOIN rent_flow AS rf
     ON eca.id_user = rf.id_client
     AND eca.id_house = rf.id_house
-LEFT JOIN 
-    datalake_ebdb_clean.offer AS offer
-    ON offer.id = rf.id_offer
+    AND (
+      eca.ts_created between rf.ts_offer_created -  interval '1' month and rf.ts_offer_created
+      OR eca.ts_created between rf.dt_booking_created -  interval '1' month and rf.dt_booking_created
+    )
