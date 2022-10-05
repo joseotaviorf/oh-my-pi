4ca04d678a2ad366d3f39639ccbdbb0b2519c22b
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2022-10-05 16:19:15 -0300

    [AESS-261] Update Servicate Dag (#9147)
    * Update survicate cluster

--- bietlejuice/dags/survicate/survicate.py
+++ bietlejuice/dags/survicate/survicate.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pendulum
 import os
 from airflow.utils.helpers import chain, cross_downstream
-from airflow.models import DAG, Variable
+from airflow.models import DAG
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -11,6 +11,8 @@ from airflow.operators.quintoandar_databricks import (
 
 from bietlejuice.base.airflow import BaseDAG, DAGOwnerEnum
 from bietlejuice.base.pipeline import LayerEnum
+from bietlejuice.services.configuration_service import ConfigurationService
+from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
 from bietlejuice.dags.base.datalake_task_group import DatalakeTaskGroup
 
 
@@ -24,23 +26,32 @@ LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2020, 11, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
 MAIN_SCHEDULE_INTERVAL = "30 2 * * *"
 
-ATHENA_QUERY_RESULT_LOCATION = Variable.get("athena_query_result_location")
-S3_PREFIX = Variable.get("databricks_bietlejuice_s3_prefix")
-ARTIFACTS_S3_BUCKET = Variable.get("artifacts_default_bucket")
-DATABRICKS_BUCKET = Variable.get("databricks_s3_bucket")
-DATALAKE_BUCKET = Variable.get("datalake_bucket")
-DOC_MD_BASE_URL = Variable.get("DOC_MD_BASE_URL")
+config_service = ConfigurationService(dag_name=SOURCE)
+partition_cols = config_service.get_config("partition_cols")
 
-LOGS_OUTPUT_PATH = f"s3://{DATABRICKS_BUCKET}/logs/jobs/{DAG_NAME}"
-BASE_SPARK_JOB_PATH = f"{S3_PREFIX}/spark_jobs/base/"
-SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/{DAG_NAME}"
-RAW_SPARK_JOB_PATH = f"{SPARK_JOBS_PATH}/load_survicate_into_datalake.py"
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+artifacts_s3_bucket = config_service.get_config("artifacts_bucket")
+databricks_bietlejuice_repo_path = config_service.get_config(
+    "databricks_bietlejuice_repo_path"
+)
+datalake_bucket = config_service.get_config("datalake_bucket")
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
-CLUSTER_DESCRIPTION = Variable.get("databricks_default_cluster", deserialize_json=True)
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"]["destination"] = LOGS_OUTPUT_PATH
-CUSTOM_LIBRARIES = [
+DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
-        "whl": f"{ARTIFACTS_S3_BUCKET}/survicate-api-client-python/"
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+
+logs_output_path = f"s3://{databricks_bietlejuice_repo_path}/logs/jobs/{DAG_NAME}"
+base_spark_job_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
+raw_spark_job_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/load_survicate_into_datalake.py"
+
+cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+custom_libraries = [
+    {
+        "whl": f"{artifacts_s3_bucket}/survicate-api-client-python/"
         f"quintoandar_survicate_api_client-0.1.0-py2.py3-none-any.whl"
     }
 ]
@@ -55,15 +66,16 @@ dag = DAG(
     start_date=MAIN_START_DATE,
     schedule_interval=MAIN_SCHEDULE_INTERVAL,
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
-        chart_url=DOC_MD_BASE_URL, dag_id=DAG_ID
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
     ),
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
     dag=dag,
     task_id="create-cluster",
-    cluster_configuration=CLUSTER_DESCRIPTION,
-    libraries=CUSTOM_LIBRARIES,
+    cluster_configuration=cluster_description,
+    access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
+    libraries=custom_libraries,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
@@ -73,16 +85,16 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 task_group = DatalakeTaskGroup(
     dag=dag,
     env=ENV,
-    datalake_bucket=DATALAKE_BUCKET,
+    datalake_bucket=datalake_bucket,
     relative_query_path=CONTEXT,
-    spark_jobs_path=BASE_SPARK_JOB_PATH,
-    athena_query_result_location=ATHENA_QUERY_RESULT_LOCATION,
+    spark_jobs_path=base_spark_job_path,
+    athena_query_result_location=athena_query_results_bucket,
 )
 
 raw_task_groups = task_group.build_raw_task_group_for_all_tables(
     source=SOURCE,
     target_database_base_name=SOURCE,
-    extraction_spark_job_file=RAW_SPARK_JOB_PATH,
+    extraction_spark_job_file=raw_spark_job_path,
     raw_spark_job_extra_args=[SOURCE, "{{ ds }}"],
 )
 
@@ -91,7 +103,7 @@ clean_task_groups = task_group.build_task_group_from_sql_files(
     source_database_base_name=SOURCE,
     target_database_base_name=SOURCE,
     is_incremental=True,
-    partitions=["year", "month", "day"],
+    partitions=partition_cols,
 )
 
 chain(create_cluster_task, DatalakeTaskGroup.first_tasks(raw_task_groups))
