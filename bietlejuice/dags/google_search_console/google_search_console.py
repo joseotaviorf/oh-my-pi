035a8c77a1c2d26e9a48840de016e6266a0fe731
Ana Alves <ana.araujo@quintoandar.com.br>
2022-10-06 13:06:18 -0300

    forgot to update to raw tasks as well (#9178)

--- bietlejuice/dags/google_search_console/google_search_console.py
+++ bietlejuice/dags/google_search_console/google_search_console.py
@@ -101,8 +101,8 @@ for property_type in PROPERTY_TYPE_LIST:
             extraction_spark_job_file=raw_spark_job_file,
             raw_spark_job_extra_args=[
                 SOURCE,
-                "{{ get_date_param(dag_run, macros.ds_add(ds, -1), 'load_start_date') }}",
-                "{{ get_date_param(dag_run, macros.ds_add(ds, -1), 'load_end_date') }}",
+                "{{ get_date_param(dag_run, macros.ds_add(ds, -3), 'load_start_date') }}",
+                "{{ get_date_param(dag_run, macros.ds_add(ds, -3), 'load_end_date') }}",
                 table_name,
                 property_type,
                 aggregation_type,
