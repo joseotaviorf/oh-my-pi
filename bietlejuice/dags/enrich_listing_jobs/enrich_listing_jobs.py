d9d2212adc2c4d3289237eb35dc8be3e6f096ed0
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2022-10-17 14:43:58 -0300

    [AESS - 580] Update Enrich Listing Jobs cluster (#9208)
    * update enrich listing jobs cluster
    
    * fixed get method in config service
    
    * removed unused libraries
    
    * Update bietlejuice/dags/enrich_listing_jobs/enrich_listing_jobs.py
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
    
    * Update bietlejuice/dags/enrich_listing_jobs/enrich_listing_jobs.py
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>

--- bietlejuice/dags/enrich_listing_jobs/enrich_listing_jobs.py
+++ bietlejuice/dags/enrich_listing_jobs/enrich_listing_jobs.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 import pendulum
-from airflow.models import DAG, Variable
+from airflow.models import DAG
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -12,6 +12,7 @@ from bietlejuice.base.airflow import BaseDAG, DAGOwnerEnum
 from bietlejuice.base.pipeline.layer_enum import LayerEnum
 from bietlejuice.dags.base.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.services.configuration_service import ConfigurationService
+from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2021, 8, 24, 0, 0, 0, tzinfo=LOCAL_TZ)
@@ -32,13 +33,15 @@ spark_jobs_logs_path = config_service.get_config("spark_jobs_logs_path")
 base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
-CLUSTER_DESCRIPTION = Variable.get(
-    "databricks_minimum_resources_cluster", deserialize_json=True
-)
-CLUSTER_DESCRIPTION["spark_env_vars"]["ENVIRONMENT"] = ENV
-CLUSTER_DESCRIPTION["cluster_log_conf"]["s3"][
-    "destination"
-] = f"{spark_jobs_logs_path}{DAG_ID}"
+cluster_description = config_service.get_config("databricks_10_4_min_general_cluster")
+
+DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
+    {
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+default_libraries = config_service.get_config("default_libraries")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -55,7 +58,11 @@ dag = DAG(
 )
 
 create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
-    dag=dag, task_id="create-cluster", cluster_configuration=CLUSTER_DESCRIPTION
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=cluster_description,
+    access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
+    libraries=default_libraries,
 )
 
 terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
