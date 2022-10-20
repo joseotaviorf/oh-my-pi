85b7f7658460e5c461f37aeb0599bb3e63c04773
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2022-10-20 17:31:02 -0300

    [DIN-89] Added internal Spark cluster metrics plugin into init_script (#9329)
    * Added GangliaMetrics class as a spark plugin in init_script
    
    * Added GangliaMetrics spark plugin into config files
    
    * Replaced Graphite exporter host to its DNS
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- scripts/init_script.sh
+++ scripts/init_script.sh
@@ -9,7 +9,7 @@ apt-get install maven -yqq
 spark_jars_path="/databricks/jars"
 spark_driver_config_file="$DB_HOME/driver/conf/00-spark-plugin-driver-defaults.conf"
 spark_driver_config_content="[driver] {
-    \"spark.plugins\" = \"ch.cern.CloudFSMetrics,ch.cern.CgroupMetrics\",
+    \"spark.plugins\" = \"ch.cern.CloudFSMetrics,ch.cern.CgroupMetrics,br.com.quintoandar.GangliaMetrics\",
     \"spark.cernSparkPlugin.cloudFsName\" = \"s3a\",
     \"spark.cernSparkPlugin.registerOnDriver\" = \"true\",
     \"spark.extraListeners\" = \"ch.cern.sparkmeasure.FlightRecorderStageMetrics\"
@@ -35,6 +35,7 @@ echo "BEGIN: Installing QuintoAndar internal libs"
 # Here, the script differentiate between environments
 # using different buckets.
 
+aws s3 cp ${ARTIFACTS_BUCKET}/jars/spark-cluster-metrics_2.12-0.1-SNAPSHOT.jar $spark_jars_path/spark-cluster-metrics_2.12-0.1-SNAPSHOT.jar
 aws s3 cp ${ARTIFACTS_BUCKET}/bi-etl-ejuice/bi_etl_ejuice-latest-py3-none-any.whl /bi_etl_ejuice-latest-py3-none-any.whl
 aws s3 cp ${ARTIFACTS_BUCKET}/python-logger/quintoandar_logger-0.8.0-py3-none-any.whl /quintoandar_logger-0.8.0-py3-none-any.whl
 aws s3 cp ${ARTIFACTS_BUCKET}/inmetro/inmetro-2.3.0-py3-none-any.whl /inmetro-2.3.0-py3-none-any.whl
