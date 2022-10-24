5315de07b98d1771761c5dabe423a5cc0b216f3d
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2022-10-24 16:14:54 -0300

    [AEFR-583] Add column with email timestamp in rent_offer table (#9304)
    * Add column with email timestamp in rent_offer table

--- bietlejuice/db/datalake/queries/firestore/clean/rent_offer.sql
+++ bietlejuice/db/datalake/queries/firestore/clean/rent_offer.sql
@@ -3,9 +3,14 @@ SELECT DISTINCT
     before AS original_message,
     after AS updated_message,
     eventType AS event_type,
+    TIMESTAMP(FROM_UNIXTIME(BIGINT(GET_JSON_OBJECT(after, '$.notifications.emailSentToOwnerAt._seconds')), 'yyyy-MM-dd HH:mm:ss')) AS ts_email_sent_to_owner,
     CAST(timestamp AS TIMESTAMP) AS ts_updated,
     year,
     month, 
     day
-FROM datalake_firestore_raw.rent_offer
-WHERE year={year} AND month={month} AND day={day}
+FROM 
+    datalake_firestore_raw.rent_offer
+WHERE 
+    year={year} 
+    AND month={month} 
+    AND day={day}
