1203800f9ef4676473cbdb2d21e46d3cbe56617f
GuilhermeLCunha <84026642+GuilhermeLCunha@users.noreply.github.com>
2022-10-26 08:34:35 -0300

    deduplicating users by id_user and cpf (#9395)

--- dags/for_rent/enrich_airtable/queries/enrich/activated.sql
+++ dags/for_rent/enrich_airtable/queries/enrich/activated.sql
@@ -1,6 +1,13 @@
-WITH activated_last_version AS (
+WITH activated_last_version_id_user AS (
+    /*
+    We can't guarantee that a user is the same only by cpf or id_user.
+    Users that are deleted and inserted again gain a new id, and users
+    can have the cpf wrote wrong. So we need to first filter everything by 
+    id_user, to get the last version in ids, than filter the last
+    state by cpf, to deduplicated in what should be a person.
+    */
     SELECT
-        ROW_NUMBER() OVER(PARTITION BY cpf ORDER BY ts_updated DESC) AS ordered_version,
+        ROW_NUMBER() OVER(PARTITION BY id_user ORDER BY ts_updated DESC) AS ordered_version_id,
         id_airtable_record,
         ids_history,
         id_user,
@@ -35,6 +42,46 @@ WITH activated_last_version AS (
         day
     FROM
         datalake_airtable_clean.activated
+),
+activated_last_version AS (
+    SELECT
+        ROW_NUMBER() OVER(PARTITION BY cpf ORDER BY ts_updated DESC) AS ordered_version,
+        id_airtable_record,
+        ids_history,
+        id_user,
+        accreditations,
+        agent_actual_type,
+        agent_creci,
+        agent_email,
+        agent_name,
+        agent_phone_number,
+        cpf,
+        disqualification,
+        dynamic_status,
+        last_area,
+        last_disqualification,
+        last_disqualification_reason,
+        last_suspension,
+        last_suspension_reason,
+        permanent_deaccreditation,
+        suspensions,
+        suspensions_register,
+        tickets_jira,
+        days_since_activation_week,
+        dt_activated,
+        dt_last_disqualificated,
+        dt_last_disqualification_returned,
+        dt_last_suspended,
+        dt_last_suspension_returned,
+        dt_week_accreditated,
+        ts_updated,
+        year,
+        month,
+        day
+    FROM 
+        activated_last_version_id_user
+    WHERE
+        ordered_version_id = 1 
 ),
 id_user_changes AS (
     SELECT
