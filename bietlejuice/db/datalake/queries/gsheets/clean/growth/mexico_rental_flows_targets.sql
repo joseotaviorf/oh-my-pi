a1e2934f6924bb46be1d7764af546593c506df0c
Guilherme Dorella <72612610+guilherme-dorella@users.noreply.github.com>
2022-10-27 11:07:32 -0300

    Adding Rent Flows and Ongoing Listing target sheets (#9415)
    * Adding Rent Flows and Ongoing Listing target sheets
    
    * Fixing file name
    
    * Fixing file name
    
    * Update bietlejuice/db/datalake/queries/gsheets/clean/for_rent/mexico_ongoing_listings_target.sql
    
    * Update bietlejuice/db/datalake/queries/gsheets/clean/growth/mexico_rental_flows_targets.sql
    
    * Update bietlejuice/db/datalake/metadata/gsheets/clean/for_rent/mexico_ongoing_listings_target.yml
    
    * Update bietlejuice/db/datalake/queries/gsheets/clean/for_rent/mexico_ongoing_listings_target.sql
    
    * Update bietlejuice/db/datalake/metadata/gsheets/clean/growth/mexico_rental_flows_targets.yml
    
    * Update bietlejuice/db/datalake/queries/gsheets/clean/growth/mexico_rental_flows_targets.sql
    
    Co-authored-by: Guilherme Dorella <guilhermedorella@BRSPPM-C02GL2TAMD6M.local>
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ bietlejuice/db/datalake/queries/gsheets/clean/growth/mexico_rental_flows_targets.sql
@@ -0,0 +1,8 @@
+SELECT
+    city_group,
+    CAST(REPLACE(rf_target, ',', '') AS FLOAT) AS rf_target,
+    rf_type,
+    TO_DATE(week_start, 'yyyy-M-d') AS dt_week_started,
+    TO_DATE(date, 'yyyy-M-d') AS dt_target
+FROM
+    datalake_gsheets_raw.mexico_rental_flows_targets
