0f50dce51ada091cda2d19651698f51e637a87ce
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2022-10-27 16:03:26 -0300

    [AEFR] Adjust tracking_catalog sheets (#9432)
    * updating tracking_catalog_events_aud
    
    * updating tracking_catalog_projects

--- bietlejuice/db/datalake/queries/gsheets/clean/general/tracking_catalog_events_aud.sql
+++ bietlejuice/db/datalake/queries/gsheets/clean/general/tracking_catalog_events_aud.sql
@@ -2,15 +2,20 @@ SELECT
 	CAST(NULLIF(REPLACE(id_app, ',', ''), '') AS INT) AS id_app,
 	NULLIF(category, '') AS category,
 	NULLIF(event_type, '') AS event_type,
-	NULLIF(owner, '') AS owner,
+	NULLIF(business_owner, '') AS business_owner,
+	NULLIF(technical_owner, '') AS technical_owner,
+	NULLIF(squad_ownership, '') AS squad_ownership,
 	NULLIF(platform, '') AS platform,
 	NULLIF(project_name, '') AS project_name,
+	NULLIF(screen_image_link, '') AS screen_image_link,
 	NULLIF(status, '') AS status,
+	NULLIF(emitting_app, '') AS emitting_app,
 	NULLIF(rev_type, '') AS rev_type,
 	NULLIF(rev_user, '') AS rev_user,
 	NULLIF(description, '') AS description,
-    FROM_UNIXTIME(CAST(NULLIF(rev_timestamp, '') AS BIGINT)/1000) AS ts_rev, -- this timestamp is in milliseconds
+	CAST(NULLIF(is_key_event, '') AS BOOLEAN) AS is_key_event,
+	FROM_UNIXTIME(CAST(NULLIF(rev_timestamp, '') AS BIGINT)/1000) AS ts_rev, -- this timestamp is in milliseconds
 	TO_DATE(REPLACE(first_seen, '/', '-'), 'M-d-y') AS first_seen,
-        TO_DATE(REPLACE(last_seen, '/', '-'), 'M-d-y') AS last_seen
+	TO_DATE(REPLACE(last_seen, '/', '-'), 'M-d-y') AS last_seen
 FROM
     datalake_gsheets_raw.events_aud;
--- bietlejuice/db/datalake/queries/gsheets/clean/general/tracking_catalog_projects.sql
+++ bietlejuice/db/datalake/queries/gsheets/clean/general/tracking_catalog_projects.sql
@@ -2,6 +2,10 @@ SELECT
     NULLIF(STRING(id_app),'') AS id_app,
     NULLIF(STRING(project_name),'') AS project_name,
     NULLIF(STRING(description),'') AS description,
-    NULLIF(STRING(owner),'') AS owner
+    NULLIF(STRING(owner),'') AS owner,
+    NULLIF(STRING(tracking_tool),'') AS tracking_tool,
+    NULLIF(STRING(lines),'') AS lines,
+    NULLIF(STRING(principal_metrics),'') AS principal_metrics,
+    NULLIF(STRING(key_concepts),'') AS key_concepts
 FROM
     datalake_gsheets_raw.tracking_catalog_projects
