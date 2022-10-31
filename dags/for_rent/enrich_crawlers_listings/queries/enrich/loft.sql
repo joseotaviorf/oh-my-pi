01fea16930ab3220f709be72e6cb4cee1155b768
Guilherme Ventura <44004512+guivnt@users.noreply.github.com>
2022-10-31 09:16:45 -0300

    [DFORSALE-3279] Migrate loft house listing datamart to enrich (#9438)

---
+++ dags/for_rent/enrich_crawlers_listings/queries/enrich/loft.sql
@@ -0,0 +1,122 @@
+WITH loft_listings_raw AS (
+    SELECT 
+        *,
+        CAST(CONCAT(CAST(year AS STRING), '-', CAST(month AS STRING), '-', CAST(day AS STRING)) AS DATE) AS ts_updated
+    FROM 
+        datalake_crawlers_listings_clean.loft
+),
+loft_listings AS (
+    SELECT
+        CONCAT(id, 'Loft') AS id_house,
+        id AS id_house_platform,
+        'Loft' AS platform,
+        agency_name AS 3p_name,
+        INITCAP(house_info.unit_type) AS unit_type,
+        INITCAP(house_info.usage_type[0]) AS usage_type,
+        property_type,
+        contract_type,
+        status AS business_type,
+        address.neighborhood AS neighborhood,
+        address.city AS city,
+        address.state AS state,
+        address.street AS address,
+        address.zip_code AS zip_code,
+        address.number AS st_number,
+        geolocation.latitude AS lat,
+        geolocation.longitude AS lng,
+        house_info.floor AS floors,
+        house_info.num_floors AS num_floors,
+        house_info.total_area AS total_area,
+        house_info.bedrooms AS bedrooms,
+        house_info.suites AS suites,
+        house_info.parking_spaces AS parking_spaces,
+        house_info.bathrooms AS bathrooms,
+        house_info.year_built AS year_built,
+        CAST(price.sale.price AS DOUBLE) AS price,
+        ROUND(CAST(price.sale.price AS DOUBLE)/CAST(house_info.total_area AS INT), 2) AS price_m2,
+        installments_price,
+        CAST(price.sale.condo_fee AS DOUBLE) AS condo_fee,
+        CAST(price.sale.iptu AS DOUBLE) AS iptu,
+        has_agency AS is_3p,
+        is_marketplace,
+        metadata.loft_property AS is_platform_property,
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY ts_updated DESC) = 1 AS is_last_status,
+        ts_created,
+        ts_updated, /* May have a better name */
+        FIRST_VALUE(ts_updated) OVER (PARTITION BY 1 ORDER BY ts_updated DESC) AS ts_last_extraction
+    FROM
+        loft_listings_raw
+    QUALIFY
+        is_last_status
+),
+regions AS (
+    SELECT 
+        rm.id_house_platform,
+        rm.id_neighborhood,
+        rm.neighborhood,
+        r.city_group
+    FROM
+        datalake_crawlers_listings.loft_region_mapping AS rm
+    LEFT JOIN 
+        datalake_region.region AS r 
+            ON rm.id_neighborhood = r.id
+            AND r.level IN ('SubRegiao', 'Cidade')
+    QUALIFY 
+        ROW_NUMBER() OVER (PARTITION BY id_house_platform ORDER BY dt_updated DESC) = 1 
+)
+SELECT 
+    l.id_house,
+    l.id_house_platform,
+    r.id_neighborhood,
+    l.platform,
+    l.3p_name,
+    IF(ts_updated = ts_last_extraction, 'Publicado', 'Despublicado') AS status,
+    l.unit_type,
+    l.usage_type,
+    l.property_type,
+    l.contract_type,
+    l.business_type,
+    l.city,
+    r.city_group,
+    r.neighborhood,
+    l.state,
+    l.address,
+    l.st_number,
+    l.zip_code,
+    l.lat,
+    l.lng,
+    l.floors,
+    l.num_floors,
+    NULL::INTEGER AS unit_per_floor, /* We don't have this column in Loft, but we put this column in to standardize with Emcasa (it may be unnecessary)*/
+    l.total_area,
+    l.bedrooms,
+    l.suites,
+    l.parking_spaces,
+    l.bathrooms,
+    l.year_built,
+    l.price,
+    l.price_m2,
+    l.installments_price,
+    l.condo_fee,
+    l.iptu,
+    /* Columns referring to the distance CTE present in the old datamart. A solution to check the uniqueness of Loft listings will be developed in another table.*/
+    NULL::INTEGER AS nearby_other_platform_houses,
+    NULL::DOUBLE AS avg_nearby_price_m2,
+    NULL::DOUBLE AS avg_distance, 
+    NULL::BOOLEAN AS is_exclusive, 
+    /* */
+    /* Standardization columns for other competitors. However, Loft only works with sale.*/
+    TRUE AS is_for_sale,
+    FALSE AS is_for_rent,
+    /* */
+    l.is_3p,
+    l.is_marketplace,
+    l.is_platform_property,
+    l.is_last_status,
+    l.ts_created,
+    l.ts_updated
+FROM
+    loft_listings AS l
+LEFT JOIN
+    regions AS r
+        USING(id_house_platform)
