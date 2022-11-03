f6860d98e4ca3a06babfbb24eec046e434fc5b16
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-11-03 14:36:50 -0300

    Add collumns in gsheet tqc_leads (#9468)

--- bietlejuice/db/datalake/queries/gsheets/clean/for_sale/tqc_leads.sql
+++ bietlejuice/db/datalake/queries/gsheets/clean/for_sale/tqc_leads.sql
@@ -1,8 +1,12 @@
 SELECT
     agent_email,
     agent_cpf,
+    agent_creci,
+    lead_name,
     lead_phone,
     lead_email,
+    agent_visits,
+    lead_interest_area,
     program_phase,
     BOOLEAN(NULLIF(is_valid_lead, '')) AS is_valid_lead,
     CAST(appointment_date AS TIMESTAMP) AS ts_appointment,
