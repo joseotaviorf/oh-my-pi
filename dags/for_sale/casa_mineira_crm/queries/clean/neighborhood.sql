0d8540bb1636ab6a9a927e786139ff385342ef37
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2022-11-08 17:30:17 -0300

    new column to neighborhood table for casa_mineira_crm (#9527)

--- dags/for_sale/casa_mineira_crm/queries/clean/neighborhood.sql
+++ dags/for_sale/casa_mineira_crm/queries/clean/neighborhood.sql
@@ -4,6 +4,7 @@ SELECT
     nome AS neighborhood_name,
     slug AS neighborhood_slug_name,
     slug_completo AS neighborhood_full_slug_name,
+    CAST(codigo_regiao_quintoandar AS INTEGER) AS region_code_5a,
     CAST(prioridade AS BOOLEAN) AS is_priority,
     CAST(visibilidade AS BOOLEAN) AS is_visible,
     CAST(criado_em AS TIMESTAMP) AS ts_created
