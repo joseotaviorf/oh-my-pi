8c0be3006e9e92e3db5c7bf2ee21ab7b6d20ab5c
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2022-11-10 16:52:29 -0300

    [DFORSALE] upgrade dag_inventory cluster to databricks_10_4_med_io-memory_cluster (#9544)
    * feat: upgrade cluster to databricks_10_4_med_io-memory_cluster

--- dags/rede/dag_inventory/dag_inventory.py
+++ dags/rede/dag_inventory/dag_inventory.py
@@ -23,7 +23,7 @@ SOURCE = "dag_inventory"
 DAG_ID = f"bietlejuice.{SOURCE}"
 MAIN_START_DATE = datetime(2019, 8, 21, tzinfo=timezone("America/Sao_Paulo"))
 MAIN_SCHEDULE_INTERVAL = "10 21 * * *"
-CLUSTER_DESCRIPTION = "databricks_10_4_med_general_cluster"
+CLUSTER_DESCRIPTION = "databricks_10_4_med_io-memory_cluster"
 PARTITION_COLS = ["year", "month", "day"]
 
 config_service = ConfigurationService(SOURCE)
