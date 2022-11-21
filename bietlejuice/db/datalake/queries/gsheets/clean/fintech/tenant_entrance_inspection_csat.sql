73048a3f2c284c7538ff0197089d5082e4a58d14
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2022-11-21 11:46:30 -0300

    [AES&S] Updated tenant_entrance_inspection_csat gsheets (#9591)

--- bietlejuice/db/datalake/queries/gsheets/clean/fintech/tenant_entrance_inspection_csat.sql
+++ bietlejuice/db/datalake/queries/gsheets/clean/fintech/tenant_entrance_inspection_csat.sql
@@ -1,9 +1,11 @@
-SELECT 
+SELECT
     INT(id_do_contrato) AS id_contract,
+    id_csat,
     NULLIF(email,'') AS email,
     NULLIF(pontos_de_melhoria,'') AS improvement_tags,
     NULLIF(campo_aberto,'') AS comments,
     INT(avaliacao_csat) AS general_satisfaction_evaluation,
+    house_satisfation,
     TIMESTAMP(submitted_at) AS ts_submitted
-FROM 
+FROM
     datalake_gsheets_raw.csat_vistoria_iq_entrada
