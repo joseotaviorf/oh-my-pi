dc685c048154d7eab3877c357681253bd8c45c1a
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2022-11-22 11:12:47 -0300

    fix query (#9607)

--- dags/growth/dw_affiliate_costs/queries/dw/fact_affiliate_costs.sql
+++ dags/growth/dw_affiliate_costs/queries/dw/fact_affiliate_costs.sql
@@ -1,9 +1,9 @@
 SELECT
-    id_rh_accounting_entry AS sk_rh_accounting_entry,
-    id_payee as sk_payee,
-    dt_transaction AS sk_date,
-    id_external AS sk_user,
-    cost,
-    now() as ts_load
+    id_rh_accounting_entry::BIGINT AS sk_rh_accounting_entry,
+    id_payee::BIGINT as sk_payee,
+    dt_transaction::BIGINT AS sk_date,
+    id_external::BIGINT AS sk_user,
+    cost::FLOAT,
+    now() AS ts_load
 FROM
     datalake_robin_hood.affiliate_costs
