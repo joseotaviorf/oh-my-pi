0384b7c947a5b85144a1a7d464da6d3fba0018c4
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2022-11-23 10:37:18 -0300

    fixing data types queries (#9620)

--- dags/growth/dw_affiliate_costs/queries/dw/dim_affiliate_cost.sql
+++ dags/growth/dw_affiliate_costs/queries/dw/dim_affiliate_cost.sql
@@ -1,12 +1,12 @@
 SELECT
-    id_rh_accounting_entry AS sk_rh_accounting_entry,
-    id_payee as sk_payee,
-    city_group,
-    description,
-    source_bill_item,
-    commission_type,
-    cost_center_code,
-    mkt_origin,
+    id_rh_accounting_entry::BIGINT AS sk_rh_accounting_entry,
+    id_payee::BIGINT AS sk_payee,
+    LEFT(city_group::STRING,255) AS city_group,
+    LEFT(description::STRING,255) AS description,
+    LEFT(source_bill_item::STRING,255) AS source_bill_item,
+    LEFT(commission_type::STRING,255) AS commission_type,
+    LEFT(cost_center_code::STRING,255) AS cost_center_code,
+    LEFT(mkt_origin::STRING,255) AS mkt_origin,
     now() as ts_load
 FROM
     datalake_robin_hood.affiliate_costs
--- dags/growth/dw_affiliate_costs/queries/dw/fact_affiliate_taxes.sql
+++ dags/growth/dw_affiliate_costs/queries/dw/fact_affiliate_taxes.sql
@@ -1,8 +1,8 @@
 SELECT
-  id AS sk_tax,
-  id_payee as sk_payee,
-  due_amount,
-  accounting_year_month as dt_accounting_year_month,
+  id::BIGINT AS sk_tax,
+  id_payee::BIGINT as sk_payee,
+  due_amount::DECIMAL(20,2) AS due_amount,
+  accounting_year_month::BIGINT AS dt_accounting_year_month,
   now() as ts_load
 FROM datalake_robin_hood_clean.tax
 WHERE due_amount IS NOT NULL
