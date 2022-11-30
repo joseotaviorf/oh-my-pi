dcdfe4287ff2836ca3d5db5480291f5e3d4df74c
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2022-11-30 16:58:17 -0300

    fixed business rule on account classification column from invoice all table (#9688)

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -64,10 +64,9 @@ SELECT
     ELSE 'other'
   END AS account_type,    
   CASE 
-    WHEN (ROUND(-1.0*i.due_amount,2) >= 0 AND NOT(from_account_type = 'landlord' OR to_account_type= 'landlord') ) 
-      OR (entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant'))
-      OR (entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc'))
-      THEN 'receivable'
+    WHEN entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc') THEN 'receivable'
+    WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant') THEN 'receivable'
+    WHEN (ROUND(-1.0*i.due_amount,2) > 0 OR (ROUND(-1.0*i.due_amount,2) = 0 AND NOT(from_account_type = 'landlord' OR to_account_type= 'landlord') )) THEN 'receivable'
     ELSE 'payable'
   END AS account_classification,  
   CASE
