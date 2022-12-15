e5b9a9f38488c38c8700d29ac38a31cfd8325cc1
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2022-12-15 11:26:56 -0300

    include ts_load to domain control clean layer. (#9805)

--- dags/cross/gsheets_by_context/queries/clean/general/domain_management_metabase_dashboards.sql
+++ dags/cross/gsheets_by_context/queries/clean/general/domain_management_metabase_dashboards.sql
@@ -4,6 +4,7 @@ SELECT
     updated_domain,
     editor,
     status ,
-    CAST(updated_at as TIMESTAMP) as updated_at                                                                                  
+    CAST(updated_at as TIMESTAMP) as updated_at,
+    ts_load                                                                                 
 FROM
     datalake_gsheets_raw.domain_management_metabase_dashboards
