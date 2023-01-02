ee91998912df3b55f748a993580cfa6a360dd592
Leticia Silveira <leticia.ingrid.silveira@gmail.com>
2023-01-02 14:14:23 +0100

    [AEFF] fix missing columns pdd datamart (#9824)
    * add column ts_canceled
    
    * add columns
    
    * lineage updated

--- dags/fintech/dw_fintech_snapshot/queries/dw/dim_invoice_snapshot.sql
+++ dags/fintech/dw_fintech_snapshot/queries/dw/dim_invoice_snapshot.sql
@@ -1,12 +1,16 @@
-SELECT 
+SELECT
     sk_invoice,
     frequency,
     payment_status,
+    negotiation_status,
+    closing_mode,
+    paid_via,
     user,
     due_amount,
-    paid_amount,   
+    paid_amount,
     accrual_year_month,
     ts_created,
+    ts_canceled,
     dt_sent,
     dt_due,
     dt_paid,
@@ -14,7 +18,7 @@ SELECT
     YEAR(ts_load) AS year,
     MONTH(ts_load) AS month,
     DAY(ts_load) AS day
-FROM 
+FROM
     dw_payment.dim_invoice
 WHERE
     ts_load IS NOT NULL
