728322e3ba2aec3ed17647d2aa053bd07b01b43a
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-01-03 16:05:26 -0300

    adding costs of credit card payment (#9809)

--- dags/fintech/enrich_revenue_lines/queries/enrich/credit_card_payment.sql
+++ dags/fintech/enrich_revenue_lines/queries/enrich/credit_card_payment.sql
@@ -11,6 +11,16 @@ SELECT
     i.due_amount AS invoice_theorical_amount,
     i.paid_amount AS invoice_paid_amount,
     (paid_amount + due_amount) AS invoice_paid_fee,
+    ROUND(charged_amount * (CASE 
+                        WHEN ccp.installments = 1 THEN 0.012
+                        WHEN ccp.installments <= 6 THEN 0.016
+                        WHEN ccp.installments <= 12 THEN 0.019
+                        ELSE NULL END), 2) AS acquirer_cost,
+    ROUND(charged_amount * (1- CASE 
+                            WHEN installments = 1 THEN 0.012
+                            WHEN installments <= 6 THEN 0.016
+                            WHEN installments <= 12 THEN 0.019
+                            ELSE NULL END) * 0.006 * (installments+1)/2, 2) AS advance_cost,
     i.accrual_year_month,
     DATE(ccp.ts_created) AS dt_ccp_created,
     DATE(i.ts_due) AS dt_due,
