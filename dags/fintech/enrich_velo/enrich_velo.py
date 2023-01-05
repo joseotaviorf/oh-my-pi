33d9ae1504d8e71148ce27447052c17658550571
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2023-01-05 19:32:39 -0300

    Fixed import paths for velo DAGs (#9877)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- dags/fintech/enrich_velo/enrich_velo.py
+++ dags/fintech/enrich_velo/enrich_velo.py
@@ -11,10 +11,10 @@ from airflow.operators.quintoandar_databricks import (
 
 from bietlejuice.base.airflow.base_dag import BaseDAG
 from bietlejuice.base.airflow.dag_owner_enum import DAGOwnerEnum
-from bietlejuice.base.pipeline.layer_enum import LayerEnum
-from bietlejuice.dags.base.datalake_task_group import DatalakeTaskGroup
-from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.airflow.helpers.task_flow_helper import TaskFlowHelper
+from bietlejuice.base.airflow.task_groups.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
