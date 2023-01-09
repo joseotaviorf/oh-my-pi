d59ab6537b507d6497276e31e55ba58a7503762e
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2023-01-09 15:15:34 -0300

    [AESS - 1012] Update Condominium Payments spark job name #9886

--- dags/support_and_service/condominium_payments/spark_jobs/load_condominium_payments_raw.py
+++ dags/support_and_service/condominium_payments/spark_jobs/load_condominium_payments_raw.py
@@ -11,7 +11,7 @@ from bietlejuice.loaders import SparkMetastoreLoader
 from bietlejuice.loaders.s3_loader import S3Loader
 from bietlejuice.services.metastore_services import SparkMetastoreService
 
-JOB_NAME = "load_condominium_payments_into_datalake"
+JOB_NAME = "load_condominium_payments_raw"
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 logger = QuintoAndarLogger(JOB_NAME)
