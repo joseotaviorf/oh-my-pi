688c896e1d0f0ba243b81f6f5af5f80440f9d70d
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2023-01-10 19:04:10 -0300

    revert changes from redshift datamarts (#9922)

--- dags/for_sale/dw_datamarts_for_sale_cross/queries/dw/sale_cohort_conversions.sql
+++ dags/for_sale/dw_datamarts_for_sale_cross/queries/dw/sale_cohort_conversions.sql
@@ -103,7 +103,7 @@ SELECT
     fv.sk_offer,
     fv.sk_booking,
     fv.is_hub_flow,
-	hs.hub_name AS hub_visit,
+	sv.business_unit AS hub_visit,
     dr.city_group,
     COALESCE(db.is_3p_demand, FALSE) AS is_3p_demand,
     COALESCE(db.partner_3p_demand, '') AS demand_3p_partner,
@@ -111,16 +111,21 @@ SELECT
     COALESCE(db.partner_3p_supply, '') AS supply_3p_partner,
     DATE(NULLIF(fv.sk_booking_created_date,-1)) AS dt_created,
     DATE(NULLIF(fv.sk_visit_completed_date,-1)) AS dt_completed,
-    ROW_NUMBER() OVER (PARTITION BY fv.sk_booking ORDER BY COALESCE(hs.ts_updated,current_date) DESC ) AS order_booking
+    ROW_NUMBER() OVER (PARTITION BY fv.sk_booking ORDER BY COALESCE(bur.dt_coverage_ended,current_date) DESC ) AS order_booking
 FROM
     sale.fact_visits fv
 JOIN dim_region dr
     ON fv.sk_region = dr.sk_region
 JOIN dim_booking AS db
     ON db.sk_booking = fv.sk_booking
+LEFT JOIN
+     sale.fact_business_unit_region bur
+        ON bur.sk_region = fv.sk_region
+        AND DATE(fv.sk_booking_created_date) BETWEEN DATE (bur.dt_coverage_started) 
+        AND DATE(COALESCE(bur.dt_coverage_ended, current_date))
 LEFT JOIN 
-	datalake_hub_services_clean_prod.business_unit AS hs
-		ON hs.id = fv.sk_business_unit
+	datalake_sale_visit_hubs_prod.sale_visit_hubs AS sv
+		ON sv.id_booking = fv.sk_booking
 ),
 sale_bookings AS (
 SELECT 
@@ -343,8 +348,8 @@ SELECT
     CASE WHEN rbh.id_house IS NOT NULL THEN 1 ELSE 0 END AS is_3pbh_supply,
     rbh.partner AS supply_3pbh_partner,
     CASE
-        WHEN COALESCE(sdr.city_group,sdc.city_group) NOT IN ('RMSP', 'Rio de Janeiro','Belo Horizonte','Porto Alegre','Campinas') THEN 'Out of coverage area'
-        WHEN COALESCE(sdr.city_group,sdc.city_group) IN ('RMSP', 'Rio de Janeiro','Belo Horizonte','Porto Alegre','Campinas') THEN COALESCE(sdr.city_group,sdc.city_group)
+        WHEN COALESCE(sdr.city_group,sdc.city_group) NOT IN ('RMSP', 'Rio de Janeiro','Porto Alegre','Campinas') THEN 'Out of coverage area'
+        WHEN COALESCE(sdr.city_group,sdc.city_group) IN ('RMSP', 'Rio de Janeiro','Porto Alegre','Campinas') THEN COALESCE(sdr.city_group,sdc.city_group)
     END AS city_group,
     sdc.form_of_payment,
     sdc.dt_created,
