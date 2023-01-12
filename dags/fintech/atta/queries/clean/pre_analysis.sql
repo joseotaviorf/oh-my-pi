9377400cb3482457ee24fb2ee1bdfb7bde846ffd
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2023-01-12 16:48:27 -0300

    AEFF 749 adicao de novas tabelas da atta na modelagem de fintech for sale (#9924)
    * Added consulta_score_bradesco
    
    * Added pre_analysis_bradesco table
    
    * Deleted consulta_score_bradesco table;
    
    * Added pre_analysis_bradesco
    
    * Added pre_analysis_bradesco table
    
    * Edited pre_analysis_bradesco table
    
    * Added pre_analysis_bradesco metadata
    
    * added atta tables
    
    * Added metadata files
    
    * added atta tables
    
    * Renamed proposal_notes table
    
    * AEFF-749-adicao-de-novas-tabelas-da-atta-na-modelagem-de-fintech-for-sale
    
    * AEFF-749-adicao-de-novas-tabelas-da-atta-na-modelagem-de-fintech-for-sale
    
    * AEFF-749-adicao-de-novas-tabelas-da-atta-na-modelagem-de-fintech-for-sale
    
    * AEFF-749-adicao-de-novas-tabelas-da-atta-na-modelagem-de-fintech-for-sale
    
    * AEFF-749 Adição de novas tabelas para Clean da Atta de Fintech ForSale
    
    * AEFF-749 - Adição de novas tabelas para Clean da Atta de Fintech ForSale
    
    * AEFF-749 - Adição de novas tabelas para Clean da Atta de Fintech ForSale
    
    * Update dags/fintech/atta/queries/clean/pre_analysis.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_bradesco_ratings.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_bradesco.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_itau.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_caixa.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_itau_ratings.sql
    
    * Update dags/fintech/atta/queries/clean/proposal_notes.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_bradesco.sql
    
    * modified:   dags/fintech/atta/metadata/clean/pre_analysis_bradesco.yml
    	modified:   dags/fintech/atta/metadata/clean/pre_analysis_bradesco_ratings.yml
    	modified:   dags/fintech/atta/metadata/clean/pre_analysis_caixa.yml
    	modified:   dags/fintech/atta/metadata/clean/pre_analysis_itau.yml
    	modified:   dags/fintech/atta/metadata/clean/pre_analysis_itau_ratings.yml
    	modified:   dags/fintech/atta/metadata/clean/pre_analysis_santander.yml
    
    * updated raw metadata
    
    * Update dags/fintech/atta/queries/clean/pre_analysis_bradesco.sql
    
    Co-authored-by: Leticia Silveira <leticia.ingrid.silveira@gmail.com>
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    Co-authored-by: Leticia Silveira <leticia.ingrid.silveira@gmail.com>

--- dags/fintech/atta/queries/clean/pre_analysis.sql
+++ dags/fintech/atta/queries/clean/pre_analysis.sql
@@ -1,13 +1,16 @@
 SELECT
-    id AS id_pre_analysis,
-    idfranquia AS id_franchise,
-    idparceiro AS id_partner,
-    idpreanaliseext AS id_offer,
-    idusuario AS id_registration_user,
-    cpf AS client_cpf,
-    vlrentrada AS down_payment_amount,
-    vlrfinanciado AS financing_value,
-    vlrimovel AS house_value,
+    id                          AS id_pre_analysis,
+    idfranquia                  AS id_franchise,
+    idparceiro                  AS id_partner,
+    idpreanaliseext             AS id_offer,
+    idusuario                   AS id_registration_user,
+    cpf                         AS client_cpf,
+    `status`,
+    priority                    AS priority_status,
+    situacao                    AS situation,
+    vlrentrada                  AS down_payment_amount,
+    vlrfinanciado               AS financing_value,
+    vlrimovel                   AS house_value,
     TIMESTAMP(data_solicitacao) AS ts_registration
 FROM
     datalake_atta_raw.consulta_score
