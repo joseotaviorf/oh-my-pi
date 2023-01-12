726398f61708a4cb10891a70447aff7779d8e0fe
Guilherme Dorella <72612610+guilherme-dorella@users.noreply.github.com>
2023-01-12 16:13:59 -0300

    Creating new sheets ingestion for México cohort targets (#9941)
    * Creating new sheets ingestion for México cohort targets
    
    * updating queries and lineages
    
    Co-authored-by: Guilherme Dorella <guilhermedorella@BRSPPM-C02GL2TAMD6M.local>
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ dags/cross/gsheets_by_context/queries/clean/for_rent/mexico_rental_cohort_supply.sql
@@ -0,0 +1,15 @@
+SELECT
+    city_group,
+    first_listing,
+    mkt_channel,
+    mkt_origin,
+    operacao,
+    opportunities,
+    prospect,
+    qualified,
+    available_qualifieds,
+    CAST(year AS INTEGER) AS year,
+    week_origin,
+    TO_DATE(week, 'yyyy-MM-dd') AS dt_week_started
+FROM
+    datalake_gsheets_raw.mexico_rental_cohort_supply
