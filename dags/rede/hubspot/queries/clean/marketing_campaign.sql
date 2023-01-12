ff1d3f3711c10277bb85cb9d807af3f6bf60c26b
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-01-12 09:42:11 -0300

    [HOTFIX] Removing nonexistent columns - marketing_campaign - hubspot (#9932)

--- dags/rede/hubspot/queries/clean/marketing_campaign.sql
+++ dags/rede/hubspot/queries/clean/marketing_campaign.sql
@@ -5,12 +5,7 @@ SELECT
     appName AS app_name,
     subject,
     name,
-    processingState AS processing_state,
     type,
-    FROM_JSON(counters, 'map<string, int>') AS counters,
-    numIncluded::INT AS num_included,
-    TO_TIMESTAMP(NULLIF(lastProcessingStartedAt/1000, 0)) AS ts_last_processing_started,
-    TO_TIMESTAMP(NULLIF(lastProcessingFinishedAt/1000, 0)) AS ts_last_processing_finished,
-    TO_TIMESTAMP(NULLIF(lastProcessingStateChangeAt/1000, 0)) AS ts_last_processing_state_change
+    FROM_JSON(counters, 'map<string, int>') AS counters
 FROM
     datalake_hubspot_raw.marketing_campaign
