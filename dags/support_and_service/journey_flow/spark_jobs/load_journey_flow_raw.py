a78cfce3b1b7310de4c9dd29377b73c74fcc95cc
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2023-01-16 10:45:03 -0300

    [AESS - 1019] Journey Flow spark job name #9919

--- dags/support_and_service/journey_flow/spark_jobs/load_journey_flow_raw.py
+++ dags/support_and_service/journey_flow/spark_jobs/load_journey_flow_raw.py
@@ -11,7 +11,7 @@ from bietlejuice.loaders import SparkMetastoreLoader
 from bietlejuice.loaders.s3_loader import S3Loader
 from bietlejuice.services.metastore_services import SparkMetastoreService
 
-JOB_NAME = "load_journey_flow_into_datalake"
+JOB_NAME = "load_journey_flow_raw"
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 logger = QuintoAndarLogger(JOB_NAME)
