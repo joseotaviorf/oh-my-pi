333064f262334f1f123300661d8abda964c517ab
fgchies <118193092+fgchies@users.noreply.github.com>
2023-01-17 16:19:39 -0300

    [AEFR-1519] Updating house_rent_listing_consultant to solve CIQ issues (#9969)
    * Atualizando a house_rent_listing_consultant para resolver problemas com os dados de CIQ
    
    * Query ajustada conforme sugerido na revisão da PR
    
    * Added load-enrich-house in the dependencies file

--- dags/for_rent/enrich_big_agent/queries/enrich/house_rent_listing_consultant.sql
+++ dags/for_rent/enrich_big_agent/queries/enrich/house_rent_listing_consultant.sql
@@ -23,11 +23,15 @@ WITH historical_consultant AS (
     FROM
         datalake_ebdb_listing.house_listing AS hl
     LEFT JOIN
+        datalake_ebdb_listing.house as ho
+            ON hl.id_house = ho.id
+    LEFT JOIN
         datalake_big_agent.house_consultant_history AS hch
             ON hl.id_house = hch.id_house
             AND (
-                    (hch.ts_enrollment_started >= hl.ts_listing_version_start AND  hch.ts_enrollment_started < COALESCE(hl.ts_listing_version_end, '2100-04-01'))
-                    OR (hl.ts_listing_version_start >= hch.ts_enrollment_started AND hl.ts_listing_version_start < COALESCE(hch.ts_enrollment_ended, '2100-04-01'))
+                    (hch.ts_enrollment_started >= COALESCE(hl.ts_listing_version_start,ho.dt_creation) AND  hch.ts_enrollment_started < COALESCE(hl.ts_listing_version_end, '2100-04-01'))
+                    OR 
+                    (COALESCE(hl.ts_listing_version_start,ho.dt_creation) >= hch.ts_enrollment_started AND COALESCE(hl.ts_listing_version_start,ho.dt_creation) < COALESCE(hch.ts_enrollment_ended, '2100-04-01'))
                 )
             AND hch.is_last_status_of_day = True
 )
@@ -52,8 +56,7 @@ SELECT
     */
     CASE 
         WHEN consultant_type IS NULL AND was_ciq_full THEN 'CIQ_FULL'
-        WHEN ts_enrollment_ended < COALESCE(ts_listing_version_end, '2100-01-01') THEN 'Core'
-        WHEN consultant_type IS NULL THEN 'Core'
+        WHEN consultant_type IS NULL OR (consultant_type <> 'CIQ_FULL' AND ts_enrollment_ended < COALESCE(ts_listing_version_end, '2100-01-01')) THEN 'Core'
         ELSE consultant_type
     END AS consultant_type,
     CASE
