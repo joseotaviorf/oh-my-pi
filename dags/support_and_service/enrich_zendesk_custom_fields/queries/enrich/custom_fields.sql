e62551c2e8c818c549f33db33695c19582dc7ca7
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2023-01-18 17:10:21 -0300

    [AESS-fix] Upgrading Zendesk clusters (#9990)

--- dags/support_and_service/enrich_zendesk_custom_fields/queries/enrich/custom_fields.sql
+++ dags/support_and_service/enrich_zendesk_custom_fields/queries/enrich/custom_fields.sql
@@ -1,23 +1,11 @@
 -- The double curly brackets (chave) had to be put to escape that character in the function .format in Python. If you test this in Databricks or somewhere else, remember to replace by a single curly bracket
-WITH last_ticket_update AS (
-    SELECT
-        t.id_ticket,
-        MAX(t.ts_updated) AS ts_last_update
-    FROM
-        datalake_zendesk_tickets_clean.tickets t
-    GROUP BY 1
-),
-filtered_custom_fields AS (
+WITH filtered_custom_fields AS (
     SELECT
         tck.id_ticket,
         -- The double curly brackets (chave) had to be put to escape that character in the function .format in Python. If you test this in Databricks or somewhere else, remember to replace by a single curly bracket
         EXPLODE(SPLIT(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REGEXP_REPLACE(tck.custom_fields, '"(?!")', ''), 'id:', ''), ',value', ''), '[', ''), ']', ''), '{{', ''), '}}', ''), ',')) AS custom_field
     FROM
-        datalake_zendesk_tickets_clean.tickets tck
-    JOIN
-        last_ticket_update ltu
-            ON tck.id_ticket = ltu.id_ticket
-            AND tck.ts_updated = ltu.ts_last_update
+        datalake_zendesk_tickets_clean.tickets AS tck
 ),
 parsed_custom_fields AS (
     SELECT DISTINCT
@@ -26,9 +14,9 @@ parsed_custom_fields AS (
         SPLIT(custom_field, ':')[1] AS custom_field_value,
         tf.raw_title AS custom_field_title
     FROM
-        filtered_custom_fields tcf
+        filtered_custom_fields AS tcf
     JOIN
-        datalake_zendesk_tickets_clean.ticket_fields tf
+        datalake_zendesk_tickets_clean.ticket_fields AS tf
             ON tf.id_ticket_fields = SPLIT(custom_field, ':')[0]
     WHERE
         NULLIF(NULLIF(REPLACE(SPLIT(custom_field, ':')[1], '"', ''), ''), 'null') IS NOT NULL
