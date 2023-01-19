9450c6d396d88ad9105662555861b4d09d4e95ee
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2023-01-19 15:33:17 -0300

    [SAP]Removing regex obfuscation (#9995)

--- dags/fintech/sap/queries/clean/incoming_payments_lines.sql
+++ dags/fintech/sap/queries/clean/incoming_payments_lines.sql
@@ -27,10 +27,11 @@ SELECT
     contraact_contraact AS contra_act,
     location_profitcode AS location_profit_code,
     managerial_ocrcode3 AS managerial_code,
-    CASE
-        WHEN cte.manual_insert = TRUE THEN REGEXP_EXTRACT(memo_linememo, '((?:CI|CR|FC|FP|CP).*)')
-        ELSE REGEXP_EXTRACT(memo_linememo, '^"?.*(?<clean>(?:CI|CR|FC|FP|CP)[^ -]+)[ -]?.*"?$')
-    END AS memo_line,
+    -- CASE
+    --     WHEN cte.manual_insert = TRUE THEN REGEXP_EXTRACT(memo_linememo, '((?:CI|CR|FC|FP|CP).*)')
+    --     ELSE REGEXP_EXTRACT(memo_linememo, '^"?.*(?<clean>(?:CI|CR|FC|FP|CP)[^ -]+)[ -]?.*"?$')
+    -- END AS memo_line,
+    memo_line,
     number_number AS source_document_number,
     series_series AS series,
     account_account AS account,
--- dags/fintech/sap/queries/clean/invoices.sql
+++ dags/fintech/sap/queries/clean/invoices.sql
@@ -8,17 +8,18 @@ SELECT
     legacy_uuid_u_rsd_uuidsb AS legacy_uuid,
     document AS id_document,
     docentry AS document_entry,
-    CASE
-        WHEN
-            createdby_usersign IN (1, 83)
-            AND (
-                uuid_u_rsd_uuid IS NULL
-                OR legacy_uuid_u_rsd_uuidsb IS NULL
-                OR uuid_u_rsd_uuid RLIKE '^[a-z]{{1}}:.*$'
-                OR legacy_uuid_u_rsd_uuidsb RLIKE '^[a-z]{{1}}:.*$'
-            ) THEN REGEXP_EXTRACT(memo, '((?:CI|CR|FC|FP|CP).*)')
-        ELSE REGEXP_EXTRACT(memo, '^"?.*(?<clean>(?:CI|CR|FC|FP|CP)[^ -]+)[ -]?.*"?$')
-    END AS memo,
+    -- CASE
+    --     WHEN
+    --         createdby_usersign IN (1, 83)
+    --         AND (
+    --             uuid_u_rsd_uuid IS NULL
+    --             OR legacy_uuid_u_rsd_uuidsb IS NULL
+    --             OR uuid_u_rsd_uuid RLIKE '^[a-z]{{1}}:.*$'
+    --             OR legacy_uuid_u_rsd_uuidsb RLIKE '^[a-z]{{1}}:.*$'
+    --         ) THEN REGEXP_EXTRACT(memo, '((?:CI|CR|FC|FP|CP).*)')
+    --     ELSE REGEXP_EXTRACT(memo, '^"?.*(?<clean>(?:CI|CR|FC|FP|CP)[^ -]+)[ -]?.*"?$')
+    -- END AS memo,
+    memo,
     number_number AS source_document_number,
     series_series AS series,
     source_client_u_sourceclient AS source_client,
