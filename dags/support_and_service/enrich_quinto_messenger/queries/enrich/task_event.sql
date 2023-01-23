c8e1fc2f1a0fc5e715721b23f30bf6bde3dc34b6
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2023-01-23 09:23:48 -0300

    [DDSS-1126] Optimize Enrich Quinto Messenger Cluster (#9998)
    * Create custom cluster config files
    
    * Optimized task_event sql file
    
    * Fixed alias

--- dags/support_and_service/enrich_quinto_messenger/queries/enrich/task_event.sql
+++ dags/support_and_service/enrich_quinto_messenger/queries/enrich/task_event.sql
@@ -1,12 +1,4 @@
-WITH last_extracted_events AS (
-	SELECT
-		id_event,
-		MAX(DATE(CONCAT(CAST(te.year AS VARCHAR(4)), '-', CAST(te.month AS VARCHAR(2)), '-', CAST(te.day AS VARCHAR(2))))) AS dt_last_extracted
-	FROM
-		datalake_quinto_messenger_clean.task_event AS te
-	GROUP BY 1
-),
-twilio_date AS (
+WITH twilio_date AS (
     SELECT
         id_task_external,
         MAX(ts_created) AS ts_twilio_created,
@@ -25,10 +17,10 @@ SELECT
 	te.event_type AS type,
 	GET_JSON_OBJECT(te.event_payload,'$.TaskQueueName') AS task_queue_name,
 	GET_JSON_OBJECT(te.event_payload,'$.TaskChannelUniqueName') AS channel_name,
-	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.full_name') as agent_full_name,
-	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.email') as agent_email,
-	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.skills') as agent_skills,
-	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.location') as agent_location,
+	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.full_name') AS agent_full_name,
+	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.email') AS agent_email,
+	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.skills') AS agent_skills,
+	GET_JSON_OBJECT(GET_JSON_OBJECT(event_payload,'$.WorkerAttributes'), '$.location') AS agent_location,
 	GET_JSON_OBJECT(te.event_payload,'$.TaskAssignmentStatus') AS task_assignment_status,
 	GET_JSON_OBJECT(te.event_payload,'$.TaskCompletedReason') AS task_completion_reason,
 	CAST(GET_JSON_OBJECT(te.event_payload,'$.TaskPriority') AS INT) AS task_priority,
@@ -42,10 +34,7 @@ SELECT
 	FROM_UTC_TIMESTAMP(ts_twilio_updated, 'America/Sao_Paulo') AS ts_twilio_updated_local
 FROM
 	datalake_quinto_messenger_clean.task_event AS te
-INNER JOIN 
-    last_extracted_events lev
-      	ON lev.id_event = te.id_event
-      	AND lev.dt_last_extracted = DATE(CONCAT(CAST(te.year AS VARCHAR(4)), '-', CAST(te.month AS VARCHAR(2)), '-', CAST(te.day AS VARCHAR(2))))
-LEFT JOIN 
-    twilio_date ted
+LEFT JOIN
+    twilio_date AS ted
     	ON te.id_task_external = ted.id_task_external
+QUALIFY ROW_NUMBER() OVER(PARTITION BY te.id_event ORDER BY te.year DESC, te.month DESC, te.day DESC) = 1
