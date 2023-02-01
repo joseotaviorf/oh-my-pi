756ca309f761a6c0ab8b8952f7f193a074962121
heri-macedo <61527726+heri-macedo@users.noreply.github.com>
2023-02-01 18:59:35 -0300

    [DFORSALE]: Fix table name (#10093)

--- dags/for_sale/enrich_sale_offer_flows/queries/enrich/sale_offer_users_info.sql
+++ dags/for_sale/enrich_sale_offer_flows/queries/enrich/sale_offer_users_info.sql
@@ -9,10 +9,12 @@ WITH possible_combinations AS (
         CASE
             WHEN is_seller_ccv_signer IS TRUE THEN "Signer"
             WHEN is_seller_ccv_signer IS FALSE THEN "Not Signer"
+        END AS seller_ccv_signer_status,
+        CASE
             WHEN buyer_signer_status = "SIGNER" THEN "Signer"
             WHEN buyer_signer_status = "NOT_SIGNER" THEN "Not Signer"
             ELSE "Unknown"
-        END AS ccv_signer_status,
+        END AS buyer_ccv_signer_status,
         CASE
             WHEN is_pj IS TRUE THEN "Pessoa Jurídica"
             WHEN is_pj IS FALSE THEN "Pessoa Física"
@@ -31,7 +33,14 @@ WITH possible_combinations AS (
             WHEN has_joint_buyer IS TRUE THEN "Has a joint buyer"
             WHEN has_joint_buyer IS FALSE THEN "Has no joint buyer"
             ELSE "Unknown"
-        END AS joint_buyer_status
+        END AS joint_buyer_status,
+        is_ccv_buyer,
+        is_seller_ccv_signer,
+        buyer_signer_status,
+        is_pj,
+        is_original_user,
+        is_income_partner,
+        has_joint_buyer
     FROM
         datalake_sale_offer_flows.sale_offer_users
 ),
@@ -39,27 +48,36 @@ last_sk_values AS (
     SELECT
         COALESCE(MAX(sk_user_info), 0) AS sk_user_info
     FROM
-        datalake_sale_offer_flows.sale_offer_user_info
+        datalake_sale_offer_flows.sale_offer_users_info
 )
 SELECT
     COALESCE(sui.sk_user_info, l_sk.sk_user_info + MONOTONICALLY_INCREASING_ID() + 1) AS sk_user_info,
     pos.user_type,
     pos.ccv_buyer_status,
-    pos.ccv_signer_status,
+    pos.seller_ccv_signer_status,
+    pos.buyer_ccv_signer_status,
     pos.user_legal_type,
     pos.original_user_status,
     pos.income_partner_status,
-    pos.joint_buyer_status
+    pos.joint_buyer_status,
+    pos.is_ccv_buyer,
+    pos.is_seller_ccv_signer,
+    pos.buyer_signer_status,
+    pos.is_pj,
+    pos.is_original_user,
+    pos.is_income_partner,
+    pos.has_joint_buyer
 FROM
     possible_combinations AS pos,
     last_sk_values AS l_sk
 LEFT JOIN
-    datalake_sale_offer_flows.sale_offer_user_info AS sui
+    datalake_sale_offer_flows.sale_offer_users_info AS sui
         USING (
           sk_user_info,
           user_type,
           ccv_buyer_status,
-          ccv_signer_status,
+          seller_ccv_signer_status,
+          buyer_ccv_signer_status,
           user_legal_type,
           original_user_status,
           income_partner_status,
