34494315044dba5363150e1901bb3ef7e944a3c8
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2023-02-02 12:47:12 -0300

    [DFORSALE] Fix json extraction in datalake_hubspot.company_history (#10099)

--- dags/rede/enrich_hubspot/queries/enrich/company_history.sql
+++ dags/rede/enrich_hubspot/queries/enrich/company_history.sql
@@ -63,10 +63,10 @@ SELECT
         'array<struct<
             value:string,
             timestamp:timestamp,
-            source_type:string,
-            source_id:string,
-            source_label:string,
-            updated_by_user_id:string
+            sourceType:string,
+            sourceId:string,
+            sourceLabel:string,
+            updatedByUserId:string
         >>'
     ) AS lead_status_history,
     COALESCE(GET_JSON_OBJECT(properties, '$.num_associated_deals')::INT, 0) AS num_associated_deals,
