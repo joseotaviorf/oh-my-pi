67524363a7b0689bc906194e6b0adbc4d867e0df
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2023-02-09 15:00:36 -0300

    [DDSS - 1127] enrich_repair_services photon #10152

--- dags/support_and_service/enrich_repair_services/enrich_repair_services.py
+++ dags/support_and_service/enrich_repair_services/enrich_repair_services.py
@@ -41,7 +41,7 @@ s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
 spark_jobs_path = f"{s3_prefix}/spark_jobs/base"
 
 # databricks var
-cluster_description = config_service.get_config("databricks_10_4_med_memory_cluster")
+cluster_description = config_service.get_config("databricks_10_4_med_general_photon_cluster")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
