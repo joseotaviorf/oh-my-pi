afd1672e02d642ab3c339dc45582e9317b527873
matheusmello-o <31773902+matheusmello-o@users.noreply.github.com>
2023-02-13 10:56:19 -0300

    Removing temporary filter that removed nBP from Casa Mineira migration (#10133)
    Co-authored-by: Matheus Mello <matheuscunha@BRSPPM-C02GKGR9MD6M.local>

--- dags/cross/dw_datamarts_spark/queries/dw/growth_cross/sale_performance_marketing_metrics_demand.sql
+++ dags/cross/dw_datamarts_spark/queries/dw/growth_cross/sale_performance_marketing_metrics_demand.sql
@@ -68,7 +68,6 @@ events AS (
         AND db.visit_intent = 'SALE'
         AND db.type = 'Visita'
         AND db.dt_created IS NOT NULL
-        AND db.first_update_source != 'MigracaoCasaMineira'
 
     UNION ALL
 
