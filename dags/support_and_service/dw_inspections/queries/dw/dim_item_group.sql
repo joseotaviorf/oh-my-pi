a246c218121c704c2fffaf23c3fc70f612e15998
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-02-15 11:22:25 -0300

    [AES&S] Adding new indicators in the dim_group_item table (#10070)
    * Update dim_item_group.sql
    
    * Update dim_item_group.sql
    
    * Update dim_item_group.sql
    
    * add photon runtime engine
    
    * Revert "add photon runtime engine"
    
    This reverts commit e1da206e863a718e1aa1048b5f148eae0a238a82.
    
    * update dependencies
    
    * fix rebase conflicts

--- dags/support_and_service/dw_inspections/queries/dw/dim_item_group.sql
+++ dags/support_and_service/dw_inspections/queries/dw/dim_item_group.sql
@@ -1,8 +1,50 @@
-SELECT
+WITH item_group_comments AS (
+    SELECT
+        i.id_item_group,
+        SUM(
+            INT(
+                CASE
+                    WHEN ii.id_item_issue IS NOT NULL AND i.display_type in ('CHECK_DESCRIPTION', 'CHECK') THEN TRUE
+                    ELSE FALSE
+                END
+            )
+        ) > 0 AS has_item_checklist,
+        SUM(
+            INT(
+                CASE
+                    WHEN ii.id_item_issue IS NOT NULL AND i.display_type in ('CHIP_CHOICE_SINGLE', 'OVERVIEW_WITH_CONDITIONS') THEN TRUE
+                    ELSE FALSE
+                END
+            )
+        ) > 0 AS has_item_chip_choice,
+        SUM(
+            INT(
+                CASE
+                    WHEN i.comment IS NOT NULL AND i.item_type IN ('overview','other') THEN TRUE
+                    WHEN ii.issue_comment IS NOT NULL THEN TRUE
+                    ELSE FALSE
+                END
+            )
+        ) > 0 AS has_inspector_open_comment
+    FROM
+        datalake_inspections.item AS i
+    LEFT JOIN
+        datalake_inspections.item_issue AS ii
+            ON ii.id_item = i.id_item
+    WHERE
+        i.year = {year}
+        AND i.month = {month}
+        AND i.day = {day}
+    GROUP BY 1
+)
+SELECT DISTINCT
     ig.id_item_group AS sk_item_group,
     ig.item_group_name,
     ig.item_group_type,
     ig.status,
+    COALESCE(igc.has_item_checklist, FALSE) AS has_item_checklist,
+    COALESCE(igc.has_item_chip_choice, FALSE) AS has_item_chip_choice,
+    COALESCE(igc.has_inspector_open_comment, FALSE) AS has_inspector_open_comment,
     ig.is_inferior_quality,
     ig.ts_created,
     ig.ts_updated,
@@ -11,6 +53,9 @@ SELECT
     ig.day
 FROM
     datalake_inspections.item_group AS ig
+LEFT JOIN
+    item_group_comments AS igc
+        ON igc.id_item_group = ig.id_item_group
 WHERE
     ig.year = {year}
     AND ig.month = {month}
