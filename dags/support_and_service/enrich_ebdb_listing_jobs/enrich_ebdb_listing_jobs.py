9bf630f2d671b766b2f68c71942f710f47304b03
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2023-02-16 12:11:07 -0300

    [DDSS - 1117] Update EBDB Listing Jobs cluster to Photon (#10187)
    * Update cluster to med_io_general_photon
    
    * Insert partition_col in config_file

--- dags/support_and_service/enrich_ebdb_listing_jobs/enrich_ebdb_listing_jobs.py
+++ dags/support_and_service/enrich_ebdb_listing_jobs/enrich_ebdb_listing_jobs.py
@@ -33,7 +33,9 @@ databricks_bietlejuice_repo_path = config_service.get_config(
     "databricks_bietlejuice_repo_path"
 )
 base_spark_job_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
-cluster_description = config_service.get_config("custom_cluster")
+cluster_description = config_service.get_config(
+    "databricks_10_4_med_io-general_photon_cluster"
+)
 default_libraries = config_service.get_config("default_libraries")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
