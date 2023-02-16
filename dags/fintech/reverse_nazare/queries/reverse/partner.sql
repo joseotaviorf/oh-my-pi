d94d087f677a8b3e90c66a2b88fe9627b0231357
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2023-02-16 19:12:52 -0300

    AEFF-977 reverse_nazare add category dates(#10225)

--- dags/fintech/reverse_nazare/queries/reverse/partner.sql
+++ dags/fintech/reverse_nazare/queries/reverse/partner.sql
@@ -5,6 +5,8 @@ SELECT
     adm_5a AS quintoandar_administration_fee,
     demand_partner_revenue_share AS demand_partner_fee,
     supply_partner_revenue_share AS supply_partner_fee,
+    COALESCE(LAG(DATE_ADD(dt_category_end, 1)) OVER(PARTITION BY partner_short_name ORDER BY COALESCE(dt_category_end, CURRENT_DATE)), DATE('2021-01-01')) AS category_start_date,
+    dt_category_end AS category_end_date,
     YEAR(CURRENT_DATE) AS year,
     MONTH(CURRENT_DATE) AS month,
     DAY(CURRENT_DATE) AS day
