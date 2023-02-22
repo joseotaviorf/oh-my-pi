ea03e661bc8376af3f544cfb9914c1b2ac2a2827
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-02-22 14:28:18 -0300

    [AESS-1230] Optimize Zendesk custom_fields table (#10181)
    * feat: add qualify to ticket_fields table
    
    * refactor: optimize custom_fields query
    
    * feat: add comments to explain regexes
    
    * fix: wrong reference on query

--- dags/support_and_service/zendesk_tickets/queries/clean/ticket_fields.sql
+++ dags/support_and_service/zendesk_tickets/queries/clean/ticket_fields.sql
@@ -1,42 +1,38 @@
-WITH max_stitch_data AS (
+WITH dedup_ticket_fields AS (
     SELECT
-        id,
-        MAX(CAST(updated_at AS TIMESTAMP)) AS max_updated_at
+        *
     FROM
         datalake_zendesk_tickets_raw.ticket_fields
-    GROUP BY 1
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1
 )
 SELECT
-    tf.id AS id_ticket_fields,
-    tf.title,
-    tf.description,
-    tf.agent_description,
-    tf.url AS url_ticket_fields,
-    tf.raw_title,
-    tf.raw_title_in_portal,
-    tf.raw_description,
-    tf.custom_field_options,
-    tf.removable AS is_removable,
-    tf.position AS is_position,
-    tf.required AS is_required,
-    tf.type,
-    tf.active AS is_active,
-    tf.collapsed_for_agents AS is_collapsed_for_agents,
-    tf.visible_in_portal AS is_visible_in_portal,
-    tf.required_in_portal AS is_required_in_portal,
-    tf.editable_in_portal AS is_editable_in_portal,
-    tf.title_in_portal AS is_title_in_portal,
-    tf.dt AS dt_extracted,
-    CAST(FROM_UTC_TIMESTAMP(tf.created_at, 'Brazil/East') AS TIMESTAMP) AS ts_created_local,
-    CAST(tf.created_at AS TIMESTAMP) AS ts_created,
-    CAST(tf.updated_at AS TIMESTAMP) AS ts_updated,
+    id AS id_ticket_fields,
+    title,
+    description,
+    agent_description,
+    url AS url_ticket_fields,
+    raw_title,
+    raw_title_in_portal,
+    raw_description,
+    custom_field_options,
+    removable AS is_removable,
+    position AS is_position,
+    required AS is_required,
+    type,
+    active AS is_active,
+    collapsed_for_agents AS is_collapsed_for_agents,
+    visible_in_portal AS is_visible_in_portal,
+    required_in_portal AS is_required_in_portal,
+    editable_in_portal AS is_editable_in_portal,
+    title_in_portal AS is_title_in_portal,
+    dt AS dt_extracted,
+    CAST(FROM_UTC_TIMESTAMP(created_at, 'Brazil/East') AS TIMESTAMP) AS ts_created_local,
+    CAST(created_at AS TIMESTAMP) AS ts_created,
+    CAST(updated_at AS TIMESTAMP) AS ts_updated,
     NOW() AS ts_load,
-    YEAR(CAST(tf.updated_at AS DATE)) AS year,
-    MONTH(CAST(tf.updated_at AS DATE)) AS month,
-    DAY(CAST(tf.updated_at AS DATE)) AS day
+    YEAR(CAST(updated_at AS DATE)) AS year,
+    MONTH(CAST(updated_at AS DATE)) AS month,
+    DAY(CAST(updated_at AS DATE)) AS day
 FROM
-    datalake_zendesk_tickets_raw.ticket_fields tf
-JOIN
-    max_stitch_data max_sd
-        ON max_sd.id = tf.id
-        AND max_sd.max_updated_at = CAST(tf.updated_at AS TIMESTAMP)
+    dedup_ticket_fields
