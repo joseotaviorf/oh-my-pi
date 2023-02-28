22f4a89aff78b1e5a56f229360ef4fd6402535b5
Rafael Augusto Monteiro <rafael.augusto@quintoandar.com.br>
2023-02-28 12:08:55 -0300

    Fix execution date in documentation_metrics (#10271)

--- dags/governance/documentation_metrics/documentation_metrics.py
+++ dags/governance/documentation_metrics/documentation_metrics.py
@@ -36,7 +36,7 @@ RAW_SPARK_JOB_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/"
 cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
 default_libraries = config_service.get_config("default_libraries")
 
-EXECUTION_DATE = datetime.today().strftime("%Y-%m-%d")
+EXECUTION_DATE = "{{ds}}"
 
 dag = DAG(
     dag_id=DAG_ID,
