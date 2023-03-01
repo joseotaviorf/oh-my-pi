3ca662060dcad46587fdc4a41771207ab6f8d386
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-03-01 09:12:45 -0300

    fix: cast null to avoid parquet error (#10270)

--- dags/fintech/retsuko/queries/clean/invoice.sql
+++ dags/fintech/retsuko/queries/clean/invoice.sql
@@ -5,7 +5,7 @@ select
     contract_id as id_contract,
     status,
     sub_status as substatus,
-    NULL AS negotiation_status,
+    CAST(NULL AS STRING) AS negotiation_status,
     paid_via,
     purpose,
     closing_mode,
