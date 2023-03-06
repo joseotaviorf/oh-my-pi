a72a2396e2ef1e29eca37adb1c29be9d7f0b06bb
rodrigorochedo <96083546+rodrigorochedo@users.noreply.github.com>
2023-03-06 17:27:04 -0300

    [DFORSALE-4315] add new seller documentation tables and lineages to sales flow clean layer (#10200)

---
+++ dags/for_sale/sales_flow/queries/clean/seller_documentation.sql
@@ -0,0 +1,16 @@
+SELECT
+    id,
+    house_id AS id_house,
+    started_at AS ts_started,
+    submitted_at AS ts_submitted,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_sales_flow_raw.seller_documentation
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
---
+++ dags/for_sale/sales_flow/queries/clean/seller_documentation_aud.sql
@@ -0,0 +1,22 @@
+SELECT
+    id,
+    house_id AS id_house,
+    rev,
+    revtype AS rev_type,
+    revend AS rev_end,
+    house_id_mod AS mod_id_house,
+    started_at_mod AS mod_ts_started,
+    submitted_at_mod AS mod_ts_submitted,
+    started_at AS ts_started,
+    submitted_at AS ts_submitted,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_sales_flow_raw.seller_documentation_aud
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
