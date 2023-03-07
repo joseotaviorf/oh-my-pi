e5d418de7ba1d8a1c3b1349d4575dd06cade2390
Giuseppe Giovanelli <50770057+giuseppecg@users.noreply.github.com>
2023-03-07 10:58:36 -0300

    feat: adding fraud_type column gsheet (#10192)

--- dags/cross/gsheets_by_context/queries/clean/fintech/fraud_contracts.sql
+++ dags/cross/gsheets_by_context/queries/clean/fintech/fraud_contracts.sql
@@ -1,5 +1,6 @@
 SELECT 
     CAST(imovel AS BIGINT) AS id_house,
-    CAST(contrato AS BIGINT) AS id_contract
+    CAST(contrato AS BIGINT) AS id_contract,
+    tipo_de_fraude AS fraud_type
 FROM
     datalake_gsheets_raw.fraud_contracts
