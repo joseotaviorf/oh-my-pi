084abfd2bc364d1ef9463929e80d9d7e3b543b1d
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2023-03-13 13:23:20 -0300

    Fix dt_created schema (#10481)

--- dags/support_and_service/twilio_flex_insights/queries/clean/conversation_time_metrics.sql
+++ dags/support_and_service/twilio_flex_insights/queries/clean/conversation_time_metrics.sql
@@ -1,13 +1,13 @@
 SELECT
-    conversation AS id_conversation,
-    segment AS id_segment,
-    CAST(total_queue_time AS FLOAT) AS total_queue_time,
-    CAST(total_talk_time AS FLOAT) AS total_talk_time,
-    CAST(total_wrap_up_time AS FLOAT) AS total_wrap_up_time,
-    CAST(total_handling_time AS FLOAT) AS total_handling_time,
-    date AS dt_created,
-    year,
-    month,
-    day
+    ctm.conversation AS id_conversation,
+    ctm.segment AS id_segment,
+    CAST(ctm.total_queue_time AS FLOAT) AS total_queue_time,
+    CAST(ctm.total_talk_time AS FLOAT) AS total_talk_time,
+    CAST(ctm.total_wrap_up_time AS FLOAT) AS total_wrap_up_time,
+    CAST(ctm.total_handling_time AS FLOAT) AS total_handling_time,
+    CAST(ctm.date AS DATE) AS dt_created,
+    ctm.year,
+    ctm.month,
+    ctm.day
 FROM
-    datalake_twilio_flex_insights_raw.conversation_time_metrics
+    datalake_twilio_flex_insights_raw.conversation_time_metrics AS ctm
