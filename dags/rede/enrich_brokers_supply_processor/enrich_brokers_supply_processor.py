fdcb2a8f6c897737bad4707b23df251dc34d8796
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2023-03-16 17:48:57 -0300

    feat: upgrade cluster to databricks_10_4_med_general_cluster (#10525)

--- dags/rede/enrich_brokers_supply_processor/enrich_brokers_supply_processor.py
+++ dags/rede/enrich_brokers_supply_processor/enrich_brokers_supply_processor.py
@@ -37,7 +37,7 @@ databricks_bietlejuice_repo_path = config_service.get_config(
 base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
-cluster_description = config_service.get_config("databricks_10_4_min_general_cluster")
+cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
