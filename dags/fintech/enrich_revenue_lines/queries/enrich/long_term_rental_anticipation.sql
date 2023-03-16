06d4bcf2aeef43ed9c374fcce9a3fecd7619ca01
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-03-16 10:32:14 -0300

    [AEFR-2003] Updating long_term_rental_anticipation adding mova source (#10448)
    * updating long_term_rental_anticipation

--- dags/fintech/enrich_revenue_lines/queries/enrich/long_term_rental_anticipation.sql
+++ dags/fintech/enrich_revenue_lines/queries/enrich/long_term_rental_anticipation.sql
@@ -1,90 +1,89 @@
-WITH recurrence_signed AS (
-    SELECT
-        id_contract,
-        id,
-        ts_created,
-        row_number() OVER(PARTITION BY id_contract ORDER BY ts_created) AS nbr_transactions_signed
-    FROM 
-        datalake_fastforward_clean.long_term_anticipation 
-    WHERE 
-        ts_signed IS NOT NULL 
-    ),
-invoice_entries AS (
-    SELECT
-        ies.id_contract,
-        ies.id_invoice,
-        ri.accrual_year_month,
-        dd_due.date AS dt_due,
-        dd_paid.date AS dt_paid,
-        MAX(ies.ts_created) AS ts_created,
-        SUM(ies.brl_entry_due_amount) AS amount_brl_entry_due_amount,
-        SUM(CASE
-                WHEN i.payment_status = 'not payable' THEN ies.brl_entry_due_amount
-                ELSE ies.brl_entry_paid_amount
-            END) AS amount_brl_entry_paid_amount
-    FROM 
-        datalake_invoice.invoice_entries AS ies
-    INNER JOIN 
-        datalake_retsuko.invoice_entry AS ie
-            ON ies.id = ie.id
-    INNER JOIN 
-        datalake_retsuko.invoice AS i
-            ON ies.id_invoice = i.id_invoice
-    LEFT JOIN
-        datalake_retsuko_clean.invoice ri 
-            ON i.id_invoice = ri.id_external
-    LEFT JOIN
-        datalake_quintoandar.aux_date AS dd_due
-            ON dd_due.id_date = ies.id_due_date  
-    LEFT JOIN
-        datalake_quintoandar.aux_date AS dd_paid
-            ON dd_paid.id_date = ies.id_paid_date  
-    WHERE
-        ie.from_account_type IN ('landlord','contract')
-        AND ie.to_account_type IN ('contract','landlord')
-        AND ie.entry_type IN ('installment lra')
-    GROUP BY 1,2,3,4,5
-    )
-SELECT
+WITH mova_recurrence_signed AS (
+  SELECT DISTINCT
+    sk_contract AS id_contract,
+    sk_base_proposal AS id_base_proposal,
+    DATE(ts_created_at) AS dt_created,
+    DENSE_RANK() OVER(PARTITION BY sk_contract ORDER BY ts_created_at) AS nbr_transactions_signed
+  FROM 
+    datalake_gsheets_clean.mova_lra_owners
+  WHERE 
+    dt_signed_contract IS NOT NULL
+),
+fastforward_recurrence_signed AS (
+  SELECT
+    id_contract,
+    id,
+    ts_created,
+    ROW_NUMBER() OVER(PARTITION BY id_contract ORDER BY ts_created) AS nbr_transactions_signed
+  FROM
+    datalake_fastforward_clean.long_term_anticipation
+  WHERE
+    ts_signed IS NOT NULL
+),
+fastforward AS (
+  SELECT
     lra.id AS id_lra,
     c.id_external AS id_contract_ebdb,
-    lio.months_anticipated,
-    li.installment_number as installment,
-    lio.installments AS total_installments,
     lra.total_rent,
-    rs.nbr_transactions_signed,
-    COALESCE((li.amount + li.interest_value),lra.installment_value) AS prod_theorical_amount, 
-    li.interest_value AS prod_theorical_fee,
-    ie.amount_brl_entry_due_amount AS invoice_theorical_amount,
-    ie.amount_brl_entry_paid_amount AS invoice_paid_amount,
-    DATE_FORMAT(ADD_MONTHS(DATE_TRUNC('month', li.ts_expected_due), -1), 'yyyyMM') AS accrual_year_month,
-    CASE
-        WHEN ie.amount_brl_entry_due_amount IS NOT NULL 
-            THEN  li.interest_value 
-    END AS invoice_theorical_fee,
-    CASE
-        WHEN ie.amount_brl_entry_paid_amount IS NOT NULL 
-            THEN li.interest_value 
-    END AS invoice_paid_fee,    
-    ie.dt_due,
-    ie.dt_paid,
-    DATE(lra.ts_created) AS dt_created,
-    DATE(lra.ts_signed) AS dt_signed
-FROM 
+    lio.installments AS total_installments,
+    li.installment_number as installment,
+    frs.nbr_transactions_signed,
+    lio.months_anticipated,
+    COALESCE((li.amount + li.interest_value),lra.installment_value) AS prod_theorical_amount,
+    li.interest_value AS prod_theorical_fee
+  FROM
     datalake_fastforward_clean.long_term_anticipation AS lra
-INNER JOIN 
-    datalake_fastforward_clean.contract AS c
+  INNER JOIN
+      datalake_fastforward_clean.contract AS c
         ON lra.id_contract = c.id
-INNER JOIN
-    datalake_fastforward_clean.lra_installment_option AS lio
+  INNER JOIN
+      datalake_fastforward_clean.lra_installment_option AS lio
         ON lra.id_installment_option = lio.id
-INNER JOIN
-    datalake_fastforward_clean.lra_installment AS li
+  INNER JOIN
+      datalake_fastforward_clean.lra_installment AS li
         ON lra.id = li.id_long_term_anticipation
-INNER JOIN 
-    recurrence_signed AS rs
-        ON lra.id = rs.id
+  INNER JOIN
+      fastforward_recurrence_signed AS frs
+        ON lra.id = frs.id
+)
+
+SELECT
+  lra.id_lra,
+  mova.sk_contract AS id_contract_ebdb,
+  mova.installment_status AS status,
+  lra.months_anticipated,
+  mova.installment_number AS installment,
+  mova.installment_total_count AS total_installments,
+  mova.main_value AS total_rent,
+  mrs.nbr_transactions_signed,
+  lra.prod_theorical_amount,
+  lra.prod_theorical_fee,
+  mova.installment_value AS mova_invoice_theorical_amount,
+  mova.paid_value AS mova_invoice_paid_amount,
+  mova.contract_installment_interest AS mova_invoice_theorical_fee,
+  IF(installment_status = 'PAGA', mova.contract_installment_interest, NULL) AS mova_invoice_paid_fee,
+  mova.interest_value AS mova_interest_value,
+  DATE_FORMAT(ADD_MONTHS(DATE_TRUNC('month', mova.dt_expiration), -1), 'yyyyMM') AS accrual_year_month,
+  mova.dt_expiration AS dt_due,
+  mova.dt_payment AS dt_paid,
+  DATE(mova.ts_created_at) AS dt_created,
+  mova.dt_signed_contract AS dt_signed
+FROM 
+  datalake_gsheets_clean.mova_lra_owners AS mova
 LEFT JOIN
-    invoice_entries ie
-        ON ie.id_contract = c.id_external
-        AND ie.accrual_year_month = DATE_FORMAT(ADD_MONTHS(DATE_TRUNC('month', li.ts_expected_due), -1), 'yyyyMM')
+  mova_recurrence_signed AS mrs
+    ON mova.sk_contract = mrs.id_contract
+    AND mova.sk_base_proposal = mrs.id_base_proposal
+LEFT JOIN 
+  fastforward AS lra
+    ON mova.sk_contract = lra.id_contract_ebdb
+    AND mova.main_value = lra.total_rent
+    AND mova.installment_total_count = lra.total_installments
+    AND mova.installment_number = lra.installment
+    AND mrs.nbr_transactions_signed = lra.nbr_transactions_signed
+LEFT JOIN
+  datalake_fastforward_clean.gateway_loan_installment bfi 
+    ON mova.sk_installment = CAST(bfi.id_partner_reference AS BIGINT)
+WHERE
+  bfi.id IS NULL --excluding bfi data
+  AND mova.installment_status <> 'RENEGOCIADA'
