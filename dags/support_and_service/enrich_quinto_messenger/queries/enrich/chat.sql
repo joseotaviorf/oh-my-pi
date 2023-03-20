7d2511206500f5205e3413bbae401270fe53ec67
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-03-20 14:51:30 -0300

    [AESS-1392] Add Chat5A table to enrich_quinto_messenger (#10447)
    * refactor: identation and column aliases
    
    * feat: add new table to enrich_quinto_messenger
    
    * feat: lineage
    
    * fix: use correct source table and fix lineage
    
    * fix: lineage
    
    * fix: lineage description
    
    * refactor: sessions by last sauron update
    
    * refactor: change source query, add chat_id to task table
    
    * refactor: remove sauron and twilio ctes
    
    * refactor: update lineage and sql
    
    * refactor: fields with empty description on lineage
    
    * fix: remove unused joins
    
    * fix: typo
    
    * feat: inner dependencies on dag
    
    * feat: add description on lineage file
    
    * refactor: remove relation with task table and inner dependency
    
    * refactor: remove inner_dependency param

---
+++ dags/support_and_service/enrich_quinto_messenger/queries/enrich/chat.sql
@@ -0,0 +1,19 @@
+WITH last_updated_chat AS (
+    SELECT
+        *
+    FROM
+        datalake_quinto_messenger_clean.chat
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY ts_updated DESC) = 1
+)
+
+SELECT
+    id AS id_chat,
+    GET_JSON_OBJECT(attributes,'$.channel_sid') AS id_channel,
+    source_id AS id_session,
+    source,
+    status AS session_status,
+    ts_created,
+    ts_updated
+FROM
+    last_updated_chat
