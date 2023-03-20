3604fc047c49b9749855936bd35dae2006d83736
Leticia Silveira <leticia.ingrid.silveira@gmail.com>
2023-03-20 15:16:01 +0100

    [AEFF] Adding new fact to DW retsuko (#10537)
    * adding query and metadata files to new fact of bill items
    
    * updated dependencies
    
    * minor fixes
    
    * minor fixes
    
    * updated metadata

---
+++ dags/fintech/dw_retsuko/queries/dw/fact_bill_item_cluster_at_invoice_mobs.sql
@@ -0,0 +1,46 @@
+WITH bill_items_cohort_rules AS (
+    SELECT
+        id_invoice AS sk_invoice,
+        id_contract AS sk_contract,
+        bi.bill_item_cluster_name,
+        dt_created,
+        dt_due,
+        dt_paid,
+        dt_canceled,
+        sum(value_sign_bill_item) as value_bill_item_cluster
+    FROM datalake_retsuko.bill_items AS bi
+    WHERE bi.payment_status IN ('open','paid','canceled')
+        AND bi.due_amount <= 0
+        AND bi.bill_item_cluster_name IS NOT NULL
+    GROUP BY 1,2,3,4,5,6,7
+)
+SELECT
+    sk_invoice,
+    sk_contract,
+    id_lvl_1 AS sk_junk_bill_items_cluster,
+    value_bill_item_cluster,
+    GREATEST(12*(YEAR(current_date)-YEAR(dt_created))+(MONTH(current_date)-MONTH(dt_created)),0) AS mobs_possible_invoice_by_created_date,
+    GREATEST(12*(YEAR(current_date)-YEAR(dt_due))+(MONTH(current_date)-MONTH(dt_due)),0) AS mobs_possible_invoice_by_due_date,
+    IF(dt_paid IS NOT NULL,
+      GREATEST(12*(YEAR(dt_paid)-YEAR(dt_created))+(MONTH(dt_paid)-MONTH(dt_created)),0),
+      NULL
+    ) AS mob_paid_by_created_date,
+    IF(dt_canceled IS NOT NULL,
+      GREATEST(12*(YEAR(dt_canceled)-YEAR(dt_created))+(MONTH(dt_canceled)-MONTH(dt_created)),0),
+      NULL
+    ) AS mob_canceled_by_created_date,
+    IF(dt_paid IS NOT NULL,
+      GREATEST(12*(YEAR(dt_paid)-YEAR(dt_due))+(MONTH(dt_paid)-MONTH(dt_due)),0),
+      NULL
+    ) AS mob_paid_by_due_date,
+    IF(dt_canceled IS NOT NULL,
+      GREATEST(12*(YEAR(dt_canceled)-YEAR(dt_due))+(MONTH(dt_canceled)-MONTH(dt_due)),0),
+      NULL
+    ) AS mob_canceled_by_due_date,
+    GREATEST(12*(YEAR(dt_due)-YEAR(dt_created))+(MONTH(dt_due)-MONTH(dt_created)),0) AS  mob_due_by_created_date,
+    GREATEST(12*(YEAR(dt_due)-YEAR(dt_due))+(MONTH(dt_due)-MONTH(dt_due)),0) AS  mob_due_by_due_date
+FROM
+    bill_items_cohort_rules AS cr
+LEFT JOIN enrich_retsuko_junk AS jk
+    ON jk.desc_master_type = 'bill_item_cluster_name'
+    AND jk.desc_lvl_1 = cr.bill_item_cluster_name
