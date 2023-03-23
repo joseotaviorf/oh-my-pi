dbe53452d7b1358f0641de78c50c14e41103c3eb
fgchies <118193092+fgchies@users.noreply.github.com>
2023-03-23 15:13:38 -0300

    [AEFR-1841-2] New enrich for rent demand events (#10544)
    * Creating enrich DAG for rent demand event
    
    * removing redshift load
    
    * Updating documentation of rent_demand_event
    
    * Updated dependencies file
    
    * Updating title of md file
    
    * Updating enrich rde to remove dim sources
    
    * Updating rde enrich query
    
    * Updating rde enrich query and doc
    
    * Update dags/for_rent/enrich_rent_demand_event/enrich_rent_demand_event.md
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    ---------
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ dags/for_rent/enrich_rent_demand_event/enrich_rent_demand_event.md
@@ -0,0 +1,17 @@
+## Enrich Rent Demand Event
+
+### Purpose
+
+Creates the enrich table rent_demand_event, containing the ForRent demand events, each line is an event related to the scope. This is an incremental table.
+
+### Execution Interval
+
+Daily.
+
+### Outputs
+
+Produces the following output table:
+ 
+- `datalake_rent_demand_event.rent_demand_event`
+
+
---
+++ dags/for_rent/enrich_rent_demand_event/queries/enrich/rent_demand_event.sql
@@ -0,0 +1,311 @@
+WITH rent_flow_house_listing AS (
+  SELECT 
+    rf.id_rent_flow,
+    rf.id_user_agent,
+    rf.id_contract,
+    COALESCE(
+      CAST(
+        rf.id_house ||
+        LPAD(
+        COALESCE(
+          CAST(COALESCE(hl_contract.version, hl.version) AS VARCHAR(3)),'1'),3,'0') AS BIGINT), CAST(-1 AS BIGINT)
+    ) AS id_house_listing,
+    h.id_region,
+    h.id_user,
+    rf.id_booking,   
+    rf.id_offer,
+    rf.id_proposal,
+    COALESCE(h.country_code, 'Undefined') AS country_code
+  FROM datalake_ebdb_rent_flow.rent_flow rf
+  JOIN datalake_ebdb_listing.house_listing hl
+    ON hl.id_house = rf.id_house
+    AND COALESCE(rf.dt_rent_flow_created, '1900-01-01') BETWEEN COALESCE(hl.ts_listing_version_start, '1900-01-01')
+      AND COALESCE(hl.ts_listing_version_end, NOW())
+  LEFT JOIN datalake_ebdb_listing.house h
+    ON hl.id_house = h.id
+  LEFT JOIN datalake_ebdb_clean.contract AS con
+    ON con.id_house = rf.id_house
+    AND con.id = rf.id_contract
+  LEFT JOIN datalake_ebdb_listing.house_listing AS hl_contract
+    ON con.id_house = hl_contract.id_house 
+    AND con.ts_created BETWEEN COALESCE(hl_contract.ts_listing_version_start, '2000-01-01 00:00:00') 
+      AND COALESCE(hl_contract.ts_listing_version_end, CURRENT_DATE)
+  LEFT JOIN datalake_booking.booking bk
+    ON bk.id = rf.id_booking  
+  LEFT JOIN datalake_ebdb_listing.listing_business_context lbc
+    ON lbc.id_house = h.id
+  WHERE 
+    lbc.business_context = 'RENT'
+    AND (
+      COALESCE(bk.visit_intent, '') <> 'SALE'
+      OR (
+        bk.visit_intent = 'SALE'
+        AND rf.id_contract IS NOT NULL
+      )
+    )
+),
+
+rent_demand_events AS (                                                                  
+  SELECT --visits_booked
+    bk.id AS id_event,                                                                              
+    1 AS id_event_type,                                                                                  
+    bk.id_visitor AS id_client,                                                                      
+    bk.id_house,                                                                        
+    bk.id_agent AS id_user_agent,                                                                        
+    bk.id_rent_flow,                                                                    
+    bk.ts_created AS ts_event,                                                                      
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(ts_created) AS year,
+    MONTH(ts_created) AS month,
+    DAY(ts_created) AS day
+  FROM
+    datalake_booking.booking bk
+  JOIN rent_flow_house_listing rf
+    ON rf.id_booking = bk.id
+  WHERE 
+    bk.type = 'Visita'
+    AND bk.ts_created is not null
+    AND YEAR(ts_created) = {year} 
+    AND MONTH(ts_created) = {month} 
+    AND DAY(ts_created) = {day}
+  UNION ALL
+  SELECT --visits_completed 
+    bk.id AS id_event,                                                                              
+    2 AS id_event_type,                                                                                   
+    bk.id_visitor AS id_client,                                                                       
+    bk.id_house,                                                                        
+    bk.id_agent AS id_user_agent,                                                                        
+    bk.id_rent_flow,                                                                    
+    bk.dt_booking AS ts_event,                                                                  
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(dt_booking) AS year,
+    MONTH(dt_booking) AS month,
+    DAY(dt_booking) AS day
+  FROM
+    datalake_booking.booking bk
+  JOIN rent_flow_house_listing rf
+    ON rf.id_booking = bk.id
+  WHERE 
+    bk.is_visit_completed = true 
+    AND bk.type = 'Visita'
+    AND bk.visit_fup IN ('VaiNegociar',
+          'NaoGostou',
+          'VisitouSozinho',
+          'Talvez')
+    AND bk.dt_booking is not null
+    AND YEAR(dt_booking) = {year} 
+    AND MONTH(dt_booking) = {month} 
+    AND DAY(dt_booking) = {day}
+  UNION ALL
+  SELECT --offer_submitted 
+    off.id AS id_event,                                                                            
+    3 AS id_event_type,                                                                                   
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    off.ts_first_sent AS ts_event,                                                                 
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code, 
+    YEAR(ts_first_sent) AS year,
+    MONTH(ts_first_sent) AS month,
+    DAY(ts_first_sent) AS day
+  FROM
+    datalake_offer.offer off
+  JOIN rent_flow_house_listing rf
+    ON rf.id_offer= off.id
+  WHERE 
+    off.is_offer_submitted = true
+    AND YEAR(ts_first_sent) = {year} 
+    AND MONTH(ts_first_sent) = {month} 
+    AND DAY(ts_first_sent) = {day}
+  UNION ALL
+  SELECT --offer_accepted
+    off.id AS id_event,                                                                             
+    4 AS id_event_type,                                                                                 
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    off.ts_analyzed AS ts_event,                                                                    
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(ts_analyzed) AS year,
+    MONTH(ts_analyzed) AS month,
+    DAY(ts_analyzed) AS day
+  FROM
+    datalake_offer.offer off
+  JOIN rent_flow_house_listing rf
+    ON rf.id_offer = off.id
+  WHERE 
+    off.status = 'Aprovada'
+    AND YEAR(ts_analyzed) = {year} 
+    AND MONTH(ts_analyzed) = {month}
+    AND DAY(ts_analyzed) = {day}
+  UNION ALL
+  SELECT --evaluation_started
+    pp.id AS id_event,                                                                            
+    5 AS id_event_type,                                                                                 
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    pp.ts_credit_evaluation_first_init AS ts_event,                                                
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(ts_credit_evaluation_first_init) AS year,
+    MONTH(ts_credit_evaluation_first_init) AS month,
+    DAY(ts_credit_evaluation_first_init) AS day
+  FROM
+    datalake_proposal.proposal pp
+  JOIN datalake_offer.offer off
+    ON off.id = pp.id_offer 
+  JOIN rent_flow_house_listing rf
+    ON rf.id_proposal = pp.id
+  WHERE 
+    pp.ts_credit_evaluation_first_init is not null
+    AND YEAR(ts_credit_evaluation_first_init) = {year} 
+    AND MONTH(ts_credit_evaluation_first_init) = {month}
+    AND DAY(ts_credit_evaluation_first_init) = {day}
+  UNION ALL
+  SELECT --evaluation_positive
+    pp.id AS id_event,                                                                             
+    6 AS id_event_type,                                                                                 
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    pp.ts_first_credit_evaluation_positive AS ts_event,                                              
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,     
+    YEAR(ts_first_credit_evaluation_positive) AS year,
+    MONTH(ts_first_credit_evaluation_positive) AS month,
+    DAY(ts_first_credit_evaluation_positive) AS day
+  FROM
+    datalake_proposal.proposal pp
+   JOIN datalake_offer.offer off
+    ON off.id = pp.id_offer 
+  JOIN rent_flow_house_listing rf
+    ON rf.id_proposal = pp.id
+  WHERE 
+    pp.ts_first_credit_evaluation_positive is not null
+    AND YEAR(ts_first_credit_evaluation_positive) = {year} 
+    AND MONTH(ts_first_credit_evaluation_positive) = {month}
+    AND DAY(ts_first_credit_evaluation_positive) = {day}
+  UNION ALL
+  SELECT --document_sent
+    pp.id AS id_event,                                                                            
+    7 AS id_event_type,                                                                                 
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent) AS ts_event,             
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) AS year,
+    MONTH(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) AS month,
+    DAY(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) AS day
+  FROM
+    datalake_proposal.proposal pp
+  JOIN datalake_offer.offer off
+    ON off.id = pp.id_offer 
+  JOIN rent_flow_house_listing rf
+    ON rf.id_proposal = pp.id
+  WHERE 
+    pp.has_tenant_sent_documentation is not null
+    AND YEAR(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) = {year} 
+    AND MONTH(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) = {month}
+    AND DAY(COALESCE(pp.ts_tenant_auto_first_doc_sent, pp.ts_tenant_first_doc_sent)) = {day}
+  UNION ALL
+  SELECT --credit_approved 
+    pp.id AS id_event,                                                                            
+    8 AS id_event_type,                                                                                   
+    off.id_client,                                                                      
+    off.id_house,                                                                       
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    pp.ts_credit_approved_last AS ts_event,                                                         
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(ts_credit_approved_last) AS year,
+    MONTH(ts_credit_approved_last) AS month,
+    DAY(ts_credit_approved_last) AS day
+  FROM
+    datalake_proposal.proposal pp
+  JOIN datalake_offer.offer off
+    ON off.id = pp.id_offer 
+  JOIN rent_flow_house_listing rf
+    ON rf.id_proposal = pp.id
+  WHERE 
+    pp.ts_credit_approved_last is not null
+    AND YEAR(ts_credit_approved_last) = {year} 
+    AND MONTH(ts_credit_approved_last) = {month}
+    AND DAY(ts_credit_approved_last) = {day}
+  UNION ALL
+  SELECT --contract_signed
+    ct.id AS id_event,                                                                             
+    9 AS id_event_type,                                                                                  
+    off.id_client,                                                                      
+    ct.id_house,                                                                        
+    rf.id_user_agent,                                                                   
+    off.id_rent_flow,                                                                   
+    ct.ts_signed AS ts_event,                                                                     
+    rf.id_house_listing,                                                                
+    rf.id_region,                                                                       
+    rf.id_user,                                                                         
+    rf.country_code,
+    YEAR(ts_signed) AS year,
+    MONTH(ts_signed) AS month,
+    DAY(ts_signed) AS day
+  FROM
+    datalake_ebdb_contract.contract ct
+  JOIN datalake_proposal.proposal pp
+    ON ct.id_proposal = pp.id
+  JOIN datalake_offer.offer off
+    ON off.id = pp.id_offer 
+  JOIN rent_flow_house_listing rf
+    ON rf.id_contract = ct.id
+  WHERE 
+    ct.ts_signed is not null
+    AND YEAR(ts_signed) = {year} 
+    AND MONTH(ts_signed) = {month}
+    AND DAY(ts_signed) = {day}
+)
+
+SELECT
+  id_event,
+  id_event_type,
+  id_client AS id_tenant_prospect,                                              
+  id_house,                                                       
+  id_user_agent AS id_agent,                                                         
+  id_rent_flow,                                                   
+  id_house_listing,                                                
+  id_region,                                                       
+  id_user AS id_owner,                                                          
+  ts_event,                         
+  country_code,
+  year,
+  month,
+  day,
+  NOW() AS ts_updated
+FROM
+  rent_demand_events     
