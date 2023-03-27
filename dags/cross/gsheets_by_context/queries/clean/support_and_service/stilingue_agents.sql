2367ef4dd05bcc8396d902259af906db63eaa920
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-03-27 10:43:50 -0300

    [AES&S] Remove Stilingue DAGs (#10622)
    * remove stilingue context
    
    * Update fact_agent_daily_productivity.yml
    
    * debug steps eith empty commit

--- dags/cross/gsheets_by_context/queries/clean/support_and_service/stilingue_agents.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    CAST(id_external_agent AS BIGINT) AS id_external_agent,
-    external_agent_name,
-    agent_email,
-    channel,
-    CAST(is_moderator AS BOOLEAN) AS is_moderator
-FROM
-    datalake_gsheets_raw.stilingue_agents
