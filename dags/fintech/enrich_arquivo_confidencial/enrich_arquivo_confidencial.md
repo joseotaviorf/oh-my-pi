44a5c222d3d192328408605fcf05e4f616ceab19
Leticia Silveira <leticia.ingrid.silveira@gmail.com>
2023-03-28 19:01:45 +0200

    [AEFF] Enrich Arquivo Confidencial (#10648)
    * add DAG to enrich arquivo confidencial
    
    * updated dependencies file
    
    * fix lineage
    
    * add config files
    
    * test
    
    * feat: add scr documentation
    
    * fix: dag start date set to jan/23
    
    * Update dags/fintech/enrich_arquivo_confidencial/metadata/enrich/scr.yml
    
    ---------
    
    Co-authored-by: giuseppe <giovanelli.g@gmail.com>

---
+++ dags/fintech/enrich_arquivo_confidencial/enrich_arquivo_confidencial.md
@@ -0,0 +1,15 @@
+## Enrich Arquivo Confidencial
+
+### Purpose
+
+Creates enriched tables for Arquivo Confidencial data.
+
+### Execution Interval
+
+Daily. More information about run time [here]({chart_url}{dag_id}).
+
+### Outputs
+
+Produces the following output tables, via full load:
+
+- `scr`
---
+++ dags/fintech/enrich_arquivo_confidencial/queries/enrich/scr.sql
@@ -0,0 +1,66 @@
+WITH scr_data AS (
+    SELECT
+    id,
+    cpf,
+    version,
+    EXPLODE(FROM_JSON(raw_data:analysis_output:scr.scr_data,
+        'array<
+        struct<
+            source:string,
+            indirect_risk:int,
+            reference_date:string,
+            operation_count:int,
+            operation_items:array<struct<value:int,
+            domain:string,
+            modality:string,
+            submodality:string,
+            domain_group:string,
+            modality_description:string,
+            submodality_description:string,
+            linked_to_foreign_currency:string>>,
+            assumed_coobligation:string,
+            receive_coobligation:string,
+            start_relationship_date:string,
+            subjudice_operations_count:string,
+            subjudice_operations_value:string,
+            financial_institution_count:string ,
+            disagreement_operations_count:string,
+            disagreement_operations_value:string
+        >
+        >')
+    ) scr_data,
+    ts_created,
+    ts_updated
+  FROM datalake_arquivo_confidencial_clean.integration_report
+  WHERE integration_provider = 'QI_TECH_SCR'
+    AND ts_created >= DATE('2022-09-01')
+),
+mobs AS (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (PARTITION BY cpf, VERSION ORDER BY scr_data.reference_date DESC) mob
+  FROM scr_data
+),
+operation_items_exploded AS (
+  SELECT
+    id,
+    cpf,
+    version,
+    mob,
+    scr_data.reference_date,
+    EXPLODE(scr_data.operation_items) AS dat,
+    ts_created,
+    ts_updated
+FROM mobs
+)
+
+SELECT
+    id,
+    cpf,
+    version,
+    mob,
+    reference_date,
+    dat.*,
+    ts_created,
+    ts_updated
+FROM operation_items_exploded
