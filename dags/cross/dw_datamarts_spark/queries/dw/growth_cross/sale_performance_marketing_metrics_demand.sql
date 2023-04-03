99aa6d63fa076604312997f0c11bf70c978b49ca
matheusmello-o <31773902+matheusmello-o@users.noreply.github.com>
2023-04-03 10:47:13 -0300

    Adding new rule to attribute Sale campaigns by a combination of utms (#10741)
    Co-authored-by: Matheus Mello <matheuscunha@BRSPPM-C02GKGR9MD6M.local>

--- dags/cross/dw_datamarts_spark/queries/dw/growth_cross/sale_performance_marketing_metrics_demand.sql
+++ dags/cross/dw_datamarts_spark/queries/dw/growth_cross/sale_performance_marketing_metrics_demand.sql
@@ -218,6 +218,9 @@ sale_flows_funnel_events AS (
                     OR LOWER(sf.utm_campaign) LIKE '%girafa%'
                     OR LOWER(sf.utm_campaign) LIKE '%vender%'
                     OR LOWER(sf.utm_campaign) = 'whatsapp_s'
+                    OR LOWER(CONCAT(sf.utm_source, sf.utm_campaign, sf.utm_medium, sf.utm_term)) IN 
+                            ('subscribedpush.whatsapp.produto.frfs.homes.canaiswhatsapponly.whatsapp.sale',
+                            'subscribedpush.whatsapp.produto.frfs.homeswhatsappsale')
                 THEN 'Sale'
             WHEN sf.utm_campaign IS NULL
                     OR sf.utm_campaign = ''
