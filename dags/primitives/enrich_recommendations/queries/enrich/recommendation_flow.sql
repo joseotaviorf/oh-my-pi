a05ced5cf95ba188a5e4685245f7a02f43cf90e9
JoaoFidalgoQA <119935733+JoaoFidalgoQA@users.noreply.github.com>
2023-04-05 14:04:06 +0100

    [PRS-127] Recommendation metrics calculations (#10637)
    * added metric calculations
    
    * replaced _k_ by _at_
    
    * fixe small changes
    
    * minor changes nomenclature
    
    * fixed typo
    
    * fixed format

--- dags/primitives/enrich_recommendations/queries/enrich/recommendation_flow.sql
+++ dags/primitives/enrich_recommendations/queries/enrich/recommendation_flow.sql
@@ -22,6 +22,7 @@ WITH recommendation_to_rent_flow AS (
             ON
                 recommendation.id_user = item_interaction.id_user
                 AND recommendation.id_item = item_interaction.id_item
+                AND recommendation.business_context = item_interaction.business_context
                 AND recommendation.ts_rec_received <= item_interaction.ts_interaction
         WHERE interaction_type = 'rent-flow-created'
     )
@@ -53,6 +54,7 @@ recommendation_to_sale_flow AS (
             ON
                 recommendation.id_user = item_interaction.id_user
                 AND recommendation.id_item = item_interaction.id_item
+                AND recommendation.business_context = item_interaction.business_context
                 AND recommendation.ts_rec_received
                 <= item_interaction.ts_interaction
         WHERE interaction_type = 'sale-flow-created'
@@ -142,16 +144,54 @@ recommendation_to_click AS (
     WHERE
         first_click_after_rec IS TRUE
         AND hours_between_interaction_and_rec <= 24
+),
+
+recommendation_to_relevant_interaction AS (
+    SELECT
+        id_recset,
+        COUNT(DISTINCT id_item) AS count_relevant_items
+    FROM (
+        SELECT DISTINCT
+            id_recset,
+            item_interaction.id_item,
+            (
+                CAST(item_interaction.ts_interaction as long)
+                - CAST(recommendation.ts_rec_received as long)
+            )
+            / 3600 AS hours_between_interaction_and_rec,
+            DENSE_RANK() OVER (
+                PARTITION BY
+                    recommendation.id_recset,
+                    item_interaction.id_item
+                ORDER BY item_interaction.ts_interaction ASC
+            )
+            = 1 AS first_relevant_interaction_after_rec
+        FROM
+            datalake_recommendations.recommendation AS recommendation
+        LEFT JOIN
+            datalake_recommendations.item_interaction AS item_interaction
+            ON  recommendation.id_user = item_interaction.id_user
+                AND recommendation.business_context = item_interaction.business_context
+                AND recommendation.ts_rec_received < item_interaction.ts_interaction
+        WHERE
+            interaction_type in ('similar-house-clicked', 'listing-page-viewed', 'house-favorited')
+    )
+    WHERE
+        first_relevant_interaction_after_rec IS TRUE
+        AND hours_between_interaction_and_rec <= 24
+    GROUP BY 1
 )
 
 SELECT
     recommendation.*,
     COALESCE(true_positive, FALSE) AS true_positive,
+    SUM(INT(COALESCE(true_positive, FALSE))) OVER(PARTITION BY recommendation.id_recset ORDER BY item_rank) AS cum_true_positive,
     COALESCE(rec_to_favorite, FALSE) AS rec_to_favorite,
     COALESCE(recommendation_to_sale_flow.rec_to_sale_flow_one_day, FALSE) AS rec_to_sale_flow_one_day,
     COALESCE(recommendation_to_sale_flow.rec_to_sale_flow_fourteen_days, FALSE) AS rec_to_sale_flow_fourteen_days,
     COALESCE(recommendation_to_rent_flow.rec_to_rent_flow_one_day, FALSE) AS rec_to_rent_flow_one_day,
-    COALESCE(recommendation_to_rent_flow.rec_to_rent_flow_seven_days, FALSE) AS rec_to_rent_flow_seven_days
+    COALESCE(recommendation_to_rent_flow.rec_to_rent_flow_seven_days, FALSE) AS rec_to_rent_flow_seven_days,
+    COALESCE(recommendation_to_relevant_interaction.count_relevant_items, 0) AS count_relevant_items
 FROM datalake_recommendations.recommendation AS recommendation
 LEFT JOIN
     recommendation_to_click
@@ -165,3 +205,6 @@ LEFT JOIN
 LEFT JOIN
     recommendation_to_rent_flow
     ON recommendation.id_rec = recommendation_to_rent_flow.id_rec
+LEFT JOIN
+    recommendation_to_relevant_interaction
+    ON recommendation.id_recset = recommendation_to_relevant_interaction.id_recset
