2dc933da5521ce4f5cdcc0526ebf25e044cb5843
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-04-13 18:10:22 -0300

    Adjusting brokerage_fee of revenue_lines (#10843)

--- dags/fintech/enrich_revenue_lines/queries/enrich/brokerage_fee.sql
+++ dags/fintech/enrich_revenue_lines/queries/enrich/brokerage_fee.sql
@@ -8,9 +8,10 @@ WITH brokerage_fee AS (
             c.first_rental_commission,
             ct.agent_brokerage_share AS agent_brokerage_share,
             p.brokerage_split_percentage,
-            ROUND(c.rent * c.first_rental_commission * (1.00 - COALESCE(ct.agent_brokerage_share,0.00) - COALESCE(p.brokerage_split_percentage,0.00)),2) AS prod_theorical_amount,
-            ROUND(sum(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
-            ROUND(sum(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
+            pp.brokerage_split_percentage AS select_agent_brokerage_split_percentage,
+            ROUND(c.rent * c.first_rental_commission * (1.00 - COALESCE(ct.agent_brokerage_share,0.00) - COALESCE(p.brokerage_split_percentage,0.00) - COALESCE(pp.brokerage_split_percentage,0.00)),2) AS prod_theorical_amount,
+            ROUND(SUM(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
+            ROUND(SUM(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
             i.accrual_year_month AS accrual_year_month,
             DATE(di.ts_due) AS dt_due,
             DATE(di.ts_paid) AS dt_paid,
@@ -25,17 +26,19 @@ WITH brokerage_fee AS (
         LEFT JOIN dw_public.dim_contract c
             ON c.sk_contract = fie.id_contract
         LEFT JOIN datalake_ebdb_clean.contract_partnership_data p
-            ON c.sk_contract = p.id_contract
+            ON c.sk_contract = p.id_contract 
+            AND p.partner_type = 'AUTONOMOUS_AGENT'
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data pp
+            ON c.sk_contract = pp.id_contract 
+            AND pp.partner_type = 'EXECUTIVE_FOR_RENT'
         LEFT JOIN datalake_ebdb_clean.contract ct
             ON c.sk_contract = ct.id
-        WHERE
+        WHERE 
             die.from_account_type NOT IN ('quinto andar', 'contract expenses')
-        AND
-            di.payment_status <> 'canceled'
-        AND
-            die.entry_type IN ('brokerage quinto andar')
+            AND di.payment_status <> 'canceled'
+            AND die.entry_type IN ('brokerage quinto andar')
         GROUP BY
-        1,2,3,4,5,6,7,8,11,12,13,14
+            1,2,3,4,5,6,7,8,9,12,13,14,15
     ),
     rental_agents_commission AS (
         SELECT
@@ -46,9 +49,10 @@ WITH brokerage_fee AS (
             c.first_rental_commission,
             ct.agent_brokerage_share AS agent_brokerage_share,
             p.brokerage_split_percentage,
+            pp.brokerage_split_percentage AS select_agent_brokerage_split_percentage,
             ROUND(c.rent * c.first_rental_commission * COALESCE(ct.agent_brokerage_share,0.00),2) AS prod_theorical_amount,
-            ROUND(sum(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
-            ROUND(sum(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
+            ROUND(SUM(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
+            ROUND(SUM(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
             i.accrual_year_month AS accrual_year_month,
             DATE(di.ts_due) AS dt_due,
             DATE(di.ts_paid) AS dt_paid,
@@ -64,18 +68,19 @@ WITH brokerage_fee AS (
             ON c.sk_contract = fie.id_contract
         LEFT JOIN datalake_ebdb_clean.contract_partnership_data p
             ON c.sk_contract = p.id_contract
+            AND p.partner_type = 'AUTONOMOUS_AGENT'
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data pp
+            ON c.sk_contract = pp.id_contract
+            AND pp.partner_type = 'EXECUTIVE_FOR_RENT'
         LEFT JOIN datalake_ebdb_clean.contract ct
             ON c.sk_contract = ct.id
         WHERE
             die.from_account_type NOT IN ('quinto andar', 'contract expenses')
-        AND
-            di.payment_status <> 'canceled'
-        AND
-            die.entry_type IN ('brokerage estate agent')
-        AND
-            di.invoice_user = 'landlord'
+            AND di.payment_status <> 'canceled'
+            AND die.entry_type IN ('brokerage estate agent')
+            AND di.invoice_user = 'landlord'
         GROUP BY
-        1,2,3,4,5,6,7,8,11,12,13,14
+            1,2,3,4,5,6,7,8,9,12,13,14,15
     ),
     partner_revenue_share_brokerage AS (
         SELECT
@@ -86,9 +91,10 @@ WITH brokerage_fee AS (
             c.first_rental_commission,
             ct.agent_brokerage_share AS agent_brokerage_share,
             p.brokerage_split_percentage,
+            pp.brokerage_split_percentage AS select_agent_brokerage_split_percentage,
             ROUND(c.rent * c.first_rental_commission * COALESCE(p.brokerage_split_percentage,0.00),2) AS prod_theorical_amount,
-            ROUND(sum(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
-            ROUND(sum(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
+            ROUND(SUM(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
+            ROUND(SUM(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
             i.accrual_year_month AS accrual_year_month,
             DATE(di.ts_due) AS dt_due,
             DATE(di.ts_paid) AS dt_paid,
@@ -104,18 +110,19 @@ WITH brokerage_fee AS (
             ON c.sk_contract = fie.id_contract
         LEFT JOIN datalake_ebdb_clean.contract_partnership_data p
             ON c.sk_contract = p.id_contract
+            AND p.partner_type = 'AUTONOMOUS_AGENT'
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data pp
+            ON c.sk_contract = pp.id_contract
+            AND pp.partner_type = 'EXECUTIVE_FOR_RENT'
         LEFT JOIN datalake_ebdb_clean.contract ct
             ON c.sk_contract = ct.id
         WHERE
             die.from_account_type NOT IN ('quinto andar', 'contract expenses')
-        AND
-            di.payment_status <> 'canceled'
-        AND
-            die.entry_type IN ('brokerage adm partner')
-        AND
-            die.description NOT IN ('Taxa de corretagem - Consultor Imobiliário')
+            AND di.payment_status <> 'canceled'
+            AND die.entry_type IN ('brokerage adm partner')
+            AND die.description NOT IN ('Taxa de corretagem - Consultor Imobiliário')
         GROUP BY
-        1,2,3,4,5,6,7,8,11,12,13,14
+            1,2,3,4,5,6,7,8,9,12,13,14,15
     ),
     rental_ciq_commission AS (
         SELECT
@@ -126,9 +133,10 @@ WITH brokerage_fee AS (
             c.first_rental_commission,
             ct.agent_brokerage_share AS agent_brokerage_share,
             p.brokerage_split_percentage,
+            pp.brokerage_split_percentage AS select_agent_brokerage_split_percentage,
             ROUND(c.rent * c.first_rental_commission * COALESCE(p.brokerage_split_percentage,0.00),2) AS prod_theorical_amount,
-            ROUND(sum(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
-            ROUND(sum(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
+            ROUND(SUM(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
+            ROUND(SUM(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
             i.accrual_year_month AS accrual_year_month,
             DATE(di.ts_due) AS dt_due,
             DATE(di.ts_paid) AS dt_paid,
@@ -144,103 +152,121 @@ WITH brokerage_fee AS (
             ON c.sk_contract = fie.id_contract
         LEFT JOIN datalake_ebdb_clean.contract_partnership_data p
             ON c.sk_contract = p.id_contract
+            AND p.partner_type = 'AUTONOMOUS_AGENT'
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data pp
+            ON c.sk_contract = pp.id_contract
+            AND pp.partner_type = 'EXECUTIVE_FOR_RENT'
         LEFT JOIN datalake_ebdb_clean.contract ct
             ON c.sk_contract = ct.id
         WHERE
             die.from_account_type NOT IN ('quinto andar', 'contract expenses')
-        AND
-            di.payment_status <> 'canceled'
-        AND
-            die.entry_type IN ('brokerage adm partner')
-        AND
-            die.description IN ('Taxa de corretagem - Consultor Imobiliário')
+            AND di.payment_status <> 'canceled'
+            AND die.entry_type IN ('brokerage adm partner')
+            AND die.description IN ('Taxa de corretagem - Consultor Imobiliário')
         GROUP BY
-        1,2,3,4,5,6,7,8,11,12,13,14
+            1,2,3,4,5,6,7,8,9,12,13,14,15
     ),
-rental_brokerage_fee_desconto AS (
-    WITH rental_brokerage_fee_desconto_gds AS (
+    rental_ciq_select_commission AS (
         SELECT
-            fie.id_invoice AS sk_invoice,
+            c.sk_contract AS id_contract_ebdb,
             die.id AS id_invoice_entry,
-            die.description,
-            fie.ts_created,
-            ROW_NUMBER() OVER(PARTITION BY fie.id_invoice, die.description ORDER BY fie.ts_created) AS rn
+            'ciq select' AS brokerage_share,
+            di.payment_status AS invoice_payment_status,
+            c.first_rental_commission,
+            ct.agent_brokerage_share AS agent_brokerage_share,
+            p.brokerage_split_percentage,
+            pp.brokerage_split_percentage AS select_agent_brokerage_split_percentage,
+            ROUND(c.rent * c.first_rental_commission * COALESCE(pp.brokerage_split_percentage,0.00),2) AS prod_theorical_amount,
+            ROUND(SUM(fie.brl_entry_due_amount),2) AS invoice_theorical_amount,
+            ROUND(SUM(fie.brl_entry_paid_amount),2) AS invoice_paid_amount,
+            i.accrual_year_month AS accrual_year_month,
+            DATE(di.ts_due) AS dt_due,
+            DATE(di.ts_paid) AS dt_paid,
+            c.dt_start AS dt_contract_start
         FROM datalake_invoice.invoice_entries fie
         INNER JOIN datalake_retsuko.invoice_entry die
             ON fie.id = die.id
+        LEFT JOIN datalake_retsuko.invoice_info di
+            ON di.id_invoice = fie.id_invoice
+        LEFT JOIN datalake_retsuko.invoice i
+            ON di.id_invoice = i.id_external
+        LEFT JOIN dw_public.dim_contract c
+            ON c.sk_contract = fie.id_contract
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data p
+            ON c.sk_contract = p.id_contract
+            AND p.partner_type = 'AUTONOMOUS_AGENT'
+        LEFT JOIN datalake_ebdb_clean.contract_partnership_data pp
+            ON c.sk_contract = pp.id_contract
+            AND pp.partner_type = 'EXECUTIVE_FOR_RENT'
+        LEFT JOIN datalake_ebdb_clean.contract ct
+            ON c.sk_contract = ct.id
         WHERE
-            die.description LIKE '%Desconto por cadastro com link de indicação%'
-    )
-    SELECT id_invoice_entry
-    FROM rental_brokerage_fee_desconto_gds
-    WHERE rn > 1
-    AND sk_invoice > 0),
-rental_brokerage_fee_parcelamento AS (
-    WITH rental_brokerage_fee_parcelamento_gds AS (
-        SELECT
-            fie.id_invoice AS sk_invoice,
-            die.id AS id_invoice_entry,
-            die.description,
-            fie.ts_created,
-            ROW_NUMBER() OVER(PARTITION BY fie.id_invoice, die.description ORDER BY fie.ts_created) AS rn
-        FROM datalake_invoice.invoice_entries fie
-        INNER JOIN datalake_retsuko.invoice_entry die
-            ON fie.id = die.id
-        WHERE
-            die.description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%'
-    )
-    SELECT
-        id_invoice_entry
-    FROM rental_brokerage_fee_parcelamento_gds
-    WHERE rn = 1
-    AND sk_invoice > 0
+            die.from_account_type NOT IN ('quinto andar', 'contract expenses')
+            AND di.payment_status <> 'canceled'
+            AND die.entry_type IN ('brokerage partner select')
+            AND die.description IN ('Taxa de corretagem - Corretor Parceiro Select')
+        GROUP BY
+            1,2,3,4,5,6,7,8,9,12,13,14,15
 ),
-rental_brokerage_fee_parcela AS (
+rental_brokerage_base AS (
     SELECT
-        die.id AS id_invoice_entry
-    FROM datalake_retsuko.invoice_entry die
+        fie.id_invoice AS sk_invoice,
+        die.id AS id_invoice_entry,
+        die.description,
+        fie.ts_created
+    FROM 
+        datalake_invoice.invoice_entries fie
+    INNER JOIN 
+        datalake_retsuko.invoice_entry die
+            ON fie.id = die.id
     WHERE
-        die.description LIKE 'Parcela%'
+        fie.id_invoice > 0
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY fie.id_invoice, die.description ORDER BY fie.ts_created) > 1
 )
 SELECT
-  *
-FROM rental_brokerage_fee
-WHERE
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_desconto)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcelamento)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcela)
+    *
+FROM 
+    rental_brokerage_fee
+WHERE 
+    id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Desconto por cadastro com link de indicação%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE 'Parcela%')
 UNION
 SELECT
-  *
-FROM rental_agents_commission
-WHERE
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_desconto)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcelamento)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcela)
+    *
+FROM 
+    rental_agents_commission
+WHERE 
+    id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Desconto por cadastro com link de indicação%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE 'Parcela%')
 UNION
 SELECT
-  *
+    *
 FROM partner_revenue_share_brokerage
-WHERE
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_desconto)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcelamento)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcela)
+WHERE 
+    id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Desconto por cadastro com link de indicação%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE 'Parcela%')
 UNION
 SELECT
-  *
-FROM rental_ciq_commission
-WHERE
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_desconto)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcelamento)
-AND
-  id_invoice_entry NOT IN (SELECT * FROM rental_brokerage_fee_parcela)
+    *
+FROM 
+    rental_ciq_commission
+WHERE 
+    id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Desconto por cadastro com link de indicação%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE 'Parcela%')
+UNION
+SELECT
+    *
+FROM 
+    rental_ciq_select_commission
+WHERE 
+    id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Desconto por cadastro com link de indicação%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE '%Crédito - Parcelamento corretagem - QuintoAndar%')
+    AND id_invoice_entry NOT IN (SELECT id_invoice_entry FROM rental_brokerage_base WHERE description LIKE 'Parcela%')
 )
 SELECT
   id_contract_ebdb,
@@ -250,6 +276,7 @@ SELECT
   first_rental_commission,
   agent_brokerage_share,
   brokerage_split_percentage,
+  select_agent_brokerage_split_percentage,
   prod_theorical_amount,
   SUM(invoice_theorical_amount) AS invoice_theorical_amount,
   SUM(invoice_paid_amount) AS invoice_paid_amount,
@@ -257,5 +284,6 @@ SELECT
   dt_due,
   dt_paid,
   dt_contract_start
-FROM brokerage_fee
-GROUP BY 1,2,3,4,5,6,7,8,11,12,13,14
+FROM 
+    brokerage_fee
+GROUP BY 1,2,3,4,5,6,7,8,9,12,13,14,15
