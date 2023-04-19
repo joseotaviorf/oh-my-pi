cb4cce45d6669064ad26a6b88e52f02880f66c87
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-04-19 10:01:47 -0300

    [AES&S] Adding new table in Repairs DAG (#10868)

---
+++ dags/support_and_service/repairs/queries/clean/repair_request_tenant_negotiation_follow_up.sql
@@ -0,0 +1,17 @@
+SELECT
+	id,
+	repair_request_id AS id_repair_request,
+	description,
+	status,
+	help_action,
+	solving_action,
+	expected_execution_date AS ts_expected_execution,
+	reassign_date AS ts_reassign,
+	created_at AS ts_created,
+	updated_at AS ts_updated
+FROM
+    datalake_repairs_raw.repair_request_tenant_negotiation_follow_up
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
