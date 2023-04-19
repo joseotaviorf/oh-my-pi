479061905d777b283bdba4c6dcae367497219175
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-04-19 10:28:17 -0300

    [AES&S] Fixing repair_request_tenant_negotiation_follow_up query  (#10896)

--- dags/support_and_service/repairs/queries/clean/repair_request_tenant_negotiation_follow_up.sql
+++ dags/support_and_service/repairs/queries/clean/repair_request_tenant_negotiation_follow_up.sql
@@ -8,7 +8,10 @@ SELECT
 	expected_execution_date AS ts_expected_execution,
 	reassign_date AS ts_reassign,
 	created_at AS ts_created,
-	updated_at AS ts_updated
+	updated_at AS ts_updated,
+	year,
+	month,
+	day
 FROM
     datalake_repairs_raw.repair_request_tenant_negotiation_follow_up
 WHERE
