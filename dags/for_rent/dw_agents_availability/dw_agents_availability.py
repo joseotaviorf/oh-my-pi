3b674edc8db090b23ecf9ebead2f3c5e4ec238e2
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-04-27 09:41:28 -0300

    changing cluster to photon of dw_agents_availability (#10959)

--- dags/for_rent/dw_agents_availability/dw_agents_availability.py
+++ dags/for_rent/dw_agents_availability/dw_agents_availability.py
@@ -43,7 +43,7 @@ dw_query_filters = incremental_load_parameters["dw_query_filters"]
 SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base"
 
 default_libraries = config_service.get_config("default_libraries")
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("databricks_10_4_med_general_photon_cluster")
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
