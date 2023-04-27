6d8deeb1f31e252810247aa08cfdce35622604fb
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2023-04-27 10:59:45 -0300

    add new category on dim_partner_atta (#10971)
    * add new category on dim_partner_atta
    
    * fix metadata

--- dags/fintech/dw_atta/queries/dw/dim_partner_atta.sql
+++ dags/fintech/dw_atta/queries/dw/dim_partner_atta.sql
@@ -5,7 +5,7 @@ SELECT DISTINCT
   COALESCE(id_registration_user,-1) AS sk_registration_user,
   partner_name,
   CASE
-        WHEN REPLACE(LOWER(partner_name), ' ') LIKE '%5a>>atta%'    THEN 'QuintoAndar Full Atta'
+        WHEN REPLACE(LOWER(partner_name), ' ') LIKE '%5a>>atta%' OR LOWER(partner_name) LIKE 'carteira h%'  THEN 'QuintoAndar Full Atta'
         WHEN REPLACE(LOWER(partner_name), ' ') LIKE '%5a%'          THEN 'QuintoAndar Backlog'
         WHEN LOWER(partner_name) LIKE '%casa mineira%'              THEN 'Casa Mineira'
         ELSE 'Other partnerships'
