5b000308df66465337d8ef0a73c6f5b7a2a86651
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-05-02 13:51:07 -0300

    [AES&S] Adding new workday columns to the workday_window table (#10994)

--- dags/support_and_service/enrich_date/queries/enrich/workday_window.sql
+++ dags/support_and_service/enrich_date/queries/enrich/workday_window.sql
@@ -12,10 +12,16 @@ SELECT
     MAX(CASE WHEN count_workdays = 7 THEN dt_workday ELSE NULL END) AS dt_end_6,
     MAX(CASE WHEN count_workdays = 8 THEN dt_workday ELSE NULL END) AS dt_end_7,
     MAX(CASE WHEN count_workdays = 9 THEN dt_workday ELSE NULL END) AS dt_end_8,
+    MAX(CASE WHEN count_workdays = 10 THEN dt_workday ELSE NULL END) AS dt_end_9,
+    MAX(CASE WHEN count_workdays = 11 THEN dt_workday ELSE NULL END) AS dt_end_10,
+    MAX(CASE WHEN count_workdays = 12 THEN dt_workday ELSE NULL END) AS dt_end_11,
+    MAX(CASE WHEN count_workdays = 13 THEN dt_workday ELSE NULL END) AS dt_end_12,
+    MAX(CASE WHEN count_workdays = 14 THEN dt_workday ELSE NULL END) AS dt_end_13,
+    MAX(CASE WHEN count_workdays = 15 THEN dt_workday ELSE NULL END) AS dt_end_14,
     MAX(CASE WHEN count_workdays = 16 THEN dt_workday ELSE NULL END) AS dt_end_15
 FROM
     datalake_date.cities_calendar
 WHERE
-    count_workdays IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 16)
+    count_workdays BETWEEN 1 AND 16
 GROUP BY
     1, 2, 3, 4
