54fe098db1940284f38402c53e411dde51616ad0
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-05-02 16:32:05 -0300

    [AEFR-845] Migrating VB2OL metrics to metric layer and creating DAG (#10115)
    * adding VB2OL metrics

---
+++ dags/for_rent/metric_rent__bookings/queries/metric/visits_booked_per_ongoing_listings_daily_by_sk_region.sql
@@ -0,0 +1,57 @@
+WITH ongoing_listings AS (
+  SELECT
+    MAKE_DATE(year, month, day) AS date,
+    dr.sk_region,
+    ch.country_code,
+    COUNT(DISTINCT id_house_listing) AS ongoing_listings
+  FROM
+    datalake_rental_historical_follow_up.house_listings_daily_info AS h
+  JOIN 
+    dw_public.dim_region AS dr
+      ON h.id_region = dr.sk_region
+  JOIN
+    datalake_ebdb_country.house AS ch
+      ON h.id_house = ch.id_house
+  WHERE
+    status_history = 'publicado'
+    AND dr.city_group IS NOT NULL
+  GROUP BY 1, 2, 3
+),
+
+visits_booked AS (
+  SELECT
+    dd.date,
+    dr.sk_region,
+    rf.country_code,
+    COUNT(DISTINCT CASE WHEN (rf.sk_booking_created_date  > 0) THEN rf.sk_booking ELSE NULL END) AS visits_booked
+  FROM 
+    dw_public.fact_listing_rent_flows AS rf
+  JOIN 
+    dw_public.dim_booking AS db
+      ON db.sk_booking = rf.sk_booking
+  JOIN 
+    dw_public.dim_date AS dd
+      ON dd.sk_date = rf.sk_booking_created_date
+  JOIN 
+    dw_public.dim_region AS dr
+      ON dr.sk_region = rf.sk_region
+  WHERE 
+    dr.city_group IS NOT NULL
+    AND dd.date < CURRENT_DATE()
+  GROUP BY 1, 2, 3
+)
+
+SELECT
+  vb.date,
+  vb.sk_region,
+  vb.country_code,
+  vb.visits_booked/ol.ongoing_listings AS visits_booked_per_ongoing_listings
+FROM 
+  visits_booked AS vb
+LEFT JOIN 
+  ongoing_listings AS ol
+    ON ol.date = vb.date
+    AND vb.country_code = ol.country_code
+    AND vb.sk_region = ol.sk_region 
+WHERE 
+  vb.date IS NOT NULL
