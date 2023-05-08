1602b357656d437354ab98f8f217ea584cbf4ad6
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2023-05-08 14:46:17 -0300

    [DFORSALE-4555] Reverse Listings Report - Access Layer (#11045)
    - Create reverse_listings_report_access DAG
    - Create Spark Job to send messages to SNS
    - Create config files

---
+++ dags/rede/reverse_listings_report_access/spark_jobs/load_into_sns.py
@@ -0,0 +1,104 @@
+import logging
+import json
+from datetime import datetime
+from argparse import ArgumentParser
+from datetime import datetime, date
+from typing import Tuple
+import boto3
+
+from quintoandar_logger import QuintoAndarLogger
+
+JOB_NAME = "load_into_sns"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+
+def main():
+    database_name, table_name, event_type, sns_topic_arn, execution_date = (
+        parse_arguments()
+    )
+    logger.info(
+        f"""m=__main__, database_name={database_name}, table_name={table_name},
+        event_type={event_type}, sns_topic_arn={sns_topic_arn}, execution_date={execution_date}"""
+    )
+
+    load_table_into_sns(
+        database_name, table_name, event_type, sns_topic_arn, execution_date
+    )
+
+
+def parse_arguments() -> Tuple[str, str, str, str, datetime]:
+    """
+    Parse the arguments passed to the job.
+    Returns a tuple with the database name, table name, event type, ARN of the SNS topic and execution date.
+    """
+
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument("database_name", help="Name of the database where the table is")
+    parser.add_argument("table_name", help="Name of the table to be loaded")
+    parser.add_argument("event_type", help="Type of event to be sent to SNS")
+    parser.add_argument("sns_topic_arn", help="ARN of the SNS topic")
+    parser.add_argument(
+        "execution_date", help="Date of the execution in the format YYYY-MM-DD"
+    )
+
+    args = parser.parse_args()
+
+    database_name = args.database_name
+    table_name = args.table_name
+    event_type = args.event_type
+    sns_topic_arn = args.sns_topic_arn
+    execution_date = datetime.fromisoformat(args.execution_date)
+
+    return database_name, table_name, event_type, sns_topic_arn, execution_date
+
+
+def load_table_into_sns(
+    database_name: str,
+    table_name: str,
+    event_type: str,
+    sns_topic_arn: str,
+    execution_date: datetime,
+):
+    """
+    Load the table into SNS.
+    """
+
+    df = spark.table(f"{database_name}.{table_name}")
+    filtered_df = df.filter(
+        (df.year == execution_date.year)
+        & (df.month == execution_date.month)
+        & (df.day == execution_date.day)
+    ).drop("year", "month", "day")
+    message_contents = filtered_df.collect()
+    messages = [
+        {"event_type": event_type, "payload": row.asDict()} for row in message_contents
+    ]
+
+    region = sns_topic_arn.split(":")[3]
+    sns_client = boto3.client("sns", region_name=region)
+    for message in messages:
+        send_message_dict_to_sns(sns_client, sns_topic_arn, message)
+
+
+def send_message_dict_to_sns(sns_client, sns_topic_arn: str, message: dict):
+    """
+    Send a message to SNS.
+    """
+    sns_client.publish(
+        TopicArn=sns_topic_arn, Message=json.dumps(message, default=json_serial)
+    )
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+if __name__ == "__main__":
+    main()
