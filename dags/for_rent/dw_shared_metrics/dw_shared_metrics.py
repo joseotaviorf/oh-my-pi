61939504523bd524bf25f633899922d8e70486cf
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-05-08 13:25:17 -0300

    Increasing timeout (#11049)

--- dags/for_rent/dw_shared_metrics/dw_shared_metrics.py
+++ dags/for_rent/dw_shared_metrics/dw_shared_metrics.py
@@ -20,7 +20,7 @@ DW_SCHEMA = "metrics"
 CONTEXT = "shared_metrics"
 DAG_NAME = f"dw_{CONTEXT}"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
-EXECUTION_TIMEOUT_HOURS = 3.0
+EXECUTION_TIMEOUT_HOURS = 4.0
 
 
 configs_service = ConfigurationService(DAG_NAME)
