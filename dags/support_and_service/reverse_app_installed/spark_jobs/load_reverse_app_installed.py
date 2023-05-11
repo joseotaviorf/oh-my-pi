5c2c194adc91e0c54f554d21b04ffe8b107aa567
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-05-11 11:44:30 -0300

    [DDSS-1834] Create reverse_app_installed DAG (#11077)
    * feat: create new reverse
    
    * feat: add files for reverse app installed dag
    
    * feat: add dag file
    
    * feat: send data in batches to minority report
    
    * feat: use request session for header persistence

---
+++ dags/support_and_service/reverse_app_installed/spark_jobs/load_reverse_app_installed.py
@@ -0,0 +1,70 @@
+import json
+import requests
+from argparse import ArgumentParser
+
+from bietlejuice.clients.db_clients import SparkClient
+from quintoandar_logger import QuintoAndarLogger
+
+
+JOB_NAME = "load_reverse_app_installed"
+
+
+def create_data_payload(item):
+    data = {
+        "keyName": "user",
+        "type": "CUSTOMER_HAS_APP",
+    }
+    data["keyValue"] = item["id_user"]
+    data["contextFields"] = {"dateTimeLastAccessApp": item["dt_last_event"]}
+    return data
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    logger = QuintoAndarLogger("MinorityReportAPIClient")
+
+    parser.add_argument("environment")
+    parser.add_argument("datalake_bucket")
+    parser.add_argument("source")
+    parser.add_argument("minority_report_endpoint")
+    parser.add_argument("table")
+    parser.add_argument("api_type")
+    parser.add_argument("key_name")
+    parser.add_argument("execution_date")
+
+    args = parser.parse_args()
+
+    environment = args.environment
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+    minority_report_endpoint = args.minority_report_endpoint
+    table = args.table
+    api_type = args.api_type
+    key_name = args.key_name
+    execution_date = args.execution_date
+
+    spark_client = SparkClient()
+
+    df = spark_client.conn.sql(
+        f"""
+        SELECT * FROM datalake_app_installed.{table}
+        WHERE dt_last_event >= '{execution_date}' - INTERVAL "7" DAY
+        """
+    )
+    df_list = df.toJSON().map(lambda str_json: json.loads(str_json)).collect()
+    batches = [df_list[x : x + 100] for x in range(0, len(df_list), 100)].copy()
+    total_batches = len(batches)
+
+    session = requests.Session()
+    session.headers.update({"Content-Type": "application/json"})
+
+    for idx, batch in enumerate(batches):
+        new_batch = [create_data_payload(x) for x in batch]
+        resp = session.post(
+            minority_report_endpoint,
+            data=json.dumps(new_batch),
+        )
+        if resp.status_code == 200:
+            logger.info(f" Succesfully loaded batch {idx + 1}/{len(batches)}")
+        else:
+            logger.info(resp.raise_for_status())
