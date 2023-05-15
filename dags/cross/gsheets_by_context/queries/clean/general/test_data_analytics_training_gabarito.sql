9e5456b44c1def8e612fa9b389f3ac488cf403f0
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2023-05-15 11:46:47 -0300

    ingest data from data analytics training. (#11098)

---
+++ dags/cross/gsheets_by_context/queries/clean/general/test_data_analytics_training_gabarito.sql
@@ -0,0 +1,7 @@
+SELECT
+    class,
+    question,
+    answer,
+    ts_load
+FROM
+    datalake_gsheets_raw.test_data_analytics_training_gabarito
---
+++ dags/cross/gsheets_by_context/queries/clean/general/test_data_analytics_training_quiz_1.sql
@@ -0,0 +1,11 @@
+SELECT
+    timestamp(date) as date,
+    email,
+    points,
+    question_1,
+    question_2,
+    question_3,
+    line,
+    ts_load
+FROM
+    datalake_gsheets_raw.test_data_analytics_training_quiz_1
