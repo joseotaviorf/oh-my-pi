866eebb5124ddebfd6e90740faad602e20ea823d
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2023-05-17 10:33:29 -0300

    [AEFR-2218] Updating ForRent clusters to photon (#11134)
    * kill queue
    
    * enrich_ebdb_work_contract
    
    * enrich_ebdb_listing

--- dags/for_rent/enrich_kill_queue/enrich_kill_queue.py
+++ dags/for_rent/enrich_kill_queue/enrich_kill_queue.py
@@ -32,7 +32,7 @@ default_libraries = config_service.get_config("default_libraries")
 
 s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
 SPARK_JOBS_PATH = f"{s3_prefix}/spark_jobs/base"
-cluster_description = config_service.get_config("databricks_10_4_min_general_cluster")
+cluster_description = config_service.get_config("databricks_10_4_min_general_photon_cluster")
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
