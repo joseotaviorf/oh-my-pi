6da35de3f113ed2979cd6bddecfdcfaca7d6bdf7
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-05-24 15:22:43 -0300

    [AEFR] Updating sources from ongoing listings metrics (#11211)
    * updating ongoing_listing metrics

--- dags/for_rent/metric_rent__listings/queries/metric/ongoing_listings_daily_by_sk_region.sql
+++ dags/for_rent/metric_rent__listings/queries/metric/ongoing_listings_daily_by_sk_region.sql
@@ -1,17 +1,35 @@
+WITH ongoing_listings AS (
+  SELECT /*+ RANGE_JOIN(fhls, 800) */
+    fhls.sk_house_listing,
+    fhls.sk_region,
+    fhls.country_code,
+    d.date
+  FROM 
+    dw_public.fact_house_listing_status AS fhls
+  JOIN
+    dw_public.dim_house_listing AS dhl
+      ON fhls.sk_house_listing = dhl.sk_house_listing
+  JOIN
+    dw_public.dim_region AS dr
+      ON fhls.sk_region = dr.sk_region
+  JOIN 
+    dw_public.dim_date AS d
+      ON d.date BETWEEN COALESCE(DATE(fhls.ts_status_start), DATE('2000-01-01')) 
+        AND COALESCE(DATE_ADD(DATE(fhls.ts_status_end), -1), CURRENT_DATE())
+  WHERE 
+    fhls.status_history = 'publicado' -- consider only published status
+    AND dhl.version <> 0 -- consider only listings that already started publication
+    AND dr.city_group IS NOT NULL
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY fhls.sk_house_listing, d.date ORDER BY fhls.ts_status_start DESC) = 1
+)
+  
 SELECT
-  MAKE_DATE(year, month, day) AS date,
-  dr.sk_region,
-  ch.country_code,
-  COUNT(DISTINCT id_house_listing) AS ongoing_listings
+  date,
+  sk_region,
+  country_code,
+  COUNT(DISTINCT sk_house_listing) AS ongoing_listings
 FROM
-  datalake_rental_historical_follow_up.house_listings_daily_info AS h
-JOIN 
-  dw_public.dim_region AS dr
-    ON h.id_region = dr.sk_region
-JOIN
-  datalake_ebdb_country.house AS ch
-    ON h.id_house = ch.id_house
-WHERE
-  status_history = 'publicado'
-  AND dr.city_group IS NOT NULL
-GROUP BY  1, 2, 3
+  ongoing_listings 
+GROUP BY 1, 2, 3
+ 
--- dags/for_rent/metric_rent__listings/queries/metric/ongoing_listings_monthly_by_sk_region.sql
+++ dags/for_rent/metric_rent__listings/queries/metric/ongoing_listings_monthly_by_sk_region.sql
@@ -1,25 +1,35 @@
+WITH ongoing_listings AS (
+  SELECT /*+ RANGE_JOIN(fhls, 800) */
+    fhls.sk_house_listing,
+    fhls.sk_region,
+    fhls.country_code,
+    d.month_start
+  FROM 
+    dw_public.fact_house_listing_status AS fhls
+  JOIN
+    dw_public.dim_house_listing AS dhl
+      ON fhls.sk_house_listing = dhl.sk_house_listing
+  JOIN
+    dw_public.dim_region AS dr
+      ON fhls.sk_region = dr.sk_region
+  JOIN 
+    dw_public.dim_date AS d
+      ON d.date BETWEEN COALESCE(DATE(fhls.ts_status_start), DATE('2000-01-01')) 
+        AND COALESCE(DATE_ADD(DATE(fhls.ts_status_end), -1), CURRENT_DATE())
+  WHERE 
+    fhls.status_history = 'publicado' -- consider only published status
+    AND dhl.version <> 0 -- consider only listings that already started publication
+    AND dr.city_group IS NOT NULL
+    AND d.date = d.month_end
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY fhls.sk_house_listing, d.date ORDER BY fhls.ts_status_start DESC) = 1
+)
+  
 SELECT
   month_start,
-  dr.sk_region,
-  ch.country_code,
-  COUNT(DISTINCT id_house_listing) AS ongoing_listings
+  sk_region,
+  country_code,
+  COUNT(DISTINCT sk_house_listing) AS ongoing_listings
 FROM
-  datalake_rental_historical_follow_up.house_listings_daily_info AS h
-JOIN 
-  dw_public.dim_region AS dr
-    ON h.id_region = dr.sk_region
-JOIN
-  datalake_ebdb_country.house AS ch
-    ON h.id_house = ch.id_house
-JOIN 
-  dw_public.dim_date AS d
-    ON d.date >= DATE(h.ts_status_started)
-    AND d.date < COALESCE(DATE(h.ts_status_ended), CURRENT_DATE())
-WHERE
-  status_history = 'publicado'
-  AND dr.city_group IS NOT NULL
-  AND d.date = d.month_end
-  AND h.year = d.year
-  AND h.month = d.month
-  AND h.day = d.day
+  ongoing_listings 
 GROUP BY 1, 2, 3
