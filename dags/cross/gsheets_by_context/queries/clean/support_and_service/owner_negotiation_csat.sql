0d3306956492ab262f81d30b5dd08e688dd0e6f1
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-05-24 10:04:26 -0300

    [AES&S] Restoring removed gsheets (#11203)

---
+++ dags/cross/gsheets_by_context/queries/clean/support_and_service/owner_negotiation_csat.sql
@@ -0,0 +1,8 @@
+SELECT
+    NULLIF(token,'') AS id_csat_answer,
+    INT(NULLIF(contract_id,'')) AS id_contract,
+    NULLIF(comments,'') AS comments,
+    NULLIF(score_text,'') AS score_text,
+    TO_TIMESTAMP(NULLIF(submitted_at,''),'M/d/y H:m:s') AS ts_submitted
+FROM
+    datalake_gsheets_raw.csat_negociacao_pp
---
+++ dags/cross/gsheets_by_context/queries/clean/support_and_service/owner_offboarding_repair_csat.sql
@@ -0,0 +1,8 @@
+SELECT
+    NULLIF(email,'') AS email,
+    NULLIF(tags,'') AS improvement_tags,
+    NULLIF(comments,'') AS comments,
+    INT(NULLIF(score,'')) AS score,
+    TO_TIMESTAMP(NULLIF(ts_input,''),'M/d/y H:m:s') AS ts_input
+FROM
+    datalake_gsheets_raw.csat_reparos_off_pp
