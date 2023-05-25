5f9183da9b2beaba833ee476553e77228aa64dba
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2023-05-25 16:23:32 -0300

    [DFORSALE - 5234] Add new sales flow tables. (#11222)

---
+++ dags/for_sale/sales_flow/queries/clean/bypass.sql
@@ -0,0 +1,19 @@
+SELECT
+    id,
+    house_id AS id_house,
+    register_number,
+    rating,
+    status,
+    filepath_s3 AS file_path_s3,
+    message,
+    found_data,
+    bypass_confirmation AS is_bypass,
+    processing_date AS dt_processing,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_sales_flow_raw.bypass
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
---
+++ dags/for_sale/sales_flow/queries/clean/house_registration_order.sql
@@ -0,0 +1,18 @@
+SELECT
+    id,
+    external_house_id AS id_house_external,
+    house_registration_number,
+    city,
+    state,
+    registry_office,
+    status,
+    order_number,
+    error_status,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_sales_flow_raw.house_registration_order
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
