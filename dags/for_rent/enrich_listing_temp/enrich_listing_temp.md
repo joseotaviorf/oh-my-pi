f42eba423d285a6569fce9d1519e254b4677c2d1
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2023-06-01 10:19:50 -0300

    [AEFR-2452] Create table house_listing_status_lbc (#11283)
    * Create table house_listing_status_lbc

--- dags/for_rent/enrich_listing_temp/enrich_listing_temp.md
+++ dags/for_rent/enrich_listing_temp/enrich_listing_temp.md
@@ -14,4 +14,5 @@ Daily. More information about run time [here]({chart_url}{dag_id}).
 Produces the following output tables, via full load:
 
 - `dim_house_listing_lbc`
+- `house_listing_status_lbc`
 - `lbc_house_listing`
---
+++ dags/for_rent/enrich_listing_temp/queries/enrich/house_listing_status_lbc.sql
@@ -0,0 +1,22 @@
+SELECT
+    CAST(CAST(lbc_version.id_house AS STRING)||'00'||CAST(lbc_version.listing_version AS STRING) AS BIGINT) AS id_house_listing,
+    lbc_version.id_house,
+    house.id_region,
+    lbc_version.country_code,
+    lbc_version.listing_version AS version,
+    lbc_version.status AS status_history,
+    lbc_version.status_reason AS status_change_reason,
+    COALESCE(
+      -- get MAX ts per id_house_listings per day
+      MAX(lbc_version.rev) OVER(
+      PARTITION BY lbc_version.id_house, lbc_version.listing_version, CAST(lbc_version.ts_state_started AS DATE)
+                    ) = lbc_version.rev,
+    FALSE) AS is_last_status_of_day,
+    lbc_version.ts_first_publication,
+    lbc_version.ts_state_started AS ts_status_started,
+    lbc_version.ts_state_ended AS ts_status_ended
+FROM 
+  datalake_ebdb_listing.lbc_status_version_order AS lbc_version
+JOIN 
+    datalake_ebdb_clean.house AS house
+        ON house.id = lbc_version.id_house
