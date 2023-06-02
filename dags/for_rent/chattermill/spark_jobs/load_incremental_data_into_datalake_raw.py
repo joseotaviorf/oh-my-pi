8cfc8cfa92cbd57bd5fa8e964acbd20a213da489
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2023-06-02 14:00:28 -0300

    Fix force recreate param (#11302)

--- dags/for_rent/chattermill/spark_jobs/load_incremental_data_into_datalake_raw.py
+++ dags/for_rent/chattermill/spark_jobs/load_incremental_data_into_datalake_raw.py
@@ -165,7 +165,7 @@ if __name__ == "__main__":
                 format_options,
                 database_location,
                 partition_cols,
-                force_recreate=False,
+                force_recreate=True,
             )
             spark_metastore_service.create_new_partitions_from_df(
                 database_name=database_name,
