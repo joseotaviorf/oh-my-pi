08b490ce0bcacc8557d0b05e5ee62c86e5539894
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2023-06-06 15:32:27 -0300

    Revert "[DG-941] Ingest data from data training quizzes. (#11296)" (#11335)
    This reverts commit 1a993e016af6797bfc80db041f18b1eb95fa8dc3.

--- dags/cross/gsheets_by_context/queries/clean/general/data_analytics_training_10_basic_statistical_concepts.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    to_timestamp(date, "dd/MM/yyyy HH:mm:ss") AS answer_date,
-    email,
-    points,
-    line,
-    question_1,
-    ts_load
-FROM
-    datalake_gsheets_raw.data_analytics_training_10_basic_statistical_concepts
--- dags/cross/gsheets_by_context/queries/clean/general/data_analytics_training_gabarito.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    class,
-    question,
-    answer,
-    ts_load
-FROM
-    datalake_gsheets_raw.data_analytics_training_gabarito
