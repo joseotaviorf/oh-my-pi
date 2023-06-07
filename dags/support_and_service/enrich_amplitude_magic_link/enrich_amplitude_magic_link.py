67aa26a4232d0820345a447f2f9be3dccc1f836f
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2023-06-07 09:55:48 -0300

    Apply photon (#11323)

--- dags/support_and_service/enrich_amplitude_magic_link/enrich_amplitude_magic_link.py
+++ dags/support_and_service/enrich_amplitude_magic_link/enrich_amplitude_magic_link.py
@@ -42,7 +42,7 @@ s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
 spark_jobs_path = f"{s3_prefix}/spark_jobs/base/"
 
 # databricks var
-cluster_description = config_service.get_config("databricks_10_4_med_io-memory_cluster")
+cluster_description = config_service.get_config("databricks_10_4_med_io-memory_photon_cluster")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
