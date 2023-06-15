3c9c39d7de8c4e1dc410a200869d75d3b477f960
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2023-06-15 17:17:29 -0300

    [AEFR] Changing owners from Chattermill pipeline and Reverse Tracksale (#11435)
    * [AEFR] Changing owners from Chattermill pipeline and Reverse Tracksale
    
    * skip list

--- dags/cross/reverse_tracksale/reverse_tracksale.py
+++ dags/cross/reverse_tracksale/reverse_tracksale.py
@@ -58,7 +58,7 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 dag = DAG(
     dag_id=DAG_ID,
     default_args={
-        "owner": DAGOwnerEnum.DATA_SS,
+        "owner": DAGOwnerEnum.DEFAULT_OWNER,
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
--- dags/support_and_service/chattermill/chattermill.py
+++ dags/support_and_service/chattermill/chattermill.py
@@ -64,7 +64,7 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 dag = DAG(
     dag_id=DAG_ID,
     default_args={
-        "owner": DAGOwnerEnum.DATA_FOR_RENT,
+        "owner": DAGOwnerEnum.DATA_SS,
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
