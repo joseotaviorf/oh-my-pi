79c771507f0a11d5069e68315cb1c28a96d3f24a
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2023-06-19 09:35:37 -0300

    Fix early_demand and early_relisting flags (#11433)

--- dags/for_rent/enrich_ebdb_listing/queries/enrich/house_listing_early_demand.sql
+++ dags/for_rent/enrich_ebdb_listing/queries/enrich/house_listing_early_demand.sql
@@ -1,92 +1,50 @@
-WITH
-early_relisting_house_state AS (
+WITH relisting AS (
   SELECT
-      bch.id_house,
-      bch.country_code,
-      bch.status,
-      bch.status_reason,
-      bch.suspension_reason,
-      bch.ts_state_started,
-      bch.ts_state_ended,
-      ure.id_user,
-      TRUE AS is_early_demand
-  FROM
-    datalake_ebdb_listing.business_context_history AS bch
-  JOIN 
-    datalake_ebdb_clean.user_revision_entity AS ure
-      ON bch.rev = ure.id
-  WHERE
-      bch.business_context = 'RENT'
-      AND bch.status = 'PUBLISHED'
-      AND bch.status_reason LIKE 'RELISTING_%'
-      AND bch.suspension_reason = 'RELISTING'
-      AND ure.id_user != 4299181 --This filters data inputed by a faulty script. This rule will be replaced in the near future.
-), early_relisting_dates AS (
-  SELECT 
-    hl.id_house_listing,
-    hl.id_house,
-    hs.country_code,
-    hs.status,
-    hs.status_reason,
-    hs.suspension_reason,
-    hs.is_early_demand,
-    IF(hs.is_early_demand, hs.ts_state_started, NULL) AS ts_early_demand_started,
-    IF(hs.is_early_demand, hs.ts_state_ended, NULL) AS ts_early_demand_ended,
-    hs.ts_state_started
-  FROM 
-    early_relisting_house_state AS hs
-  JOIN 
-    datalake_ebdb_listing.house_listing_category AS hl 
-      ON hl.id_house = hs.id_house
-        AND hs.ts_state_started BETWEEN hl.ts_listing_version_start AND COALESCE(hl.ts_listing_version_end, '2700-01-01')
-),
-early_relisting_date_selection AS (
-  SELECT 
-    id_house_listing,
+    CAST(CAST(id_house AS STRING)||'00'||CAST(listing_version AS STRING) AS BIGINT) AS id_house_listing,
     id_house,
     country_code,
-    status,
-    status_reason,
-    suspension_reason,
-    is_early_demand, 
-    MIN(ts_early_demand_started) AS ts_early_demand_started,
-    MIN(ts_early_demand_ended) AS ts_early_demand_ended
+    listing_version,
+    ts_state_started AS ts_early_relisting_started
   FROM 
-    early_relisting_dates
-  GROUP BY 
-    id_house_listing,
-    id_house,
-    country_code,
-    status,
-    status_reason,
-    suspension_reason,
-    is_early_demand
-), 
-house_lbc_state AS (
-  SELECT 
-    id_house,
-    country_code,
-    status,
-    status_reason,
-    suspension_reason,
-    ts_state_started,
-    ts_state_ended,
-    ROW_NUMBER() OVER(PARTITION BY id_house ORDER BY ts_state_started DESC) AS state_order
-  FROM 
-    datalake_ebdb_listing.business_context_history 
+    datalake_ebdb_listing.lbc_status_version_order
   WHERE 
-    business_context = 'RENT'
+    status = 'PUBLISHED'
+    AND status_reason like 'RELISTING'
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY id_house, listing_version ORDER BY ts_state_started, ts_state_ended) = 1
+),
+demand AS (
+  SELECT
+    CAST(CAST(id_house AS STRING)||'00'||CAST(listing_version AS STRING) AS BIGINT) AS id_house_listing,
+    id_house,
+    country_code,
+    listing_version,
+    ts_state_started AS ts_early_demand_started
+  FROM 
+    datalake_ebdb_listing.lbc_status_version_order
+  WHERE 
+    status = 'PUBLISHED'
+    AND status_reason like 'RELISTING_%'
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY id_house, listing_version ORDER BY ts_state_started, ts_state_ended) = 1
 )
 SELECT 
-  ds.id_house_listing,
-  ds.id_house,
-  ds.country_code,
-  IF(hs.status = 'PUBLISHED' and hs.status_reason = 'RELISTING_OFFER' and hs.suspension_reason = 'RELISTING', TRUE, FALSE) AS is_early_demand,
-  CAST(MAX(ds.ts_early_demand_started) AS TIMESTAMP) AS ts_early_demand_started
+  CAST(CAST(lbc_version.id_house AS STRING)||'00'||CAST(lbc_version.listing_version AS STRING) AS BIGINT) AS id_house_listing,
+  lbc_version.id_house,
+  lbc_version.country_code,
+  lbc_version.listing_version,
+  IF(relisting.id_house_listing IS NOT NULL, TRUE, FALSE) AS is_early_relisting,
+  IF(demand.id_house_listing IS NOT NULL, TRUE, FALSE) AS is_early_demand,
+  ts_early_demand_started
 FROM 
-  early_relisting_date_selection AS ds
-JOIN house_lbc_state AS hs
-  ON hs.id_house = ds.id_house
-    AND hs.state_order = 1
-GROUP BY 
-  1,2, 3, hs.status, hs.status_reason, hs.suspension_reason
+  datalake_ebdb_listing.lbc_status_version_order AS lbc_version
+LEFT JOIN 
+  relisting ON 
+    relisting.id_house = lbc_version.id_house
+    AND relisting.listing_version = lbc_version.listing_version
+LEFT JOIN 
+  demand ON 
+    demand.id_house = lbc_version.id_house
+    AND demand.listing_version = lbc_version.listing_version
+GROUP BY
+  1,2,3,4,5,6,7
