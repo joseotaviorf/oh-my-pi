c849498b39c15c806d1da4b29280ea5904ea66f8
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-06-19 15:48:19 -0300

    [AES&S] Removing audit tables from the Repair DAG (#11450)
    * remove instant_approval_preference_aud table
    
    * remove service_request_aud table
    
    * remove rev_info table
    
    * remove   repair_expense_aud table
    
    * remove   repair_request_aud table
    
    * remove   repair_request_item_aud table
    
    * remove   repair_expense_group_aud table
    
    * remove  repair_evidence_aud table

--- dags/support_and_service/repairs/queries/clean/instant_approval_preference_aud.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    house_id AS id_house,
-    rev,
-    revend AS rev_end, 
-    CAST(revtype AS INTEGER) AS rev_type, 
-    covered_amount,
-    covered_amount_mod AS mod_covered_amount
-FROM 
-    datalake_repairs_raw.instant_approval_preference_aud
