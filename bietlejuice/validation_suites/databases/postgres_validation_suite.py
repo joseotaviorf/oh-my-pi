cc1a56986079b29d5ee3f9191022b235470f35bc
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2023-06-21 21:57:33 -0300

    remove redshift validation (#11499)

--- bietlejuice/validation_suites/databases/postgres_validation_suite.py
+++ bietlejuice/validation_suites/databases/postgres_validation_suite.py
@@ -159,12 +159,6 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
         )
         self._validate_connection(db_consumer)
 
-    def validate_redshift(self):
-        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
-            self.auth[DatabaseEnum.DW], SparkClient()
-        )
-        self._validate_connection(db_consumer)
-
     def validate_rental_guarantee(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.RENTAL_GUARANTEE], SparkClient()
