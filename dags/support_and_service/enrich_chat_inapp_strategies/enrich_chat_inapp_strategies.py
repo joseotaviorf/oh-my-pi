49c9ed355fcd7ac30b1775fdf45f64f1f5a38ebf
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-06-26 16:24:33 -0300

    [DDSS-2210] Refactor strategy_for_chat5a Datamart as an enrich (#11532)
    * feat: chat5a datarmat as enrich
    
    * fix: config filenames
    
    * fix: datamart dependencies

---
+++ dags/support_and_service/enrich_chat_inapp_strategies/enrich_chat_inapp_strategies.py
@@ -0,0 +1,96 @@
+import os
+from datetime import datetime
+
+import pendulum
+from airflow.models import DAG
+from airflow.operators.quintoandar_databricks import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from airflow.utils.helpers import chain
+from bietlejuice.base.airflow.base_dag import BaseDAG
+from bietlejuice.base.airflow.dag_owner_enum import DAGOwnerEnum
+from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
+from bietlejuice.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.base.airflow.task_groups.datalake_task_group import DatalakeTaskGroup
+from bietlejuice.services.configuration_service import ConfigurationService
+
+
+CONTEXT = "chat_inapp_strategies"
+DAG_NAME = f"enrich_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+ENV = os.environ.get("ENVIRONMENT")
+LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
+MAIN_START_DATE = datetime(2023, 4, 18, 0, 0, 0, tzinfo=LOCAL_TZ)
+
+config_service = ConfigurationService(DAG_NAME)
+
+# s3 paths setup
+datalake_bucket = config_service.get_config("datalake_bucket")
+athena_query_results_bucket = config_service.get_config("athena_query_results_bucket")
+
+s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
+base_spark_jobs_path = f"{s3_prefix}/spark_jobs/base/"
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+
+dag_documentation = config_service.get_config("dag_documentation")
+cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+
+DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
+    {
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+
+default_libraries = config_service.get_config("default_libraries")
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": DAGOwnerEnum.DATA_SS,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.generate_doc_md_str(
+        dag_name=DAG_NAME,
+        doc_md_chart_url=doc_md_chart_url,
+        dag_documentation=dag_documentation,
+        schedule_interval=None,
+        dag_owner=DAGOwnerEnum.DEFAULT_OWNER,
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=cluster_description,
+    access_control_list=DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST,
+    libraries=default_libraries,
+)
+
+datalake_task_group = DatalakeTaskGroup(
+    dag=dag,
+    env=ENV,
+    datalake_bucket=datalake_bucket,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=base_spark_jobs_path,
+    athena_query_result_location=athena_query_results_bucket,
+)
+
+enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.ENRICH,
+    source_database_base_name=CONTEXT,
+    target_database_base_name=CONTEXT,
+    has_create_external_table_task=False,
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    dag=dag, task_id="terminate-cluster"
+)
+
+chain(create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
