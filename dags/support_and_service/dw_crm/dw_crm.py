7108a5661b0a6b03484b1c64a703d44c39fb2d84
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2023-06-26 17:17:33 -0300

    Reset dw_crm to use All-Purpose operators (#11538)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

---
+++ dags/support_and_service/dw_crm/dw_crm.py
@@ -0,0 +1,120 @@
+from datetime import datetime
+from pendulum import timezone
+import os
+
+from airflow.models import DAG
+from airflow.utils.helpers import chain
+from databricks_plugin import (
+    QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksTerminateClusterOperator,
+)
+
+from bietlejuice.base.airflow.helpers import TaskFlowHelper
+from bietlejuice.base.airflow.base_dag import BaseDAG
+from bietlejuice.base.airflow.dag_owner_enum import DAGOwnerEnum
+from bietlejuice.base.airflow.task_groups.dw_task_group import DWTaskGroup
+from bietlejuice.base.pipeline import LayerEnum
+from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
+from bietlejuice.services.configuration_service import ConfigurationService
+
+# ENV setup
+ENV = os.environ.get("ENVIRONMENT")
+
+# DAG and Jobs params setup
+CONTEXT = "crm"
+DAG_NAME = f"dw_{CONTEXT}"
+DAG_ID = f"bietlejuice.{DAG_NAME}"
+
+MAIN_START_DATE = datetime(2020, 11, 1, tzinfo=timezone("America/Sao_Paulo"))
+
+config_service = ConfigurationService(DAG_NAME)
+dw_bucket = config_service.get_config("dw_bucket")
+s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
+base_spark_jobs_path = f"{s3_prefix}/spark_jobs/base"
+doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+default_libraries = config_service.get_config("default_libraries")
+
+tables_customization = config_service.get_config("tables_customization")
+inner_dependencies = config_service.get_config("inner_dependencies")
+
+cluster_description = config_service.get_config(
+    "databricks_10_4_med_memory_photon_cluster"
+)
+
+databricks_cluster_access_control_list = [
+    {
+        "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
+        "permission_level": ClusterPermissionEnum.MANAGE,
+    }
+]
+
+dag = DAG(
+    dag_id=DAG_ID,
+    default_args={
+        "owner": DAGOwnerEnum.DATA_SS,
+        "wait_for_downstream": False,
+        "depends_on_past": False,
+    },
+    start_date=MAIN_START_DATE,
+    schedule_interval=None,
+    doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
+        chart_url=doc_md_chart_url, dag_id=DAG_ID
+    ),
+)
+
+create_cluster_task = QuintoAndarDatabricksCreateClusterOperator(
+    databricks_conn_id="databricks_job_cluster",
+    dag=dag,
+    task_id="create-cluster",
+    cluster_configuration=cluster_description,
+    access_control_list=databricks_cluster_access_control_list,
+    libraries=default_libraries,
+)
+
+terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
+    databricks_conn_id="databricks_job_cluster", dag=dag, task_id="terminate-cluster"
+)
+
+task_group = DWTaskGroup(
+    dag=dag,
+    env=ENV,
+    dw_bucket=dw_bucket,
+    dw_schema=CONTEXT,
+    relative_query_path=DAG_NAME,
+    spark_jobs_path=base_spark_jobs_path,
+)
+
+dw_staging_task_group = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW_STAGING, tables_customization=tables_customization
+)
+dw_task_group = task_group.build_task_group_from_sql_files(
+    layer=LayerEnum.DW, tables_customization=tables_customization
+)
+
+dw_task_group_boundaries = {}
+for table in dw_task_group:
+    initial_tasks = DWTaskGroup.first_tasks(dw_staging_task_group[table])
+    final_tasks = DWTaskGroup.last_tasks(dw_task_group[table])
+    dw_task_group_boundaries[table] = DWTaskGroup.format_tasks_boundaries(
+        initial_tasks=initial_tasks, final_tasks=final_tasks
+    )
+
+(
+    task_groups_boundaries_without_inner_dependencies,
+    inner_dependencies_task_groups_boundaries,
+) = task_group.set_inner_dag_dependencies(
+    task_flow_helper=TaskFlowHelper(),
+    task_groups_boundaries=dw_task_group_boundaries,
+    dag_inner_dependencies=inner_dependencies,
+)
+
+
+chain(
+    create_cluster_task,
+    DWTaskGroup.all_first_tasks(task_groups_boundaries_without_inner_dependencies)
+    + DWTaskGroup.first_tasks(inner_dependencies_task_groups_boundaries),
+)
+
+TaskFlowHelper.chain_task_groups_via_common_table(dw_staging_task_group, dw_task_group)
+
+chain(DWTaskGroup.all_last_tasks(dw_task_group), terminate_cluster_task)
