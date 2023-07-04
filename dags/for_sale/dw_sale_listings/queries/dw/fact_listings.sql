1885a71e5d1061df616f3e95eef59c1a1dec2737
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2023-07-04 09:35:09 -0300

    [DFORSALE-5653] Fix fact_listings join  (#11617)
    Update dw_sale.fact_listings query to use uuid_company on join with company_sks

--- dags/for_sale/dw_sale_listings/queries/dw/fact_listings.sql
+++ dags/for_sale/dw_sale_listings/queries/dw/fact_listings.sql
@@ -50,7 +50,15 @@ LEFT JOIN
     AND hslc.is_last_ciq_on_listing = True
 LEFT JOIN
   datalake_rede_company.company_sks AS cs_supply
-    ON (h.id_company_hubspot IS NOT NULL
-    AND h.id_company_hubspot = cs_supply.id_hubspot)
-    OR (h.id_company_hubspot IS NULL
-    AND h.partner_3p_supply = cs_supply.extracted_3p_tag)
+    ON (
+      h.uuid_company IS NOT NULL
+      AND h.uuid_company = cs_supply.uuid_company
+    ) OR (
+      h.uuid_company IS NULL
+      AND h.id_company_hubspot IS NOT NULL
+      AND h.id_company_hubspot = cs_supply.id_hubspot
+    ) OR (
+      h.uuid_company IS NULL
+      AND h.id_company_hubspot IS NULL
+      AND h.partner_3p_supply = cs_supply.extracted_3p_tag
+    )
