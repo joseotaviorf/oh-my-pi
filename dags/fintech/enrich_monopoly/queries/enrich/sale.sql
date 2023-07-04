470f427145964af834504a23282179db568aaae0
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2023-07-04 15:31:44 -0300

    [DFORSALE-5786] Fix cashflow cte (#11607)

--- dags/fintech/enrich_monopoly/queries/enrich/sale.sql
+++ dags/fintech/enrich_monopoly/queries/enrich/sale.sql
@@ -1,52 +1,21 @@
-WITH old_dt_occurence_rule AS(
-    WITH last_sale_transaction AS (
-        SELECT
-            *,
-            ROW_NUMBER() OVER (PARTITION BY id_sale ORDER BY ts_created DESC) AS rw_sale_transaction
-        FROM
-            datalake_monopoly_clean.sale_transaction AS st
-        WHERE
-            st.event = 'income-down-payment'
-            AND st.status = 'created'
-        )
-        SELECT
-            s.id AS id_sale,
-            s.id_external_offer,
-            st.id_income_reference,
-            MAX(ir.dt_income) AS dt_occurence,
-            s.ts_created
-        FROM
-            datalake_monopoly_clean.sale AS s
-        LEFT JOIN
-            last_sale_transaction AS st
-                ON st.id_sale = s.id
-                AND rw_sale_transaction = 1
-        LEFT JOIN
-            datalake_monopoly_clean.income_reference AS ir
-                ON st.id_income_reference = ir.id
-        GROUP BY 1, 2, 3, 5
-),
-cash_flow_base AS
+WITH cash_flow_base AS
 (
     SELECT
         s.id_external_offer AS sk_offer,
         DATE(st.ts_created) AS dt_created,
-        SUM(COALESCE(ae.debit,0) - COALESCE(ae.credit,0)) AS cash_flow_amount
+        COALESCE(SUM(ir.amount), 0) - COALESCE(SUM(otr.amount), 0) AS cash_flow_amount
     FROM
-        datalake_monopoly_clean.accounting_entry AS ae
-    --LEFT JOIN
-    --    datalake_monopoly_clean.account AS a
-    --        ON ae.id_account = a.id
-    LEFT JOIN
-        datalake_monopoly_clean.sale_transaction AS st
-            ON ae.id_sale_transaction = st.id
-    LEFT JOIN
         datalake_monopoly_clean.sale AS s
-            ON st.id_sale = s.id
-    --WHERE
-    --    a.id IN (8, 23) -- 8 Itaú | 23 Stark Bank
-    GROUP BY
-        1, 2
+    JOIN
+        datalake_monopoly_clean.sale_transaction AS st
+            ON s.id = st.id_sale
+    LEFT JOIN
+        datalake_monopoly_clean.income_reference AS ir
+            ON st.id_income_reference = ir.id
+    LEFT JOIN
+        datalake_monopoly_clean.outcome_reference AS otr
+            ON st.id_outcome_reference = otr.id
+    GROUP BY ALL
 ),
 cash_flow AS
 (
@@ -85,7 +54,6 @@ SELECT
     cf_total.cash_flow_amount - (so.sale_price_agreed * so.brokerage_fee) AS delta_total_cash_flow_to_broakerage_amount,
     cf.cash_flow_amount AS cash_flow_amount_when_payment_allowed,
     ad.dt_first_full_down_payment_event IS NOT NULL AS is_payment_allowed,
-    old_rule.dt_occurence AS dt_occurence_old,
     DATE(so.dt_sale_agreement_signed) AS dt_sale_agreement_signed,
     ad.dt_paid_last_update AS dt_total_cash_flow_amount_last_updated,
     ad.dt_first_full_down_payment_event AS dt_occurence
@@ -102,8 +70,5 @@ LEFT JOIN
     cash_flow AS cf_total
         ON cf_total.sk_offer = ad.id_offer
         AND cf_total.dt_created = ad.dt_paid_last_update
-LEFT JOIN
-    old_dt_occurence_rule AS old_rule
-        ON old_rule.id_external_offer = so.id_offer
 WHERE
     so.dt_sale_agreement_signed IS NOT NULL
