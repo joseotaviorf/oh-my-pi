ed85e5870245b3f18013c537d180add4e99f7a62
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2023-07-05 18:36:42 -0300

    fix att filter on dw (#11644)

--- dags/fintech/dw_atta/queries/dw/fact_pre_analysis_proposal_flow.sql
+++ dags/fintech/dw_atta/queries/dw/fact_pre_analysis_proposal_flow.sql
@@ -229,7 +229,8 @@ pre_analysis_proposal_flow AS (
                     LEFT JOIN
                         datalake_sales_flow_clean.negotiation AS neg ON ds.id_sales_flow = neg.id_sales_flow AND ds.ts_updated = neg.ts_updated
                     WHERE
-                        pp.id_proposal IS NULL),
+                        pr.id_product IN (1,11)
+                        OR pp.id_proposal IS NULL),
 
 ongoing_proposals AS (
                         SELECT
