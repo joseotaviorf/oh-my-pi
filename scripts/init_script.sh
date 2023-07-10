ba3ad816cde10c42b4fe3199bbe9b862f934306b
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2023-07-10 16:58:28 -0300

    [DIN] Reorganized order of steps inside init_script.sh (#11646)
    * Reorganized order of steps inside init_script.sh
    
    * Empty commit to unstuck CD step
    
    ---------
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- scripts/init_script.sh
+++ scripts/init_script.sh
@@ -1,17 +1,9 @@
 #! /bin/bash
 
-echo "BEGIN: Modify Spark config settings"
+echo "BEGIN: Install Spark external JARs with Maven"
 apt-get install maven -yqq
 
 spark_jars_path="/databricks/jars"
-spark_driver_config_file="$DB_HOME/driver/conf/00-spark-plugin-driver-defaults.conf"
-spark_driver_config_content="[driver] {
-    \"spark.plugins\" = \"ch.cern.CloudFSMetrics,ch.cern.CgroupMetrics,br.com.quintoandar.GangliaMetrics\",
-    \"spark.cernSparkPlugin.cloudFsName\" = \"s3a\",
-    \"spark.cernSparkPlugin.registerOnDriver\" = \"true\",
-    \"spark.extraListeners\" = \"ch.cern.sparkmeasure.FlightRecorderStageMetrics\"
-}
-"
 jars=(\
     ch.cern.sparkmeasure:spark-plugins_2.12:0.2\
     ch.cern.sparkmeasure:spark-measure_2.12:0.21\
@@ -22,15 +14,22 @@ for artifact in "${jars[@]}"
         mvn dependency:get -q -Dartifact="$artifact"
         mvn dependency:copy -q -Dartifact="$artifact" -DoutputDirectory="$spark_jars_path"
     done
+echo "END: Install Spark external JARs with Maven"
 
+echo "BEGIN: Modify Spark config settings"
+spark_driver_config_file="$DB_HOME/driver/conf/00-spark-plugin-driver-defaults.conf"
+spark_driver_config_content="[driver] {
+    \"spark.plugins\" = \"ch.cern.CloudFSMetrics,ch.cern.CgroupMetrics,br.com.quintoandar.GangliaMetrics\",
+    \"spark.cernSparkPlugin.cloudFsName\" = \"s3a\",
+    \"spark.cernSparkPlugin.registerOnDriver\" = \"true\",
+    \"spark.extraListeners\" = \"ch.cern.sparkmeasure.FlightRecorderStageMetrics\"
+}
+"
 echo "$spark_driver_config_content" | tee $spark_driver_config_file 1>/dev/null
 echo "END: Modify Spark config settings"
 
-echo "BEGIN: Installing QuintoAndar internal libs"
+echo "BEGIN: Install QuintoAndar internal libs"
 /databricks/python/bin/pip install -q awscli
-
-# Here, the script differentiate between environments
-# using different buckets.
 
 aws s3 cp ${ARTIFACTS_BUCKET}/jars/spark-cluster-metrics_2.12-0.1-SNAPSHOT.jar $spark_jars_path/spark-cluster-metrics_2.12-0.1-SNAPSHOT.jar
 aws s3 cp ${ARTIFACTS_BUCKET}/bi-etl-ejuice/bi_etl_ejuice-latest-py3-none-any.whl /bi_etl_ejuice-latest-py3-none-any.whl
@@ -40,4 +39,4 @@ aws s3 cp ${ARTIFACTS_BUCKET}/inmetro/inmetro-2.3.0-py3-none-any.whl /inmetro-2.
 /databricks/python/bin/pip install -q /bi_etl_ejuice-latest-py3-none-any.whl
 /databricks/python/bin/pip install -q /quintoandar_logger-0.8.0-py3-none-any.whl
 /databricks/python/bin/pip install -q '/inmetro-2.3.0-py3-none-any.whl[pydeequ]'
-echo "END: Installing QuintoAndar internal libs"
+echo "END: Install QuintoAndar internal libs"
