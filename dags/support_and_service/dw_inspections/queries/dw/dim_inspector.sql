b2637a5d189d41c9dae6afd4f009f0098e666658
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-07-10 11:39:10 -0300

    [AES&S] Fixing data clearing of Inspector Control gsheets (#11635)
    * add reverse
    
    * add dependencies
    
    * fix dependencies
    
    * fix metric dependencies
    
    * Update dependencies.yaml
    
    * fix
    
    * fix
    
    * fix
    
    * Update bietlejuice_dependency_helper.py
    
    * fix: reverse key_value
    
    * fix dependencies
    
    * fix: clean inspectors_control
    
    * Update inspector_performance.sql
    
    * remove reverse
    
    * remove inspector_performance
    
    * fix
    
    * Revert "remove inspector_performance"
    
    This reverts commit 6af887107af4c3e52112f28b98d4891410f63f98.
    
    * Revert remove inspector_performance
    
    This reverts commit e7287a00f6e1ab987d7d02f7812c49bd883520b4.
    
    * fix
    
    * Update inspector_performance.sql
    
    * Update inspectors_control.yml
    
    * review suggestions
    
    ---------
    
    Co-authored-by: André Melo <47090012+Andre-L-Melo@users.noreply.github.com>

--- dags/support_and_service/dw_inspections/queries/dw/dim_inspector.sql
+++ dags/support_and_service/dw_inspections/queries/dw/dim_inspector.sql
@@ -1,35 +1,17 @@
 SELECT DISTINCT
     COALESCE(id_inspector, -1) AS sk_inspector,
-    IF(inspector_name = '', NULL, inspector_name) AS inspector_name,
-    IF(insperctor_email = '', NULL, insperctor_email) AS inspector_email,
-    IF(inspector_corporate_name = '', NULL, inspector_corporate_name) AS inspector_corporate_name,
-    IF(beneficiary_name = '', NULL, beneficiary_name) AS beneficiary_name,
-    IF(employee_contract_type = '', NULL, employee_contract_type) AS employee_contract_type,
-    IF(company = '', NULL, company) AS company,
-    IF(status = '', NULL, status) AS status,
-    IF(operating_city = '', NULL, operating_city) AS operating_city,
-    IF(operating_regions = '', NULL, operating_regions) AS operating_regions,
-    IF(transportation_type = '', NULL, transportation_type) AS transportation_type,
-    CASE
-        WHEN CHAR_LENGTH(dt_start) = 8 THEN to_date(dt_start, 'M/d/yyyy')
-        WHEN CHAR_LENGTH(dt_start) = 9
-            AND CHAR_LENGTH(SPLIT(dt_start, '/')[0]) = 1
-            THEN to_date(dt_start, 'M/dd/yyyy')
-        WHEN CHAR_LENGTH(dt_start) = 9
-            AND CHAR_LENGTH(SPLIT(dt_start, '/')[0]) = 2
-            THEN to_date(dt_start, 'MM/d/yyyy')
-        WHEN CHAR_LENGTH(dt_start) = 10 THEN to_date(dt_start, 'MM/dd/yyyy')
-    END AS dt_start,
-    CASE
-        WHEN CHAR_LENGTH(dt_end) = 8 THEN to_date(dt_end, 'M/d/yyyy')
-        WHEN CHAR_LENGTH(dt_end) = 9
-            AND CHAR_LENGTH(SPLIT(dt_end, '/')[0]) = 1
-            THEN to_date(dt_end, 'M/dd/yyyy')
-        WHEN CHAR_LENGTH(dt_end) = 9
-            AND CHAR_LENGTH(SPLIT(dt_end, '/')[0]) = 2
-            THEN to_date(dt_end, 'MM/d/yyyy')
-        WHEN CHAR_LENGTH(dt_end) = 10 THEN to_date(dt_end, 'MM/dd/yyyy')
-    END AS dt_end,
+    inspector_name,
+    inspector_email,
+    inspector_corporate_name,
+    beneficiary_name,
+    employee_contract_type,
+    company,
+    status,
+    operating_city,
+    operating_regions,
+    transportation_type,
+    dt_start,
+    dt_end,
     NOW() AS ts_load
 FROM
     datalake_gsheets_clean.inspectors_control
--- dags/support_and_service/gsheets_support_and_service/queries/clean/inspectors_control.sql
+++ dags/support_and_service/gsheets_support_and_service/queries/clean/inspectors_control.sql
@@ -1,27 +1,46 @@
 SELECT
     CAST(sk_inspector AS BIGINT) AS id_inspector,
-    agenda_type,
-    beneficiary_document,
-    beneficiary_name,
-    company,
-    employee_contract_type,
-    inspector_address,
-    inspector_cnpj,
-    inspector_corporate_name,
-    inspector_cpf,
-    inspector_name,
-    inspector_phone,
-    inspector_rg,
-    inspector_secondary_phone,
-    insperctor_email,
-    main_operating_region,
-    operating_city,
-    operating_regions,
-    registry_type,
-    salary,
-    status,
-    transportation_type,
-    dt_start,
-    dt_end
+    IF(agenda_type = '', NULL, agenda_type) AS agenda_type,
+    IF(beneficiary_document = '', NULL, beneficiary_document) AS beneficiary_document,
+    IF(beneficiary_name = '', NULL, beneficiary_name) AS beneficiary_name,
+    IF(company = '', NULL, company) AS company,
+    IF(employee_contract_type = '', NULL, employee_contract_type) AS employee_contract_type,
+    IF(inspector_address = '', NULL, inspector_address) AS inspector_address,
+    IF(inspector_cnpj = '', NULL, inspector_cnpj) AS inspector_cnpj,
+    IF(inspector_corporate_name = '', NULL, inspector_corporate_name) AS inspector_corporate_name,
+    IF(inspector_cpf = '', NULL, inspector_cpf) AS inspector_cpf,
+    IF(inspector_name = '', NULL, inspector_name) AS inspector_name,
+    IF(inspector_phone = '', NULL, inspector_phone) AS inspector_phone,
+    IF(inspector_rg = '', NULL, inspector_rg) AS inspector_rg,
+    IF(inspector_secondary_phone = '', NULL, inspector_secondary_phone) AS inspector_secondary_phone,
+    IF(insperctor_email = '', NULL, insperctor_email) AS inspector_email,
+    IF(main_operating_region = '', NULL, main_operating_region) AS main_operating_region,
+    IF(operating_city = '', NULL, operating_city) AS operating_city,
+    IF(operating_regions = '', NULL, operating_regions) AS operating_regions,
+    IF(registry_type = '', NULL, registry_type) AS registry_type,
+    IF(salary = '', NULL, salary) AS salary,
+    IF(status = '', NULL, status) AS status,
+    IF(transportation_type = '', NULL, transportation_type) AS transportation_type,
+    CASE
+        WHEN CHAR_LENGTH(dt_start) = 8 THEN to_date(dt_start, 'M/d/yyyy')
+        WHEN CHAR_LENGTH(dt_start) = 9
+            AND CHAR_LENGTH(SPLIT(dt_start, '/')[0]) = 1
+            THEN to_date(dt_start, 'M/dd/yyyy')
+        WHEN CHAR_LENGTH(dt_start) = 9
+            AND CHAR_LENGTH(SPLIT(dt_start, '/')[0]) = 2
+            THEN to_date(dt_start, 'MM/d/yyyy')
+        WHEN CHAR_LENGTH(dt_start) = 10 THEN to_date(dt_start, 'MM/dd/yyyy')
+    END AS dt_start,
+    CASE
+        WHEN CHAR_LENGTH(dt_end) = 8 THEN to_date(dt_end, 'M/d/yyyy')
+        WHEN CHAR_LENGTH(dt_end) = 9
+            AND CHAR_LENGTH(SPLIT(dt_end, '/')[0]) = 1
+            THEN to_date(dt_end, 'M/dd/yyyy')
+        WHEN CHAR_LENGTH(dt_end) = 9
+            AND CHAR_LENGTH(SPLIT(dt_end, '/')[0]) = 2
+            THEN to_date(dt_end, 'MM/d/yyyy')
+        WHEN CHAR_LENGTH(dt_end) = 10 THEN to_date(dt_end, 'MM/dd/yyyy')
+    END AS dt_end,
+    NOW() AS ts_load
 FROM
     datalake_gsheets_raw.inspectors_control
