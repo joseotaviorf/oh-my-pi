db97fcd68ce43c1541b40f871f9dd39d022b7824
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2023-07-11 16:54:09 -0300

    Replaced kwargs by args and added 1 day to ds (#11684)
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- dags/fintech/enrich_fintech_snapshot/enrich_fintech_snapshot.py
+++ dags/fintech/enrich_fintech_snapshot/enrich_fintech_snapshot.py
@@ -15,7 +15,9 @@ from bietlejuice.base.airflow.task_groups.datalake_task_group import DatalakeTas
 from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.airflow.helpers.task_flow_helper import TaskFlowHelper
 from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
-from bietlejuice.base.airflow.dag_builders.main_builder.short_circuit_functions.dag_run_date_validators import DAGRunDateValidators
+from bietlejuice.base.airflow.dag_builders.main_builder.short_circuit_functions.dag_run_date_validators import (
+    DAGRunDateValidators,
+)
 
 LOCAL_TZ = pendulum.timezone("America/Sao_Paulo")
 MAIN_START_DATE = datetime(2020, 7, 1, 0, 0, 0, tzinfo=LOCAL_TZ)
@@ -36,7 +38,9 @@ base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 default_libraries = config_service.get_config("default_libraries")
 
-CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_general_photon_cluster")
+CLUSTER_DESCRIPTION = config_service.get_config(
+    "databricks_10_4_med_general_photon_cluster"
+)
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
@@ -76,7 +80,7 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
 skip_run_task = ShortCircuitOperator(
     task_id=f"check-day-to-skip-execution",
     python_callable=DAGRunDateValidators.check_is_first_business_day_of_month,
-    op_kwargs={"dag_execution_date": "{{ds}}"},
+    op_args=["{{ macros.ds_add(ds, 1) }}"],
 )
 
 datalake_task_group = DatalakeTaskGroup(
@@ -98,5 +102,9 @@ enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
 )
 
 
-chain(skip_run_task, create_cluster_task, DatalakeTaskGroup.all_first_tasks(enrich_task_groups))
+chain(
+    skip_run_task,
+    create_cluster_task,
+    DatalakeTaskGroup.all_first_tasks(enrich_task_groups),
+)
 chain(DatalakeTaskGroup.all_last_tasks(enrich_task_groups), terminate_cluster_task)
