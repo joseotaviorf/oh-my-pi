c71fca279eea006a1cb5a1244cd71015d39d4d3e
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2023-07-19 12:24:41 -0300

    [AEFR-2704] Change column name and add total_tickets_distributed and total_tickets_proportional (#11744)
    * Change column name and add total_tickets_distributed and total_tickets_proportional

--- dags/for_rent/metric_rent__tickets/queries/metric/total_tickets.sql
+++ dags/for_rent/metric_rent__tickets/queries/metric/total_tickets.sql
@@ -1,44 +1,77 @@
-WITH base_tickets as (
-  SELECT 
-    DATE_TRUNC('month', ft.ts_solved) AS started_month,
-    dt.theme AS contact_theme_tag,
-    dt.theme_detail AS contact_theme_detail,
-    dd.front_or_back AS ticket_type,
-    dt.journey,
-    dt.sub_journey,
-    dt.line_owner,
-    dd.team,
-    COUNT(DISTINCT 
-        CASE 
-            WHEN dd.front_or_back = 'back' THEN ft.sk_ticket
-            WHEN dd.front_or_back = 'front' AND ft.channel = 'email' THEN ft.sk_ticket
-            WHEN dd.front_or_back = 'front' AND ft.channel = 'chat' AND dc.direction = 'inbound' THEN ft.sk_ticket
-            WHEN dd.front_or_back = 'front' AND ft.channel = 'call' AND dc.direction = 'inbound' THEN ft.sk_ticket
-    END) AS tickets
-  FROM 
-    dw_customer_support.fact_ticket AS ft
-  LEFT JOIN 
-    dw_customer_support.dim_department AS dd 
-      ON ft.sk_main_department = dd.sk_department
-  LEFT JOIN
-    dw_customer_support.dim_taxonomy AS dt
-      ON dt.sk_taxonomy = ft.sk_taxonomy
-  LEFT JOIN 
-    dw_customer_support.dim_channel AS dc 
-      ON ft.sk_channel = dc.sk_channel
-  WHERE 
-    ft.main_department NOT IN ('Offboarding Reparos [OFF] [POS] [BACK]', 'Offboarding pré saída [OFF] [POS] [BACK]', 'Proteção QuintoAndar [OFF] [POS] [BACK]', 'Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão 1 [OFF] [POS] [BACK]')
-    AND ft.ts_solved >= CAST('2022-01-01' AS DATE)
-    AND dd.front_or_back IN ('back', 'front')
-    AND dd.journey_step NOT IN ('Compra e Venda', 'Cross', 'Rental Manager')
-    AND dd.team <> 'Ong Back'
-    AND dd.area = 'CX'
-  GROUP BY
-    1,2,3,4,5,6,7,8
+WITH 
+base_tickets AS (
+    SELECT 
+      DATE(ft.ts_solved) AS dt_started,
+      dt.theme AS contact_theme_tag,
+      dt.theme_detail AS contact_theme_detail,
+      dd.front_or_back AS ticket_type,
+      dt.journey,
+      dt.sub_journey,
+      dt.line_owner,
+      dd.team,
+      COUNT(DISTINCT 
+          CASE 
+              WHEN dd.front_or_back = 'back' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'email' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'chat' AND dc.direction = 'inbound' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'call' AND dc.direction = 'inbound' THEN ft.sk_ticket
+      END) AS tickets
+    FROM 
+      dw_customer_support.fact_ticket AS ft
+    LEFT JOIN 
+      dw_customer_support.dim_department AS dd 
+        ON ft.sk_main_department = dd.sk_department
+    LEFT JOIN
+      dw_customer_support.dim_taxonomy AS dt
+        ON dt.sk_taxonomy = ft.sk_taxonomy
+    LEFT JOIN 
+      dw_customer_support.dim_channel AS dc 
+        ON ft.sk_channel = dc.sk_channel
+    WHERE 
+      ft.main_department NOT IN ('Offboarding Reparos [OFF] [POS] [BACK]', 'Offboarding pré saída [OFF] [POS] [BACK]', 'Proteção QuintoAndar [OFF] [POS] [BACK]', 'Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão 1 [OFF] [POS] [BACK]')
+      AND ft.ts_solved >= CAST('2022-01-01' AS DATE)
+      AND dd.front_or_back IN ('back', 'front')
+      AND dd.journey_step NOT IN ('Compra e Venda', 'Cross', 'Rental Manager')
+      AND dd.team <> 'Ong Back'
+      AND dd.area = 'CX'
+    GROUP BY
+      1,2,3,4,5,6,7,8
+),
+missing_tickets AS (
+    SELECT 
+      DATE(ft.ts_solved) AS dt_started,
+      dd.front_or_back AS ticket_type,
+      COUNT(DISTINCT 
+          CASE 
+              WHEN dd.front_or_back = 'back' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'email' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'chat' AND dc.direction = 'inbound' THEN ft.sk_ticket
+              WHEN dd.front_or_back = 'front' AND ft.channel = 'call' AND dc.direction = 'inbound' THEN ft.sk_ticket
+      END) AS missing_theme_tickets
+    FROM 
+      dw_customer_support.fact_ticket AS ft
+    LEFT JOIN 
+      dw_customer_support.dim_department AS dd 
+        ON ft.sk_main_department = dd.sk_department
+    LEFT JOIN
+      dw_customer_support.dim_taxonomy AS dt
+        ON dt.sk_taxonomy = ft.sk_taxonomy
+    LEFT JOIN 
+      dw_customer_support.dim_channel AS dc 
+        ON ft.sk_channel = dc.sk_channel
+    WHERE 
+      ft.main_department NOT IN ('Offboarding Reparos [OFF] [POS] [BACK]', 'Offboarding pré saída [OFF] [POS] [BACK]', 'Proteção QuintoAndar [OFF] [POS] [BACK]', 'Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão 1 [OFF] [POS] [BACK]')
+      AND ft.ts_solved >= CAST('2022-01-01' AS DATE)
+      AND dd.front_or_back IN ('back', 'front')
+      AND dt.theme_detail IS NULL
+      AND dd.team <> 'Ong Back'
+      AND dd.area = 'CX'
+    GROUP BY
+      1,2
 ),
 abandoned_calls AS (
     SELECT
-        DATE_TRUNC('month', frc.ts_created) AS started_month,
+        DATE(frc.ts_created) AS dt_started,
         'Abandonado' AS contact_theme_tag,
         'Abandonado' AS contact_theme_detail,
         dd.front_or_back AS ticket_type,
@@ -68,21 +101,46 @@ final_table AS (
     SELECT * FROM base_tickets
     UNION ALL 
     SELECT * FROM abandoned_calls
+),
+base_themes AS (
+  SELECT
+    dt_started,
+    COUNT(DISTINCT contact_theme_detail, ticket_type) AS qtd_theme_details,
+    SUM(tickets) AS qt_tickets
+  FROM 
+    final_table
+  GROUP BY 1
 )
 SELECT
-    date(started_month) AS started_month,
-    contact_theme_tag,
-    contact_theme_detail,
-    ticket_type,
-    journey,
-    sub_journey,
-    line_owner,
-    team,
-    SUM(CASE WHEN ticket_type = 'front' AND contact_theme_tag = 'Abandonado' THEN tickets END) AS total_abandoned_calls,
-    SUM(tickets) AS total_tickets
+    ft.dt_started,
+    ft.contact_theme_tag,
+    ft.contact_theme_detail,
+    ft.ticket_type,
+    ft.journey,
+    ft.sub_journey,
+    ft.line_owner,
+    ft.team,
+    SUM(IF(ft.ticket_type = 'front' AND ft.contact_theme_tag = 'Abandonado', ft.tickets, 0)) AS total_abandoned_calls,
+    SUM(ft.tickets) AS total_tickets_identified,
+    CAST((SUM(ft.tickets)/bt.qt_tickets) * mt.missing_theme_tickets AS NUMERIC(12,2)) AS total_tickets_distributed,
+    CAST(SUM(ft.tickets) + ((SUM(ft.tickets)/bt.qt_tickets) * mt.missing_theme_tickets) AS NUMERIC(12,2)) AS total_tickets_proportional
 FROM 
-    final_table 
+  final_table AS ft
+JOIN
+  base_themes AS bt
+    ON bt.dt_started = ft.dt_started
+LEFT JOIN
+  missing_tickets AS mt
+    ON mt.dt_started = ft.dt_started
+      AND mt.ticket_type = ft.ticket_type
 GROUP BY
-    1,2,3,4,5,6,7,8
-ORDER BY 
-    1 DESC 
+    ft.dt_started,
+    ft.contact_theme_tag,
+    ft.contact_theme_detail,
+    ft.ticket_type,
+    ft.journey,
+    ft.sub_journey,
+    ft.line_owner,
+    ft.team, 
+    bt.qt_tickets, 
+    mt.missing_theme_tickets
