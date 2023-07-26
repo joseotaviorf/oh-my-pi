b56dcafd35b48d72a84d7939fd1917c052926f5e
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2023-07-26 17:35:42 -0300

    [AEFF][VELO] Fix velo Propose Timestamps (#11814)

--- dags/fintech/enrich_velo/queries/enrich/propose.sql
+++ dags/fintech/enrich_velo/queries/enrich/propose.sql
@@ -523,16 +523,16 @@ SELECT DISTINCT
     COALESCE(DATE(c.ts_began), old.dt_contract_started) AS dt_contract_started,
     COALESCE(COALESCE(DATE(pcd.ts_ended), c.ts_done), old.dt_ended) AS dt_ended,
     COALESCE(p.ts_inserted, old.ts_propose_started) AS ts_propose_started,
-    COALESCE(wndd.ts_waiting_new_docs, old.ts_waiting_new_docs) AS ts_waiting_new_docs,
-    COALESCE(esd.ts_evaluation_started, old.ts_evaluation_started) AS ts_evaluation_started,
-    COALESCE(rd.ts_rejected, old.ts_rejected) AS ts_rejected,
-    COALESCE(ssd.ts_sign_started, old.ts_sign_started) AS ts_sign_started,
-    COALESCE(psd.ts_signed, old.ts_signed) AS ts_signed,
-    COALESCE(pd.ts_paid, old.ts_paid) AS ts_paid,
-    COALESCE(ad.ts_activation, old.ts_activation) AS ts_activation,
-    COALESCE(sed.ts_secured, old.ts_secured) AS ts_secured,
-    COALESCE(aad.ts_activation_analysis, old.ts_activation_analysis) AS ts_activation_analysis,
-    COALESCE(spd.ts_secure_pending, old.ts_secure_pending) AS ts_secure_pending
+    COALESCE(old.ts_waiting_new_docs, wndd.ts_waiting_new_docs) AS ts_waiting_new_docs,
+    COALESCE(old.ts_evaluation_started, esd.ts_evaluation_started) AS ts_evaluation_started,
+    COALESCE(old.ts_rejected, rd.ts_rejected) AS ts_rejected,
+    COALESCE(old.ts_sign_started, ssd.ts_sign_started) AS ts_sign_started,
+    COALESCE(old.ts_signed, psd.ts_signed) AS ts_signed,
+    COALESCE(old.ts_paid, pd.ts_paid) AS ts_paid,
+    COALESCE(old.ts_activation, ad.ts_activation) AS ts_activation,
+    COALESCE(old.ts_secured, sed.ts_secured) AS ts_secured,
+    COALESCE(old.ts_activation_analysis, aad.ts_activation_analysis) AS ts_activation_analysis,
+    COALESCE(old.ts_secure_pending, spd.ts_secure_pending) AS ts_secure_pending
 FROM
     datalake_rental_guarantee_platform_clean.propose AS p
 LEFT JOIN
