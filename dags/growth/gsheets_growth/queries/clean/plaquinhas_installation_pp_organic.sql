d54dca2072b0ab0ba98cbd1fbde1fa737232d4a0
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2023-07-31 16:01:55 -0300

    Adding Installs Plaquinhas information from Organic PP into datalake (#11835)
    * Adding Installs Plaquinhas information from Organic PP into datalake
    
    * Fixing clumn name ticked+id to id_ticket in order to match name convention
    
    * Update dags/growth/gsheets_growth/queries/clean/plaquinhas_installation_pp_organico.sql
    
    Co-authored-by: Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
    
    * Update dags/growth/gsheets_growth/queries/clean/plaquinhas_installation_pp_organico.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Fixing table name to english in order to attend name convention
    
    ---------
    
    Co-authored-by: Uilson Bonfim <uilsonbonfim@BRSPPM-C02DXG7JMD6M.local>
    Co-authored-by: Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ dags/growth/gsheets_growth/queries/clean/plaquinhas_installation_pp_organic.sql
@@ -0,0 +1,8 @@
+SELECT
+    CAST(id_ticket AS INTEGER) AS id_ticket,
+    CAST(id_house AS INTEGER) AS id_house, 
+    client_type, 
+    business_context,
+    TO_DATE(dt_install, "dd/MM/yy") AS dt_install
+FROM
+    datalake_gsheets_raw.plaquinhas_installation_pp_organic
