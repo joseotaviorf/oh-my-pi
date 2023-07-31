a17be10af2e46caf8a4334b68ba082558382fa72
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2023-07-31 10:29:22 -0300

    fix date parse format (#11836)

--- dags/for_sale/gsheets_for_sale/queries/clean/agent_tiers_rounds.sql
+++ dags/for_sale/gsheets_for_sale/queries/clean/agent_tiers_rounds.sql
@@ -1,6 +1,6 @@
 SELECT
     CAST(round AS INTEGER) AS round,
-    TO_DATE(start_date, "dd/MM/YY") AS ts_start,
-    TO_DATE(end_date, "dd/MM/YY") AS ts_end
+    TO_DATE(start_date, "dd/MM/yy") AS ts_start,
+    TO_DATE(end_date, "dd/MM/yy") AS ts_end
 FROM
     datalake_gsheets_raw.agent_tiers_rounds
