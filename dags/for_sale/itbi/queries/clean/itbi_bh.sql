d0a7d22ff9fd4262802b488a32d0b086b9e04a0c
Guilherme Ventura <44004512+guivnt@users.noreply.github.com>
2023-08-02 15:59:38 -0300

    [DFORSALE] Propagating column that no longer exists (#11856)

--- dags/for_sale/itbi/queries/clean/itbi_bh.sql
+++ dags/for_sale/itbi/queries/clean/itbi_bh.sql
@@ -11,7 +11,7 @@ SELECT
   area_adquirida_unidades_somadas::FLOAT AS all_units_area_m2,
   fracao_ideal_adquirida::FLOAT AS ideal_fraction,
   valor_base_calculo::FLOAT AS adopted_calculation_basis,
-  valor_imposto_cobrado_sumarizado::FLOAT AS amount_tax_paid_summarized,
+  CAST(NULL AS FLOAT) AS amount_tax_paid_summarized,
   ano_construcao_unidade::INTEGER AS year_built,
   data_inclusao_transacao::DATE AS dt_transaction,
   dt_load,
