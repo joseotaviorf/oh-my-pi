619b1a642ac05454f9c5167dcbe574301ddf6750
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2023-08-07 14:47:23 -0300

    Grow 2273 (#11896)
    * Updating plaquinhas instalation target process
    
    * Fixing Target Extraction Rational
    
    ---------
    
    Co-authored-by: Uilson Bonfim <uilsonbonfim@BRSPPM-C02DXG7JMD6M.local>

--- dags/growth/gsheets_growth/queries/clean/plaquinhas_cover_targets.sql
+++ dags/growth/gsheets_growth/queries/clean/plaquinhas_cover_targets.sql
@@ -1,7 +1,8 @@
 SELECT
     NULLIF(business_context,'') AS business_context,
+    NULLIF(timeframe,'') AS timeframe,
     NULLIF(city_group,'') AS city_group,
-    NULLIF(FLOAT(ongoing_listings_target),'') AS ongoing_listings_target,
+    NULLIF(FLOAT(cover_target),'') AS cover_target,
     NULLIF(FLOAT(active_plaquinhas_target),'') AS active_plaquinhas_target,
     NULLIF(TO_DATE(date, "dd/MM/yy"),'') AS dt_target
 FROM
