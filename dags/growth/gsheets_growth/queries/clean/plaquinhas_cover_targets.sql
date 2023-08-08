925f8895274a2d40c08266bc522c6cc6ea8e485c
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2023-08-08 20:18:19 -0300

    Revert "Grow 2273 (#11896)" (#11915)
    This reverts commit 619b1a642ac05454f9c5167dcbe574301ddf6750.

--- dags/growth/gsheets_growth/queries/clean/plaquinhas_cover_targets.sql
+++ dags/growth/gsheets_growth/queries/clean/plaquinhas_cover_targets.sql
@@ -1,8 +1,7 @@
 SELECT
     NULLIF(business_context,'') AS business_context,
-    NULLIF(timeframe,'') AS timeframe,
     NULLIF(city_group,'') AS city_group,
-    NULLIF(FLOAT(cover_target),'') AS cover_target,
+    NULLIF(FLOAT(ongoing_listings_target),'') AS ongoing_listings_target,
     NULLIF(FLOAT(active_plaquinhas_target),'') AS active_plaquinhas_target,
     NULLIF(TO_DATE(date, "dd/MM/yy"),'') AS dt_target
 FROM
