17225f2f0748005fbe9388d3df1e66d989e64247
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-08-23 15:12:01 -0300

    adding custom cluster to enrich_property_tax (#12050)

--- dags/for_rent/enrich_property_tax/enrich_property_tax.py
+++ dags/for_rent/enrich_property_tax/enrich_property_tax.py
@@ -34,7 +34,7 @@ default_libraries = config_service.get_config("default_libraries")
 
 s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
 SPARK_JOBS_PATH = f"{s3_prefix}/spark_jobs/base/"
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("custom_cluster")
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
