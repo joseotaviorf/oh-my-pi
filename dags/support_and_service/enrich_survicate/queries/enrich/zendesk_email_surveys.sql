264135eeb1086261b881634874444bf6be0236e1
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-08-30 14:40:21 -0300

    [AES&S] Updating Survicate CSAT rules for Zendesk Email (#12082)
    * Update zendesk_email_surveys.sql
    
    * update zendesk surveys

--- dags/support_and_service/enrich_survicate/queries/enrich/zendesk_email_surveys.sql
+++ dags/support_and_service/enrich_survicate/queries/enrich/zendesk_email_surveys.sql
@@ -1,11 +1,18 @@
 SELECT
   id AS id_survey,
-  custom_attributes.ticket_id AS id_ticket,
+  CAST(COALESCE(custom_attributes.ticket_id, PARSE_URL(page_url, 'QUERY', 't_id')) AS BIGINT) AS id_ticket,
   id_visitor,
   response_uuid,
   visitor_uuid,
   answers[2].content AS user_comment,
-  CAST(answers[1].content AS INT) AS csat_score,
+  CASE
+    WHEN answers[1].survey_point.pretty_type = 'Smiley scale' AND answers[1].content = 'Extremely happy' THEN 5
+    WHEN answers[1].survey_point.pretty_type = 'Smiley scale' AND answers[1].content = 'Happy' THEN 4
+    WHEN answers[1].survey_point.pretty_type = 'Smiley scale' AND answers[1].content = 'Neutral' THEN 3
+    WHEN answers[1].survey_point.pretty_type = 'Smiley scale' AND answers[1].content = 'Unsatisfied' THEN 2
+    WHEN answers[1].survey_point.pretty_type = 'Smiley scale' AND answers[1].content = 'Extremely unsatisfied' THEN 1
+    ELSE CAST(answers[1].content AS INT)
+  END AS csat_score,
   LOWER(answers[0].content) = 'sim' AS is_solved,
   ts_first_seen,
   ts_first_response,
@@ -15,7 +22,10 @@ SELECT
 FROM
   datalake_survicate_clean.surveys
 WHERE
-  custom_attributes.ticket_id IS NOT NULL
+  (
+    custom_attributes.ticket_id IS NOT NULL
+    OR PARSE_URL(page_url, 'QUERY', 't_id') IS NOT NULL
+  )
   AND year = {year}
   AND month = {month}
   AND day = {day}
