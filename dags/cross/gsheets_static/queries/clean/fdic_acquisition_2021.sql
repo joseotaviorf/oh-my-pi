90a41feb704c84c05465b10541b87494f6232069
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-08-30 10:00:20 -0300

    [AEFR-3057] Adding acquisition FDIC gsheets (#12076)
    * adding acquisition 2021 and 2022 and 2023

---
+++ dags/cross/gsheets_static/queries/clean/fdic_acquisition_2021.sql
@@ -0,0 +1,19 @@
+SELECT
+    CAST(idtitulovx AS BIGINT) AS id_vx_security,
+    cedentecnpjcpf AS assignor_cnpj,
+    cedentenome AS assignor_name,
+    sacadocnpjcpf AS withdraw_cnpj_cpf,
+    sacadonome AS withdraw_name,
+    tipo AS type,
+    tipoativo AS asset_type,
+    campochave AS key_field,
+    NULLIF(cmc7, "") AS cmc7,
+    numerotitulo AS security_number,
+    CAST(valoraquisicao AS FLOAT) AS acquisition_value,
+    CAST(valornominal AS FLOAT) AS nominal_value,
+    CAST(numeroboleto AS BIGINT) AS invoice_number,
+    TO_DATE(dataemissao, "d/M/y") AS dt_emission,
+    TO_DATE(dataaquisicao, "d/M/y") AS dt_acquisition,
+    TO_DATE(datavencimento, "d/M/y") AS dt_due
+FROM
+    datalake_gsheets_raw.fdic_acquisition_2021
