a03d2f1ca805d1f6d7da823032d9b4cad178ddc1
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-08-30 19:06:08 -0300

    Aeff 1759 credit debit condominium divergence recon (#12110)
    * fixed missing contract expenses in condominium divergence recomn query
    
    * Empty commit

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -145,7 +145,8 @@ LEFT JOIN
 WHERE
     c.country_code = 'BR'
     AND c.status IN ('Ativo','Finalizado')
-    AND ie.from_account_type IN ('contract', 'tenant','landlord')
+    AND ( (ie.from_account_type IN ('contract', 'tenant','landlord')) OR 
+        (ie.from_account_type = 'contract expenses' AND ie.entry_type in ('condominium fine', 'condominium 5A paid')))
     AND ( (ie.to_account_type IN ('contract', 'tenant','landlord')) OR
         (ie.to_account_type = 'quinto andar' AND ie.entry_type in ('condominium' , 'condominium usage', 'condominium defaulting', 'condominium fine')) OR
         (ie.to_account_type = 'contract expenses' AND ie.entry_type in ('postponement')))
