c22dfb9f86345ea7fefa5f8c2d6dbed9f0fa5fa2
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2023-08-31 14:50:59 -0300

    [AEFR-2981] Creating snapshot for coincident metrics (#12085)
    * creating monthly-weekly views
    
    * updating query
    
    * updating filters
    
    * updates
    
    * casting
    
    * fixing between
    
    * metadata
    
    * fixes
    
    * fixes
    
    * indentation
    
    * fixing
    
    * updates
    
    * updating target name
    
    * updates
    
    * fixing
    
    * fixes
    
    * fixing
    
    * undo
    
    * Update dags/for_rent/dw_rent_demand_events_snapshot/metadata/dw/coincident_events_time_variations.yml
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
    
    * Update dags/for_rent/dw_rent_demand_events_snapshot/metadata/dw/coincident_events_time_variations.yml
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
    
    * fix
    
    * Update dags/for_rent/dw_rent_demand_events_snapshot/metadata/dw/coincident_events_time_variations.yml
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
    
    * Update dags/for_rent/dw_rent_demand_events_snapshot/metadata/dw/coincident_events_time_variations.yml
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
    
    ---------
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>

---
+++ dags/for_rent/dw_rent_demand_events_snapshot/queries/dw/coincident_events_time_variations.sql
@@ -0,0 +1,430 @@
+WITH metrics_d1 AS (
+    SELECT
+        'D-1' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        NULL AS diff_vb,
+        SUM(visits_completed) AS visits_completed,
+        NULL AS diff_vc,
+        SUM(offers_submitted) AS offers_submitted,
+        NULL AS diff_os,
+        SUM(offers_accepted) AS offers_accepted,
+        NULL AS diff_oa,
+        SUM(evaluation_started) AS evaluation_started,
+        NULL AS diff_es,
+        SUM(evaluation_positive) AS evaluation_positive,
+        NULL AS diff_ep,
+        SUM(documentation_sent) AS documentation_sent,
+        NULL AS diff_ds,
+        SUM(credit_approved) AS credit_approved,
+        NULL AS diff_ca,
+        SUM(contracts_signed) AS contracts_signed,
+        NULL AS diff_cs,
+        DATE(dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND dt_event = DATE_ADD(DATE(NOW()), -1)
+    GROUP BY 1, 2, 3, 22, 23, 24, 25, 26, 27
+),
+metrics_5w AS (
+    SELECT
+        'W1-W5' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('week', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND DATE_TRUNC('week', dt_event)
+            BETWEEN DATE_TRUNC('week', DATE_ADD(NOW(), -35))
+                AND DATE_TRUNC('week', DATE_ADD(NOW(), -7)) -- Between -1 to -5 weeks
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+divergences_5w AS (
+    SELECT
+        'W1-W5' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('week', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot AS r
+    WHERE
+        DATE(ts_snapshot) = DATE_TRUNC('week', DATE_ADD(dt_event, 7)) -- The snapshot should be from the week start of the following week
+        AND DATE_TRUNC('week', dt_event)
+            BETWEEN DATE_TRUNC('week', DATE_ADD(NOW(), -35))
+                AND DATE_TRUNC('week', DATE_ADD(NOW(), -7)) -- Between -1 to -5 weeks
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+final_5w AS (
+    SELECT
+        m.target_type,
+        m.business_type,
+        m.rent_flow_origin,
+        m.visits_booked,
+        m.visits_booked-d.visits_booked AS diff_vb,
+        m.visits_completed,
+        m.visits_completed-d.visits_completed AS diff_vc,
+        m.offers_submitted,
+        m.offers_submitted-d.offers_submitted AS diff_os,
+        m.offers_accepted,
+        m.offers_accepted-d.offers_accepted AS diff_oa,
+        m.evaluation_started,
+        m.evaluation_started-d.evaluation_started AS diff_es,
+        m.evaluation_positive,
+        m.evaluation_positive-d.evaluation_positive AS diff_ep,
+        m.documentation_sent,
+        m.documentation_sent-d.documentation_sent AS diff_ds,
+        m.credit_approved,
+        m.credit_approved-d.credit_approved AS diff_ca,
+        m.contracts_signed,
+        m.contracts_signed-d.contracts_signed AS diff_cs,
+        DATE(m.dt_target) AS dt_target,
+        m.dt_snapshot,
+        m.country_code,
+        m.year,
+        m.month,
+        m.day
+    FROM
+        metrics_5w AS m
+    JOIN
+        divergences_5w AS d
+            ON d.country_code = m.country_code
+            AND d.dt_target = m.dt_target
+            AND d.business_type = m.business_type
+            AND d.rent_flow_origin = m.rent_flow_origin
+),
+metrics_m AS (
+    SELECT
+        'LM' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('month', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND DATE_TRUNC('month', dt_event) = DATE_TRUNC('month', ADD_MONTHS(NOW(), -1))  -- Gets the events truncated by the beginning of the previous month
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+divergences_m AS (
+    SELECT
+        'LM' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('month', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot AS r
+    WHERE
+        DATE(ts_snapshot) = LAST_DAY(DATE_TRUNC('month', DATE(ADD_MONTHS(NOW(), -1))))  -- Gets the snapshot of the last day of the previous month
+        AND DATE(DATE_TRUNC('month', dt_event)) = DATE_TRUNC('month', (ADD_MONTHS(NOW(), -1)))  -- Gets the events truncated by the previous month start date
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+final_m AS (
+    SELECT
+        m.target_type,
+        m.business_type,
+        m.rent_flow_origin,
+        m.visits_booked,
+        m.visits_booked-d.visits_booked AS diff_vb,
+        m.visits_completed,
+        m.visits_completed-d.visits_completed AS diff_vc,
+        m.offers_submitted,
+        m.offers_submitted-d.offers_submitted AS diff_os,
+        m.offers_accepted,
+        m.offers_accepted-d.offers_accepted AS diff_oa,
+        m.evaluation_started,
+        m.evaluation_started-d.evaluation_started AS diff_es,
+        m.evaluation_positive,
+        m.evaluation_positive-d.evaluation_positive AS diff_ep,
+        m.documentation_sent,
+        m.documentation_sent-d.documentation_sent AS diff_ds,
+        m.credit_approved,
+        m.credit_approved-d.credit_approved AS diff_ca,
+        m.contracts_signed,
+        m.contracts_signed-d.contracts_signed AS diff_cs,
+        DATE(m.dt_target) AS dt_target,
+        m.dt_snapshot,
+        m.country_code,
+        m.year,
+        m.month,
+        m.day
+    FROM
+        metrics_m AS m
+    JOIN
+        divergences_m AS d
+            ON d.country_code = m.country_code
+            AND d.dt_target = m.dt_target
+            AND d.business_type = m.business_type
+            AND d.rent_flow_origin = m.rent_flow_origin
+),
+metrics_q AS (
+    SELECT
+        'LQ' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('quarter', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND DATE_TRUNC('quarter', dt_event) = DATE_TRUNC('quarter', ADD_MONTHS(NOW(), -3))  -- Gets the events truncated by the beginning of the previous quarter
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+divergences_q AS (
+    SELECT
+        'LQ' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('quarter', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot AS r
+    WHERE
+        DATE(ts_snapshot) = LAST_DAY(DATE_TRUNC('quarter', DATE(ADD_MONTHS(NOW(), -3))))    -- Gets the last day of the previous quarter
+        AND DATE(DATE_TRUNC('quarter', dt_event)) = DATE_TRUNC('quarter', (ADD_MONTHS(NOW(), -3)))  -- Gets the events truncated by the previous quarter start date
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+final_q AS (
+    SELECT
+        m.target_type,
+        m.business_type,
+        m.rent_flow_origin,
+        m.visits_booked,
+        m.visits_booked-d.visits_booked AS diff_vb,
+        m.visits_completed,
+        m.visits_completed-d.visits_completed AS diff_vc,
+        m.offers_submitted,
+        m.offers_submitted-d.offers_submitted AS diff_os,
+        m.offers_accepted,
+        m.offers_accepted-d.offers_accepted AS diff_oa,
+        m.evaluation_started,
+        m.evaluation_started-d.evaluation_started AS diff_es,
+        m.evaluation_positive,
+        m.evaluation_positive-d.evaluation_positive AS diff_ep,
+        m.documentation_sent,
+        m.documentation_sent-d.documentation_sent AS diff_ds,
+        m.credit_approved,
+        m.credit_approved-d.credit_approved AS diff_ca,
+        m.contracts_signed,
+        m.contracts_signed-d.contracts_signed AS diff_cs,
+        DATE(m.dt_target) AS dt_target,
+        m.dt_snapshot,
+        m.country_code,
+        m.year,
+        m.month,
+        m.day
+    FROM
+        metrics_q AS m
+    JOIN
+        divergences_q AS d
+            ON d.country_code = m.country_code
+            AND d.dt_target = m.dt_target
+            AND d.business_type = m.business_type
+            AND d.rent_flow_origin = m.rent_flow_origin
+),
+metrics_y AS (
+    SELECT
+        'LY' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('year', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        year,
+        month,
+        day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND DATE_TRUNC('year', dt_event) = DATE_TRUNC('year', ADD_MONTHS(NOW(), -12))   -- Gets the events truncated by the beginning of the previous year
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+divergences_y AS (
+    SELECT
+        'LY' AS target_type,
+        business_type,
+        rent_flow_origin,
+        SUM(visits_booked) AS visits_booked,
+        SUM(visits_completed) AS visits_completed,
+        SUM(offers_submitted) AS offers_submitted,
+        SUM(offers_accepted) AS offers_accepted,
+        SUM(evaluation_started) AS evaluation_started,
+        SUM(evaluation_positive) AS evaluation_positive,
+        SUM(documentation_sent) AS documentation_sent,
+        SUM(credit_approved) AS credit_approved,
+        SUM(contracts_signed) AS contracts_signed,
+        DATE_TRUNC('year', dt_event) AS dt_target,
+        DATE(ts_snapshot) AS dt_snapshot,
+        country_code,
+        r.year,
+        r.month,
+        r.day
+    FROM
+        dw_rent_snapshot.rent_demand_events_snapshot AS r
+    JOIN
+        dw_public.dim_date AS d
+            ON d.date = r.dt_event
+    WHERE
+        DATE(ts_snapshot) = MAKE_DATE(YEAR(d.last_year), 12, 31)   -- Gets the last day of the previous year
+        AND DATE(DATE_TRUNC('year', dt_event)) = MAKE_DATE(YEAR(d.last_year), 1, 1)   -- Gets the events truncated by the previous year start date
+    GROUP BY 1, 2, 3, 13, 14, 15, 16, 17, 18
+),
+final_y AS (
+    SELECT
+        m.target_type,
+        m.business_type,
+        m.rent_flow_origin,
+        m.visits_booked,
+        m.visits_booked-d.visits_booked AS diff_vb,
+        m.visits_completed,
+        m.visits_completed-d.visits_completed AS diff_vc,
+        m.offers_submitted,
+        m.offers_submitted-d.offers_submitted AS diff_os,
+        m.offers_accepted,
+        m.offers_accepted-d.offers_accepted AS diff_oa,
+        m.evaluation_started,
+        m.evaluation_started-d.evaluation_started AS diff_es,
+        m.evaluation_positive,
+        m.evaluation_positive-d.evaluation_positive AS diff_ep,
+        m.documentation_sent,
+        m.documentation_sent-d.documentation_sent AS diff_ds,
+        m.credit_approved,
+        m.credit_approved-d.credit_approved AS diff_ca,
+        m.contracts_signed,
+        m.contracts_signed-d.contracts_signed AS diff_cs,
+        DATE(m.dt_target) AS dt_target,
+        m.dt_snapshot,
+        m.country_code,
+        m.year,
+        m.month,
+        m.day
+    FROM
+        metrics_y AS m
+    JOIN
+        divergences_y AS d
+            ON d.country_code = m.country_code
+            AND d.dt_target = m.dt_target
+            AND d.business_type = m.business_type
+            AND d.rent_flow_origin = m.rent_flow_origin
+)
+SELECT *, NOW() AS ts_load FROM metrics_d1
+UNION ALL
+SELECT *, NOW() AS ts_load FROM final_5w
+UNION ALL
+SELECT *, NOW() AS ts_load FROM final_m
+UNION ALL
+SELECT *, NOW() AS ts_load FROM final_q
+UNION ALL
+SELECT *, NOW() AS ts_load FROM final_y
