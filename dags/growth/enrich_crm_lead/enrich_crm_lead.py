2aac26f6800f6a9774ebcfa411a31899617911c4
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2023-09-06 09:26:43 -0300

    [AES&S] Change enrich_crm_lead owner from S&S to Growth (#11276)
    * S&S -> growth
    
    * update documentation
    
    * update dag documentation
    
    * Revert "update dag documentation"
    
    This reverts commit 94e8e846906e1066ff0b8c829e015de4527d4dfd.

--- dags/growth/enrich_crm_lead/enrich_crm_lead.py
+++ dags/growth/enrich_crm_lead/enrich_crm_lead.py
@@ -46,7 +46,7 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 dag = DAG(
     dag_id=DAG_ID,
     default_args={
-        "owner": DAGOwnerEnum.DATA_SS,
+        "owner": DAGOwnerEnum.DATA_GROWTH,
         "wait_for_downstream": False,
         "depends_on_past": False,
     },
