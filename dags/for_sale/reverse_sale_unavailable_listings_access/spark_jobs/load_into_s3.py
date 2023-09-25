be161651854e00fc96b77cc3b50cb89814c56fe9
Guilherme Ventura <44004512+guivnt@users.noreply.github.com>
2023-09-25 09:39:48 -0300

    [DFORSALE-5779] Suspected Unavailable Listings Reverse ETL (#11787)

---
+++ dags/for_sale/reverse_sale_unavailable_listings_access/spark_jobs/load_into_s3.py
@@ -0,0 +1,119 @@
+import json
+import boto3
+import logging
+
+from typing import Tuple
+from datetime import datetime
+from argparse import ArgumentParser
+from http.client import HTTPException
+
+from bietlejuice.services import S3Service
+from bietlejuice.clients.db_clients import SparkClient
+
+from quintoandar_logger import QuintoAndarLogger
+
+JOB_NAME = "load_into_s3"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+s3_service = S3Service(boto3.resource("s3"))
+spark_client = SparkClient()
+
+def main():
+    bucket, database_name, table_name, execution_date = (
+        parse_arguments()
+    )
+
+    logger.info(
+        f"""m=__main__, bucket= {bucket}, database_name={database_name}, 
+        table_name={table_name}, execution_date={execution_date}"""
+    )
+
+    result = prepare_table(database_name, table_name, execution_date)
+    destination_path = create_s3_path(bucket, table_name, execution_date)
+    send_to_s3_bucket(result, table_name, destination_path)
+
+
+def parse_arguments() -> Tuple[str, str, str, datetime]:
+    
+    parser = ArgumentParser(description=JOB_NAME)
+
+    parser.add_argument("bucket", help="URL of the Reverse ETL destination bucket")
+    parser.add_argument("database_name", help="Name of the reverse etl schema")
+    parser.add_argument("table_name", help="Name of the table to be loaded")
+    parser.add_argument(
+        "execution_date", help="Date of the execution in the format YYYY-MM-DD"
+    )
+
+    args = parser.parse_args()
+
+    bucket = args.bucket
+    database_name = args.database_name
+    table_name = args.table_name
+    execution_date = datetime.fromisoformat(args.execution_date)
+
+    return bucket, database_name, table_name, execution_date
+
+def prepare_table(database_name: str, table_name: str, execution_date: datetime):
+    """
+    Retrieves a table from Spark, filters it by a specified execution date, and prepares its payload as a JSON string.
+
+    Parameters:
+    - database_name (str): Name of the Spark database containing the desired table.
+    - table_name (str): Name of the table to retrieve and process.
+    - execution_date (datetime): Date used to filter the table's rows. Rows with a matching year, month, and day are selected.
+    """
+    df = spark.table(f"{database_name}.{table_name}")
+    filtered_df = df.filter(
+        (df.year == execution_date.year)
+        & (df.month == execution_date.month)
+        & (df.day == execution_date.day)
+    ).drop("year", "month", "day")
+
+    houses = [row['sk_house'] for row in filtered_df.collect()]
+    
+    logger.info(
+        f"m=__main__, message=Table retrieved: {len(houses)} rows"
+    )
+
+    return json.dumps({"table": table_name, "dt_load": execution_date.strftime('%Y-%m-%d'), "payload": houses})
+
+def create_s3_path(bucket: str, table_name: str, execution_date: datetime):
+    """
+    Constructs an S3 path string based on the given bucket, table name, and execution date.
+
+    Parameters:
+    - bucket (str): The name of the S3 bucket.
+    - table_name (str): The name of the table, which will be part of the constructed path.
+    - execution_date (datetime): The date for which the path needs to be created. The year, month, and day are extracted from this.
+    """
+    path = f"s3://{bucket}/reverse/{table_name}/year={execution_date.year}/month={execution_date.month}/day={execution_date.day}"
+    return path
+
+def send_to_s3_bucket(file: str, table_name: str, path: str):
+    """
+    Uploads a specified file to an S3 bucket.
+
+    Parameters:
+    - file (str): The file path of the file to be uploaded.
+    - table_name (str): The name of the table, used mainly for logging purposes.
+    - path (str): The S3 bucket path where the file will be uploaded.
+    """
+    
+    try:
+        s3_service.upload_file(file, path)
+
+        logger.info(
+            f"m=__main__, message=successful S3 put_object for table {table_name}"
+            )
+        
+    except Exception as e:
+
+        raise HTTPException(
+            f"m=__main__, message=UNSUCCESSFULL S3 put_object for table {table_name}, error={e}"
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ dags/for_sale/reverse_sale_unavailable_listings_load/queries/reverse/suspected_unavailable_listings.sql
@@ -0,0 +1,113 @@
+WITH listings_1p AS (
+    SELECT 
+        sk_house,
+        sk_region, 
+        CAST(ts_first_publication AS DATE) dt_first_publication,
+        CAST(ts_last_publication AS DATE)  AS dt_last_quintoandar_publication
+    FROM 
+        dw_sale.fact_listings
+    INNER JOIN 
+        dw_sale.dim_listing
+          USING(sk_house)
+    WHERE
+        is_3p_supply = FALSE
+        AND status = 'PUBLISHED'
+),
+listings_3p AS (
+    SELECT 
+        sk_house,
+        sk_region,
+        CAST(ts_house_created AS DATE) AS dt_first_publication,
+        CAST(ts_last_publication AS DATE) AS dt_last_quintoandar_publication
+    FROM 
+        dw_rede.dim_lead_3p
+    INNER JOIN 
+        dw_rede.fact_lead_3p_flows
+            USING(sk_lead_3p)
+    INNER JOIN 
+        dw_rede.dim_lead_3p_context
+            USING(sk_lead_3p_context)
+    INNER JOIN 
+        dw_sale.dim_listing
+            USING(sk_house)
+    WHERE 
+        status = 'PUBLISHED' 
+        AND business_context = 'SALE'
+),
+published_listings AS (
+    SELECT
+        sk_house,
+        sk_region,
+        dt_first_publication,
+        dt_last_quintoandar_publication
+    FROM 
+        listings_1p
+    UNION ALL 
+    SELECT 
+        sk_house,
+        sk_region,
+        dt_first_publication,
+        dt_last_quintoandar_publication
+    FROM
+        listings_3p
+), 
+listings_without_bookings AS (
+    SELECT
+        sk_house, 
+        COUNT(DISTINCT sk_house) AS vbs_last_30_days
+    FROM 
+        dw_sale.fact_visits
+    WHERE 
+        ts_booking_created >= CURRENT_DATE - INTERVAL '30 days'
+    GROUP BY 
+        1
+),
+suspected_unavailability_listings AS (
+    SELECT 
+        id_house,
+        contact_attempts,
+        is_confirmed
+    FROM 
+        datalake_ebdb_clean.suspected_unavailability_listings
+    WHERE 
+        ts_last_contact_attempt >= CURRENT_DATE - INTERVAL '30 DAYS'
+),
+suspected_unavailable_listings AS (
+    SELECT DISTINCT
+        ol.sk_house,
+        dt_last_quintoandar_publication
+    FROM 
+        published_listings AS ol
+    LEFT JOIN 
+        listings_without_bookings
+            USING(sk_house)
+    LEFT JOIN 
+        dw_public.dim_region
+            USING(sk_region)
+    LEFT JOIN 
+        suspected_unavailability_listings AS sl 
+            ON sl.id_house = ol.sk_house
+    WHERE 
+        country_code = 'BR'
+        AND DATEDIFF(CURRENT_DATE,dt_last_quintoandar_publication) >= 30
+        AND DATEDIFF(CURRENT_DATE,dt_first_publication) >= 120
+        AND vbs_last_30_days IS NULL
+        AND (sl.is_confirmed IS NULL OR sl.is_confirmed = FALSE) -- If the house is not confirmed as available
+        AND (sl.contact_attempts IS NULL OR sl.contact_attempts < 3) -- Can't have received this HSM more than twice and not replied to it
+),
+dispatch_rules (
+    SELECT 
+        sk_house,
+        MOD(DATEDIFF(CAST(CURRENT_DATE AS DATE), CAST(dt_last_quintoandar_publication AS DATE)), 30) = 0 AS need_to_check_available
+    FROM 
+        suspected_unavailable_listings
+)
+SELECT 
+    sk_house,
+    YEAR(CURRENT_DATE) AS year,
+    MONTH(CURRENT_DATE) AS month,
+    DAY(CURRENT_DATE) AS day
+FROM 
+    dispatch_rules 
+WHERE
+    need_to_check_available = TRUE
