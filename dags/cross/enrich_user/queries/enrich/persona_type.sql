edffd4303230a61a08bb1b6ca93601a97a8866e6
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-09-27 17:15:23 -0300

    [AESS] Fix persona_type columns (#12373)
    * fix: columns on persona type
    
    * refactor: remove aliases
    
    * fix: column order

--- dags/cross/enrich_user/queries/enrich/persona_type.sql
+++ dags/cross/enrich_user/queries/enrich/persona_type.sql
@@ -1,92 +1,92 @@
 SELECT
-    po.id_snapshot,
-    po.id_user,
+    id_snapshot,
+    id_user,
     1 AS id_user_type,
-    po.id_country,
-    po.client_type,
-    po.country_code,
-    po.main_phone,
-    po.name,
-    po.email,
-    po.cpf,
-    NULL AS journey_step,
-    NULL AS persona_step,
-    po.is_active,
-    po.is_blocked,
-    po.is_pp_multi,
-    NULL AS is_offboarding,
-    NULL AS is_ongoing,
-    NULL AS is_onboarding,
-    NULL AS is_contract_to_entrance,
-    NULL AS is_visits_to_offer,
-    NULL AS is_listing_and_search,
-    NULL AS is_pre_contract,
-    NULL AS is_post_contract,
-    po.dt_birth,
-    po.ts_user_created,
-    po.ts_user_updated,
-    po.year,
-    po.month,
-    po.day
+    id_country,
+    client_type,
+    country_code,
+    main_phone,
+    name,
+    email,
+    cpf,
+    journey_step,
+    persona_step,
+    is_active,
+    is_blocked,
+    is_pp_multi,
+    is_offboarding,
+    is_ongoing,
+    is_onboarding,
+    is_contract_to_entrance,
+    is_visits_to_offer,
+    is_listing_and_search,
+    is_pre_contract,
+    is_post_contract,
+    dt_birth,
+    ts_user_created,
+    ts_user_updated,
+    year,
+    month,
+    day
 FROM
-    datalake_user.landlord AS po
+    datalake_user.landlord
 WHERE
-    po.year = {year}
-    AND po.month = {month}
-    AND po.day = {day}
+    year = {year}
+    AND month = {month}
+    AND day = {day}
 UNION ALL
 SELECT
-    tu.id_snapshot,
-    tu.id_user,
+    id_snapshot,
+    id_user,
     2 AS id_user_type,
-    tu.id_country,
-    tu.client_type,
-    tu.country_code,
-    tu.main_phone,
-    tu.name,
-    tu.email,
-    tu.cpf,
-    tu.journey_step,
-    tu.persona_step,
-    tu.is_active,
-    tu.is_blocked,
+    id_country,
+    client_type,
+    country_code,
+    main_phone,
+    name,
+    email,
+    cpf,
+    journey_step,
+    persona_step,
+    is_active,
+    is_blocked,
     NULL AS is_pp_multi,
-    tu.is_offboarding,
-    tu.is_ongoing,
-    tu.is_onboarding,
-    tu.is_contract_to_entrance,
-    tu.is_visits_to_offer,
-    tu.is_listing_and_search,
-    tu.is_pre_contract,
-    tu.is_post_contract,
-    tu.dt_birth,
-    tu.ts_user_created,
-    tu.ts_user_updated,
-    tu.year,
-    tu.month,
-    tu.day
+    is_offboarding,
+    is_ongoing,
+    is_onboarding,
+    is_contract_to_entrance,
+    is_visits_to_offer,
+    is_listing_and_search,
+    is_pre_contract,
+    is_post_contract,
+    dt_birth,
+    ts_user_created,
+    ts_user_updated,
+    year,
+    month,
+    day
 FROM
-    datalake_user.tenant_user AS tu
+    datalake_user.tenant_user
 WHERE
-    tu.year = {year}
-    AND tu.month = {month}
-    AND tu.day = {day}
+    year = {year}
+    AND month = {month}
+    AND day = {day}
 UNION ALL
 SELECT
-    phu.id_snapshot,
-    phu.id_user,
+    id_snapshot,
+    id_user,
     3 AS id_user_type,
-    phu.id_country,
-    phu.client_type,
-    phu.country_code,
-    phu.main_phone,
-    phu.name,
-    phu.email,
-    phu.cpf,
+    id_country,
+    client_type,
+    country_code,
+    main_phone,
+    name,
+    email,
+    cpf,
     NULL AS journey_step,
     NULL AS persona_step,
-    phu.is_active,
-    phu.is_blocked,
+    is_active,
+    is_blocked,
     NULL AS is_pp_multi,
     NULL AS is_offboarding,
     NULL AS is_ongoing,
@@ -96,34 +96,34 @@ SELECT
     NULL AS is_listing_and_search,
     NULL AS is_pre_contract,
     NULL AS is_post_contract,
-    phu.dt_birth,
-    phu.ts_user_created,
-    phu.ts_user_updated,
-    phu.year,
-    phu.month,
-    phu.day
+    dt_birth,
+    ts_user_created,
+    ts_user_updated,
+    year,
+    month,
+    day
 FROM
-    datalake_user.photographer_user AS phu
+    datalake_user.photographer_user
 WHERE
-    phu.year = {year}
-    AND phu.month = {month}
-    AND phu.day = {day}
+    year = {year}
+    AND month = {month}
+    AND day = {day}
 UNION ALL
 SELECT
-    bu.id_snapshot,
-    bu.id_user,
+    id_snapshot,
+    id_user,
     4 AS id_user_type,
-    bu.id_country,
-    bu.client_type,
-    bu.country_code,
-    bu.main_phone,
-    bu.name,
-    bu.email,
-    bu.cpf,
+    id_country,
+    client_type,
+    country_code,
+    main_phone,
+    name,
+    email,
+    cpf,
     NULL AS journey_step,
     NULL AS persona_step,
-    bu.is_active,
-    bu.is_blocked,
+    is_active,
+    is_blocked,
     NULL AS is_pp_multi,
     NULL AS is_offboarding,
     NULL AS is_ongoing,
@@ -133,15 +133,15 @@ SELECT
     NULL AS is_listing_and_search,
     NULL AS is_pre_contract,
     NULL AS is_post_contract,
-    bu.dt_birth,
-    bu.ts_user_created,
-    bu.ts_user_updated,
-    bu.year,
-    bu.month,
-    bu.day
+    dt_birth,
+    ts_user_created,
+    ts_user_updated,
+    year,
+    month,
+    day
 FROM
-    datalake_user.broker_user AS bu
+    datalake_user.broker_user
 WHERE
-    bu.year = {year}
-    AND bu.month = {month}
-    AND bu.day = {day}
+    year = {year}
+    AND month = {month}
+    AND day = {day}
