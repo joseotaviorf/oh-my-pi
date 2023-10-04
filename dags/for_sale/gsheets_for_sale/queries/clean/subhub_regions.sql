bf17199607de82fe9006eac79e30f590fb230241
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2023-10-04 13:55:49 -0300

    [DFORSALE-6527] Subhub columns ingestion and their description (#12405)

--- dags/for_sale/gsheets_for_sale/queries/clean/subhub_regions.sql
+++ dags/for_sale/gsheets_for_sale/queries/clean/subhub_regions.sql
@@ -1,6 +1,9 @@
 SELECT
     CAST(id_do_bairro AS INTEGER) AS id_region,
-    CAST(hub AS STRING) AS hub_name, 
+    CAST(cidade AS STRING) AS city,
+    CAST(praca AS STRING) AS city_group,
+    CAST(bairro AS STRING) AS region,
+    CAST(hub AS STRING) AS hub_name,
     CAST(subhub AS STRING) AS subhub_name
 FROM
     datalake_gsheets_raw.subhub_regions
