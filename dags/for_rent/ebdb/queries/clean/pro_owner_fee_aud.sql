a76c938747a77b49eb52acaee3c4fef0cf9e5d15
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-10-05 16:53:32 -0300

    fixing query (#12444)

--- dags/for_rent/ebdb/queries/clean/pro_owner_fee_aud.sql
+++ dags/for_rent/ebdb/queries/clean/pro_owner_fee_aud.sql
@@ -6,7 +6,7 @@ SELECT
     adm_fee,
     active AS is_active,
     user_pro_owner_id_mod AS mod_id_user_pro_owner,
-    adm_fee_mod AS mod_adm_fee
-    active_mod AS mod_is_active,
+    adm_fee_mod AS mod_adm_fee,
+    active_mod AS mod_is_active
 FROM 
     datalake_ebdb_raw.proownerfee_aud
