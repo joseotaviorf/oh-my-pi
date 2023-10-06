7861c5edf4c960abf68c9e150c324c2e9608d7be
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-10-06 09:16:07 -0300

    changed account classification rule for payment adjustment account (#12446)

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -110,7 +110,7 @@ SELECT DISTINCT
   CASE
     WHEN entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc') THEN 'receivable'
     WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant') THEN 'receivable'
-    WHEN entry_type IN ('postponement') AND (ie.from_account_type = 'landlord' OR ie.to_account_type = 'landlord') THEN 'payable'
+    WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'landlord' OR ie.to_account_type = 'landlord') THEN 'payable'
     WHEN (ROUND(-1.0*i.due_amount,2) > 0 OR (ROUND(-1.0*i.due_amount,2) = 0 AND NOT(from_account_type = 'landlord' OR to_account_type= 'landlord') )) THEN 'receivable'
     ELSE 'payable'
   END AS account_classification,
