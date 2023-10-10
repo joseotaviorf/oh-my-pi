3570ceb749ac7489e0903b5dd170bc1d418929ba
felipepeixotosc <110105673+felipepeixotosc@users.noreply.github.com>
2023-10-10 07:00:07 -0300

    [DAS&S] Add new tables from terminator (Characteristis and checklist-item) (#12431)
    * add new tables from terminator
    
    * update the checklist table to cheklists
    
    * update the table name
    
    * update table name
    
    * corret some indentation
    
    * update name from checklist-item, remove termination from the name
    
    * [DAS&S] Update .yml file from checklist-item
    
    * fix some issues on PR and change to incremental tables
    
    * add new columns for incremental tables with year, month and day
    
    ---------
    
    Co-authored-by: Felipe Peixoto <felipepeixoto@BRSPPM-C02FJ3NDMD6M.local>

--- dags/support_and_service/terminator/queries/clean/checklist_item.sql
+++ dags/support_and_service/terminator/queries/clean/checklist_item.sql
@@ -1,14 +1,16 @@
-SELECT
+SELECT 
     id,
     termination_id AS id_termination,
-    step,
+    name AS checklist_item,
+    active AS is_active,
+    done AS is_done, 
     created_at AS ts_created,
     updated_at AS ts_updated,
     year,
     month,
     day
 FROM 
-    datalake_terminator_raw.termination_step
+    datalake_terminator_raw.checklist_item
 WHERE
     year = {year}
     AND month = {month}
