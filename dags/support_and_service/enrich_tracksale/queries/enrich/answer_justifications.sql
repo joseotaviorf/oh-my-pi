196e49b452272ae5fef82ba8f6712fef6e324c65
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2023-10-16 15:58:58 -0300

    Fix region language to get answer (#12506)

--- dags/support_and_service/enrich_tracksale/queries/enrich/answer_justifications.sql
+++ dags/support_and_service/enrich_tracksale/queries/enrich/answer_justifications.sql
@@ -2,31 +2,40 @@ WITH exploded_justifications AS (
   SELECT
       id AS id_answer,
       justifications,
-      explode(FROM_json(justifications, 'array<string>')) AS just_json
-  FROM datalake_tracksale.answer
-  WHERE justifications != '[]'  
+      EXPLODE(FROM_JSON(justifications, 'array<string>')) AS just_json
+  FROM 
+      datalake_tracksale.answer
+  WHERE 
+      justifications != '[]'  
 ),
 main_justifications AS (
     SELECT
         id_answer,
         'main' AS level,
-        GET_JSON_OBJECT(just_json, '$.name') AS justification
-    FROM exploded_justifications
+        GET_JSON_OBJECT(just_json, '$.name') AS justification,
+        JSON_OBJECT_KEYS(GET_JSON_OBJECT(just_json, '$.name'))[0] AS region_language
+    FROM 
+        exploded_justifications
 ),
 exploded_second_justifications AS (
     SELECT
         id_answer,
         'second' AS level,
-        FROM_json(GET_JSON_OBJECT(just_json, '$.children'), 'array<string>') AS justification    
-    FROM exploded_justifications
-    where GET_JSON_OBJECT(just_json, '$.children') <> '[]'
+        FROM_JSON(GET_JSON_OBJECT(just_json, '$.children'), 'array<string>') AS justification,
+        JSON_OBJECT_KEYS(GET_JSON_OBJECT(just_json, '$.name'))[0] AS region_language 
+    FROM 
+        exploded_justifications
+    WHERE 
+        GET_JSON_OBJECT(just_json, '$.children') <> '[]'
 ),
 second_justifications AS (
     SELECT 
         id_answer,
         level,
-        explode(justification) AS justification
-    FROM exploded_second_justifications
+        EXPLODE(justification) AS justification,
+        region_language
+    FROM 
+        exploded_second_justifications
 ),
 justifications AS (
     SELECT * FROM main_justifications
@@ -36,5 +45,6 @@ justifications AS (
 SELECT 
     id_answer,
     level,
-    GET_JSON_OBJECT(justification, '$.pt-br') AS justification
-FROM justifications
+    GET_JSON_OBJECT(justification, concat('$.', region_language)) AS justification
+FROM 
+    justifications
