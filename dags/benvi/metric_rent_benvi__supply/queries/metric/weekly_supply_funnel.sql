81b6365433cb29b2c1e5c043090e415de488e228
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2023-10-18 12:08:36 -0300

    [AEFR-3407] Update metric_rent_benvi__supply to contain only currently used metrics (#12522)
    * Remove unused columns from metric_rent_benvi__supply tables
    
    * Update dependencies file
    
    * Update lineage indentation
    
    * Update lineage indentation for weekly_supply_funnel
    
    * Add dimension and metric fields for yaml
    
    * Try to remove some fields from metrics
    
    * Try to remove some fields from metrics
    
    * Back all fields
    
    * Update hierarchy from Growth to Operations Mgmt

--- dags/benvi/metric_rent_benvi__supply/queries/metric/weekly_supply_funnel.sql
+++ dags/benvi/metric_rent_benvi__supply/queries/metric/weekly_supply_funnel.sql
@@ -9,133 +9,6 @@ WITH mexico_regions AS (
         country_code = 'MX'
         AND dt_month_started >= DATE('2022-06-01')
 ),
-supply_funnel_targets AS (
-    SELECT
-        COALESCE(city_group, 'Not Mapped') AS city_group,
-        CASE 
-            WHEN supply_origin IN ('Landing page - PWA', 'Organic traffic') THEN 'Owner PWA'
-            WHEN supply_origin = 'Indica Ai - General' THEN 'Indica Aí - General'
-            WHEN supply_origin = 'Refiere y Gana' THEN 'Indica Aí - General'
-            WHEN supply_origin = 'CIB' THEN 'CIQ'
-            WHEN supply_origin = 'I24' THEN 'LP Navent'
-            WHEN supply_origin LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            ELSE supply_origin
-        END AS supply_mkt_origin,
-        CASE 
-            WHEN supply_origin = 'Organic traffic' THEN 'Organic'
-            WHEN supply_origin = 'Landing page - PWA' AND supply_channel LIKE '%Paid%' THEN 'Paid' 
-            WHEN supply_origin = 'Indica Ai - General' THEN 'Indica Aí - General'
-            WHEN supply_origin = 'Refiere y Gana' THEN 'Indica Aí - General'
-            WHEN supply_origin = 'CIB' THEN 'CIQ'
-            WHEN supply_origin = 'Owner PWA' THEN supply_channel
-            WHEN supply_origin = 'I24' THEN 'LP Navent'
-            WHEN supply_origin LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            ELSE supply_origin
-        END AS supply_mkt_origin_detailed,
-        CASE 
-            WHEN supply_origin = 'Owner PWA' AND supply_channel = 'Paid' THEN 'Landing Page - PWA'
-            WHEN supply_origin = 'Owner PWA' AND supply_channel = 'Organic' THEN 'Organic Traffic - PWA'
-            WHEN supply_origin LIKE '%Indica%' THEN 'Refiere y Gana'
-            WHEN supply_origin = 'CIQ' THEN 'CIB'
-            WHEN supply_origin LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            ELSE supply_origin
-        END AS mexico_channel,
-        SUM(CAST(NULLIF(top_of_funnel,'') AS REAL)) AS leads_targets,
-        SUM(CAST(NULLIF(prospects,'') AS REAL)) AS prospects_targets,
-        SUM(CAST(NULLIF(available_qualifieds, '') AS REAL)) AS available_qualifieds_targets,
-        SUM(CAST(NULLIF(qualifieds,'') AS REAL)) AS qualifieds_targets,
-        SUM(CAST(NULLIF(opportunities,'') AS REAL)) AS opportunities_targets,
-        SUM(CAST(NULLIF(first_listings,'') AS REAL)) AS first_listings_targets,
-        DATE(DATE_TRUNC('week', DATE(dt_target))) AS dt_week_started
-    FROM 
-        datalake_gsheets_clean.mexico_supply_targets_2022
-    WHERE
-        DATE_TRUNC('week', DATE(dt_target)) <= DATE_ADD(CURRENT_DATE, -1) 
-        AND supply_origin NOT IN ('Crawlers classifieds', 'Price Calculator')
-    GROUP BY 1, 2, 3, 4, 11
-),
-supply_budget_targets AS (
-    SELECT 
-        COALESCE(city_group, 'Not Mapped') AS city_group,
-        CASE 
-            WHEN planning_mkt_level3 IN ('Landing page - PWA', 'Organic traffic') THEN 'Owner PWA'
-            WHEN planning_mkt_level3 LIKE '%Refiere y Gana%' THEN 'Indica Aí - General'
-            WHEN planning_mkt_level3 LIKE '%CIB%' THEN 'CIQ'
-            WHEN planning_mkt_level3 LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            WHEN planning_mkt_level3 LIKE '%I24%' THEN 'LP Navent'
-            ELSE planning_mkt_level3 
-        END AS supply_mkt_origin,
-        CASE 
-            WHEN planning_mkt_level3 = 'Landing page - PWA' THEN 'Paid'
-            WHEN planning_mkt_level3 = 'Organic traffic' THEN 'Organic'
-            WHEN planning_mkt_level3 LIKE '%Refiere y Gana%' THEN 'Indica Aí - General'
-            WHEN planning_mkt_level3 LIKE '%CIB%' THEN 'CIQ'
-            WHEN planning_mkt_level3 LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            WHEN planning_mkt_level3 LIKE '%I24%' THEN 'LP Navent'
-            ELSE planning_mkt_level3 
-        END AS supply_mkt_origin_detailed,
-        SUM(budget) AS budget_total,
-        SUM(CASE
-                WHEN UPPER(planning_mkt_level3) LIKE '%COMISION%' OR UPPER(planning_mkt_level3) LIKE '%COMISIÓN%' THEN budget
-        END) AS budget_comission,
-        SUM(CASE
-                WHEN UPPER(planning_mkt_level3) NOT LIKE '%COMISION%' AND UPPER(planning_mkt_level3) NOT LIKE '%COMISIÓN%' THEN budget
-        END) AS budget,
-        dt_week AS dt_week_started
-    FROM
-        datalake_gsheets_clean.mexico_costs_targets
-    WHERE
-        planning_mkt_level3 <> 'Price Calculator'
-    GROUP BY 1, 2, 3, 7
-),
-supply_actual_costs AS (
-    SELECT
-        COALESCE(city_group, 'Not Mapped') AS city_group,
-        CASE 
-            WHEN planning_mkt_level3 IN ('Landing page - PWA', 'Organic traffic') THEN 'Owner PWA'
-            WHEN planning_mkt_level3 LIKE '%Refiere y Gana%' THEN 'Indica Aí - General'
-            WHEN planning_mkt_level3 LIKE '%CIB%' THEN 'CIQ'
-            WHEN planning_mkt_level3 LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            WHEN planning_mkt_level3 LIKE '%I24%' THEN 'LP Navent'
-            ELSE planning_mkt_level3 
-        END AS supply_mkt_origin,
-        CASE 
-            WHEN planning_mkt_level3 = 'Landing page - PWA' THEN 'Paid'
-            WHEN planning_mkt_level3 = 'Organic traffic' THEN 'Organic'
-            WHEN planning_mkt_level3 = 'I24' THEN 'LP Navent'
-            WHEN planning_mkt_level3 LIKE '%Refiere y Gana%' THEN 'Indica Aí - General'
-            WHEN planning_mkt_level3 LIKE '%CIB%' THEN 'CIQ'
-            WHEN planning_mkt_level3 LIKE '%Human crawlers%' THEN 'Human Crawlers'
-            ELSE planning_mkt_level3 
-        END AS supply_mkt_origin_detailed,
-        SUM(budget) AS actual_cost,
-        SUM(CASE WHEN UPPER(planning_mkt_level3) LIKE '%COMISION%' OR UPPER(planning_mkt_level3) LIKE '%COMISIÓN%' THEN budget END) AS actual_cost_comission,
-        SUM(CASE WHEN UPPER(planning_mkt_level3) NOT LIKE '%COMISION%' AND UPPER(planning_mkt_level3) NOT LIKE '%COMISIÓN%' THEN budget END) AS cost,
-        dt_week_started
-    FROM
-        datalake_gsheets_clean.mexico_supply_costs_financial_and_actual
-    WHERE
-        planning_mkt_level3 <> 'Price Calculator'
-    GROUP BY 1, 2, 3, 7
-),
-market_place AS (
-    -- We are still using the datamart as a source since it's the actual solution for churn metric
-    SELECT 
-        COALESCE(dr.city_group, 'Not Mapped') AS city_group,
-        COUNT(DISTINCT CASE WHEN rmf.status_short = 'Ongoing Listing' THEN rmf.sk_house_listing END) AS ongoing_listings,
-        COUNT(DISTINCT CASE WHEN rmf.status_short = 'Other' AND rmf.status_last_week_short IN ('Ongoing Listing', 'Advanced Negociation', 'New Listing') THEN rmf.sk_house_listing END) - 
-            COUNT(DISTINCT CASE WHEN rmf.status_last_week_short = 'Other' AND rmf.status_short = 'Ongoing Listing' THEN rmf.sk_house_listing END) AS net_churn,
-        rmf.week_start AS dt_week_started
-    FROM 
-        dw_datamarts.rental_marketplace_flows AS rmf
-    JOIN 
-        datalake_region.region AS dr
-            ON rmf.sk_region = dr.id 
-    WHERE
-        dr.country_code = 'MX'
-        AND rmf.week_start >= DATE('2022-06-01')
-    GROUP BY 1, 4
-),
 coincident_funnel AS (
     SELECT
         city_group,
@@ -172,15 +45,15 @@ cohort_funnel AS (
         SUM(o2fl_cohort) AS o2fl_cohort,
         dt_lead_week_started
     FROM
-        datalake_mexico_rent_supply.cohort_funnel 
+        datalake_mexico_rent_supply.cohort_funnel
     GROUP BY 1, 2, 3, 4, 16
 )
-SELECT 
+SELECT
     COALESCE(dim.country_code, 'Undefined') AS country_code,
     COALESCE(dim.city_group, 'Undefined') AS city_group,
-    cf.supply_mkt_origin, 
+    cf.supply_mkt_origin,
     cf.supply_mkt_origin_detailed,
-    COALESCE(cf.mexico_channel, sft.mexico_channel) AS mexico_channel,     
+    cf.mexico_channel AS mexico_channel,
     cf.leads,
     -- Coincident actual volumes
     cf.prospects,
@@ -210,58 +83,17 @@ SELECT
     cfl.q2o_cohort,
     cfl.aq2o_cohort,
     cfl.o2fl_cohort,
-    -- Actual volume targets
-    sft.leads_targets,
-    sft.prospects_targets,
-    sft.available_qualifieds_targets,
-    sft.qualifieds_targets,
-    sft.opportunities_targets,
-    sft.first_listings_targets,
-    -- Actual costs
-    CAST(sac.actual_cost AS FLOAT) AS actual_cost,
-    CAST(sac.actual_cost_comission AS FLOAT) AS actual_cost_comission,
-    CAST(sac.cost AS FLOAT) AS cost,
-    -- Targets cost 
-    CAST(sbt.budget_total AS FLOAT) AS budget_total_target,
-    CAST(sbt.budget_comission AS FLOAT) AS budget_comission_target,
-    CAST(sbt.budget AS FLOAT) AS budget_target,
-    -- Listings
-    mp.ongoing_listings,
-    mp.net_churn,
-    DATE(COALESCE(dim.dt_week_started, sft.dt_week_started)) AS dt_week_started
+    DATE(dim.dt_week_started) AS dt_week_started
 FROM
     mexico_regions AS dim
 LEFT JOIN
     coincident_funnel AS cf
-        ON dim.dt_week_started = cf.dt_week_started 
+        ON dim.dt_week_started = cf.dt_week_started
         AND dim.city_group = cf.city_group
 LEFT JOIN
     cohort_funnel AS cfl
-        ON dim.dt_week_started = cfl.dt_lead_week_started 
-        AND dim.city_group = cfl.city_group 
+        ON dim.dt_week_started = cfl.dt_lead_week_started
+        AND dim.city_group = cfl.city_group
         AND cf.supply_mkt_origin = cfl.supply_mkt_origin
         AND cf.supply_mkt_origin_detailed = cfl.supply_mkt_origin_detailed
         AND cf.mexico_channel = cfl.mexico_channel
-LEFT JOIN
-    supply_funnel_targets AS sft
-        ON dim.dt_week_started = sft.dt_week_started 
-        AND dim.city_group = sft.city_group 
-        AND cf.supply_mkt_origin = sft.supply_mkt_origin
-        AND cf.supply_mkt_origin_detailed = sft.supply_mkt_origin_detailed 
-        AND cf.mexico_channel = sft.mexico_channel
-LEFT JOIN
-    supply_budget_targets AS sbt
-        ON dim.dt_week_started = sbt.dt_week_started 
-        AND dim.city_group = sbt.city_group 
-        AND cf.supply_mkt_origin = sbt.supply_mkt_origin
-        AND cf.supply_mkt_origin_detailed = sbt.supply_mkt_origin_detailed
-LEFT JOIN
-    supply_actual_costs AS sac
-        ON dim.dt_week_started = sac.dt_week_started 
-        AND dim.city_group = sac.city_group 
-        AND cf.supply_mkt_origin = sac.supply_mkt_origin
-        AND cf.supply_mkt_origin_detailed = sac.supply_mkt_origin_detailed
-LEFT JOIN
-    market_place AS mp
-        ON dim.dt_week_started = mp.dt_week_started 
-        AND dim.city_group = mp.city_group
