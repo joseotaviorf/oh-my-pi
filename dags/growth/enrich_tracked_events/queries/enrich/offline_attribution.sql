8d4c45aae4dabab0984d5496ea732c450bbc980f
Thiago Felipe Nogueira De Jesus <thiago.jesus@quintoandar.com.br>
2023-10-19 12:29:30 -0300

    [GROW] Refactor enrich offline_attribution (#12453)
    * Refactor offline attribution

--- dags/growth/enrich_tracked_events/queries/enrich/offline_attribution.sql
+++ dags/growth/enrich_tracked_events/queries/enrich/offline_attribution.sql
@@ -18,161 +18,23 @@ contact_secretaria AS (
     ON oc.id = cmc.id_origin
   LEFT JOIN datalake_casa_mineira_crm_clean.media_contact AS mc
     ON mc.id = cmc.id_media
-),
-ivr_events AS (
-  SELECT
-    id_call,
-    from_number,
-    to_number,
-    MIN(id_task) AS id_task,
-    MIN(ts_created_local) AS ts_first_event
-  FROM
-    datalake_bigfone_twilio.call_ivr_events
-  WHERE
-    -- CX's exclusive phone number to plaquinhas contacts
-    to_number IN ('+5511933058701', '+5531933007908', '+5540202507')
-  GROUP BY
-    1,2,3
-),
-flex_events AS (
-  SELECT
-    id_task,
-    id_call,
-    id_conversation,
-    from_number,
-    to_number,
-    MIN(ts_created_local) AS ts_first_event
-  FROM
-    datalake_bigfone_twilio.call_flex_events
-  WHERE
-    -- CX's exclusive phone number to plaquinhas contacts
-    to_number IN ('+5511933058701', '+5531933007908', '+5540202507')
-  GROUP BY
-    1,2,3,4,5
-),
-phone_users_number AS (
-  SELECT
-    COALESCE(ie.from_number, fe.from_number) AS from_phone_number,
-    COALESCE(ie.ts_first_event, fe.ts_first_event) AS ts_event
-  FROM
-    ivr_events AS ie
-  FULL JOIN flex_events AS fe
-    ON fe.id_call = ie.id_call
-),
-chat_users_number AS (
-  SELECT
-    qmc.from_phone_number AS customer_phone,
-    qmc.ts_created AS ts_event
-  FROM
-    datalake_quinto_messenger.task_event AS qmte
-    INNER JOIN datalake_quinto_messenger.task AS qmt
-      ON qmte.id_task = qmt.id_task
-    INNER JOIN datalake_quinto_messenger.channel AS qmc
-      ON qmt.id_channel = qmc.id_channel
-  WHERE
-    LOWER(qmte.task_queue_name) LIKE '%plaquinhas%'
-  GROUP BY
-    1,2
-),
---  New CX taxonomies created to identify tickets about Plaquinhas. Effective from 17/10/2022.	
-cx_taxonomy_phone_events AS (	
-  SELECT	
-    id_user,	
-    ts_ticket_started AS ts_event	
-  FROM 	
-     datalake_customer_support.call 	
-  WHERE 	
-	contact_theme_detail_tag IN ( 	
-	  'rental_listing_register_search_properties_sale_or_lease_signs' ,	
-	  'rental_listing_register_sign_real_estate_info',	
-    'house_plate_info', 
-    'real_estate_information_plates') 	
-	AND id_user IS NOT NULL	
-    AND DATE(ts_ticket_started) >= DATE('2022-10-17')	
-), 	
---  New CX taxonomies created to identify tickets about Plaquinhas. Effective from 17/10/2022.	
-cx_taxonomy_chat_events AS (	
-  SELECT	
-    id_user,	
-    ts_ticket_started AS ts_event	
-  FROM 	
-     datalake_customer_support.chat	
-  WHERE 	
-	contact_theme_detail_tag IN ( 	
-	  'rental_listing_register_search_properties_sale_or_lease_signs' ,	
-	  'rental_listing_register_sign_real_estate_info',	
-    'house_plate_info', 
-    'real_estate_information_plates') 	
-	AND id_user IS NOT NULL	
-    AND DATE(ts_ticket_started) >= DATE('2022-10-17')	
-), 
-contact_cx AS (
-  SELECT
-    user.id AS id_user,
-    'Telefone' AS canal,
-    ts_event
-  FROM
-    datalake_ebdb_user.user AS user
-    JOIN phone_users_number AS pn
-      ON pn.from_phone_number = user.main_phone
-  WHERE
-    user.id > 0
-    AND DATE(ts_event) < DATE('2022-10-17')
-  UNION ALL
-  SELECT	
-    id_user,	
-    'Telefone' AS canal,	
-    ts_event	
-  FROM	
-    cx_taxonomy_phone_events	
-  UNION ALL
-  SELECT
-    user.id AS id_user,
-    'Chat' AS canal,
-    ts_event
-  FROM
-    datalake_ebdb_user.user AS user
-    JOIN chat_users_number AS cn
-      ON cn.customer_phone = user.main_phone
-  WHERE
-    user.id > 0
-  UNION ALL
-	SELECT	
-    id_user,	
-    'Chat' AS canal,	
-    ts_event	
-  FROM	
-    cx_taxonomy_chat_events
-  UNION ALL
-  SELECT
-    INT(id_user) AS id_user,
-    canal,
-    data_hora AS ts_event
-  FROM
-    datalake_gsheets_clean.users_cx_plaquinhas
-  WHERE
-    INT(id_user) > 0
 )
 
 SELECT
-DISTINCT
   id_user,
-  NULL AS id_contact,
-  'Contact' AS event_name,
-  'Placas' AS origin,
-  canal AS channel,
-  'CX' AS agent,
+  id_contact,
+  event_name,
+  origin,
+  channel,
+  agent,
   ts_event,
-  YEAR(ts_event) AS year,
-  MONTH(ts_event) AS month,
-  DAY(ts_event) AS day
-FROM 
-  contact_cx AS cx
-GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  year,
+  month,
+  day
+FROM
+  datalake_tracked_events.plaquinhas_offline_touchpoints
 UNION ALL
-
-SELECT
-DISTINCT
+SELECT DISTINCT
   id_user,
   sk_contact AS id_contact,
   event_name,
@@ -186,9 +48,7 @@ DISTINCT
 FROM 
   contact_secretaria AS cs
 GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-
 UNION ALL
-
 SELECT
   id_user,
   COALESCE(id_user,user_email,id_contact_submission) AS id_contact,
---
+++ dags/growth/enrich_tracked_events/queries/enrich/plaquinhas_offline_touchpoints.sql
@@ -0,0 +1,177 @@
+-- Legacy chat touchpoints
+WITH legacy_chat_flow AS (
+  SELECT DISTINCT
+    user.id AS id_user,
+    'Chat' AS contact_channel,
+    qmc.ts_created AS ts_event
+  FROM
+    datalake_quinto_messenger.task_event AS qmte
+  INNER JOIN 
+    datalake_quinto_messenger.task AS qmt
+      ON qmte.id_task = qmt.id_task
+  INNER JOIN 
+    datalake_quinto_messenger.channel AS qmc
+      ON qmt.id_channel = qmc.id_channel
+  INNER JOIN
+    datalake_ebdb_user.user AS user
+      ON qmc.from_phone_number = user.main_phone
+  WHERE
+    LOWER(qmte.task_queue_name) LIKE '%plaquinhas%'
+    AND  user.id > 0
+), 
+
+-- New chat flows
+pre_chatbot_sessions AS (
+  SELECT
+    id_session,
+    ts_started
+  FROM
+    datalake_greenseer_clean.session
+  WHERE
+    LOWER(id_pipeline) = 'whatsapp_direct_transfer'
+    AND LOWER(GET_JSON_OBJECT(memory, '$.basic.session.department_key')) = 'rent_board_test'
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY id_session ORDER BY ts_updated DESC) = 1
+),
+
+pos_chatbot_sessions AS (
+  SELECT
+    id_user,
+    id_session,
+    ts_started
+  FROM
+    datalake_greenseer.greenseer_session
+  WHERE
+    ts_started >= DATE('2023-03-20') -- data de inicio do bot de plaquinhas
+    AND LOWER(id_pipeline) IN ('whatsapp_real_state_signs', 'whatsapp_real_estate_signs')
+),
+
+plaquinhas_sessions AS (
+  SELECT
+    gs.id_user,
+    gs.ts_started AS ts_event
+  FROM
+    datalake_greenseer.greenseer_session AS gs
+  INNER JOIN
+    pre_chatbot_sessions AS pcs
+      ON pcs.id_session = gs.id_session
+  UNION ALL
+  SELECT
+    id_user,
+    ts_started AS ts_event
+  FROM
+    pos_chatbot_sessions
+),
+
+total_chat AS (
+  SELECT DISTINCT
+    id_user,
+    'Chat'AS contact_channel,
+    ts_event
+  FROM
+    plaquinhas_sessions
+  WHERE
+    DATE(ts_event) >= DATE('2022-10-17')
+    AND id_user IS NOT NULL
+  UNION ALL
+  SELECT
+    id_user,
+    contact_channel,
+    ts_event
+  FROM
+    legacy_chat_flow
+  WHERE
+    DATE(ts_event) < DATE('2022-10-17')	
+    AND id_user IS NOT NULL
+),
+
+-- Phone calls to Plaquinhas exclusive numbers
+ivr_events AS (
+  SELECT
+    id_call,
+    from_number,
+    to_number,
+    MIN(id_task) AS id_task,
+    MIN(ts_created_local) AS ts_first_event
+  FROM
+    datalake_bigfone_twilio.call_ivr_events
+  WHERE
+    -- CX's exclusive phone number to plaquinhas contacts
+    (to_number IN ('+5511933058701', '+5531933007908', '+5540202507') AND DATE(ts_created_local) < DATE('2022-10-17'))
+    OR (to_number LIKE '%40202507%' AND DATE(ts_created_local) >= DATE('2022-10-17'))
+  GROUP BY
+    1, 2, 3
+),
+
+flex_events AS (
+  SELECT
+    id_task,
+    id_call,
+    id_conversation,
+    from_number,
+    to_number,
+    MIN(ts_created_local) AS ts_first_event
+  FROM
+    datalake_bigfone_twilio.call_flex_events
+  WHERE
+    -- CX's exclusive phone number to plaquinhas contacts
+    (to_number IN ('+5511933058701', '+5531933007908', '+5540202507') AND DATE(ts_created_local) < DATE('2022-10-17'))
+    OR (to_number LIKE '%40202507%' AND DATE(ts_created_local) >= DATE('2022-10-17'))
+  GROUP BY
+    1, 2, 3, 4, 5
+),
+
+total_phone AS (
+  SELECT
+    user.id AS id_user,
+    'Telefone' AS contact_channel,
+    COALESCE(ie.ts_first_event, fe.ts_first_event) AS ts_event
+  FROM
+    ivr_events AS ie
+  FULL JOIN flex_events AS fe
+    ON fe.id_call = ie.id_call
+  INNER JOIN
+    datalake_ebdb_user.user AS user
+    ON COALESCE(ie.from_number, fe.from_number) = user.main_phone
+  WHERE
+    user.id > 0
+),
+
+plaquinhas_offline_touchpoints AS (
+  SELECT
+    id_user, 
+    contact_channel,
+    ts_event
+  FROM
+    total_chat
+  UNION ALL
+  SELECT
+    id_user, 
+    contact_channel,
+    ts_event
+  FROM
+    total_phone
+  UNION ALL
+  SELECT
+    INT(id_user) AS id_user,
+    canal AS contact_channel,
+    TO_TIMESTAMP(data_hora) AS ts_event
+  FROM
+    datalake_gsheets_clean.users_cx_plaquinhas
+  WHERE
+    INT(id_user) > 0
+)
+
+SELECT DISTINCT
+  id_user,
+  (id_user || 0 || TO_UNIX_TIMESTAMP(ts_event)) AS id_contact,
+  'Contact' AS event_name,
+  'Placas' AS origin,
+  contact_channel AS channel,
+  'CX' AS agent,
+  ts_event,
+  YEAR(ts_event) AS year,
+  MONTH(ts_event) AS month,
+  DAY(ts_event) AS day
+FROM 
+  plaquinhas_offline_touchpoints 
