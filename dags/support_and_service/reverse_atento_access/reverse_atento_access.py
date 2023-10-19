a15c0bd2e6cf530d698e64ac2440274e86841597
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2023-10-19 10:57:52 -0300

    [DDSS]: break atento reverse dag (#12529)

--- dags/support_and_service/reverse_atento_access/reverse_atento_access.py
+++ dags/support_and_service/reverse_atento_access/reverse_atento_access.py
@@ -2,17 +2,17 @@ from datetime import datetime
 from bietlejuice.base.airflow.dag_owner_enum import DAGOwnerEnum
 from pendulum import timezone
 import os
+import json
 
 from airflow.models import DAG
 from airflow.utils.helpers import chain
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
+    QuintoAndarDatabricksSubmitRunOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
 )
 
 from bietlejuice.base.airflow.base_dag import BaseDAG
-from bietlejuice.base.pipeline.layer_enum import LayerEnum
-from bietlejuice.base.airflow.task_groups.reverse_task_group import ReverseTaskGroup
 from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
 
@@ -21,10 +21,10 @@ ENV = os.environ.get("ENVIRONMENT")
 
 # DAG and Jobs params setup
 SOURCE = "atento"
-DAG_NAME = f"reverse_{SOURCE}"
+DAG_NAME = f"reverse_{SOURCE}_access"
 DAG_ID = f"bietlejuice.{DAG_NAME}"
 
-MAIN_START_DATE = datetime(2022, 7, 12, 0, 0, 0, tzinfo=timezone("America/Sao_Paulo"))
+MAIN_START_DATE = datetime(2023, 10, 11, 0, 0, 0, tzinfo=timezone("America/Sao_Paulo"))
 MAIN_SCHEDULE_INTERVAL = None
 DAG_OWNER = DAGOwnerEnum.DATA_SS
 
@@ -33,8 +33,14 @@ config_service = ConfigurationService(DAG_NAME)
 # S3 path setup
 datalake_bucket = config_service.get_config("datalake_bucket")
 s3_prefix = config_service.get_config("databricks_bietlejuice_repo_path")
-base_spark_jobs_path = f"{s3_prefix}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
+
+reverse_spark_job_path = (
+    f"{s3_prefix}/spark_jobs/{DAG_NAME}/load_s3_data_into_external_bucket.py"
+)
+execution_tracking_spark_job_path = (
+    f"{s3_prefix}/spark_jobs/{DAG_NAME}/load_execution_tracking.py"
+)
 
 cluster_description = config_service.get_config("custom_cluster")
 
@@ -46,8 +52,9 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 ]
 
 default_libraries = config_service.get_config("default_libraries")
-partition_cols = config_service.get_config("partition_cols")
+external_s3_bucket = config_service.get_config("external_s3_bucket")
 dag_documentation = config_service.get_config("dag_documentation")
+table_schema = config_service.get_config("schema")
 
 dag = DAG(
     dag_id=DAG_ID,
@@ -79,22 +86,39 @@ terminate_cluster_task = QuintoAndarDatabricksTerminateClusterOperator(
     dag=dag, task_id="terminate-cluster"
 )
 
-
-task_group = ReverseTaskGroup(
+external_bucket_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id=f"load_s3_data_into_external_bucket",
     dag=dag,
-    env=ENV,
-    s3_bucket=datalake_bucket,
-    relative_query_path=DAG_NAME,
-    spark_jobs_path=base_spark_jobs_path,
+    json={
+        "spark_python_task": {
+            "python_file": reverse_spark_job_path,
+            "parameters": [
+                ENV, 
+                datalake_bucket, 
+                SOURCE, 
+                external_s3_bucket
+            ],
+        }
+    },
 )
 
-datalake_task_groups = task_group.build_task_group_from_sql_files(
-    layer=LayerEnum.REVERSE,
-    source_database_base_name=SOURCE,
-    target_database_base_name=SOURCE,
-    is_incremental=True,
-    partitions=partition_cols,
+execution_tracking_task = QuintoAndarDatabricksSubmitRunOperator(
+    task_id=f"load-execution-tracking",
+    dag=dag,
+    json={
+        "spark_python_task": {
+            "python_file": execution_tracking_spark_job_path,
+            "parameters": [
+                ENV, 
+                datalake_bucket, 
+                SOURCE, 
+                external_s3_bucket,
+                json.dumps(table_schema)
+            ],
+        }
+    },
 )
 
-chain(create_cluster_task, ReverseTaskGroup.all_first_tasks(datalake_task_groups))
-chain(ReverseTaskGroup.all_last_tasks(datalake_task_groups), terminate_cluster_task)
+chain(create_cluster_task, external_bucket_task)
+chain(external_bucket_task, execution_tracking_task)
+chain(execution_tracking_task, terminate_cluster_task)
