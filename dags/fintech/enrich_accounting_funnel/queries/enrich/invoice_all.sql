b0c2d8ff798853bbd3397a8dc9f22b05c14e787f
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-10-25 07:47:00 -0300

    changed multa danos and mra account_classification rule (#12585)

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -108,7 +108,8 @@ SELECT DISTINCT
     ELSE 'other'
   END AS account_type,
   CASE
-    WHEN entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc') THEN 'receivable'
+    WHEN entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc', 'property damage fine') THEN 'receivable'
+    WHEN entry_type IN ('rental anticipation fee') THEN 'payable'
     WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant') THEN 'receivable'
     WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'landlord' OR ie.to_account_type = 'landlord') THEN 'payable'
     WHEN (ROUND(-1.0*i.due_amount,2) > 0 OR (ROUND(-1.0*i.due_amount,2) = 0 AND NOT(from_account_type = 'landlord' OR to_account_type= 'landlord') )) THEN 'receivable'
