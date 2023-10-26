56bf0a951428b62d3f9e2ebd12edc75a6333c7eb
Thiago Felipe Nogueira De Jesus <thiago.jesus@quintoandar.com.br>
2023-10-26 13:48:13 -0300

    [GROW] Create demand prospect conversion events staging (#12594)
    * Create staging table

--- dags/growth/enrich_demand_conversion_events/enrich_demand_conversion_events.py
+++ dags/growth/enrich_demand_conversion_events/enrich_demand_conversion_events.py
@@ -4,6 +4,7 @@ from pendulum import timezone
 import re
 
 from airflow.models import DAG
+from airflow.utils.helpers import chain
 from airflow.operators.quintoandar_databricks import (
     QuintoAndarDatabricksCreateClusterOperator,
     QuintoAndarDatabricksTerminateClusterOperator,
@@ -12,6 +13,7 @@ from airflow.operators.quintoandar_databricks import (
 from bietlejuice.base.airflow.base_dag import BaseDAG
 from bietlejuice.base.airflow.dag_owner_enum import DAGOwnerEnum
 from bietlejuice.base.pipeline.layer_enum import LayerEnum
+from bietlejuice.base.airflow.helpers import TaskFlowHelper
 from bietlejuice.base.airflow.task_groups.datalake_task_group import DatalakeTaskGroup
 from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissionEnum
@@ -24,7 +26,7 @@ DAG_ID = f"bietlejuice.{DAG_NAME}"
 MAIN_START_DATE = datetime(2023, 9, 25, tzinfo=timezone("America/Sao_Paulo"))
 MAIN_SCHEDULE_INTERVAL = None
 CLUSTER_DESCRIPTION = "databricks_10_4_min_general_photon_cluster"
-PARTITION_COLS = ['id_event_date']
+
 
 config_service = ConfigurationService(DAG_NAME)
 datalake_bucket = config_service.get_config("datalake_bucket")
@@ -35,6 +37,9 @@ base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 cluster_configuration = config_service.get_config(CLUSTER_DESCRIPTION)
 default_libraries = config_service.get_config("default_libraries")
+table_task_group_parameters = config_service.get_config("table_task_group_parameters")
+
+INNER_DEPENDENCIES = config_service.get_config("inner_dependencies")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
@@ -85,22 +90,46 @@ datalake_task_group = DatalakeTaskGroup(
     spark_jobs_path=base_spark_jobs_path,
 )
 
-enrich_task_groups = datalake_task_group.build_task_group_from_sql_files(
-    layer=LayerEnum.ENRICH,
-    source_database_base_name=CONTEXT,
-    target_database_base_name=CONTEXT,
-    is_incremental=True,
-    partitions=PARTITION_COLS,
-    execution_date="",
-    extra_query_template_params={
-        "load_start_date": "{{ get_date_param(dag_run, macros.ds_add(ds, -60), 'load_start_date') }}",
-        "load_end_date": "{{ get_date_param(dag_run, ds, 'load_end_date') }}",
-    },
+tables = datalake_task_group._get_table_names_from_sql_files(layer=LayerEnum.ENRICH)
+
+enrich_task_groups = {}
+
+for table in tables:
+    enrich_task_groups[table] = datalake_task_group.build_enrich_task_group(
+        source_database_base_name=CONTEXT,
+        target_database_base_name=CONTEXT,
+        table_name=table,
+        partitions=table_task_group_parameters[table]["partition_cols"],
+        is_incremental=table_task_group_parameters[table]["is_incremental"],
+        has_create_external_table_task=False,
+        has_hive_sync=table_task_group_parameters[table]["has_hive_sync"],
+        extra_query_template_params={
+            "load_start_date": "{{ get_date_param(dag_run, macros.ds_add(ds, -60), 'load_start_date') }}",
+            "load_end_date": "{{ get_date_param(dag_run, ds, 'load_end_date') }}",
+        },
+
+    )
+(
+    task_groups_boundaries_without_inner_dependencies,
+    inner_dependencies_task_groups_boundaries,
+) = datalake_task_group.set_inner_dag_dependencies(
+    task_flow_helper=TaskFlowHelper(),
+    task_groups_boundaries=enrich_task_groups,
+    dag_inner_dependencies=INNER_DEPENDENCIES,
 )
 
-create_cluster_task.set_downstream(
-    DatalakeTaskGroup.all_first_tasks(enrich_task_groups)
+chain(
+    create_cluster_task,
+    datalake_task_group.all_first_tasks(
+        task_groups_boundaries_without_inner_dependencies
+    )
+    + datalake_task_group.first_tasks(inner_dependencies_task_groups_boundaries),
 )
-terminate_cluster_task.set_upstream(
-    DatalakeTaskGroup.all_last_tasks(enrich_task_groups)
-)
+
+chain(
+    datalake_task_group.all_last_tasks(
+        task_groups_boundaries_without_inner_dependencies
+    )
+    + datalake_task_group.last_tasks(inner_dependencies_task_groups_boundaries),
+    terminate_cluster_task,
+) 
