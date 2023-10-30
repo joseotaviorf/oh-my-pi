75eceee60c8dda03821b95d27a46971c8179c49b
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-10-30 17:07:48 -0300

    [DDSS-2735] New `message_voting` table on chat_fup (#12647)
    * feat: new table
    
    * refactor: fix column names and lineage

---
+++ dags/support_and_service/chat_fup/queries/clean/message_voting.sql
@@ -0,0 +1,12 @@
+SELECT
+    id,
+    message_id AS id_message,
+    content_id AS id_content,
+    session_id AS id_session,
+    user_id AS id_user,
+    taxonomy,
+    is_effective,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_chat_fup_raw.message_voting
