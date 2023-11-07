cb248ae12151a40bb398c432f2d9a4f3700fd957
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-11-07 10:40:27 -0300

    change postponement account classification rule (#12706)

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -110,8 +110,8 @@ SELECT DISTINCT
   CASE
     WHEN entry_type IN ('brokerage installment fee', 'brokerage loan fidc', 'brokerage fidc', 'property damage fine') THEN 'receivable'
     WHEN entry_type IN ('rental anticipation fee') THEN 'payable'
-    WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant') THEN 'receivable'
-    WHEN entry_type IN ('postponement', 'payment adjustment') AND (ie.from_account_type = 'landlord' OR ie.to_account_type = 'landlord') THEN 'payable'
+    WHEN entry_type IN ('payment adjustment') AND (ie.from_account_type = 'tenant' OR ie.to_account_type = 'tenant') THEN 'receivable'
+    WHEN entry_type IN ('payment adjustment') AND (ie.from_account_type = 'landlord' OR ie.to_account_type = 'landlord') THEN 'payable'
     WHEN (ROUND(-1.0*i.due_amount,2) > 0 OR (ROUND(-1.0*i.due_amount,2) = 0 AND NOT(from_account_type = 'landlord' OR to_account_type= 'landlord') )) THEN 'receivable'
     ELSE 'payable'
   END AS account_classification,
