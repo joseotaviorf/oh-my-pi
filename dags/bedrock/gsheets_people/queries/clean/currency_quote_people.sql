1d80c333aaf17f7f36cd5a2ff12438f73e951fb6
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2023-11-08 14:32:58 -0300

    Dbp/fix quote people (#12727)
    * 🐛 fix columns currency_quote_peolpe
    
    * 🐛 fix columns name cotacao

--- dags/bedrock/gsheets_people/queries/clean/currency_quote_people.sql
+++ dags/bedrock/gsheets_people/queries/clean/currency_quote_people.sql
@@ -1,6 +1,5 @@
 SELECT
-    FLOAT(euro) AS euro_cotation,
-    FLOAT(dolar) AS dolar_cotation,
-    FLOAT(mexico) AS mexican_peso_cotation,
+    moeda AS currency,
+    FLOAT(cotacao) AS cotation,
     ts_load
 FROM datalake_gsheets_raw.currency_quote_people
