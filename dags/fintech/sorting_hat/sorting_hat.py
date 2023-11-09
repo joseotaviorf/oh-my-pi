2fb392f64418fd132c0449c0ebac21415d6ec643
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2023-11-09 18:17:26 -0300

    [AEFR] Updating Sorting Hat schedule interval (#12763)

--- dags/fintech/sorting_hat/sorting_hat.py
+++ dags/fintech/sorting_hat/sorting_hat.py
@@ -46,7 +46,7 @@ DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
 # dag params
 local_tz = pendulum.timezone("America/Sao_Paulo")  # use cron expressions in local time
 MAIN_START_DATE = datetime(2019, 5, 31, 0, 0, 0, tzinfo=local_tz)
-MAIN_SCHEDULE_INTERVAL = "30 0 * * *"
+MAIN_SCHEDULE_INTERVAL = "0 0 * * *"
 
 dag = DAG(
     dag_id=DAG_ID,
