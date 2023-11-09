479894f12a6bf93eb0815233c2c98b977cfcf678
Bianca Garcia <biigarciam@gmail.com>
2023-11-09 16:35:42 -0300

    [DFFRS-375] Update classification of collection table (#12758)
    * add exemplo to agreement_type
    
    * make explicit in the contract table that all customer will be included in this table
    
    * update classification

--- dags/fintech/enrich_recupera/queries/enrich/collection.sql
+++ dags/fintech/enrich_recupera/queries/enrich/collection.sql
@@ -5,19 +5,62 @@ SELECT
     historical_code AS occurrence,
     occurence_description AS occurrence_description,
     CASE
-        WHEN historical_code IN ('ACORDO', 'ALEGAPGT', 'ALFRAUDE', 'EM FINAL', 'ESTRECEB', 'NEGOCIAC', 'PROBLE5A', 'PROMESSA', 'SEMPREVI', 'QUITEI', 'PROEMAIL', 'PAGO','EMPROCES', 'PROWHATS', 'DISCORDA', 'PROLIGAC', 'RETCLIEN', 'CDESCONH', 'RECADO', 'NATENDE', 'OCUPADO','CAIXAPOS', 'ALTVIGENCIA', 'QUEBRAPRO','EM PROCESS','QBRACOR', 'TENTESGO', 'CDESCONH','QUEBRAPR','RETORTER','PASCHOAL','ALTVIGEN','EMISBOL','GERALINK','ENVIPROP','NCONFIR','PAGOCBX','PAGOSBX','ABANDONO') THEN 1 ELSE 0
+      WHEN id_creditor IN (3,5)
+        AND historical_code IN ('ACORDO', 'ALEGAPGT', 'ALFRAUDE', 'ALTFONE', 'CDESCONH', 'EM FINAL', 'EMISBOL', 'EMISPIX', 'NEGOCIAC', 'PAGO', 'PROLIGAC', 'PROWHATS', 'RECADO',
+        'RETCLIEN', 'SEMPREVI', 'VACORDO', 'VDIMOVEL', 'VDISCORD', 'VPINSUFI', 'VPROACOR', 'VRECADO', 'VSCONDIC','CAIXAPOS', 'CLNLOCAL', 'LIGAMUDA', 'NATENDE', 'OCUPADO', 'QUEDALIG', 'TENTESGO', 'VELOATIV', 'VSCONTAT','ALEGAPGT', 'PROLIGAC', 'RECADO', 'VMENSENV')
+        THEN 1
+      ELSE 0
+    END AS esforco,
+    CASE
+        WHEN historical_code IN ('ACORDO', 'ALEGAPGT', 'ALFRAUDE', 'EM FINAL', 'NEGOCIAC', 'SEMPREVI',
+        'PAGO','CDESCONH','EMISBOL', 'PROWHATS', 'PROLIGAC', 'RECADO', 'RETCLIEN')
+          THEN 1
+
+        WHEN id_creditor IN (1, 2, 4, 6, 7, 8, 9)
+            AND historical_code IN ('QUITEI', 'ESTRECEB', 'PROBLE5A', 'PROMESSA', 'PROEMAIL', 'EMPROCES', 'DISCORDA',
+            'NATENDE', 'OCUPADO','CAIXAPOS', 'ALTVIGENCIA', 'QUEBRAPRO', 'EM PROCESS', 'QBRACOR', 'TENTESGO', 'QUEBRAPR',
+            'RETORTER','PASCHOAL','ALTVIGEN','GERALINK', 'ENVIPROP','NCONFIR','PAGOCBX','PAGOSBX','ABANDONO')
+          THEN 1
+
+        WHEN id_creditor IN (3,5)
+         AND historical_code IN ('ALTFONE', 'EMISPIX', "VACORDO", "VDIMOVEL", "VDISCORD", "VPINSUFI", "VPROACOR", "VRECADO", "VSCONDIC")
+          THEN 1
+        ELSE 0
     END AS alo,
     CASE
-        WHEN historical_code IN ('ACORDO', 'ALEGAPGT', 'ALFRAUDE', 'EM FINAL', 'ESTRECEB', 'NEGOCIAC', 'PROBLE5A', 'PROMESSA', 'SEMPREVI', 'QUITEI', 'PAGO', 'EMPROCES', 'DISCORDA', 'RETCLIEN', 'PROEMAIL', 'PROWHATS', 'PROLIGAC', 'ALTVIGENCIA', 'QUEBRAPRO', 'EM PROCESS', 'QBRACOR', 'TENTESGO','QUEBRAPR','PASCHOAL','ALTVIGEN','EMISBOL','GERALINK','ENVIPROP','NCONFIR','PAGOCBX','PAGOSBX','ABANDONO') THEN 1 ELSE 0
+        WHEN historical_code IN ('ACORDO', 'ALEGAPGT', 'EMISBOL', 'NEGOCIAC', 'PAGO', 'PROLIGAC', 'PROWHATS',
+            'SEMPREVI', 'PROEMAIL')
+            THEN 1
+        WHEN id_creditor IN (1, 2, 4, 6, 7, 8, 9)
+            AND historical_code IN ('ALFRAUDE', 'EM FINAL', 'ESTRECEB', 'PROBLE5A', 'PROMESSA',
+            'QUITEI', 'EMPROCES', 'DISCORDA', 'RETCLIEN', 'ALTVIGENCIA', 'QUEBRAPRO', 'EM PROCESS',
+            'QBRACOR', 'TENTESGO','QUEBRAPR','PASCHOAL', 'ALTVIGEN', 'GERALINK','ENVIPROP','NCONFIR',
+            'PAGOCBX','PAGOSBX','ABANDONO')
+            THEN 1
+        WHEN id_creditor IN (3,5)
+            AND historical_code IN ('ALTFONE', 'EMISPIX', 'VACORDO', 'VDIMOVEL', 'VDISCORD',
+                'VPINSUFI', 'VPROACOR', 'VSCONDIC', 'VELOASSE', 'VELOIAF')
+            THEN 1
+        ELSE 0
     END AS cpc,
     CASE
-        WHEN historical_code IN ('PROMESSA','PROEMAIL','PROWHATS','PROLIGAC') THEN 1 ELSE 0
+        WHEN id_creditor IN (1, 2, 4, 6, 7, 8, 9)
+            AND historical_code IN ('PROMESSA','PROEMAIL','PROWHATS','PROLIGAC') THEN 1 ELSE 0
     END AS promisse,
     CASE
-        WHEN historical_code IN ('ACORDO','EMISBOL','GERALINK') THEN 1 ELSE 0
+        WHEN historical_code IN ('ACORDO','EMISBOL')
+            THEN 1
+        WHEN id_creditor IN (1, 2, 4, 6, 7, 8, 9)
+            AND historical_code IN ('GERALINK')
+            THEN 1
+        WHEN id_creditor IN (3,5)
+            AND historical_code IN ('EMISPIX', 'VACORDO')
+            THEN 1
+         ELSE 0
     END AS agreement,
     CASE
-        WHEN historical_code IN ('CLNLOCAL','NATENDE','OCUPADO','FAX','LIGAMUDA','QUEDALIG') THEN 1 ELSE 0
+        WHEN id_creditor IN (1, 2, 4, 6, 7, 8, 9)
+            AND historical_code IN ('CLNLOCAL','NATENDE','OCUPADO','FAX','LIGAMUDA','QUEDALIG') THEN 1 ELSE 0
     END AS failure,
     ts_occurrence,
     NOW() AS ts_load,
