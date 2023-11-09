cad4d54faedf4217e2ced9a71d60a02b677bc782
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2023-11-09 16:21:49 -0300

    upating query (#12759)

--- dags/for_rent/metric_rent__demand_events/queries/metric/cohort_conversions.sql
+++ dags/for_rent/metric_rent__demand_events/queries/metric/cohort_conversions.sql
@@ -24,7 +24,7 @@ SELECT
     CAST(rc.ep_converted_ds/rd.evaluation_positive AS DECIMAL (2,2)) AS ep2ds,
     CAST(rc.ds_converted_ca/rd.documentation_sent AS DECIMAL (2,2)) AS ds2ca,
     CAST(rc.ca_converted_cs/rd.credit_approved AS DECIMAL (2,2)) AS ca2cs,
-    CAST(rc.cc_converted_cs/rd.contract_created AS DECIMAL (2,2)) AS cc2cs,
+    CAST(rc.cc_converted_cs/rd.contracts_created AS DECIMAL (2,2)) AS cc2cs,
     rc.has_guarantee,
     rc.dt_event,
     rc.dt_week_started,
