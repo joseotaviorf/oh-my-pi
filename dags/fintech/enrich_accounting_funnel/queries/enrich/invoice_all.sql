9244b103c780403acbfc402176dc7e7079c600cf
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-11-13 17:41:26 -0300

    added condominium 5A paid rule  in to_account_type (#12797)

--- dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/invoice_all.sql
@@ -191,5 +191,5 @@ WHERE
     AND ( (ie.from_account_type IN ('contract', 'tenant','landlord')) OR 
         (ie.from_account_type = 'contract expenses' AND ie.entry_type IN ('condominium fine', 'condominium 5A paid')))
     AND ( (ie.to_account_type IN ('contract', 'tenant','landlord')) OR
-        (ie.to_account_type = 'quinto andar' AND ie.entry_type IN ('condominium' , 'condominium usage', 'condominium defaulting', 'condominium fine')) OR
+        (ie.to_account_type = 'quinto andar' AND ie.entry_type IN ('condominium' , 'condominium usage', 'condominium defaulting', 'condominium fine', 'condominium 5A paid')) OR
         (ie.to_account_type = 'contract expenses' AND ie.entry_type IN ('postponement', 'condominium fine', 'condominium 5A paid')))
