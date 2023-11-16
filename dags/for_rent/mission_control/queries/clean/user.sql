3715308fd992a2cb218c83421dbcdd48b93305c4
Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
2023-11-16 16:31:47 -0300

    [AEFR-3606] Adding ContractPerson and User table to raw and clean layer (#12826)
    * [AEFR-3606] Adding ContractPerson and User table to raw and clean layer
    
    * [AEFR-3606] Sintax adjustment
    
    ---------
    
    Co-authored-by: Allan Moura <allan.melao@quintoandar.com>

---
+++ dags/for_rent/mission_control/queries/clean/user.sql
@@ -0,0 +1,16 @@
+SELECT
+	id AS id_user,
+	name AS full_name,
+	phone AS phone_number,
+	deleted AS is_deleted,
+	created_on AS ts_created,
+	updated_on AS ts_updated,
+	year,
+	month,
+	day
+FROM
+	datalake_mission_control_raw.user
+WHERE
+	year = {year}
+	AND month = {month}
+	AND day = {day}
