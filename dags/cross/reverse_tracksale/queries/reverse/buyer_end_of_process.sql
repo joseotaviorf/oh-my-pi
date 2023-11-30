c9b04bf68902fc8f10298dcf023560a1089cc43b
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2023-11-30 19:57:30 -0300

    [DFORSALE-7005]  - Update NPS's campaigns filters and turn off a campaign (#12974)

--- dags/cross/reverse_tracksale/queries/reverse/buyer_end_of_process.sql
+++ dags/cross/reverse_tracksale/queries/reverse/buyer_end_of_process.sql
@@ -128,5 +128,5 @@ FROM
     customers_info
 WHERE
   (ts_house_registry_ended IS NOT NULL AND DATEDIFF(CURRENT_DATE, ts_house_registry_ended) = 2)
-  OR ((payment_method IS NULL OR payment_method  = 'FINANCED') AND ts_house_registry_ended IS NULL AND DATEDIFF(CURRENT_DATE, dt_event) = 114)
-  OR (payment_method  = 'CASH' AND ts_house_registry_ended IS NULL AND DATEDIFF(CURRENT_DATE, dt_event) = 45)
+  OR ((payment_method IS NULL OR payment_method LIKE 'FINANCED%') AND ts_house_registry_ended IS NULL AND DATEDIFF(CURRENT_DATE, dt_event) = 114)
+  OR (payment_method LIKE 'CASH%' AND ts_house_registry_ended IS NULL AND DATEDIFF(CURRENT_DATE, dt_event) = 45)
