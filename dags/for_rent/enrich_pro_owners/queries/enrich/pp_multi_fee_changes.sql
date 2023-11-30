4dfcb16659d4d5e75d51c9eef87d1e9d1802687c
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2023-11-30 15:44:59 -0300

    fixing typo (#12963)

--- dags/for_rent/enrich_pro_owners/queries/enrich/pp_multi_fee_changes.sql
+++ dags/for_rent/enrich_pro_owners/queries/enrich/pp_multi_fee_changes.sql
@@ -1,7 +1,7 @@
 WITH administration_fee_changes AS (
   SELECT
     upo.id_user AS id_owner,
-    aud.adm_fee AS adminstration_fee,
+    aud.adm_fee AS administration_fee,
     LAG(aud.adm_fee) OVER (PARTITION BY upo.id_user ORDER BY aud.rev) AS previous_administration_fee,
     aud.is_active,
     LAG(aud.is_active) OVER (PARTITION BY upo.id_user ORDER BY aud.rev) AS previous_is_active,
@@ -19,7 +19,7 @@ WITH administration_fee_changes AS (
 
 SELECT
   id_owner,
-  adminstration_fee,
+  administration_fee,
   is_active,
   MAX(ts_revision) OVER(PARTITION BY id_owner, DATE(ts_revision)) = ts_revision AS is_last_status_of_day,
   ts_revision AS ts_adm_fee_started,
@@ -27,5 +27,5 @@ SELECT
 FROM
   administration_fee_changes
 WHERE
-  (adminstration_fee <> previous_administration_fee OR previous_administration_fee IS NULL)
+  (administration_fee <> previous_administration_fee OR previous_administration_fee IS NULL)
   OR (is_active <> previous_is_active OR previous_is_active IS NULL)
