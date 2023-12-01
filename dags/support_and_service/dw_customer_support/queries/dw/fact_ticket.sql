954f1223b7f25b4c051904e82e58d131ee8bc4a0
William Angelo <39243586+williamangelo@users.noreply.github.com>
2023-12-01 15:35:57 -0300

    fix: coma (#12994)

--- dags/support_and_service/dw_customer_support/queries/dw/fact_ticket.sql
+++ dags/support_and_service/dw_customer_support/queries/dw/fact_ticket.sql
@@ -48,8 +48,8 @@ WITH call_tickets AS (
         'Rescisão - Despejo [OFF][POS][BACK]',
         'Rescisão 1 [OFF] [POS] [BACK]'
       )
-      AND direction IN ('inbound', 'outbound-api'),
-      AND channel_type = 'call-in-app'
+      AND direction IN ('inbound', 'outbound-api')
+      AND channel_type = 'call-in-app',
       TRUE,
       FALSE
     ) AS is_ticket_rate,
