3b08e6a6176f0125c0d711614af77db0cfbe64cd
Julia Rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2023-12-11 11:37:48 -0300

    [din-714] Create crm_test dag on new Dag Builder (#13020)
    * Renamed data quality method
    
    * Moved _get_dag_execution_context method
    
    * Added RawJdbcWorkflow
    
    * Added load_raw_task as final task
    
    * Set bucket as an arg in _get_dag_execution_context
    
    * Added typing for Tuple
    
    * Removed RawJdbcWorkflow from this branch
    
    * Applied lint
    
    * Added RawJdbcWorkflow
    
    * Renamed to DatabasePull
    
    * Applied lint
    
    * create crm dag declaration
    
    * fix workflow get database_type
    
    * fix double quotes
    
    * fix load_database_pull_raw_task_creator call
    
    * linter
    
    * add all tables to crm_test dag
    
    * add workgroups table
    
    * Added get_database method into factory
    
    * Created DATABASE_PULL_WORKFLOW
    
    * Created RawDatabasePullWorkflow
    
    * Added RawDatabasePullWorkflow into RawFactory
    
    * Changed error message
    
    * create metadata and queries folders
    
    * adjusting metadata and queries for crm_test
    
    * Added load_query_clean_task_creator instance
    
    * add schedule_start_date
    
    * Added database_type into DAGDeclarationValidator
    
    * add schedule interval
    
    * add todo to custom cluster
    
    * set full load as default
    
    * add custom cluster
    
    * remove custom cluster config
    
    * add custom cluster
    
    ---------
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>

---
+++ dags/platform/crm_test/queries/clean/tasks.sql
@@ -0,0 +1,44 @@
+SELECT
+    _id AS id,
+    origemId AS id_origin,
+    assigneeId AS id_assignee,
+    imovelId AS id_house,
+    openedById AS id_opened_by,
+    inquilinoId AS id_tenant,
+    negociacaoId AS id_negotiation,
+    gerenteId AS id_manager,
+    proprietarioId AS id_owner,
+    destinatarioId AS id_receiver,
+    workgroupId AS id_workgroup,
+    __v AS version,
+    actions,
+    score,
+    scoreFactor AS score_factor,
+    nomeDestinatario AS receiver_name,
+    comentario AS task_comment,
+    origem AS origin,
+    type,
+    tipoDestinatario AS receiver_type,
+    descricao AS description,
+    fase AS phase,
+    dataInicio AS start_date_object,
+    origemData AS origin_date_object,
+    realizadaEm AS completed_date_object,
+    silenciadaAte AS silenced_until_date_object,
+    dataCriacao AS created_date_object,
+    followUpVisita AS visit_fup,
+    CAST(dataFup AS DOUBLE) AS unix_fup,
+    startedAt AS analyst_started,
+    tags,
+    metadata,
+    resolvida AS is_resolved,
+    DATE(dataVisita) AS dt_visit,
+    year,
+    month,
+    day
+FROM
+    datalake_crm_test_raw.tasks
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
