207cff7b360b499ba86766fb54fafc7228fdba8d
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2023-12-11 17:16:26 -0300

    [DFORSALE-7043] Add new column to pre analysis atta dimension (#13033)

--- dags/for_sale/dw_atta/queries/dw/dim_pre_analysis_atta.sql
+++ dags/for_sale/dw_atta/queries/dw/dim_pre_analysis_atta.sql
@@ -1,17 +1,25 @@
 SELECT
-    id_pre_analysis      AS sk_pre_analysis,
-    id_offer             AS sk_offer,
-    `status`             AS sk_pre_analysis_status,
-    priority_status      AS sk_priority_status,
-    situation            AS sk_pre_analysis_situation,
+    id_pre_analysis AS sk_pre_analysis,
+    id_offer AS sk_offer,
+    `status` AS sk_pre_analysis_status,
+    priority_status AS sk_priority_status,
+    situation AS sk_pre_analysis_situation,
     archive_reason,
     archive_complement,
+    CASE
+      WHEN created_source == 0 THEN 'iSolve'
+      WHEN created_source == 1 THEN 'Partner Link'
+      WHEN created_source == 2 THEN 'Buyer Offer Sent'
+      WHEN created_source == 3 THEN 'Cash Converted'
+      WHEN created_source == 4 THEN 'Recreated'
+      ELSE created_source
+    END AS created_source_description,
     down_payment_amount,
     financing_value,
     house_value,
     is_archived,
     ts_archived,
     ts_registration,
-    NOW()                AS ts_load
+    NOW() AS ts_load
 FROM
     datalake_atta_clean.pre_analysis
