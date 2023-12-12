0ffbca7066168bfe499495b059d877de79264b05
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2023-12-12 10:17:24 -0300

    Update enrich cib_segmentation (#13003)
    * Add id_segmentation column
    
    * Fix dt_started for months_calculation of 2
    
    * Cast id_cib to integer
    
    * Update segmentation rule
    
    * Update partition columns
    
    * Fix yml columns order
    
    * Fix fl and cs lineage

--- dags/benvi/enrich_mexico_cib_segmentation/queries/enrich/cib_segmentation.sql
+++ dags/benvi/enrich_mexico_cib_segmentation/queries/enrich/cib_segmentation.sql
@@ -1,6 +1,6 @@
 WITH dim_cib AS (
     SELECT
-        GET_JSON_OBJECT(a.details, '$.userExternalId') AS id_user,
+        CAST(GET_JSON_OBJECT(a.details, '$.userExternalId') AS INTEGER) AS id_user,
         DATE(GET_JSON_OBJECT(a.details, '$.registeredAt')) AS dt_registered
     FROM
         datalake_big_agent.agent AS a
@@ -13,77 +13,89 @@ WITH dim_cib AS (
 base_months_since_registration AS (
     SELECT DISTINCT
         dc.id_user,
+        dc.dt_registered,
         ROUND(MONTHS_BETWEEN(ad.month_end, dc.dt_registered), 1) AS months_registered,
         ADD_MONTHS(ad.month_start, 1) AS dt_month_started_segmentation,
         GREATEST(ad.month_start, dc.dt_registered) AS dt_started,
-        ad.month_end AS dt_ended,
-        ad.year,
-        ad.month
+        ad.month_end AS dt_ended
     FROM
         datalake_quintoandar.aux_date AS ad
     INNER JOIN
         dim_cib AS dc
             ON ad.month_start >= DATE_TRUNC('month' , dt_registered)
-            AND ad.month_start <= MAKE_DATE({year}, {month}, '01')
+            AND ad.month_start <= MAKE_DATE({year}, {month}, 01)
     WHERE
         dc.id_user IS NOT NULL
-        AND ad.month_start >= GREATEST(ADD_MONTHS(MAKE_DATE({year}, {month}, '01'), -1), DATE_TRUNC('month', dt_registered))
+        AND ad.month_start >= GREATEST(ADD_MONTHS(MAKE_DATE({year}, {month}, 01), -1), DATE_TRUNC('month', dt_registered))
 ),
 events_by_month AS (
     SELECT
         bmsr.id_user,
         SUM(IF(ce.event_type = 'FL', 1, 0)) AS first_listings,
         SUM(IF(ce.event_type = 'CS', 1, 0)) AS contracts_signed,
-        bmsr.year,
-        bmsr.month
+        bmsr.dt_started
     FROM
         base_months_since_registration AS bmsr
     LEFT JOIN
         datalake_mexico_cib_events.cib_events AS ce
-            ON ce.year = bmsr.year
-            AND ce.month = bmsr.month
-            AND ce.id_cib = bmsr.id_user
+            ON ce.id_cib = bmsr.id_user
+            AND DATE_TRUNC('month', ce.dt_event) = bmsr.dt_started
     GROUP BY
-        1, 4, 5
+        1, 4
 ),
 segmentation_rule_calculation AS (
     SELECT
         bmsr.id_user AS id_cib,
         'AVG' AS type_calculation,
-        CASE
-            WHEN bmsr.months_registered < 2 THEN AVG(COALESCE(ebm.first_listings, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 0 PRECEDING AND CURRENT ROW)
-            ELSE AVG(COALESCE(ebm.first_listings, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW)
-        END AS avg_fl,
-        CASE
-            WHEN bmsr.months_registered < 2 THEN AVG(COALESCE(ebm.contracts_signed, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 0 PRECEDING AND CURRENT ROW)
-            ELSE AVG(COALESCE(ebm.contracts_signed, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW)
-        END AS avg_cs,
+        IF(
+            bmsr.months_registered <= 2,
+            AVG(COALESCE(ebm.first_listings, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 0 PRECEDING AND CURRENT ROW),
+            AVG(COALESCE(ebm.first_listings, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW)
+        ) AS avg_fl,
+        IF(
+            bmsr.months_registered <= 2,
+            AVG(COALESCE(ebm.contracts_signed, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 0 PRECEDING AND CURRENT ROW),
+            AVG(COALESCE(ebm.contracts_signed, 0)) OVER (PARTITION BY bmsr.id_user ORDER BY bmsr.dt_started ASC ROWS BETWEEN 1 PRECEDING AND CURRENT ROW)
+        ) AS avg_cs,
         bmsr.months_registered,
+        IF(months_registered <= 2, 1, 2) AS months_calculation,
         bmsr.dt_month_started_segmentation,
-        bmsr.dt_started,
+        IF(months_registered <= 2, bmsr.dt_started, GREATEST(ADD_MONTHS(bmsr.dt_started, -1), bmsr.dt_registered)) AS dt_started,
         bmsr.dt_ended,
-        bmsr.year,
-        bmsr.month
+        YEAR(dt_month_started_segmentation) AS year,
+        MONTH(dt_month_started_segmentation) AS month
     FROM
         base_months_since_registration AS bmsr
     INNER JOIN
         events_by_month AS ebm
-            ON ebm.year = bmsr.year
-            AND ebm.month = bmsr.month
-            AND ebm.id_user = bmsr.id_user
+            ON ebm.id_user = bmsr.id_user
+            AND ebm.dt_started = bmsr.dt_started
 )
 SELECT
     id_cib,
+    CASE
+        WHEN months_registered <= 2 THEN 1
+        ELSE
+          CASE
+            WHEN avg_fl >= 5 AND avg_cs >= 1 THEN 2
+            WHEN avg_fl > 1 THEN 1
+            ELSE 0
+          END
+    END AS id_segmentation,
     type_calculation,
     CASE
-        WHEN avg_fl >= 5 AND avg_cs >= 1 THEN 'Elite'
-        WHEN avg_fl > 1 THEN 'Plus'
-        ELSE 'Inter'
+        WHEN months_registered <= 2 THEN 'Plus'
+        ELSE
+          CASE
+            WHEN avg_fl >= 5 AND avg_cs >= 1 THEN 'Elite'
+            WHEN avg_fl > 1 THEN 'Plus'
+            ELSE 'Inter'
+          END
     END AS segmentation,
     avg_fl AS first_listings,
     avg_cs AS contracts_signed,
     months_registered,
-    IF(months_registered < 2, 1, 2) AS months_calculation,
+    months_calculation,
     dt_month_started_segmentation,
     dt_started,
     dt_ended,
@@ -92,5 +104,4 @@ SELECT
 FROM
     segmentation_rule_calculation
 WHERE
-    year = {year}
-    AND month = {month}
+    dt_month_started_segmentation = MAKE_DATE({year},{month},{day}) + INTERVAL 1 DAY
