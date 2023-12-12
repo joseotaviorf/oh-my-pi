20ab5ad3ff3e0c15c7b5d908139a5e3e9126bd4e
gustavohalencar <90861839+gustavohalencar@users.noreply.github.com>
2023-12-12 10:37:50 -0300

    [DRO] Adding new files for rental supply targets (#13035)
    * Adding new files for supply targets
    
    * Separating supply_targets_retro into 1 and 2
    
    * Update dags/cross/gsheets_static/metadata/clean/supply_targets_retro_cohort.yml
    
    * Update dags/cross/gsheets_static/queries/clean/supply_targets_retro_cohort.sql
    
    * Update dags/for_rent/gsheets_for_rent/metadata/clean/supply_targets_2024_cohort.yml
    
    * Update dags/for_rent/gsheets_for_rent/queries/clean/supply_targets_2024_cohort.sql
    
    * Update dags/cross/gsheets_static/queries/clean/supply_targets_retro_1.sql
    
    * Update dags/cross/gsheets_static/queries/clean/supply_targets_retro_2.sql
    
    * Update dags/for_rent/gsheets_for_rent/queries/clean/supply_targets_2024.sql
    
    ---------
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ dags/cross/gsheets_static/queries/clean/supply_targets_retro_1.sql
@@ -0,0 +1,14 @@
+SELECT
+    city_group,
+    lead_processing_operation,
+    supply_mkt_origin_detailed,
+    planning_taxonomy,
+    rental_administrator,
+    prospects,
+    qualifieds,
+    opportunities,
+    available_qualifieds,
+    first_listings,
+    TO_DATE(date, 'yyyy-MM-dd') AS dt_target
+FROM
+    datalake_gsheets_raw.supply_targets_retro_1
---
+++ dags/for_rent/gsheets_for_rent/queries/clean/supply_targets_2024_cohort.sql
@@ -0,0 +1,16 @@
+SELECT
+    city_group,
+    planning_taxonomy,
+    supply_mkt_origin_detailed,
+    lead_processing_operation,
+    rental_administrator,
+    weeks_conversion,
+    op2q,
+    q2opp,
+    opp2fl,
+    fl,
+    q2avq,
+    avq2opp,
+    TO_DATE(week_start, 'yyyy-MM-dd') AS dt_week_started
+FROM
+    datalake_gsheets_raw.supply_targets_2024_cohort
