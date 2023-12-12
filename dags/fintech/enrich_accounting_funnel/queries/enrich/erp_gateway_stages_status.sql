999a5aed97f8388fe6672d91abb1c89325c2625e
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2023-12-12 14:10:24 -0300

    Aeff adding new columns in erp gateway stages status (#13048)
    * added new columns in erp_gateway_stages_status enrich table
    
    * changed columns order

--- dags/fintech/enrich_accounting_funnel/queries/enrich/erp_gateway_stages_status.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/erp_gateway_stages_status.sql
@@ -1,16 +1,26 @@
 SELECT 
-  f.id_feature,
-  s.hash,
   f.id_source,
   f.id_finance_entity,
+  f.id_feature,
   s.id_sync_sap_job,
-  f.source as feature_source,
+  s.hash,
+  f.source_client,
+  f.source as source_type,
+  f.user_type, 
+  f.finance_entity_type, 
+  f.transaction_type,
   s.type as sync_sap_job_type,
   f.sync_sap_status, 
   s.status as sync_sap_job_status,
+  CASE
+    WHEN f.ts_created IS NOT NULL AND s.ts_created IS NULL AND s.ts_synced IS NULL THEN 'sap job not created'
+    WHEN f.ts_created IS NOT NULL AND s.ts_created IS NOT NULL AND s.ts_synced IS NULL THEN 'sap job not synced'
+    WHEN f.ts_created IS NOT NULL AND s.ts_created IS NOT NULL AND s.ts_synced IS NOT NULL THEN 'synced'
+    END AS stage_status,
   DATE(f.ts_created) as dt_feature_created,
   DATE(s.ts_created) as dt_sync_sap_job_created,
-  DATE(s.ts_synced) as dt_sync_sap_job_synced
+  DATE(s.ts_synced) as dt_sap_job_synced,
+  DATE(COALESCE(s.ts_updated, f.ts_updated)) AS dt_updated
 FROM
   datalake_sap_gateway.feature f
 LEFT JOIN
@@ -21,3 +31,5 @@ WHERE
     type != 'PN' OR 
     type IS NULL
   )
+AND 
+  f.ts_created >= '2023-01-01'
