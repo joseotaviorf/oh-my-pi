91a48e1c08453447eb72c2e474145e418039dab7
gustavohalencar <90861839+gustavohalencar@users.noreply.github.com>
2023-12-12 13:41:27 -0300

    [DRO] Adding new files for rental demand targets (#13038)
    * Adding new files for demand targets
    
    * Small fix
    
    * Small fix 2

---
+++ dags/for_rent/gsheets_for_rent/queries/clean/demand_targets_2024.sql
@@ -0,0 +1,17 @@
+SELECT
+    city_group,
+    business_type,
+    rent_flow_origin,
+    rental_administrator,
+    visits_booked,
+    visits_completed,
+    offers_submitted,
+    offers_accepted,
+    evaluation_started,
+    evaluation_positive,
+    documentation_sent,
+    credit_approved,
+    contracts_signed,
+    TO_DATE(date, 'yyyy-MM-dd') AS dt_target
+FROM
+    datalake_gsheets_raw.demand_targets_2024
---
+++ dags/for_rent/gsheets_for_rent/queries/clean/demand_targets_2024_cohort.sql
@@ -0,0 +1,16 @@
+SELECT
+    city_group,
+    rental_administrator,
+    business_type,
+    rent_flow_origin,
+    weeks_conversion,
+    vb2vc,
+    vc2os,
+    os2oa,
+    oa2ca,
+    ca2cs,
+    cs,
+    vb2os,
+    TO_DATE(week_start, 'yyyy-MM-dd') AS dt_week_started
+FROM
+    datalake_gsheets_raw.demand_targets_2024_cohort
