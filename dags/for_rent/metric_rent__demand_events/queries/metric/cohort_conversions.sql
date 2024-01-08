011297e3c5030381ee07a6d782d7a746b6fcfd6e
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-01-08 14:47:10 -0300

    [AEFR-3737] Adjusting calucation of cohort conversions on metric layer (#13079)
    * [AEFR-3737] Adjusting calucation of cohort conversions on metric layer
    
    * fixing query
    
    * coalesce
    
    * filter
    
    * fixing query
    
    * Update dags/for_rent/metric_rent__demand_events/queries/metric/cohort_conversions.sql
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
    
    * adjusting group by
    
    * decimal
    
    * reordering
    
    ---------
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>

--- dags/for_rent/metric_rent__demand_events/queries/metric/cohort_conversions.sql
+++ dags/for_rent/metric_rent__demand_events/queries/metric/cohort_conversions.sql
@@ -1,52 +1,125 @@
+WITH unconverted_events AS (
+  /** As unconverted events will always have weeks_to_conversion = -1, we need to group it separately
+    so we can join it later. **/
+    SELECT
+        rc.country_code,
+        rc.halfyear,
+        rc.quarter,
+        rc.business_type,
+        rc.city_group,
+        rc.tier,
+        rc.listing_category_start,
+        rc.rent_flow_origin,
+        rc.rental_administrator,
+        COALESCE(SUM(rc.vb_unconverted), 0) AS vb_unconverted,
+        COALESCE(SUM(rc.vc_unconverted), 0) AS vc_unconverted,
+        COALESCE(SUM(rc.os_unconverted), 0) AS os_unconverted,
+        COALESCE(SUM(rc.oa_unconverted), 0) AS oa_unconverted,
+        COALESCE(SUM(rc.es_unconverted), 0) AS es_unconverted,
+        COALESCE(SUM(rc.ep_unconverted), 0) AS ep_unconverted,
+        COALESCE(SUM(rc.ds_unconverted), 0) AS ds_unconverted,
+        COALESCE(SUM(rc.ca_unconverted), 0) AS ca_unconverted,
+        COALESCE(SUM(rc.cc_unconverted), 0) AS cc_unconverted,
+        rc.has_guarantee,
+        rc.dt_event,
+        rc.dt_week_started
+    FROM
+        dw_rent_snapshot.rent_cohort_conversions_snapshot AS rc
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND weeks_to_conversion = -1
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 19, 20, 21
+),
+base AS (
+    SELECT
+        rc.country_code,
+        rc.halfyear,
+        rc.quarter,
+        rc.business_type,
+        rc.city_group,
+        rc.tier,
+        rc.listing_category_start,
+        rc.rent_flow_origin,
+        rc.rental_administrator,
+        rc.weeks_to_conversion,
+        COALESCE(SUM(rc.vb_converted_vc), 0) AS vb_converted_vc,
+        COALESCE(SUM(rc.vb_converted_os), 0) AS vb_converted_os,
+        COALESCE(SUM(rc.vb_converted_oa), 0) AS vb_converted_oa,
+        COALESCE(SUM(rc.vb_converted_cs), 0) AS vb_converted_cs,
+        COALESCE(SUM(rc.vc_converted_os), 0) AS vc_converted_os,
+        COALESCE(SUM(rc.os_converted_oa), 0) AS os_converted_oa,
+        COALESCE(SUM(rc.os_converted_cs), 0) AS os_converted_cs,
+        COALESCE(SUM(rc.oa_converted_es), 0) AS oa_converted_es,
+        COALESCE(SUM(rc.oa_converted_ds), 0) AS oa_converted_ds,
+        COALESCE(SUM(rc.oa_converted_ca), 0) AS oa_converted_ca,
+        COALESCE(SUM(rc.oa_converted_cs), 0) AS oa_converted_cs,
+        COALESCE(SUM(rc.es_converted_ep), 0) AS es_converted_ep,
+        COALESCE(SUM(rc.es_converted_ca), 0) AS es_converted_ca,
+        COALESCE(SUM(rc.ep_converted_ds), 0) AS ep_converted_ds,
+        COALESCE(SUM(rc.ds_converted_ca), 0) AS ds_converted_ca,
+        COALESCE(SUM(rc.ca_converted_cc), 0) AS ca_converted_cc,
+        COALESCE(SUM(rc.ca_converted_cs), 0) AS ca_converted_cs,
+        COALESCE(SUM(rc.cc_converted_cs), 0) AS cc_converted_cs,
+        rc.has_guarantee,
+        rc.dt_event,
+        rc.dt_week_started
+    FROM
+        dw_rent_snapshot.rent_cohort_conversions_snapshot AS rc
+    WHERE
+        year = YEAR(NOW())
+        AND month = MONTH(NOW())
+        AND day = DAY(NOW())
+        AND weeks_to_conversion > -1
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 29, 30, 31
+)
 SELECT
-    rc.halfyear,
-    rc.quarter,
-    rc.business_type,
-    rc.city_group,
-    rc.tier,
-    rc.listing_category_start,
-    rc.rent_flow_origin,
-    rc.rental_administrator,
-    rc.weeks_to_conversion,
-    CAST(rc.vb_converted_vc/rd.visits_booked AS DECIMAL (2,2)) AS vb2vc,
-    CAST(rc.vb_converted_os/rd.visits_booked AS DECIMAL (2,2)) AS vb2os,
-    CAST(rc.vb_converted_oa/rd.visits_booked AS DECIMAL (2,2)) AS vb2oa,
-    CAST(rc.vb_converted_cs/rd.visits_booked AS DECIMAL (2,2)) AS vb2cs,
-    CAST(rc.vc_converted_os/rd.visits_completed AS DECIMAL (2,2)) AS vc2os,
-    CAST(rc.os_converted_oa/rd.offers_submitted AS DECIMAL (2,2)) AS os2oa,
-    CAST(rc.os_converted_cs/rd.offers_submitted AS DECIMAL (2,2)) AS os2cs,
-    CAST(rc.oa_converted_es/rd.offers_accepted AS DECIMAL (2,2)) AS oa2es,
-    CAST(rc.oa_converted_ds/rd.offers_accepted AS DECIMAL (2,2)) AS oa2ds,
-    CAST(rc.oa_converted_ca/rd.offers_accepted AS DECIMAL (2,2)) AS oa2ca,
-    CAST(rc.oa_converted_cs/rd.offers_accepted AS DECIMAL (2,2)) AS oa2cs,
-    CAST(rc.es_converted_ep/rd.evaluation_started AS DECIMAL (2,2)) AS es2ep,
-    CAST(rc.es_converted_ca/rd.evaluation_started AS DECIMAL (2,2)) AS es2ca,
-    CAST(rc.ep_converted_ds/rd.evaluation_positive AS DECIMAL (2,2)) AS ep2ds,
-    CAST(rc.ds_converted_ca/rd.documentation_sent AS DECIMAL (2,2)) AS ds2ca,
-    CAST(rc.ca_converted_cc/rd.credit_approved AS DECIMAL (2,2)) AS ca2cc,
-    CAST(rc.ca_converted_cs/rd.credit_approved AS DECIMAL (2,2)) AS ca2cs,
-    CAST(rc.cc_converted_cs/rd.contracts_created AS DECIMAL (2,2)) AS cc2cs,
-    rc.has_guarantee,
-    rc.dt_event,
-    rc.dt_week_started,
-    rc.country_code,
+    b.country_code,
+    b.halfyear,
+    b.quarter,
+    b.business_type,
+    b.city_group,
+    b.tier,
+    b.listing_category_start,
+    b.rent_flow_origin,
+    b.rental_administrator,
+    b.weeks_to_conversion,
+    COALESCE(CAST((vb_converted_vc/(vb_converted_vc + vb_unconverted)) AS DECIMAL (10,2)), 0.00) AS vb2vc,
+    COALESCE(CAST((vb_converted_os/(vb_converted_os + vb_unconverted)) AS DECIMAL (10,2)), 0.00) AS vb2os,
+    COALESCE(CAST((vb_converted_oa/(vb_converted_oa + vb_unconverted)) AS DECIMAL (10,2)), 0.00) AS vb2oa,
+    COALESCE(CAST((vb_converted_cs/(vb_converted_cs + vb_unconverted)) AS DECIMAL (10,2)), 0.00) AS vb2cs,
+    COALESCE(CAST((vc_converted_os/(vc_converted_os + vc_unconverted)) AS DECIMAL (10,2)), 0.00) AS vc2os,
+    COALESCE(CAST((os_converted_oa/(os_converted_oa + os_unconverted)) AS DECIMAL (10,2)), 0.00) AS os2oa,
+    COALESCE(CAST((os_converted_cs/(os_converted_cs + os_unconverted)) AS DECIMAL (10,2)), 0.00) AS os2cs,
+    COALESCE(CAST((oa_converted_es/(oa_converted_es + oa_unconverted)) AS DECIMAL (10,2)), 0.00) AS oa2es,
+    COALESCE(CAST((oa_converted_ds/(oa_converted_ds + oa_unconverted)) AS DECIMAL (10,2)), 0.00) AS oa2ds,
+    COALESCE(CAST((oa_converted_ca/(oa_converted_ca + oa_unconverted)) AS DECIMAL (10,2)), 0.00) AS oa2ca,
+    COALESCE(CAST((oa_converted_cs/(oa_converted_cs + oa_unconverted)) AS DECIMAL (10,2)), 0.00) AS oa2cs,
+    COALESCE(CAST((es_converted_ep/(es_converted_ep + es_unconverted)) AS DECIMAL (10,2)), 0.00) AS es2ep,
+    COALESCE(CAST((es_converted_ca/(es_converted_ca + es_unconverted)) AS DECIMAL (10,2)), 0.00) AS es2ca,
+    COALESCE(CAST((ep_converted_ds/(ep_converted_ds + ep_unconverted)) AS DECIMAL (10,2)), 0.00) AS ep2ds,
+    COALESCE(CAST((ds_converted_ca/(ds_converted_ca + ds_unconverted)) AS DECIMAL (10,2)), 0.00) AS ds2ca,
+    COALESCE(CAST((ca_converted_cc/(ca_converted_cc + ca_unconverted)) AS DECIMAL (10,2)), 0.00) AS ca2cc,
+    COALESCE(CAST((ca_converted_cs/(ca_converted_cs + ca_unconverted)) AS DECIMAL (10,2)), 0.00) AS ca2cs,
+    COALESCE(CAST((cc_converted_cs/(cc_converted_cs + cc_unconverted)) AS DECIMAL (10,2)), 0.00) AS cc2cs,
+    b.has_guarantee,
+    b.dt_event,
+    b.dt_week_started,
     NOW() AS ts_load
 FROM
-    dw_rent_snapshot.rent_cohort_conversions_snapshot AS rc
+    base AS b
 JOIN
-    dw_rent_snapshot.rent_demand_events_snapshot AS rd
-        ON rc.year = YEAR(NOW())
-        AND rc.month = MONTH(NOW())
-        AND rc.day = DAY(NOW())
-        AND rc.halfyear = rd.halfyear
-        AND rc.quarter = rd.quarter
-        AND rc.business_type = rd.business_type
-        AND rc.city_group = rd.city_group
-        AND rc.tier = rd.tier
-        AND rc.listing_category_start = rd.listing_category_start
-        AND rc.rent_flow_origin = rd.rent_flow_origin
-        AND rc.rental_administrator = rd.rental_administrator
-        AND rc.has_guarantee = rd.has_guarantee
-        AND rc.country_code = rd.country_code
-        AND rc.dt_event = rd.dt_event
-        AND rc.dt_week_started = rd.dt_week_started
+    unconverted_events AS u
+      ON b.halfyear = u.halfyear
+      AND b.quarter = u.quarter
+      AND b.business_type = u.business_type
+      AND b.city_group = u.city_group
+      AND b.tier = u.tier
+      AND b.listing_category_start = u.listing_category_start
+      AND b.rent_flow_origin = u.rent_flow_origin
+      AND b.rental_administrator = u.rental_administrator
+      AND b.has_guarantee = u.has_guarantee
+      AND b.dt_event = u.dt_event
+      AND b.dt_week_started = u.dt_week_started
+      AND b.country_code = u.country_code
