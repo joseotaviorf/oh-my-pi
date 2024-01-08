750a3c4a9bd20649a5f6d7b36d66e5249925f6b5
Eduardo Trevisani <11429718+dutrevis@users.noreply.github.com>
2024-01-08 09:05:04 -0300

    [DIN-635] Added unit tests for BaseFactory (#13113)
    * Removed class properties from BaseFactory
    
    * Fixed gitignore to include tests
    
    * Added TestBaseFactory
    
    * Removed BaseWorkflow import from BaseFactory
    
    * Applied linting
    
    * Changed E711 error comparison to None
    
    ---------
    
    Co-authored-by: Eduardo Trevisani <eduardo.trevisani@quintoandar.com.br>

--- bietlejuice/base/airflow/dag_builders/main_builder/factories/base_factory.py
+++ bietlejuice/base/airflow/dag_builders/main_builder/factories/base_factory.py
@@ -1,20 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import Dict
 
-from bietlejuice.base.airflow.dag_builders.main_builder.workflows.base_workflow import (
-    BaseWorkflow,
-)
 from bietlejuice.base.airflow.dag_builders.main_builder.workflows.workflow_enum import (
     WorkflowEnum,
 )
 
 
 class BaseFactory(ABC):
-
     __WORKFLOW_ENUM_TO_CLASS_MAPPING = {}
 
     @abstractmethod
-    def get_workflow(self) -> BaseWorkflow:
+    def get_workflow(self):
         """
         Returns an instance of a workflow class, based on the `type` parameter
         provided in the workflow component of the DAG declaration file.
@@ -22,20 +18,18 @@ class BaseFactory(ABC):
         raise NotImplementedError()
 
     @property
-    @classmethod
-    def _WORKFLOW_ENUM_TO_CLASS_MAPPING(cls) -> Dict:
-        return cls.__WORKFLOW_ENUM_TO_CLASS_MAPPING
+    def _WORKFLOW_ENUM_TO_CLASS_MAPPING(self) -> Dict:
+        return self.__WORKFLOW_ENUM_TO_CLASS_MAPPING
 
     @_WORKFLOW_ENUM_TO_CLASS_MAPPING.setter
-    @classmethod
-    def _WORKFLOW_ENUM_TO_CLASS_MAPPING(cls, wokflow_mapping):
-        assert not bool(
-            cls.__WORKFLOW_ENUM_TO_CLASS_MAPPING
-        ), "You can't change the value of workflow mapping during runtime."
-        assert isinstance(wokflow_mapping, dict), "Workflow mapping must be a dict."
-        cls.__WORKFLOW_ENUM_TO_CLASS_MAPPING = wokflow_mapping
+    def _WORKFLOW_ENUM_TO_CLASS_MAPPING(self, wokflow_mapping):
+        if self.__WORKFLOW_ENUM_TO_CLASS_MAPPING:
+            raise RuntimeError("You can't change the workflow mapping during runtime.")
+        if not isinstance(wokflow_mapping, dict):
+            raise TypeError("Workflow mapping must be a dict.")
+        self.__WORKFLOW_ENUM_TO_CLASS_MAPPING = wokflow_mapping
 
-    def _dispatch_workflow_class(self, workflow_enum: WorkflowEnum) -> BaseWorkflow:
+    def _dispatch_workflow_class(self, workflow_enum: WorkflowEnum):
         """
         Gets the Workflow class based on a provided workflow enum object.
 
@@ -44,10 +38,9 @@ class BaseFactory(ABC):
         """
 
         self.__validate_workflow_enum(workflow_enum)
-        return self.__class__._WORKFLOW_ENUM_TO_CLASS_MAPPING[workflow_enum]
+        return self._WORKFLOW_ENUM_TO_CLASS_MAPPING[workflow_enum]
 
-    @classmethod
-    def __validate_workflow_enum(cls, workflow_enum: WorkflowEnum) -> None:
+    def __validate_workflow_enum(self, workflow_enum: WorkflowEnum) -> None:
         """
         Validates that the provided workflow enum has a mapped reference of a class.
 
@@ -56,7 +49,7 @@ class BaseFactory(ABC):
         :raise: ValueError
         :return: None
         """
-        available_enums = cls._WORKFLOW_ENUM_TO_CLASS_MAPPING.keys()
+        available_enums = self._WORKFLOW_ENUM_TO_CLASS_MAPPING.keys()
         if workflow_enum not in available_enums:
             raise ValueError(
                 f"m=__validate_workflow_class, msg=DAG Workflow does not exist, "
---
+++ tests/unit/airflow/dag_builders/factories/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from bietlejuice.base.airflow.dag_builders.main_builder.factories.base_factory import (
+    BaseFactory,
+)
+
+
+@pytest.fixture(scope="function")
+def base_factory():
+    BaseFactory.__abstractmethods__ = set()
+    return BaseFactory()
