3aec766006b4a31fbfd7bab291b383452792c203
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2024-01-15 17:30:54 -0300

    [People - 156] Updated enrich_jira cluster #13217

--- dags/bedrock/enrich_jira/enrich_jira.py
+++ dags/bedrock/enrich_jira/enrich_jira.py
@@ -34,7 +34,7 @@ DOC_MD_BASE_URL = config_service.get_config("doc_md_chart_url")
 REPO_PATH = config_service.get_config("databricks_bietlejuice_repo_path")
 SPARK_JOBS_PATH = f"{REPO_PATH}/spark_jobs/base/"
 
-CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_general_photon_cluster")
+CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_io-memory_photon_cluster")
 
 default_libraries = config_service.get_config("default_libraries")
 
