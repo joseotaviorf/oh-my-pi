b734932fbd8005ba12f861d3acf71abe03b19ea2
dmarcolongo <4944228+dmarcolongo@users.noreply.github.com>
2024-01-29 11:08:18 -0300

    [REDE] Add Granularity To House Enrichment Clean (#13292)
    * Added granularity column
    
    * Fix path
    
    ---------
    
    Co-authored-by: Danilo Marcolongo Afonso <danilo.afonso@BRSPPM-JTWRWGFHHX.local>

--- dags/for_rent/ebdb/queries/clean/house_enrichment.sql
+++ dags/for_rent/ebdb/queries/clean/house_enrichment.sql
@@ -4,6 +4,7 @@ SELECT
     dejavu_id AS id_dejavu,
     status,
     event_trigger,
+    granularity,
     dejavu_id_version AS version_id_dejavu,
     created_at AS ts_created,
     updated_at AS ts_updated
