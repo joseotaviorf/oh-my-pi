bbd2bd44f1b72d4909ddb3d8901ebe0938cf047c
Giuseppe Giovanelli <50770057+giuseppecg@users.noreply.github.com>
2024-01-30 11:51:35 -0300

    [DG-1285] Adding temp Looker DAG (#13256)
    * feat: adding temp tables DAG
    
    * fix: moving files to correct folder
    
    * fix: table_name key on metadata and add table
    
    * feat: add dependencies
    
    * fix: adding dags to non standard dags
    
    * fix: adding domain to docs
    
    * fix: removing extra docs
    
    * fix: moving half of the tables to another dag due to job cluster limitations
    
    * fix: moving inside an enrich folder
    
    * feat: ran make dependencies

---
+++ dags/governance/looker_temps/queries/enrich/sql_weekly_ongoing_listings_migrated.sql
@@ -0,0 +1,17 @@
+SELECT
+  REPLACE(
+    SUBSTR(CAST(olsl.sk_house_listing AS STRING), 1, 9) || CAST(week_start AS STRING),
+    '-',
+    ''
+  ) AS id_house_week,
+  olsl.sk_house_listing,
+  olsl.week_start,
+  olsl.weeks_since_publication,
+  olsl.status_change_reason
+FROM dw_datamarts.ongoing_listed_suspended_listings AS olsl
+LEFT JOIN dw_public.fact_house_listings AS fhl
+  ON fhl.sk_house_listing = olsl.sk_house_listing
+WHERE
+  olsl.week_start > DATE_ADD(CURRENT_DATE, -180)
+  AND olsl.status_history IN ('publicado', 'PUBLISHED')
+  AND fhl.sk_region > 0
