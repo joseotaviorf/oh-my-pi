0bc247a875d56b5a9545822e9f1754c0babc71e4
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2024-01-30 10:50:47 -0300

    Update enrich_sap_gateway.py (#13298)

--- dags/fintech/enrich_sap_gateway/enrich_sap_gateway.py
+++ dags/fintech/enrich_sap_gateway/enrich_sap_gateway.py
@@ -32,7 +32,7 @@ DOC_MD_BASE_URL = config_service.get_config("doc_md_chart_url")
 S3_PREFIX = config_service.get_config("databricks_bietlejuice_repo_path")
 SPARK_JOBS_PATH = f"{S3_PREFIX}/spark_jobs/base/"
 
-CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_general_cluster")
+CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_memory_photon_cluster")
 default_libraries = config_service.get_config("default_libraries")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
