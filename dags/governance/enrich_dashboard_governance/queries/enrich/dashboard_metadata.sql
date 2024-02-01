73d9aa391ac71c2c99b0217973f4c168c1ea3d0c
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-02-01 15:17:52 -0300

    [DBP-167] 🚚 DAG Refactoring: Move people's DAG and Update Owner (#13346)
    * 🚚 Move people dags from bedrock to new people folder
    
    * ✏️ fix owner dags
    
    * ✏️ fix typo error in dag's owner
    
    * 🚚  editing files to have the people reference
    
    * ✏️ fix typo

--- dags/governance/enrich_dashboard_governance/queries/enrich/dashboard_metadata.sql
+++ dags/governance/enrich_dashboard_governance/queries/enrich/dashboard_metadata.sql
@@ -129,6 +129,7 @@ metabase_dashboard_enrich as (
         WHEN mc.readable_location like 'BedRock%' THEN 'BedRock'
         WHEN mc.readable_location like 'Tech Platform%' THEN 'Tech Platform'
         WHEN mc.readable_location like 'Support and Services%' THEN 'Support and Services'
+        WHEN mc.readable_location like 'People%' THEN 'People'
         ELSE Null
       END as domain,
       case
