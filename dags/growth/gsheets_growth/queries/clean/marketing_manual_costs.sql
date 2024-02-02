1d8614204c2836d6da2ad06809a410549a023556
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2024-02-02 11:19:20 -0300

    Creating Manual Cost Table applying new Taxonomy (#13371)
    * Creating Manual Cost Table applying new Taxonomy
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Uilson Bonfim <uilsonbonfim@BRSPPM-C02DXG7JMD6M.local>
    Co-authored-by: Marcio Gomes <marcio.gomes@quintoandar.com.br>

---
+++ dags/growth/gsheets_growth/queries/clean/marketing_manual_costs.sql
@@ -0,0 +1,19 @@
+SELECT
+  NULLIF(account_name, '') AS account_name,
+  NULLIF(campaign_name, '') AS campaign_name,
+  NULLIF(ad_group_name, '') AS ad_group_name,
+  NULLIF(utm_term, '') AS utm_term,
+  NULLIF(utm_content, '') AS utm_content,
+  NULLIF(funnel_side, '') AS funnel_side,
+  NULLIF(country_code, '') AS country_code,
+  NULLIF(city_group, '') AS city_group,
+  NULLIF(campaign_strategy_intent, '') AS campaign_strategy_intent,
+  NULLIF(behavior_type, '') AS behavior_type,
+  NULLIF(medium, '') AS medium,
+  NULLIF(source, '') AS source,
+  NULLIF(campaign_business_context, '') AS campaign_business_context,
+  NULLIF(landing_page, '') AS landing_page,
+  CAST(REPLACE(NULLIF(cost, ''), ',', '') AS FLOAT) AS cost,
+  CAST(NULLIF(dt, '') AS DATE) AS dt_cost
+FROM
+  datalake_gsheets_raw.marketing_manual_costs
