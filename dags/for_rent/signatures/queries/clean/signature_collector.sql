bdfa14c32b7ba616fae9f14befc57bed834b08a3
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-02-05 09:50:09 -0300

    deleting old DAG and renaming the new one (#13353)

--- dags/for_rent/signatures/queries/clean/signature_collector.sql
+++ dags/for_rent/signatures/queries/clean/signature_collector.sql
@@ -9,10 +9,12 @@ SELECT
     sent_at AS ts_sent,
     created_at AS ts_created,
     updated_at AS ts_updated,
-    year(updated_at) AS year,
-    month(updated_at) AS month,
-    day(updated_at) AS day
+    year,
+    month,
+    day
 FROM
     datalake_signatures_raw.signature_collector
 WHERE
-    date(updated_at) = date('{year}-{month}-{day}')
+    year = {year}
+    AND month = {month}
+    AND day = {day}
