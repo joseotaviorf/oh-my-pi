ef8cfc589f26f7786695c08ad1df8a305b01e3d6
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-02-08 11:46:07 -0300

    [AEFR] Update viva_real cluster (#13420)
    * add viva_real_custom_cluster field
    
    * fix custom cluster
    
    * fix cluster

--- dags/for_rent/crawlers/listings/viva_real.py
+++ dags/for_rent/crawlers/listings/viva_real.py
@@ -49,7 +49,7 @@ RAW_SPARK_JOB_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/{DAG_NAME_P
 
 
 default_libraries = config_service.get_config("default_libraries")
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("custom_cluster")
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.ANALYTICS_ENGINEERS,
