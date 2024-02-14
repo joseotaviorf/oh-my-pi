0d3a8dfbcd199a790801b1a96ba520bcc7b75584
Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
2024-02-14 17:43:25 -0300

    [DIN-847] Adding spark job to load table from Raw to Clean layer for CDC (#13401)
    * feat: adding raw to clean spark job for CDC
    
    * feat: removing comments
    
    * chore: adding the parse_arguments function call
    
    * chore: adding dag_name parameter to log
    
    * feat: removing partition
    
    * fix: adjusting parser.add_argument sintax
    
    * feat: adding raw and clean table_id param
    
    * feat: removing raw and clean table id

--- dags/cross/base/spark_jobs/load_cdc_clean.py
+++ dags/cross/base/spark_jobs/load_cdc_clean.py
@@ -1,1 +1,154 @@
-"""Dummy Spark Job to load CDC tables into the clean layer"""
+from argparse import ArgumentParser
+from datetime import datetime
+import re
+from bietlejuice.base.service.dag_packages_path_service import DAGPackagesPathService
+
+from quintoandar_logger import QuintoAndarLogger
+
+from delta.tables import DeltaTable
+
+JOB_NAME = "load_cdc_clean"
+
+logger = QuintoAndarLogger(JOB_NAME)
+
+
+def parse_arguments():
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("dag_name")
+    parser.add_argument("env", type=str, help="forno/prod environment")
+    parser.add_argument("datalake_bucket")
+    parser.add_argument("schema")
+    parser.add_argument("table_name")
+    parser.add_argument("execution_date")
+    parser.add_argument("table_id")
+
+    return parser.parse_args()
+
+
+def insert_columns_into_query(query, columns):
+    """
+    Insert CDC columns to persist those informations
+    through clean table.
+    """
+    logger.info(
+        f"m=insert_columns_into_query, columns={columns}, msg=Inserting CDC columns into query..."
+    )
+    split_values_in_from = re.split(
+        r"(FROM\s+`?\w+\`?\.`?\w+`?)", query, flags=re.IGNORECASE
+    )
+    split_values_in_from[-3] += "".join([f",{column}\n" for column in columns])
+    rejoined_query = "".join(split_values_in_from)
+    return rejoined_query
+
+
+def load_clean_delta_table(full_table_name):
+    """
+    Load and return Clean Delta table if exists.
+    """
+    logger.info(
+        f"m=load_clean_delta_table, table_name={full_table_name}, msg=Reading Clean Delta table..."
+    )
+    try:
+        delta_table = DeltaTable.forName(spark, full_table_name)
+    except Exception as e:
+        logger.info(
+            f"m=load_clean_delta_table, msg=Unable to read delta table, error={e}"
+        )
+        return None
+
+    return delta_table
+
+
+def create_clean_delta_table(
+    df, full_clean_table_name, datalake_bucket, schema, table_name
+):
+    """
+    Create Clean Delta table.
+    """
+    logger.info(
+        f"m=create_clean_delta_table, msg=Creating clean table using Delta format..."
+    )
+    df.write.format("delta").option("mergeSchema", True).mode("overwrite").saveAsTable(
+        full_clean_table_name,
+        path=f"s3://{datalake_bucket}/clean/{schema}/{table_name}/",
+    )
+    clean_delta_table = DeltaTable.forName(spark, full_clean_table_name)
+
+    return clean_delta_table
+
+
+def apply_deletes_to_clean_table(clean_delta_table, table_id, clean_updates):
+    """
+    Updates clean table based on raw modifications by applying
+    deletes.
+    """
+    logger.info(
+        f"m=consolidate_clean_table, msg=Updating clean table based on raw modifications"
+    )
+    clean_delta_table.alias("clean_table").merge(
+        clean_updates.alias("clean_updates"),
+        f"clean_table.{table_id} = clean_updates.{table_id}",
+    ).whenMatchedDelete(condition=f"clean_updates.op_cdc = 'd'").whenMatchedUpdate(
+        condition=f"clean_updates.ts_cdc_transaction >= clean_table.ts_cdc_transaction",
+        set=dict((col, f"clean_updates.{col}") for col in clean_updates.columns),
+    ).whenNotMatchedInsert(
+        condition="clean_updates.op_cdc != 'd'",
+        values=dict((col, f"clean_updates.{col}") for col in clean_updates.columns),
+    ).execute()
+
+
+def main():
+    args = parse_arguments()
+    dag_name = args.dag_name
+    environment = args.env
+    datalake_bucket = args.datalake_bucket
+    schema = args.schema
+    table_name = args.table_name
+    execution_date = args.execution_date
+    table_id = args.table_id
+
+    logger.info(
+        f"""
+        m=__main__, environment={environment}, dag_name={dag_name}
+        datalake_bucket={datalake_bucket},
+        schema={schema}, table_name={table_name}, execution_date={execution_date},
+        clean_table_id={table_id}
+        msg=Starting spark job...
+        """
+    )
+
+    clean_query = DAGPackagesPathService.get_query_file_content_in_spark_jobs(
+        dag_name=dag_name,
+        layer="clean",
+        table_name=table_name,
+    )
+
+    cdc_columns = ["op_cdc", "ts_cdc_transaction"]
+
+    query_with_cdc_columns = insert_columns_into_query(clean_query, cdc_columns)
+    clean_updates_df = spark.sql(query_with_cdc_columns).where(
+        f"ts_cdc_transaction >= '{execution_date}'"
+    )
+
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS `datalake_{schema}_clean`")
+    full_clean_table_name = f"`datalake_{schema}_clean`.`{table_name}`"
+
+    clean_delta_table = load_clean_delta_table(full_clean_table_name)
+
+    if not clean_delta_table:
+        clean_updates_without_deletes = clean_updates_df.filter(
+            clean_updates_df.op_cdc != "d"
+        )
+        clean_delta_table = create_clean_delta_table(
+            clean_updates_without_deletes,
+            full_clean_table_name,
+            datalake_bucket,
+            schema,
+            table_name,
+        )
+
+    apply_deletes_to_clean_table(clean_delta_table, table_id, clean_updates_df)
+
+
+if __name__ == "__main__":
+    main()
