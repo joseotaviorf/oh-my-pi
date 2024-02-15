716cb51279c74e4c96f8f74dc8f72eebe67e231a
Julia Rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2024-02-15 10:14:43 -0300

    adjust cdc clean task creator to pass more parameters (#13419)
    * adjust cdc clean task creator to pass more parameters
    
    * feat: adding raw and clean table id
    
    * chore: lint
    
    * feat: adding default value for clean_table_id
    
    * chore: lint
    
    * add clean_table_id
    
    ---------
    
    Co-authored-by: Julia Rolemberg <julia-rolemberg>
    Co-authored-by: Gustavo Akashi <gustavo.akashi@quintoandar.com.br>

--- bietlejuice/base/airflow/task_creators/load_cdc_clean_task_creator.py
+++ bietlejuice/base/airflow/task_creators/load_cdc_clean_task_creator.py
@@ -9,15 +9,26 @@ class LoadCDCCleanTaskCreator(BaseTaskCreator):
     _TASK_ID_TEMPLATE = "load-{layer}-{table_name}"
     SPARK_JOB_NAME = "load_cdc_clean"
 
-    def create_task(
-        self, table_attributes: TableAttributes
-    ) -> QuintoAndarDatabricksCheckJobTaskOperator:
-        task_id = self.generate_task_id(table_attributes)
-        parameters = [
+    def _get_parameters(self, table_attributes: TableAttributes) -> list:
+        raw_table_id = table_attributes.table_customization.get("raw_table_id")
+        clean_table_id = table_attributes.table_customization.get(
+            "clean_table_id", raw_table_id
+        )
+
+        return [
+            self.dag_execution_context.dag_args["name"],
             self.dag_execution_context.environment,
             self.dag_execution_context.bucket,
             table_attributes.schema,
             table_attributes.table_name,
+            self.dag_execution_context.execution_date,
+            clean_table_id,
         ]
+
+    def create_task(
+        self, table_attributes: TableAttributes
+    ) -> QuintoAndarDatabricksCheckJobTaskOperator:
+        task_id = self.generate_task_id(table_attributes)
+        parameters = self._get_parameters(table_attributes)
 
         return self._create_spark_job_task(self.SPARK_JOB_NAME, task_id, parameters)
