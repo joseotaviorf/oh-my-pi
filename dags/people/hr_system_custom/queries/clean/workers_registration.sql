49a9807d630bab05a73fb3e889602443485394fe
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-02-22 14:30:09 -0300

    [DBP-261][People] HR System Customs (#13510)
    * ✨ add new dag hr_system_custom
    
    * 🚑 delete endpoints and partitions
    
    * 🚚 rename spark job
    
    * 🐛 input args for spark job

---
+++ dags/people/hr_system_custom/queries/clean/workers_registration.sql
@@ -0,0 +1,7 @@
+SELECT  
+  assignmentNumber AS assignment_number,
+  matricula AS registration,
+  personName AS person_name,
+  personNumber AS person_number,
+  ts_load
+FROM datalake_hr_system_custom_raw.workers_registration
