a2ca4879d13242081f16fca17727bc2939377395
Matheus Gonçalves <matheus.ferreira@quintoandar.com.br>
2024-02-23 08:52:31 -0300

    removed 13 from group by (#13524)

--- dags/support_and_service/enrich_ss_logic_model/queries/enrich/user_ss_metrics.sql
+++ dags/support_and_service/enrich_ss_logic_model/queries/enrich/user_ss_metrics.sql
@@ -46,7 +46,7 @@ WITH persona_type_pivot AS (
     pt.year = {year}
     AND pt.month = {month}
     AND pt.day = {day}
-  GROUP BY 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13
+  GROUP BY 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12
 )
 SELECT
   ptp.id_snapshot,
