4745f44265f7d10d3a3dba1649d5c84bc9b56634
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-02-26 11:16:04 -0300

    [AESS-3139] New `quinturk` DAG (#13519)
    * feat: quinturk dag
    
    * refactor: change task table to incremental load
    
    * fix: dag name
    
    * feat: add task_completion table
    
    * feat: add project table
    
    * feat: add new tables on declaration file
    
    * refactor: ident
    
    * refactor: rename cols
    
    * fix: rename cols
    
    * feat: add doc

---
+++ dags/support_and_service/quinturk/queries/clean/task_completion.sql
@@ -0,0 +1,25 @@
+SELECT
+    id,
+    task_id AS id_task,
+    project_id AS id_project,
+    parent_annotation_id AS id_parent_annotation,
+    parent_prediction_id AS id_parent_prediction,
+    completed_by_id AS id_completed_by,
+    last_created_by_id AS id_last_created_by,
+    pediction,
+    lead_time,
+    result_count,
+    last_action,
+    ground_truth AS is_ground_truth,
+    was_canceled,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_quinturk_raw.task_completion
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
