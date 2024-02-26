dbac2790974ce62ae0ae81b7e8ac5f558904f8a9
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2024-02-26 14:42:18 -0300

    [GROW-2897] Recreating `consolidated_media_metrics`as part of Marketing cost pipeline refactoring (#13349)

---
+++ dags/growth/enrich_consolidated_growth_metrics/queries/enrich/consolidated_media_metrics.sql
@@ -0,0 +1,222 @@
+WITH consolidated_sources AS (
+    -- GOOGLE
+    SELECT
+        id_date,
+        'google' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        report_type,
+        ad_type,
+        utm_term,
+        utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        desktop_cost,
+        mobile_cost,
+        other_cost,
+        total_cost,
+        impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.google_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- CRITEO
+    UNION ALL
+    SELECT
+        id_date,
+        'criteo' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        NULL AS utm_term,
+        NULL AS utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        0.0 AS desktop_cost,
+        0.0 AS mobile_cost,
+        other_cost,
+        total_cost,
+        impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.criteo_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- RTB
+    UNION ALL
+    SELECT
+        id_date,
+        'rtb' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        NULL AS utm_term,
+        NULL AS utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        0.0 AS desktop_cost,
+        0.0 AS mobile_cost,
+        other_cost,
+        total_cost,
+        impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.rtb_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- MITULA
+    UNION ALL
+    SELECT
+        id_date,
+        'mitula' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        NULL AS utm_term,
+        NULL AS utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        desktop_cost,
+        mobile_cost,
+        0.0 AS other_cost,
+        total_cost,
+        0 AS impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.mitula_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- TROVIT
+    UNION ALL
+    SELECT
+        id_date,
+        'trovit' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        NULL AS utm_term,
+        NULL AS utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        desktop_cost,
+        mobile_cost,
+        0.0 AS other_cost,
+        total_cost,
+        0 AS impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.trovit_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- FACEBOOK
+    UNION ALL
+    SELECT
+        id_date,
+        'facebook' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        utm_term,
+        utm_content,
+        utm_campaign,
+        NULL AS city_group,
+        desktop_cost,
+        mobile_cost,
+        other_cost,
+        total_cost,
+        impressions,
+        clicks
+    FROM
+        datalake_consolidated_growth_metrics.facebook_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+    -- BRAZE
+    UNION ALL
+    SELECT 
+        id_date,
+        'braze' AS origin,
+        NULL AS business_context,
+        campaign_name,
+        account_name,
+        NULL AS report_type,
+        NULL AS ad_type,
+        NULL AS utm_term,
+        NULL AS utm_content,   
+        NULL AS utm_campaign,
+        city_group,
+        0.0 AS desktop_cost,
+        0.0 AS mobile_cost,
+        0.0 AS other_cost,
+        cost AS total_cost,
+        0 AS impressions,
+        0 AS clicks
+    FROM
+        datalake_consolidated_growth_metrics.braze_consolidated_metrics
+    WHERE
+        id_date BETWEEN INT(REPLACE(DATE('{load_start_date}'), '-', '')) AND INT(REPLACE(DATE('{load_end_date}'), '-', ''))
+        AND id_date >= 20220912        
+)
+
+SELECT
+    cs.id_date,
+    cs.origin,
+    COALESCE(sr.business_context, cs.business_context) AS business_context,
+    cs.account_name,
+    COALESCE(ctd.campaign_name_convention, cs.campaign_name) AS campaign_name,
+    CASE
+        WHEN LOWER(cs.campaign_name) LIKE '%calc%'
+            THEN 'Calculator'
+        WHEN LOWER(cs.campaign_name) LIKE '%newchannel%'
+            THEN 'New Channels'
+        WHEN LOWER(cs.campaign_name) LIKE '%agents%'
+            THEN 'Agents'
+        WHEN LOWER(cs.campaign_name) LIKE '%doorman%'
+            THEN 'Doorman'
+        ELSE 'Other'
+    END AS campaign_origin_acquisition,
+    CASE 
+        WHEN cs.origin = 'braze' THEN cs.city_group
+        ELSE COALESCE(ch.city_group, sr.city_group, dr.city_group, 'Not Mapped') 
+    END AS city_group,
+    cs.report_type,
+    cs.ad_type,
+    cs.utm_campaign,
+    cs.utm_term,
+    cs.utm_content,
+    cs.desktop_cost * COALESCE(sr.share, 1) AS desktop_cost,
+    cs.mobile_cost * COALESCE(sr.share, 1) AS mobile_cost,
+    cs.other_cost * COALESCE(sr.share, 1) AS other_cost,
+    cs.total_cost * COALESCE(sr.share, 1) AS total_cost,
+    cs.impressions * COALESCE(sr.share, 1) AS impressions,
+    cs.clicks * COALESCE(sr.share, 1) AS clicks
+FROM
+    consolidated_sources cs
+LEFT JOIN
+    datalake_marketing_costs_sharing_rules.sharing_rules sr
+        ON SPLIT(cs.campaign_name, '[.]')[0] = sr.id_rule
+        AND cs.id_date = sr.id_date
+LEFT JOIN
+    datalake_consolidated_marketing_costs.city_group_old_campaigns_historic ch
+        ON cs.campaign_name = ch.campaign_name
+LEFT JOIN
+    datalake_region.region dr
+        ON SPLIT(cs.campaign_name, '[.]')[0] = dr.id
+LEFT JOIN datalake_gsheets_clean.cost_taxonomy_translation_dictionary AS ctd
+ON
+  cs.account_name = ctd.account_name
+  AND cs.campaign_name = ctd.campaign_name
+  AND cs.report_type = ctd.report_type
+  AND cs.ad_type = ctd.ad_type
+  AND cs.origin = ctd.origin
---
+++ dags/growth/enrich_consolidated_growth_metrics/queries/enrich/rtb_consolidated_metrics.sql
@@ -0,0 +1,13 @@
+SELECT
+    INT(DATE_FORMAT(dt_attribution, 'yyyyMMdd')) AS id_date,
+    sub_campaign_name AS campaign_name,
+    account_name,
+    sub_campaign_name AS utm_campaign,
+    cost AS other_cost,
+    cost AS total_cost,
+    impressions,
+    clicks
+FROM
+    datalake_rtb_campaigns_clean.rtb_campaigns
+WHERE
+    dt_attribution BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
