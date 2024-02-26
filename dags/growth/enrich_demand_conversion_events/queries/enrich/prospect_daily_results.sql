ab2af61e22372fe579e8750dda99dc295878f48b
Isabela Sayuri Matsumoto <isabela.matsumoto@quintoandar.com.br>
2024-02-26 10:08:00 -0300

    Adding prospect_results and prospect_daily_results tables in enrich_demand_conversion_events DAG  (#13523)
    * Adding prospect_result and prospect_daily_result tables
    
    * Adding SF_Normalize_String function on declaration.yml
    
    * Fixing prospect_results query

---
+++ dags/growth/enrich_demand_conversion_events/queries/enrich/prospect_daily_results.sql
@@ -0,0 +1,152 @@
+WITH prospect_status_events AS (
+  SELECT
+    tps.id_demand_prospect_conversion_event,
+    tps.id_tenant_prospect AS id_prospect,
+    'rent' AS business_context,
+    'CONVERSION' AS event_type,
+    tps.prospect_event_name AS event_name,
+    tps.status_trigger_event_name AS event_detail,
+    tps.ts_status_started AS ts_event
+  FROM
+    datalake_demand_flows.tenant_prospect_status AS tps
+  WHERE 
+    tps.year = {year}
+    AND tps.month = {month}
+    AND tps.day = {day}   
+  UNION ALL 
+  SELECT
+    bps.id_demand_prospect_conversion_event,
+    bps.id_buyer_prospect AS id_prospect,
+    'sale' AS business_context,
+    'CONVERSION' AS event_type,
+    bps.prospect_event_name AS event_name,
+    bps.status_trigger_event_name AS event_detail,
+    bps.ts_status_started AS ts_event
+  FROM
+    datalake_demand_flows.buyer_prospect_status AS bps
+  WHERE 
+    bps.year = {year}
+    AND bps.month = {month}
+    AND bps.day = {day}   
+),
+
+prospect_results AS (
+  SELECT
+    pcr.id_demand_prospect_conversion_event,
+    pse.id_prospect,
+    pse.business_context,
+    pse.event_type,
+    pse.event_name,
+    pse.event_detail,
+    pcr.final_id_media_setup AS id_media_setup,
+    pcr.id_rent_flow,
+    pcr.id_sale_flow, 
+    pcr.id_booking,
+    pcr.id_offer,
+    pcr.id_talk_to_agent,
+    pcr.id_house,
+    pcr.id_region,
+    pcr.id_owner,
+    pcr.id_agent,
+    pcr.product_origin,
+    pcr.app_type,
+    pcr.origin,
+    pcr.platform,
+    pcr.content_page,
+    pcr.operation_channel,
+    pcr.referral_type,
+    pcr.utm_campaign,
+    pcr.utm_medium,
+    pcr.utm_source,
+    pcr.utm_term,
+    pse.ts_event,
+    pcr.year,
+    pcr.month,
+    pcr.day
+  FROM
+    prospect_status_events AS pse
+  LEFT JOIN
+    datalake_demand_flows.prospect_results AS pcr
+      ON pse.id_demand_prospect_conversion_event = pcr.id_demand_prospect_conversion_event
+        AND pcr.year = {year}
+        AND pcr.month = {month}
+        AND pcr.day = {day}  
+  UNION ALL
+  SELECT
+    pcr.id_demand_prospect_conversion_event,
+    pcr.id_prospect,
+    pcr.business_context,
+    'FLOW' AS event_type,
+    pcr.event_name,
+    NULL AS event_detail,
+    pcr.final_id_media_setup AS id_media_setup,
+    pcr.id_rent_flow,
+    pcr.id_sale_flow, 
+    pcr.id_booking,
+    pcr.id_offer,
+    pcr.id_talk_to_agent,
+    pcr.id_house,
+    pcr.id_region,
+    pcr.id_owner,
+    pcr.id_agent,
+    pcr.product_origin,
+    pcr.app_type,
+    pcr.origin,
+    pcr.platform,
+    pcr.content_page,
+    pcr.operation_channel,
+    pcr.referral_type,
+    pcr.utm_campaign,
+    pcr.utm_medium,
+    pcr.utm_source,
+    pcr.utm_term,
+    pcr.ts_event,
+    pcr.year,
+    pcr.month,
+    pcr.day 
+  FROM
+    datalake_demand_flows.prospect_results AS pcr
+  WHERE 
+    pcr.year = {year}
+    AND pcr.month = {month}
+    AND pcr.day = {day}    
+  QUALIFY   
+    ROW_NUMBER() OVER(PARTITION BY id_demand_prospect_conversion_event ORDER BY ts_event ASC) = 1 
+)
+
+SELECT
+  MONOTONICALLY_INCREASING_ID() AS id_prospect_event,
+  id_demand_prospect_conversion_event,
+  id_prospect,
+  business_context,
+  event_type,
+  event_name,
+  event_detail,
+  id_media_setup,
+  id_rent_flow,
+  id_sale_flow, 
+  id_booking,
+  id_offer,
+  id_talk_to_agent,
+  id_house,
+  id_region,
+  id_owner,
+  id_agent,
+  product_origin,
+  app_type,
+  origin,
+  platform,
+  content_page,
+  operation_channel,
+  referral_type,
+  utm_campaign,
+  utm_medium,
+  utm_source,
+  utm_term,
+  INT(DATE_FORMAT(ts_event,'yyyyMMdd')) AS id_event_date,
+  ts_event,
+  year,
+  month,
+  day
+FROM
+  prospect_results
---
+++ dags/growth/enrich_demand_conversion_events/queries/enrich/prospect_results.sql
@@ -0,0 +1,178 @@
+WITH adhoc_rules AS (
+  SELECT
+    dpce.id_demand_prospect_conversion_event,
+    dpce.id_prospect,
+    dpce.business_context,
+    dpce.id_event_type,
+    dpce.event_name,
+    CASE 
+      WHEN dpce.product_origin = 'Corretores'
+      THEN "referral.2."
+      WHEN utm_campaign IS NULL
+        AND utm_source IS NULL
+        AND utm_medium IS NULL
+        AND app_type IS NULL
+      THEN "losttracking.-2."
+      WHEN utm_campaign IS NULL
+        AND utm_source IS NULL
+        AND utm_medium IS NULL
+        AND app_type IS NOT NULL
+      THEN "direct.1." 
+    END AS utm_adhoc_rule,
+    dpce.id_rent_flow,
+    dpce.id_sale_flow, 
+    dpce.id_booking,
+    dpce.id_offer,
+    dpce.id_talk_to_agent,
+    dpce.id_house,
+    dpce.id_region,
+    dpce.id_owner,
+    dpce.id_agent,
+    dpce.utm_campaign, 
+    dpce.utm_medium,
+    dpce.utm_source,
+    dpce.utm_term,
+    dpce.app_type,
+    dpce.booking_creator,
+    dpce.product_origin,
+    dpce.is_3p_demand,
+    CASE
+      WHEN dpce.booking_creator IS NULL AND dpce.id_booking IS NOT NULL THEN "Lost Tracking"
+      WHEN dpce.booking_creator IS NULL AND dpce.id_booking IS NULL THEN "SelfService"
+      WHEN dpce.booking_creator = 'SelfService' THEN 'SelfService' 
+      WHEN dpce.booking_creator = 'Admin/CX' THEN 'CX'
+      ELSE dpce.booking_creator 
+    END AS operation_channel,
+    CASE
+      WHEN dpce.is_3p_demand = TRUE THEN 'Rede'
+      WHEN dpce.booking_creator = 'Agent' AND dpce.is_3p_demand = FALSE THEN 'Agent'
+      ELSE 'NA' -- TQC
+    END AS referral_type,
+    CASE 
+      WHEN dpce.product_origin IS NULL THEN 'Lost Tracking'
+      ELSE dpce.product_origin
+    END AS origin,
+    CASE
+      WHEN dpce.app_type IS NULL THEN "Lost Tracking"
+      WHEN LOWER(dpce.app_type) LIKE '%android%' THEN 'App Android'
+      WHEN LOWER(dpce.app_type) LIKE '%ios%' THEN 'App iOS'
+      WHEN LOWER(dpce.app_type) LIKE '%web_desktop%' THEN 'Web Desktop'
+      WHEN LOWER(dpce.app_type) LIKE '%web_mobile%' THEN 'Web Mobile'
+      ELSE "Other"
+    END AS platform,
+    '' AS content_page,
+    dpce.ts_event,
+    dpce.year,
+    dpce.month,
+    dpce.day
+  FROM
+    datalake_demand_flows.demand_prospect_conversion_events AS dpce
+  WHERE 
+    dpce.year = {year}
+    AND dpce.month = {month}
+    AND dpce.day = {day}  
+),
+
+media_setup_ids AS (
+  SELECT
+    dpce.id_demand_prospect_conversion_event,
+    dpce.id_prospect,
+    dpce.business_context,
+    dpce.id_event_type,
+    dpce.event_name,
+    INT(SPLIT(dpce.utm_adhoc_rule, "[.]")[1]) AS id_media_setup_from_adhoc,
+    INT(SPLIT(dpce.utm_campaign, "[.]")[1]) AS id_media_setup_from_naming_convention,
+    INT(SPLIT(ef.correct_utm_camapign, "[.]")[1]) AS id_media_setup_from_exception_flow,
+    dict.sk_media_setup AS id_media_setup_from_dictionary,
+    dpce.id_rent_flow,
+    dpce.id_sale_flow, 
+    dpce.id_booking,
+    dpce.id_offer,
+    dpce.id_talk_to_agent,
+    dpce.id_house,
+    dpce.id_region,
+    dpce.id_owner,
+    dpce.id_agent,
+    dpce.utm_adhoc_rule,
+    dpce.utm_campaign, 
+    dpce.utm_medium,
+    dpce.utm_source,
+    dpce.utm_term,
+    dpce.app_type,
+    dpce.booking_creator,
+    dpce.product_origin,
+    dpce.is_3p_demand,
+    dpce.operation_channel,
+    dpce.referral_type,
+    dpce.origin,
+    dpce.platform,
+    dpce.content_page,
+    dpce.ts_event,
+    dpce.year,
+    dpce.month,
+    dpce.day
+  FROM
+    adhoc_rules AS dpce
+  LEFT JOIN
+    datalake_growth_taxonomy.unified_taxonomy_dictionary AS dict
+      ON COALESCE(SF_NORMALIZE_STRING(dpce.utm_campaign),0) = COALESCE(LOWER(dict.utm_campaign),0)
+      AND COALESCE(SF_NORMALIZE_STRING(dpce.utm_source),0) = COALESCE(LOWER(dict.utm_source),0)
+      AND COALESCE(SF_NORMALIZE_STRING(dpce.utm_medium),0) = COALESCE(LOWER(dict.utm_medium),0)
+  LEFT JOIN
+    datalake_gsheets_raw.taxonomy_demand_exception_flow AS ef
+      ON LOWER(dpce.utm_campaign) = LOWER(ef.utm_campaign)
+      AND LOWER(dpce.utm_source) = LOWER(ef.utm_source)
+      AND LOWER(dpce.utm_medium) = LOWER(ef.utm_medium)
+)
+
+SELECT
+  id_demand_prospect_conversion_event,
+  id_prospect,
+  business_context,
+  id_event_type,
+  event_name,
+  id_media_setup_from_adhoc,
+  id_media_setup_from_dictionary,
+  id_media_setup_from_exception_flow,
+  id_media_setup_from_naming_convention,
+  CASE
+    WHEN id_media_setup_from_adhoc IS NOT NULL THEN id_media_setup_from_adhoc
+    WHEN id_media_setup_from_dictionary IS NOT NULL THEN id_media_setup_from_dictionary
+    WHEN id_media_setup_from_exception_flow IS NOT NULL THEN id_media_setup_from_exception_flow
+    WHEN id_media_setup_from_naming_convention IS NOT NULL THEN id_media_setup_from_naming_convention
+  END AS final_id_media_setup,
+  CASE
+    WHEN id_media_setup_from_adhoc IS NOT NULL THEN "adhoc rule"
+    WHEN id_media_setup_from_dictionary IS NOT NULL THEN "dictionary"
+    WHEN id_media_setup_from_exception_flow IS NOT NULL THEN "exception flow"
+    WHEN id_media_setup_from_naming_convention IS NOT NULL THEN "naming convention"
+  END AS final_media_setup_id_origin,
+  id_rent_flow,
+  id_sale_flow, 
+  id_booking,
+  id_offer,
+  id_talk_to_agent,
+  id_house,
+  id_region,
+  id_owner,
+  id_agent,
+  utm_adhoc_rule,
+  utm_campaign, 
+  utm_medium,
+  utm_source,
+  utm_term,
+  app_type,
+  booking_creator,
+  product_origin,
+  is_3p_demand,
+  operation_channel,
+  referral_type,
+  origin,
+  platform,
+  content_page,
+  ts_event,
+  year,
+  month,
+  day
+FROM
+  media_setup_ids
