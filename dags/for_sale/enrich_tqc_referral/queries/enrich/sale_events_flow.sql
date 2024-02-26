fe377aef8a18bc94f88b8d3cdb6d0b59ece5d085
rodrigorochedo <96083546+rodrigorochedo@users.noreply.github.com>
2024-02-26 14:30:19 -0300

    [DFORSALE-7291] add boolean column to identify the status of the visit (#13518)
    * add boolean column to identify the status of the visit
    
    * Update dags/for_sale/enrich_tqc_referral/queries/enrich/sale_events_flow.sql
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>

--- dags/for_sale/enrich_tqc_referral/queries/enrich/sale_events_flow.sql
+++ dags/for_sale/enrich_tqc_referral/queries/enrich/sale_events_flow.sql
@@ -1,5 +1,5 @@
 WITH sale_visit_flows AS (
-  SELECT 
+  SELECT
     b.id_visit,
     b.id_house,
     b.id_visitor AS id_buyer,
@@ -10,7 +10,7 @@ WITH sale_visit_flows AS (
     b.user_sale_booking_creator AS visit_creation_origin,
     b.last_update_source AS visit_update_origin,
     b.code AS visit_code,
-    CASE 
+    CASE
       WHEN status = 'Realizado' AND b.visit_fup in ('EntradaNaoAutorizada','NaoCompareceu')
         THEN 'Cancelado'
       ELSE b.status
@@ -19,7 +19,7 @@ WITH sale_visit_flows AS (
     FIRST_VALUE(b.ts_created_local_tz) OVER(PARTITION BY b.id_visit ORDER BY b.ts_created_local_tz) AS ts_first_visit_created,
     b.ts_created_local_tz AS ts_visit_created,
     b.ts_updated AS ts_visit_updated
-  FROM 
+  FROM
     datalake_booking.booking AS b
   WHERE
     b.type = 'Visita'
@@ -41,19 +41,20 @@ WITH sale_visit_flows AS (
     svf.visit_code,
     svf.visit_status,
     tqc.tqc_flow,
-    CASE 
+    CASE
       WHEN tqc.tqc_flow = 'Old'
         THEN 1
       WHEN tqc.tqc_flow = 'New'
         THEN 2
-      ELSE NULL 
+      ELSE NULL
     END AS id_tqc_flow,
-    CASE 
+    svf.visit_status = 'Realizado' AS is_visit_completed,
+    CASE
       WHEN tqc.id_user_lead IS NOT NULL
         THEN TRUE
       ELSE FALSE
     END AS is_tqc,
-    tqc.ts_created AS ts_tqc_referral,  
+    tqc.ts_created AS ts_tqc_referral,
     svf.ts_visit_scheduled_for,
     svf.ts_first_visit_created,
     svf.ts_visit_created,
@@ -62,12 +63,12 @@ WITH sale_visit_flows AS (
     so.dt_offer_accepted AS ts_offer_accepted,
     so.dt_sale_agreement_created AS ts_sale_agreement_created,
     so.dt_sale_agreement_signed AS ts_sale_agreement_signed
-  FROM 
+  FROM
     sale_visit_flows AS svf
   FULL OUTER JOIN
     datalake_offer.sale_offer AS so
       ON svf.id_sale_flow = so.id_sale_flow
-  LEFT JOIN 
+  LEFT JOIN
     datalake_tqc_referral.unified_lead_referral_flow AS tqc
       ON CONCAT(COALESCE(svf.id_agent,svf.id_user_sale_agent,so.id_agent,so.id_user_agent),'_',COALESCE(svf.id_buyer, so.id_buyer)) = tqc.id_referral_flow
     AND (tqc.status = 'TRUE' OR tqc.status = 'CONFIRMED')
