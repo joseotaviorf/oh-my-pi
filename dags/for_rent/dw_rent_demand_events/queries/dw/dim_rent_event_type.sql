ae287c6c997c2aca0d95cd5b611a8d49a05fb330
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-02-29 10:57:21 -0300

    [AEFR] Adding ts_load to dim_rent_event_type (#13590)

--- dags/for_rent/dw_rent_demand_events/queries/dw/dim_rent_event_type.sql
+++ dags/for_rent/dw_rent_demand_events/queries/dw/dim_rent_event_type.sql
@@ -2,58 +2,68 @@ SELECT
       1 AS sk_event_type,
       'VISIT_BOOKED' AS event_name,
       'VB' AS abbreviation,
-      'BOOKING' AS stage
+      'BOOKING' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       2 AS sk_event_type,
       'VISIT_COMPLETED' AS event_name,
       'VC' AS abbreviation,
-      'BOOKING' AS stage
+      'BOOKING' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       3 AS sk_event_type,
       'OFFER_SUBMITTED' AS event_name,
       'OS' AS abbreviation,
-      'OFFER' AS stage
+      'OFFER' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       4 AS sk_event_type,
       'OFFER_ACCEPTED' AS event_name,
       'OA' AS abbreviation,
-      'OFFER' AS stage
+      'OFFER' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       5 AS sk_event_type,
       'EVALUATION STARTED' AS event_name,
       'ES' AS abbreviation,
-      'PROPOSAL' AS stage
+      'PROPOSAL' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       6 AS sk_event_type,
       'EVALUATION POSITIVE' AS event_name,
       'EP' AS abbreviation,
-      'PROPOSAL' AS stage
+      'PROPOSAL' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       7 AS sk_event_type,
       'DOCUMENT_SENT' AS event_name,
       'DS' AS abbreviation,
-      'PROPOSAL' AS stage
+      'PROPOSAL' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       8 AS sk_event_type,
       'CREDIT_APPROVED' AS event_name,
       'CA' AS abbreviation,
-      'PROPOSAL' AS stage
+      'PROPOSAL' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       9 AS sk_event_type,
       'CONTRACT_SIGNED' AS event_name,
       'CS' AS abbreviation,
-      'CONTRACT' AS stage
+      'CONTRACT' AS stage,
+      NOW() AS ts_load
   UNION ALL
   SELECT
       10 AS sk_event_type,
       'CONTRACT_CREATED' AS event_name,
       'CC' AS abbreviation,
-      'CONTRACT' AS stage
+      'CONTRACT' AS stage,
+      NOW() AS ts_load
