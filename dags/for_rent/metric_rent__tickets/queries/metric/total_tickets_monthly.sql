e147a4210f3bafd2366ac6b49643ba634c7fb7e1
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-03-05 13:45:45 -0300

    [AEFR-4173] Creating Ticket Rate V2 metric (#13622)
    * creating TR v2 metric
    
    * fixing filter
    
    * query fixes
    
    * query updates
    
    * query fixes
    
    * metadata file
    
    * updating date rule for ongoing rentals
    
    * truncating in a monthly view
    
    * data quality
    
    * doc updates
    
    * fix indentation
    
    * dependencies
    
    * descriptions updates

---
+++ dags/for_rent/metric_rent__tickets/queries/metric/total_tickets_monthly.sql
@@ -0,0 +1,142 @@
+WITH contract_signed AS (
+    SELECT
+        SUM(new_contracts_signed) AS new_contracts_signed,
+        DATE_TRUNC('month', day) AS dt_month
+    FROM
+        metric_rent.new_contracts_signed_daily
+    WHERE
+        day >= DATE('2022-01-01')
+        AND country_code = 'BR'
+    GROUP BY 2
+), 
+ccv AS (
+    SELECT 
+        COUNT(DISTINCT CASE WHEN fo.sk_sale_agreement_signed_date > 0 THEN sk_offer END) AS ccvs,
+        DATE_TRUNC('month', dd.date) AS dt_month
+    FROM 
+        dw_sale.fact_offers fo
+    INNER JOIN 
+        dw_public.dim_date dd 
+            ON dd.sk_date = fo.sk_sale_agreement_signed_date
+    WHERE
+        dd.date >= DATE('2022-01-01')
+    GROUP BY 2
+),
+aux_ongoing_rentals AS (
+    /** We only want to analyze the number of ongoing rentals on the last day of the month if it's a closed month.
+        If the current month hasn't finished yet, we want to analyze the number of ongoing rentals for the most
+        recent day.
+    **/
+    SELECT
+        MAX(day) AS dt_last_day,
+        DATE_TRUNC('month', day) AS dt_month
+    FROM
+        metric_rent.ongoing_rentals_daily
+    GROUP BY 2
+),
+ongoing_rentals AS (
+    SELECT DISTINCT
+        o.ongoing_rentals,
+        a.dt_month
+    FROM
+        metric_rent.ongoing_rentals_daily AS o
+    INNER JOIN
+        aux_ongoing_rentals AS a
+            ON a.dt_last_day = o.day
+    WHERE
+        o.day >= DATE('2022-01-01')
+        AND o.country_code = 'BR'
+),
+ended_rentals AS (
+    SELECT
+        SUM(ended_rentals_confirmed) AS ended_rentals,
+        DATE_TRUNC('month', day) AS dt_month
+    FROM 
+        metric_rent.ended_rentals_confirmed_daily
+    WHERE 
+        day >= DATE('2022-01-01')
+        AND country_code = 'BR'
+    GROUP BY 2
+),
+drivers AS (
+    SELECT 
+        cs.new_contracts_signed,
+        ccv.ccvs,
+        ors.ongoing_rentals,
+        ers.ended_rentals,
+        d.month_start AS dt_month
+    FROM
+        dw_public.dim_date AS d 
+    LEFT JOIN
+        contract_signed AS cs 
+            ON cs.dt_month = d.month_start
+    LEFT JOIN 
+        ccv
+            ON ccv.dt_month = d.month_start
+    LEFT JOIN 
+        ongoing_rentals ors
+            ON ors.dt_month = d.month_start
+    LEFT JOIN 
+        ended_rentals ers
+            ON ers.dt_month = d.month_start
+    WHERE
+        d.sk_date >= 20220101
+),
+total_tickets_prep AS (
+    SELECT 
+        DATE_TRUNC('month', ts_solved) AS dt_month,
+        CASE 
+            WHEN dt.sub_journey NOT IN ('Partners', 'For Sale') THEN 'ForRent'
+            WHEN dt.sub_journey IN ('For Sale') THEN 'ForSale'
+            WHEN dt.sub_journey IN ('Partners') THEN dt.sub_journey
+            ELSE NULL
+        END AS context,
+        dt.journey,
+        dt.sub_journey,
+        dt.line_owner,
+        dd.team,
+        dd.department,
+        dd.front_or_back AS ticket_type,
+        dt.theme,
+        dt.theme_detail,
+        CASE
+            WHEN dt.sub_journey IN ('Listing & Search', 'Visits to Offer', 'Contract to Entrance', 'Onboarding') THEN dv.new_contracts_signed
+            WHEN dt.sub_journey = 'For Sale' THEN dv.ccvs
+            WHEN dt.sub_journey = 'Partners' THEN (dv.new_contracts_signed + dv.ccvs)
+            WHEN dt.sub_journey = 'Ongoing' THEN dv.ongoing_rentals
+            WHEN dt.sub_journey = 'Offboarding' THEN dv.ended_rentals
+            ELSE NULL 
+        END AS driver,
+        COUNT(ft.sk_ticket) AS total_tickets_pure,
+        SUM(ft.total_tickets_proportional) AS total_tickets_proportional
+    FROM 
+        dw_customer_support.fact_ticket AS ft 
+    LEFT JOIN
+        dw_customer_support.dim_taxonomy AS dt
+            ON ft.sk_taxonomy = dt.sk_taxonomy 
+    LEFT JOIN 
+        dw_customer_support.dim_department AS dd 
+            ON ft.sk_main_department = dd.sk_department
+    LEFT JOIN 
+        drivers AS dv
+            ON DATE_TRUNC('month', ft.ts_solved) = dv.dt_month
+    WHERE 
+        ft.is_ticket_rate = TRUE
+    GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+)
+SELECT
+    DATE(dt_month) AS dt_month,
+    context,
+    journey,
+    sub_journey,
+    line_owner,
+    team,
+    department,
+    ticket_type,
+    theme,
+    theme_detail,
+    total_tickets_pure,
+    total_tickets_proportional,
+    (total_tickets_proportional/driver) AS ticket_rate_monthly
+FROM 
+    total_tickets_prep
