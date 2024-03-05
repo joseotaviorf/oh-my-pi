d8ae6541f5418525a1637d1040dd7deac2b96cdd
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2024-03-05 09:45:44 -0300

    [DIN-928] Force type casts in CDC transactional layer (#13629)
    Add methods to treat decimals and booleans
    Add method to map MySQL to Spark data types
    Add method to preserve data types by looking at existing datalake table

--- bietlejuice/base/cdc/schema_treatment/cdc_schema_treatment.py
+++ bietlejuice/base/cdc/schema_treatment/cdc_schema_treatment.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
 
 
 class CdcSchemaTreatment(ABC):
@@ -9,3 +10,20 @@ class CdcSchemaTreatment(ABC):
     ) -> DataFrame:
         """Treats the dataframe columns of the transactional layer, such as converting unix timestamps to datetime."""
         pass
+
+    def _cast_differing_types(
+        self, transactional_dataframe: DataFrame, datalake_dataframe: DataFrame
+    ) -> DataFrame:
+        """Cast columns in the transactional dataframe to the same type as the datalake dataframe, to avoid type mismatches"""
+
+        for column in datalake_dataframe.columns:
+            if column not in transactional_dataframe.columns:
+                continue
+            if (
+                transactional_dataframe.schema[column].dataType
+                != datalake_dataframe.schema[column].dataType
+            ):
+                transactional_dataframe = transactional_dataframe.withColumn(
+                    column, col(column).cast(datalake_dataframe.schema[column].dataType)
+                )
+        return transactional_dataframe
--- bietlejuice/base/cdc/schema_treatment/mysql_cdc_schema_treatment.py
+++ bietlejuice/base/cdc/schema_treatment/mysql_cdc_schema_treatment.py
@@ -12,6 +12,21 @@ from bietlejuice.base.spark import BaseSparkContext
 
 
 class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
+    TYPE_MAPPING = {
+        "SMALLINT": "int",
+        "MEDIUMINT": "int",
+        "INT": "int",
+        "BIGINT": "bigint",
+        "FLOAT": "double",
+        "DOUBLE": "double",
+        "TINYBLOB": "binary",
+        "BLOB": "binary",
+        "MEDIUMBLOB": "binary",
+        "LONGBLOB": "binary",
+        "BINARY": "binary",
+        "VARBINARY": "binary",
+    }
+
     def __init__(
         self, schema_finder: MySqlCdcSchemaFinder, datalake_table_schema: str
     ) -> None:
@@ -22,13 +37,18 @@ class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
         self, schema: str, table_name: str, transactional_dataframe: DataFrame
     ) -> DataFrame:
         latest_table_change = self._try_find_latest_table_definition(schema, table_name)
+        if latest_table_change:
+            transactional_dataframe = self._treat_columns_from_latest_table_change(
+                latest_table_change, transactional_dataframe
+            )
+
         datalake_dataframe = self._try_find_existing_datalake_table(
             self.datalake_table_schema, table_name
         )
-
-        transactional_dataframe = self._treat_timestamp_columns(
-            transactional_dataframe, latest_table_change, datalake_dataframe
-        )
+        if datalake_dataframe:
+            transactional_dataframe = self._treat_columns_from_existing_datalake_table(
+                datalake_dataframe, transactional_dataframe
+            )
 
         return transactional_dataframe
 
@@ -50,20 +70,37 @@ class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
         except AnalysisException:
             return None
 
-    def _treat_timestamp_columns(
-        self,
-        transactional_dataframe: DataFrame,
-        latest_table_change: dict = None,
-        datalake_dataframe: DataFrame = None,
+    def _treat_columns_from_latest_table_change(
+        self, latest_table_change: dict, transactional_dataframe: DataFrame
     ) -> DataFrame:
-        if latest_table_change:
-            transactional_dataframe = self._treat_timestamp_columns_from_schema_topic(
-                transactional_dataframe, latest_table_change
-            )
-        if datalake_dataframe:
-            transactional_dataframe = self._treat_timestamp_columns_from_existing_datalake_table(
-                transactional_dataframe, datalake_dataframe
-            )
+        """Forces the columns of the transactional dataframe to match the latest table DDL change in the source database, which is found in the schema topic."""
+
+        transactional_dataframe = self._treat_timestamp_columns_from_schema_topic(
+            transactional_dataframe, latest_table_change
+        )
+        transactional_dataframe = self._treat_boolean_columns(
+            transactional_dataframe, latest_table_change
+        )
+        transactional_dataframe = self._treat_decimal_columns(
+            transactional_dataframe, latest_table_change
+        )
+        transactional_dataframe = self._map_column_types(
+            transactional_dataframe, latest_table_change
+        )
+
+        return transactional_dataframe
+
+    def _treat_columns_from_existing_datalake_table(
+        self, datalake_dataframe: DataFrame, transactional_dataframe: DataFrame
+    ) -> DataFrame:
+        """Forces the columns of the transactional dataframe to match the schema of the existing datalake table, to avoid type mismatches."""
+
+        transactional_dataframe = self._treat_timestamp_columns_from_existing_datalake_table(
+            transactional_dataframe, datalake_dataframe
+        )
+        transactional_dataframe = self._cast_differing_types(
+            transactional_dataframe, datalake_dataframe
+        )
 
         return transactional_dataframe
 
@@ -79,6 +116,8 @@ class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
         string_columns = []
 
         for column in latest_table_change["columns"]:
+            if column["name"] not in transactional_dataframe.columns:
+                continue
             if column["typeName"] == "DATE":
                 days_unix_columns.append(column["name"])
             elif column["typeName"] == "DATETIME":
@@ -105,7 +144,9 @@ class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
         string_columns = []
 
         for column in datalake_dataframe.columns:
-            if transactional_dataframe.schema[column].dataType.typeName() in (
+            if column not in transactional_dataframe.columns or transactional_dataframe.schema[
+                column
+            ].dataType.typeName() in (
                 "date",
                 "timestamp",
             ):
@@ -151,4 +192,48 @@ class MySqlCdcSchemaTreatment(CdcSchemaTreatment):
                 column, to_timestamp(col(column))
             )
 
+        return transactional_dataframe
+
+    def _treat_boolean_columns(
+        self, transactional_dataframe: DataFrame, latest_table_change: dict
+    ) -> DataFrame:
+        for column in latest_table_change["columns"]:
+            if column["name"] not in transactional_dataframe.columns:
+                continue
+            if column["typeName"] not in ("BIT", "TINYINT"):
+                continue
+            if column["length"] == 1:
+                transactional_dataframe = transactional_dataframe.withColumn(
+                    column["name"], col(column["name"]).cast("boolean")
+                )
+            else:
+                transactional_dataframe = transactional_dataframe.withColumn(
+                    column["name"], col(column["name"]).cast("int")
+                )
+        return transactional_dataframe
+
+    def _treat_decimal_columns(
+        self, transactional_dataframe: DataFrame, latest_table_change: dict
+    ) -> DataFrame:
+        for column in latest_table_change["columns"]:
+            if column["name"] not in transactional_dataframe.columns:
+                continue
+            if column["typeName"] in ("DECIMAL", "NUMERIC"):
+                type = f"decimal({column['length']}, {column['scale']})"
+                transactional_dataframe = transactional_dataframe.withColumn(
+                    column["name"], col(column["name"]).cast(type)
+                )
+        return transactional_dataframe
+
+    def _map_column_types(
+        self, transactional_dataframe: DataFrame, latest_table_change: dict
+    ) -> DataFrame:
+        for column in latest_table_change["columns"]:
+            if column["name"] not in transactional_dataframe.columns:
+                continue
+            if column["typeName"] in self.TYPE_MAPPING:
+                transactional_dataframe = transactional_dataframe.withColumn(
+                    column["name"],
+                    col(column["name"]).cast(self.TYPE_MAPPING[column["typeName"]]),
+                )
         return transactional_dataframe
