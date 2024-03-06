89a160c27112ef2de340bfc70e3a8578988e7bde
Raimundo Neto <44676140+RaimundoNeto7@users.noreply.github.com>
2024-03-06 13:44:32 -0400

    chore(vespucio-pipeline): Bump v0.2.8 (#13634)
    Co-authored-by: Gabriel Santos <ogabrielsantos@users.noreply.github.com>

--- dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
+++ dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
@@ -20,7 +20,7 @@ from bietlejuice.base.databricks.databricks_group_name_enum import (
 from bietlejuice.services.configuration_service import ConfigurationService
 
 VESPUCIO_PACKAGE_NAME = "vespucio"
-VESPUCIO_PACKAGE_VERSION = "0.2.7"
+VESPUCIO_PACKAGE_VERSION = "0.2.8"
 VESPUCIO_WHEEL_FILE = (
     f"{VESPUCIO_PACKAGE_NAME}-{VESPUCIO_PACKAGE_VERSION}-py3-none-any.whl"
 )
