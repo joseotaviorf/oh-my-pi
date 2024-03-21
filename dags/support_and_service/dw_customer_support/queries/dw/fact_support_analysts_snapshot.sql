3cfd781329d7249e0d59728086bbe398c01460eb
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2024-03-21 15:16:40 -0300

    [DDSS]: Create new analysts model (#13774)

---
+++ dags/support_and_service/dw_customer_support/queries/dw/fact_support_analysts_snapshot.sql
@@ -0,0 +1,123 @@
+WITH without_agg_infos AS (
+    SELECT
+        ut.id_ticket,
+        MD5(a.email) AS id_agent,
+        dc.team,
+        rd.status,
+        ut.resolution_survey,
+        ut.reopens AS reopened_tickets,
+        ut.replies AS replied_tickets,
+        ut.first_csat_score AS first_csat_score,
+        DATEDIFF(ut.ts_solved, ut.ts_started) AS attendance_time,
+        CASE
+            WHEN ROW_NUMBER() OVER(PARTITION BY MD5(
+              COALESCE(CONCAT(rd.id_call, 'call'), 
+              CONCAT(rd.id_session, 'chat'), 
+              CONCAT(rd.id_ticket, 'email'))
+            ), rd.department ORDER BY rd.ts_created) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_first_department_interaction,
+        bmt.is_backlog_in_time,
+        bmt.is_backlog_not_in_time,
+        IF(bmt.is_backlog_in_time OR bmt.is_backlog_not_in_time, TRUE, FALSE) AS is_backlog,
+        ut.ts_csat_response,
+        DATE(tf.ts_updated) AS dt_ticket_updated
+    FROM
+        datalake_customer_support.unified_tickets AS ut
+    LEFT JOIN
+        datalake_zendesk_ticket_funnels.ticket_funnel AS tf
+            ON ut.id_ticket = tf.id_ticket
+    LEFT JOIN
+        datalake_customer_support.received_demand AS rd
+            ON ut.id_ticket = rd.id_ticket
+    LEFT JOIN
+        datalake_customer_demand.backlog_metrics_tasks AS bmt
+            ON ut.id_ticket = bmt.id_task
+    LEFT JOIN
+        datalake_gsheets_clean.department_control AS dc
+            ON ut.id_main_department = MD5(dc.department)
+    LEFT JOIN
+        datalake_zendesk_users.agents AS a
+            ON ut.id_last_agent = MD5(a.email)
+    WHERE
+        ut.front_or_back <> "undefined"
+        AND dc.team IS NOT NULL
+        AND DATE(tf.ts_updated) = MAKE_DATE({year}, {month}, {day})
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY ut.id_ticket ORDER BY tf.ts_updated DESC) = 1
+)
+SELECT
+    wa.id_agent || dt_ticket_updated AS sk_snapshot,
+    wa.id_agent AS sk_agent,
+    COUNT(id_ticket) AS total_tickets,
+    SUM(wa.reopened_tickets) AS ticket_reopenings,
+    SUM(wa.replied_tickets) AS ticket_responses,
+    SUM(wa.attendance_time) AS total_attendance_time,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.first_csat_score IS NOT NULL 
+                THEN wa.id_ticket
+            ELSE NULL 
+        END
+    ) AS tickets_with_csat_score,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.first_csat_score IN (4,5) 
+                THEN wa.id_ticket
+            ELSE NULL 
+        END
+    ) AS tickets_csat_satisfied,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.first_csat_score = 3 
+                THEN wa.id_ticket
+            ELSE NULL 
+        END
+    ) AS tickets_csat_neutral,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.first_csat_score IN (1,2) 
+                THEN wa.id_ticket
+            ELSE NULL 
+        END
+    ) AS tickets_csat_dissatisfied,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.resolution_survey IS NOT NULL 
+                OR wa.ts_csat_response IS NOT NULL 
+                THEN wa.id_ticket 
+        END
+    ) AS tickets_with_resolution_answered,
+    COUNT(
+        CASE 
+            WHEN 
+                wa.resolution_survey = True 
+                OR wa.ts_csat_response IS NOT NULL 
+                THEN wa.id_ticket 
+        END
+    ) AS tickets_with_resolution,
+    COUNT(
+        CASE 
+            WHEN wa.status = 'TRANSFERRED' 
+            AND wa.is_first_department_interaction = True 
+            AND wa.team <> 'Inside Sales' 
+            THEN wa.id_ticket
+            ELSE NULL
+        END
+    ) AS tickets_transferred,
+    COUNT_IF(wa.is_backlog = TRUE) AS tickets_in_backlog,
+    COUNT_IF(wa.is_backlog_in_time = TRUE) AS backlog_within_sla,
+    COUNT_IF(wa.is_backlog_not_in_time = TRUE) AS backlog_with_exceed_sla,
+    wa.dt_ticket_updated AS dt_last_ticket_updated,
+    YEAR(wa.dt_ticket_updated) AS year,
+    MONTH(wa.dt_ticket_updated) AS month,
+    DAY(wa.dt_ticket_updated) AS day
+FROM 
+    without_agg_infos AS wa
+GROUP BY 
+    2, dt_ticket_updated
