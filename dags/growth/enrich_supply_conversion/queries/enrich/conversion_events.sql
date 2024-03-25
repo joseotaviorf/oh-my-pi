b52dce9ad69b4e120e01401f2ea3f8123006797a
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-03-25 15:01:29 -0300

    [GROW-2976] Supply Deploy - Part 3 (#13766)
    * first commit
    
    * feat: create first tables
    
    * feat: create dependencies
    
    * feat: set inner dependencies
    
    * feat: insert qualified events
    
    * feat: refact column
    
    * feat: fix dependencies
    
    * fix: adding weight
    
    * fix: change column name
    
    * adjusting sql
    
    * fix: revert column name
    
    * feat: creating conversion_staging
    
    * feat: create last pj attribution
    
    * fix: change to one query
    
    * fix: change CTE alias
    
    * feat: adjusting dependencies
    
    * feat: adding fl and opp events
    
    * feat: step aq2o
    
    * feat: prospects q2aq
    
    * feat: conversion original events
    
    * chore: remove 3p reference
    
    * feat: create conversion_events
    
    * create first version of 3p conversion
    
    * adjusting partitions and load type
    
    * adjusting partitions
    
    * adjusting queries
    
    * fix: ajuste para schema temporário
    
    * fix: adjusting dependencies
    
    * fix: adjusting 3P dependencies
    
    * fix: adjusting inner dependencies
    
    * fix: adjusting queries
    
    * feat: creating metadata
    
    * feat: adjusting prospects_events
    
    * feat: create metadata
    
    * feat: adjusting last_pj_attribution metadata
    
    * fix: adjusting metadata
    
    * fix: adjusting database name
    
    * fix: change 3p path
    
    * adjusting dependencies
    
    * create metadata
    
    * fix: adding conversion_staging lineage
    
    * fix: conversion staging
    
    * fix: conversion_original_events
    
    * fix: conversion_original_events
    
    * feat: create conversion events metadata
    
    * remove comments
    
    * fix query
    
    * fix: change schema
    
    * fix: create partitions
    
    * fix: adjusting dependencies
    
    * fix: change partitions
    
    * fix: remove year, month, day

---
+++ dags/growth/enrich_supply_conversion/queries/enrich/conversion_events.sql
@@ -0,0 +1,142 @@
+WITH events_3p AS (
+    SELECT
+    c3p.id_lead,
+    COALESCE(c3p.id_house, -1) AS id_entity,
+    NULL AS id_user_registrant,
+    c3p.business_context,
+    c3p.supply_source,
+    c3p.funnel_step AS step,
+    c3p.weight,
+    c3p.ts_event,
+    c3p.ts_event AS ts_event_adjusted,
+    IF(c3p.business_event LIKE 'drop%', c3p.ts_event, NULL) AS ts_first_discard,
+    IF(c3p.business_event LIKE 'drop%', c3p.ts_event, NULL) AS ts_last_discard,
+    NULL AS rev,
+    c3p.business_event,
+    c3p.drop_step_reason,
+    'T7.0' AS aux_group,
+    FALSE AS aux_data_event
+    FROM datalake_supply_flows_migrate.conversion_events_3p AS c3p
+    LEFT ANTI JOIN datalake_supply_flows_migrate.conversion_original_events AS t2
+        ON (c3p.id_house = t2.id_entity)
+        AND (c3p.business_context = t2.business_context)
+        AND (t2.aux_group LIKE 'T2%')
+),
+original_events AS (
+    SELECT * EXCEPT (year, month, day)
+    FROM datalake_supply_flows_migrate.conversion_original_events
+    UNION ALL
+    SELECT *
+    FROM events_3p
+),
+base AS (
+  SELECT
+    id_lead,
+    id_entity,
+    business_context,
+    supply_source,
+    step,
+    ts_event_adjusted
+  FROM original_events
+),
+pivot_table AS (
+  SELECT 
+    *
+  FROM base
+  PIVOT (
+    MIN(ts_event_adjusted)
+    FOR (step) IN (
+        'QUALIFIED' AS ts_qualified,
+        'AV_QUALIFIED' AS ts_available_qualified,
+        'OPPORTUNITY' AS ts_opportunity,
+        'FIRST_LISTING' AS ts_first_listing
+    )
+  )
+),
+fill_date (
+  SELECT 
+    id_lead,
+    id_entity,
+    business_context,
+    supply_source,
+    ts_qualified AS ts_qualified_original, 
+    COALESCE(ts_qualified, ts_available_qualified, ts_opportunity, ts_first_listing) AS ts_qualified,
+    ts_available_qualified AS ts_available_qualified_original, 
+    COALESCE(ts_available_qualified, ts_opportunity, ts_first_listing) AS ts_available_qualified,
+    ts_opportunity AS ts_opportunity_original, 
+    COALESCE(ts_opportunity, ts_first_listing) AS ts_opportunity,
+    ts_first_listing
+  FROM pivot_table
+),
+events_to_insert AS (
+  SELECT 
+    id_lead,
+    id_entity,
+    business_context,
+    supply_source,
+    step,
+    ts_event
+  FROM fill_date
+  UNPIVOT (
+      ts_event FOR step IN (
+        ts_qualified AS QUALIFIED,
+        ts_available_qualified AS AV_QUALIFIED,
+        ts_opportunity AS OPPORTUNITY,
+        ts_first_listing AS FIRST_LISTING
+      )
+  )
+  EXCEPT ALL
+  SELECT *
+  FROM base
+),
+last_extract AS (
+  SELECT 
+    id_lead,
+    id_entity,
+    NULL AS id_user_registrant,
+    business_context,
+    supply_source,
+    step,
+    CASE 
+      WHEN step = 'QUALIFIED' THEN 3
+      WHEN step = 'AV_QUALIFIED' THEN 3
+      WHEN step = 'OPPORTUNITY' THEN 2
+      WHEN step = 'FIRST_LISTING' THEN 1
+    END AS weight,
+    NULL AS ts_event_original,
+    ts_event AS ts_event_adjusted,
+    NULL AS ts_first_discard,
+    NULL AS ts_last_discard,
+    NULL AS rev,
+    CASE 
+      WHEN step = 'QUALIFIED' THEN 'conversion_p2q'
+      WHEN step = 'AV_QUALIFIED' THEN 'conversion_q2aq'
+      WHEN step = 'OPPORTUNITY' THEN 'conversion_aq2o'
+      WHEN step = 'FIRST_LISTING' THEN 'conversion_o2fl'
+    END AS business_event,
+    NULL AS reason,
+    'T6.0' AS aux_group,
+    TRUE AS aux_data_event
+  FROM events_to_insert
+),
+fill_events (
+    SELECT * 
+    FROM last_extract AS le
+    LEFT ANTI JOIN datalake_supply_flows_migrate.conversion_events_3p AS ce3
+    ON (le.id_entity = ce3.id_house)
+        AND (le.business_context = ce3.business_context)
+        AND (le.step = ce3.funnel_step)
+)
+
+SELECT 
+  *,
+  YEAR(ts_event_adjusted) AS year,
+  MONTH(ts_event_adjusted) AS month,
+  DAY(ts_event_adjusted) AS day
+FROM original_events
+UNION ALL 
+SELECT *,
+  YEAR(ts_event_adjusted) AS year,
+  MONTH(ts_event_adjusted) AS month,
+  DAY(ts_event_adjusted) AS day
+FROM fill_events
---
+++ dags/growth/enrich_supply_conversion/queries/enrich/prospects_events.sql
@@ -0,0 +1,79 @@
+WITH extracted AS (
+  SELECT 
+    id AS id_prospect,
+    is_for_rent,
+    is_for_sale
+  FROM datalake_wololo_clean.prospect
+),
+corner_cases AS (
+  SELECT 
+    id_prospect,
+    'RENT' AS bc_rent,
+    'SALE' AS bc_sale
+  FROM extracted
+  WHERE is_for_rent IS FALSE
+    AND is_for_sale IS FALSE
+),
+fill_bc AS (
+  SELECT 
+    id_prospect,
+    IF(COALESCE(is_for_rent, TRUE), 'RENT', NULL) AS bc_rent,
+    IF(COALESCE(is_for_sale, TRUE), 'SALE', NULL) AS bc_sale
+  FROM extracted
+  WHERE NOT(is_for_rent IS FALSE
+    AND is_for_sale IS FALSE)
+),
+pivot_tb AS (
+  SELECT 
+    id_prospect,
+    EXPLODE(ARRAY(bc_rent, bc_sale)) AS business_context
+  FROM fill_bc
+  UNION ALL
+  SELECT 
+    id_prospect,
+    EXPLODE(ARRAY(bc_rent, bc_sale)) AS business_context
+  FROM corner_cases
+),
+prospect_business_context AS (
+  SELECT *
+  FROM pivot_tb
+  WHERE business_context IS NOT NULL
+),
+extract_wololo AS (
+  SELECT
+    p.id AS id_prospect,
+    p.id_reference AS id_lead_ebdb,
+    p.id_external AS id_lead_rene,
+    p.status,
+    bc.business_context,
+    p.ts_created,
+    p.ts_updated
+  FROM
+    datalake_wololo_clean.prospect AS p
+  LEFT JOIN prospect_business_context AS bc
+    ON (p.id = bc.id_prospect)
+)
+
+SELECT
+  id_prospect AS id_entity,
+  id_prospect AS id_source,
+  cd.id_user AS id_user_registrant,
+  p.id_lead_ebdb AS id_lead,
+  cd.id AS rev,
+  business_context,
+  p.status,
+  'WOLOLO' AS source,
+  'PROSPECT' AS step,
+  4 AS weight,
+  cd.reason,
+  sds.drop_step,
+  cd.ts_created AS ts_event,
+  YEAR(cd.ts_created) AS year,
+  MONTH(cd.ts_created) AS month,
+  DAY(cd.ts_created) AS day
+FROM
+  extract_wololo AS p
+JOIN datalake_wololo_clean.context_discard AS cd
+  USING (id_prospect, business_context)
+LEFT JOIN datalake_supply_flows.supply_discards_settings AS sds
+  ON cd.reason = sds.discards_reason
