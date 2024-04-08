debdd84ca5aa4f6dacdff6961177decba3903f5a
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-04-08 15:25:45 -0300

    [AEFR-4429] Add columns to dim_termination (#13943)
    * add columns to dim_retention

--- dags/for_rent/dw_retention/queries/dw/dim_termination.sql
+++ dags/for_rent/dw_retention/queries/dw/dim_termination.sql
@@ -2,6 +2,9 @@ SELECT
     id_termination AS sk_termination,
     repair_cost,
     status,
+    send_utility_bills_receipt,
+    attachment_type_list,
+    dt_ended_termination,
     ts_created,
     NOW() AS ts_load
 FROM
