fc958b47caa6304af0995fd0428e14fe8e5c8e63
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-04-10 10:25:16 -0300

    [AEFR-4447] Create termination_with_proof_payment metric (#13976)
    * create termination_with_proof_payment metric
    
    * update description

---
+++ dags/for_rent/metric_rent__retention/queries/metric/termination_with_proof_payment_monthly.sql
@@ -0,0 +1,31 @@
+WITH tenant_pending_tasks AS (
+  SELECT
+    fct.sk_contract,
+    dt.send_utility_bills_receipt,
+    dt.attachment_type_list,
+    dt.dt_ended_termination
+  FROM
+    dw_retention.fact_contract_termination AS fct
+  JOIN
+    dw_retention.dim_termination AS dt
+      ON fct.sk_termination = dt.sk_termination
+  WHERE
+    DATE_TRUNC('MONTH', dt.dt_ended_termination) >= DATE_TRUNC('MONTH', CURRENT_DATE() - INTERVAL '12' MONTH)
+    AND dt.category != 'BEFORE_RENTAL'
+),
+metrics_calculation AS (
+  SELECT
+    DATE_TRUNC('MONTH', dt_ended_termination) AS dt_ended_termination_month,
+    COUNT(sk_contract) AS terminations,
+    COUNT(IF(send_utility_bills_receipt = 'COMPLETED' AND (ARRAY_CONTAINS(attachment_type_list, 'CONDO_VOUCHER') OR ARRAY_CONTAINS(attachment_type_list, 'PAYMENT_VOUCHER')), sk_contract, NULL)) AS terminations_paid
+  FROM
+    tenant_pending_tasks
+  GROUP BY 1
+)
+SELECT
+  dt_ended_termination_month,
+  terminations,
+  terminations_paid,
+  terminations_paid/terminations AS percentage_terminations_paid
+FROM
+  metrics_calculation
