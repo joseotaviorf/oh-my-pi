5885c1b9c4ce919e0f5d7a6ef8e01f3d82b0eede
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-04-10 17:45:41 -0300

    [AEFR-4458-2] Creating dag and line enriched tables (#13965)
    * enrich_pipeline creation
    
    * metadata
    
    * docstrings
    
    * data quality
    
    * dependencies
    
    * dag table
    
    * inner dep
    
    * fix
    
    * fix
    
    * fixing data quality
    
    * dag_run updates
    
    * updating dag query
    
    * reverse
    
    * data quality and line
    
    * metadata
    
    * data quality
    
    * partitions
    
    * query updates
    
    * qualify
    
    * is_paused
    
    * line
    
    * updates
    
    * updates
    
    * data quality
    
    * domain

---
+++ dags/cross/enrich_pipeline/queries/enrich/dag.sql
@@ -0,0 +1,121 @@
+WITH most_recent_run AS (
+    SELECT
+        id_dag,
+        state,
+        is_first_run_ever,
+        is_in_exclusion_list,
+        is_first_execution_inside_sla,
+        DATE(ts_execution) AS dt_execution,
+        ts_started AS ts_last_run_started,
+        ts_started_brt AS ts_last_run_started_brt,
+        ts_ended AS ts_last_run_ended,
+        ts_ended_brt AS ts_last_run_ended_brt,
+        ts_first_execution_success AS ts_last_run_first_success,
+        ts_first_execution_success_brt AS ts_last_run_first_success_brt
+    FROM
+        datalake_pipeline.dag_run
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY id_dag ORDER BY ts_execution DESC) = 1
+),
+amount_of_tasks AS (
+    SELECT
+        id_dag,
+        COUNT(DISTINCT id_task) AS number_of_tasks,
+        ts_executed
+    FROM
+        datalake_composer_clean.log
+    WHERE
+        year = YEAR(CURRENT_DATE)
+        AND month = MONTH(CURRENT_DATE)
+        AND day = DAY(CURRENT_DATE)
+        AND ts_executed IS NOT NULL
+        AND id_dag LIKE 'bietlejuice%'
+    GROUP BY 1, 3
+),
+dag_info AS (
+    SELECT
+        id_dag,
+        REPLACE(REPLACE(owners, 'airflow, ', ''), ', airflow', '') AS line_name,
+        CASE
+            WHEN id_dag LIKE '%.enrich_%' THEN 'enrich'
+            WHEN id_dag LIKE '%.dw_%' THEN 'dw'
+            WHEN id_dag LIKE '%.metric_%' THEN 'metric'
+            WHEN id_dag LIKE '%.reverse_%' THEN 'reverse'
+            ELSE 'raw/clean'
+        END AS layer,
+        schedule_interval,
+        is_active,
+        is_paused,
+        IF(id_dag LIKE '%datamarts%', TRUE, FALSE) AS is_datamart
+    FROM
+        datalake_composer_clean.dag
+    WHERE
+        id_dag LIKE 'bietlejuice%'
+),
+sla_base AS (
+    SELECT
+        d.id_dag,
+        l.id_line,
+        d.line_name,
+        d.layer,
+        d.schedule_interval,
+        mc.state,
+        CASE
+            WHEN d.is_datamart = FALSE AND d.layer IN ('raw/clean', 'enrich', 'dw', 'metric') THEN 11 -- UTC hour
+            WHEN d.is_datamart = TRUE THEN 13
+            ELSE NULL   -- Reverse layer doesn't have a SLA
+        END AS utc_sla_hour,
+        d.is_active,
+        d.is_paused,
+        mc.is_in_exclusion_list,
+        d.is_datamart,
+        mc.is_first_run_ever,
+        mc.is_first_execution_inside_sla,
+        mc.dt_execution,
+        mc.ts_last_run_started,
+        mc.ts_last_run_started_brt,
+        mc.ts_last_run_ended,
+        mc.ts_last_run_ended_brt,
+        mc.ts_last_run_first_success,
+        mc.ts_last_run_first_success_brt
+    FROM
+        dag_info AS d
+    JOIN
+        most_recent_run AS mc
+            ON mc.id_dag = d.id_dag
+    JOIN
+        datalake_pipeline.line AS l
+            ON l.line_name = d.line_name
+)
+SELECT
+    s.id_dag,
+    s.id_line,
+    s.line_name,
+    s.layer,
+    s.schedule_interval,
+    s.state,
+    s.utc_sla_hour,
+    CASE
+        WHEN s.utc_sla_hour = 11 THEN 8
+        WHEN s.utc_sla_hour = 13 THEN 10
+        ELSE NULL
+    END AS brt_sla_hour,
+    ao.number_of_tasks,
+    s.is_active,
+    s.is_paused,
+    s.is_in_exclusion_list,
+    s.is_first_execution_inside_sla AS is_inside_sla,
+    s.is_datamart,
+    IF(DATE(s.ts_last_run_started) = CURRENT_DATE, TRUE, FALSE) AS has_todays_run_happened,   -- Cases of D0 runs
+    s.dt_execution AS dt_last_execution,
+    s.ts_last_run_started,
+    s.ts_last_run_started_brt,
+    s.ts_last_run_ended,
+    s.ts_last_run_ended_brt,
+    s.ts_last_run_first_success,
+    s.ts_last_run_first_success_brt
+FROM
+    sla_base AS s
+LEFT JOIN
+    amount_of_tasks AS ao
+        ON ao.id_dag = s.id_dag
---
+++ dags/cross/enrich_pipeline/queries/enrich/line.sql
@@ -0,0 +1,20 @@
+WITH base AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY MIN(ts_event) ASC) AS id_line,
+        REPLACE(REPLACE(owners, 'airflow, ', ''), ', airflow', '') AS line_name,
+        MIN(ts_event) AS ts_line_first_event
+    FROM
+        datalake_composer_clean.log AS l
+    JOIN
+        datalake_composer_clean.dag AS d
+            ON d.id_dag = l.id_dag
+            AND d.id_dag LIKE 'bietlejuice%'
+    GROUP BY 2
+)
+SELECT
+    id_line,
+    line_name,
+    IF(line_name LIKE 'Data%', TRUE, FALSE) AS is_data_line,
+    ts_line_first_event
+FROM
+    base
