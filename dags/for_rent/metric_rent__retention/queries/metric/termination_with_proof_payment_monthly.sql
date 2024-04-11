e1351b959b06fb8ac1aacfddc9a764c4fa330081
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-04-11 12:35:53 -0300

    [AEFR] Add category column to termination_with_proof_payment (#14012)
    * add category column

--- dags/for_rent/metric_rent__retention/queries/metric/termination_with_proof_payment_monthly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/termination_with_proof_payment_monthly.sql
@@ -1,6 +1,7 @@
 WITH tenant_pending_tasks AS (
   SELECT
     fct.sk_contract,
+    dc.value_segment,
     dt.send_utility_bills_receipt,
     dt.attachment_type_list,
     dt.dt_ended_termination
@@ -9,6 +10,9 @@ WITH tenant_pending_tasks AS (
   JOIN
     dw_retention.dim_termination AS dt
       ON fct.sk_termination = dt.sk_termination
+  JOIN
+    dw_rent.dim_contract AS dc
+      ON fct.sk_contract = dc.sk_contract
   WHERE
     DATE_TRUNC('MONTH', dt.dt_ended_termination) >= DATE_TRUNC('MONTH', CURRENT_DATE() - INTERVAL '12' MONTH)
     AND dt.category != 'BEFORE_RENTAL'
@@ -16,16 +20,30 @@ WITH tenant_pending_tasks AS (
 metrics_calculation AS (
   SELECT
     DATE_TRUNC('MONTH', dt_ended_termination) AS dt_ended_termination_month,
+    value_segment,
     COUNT(sk_contract) AS terminations,
     COUNT(IF(send_utility_bills_receipt = 'COMPLETED' AND (ARRAY_CONTAINS(attachment_type_list, 'CONDO_VOUCHER') OR ARRAY_CONTAINS(attachment_type_list, 'PAYMENT_VOUCHER')), sk_contract, NULL)) AS terminations_paid
   FROM
     tenant_pending_tasks
-  GROUP BY 1
+  GROUP BY
+    1, 2
 )
 SELECT
   dt_ended_termination_month,
+  value_segment AS category,
   terminations,
   terminations_paid,
   terminations_paid/terminations AS percentage_terminations_paid
 FROM
   metrics_calculation
+UNION ALL
+SELECT
+  dt_ended_termination_month,
+  'OVERALL' AS category,
+  SUM(terminations) AS terminations,
+  SUM(terminations_paid) AS terminations_paid,
+  SUM(terminations_paid)/SUM(terminations) AS percentage_terminations_paid
+FROM
+  metrics_calculation
+GROUP BY
+  1
