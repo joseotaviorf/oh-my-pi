f47588bfb9633fd9283726b185b91d8a2e7d24f0
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2024-04-11 11:19:59 -0300

    fix ts_approval null (#13856)

--- dags/support_and_service/dw_inspections/queries/dw/fact_reviewer_approval.sql
+++ dags/support_and_service/dw_inspections/queries/dw/fact_reviewer_approval.sql
@@ -1,4 +1,4 @@
-SELECT
+SELECT DISTINCT
     r.id_reviewer AS sk_reviewer,
     r.id_assessment AS sk_assessment,
     CAST(r.id_inspection AS STRING) AS sk_inspection,
@@ -6,7 +6,7 @@ SELECT
     r.approval_comment,
     r.approval_type,
     r.is_approved,
-    r.ts_approved,
+    COALESCE(r.ts_approved, r.ts_updated) AS ts_approved,
     NOW() AS ts_load,
     r.year,
     r.month,
