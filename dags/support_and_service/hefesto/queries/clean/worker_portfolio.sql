535a621d454e535055f74db8157f247d986b8948
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-04-15 10:56:47 -0300

    feat: new column on worker_portfolio (#14054)

--- dags/support_and_service/hefesto/queries/clean/worker_portfolio.sql
+++ dags/support_and_service/hefesto/queries/clean/worker_portfolio.sql
@@ -4,6 +4,7 @@ SELECT
     user_id AS id_user,
     contract_id AS id_contract,
     entity_origin_id AS id_entity_origin,
+    chat_uuid AS id_chat,
     status,
     entity_origin_name,
     entity_origin_persona,
