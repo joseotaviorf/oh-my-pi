d1b2565972c70d4732898173886fddd1d22e477e
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-04-15 15:03:06 -0300

    [DFORSALE-7578] - Updating decode source (#14062)
    * decode
    
    * Delete dags/cross/enrich_visit/data_quality/enrich/visit.yml
    
    * Delete dags/cross/enrich_visit/metadata/enrich/visit.yml
    
    * Delete dags/cross/enrich_visit/queries/enrich/visit.sql

--- dags/for_sale/itbi/spark_jobs/load_itbi_bh_raw.py
+++ dags/for_sale/itbi/spark_jobs/load_itbi_bh_raw.py
@@ -102,7 +102,7 @@ def get_data(
         )
 
         response = requests.get(url, headers=source_headers)
-        csv_content = StringIO(unidecode.unidecode(response.text.encode('latin1').decode('utf-8')))
+        csv_content = StringIO(unidecode.unidecode(response.text.encode('latin1').decode('latin1')))
 
         df = pd.read_csv(csv_content, encoding='utf-8', sep = ';', thousands = '.', decimal=',', skip_blank_lines=True)
         df.columns = df.columns.str.strip()
