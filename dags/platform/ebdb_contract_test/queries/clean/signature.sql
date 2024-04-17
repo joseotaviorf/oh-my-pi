4a08f8e664334a1e7a96bdb2a2a7e47f06d4586f
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-04-17 09:49:17 -0300

    [AEFR-3869] Migrate ebdb_contract DAG to for_rent folder (#13906)
    * migrate ebdb_contract to builder

--- dags/platform/ebdb_contract_test/queries/clean/signature.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    atualizadoEm AS ts_updated,
-    criadoEm AS ts_created,
-    dataAssinatura AS ts_signature,
-    contrato_id AS id_contract,
-    contratoPessoa_id AS id_contract_person
-FROM
-    datalake_ebdb_test_raw.assinatura
--- dags/platform/ebdb_contract_test/queries/clean/work_contract.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  id,
-  contractName AS contract_name,
-  criadoEm AS ts_created,
-  atualizadoEm AS ts_updated
-FROM
-  datalake_ebdb_test_raw.workcontract
