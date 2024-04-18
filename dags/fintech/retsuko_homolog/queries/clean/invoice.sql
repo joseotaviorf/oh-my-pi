590bd4f2ada1e6a489b2a48cc83de4395b953b6e
Bianca Garcia <biigarciam@gmail.com>
2024-04-18 17:14:29 -0300

    [DFFRS-832][Retsuko] Homolog: use date range instead of sql for incremental table (#14118)
    * use date range instead of sql for incremental table
    
    * fix end date
    
    * fix position of extra_query_template_params
    
    * fix clean query filter
    
    * fix end date
    
    * fix macro function
    
    * Update dags/fintech/retsuko_homolog/queries/clean/boleto.sql
    
    Co-authored-by: Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
    
    * Update dags/fintech/retsuko_homolog/queries/clean/entry.sql
    
    Co-authored-by: Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
    
    * Update dags/fintech/retsuko_homolog/queries/clean/invoice.sql
    
    Co-authored-by: Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
    
    ---------
    
    Co-authored-by: bianca.martins <bianca.martins@quintoandar.com.br>
    Co-authored-by: Gustavo Akashi <gustavo.akashi@quintoandar.com.br>

--- dags/fintech/retsuko_homolog/queries/clean/invoice.sql
+++ dags/fintech/retsuko_homolog/queries/clean/invoice.sql
@@ -29,13 +29,4 @@ SELECT
     day
 FROM
     datalake_retsuko_homolog_raw.invoice
-WHERE
-    (
-        MAKE_DATE(year,month,day) = MAKE_DATE({year}, {month}, {day}) - 1
-    )
-    OR
-    (
-        year = {year}
-        AND month = {month}
-        AND day = {day}
-    )
+WHERE MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
