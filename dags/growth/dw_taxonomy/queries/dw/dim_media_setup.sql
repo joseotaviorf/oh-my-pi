5d0e66a2a5ad0e704d48abdeedfdf79bae4866eb
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-04-22 10:21:35 -0300

    fix dim media setup to avoid duplicate records (#14135)

--- dags/growth/dw_taxonomy/queries/dw/dim_media_setup.sql
+++ dags/growth/dw_taxonomy/queries/dw/dim_media_setup.sql
@@ -20,3 +20,4 @@ SELECT
 FROM datalake_growth_taxonomy.media_setup AS ms
 JOIN datalake_growth_taxonomy.unified_taxonomy_dictionary as utd
     ON (ms.id_media_setup = utd.sk_media_setup)
+QUALIFY ROW_NUMBER() OVER (PARTITION BY ms.id_media_setup ORDER BY ms.ts_load DESC) = 1
