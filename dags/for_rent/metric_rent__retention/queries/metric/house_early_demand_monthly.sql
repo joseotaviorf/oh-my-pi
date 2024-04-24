085bdb3a26ebdc340a8ebe6cf75ac959f8da5ecc
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2024-04-24 16:32:12 -0300

    [AEFR-4374] Add cohort 2W to early demand opt-in metric (#14189)
    * Add cohort 2W to early demand opt-in metric

--- dags/for_rent/metric_rent__retention/queries/metric/house_early_demand_monthly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/house_early_demand_monthly.sql
@@ -2,9 +2,26 @@ SELECT
     DATE(DATE_TRUNC('MONTH', dc.ts_termination_requested)) AS dt_month_rescission_created,
     fct_or.country_code,
     'OVERALL' AS category,
-    COUNT(DISTINCT IF(dhl.ts_early_demand_started IS NOT NULL, dhl.id_house, NULL)) AS qtd_houses_early_demand,
+    COUNT(DISTINCT 
+      IF(
+        dhl.ts_early_demand_started IS NOT NULL 
+        AND dhl.ts_early_demand_started <= DATEADD(WEEK, 2, dc.ts_termination_requested), dhl.id_house, NULL
+      )
+    ) AS qtd_houses_early_relisting_2w,
     COUNT(DISTINCT fct_or.sk_house) AS qtd_terminations,
-    CAST(COUNT(DISTINCT IF(dhl.ts_early_demand_started IS NOT NULL, dhl.id_house, NULL)) AS DOUBLE) / COUNT(DISTINCT fct_or.sk_house) AS pct_early_demand_opt_in
+    CAST(
+      COUNT(DISTINCT 
+        IF(
+          dhl.ts_early_demand_started IS NOT NULL 
+          AND dhl.ts_early_demand_started <= DATEADD(WEEK, 2, dc.ts_termination_requested), dhl.id_house, NULL
+        )
+    ) AS DOUBLE) 
+    / 
+    CAST(
+      COUNT(DISTINCT 
+        fct_or.sk_house
+      ) AS DOUBLE
+    ) AS pct_early_demand_opt_in_2w
 FROM 
     dw_retention.fact_owner_retention AS fct_or
 INNER JOIN
@@ -23,9 +40,26 @@ SELECT
     DATE(DATE_TRUNC('MONTH', dc.ts_termination_requested)) AS dt_month_rescission_created,
     fct_or.country_code,
     dc.value_segment AS category,
-    COUNT(DISTINCT IF(dhl.ts_early_demand_started IS NOT NULL, dhl.id_house, NULL)) AS qtd_houses_early_demand,
+    COUNT(DISTINCT 
+      IF(
+        dhl.ts_early_demand_started IS NOT NULL 
+        AND dhl.ts_early_demand_started <= DATEADD(WEEK, 2, dc.ts_termination_requested), dhl.id_house, NULL
+      )
+    ) AS qtd_houses_early_relisting_2w,
     COUNT(DISTINCT fct_or.sk_house) AS qtd_terminations,
-    CAST(COUNT(DISTINCT IF(dhl.ts_early_demand_started IS NOT NULL, dhl.id_house, NULL)) AS DOUBLE) / COUNT(DISTINCT fct_or.sk_house) AS pct_early_demand_opt_in
+    CAST(
+      COUNT(DISTINCT 
+        IF(
+          dhl.ts_early_demand_started IS NOT NULL 
+          AND dhl.ts_early_demand_started <= DATEADD(WEEK, 2, dc.ts_termination_requested), dhl.id_house, NULL
+        )
+    ) AS DOUBLE) 
+    / 
+    CAST(
+      COUNT(DISTINCT 
+        fct_or.sk_house
+      ) AS DOUBLE
+    ) AS pct_early_demand_opt_in_2w
 FROM 
     dw_retention.fact_owner_retention AS fct_or
 INNER JOIN
