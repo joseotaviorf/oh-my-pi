523af87f4f0831b5fe7465a77d887295dfe7937e
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-04-24 14:04:43 -0300

    [AEFR-4497] Creating query_information table based on Trino data (#14183)
    * [AEFR-4497] Creating query_information table
    
    * fix
    
    * doc

---
+++ dags/governance/enrich_trino/queries/enrich/query_information.sql
@@ -0,0 +1,52 @@
+WITH base AS (
+    SELECT
+        queryId AS id_query,
+        resourceGroupId AS id_resource_group,
+        referencedTables AS referenced_tables,
+        state,
+        execution_time, 
+        execution_start_time AS ts_execution_started,
+        execution_end_time AS ts_execution_ended,
+        year,
+        month,
+        day
+    FROM
+        data_platform_metrics.trino_query_log_events_metrics_clean_batch
+    WHERE
+        year = {year}
+        AND month = {month}
+        AND day = {day}
+),
+exploded_referenced_tables AS (
+    SELECT
+        id_query,
+        EXPLODE(referenced_tables) AS referenced_table
+    FROM
+        base
+),
+extract_schema_and_table AS (
+    SELECT
+        id_query,
+        REGEXP_EXTRACT(referenced_table, '"schema":"(\\w+)",') AS database_name,
+        SPLIT(REGEXP_EXTRACT(referenced_table, '"table":"(\\w+(\\$)*\\w+)",'), '[\$]') [0] AS table_name
+    FROM
+        exploded_referenced_tables
+)
+SELECT
+    b.id_query,
+    GET(b.id_resource_group, 2) AS tool,
+    GET(b.id_resource_group, 3) AS user,
+    b.state,
+    b.execution_time,
+    es.database_name,
+    es.table_name,
+    b.ts_execution_started,
+    b.ts_execution_ended,
+    b.year,
+    b.month,
+    b.day
+FROM
+    base AS b
+JOIN
+    extract_schema_and_table AS es
+        ON es.id_query = b.id_query
