fcc76cda46c72969b061fe0c8b6f2e34cf857b91
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-04-25 10:43:02 -0300

    [AEFR] Updating enrich_trino pipeline (#14196)
    * [AEFR] Updating enrich_trino pipeline
    
    * dag builder
    
    * Update dags/governance/enrich_trino/enrich_trino_declaration.yml
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
    
    * updating queries and dag file
    
    * updates
    
    * fixing query
    
    * adding distinct
    
    ---------
    
    Co-authored-by: Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>

--- dags/governance/enrich_trino/queries/enrich/query_information.sql
+++ dags/governance/enrich_trino/queries/enrich/query_information.sql
@@ -13,9 +13,7 @@ WITH base AS (
     FROM
         data_platform_metrics.trino_query_log_events_metrics_clean_batch
     WHERE
-        year = {year}
-        AND month = {month}
-        AND day = {day}
+        MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
 ),
 exploded_referenced_tables AS (
     SELECT
@@ -31,22 +29,45 @@ extract_schema_and_table AS (
         SPLIT(REGEXP_EXTRACT(referenced_table, '"table":"(\\w+(\\$)*\\w+)",'), '[\$]') [0] AS table_name
     FROM
         exploded_referenced_tables
+),
+tools AS (
+    SELECT
+        b.id_query,
+        GET(b.id_resource_group, 2) AS tool,
+        GET(b.id_resource_group, 3) AS user,
+        b.state,
+        b.execution_time,
+        es.database_name,
+        es.table_name,
+        b.ts_execution_started,
+        b.ts_execution_ended,
+        b.year,
+        b.month,
+        b.day
+    FROM
+        base AS b
+    JOIN
+        extract_schema_and_table AS es
+            ON es.id_query = b.id_query
 )
 SELECT
-    b.id_query,
-    GET(b.id_resource_group, 2) AS tool,
-    GET(b.id_resource_group, 3) AS user,
-    b.state,
-    b.execution_time,
-    es.database_name,
-    es.table_name,
-    b.ts_execution_started,
-    b.ts_execution_ended,
-    b.year,
-    b.month,
-    b.day
+    id_query,
+    CASE
+        WHEN tool = 'bi-metabase' THEN 'Metabase'
+        WHEN tool = 'bi-looker' THEN 'Looker'
+        WHEN tool = 'bi-superset' THEN 'Superset'
+        ELSE tool
+    END AS tool,
+    user,
+    state,
+    execution_time,
+    database_name,
+    table_name,
+    MAKE_DATE(year, month, day) AS dt_extraction,
+    ts_execution_started,
+    ts_execution_ended,
+    year,
+    month,
+    day
 FROM
-    base AS b
-JOIN
-    extract_schema_and_table AS es
-        ON es.id_query = b.id_query
+    tools
--- dags/governance/enrich_trino/queries/enrich/table_usage_information.sql
+++ dags/governance/enrich_trino/queries/enrich/table_usage_information.sql
@@ -1,40 +1,12 @@
-with explode_referenced_tables as (
-  select
-    queryId,
-    explode(referencedTables) as referencedTable,
-    year,
-    month,
-    day
-  from
-    data_platform_metrics.trino_query_log_events_metrics_clean_batch
-  where
-    year = {year}
-    and month = {month}
-    and day = {day}
-), extrac_schema_and_table as (
-  select
-    queryId,
-    regexp_extract(referencedTable, '"schema":"(\\w+)",') as database_name,
-    split(regexp_extract(referencedTable, '"table":"(\\w+(\\$)*\\w+)",'), '[\$]') [0] as table_name,
-    year,
-    month,
-    day
-  from
-    explode_referenced_tables
-)
-select
+SELECT
   database_name,
   table_name,
-  count(*) as count_visualization,
+  COUNT(DISTINCT id_query) AS count_visualization,
   year,
   month,
   day
-from
-  extrac_schema_and_table
-group by 
-  database_name,
-  table_name,
-  year,
-  month,
-  day
-
+FROM
+  datalake_trino.query_information
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
+GROUP BY 1, 2, 4, 5, 6
