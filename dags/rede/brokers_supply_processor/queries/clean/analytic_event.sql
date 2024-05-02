383020b291dca7385c6b44bbeadffbe9f2dfb52a
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-05-02 18:50:33 -0300

    [RD-396] Analytic Event Ingestion (#14162)
    * add analytic_event table in raw declaration and clean layer
    
    * update clean layer query
    
    * create analytic_event metadata file
    
    * update metadata and field names
    
    * update documentation
    
    * delete readme

---
+++ dags/rede/brokers_supply_processor/queries/clean/analytic_event.sql
@@ -0,0 +1,19 @@
+SELECT
+    id,
+    tracking_id AS id_tracking,
+    event_uuid AS uuid_event,
+    lead_uuid AS uuid_lead,
+    event_type,
+    event_properties,
+    event_date AS ts_event,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_brokers_supply_processor_raw.analytic_event
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
