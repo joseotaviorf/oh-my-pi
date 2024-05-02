5ed461774f77e7e6b7809022ad385d33c3b86ffb
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-05-02 09:16:01 -0300

    DRFFS-790: add 2 new sorting hat tables (#14242)
    * DRFFS-790: add 2 new sorting hat tables
    
    * fix metada file
    
    * fix metada file
    
    * update dependencies file
    
    * fix user table
    
    * fix dependecies file
    
    * update sql scripts
    
    * Update dags/fintech/enrich_sorting_hat/metadata/enrich/policy_report.yml
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/enrich_sorting_hat/queries/enrich/policy_report_proponents.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/enrich_sorting_hat/queries/enrich/policy_report_proponents.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    * Update dags/fintech/enrich_sorting_hat/queries/enrich/policy_report.sql
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
    
    ---------
    
    Co-authored-by: Isadora Gallerani <isadora.gallerani@quintoandar.com.br>

---
+++ dags/fintech/enrich_sorting_hat/queries/enrich/policy_report.sql
@@ -0,0 +1,52 @@
+WITH policy_report_extract_json AS (
+    SELECT
+        id,
+        EXPLODE_OUTER(
+            FROM_JSON(
+                GET_JSON_OBJECT(result, '$.proponents'),
+                'ARRAY<
+                    STRUCT<
+                        document_number: STRING,
+                        is_retenant: BOOLEAN, 
+                        retenant_contract_id: STRING, 
+                        retenant_contract_proposal_id: STRING, 
+                        retenant_contract_user_id: STRING, 
+                        retenant_score: STRING
+                    >
+                >'
+                )
+        ) AS extract_json
+    FROM
+        datalake_sorting_hat_clean.policy_report
+),
+proponents AS (
+SELECT
+    id AS id_policy_evaluation,
+    extract_json.retenant_score,
+    extract_json.document_number,
+    extract_json.is_retenant
+FROM 
+    policy_report_extract_json
+)
+SELECT
+    pr.id AS id_policy_evaluation,
+    pr.id_external,
+    pr.type AS policy_name,
+    pr.external_source,
+    GET_JSON_OBJECT(pr.result, '$.type') AS proponent_group_class,
+    GET_JSON_OBJECT(pr.result, '$.subtype') AS proponent_class_detail,
+    GET_JSON_OBJECT(pr.result, '$.full_type') AS proponent_group_class_detail,
+    pr.version,
+    COUNT(proponents.document_number) AS number_of_proponents,
+    AVG(proponents.retenant_score) AS average_retenants_score,
+    CASE 
+        WHEN COUNT(DISTINCT proponents.is_retenant) = 1 AND MAX(proponents.is_retenant) = TRUE THEN TRUE 
+        ELSE FALSE
+    END AS are_all_proponents_retenants,
+    pr.ts_created,
+    pr.ts_updated
+FROM
+    datalake_sorting_hat_clean.policy_report AS pr
+LEFT JOIN
+    proponents ON pr.id = proponents.id_policy_evaluation
+GROUP BY ALL
---
+++ dags/fintech/enrich_sorting_hat/queries/enrich/policy_report_proponents.sql
@@ -0,0 +1,28 @@
+WITH policy_report_extract_json AS (
+SELECT
+    id,
+    EXPLODE(FROM_JSON(GET_JSON_OBJECT(result, '$.proponents'), 'ARRAY<STRING>')) AS extract_json
+FROM
+    datalake_sorting_hat_clean.policy_report
+), 
+user ( 
+SELECT 
+  id AS id_user,
+  cpf
+FROM 
+  datalake_ebdb_user.user
+QUALIFY --filter to return only the user with the most recent creation date.
+  ROW_NUMBER() OVER (PARTITION BY cpf ORDER BY ts_created DESC) = 1 
+)
+SELECT
+    policy_report_extract_json.id AS id_policy_evaluation,
+    GET_JSON_OBJECT(extract_json, '$.retenant_contract_id') AS id_retenant_contract,
+    GET_JSON_OBJECT(extract_json, '$.retenant_contract_proposal_id') AS id_retenant_contract_proposal,
+    GET_JSON_OBJECT(extract_json, '$.retenant_contract_user_id') AS id_retenant_contract_user,
+    user.id_user,
+    GET_JSON_OBJECT(extract_json, '$.retenant_score') AS retenant_score,
+    GET_JSON_OBJECT(extract_json, '$.is_retenant') AS is_retenant
+FROM 
+    policy_report_extract_json
+LEFT JOIN --join to remove cpf from query
+    user ON GET_JSON_OBJECT(extract_json, '$.document_number') = user.cpf
