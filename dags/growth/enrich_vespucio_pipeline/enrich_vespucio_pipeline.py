ea65509efb2e32860e1a47781373806a267e32fc
Raimundo Neto <44676140+RaimundoNeto7@users.noreply.github.com>
2024-05-06 16:36:59 -0400

    fix(vespucio-pipeline): parallel tasks (#14294)

--- dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
+++ dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
@@ -334,4 +334,4 @@ plugin_tasks = [
 execute_job_cluster_task >> source_tasks
 core_tasks[0] << source_tasks
 chain(*core_tasks)
-core_tasks[-1] >> (listing_task, linking_task) >> plugin_tasks
+core_tasks[-1] >> [listing_task, linking_task] >> plugin_tasks
