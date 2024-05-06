6ac34a7dc7b232ca915a79b22353954ec59bbf3d
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-05-06 16:11:26 -0300

    [GROW] Supply Migration  - Part 6 (#14268)
    * first commit
    
    * test merge
    
    * add: remove partitions
    
    * change custom_schema
    
    * adjusting query
    
    * fix dependencies

---
+++ dags/growth/enrich_supply_leads/queries/enrich/leads_sks.sql
@@ -0,0 +1,65 @@
+WITH 1p_tb AS (
+    SELECT 
+        id AS id_lead,
+        '1P' AS source,
+        CURRENT_TIMESTAMP AS ts_created,
+        FALSE AS is_backfill
+    FROM 
+        datalake_rene_descartes_clean.house_lead
+    WHERE 
+        DATE(ts_created) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    GROUP BY ALL
+),
+3p_tb AS (
+    SELECT
+        id AS id_lead,
+        '3P' AS source,
+        CURRENT_TIMESTAMP AS ts_created,
+        FALSE AS is_backfill
+    FROM
+        datalake_brokers_supply_processor_clean.lead_3p
+    WHERE 
+        DATE(ts_created) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    GROUP BY ALL
+),
+draft_with_rene_informations AS (
+    SELECT
+    hd.id AS id_draft,
+    s.id_external AS id_house,
+    hlc.id_lead AS id_lead_ebdb,
+    hl.id AS id_lead_rene
+    FROM
+        datalake_bob_clean.house_draft AS hd
+    LEFT JOIN datalake_bob_clean.submission_progress AS s 
+        ON hd.id = s.id_house_draft
+    LEFT JOIN datalake_rene_descartes_clean.house_lead_conversion AS hlc 
+        ON s.id_external = hlc.id_house
+    LEFT JOIN datalake_rene_descartes_clean.house_lead AS hl 
+        ON hlc.id_lead = hl.id_lead_ebdb
+    WHERE
+        hd.type = 'ADMIN_CONFIRMATION'
+        AND hl.id IS NOT NULL
+),
+ciq_tb AS (
+    SELECT 
+        hd.id AS id_lead,
+        'CIQ' AS source,
+        CURRENT_TIMESTAMP AS ts_created,
+        FALSE AS is_backfill
+    FROM 
+        datalake_bob_clean.house_draft AS hd
+    WHERE
+        (hd.id NOT IN (SELECT DISTINCT id_draft FROM draft_with_rene_informations)) -- INFORMAÇÕES CAPTURADAS PELO RENE
+        AND (hd.type = 'ADMIN_CONFIRMATION') -- PEGANDO SOMENTE O TIPO DE CIQ
+        AND (DATE(ts_created) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}'))
+    GROUP BY ALL
+)
+
+SELECT *
+FROM 1p_tb
+UNION ALL
+SELECT *
+FROM 3p_tb
+UNION ALL
+SELECT *
+FROM ciq_tb
