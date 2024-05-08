4a9dbf6c04ce2912dd11a19b179411221aa83350
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-05-08 18:25:49 -0300

    new columns (#14337)

--- dags/for_rent/ebdb/queries/clean/visit_cancellation_details.sql
+++ dags/for_rent/ebdb/queries/clean/visit_cancellation_details.sql
@@ -1,8 +1,10 @@
 SELECT
   id,
   visita_id AS id_visit,
+  visit_status_log_id AS id_visit_status_log,
   reason,
   on_behalf_of,
+  channel,
   criadoEm AS ts_created,
   atualizadoEm AS ts_updated
 FROM
