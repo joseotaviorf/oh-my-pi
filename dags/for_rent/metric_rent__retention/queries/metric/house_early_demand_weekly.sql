7130dd9cbf30c5a75e08139239e34b06bddffd86
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-05-13 17:30:07 -0300

    add missing column (#14395)

--- dags/for_rent/metric_rent__retention/queries/metric/house_early_demand_weekly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/house_early_demand_weekly.sql
@@ -75,6 +75,7 @@ SELECT
       )
     ) AS qtd_houses_early_relisting_matured_2w,
     COUNT(DISTINCT fct_or.sk_house) AS qtd_terminations,
+    COUNT(DISTINCT IF(dc.ts_termination_requested <= DATE_ADD(CURRENT_DATE, -14), fct_or.sk_house, NULL)) AS qtd_terminations_matured_2w,
     CAST(
       COUNT(DISTINCT 
         IF(
