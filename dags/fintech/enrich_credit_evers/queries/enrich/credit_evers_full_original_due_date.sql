d963fe768bc85c530186850db6d2e8e23cdf4a84
Bianca Garcia <biigarciam@gmail.com>
2024-05-14 10:15:14 -0300

    [DFFRS-701] Improve ever sujo to segregate debt_forgiveness invoices (#14000)
    * segregate ever by debt_forgiveness or not
    
    * remove order by
    
    ---------
    
    Co-authored-by: bianca.martins <bianca.martins@quintoandar.com.br>

--- dags/fintech/enrich_credit_evers/queries/enrich/credit_evers_full_original_due_date.sql
+++ dags/fintech/enrich_credit_evers/queries/enrich/credit_evers_full_original_due_date.sql
@@ -4,6 +4,7 @@ WITH debtors_all_time AS (
     id_contract_ebdb,
     id_contract_retsuko,
     id_proposal,
+    is_debt_forgiveness,
     MONTHS_BETWEEN(
       DATE_ADD(
         LAST_DAY(
@@ -49,35 +50,65 @@ ever_array AS (
       ARRAY(15, 30, 40, 60, 90, 120)
     ) AS contract_ever_number
 ),
+ever_rule_debt_forgiveness AS (
+  SELECT
+      id_invoice,
+      id_contract_ebdb,
+      id_contract_retsuko,
+      id_proposal,
+      CAST(months_of_contract AS INTEGER) AS months_of_contract,
+      ma.contract_mob_number,
+      ea.contract_ever_number,
+      CASE
+            WHEN invoice_over_number >= ea.contract_ever_number
+              AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
+                        (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
+            ELSE 0
+          END AS flag_ever,
+      invoice_over_number,
+      FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
+      due_amount,
+      ts_signature,
+      DATE_ADD(ts_signature,INT(contract_mob_number)) AS dt_reference,
+      dt_contract_updated
+  FROM
+      debtors_all_time
+  CROSS JOIN
+      mob_array AS ma
+  CROSS JOIN
+      ever_array AS ea
+),
 ever_rule AS (
-SELECT
-    id_invoice,
-    id_contract_ebdb,
-    id_contract_retsuko,
-    id_proposal,
-    CAST(months_of_contract AS INTEGER) AS months_of_contract,
-    ma.contract_mob_number,
-    ea.contract_ever_number,
-    CASE
-          WHEN invoice_over_number >= ea.contract_ever_number
-            AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
-                      (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
-          ELSE 0
-        END AS flag_ever,
-    invoice_over_number,
-    FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
-    due_amount,
-    ts_signature,
-    DATE_ADD(ts_signature,INT(contract_mob_number)) AS dt_reference,
-    dt_contract_updated
-FROM
-    debtors_all_time
-CROSS JOIN
-    mob_array AS ma
-CROSS JOIN
-    ever_array AS ea
-)
-SELECT
+  SELECT
+      id_invoice,
+      id_contract_ebdb,
+      id_contract_retsuko,
+      id_proposal,
+      CAST(months_of_contract AS INTEGER) AS months_of_contract,
+      ma.contract_mob_number,
+      ea.contract_ever_number,
+      CASE
+            WHEN invoice_over_number >= ea.contract_ever_number
+              AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
+                        (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
+            ELSE 0
+          END AS flag_ever,
+      invoice_over_number,
+      FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
+      due_amount,
+      ts_signature,
+      DATE_ADD(ts_signature,INT(contract_mob_number)) AS dt_reference,
+      dt_contract_updated
+  FROM
+      debtors_all_time
+  CROSS JOIN
+      mob_array AS ma
+  CROSS JOIN
+      ever_array AS ea
+  WHERE is_debt_forgiveness IS FALSE
+),
+calculate_final_ever AS (
+  SELECT
     id_contract_ebdb,
     id_contract_retsuko,
     id_proposal,
@@ -101,3 +132,55 @@ HAVING
         AND (months_of_contract > contract_mob_number)
             OR (is_ever = TRUE)
             )
+),
+calculate_final_ever_debt_forgiveness AS (
+  SELECT
+    id_contract_ebdb,
+    id_contract_retsuko,
+    id_proposal,
+    months_of_contract,
+    contract_mob_number,
+    contract_ever_number,
+    MAX(IF(flag_ever = 1, max_invoice_mob, NULL)) AS invoice_anchor,
+    CAST(MAX(flag_ever) AS BOOLEAN) AS is_ever,
+    CAST(SUM(flag_ever) AS INTEGER) AS num_overs_in_mob_window,
+    SUM(IF(flag_ever = 1, due_amount, 0)) AS total_due_amount_in_mob_window,
+    CAST(SUM(IF(invoice_over_number >= contract_ever_number,1,0)) AS INTEGER) AS num_total_overs,
+    SUM(IF(invoice_over_number >= contract_ever_number,due_amount,0)) AS total_due_amount,
+    ts_signature,
+    dt_reference,
+    dt_contract_updated
+FROM ever_rule_debt_forgiveness
+GROUP BY
+    1,2,3,4,5,6,13,14,15
+HAVING
+    (is_ever = FALSE
+        AND (months_of_contract > contract_mob_number)
+            OR (is_ever = TRUE)
+            )
+)
+SELECT
+    erdf.id_contract_ebdb,
+    erdf.id_contract_retsuko,
+    erdf.id_proposal,
+    erdf.months_of_contract,
+    erdf.contract_mob_number,
+    erdf.contract_ever_number,
+    er.invoice_anchor,
+    erdf.invoice_anchor AS invoice_anchor_debt_forgiveness,
+    er.is_ever,
+    erdf.is_ever AS is_ever_debt_forgiveness,
+    er.num_overs_in_mob_window,
+    erdf.num_overs_in_mob_window AS num_overs_in_mob_window_debt_forgiveness,
+    er.num_total_overs,
+    erdf.num_total_overs AS num_total_overs_debt_forgiveness,
+    er.total_due_amount,
+    erdf.total_due_amount AS total_due_amount_debt_forgiveness,
+    erdf.ts_signature,
+    erdf.dt_reference,
+    erdf.dt_contract_updated
+FROM calculate_final_ever_debt_forgiveness AS erdf
+LEFT JOIN calculate_final_ever AS er
+   ON er.id_contract_retsuko = erdf.id_contract_retsuko
+   AND er.contract_mob_number = erdf.contract_mob_number
+   AND er.contract_ever_number = erdf.contract_ever_number
