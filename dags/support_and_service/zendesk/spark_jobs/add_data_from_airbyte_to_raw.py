ed2227e0640bdb4df41c91d19342473c67dd2cc0
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-05-15 16:03:30 -0300

    [AESS-3552] Fix `zendesk` DAG spark job for Airbyte data (#14399)
    * fix: check if there are any valid files on airbyte spark job
    
    * feat: enable ticket_comments table etl

--- dags/support_and_service/zendesk/spark_jobs/add_data_from_airbyte_to_raw.py
+++ dags/support_and_service/zendesk/spark_jobs/add_data_from_airbyte_to_raw.py
@@ -2,9 +2,11 @@ import json
 import logging
 import multiprocessing
 import concurrent.futures
-
 from argparse import ArgumentParser
 from datetime import datetime
+
+import boto3
+from pyspark.sql.functions import lit
 
 from quintoandar_logger import QuintoAndarLogger
 from bietlejuice.base.db import DatalakeMetastoreService
@@ -15,10 +17,11 @@ from bietlejuice.loaders import SparkMetastoreLoader
 from bietlejuice.loaders.s3_loader import S3Loader
 from bietlejuice.services.metastore_services import SparkMetastoreService
 
-from pyspark.sql.functions import lit
 
 JOB_NAME = "add_data_from_airbyte_to_raw"
 SOURCE = "zendesk"
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=JOB_NAME)
@@ -39,6 +42,9 @@ if __name__ == "__main__":
 
     spark_client = SparkClient()
     s3_consumer = S3Consumer(spark_client)
+    s3_loader = S3Loader()
+    s3_service = S3Service(boto3.resource("s3"))
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
 
     db_info = DatalakeMetastoreService.get_db_info(environment, SOURCE, datalake_bucket)
     spark_metastore_service = SparkMetastoreService(spark_client)
@@ -48,38 +54,41 @@ if __name__ == "__main__":
     database_location = db_info["db_raw_path"]
     spark_metastore_service.create_database(database_name)
 
-    s3_loader = S3Loader()
-    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+    objs = s3_service.list_objects(f"s3://5a-datalake-prod/airbyte/zendesk_{table_name}/{table_name}")
+    valid_files = [filename for filename in objs if f"{execution_date_str.replace('-', '_')}" in filename]
+    if valid_files:
 
-    df = s3_consumer.get_data_from_file(
-        proxy_path,
-        format="json",
-    ).select("_airbyte_data.*").drop('metadata') # this column exists on ticket_audits and causes schema errors
+        df = s3_consumer.get_data_from_file(
+            proxy_path,
+            format="json",
+        ).select("_airbyte_data.*").drop('metadata') # this column exists on ticket_audits and causes schema errors
 
-    df = df.withColumn("dt", lit(execution_date_str))
+        df = df.withColumn("dt", lit(execution_date_str))
 
-    s3_loader.load_df(
-        df=df,
-        s3_path=f"{database_location}{table_name}",
-        format_options=format_options,
-        partitions=["dt"],
-        optimize_dataframe=False,
-        compression="gzip",
-    )
+        s3_loader.load_df(
+            df=df,
+            s3_path=f"{database_location}{table_name}",
+            format_options=format_options,
+            partitions=["dt"],
+            optimize_dataframe=False,
+            compression="gzip",
+        )
 
-    spark_metastore_loader.update_metastore(
-        df=df,
-        database_name=database_name,
-        table_name=table_name,
-        format_options=format_options,
-        database_location=database_location,
-        partitions=["dt"],
-        force_recreate=False,
-    )
+        spark_metastore_loader.update_metastore(
+            df=df,
+            database_name=database_name,
+            table_name=table_name,
+            format_options=format_options,
+            database_location=database_location,
+            partitions=["dt"],
+            force_recreate=False,
+        )
 
-    spark_metastore_service.create_new_partitions_from_df(
-        df=df,
-        database_name=database_name,
-        table_name=table_name,
-        partition_cols=["dt"],
-    )
+        spark_metastore_service.create_new_partitions_from_df(
+            df=df,
+            database_name=database_name,
+            table_name=table_name,
+            partition_cols=["dt"],
+        )
+    else:
+        logger.info(f"No files found for date {execution_date_str}")
