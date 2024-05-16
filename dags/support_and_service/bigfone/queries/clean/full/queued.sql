04d865f1734258b4dc634abb062fade6549bf0ef
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-05-16 11:09:53 -0300

    [AESS] Migrate `bigfone` DAG to DAGBuilder (#14413)
    * feat: migrate bigfone to dagbuilder
    
    * feat: update dre version

--- dags/support_and_service/bigfone/queries/clean/full/queued.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    call_id AS id_call,
-    queue,
-    abandoned,
-    joined_queue_time AS ts_joined_queue,
-    left_queue_time AS ts_left_queue
-FROM
-    datalake_bigfone_raw.queued
