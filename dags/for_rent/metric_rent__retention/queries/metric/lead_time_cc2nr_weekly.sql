4d2d8e3f22a3516a6a93943517112b542360078d
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-05-16 14:28:36 -0300

    [AEFR-4373] Add leadtime_cc2nr metric to Metric Layer (#14425)
    * add queries and metadata

---
+++ dags/for_rent/metric_rent__retention/queries/metric/lead_time_cc2nr_weekly.sql
@@ -0,0 +1,54 @@
+WITH leadtime_calculation AS (
+  SELECT
+    fct.sk_contract,
+    dc.value_segment,
+    COALESCE(dc.dt_start, dc.dt_entrance) AS dt_start,
+    DATEDIFF(DATE(COALESCE(dc.dt_start, dc.dt_entrance)), DATE(dc.ts_created)) leadtime
+  FROM
+    dw_retention.fact_contract_termination AS fct
+  JOIN
+    dw_retention.dim_termination AS dt
+      ON fct.sk_termination = dt.sk_termination
+  JOIN
+    dw_rent.fact_house_listings AS fhl
+      ON fct.sk_contract = fhl.sk_contract
+  JOIN
+    dw_rent.dim_house_listing AS dhl
+      ON fhl.sk_house_listing + 1 = dhl.sk_house_listing
+  JOIN
+    dw_rent.fact_listing_rent_flows AS flrf
+      ON dhl.sk_house_listing = flrf.sk_house_listing
+  JOIN
+    dw_rent.dim_contract AS dc
+      ON flrf.sk_contract = dc.sk_contract
+  WHERE
+    dt.status = 'DONE'
+    AND dc.status IN ('Ativo', 'Finalizado')
+    AND dhl.listing_category_start = 'Re-Listing'
+)
+
+SELECT
+    DATE(DATE_TRUNC('WEEK', dt_start)) AS dt_reference_week,
+    value_segment,
+    APPROX_PERCENTILE(leadtime, 0.5) AS median_leadtime,
+    COUNT(DISTINCT sk_contract) AS volume_contracts
+FROM
+    leadtime_calculation
+WHERE
+    dt_start < CURRENT_DATE()
+GROUP BY
+    1, 2
+
+UNION ALL
+
+SELECT
+    DATE(DATE_TRUNC('WEEK', dt_start)) AS dt_reference_week,
+    'OVERALL' AS value_segment,
+    APPROX_PERCENTILE(leadtime, 0.5) AS median_leadtime,
+    COUNT(DISTINCT sk_contract) AS volume_contracts
+FROM
+    leadtime_calculation
+WHERE
+    dt_start < CURRENT_DATE()
+GROUP BY
+    1, 2
