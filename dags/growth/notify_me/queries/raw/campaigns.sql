4960d3cadcf7bbc3cf180a0e3edec261fb751b56
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2024-05-20 14:29:43 -0300

    delete notify me dag (#14464)

--- dags/growth/notify_me/queries/raw/campaigns.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    *,
-    DATE(updated_at) AS dt
-FROM
-    campaigns_aud
-WHERE
-	DATE(updated_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
--- dags/growth/notify_me/queries/raw/messages_aud.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    *,
-    DATE(updated_at) AS dt
-FROM
-    messages_aud
-WHERE
-	DATE(updated_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
