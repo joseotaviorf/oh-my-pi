1218359eee101ada50e467613b3a398c809bad86
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2024-05-27 15:08:26 -0300

    [AE-GROWTH] AXP-1276: Adjust MDAPE Region Metrics Pricing Report (#14566)
    * adjust mdape city rule
    
    * update dependencies file
    
    * adjust unused cte and query pattern

--- dags/growth/enrich_atlas_pricing_report/queries/enrich/region_metrics.sql
+++ dags/growth/enrich_atlas_pricing_report/queries/enrich/region_metrics.sql
@@ -1,98 +1,72 @@
 WITH sale_contracts AS (
   SELECT
-    so.id_house,
-    COALESCE(CAST(REPLACE(SUBSTRING(so.dt_sale_agreement_signed, 1, 10), '-', '') AS BIGINT), -1) AS id_sale_agreeement_signed_date,
-    so.last_price_offered_by_buyer AS sale,
-    dd.date AS signed_date
+    offer.id_house,
+    'SALE' AS business_context,
+    h.city,
+    100*(ABS(offer.last_price_offered_by_buyer - hpp.p_50)/offer.last_price_offered_by_buyer) AS percentual_error
   FROM
-    datalake_offer.sale_offer AS so
-  INNER JOIN
-    dw_public.dim_date AS dd
-      ON dd.sk_date = COALESCE(CAST(REPLACE(SUBSTRING(so.dt_sale_agreement_signed, 1, 10), '-', '') AS BIGINT), -1)
-  WHERE
-    COALESCE(CAST(REPLACE(SUBSTRING(so.dt_sale_agreement_signed, 1, 10), '-', '') AS BIGINT), -1) > 0
-    AND DATEDIFF(CURRENT_DATE, dd.date) <= 60
-    AND so.last_price_offered_by_buyer BETWEEN 100000 AND 20000000
-    AND so.id_house IS NOT NULL
-  QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY id_house ORDER BY signed_date DESC) = 1
-),
-sale_prices_errors AS (
-  SELECT DISTINCT
-    sc.id_house,
-    h.city AS city_name,
-    p.business_context,
-    100*(ABS(sc.sale - p.p_50)/sc.sale) AS percentual_error
-  FROM
-    sale_contracts AS sc
-  INNER JOIN
-    datalake_ebdb_clean.house_predicted_price_aud AS p
-      ON sc.id_house = p.id_house
-  INNER JOIN
-    dw_sale.fact_listings AS fl
-      ON sc.id_house = fl.sk_house
-  INNER JOIN
-    datalake_ebdb_listing.house  AS h
-      ON h.id_region = fl.sk_region
-  WHERE
-    p.business_context = 'SALE'
-    AND h.country_code != 'MX'
-  QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY p.id_house ORDER BY p.rev DESC) = 1
-),
-sale_mdape AS (
-  SELECT
-    p.city_name,
-    p.business_context,
-    MEDIAN(p.percentual_error) AS mdape_city
-  FROM
-    sale_prices_errors AS p
-  GROUP BY ALL
-),
-rent_mdape AS (
-  SELECT
-    dhl.house_city AS city_name,
-    hpp.business_context,
-    MEDIAN(ABS(100.0*dc.rent / p_50 - 100)) AS mdape_city
-  FROM
-    dw_rent.fact_listing_rent_flows AS rf
-  INNER JOIN
-    dw_public.dim_contract AS dc
-      ON rf.sk_contract = dc.sk_contract
-  INNER JOIN
-    dw_public.dim_house_listing AS dhl
-      ON dhl.sk_house_listing = rf.sk_house_listing
+    datalake_offer.sale_offer AS offer
   INNER JOIN
     datalake_ebdb_clean.house_predicted_price AS hpp
-      ON dhl.id_house = hpp.id_house
+      ON offer.id_house = hpp.id_house
+  INNER JOIN
+    datalake_ebdb_clean.house AS h
+      ON h.id = offer.id_house
   WHERE
-    DATEDIFF(CURRENT_DATE, dc.dt_start) <= 60
-    AND hpp.business_context = 'RENT'
-    AND dhl.country_code != 'MX'
-  GROUP BY ALL
+    1=1
+    AND offer.dt_sale_agreement_signed IS NOT NULL
+    AND DATE_DIFF(CURRENT_DATE, DATE(offer.dt_sale_agreement_signed)) BETWEEN 0 AND 60
+    AND offer.last_price_offered_by_buyer BETWEEN 100000 AND 20000000
+    AND offer.id_house IS NOT NULL
+    AND hpp.business_context = 'SALE'
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY offer.id_house ORDER BY offer.dt_sale_agreement_signed DESC) = 1
 ),
-union_context AS (
+rent_contracts AS (
   SELECT
-    city_name,
-    business_context,
-    mdape_city
+    h.id AS id_house,
+    'RENT' AS business_context,
+    h.city,
+    100*(ABS(c.rent - hpp.p_50)/c.rent) AS percentual_error
   FROM
-    sale_mdape
-  UNION
+    datalake_ebdb_clean.contract AS c
+  INNER JOIN
+    datalake_ebdb_clean.house AS h
+      ON h.id = c.id_house
+  INNER JOIN
+    datalake_ebdb_clean.house_predicted_price AS hpp
+      ON h.id = hpp.id_house
+  WHERE
+    DATE_DIFF(CURRENT_DATE, DATE(c.ts_signed)) BETWEEN 0 AND 60
+    AND hpp.business_context = 'RENT'
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY h.id ORDER BY c.ts_signed DESC) = 1
+),
+mdape_union AS (
   SELECT
-    city_name,
+    city,
     business_context,
-    mdape_city
+    MEDIAN(percentual_error) AS mdape
   FROM
-    rent_mdape
+    sale_contracts
+  GROUP BY ALL
+  UNION ALL
+  SELECT
+    city,
+    business_context,
+    MEDIAN(percentual_error) AS mdape
+  FROM
+    rent_contracts
+  GROUP BY ALL
 )
-SELECT DISTINCT
+SELECT
   dr.sk_region AS id_region,
-  uc.city_name,
-  uc.business_context,
-  ROUND(uc.mdape_city, 2) AS mdape_city
+  mun.city AS city_name,
+  mun.business_context,
+  ROUND(mun.mdape, 2) AS mdape_city
 FROM
-  union_context AS uc
+  mdape_union AS mun
 INNER JOIN
   dw_public.dim_region AS dr
-    ON uc.city_name = dr.city_name
+    ON mun.city = dr.city_name
+      AND dr.country_code != 'MX'
