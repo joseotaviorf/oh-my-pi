2eaeb592c13bbd1755befd7efac8ee027cc27265
monicalafaria <87429592+monicalafaria@users.noreply.github.com>
2024-05-27 16:57:47 -0300

    Adding affiliates sharing rule (#14572)

--- dags/growth/enrich_growth_costs_sharing_rules/queries/enrich/a036a.sql
+++ dags/growth/enrich_growth_costs_sharing_rules/queries/enrich/a036a.sql
@@ -11,7 +11,6 @@ SELECT
     dd.id_date,
     dr.country_code,
     dr.city_group,
-    NULL AS utm_campaign,
     fse.nm_business_context as business_context,
     COUNT(DISTINCT fse.sk_supply) AS metric 
 FROM dw_growth.fact_supply_events AS fse 
@@ -30,14 +29,13 @@ WHERE fse.sk_funnel_step = 9
     AND fse.nm_business_context = 'RENT'
     AND dat.version = 1 --somente novos usuários
     AND dd.month_start = ddaf.month_start --prospects no mesmo mês dos novos usuários
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5
 UNION ALL 
 SELECT
     dd.date,
     dd.id_date,
     dr.country_code,
     dr.city_group,
-    NULL AS utm_campaign,
     fse.nm_business_context as business_context,
     COUNT(DISTINCT fse.sk_supply) AS metric 
 FROM dw_growth.fact_supply_events AS fse 
@@ -56,7 +54,7 @@ WHERE fse.sk_funnel_step = 9
     AND fse.nm_business_context = 'SALE'
     AND dat.version = 1 --somente novos usuários
     AND dd.month_start = ddaf.month_start --prospects no mesmo mês dos novos usuários
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5
 ),
 -----------------------------------------------------------------------------------------------
 -- This CTE seeks to calculate the moving average of the outcomes from the last 7 days, 
@@ -67,14 +65,13 @@ moving_average AS (
     adt.id_date,   
     cm.country_code,
     cm.city_group, 
-    cm.utm_campaign,
     cm.business_context,
     SUM(cm.metric) AS metric
   FROM conversion_metrics AS cm 
   INNER JOIN datalake_quintoandar.aux_date AS adt
     ON cm.date BETWEEN adt.date - INTERVAL '6' DAY AND adt.date
     AND adt.date <= CURRENT_DATE()
-  GROUP BY 1,2,3,4,5
+  GROUP BY 1,2,3,4
 ), 
 --------------------------------------------------------------------
 -- This CTE calculates the share based on the moving average CTE. --
@@ -84,12 +81,11 @@ fall_back AS (
       mm.id_date,
       mm.country_code,
       mm.city_group,
-      mm.utm_campaign, 
       mm.business_context,
-      FLOAT(SUM(mm.metric))/NULLIF(SUM(SUM(mm.metric)) OVER(PARTITION BY mm.id_date, mm.utm_campaign), 0) AS share
+      FLOAT(SUM(mm.metric))/NULLIF(SUM(SUM(mm.metric)) OVER(PARTITION BY mm.id_date), 0) AS share
   FROM moving_average AS mm
   WHERE TRUE 
-  GROUP BY 1,2,3,4,5
+  GROUP BY 1,2,3,4
 ), 
 -----------------------------------------------------------------------
 -- This CTE calculates the share of the segmentation day's outcomes. --
@@ -98,12 +94,11 @@ actual AS (
   SELECT 
       cm.id_date,
       cm.country_code,
-      cm.city_group,
-      cm.utm_campaign, 
+      cm.city_group, 
       cm.business_context,
-      FLOAT(SUM(cm.metric))/NULLIF(SUM(SUM(cm.metric)) OVER(PARTITION BY cm.id_date, cm.utm_campaign), 0) AS share
+      FLOAT(SUM(cm.metric))/NULLIF(SUM(SUM(cm.metric)) OVER(PARTITION BY cm.id_date), 0) AS share
   FROM conversion_metrics AS cm
-  GROUP BY 1,2,3,4,5
+  GROUP BY 1,2,3,4
 ), 
 -----------------------------------------------------------------------------------------------
 -- This CTE identifies whether there was an outcome on that specific day 
@@ -113,12 +108,11 @@ actual AS (
 validacao AS (
 SELECT
 	adt.id_date,
-  cm.utm_campaign, 
 	SUM(cm.metric) AS validador
 FROM datalake_quintoandar.aux_date AS adt
 LEFT JOIN conversion_metrics AS cm
   ON cm.id_date = adt.id_date
-GROUP BY 1,2
+GROUP BY 1
 )
 -------------------------------------------------------------------------------------------------------
 -- Applying share factor from the correct table based on status: it has or hasn't result on that day --
@@ -126,16 +120,13 @@ GROUP BY 1,2
 SELECT DISTINCT
 	v.id_date,
 	'{id_rule}' AS id_rule,
-  'supply' AS funnel_side, 
-  IF(validador > 0, a.country_code, fb.country_code) AS country_code,
-  IF(validador > 0, a.city_group, fb.city_group) AS city_group, 
-  IF(validador > 0, a.utm_campaign, fb.utm_campaign) AS utm_campaign, 
-  IF(validador > 0, a.business_context, fb.business_context) AS business_context, 
-  IF(validador > 0, a.share, fb.share) AS share
+    'affiliates' AS funnel_side, 
+    IF(validador > 0, a.country_code, fb.country_code) AS country_code,
+    IF(validador > 0, a.city_group, fb.city_group) AS city_group, 
+    IF(validador > 0, a.business_context, fb.business_context) AS business_context, 
+    IF(validador > 0, a.share, fb.share) AS share
 FROM validacao v
 LEFT JOIN actual AS a
 	ON a.id_date = v.id_date
-  AND a.utm_campaign = v.utm_campaign
 LEFT JOIN fall_back AS fb
 	ON fb.id_date = v.id_date
-  AND fb.utm_campaign = v.utm_campaign
