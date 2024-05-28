aebb8e72a02367a32e002112687edee6458edcd8
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-05-28 14:58:38 -0300

    [RD-673] Create Company Cluster dimension (#14577)
    * add dim_company_cluster and update databricks version
    
    * update dependencies
    
    * Update dags/rede/dw_rede_company/queries/dw/dim_company_cluster.sql
    
    update WHERE condition
    
    Co-authored-by: Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
    
    * Update dags/rede/dw_rede_company/queries/dw/dim_company_cluster.sql
    
    update WHERE condition
    
    Co-authored-by: Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
    
    * update query alias and metadata lineage
    
    ---------
    
    Co-authored-by: Allan Moura <145588415+allanmoura5A@users.noreply.github.com>

---
+++ dags/rede/dw_rede_company/queries/dw/dim_company_cluster.sql
@@ -0,0 +1,27 @@
+WITH cluster_changes AS (
+  SELECT
+    ch.id_company,
+    ch.name AS company_name,
+    ch.company_cluster,
+    ch.ts_updated,
+    LAG(ch.company_cluster) OVER (PARTITION BY ch.id_company ORDER BY ch.ts_updated) AS prev_cluster,
+    LAG(ch.ts_updated) OVER (PARTITION BY ch.id_company ORDER BY ch.ts_updated) AS prev_ts_updated
+  FROM
+    datalake_hubspot.company_history AS ch
+  WHERE 
+    ch.company_cluster != 'Sim' 
+    AND ch.company_cluster IS NOT NULL
+)
+
+SELECT
+  MD5(CONCAT(cc.id_company, cc.company_cluster, cc.ts_updated)) AS sk_company_cluster,
+  cc.id_company AS sk_company,
+  cc.company_name,
+  cc.company_cluster,
+  cc.ts_updated AS ts_cluster_start,
+  LEAD(cc.ts_updated) OVER (PARTITION BY cc.id_company ORDER BY cc.ts_updated) AS ts_cluster_end
+FROM
+  cluster_changes AS cc
+WHERE
+  cc.company_cluster != prev_cluster 
+  OR cc.prev_cluster IS NULL
