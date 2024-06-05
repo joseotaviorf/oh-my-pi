9972c7d2a368cf0dc66c61cc14aeb27217fd0d57
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2024-06-05 14:54:02 -0300

    [DIN-1215] Parametrize CDC alert channel (#14655)
    Create webhook for EBDB alerts
    Update DAG declaration to pass webhook customization
    Update Spark job to accept custom webhooks
    Fix duplicate tables in alerts

--- dags/platform/enrich_cdc_replication_metrics/spark_jobs/load_cdc_replication_metrics.py
+++ dags/platform/enrich_cdc_replication_metrics/spark_jobs/load_cdc_replication_metrics.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
 import logging
 import json
-from argparse import ArgumentParser
-from pyspark.sql.functions import col
+import re
+from argparse import ArgumentParser, Namespace
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
 from quintoandar_logger import QuintoAndarLogger
 from bietlejuice.loaders.delta_loader import DeltaLoader
 from bietlejuice.services.messaging_services.gchat_service import GChatService
@@ -10,24 +13,27 @@ from bietlejuice.base.spark.base_spark import BaseDBUtils
 from bietlejuice.base.notification.gchat_webhooks_enum import GchatWebhooksEnum
 
 
+THRESHOLD_IN_SECONDS = 1200
 JOB_NAME = "load_cdc_replication_metrics"
 
 logging.getLogger("py4j").setLevel(logging.ERROR)
 logger = QuintoAndarLogger(JOB_NAME)
 spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
 
-def parse_arguments():
+
+def parse_arguments() -> Namespace:
     parser = ArgumentParser(description=JOB_NAME)
     parser.add_argument("datalake_bucket")
     parser.add_argument("partition_cols")
     parser.add_argument("start_date")
     parser.add_argument("end_date")
     parser.add_argument("schema")
+    parser.add_argument("gchat_channel_customizations")
 
     return parser.parse_args()
 
 
-def get_df_with_metrics(df, table, database_name):
+def get_df_with_metrics(df: DataFrame, table: str, database_name: str) -> DataFrame:
     """
     Generates a df with cdc metrics: count_of_hard_deletes and timestamp_diff_in_seconds. 
     Quantify hard_delete operations (op_cdc = "d")
@@ -38,9 +44,10 @@ def get_df_with_metrics(df, table, database_name):
     logger.info(
         "m=get_df_with_metrics, msg=Extracting cdc metrics from the Transactional layer..."
     )
-    
+
     df.createOrReplaceTempView("temp_df")
-    df_metrics = spark.sql(f"""
+    df_metrics = spark.sql(
+        f"""
                                 SELECT
                                     '{table}' AS table_name,
                                     '{database_name}' AS database_name,
@@ -49,95 +56,159 @@ def get_df_with_metrics(df, table, database_name):
                                     year, month, day
                                 FROM temp_df 
                                 GROUP BY year, month, day
-                            """)
+                            """
+    )
     return df_metrics
 
 
-def send_message(df_tables_delayed, start_date, end_date):
+def send_messages(
+    df_tables_delayed: DataFrame,
+    start_date: str,
+    end_date: str,
+    gchat_channel_customizations: dict,
+) -> None:
     """
-    Sends an alert message to a Gchat Channel.
+    Send the alert messages to a default Gchat Channel and, optionally, to a custom channel.
     """
-    logger.info(
-        "m=send_message, msg=Sending the alert message..."
-    )
+    logger.info("m=send_message, msg=Formatting the alert messages...")
 
     base_dbutils = BaseDBUtils()
     dbutils = base_dbutils.get_dbutils()
-
-    tables_message = ""
-    for row in df_tables_delayed.collect():
-        tables_message += f"Table: {row['database_name']}.{row['table_name']}, Delay in seconds: {row['timestamp_diff_in_seconds']}\n"
-
-        formatted_string =\
-    f"""⚠️ *Delays were found between Database Transaction and DBZ Replication* ⚠️\n\n*The following tables are being replicated by debezium with a delay*\nDate Interval: {start_date} - {end_date}\n{tables_message}\nThere might be a problem with the source database, check the monitoring Dashboards."""
-
-    channel =  dbutils.secrets.get(scope="quintoandar", key=GchatWebhooksEnum.GCHAT_CDC_METRICS)
-    message = Message(
-        formatted_string,
-        channel
+    channel_key_message_mapping = get_formatted_message_content_per_channel(
+        df_tables_delayed, start_date, end_date, gchat_channel_customizations
     )
-    GChatService.send_message(message)
 
-def trigger_gchat_alerts(df, start_date, end_date):
-  """
+    logger.info("m=send_message, msg=Sending the alert messages...")
+    for channel_key, message_content in channel_key_message_mapping.items():
+        channel = dbutils.secrets.get(scope="quintoandar", key=channel_key)
+        message = Message(message_content, channel)
+        GChatService.send_message(message)
+
+
+def get_formatted_message_content_per_channel(
+    df_tables_delayed: DataFrame,
+    start_date: str,
+    end_date: str,
+    gchat_channel_customizations: dict,
+) -> dict:
+    """
+    Returns a dictionary mapping each channel key to its corresponding message content
+    """
+    channel_key_message_mapping = defaultdict(str)
+    default_channel_key = GchatWebhooksEnum.GCHAT_CDC_METRICS
+    for row in df_tables_delayed.collect():
+        if row["timestamp_diff_in_seconds"] < THRESHOLD_IN_SECONDS:
+            continue
+        schema = re.search(r"datalake_(\w+)_transactional", row["database_name"]).group(
+            1
+        )
+        table_message = f"Table: {row['database_name']}.{row['table_name']}, Delay in seconds: {row['timestamp_diff_in_seconds']}\n"
+
+        # We want to send the message to the default channel AND the custom channel
+        channel_key_message_mapping[default_channel_key] += table_message
+        if schema in gchat_channel_customizations:
+            channel_key = gchat_channel_customizations[schema]
+            channel_key_message_mapping[channel_key] += table_message
+
+    message_template = """
+⚠️ *Delays were found between Database Transaction and DBZ Replication* ⚠️
+    
+*The following tables are being replicated by debezium with a delay*
+Date Interval: {start_date} - {end_date}
+{tables_message}
+There might be a problem with the source database, check the monitoring Dashboards.
+    """.strip()
+
+    return {
+        channel_key: message_template.format(
+            start_date=start_date, end_date=end_date, tables_message=tables_message
+        )
+        for channel_key, tables_message in channel_key_message_mapping.items()
+    }
+
+
+def trigger_gchat_alerts(
+    df: DataFrame, start_date: str, end_date: str, gchat_channel_customizations: dict
+) -> None:
+    """
   Checks if the timestamp_diff_in_seconds column reaches the threshold.
   If so, calls the function that sends the Gchat Alert
   """
-  logger.info(
+    logger.info(
         "m=trigger_gchat_alerts, msg=Checking if the timestamp_diff_in_seconds reached the threshold..."
     )
-  threshold_in_second = 1200 
-  df_tables_delayed = df.filter(col("timestamp_diff_in_seconds") >= threshold_in_second)\
-                .select("database_name",
-                  "table_name",
-                  "timestamp_diff_in_seconds")
-  if not df_tables_delayed.isEmpty():
-    send_message(df_tables_delayed, start_date, end_date)
-                
+    df_tables_delayed = get_tables_to_alert_delay(df)
+    if not df_tables_delayed.isEmpty():
+        send_messages(
+            df_tables_delayed, start_date, end_date, gchat_channel_customizations
+        )
 
-def get_metrics_df(start_date, end_date):
+
+def get_tables_to_alert_delay(df_metrics: DataFrame) -> DataFrame:
+    """
+    Returns a DataFrame with the tables that have delays above the threshold in the CDC replication
+    """
+    return (
+        df_metrics.filter(F.col("timestamp_diff_in_seconds") >= THRESHOLD_IN_SECONDS)
+        .select("database_name", "table_name", "timestamp_diff_in_seconds")
+        .groupBy("database_name", "table_name")
+        .agg(F.max("timestamp_diff_in_seconds").alias("timestamp_diff_in_seconds"))
+    )
+
+
+def get_metrics_df(start_date: str, end_date: str) -> DataFrame:
     """
     Returns a df with cdc metrics from all transactional tables that will be used to load the final table
     """
 
-    logger.info(
-        "m=get_metrics_df, msg=Generating the df_metrics for all tables..."
-    )
-    is_first_iteration = True 
-    database_names = [database.name for database in spark.catalog.listDatabases() if database.name.endswith("_transactional") and not database.name.endswith("test_transactional")]
+    logger.info("m=get_metrics_df, msg=Generating the df_metrics for all tables...")
+    is_first_iteration = True
+    database_names = [
+        database.name
+        for database in spark.catalog.listDatabases()
+        if database.name.endswith("_transactional")
+        and not database.name.endswith("test_transactional")
+    ]
 
     for database_name in database_names:
         tables = [table.name for table in spark.catalog.listTables(database_name)]
         for table in tables:
-            logger.info(f"m=get_metrics_df, msg=Generating table {database_name}.{table} metrics based on start_date={start_date} and end_date={end_date}...")
-            df = spark.sql(f"""
+            logger.info(
+                f"m=get_metrics_df, msg=Generating table {database_name}.{table} metrics based on start_date={start_date} and end_date={end_date}..."
+            )
+            df = spark.sql(
+                f"""
                     SELECT
                         op_cdc, ts_database_transaction, ts_cdc_transaction, year, month, day
                     FROM {database_name}.{table}
                     WHERE make_date(year, month, day) BETWEEN '{start_date}' AND '{end_date}'
-                """)
+                """
+            )
 
-            df_metrics = get_df_with_metrics(df, table, database_name) 
-            
+            df_metrics = get_df_with_metrics(df, table, database_name)
+
             if is_first_iteration:
                 df_all_tables = df_metrics
-                is_first_iteration = False 
-            else: 
+                is_first_iteration = False
+            else:
                 df_all_tables = df_all_tables.union(df_metrics)
 
     return df_all_tables
 
 
-def main():
+def main() -> None:
     args = parse_arguments()
     datalake_bucket = args.datalake_bucket
     partition_cols = json.loads(args.partition_cols)
     start_date = args.start_date
     end_date = args.end_date
     schema = args.schema
+    gchat_channel_customizations = json.loads(args.gchat_channel_customizations)
 
     loader = DeltaLoader()
-    destination_table_path = f"s3://{datalake_bucket}/enrich/{schema}/cdc_replication_metrics"
+    destination_table_path = (
+        f"s3://{datalake_bucket}/enrich/{schema}/cdc_replication_metrics"
+    )
     destination_table_name = f"datalake_{schema}.cdc_replication_metrics"
 
     logger.info(
@@ -145,7 +216,6 @@ def main():
         start_date={start_date}, end_date={end_date}, partition_cols={partition_cols}, schema={schema}"""
         "msg=Starting spark job..."
     )
-
 
     df = get_metrics_df(start_date, end_date)
     df.cache()
@@ -161,9 +231,11 @@ def main():
         destination_table_name,
         path=destination_table_path,
         partition_by=partition_cols,
-        source_df=df)
-        
-    trigger_gchat_alerts(df, start_date, end_date)
+        source_df=df,
+    )
+
+    trigger_gchat_alerts(df, start_date, end_date, gchat_channel_customizations)
+
 
 if __name__ == "__main__":
     main()
