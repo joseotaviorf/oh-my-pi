b0d99ec7f65d23f851beb871efb7eb88f219b5d7
gustavohalencar <90861839+gustavohalencar@users.noreply.github.com>
2024-06-05 14:54:37 -0300

    [DRO] Removing columns from ongoing_listings_budget_2024 (#14667)

--- dags/for_rent/gsheets_for_rent/queries/clean/rent_ongoing_listings_budget_2024.sql
+++ dags/for_rent/gsheets_for_rent/queries/clean/rent_ongoing_listings_budget_2024.sql
@@ -9,9 +9,6 @@ SELECT
     CAST(REPLACE(suspensions, ',', '') AS DECIMAL(14,6)) AS suspensions,
     CAST(REPLACE(ended_rentals, ',', '') AS DECIMAL(14,6)) AS ended_rentals,
     CAST(REPLACE(erc, ',', '') AS DECIMAL(14,6)) AS erc,
-    CAST(REPLACE(ongoing_rentals, ',', '') AS DECIMAL(14,6)) AS ongoing_rentals,
-    CAST(REPLACE(new_rental, ',', '') AS DECIMAL(14,6)) AS new_rental,
-    CAST(REPLACE(new_first_rental, ',', '') AS DECIMAL(14,6)) AS new_first_rental,
     TO_DATE(date, 'yyyy-M-d') AS dt_created
 FROM
     datalake_gsheets_raw.rent_ongoing_listings_budget_2024
