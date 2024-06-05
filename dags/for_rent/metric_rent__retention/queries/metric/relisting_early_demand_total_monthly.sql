55c1291b536bc08839cadf92b737a54605c0cee2
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2024-06-05 15:40:58 -0300

    [AEFR-4772] Add category column into relisting_ed_total (#14662)
    * Add category column into relisting_ed_total

--- dags/for_rent/metric_rent__retention/queries/metric/relisting_early_demand_total_monthly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/relisting_early_demand_total_monthly.sql
@@ -1,41 +1,120 @@
+WITH
+contracts AS (
+  SELECT
+    fhl.sk_house_listing,
+    CASE 
+      WHEN 
+        dhl.rent <= 1500 THEN 'LOW'
+      WHEN 
+        dhl.rent < 2500 THEN 'MEDIUM'
+      WHEN 
+        dhl.rent >= 2500 THEN 'HIGH'
+      ELSE 
+        'UNDEFINED'
+    END AS value_segment,
+    COALESCE(fhl.sk_contract, fhl.sk_previous_contract) AS id_contract_listing
+  FROM
+    dw_rent.fact_house_listings AS fhl
+  JOIN
+    dw_rent.dim_house_listing AS dhl
+      ON dhl.sk_house_listing = fhl.sk_house_listing
+),
+base_contract AS (
+  SELECT
+    c.sk_house_listing,
+    c.id_contract_listing,
+    COALESCE(dc.value_segment, c.value_segment) AS category
+  FROM
+    contracts AS c
+  LEFT JOIN
+    dw_rent.dim_contract AS dc
+      ON dc.sk_contract = c.id_contract_listing
+)
+SELECT 
+  CAST(DATE_TRUNC('MONTH', dhl.ts_publication) AS DATE) AS dt_month_publication,
+  'OVERALL' AS category,
+  dhl.country_code,
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing' 
+      AND dhl.is_early_demand IS TRUE
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) AS qtd_ed_rl, 
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing'
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) AS qtd_rl,
+  CAST(
+    COUNT(DISTINCT 
+      IF(
+        dhl.listing_category_start = 'Re-Listing' 
+        AND dhl.is_early_demand IS TRUE
+        , dhl.sk_house_listing
+        , NULL
+      )
+    ) AS DOUBLE
+  ) 
+  / 
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing'
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) * 1.0 AS pct_ed_rl
+FROM 
+  dw_rent.dim_house_listing AS dhl
+GROUP BY 
+  1, 2, 3
+
+UNION ALL
 
 SELECT 
-    CAST(DATE_TRUNC('MONTH', ts_publication) AS DATE) AS dt_month_publication,
-    country_code,
+  CAST(DATE_TRUNC('MONTH', dhl.ts_publication) AS DATE) AS dt_month_publication,
+  bc.category,
+  dhl.country_code,
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing' 
+      AND dhl.is_early_demand IS TRUE
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) AS qtd_ed_rl, 
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing'
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) AS qtd_rl,
+  CAST(
     COUNT(DISTINCT 
       IF(
-        listing_category_start = 'Re-Listing' 
-        AND is_early_demand IS TRUE
-        , sk_house_listing
+        dhl.listing_category_start = 'Re-Listing' 
+        AND dhl.is_early_demand IS TRUE
+        , dhl.sk_house_listing
         , NULL
       )
-    ) AS qtd_ed_rl, 
-    COUNT(DISTINCT 
-      IF(
-        listing_category_start = 'Re-Listing'
-        , sk_house_listing
-        , NULL
-      )
-    ) AS qtd_rl,
-    CAST(
-      COUNT(DISTINCT 
-        IF(
-          listing_category_start = 'Re-Listing' 
-          AND is_early_demand IS TRUE
-          , sk_house_listing
-          , NULL
-        )
-      ) AS DOUBLE
-    ) 
-    / 
-    COUNT(DISTINCT 
-      IF(
-        listing_category_start = 'Re-Listing'
-        , sk_house_listing
-        , NULL
-      )
-    ) * 1.0 AS pct_ed_rl
+    ) AS DOUBLE
+  ) 
+  / 
+  COUNT(DISTINCT 
+    IF(
+      dhl.listing_category_start = 'Re-Listing'
+      , dhl.sk_house_listing
+      , NULL
+    )
+  ) * 1.0 AS pct_ed_rl
 FROM 
-    dw_rent.dim_house_listing
+  dw_rent.dim_house_listing AS dhl
+JOIN 
+  base_contract AS bc
+    ON bc.sk_house_listing  = dhl.sk_house_listing
 GROUP BY 
-    1, 2
+  1, 2, 3
