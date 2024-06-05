8c98f1ff5a8fed48a8935b3c4c668b93a1c7a479
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-06-05 14:02:35 -0300

    [AEFR] Updating enrich_pipeline.table logic (#14666)

--- dags/cross/enrich_pipeline/queries/enrich/table.sql
+++ dags/cross/enrich_pipeline/queries/enrich/table.sql
@@ -1,3 +1,9 @@
+WITH last_dag_inventory_update AS (
+  SELECT
+    MAX(MAKE_DATE(t.year, t.month, t.day)) AS dt_last_update
+  FROM
+    datalake_dag_inventory_clean.table AS t
+)
 SELECT
   t.table AS id_table,
   t.dag AS id_dag,
@@ -9,9 +15,15 @@ SELECT
   t.avg_file_size_in_bytes,
   t.is_delta
 FROM
-    datalake_dag_inventory_clean.table AS t
+  datalake_dag_inventory_clean.table AS t
 JOIN
-    datalake_pipeline.dag AS d
-        ON d.id_dag = t.dag
+  last_dag_inventory_update AS l
+    ON l.dt_last_update = MAKE_DATE(t.year, t.month, t.day)
+JOIN
+  datalake_pipeline.dag AS d
+    ON d.id_dag = t.dag
+JOIN
+  datalake_composer_clean.dag AS dd
+    ON dd.id_dag = d.id_dag
 WHERE
-  MAKE_DATE(t.year, t.month, t.day) = DATE_ADD(CURRENT_DATE, -1)
+  DATE(dd.ts_last_scheduler_ran) = CURRENT_DATE -- Assuring only active DAGs on Airflow, due to interface bugs which can make it still appear on dag_inventory
