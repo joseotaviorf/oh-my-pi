a101270d3ff3e6db90a0869af25a9172dae61fb1
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-06-13 19:21:43 -0300

    [GROW-SUP] Change Supply Tracking (#14776)
    * lint: adjusting query
    
    * feat: adjusting workflow
    
    * feat: add term and content
    
    * fix: adjusting query
    
    * add zorder for tracking

--- dags/growth/enrich_supply_tracking/queries/enrich/supply_events_tracking.sql
+++ dags/growth/enrich_supply_tracking/queries/enrich/supply_events_tracking.sql
@@ -5,7 +5,7 @@ WITH conversion_info AS (
         business_context,
         supply_source
     FROM 
-     datalake_supply_flows_migrate.conversion_lookup
+        datalake_supply_flows.conversion_lookup
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id_lead, business_context, supply_source ORDER BY db_source, ts_conversion) = 1
 ),
 acquisition AS (
@@ -28,8 +28,8 @@ acquisition AS (
         acq.id_lead AS id_entity,
         acq.id_referred_by,
         acq.id_region,
-        NULL AS id_user_registrant,
-        NULL AS id_user_conversion,
+        CAST(NULL AS BIGINT) AS id_user_registrant,
+        CAST(NULL AS BIGINT) AS id_user_conversion,
         acq.supply_source,
         acq.business_event,
         acq.funnel_step,
@@ -43,6 +43,8 @@ acquisition AS (
         acq.source,
         acq.platform,
         acq.campaign,
+        acq.term,
+        acq.content,
         acq.ops_agent,
         acq.ops_approach,
         acq.ops_contact_medium,
@@ -56,26 +58,29 @@ acquisition AS (
         acq.database_tracking_campaign AS aux_database_tracking_campaign,
         acq.database_tracking_medium AS aux_database_tracking_medium,
         acq.database_tracking_source AS aux_database_tracking_source,
+        acq.database_tracking_term AS aux_database_tracking_term,
+        acq.database_tracking_content AS aux_database_tracking_content,
         FALSE AS aux_data_event,
         'T0.0' AS aux_group,
-        acq.funnel_level AS aux_funnel_level,
         acq.aux_product_status,
         'acquisition' AS aux_origin_table,
+        acq.funnel_level AS aux_funnel_level,
         acq.ts_event AS ts_event_adjusted,
         acq.ts_event AS ts_event_original,
-        NULL AS ts_first_discard,
-        NULL AS ts_last_discard,
-        NULL AS ts_reprocessing_event,
+        CAST(NULL AS TIMESTAMP) AS ts_first_discard,
+        CAST(NULL AS TIMESTAMP) AS ts_last_discard,
+        CAST(NULL AS TIMESTAMP) AS ts_reprocessing_event,
         acq.ts_load,
         YEAR(acq.ts_event) AS year, 
         MONTH(acq.ts_event) AS month, 
         DAY(acq.ts_event) AS day
     FROM
-    datalake_supply_flows_migrate.acquisition_tracking AS acq
-    LEFT JOIN conversion_info AS ci
-    ON (acq.id_lead_ebdb = ci.id_lead_ebdb)
-        AND (acq.business_context = ci.business_context)
-        AND (acq.supply_source = ci.supply_source)
+        datalake_supply_flows.acquisition_tracking AS acq
+    LEFT JOIN 
+        conversion_info AS ci
+            ON (acq.id_lead_ebdb = ci.id_lead_ebdb)
+            AND (acq.business_context = ci.business_context)
+            AND (acq.supply_source = ci.supply_source)
 ),
 conversion_without_leads AS (
     SELECT
@@ -88,14 +93,14 @@ conversion_without_leads AS (
             COALESCE(con.id_entity, -1),
             con.business_event
         ) AS bk_supply,
-        NULL AS sk_supply_lead,
+        CAST(NULL AS BIGINT) AS sk_supply_lead,
         con.business_context,
         con.id_lead_ebdb,
         con.id_house,
-        NULL AS id_lead,
+        CAST(NULL AS STRING) AS id_lead,
         con.id_prospect,
         con.id_entity,
-        NULL AS id_referred_by,
+        CAST(NULL AS BIGINT) AS id_referred_by,
         con.id_region,
         con.id_user_registrant,
         con.id_user_conversion,
@@ -103,32 +108,35 @@ conversion_without_leads AS (
         con.business_event,
         con.funnel_step,
         con.reason AS drop_step_reason,
-        NULL AS affiliate_type,
+        CAST(NULL AS STRING) AS affiliate_type,
         con.application,
-        NULL AS landing_page,
-        NULL AS lead_type,
-        NULL AS lead_application,
-        -- NULL AS branded, # DEPRECATED
-        NULL AS medium,
-        NULL AS source,
-        NULL AS platform,
-        NULL AS campaign,
+        CAST(NULL AS STRING) AS landing_page,
+        CAST(NULL AS STRING) AS lead_type,
+        CAST(NULL AS STRING) AS lead_application,
+        CAST(NULL AS STRING) AS medium,
+        CAST(NULL AS STRING) AS source,
+        CAST(NULL AS STRING) AS platform,
+        CAST(NULL AS STRING) AS campaign,
+        CAST(NULL AS STRING) AS term,
+        CAST(NULL AS STRING) AS content,
         con.ops_agent,
         con.ops_approach,
         con.ops_contact_medium,
         con.ops_objective,
         con.ops_partner,
-        NULL AS ops_assigned,
-        NULL AS original_lead,
+        CAST(NULL AS STRING) AS ops_assigned,
+        CAST(NULL AS BIGINT) AS original_lead,
         con.reprocessed,
         con.reprocessing_entity_type,
         con.reprocessing_table_name,
-        NULL AS aux_database_tracking_campaign,
-        NULL AS aux_database_tracking_medium,
-        NULL AS aux_database_tracking_source,
+        CAST(NULL AS STRING) AS aux_database_tracking_campaign,
+        CAST(NULL AS STRING) AS aux_database_tracking_medium,
+        CAST(NULL AS STRING) AS aux_database_tracking_source,
+        CAST(NULL AS STRING) AS aux_database_tracking_term,
+        CAST(NULL AS STRING) AS aux_database_tracking_content,
         con.aux_data_event,
         con.aux_group,
-        NULL AS aux_product_status,
+        CAST(NULL AS STRING) AS aux_product_status,
         'conversion' AS aux_origin_table,
         con.ts_event_adjusted,
         con.ts_event_original,
@@ -140,8 +148,9 @@ conversion_without_leads AS (
         MONTH(con.ts_event_adjusted) AS month, 
         DAY(con.ts_event_adjusted) AS day
     FROM
-        datalake_supply_flows_migrate.conversion_tracking AS con
-    WHERE id_lead_ebdb IS NULL
+        datalake_supply_flows.conversion_tracking AS con
+    WHERE 
+        id_lead_ebdb IS NULL
 ),
 acq_taxonomy AS (
     SELECT 
@@ -158,19 +167,24 @@ acq_taxonomy AS (
         acq.source,
         acq.platform,
         acq.campaign,
+        acq.term,
+        acq.content,
         acq.original_lead,
         acq.ops_assigned,
         acq.aux_database_tracking_campaign,
         acq.aux_database_tracking_medium,
         acq.aux_database_tracking_source,
+        acq.aux_database_tracking_term,
+        acq.aux_database_tracking_content,
         acq.aux_product_status
     FROM 
-     acquisition AS acq 
-    WHERE acq.id_lead_ebdb IS NOT NULL
+        acquisition AS acq 
+    WHERE 
+        acq.id_lead_ebdb IS NOT NULL
     QUALIFY ROW_NUMBER() OVER (PARTITION BY acq.id_lead_ebdb, acq.supply_source ORDER BY acq.aux_funnel_level DESC, acq.ts_event_adjusted DESC) = 1
 ),
 conversion_with_leads AS (
-        SELECT
+    SELECT
         CONCAT_WS(
             '#',
             con.funnel_step,
@@ -204,6 +218,8 @@ conversion_with_leads AS (
         acq.source,
         acq.platform,
         acq.campaign,
+        acq.term,
+        acq.content,
         con.ops_agent,
         con.ops_approach,
         con.ops_contact_medium,
@@ -217,6 +233,8 @@ conversion_with_leads AS (
         acq.aux_database_tracking_campaign,
         acq.aux_database_tracking_medium,
         acq.aux_database_tracking_source,
+        acq.aux_database_tracking_term,
+        acq.aux_database_tracking_content,
         con.aux_data_event,
         con.aux_group,
         acq.aux_product_status,
@@ -231,18 +249,26 @@ conversion_with_leads AS (
         MONTH(con.ts_event_adjusted) AS month,
         DAY(con.ts_event_adjusted) AS day
     FROM
-        datalake_supply_flows_migrate.conversion_tracking AS con
-    LEFT JOIN acq_taxonomy AS acq
-        ON (con.id_lead_ebdb = acq.id_lead_ebdb)
-        AND (con.supply_source = acq.supply_source)
-    WHERE con.id_lead_ebdb IS NOT NULL
+        datalake_supply_flows.conversion_tracking AS con
+    LEFT JOIN 
+        acq_taxonomy AS acq
+            ON (con.id_lead_ebdb = acq.id_lead_ebdb)
+            AND (con.supply_source = acq.supply_source)
+    WHERE 
+        con.id_lead_ebdb IS NOT NULL
 )
 
-SELECT * EXCEPT(aux_funnel_level)
-FROM acquisition
+SELECT 
+    * EXCEPT (aux_funnel_level)
+FROM 
+    acquisition
 UNION ALL
-SELECT *
-FROM conversion_without_leads
+SELECT 
+    *
+FROM 
+    conversion_without_leads
 UNION ALL
-SELECT *
-FROM conversion_with_leads
+SELECT 
+    *
+FROM 
+    conversion_with_leads
