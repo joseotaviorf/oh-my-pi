8dc2415eb588aad519bb8fd27220d7ef9d3836a8
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-06-18 10:54:34 -0300

    [DFORSALE-7865] - Adding new atta table (#14795)
    * [DFORSALE-7865] - Adding new atta table
    
    * Delete dags/dependencies.yaml
    
    * dependencies
    
    * Update dependencies.yaml

---
+++ dags/for_sale/atta/queries/clean/bank_tracking.sql
@@ -0,0 +1,13 @@
+SELECT
+    id,
+    bankid AS id_bank,
+    proposalId AS id_proposal,
+    bankProposalId AS id_bank_proposal,
+    proposalStatus AS proposal_status,
+    proposalSituation AS proposal_situation,
+    status AS global_status,
+    itauStatusDescription AS status_description_itau,
+    createdAt AS ts_created,
+    updatedAt AS ts_updated
+FROM
+    datalake_atta_raw.banktracking
