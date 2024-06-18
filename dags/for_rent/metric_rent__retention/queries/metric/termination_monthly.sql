fb4826fe27e4e32b32acfc830b8c1a1892335794
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2024-06-18 15:02:44 -0300

    [AEFR-4837] Add offboarding leadtime metric to Metric Layer (#14830)
    * Add offboarding leadtime metric to Metric Layer

--- dags/for_rent/metric_rent__retention/queries/metric/termination_monthly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/termination_monthly.sql
@@ -2,11 +2,32 @@ SELECT
   CAST(DATE_TRUNC('MONTH', dt.ts_termination_finished) AS DATE) AS dt_month_termination_finished,
   dc.country_code,
   'OVERALL' AS category,
+  'OVERALL' AS repair_resolution_class,
+  COUNT(DISTINCT fct.sk_termination) AS total_terminations,
+  CAST(AVG(DATEDIFF(dt.ts_termination_finished, dt.dt_termination)) AS NUMERIC(5,2)) AS leadtime_offboarding
+FROM 
+  dw_retention.fact_contract_termination AS fct
+JOIN
+  dw_retention.dim_termination AS dt
+    ON dt.sk_termination = fct.sk_termination
+JOIN
+  dw_rent.dim_contract AS dc
+    ON dc.sk_contract = fct.sk_contract
+GROUP BY 
+  1, 2, 3, 4
+
+UNION ALL
+
+SELECT 
+  CAST(DATE_TRUNC('MONTH', dt.ts_termination_finished) AS DATE) AS dt_month_termination_finished,
+  dc.country_code,
+  'OVERALL' AS category,
   CASE 
     WHEN dt.repair_resolution IS NULL OR dt.repair_resolution IN ('NO_REPAIR_NEEDED', 'FORGIVEN_BY_LANDLORD') THEN 'without_repair'
     ELSE 'with_repair'
   END AS repair_resolution_class,
-  COUNT(DISTINCT fct.sk_termination) AS total_terminations
+  COUNT(DISTINCT fct.sk_termination) AS total_terminations,
+  CAST(AVG(DATEDIFF(dt.ts_termination_finished, dt.dt_termination)) AS NUMERIC(5,2)) AS leadtime_offboarding
 FROM 
   dw_retention.fact_contract_termination AS fct
 JOIN
@@ -28,7 +49,8 @@ SELECT
     WHEN dt.repair_resolution IS NULL OR dt.repair_resolution IN ('NO_REPAIR_NEEDED', 'FORGIVEN_BY_LANDLORD') THEN 'without_repair'
     ELSE 'with_repair'
   END AS repair_resolution_class,
-  COUNT(DISTINCT fct.sk_termination) AS total_terminations
+  COUNT(DISTINCT fct.sk_termination) AS total_terminations,
+  CAST(AVG(DATEDIFF(dt.ts_termination_finished, dt.dt_termination)) AS NUMERIC(5,2)) AS leadtime_offboarding
 FROM 
   dw_retention.fact_contract_termination AS fct
 JOIN
