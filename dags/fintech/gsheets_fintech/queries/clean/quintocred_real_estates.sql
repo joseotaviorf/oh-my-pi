193edc06482c9bfd1659fcb060129864bf2f7f7d
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2024-06-19 17:56:43 -0300

    Add new gsheet for quintocred context (#14845)
    * Add new gsheet for quintocred context
    
    * add termination form
    
    * delete termination_form
    
    * add nullif

---
+++ dags/fintech/gsheets_fintech/queries/clean/quintocred_real_estates.sql
@@ -0,0 +1,9 @@
+SELECT
+    NULLIF(sk_broker, '') AS sk_broker,
+    NULLIF(id_pipedrive, '') AS id_pipedrive,
+    NULLIF(broker_name, '') AS broker_name,
+    NULLIF(cnpj, '') AS cnpj,
+    NULLIF(potential_segmentation, '') AS potential_segmentation,
+    NULLIF(credit_segmentation, '') AS credit_segmentation
+FROM
+    datalake_gsheets_raw.quintocred_real_estates
