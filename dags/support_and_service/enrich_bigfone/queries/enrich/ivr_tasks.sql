89a17cc15e396621781c8f0dc63004137b98dc15
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-06-20 11:38:53 -0300

    fix: remove where clause (#14851)

--- dags/support_and_service/enrich_bigfone/queries/enrich/ivr_tasks.sql
+++ dags/support_and_service/enrich_bigfone/queries/enrich/ivr_tasks.sql
@@ -13,7 +13,6 @@ FROM
     datalake_bigfone_clean.event
 WHERE
     workflow_name = 'IVR Events'
-    AND ivr_steps IS NOT NULL
     AND MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
 QUALIFY
     ROW_NUMBER() OVER(PARTITION BY id_task ORDER BY ts_created) = 1
