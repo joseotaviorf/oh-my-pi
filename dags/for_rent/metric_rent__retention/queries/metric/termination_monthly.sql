70ba77175323e25155bb8fb00a3d1992d65234e6
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2024-06-26 16:42:47 -0300

    Add termination metrics without consider repairs (#14930)

--- dags/for_rent/metric_rent__retention/queries/metric/termination_monthly.sql
+++ dags/for_rent/metric_rent__retention/queries/metric/termination_monthly.sql
@@ -61,3 +61,23 @@ JOIN
     ON dc.sk_contract = fct.sk_contract
 GROUP BY 
   1, 2, 3, 4
+
+UNION ALL
+
+SELECT 
+  CAST(DATE_TRUNC('MONTH', dt.ts_termination_finished) AS DATE) AS dt_month_termination_finished,
+  dc.country_code,
+  dc.value_segment AS category,
+  'OVERALL' AS repair_resolution_class,
+  COUNT(DISTINCT fct.sk_termination) AS total_terminations,
+  CAST(AVG(DATEDIFF(dt.ts_termination_finished, dt.dt_termination)) AS NUMERIC(5,2)) AS leadtime_offboarding
+FROM 
+  dw_retention.fact_contract_termination AS fct
+JOIN
+  dw_retention.dim_termination AS dt
+    ON dt.sk_termination = fct.sk_termination
+JOIN
+  dw_rent.dim_contract AS dc
+    ON dc.sk_contract = fct.sk_contract
+GROUP BY 
+  1, 2, 3, 4
