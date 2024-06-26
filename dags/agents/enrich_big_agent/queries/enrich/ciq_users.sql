9895b44333374e9ee745d7993730ceadafb737f2
Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
2024-06-26 17:37:05 -0300

    [DFORSALE-7780] Enrich table adjustment - ciq_users (#14923)
    * [DFORSALE-7780] Query adjustment to handle merged users and bring their update history
    
    * fix dependencies

--- dags/agents/enrich_big_agent/queries/enrich/ciq_users.sql
+++ dags/agents/enrich_big_agent/queries/enrich/ciq_users.sql
@@ -1,36 +1,62 @@
-WITH last_updated_agents AS (
+WITH partner_type AS (
+   SELECT
+        p.id, -- id_partner
+        p.type,
+        p.ts_updated
+    FROM
+        datalake_ebdb_clean.partner AS p
+    WHERE 
+        p.type = 'AUTONOMOUS_AGENT'    -- Type for CIQ user
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY p.id ORDER BY p.ts_updated DESC) = 1
+),
+big_agent_enrollment_type AS (
+    SELECT 
+        id_agent,
+        id_program,
+        ts_updated
+    FROM
+        datalake_big_agent_clean.enrollment 
+    WHERE 
+        id_program IN (1,2) -- CIQ Manager or CIQ Full
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY id_agent ORDER BY ts_updated DESC) = 1
+),
+updated_id_user_main AS (
+    SELECT
+      id_user,
+      EXPLODE(predecessor_user_list) AS id_predecessor_user
+    FROM
+      datalake_ebdb_user.user_merge
+),
+ciq_agents AS (
     SELECT
         a.id AS id_agent,
         CAST(GET_JSON_OBJECT(a.details, '$.userExternalId') AS LONG) AS id_user,
         CAST(GET_JSON_OBJECT(a.details, '$.partnerExternalId') AS LONG) AS id_partner,
-        CAST(GET_JSON_OBJECT(a.details, '$.active') AS BOOLEAN) AS is_active_in_big_agent,
+        CAST(GET_JSON_OBJECT(a.details, '$.active') AS BOOLEAN) AS is_active,
+        COALESCE(uim.id_user, CAST(GET_JSON_OBJECT(a.details, '$.userExternalId') AS LONG)) AS id_main_user,
+        CASE 
+          WHEN uim.id_user IS NOT NULL THEN TRUE
+          ELSE FALSE
+        END is_user_merged,
+        e.id_program,
         a.ts_created,
         a.ts_updated
     FROM
         datalake_big_agent_clean.agent AS a
-    QUALIFY
-        ROW_NUMBER() OVER(PARTITION BY GET_JSON_OBJECT(a.details, '$.partnerExternalId') ORDER BY a.ts_updated DESC) = 1
-),
-partner_agent AS (
-    SELECT
-        p.id,
-        p.name,
-        p.phone,
-        p.cnpj,
-        p.email,
-        p.creci,
-        p.type,
-        pa.status,
-        p.ts_created,
-        p.ts_updated,
-        pa.ts_updated AS ts_status_updated
-    FROM
-        datalake_ebdb_clean.partner AS p
-    LEFT JOIN datalake_ebdb_clean.partner_agent AS pa 
-        ON pa.id_partner = p.id
-    WHERE p.type = 'AUTONOMOUS_AGENT'    
-    QUALIFY
-        ROW_NUMBER() OVER(PARTITION BY p.id ORDER BY pa.ts_updated DESC) = 1
+    LEFT JOIN
+        partner_type AS pt
+        ON CAST(GET_JSON_OBJECT(a.details, '$.partnerExternalId') AS LONG) = pt.id
+    LEFT JOIN
+        big_agent_enrollment_type AS e 
+            ON e.id_agent = a.id
+    LEFT JOIN
+        updated_id_user_main AS uim
+            ON CAST(GET_JSON_OBJECT(a.details, '$.userExternalId') AS LONG) = uim.id_predecessor_user
+    WHERE 
+        e.id_program IS NOT NULL   -- CIQ Manager or CIQ Full
+        OR pt.type IS NOT NULL     -- pt.type = 'AUTONOMOUS_AGENT' is a CIQ Agent        
 ),
 hub_services_users AS (
     SELECT
@@ -45,31 +71,21 @@ hub_services_users AS (
 SELECT 
     a.id_agent,
     a.id_partner,
-    u.id AS id_main_user,
+    a.id_main_user,
     CAST(u.id_agent AS INTEGER) AS id_agent_data,
     hs.id_hub_services_user,
     u.name,
     u.email,
-    u.main_phone,
-    ad.creci_number,
-    u.cpf,
-    u.city,
-    pa.status,
+    a.is_active,
+    a.is_user_merged,   
     a.ts_created AS ts_agent_created,
-    a.ts_updated AS ts_agent_updated,
-    u.ts_created AS ts_user_created,
-    u.ts_updated AS ts_user_updated
+    a.ts_updated AS ts_agent_updated_start,
+    LEAD(a.ts_updated) OVER (PARTITION BY a.id_agent ORDER BY a.ts_updated ASC) AS ts_agent_updated_end
 FROM
-    last_updated_agents AS a
+    ciq_agents AS a
 LEFT JOIN
     datalake_ebdb_user.user AS u
-        ON u.id = a.id_user
-LEFT JOIN
-    datalake_ebdb_user.agent_data AS ad
-        ON ad.id = u.id_agent
-LEFT JOIN 
-    partner_agent AS pa
-        ON a.id_partner = pa.id
+        ON u.id = a.id_main_user
 LEFT JOIN 
     hub_services_users AS hs
         ON u.id = hs.id_main_user
