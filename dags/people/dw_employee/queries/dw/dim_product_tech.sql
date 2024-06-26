5bab35a506e6c59a9cad3e4668b9dc1a39671bbc
araujoic <45436397+araujoic@users.noreply.github.com>
2024-06-26 10:40:50 -0300

    add table dim_product_tech (#14921)

---
+++ dags/people/dw_employee/queries/dw/dim_product_tech.sql
@@ -0,0 +1,25 @@
+SELECT
+    dim_employee.sk_employee,
+    CASE
+        WHEN team_formation_product_tech.team_1 = '' THEN NULL
+        ELSE team_formation_product_tech.team_1
+    END AS team_1,
+    CASE
+        WHEN team_formation_product_tech.team_2 = '' THEN NULL
+        ELSE team_formation_product_tech.team_2
+    END AS team_2,
+    CASE
+        WHEN team_formation_product_tech.team_3 = '' THEN NULL
+        ELSE team_formation_product_tech.team_3
+    END AS team_3,
+    CASE
+        WHEN team_formation_product_tech.team_4 = '' THEN NULL
+        ELSE team_formation_product_tech.team_4
+    END AS team_4,
+    CASE
+        WHEN team_formation_product_tech.team_5 = '' THEN NULL
+        ELSE team_formation_product_tech.team_5
+    END AS team_5
+FROM
+    datalake_gsheets_clean.team_formation_product_tech
+    LEFT JOIN dw_employee.dim_employee ON dim_employee.work_email = team_formation_product_tech.email
