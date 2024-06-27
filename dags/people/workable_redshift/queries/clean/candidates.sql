f8f6238cf6000c6694bf522bfaeba5275c1b2b4c
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-06-27 17:42:39 -0300

    [DBP-415] Workable Redshift (#14955)
    * 🚧 test dag workable redshift
    
    * ✨ add new candidates
    
    * ✨ add new table jobs to workable redshift
    
    * ✨ add requisitions table to workable redshift
    
    * 🐛 forgot to add in declaration
    
    * 📝 att doc workable redshift
    
    * ⚡️ improve DBOR to 13.3
    
    * 🚚 rename column last_name and first_name
    
    * 🐛 fix rename

---
+++ dags/people/workable_redshift/queries/clean/candidates.sql
@@ -0,0 +1,49 @@
+SELECT
+    -- ids
+    id,
+    -- non-ids
+    job_id AS id_job,
+    current_stage_id AS id_current_stage,
+    source_member_id AS id_source_member,
+    recommender_id AS id_recommender,
+    recruiter_id AS id_recruiter,
+    uploader_id AS id_uploader,
+    api_id AS id_api,
+    -- non-metrics
+    job_title,
+    job_department,
+   "name",
+    firstname AS first_name,
+    lastname AS last_name,
+    headline,
+    email,
+    application_method,
+    source_category,
+    source_domain,
+    "source",
+    outlet,
+    current_stage_name,
+    disqualification_reason,
+    metadata,
+    tags,
+    linkedin_profile_url,
+    -- metrics
+    copied AS is_copied,
+    disqualified AS is_disqualified,
+    recommended AS is_recommended,
+    snoozed AS is_snoozed,
+    -- timestamps
+    candidate_created_at AS ts_candidate_created,
+    job_first_published_at AS ts_job_first_published, 
+    last_unanswered_email_at AS ts_last_unanswered_email,
+    first_screened_at AS ts_first_screen,
+    first_contacted_at AS ts_first_contact,
+    first_interviewed_at AS ts_first_interview,
+    first_offer_at AS ts_first_offer,
+    first_hired_at AS ts_first_hire,
+    disqualified_at AS ts_disqualified,
+    snoozed_until AS ts_snoozed_until,
+    updated_at AS ts_updated,
+    NOW() AS ts_load
+FROM
+    datalake_workable_redshift_raw.candidates;
---
+++ dags/people/workable_redshift/queries/clean/jobs.sql
@@ -0,0 +1,53 @@
+SELECT
+    -- ids
+    id,
+    -- non-ids
+    department_id AS id_department,
+    pipeline_id AS id_pipeline,
+    location_id AS id_location,
+    -- non-metrics
+    title,
+    department,
+    code,
+    state,
+    shortcode,
+    country_code,
+    state_code,
+    city,
+    zip_code,
+    advertise_country_code,
+    advertise_state_code,
+    advertise_city,
+    advertise_zip_code,
+    employment_type,
+    "function",
+    industry,
+    experience,
+    education,
+    salary_currency,
+    subregion,
+    coords,
+    access_level,
+    -- metrics
+    salary_from,
+    salary_to,
+    advertise_cost,
+    candidates_count,
+    free_postings_count,
+    paid_postings_count,
+    owned_postings_count,
+    -- booleans
+    telecommuting AS is_telecommuting,
+    advertise_elsewhere AS has_advertise_elsewhere,
+    -- timestamps
+    job_created_at AS ts_job_created,
+    first_published_at AS ts_first_publis,
+    last_published_at AS ts_last_publish,
+    first_screening_at AS ts_first_screening,
+    first_interview_at AS ts_first_interview,
+    first_offer_at AS ts_first_offer,
+    first_hire_at AS ts_first_hire,
+    updated_at AS ts_updated,
+    NOW() AS ts_load
+FROM
+    datalake_workable_redshift_raw.jobs
