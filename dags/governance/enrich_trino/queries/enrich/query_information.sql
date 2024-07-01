2ce1b2f5ed415945d0f1c193393cc11dc4e23b2a
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-07-01 12:24:50 -0300

    [AEFR] Adding DataHub as a tool on query_information (#14978)
    * [AEFR] Adding DataHub as a tool on query_information
    
    * description
    
    * execution hour
    
    * session user

--- dags/governance/enrich_trino/queries/enrich/query_information.sql
+++ dags/governance/enrich_trino/queries/enrich/query_information.sql
@@ -2,9 +2,11 @@ WITH base AS (
     SELECT
         queryId AS id_query,
         resourceGroupId AS id_resource_group,
+        GET_JSON_OBJECT(session, '$.user') AS session_user,
         referencedTables AS referenced_tables,
         state,
         execution_time, 
+        hour AS execution_hour,
         execution_start_time AS ts_execution_started,
         execution_end_time AS ts_execution_ended,
         year,
@@ -35,10 +37,12 @@ tools AS (
         b.id_query,
         GET(b.id_resource_group, 2) AS tool,
         GET(b.id_resource_group, 3) AS user,
+        b.session_user,
         b.state,
         b.execution_time,
-        es.database_name,
-        es.table_name,
+        b.execution_hour,
+        NULLIF(es.database_name, '') AS database_name,
+        NULLIF(es.table_name, '') AS table_name,
         b.ts_execution_started,
         b.ts_execution_ended,
         b.year,
@@ -52,10 +56,12 @@ tools AS (
 )
 SELECT
     id_query,
+    CONCAT(database_name, '.', table_name) AS id_table,
     CASE
         WHEN tool = 'bi-metabase' THEN 'Metabase'
         WHEN tool = 'bi-looker' THEN 'Looker'
         WHEN tool = 'bi-superset' THEN 'Superset'
+        WHEN tool = 'other' AND session_user LIKE 'trino_datahub%' THEN 'DataHub'
         ELSE tool
     END AS tool,
     user,
@@ -63,6 +69,7 @@ SELECT
     execution_time,
     database_name,
     table_name,
+    execution_hour,
     MAKE_DATE(year, month, day) AS dt_extraction,
     ts_execution_started,
     ts_execution_ended,
