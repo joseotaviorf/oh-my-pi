0dc905742d29173ee8b00868bd663f4adbc990df
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-07-02 10:19:55 -0300

    [AEFR-4543] Create enrich_tracksale_dispatches (#14884)
    * add enrich_tracksale_dispatches DAG and true_offboarding_iq/pp_dispatches tables

---
+++ dags/for_rent/enrich_tracksale_dispatches/queries/enrich/true_offboarding_pp_dispatches.sql
@@ -0,0 +1,143 @@
+WITH b2b_listings AS (
+  SELECT
+    rf.sk_contract,
+    b2b.is_b2b
+  FROM 
+    dw_rent.fact_listing_rent_flows AS rf
+  JOIN 
+    datalake_b2b.house_listing AS b2b
+      ON rf.sk_house_listing = b2b.id_house_listing
+      AND b2b.is_b2b
+  WHERE
+    rf.sk_contract != -1
+    AND rf.sk_contract_signed_date > 0
+),
+contracts AS (
+  SELECT
+    c.id AS id_contract,
+    c.dt_started,
+    ct.ts_termination_finished,
+    DATE_ADD(ad.date, 1) - INTERVAL '15' day AS dt_recap,
+    DATEDIFF(DATE(ct.ts_termination_finished), DATE_ADD(ad.date, 1)) AS ndays_termination2today,
+    CASE
+      WHEN DATEDIFF(DATE_ADD(ad.date, 1), DATE(ct.ts_termination_finished)) = 2 THEN 'termination'
+      WHEN DATEDIFF(DATE_ADD(ad.date, 1), DATE(ct.ts_termination_finished)) = 15 THEN 'recap_termination'
+      ELSE NULL
+    END AS termination_type
+  FROM
+    datalake_ebdb_contract.contract AS c
+  JOIN
+    datalake_quintoandar.aux_date AS ad
+      ON ad.date = DATE('{load_start_date}')
+  INNER JOIN
+    datalake_offboarding.contract_termination AS ct
+      ON c.id = ct.id_contract
+  LEFT JOIN
+    b2b_listings AS bl
+      ON c.id = bl.sk_contract
+  WHERE
+    c.country_code = 'BR'
+    AND ct.status = 'DONE'
+    AND bl.sk_contract IS NULL
+    AND ct.dt_termination >= c.dt_started
+    AND c.rental_administrator = 'QUINTOANDAR'
+),
+status_send AS (
+  SELECT
+    c.id_contract,
+    ut.id_ticket,
+    c.ndays_termination2today,
+    c.termination_type,
+    dp.department,
+    dp.team,
+    c.dt_started,
+    c.ts_termination_finished,
+    c.dt_recap,
+    ut.ts_started,
+    ut.ts_solved,
+    CASE
+      WHEN (dp.department IN ('Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão por Inadimplência [OFF] [POS] [BACK]', 'Notificação Extrajudicial [CE] [POS] [BACK]','Dados Bancários [CE] [POS] [BACK]','CX ReclameAqui Adquiridas [CE] [POS] [BACK]')
+        OR dp.team IN ('Casos Especiais','Ouvidoria','ReclameAqui','Evictions'))
+        OR (c.termination_type = 'termination'
+        AND ut.id_ticket IS NOT NULL
+        AND ut.ts_solved IS NULL
+        AND dp.sk_department IS NOT NULL) THEN 1
+      ELSE 0
+    END AS flg_not_send,
+    CASE
+      WHEN (dp.department IN ('Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão por Inadimplência [OFF] [POS] [BACK]', 'Notificação Extrajudicial [CE] [POS] [BACK]','Dados Bancários [CE] [POS] [BACK]','CX ReclameAqui Adquiridas [CE] [POS] [BACK]')
+        OR dp.team IN ('Casos Especiais','Ouvidoria','ReclameAqui','Evictions')) THEN 0
+      WHEN c.termination_type = 'recap_termination'
+        AND ut.id_ticket IS NOT NULL
+        AND ut.ts_started < c.dt_recap + INTERVAL '2' day
+        AND dp.department IN ('Offboarding [OFF] [POS] [BACK]', 'Atendimento Escalado [OFF] [POS] [BACK]', 'Proteção QuintoAndar [OFF] [POS] [BACK]')
+        AND (ut.ts_solved >= c.dt_recap + INTERVAL '2' day OR ut.ts_solved IS NULL) THEN 1
+      ELSE 0
+    END AS flg_recap_send
+  FROM
+    contracts AS c
+  LEFT JOIN
+    datalake_customer_support.unified_tickets AS ut
+      ON c.id_contract = ut.id_contract
+      AND ut.id_contract IS NOT NULL
+  LEFT JOIN
+    dw_customer_support.dim_department AS dp
+      ON ut.id_main_department = dp.sk_department
+      AND (dp.department IN ('Offboarding [OFF] [POS] [BACK]', 'Atendimento Escalado [OFF] [POS] [BACK]','Proteção QuintoAndar [OFF] [POS] [BACK]','Rescisão - Despejo [OFF][POS][BACK]', 'Rescisão por Inadimplência [OFF] [POS] [BACK]', 'Notificação Extrajudicial [CE] [POS] [BACK]','Dados Bancários [CE] [POS] [BACK]','CX ReclameAqui Adquiridas [CE] [POS] [BACK]')
+        OR dp.team IN ('Casos Especiais','Ouvidoria','ReclameAqui','Evictions'))
+  WHERE
+    c.termination_type IS NOT NULL  --('termination', 'recap_termination')
+),
+contracts_to_send AS (
+  SELECT
+    id_contract,
+    ndays_termination2today,
+    MAX(termination_type) AS termination_type,
+    MAX(flg_not_send) AS flg_not_send,
+    MAX(flg_recap_send) AS flg_recap_send
+  FROM
+    status_send
+  GROUP BY
+    1, 2
+  HAVING
+    (MAX(termination_type) = 'termination' AND MAX(flg_not_send) = 0)
+    OR (MAX(termination_type) = 'recap_termination' AND MAX(flg_recap_send) = 1)
+)
+SELECT
+  cp.name AS customer_name,
+  cp.email AS customer_email,
+  cp.phone_number AS customer_phone,
+  'Rescisão' AS campaign_step,
+  'Proprietário' AS customer_type,
+  cp.cpf AS customer_cpf,
+  cp.id_user,
+  'true' AS campaign_type,
+  'contract' AS driver_type,
+  cp.id_contract AS id_driver,
+  YEAR(DATE_ADD(ad.date, 1)) AS year,
+  MONTH(DATE_ADD(ad.date, 1)) AS month,
+  DAY(DATE_ADD(ad.date, 1)) AS day
+FROM
+  datalake_ebdb_clean.contract_person AS cp
+INNER JOIN
+  contracts_to_send AS cs
+    ON cp.id_contract = cs.id_contract
+JOIN
+  datalake_quintoandar.aux_date AS ad
+    ON ad.date = DATE('{load_start_date}')
+LEFT JOIN
+  datalake_ebdb_clean.user AS u
+    ON (cp.email = u.email
+      OR cp.email = u.alternative_email)
+LEFT JOIN
+  datalake_ebdb_customer_contact_identification.customer_contact_identification AS cci
+    ON cp.email = cci.customer_contact
+LEFT JOIN
+  datalake_ebdb_clean.user_pro_owner AS po
+    ON (cp.id_user = po.id_user
+      OR u.id = po.id_user
+      OR cci.id_user = po.id_user)
+    AND po.is_active = TRUE
+WHERE
+  cp.type IN ('Proprietario')
+  AND po.is_active IS NULL
