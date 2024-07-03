b32be264c87e2a309a4b6eb1e0d3f5c669400aed
Rodrigo Arnaldo Kreuzberg <rodrigokreuzberg@gmail.com>
2024-07-03 14:58:38 -0400

    Ddss 3762 add gsheet polygon experience (#15013)
    * Add gsheet polygon experience
    
    * renamed_files
    
    * Fix - declaration name file
    
    * Fix - Datasource sql

---
+++ dags/support_and_service/gsheets_support_and_service/queries/clean/manual_kpis_polygon.sql
@@ -0,0 +1,9 @@
+SELECT
+  TO_DATE(date_month, 'dd/MM/yyyy') AS dt_month,
+  CAST(uc_okr_fr_operations AS DECIMAL(10, 4)) AS uc_okr_fr_operations,
+  CAST(seamless_journey AS DECIMAL(10, 4)) AS seamless_journey,
+  CAST(rent_app_share_of_postcontract AS DECIMAL(10, 4)) AS rent_app_share_post_contract,
+  CAST(rent_app_share_of_postcontract_landlord AS DECIMAL(10, 4)) AS rent_app_share_post_contract_landlord,
+  CAST(rent_app_share_of_postcontract_tenant AS DECIMAL(10, 4)) AS rent_app_share_post_contract_tenant
+FROM
+  datalake_gsheets_raw.manual_kpis_polygon
