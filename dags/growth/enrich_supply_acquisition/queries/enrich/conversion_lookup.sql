4c4bec7c9d2f9f05d1ac41f39d869000a13cd679
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-07-10 09:31:16 -0300

    [GROW-FIX] Removing merge from Conversion Lookup (#15051)
    * fix: removing merge
    
    * fix: adjusting dependencies

--- dags/growth/enrich_supply_acquisition/queries/enrich/conversion_lookup.sql
+++ dags/growth/enrich_supply_acquisition/queries/enrich/conversion_lookup.sql
@@ -2,17 +2,20 @@
 WITH bob_houses AS (
   SELECT 
     sp.id_external AS id_house,
-    hd.business_context,
+    EXPLODE(
+      ARRAY(
+        IF(hd.is_for_rent, 'RENT', NULL),
+        IF(hd.is_for_sale, 'SALE', NULL)
+      )
+    ) AS business_context,
     'BOB' AS source,
     '1P' AS supply_source
-  FROM
-    datalake_bob.house_draft_business_context AS hd
-  JOIN
+  FROM 
+    datalake_bob.house_draft AS hd
+  JOIN 
     datalake_bob_clean.submission_progress AS sp 
-      ON (hd.id_draft = sp.id_house_draft) 
+        ON (hd.id = sp.id_house_draft) 
         AND (sp.id_external IS NOT NULL)
-  WHERE 
-    DATE(hd.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
 ),
 lbc_houses AS (
   SELECT 
@@ -31,12 +34,12 @@ lbc_houses AS (
         AND (aud.id_house = lbc.id_house)
   WHERE
     aud.status IN ('EDITING', 'PUBLISHED')
-      AND DATE(lbc.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
   QUALIFY ROW_NUMBER() OVER (PARTITION BY aud.id_house, aud.business_context ORDER BY aud.rev) = 1
 ),
 joined_tb AS (
   SELECT *
   FROM bob_houses
+  WHERE business_context IS NOT NULL
   UNION ALL
   SELECT *
   FROM lbc_houses
@@ -75,8 +78,6 @@ lead_conversion_1p AS (
     LEFT JOIN 
       houses_with_context AS hw
         ON (hlc.id_house = hw.id_house)
-    WHERE 
-      DATE(hlc.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
     UNION ALL
     -- EBDB is a source of historic information
     SELECT 
@@ -102,7 +103,6 @@ lead_conversion_1p AS (
         ON (cl.id_house = hw.id_house)
     WHERE 
       id_converted_lead IS NOT NULL
-        AND DATE(cl.ts_conversion) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
 ),
 lead_conversion_3p AS (
     SELECT 
@@ -121,8 +121,6 @@ lead_conversion_3p AS (
         ON h.id_external = l.uuid_lead
     JOIN datalake_ebdb_clean.listing_business_context_aud AS lbca
         ON lbca.id_house = h.id
-    WHERE 
-      DATE(l.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
     QUALIFY ROW_NUMBER() OVER (PARTITION BY l.id, lbca.id_house, lbca.business_context ORDER BY l.ts_created) = 1
 ),
 lead_conversion_ciq AS (
@@ -144,7 +142,6 @@ lead_conversion_ciq AS (
           AND (s.id_external IS NOT NULL)
     WHERE 
       (hd.type = 'ADMIN_CONFIRMATION')
-        AND DATE(hd.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
 ),
 
 all_conversions AS (
