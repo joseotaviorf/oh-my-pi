9761bfd656857f409976ccae16cbc3704037d6d5
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-07-10 16:17:41 -0300

    [AEFR] Adding table task success as part of the SLA condition (#15052)
    * updating SLA rules
    
    * description
    
    * comment
    
    * updates
    
    * more columns
    
    * undo
    
    * undo 2
    
    * Update dags/cross/enrich_pipeline/queries/enrich/dag_run.sql
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
    
    ---------
    
    Co-authored-by: Jonas Mendes Fiorini <jonasfiorini7@gmail.com>

--- dags/cross/enrich_pipeline/queries/enrich/dag_run.sql
+++ dags/cross/enrich_pipeline/queries/enrich/dag_run.sql
@@ -12,21 +12,121 @@ WITH first_run_ever AS (
   QUALIFY
     ROW_NUMBER() OVER (PARTITION BY id_dag ORDER BY ts_event ASC) = 1
 ),
-success_run AS (
-  -- Checking the first time that the DAG run was marked as successful, which will be used to understand if it's inside or not the SLA
+/** 
+  Using DAG Inventory, we can related the id_task with the table. Usually we use the cluster termination task to understand if a DAG was
+  inside or outside the SLA, but in Q3 and Q4/2023, we wrongly removed this task and we ended up with a gap in our SLA.
+  In order to "fix" it, we're using the id_task timestamp of success to understand if the DAG was inside or outside the SLA. 
+  If by some reason we don't have information related to the task (some error with DAG Inventory, for example), we'll be considering
+  the cluster termination successful timestamp.
+**/
+dag_inventory AS (
+  SELECT
+    dag AS id_dag,
+    task AS id_task,
+    layer,
+    MAKE_DATE(year, month, day) AS dt_extracted,
+    DATE_ADD(MAKE_DATE(year, month, day), 1) AS dt_current
+  FROM
+    datalake_dag_inventory_clean.table
+  WHERE
+    dag LIKE 'bietlejuice%'
+),
+first_task_success_log AS (
+  -- Checking the first time that the DAG run was marked as successful, based on the table tasks
   SELECT
     id_dag,
-    ts_event AS ts_success_event,
-    ts_executed
+    id_task,
+    TIMESTAMP(ts_event) AS ts_success_event,
+    DATE(ts_executed) AS dt_run
   FROM
     datalake_composer_clean.log
   WHERE
     id_dag LIKE 'bietlejuice%'
     AND event = 'success'
-    AND (id_task IN ('terminate-cluster', 'job-cluster-finished')
-      OR id_task LIKE '%-skip-execution%') -- Some DAGs may have as the first task a short-circuit that skips the cluster/job creation
   QUALIFY
-    ROW_NUMBER() OVER (PARTITION BY id_dag, ts_executed ORDER BY ts_event ASC) = 1
+    ROW_NUMBER() OVER (PARTITION BY id_dag, id_task, DATE(ts_executed) ORDER BY ts_event ASC) = 1
+),
+table_task_success AS (
+  SELECT
+    l.id_dag,
+    l.id_task,
+    di.layer,
+    dt_run,
+    dt_extracted,
+    ts_success_event,
+    CASE
+      WHEN layer = 'reverse' THEN COALESCE(DATE(ts_success_event), DATE_ADD(dt_run, 1)) + INTERVAL 15 HOUR
+      WHEN l.id_dag LIKE '%datamart%' THEN COALESCE(DATE(ts_success_event), DATE_ADD(dt_run, 1)) + INTERVAL 13 HOUR
+      ELSE COALESCE(DATE(ts_success_event), DATE_ADD(dt_run, 1)) + INTERVAL 11 HOUR
+    END AS ts_expected_sla
+  FROM
+    first_task_success_log AS l
+  LEFT JOIN -- Left because we need to consider dummy tasks
+    dag_inventory AS di
+      ON l.id_task = di.id_task
+      AND l.dt_run = di.dt_extracted
+  WHERE
+    di.id_task IS NOT NULL  -- Assuring that we're only having the task that loads a table or dummy tasks
+    OR l.id_task LIKE '%-skip-execution%'
+    OR l.id_task LIKE 'done-clean%'
+),
+expected_task_sla AS (
+  -- Checking if the task tables of a DAG are inside the expected SLA
+  SELECT
+    id_dag,
+    id_task,
+    IF(ts_success_event IS NULL OR ts_success_event > ts_expected_sla, FALSE, TRUE) AS is_inside_sla,
+    dt_run,
+    ts_success_event,
+    ts_expected_sla
+  FROM
+    table_task_success AS tts 
+),
+expected_tasks AS (
+  SELECT
+    es.id_dag,
+    dt_run,
+    COUNT(DISTINCT es.id_task) AS tables,
+    COUNT(DISTINCT es.id_task) FILTER (WHERE is_inside_sla = TRUE) AS tables_inside_sla,
+    COUNT(DISTINCT es.id_task) FILTER (WHERE is_inside_sla = FALSE) AS tables_outside_sla,
+    MAX(es.ts_success_event) AS ts_last_task_successful
+  FROM
+    expected_task_sla AS es
+  GROUP BY 1, 2
+),
+all_tasks_sla AS (
+  SELECT
+    id_dag,
+    IF(tables = tables_inside_sla, TRUE, FALSE) AS is_all_tables_inside_sla,
+    dt_run,
+    ts_last_task_successful
+  FROM
+    expected_tasks
+),
+success_run AS (
+  -- Checking the first time that the DAG run was marked as successful, based on the job/cluster termination task
+  SELECT
+    l.id_dag,
+    TIMESTAMP(l.ts_executed) AS ts_executed,
+    l.ts_event AS ts_success_event,
+    CASE
+        WHEN layer = 'reverse' THEN COALESCE(DATE(l.ts_event), DATE_ADD(l.ts_executed, 1)) + INTERVAL 15 HOUR
+        WHEN di.id_dag LIKE '%datamart%' THEN COALESCE(DATE(l.ts_event), DATE_ADD(l.ts_executed, 1)) + INTERVAL 13 HOUR
+        ELSE COALESCE(DATE(l.ts_event), DATE_ADD(l.ts_executed, 1)) + INTERVAL 11 HOUR
+      END AS ts_expected_sla
+  FROM
+    datalake_composer_clean.log AS l
+  JOIN
+    dag_inventory AS di
+      ON di.id_dag = l.id_dag
+      AND di.dt_extracted = DATE(l.ts_executed)
+  WHERE
+    l.id_dag LIKE 'bietlejuice%'
+    AND event = 'success'
+    AND (l.id_task IN ('terminate-cluster', 'job-cluster-finished')
+      OR l.id_task LIKE '%-skip-execution%') -- Some DAGs may have as the first task a short-circuit that skips the cluster/job creation
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY l.id_dag, DATE(ts_executed) ORDER BY ts_event ASC) = 1
 ),
 dag_clear AS (
   -- Checking if the DAG run suffered any clear, which indicates that it ran more than once
@@ -69,48 +169,12 @@ SELECT
   IF(fre.id_dag IS NOT NULL, TRUE, FALSE) AS is_first_run_ever,
   IF(dr.state = 'failed', FALSE, TRUE) AS is_run_successful,
   CASE
-    WHEN fre.id_dag IS NULL THEN -- If the DAG isn't in its first run, we can consider it
-      (
-        CASE
-          WHEN ds.dag IS NULL 
-            AND dr.id_run NOT LIKE 'manual%' THEN  -- Checking if the DAG isn't in the list to be ignored and if it's not a manual run
-          (
-            CASE  -- Layers raw/clean, enrich, dw (except for datamarts) and metric has 8h AM BRT as SLA
-              WHEN dr.id_dag NOT LIKE '%datamarts%'
-                AND dr.id_dag NOT LIKE '%.reverse%' THEN
-              (
-                CASE
-                  WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event <= TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 11 HOUR THEN TRUE
-                  WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event > TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 11 HOUR THEN FALSE
-                  WHEN s.ts_success_event IS NULL AND NOW() > TO_TIMESTAMP(CURRENT_DATE, 'yyyy-MM-dd HH:mm:ss') + INTERVAL 11 HOUR THEN FALSE
-                  ELSE NULL
-                END
-              )
-              WHEN dr.id_dag LIKE '%datamarts%' THEN
-                  (
-                    CASE
-                      WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event <= TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 13 HOUR THEN TRUE
-                      WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event > TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 13 HOUR THEN FALSE
-                      WHEN s.ts_success_event IS NULL AND NOW() > TO_TIMESTAMP(CURRENT_DATE, 'yyyy-MM-dd HH:mm:ss') + INTERVAL 13 HOUR THEN FALSE
-                      ELSE NULL
-                    END
-                  )
-              WHEN dr.id_dag LIKE '%.reverse%' THEN
-                (
-                    CASE
-                      WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event <= TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 15 HOUR THEN TRUE
-                      WHEN s.ts_success_event IS NOT NULL AND s.ts_success_event > TO_TIMESTAMP(DATE(s.ts_success_event), 'yyyy-MM-dd HH:mm:ss') + INTERVAL 15 HOUR THEN FALSE
-                      WHEN s.ts_success_event IS NULL AND NOW() > TO_TIMESTAMP(CURRENT_DATE, 'yyyy-MM-dd HH:mm:ss') + INTERVAL 15 HOUR THEN FALSE
-                      ELSE NULL
-                    END
-                  )
-              ELSE NULL
-            END
-          )
-          ELSE NULL
-        END
-      )
-    ELSE NULL -- If the DAG is in its first run, it doesn't have a SLA
+    WHEN fre.id_dag IS NOT NULL OR ds.dag IS NOT NULL OR dr.id_run LIKE 'manual%' THEN NULL -- Excluding DAGs on the SLA exclusion list
+    WHEN s.ts_success_event <= s.ts_expected_sla THEN TRUE 
+    WHEN s.ts_success_event > s.ts_expected_sla AND is_all_tables_inside_sla <> TRUE THEN FALSE
+    WHEN s.ts_success_event > s.ts_expected_sla AND is_all_tables_inside_sla = TRUE THEN TRUE
+    WHEN s.ts_success_event IS NULL THEN is_all_tables_inside_sla
+    ELSE NULL
   END AS is_first_execution_inside_sla,
   IF(c.id_dag IS NOT NULL, TRUE, FALSE) AS has_been_cleared,
   dr.ts_executed AS ts_run,
@@ -121,6 +185,8 @@ SELECT
   FROM_UTC_TIMESTAMP(dr.ts_ended, 'America/Sao_Paulo') AS ts_ended_brt,
   TIMESTAMP(s.ts_success_event) AS ts_first_execution_success,
   FROM_UTC_TIMESTAMP(s.ts_success_event, 'America/Sao_Paulo') AS ts_first_execution_success_brt,
+  IF(et.is_all_tables_inside_sla = TRUE, et.ts_last_task_successful, NULL) AS ts_last_table_task_successful,
+  IF(et.is_all_tables_inside_sla = TRUE, FROM_UTC_TIMESTAMP(et.ts_last_task_successful, 'America/Sao_Paulo'), NULL) AS ts_last_table_task_successful_brt,
   cr.ts_last_execution_ended AS ts_last_composer_run,
   FROM_UTC_TIMESTAMP(cr.ts_last_execution_ended, 'America/Sao_Paulo') AS ts_last_composer_run_brt,
   NOW() AS ts_load,
@@ -139,6 +205,10 @@ LEFT JOIN
   success_run AS s
     ON s.id_dag = dr.id_dag
     AND s.ts_executed = dr.ts_executed
+LEFT JOIN
+  all_tasks_sla AS et
+    ON et.id_dag = dr.id_dag
+    AND et.dt_run = DATE(dr.ts_executed)
 LEFT JOIN
   dag_clear AS c
     ON c.id_dag = dr.id_dag
