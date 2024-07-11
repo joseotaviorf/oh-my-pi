fc60288e2b038860ea17371d96d996e17b89c4e4
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-07-11 10:56:50 -0300

    🐛 fix bug in hr system custom job (#15080)

--- dags/people/enrich_hr_system_custom/spark_jobs/load_management_hierarchy_enrich.py
+++ dags/people/enrich_hr_system_custom/spark_jobs/load_management_hierarchy_enrich.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     spark_client = SparkClient()
 
     df = spark_client.conn.sql(managers_query)
-    df = df.filter(df.sk_manager.isNotNull())
+    df = df.filter(df.id_manager_assignment.isNotNull())
 
     df_degree = df.withColumn("separation_degree", lit(1))\
                 .withColumn("is_direct_manager", lit(True))
