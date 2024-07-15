44eb34c02e94172c586b5ca668f4366a3c1cf478
Isabela Sayuri Matsumoto <isabela.matsumoto@quintoandar.com.br>
2024-07-15 09:54:15 -0300

    Adding new organization in olos outbound_mailling (#15091)

--- dags/growth/enrich_olos_dialer/queries/enrich/outbound_mailing.sql
+++ dags/growth/enrich_olos_dialer/queries/enrich/outbound_mailing.sql
@@ -49,4 +49,11 @@ LEFT JOIN
         ON customer.id_customer = campaign_customer.id_customer
 WHERE
     MAKE_DATE(mailing.year, mailing.month, mailing.day) BETWEEN DATE_SUB(DATE('{load_start_date}'), {days_past}) AND DATE('{load_end_date}')
-    AND customer.name = 'Inside Sales'
+    AND customer.name in (
+        'Inside Sales',
+        'AL Inside Sales',
+        'AeC Inside Sales',
+        'Atento Inside Sales',
+        'Inside Sales',
+        'QA Inside Sales'
+    )
