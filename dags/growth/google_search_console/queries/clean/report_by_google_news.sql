7e07c5375fd77c0348aa38a469ae650ef69d4255
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2024-07-16 10:32:29 -0300

    adding new tables to ingestion (#15112)

---
+++ dags/growth/google_search_console/queries/clean/report_by_google_news.sql
@@ -0,0 +1,19 @@
+SELECT
+  site_url,
+  type,
+  keys[0] AS page,
+  keys[1] AS country,
+  keys[2] AS device,
+  CAST(clicks AS INT) AS clicks,
+  CAST(impressions AS INT) AS impressions,
+  CAST(ctr AS DOUBLE) AS ctr,
+  CAST(position AS DOUBLE) AS position,
+  CAST(position*impressions AS DOUBLE) AS posimp,
+  date AS dt_created,
+  YEAR(date) AS year,
+  MONTH(date) AS month,
+  DAY(date) AS day
+FROM
+    datalake_google_search_console_raw.report_by_google_news
+WHERE
+    date BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
