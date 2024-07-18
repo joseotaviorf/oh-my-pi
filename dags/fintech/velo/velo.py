0a47838f5ed2a408d167045ec1e2ba34db1b229e
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2024-07-18 13:43:33 -0300

    [AEFF-2331] Upgrade DBR on velo DAG (#15152)
    * Upgrade DBR on velo DAG
    
    * upgrade dbr on enrich velo

--- dags/fintech/velo/velo.py
+++ dags/fintech/velo/velo.py
@@ -35,7 +35,7 @@ databricks_bietlejuice_repo_path = config_service.get_config(
 )
 BASE_SPARK_JOBS_PATH = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 
-cluster_description = config_service.get_config("databricks_10_4_med_general_cluster")
+cluster_description = config_service.get_config("databricks_13_3_med_general_cluster")
 default_libraries = config_service.get_config("default_libraries")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
