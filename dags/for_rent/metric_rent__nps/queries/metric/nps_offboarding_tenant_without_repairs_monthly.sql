fb7d11f4d86c33783474c02808cceda2f1d06e41
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-07-18 15:43:01 -0300

    [AEFR] Add Tenant NPS metrics with and without repairs (#15157)
    * add queries/metadata nps with and without repairs
    
    * add without repairs and change column name
    
    * update column name

---
+++ dags/for_rent/metric_rent__nps/queries/metric/nps_offboarding_tenant_without_repairs_monthly.sql
@@ -0,0 +1,61 @@
+SELECT
+  DATE(DATE_TRUNC('MONTH', dd_answer.date)) AS dt_month_answered,
+  dc.value_segment AS category,
+  dc.country_code,
+  COUNT(fnd.sk_nps_answer) AS total_answers_no_repairs_needed,
+  COUNT(IF(fnd.score < 7, fnd.sk_nps_answer, NULL)) AS detractors_no_repairs_needed,
+  COUNT(IF(fnd.score > 8, fnd.sk_nps_answer, NULL)) AS promoters_no_repairs_needed,
+  COUNT(IF(fnd.score between 7 and 8, fnd.sk_nps_answer, NULL)) AS neutrals_no_repairs_needed,
+  100 * (COUNT(IF(fnd.score > 8, sk_nps_answer, NULL)) - COUNT(IF(fnd.score < 7, sk_nps_answer, NULL))) / COUNT(sk_nps_answer) AS nps_no_repairs_needed
+FROM
+  dw_tracksale.fact_nps_dispatches AS fnd
+JOIN
+  dw_tracksale.dim_nps_campaign AS dnc
+    ON fnd.sk_nps_campaign = dnc.sk_nps_campaign
+JOIN
+  dw_rent.dim_contract AS dc
+    ON fnd.sk_contract = dc.sk_contract
+JOIN
+  dw_public.dim_date AS dd_answer
+    ON fnd.sk_answered_date = dd_answer.sk_date
+WHERE
+  dd_answer.year >= 2022
+  AND fnd.sk_nps_answer > 0
+  AND dnc.business_context = 'forRent'
+  AND dnc.metric_group = 'iqoffboarding'
+  AND dnc.customer_type = 'IQ'
+  AND dc.is_repair_tenant_duty <> TRUE
+GROUP BY
+  1, 2, 3
+
+UNION ALL
+
+SELECT
+  DATE(DATE_TRUNC('MONTH', dd_answer.date)) AS dt_month_answered,
+  'OVERALL' AS category,
+  dc.country_code,
+  COUNT(fnd.sk_nps_answer) AS total_answers_no_repairs_needed,
+  COUNT(IF(fnd.score < 7, fnd.sk_nps_answer, NULL)) AS detractors_no_repairs_needed,
+  COUNT(IF(fnd.score > 8, fnd.sk_nps_answer, NULL)) AS promoters_no_repairs_needed,
+  COUNT(IF(fnd.score between 7 and 8, fnd.sk_nps_answer, NULL)) AS neutrals_no_repairs_needed,
+  100 * (COUNT(IF(fnd.score > 8, sk_nps_answer, NULL)) - COUNT(IF(fnd.score < 7, sk_nps_answer, NULL))) / COUNT(sk_nps_answer) AS nps_no_repairs_needed
+FROM
+  dw_tracksale.fact_nps_dispatches AS fnd
+JOIN
+  dw_tracksale.dim_nps_campaign AS dnc
+    ON fnd.sk_nps_campaign = dnc.sk_nps_campaign
+JOIN
+  dw_rent.dim_contract AS dc
+    ON fnd.sk_contract = dc.sk_contract
+JOIN
+  dw_public.dim_date AS dd_answer
+    ON fnd.sk_answered_date = dd_answer.sk_date
+WHERE
+  dd_answer.year >= 2022
+  AND fnd.sk_nps_answer > 0
+  AND dnc.business_context = 'forRent'
+  AND dnc.metric_group = 'iqoffboarding'
+  AND dnc.customer_type = 'IQ'
+  AND dc.is_repair_tenant_duty <> TRUE
+GROUP BY
+  1, 2, 3
