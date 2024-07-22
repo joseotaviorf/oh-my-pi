7f86691ead32c4a80ae41b7ff3cf9923f809954a
a-chs <75737674+a-chs@users.noreply.github.com>
2024-07-22 17:53:22 -0300

    [DFORSALE-8006]Fix NPS EoP Cash Dispaches (#15192)
    * update filters
    
    * update filters

--- dags/cross/reverse_tracksale/queries/reverse/buyer_end_of_process.sql
+++ dags/cross/reverse_tracksale/queries/reverse/buyer_end_of_process.sql
@@ -120,11 +120,12 @@ previous_sent_id_drivers AS (
       (payment_method IS NULL OR payment_method LIKE 'FINANCED%')
       AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 114) <= CAST(ts_house_registry_ended AS TIMESTAMP))) 
       OR (
-      (payment_method IS NULL OR payment_method LIKE 'CASH%') 
-      AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 60) <= CAST(ts_house_registry_ended AS TIMESTAMP))
+      (payment_method IS NULL OR payment_method LIKE 'CASH%')
+      AND ((dt_event < DATE('2024-05-18')) AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 45) <= CAST(ts_house_registry_ended AS TIMESTAMP))
+      ) OR ((dt_event >= DATE('2024-05-18')) AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 60) <= CAST(ts_house_registry_ended AS TIMESTAMP)))
     ) OR (
       (payment_method IS NULL OR payment_method LIKE 'CASH%') 
-      AND (dt_event <= DATE('2024-05-18'))
+      AND (dt_event < DATE('2024-05-18'))
       AND (ts_house_registry_ended IS NULL OR DATE_ADD(CAST(ts_house_registry_ended AS TIMESTAMP), -2) <= DATE('2024-06-30'))
     )
   )
--- dags/cross/reverse_tracksale/queries/reverse/seller_end_of_process.sql
+++ dags/cross/reverse_tracksale/queries/reverse/seller_end_of_process.sql
@@ -122,16 +122,17 @@ previous_sent_id_drivers AS (
   SELECT id_driver
     FROM customer_info
     WHERE (
-    (payment_method IS NULL OR payment_method LIKE 'FINANCED%')
-    AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 114) <= CAST(ts_house_registry_ended AS TIMESTAMP)))
-  OR (
-    (payment_method IS NULL OR payment_method LIKE 'CASH%') 
-    AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 60) <= CAST(ts_house_registry_ended AS TIMESTAMP))
-  ) OR (
-    (payment_method IS NULL OR payment_method LIKE 'CASH%') 
-    AND (dt_event <= DATE('2024-05-18'))
-    AND (ts_house_registry_ended IS NULL OR DATE_ADD(CAST(ts_house_registry_ended AS TIMESTAMP), -2) <= DATE('2024-06-30'))
-  )
+      (payment_method IS NULL OR payment_method LIKE 'FINANCED%')
+      AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 114) <= CAST(ts_house_registry_ended AS TIMESTAMP))) 
+      OR (
+      (payment_method IS NULL OR payment_method LIKE 'CASH%')
+      AND ((dt_event < DATE('2024-05-18')) AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 45) <= CAST(ts_house_registry_ended AS TIMESTAMP))
+      ) OR ((dt_event >= DATE('2024-05-18')) AND (ts_house_registry_ended IS NOT NULL AND DATE_ADD(dt_event, 60) <= CAST(ts_house_registry_ended AS TIMESTAMP)))
+    ) OR (
+      (payment_method IS NULL OR payment_method LIKE 'CASH%') 
+      AND (dt_event < DATE('2024-05-18'))
+      AND (ts_house_registry_ended IS NULL OR DATE_ADD(CAST(ts_house_registry_ended AS TIMESTAMP), -2) <= DATE('2024-06-30'))
+    )
 )
 SELECT
     customer_name,
