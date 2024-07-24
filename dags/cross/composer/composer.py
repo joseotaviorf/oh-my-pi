bfa31263b3bf75cbb7e0e00b1fa67e98709e2bfa
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-07-24 14:58:21 -0300

    [AEFR] Updating composer scheduler (#15250)

--- dags/cross/composer/composer.py
+++ dags/cross/composer/composer.py
@@ -23,7 +23,7 @@ from bietlejuice.base.databricks import DatabricksGroupNameEnum, ClusterPermissi
 SOURCE = "composer"
 DAG_ID = f"bietlejuice.{SOURCE}"
 MAIN_START_DATE = datetime(2019, 8, 21, tzinfo=timezone("America/Sao_Paulo"))
-MAIN_SCHEDULE_INTERVAL = "0 6-18/1 * * *"
+MAIN_SCHEDULE_INTERVAL = "0 5-18/1 * * *"
 CLUSTER_DESCRIPTION = "databricks_12_2_min_io-memory_cluster"
 
 config_service = ConfigurationService(SOURCE)
