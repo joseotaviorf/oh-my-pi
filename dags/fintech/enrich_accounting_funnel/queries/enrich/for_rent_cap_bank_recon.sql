1e2483a69a0b64aa5baa96ef2db1410d769f1a0a
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2024-07-25 10:52:08 -0300

    added sap account_number in model and insert vans information again (#15268)
    * added sap account_number in model and insert vans information again
    
    * add metadata information
    
    * added is_vans_compliance in lineage

--- dags/fintech/enrich_accounting_funnel/queries/enrich/for_rent_cap_bank_recon.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/for_rent_cap_bank_recon.sql
@@ -20,6 +20,31 @@ WITH francesinha AS (
         bank_account IN (426887, 79952, 433065, 502307)
 ),
 
+cap AS (
+    SELECT
+        dt_paid,
+        UPPER(CASE
+            WHEN reference_1 RLIKE '[0-9]P[0-9]' AND LENGTH(TRIM(reference_1)) = 11 AND SUBSTRING(reference_1, 1, 1) = '0' THEN SUBSTRING(REGEXP_REPLACE(SUBSTRING(REPLACE(reference_1, '|', '!'), 1, LENGTH(reference_1) - 2), 'P.', 'P'), 2, LENGTH(reference_1))
+            WHEN reference_1 RLIKE '[0-9]P[0-9]' THEN REGEXP_REPLACE(SUBSTRING(REPLACE(reference_1, '|', '!'), 1, LENGTH(reference_1) - 2), 'P.', 'P')
+            ELSE REPLACE(reference_1, '|', '!')
+        END) AS company_use,
+        payment_status,
+        reference_3 AS payment_name,
+        reference_4 AS payment_type,
+        SPLIT(reference_5, ':') AS id_banking_payment,
+        SUM(paid_amount) AS paid_amount
+    FROM
+        datalake_accounting_funnel.payment_platforms
+    WHERE
+        payment_platform = 'vans_cap'
+    AND
+        payment_status IN ('paid', 'chargeback')
+    AND
+        CAST(SPLIT(reference_5, ':')[0] AS INT) IN (1, 2, 7, 10, 13)
+    GROUP BY
+        1, 2, 3, 4, 5, 6
+),
+
 sap AS (
     SELECT
         UPPER(id_external_payment) AS company_use,
@@ -40,6 +65,12 @@ cap_sap AS (
         company_use,
         dt_paid
     FROM
+        cap
+    UNION ALL
+    SELECT DISTINCT
+        company_use,
+        dt_paid
+    FROM
         sap
     UNION ALL
     SELECT DISTINCT
@@ -51,16 +82,24 @@ cap_sap AS (
 
 SELECT DISTINCT
     cs.company_use AS id_company_use,
-    f.bank_account AS id_bank_account,
+    f.bank_account AS bank_account_number,
+    sap.account_number AS sap_account_number,
     f.last_occurrence_code,
     f.paid_amount AS bank_paid_amount,
+    cap.paid_amount AS cap_paid_amount,
     sap.paid_amount AS sap_paid_amount,
+    CASE
+        WHEN payment_status = 'chargeback' AND sap.paid_amount = 0 THEN TRUE
+        WHEN ABS(f.paid_amount) = ABS(cap.paid_amount) THEN TRUE
+    ELSE FALSE
+    END AS is_vans_compliance,
     CASE
         WHEN (ABS(f.paid_amount) = ABS(sap.paid_amount)) THEN TRUE
         WHEN last_occurrence_code = 'DV' AND (sap.paid_amount = 0 OR sap.paid_amount IS NULL) THEN TRUE
     ELSE FALSE
     END AS is_reconcilied,
     f.dt_paid AS dt_bank_paid,
+    cap.dt_paid AS dt_cap_paid,
     sap.dt_paid AS dt_sap_paid,
     sap.dt_tax AS dt_sap_tax,
     cs.dt_paid
@@ -70,6 +109,10 @@ LEFT JOIN
     francesinha f
         ON f.company_use = cs.company_use
         AND cs.dt_paid = f.dt_paid
+LEFT JOIN
+    cap
+        ON cs.company_use = cap.company_use
+        AND cs.dt_paid = cap.dt_paid
 LEFT JOIN
     sap
         ON cs.company_use = sap.company_use
