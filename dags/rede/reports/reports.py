976c5a254ec18a9d7833417531b171c9b713aec6
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-07-29 11:00:27 -0300

    update databricks runtime (#15237)

--- dags/rede/reports/reports.py
+++ dags/rede/reports/reports.py
@@ -40,7 +40,7 @@ RAW_SPARK_JOB_PATH = (
     f"{databricks_bietlejuice_repo_path}/spark_jobs/{SOURCE}/" "load_reports_raw.py"
 )
 
-CLUSTER_DESCRIPTION = config_service.get_config("databricks_10_4_med_general_cluster")
+CLUSTER_DESCRIPTION = config_service.get_config("databricks_12_2_med_general_cluster")
 DAG_DOCUMENTATION = config_service.get_config("dag_documentation")
 
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
