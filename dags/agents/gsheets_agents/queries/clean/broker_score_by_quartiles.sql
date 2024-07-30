a2b5ad6ca3a0989512e7d27b6ab5eb094b40a0de
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2024-07-30 10:16:46 -0300

    [AE-AGENTS] Converting broker_score_by_quartiles gsheets data (#15270)
    * Update broker_score_by_quartiles.sql
    
    * Update broker_score_by_quartiles.sql

--- dags/agents/gsheets_agents/queries/clean/broker_score_by_quartiles.sql
+++ dags/agents/gsheets_agents/queries/clean/broker_score_by_quartiles.sql
@@ -1,15 +1,15 @@
 -- The source spreadsheet is generated with data from the datalake, 
 -- therefore, it should not be used as a dependency for other tables
 SELECT
-    sk_user_agent AS id_user_agent,
-    sk_agent AS id_agent,
+    CAST(REPLACE(sk_user_agent, '.', '') AS BIGINT) AS id_user_agent,
+    CAST(sk_agent  AS BIGINT) AS id_agent,
     mes_descritivo AS month_description,
     praca AS square,
     hub,
     quartil AS quartile,
     CAST(REPLACE(score_gp, ',', '.') AS FLOAT) AS score_gp,
-    mes AS month,
-    ano AS year
+    CAST(mes AS INT) AS month,
+    CAST(ano AS INT) AS year
 FROM
     datalake_gsheets_raw.broker_score_by_quartiles
 
