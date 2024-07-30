3f03a3d7f0a634108ccc25a389bca96807fb45ae
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2024-07-30 15:19:17 -0300

    [DFF-181] Revert Vans DAGBuilder but keep DBR version (#15350)
    * Revert "DFF-181 - Fix Vans DAG Declaration (#15341)"
    
    This reverts commit 896b54a046b71682ef92905ece43601412adfb7e.
    
    * Revert "DFF-181 - Migrating Vans to DAGBuilder and DBR 12 #15337"
    
    This reverts commit 7546e9e11aece869cf2389cb3e333679384b5e43.
    
    * Revert DAGBuilder for Vans but keep DBR version

---
+++ dags/fintech/vans/spark_jobs/load_vans_into_datalake.py
@@ -0,0 +1,68 @@
+import json
+import logging
+from argparse import ArgumentParser
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.base.db import DatabaseEnum, DatalakeMetastoreService
+from bietlejuice.base.spark import BaseDBUtils, SparkTableStorageFormat
+from bietlejuice.clients.db_clients import SparkClient
+from bietlejuice.consumers.db_consumers import MySqlConsumer
+from bietlejuice.loaders import SparkMetastoreLoader
+from bietlejuice.loaders.s3_loader import S3Loader
+from bietlejuice.services.metastore_services import SparkMetastoreService
+
+JOB_NAME = "load_vans_into_datalake"
+BLOCK_LIST = ["schema_migrations", "Boleto_Status_Log"]
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("env")
+    parser.add_argument("datalake_bucket")
+    args = parser.parse_args()
+    environment = args.env
+    datalake_bucket = args.datalake_bucket
+    source = "vans"
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    conn_config_json = dbutils.secrets.get(scope="quintoandar", key=DatabaseEnum.VANS)
+    conn_config = json.loads(conn_config_json)
+    spark_client = SparkClient()
+    mysql_consumer = MySqlConsumer(conn_config, spark_client)
+
+    tables = mysql_consumer.get_table_names_and_sizes().collect()
+    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
+    metastore_service = SparkMetastoreService(spark_client)
+    s3_loader = S3Loader()
+    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+
+    # create database if not exists
+    database_name = db_info["db_raw_databricks"]
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = db_info["db_raw_path"]
+    metastore_service.create_database(database_name)
+
+    for table in tables:
+        if table.table_name not in BLOCK_LIST:
+            df = mysql_consumer.get_data_from_table(table.table_name)
+            # the table names in the datalake must be lowercase
+            s3_loader.load_df(
+                df=df,
+                s3_path=f"{database_location}{table.table_name.lower()}",
+                format_options=format_options,
+                database_location=database_location,
+                max_records_per_file=100000,
+            )
+            spark_metastore_loader.update_metastore(
+                df,
+                database_name,
+                table.table_name.lower(),
+                format_options,
+                database_location,
+            )
