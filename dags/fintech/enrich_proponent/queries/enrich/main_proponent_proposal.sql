98b852cb23f876bd28a7a37aaa0d7ae530ed8830
Bianca Garcia <biigarciam@gmail.com>
2024-08-06 10:01:13 -0300

    [DFFRS-1070] Delete enrich_proponent  and doubtful_debtors_provision  tables (#15346)
    * delete enrich_proponent
    
    * delete dw_datamarts.doubtful_debtors_provision
    
    * remove dw_datamart from dependency file
    
    * remove dw_datamart from manual dependency file
    
    ---------
    
    Co-authored-by: bianca.martins <bianca.martins@quintoandar.com.br>

--- dags/fintech/enrich_proponent/queries/enrich/main_proponent_proposal.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    id_proponent,
-    id_proposal,
-    cpf,
-    id_proponent_docx,
-    id_proponent_sorting_hat,
-    id_proponent_ebdb
-FROM datalake_proponent.proponent 
-WHERE is_main_proponent = TRUE
