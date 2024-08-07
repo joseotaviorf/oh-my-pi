ec111b8fb1ebe7b1266d7522e47bc5815082a7a5
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2024-08-07 15:05:36 -0300

    [PEOPLE] Update clean performance goals (#15457)
    * add columns to performance goals
    
    * fix query

--- dags/people/hr_system/queries/clean/performance_goals.sql
+++ dags/people/hr_system/queries/clean/performance_goals.sql
@@ -1,15 +1,36 @@
-SELECT 
-  GoalId AS id_goal,
-  PersonId AS id_person,
-  PersonNumber AS person_number,
-  AssignmentId AS id_assignment,
-  GoalName AS goal_name,
-  description,
-  associatedGoalPlans AS associated_goal_plans,
-  status,
-  StatusMeaning AS status_meaning,
-  FLOAT(PercentComplete) AS percent_complete,
-  to_date(StartDate) AS dt_start,
-  to_date(TargetCompletionDate) AS dt_target_completion,
-  ts_load
-FROM datalake_hr_system_raw.performance_goals
+WITH goal_plans_cte AS (
+  SELECT
+    GoalId,
+    explode(associatedGoalPlans) AS associatedGoalPlans
+  FROM
+    datalake_hr_system_raw.performance_goals
+)
+
+SELECT
+  pg.GoalId AS id_goal,
+  pg.PersonId AS id_person,
+  gp.associatedGoalPlans.GoalPlanGoalId AS id_Goal_Plan_Goal,
+  gp.associatedGoalPlans.ReviewPeriodId AS id_Review_Period,
+  gp.associatedGoalPlans.GoalPlanId AS id_Goal_Plan,
+  pg.AssignmentId AS id_assignment,
+  pg.PersonNumber AS person_number,
+  pg.GoalName AS goal_name,
+  pg.description,
+  pg.associatedGoalPlans AS associated_goal_plans,
+  gp.associatedGoalPlans.PriorityMeaning AS Priority_Meaning,
+  gp.associatedGoalPlans.ReviewPeriod AS Review_Period,
+  gp.associatedGoalPlans.Priority AS Priority,
+  gp.associatedGoalPlans.GoalPlanName AS Goal_Plan_Name,
+  gp.associatedGoalPlans.ReviewPeriodName AS Review_Period_Name,
+  pg.status,
+  pg.StatusMeaning AS status_meaning,
+  gp.associatedGoalPlans.Weight AS Weight,
+  FLOAT(pg.PercentComplete) AS percent_complete,
+  to_date(pg.StartDate) AS dt_start,
+  to_date(pg.TargetCompletionDate) AS dt_target_completion,
+  pg.ts_load
+FROM
+  datalake_hr_system_raw.performance_goals AS pg
+LEFT JOIN
+  goal_plans_cte gp
+    ON pg.GoalId = pg.GoalId
