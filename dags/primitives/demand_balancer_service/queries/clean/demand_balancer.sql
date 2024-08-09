17157cf6497d454b3f43218f30f64c5584ca298c
Thaysfsil <36057756+Thaysfsil@users.noreply.github.com>
2024-08-09 09:51:38 +0100

    add maestro service to bi-etl-ejuice (#15424)
    * add maestro service to bi-etl-ejuice
    
    * fix dag declaration
    
    * update cluster
    
    * fix variable
    
    * adjust source
    
    * adjust argument inputs
    
    * fix declaration
    
    * gits fix dag declaration
    
    * update folder
    
    * adjust table name
    
    * fix query
    
    * fix: add demand balancer
    
    * fix dag
    
    * adjust the origin data

---
+++ dags/primitives/demand_balancer_service/queries/clean/demand_balancer.sql
@@ -0,0 +1,11 @@
+select
+  id_house,
+  timestamp_available,
+  prediction,
+  model_version,
+  year,
+  month,
+  day
+from
+  datalake_search_raw.demand_balancer
+where year={year} AND month={month} AND day={day}
