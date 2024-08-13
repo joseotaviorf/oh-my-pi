b8b659f5a6e527f720b6370b96a04ee42f974763
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-08-13 09:51:41 -0300

    [GROW-FIX] Removing Wololo validation from services integration (#15519)
    * fix: removing wololo validation from services integration
    
    * Trigger Build

--- bietlejuice/validation_suites/databases/postgres_validation_suite.py
+++ bietlejuice/validation_suites/databases/postgres_validation_suite.py
@@ -237,12 +237,6 @@ class PostgresValidationSuite(DatabaseValidationSuitesExecutor):
         )
         self._validate_connection(db_consumer)
 
-    def validate_wololo(self):
-        db_consumer = self.REPOSITORY_CONSUMER_CLASS(
-            self.auth[DatabaseEnum.WOLOLO], SparkClient()
-        )
-        self._validate_connection(db_consumer)
-
     def validate_rene_descartes(self):
         db_consumer = self.REPOSITORY_CONSUMER_CLASS(
             self.auth[DatabaseEnum.RENE_DESCARTES], SparkClient()
