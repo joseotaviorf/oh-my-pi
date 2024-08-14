8aa63f0c166a32d0e846dfee3047045036ee0962
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-08-14 13:35:01 -0300

    [DFORSALE-8082] - Fix to_date  metric fintech (#15542)

--- dags/fintech/metric_fintech__atta/queries/metric/financed_deals.sql
+++ dags/fintech/metric_fintech__atta/queries/metric/financed_deals.sql
@@ -1,5 +1,5 @@
 SELECT
-    ts_financing_ended,
+    TO_DATE(ts_financing_ended) AS dt_financing_contract,
     COUNT(DISTINCT sk_offer) AS offer,
     COUNT(DISTINCT sk_pre_analysis) AS pre_analysis,
     COUNT(DISTINCT sk_proposal) AS proposals
@@ -7,5 +7,6 @@ FROM
   dw_atta.fact_pre_analysis_proposal_flow
 WHERE
     ts_financing_ended IS NOT NULL
+    AND ts_financing_ended >= '2000-01-01'
 GROUP BY
     1
