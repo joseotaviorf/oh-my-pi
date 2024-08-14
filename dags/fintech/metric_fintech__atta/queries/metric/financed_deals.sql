34223e10ccbb57762a8556dd05436222349d9da3
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-08-14 10:38:27 -0300

    [DFORSALE-8082] - Fix timestamp metric fintech (#15540)
    * [DFORSALE-8082] - Fix timestamp metric fintech
    
    * Identation

--- dags/fintech/metric_fintech__atta/queries/metric/financed_deals.sql
+++ dags/fintech/metric_fintech__atta/queries/metric/financed_deals.sql
@@ -1,9 +1,11 @@
 SELECT
-    date_trunc('day',ts_financing_ended) AS dt_financing_contract,
+    ts_financing_ended,
     COUNT(DISTINCT sk_offer) AS offer,
     COUNT(DISTINCT sk_pre_analysis) AS pre_analysis,
     COUNT(DISTINCT sk_proposal) AS proposals
 FROM
   dw_atta.fact_pre_analysis_proposal_flow
-WHERE ts_financing_ended IS NOT NULL
-GROUP BY 1
+WHERE
+    ts_financing_ended IS NOT NULL
+GROUP BY
+    1
