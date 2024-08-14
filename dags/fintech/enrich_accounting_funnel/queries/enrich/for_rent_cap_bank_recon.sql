e0164cfc8b90fb7ee2681a936faad3e17ddc3e4c
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2024-08-14 09:46:18 -0300

    [AEFF] Removed company_use treatment in cap bank recon model (#15539)
    * removed company_use treatment
    
    * remove treatment from cap too

--- dags/fintech/enrich_accounting_funnel/queries/enrich/for_rent_cap_bank_recon.sql
+++ dags/fintech/enrich_accounting_funnel/queries/enrich/for_rent_cap_bank_recon.sql
@@ -1,9 +1,6 @@
 WITH francesinha AS (
     SELECT
-        UPPER(CASE
-            WHEN company_use RLIKE '[0-9]P[0-9]' AND LENGTH(TRIM(company_use)) = 11 AND SUBSTRING(company_use, 1, 1) = '0' THEN SUBSTRING(REGEXP_REPLACE(SUBSTRING(REPLACE(company_use, '|', '!'), 1, LENGTH(company_use) - 2), 'P.', 'P'), 2, LENGTH(company_use))
-            ELSE REPLACE(company_use, '|', '!')
-        END) AS company_use,
+        UPPER(REPLACE(company_use, '|', '!')) AS company_use,
         our_number,
         bank_account,
         dt_paid,
@@ -22,10 +19,7 @@ WITH francesinha AS (
 cap AS (
     SELECT
         dt_paid,
-        UPPER(CASE
-            WHEN reference_1 RLIKE '[0-9]P[0-9]' AND LENGTH(TRIM(reference_1)) = 11 AND SUBSTRING(reference_1, 1, 1) = '0' THEN SUBSTRING(REGEXP_REPLACE(SUBSTRING(REPLACE(reference_1, '|', '!'), 1, LENGTH(reference_1) - 2), 'P.', 'P'), 2, LENGTH(reference_1))
-            ELSE REPLACE(reference_1, '|', '!')
-        END) AS company_use,
+        UPPER(REPLACE(reference_1, '|', '!')) AS company_use,
         payment_status,
         reference_3 AS payment_name,
         reference_4 AS payment_type,
