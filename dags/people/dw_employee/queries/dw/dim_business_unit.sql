a03a8edbe522d58c0a0b03eb105f784cb1cf39b2
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-08-19 17:10:37 -0300

    [DBP-549] 🐛 fix duplications in business unit (#15591)
    * 🐛 fix duplications in business unit
    
    * ✨ using the organizations table to get the name of BU

--- dags/people/dw_employee/queries/dw/dim_business_unit.sql
+++ dags/people/dw_employee/queries/dw/dim_business_unit.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
     a.id_business_unit AS sk_business_unit,
-    a.business_unit_name,
+    o.name AS business_unit_name,
     wr.legal_employer_name,
     wr.legislation_code,
     NOW() AS ts_load
@@ -9,3 +9,6 @@ FROM
 JOIN 
     datalake_hr_system.work_relationships AS wr 
         ON a.id_period_of_service = wr.id_period_of_service
+JOIN 
+    datalake_hr_system_clean.organizations AS o
+        ON a.id_business_unit = o.id_organization
