a2bca31cbc8128506d5412456a3c786c1b9febf3
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-08-21 15:25:44 -0300

    [DFORSALE-8080] - New Visit Table (#15614)
    * [DFORSALE-8080] - New Visit Table
    
    * Update dags/for_rent/ebdb/metadata/clean/visit_fitting.yml
    
    Co-authored-by: Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Allan Moura <145588415+allanmoura5A@users.noreply.github.com>

---
+++ dags/for_rent/ebdb/queries/clean/visit_fitting.sql
@@ -0,0 +1,19 @@
+SELECT
+    id,
+    fitted_visit_id AS id_visit,
+    visitor_id AS id_visitor,
+    house_id AS id_house,
+    begin_slot,
+    end_slot,
+    business_context,
+    intention,
+    status,
+    channel,
+    request_context,
+    visit_date AS dt_visit,
+    expiration_date AS ts_expiration,
+    criadoEm AS ts_created,
+    atualizadoEm AS ts_updated
+FROM
+    datalake_ebdb_raw.VisitFitting
+
