b06ea6ecce35f42e0bd16af434dccd5fad4afff6
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2024-08-27 13:59:13 -0300

    Updating rules on delinquency_timeline (#15683)

--- dags/fintech/enrich_velo/queries/enrich/delinquency_timeline.sql
+++ dags/fintech/enrich_velo/queries/enrich/delinquency_timeline.sql
@@ -1,4 +1,5 @@
-WITH cte_base AS (
+WITH
+  cte_base AS (
     SELECT DISTINCT
         d.id AS id_delinquency,
         del.id_propose,
@@ -18,7 +19,7 @@ WITH cte_base AS (
         d.value <= d.amount_paid AS is_finished,
         del.is_legacy_agreement,
         del.id_propose < 5000000 AS is_legacy_propose,
-        d.is_active AS is_currently_active,
+        del.is_active AS is_currently_active,
         d.dt_due,
         CASE
             WHEN d.dt_paid IS NOT NULL THEN d.dt_paid
@@ -28,8 +29,9 @@ WITH cte_base AS (
             ELSE d.dt_paid
         END AS dt_paid,
         p.dt_ended AS dt_ended_propose,
-        DATE(COALESCE(r.ts_created, d.dt_paid)) AS dt_updated,
-        DATE(del.ts_created) AS dt_created,
+        DATE(COALESCE(DATEADD(HOUR, -3, r.ts_created), d.dt_paid)) AS dt_updated,
+        IF(is_currently_active, DATE(COALESCE(r.ts_created, d.dt_paid)),DATE(del.ts_updated)) AS dt_updated_arq,
+        IF(DATE(del.dt_payment_scheduled) < DATE(del.ts_created), DATE(del.dt_payment_scheduled), DATE(del.ts_created)) AS dt_created,
         ROW_NUMBER() OVER (PARTITION BY d.id ORDER BY DATE(COALESCE(r.ts_created, d.dt_paid)) ASC) AS rn
     FROM
         datalake_rental_guarantee_platform_clean.delinquency_aud AS d
@@ -43,8 +45,8 @@ WITH cte_base AS (
         datalake_velo.propose AS p
             ON d.id_propose = p.id_propose
     WHERE
-        d.mod_amount_paid <> 0
-        OR d.rev_type = 0
+        (d.mod_amount_paid <> 0
+        OR d.rev_type = 0)
 ),
 cte_final AS (
     SELECT
@@ -65,6 +67,7 @@ cte_final AS (
             WHEN amount_paid > 0 AND dt_paid IS NULL AND (LAG(amount_paid) OVER (PARTITION BY id_delinquency ORDER BY dt_updated) = amount_paid) THEN LAG(dt_paid) IGNORE NULLS OVER (PARTITION BY id_delinquency ORDER BY dt_updated)
             ELSE dt_paid
         END AS dt_paid,
+        dt_updated_arq,
         dt_ended_propose,
         dt_updated,
         dt_created
@@ -94,20 +97,22 @@ base_timeline AS (
         dt_due,
         dt_paid,
         dt_ended_propose,
+        dt_updated_arq,
         MIN(dt_updated) AS dt_updated,
         dt_created
     FROM
         cte_final
-    GROUP BY 1,2,3,4,5,6,8,9,10,11,12,13,14,15,17
+    GROUP BY 1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,18
 ),
 delinquency_end AS (
     SELECT DISTINCT
         id_delinquency,
-        COALESCE(dt_paid, dt_updated) AS dt_updated
+        IF(is_currently_active, COALESCE(dt_paid, dt_updated), dt_updated_arq) AS dt_updated
     FROM
         base_timeline
     WHERE
-        is_finished
+        (is_finished
+        OR is_currently_active = false)
 ),
 timeline_base AS (
     SELECT
@@ -130,10 +135,10 @@ cte_timeline_daily AS (
         datalake_quintoandar.aux_date AS d
     LEFT JOIN
         timeline_base AS t
-            ON IF(t.is_finished, d.`date` >= t.dt_updated_paid AND d.`date` <= LAST_DAY(t.dt_updated_paid), d.`date` >= t.dt_updated_paid AND d.`date` < t.dt_delinquency_last_register)
+            ON IF(t.is_finished, d.`date` >= t.dt_updated_paid AND d.`date` <= LAST_DAY(t.dt_updated_paid), IF(t.is_currently_active = true, d.`date` >= t.dt_updated_paid AND (d.`date` <= t.dt_delinquency_last_register OR t.dt_delinquency_last_register IS NULL), d.`date` >= t.dt_created AND d.`date` <= t.dt_delinquency_last_register))
     WHERE
         d.`date` > DATE('2020-01-01')
-        AND d.`date` < CURRENT_DATE()
+        AND d.`date` <= CURRENT_DATE()
     GROUP BY 1,2,3
     ORDER BY 3,1
 ),
@@ -153,29 +158,38 @@ cte_timeline_status AS (
     WITH base_cte_status AS (
         SELECT
             a.id,
+            a.id_propose,
             LAST_VALUE(a.id_status) AS id_status,
-            DATE(r.ts_created)
+            r.ts_created,
+            a.dt_payment_scheduled,
+            DATEADD(HOUR, -3, r.ts_created) AS ts_created_local,
+            re.ts_created AS ts_ended,
+            IF(re.ts_created IS NULL AND a.id_status = 3, LAST_DAY(r.ts_created), re.ts_created) AS ts_ended_3
+
         FROM
             datalake_rental_guarantee_platform_clean.delinquency_aud a
         LEFT JOIN
             datalake_rental_guarantee_platform_clean.rev_info r
             ON a.rev = r.rev
-        WHERE a.mod_id_status = true
-        GROUP BY 1,3
+        LEFT JOIN
+            datalake_rental_guarantee_platform_clean.rev_info re
+            ON a.rev_end = re.rev
+        GROUP BY 1,2,4,5,6,7,8
+        QUALIFY
+            ROW_NUMBER() OVER(PARTITION BY a.id, DATE(r.ts_created) ORDER BY r.ts_created DESC) = 1
         )
     SELECT
         s.id,
-        LAST_VALUE(s.id_status) AS id_status,
+        s.id_status AS id_status,
         d.date AS dt_status_updated
     FROM
         datalake_quintoandar.aux_date AS d
     LEFT JOIN
         base_cte_status s
-        ON d.`date` >= DATE(s.ts_created)
+        ON IF( s.ts_ended IS NOT NULL AND s.ts_ended_3 IS NOT NULL, d.`date` >= IF(DATE(s.dt_payment_scheduled) < DATE(s.ts_created_local), DATE(s.dt_payment_scheduled), DATE(s.ts_created_local)) AND IF(s.ts_ended IS NOT NULL, d.`date` < DATE(s.ts_ended), d.`date` <= DATE(s.ts_ended_3)), d.`date` >= IF(DATE(s.dt_payment_scheduled) < DATE(s.ts_created_local), DATE(s.dt_payment_scheduled), DATE(s.ts_created_local)))
     WHERE
         d.`date` > DATE('2020-01-01')
-        AND d.`date` < CURRENT_DATE()
-    GROUP BY 1,3
+        AND d.`date` <= CURRENT_DATE()
 )
 SELECT
     t.id_delinquency,
@@ -185,7 +199,7 @@ SELECT
     MAX(t.delinquency_amount) AS delinquency_amount,
     MAX(t.original_value) AS original_value,
     MAX(t.amount_paid) AS amount_paid,
-    SUM(t.amount_paid_added_fixed) AS amount_paid_added,
+    MAX(t.amount_paid_added_fixed) AS amount_paid_added,
     MIN(t.open_amount) AS open_amount,
     FIRST_VALUE(MIN(t.open_amount)) OVER (PARTITION BY t.id_delinquency, YEAR(t.`date`), MONTH(t.`date`) ORDER BY t.`date`) AS open_amount_first_day_of_month,
     MAX(t.is_finished) As is_finished,
@@ -196,11 +210,12 @@ SELECT
     MAX(t.dt_ended_propose) AS dt_ended_propose,
     t.`date` AS dt_base,
     MAX(t.dt_paid) AS dt_paid,
-    MAX(t.dt_created) AS dt_created
+    MIN(t.dt_created) AS dt_created
 FROM
     timeline AS t
 LEFT JOIN
     cte_timeline_status s
     ON t.id_delinquency = s.id
-    AND t.`date` = DATE(s.dt_status_updated)
+    AND DATE(t.`date`) = DATE(s.dt_status_updated)
+WHERE s.id_status IS NOT NULL
 GROUP BY 1,2,3,17
