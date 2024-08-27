91f821c569260d1e4f9152110349009cc19a509d
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-08-27 11:22:29 -0300

    [DBP-484] Fix workers paralells (#15640)
    * ⚡️ v1 fix for partitions
    
    * 🐛 fix some erros in spark job for hr systems
    
    * ⚡️ final adjustments in spark for hr system
    
    * ⚡️ increase cluster size temp
    
    * 🐛 fix args reading of execeution_date
    
    * ⏪ revert changes in cluster config in forno
    
    * 🐛 fix bug in clean salaries
    
    * ⚰️ remove redundant configs
    
    * ⚰️ remove for that is not used
    
    * Empty Commit

--- dags/people/hr_system/spark_jobs/load_hr_system_raw.py
+++ dags/people/hr_system/spark_jobs/load_hr_system_raw.py
@@ -1,8 +1,7 @@
 import json
 import argparse
-import os
+from multiprocessing import Pool
 from datetime import datetime, timedelta
-from pyspark import Row
 from pyspark.sql.types import StructType
 from pyspark.sql.functions import current_timestamp, date_format, col, lit
 from quintoandar_logger import QuintoAndarLogger
@@ -22,120 +21,151 @@ JOB_NAME = "load_hr_system_raw"
 logger = QuintoAndarLogger(JOB_NAME)
 
 
-def create_spark_dataframe(endpoint_id, json_data, spark_client, hr_system_client):
-    spark_schema = EndPointService.get_spark_schema(endpoint_id, hr_system_client)
-    schema = StructType.fromJson(spark_schema)
-    return spark_client.create_dataframe(json_data, schema=schema)
-
-
-def run_sync(endpoint_id, url, token, endpoint_details, spark_client):
-    logger.info(f"m={JOB_NAME}, msg=Endpoint Details {endpoint_details}")
-    hr_system_client = HrSystemClient(api_url=url, api_token=token)
-    deduplication_key = endpoint_details.get("deduplication_key", None)
-    endpoint_id = endpoint_id.replace("_", "").upper()
-    consumer_instance = get_consumer(hr_system_client, endpoint_id)
-    path = consumer_instance.path
-    json_data = consumer_instance.sync(
-        params=endpoint_details["params"], deduplication_key=deduplication_key
-    )
-    return create_spark_dataframe(path, json_data, spark_client, hr_system_client)
-
-
-def insert_partitions(df, endpoint_details):
-    column_to_partition = endpoint_details["column_to_partition"]
-    df = df.withColumn("year", date_format(col(column_to_partition), "yyyy"))
-    df = df.withColumn("month", date_format(col(column_to_partition), "MM"))
-    df = df.withColumn("day", date_format(col(column_to_partition), "dd"))
-    return df
-
-
-def insert_columns(df, endpoint_details, has_dt_effective):
-    df = df.withColumn("ts_load", current_timestamp())
-    if has_dt_effective:
-        dt_effective = endpoint_details["params"]["effectiveDate"]
-        df = df.withColumn("dt_effective", lit(dt_effective.replace("-", "")))
-    return df
-
-
-def delete_columns(df, endpoint_details):
-    spark.conf.set("spark.sql.caseSensitive", True)
-    columns_to_delete = endpoint_details["columns_to_delete"]
-    for column in columns_to_delete:
-        df = df.drop(column)
-    spark.conf.set("spark.sql.caseSensitive", False)
-    return df
-
-
-def load_raw(
-    spark_client,
-    df,
-    environment,
-    source,
-    datalake_bucket,
-    endpoint_id,
-    partition_cols=None,
-):
-    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
-    database_name = db_info["db_raw_databricks"]
-    format_options = SparkTableStorageFormat.DEFAULT_RAW
-    database_location = db_info["db_raw_path"]
-    logger.info(
-        f"m={JOB_NAME}, msg=Creating database in Spark Metastore if not exists..."
-    )
-    metastore_service = SparkMetastoreService(spark_client)
-    metastore_service.create_database(database_name)
-    s3_loader = S3Loader()
-    s3_loader.load_df(
-        df=df,
-        s3_path=f"{database_location}{endpoint_id}",
-        format_options=format_options,
-        partitions=partition_cols,
-    )
-    spark_metastore_loader = SparkMetastoreLoader(metastore_service)
-    spark_metastore_loader.update_metastore(
-        df=df,
-        database_name=database_name,
-        table_name=endpoint_id,
-        format_options=format_options,
-        force_recreate=True,
-        database_location=database_location,
-        partitions=partition_cols,
-    )
-    metastore_service.refresh_table(database_name, endpoint_id)
-
-
-def pipeline_raw(
-    spark_client,
-    endpoint_id,
-    url,
-    token,
-    endpoint_details,
-    environment,
-    source,
-    datalake_bucket,
-    partition_cols,
-    has_dt_effective,
-):
-    df = run_sync(endpoint_id, url, token, endpoint_details, spark_client)
-    if endpoint_details["has_columns_to_delete"]:
-        df = delete_columns(df, endpoint_details)
-    df = insert_columns(df, endpoint_details, has_dt_effective)
-    if endpoint_details["has_partitions"]:
-        df = insert_partitions(df, endpoint_details)
-        load_raw(
-            spark_client,
-            df,
-            environment,
-            source,
-            datalake_bucket,
-            endpoint_id,
-            partition_cols,
+class Ingestion:
+    def __init__(self, args, url, token) -> None:
+        self.environment = args.environment
+        self.datalake_bucket = args.datalake_bucket
+        self.source = args.source
+        self.table_name = args.endpoint_id
+        self.endpoint_id = args.endpoint_id.replace("_", "").upper()
+        self.execution_date = datetime.strptime(args.execution_date, "%Y-%m-%d")
+        self.partition_cols = json.loads(args.partition_cols)
+        self.endpoint_details = self.treat_params(args.endpoint_details)
+        self.has_dt_effective = self.endpoint_details.get("has_dt_effective", False)
+        self.offsets = self.define_offset()
+        self.client = HrSystemClient(api_url=url, api_token=token)
+        self.consumer = get_consumer(self.client, self.endpoint_id)
+        self.deduplication_key = self.endpoint_details.get("deduplication_key", None)
+        self.has_columns_to_delete = self.endpoint_details.get(
+            "has_columns_to_delete", None
         )
-    else:
-        load_raw(spark_client, df, environment, source, datalake_bucket, endpoint_id)
+        self.columns_to_delete = self.endpoint_details.get("columns_to_delete", None)
+        self.column_to_partition = self.endpoint_details.get(
+            "column_to_partition", None
+        )
+
+    def treat_params(self, endpoint_details):
+        endpoint_details = json.loads(endpoint_details)
+        endpoint_details["params"] = json.loads(endpoint_details["params"])
+        return endpoint_details
+
+    def define_offset(self):
+        offsets_number = {
+            (self.endpoint_details.get("future_offset", None), 1),
+            (self.endpoint_details.get("past_offset", None), -1),
+        }
+        offsets = [self.execution_date]
+        for offset in offsets_number:
+            if offset[0] is not None:
+                days_offset = int(offset[0]) * offset[1]
+                offset_date = self.execution_date + timedelta(days=days_offset)
+                offsets.append(offset_date)
+        return offsets
+
+    def get_data_from_api(self, list_ingestion_tables):
+        arguments = list()
+        for ingestion_table in list_ingestion_tables:
+            aux_arguments = (ingestion_table, self.deduplication_key)
+            arguments.append(aux_arguments)
+        with Pool(len(arguments)) as p:
+            json_data = p.starmap(run_sync, arguments)
+        return json_data
+
+    def create_spark_dataframe(self, dt_data):
+        spark_schema = EndPointService.get_spark_schema(self.consumer.path, self.client)
+        schema = StructType.fromJson(spark_schema)
+        return spark_client.create_dataframe(dt_data, schema=schema)
+
+    def get_df(self, json_data):
+        dfs = list()
+        for data in json_data:
+            for dt_effective in data:
+                dt_data = data[dt_effective]
+                df = self.create_spark_dataframe(dt_data)
+                df = self.delete_columns(df)
+                df = self.insert_columns(df, dt_effective)
+                df = self.insert_partitions(df)
+                dfs.append(df)
+        return dfs
+
+    def delete_columns(self, df):
+        if self.columns_to_delete:
+            spark.conf.set("spark.sql.caseSensitive", True)
+            for column in self.columns_to_delete:
+                df = df.drop(column)
+            spark.conf.set("spark.sql.caseSensitive", False)
+            return df
+        return df
+
+    def insert_columns(self, df, dt_effective=None):
+        df = df.withColumn("ts_load", current_timestamp())
+        if self.has_dt_effective:
+            df = df.withColumn("dt_effective", lit(dt_effective.replace("-", "")))
+        return df
+
+    def insert_partitions(self, df):
+        if self.partition_cols:
+            df = df.withColumn(
+                "year", date_format(col(self.column_to_partition), "yyyy")
+            )
+            df = df.withColumn(
+                "month", date_format(col(self.column_to_partition), "MM")
+            )
+            df = df.withColumn("day", date_format(col(self.column_to_partition), "dd"))
+            return df
+        return df
+
+    def load_raw(self, df, spark_client):
+        db_info = DatalakeMetastoreService.get_db_info(
+            self.environment, self.source, self.datalake_bucket
+        )
+        database_name = db_info["db_raw_databricks"]
+        format_options = SparkTableStorageFormat.DEFAULT_RAW
+        database_location = db_info["db_raw_path"]
+        logger.info(
+            f"m={JOB_NAME}, msg=Creating database in Spark Metastore if not exists..."
+        )
+        metastore_service = SparkMetastoreService(spark_client)
+        metastore_service.create_database(database_name)
+        s3_loader = S3Loader()
+        s3_loader.load_df(
+            df=df,
+            s3_path=f"{database_location}{self.table_name}",
+            format_options=format_options,
+            partitions=self.partition_cols,
+        )
+        spark_metastore_loader = SparkMetastoreLoader(metastore_service)
+        spark_metastore_loader.update_metastore(
+            df=df,
+            database_name=database_name,
+            table_name=self.table_name,
+            format_options=format_options,
+            force_recreate=True,
+            database_location=database_location,
+            partitions=self.partition_cols,
+        )
+        metastore_service.refresh_table(database_name, self.table_name)
+
+    def clear_directory(self, layer):
+        if self.has_dt_effective == True:
+            dbutils.fs.rm(
+                f"s3://{self.datalake_bucket}/{layer}/{self.source}/{self.table_name}/",
+                True,
+            )
 
 
-def get_parser():
+class IngestionTable:
+    def __init__(self, ingestion: Ingestion, dt_effective: datetime) -> None:
+        self.ingestion = ingestion
+        self.dt_effective = dt_effective.strftime("%Y-%m-%d")
+        self.table_name = ingestion.endpoint_id
+        self.params = ingestion.endpoint_details["params"]
+        if ingestion.has_dt_effective:
+            self.params["effectiveDate"] = self.dt_effective
+        self.consumer = get_consumer(ingestion.client, ingestion.endpoint_id)
+
+
+def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description=JOB_NAME)
     parser.add_argument("environment", help="forno/prod values")
     parser.add_argument("datalake_bucket", help="bucket value in forno/prod")
@@ -147,56 +177,12 @@ def get_parser():
     return parser
 
 
-def define_offset(endpoint_details, offset, execution_date):
-    days_offset = int(offset[0]) * offset[1]
-    offset_date = execution_date + timedelta(days=days_offset)
-    offset_date_str = offset_date.strftime("%Y-%m-%d")
-    offset_details = endpoint_details.copy()
-    offset_details["params"] = endpoint_details["params"].copy()
-    offset_details["params"]["effectiveDate"] = offset_date_str
-    return offset_date_str, offset_details
-
-
-def clear_directory(
-    datalake_bucket, source, endpoint_id, has_dt_effective, layer
-):
-    if has_dt_effective == True:
-        dbutils.fs.rm(
-            f"s3://{datalake_bucket}/{layer}/{source}/{endpoint_id}/",
-            True
-        )
-
-
-def main():
-
-    parser = get_parser()
-    args = parser.parse_args()
-    environment = args.environment
-    datalake_bucket = args.datalake_bucket
-    source = args.source
-    endpoint_id = args.endpoint_id
-    execution_date_str = args.execution_date
-    execution_date = datetime.strptime(execution_date_str, "%Y-%m-%d")
-
-    if not isinstance(args.endpoint_details, dict):
-        endpoint_details = json.loads(args.endpoint_details)
-
-    if not isinstance(endpoint_details["params"], dict):
-        endpoint_details["params"] = json.loads(endpoint_details["params"])
-
-    if "expand" in endpoint_details:
-        if not isinstance(endpoint_details["params"]["expand"], list):
-            endpoint_details["params"]["expand"] = json.loads(endpoint_details["params"]["expand"])
-
-    partition_cols = json.loads(args.partition_cols)
-    has_dt_effective = endpoint_details.get("has_dt_effective", False)
-    offsets = {
-        (endpoint_details.get("future_offset", None), 1),
-        (endpoint_details.get("past_offset", None), -1),
-    }
+def get_conn_config() -> tuple[str, str]:
+    """Returns the connection configuration from Databricks Secrets."""
 
     base_dbutils = BaseDBUtils()
     if base_dbutils.get_dbutils() is not None:
+        global dbutils
         dbutils = base_dbutils.get_dbutils()
     json_credentials = dbutils.secrets.get(
         scope=DATABRICKS_SCOPE, key=APIEnum.HR_SYSTEM
@@ -205,47 +191,38 @@ def main():
     url = credentials["url"]
     token = credentials["token"]
 
-    spark_client = SparkClient()
+    return url, token
+
+
+def run_sync(ingestion_table, deduplication_key):
     logger.info(
-        f"m={JOB_NAME}, environment={environment}, source={source}, datalake_bucket={datalake_bucket}, "
-        f"table_name={endpoint_id}, msg=Starting spark job..."
+        f"m={JOB_NAME}, msg=Endpoint Details {ingestion_table.ingestion.endpoint_details}"
     )
-
-    if has_dt_effective == True:
-        endpoint_details["params"]["effectiveDate"] = execution_date_str
-    list_endpoint_details = {execution_date_str: endpoint_details}
-
-    for offset in offsets:
-        if offset[0] is not None:
-            offset_date_str, offset_details = define_offset(
-                endpoint_details, offset, execution_date
-            )
-            list_endpoint_details[offset_date_str] = offset_details
-
-    clear_directory(
-        datalake_bucket, source, endpoint_id, has_dt_effective, "raw"
-    )
-    for execution_date_str, endpoint_details in list_endpoint_details.items():
-        logger.info(
-            f"m={JOB_NAME}, environment={environment}, source={source}, datalake_bucket={datalake_bucket}, "
-            f"table_name={endpoint_id}, dt_effective={execution_date_str} msg=Getting data from API..."
+    return {
+        ingestion_table.dt_effective: ingestion_table.consumer.sync(
+            params=ingestion_table.params, deduplication_key=deduplication_key
         )
-        pipeline_raw(
-            spark_client,
-            endpoint_id,
-            url,
-            token,
-            endpoint_details,
-            environment,
-            source,
-            datalake_bucket,
-            partition_cols,
-            has_dt_effective,
-        )
-    clear_directory(
-        datalake_bucket, source, endpoint_id, has_dt_effective, "clean"
-    )
+    }
 
 
 if __name__ == "__main__":
-    main()
+    parser = get_parser()
+    args = parser.parse_args()
+    url, token = get_conn_config()
+    ingestion = Ingestion(args, url, token)
+    spark_client = SparkClient()
+    logger.info(
+        f"m={JOB_NAME}, environment={ingestion.environment}, source={ingestion.source}, datalake_bucket={ingestion.datalake_bucket}, "
+        f"table_name={ingestion.endpoint_id}, msg=Starting spark job..."
+    )
+    list_ingestion_tables = list()
+    for offset in ingestion.offsets:
+        ingestion_table = IngestionTable(ingestion, offset)
+        list_ingestion_tables.append(ingestion_table)
+    json_data = ingestion.get_data_from_api(list_ingestion_tables)
+    dfs = ingestion.get_df(json_data)
+    ingestion.clear_directory("raw")
+    for df in dfs:
+        ingestion.load_raw(df, spark_client)
+    ingestion.clear_directory("clean")
+    logger.info(f"m={JOB_NAME}, msg=Spark job finished.")
