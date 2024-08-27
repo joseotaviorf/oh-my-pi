9fa32d0d9a1c07e3b84f97eca56874bcc1c0453a
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2024-08-27 16:11:46 -0300

    Remove cast and coalesce to fix datatype (#15689)

--- dags/support_and_service/ebdb_inspections/queries/clean/conservation_item.sql
+++ dags/support_and_service/ebdb_inspections/queries/clean/conservation_item.sql
@@ -3,7 +3,7 @@ SELECT
     CAST(roomId AS BIGINT) AS id_room,
     itemType AS item_type,
     itemGroup AS item_group,
-    CAST(COALESCE(present, FALSE) AS BOOLEAN) AS is_present,
+    present AS is_present,
     CAST(criadoEm AS TIMESTAMP) AS ts_created,
     CAST(atualizadoEm AS TIMESTAMP) AS ts_updated
 FROM 
