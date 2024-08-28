36a6a1819f70d6f9d7f031d4733e1820db4cf7a8
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2024-08-28 11:06:24 -0300

    [DAC] Fix partitions (#15702)
    * fix partition
    
    * fix extraction type
    
    * fix partitions

--- dags/governance/enrich_trino_costs/queries/enrich/trino_costs_per_line.sql
+++ dags/governance/enrich_trino_costs/queries/enrich/trino_costs_per_line.sql
@@ -45,7 +45,9 @@ SELECT
     pdlpd.total_processed_input_data_gbytes,
     tpcpd.total_cost_in_dollars * (pdlpd.total_processed_input_data_gbytes / SUM(pdlpd.total_processed_input_data_gbytes) OVER (PARTITION BY pdlpd.created_date)) AS proportional_cost_in_dollars,
     pdlpd.created_date AS dt_created,
-    d.year
+    d.year,
+    d.month,
+    d.day
 FROM
     processed_data_per_line_per_day AS pdlpd
 LEFT JOIN
