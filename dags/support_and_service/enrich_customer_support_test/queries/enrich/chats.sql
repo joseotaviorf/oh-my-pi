2005bacd988d5cd2b78ef9af789f4ec877df2896
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-08-29 17:43:35 -0300

    [AESS] Fix inner_dependencies and query on enrich_customer_support_test (#15731)
    * fix errors
    
    * fix declaration

--- dags/support_and_service/enrich_customer_support_test/queries/enrich/chats.sql
+++ dags/support_and_service/enrich_customer_support_test/queries/enrich/chats.sql
@@ -86,8 +86,8 @@ SELECT
   COALESCE(ss.user_phone, t.from_phone_number) AS customer_phone_number,
   t.twilio_phone_number,
   CASE
-    WHEN ias.id_session THEN 'IN APP'
-    WHEN ws.id_session THEN 'WHATSAPP'
+    WHEN ias.id_session IS NOT NULL THEN 'IN APP'
+    WHEN ws.id_session IS NOT NULL THEN 'WHATSAPP'
   END AS source,
   t.worker_email,
   t.channel_type,
