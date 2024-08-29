09f876034e994f175a711d17a8db031190a9491e
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2024-08-29 12:11:29 -0300

    Revert "Revert "[AEFR-4264] Update Facebook source and dependent tables (#143…" (#15420)
    This reverts commit 21cf7c9099c65cee75d68a86f4d6185d86f237a9.

--- dags/growth/enrich_consolidated_growth_metrics/queries/enrich/facebook_consolidated_metrics.sql
+++ dags/growth/enrich_consolidated_growth_metrics/queries/enrich/facebook_consolidated_metrics.sql
@@ -2,6 +2,7 @@ SELECT
     INT(year*10000 + month*100 + day) AS id_date,
     campaign_name,
     account_name_snake_case AS account_name,
+    country_code,
     campaign_name AS utm_campaign,
     adset_name AS utm_term,
     ad_name AS utm_content,
@@ -15,4 +16,4 @@ FROM
     datalake_facebook_insights_clean.facebook_insights
 WHERE
     dt_start BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-GROUP BY 1,2,3,4,5,6
+GROUP BY 1,2,3,4,5,6,7
