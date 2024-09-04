4ca47a90f6a3dd382cb2005e764536776b7b2265
Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
2024-09-04 12:06:20 -0300

    [DFORSALE-8087] Adjust params - ciq_portfolio (#15789)
    * [DFORSALE-8087] Adjust params
    
    * [DFORSALE-8087] Adjust sql

--- dags/agents/gsheets_agents/queries/clean/ciq_portfolio.sql
+++ dags/agents/gsheets_agents/queries/clean/ciq_portfolio.sql
@@ -1,7 +1,10 @@
 SELECT
-  CAST(ciq_id AS LONG) AS id_partner,
-  carteira AS portfolio_user_name,
-  cluster AS cluster_name,
-  ts_load AS ts_created
+    CAST(id_partner AS BIGINT) AS id_partner,
+    carteira_vinculada AS portfolio_user_name,
+    cluster AS cluster_name,
+    ts_load AS ts_created,
+    YEAR(ts_load) AS year,
+    MONTH(ts_load) AS month,
+    DAY(ts_load) AS day
 FROM
-  datalake_gsheets_raw.ciq_portfolio
+    datalake_gsheets_raw.ciq_portfolio
