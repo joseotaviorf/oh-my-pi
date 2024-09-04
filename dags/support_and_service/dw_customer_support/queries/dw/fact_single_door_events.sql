3a5d8cadc5d0a86b39435380098191a0ae449a59
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-09-04 17:59:27 -0300

    [AESS-3872] New fact table with Single Door events (#15766)
    * feat: new single door events table
    
    * fix md5

---
+++ dags/support_and_service/dw_customer_support/queries/dw/fact_single_door_events.sql
@@ -0,0 +1,57 @@
+SELECT
+  MD5(CAST(id_amplitude AS STRING)) AS sk_event,
+  id_user AS sk_user,
+  CASE
+    WHEN event_type = 'ongoing_requests_timeline_page_viewed' THEN 'ONGOING REQUESTS'
+    WHEN event_type = 'faq_article_page_viewed' THEN 'FAQ'
+    WHEN event_type IN ('owner_income_tax_shortcut_tapped', 'owner_urban_property_tax_shortcut_tapped') THEN 'SHORTCUT CENTER SEASONAL'
+    ELSE 'SHORTCUT CENTER'
+  END AS event_from,
+  event_type,
+  CASE
+    WHEN id_app = 170698 THEN 'TENANT APP'
+    WHEN id_app = 183047 THEN 'LANDLORD APP'
+  END AS app,
+  city,
+  region,
+  country,
+  device_carrier,
+  device_family,
+  device_type,
+  os_name,
+  os_version,
+  version_name,
+  ts_event,
+  year,
+  month,
+  day
+FROM
+  datalake_amplitude_clean.events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
+  AND id_user NOT IN ('false', 'userId')
+  AND id_user IS NOT NULL
+  AND id_app IN (170698, 183047)
+  AND event_type IN (
+    'help_center_page_viewed',
+    'faq_article_page_viewed',
+    'ongoing_requests_timeline_page_viewed',
+    'tenant_repair_request_shortcut_tapped',
+    'tenant_simulate_termination_fee_shortcut_tapped',
+    'tenant_termination_request_shortcut_tapped',
+    'last_invoice_shortcut_tapped',
+    'tenant_refund_request_shortcut_tapped',
+    'tenant_termination_management_shortcut_tapped',
+    'request_sign_shortcut_tapped',
+    'request_new_photos_shortcut_tapped',
+    'request_lockbox_shortcut_tapped',
+    'owners_how_to_improve_ad_shortcut_tapped',
+    'owners_visit_schedule_shortcut_tapped',
+    'owners_edit_ad_shortcut_tapped',
+    'owner_send_documents_shortcut_tapped',
+    'owner_report_non_payment_shortcut_tapped',
+    'owner_inspection_report_shortcut_tapped',
+    'menu_transfers_shortcut_tapped',
+    'owner_income_tax_shortcut_tapped',
+    'owner_urban_property_tax_shortcut_tapped'
+  )
