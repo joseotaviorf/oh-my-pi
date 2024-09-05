93ba4c1b2d029894235ba9b4de44bde8f8183351
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-09-05 15:40:45 -0300

    [AESS-3795] Ticket SLA data on `enrich_customer_support_test` (#15807)
    * feat: sla metrics on tickets table
    
    * docs: metadata file

--- dags/support_and_service/enrich_customer_support_test/queries/enrich/tickets.sql
+++ dags/support_and_service/enrich_customer_support_test/queries/enrich/tickets.sql
@@ -1,6 +1,24 @@
-WITH incoming_tickets AS (
+WITH weekends_and_holidays AS (
+  SELECT
+    ad.date AS dt_non_working
+  FROM
+    datalake_quintoandar.aux_date AS ad
+  WHERE
+    ad.weekend = 'Weekend'
+  UNION
+  SELECT
+    sch.dt_holiday AS dt_non_working
+  FROM
+    datalake_gsheets_clean.service_city_holidays AS sch
+  WHERE
+    sch.category = 'Nacional'
+),
+incoming_tickets AS (
   SELECT
     id_ticket,
+    id_problem_ticket,
+    id_user_main,
+    id_contract,
     id_call,
     id_session,
     group_name AS ticket_queue,
@@ -28,10 +46,10 @@ WITH incoming_tickets AS (
     reopens,
     replies,
     CAST(dt_budgeted AS TIMESTAMP) AS ts_budget,
-    ts_created,
-    ts_solved,
-    ts_closed,
-    ts_updated,
+    ts_created - INTERVAL 3 HOUR AS ts_created,
+    ts_solved - INTERVAL 3 HOUR AS ts_solved,
+    ts_closed - INTERVAL 3 HOUR AS ts_closed,
+    ts_updated - INTERVAL 3 HOUR AS ts_updated,
     year,
     month,
     day
@@ -40,7 +58,7 @@ WITH incoming_tickets AS (
   WHERE
     MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
 ),
-dedup_twilio_tickets AS (
+unique_twilio_tickets AS (
   SELECT
     it.id_session,
     'chat' AS channel,
@@ -68,7 +86,7 @@ dedup_tickets AS (
     id_ticket,
     channel
   FROM
-    dedup_twilio_tickets
+    unique_twilio_tickets
   UNION ALL
   SELECT DISTINCT
     it.id_ticket,
@@ -90,7 +108,7 @@ dedup_tickets AS (
   FROM
     incoming_tickets AS it
   LEFT JOIN
-    dedup_twilio_tickets AS dtt
+    unique_twilio_tickets AS dtt
       ON dtt.id_ticket = it.id_ticket
   WHERE
     dtt.id_ticket IS NULL
@@ -98,6 +116,9 @@ dedup_tickets AS (
 unique_tickets AS (
   SELECT
     dt.id_ticket,
+    t.id_problem_ticket,
+    t.id_user_main,
+    t.id_contract,
     t.id_call,
     t.id_session,
     COALESCE(ch.queue_name, ca.queue_name, t.ticket_queue) AS queue,
@@ -141,7 +162,7 @@ unique_tickets AS (
     datalake_customer_support_test.calls AS ca
       ON COALESCE(ca.id_call, ca.id_task) = t.id_call
 ),
-ticket_queue AS (
+ticket_queue_attributes AS (
   SELECT
     tq.id_ticket,
     FIRST(tq.queue) OVER (PARTITION BY tq.id_ticket ORDER BY tq.ts_created) AS first_queue,
@@ -156,9 +177,12 @@ ticket_queue AS (
     datalake_gsheets_clean.department_control dc
       ON dc.department = tq.queue
 ),
-tickets_with_queue AS (
+tickets AS (
   SELECT
     t.id_ticket,
+    t.id_problem_ticket,
+    t.id_user_main,
+    t.id_contract,
     t.id_call,
     t.id_session,
     tq.first_queue,
@@ -208,7 +232,7 @@ tickets_with_queue AS (
   FROM
     unique_tickets AS t
   LEFT JOIN
-    ticket_queue AS tq
+    ticket_queue_attributes AS tq
       ON tq.id_ticket = t.id_ticket
 ),
 back_tickets AS (
@@ -228,13 +252,13 @@ back_tickets AS (
     bt.ts_created,
     bt.ts_solved
   FROM
-    tickets_with_queue AS bt
+    tickets AS bt
   LEFT JOIN
-    tickets_with_queue AS ft_chat
+    tickets AS ft_chat
       ON ft_chat.id_session = bt.id_session
       AND ft_chat.is_back_ticket IS FALSE
   LEFT JOIN
-    tickets_with_queue AS ft_call
+    tickets AS ft_call
       ON ft_call.task_sid_twilio = bt.task_sid_twilio
       AND ft_call.is_back_ticket IS FALSE
   WHERE
@@ -244,16 +268,18 @@ back_ticket_timestamps AS (
   SELECT
     id_front_ticket,
     ARRAY_AGG(id_back_ticket) AS back_ticket_list,
-    SUM(CAST(status IN ('closed','deleted','solved') AS SMALLINT)) - COUNT(DISTINCT(id_back_ticket)) != 0 AS has_open_back_ticket,
+    SUM(
+      CAST(status IN ('closed', 'deleted', 'solved') AS SMALLINT)
+    ) - COUNT(DISTINCT(id_back_ticket)) != 0 AS has_open_back_ticket,
     CAST((TO_UNIX_TIMESTAMP(MIN(ts_created)) - TO_UNIX_TIMESTAMP(MAX(ts_solved))) / 60.0 AS DOUBLE) AS total_backoffice_minutes_time
   FROM
     back_tickets
   GROUP BY 1
 ),
 /*
-TODO: the following 'sla' CTEs are a replica of the ones in datalake_customer_demand.base_tasks and were built with a
-full load perspective on the table. They should be adapted in order to take full advantage of the MERGE operation and
-update SLA incrementally - this means removing the EXPLODE(SEQUENCE(dt_start, COALESCE(dt_end, DATE(NOW())))) part
+TODO: the following 'sla' CTEs are a replica of the ones in datalake_customer_demand.base_tasks and should be
+revisited, ideally performing array operations on the date columns instead of JOINing on the result of
+EXPLODE(SEQUENCE())
 */
 unique_theme_detail_sla_target AS (
   SELECT DISTINCT
@@ -306,8 +332,8 @@ unique_journey_sla_target AS (
     exploded_journey_taxonomy
   GROUP BY 1,3
 ),
-ticket_sla AS (
-  SELECT
+ticket_sla_target AS (
+  SELECT /*+ RANGE_JOIN(t, 500) */
     t.id_ticket,
     MIN(
       CASE
@@ -315,9 +341,17 @@ ticket_sla AS (
         ELSE COALESCE(tst.sla, tds.sla_in_days, ts.sla_in_days, ujst.sla_in_days)
       END
     ) AS sla_target,
-    t.ts_budget
+    MAX(
+      CASE
+        WHEN t.ts_budget IS NOT NULL
+          AND t.ts_budget >= ts_created - INTERVAL 3 HOUR
+          AND t.ts_budget < COALESCE(ts_solved, NOW()) THEN t.ts_budget
+        ELSE t.ts_created
+      END
+    ) AS ts_sla_started,
+    ts_solved
   FROM
-    tickets_with_queue AS t
+    tickets AS t
   LEFT JOIN
     datalake_gsheets_clean.tag_sla_target AS tst
       ON t.journey_step = tst.journey
@@ -344,12 +378,45 @@ ticket_sla AS (
         t.last_queue = 'Offboarding Reparos [OFF] [POS] [BACK]'
         AND t.tags LIKE '%orçamentação_realizada%'
       )
-      OR t.last_queue <> 'Offboarding Reparos [OFF] [POS] [BACK]'
+      OR t.last_queue != 'Offboarding Reparos [OFF] [POS] [BACK]'
     )
   GROUP BY ALL
+),
+ticket_date_interval AS (
+  SELECT
+    id_ticket,
+    sla_target,
+    SEQUENCE(DATE(ts_sla_started), COALESCE(DATE(ts_solved), CURRENT_DATE())) AS dt_interval,
+    ts_sla_started,
+    ts_solved
+  FROM
+    ticket_sla_target
+),
+ticket_days_off AS (
+  SELECT
+    id_ticket,
+    sla_target,
+    ARRAY_SIZE(ARRAY_INTERSECT(dt_interval, (SELECT ARRAY_AGG(dt_non_working) FROM weekends_and_holidays))) AS days_off,
+    ts_sla_started,
+    ts_solved
+  FROM
+    ticket_date_interval
+),
+ticket_days_worked AS (
+  SELECT
+    id_ticket,
+    sla_target,
+    days_off,
+    DATEDIFF(DATE(ts_solved), DATE(ts_sla_started)) - COALESCE(days_off, 0) AS days_worked,
+    ts_sla_started
+  FROM
+    ticket_days_off
 )
 SELECT DISTINCT
   t.id_ticket,
+  t.id_problem_ticket,
+  t.id_user_main,
+  t.id_contract,
   t.id_call,
   t.id_session,
   t.first_queue,
@@ -371,12 +438,32 @@ SELECT DISTINCT
   t.replies,
   btt.back_ticket_list,
   btt.total_backoffice_minutes_time,
-  ts.sla_target,
+  tdw.sla_target,
+  IF(tdw.days_worked < 0, 0, tdw.days_worked) AS days_worked,
+  IF(tdw.days_off < 0, 0, tdw.days_off) AS days_off,
+  CASE
+    WHEN IF(tdw.days_worked < 0, 0, tdw.days_worked) <= sla_target THEN IF(tdw.days_worked < 0, 0, tdw.days_worked)
+    ELSE 0
+  END AS time_spent_solved_in_time,
+  CASE
+    WHEN IF(tdw.days_worked < 0, 0, tdw.days_worked) > sla_target THEN IF(tdw.days_worked < 0, 0, tdw.days_worked)
+    ELSE 0
+  END AS time_spent_not_solved_in_time,
+  CASE
+    WHEN IF(tdw.days_worked < 0, 0, tdw.days_worked) <= sla_target THEN TRUE
+    ELSE FALSE
+  END AS is_ticket_solved_in_time,
+  CASE
+    WHEN IF(tdw.days_worked < 0, 0, tdw.days_worked) <= sla_target
+      OR ISNULL(tdw.days_worked) THEN FALSE
+    ELSE TRUE
+  END AS is_ticket_not_solved_in_time,
   btt.has_open_back_ticket,
   t.tags LIKE '%closed_by_merge%' AS is_closed_by_merge,
   t.is_call_answered,
-  ts.ts_budget,
+  t.ts_budget,
   t.ts_created,
+  tdw.ts_sla_started,
   t.ts_solved,
   t.ts_closed,
   t.ts_updated,
@@ -384,10 +471,10 @@ SELECT DISTINCT
   t.month,
   t.day
 FROM
-  tickets_with_queue AS t
+  tickets AS t
 LEFT JOIN
   back_ticket_timestamps AS btt
     ON btt.id_front_ticket = t.id_ticket
 LEFT JOIN
-  ticket_sla AS ts
-    ON ts.id_ticket = t.id_ticket
+  ticket_days_worked AS tdw
+    ON tdw.id_ticket = t.id_ticket
