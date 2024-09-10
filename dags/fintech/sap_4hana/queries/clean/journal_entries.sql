7211012686f4b01ad8d9a3327172e6ce508c332d
Athos Sampayo <67998510+athossampayo@users.noreply.github.com>
2024-09-10 17:17:01 -0300

    [DFF-231]Fix clean query params #15863

--- dags/fintech/sap_4hana/queries/clean/journal_entries.sql
+++ dags/fintech/sap_4hana/queries/clean/journal_entries.sql
@@ -41,7 +41,4 @@ SELECT
 FROM
     datalake_sap_4hana_raw.journal_entries
 WHERE
-    year = {year}
-    AND month = {month}
-    AND day = {day}
-
+    MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
