6752b5f58a76134acbd2ac8e6893c3095599886d
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2024-09-17 10:43:02 -0300

    [AE-AGENTS] Removing unused EBDB tables from the context of Agents (#15891)
    * delete tables
    
    * add tables in block list
    
    * empty commit

--- dags/for_rent/ebdb/queries/clean/agent_region_data.sql
+++
@@ -1,5 +0,0 @@
-select
-    DadosAgente_id as id_agent_data,
-    regioes_id as id_regions
-from
-    datalake_ebdb_raw.dadosagente_regiao
--- dags/for_rent/ebdb/queries/clean/real_estate_agency_lead.sql
+++
@@ -1,8 +0,0 @@
-select
-    id,
-    nome as name,
-    criadoEm as dt_created,
-    telefone as phone,
-    telefonesecundario as second_phone
-from
-    datalake_ebdb_raw.leadimobiliaria
