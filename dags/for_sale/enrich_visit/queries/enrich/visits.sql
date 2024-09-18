5db4809f5abf72ec509dd6f9a2736b02946a7a69
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-09-18 13:12:20 -0300

    [DFORSALE-8197] - Tirando Dependência da Booking (#15944)
    * Update fact_visit_events.sql
    
    * Update visits.sql
    
    * Update visit_status_events.sql
    
    * Update visit_status_events.sql
    
    * Update dependencies.yaml
    
    * Update dependencies.yaml
    
    * Update dependencies.yaml
    
    * Ajustando lineage
    
    * Ajustando lineage
    
    * Ajustando data quality

--- dags/for_sale/enrich_visit/queries/enrich/visits.sql
+++ dags/for_sale/enrich_visit/queries/enrich/visits.sql
@@ -58,16 +58,16 @@ SELECT
     visit.id_agent,
     visit.id_visitor,
     lh.id_user AS id_owner,
-    b.id_house,
+    visit.id_house,
     hl.id_house_listing,
-    b.id_rent_flow,
-    IF(b.business_context = 'SALE', CONCAT(b.id_visitor, '_', b.id_house), NULL) AS id_sale_flow,
+    '' AS id_rent_flow,
+    '' AS id_sale_flow,
     lh.id_company_hubspot AS id_company_supply,
     lh.uuid_company AS uuid_company_supply,
     visit_demand.id_company_demand,
     visit_cancellation.id AS id_cancellation_detail,
-    fup.id AS id_follow_up,
-    entrance.id AS id_entrance_type,
+    '' AS id_follow_up,
+    '' AS id_entrance_type,
     visit.code,
     hl.country_code,
     lh.partner_3p_supply,
@@ -83,7 +83,7 @@ SELECT
         ELSE visit_log.last_event
     END AS last_event,
     visit.slot,
-    b.business_context,
+    '' AS business_context,
     COALESCE(ct.default_timezone, 'UTC') AS default_timezone,
     visit_cancellation.reason AS cancellation_reason,
     visit_cancellation.on_behalf_of AS cancellation_on_behalf_of,
@@ -97,14 +97,7 @@ SELECT
     visit.status = 'Canceled' AS is_canceled,
     visit.computed_status = 'UNSUCCESSFUL' AS is_unsuccessful,
     IF(visit_demand.id_visit IS NOT NULL, TRUE, FALSE) AS is_3p_demand,
-    COALESCE(
-            (lh.is_sale_3p_supply
-             AND b.business_context = 'SALE'
-            )
-             OR
-            (lh.is_rent_3p_supply
-             AND b.business_context = 'RENT'
-            ), FALSE) AS is_3p_supply,
+    '' AS is_3p_supply,
     visit.is_fixed_agent,
     CASE
         WHEN visit_log.ts_visit_tenant_answer IS NOT NULL
@@ -112,10 +105,7 @@ SELECT
         ELSE FALSE
     END AS has_tenant_living,
     visit_log.last_event = 'ANSWER_PENDING' AS is_waiting_for_response,
-    CASE
-        WHEN entrance.is_successful THEN FALSE
-        ELSE TRUE
-    END AS has_entrance_problem,
+    '' AS has_entrance_problem,
     visit_log.ts_visit_supply_answer IS NOT NULL AS has_supply_answered,
     visit_log.ts_visit_supply_confirmed IS NOT NULL AS has_supply_confirmed,
     visit_log.ts_visit_tenant_answer IS NOT NULL AS has_tenant_answered,
@@ -151,17 +141,14 @@ INNER JOIN
     visit_log
         ON visit.id = visit_log.id_visit
 LEFT JOIN
-    datalake_ebdb_clean.booking AS b
-        ON visit_log.last_id_schedule = b.id
-LEFT JOIN
     datalake_ebdb_listing.house AS lh
-        ON lh.id = b.id_house
+        ON lh.id = visit.id_house
 LEFT JOIN
     datalake_ebdb_listing.house_listing AS hl
-        ON b.id_house = hl.id_house
-        AND b.ts_created >= hl.ts_listing_version_start
+        ON visit.id_house = hl.id_house
+        AND visit.ts_created >= hl.ts_listing_version_start
         AND (
-            b.ts_created <= hl.ts_listing_version_end
+            visit.ts_created <= hl.ts_listing_version_end
             OR hl.ts_listing_version_end IS NULL
         )
 LEFT JOIN
@@ -173,12 +160,6 @@ LEFT JOIN
 LEFT JOIN
     datalake_ebdb_clean.country AS ct
         ON ct.code = hl.country_code
-LEFT JOIN
-    datalake_ebdb_clean.follow_up_details AS fup
-        ON b.id_fup_details = fup.id
-LEFT JOIN
-    datalake_ebdb_clean.entrance AS entrance
-        ON fup.id_entrance = entrance.id
 WHERE
     visit.structured IS TRUE
     AND visit.ts_created >= '2024-04-01'
