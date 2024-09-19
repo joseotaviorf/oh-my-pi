2275ebe4427fe0a02c877fb843562c6fbc38814c
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-09-19 14:06:19 -0300

    [DEI-7705] - Arrumando dependencia booking (#15973)
    * [DEI-7705] - Tirando dependencias booking enrich
    
    * Tirando dependencia bookink dw
    
    * dependencies

--- dags/for_sale/enrich_visit/queries/enrich/visit_status_events.sql
+++ dags/for_sale/enrich_visit/queries/enrich/visit_status_events.sql
@@ -22,22 +22,18 @@ vsl AS (
   FROM
     datalake_ebdb_clean.visit_status_log
 ),
-booking AS(
+visit AS(
   SELECT
-    vsl.id_schedule,
-    b.id_visitor,
-    b.id_agent,
-    b.id_house,
-    b.id_rent_flow,
-    IF(b.business_context = 'SALE',
-    CONCAT(b.id_visitor, '_', b.id_house),
-    NULL
-    ) AS id_sale_flow,
-    b.id_fup_details,
-    b.business_context,
-    b.ts_created,
-    vsl.id_visit_status_log,
     vsl.id_visit,
+    v.id_visitor,
+    v.id_agent,
+    v.id_house,
+    NULL AS id_rent_flow,
+    NULL AS id_sale_flow,
+    NULL AS id_fup_details,
+    NULL AS business_context,
+    v.ts_created,
+    vsl.id_visit_status_log,
     vsl.id_author_user,
     vsl.id_schedule,
     vsl.event_type,
@@ -56,33 +52,33 @@ booking AS(
     hl.id_house_listing,
     hl.country_code
   FROM
-    datalake_ebdb_clean.booking AS b
+    datalake_ebdb_clean.visit AS v
   INNER JOIN
     vsl
-      ON  b.id = vsl.id_schedule
+      ON  v.id = vsl.id_visit
   INNER JOIN
     datalake_ebdb_listing.house lh
-      ON lh.id = b.id_house
+      ON lh.id = v.id_house
   LEFT JOIN
     datalake_ebdb_clean.visit_cancellation_details AS vcd
       ON vsl.id_visit = vcd.id_visit
   INNER JOIN
     datalake_ebdb_listing.house_listing AS hl
-      ON b.id_house = hl.id_house
-      AND b.ts_created >= hl.ts_listing_version_start
-      AND (b.ts_created <= hl.ts_listing_version_end OR hl.ts_listing_version_end IS NULL)
+      ON v.id_house = hl.id_house
+      AND v.ts_created >= hl.ts_listing_version_start
+      AND (v.ts_created <= hl.ts_listing_version_end OR hl.ts_listing_version_end IS NULL)
 ),
 booking_3p_demand_agent AS (
     SELECT
-        b.id_schedule,
+        v.id_visit,
         wc.id_company_hubspot AS id_company_demand,
         wc.3p_partner AS partner_3p_demand
     FROM
          datalake_ebdb_agents.agent_contract AS ac
     JOIN
-        booking AS b
-            ON b.ts_created BETWEEN ac.ts_work_contract_started AND COALESCE(ac.ts_work_contract_ended, CURRENT_TIMESTAMP)
-            AND ac.id_agent = b.id_agent
+        visit AS v
+            ON v.ts_created BETWEEN ac.ts_work_contract_started AND COALESCE(ac.ts_work_contract_ended, CURRENT_TIMESTAMP)
+            AND ac.id_agent = v.id_agent
     JOIN
         datalake_ebdb_work_contract.work_contract AS wc
             ON wc.id = ac.id_work_contract
@@ -91,37 +87,37 @@ booking_3p_demand_agent AS (
     GROUP BY 1, 2, 3
 )
 SELECT
-  CONCAT(b.id_visit_status_log,'R',ranking) AS id_visit_status_events,
-  b.id_visit_status_log,
-  b.id_visit,
-  b.id_schedule,
-  b.id_visitor,
-  b.id_owner,
-  b.id_agent,
-  b.id_author_user,
-  b.id_house,
-  b.id_house_listing,
-  b.id_rent_flow,
-  b.id_sale_flow,
-  b.id_fup_details,
-  b.id_cancellation_detail,
-  b.id_company_supply,
-  b.uuid_company_supply,
+  CONCAT(v.id_visit_status_log,'R',ranking) AS id_visit_status_events,
+  v.id_visit_status_log,
+  v.id_visit,
+  v.id_schedule,
+  v.id_visitor,
+  v.id_owner,
+  v.id_agent,
+  v.id_author_user,
+  v.id_house,
+  v.id_house_listing,
+  v.id_rent_flow,
+  v.id_sale_flow,
+  v.id_fup_details,
+  v.id_cancellation_detail,
+  v.id_company_supply,
+  v.uuid_company_supply,
   b3da.id_company_demand,
-  b.partner_3p_supply,
+  v.partner_3p_supply,
   b3da.partner_3p_demand,
-  b.business_context,
-  b.event_type,
-  b.ranking,
-  b.author_user_type,
-  b.author_user_role,
-  b.on_behalf_of,
-  b.channel,
-  b.country_code,
-  b.is_visit_last_event,
-  b.ts_event_created
+  v.business_context,
+  v.event_type,
+  v.ranking,
+  v.author_user_type,
+  v.author_user_role,
+  v.on_behalf_of,
+  v.channel,
+  v.country_code,
+  v.is_visit_last_event,
+  v.ts_event_created
 FROM
-   booking AS b
+   visit AS v
 LEFT JOIN
   booking_3p_demand_agent AS b3da
-    ON b.id_schedule = b3da.id_schedule
+    ON v.id_visit = b3da.id_visit
