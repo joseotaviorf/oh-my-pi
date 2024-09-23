d8479b50eb61c73fc10ac958a5526ebf58d9790a
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2024-09-23 09:37:56 -0300

    add query (#15991)

--- dags/people/dw_employee/queries/dw/fact_assignment_events.sql
+++ dags/people/dw_employee/queries/dw/fact_assignment_events.sql
@@ -12,3 +12,20 @@ FROM
 LEFT JOIN
   datalake_hr_system.employee_ids AS ei
     ON s.id_assignment = ei.id_assignment
+
+UNION ALL
+
+SELECT
+  a.id_person AS sk_employee,
+  a.id_period_of_service AS sk_assignment,
+  COALESCE(CAST(DATE_FORMAT(a.dt_effective_start, "yyyyMMdd") AS BIGINT), -1) AS sk_started_date,
+  COALESCE(CAST(DATE_FORMAT(a.dt_effective_end, "yyyyMMdd") AS BIGINT), -1) AS sk_ended_date,
+  'ASSIGNMENT_CHANGE' AS event_type,
+  UPPER(a.assignment_name) AS event_value,
+  UPPER(a.action_code) AS action_reason,
+  NULL AS currency_code
+FROM
+  datalake_hr_system.assignments AS a
+QUALIFY
+  LEAD(UPPER(a.assignment_name))
+  OVER (PARTITION BY a.id_period_of_service ORDER BY a.dt_effective_start) <> UPPER(a.assignment_name)
