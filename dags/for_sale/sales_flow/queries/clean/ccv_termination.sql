eed58fe865c4c2e781cf9213ff173da18f8b516b
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-09-23 17:23:52 -0300

    [DFORSALE-8128] - New Table Sales Flow (#16005)
    * [DFORSALE-8128] - New Table Sales Flow
    
    * Update ccv_termination.yml
    
    * Update ccv_termination.yml
    
    * Update ccv_termination.sql

---
+++ dags/for_sale/sales_flow/queries/clean/ccv_termination.sql
@@ -0,0 +1,21 @@
+SELECT
+    id AS id_ccv_termination,
+    sales_flow_id AS id_sales_flow,
+    condition,
+    reason,
+    document_token,
+    payer_type,
+    payment_value,
+    approval_email,
+    zendesk_link,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_sales_flow_raw.ccv_termination
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
