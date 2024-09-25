ccd3cf54104a77da4c8da8cecad28f4042190523
Bianca Garcia <biigarciam@gmail.com>
2024-09-25 20:10:56 -0300

    [DFFRS-1185][DFFRS-1187] Webhelp - update incremental tables (#16057)
    * update historical_customers and customer_occurrences
    
    * fix historical_customers
    
    ---------
    
    Co-authored-by: bianca.martins <bianca.martins@quintoandar.com.br>

--- dags/fintech/webhelp/queries/clean/customer_occurrences.sql
+++ dags/fintech/webhelp/queries/clean/customer_occurrences.sql
@@ -1,8 +1,14 @@
 SELECT
     cod_ocor AS id_occurrence,
     cod_gru AS id_group_occurrence,
-    estagio_ocor AS id_occurrence_stage,
     titulo_ocor AS occurrence_name,
+    estagio AS stage,
+    alo,
+    cpc,
+    cpca,
+    promessa,
+    year,
+    month,
+    day,
     NOW() AS ts_load
 FROM datalake_webhelp_raw.ocorrencias_clientes
-QUALIFY ROW_NUMBER() OVER(PARTITION BY cod_ocor ORDER BY subfolder DESC) = 1
--- dags/fintech/webhelp/queries/clean/historical_customers.sql
+++ dags/fintech/webhelp/queries/clean/historical_customers.sql
@@ -3,8 +3,8 @@ SELECT
     cod_tit AS id_contract,
     cod_dev AS id_debtor,
     cod_ocor AS id_occurrence,
+    titulo_ocor AS occurrence,
     usuario_cad AS id_operator,
-    cod_hist_cli_mat AS id_service_digital_agent,
     complemento_hist_cli AS extra_information_occurrence,
     data_cad AS ts_service,
     dt_agen_hist AS ts_scheduling,
@@ -15,4 +15,3 @@ SELECT
 FROM datalake_webhelp_raw.historicos_clientes
 WHERE
     MAKE_DATE(year,month,day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-QUALIFY ROW_NUMBER() OVER(PARTITION BY cod_hist_cli ORDER BY subfolder DESC) = 1
