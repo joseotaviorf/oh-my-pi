fa55a7adf7c8cf1a8977d030ef4f8b5e9b07e8d9
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-10-01 15:53:23 -0300

    [AESS] Fix `dim_ra_ticket` (#16129)
    * fix: syntax error
    
    * fix: query errors

--- dags/support_and_service/dw_customer_support/queries/dw/dim_ra_ticket.sql
+++ dags/support_and_service/dw_customer_support/queries/dw/dim_ra_ticket.sql
@@ -1,15 +1,15 @@
 SELECT
-    CAST(id_hugme AS BIGINT) AS sk_ra_ticket,
+    CAST(id AS BIGINT) AS sk_ra_ticket,
     ra_status["name"] AS ra_status,
     hugme_status["name"] AS hugme_status,
     source["name"] AS source,
     complaint_title AS title,
-    customer_data["cpf"] AS customer_cpf,
-    REPLACE(customer_data["city"], "name:") AS customer_city,
-    REPLACE(customer_data["state"], "name:") AS customer_state,
-    customer_data["email"] AS customer_email,
-    customer_data["phone_numbers"] AS customer_phone_numbers,
-    customer_data["tags"] AS customer_tags,
+    customer_data:["cpf"] AS customer_cpf,
+    REPLACE(customer_data:["city"], "name:") AS customer_city,
+    REPLACE(customer_data:["state"], "name:") AS customer_state,
+    customer_data:["email"] AS customer_email,
+    customer_data:["phone_numbers"] AS customer_phone_numbers,
+    customer_data:["tags"] AS customer_tags,
     moderation["status"] AS moderation_status,
     moderation["reason"] AS moderation_reason,
     ticket_moderations_count AS total_moderations,
@@ -18,7 +18,7 @@ SELECT
         ELSE rating
     END AS rating,
     rating_without_response AS total_ratings_without_response,
-    is_resolved,
+    is_resolved_issue AS is_resolved,
     would_do_business_again,
     ts_created,
     ts_last_replica,
@@ -30,4 +30,4 @@ WHERE
     MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
 -- this QUALIFY enables backfills as id_hugme is not unique on clean
 QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY id_hugme ORDER BY ts_last_modification DESC) = 1
+    ROW_NUMBER() OVER(PARTITION BY id ORDER BY ts_last_modification DESC) = 1
