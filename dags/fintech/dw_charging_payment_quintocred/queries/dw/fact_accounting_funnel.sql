e9346c0c147dc03a6f3113021a11e87d77a5305f
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-10-02 16:24:27 -0300

    Aeff 2426 (#16156)
    * AEFF-2426 add new tables
    
    * update declaration file
    
    * add new table

---
+++ dags/fintech/dw_charging_payment_quintocred/queries/dw/fact_accounting_funnel.sql
@@ -0,0 +1,253 @@
+WITH 
+propose AS (
+    SELECT 
+        id, 
+        billing_model 
+    FROM 
+        datalake_rental_guarantee_platform_clean.propose
+),
+direct_billing AS (
+    SELECT 
+        e.propose AS id_business_entity,
+        br.dt_due, 
+        br.status AS payment_status,
+        cast( br.id AS varchar(10) ) AS id_finance_entity, 
+        e.amount AS source_amount
+    FROM 
+        datalake_rental_guarantee_platform_clean.billing_report br
+    LEFT JOIN 
+        datalake_rental_guarantee_platform_clean.entry e 
+        ON br.id = e.id_billing_report
+    LEFT JOIN 
+        propose p 
+        ON e.propose = p.id
+    WHERE
+        p.billing_model = 'BROKER'
+    QUALIFY
+        ROW_NUMBER() OVER ( 
+            PARTITION BY e.propose , br.dt_due 
+            ORDER BY br.ts_updated DESC
+        ) = 1
+),
+payment_no_ws AS (
+    SELECT 
+        CAST( p.id AS VARCHAR(10) ) AS id_finance_entity,
+        p.id_propose AS id_business_entity,
+        'rental guarantee platform' AS source_name,
+        p.gateway,
+        p.billing_type,
+        p.status AS payment_status,
+        COALESCE( DATE( p.ts_due ), DATE( p.ts_created ) ) as due_date,
+        p.value AS source_amount,
+        p.product_type AS revenue_name
+    FROM 
+        datalake_rental_guarantee_platform_clean.payment p 
+    LEFT JOIN propose pp 
+        ON p.id_propose = pp.id
+    WHERE
+    ( p.gateway != 'WALLSTREET' OR p.billing_type = 'ANNUAL_CREDIT_CARD')
+    AND p.product_type IN ( 'GUARANTEE', 'ACTIVATION')
+    QUALIFY
+        ROW_NUMBER() OVER (
+            PARTITION BY p.id_propose, COALESCE( date_trunc( 'MONTH', DATE( p.ts_due ) ), date_trunc('MOMTH', date( p.ts_created ) ) ) ORDER BY p.ts_updated DESC
+        ) = 1
+),
+payment_ws AS (
+    SELECT 
+        COALESCE( p.unicid, CAST( p.id AS VARCHAR(10) ) ) AS id_finance_entity,
+        p.id_propose AS id_business_entity,
+        'rental guarantee platform' AS source_name,
+        gateway,
+        billing_type,
+        p.status AS payment_status,
+        ts_due AS due_date,
+        p.value AS source_amount,
+        p.product_type AS revenue_name
+    FROM 
+        datalake_rental_guarantee_platform_clean.payment p 
+    LEFT JOIN 
+        propose pp ON p.id_propose = pp.id
+    WHERE 
+    p.gateway = 'WALLSTREET'
+    AND p.product_type IN ('GUARANTEE', 'ACTIVATION')
+    AND p.billing_type <> 'ANNUAL_CREDIT_CARD'
+    QUALIFY 
+        ROW_NUMBER() OVER (
+            PARTITION BY p.id_propose, date_trunc( 'MONTH', DATE( p.ts_due ) ) 
+            ORDER BY p.ts_updated DESC
+        ) = 1
+    ),
+recurrency_delinquency AS (
+    SELECT 
+        * 
+    FROM 
+        datalake_rental_guarantee_platform_clean.delinquency
+    WHERE
+        id_type IN (0,4)
+),
+sap_feature AS (
+    SELECT 
+        f.id_feature,
+        f.id_business_entity,
+        f.id_finance_entity,
+        f.id_source,
+        f.sync_sap_status AS feature_sync_status,
+        f.request_payload,
+        ssj.sap_payload,
+        ssj.status AS sync_sap_status,
+        DATE( ssj.ts_synced ) AS dt_sync,
+        hash,
+        ssj.type
+    FROM 
+        datalake_sap_gateway_clean.sync_sap_job ssj
+    LEFT JOIN 
+        datalake_sap_gateway_clean.feature f 
+        ON f.id_feature = ssj.id_feature
+    WHERE
+    f.source = 'rental-guarantee-platform'
+    AND ssj.status = 'done'
+    AND ssj.type = 'NF'
+),
+sap_ledger AS (
+    SELECT 
+        * 
+    FROM 
+        datalake_accounting_funnel.ledger
+    WHERE 
+        account_number IN ('31101.07.07', '31102.01.02')
+),
+accounting_funnel_no_ws AS (
+    SELECT 
+        pnws.id_business_entity,
+        pnws.id_finance_entity,
+        CAST( NULL AS VARCHAR(20) ) AS id_finance_entity_entry,
+        sf.hash,
+        l.id_document,
+        pnws.source_name,
+        'revenue accounting' AS accounting_type,
+        'Serviços Prestados - Velo' AS accounting_name,
+        sf.sync_sap_status AS status,
+        pnws.billing_type AS source_billing_type,
+        pnws.payment_status AS source_payment_status,
+        pnws.source_amount,
+        l.debit_credit AS sap_amount,
+        date_trunc('month',due_date) AS accrual_year_month,
+        l.accrual_year_month AS accrual_year_month_sap,
+        pnws.due_date AS dt_source_trigger,
+        l.dt_reference AS dt_sap_reference,
+        l.dt_created AS dt_sap_created
+    FROM 
+        payment_no_ws pnws
+    LEFT JOIN 
+        sap_feature sf 
+        ON  CAST( pnws.id_business_entity AS VARCHAR(10) ) = sf.id_business_entity 
+        AND CAST( pnws.id_finance_entity  AS VARCHAR(10) ) = sf.id_finance_entity
+    LEFT JOIN sap_ledger l 
+        ON  sf.hash = l.hash 
+        AND l.account_number = '31101.07.07'
+),
+accounting_funnel_ws AS (
+    SELECT 
+        pws.id_business_entity,
+        pws.id_finance_entity,
+        CAST( NULL AS VARCHAR(20) ) AS id_finance_entity_entry,
+        sf.hash,
+        l.id_document,
+        pws.source_name,
+        'revenue accounting' AS accounting_type,
+        'Serviços Prestados - Velo' AS accounting_name,
+        sf.sync_sap_status AS status,
+        pws.billing_type AS source_billing_type,
+        pws.payment_status AS source_payment_status,
+        pws.source_amount,
+        l.debit_credit AS sap_amount,
+        date_trunc('month',pws.due_date) AS accrual_year_month,
+        l.accrual_year_month AS accrual_year_month_sap,
+        pws.due_date AS dt_source_trigger,
+        l.dt_reference AS dt_sap_reference,
+        l.dt_created AS dt_sap_created
+    FROM 
+        payment_ws pws
+    LEFT JOIN sap_feature sf 
+        ON  CAST(pws.id_business_entity AS VARCHAR(10)) = sf.id_business_entity 
+        AND CAST(pws.id_finance_entity  AS VARCHAR(10)) = sf.id_finance_entity
+    LEFT JOIN sap_ledger l 
+        ON  sf.hash = l.hash 
+        AND l.account_number = '31101.07.07'
+),
+accounting_funnel_billing AS (
+    SELECT 
+        db.id_business_entity,
+        db.id_finance_entity,
+        CAST( NULL AS VARCHAR(20) ) AS id_finance_entity_entry,
+        sf.hash,
+        l.id_document,
+        'rental guarantee platform - billing' AS source_name,
+        'revenue accounting' AS accounting_type,
+        'Serviços Prestados - Velo' AS accounting_name,
+        sf.sync_sap_status AS status,
+        'Billing Direto' AS source_billing_type,
+        db.payment_status AS source_payment_status,
+        db.source_amount,
+        l.debit_credit AS sap_amount,
+        date_trunc( 'MONTH', db.dt_due)  AS accrual_year_month,
+        l.accrual_year_month AS accrual_year_month_sap,
+        db.dt_due AS dt_source_trigger,
+        l.dt_reference AS dt_sap_reference,
+        l.dt_created AS dt_sap_created
+    FROM 
+        direct_billing db
+    LEFT JOIN 
+        sap_feature sf 
+        ON  CAST(db.id_business_entity AS VARCHAR(10)) = sf.id_business_entity 
+        AND CAST(db.id_finance_entity  AS VARCHAR(10)) = sf.id_finance_entity
+    LEFT JOIN 
+        sap_ledger l 
+        ON sf.hash = l.hash 
+        AND l.account_number = '31101.07.07'
+),
+accounting_funnel_qc AS (
+    SELECT 
+        * 
+    FROM 
+        accounting_funnel_billing
+
+    UNION ALL
+
+    SELECT 
+        * 
+    FROM 
+        accounting_funnel_no_ws
+
+    UNION ALL
+
+    SELECT 
+        * 
+    FROM 
+        accounting_funnel_ws
+)
+SELECT 
+    id_business_entity,
+    id_finance_entity,
+    id_finance_entity_entry,
+    hash,
+    id_document,
+    source_name,
+    accounting_type,
+    accounting_name,
+    status,
+    source_billing_type,
+    source_payment_status,
+    ROW_NUMBER() OVER(
+        PARTITION BY id_business_entity, accrual_year_month 
+        ORDER BY hash ASC NULLS LAST
+    ) AS rn_nf,
+    source_amount,
+    sap_amount,
+    accrual_year_month,
+    accrual_year_month_sap,
+    dt_source_trigger,
+    dt_sap_reference,
+    dt_sap_created
+FROM 
+    accounting_funnel_qc
---
+++ dags/fintech/dw_charging_payment_quintocred/queries/dw/fact_theoretical_payment_delinquency.sql
@@ -0,0 +1,86 @@
+SELECT
+  pl.sk_propose AS sk_propose_theoretical,
+  p.id_array_delinquency AS sk_array_delinquency, 
+  CASE 
+    WHEN p.gateway_payment IS NOT NULL
+      THEN p.sk_propose_charge
+    WHEN p.gateway_delinquency IS NOT NULL 
+    AND p.is_delinquency_active 
+      THEN p.sk_propose_charge
+    ELSE NULL
+  END AS sk_propose_charge,
+  p.sk_propose_payment,
+  p.sk_transaction,
+  p.sk_propose_delinquency,
+  p.sk_delinquency,
+  CASE 
+    WHEN pl.month_start IS NOT NULL 
+    AND COALESCE( p.gateway_payment, IF( p.is_delinquency_active, p.gateway_delinquency, NULL ) ) IS NULL 
+      THEN 'vivendo de graça'
+    WHEN COALESCE( p.gateway_payment, IF( p.is_delinquency_active, p.gateway_delinquency, NULL ) ) IS NOT NULL 
+      THEN COALESCE( p.gateway_payment, IF( p.is_delinquency_active, p.gateway_delinquency, NULL ) )
+  END AS gateway_final,
+  pl.annual_guarantee_renewal AS annual_timeline_renewal,
+  pl.annual_guarantee_propose_aud AS annual_timeline_propose_aud,
+  pl.annual_value_propose,
+  pl.status_at_ref,
+  pl.mob_of_life,
+  pl.mob_of_death,
+  p.status_payment,
+  IF( p.gateway_payment IS NULL, 'sem cobrança', p.gateway_payment) AS gateway_payment,
+  p.billing_type_payment,
+  p.status_delinquency,
+  p.gateway_delinquency,
+  p.value,
+  p.value_paid,
+  p.total_amount_delinquency,
+  p.delinquency_monthly_value,
+  p.delinquency_amount_paid_monthly,
+  p.total_paid_delinquency, 
+  p.open_amount_delinquency,
+  p.total_open_amount_delinquency,
+  p.discount_value_delinquency,
+  p.total_discount_value_delinquency,
+  p.is_delinquency_active,
+  date_trunc( 'MONTH', p.dt_due_payment ) < date_trunc( 'MONTH', p.dt_created_payment ) AS is_retroactive_charging,
+  p.is_overdue,
+  p.value BETWEEN 
+    pl.total_package_amount * 0.08 
+    AND pl.total_package_amount * 0.12 
+  AS is_payment_charge_correct_amount,
+  CASE
+    WHEN pl.monthly_guarantee_renewal > 0 
+    AND coalesce( p.gateway_payment, IF( p.is_delinquency_active, p.gateway_delinquency, NULL ) ) IS NOT NULL 
+      THEN TRUE
+    WHEN IF( p.is_delinquency_active, DATE( p.month_charge ), NULL ) IS NOT NULL
+      THEN FALSE
+  END AS is_charged_on_contract_period,
+  p.is_delinquency_charge_related,
+  pl.is_direct_billing,
+  CASE 
+    WHEN p.gateway_payment IS NOT NULL
+      THEN p.month_charge
+    WHEN p.gateway_delinquency IS NOT NULL
+    AND p.is_delinquency_active 
+      THEN p.month_charge
+    ELSE NULL
+  END AS month_charge,
+  pl.dt_contract_started,
+  pl.dt_ended,
+  pl.month_chargeble,
+  pl.monthly_guarantee_renewal AS monthly_timeline_renewal,
+  pl.monthly_guarantee_propose_aud AS monthly_timeline_propose_aud,
+  pl.monthly_value_propose,
+  pl.month_start AS month_propose_life,
+  DATE(p.month_due_original) AS month_due_original ,
+  DATE(p.month_delinquency_due) AS month_delinquency_due,
+  DATE(p.dt_paid_payment) AS dt_paid_payment,
+  p.dt_created_payment,
+  p.dt_due_payment,
+  DATE(p.dt_paid_delinquency) AS dt_paid_delinquency
+FROM 
+  dw_charging_payment_quintocred.fact_propose_timeline pl
+FULL OUTER JOIN 
+  dw_charging_payment_quintocred.fact_payment_delinquency p
+  ON pl.sk_propose = p.sk_propose_charge
+  AND pl.month_start = p.month_charge
