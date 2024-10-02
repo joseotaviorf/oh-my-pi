2463632c8e300d3d39bc9d4f5539703986eac4cf
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2024-10-02 17:40:12 -0300

    Update delinquency_timeline (#16161)

--- dags/fintech/enrich_velo/queries/enrich/delinquency_timeline.sql
+++ dags/fintech/enrich_velo/queries/enrich/delinquency_timeline.sql
@@ -3,6 +3,7 @@ WITH
     SELECT DISTINCT
         d.id AS id_delinquency,
         del.id_propose,
+        del.id_type,
         CASE
             WHEN del.id_type = 0 THEN 'SIGNATURE'
             WHEN del.id_type = 1 THEN 'GUARANTEE'
@@ -52,6 +53,7 @@ cte_final AS (
     SELECT
         id_delinquency,
         id_propose,
+        id_type,
         type_description,
         delinquency_amount,
         original_value,
@@ -84,6 +86,7 @@ base_timeline AS (
     SELECT
         id_delinquency,
         id_propose,
+        id_type,
         type_description,
         delinquency_amount,
         original_value,
@@ -102,7 +105,7 @@ base_timeline AS (
         dt_created
     FROM
         cte_final
-    GROUP BY 1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,18
+    GROUP BY 1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,19
 ),
 delinquency_end AS (
     SELECT DISTINCT
@@ -118,6 +121,11 @@ timeline_base AS (
     SELECT
         t.*,
         COALESCE(t.dt_paid, t.dt_updated) AS dt_updated_paid,
+        CASE
+            WHEN t.id_type IN (0,4,5) AND DATE(t.dt_created) >= DATE("2024-02-01") THEN t.dt_due
+            WHEN t.id_delinquency < 15 OR t.id_delinquency >= 5000000 THEN DATE(t.dt_created)
+            ELSE t.dt_due
+        END AS dt_fatura,
         de.dt_updated AS dt_delinquency_last_register
     FROM
         base_timeline AS t
@@ -130,16 +138,17 @@ cte_timeline_daily AS (
         d.`date`,
         d.month_start,
         t.id_delinquency,
+        t.dt_updated_arq,
         MAX(t.dt_updated_paid) AS dt_updated
     FROM
         datalake_quintoandar.aux_date AS d
     LEFT JOIN
         timeline_base AS t
-            ON IF(t.is_finished, d.`date` >= t.dt_updated_paid AND d.`date` <= LAST_DAY(t.dt_updated_paid), IF(t.is_currently_active = true, d.`date` >= LEAST(t.dt_updated_paid, t.dt_created, t.dt_due) AND (d.`date` <= t.dt_delinquency_last_register OR t.dt_delinquency_last_register IS NULL), d.`date` >= IF(t.dt_created > t.dt_due, t.dt_due, t.dt_created) AND d.`date` < t.dt_delinquency_last_register))
+            ON IF(t.is_finished, d.`date` >= t.dt_updated_paid AND d.`date` <= LAST_DAY(t.dt_updated_paid), IF(t.is_currently_active = true, d.`date` >= t.dt_fatura AND (d.`date` <= t.dt_delinquency_last_register OR t.dt_delinquency_last_register IS NULL), d.`date` >= IF(t.dt_created > t.dt_due, t.dt_due, t.dt_created) AND d.`date` <= t.dt_delinquency_last_register))
     WHERE
         d.`date` > DATE('2020-01-01')
         AND d.`date` <= CURRENT_DATE()
-    GROUP BY 1,2,3
+    GROUP BY 1,2,3,4
     ORDER BY 3,1
 ),
 timeline AS (
@@ -152,7 +161,7 @@ timeline AS (
     LEFT JOIN
         base_timeline AS t
             ON td.id_delinquency = t.id_delinquency
-            AND td.dt_updated = COALESCE(t.dt_paid, t.dt_updated)
+            AND COALESCE(td.dt_updated,t.dt_updated_arq) = COALESCE(t.dt_paid, t.dt_updated, t.dt_updated_arq)
 ),
 cte_timeline_status AS (
     WITH base_cte_status AS (
@@ -167,6 +176,11 @@ cte_timeline_status AS (
             a.dt_due,
             a.rev,
             a.rev_end,
+            CASE
+                WHEN a.id_type IN (0,4,5) AND DATE(a.ts_created) >= DATE("2024-02-01") THEN a.dt_due
+                WHEN a.id < 15 OR a.id >= 5000000 THEN DATE(a.ts_created)
+                ELSE a.dt_due
+            END AS dt_fatura,
             re.ts_created AS ts_ended,
             IF(re.ts_created IS NULL AND a.id_status = 3, LAST_DAY(r.ts_created), re.ts_created) AS ts_ended_3
         FROM
@@ -178,7 +192,7 @@ cte_timeline_status AS (
             datalake_rental_guarantee_platform_clean.rev_info re
             ON a.rev_end = re.rev
             AND NOT a.mod_id_propose <=> TRUE
-        GROUP BY 1,2,4,5,6,7,8,9,10,11
+        GROUP BY 1,2,4,5,6,7,8,9,10,11,12
         QUALIFY
             ROW_NUMBER() OVER(PARTITION BY a.id, DATE(r.ts_created) ORDER BY r.ts_created DESC) = 1
         )
@@ -195,7 +209,7 @@ cte_timeline_status AS (
         datalake_quintoandar.aux_date AS d
     LEFT JOIN
         base_cte_status s
-        ON IF( s.ts_ended_fixed IS NOT NULL OR s.ts_ended_3 IS NOT NULL, d.`date` >= LEAST(DATE(s.dt_payment_scheduled), DATE(s.ts_created_local), DATE(s.dt_due)) AND IF(s.ts_ended_fixed IS NOT NULL, d.`date` < DATE(s.ts_ended_fixed), d.`date` <= DATE(s.ts_ended_3)), d.`date` >= IF(DATE(s.dt_payment_scheduled) < DATE(s.ts_created_local), DATE(s.dt_payment_scheduled), DATE(s.ts_created_local)))
+        ON IF( s.ts_ended_fixed IS NOT NULL OR s.ts_ended_3 IS NOT NULL, d.`date` >= s.dt_fatura AND IF(s.ts_ended_fixed IS NOT NULL, d.`date` < DATE(s.ts_ended_fixed), d.`date` <= DATE(s.ts_ended_3)), d.`date` >= IF(DATE(s.dt_payment_scheduled) < DATE(s.ts_created_local), DATE(s.dt_payment_scheduled), DATE(s.ts_created_local)))
     WHERE
         d.`date` > DATE('2020-01-01')
         AND d.`date` <= CURRENT_DATE()
