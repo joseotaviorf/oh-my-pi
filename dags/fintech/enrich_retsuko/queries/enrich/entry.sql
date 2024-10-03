9f605dbf7c6e9ff8a906bd8556fd07b9ff4b4886
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2024-10-03 17:11:40 -0300

    [AEFF] Add accounting version in entry enrich table (#16176)
    * added accounting_version column in entry enrich table
    
    * fix dependencies

--- dags/fintech/enrich_retsuko/queries/enrich/entry.sql
+++ dags/fintech/enrich_retsuko/queries/enrich/entry.sql
@@ -1,3 +1,41 @@
+WITH sap_entity AS (
+    SELECT
+        *
+    FROM
+        datalake_retsuko_clean.sap_entity
+    WHERE
+        version IS NOT NULL
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY id_finance_entity ORDER BY ts_updated DESC) = 1
+),
+
+accounting_version AS (
+    SELECT DISTINCT
+        e.id_external AS id_entry,
+    CASE
+        WHEN se_1.id_finance_entity IS NOT NULL THEN se_1.version
+        WHEN se_2.id_finance_entity IS NOT NULL THEN se_2.version
+        WHEN se_1.id_finance_entity IS NULL AND se_2.id_finance_entity IS NULL AND c.landlord_legal_person = 'physical' AND DATE(e.ts_created) < DATE('2024-03-18') THEN 'v1'
+        WHEN se_1.id_finance_entity IS NULL AND se_2.id_finance_entity IS NULL AND c.landlord_legal_person = 'physical' AND DATE(e.ts_created) >= DATE('2024-03-18') THEN 'v2'
+        WHEN se_1.id_finance_entity IS NULL AND se_2.id_finance_entity IS NULL AND c.landlord_legal_person = 'juridical' AND DATE(e.ts_created) < DATE('2024-06-10') THEN 'v1'
+        WHEN se_1.id_finance_entity IS NULL AND se_2.id_finance_entity IS NULL AND c.landlord_legal_person = 'juridical' AND DATE(e.ts_created) >= DATE('2024-06-10') THEN 'v2'
+        ELSE 'ERROR'
+    END AS accounting_version
+    FROM
+        datalake_retsuko_clean.entry AS e
+    LEFT JOIN
+        datalake_retsuko_clean.contract AS c
+            ON e.id_contract = c.id
+    LEFT JOIN
+        datalake_retsuko_clean.invoice AS i
+            ON e.id_invoice = i.id
+    LEFT JOIN
+        sap_entity AS se_1
+            ON e.id_external = se_1.id_finance_entity
+    LEFT JOIN
+        sap_entity AS se_2
+            ON i.id_external = se_2.id_finance_entity
+)
+
 SELECT
     e.id,
     e.id_external,
@@ -8,6 +46,7 @@ SELECT
     e.id_external_reversed_entry,
     e.id_audit,
     e.accounting_transaction_identifier,
+    av.accounting_version,
     ROUND(CASE
           WHEN af.type = 'contract'
               AND at.type <> 'contract' THEN -1.0 * e.amount
@@ -34,3 +73,6 @@ LEFT JOIN
 LEFT JOIN
     datalake_retsuko_clean.contract c
         ON c.id = e.id_contract
+LEFT JOIN
+    accounting_version AS av
+        ON e.id_external = av.id_entry
