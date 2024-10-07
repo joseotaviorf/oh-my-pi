6fe2bd894491d458b3ab1ebdb295cc668d5f5aeb
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2024-10-07 16:03:58 -0300

    [DFFRS-1197] Data ingestion into datalake_sorting_hat_clean (#16188)
    * Add: context, risk_category and id_proposal to analysis_machine
    
    * Add: new columns to datalake_sorting_hat_clean.analysis_state
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/sorting_hat_credit_engine/queries/clean/analysis_machine.sql
+++ dags/fintech/sorting_hat_credit_engine/queries/clean/analysis_machine.sql
@@ -1,12 +1,15 @@
 SELECT
   id,
   analysis_request_id as id_analysis_request,
+  CAST(GET_JSON_OBJECT(context, '$.proposal_id') AS INTEGER) AS id_proposal,
   subject_id as id_subject,
   status,
   subject_type,
   type,
   is_current_machine,
   machine_version,
+  context,
+  CAST(GET_JSON_OBJECT(context, '$.risk_category') AS STRING) AS risk_category,
   TIMESTAMP(created_at) AS ts_created,
   TIMESTAMP(updated_at) AS ts_updated
 FROM
--- dags/fintech/sorting_hat_credit_engine/queries/clean/analysis_state.sql
+++ dags/fintech/sorting_hat_credit_engine/queries/clean/analysis_state.sql
@@ -7,6 +7,16 @@ SELECT
   result,
   status,
   subject_type,
+  structured_result,
+  CAST(GET_JSON_OBJECT(structured_result, '$.homogeneous_group') AS STRING) AS homogeneous_group,
+  CAST(GET_JSON_OBJECT(structured_result, '$.declared_income') AS DECIMAL) AS declared_income,
+  CAST(GET_JSON_OBJECT(structured_result, '$.bureau_income') AS DECIMAL) AS bureau_income,
+  CAST(GET_JSON_OBJECT(structured_result, '$.income_changes') AS DECIMAL) AS income_changes,
+  CAST(GET_JSON_OBJECT(structured_result, '$.max_bureau_income') AS DECIMAL) AS max_bureau_income,
+  CAST(GET_JSON_OBJECT(structured_result, '$.bureau_and_declared_ratio') AS DECIMAL) AS bureau_and_declared_ratio,
+  CAST(GET_JSON_OBJECT(structured_result, '$.is_demoted_group') AS BOOLEAN) AS is_demoted_group,
+  CAST(GET_JSON_OBJECT(structured_result, '$.elected_income') AS DECIMAL) AS elected_income,
+  CAST(GET_JSON_OBJECT(structured_result, '$.is_retenant') AS BOOLEAN) AS is_retenant,
   TIMESTAMP(created_at) AS ts_created,
   TIMESTAMP(updated_at) AS ts_updated
 FROM
