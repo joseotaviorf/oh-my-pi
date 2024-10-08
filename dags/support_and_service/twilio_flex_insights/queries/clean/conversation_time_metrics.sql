818c8a74e89a91fe0e774643a9c0cd0d2c60881a
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-10-08 10:29:36 -0300

    hotfix twilio flex (#16200)

--- dags/support_and_service/twilio_flex_insights/queries/clean/conversation_time_metrics.sql
+++ dags/support_and_service/twilio_flex_insights/queries/clean/conversation_time_metrics.sql
@@ -6,10 +6,12 @@ SELECT
     CAST(total_talk_time AS FLOAT) AS total_talk_time,
     CAST(total_wrap_up_time AS FLOAT) AS total_wrap_up_time,
     CAST(total_handling_time AS FLOAT) AS total_handling_time,
-    CAST(total_waiting_time AS FLOAT) AS total_waiting_time,
+    CAST(`total_waiting_time_[deprecated]` AS FLOAT) AS total_waiting_time,
     CAST(first_reply_time AS FLOAT) AS first_reply_time,
     year,
     month,
     day
 FROM
     datalake_twilio_flex_insights_raw.conversation_time_metrics
+WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
