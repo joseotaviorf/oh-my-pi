4a216d999d5d0173e5f30c6934a50dc67d1dd8f7
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-10-09 11:24:17 -0300

    fix: move spark job to consume from S3 (#16219)
    * fix: move spark job to consume from S3
    
    * fix app id

--- dags/growth/amplitude_supply/spark_jobs/load_amplitude_supply_raw.py
+++ dags/growth/amplitude_supply/spark_jobs/load_amplitude_supply_raw.py
@@ -1,10 +1,10 @@
 import json
 from argparse import ArgumentParser
-from datetime import datetime, timedelta
-from zipfile import ZipFile
-from time import sleep
+from datetime import datetime
 
 from quintoandar_logger import QuintoAndarLogger
+
+from pyspark.sql.functions import lit
 
 from bietlejuice.base.api import APIEnum
 from bietlejuice.base.db import DatalakeMetastoreService
@@ -13,17 +13,14 @@ from bietlejuice.base.spark import (
     SparkTableStorageFormat,
     SparkDataFrameService,
 )
-from bietlejuice.clients.api_clients.amplitude_client import AmplitudeClient
 from bietlejuice.clients.db_clients import SparkClient
 from bietlejuice.loaders import SparkMetastoreLoader
 from bietlejuice.loaders.s3_loader import S3Loader
-from bietlejuice.services.file_service import FileService
 from bietlejuice.services.metastore_services import SparkMetastoreService
 from bietlejuice.services.configuration_service import ConfigurationService
 
 
-JOB_NAME = "load_amplitude_raw"
-AMPLITUDE_API_DATE_FORMAT = "%Y%m%dT%H"
+JOB_NAME = "load_amplitude_demand_raw"
 
 # Timeout between retries in seconds.
 BACKOFF_FACTOR = 5
@@ -46,15 +43,9 @@ if __name__ == "__main__":
     source = args.source
     execution_date = args.execution_date
 
-    start_date = datetime.strptime(execution_date, "%Y-%m-%d")
-    end_date = start_date + timedelta(hours=23)
+    source_path = source.split('_')[0]
 
-    date_times = []
-    date_time = start_date
-    while date_time <= end_date:
-        date_times.append(date_time.strftime(AMPLITUDE_API_DATE_FORMAT))
-        date_time += timedelta(hours=1)
-    time_ranges_list = list(zip(*(iter(date_times),) * 2))
+    dt = datetime.strptime(execution_date, "%Y-%m-%d")
 
     base_dbutils = BaseDBUtils()
     if base_dbutils.get_dbutils() is not None:
@@ -62,18 +53,20 @@ if __name__ == "__main__":
 
     all_keys = json.loads(dbutils.secrets.get("quintoandar", APIEnum.AMPLITUDE))
 
-    keys = [key for key in all_keys if key['app_id'] == 183047]
-
+    key = [secret for secret in all_keys if secret['app_id'] == 183047][0]
+    
     config_service = ConfigurationService(source)
-    custom_records_per_file = config_service.get_config("custom_records_per_file")
-    partition_cols = config_service.get_config("partition_cols")
+    partition_cols = config_service.get_config("raw_partition_cols")
     table_name = config_service.get_config("table_name")
+    transient_location = config_service.get_config("transient_location")
+    transient_data_schema = config_service.get_config("transient_data_schema")
+    transient_expected_cols = config_service.get_config("transient_expected_cols")
 
     spark_client = SparkClient()
     spark_context = spark_client.conn.sparkContext
     dataframe_service = SparkDataFrameService()
 
-    db_info = DatalakeMetastoreService.get_db_info(environment, source, datalake_bucket)
+    db_info = DatalakeMetastoreService.get_db_info(environment, source_path, datalake_bucket)
     database_name = db_info["db_raw_databricks"]
     database_location = db_info["db_raw_path"]
     format_options = SparkTableStorageFormat.DEFAULT_RAW
@@ -81,81 +74,56 @@ if __name__ == "__main__":
     spark_metastore_service = SparkMetastoreService(spark_client)
     spark_metastore_service.create_database(database_name)
 
+    logger.info(
+        f'''msg=initiating data processing, parameters
+            environment = {environment}, datalake_bucket = {datalake_bucket},
+            source = {source}, execution_date = {execution_date}, source_path = {source_path}
+            database_name = {database_name}, database_location = {database_location}
+        '''
+    )
+
     spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
     s3_loader = S3Loader()
 
-    for key in keys:
-        logger.info(
-            "msg=starting events requests, app_id={}, app_name={}".format(
-                key["app_id"], key["app_name"]
-            )
-        )
+    transient_path = transient_location + f'183047/183047_{execution_date}_*/'
 
-        retry_count = 0
-        exceptions = []
-        while retry_count < MAX_RETRIES:
+    logger.info(
+        f'msg=starting events processing, app_id={key["app_id"]}, app_name={key["app_name"]}, path={transient_path}'
+    )
 
-            try:
-                amplitude_client = AmplitudeClient(key["app_key"], key["secret_key"])
+    try:
+        
+        df = spark_client.conn.read.json(transient_path,  schema=transient_data_schema)
 
-                time_ranges_rdd = spark_client.conn.sparkContext.parallelize(
-                    time_ranges_list
-                )
-                file_streams_responses = time_ranges_rdd.map(
-                    lambda time_range: amplitude_client.get_event_data_files(
-                        *time_range
-                    )
-                ).collect()
+        if not(df.isEmpty()):
+            logger.info(f'msg= events received from App ID {key["app_id"]} for this day.')
 
-                filtered_responses = list(filter(None, file_streams_responses))
-
-                events_json = []
-                if filtered_responses:
-
-                    for file_stream in filtered_responses:
-                        with ZipFile(file_stream, "r") as zip_file:
-                            events_json.extend(
-                                FileService.get_data_from_zip_file(zip_file)
-                            )
-
-                    logger.info(
-                        f"m=get_event_data_files, msg=received {len(events_json)} events."
-                    )
-
-                break
-
-            except Exception as error:
-                sleep(retry_count * BACKOFF_FACTOR)
-
-                logger.info(
-                    "msg=fail fetch events requests, retry={}, cause={}".format(
-                        retry_count, error
-                    )
-                )
-
-                exceptions.append(error)
-
-            retry_count += 1
-
-        if retry_count == 5:
-            logger.info("msg=fail fetch events requests, max retries exception")
-
-            exceptions_message = "\n" + "\n".join(exceptions)
-            raise Exception(
-                f"Max retries achieved. Fetch events failed {MAX_RETRIES} times. Exceptions:{exceptions_message}"
-            )
-
-        elif events_json:
-            df = spark_client.conn.read.json(spark_context.parallelize(events_json))
             df = (
                 dataframe_service.input(df)
                 .format_column_names()
                 .convert_struct_type_to_json()
+                .create_columns_from_dict({'app': key["app_id"]})
                 .create_year_month_day_columns_from_dataframe_column(
                     "server_upload_time"
                 )
                 .output()
             )
+
+            """
+            TO-DO: Schema Compatibility between API data schema and Amplitude Pull Export data schema
+
+            Creating two NULL columns that doesn't exists in Pull Export data.
+                1. data_type
+                2. insert_id
+            """
+            
+            missing_cols = [col for col in transient_expected_cols if col not in df.columns]
+            for col in missing_cols:
+
+                df = df.withColumn(col, lit(None))
+            
+            df = df.select(transient_expected_cols)            
+
             df = df.na.drop(subset=partition_cols)
 
             s3_loader.load_df(
@@ -163,7 +131,6 @@ if __name__ == "__main__":
                 s3_path=f"{database_location}{table_name}",
                 format_options=format_options,
                 partitions=partition_cols,
-                max_records_per_file=custom_records_per_file,
                 optimize_dataframe=False,
             )
 
@@ -179,9 +146,9 @@ if __name__ == "__main__":
             spark_metastore_service.create_new_partitions_from_df(
                 database_name, table_name, df, partition_cols, parallelism=8
             )
+
         else:
-            logger.info(
-                "msg=no events received from App ID {} for this day.".format(
-                    key["app_id"]
-                )
-            )
+            logger.info(f'msg=no events received from App ID {key["app_id"]} for this day.')
+
+    except Exception as error:
+        logger.info(f"msg=fail to get events from {dt}, cause={error}")
