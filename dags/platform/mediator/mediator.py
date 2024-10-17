cc61c5224edb61c148a366f8d384908a8abdbaa6
Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
2024-10-17 10:29:44 -0300

    [DIN] Fix mediator timeout (#16329)
    Increase timeout from 9min to 11min

--- dags/platform/mediator/mediator.py
+++ dags/platform/mediator/mediator.py
@@ -70,7 +70,7 @@ mediator_dag = DAG(
     doc_md=BaseDAG.get_dag_doc(DAG_NAME).format(
         chart_url=Variable.get("DOC_MD_BASE_URL"), dag_id=DAG_ID
     ),
-    dagrun_timeout=timedelta(minutes=9),
+    dagrun_timeout=timedelta(minutes=11),
 )
 
 dependencies_dict = extract_dependencies()
