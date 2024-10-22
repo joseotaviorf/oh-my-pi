281a02cb4b208adad9c019b02d4f2b0c1da0cb26
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-10-22 14:58:48 -0300

    fix: remove duplicated records (#16383)

--- dags/support_and_service/help_center_api/queries/clean/call_in_app_eligibility.sql
+++ dags/support_and_service/help_center_api/queries/clean/call_in_app_eligibility.sql
@@ -5,3 +5,7 @@ SELECT
     updated_at AS ts_updated
 FROM
     datalake_help_center_api_raw.t_call_in_app_eligibility
+WHERE
+    MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
+QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY user_id, status ORDER BY ts_created DESC) = 1
