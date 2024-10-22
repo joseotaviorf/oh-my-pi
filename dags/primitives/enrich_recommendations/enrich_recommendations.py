9adf2b42b9160668c85fc067068d679cfd2c01d5
Lucas Cardozo <8867239+lecardozo@users.noreply.github.com>
2024-10-22 13:24:34 +0100

    fix: use new recommended cluster (#16332)
    * fix: use new recommended cluster
    
    * fix: move cluster config to local configuration
    
    * Update dags/primitives/enrich_recommendations/forno_conf.yml
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>

--- dags/primitives/enrich_recommendations/enrich_recommendations.py
+++ dags/primitives/enrich_recommendations/enrich_recommendations.py
@@ -39,9 +39,8 @@ databricks_bietlejuice_repo_path = config_service.get_config(
 base_spark_jobs_path = f"{databricks_bietlejuice_repo_path}/spark_jobs/base/"
 doc_md_chart_url = config_service.get_config("doc_md_chart_url")
 
-CLUSTER_DESCRIPTION = config_service.get_config(
-    "databricks_12_2_med_memory_photon_cluster"
-)
+CLUSTER_DESCRIPTION = config_service.get_config("custom_cluster")
+
 DATABRICKS_CLUSTER_ACCESS_CONTROL_LIST = [
     {
         "group_name": DatabricksGroupNameEnum.DATA_PRODUCTS,
