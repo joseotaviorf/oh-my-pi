3eeeb3488ad5f7877cda90150b9390bd1354f62a
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-10-22 15:39:02 -0300

    [GROW-3385] Create a dag to monitor the evolution of demand_score (#16354)
    * fix: add new column
    
    * move to dag builder
    
    * add declaration file
    
    * feat: create dag and sql
    
    * feat: add metadata
    
    * fix query
    
    * fix snapshot
    
    * fix name
    
    * fix: change name
    
    * fix name
    
    * fix: change name
    
    * fix snapshot date
    
    * chore: add comment
    
    * fix: change nome
    
    * fix: add demand_score
    
    * fix: metadata
    
    * fix query

---
+++ dags/growth/enrich_demand_score_report/queries/enrich/demand_score_report.sql
@@ -0,0 +1,20 @@
+SELECT
+    id_house,
+    listing_age,
+    quality_score,
+    liquidity_score,
+    qt_lpv_1d_for_rent,
+    qt_lpv_3d_for_rent,
+    qt_lpv_7d_for_rent,
+    qt_lpv_1d_for_sale,
+    qt_lpv_3d_for_sale,
+    qt_lpv_7d_for_sale,
+    demand_score,
+    dt_publication_for_sale,
+    dt_publication_for_rent,
+    dt_snapshot
+FROM
+    reverse_demand_score.listing_demand_score
+WHERE
+    DATE(dt_snapshot) = DATE('{snapshot_date}')
+    -- TO-DO: Add filter for campaigns (collecting with BizOps team)
