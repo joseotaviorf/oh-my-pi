15d35b33fcc0d00d7a5383a7b6aaa300498c1f34
Arthur Simões Gonzaga <arthur.gonzaga@quintoandar.com.br>
2024-10-23 09:11:14 -0300

    [GROW-3417] Create a enrich to consolidate historical data from Criteo (#16363)
    * fix: add new column
    
    * move to dag builder
    
    * add declaration file
    
    * feat: create dag and sql
    
    * feat: add metadata
    
    * fix query
    
    * fix snapshot
    
    * fix name
    
    * fix: change name
    
    * fix name
    
    * fix: change name
    
    * fix snapshot date
    
    * chore: add comment
    
    * create enrich
    
    * fix: remove demand score
    
    * fix: change cluster
    
    * change dependencies
    
    * add partitions

---
+++ dags/growth/enrich_criteo/queries/enrich/criteo_campaigns.sql
@@ -0,0 +1,31 @@
+SELECT 
+    id_advertiser,
+    id_ad_set,
+    advertiser_name,
+    ad_set,
+    region,
+    zip_code,
+    cost,
+    sales,
+    audience,
+    clicks,
+    cpc,
+    ctr,
+    displays,
+    exposed_users,
+    reach,
+    visits,
+    win_rate,
+    bounce_rate,
+    cost_per_visit,
+    cpo,
+    cvr,
+    dt_report,
+    ts_load,
+    YEAR(dt_report) AS year,
+    MONTH(dt_report) AS month,
+    DAY(dt_report) AS day
+FROM 
+    datalake_gsheets_clean.criteo_costs
+WHERE 
+    DATE(dt_report) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
