e33d13d8537395f2b5a272e78b76a49be3a5c420
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-10-28 12:36:20 -0300

    fix: help center api (#16464)

--- dags/support_and_service/help_center_api/queries/clean/call_in_app_eligibility.sql
+++ dags/support_and_service/help_center_api/queries/clean/call_in_app_eligibility.sql
@@ -1,11 +1,8 @@
 SELECT
+    id,
     user_id AS id_user,
     status,
     created_at AS ts_created,
     updated_at AS ts_updated
 FROM
     datalake_help_center_api_raw.t_call_in_app_eligibility
-WHERE
-    MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
-QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY user_id, status ORDER BY ts_created DESC) = 1
--- dags/support_and_service/help_center_api/queries/clean/call_request.sql
+++ dags/support_and_service/help_center_api/queries/clean/call_request.sql
@@ -9,7 +9,3 @@ SELECT
     requested_at AS ts_requested
 FROM
     datalake_help_center_api_raw.t_call_request
-WHERE
-  MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
-QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY user_id, session_id ORDER BY ts_requested DESC) = 1
