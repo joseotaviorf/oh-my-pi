429cd7c3cbad5a750ff1f3ac8d642f0dd49010d6
Thiago Pinto <thiago.pinto@quintoandar.com.br>
2024-10-29 21:00:12 +0100

    [IDPLATF-2511] Enrich infra logs (#16424)
    * fix cf
    
    * enrich
    
    * fix comments
    
    * Optimize query
    
    * Update dags/tech_platform/enrich_access_logs/enrich_access_logs_declaration.yml
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>
    
    * add trace_id
    
    ---------
    
    Co-authored-by: Gustavo Miller Santos <91481595+Miller-GS@users.noreply.github.com>

---
+++ dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
@@ -0,0 +1,38 @@
+WITH istio AS (
+    SELECT *
+    FROM
+        datalake_access_logs_clean.istio
+    WHERE
+        MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
+)
+SELECT 
+    opa.id_trace,
+    istio.ts_event, 
+    istio.request_traceparent, 
+    istio.request_x_forwarded_for,
+    istio.request_method, 
+    istio.request_user_agent, 
+    istio.request_path,
+    istio.request_duration_ms, 
+    istio.response_flags, 
+    istio.response_code,
+    opa.request_source_principal, 
+    opa.request_destination_principal, 
+    opa.request_path as request_parameterized_path, 
+    opa.request_required_roles,
+    opa.result_http_allowed, 
+    result_http_status, 
+    opa.principal_user_email, 
+    opa.principal_user_idp, 
+    opa.principal_service,
+    opa.principal_user_provided_roles, 
+    opa.principal_service_provided_roles, 
+    opa.app,
+    opa.year, 
+    opa.month, 
+    opa.day, 
+    opa.hour
+FROM datalake_access_logs_clean.opa as opa
+LEFT JOIN istio on opa.id_trace = istio.id_trace
+WHERE
+    MAKE_DATE(opa.year, opa.month, opa.day) BETWEEN '{load_start_date}' AND '{load_end_date}'
