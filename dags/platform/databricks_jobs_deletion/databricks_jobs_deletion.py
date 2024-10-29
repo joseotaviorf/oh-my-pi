edf6a25db988d7a6b4d8dfce77de8417a290d180
Gustavo Akashi <gustavo.akashi@quintoandar.com.br>
2024-10-29 14:44:55 -0300

    feat: decreasing timedelta from log retention in databricks jobs (#16487)

--- dags/platform/databricks_jobs_deletion/databricks_jobs_deletion.py
+++ dags/platform/databricks_jobs_deletion/databricks_jobs_deletion.py
@@ -16,7 +16,7 @@ MAIN_START_DATE = datetime(2023, 7, 15, tzinfo=timezone("America/Sao_Paulo"))
 MAIN_SCHEDULE_INTERVAL = "0 20 * * *"
 
 PROJECT_TO_JOB_NAME_REGEX_MAPPING = {"bietlejuice": "^bietlejuice-", "wonka": "^wonka-"}
-JOBS_REMOVAL_TIMEDELTA = timedelta(days=15)
+JOBS_REMOVAL_TIMEDELTA = timedelta(days=7)
 
 
 def delete_databricks_jobs(remove_before_timedelta: timedelta, job_name_regex: str):
