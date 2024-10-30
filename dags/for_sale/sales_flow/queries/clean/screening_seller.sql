9ee76a57ad48b89e3af33142ece92ce985542510
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2024-10-30 17:06:32 -0300

    [DFORSALE-8297] ✨ Add new table screening_seller to sale_flows (#16507)
    * ✨ add new table screening_seller to sale_flows
    
    * 📝 add metadata screening_seller

---
+++ dags/for_sale/sales_flow/queries/clean/screening_seller.sql
@@ -0,0 +1,18 @@
+SELECT
+  id,
+  screening_id AS id_screening,
+  seller_data_id AS id_seller_data,
+  status,
+  proposal_validation_status,
+  legal_pendency_note,
+  created_at AS ts_created,
+  updated_at AS ts_updated,
+  year,
+  month,
+  day
+FROM
+  datalake_sales_flow_raw.screening_seller
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
