00b468307c59031c57e44f99e36cb5b15ee7615f
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2024-11-04 12:02:38 -0300

    [AE-GROW] Applying DAG Builder to braze_events (#16534)
    * [AE-GROW] Applying DAG Builder to braze_events
    
    * new updates
    
    * schedule interval
    
    * updates
    
    * commenting
    
    * fix
    
    * log
    
    * updates
    
    * fixing execution_date
    
    * adding comment
    
    * dt_execution fix
    
    * fixing query

---
+++ dags/growth/braze_events_test/queries/clean/events_owners.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT
+    id,
+    event_info,
+    event_type,
+    year,
+    month,
+    day
+FROM
+    datalake_braze_events_test_raw.events_owners
+WHERE
+    year = {year}
+    AND month = {month}
+    AND day = {day}
---
+++ dags/growth/braze_events_test/spark_jobs/load_braze_events_test_raw.py
@@ -0,0 +1,160 @@
+import logging
+
+from argparse import ArgumentParser
+from collections import OrderedDict
+from datetime import datetime
+from pyspark.sql.functions import to_json, struct
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.services.configuration_service import ConfigurationService
+from bietlejuice.services.metastore_services import SparkMetastoreService
+from bietlejuice.loaders import SparkMetastoreLoader
+from bietlejuice.loaders.s3_loader import S3Loader
+from bietlejuice.clients.db_clients import SparkClient
+from bietlejuice.base.db import DatalakeMetastoreService
+from bietlejuice.base.spark import (
+    SparkTableStorageFormat,
+    SparkDataFrameService,
+    BaseDBUtils,
+    spark,
+)
+
+
+JOB_NAME = "load_braze_events_into_datalake"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger = QuintoAndarLogger(JOB_NAME)
+
+
+def get_events_from_app_group(app_group_path, dbutils):
+    data_export_folders = base_dbutils.discover_directories_in_path(
+        app_group_path, dbutils
+    )
+    events = []
+    for folder in data_export_folders:
+        events_in_folder = base_dbutils.discover_partition_values_in_path(
+            f"{app_group_path}/{folder}", dbutils
+        )
+        events = list(set(events) | set(events_in_folder))
+    return events
+
+
+def get_event_data(app_group_path, event, execution_date):
+    try:
+        df = spark.read.format("avro").load(
+            f"{app_group_path}/dataexport*/event_type={event}/date={execution_date}*/*/*/"
+        )
+    except Exception as e:
+        logger.info(f"m={JOB_NAME}, error={e}")
+        return None
+
+    # serialize dataframe columns into a json string
+    df = df.withColumn("event_info", to_json(struct([df[x] for x in df.columns])))
+    df = df.select("id", "event_info")
+    return df
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("environment")
+    parser.add_argument("datalake_bucket")
+    parser.add_argument("source")
+    parser.add_argument("execution_date")
+    #parser.add_argument("load_start_date")
+    #parser.add_argument("load_end_date")
+    parser.add_argument("app_group")
+    args = parser.parse_args()
+
+    environment = args.environment
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+    execution_date = args.execution_date
+    #load_start_date = args.load_start_date
+    #load_end_date = args.load_end_date
+    app_group = args.app_group
+
+    config_service = ConfigurationService(source)
+    braze_bucket = config_service.get_config("braze_events_bucket")
+
+    logger.info(
+        f"""
+        m={JOB_NAME}, environment={environment}, source={source}, datalake_bucket={datalake_bucket},
+        braze_bucket={braze_bucket}, app_group={app_group}, execution_date={execution_date},
+        msg=Starting spark job..."
+        """
+    )
+
+    base_dbutils = BaseDBUtils()
+    if base_dbutils.get_dbutils() is not None:
+        dbutils = base_dbutils.get_dbutils()
+
+    s3_loader = S3Loader()
+    spark_client = SparkClient()
+    spark_metastore_service = SparkMetastoreService(spark_client)
+    spark_metastore_loader = SparkMetastoreLoader(spark_metastore_service)
+    datalake_info = DatalakeMetastoreService.get_db_info(
+        environment, source, datalake_bucket
+    )
+
+    format_options = SparkTableStorageFormat.DEFAULT_RAW
+    database_location = datalake_info["db_raw_path"]
+    database_name = datalake_info["db_raw_databricks"]
+    spark_metastore_service.create_database(database_name)
+
+    dt_execution = datetime.strptime(execution_date, "%Y-%m-%d")
+    partitions = OrderedDict(
+        [
+            ("year", dt_execution.year),
+            ("month", dt_execution.month),
+            ("day", dt_execution.day),
+            ("event_type", ""),
+        ]
+    )
+    partition_cols = list(partitions.keys())
+
+    table_name = f"events_{app_group}"
+    app_group_path = f"{braze_bucket}/{app_group}"
+    events = get_events_from_app_group(app_group_path, dbutils)
+
+    for event in events:
+        df = get_event_data(app_group_path, event, dt_execution)
+
+        if df:
+            logger.info("msg=Dataframe not empty, starting load...")
+            partitions["event_type"] = event
+
+            df = (
+                SparkDataFrameService()
+                .input(df)
+                .create_columns_from_dict(partitions)
+                .optimize_partitions_by_partition_columns(partition_cols)
+                .output()
+            )
+
+            s3_loader.load_df(
+                df=df,
+                s3_path=database_location + table_name,
+                format_options=format_options,
+                partitions=partition_cols,
+                compression="gzip",
+            )
+
+            spark_metastore_loader.update_metastore(
+                df=df,
+                database_name=database_name,
+                table_name=table_name,
+                format_options=format_options,
+                database_location=database_location,
+                partitions=partition_cols,
+                force_recreate=False,
+            )
+
+            spark_metastore_service.create_new_partitions_from_df(
+                df=df,
+                database_name=database_name,
+                table_name=table_name,
+                partition_cols=partition_cols,
+            )
+
+            spark_metastore_service.refresh_table(database_name, table_name)
