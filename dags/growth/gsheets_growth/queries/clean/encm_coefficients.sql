cda0739ee31ca82bc434fb8aa86a301e613e7179
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2024-11-06 14:16:34 -0300

    Adding new columns to Google Sheets (#16611)
    Co-authored-by: Uilson Bonfim <uilsonbonfim@BRSPPMCXG7JMD6M.local>
    Co-authored-by: Uilson Bonfim <uilsonbonfim@BRSPPM-C02DXG7JMD6M.local>

--- dags/growth/gsheets_growth/queries/clean/encm_coefficients.sql
+++ dags/growth/gsheets_growth/queries/clean/encm_coefficients.sql
@@ -4,6 +4,8 @@ SELECT
     STRING(NULLIF(event_name,'')) AS event_name,
     FLOAT(NULLIF(coefficient,'')) AS coefficient,
     FLOAT(NULLIF(constant,'')) AS constant,
+    FLOAT(NULLIF(gross_takerate,'')) AS gross_takerate,
+    FLOAT(NULLIF(survival_curve,'')) AS survival_curve,
     DATE(NULLIF(date_start, '')) AS date_start, 
     DATE(NULLIF(date_end, '')) AS date_end
 FROM datalake_gsheets_raw.encm_coefficients
