5fbe22aec16020b596951ddaae6c627c181cdc25
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2024-11-07 11:05:01 -0300

    Add new unique user identification columns to single_station_surveys table (#16639)
    * Add new user identification columns to table
    
    * Update metadata file from single_station_surveys table
    
    * Update dependencies file

--- dags/support_and_service/enrich_survicate/queries/enrich/single_station_surveys.sql
+++ dags/support_and_service/enrich_survicate/queries/enrich/single_station_surveys.sql
@@ -1,8 +1,31 @@
+WITH emails as (
+  SELECT
+    get_json_object(regexp_replace(sr.respondent, "'", '"'), '$.uuid') AS respondent_uuid,
+    id_survey,
+    value as email
+  FROM 
+    datalake_survicate_clean.survey_responses sr
+  LEFT JOIN datalake_survicate_clean.respondent_attributes attr
+    ON get_json_object(regexp_replace(sr.respondent, "'", '"'), '$.uuid') = attr.respondent_uuid
+    WHERE name = 'email'
+), user_id as (
+  SELECT
+    get_json_object(regexp_replace(sr.respondent, "'", '"'), '$.uuid') AS respondent_uuid,
+    id_survey,
+    value as user_id
+  FROM 
+    datalake_survicate_clean.survey_responses sr
+  LEFT JOIN datalake_survicate_clean.respondent_attributes attr
+    ON get_json_object(regexp_replace(sr.respondent, "'", '"'), '$.uuid') = attr.respondent_uuid
+    WHERE name = 'user_id'
+)
 SELECT
     sr.id_response,
     sr.id_survey,
     sr.id_respondent,
-    s.survey_name,
+    u.user_id,
+    e.email,
+    sr.survey_name,
     'analyst' AS respondent_type,
     'magic_link_2' AS service_type,
     'single_station' AS service_context,
@@ -37,14 +60,15 @@ SELECT
     MONTH(rc.dt_load) AS month,
     DAY(rc.dt_load) AS day
 FROM
-    datalake_survicate_clean.surveys AS s  
-    JOIN 
-        datalake_survicate.survey_responses AS sr 
-        ON sr.id_survey = s.id_survey
-    JOIN 
-        datalake_survicate.response_content AS rc 
-        ON sr.id_response = rc.id_response 
-WHERE sr.id_survey IN ('775741557ef98468','9fa10adff80551f6','74c8f0608123f05d','9efcef20c637ee47','739bb49c771b2806') AND
-    DATE(rc.dt_load) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  datalake_survicate.survey_responses AS sr 
+JOIN 
+  datalake_survicate.response_content AS rc 
+  ON sr.id_response = rc.id_response 
+LEFT JOIN emails AS e
+  ON e.respondent_uuid = sr.id_respondent
+LEFT JOIN user_id as u
+  ON u.respondent_uuid = sr.id_respondent
+WHERE sr.id_survey IN ('775741557ef98468','9fa10adff80551f6','74c8f0608123f05d','9efcef20c637ee47','739bb49c771b2806') 
+AND DATE(rc.dt_load) BETWEEN DATE('{load_start_date}') AND DATE('{load_start_date}')
 GROUP BY
     ALL
