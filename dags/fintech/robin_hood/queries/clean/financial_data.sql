14a074103c7d31d0f81018b71ce1ed77b1a59c3e
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-11-07 12:18:20 -0300

    AEFF-2463 add new column (#16645)

--- dags/fintech/robin_hood/queries/clean/financial_data.sql
+++ dags/fintech/robin_hood/queries/clean/financial_data.sql
@@ -4,6 +4,7 @@ select
     sha2(name,256) as name,
     sha2(document_number,256) as document_number,
     bank_code,
+    bank_name,
     sha2(account_type,256) as account_type,
     sha2(agency_number,256) as agency_number,
     sha2(account_number,256) as account_number,
