6ca74e7b5eaf5e08e91c7143d3e57549baf10125
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2024-11-11 14:58:49 -0300

    [DFFRS-1009] Update  policy_report tables at enrich layer (#16662)
    * Update: datalake_sorting_hat.policy_report
    
    * Update: policy_report_proponents
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/enrich_sorting_hat/queries/enrich/policy_report.sql
+++ dags/fintech/enrich_sorting_hat/queries/enrich/policy_report.sql
@@ -7,10 +7,10 @@ WITH policy_report_extract_json AS (
                 'ARRAY<
                     STRUCT<
                         document_number: STRING,
-                        is_retenant: BOOLEAN, 
-                        retenant_contract_id: STRING, 
-                        retenant_contract_proposal_id: STRING, 
-                        retenant_contract_user_id: STRING, 
+                        is_retenant: BOOLEAN,
+                        retenant_contract_id: STRING,
+                        retenant_contract_proposal_id: STRING,
+                        retenant_contract_user_id: STRING,
                         retenant_score: STRING
                     >
                 >'
@@ -20,15 +20,17 @@ WITH policy_report_extract_json AS (
         datalake_sorting_hat_clean.policy_report
 ),
 proponents AS (
-SELECT
+  SELECT
     id AS id_policy_evaluation,
+    extract_json.retenant_contract_user_id,
     extract_json.retenant_score,
     extract_json.document_number,
     extract_json.is_retenant
-FROM 
+  FROM
     policy_report_extract_json
-)
-SELECT
+),
+retenant_policy_report AS (
+  SELECT
     pr.id AS id_policy_evaluation,
     CAST(pr.id_external AS INTEGER) AS id_external,
     pr.type AS policy_name,
@@ -37,25 +39,65 @@ SELECT
     GET_JSON_OBJECT(pr.result, '$.subtype') AS proponent_class_detail,
     GET_JSON_OBJECT(pr.result, '$.full_type') AS proponent_group_class_detail,
     pr.version,
+    GET_JSON_OBJECT(pr.result, '$.classification') AS retenant_classification,
+    CAST(GET_JSON_OBJECT(pr.result, '$.avg_retenant_score') AS DECIMAL(10,2)) AS avg_retenant_score,
+    CAST(GET_JSON_OBJECT(pr.result, '$.elected_contract_id') AS INTEGER) AS id_retenant_elected_contract,
+    CAST(proponents.retenant_contract_user_id AS INTEGER) AS id_retenant_contract_user,
     COUNT(proponents.document_number) AS number_of_proponents,
-    AVG(proponents.retenant_score) AS average_retenants_score,
+    AVG(proponents.retenant_score) AS average_retenants_score_calculated,
     CASE
-        WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'ANY_PROPONENT' THEN TRUE
-        WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'NEW_GROUP_PROPONENT' THEN TRUE
-        WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'SAME_GROUP_PROPONENT' THEN TRUE
-        WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'NEW_USER' THEN FALSE
-        ELSE NULL
+      WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'ANY_PROPONENT' THEN TRUE
+      WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'NEW_GROUP_PROPONENT' THEN TRUE
+      WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'SAME_GROUP_PROPONENT' THEN TRUE
+      WHEN GET_JSON_OBJECT(pr.result, '$.type') = 'NEW_USER' THEN FALSE
+      ELSE NULL
     END AS is_retenant,
-    CASE 
-        WHEN COUNT(DISTINCT proponents.is_retenant) = 1 AND MAX(proponents.is_retenant) = TRUE THEN TRUE 
-        ELSE FALSE
+    CASE
+      WHEN COUNT(DISTINCT proponents.is_retenant) = 1
+      AND MAX(proponents.is_retenant) = TRUE THEN TRUE
+      ELSE FALSE
     END AS are_all_proponents_retenants,
     pr.ts_created,
     pr.ts_updated
-FROM
+  FROM
     datalake_sorting_hat_clean.policy_report AS pr
-LEFT JOIN
-    proponents ON pr.id = proponents.id_policy_evaluation
-WHERE 
+    LEFT JOIN proponents ON pr.id = proponents.id_policy_evaluation
+  WHERE
     pr.type = 'RETENANT'
-GROUP BY 1,2,3,4,5,6,7,8,13,14
+  GROUP BY
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    17,
+    18
+)
+
+SELECT
+  id_policy_evaluation,
+  id_external,
+  id_retenant_elected_contract,
+  id_retenant_contract_user,
+  policy_name,
+  external_source,
+  retenant_classification,
+  proponent_group_class,
+  proponent_class_detail,
+  proponent_group_class_detail,
+  number_of_proponents,
+  COALESCE(avg_retenant_score, average_retenants_score_calculated) AS average_retenants_score,
+  version,
+  is_retenant,
+  are_all_proponents_retenants,
+  ts_created,
+  ts_updated
+FROM
+    retenant_policy_report
--- dags/fintech/enrich_sorting_hat/queries/enrich/policy_report_proponents.sql
+++ dags/fintech/enrich_sorting_hat/queries/enrich/policy_report_proponents.sql
@@ -4,15 +4,15 @@ SELECT
     EXPLODE(FROM_JSON(GET_JSON_OBJECT(result, '$.proponents'), 'ARRAY<STRING>')) AS extract_json
 FROM
     datalake_sorting_hat_clean.policy_report
-), 
-user ( 
-SELECT 
+),
+user (
+SELECT
   id AS id_user,
   cpf
-FROM 
+FROM
   datalake_ebdb_user.user
 QUALIFY --filter to return only the user with the most recent creation date.
-  ROW_NUMBER() OVER (PARTITION BY cpf ORDER BY ts_created DESC) = 1 
+  ROW_NUMBER() OVER (PARTITION BY cpf ORDER BY ts_created DESC) = 1
 )
 SELECT
     policy_report_extract_json.id AS id_policy_evaluation,
@@ -20,9 +20,11 @@ SELECT
     CAST(GET_JSON_OBJECT(extract_json, '$.retenant_contract_proposal_id') AS INTEGER) AS id_retenant_contract_proposal,
     CAST(GET_JSON_OBJECT(extract_json, '$.retenant_contract_user_id') AS INTEGER) AS id_retenant_contract_user,
     user.id_user,
+    CAST(GET_JSON_OBJECT(extract_json, '$.document_number') AS STRING) AS proponent_document_number,
     GET_JSON_OBJECT(extract_json, '$.retenant_score') AS retenant_score,
     GET_JSON_OBJECT(extract_json, '$.is_retenant') AS is_retenant
-FROM 
+FROM
     policy_report_extract_json
-LEFT JOIN --join to remove cpf from query
+LEFT JOIN
+--join to remove cpf from query
     user ON GET_JSON_OBJECT(extract_json, '$.document_number') = user.cpf
