51e44e11232344bd0bdd2b9999c9c60d7aa51b71
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-11-21 10:01:42 -0300

    Adding business context (#16830)

--- dags/for_sale/dw_visit/queries/dw/fact_visit_schedules.sql
+++ dags/for_sale/dw_visit/queries/dw/fact_visit_schedules.sql
@@ -17,6 +17,7 @@ WITH
         id_schedule,
         id_visit,
         MIN_BY(id_author_user, ts_event_created) AS id_author_creator,
+        business_context,
         CASE
           WHEN MIN(ts_event_created) FILTER (WHERE event_type = 'VISIT_RESCHEDULED') IS NULL THEN 'RESCHEDULE'
           ELSE 'REQUEST'
@@ -30,12 +31,14 @@ WITH
         datalake_visit.visit_status_events
       GROUP BY
         id_schedule,
-        id_visit
+        id_visit,
+        business_context
 )
 SELECT
   schedule_events.id_schedule AS sk_schedule,
   id_visit AS sk_visit,
   id_author_creator AS sk_author_creator,
+  bc.sk_business_context,
   reschedules.id_succeed_schedule AS sk_succeed_schedule,
   sk_origin_type,
   CASE WHEN ts_schedule_rescheduled IS NOT NULL THEN 1 ELSE 0 END AS is_reschedule,
@@ -55,3 +58,6 @@ LEFT JOIN
 INNER JOIN
    dw_visit.dim_origin_type AS dot
     ON schedule_events.schedule_origin = dot.origin_name
+LEFT JOIN
+   dw_visit.dim_business_context AS bc
+    ON schedule_events.business_context = bc.business_context
