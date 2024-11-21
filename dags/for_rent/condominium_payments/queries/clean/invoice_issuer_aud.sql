2b7a32c6d1b09c8a6f89543cd7580d67121ed57b
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2024-11-21 09:45:06 -0300

    add invoice tables (#16837)

---
+++ dags/for_rent/condominium_payments/queries/clean/invoice_issuer_aud.sql
@@ -0,0 +1,12 @@
+SELECT
+    id,
+    name,
+    document,
+    document_type,
+    rev,
+    revend AS rev_end,
+    revtype AS rev_type,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_condominium_payments_raw.boleto_issuer_aud
