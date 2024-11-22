57f39e890e7fc14e10143a3eb113cff5bd4367d7
Bianca Garcia <biigarciam@gmail.com>
2024-11-21 23:37:35 -0300

    [DFFRS-1342] Improve hourly agreement (#16865)
    * Add id_negotiation_external do negotiation hourly table
    
    * distinct down_payment_paid_amount and down_payment_amount

--- dags/fintech/enrich_agreement_hourly/queries/enrich/negotiation.sql
+++ dags/fintech/enrich_agreement_hourly/queries/enrich/negotiation.sql
@@ -27,6 +27,7 @@ installments AS (
 deduplicate_negotiation AS (
   SELECT
     id AS id_negotiation,
+    id_collector_external AS id_negotiation_external,
     id_debtor_external AS id_contract,
     consultancy AS agency,
     ts_created AS ts_negotiation_creation,
@@ -40,27 +41,31 @@ deduplicate_negotiation AS (
 ),
 installment_group_by_negotiation AS (
   SELECT
-    i.id_negotiation,
+    n.id_negotiation,
+    n.id_negotiation_external,
     n.id_contract,
     n.agency AS id_agency,
     n.ts_negotiation_creation,
     n.year,
     n.month,
     n.day,
-    SUM(IF(i.installment_number = 1 AND i.status = 'paid', i.total_amount, 0)) AS down_payment_amount,
+    SUM(IF(i.installment_number = 1, i.total_amount, 0)) AS down_payment_amount,
+    SUM(IF(i.installment_number = 1 AND i.status = 'paid', i.total_amount, 0)) AS down_payment_paid_amount,
     SUM(i.total_amount) AS total_negotiated_amount,
     MIN(i.dt_due) AS dt_down_payment_due,
     MIN(IF(i.installment_number = 1 AND i.status = 'paid', i.dt_paid, NULL)) AS dt_down_payment
   FROM deduplicate_negotiation AS n
   INNER JOIN installments AS i
     ON i.id_negotiation = n.id_negotiation
-  GROUP BY 1,2,3,4,5,6,7
+  GROUP BY 1,2,3,4,5,6,7,8
 )
 SELECT
   id_negotiation,
+  id_negotiation_external,
   id_contract,
   id_agency,
   down_payment_amount,
+  down_payment_paid_amount,
   total_negotiated_amount,
   dt_down_payment_due,
   dt_down_payment,
