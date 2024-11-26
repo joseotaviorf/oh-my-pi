e2cf2900ec661e008fa4e7d205f0b0ab044c2cb1
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-11-26 12:47:51 -0300

    fix query (#16922)

--- dags/rede/gsheets_rede/queries/clean/rede_performance_targets.sql
+++ dags/rede/gsheets_rede/queries/clean/rede_performance_targets.sql
@@ -2,7 +2,7 @@ SELECT
     NULLIF(mes, '') AS month,
     NULLIF(dia, '') AS day,
     NULLIF(cohort, '') AS cohort,
-    NULLIF(am, '') AS account_manager
+    NULLIF(am, '') AS account_manager,
     NULLIF(context, '') AS context,
     NULLIF(cidade, '') AS city,
     NULLIF(vb, '') AS visit_booked,
@@ -12,6 +12,5 @@ SELECT
     NULLIF(ccv, '') AS signed_contract,
     NULLIF(fl, '') AS first_listing,
     NULLIF(ol, '') AS ongoing_listing
-
 FROM
     datalake_gsheets_raw.rede_performance_targets
