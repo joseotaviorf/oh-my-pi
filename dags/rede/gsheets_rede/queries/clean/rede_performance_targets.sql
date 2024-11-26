3d04c414cd8ef1348d527329627ac5a411526670
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-11-26 11:03:27 -0300

    [RD-1609] Update Rede gsheets target tables (#16910)
    * update performance targets sheet table
    
    * move 3p partners conditions to rede domain
    
    * update raw metadata
    
    * update dependencies

--- dags/rede/gsheets_rede/queries/clean/rede_performance_targets.sql
+++ dags/rede/gsheets_rede/queries/clean/rede_performance_targets.sql
@@ -1,9 +1,17 @@
 SELECT
     NULLIF(mes, '') AS month,
-    NULLIF(cluster, '') AS cluster,
-    NULLIF(flow, '') AS flow,
-    NULLIF(type, '') AS metric_type,
-    NULLIF(account_manager, '') AS account_manager,
-    NULLIF(target, '') AS metric_target
+    NULLIF(dia, '') AS day,
+    NULLIF(cohort, '') AS cohort,
+    NULLIF(am, '') AS account_manager
+    NULLIF(context, '') AS context,
+    NULLIF(cidade, '') AS city,
+    NULLIF(vb, '') AS visit_booked,
+    NULLIF(vc, '') AS visit_confirmed,
+    NULLIF(os, '') AS offer_signed,
+    NULLIF(oa, '') AS offer_accepted,
+    NULLIF(ccv, '') AS signed_contract,
+    NULLIF(fl, '') AS first_listing,
+    NULLIF(ol, '') AS ongoing_listing
+
 FROM
     datalake_gsheets_raw.rede_performance_targets
