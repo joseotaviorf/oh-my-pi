d74dad5a64d7ae0397a609274178e5806217a69f
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2024-11-27 08:42:24 -0300

    [RD] Add Growth Lab `extra_query_template_params` (#16323)
    * add load start and end date
    
    * update spark job arguments
    
    * update clean date params

--- dags/rede/growth_lab/queries/clean/supply_recurrence.sql
+++ dags/rede/growth_lab/queries/clean/supply_recurrence.sql
@@ -26,6 +26,4 @@ SELECT
 FROM
     datalake_growth_lab_raw.supply_recurrence
 WHERE
-    year = {year}
-    AND month = {month}
-    AND day = {day}
+    MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
