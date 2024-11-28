f3141312d9812ef1d1ee8e0446e5523856a09467
William Angelo <39243586+williamangelo@users.noreply.github.com>
2024-11-28 15:21:33 -0300

    fix: typo (#16988)

--- dags/support_and_service/dw_satisfaction_rating/queries/dw/fact_ticket_csat.sql
+++ dags/support_and_service/dw_satisfaction_rating/queries/dw/fact_ticket_csat.sql
@@ -1,12 +1,12 @@
 SELECT
-  CAST(id_ticket AS BIGINT) AS sk_ticket,
+  CAST(id_ticket AS sk_ticket,
   first_csat_score,
   last_csat_score,
   first_csat_comment,
   last_csat_comment,
   is_answered,
-  is_solved
-  ts_first_responde,
+  is_solved,
+  ts_first_response,
   ts_last_response
 FROM
   datalake_customer_support.csat
