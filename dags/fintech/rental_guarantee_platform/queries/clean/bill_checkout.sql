6db849138ed5dbb6bf898e913d1d7b1df2ef9004
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-11-28 15:49:35 -0300

    AEFF-2522 remove fields (#16992)
    * AEFF-2522 remove fields
    
    * update metadada

--- dags/fintech/rental_guarantee_platform/queries/clean/bill_checkout.sql
+++ dags/fintech/rental_guarantee_platform/queries/clean/bill_checkout.sql
@@ -10,10 +10,7 @@ SELECT
     due_date AS dt_due,
     expired AS ts_expired,
     created_at AS ts_created,
-    updated_at AS ts_updated,
-    year,
-    month,
-    day
+    updated_at AS ts_updated
 FROM
     datalake_rental_guarantee_platform_raw.bill
 QUALIFY
