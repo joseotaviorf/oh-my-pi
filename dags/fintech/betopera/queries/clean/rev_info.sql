4536ff084feb7c837339866ea994a787610f5213
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2024-11-28 09:44:12 -0300

    fix betopera (#16966)

--- dags/fintech/betopera/queries/clean/rev_info.sql
+++ dags/fintech/betopera/queries/clean/rev_info.sql
@@ -1,5 +1,10 @@
 SELECT
     rev,
-    TO_TIMESTAMP(revtstmp/1000) AS ts_created
+    TO_TIMESTAMP(revtstmp/1000) AS ts_created,
+    year,
+    month,
+    day
 FROM
     datalake_betopera_raw.revinfo
+WHERE
+    MAKE_DATE(year,month,day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
