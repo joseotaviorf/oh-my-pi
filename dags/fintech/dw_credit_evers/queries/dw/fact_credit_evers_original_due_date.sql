e35ae33e6aa48b5482071e1b09a1e8a21345d8d7
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2024-12-05 20:28:29 -0300

    Aeff 1119 (#17152)
    * DFFRS-1119 new ever clean
    
    * update dependencies

--- dags/fintech/dw_credit_evers/queries/dw/fact_credit_evers_original_due_date.sql
+++ dags/fintech/dw_credit_evers/queries/dw/fact_credit_evers_original_due_date.sql
@@ -4,7 +4,6 @@ WITH debtors_all_time AS (
     id_contract_ebdb,
     id_contract_retsuko,
     id_proposal,
-    is_fraud,
     MONTHS_BETWEEN(
       DATE_ADD(
         LAST_DAY(
@@ -29,9 +28,51 @@ WITH debtors_all_time AS (
     ts_signature,
     ts_due
   FROM
-    datalake_invoice.credit_invoice_original_due_date_homolog
+    dw_credit_evers.fact_ever_invoices
   WHERE
     is_debt_forgiveness IS FALSE
+    AND ts_signature >= DATE('2018-01-01')
+    AND purpose IN ('monthly', 'onboarding')
+    AND (paid_amount IS NULL
+      OR status <> 'divergent-payment'
+        OR (status LIKE 'divergent-payment'
+          AND (paid_amount IS NOT NULL
+            AND (due_amount + paid_amount)/due_amount < 0.01)))
+),
+debtors_agreement AS (
+  SELECT
+    id_invoice,
+    id_contract_ebdb,
+    id_contract_retsuko,
+    id_proposal,
+    MONTHS_BETWEEN(
+      DATE_ADD(
+        LAST_DAY(
+          ADD_MONTHS(CURRENT_DATE(), -1)), 1
+      ),
+      DATE_ADD(
+        LAST_DAY(
+          ADD_MONTHS(ts_signature,-1)), 1
+      )
+    ) AS months_of_contract,
+    CASE
+      WHEN is_over_120 THEN 120
+      WHEN is_over_90 THEN 90
+      WHEN is_over_60 THEN 60
+      WHEN is_over_40 THEN 40
+      WHEN is_over_30 THEN 30
+      WHEN is_over_15 THEN 15
+    END AS invoice_over_number,
+    due_amount,
+    original_due_date,
+    dt_contract_updated,
+    ts_signature,
+    ts_due
+  FROM
+    dw_credit_evers.fact_ever_invoices
+  WHERE
+    is_debt_forgiveness IS FALSE
+    AND is_agreement = TRUE
     AND ts_signature >= DATE('2018-01-01')
     AND purpose IN ('monthly', 'onboarding')
     AND (paid_amount IS NULL
@@ -52,36 +93,8 @@ ever_array AS (
       ARRAY(15, 30, 40, 60, 90, 120)
     ) AS contract_ever_number
 ),
-ever_rule_fraud AS (
-  SELECT
-      id_invoice,
-      id_contract_ebdb,
-      id_contract_retsuko,
-      id_proposal,
-      CAST(months_of_contract AS INTEGER) AS months_of_contract,
-      ma.contract_mob_number,
-      ea.contract_ever_number,
-      CASE
-            WHEN invoice_over_number >= ea.contract_ever_number
-              AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
-                        (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
-            ELSE 0
-          END AS flag_ever,
-      invoice_over_number,
-      FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
-      due_amount,
-      ts_signature,
-      DATE_ADD(ts_signature,INT(contract_mob_number)) AS dt_reference,
-      dt_contract_updated
-  FROM
-      debtors_all_time
-  CROSS JOIN
-      mob_array AS ma
-  CROSS JOIN
-      ever_array AS ea
-),
 ever_rule AS (
-SELECT
+  SELECT
     id_invoice,
     id_contract_ebdb,
     id_contract_retsuko,
@@ -90,11 +103,11 @@ SELECT
     ma.contract_mob_number,
     ea.contract_ever_number,
     CASE
-          WHEN invoice_over_number >= ea.contract_ever_number
-            AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
-                      (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
-          ELSE 0
-        END AS flag_ever,
+      WHEN invoice_over_number >= ea.contract_ever_number
+      AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
+          (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
+      ELSE 0
+    END AS flag_ever,
     invoice_over_number,
     FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
     due_amount,
@@ -107,36 +120,36 @@ CROSS JOIN
     mob_array AS ma
 CROSS JOIN
     ever_array AS ea
-WHERE 
-    is_fraud IS FALSE
 ),
-calculate_final_ever AS (
+ever_rule_agreement AS (
 SELECT
+    id_invoice,
     id_contract_ebdb,
     id_contract_retsuko,
     id_proposal,
-    months_of_contract,
-    contract_mob_number,
-    contract_ever_number,
-    MAX(IF(flag_ever = 1, max_invoice_mob, NULL)) AS invoice_anchor,
-    CAST(MAX(flag_ever) AS BOOLEAN) AS is_ever,
-    CAST(SUM(flag_ever) AS INTEGER) AS num_overs_in_mob_window,
-    SUM(IF(flag_ever = 1, due_amount, 0)) AS total_due_amount_in_mob_window,
-    CAST(SUM(IF(invoice_over_number >= contract_ever_number,1,0)) AS INTEGER) AS num_total_overs,
-    SUM(IF(invoice_over_number >= contract_ever_number,due_amount,0)) AS total_due_amount,
+    CAST(months_of_contract AS INTEGER) AS months_of_contract,
+    ma.contract_mob_number,
+    ea.contract_ever_number,
+    CASE
+      WHEN invoice_over_number >= ea.contract_ever_number
+      AND (12*(DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "y") - DATE_FORMAT(ts_signature, "y")) +
+          (DATE_FORMAT(DATE_ADD(original_due_date, ea.contract_ever_number), "M")-DATE_FORMAT(ts_signature, "M"))) <= ma.contract_mob_number THEN 1
+      ELSE 0
+    END AS flag_ever,
+    invoice_over_number,
+    FIRST_VALUE(id_invoice) OVER(PARTITION BY id_contract_ebdb, id_proposal ORDER BY invoice_over_number DESC,original_due_date ASC) AS max_invoice_mob,
+    due_amount,
     ts_signature,
-    dt_reference,
+    DATE_ADD(ts_signature,INT(contract_mob_number)) AS dt_reference,
     dt_contract_updated
-FROM ever_rule
-GROUP BY
-    1,2,3,4,5,6,13,14,15
-HAVING
-    (is_ever = FALSE
-        AND (months_of_contract > contract_mob_number)
-            OR (is_ever = TRUE)
-            )
+FROM
+    debtors_agreement
+CROSS JOIN
+    mob_array AS ma
+CROSS JOIN
+    ever_array AS ea
 ),
-calculate_final_ever_fraud AS (
+calculate_final_ever AS (
   SELECT
     id_contract_ebdb,
     id_contract_retsuko,
@@ -145,6 +158,7 @@ calculate_final_ever_fraud AS (
     contract_mob_number,
     contract_ever_number,
     MAX(IF(flag_ever = 1, max_invoice_mob, NULL)) AS invoice_anchor,
+    MAX(max_invoice_mob) AS invoice_origin,
     CAST(MAX(flag_ever) AS BOOLEAN) AS is_ever,
     CAST(SUM(flag_ever) AS INTEGER) AS num_overs_in_mob_window,
     SUM(IF(flag_ever = 1, due_amount, 0)) AS total_due_amount_in_mob_window,
@@ -153,39 +167,99 @@ calculate_final_ever_fraud AS (
     ts_signature,
     dt_reference,
     dt_contract_updated
-FROM ever_rule_fraud
-GROUP BY
-    1,2,3,4,5,6,13,14,15
+FROM 
+  ever_rule
+GROUP BY 1,2,3,4,5,6,14,15,16
 HAVING
-    (is_ever = FALSE
-        AND (months_of_contract > contract_mob_number)
-            OR (is_ever = TRUE)
-            )
+  (is_ever = FALSE
+  AND (months_of_contract > contract_mob_number)
+  OR (is_ever = TRUE)
+  )
+),
+calculate_final_agreement AS (
+  SELECT
+    id_contract_ebdb,
+    id_contract_retsuko,
+    id_proposal,
+    months_of_contract,
+    contract_mob_number,
+    contract_ever_number,
+    MAX(IF(flag_ever = 1, max_invoice_mob, NULL)) AS invoice_anchor,
+    MAX(max_invoice_mob) AS invoice_origin,
+    CAST(MAX(flag_ever) AS BOOLEAN) AS is_ever,
+    CAST(SUM(flag_ever) AS INTEGER) AS num_overs_in_mob_window,
+    SUM(IF(flag_ever = 1, due_amount, 0)) AS total_due_amount_in_mob_window,
+    CAST(SUM(IF(invoice_over_number >= contract_ever_number,1,0)) AS INTEGER) AS num_total_overs,
+    SUM(IF(invoice_over_number >= contract_ever_number,due_amount,0)) AS total_due_amount,
+    ts_signature,
+    dt_reference,
+    dt_contract_updated,
+    TRUE as is_agreement
+FROM ever_rule_agreement
+GROUP BY 1,2,3,4,5,6,14,15,16
+HAVING
+  (is_ever = FALSE
+  AND (months_of_contract > contract_mob_number)
+  OR (is_ever = TRUE)
+  )
+),
+base_ever AS (
+  SELECT 
+  ev.*,
+  IF(ag.is_agreement = TRUE, TRUE, FALSE) AS is_agreement
+  FROM 
+    calculate_final_ever ev
+  LEFT JOIN 
+    calculate_final_agreement ag
+    ON ev.id_contract_ebdb = ag.id_contract_ebdb
+    AND ev.contract_mob_number = ag.contract_mob_number
+    AND ev.contract_ever_number = ag.contract_ever_number
+  UNION ALL
+  SELECT 
+    *
+  FROM 
+    calculate_final_agreement
+  WHERE 
+    id_contract_ebdb NOT IN (SELECT id_contract_ebdb FROM calculate_final_ever)
+),
+dt_cancellation AS (
+SELECT
+  sk_negotiation,
+  id_contract,
+  dt_cancellation
+FROM 
+  dw_collection_recovery.fact_negotiation
+WHERE 
+  creditor = 'IQ QuintoAndar'
+  AND is_down_payment_paid = TRUE
+  AND negotiation_status IN ('broken','finished','offset')
+  AND number_of_installments > 1
+  AND promisse_payment_method <> 'CARTÃO'
+QUALIFY row_number() 
+  OVER ( PARTITION BY id_contract  ORDER BY CASE WHEN negotiation_status = 'broken' THEN 1 ELSE 0 END DESC,sk_negotiation ) = 1
 )
 SELECT
-    f.id_contract_ebdb,
-    f.id_contract_retsuko,
-    f.id_proposal,
-    f.months_of_contract,
-    f.contract_mob_number,
-    f.contract_ever_number,
-    er.invoice_anchor,
-    f.invoice_anchor AS invoice_anchor_fraud,
-    er.is_ever,
-    f.is_ever AS is_ever_fraud,
-    er.num_overs_in_mob_window,
-    f.num_overs_in_mob_window AS num_overs_in_mob_window_fraud,
-    er.num_total_overs,
-    f.num_total_overs AS num_total_overs_fraud,
-    er.total_due_amount,
-    f.total_due_amount AS total_due_amount_fraud,
-    f.ts_signature,
-    f.dt_reference,
-    f.dt_contract_updated
-FROM 
-  calculate_final_ever_fraud AS f
+  b.id_contract_ebdb,
+  b.id_contract_retsuko,
+  b.id_proposal,
+  b.invoice_anchor,
+  b.invoice_origin,
+  b.months_of_contract,
+  b.contract_mob_number,
+  b.contract_ever_number,
+  b.num_overs_in_mob_window,
+  b.total_due_amount_in_mob_window,
+  b.num_total_overs,
+  b.total_due_amount,
+  b.is_ever,
+  b.is_agreement,
+  b.ts_signature,
+  b.dt_reference,
+  b.dt_contract_updated,
+  c.dt_cancellation
+FROM
+  base_ever b
 LEFT JOIN 
-  calculate_final_ever AS er
-  ON er.id_contract_retsuko = f.id_contract_retsuko
-  AND er.contract_mob_number = f.contract_mob_number
-  AND er.contract_ever_number = f.contract_ever_number
+  dt_cancellation c
+  ON b.id_contract_ebdb = c.id_contract
+  AND b.is_agreement = TRUE
---
+++ dags/fintech/dw_credit_evers/queries/dw/fact_ever_invoices.sql
@@ -0,0 +1,311 @@
+WITH debt_forgiveness AS (
+    SELECT
+        i.id_external,
+        i.id_contract_external,
+        i.id,
+        i.id_contract,
+        i.purpose,
+        i.status,
+        NULL as dt_cancellation,
+        i.due_amount
+    FROM datalake_retsuko.invoice AS i
+    INNER JOIN datalake_retsuko.debt_forgiveness AS d
+    ON i.id_external = d.id_invoice
+    WHERE status IN ("not-payable", "canceled")
+),
+fraud AS ( 
+    SELECT
+        i.id_external,
+        i.id_contract_external,
+        i.id,
+        i.id_contract,
+        i.purpose,
+        i.status,
+        NULL as dt_cancellation,
+        SUM(b.value_sign_bill_item) as due_amount 
+    FROM datalake_retsuko.invoice AS i
+    INNER JOIN datalake_retsuko.bill_items AS b
+    ON i.id_external = b.id_invoice
+    WHERE 
+        i.status = "not-payable" 
+    AND b.bill_item = "LOSS" 
+    AND LOWER(b.bill_item_description) LIKE "%ação boletos%"
+    GROUP BY 1,2,3,4,5,6,7
+),
+agreement AS ( 
+    SELECT
+        i.id_external,
+        i.id_contract_external,
+        i.id,
+        i.id_contract,
+        i.purpose,
+        i.status,
+        fn.dt_cancellation,
+        SUM(b.value_sign_bill_item) as due_amount 
+    FROM 
+      datalake_retsuko.invoice AS i
+    INNER JOIN 
+      datalake_retsuko.bill_items AS b
+      ON i.id_external = b.id_invoice
+    INNER JOIN
+      datalake_trato_feito_clean.debt d
+      ON i.id_external = d.id_external 
+    INNER JOIN 
+      datalake_trato_feito_clean.negotiation n
+      ON n.id = d.id_negotiation
+    LEFT JOIN
+      dw_collection_recovery.fact_negotiation fn
+      ON fn.sk_negotiation = n.id_collector_external
+    WHERE
+        n.status in ('offset','broken','finished')
+    AND i.purpose in ('monthly','onboarding')
+    AND fn.creditor = 'IQ QuintoAndar'
+    AND fn.is_down_payment_paid = true
+    AND fn.negotiation_status = 'broken'
+    AND fn.number_of_installments > 1
+    AND fn.promisse_payment_method <> 'CARTÃO' 
+    GROUP BY 1,2,3,4,5,6,7
+),
+credit_holidays AS (
+        SELECT
+            12 AS day,
+            10 AS month,
+            "Nossa Sra. Aparecida - Padroeira do Brasil" AS holiday_name
+        UNION ALL
+        SELECT
+            1 AS day,
+            1 AS month,
+            "Confraternização Universal" AS holiday_name
+        UNION ALL
+        SELECT
+            15 AS day,
+            11 AS month,
+            "Proclamação da República" AS holiday_name
+        UNION ALL
+        SELECT
+            7 AS day,
+            9 AS month,
+            "Independência do Brasil" AS holiday_name
+        UNION ALL
+        SELECT
+            1 AS day,
+            5 AS month,
+            "Dia do Trabalho" AS holiday_name
+        UNION ALL
+        SELECT
+            21 AS day,
+            4 AS month,
+            "Tiradentes" AS holiday_name
+        UNION ALL
+        SELECT
+            31 AS day,
+            12 AS month,
+            "Ano Novo" AS holiday_name
+        UNION ALL
+        SELECT
+        2 AS day,
+        11 AS month,
+        "Finados" AS holiday_name
+        UNION ALL
+        SELECT
+            24 AS day,
+            12 AS month,
+            "Natal" AS holiday_name
+        UNION ALL
+        SELECT
+            25 AS day,
+            12 AS month,
+            "Natal" AS holiday_name
+), 
+invoices AS (
+    SELECT
+        inv.id,
+        inv.id_external AS id_invoice,
+        inv.id_contract,
+        inv.status,
+        inv.paid_amount,
+        inv.ts_paid,
+        inv.ts_created,
+        inv.status,
+        inv.due_amount,
+        inv.ts_due,
+        inv.accrual_year_month,
+        DATE_ADD(ADD_MONTHS(DATE_FORMAT(CAST(UNIX_TIMESTAMP(CONCAT(CAST(inv.accrual_year_month AS STRING),'01'),'yyyyMMdd') AS TIMESTAMP), 'yyyy-MM-dd'),1),6) AS static_original_due_date,
+        ag.dt_cancellation,
+        inv.purpose,
+        acc.type AS account_type,
+        IF(df.id_external IS NOT NULL,TRUE,FALSE) AS is_debt_forgiveness,
+        IF(ag.id_external IS NOT NULL,TRUE,FALSE) AS is_agreement
+    FROM
+        datalake_retsuko.invoice AS inv
+    JOIN
+        datalake_retsuko_clean.account AS acc
+    ON
+        inv.id_contract = acc.id_contract
+    LEFT JOIN debt_forgiveness df
+        ON df.id_external = inv.id_external
+    LEFT JOIN agreement ag 
+        ON ag.id_external = inv.id_external
+    WHERE
+        acc.type = 'tenant'
+        AND (inv.due_amount < 0 OR df.id_external IS NOT NULL OR ag.id_external IS NOT NULL)
+), 
+invoices_fixed AS (
+    SELECT
+        inv.id,
+        inv.id_invoice,
+        inv.id_contract,
+        inv.status,
+        inv.paid_amount,
+        inv.ts_paid,
+        inv.ts_created,
+        inv.status,
+        inv.due_amount,
+        inv.ts_due,
+        inv.accrual_year_month,
+        inv.static_original_due_date,
+        (CASE
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 24
+                AND DAYOFWEEK(DATE_ADD(inv.static_original_due_date,2)) = 7 THEN DATE_ADD(inv.static_original_due_date, 4)
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 24
+                AND DAYOFWEEK(DATE_ADD(inv.static_original_due_date,2)) = 1 THEN DATE_ADD(inv.static_original_due_date, 3)
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 25
+                AND DAYOFWEEK(DATE_ADD(inv.static_original_due_date,1)) = 7 THEN DATE_ADD(inv.static_original_due_date, 3)
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 25
+                AND DAYOFWEEK(DATE_ADD(inv.static_original_due_date,1)) = 1 THEN DATE_ADD(inv.static_original_due_date, 2)
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 24 THEN DATE_ADD(inv.static_original_due_date, 2)
+            WHEN
+                hl.holiday_name = 'Natal'
+                AND DAY(inv.static_original_due_date) = 25 THEN DATE_ADD(inv.static_original_due_date, 1)
+            WHEN
+                hl.holiday_name IS NOT NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 6 THEN DATE_ADD(inv.static_original_due_date,3)
+            WHEN
+                hl.holiday_name IS NOT NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 2 THEN DATE_ADD(inv.static_original_due_date,1)
+            WHEN
+                hl.holiday_name IS NOT NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 7 THEN DATE_ADD(inv.static_original_due_date,2)
+            WHEN
+                hl.holiday_name IS NOT NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 1 THEN DATE_ADD(inv.static_original_due_date,1)
+            WHEN
+                hl.holiday_name IS NOT NULL THEN DATE_ADD(inv.static_original_due_date,1)
+            WHEN
+                hl.holiday_name IS NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 7 THEN DATE_ADD(inv.static_original_due_date,2)
+            WHEN
+                hl.holiday_name IS NULL
+                AND DAYOFWEEK(inv.static_original_due_date) = 1 THEN DATE_ADD(inv.static_original_due_date,1)
+        ELSE
+        inv.static_original_due_date END) AS original_due_date,
+        inv.dt_cancellation,
+        inv.purpose,
+        inv.account_type,
+        inv.is_debt_forgiveness,
+        inv.is_agreement
+    FROM
+        invoices  AS inv
+    LEFT JOIN
+        credit_holidays AS hl
+        ON
+            hl.month = MONTH(inv.static_original_due_date)
+            AND hl.day = DAY(inv.static_original_due_date)
+)
+SELECT
+    inv.id,
+    inv.id_invoice,
+    inv.id_contract AS id_contract_retsuko,
+    ebdb_cntrct.id AS id_contract_ebdb,
+    ebdb_cntrct.id_proposal,
+    inv.purpose,
+    inv.account_type,
+    inv.status,
+    inv.due_amount,
+    inv.paid_amount,
+    inv.ts_due,
+    DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) AS late_days,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 10 THEN TRUE
+    ELSE FALSE END) AS is_over_10,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 15 THEN TRUE
+    ELSE FALSE END) AS is_over_15,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 20 THEN TRUE
+    ELSE FALSE END) AS is_over_20,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 30 THEN TRUE
+    ELSE FALSE END) AS is_over_30,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 40 THEN TRUE
+    ELSE FALSE END) AS is_over_40,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 50 THEN TRUE
+    ELSE FALSE END) AS is_over_50,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 60 THEN TRUE
+    ELSE FALSE END) AS is_over_60,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 70 THEN TRUE
+    ELSE FALSE END) AS is_over_70,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 80 THEN TRUE
+    ELSE FALSE END) AS is_over_80,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 90 THEN TRUE
+    ELSE FALSE END) AS is_over_90,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 100 THEN TRUE
+    ELSE FALSE END) AS is_over_100,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 110 THEN TRUE
+    ELSE FALSE END) AS is_over_110,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 120 THEN TRUE
+    ELSE FALSE END) AS is_over_120,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 140 THEN TRUE
+    ELSE FALSE END) AS is_over_140,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 160 THEN TRUE
+    ELSE FALSE END) AS is_over_160,
+    (CASE
+        WHEN DATEDIFF(COALESCE(inv.ts_paid, CURRENT_DATE), inv.original_due_date) >= 180 THEN TRUE
+    ELSE FALSE END) AS is_over_180,
+    inv.is_debt_forgiveness,
+    inv.is_agreement,
+    1 as is_fraud,
+    inv.accrual_year_month,
+    inv.original_due_date,
+    inv.ts_paid,
+    inv.dt_cancellation,
+    rsk_cntrct.ts_signature,
+    DATE(ebdb_cntrct.ts_updated) AS dt_contract_updated
+FROM
+    invoices_fixed AS inv
+JOIN
+    datalake_retsuko_clean.contract AS rsk_cntrct
+ON
+    inv.id_contract = rsk_cntrct.id
+JOIN
+    datalake_ebdb_clean.contract AS ebdb_cntrct
+ON
+    ebdb_cntrct.id = rsk_cntrct.id_external
+WHERE
+    inv.original_due_date IS NOT NULL
+    AND rsk_cntrct.ts_signature IS NOT NULL
+    AND DATEDIFF(inv.original_due_date, rsk_cntrct.ts_signature) > 0
+    AND inv.status != 'canceled'
+ORDER BY
+    inv.original_due_date ASC
