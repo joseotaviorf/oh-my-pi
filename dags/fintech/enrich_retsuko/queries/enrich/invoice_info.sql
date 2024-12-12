9796139d1304664c4d2e32ee1e9cbe33378be524
Bianca Garcia <biigarciam@gmail.com>
2024-12-12 15:05:14 -0300

    Fix user (#17288)

--- dags/fintech/enrich_retsuko/queries/enrich/invoice_info.sql
+++ dags/fintech/enrich_retsuko/queries/enrich/invoice_info.sql
@@ -8,23 +8,14 @@ WITH invoice_replace AS (
         i.negotiation_status,
         i.closing_mode,
         i.paid_via,
-        MAX(CASE WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') NOT LIKE '%contract%' THEN REPLACE(at.type, '-', ' ')
-                WHEN REPLACE(af.type, '-', ' ') NOT LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN REPLACE(af.type, '-', ' ')
-                WHEN REPLACE(af.type, '-', ' ') LIKE '%contract%' AND REPLACE(at.type, '-', ' ') LIKE '%contract%' THEN 'contract'
-            ELSE NULL
-        END) AS invoice_user,
+        REPLACE(ai.type, '-', ' ') AS invoice_user,
         i.ts_sent,
         i.ts_due,
         i.ts_paid,
         i.ts_canceled
     FROM datalake_retsuko.invoice i
-    LEFT JOIN datalake_retsuko.entry e
-            ON e.id_invoice = i.id
-    LEFT JOIN datalake_retsuko_clean.account af
-        ON e.id_from_account = af.id
-    LEFT JOIN datalake_retsuko_clean.account at
-        ON e.id_to_account = at.id
-    GROUP BY 1,2,3,4,5,6,7,9,10,11,12
+    LEFT JOIN datalake_retsuko_clean.account ai
+        ON i.id_account = ai.id
 )
 SELECT
     ir.id_invoice,
