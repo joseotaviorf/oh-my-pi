9c0a744bbfcb9f4f4005bb09de0dc39cf1696c29
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2024-12-13 11:39:52 -0300

    REMOVING PARTITION (#17324)

--- dags/for_sale/enrich_visit/queries/enrich/visit_status_events.sql
+++ dags/for_sale/enrich_visit/queries/enrich/visit_status_events.sql
@@ -115,10 +115,7 @@ SELECT
   v.channel,
   v.country_code,
   v.is_visit_last_event,
-  v.ts_event_created,
-  YEAR(v.ts_event_created) AS year,
-  MONTH(v.ts_event_created) AS month,
-  DAY(v.ts_event_created) AS day
+  v.ts_event_created
 FROM
    visit AS v
 LEFT JOIN
