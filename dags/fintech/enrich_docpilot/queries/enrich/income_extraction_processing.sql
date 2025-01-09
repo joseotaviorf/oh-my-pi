046d5a0bef31c1b1e036055c20a73da82234d3fd
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-01-09 14:29:06 -0300

    [DFFRS-1404] Update datalake_docpilot.income_extraction_processing (#17425)
    * Add: id_proposal, id_user and is_main_user
    
    * Update: table metadata
    
    * Update: income_extraction_processing dependencies
    
    * Add: cpf
    
    * Update: add cpf to metadata
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
+++ dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
@@ -34,32 +34,49 @@ WITH extract_json_data AS (
   FROM
     datalake_docpilot_clean.income_extraction
 ),
+user_folder AS (
+SELECT DISTINCT
+  id_folder,
+  id_context_external AS id_proposal,
+  cpf
+FROM datalake_docx.personal_documentation
+),
 transform_income_data AS (
 SELECT
-    id_external,
-    external_source,
-    processing_result,
-    documents_type,
-    ARRAY_SIZE(extract_errors) AS number_of_documents_with_errors,
-    ARRAY_SIZE(documents) AS number_of_processed_documents,
+    CAST(jd.id_external AS bigint) AS id_document,
+    CAST(d.id_context_external AS bigint) AS id_proposal,
+    uf.cpf,
+    jd.external_source,
+    jd.processing_result,
+    jd.documents_type,
+    ARRAY_SIZE(jd.extract_errors) AS number_of_documents_with_errors,
+    ARRAY_SIZE(jd.documents) AS number_of_processed_documents,
   REPLACE(
-    REPLACE((CAST(extract_errors AS string)), '{{', ''),
+    REPLACE((CAST(jd.extract_errors AS string)), '{{', ''),
     '}}',
     ''
   ) AS error_type,
   REPLACE(
-    REPLACE((CAST(bank_statement_type AS string)), '{{', ''),
+    REPLACE((CAST(jd.bank_statement_type AS string)), '{{', ''),
     '}}',
     ''
   ) AS bank_statement_type,
-  IF(CAST(metadata_is_suspicious AS string) LIKE '%true%', TRUE, FALSE) AS any_suspicious_document,
-  IF(CAST(metadata_dates_divergent AS string) LIKE '%true%', TRUE, FALSE) AS any_divergent_date,
-  TRANSFORM(map_values(from_json(extracted_income, 'MAP<STRING, DECIMAL>')), x -> X::BIGINT) AS extracted_incomes,
-  ts_created,
-  ts_updated,
-  ts_load
+  IF(CAST(jd.metadata_is_suspicious AS string) LIKE '%true%', TRUE, FALSE) AS any_suspicious_document,
+  IF(CAST(jd.metadata_dates_divergent AS string) LIKE '%true%', TRUE, FALSE) AS any_divergent_date,
+  TRANSFORM(map_values(from_json(jd.extracted_income, 'MAP<STRING, DECIMAL>')), x -> X::BIGINT) AS extracted_incomes,
+  jd.ts_created,
+  jd.ts_updated,
+  jd.ts_load
 FROM
-  extract_json_data
+  extract_json_data AS jd
+LEFT JOIN
+  datalake_docx_clean.document AS d
+    ON jd.id_external = d.id
+LEFT JOIN
+  user_folder AS uf
+    ON (d.id_folder = uf.id_folder AND CAST(d.id_context_external AS bigint) = uf.id_proposal)
+QUALIFY
+  ROW_NUMBER() OVER (PARTITION BY d.id_context_external, uf.cpf ORDER BY jd.ts_created DESC) = 1
 ),
 explode_income AS (
   SELECT
@@ -75,10 +92,22 @@ calculate_avg_income AS (
   FROM
     explode_income
   GROUP BY id_external
+),
+get_user AS (
+  SELECT
+    id AS id_user,
+    cpf
+  FROM
+  datalake_ebdb_user.user
+QUALIFY
+  ROW_NUMBER() OVER (PARTITION BY cpf ORDER BY ts_created DESC) = 1
 )
 
 SELECT
-    id.id_external,
+    id.id_document,
+    id.id_proposal,
+    u.id_user,
+    id.cpf,
     id.external_source,
     id.processing_result,
     id.documents_type,
@@ -88,6 +117,7 @@ SELECT
     id.bank_statement_type,
     id.extracted_incomes,
     avg.avg_extracted_income,
+    IF(u.id_user IS NOT NULL, TRUE, FALSE) AS is_main_user,
     id.any_suspicious_document,
     id.any_divergent_date,
     id.ts_created,
@@ -97,4 +127,6 @@ FROM
   transform_income_data AS id
 LEFT JOIN
   calculate_avg_income AS avg
-    ON id.id_external = avg.id_external
+    ON id.id_document = avg.id_external
+LEFT JOIN get_user AS u
+    ON u.cpf = id.cpf
