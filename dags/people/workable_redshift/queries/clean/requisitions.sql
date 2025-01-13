b8357196f6d5f29e4d297262b6f7f3d1a66c3b19
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-01-13 09:03:53 -0300

    [PEOPLE] Remove Test DAG and Adjust Extraction to Full for Requisitions Table (#17456)
    * remove workable full and update extraction type from requisitions
    
    * add dependencies
    
    * remove filter

--- dags/people/workable_redshift/queries/clean/requisitions.sql
+++ dags/people/workable_redshift/queries/clean/requisitions.sql
@@ -39,10 +39,9 @@ SELECT
     -- timestamps
     created_at AS ts_created,
     updated_at AS ts_updated,
-    NOW() AS ts_load
-FROM 
-    datalake_workable_redshift_raw.requisitions
-WHERE 
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-QUALIFY 
-    updated_at = MAX(updated_at) OVER (PARTITION BY id)
+    NOW () AS ts_load
+FROM
+    datalake_workable_redshift_raw.requisitions QUALIFY updated_at = MAX(updated_at) OVER (
+        PARTITION BY
+            id
+    )
