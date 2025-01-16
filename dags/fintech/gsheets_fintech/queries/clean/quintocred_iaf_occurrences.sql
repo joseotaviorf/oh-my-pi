32bd284fafb537360d9b7230cb7103f524748c2c
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-01-16 10:37:07 -0300

    updade iaf occurrences (#17503)

--- dags/fintech/gsheets_fintech/queries/clean/quintocred_iaf_occurrences.sql
+++ dags/fintech/gsheets_fintech/queries/clean/quintocred_iaf_occurrences.sql
@@ -1,10 +1,10 @@
 SELECT
     NULLIF(code_status, '') AS code_status,
     NULLIF(desc_status, '') AS desc_status,
+    NULLIF(amount, '') AS amount,
     NULLIF(tab, '') AS tab,
     NULLIF(effort, '') AS effort,
     NULLIF(massive, '') AS massive,
     NULLIF(channel, '') AS channel
-    
 FROM
     datalake_gsheets_raw.quintocred_iaf_occurrences
