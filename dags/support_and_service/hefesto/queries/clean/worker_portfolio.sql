4499de01404fcec15d4cbaf90395eadc0ccb2829
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-01-17 14:54:08 -0300

    Fix/worker portfolio id_user column (#17517)
    * Update worker_portfolio user identification column

--- dags/support_and_service/hefesto/queries/clean/worker_portfolio.sql
+++ dags/support_and_service/hefesto/queries/clean/worker_portfolio.sql
@@ -1,8 +1,7 @@
 SELECT
     id AS id_portfolio,
     worker_id AS id_worker,
-    user_id as user,
-    user_identifier as id_user,
+    COALESCE(user_id, user_identifier) AS id_user,
     contract_id as id_contract,
     entity_origin_id AS id_entity_origin,
     context_identifier, 
