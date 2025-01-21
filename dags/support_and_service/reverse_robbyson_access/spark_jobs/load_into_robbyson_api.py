cfe25b54fb9cd6bd52f38a45c0507bf70e3eaef5
Rodrigo Arnaldo Kreuzberg <rodrigokreuzberg@gmail.com>
2025-01-21 08:41:57 -0400

    Added trigger waiting time (#17535)

--- dags/support_and_service/reverse_robbyson_access/spark_jobs/load_into_robbyson_api.py
+++ dags/support_and_service/reverse_robbyson_access/spark_jobs/load_into_robbyson_api.py
@@ -15,7 +15,7 @@ JOB_NAME = "load_into_robbyson_api"
 logging.getLogger("py4j").setLevel(logging.ERROR)
 logger = QuintoAndarLogger(JOB_NAME)
 
-def create_results_payload(results_table_path: str, agent_table_path: str, key_join_tables_fact: str, key_join_tables_dim: str, analyst_key_column: str, start_date: str, end_date: str, context: str) -> list:
+def create_results_payload(results_table_path, agent_table_path, key_join_tables_fact, key_join_tables_dim, analyst_key_column, start_date, end_date, context):
     indicators_df = spark.sql(f"SELECT * FROM datalake_static_files_ss.robbyson_indicators WHERE context = '{context}'")
     indicators_content = {row['id_indicator']: row['attributes'] for row in indicators_df.collect()}
 
@@ -48,22 +48,13 @@ def create_results_payload(results_table_path: str, agent_table_path: str, key_j
             results_list.append(results_json)
     return results_list
 
-def parse_arguments() -> dict:
-    """
-    Parse the arguments passed to the job.
-    Returns a dict with the api_url, endpoint, execution date and configurations.
-    """
-
+def parse_arguments():
     parser = ArgumentParser(description=JOB_NAME)
 
     parser.add_argument("dag_name", help="Name of the DAG")
     parser.add_argument("environment", help="forno/prod values")
-    parser.add_argument(
-        "start_date", help="Date of the execution in the format YYYY-MM-DD"
-    )
-    parser.add_argument(
-        "end_date", help="End date of the execution in the format YYYY-MM-DD"
-    )
+    parser.add_argument("start_date", help="Date of the execution in the format YYYY-MM-DD")
+    parser.add_argument("end_date", help="End date of the execution in the format YYYY-MM-DD")
     parser.add_argument("dispatch_limits", help="Dispatch limits")
     parser.add_argument("endpoint", help="Api endpoint, where we will send the data")
     parser.add_argument("context", help="Line responsible for processing")
@@ -75,85 +66,38 @@ def parse_arguments() -> dict:
 
     args = parser.parse_args()
 
-    dag_name = args.dag_name
-    environment = args.environment
-    context = args.context
-    start_date = args.start_date
-    end_date = args.end_date
-    endpoint = args.endpoint
-    dispatch_limits = args.dispatch_limits
-    results_table_path = args.results_table_path
-    agent_table_path = args.agent_table_path
-    key_join_tables_fact = args.key_join_tables_fact
-    key_join_tables_dim = args.key_join_tables_dim
-    analyst_key_column = args.analyst_key_column
+    return vars(args)
 
-    return {
-        "dag_name": dag_name,
-        "environment": environment,
-        "context": context,
-        "start_date": start_date,
-        "end_date": end_date,
-        "endpoint": endpoint,
-        "dispatch_limits": dispatch_limits,
-        "results_table_path": results_table_path,
-        "agent_table_path": agent_table_path,
-        "key_join_tables_fact": key_join_tables_fact,
-        "key_join_tables_dim": key_join_tables_dim,
-        "analyst_key_column": analyst_key_column
-    }
+def send_payload_in_batches(api_url, endpoint, transaction_id, headers, payload, batch_size):
+    total_batches = len(payload)
+    successful_batches = 0
 
-def send_payload_in_batches(api_url: str, endpoint: str, transaction_id: str, headers: dict, payload: list, batch_size: int):
-    """
-    Envia o payload em lotes para a API usando o mesmo transaction_id.
-    """
-    total = len(payload)
-    for i in range(0, total, batch_size):
+    for i in range(0, total_batches, batch_size):
         batch = payload[i:i + batch_size]
-        response = requests.post(
-            f'{api_url}/{endpoint}/?transaction_id={transaction_id}',
-            headers=headers,
-            json=batch
-        )
-        if response.status_code != 200:
-            logger.error(f"m=Error sending batch. Batch_index={i // batch_size}, status_code={response.status_code}, response={response.content}")
-        else:
-            logger.info(f"m=Batch sent successfully. Batch_index={i // batch_size}, size={len(batch)}")
+        retry = 2  # Allow a resend attempt
 
-def send_alert_message(error_message: str, dag_name: str, environment: str):
-    """
-    Envia uma mensagem de alerta no canal configurado com o erro ocorrido.
-    """
-    base_dbutils = BaseDBUtils()
-    if base_dbutils.get_dbutils() is not None:
-        dbutils = base_dbutils.get_dbutils()
+        while retry > 0:
+            response = requests.post(
+                f'{api_url}/{endpoint}/?transaction_id={transaction_id}',
+                headers=headers,
+                json=batch
+            )
 
-    DAG_NAME = dag_name
-    config_service = ConfigurationService(DAG_NAME)
-    webhook_key = config_service.get_config("notification_webhooks_keys")["data_quality"]
-    gchat_webhook = dbutils.secrets.get(scope="quintoandar", key=webhook_key)
+            if response.status_code == 200:
+                successful_batches += len(batch)
+                logger.info(f"m=Batch sent successfully. Batch_index={i // batch_size}, size={len(batch)}")
+                break
+            else:
+                logger.error(f"m=Error sending batch. Retrying... Batch_index={i // batch_size}, status_code={response.status_code}")
+                retry -= 1
+                time.sleep(1)  # 1 second delay between attempts
 
-    message_content = (
-        f"⚠️\n"
-        f"Validation: Payload sending on `{dag_name}`\n"
-        f"Environment: *{environment}*\n"
-        f"Status: *FAILED*\n"
-        f"*Existence validation failed for `{datetime.now().strftime('%Y-%m-%d')}`\n"
-    )
-    logger.info(f"m=send_alert_message, message=sending gchat message: {message_content}")
-    message = Message(content=message_content, destination=gchat_webhook)
-    GChatService.send_message(message)
-
-def safe_post_request_with_alert(url: str, headers: dict, json_data=None):
-    try:
-        response = requests.post(url, headers=headers, json=json_data)
-        response.raise_for_status()
-        return response
-    except requests.exceptions.RequestException as e:
-        error_message = f"URL: {url}\nErro: {e}"
-        logger.error(f"m=Error during POST request., {error_message}")
-        send_alert_message(error_message, job_arguments_dict["dag_name"], job_arguments_dict["environment"])
-        return None
+    if successful_batches != total_batches:
+        logger.error(f"m=Mismatch in batches sent. Expected={total_batches}, Sent={successful_batches}")
+    else:
+        logger.info(f"m=All batches sent successfully. Total_batches={total_batches}")
+        result_request = requests.get(f'{api_url}/transactions/{transaction_id}', headers=headers).json()
+        logger.info(f"m=Transaction completed successfully, transaction_status={result_request}")
 
 def main():
     job_arguments_dict = parse_arguments()
@@ -173,21 +117,14 @@ def main():
         "accept": "application/json",
     }
 
-    transaction_response = safe_post_request_with_alert(f'{api_url}/transactions/', headers=headers)
-    if transaction_response:
-        try:
-            transaction_json = json.loads(transaction_response.content.decode('utf-8'))
-            transaction_id = transaction_json['data']['_id']
-            logger.info(f"m=Success in getting the credentials., transaction_id={transaction_id}")
-        except json.JSONDecodeError as e:
-            logger.error(f"m=Error decoding transaction response., error={e}")
-            raise SystemExit("Transaction setup failed.")
-    else:
-        raise SystemExit("Transaction setup failed due to request error.")
+    transaction_response = requests.post(f'{api_url}/transactions/', headers=headers)
 
-    logger.info(
-        f"m=Success in getting the credentials., transaction_id={transaction_id}"
-    )
+    if transaction_response.status_code == 200:
+        transaction_id = transaction_response.json()['data']['_id']
+        logger.info(f"m=Transaction started successfully, transaction_id={transaction_id}")
+    else:
+        logger.error("m=Failed to start transaction.")
+        raise SystemExit("Transaction setup failed.")
 
     results_payload = create_results_payload(
         job_arguments_dict["results_table_path"],
@@ -201,22 +138,6 @@ def main():
 
     dispatch_limits = int(job_arguments_dict["dispatch_limits"])
     send_payload_in_batches(api_url, job_arguments_dict["endpoint"], transaction_id, headers, results_payload, dispatch_limits)
-
-    requests.post(f'{api_url}/{job_arguments_dict["endpoint"]}/?transaction_id={transaction_id}', headers=headers, json=results_payload)
-    requests.post(f'{api_url}/transactions/{transaction_id}', headers=headers)
-    result_request = requests.get(f'{api_url}/transactions/{transaction_id}', headers=headers).content
-
-    transaction_close_response = safe_post_request_with_alert(f'{api_url}/transactions/{transaction_id}', headers=headers)
-
-    if not transaction_close_response:
-        logger.error("m=Failed to close transaction.")
-    else:
-        result_request = requests.get(f'{api_url}/transactions/{transaction_id}', headers=headers).content
-        logger.info(f"m=Success!!, transaction status={result_request}")
-
-    logger.info(
-        f"m=Success!!, transaction status={result_request}"
-    )
 
 if __name__ == '__main__':
     main()
