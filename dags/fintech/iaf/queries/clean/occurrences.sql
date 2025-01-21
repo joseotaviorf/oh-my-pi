eaec9547154c3847a6f2b70a4260025657b17358
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-01-21 09:28:34 -0300

    AEFF-2465 add alo cpc metrics to iaf (#17532)

--- dags/fintech/iaf/queries/clean/occurrences.sql
+++ dags/fintech/iaf/queries/clean/occurrences.sql
@@ -1,21 +1,41 @@
+WITH gs AS (
+    SELECT 
+        desc_status,	
+        tab,
+        effort,	
+        massive,	
+        channel
+    FROM 
+        datalake_gsheets_clean.quintocred_iaf_occurrences
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY desc_status ORDER BY desc_status ASC) = 1
+)
+
 SELECT
-    idempresa AS id_company,
-    idocorrencia AS id_occurrence,
-    idcredor AS id_creditor,
-    iddevedor AS id_debtor,
-    idrecebimento AS id_receipt,
-    processo AS process,
-    operador AS operator,
-    anotaint AS int_note,
-    anotaext AS ext_note,
-    descstatus AS desc_status,
-    segundostotal AS total_seconds,
-    operadortipo AS operator_type,
-    codstatus AS code_status,
-    origem AS origin,
-    horainicial AS hr_begin,
-    horafinal AS hr_end,
-    tempototal AS total_time,
-    datacad AS dt_cad
+    oc.idempresa AS id_company,
+    oc.idocorrencia AS id_occurrence,
+    oc.idcredor AS id_creditor,
+    oc.iddevedor AS id_debtor,
+    oc.idrecebimento AS id_receipt,
+    oc.processo AS process,
+    oc.operador AS operator,
+    oc.anotaint AS int_note,
+    oc.anotaext AS ext_note,
+    oc.descstatus AS desc_status,
+    oc.segundostotal AS total_seconds,
+    oc.operadortipo AS operator_type,
+    oc.codstatus AS code_status,
+    oc.origem AS origin,
+    oc.horainicial AS hr_begin,
+    oc.horafinal AS hr_end,
+    oc.tempototal AS total_time,
+    gs.tab,
+    CAST(COALESCE(gs.effort, 0) AS INTEGER) AS effort,
+    CAST(COALESCE(gs.massive, 0) AS INTEGER) AS massive,
+    gs.channel,
+    oc.datacad AS dt_cad
 FROM
-    datalake_iaf_raw.vi_319_tb_ocorrencias
+    datalake_iaf_raw.vi_319_tb_ocorrencias oc
+LEFT JOIN 
+    gs
+    ON oc.descstatus = gs.desc_status
--- dags/fintech/iaf/queries/clean/qualifies_calls.sql
+++ dags/fintech/iaf/queries/clean/qualifies_calls.sql
@@ -1,21 +1,37 @@
+WITH gs AS (
+    SELECT
+        qualifies,
+        alo,
+        cpc
+    FROM
+        datalake_gsheets_clean.quintocred_iaf_qualifies_calls
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY qualifies ORDER BY qualifies ASC) = 1
+)
+
 SELECT
-    id,
-    idempresa AS id_company,
-    idcredor AS id_creditor,
-    iddevedor AS id_debtor,
-    id_uuid,
-    idocorrencia AS id_occurrence,
-    processo AS process,
-    operador AS operator,
-    qualifica AS qualifies,
-    fone AS phone,
-    userfield AS user_field,
-    ramal AS extension,
-    disposition,
-    positivo AS positive,
-    userfield2 AS user_field_2,
-    segundostotal AS total_seconds,
-    hora_cad AS hr_cad,
-    data_cad AS dt_cad
+    qc.id,
+    qc.idempresa AS id_company,
+    qc.idcredor AS id_creditor,
+    qc.iddevedor AS id_debtor,
+    qc.id_uuid,
+    qc.idocorrencia AS id_occurrence,
+    qc.processo AS process,
+    qc.operador AS operator,
+    qc.qualifica AS qualifies,
+    qc.fone AS phone,
+    qc.userfield AS user_field,
+    qc.ramal AS extension,
+    qc.disposition,
+    qc.positivo AS positive,
+    qc.userfield2 AS user_field_2,
+    qc.segundostotal AS total_seconds,
+    CAST(COALESCE(gs.alo, 0) AS INTEGER) AS alo,
+    CAST(COALESCE(gs.cpc, 0) AS INTEGER) AS cpc,
+    qc.hora_cad AS hr_cad,
+    qc.data_cad AS dt_cad
 FROM
-    datalake_iaf_raw.vi_319_tb_qualifica_chamadas
+    datalake_iaf_raw.vi_319_tb_qualifica_chamadas qc
+LEFT JOIN 
+    gs
+    ON qc.qualifica = gs.qualifies
