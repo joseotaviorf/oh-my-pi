df049a0c2e5eecd80ad7660620e74128353e7ae1
Matheus Mesquita <matheus.sampaio@quintoandar.com.br>
2025-01-23 13:17:34 -0300

    [DFORSALE-8314] Bringing new tables to cdc (#17567)

--- dags/for_sale/ebdb_sale_test/queries/clean/appointment_change_reason_category.sql
+++
@@ -1,7 +0,0 @@
-select
-  id,
-  name,
-  description,
-  criadoem as ts_created,
-  atualizadoem as ts_updated
-from datalake_ebdb_test_raw.AppointmentChangeReasonCategory
--- dags/for_sale/ebdb_sale_test/queries/clean/shop_window_listing_business_context.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    shopWindow_id AS id_shop_window,
-    listingBusinessContext_id AS id_listing_business_context,
-    isActive AS is_active,
-    criadoEm AS ts_created,
-    atualizadoEm AS ts_updated
-FROM
-    datalake_ebdb_test_raw.shopwindow_listingbusinesscontext
