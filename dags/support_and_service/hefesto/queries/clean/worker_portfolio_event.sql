a2e0dd644c08836f267b2a66d0d672434264b710
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-02-06 13:21:07 -0300

    Fix hefesto new columns (#17719)
    * Coalesce between new and old context identifier column
    
    * Fix coalesce between portfolio identification
    
    * Update lineage files
    
    * Update dags/support_and_service/hefesto/queries/clean/worker_portfolio.sql
    
    Co-authored-by: William Angelo <39243586+williamangelo@users.noreply.github.com>
    
    * Update dags/support_and_service/hefesto/queries/clean/worker_portfolio_event.sql
    
    Co-authored-by: William Angelo <39243586+williamangelo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: William Angelo <39243586+williamangelo@users.noreply.github.com>

--- dags/support_and_service/hefesto/queries/clean/worker_portfolio_event.sql
+++ dags/support_and_service/hefesto/queries/clean/worker_portfolio_event.sql
@@ -1,7 +1,6 @@
 SELECT
     id,
-    portfolio_id as portfolio,
-    worker_portfolio_id AS id_portfolio,
+    COALESCE(portfolio_id, worker_portfolio_id) AS id_portfolio,
     portfolio_file_id AS id_portfolio_file,
     event_type,
     author,
