b5965c95df983b9f0726cdaeee71ec04de5f068d
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-02-10 14:30:24 -0300

    [PEOPLE] Fix dim_assignments query (#17709)
    * fix dim_assignments
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * fix identation
    
    * cast dt_effective
    
    * rename column
    
    * rename column
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * Update dags/people/dw_employee/queries/dw/dim_assignment.sql
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
    
    * adjust format
    
    * adjust date
    
    ---------
    
    Co-authored-by: Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>

--- dags/people/dw_employee/queries/dw/dim_assignment.sql
+++ dags/people/dw_employee/queries/dw/dim_assignment.sql
@@ -8,10 +8,9 @@ WITH
       datalake_hr_system_clean.salaries
     WHERE
       dt_from <= DATE ('{load_start_date}')
-      AND assignment_number NOT LIKE 'P%' QUALIFY dt_from = MAX(dt_from) OVER (
-        PARTITION BY
-          assignment_number
-      )
+      AND assignment_number NOT LIKE 'P%'
+    QUALIFY
+      MAX(dt_from) OVER (PARTITION BY assignment_number) = dt_from
   ),
   assignments AS (
     SELECT
@@ -28,7 +27,12 @@ WITH
       a.dt_effective_start,
       a.dt_effective_end,
       a.dt_projected_start,
-      a.action_code
+      a.action_code,
+      IF (
+        business_unit_name IN ('Classifieds Latam', 'Benvi MX'),
+        ts_last_update,
+        TIMESTAMP(dt_effective_start)
+      ) AS ts_valid_from
     FROM
       datalake_hr_system.assignments a
   ),
@@ -46,17 +50,22 @@ WITH
       a.dt_effective_start,
       a.dt_projected_start,
       a.action_code,
-      al.description AS action_description
+      al.description AS action_description,
+      a.ts_valid_from
     FROM
       assignments AS a
-      LEFT JOIN
-        datalake_hr_system_clean.actions_lov al
-          ON a.action_code = al.action_code
+    LEFT JOIN
+      datalake_hr_system_clean.actions_lov AS al
+        ON a.action_code = al.action_code
     WHERE
-      dt_effective_start < DATE ('{load_start_date}') QUALIFY dt_effective_start = MAX(dt_effective_start) over (
+      DATE(a.ts_valid_from) < DATE('{load_start_date}')
+    QUALIFY ROW_NUMBER() OVER (
         PARTITION BY
           id_assignment
-      )
+        ORDER BY
+          ts_valid_from DESC,
+          dt_effective_start DESC
+      ) = 1
   ),
   assignment_future AS (
     SELECT DISTINCT
@@ -72,17 +81,22 @@ WITH
       a.dt_effective_start,
       a.dt_projected_start,
       a.action_code,
-      al.description AS action_description
+      al.description AS action_description,
+      a.ts_valid_from
     FROM
       assignments AS a
-      LEFT JOIN
-        datalake_hr_system_clean.actions_lov al
-          ON a.action_code = al.action_code
+    LEFT JOIN
+      datalake_hr_system_clean.actions_lov AS al
+        ON a.action_code = al.action_code
     WHERE
-      dt_effective_start >= DATE ('{load_start_date}') QUALIFY dt_effective_start = MAX(dt_effective_start) over (
-        PARTITION BY
-          a.id_assignment
-      )
+      DATE(a.ts_valid_from) >= DATE('{load_start_date}')
+    QUALIFY ROW_NUMBER() OVER (
+      PARTITION BY
+        id_assignment
+      ORDER BY
+        ts_valid_from DESC,
+        dt_effective_start DESC
+    ) = 1
   )
 SELECT
   wr.id_period_of_service AS sk_assignment,
@@ -102,27 +116,29 @@ SELECT
   COALESCE(
     CASE
       WHEN wr.dt_termination < DATE ('{load_start_date}')
-      AND wr.worker_type IN ('E', 'C') THEN ap.action_description
+      AND wr.worker_type IN ('E', 'C')
+      THEN ap.action_description
       WHEN wr.dt_termination >= DATE ('{load_start_date}')
-      AND wr.worker_type IN ('E', 'C') THEN af.action_description
+      AND wr.worker_type IN ('E', 'C')
+      THEN af.action_description
     END,
     '-1'
   ) AS dismissal_type,
   s.action_reason AS reason_last_increase
 FROM
   datalake_hr_system.work_relationships wr
-  LEFT JOIN
-    assignments_present AS ap
-      ON wr.id_period_of_service = ap.id_period_of_service
-  LEFT JOIN
-    assignment_future AS af
-      ON wr.id_period_of_service = af.id_period_of_service
-  LEFT JOIN
-    salaries AS s
-      ON COALESCE(ap.id_assignment, af.id_assignment) = s.id_assignment
-  LEFT JOIN
-    datalake_hr_system.employee_ids AS ei
-      ON ei.id_period_of_service = wr.id_period_of_service
+LEFT JOIN
+  assignments_present AS ap
+    ON wr.id_period_of_service = ap.id_period_of_service
+LEFT JOIN
+  assignment_future AS af
+    ON wr.id_period_of_service = af.id_period_of_service
+LEFT JOIN
+  salaries AS s
+    ON COALESCE(ap.id_assignment, af.id_assignment) = s.id_assignment
+LEFT JOIN
+  datalake_hr_system.employee_ids AS ei
+    ON ei.id_period_of_service = wr.id_period_of_service
 WHERE
   (
     wr.dt_start <= DATE ('{load_start_date}')
