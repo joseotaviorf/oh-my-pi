44a02b27d9005b0831fd53d190eb93bffdf5132d
Rodrigo Arnaldo Kreuzberg <rodrigokreuzberg@gmail.com>
2025-02-10 09:27:17 -0400

    DDSS-4165 Adding new KPIs to analyts perfomance (#16598)
    * Adding new KPIs to analyts perfomance
    
    Context: Repair Ongoing
    - DSAT
    - FRT
    - Replies
    - Reopens
    
    * Changed to new sources
    
    * Adjusts in KPIs
    
    * Fixing dependencies
    
    * Added ongoing repair indicators
    
    - FRT
    - Productivity
    - DSAT
    - Reopen
    - First Reply
    
    * Fix - Dag declaration
    
    * Fix query

--- dags/support_and_service/dw_customer_support/queries/dw/fact_analysts_performance.sql
+++ dags/support_and_service/dw_customer_support/queries/dw/fact_analysts_performance.sql
@@ -5,11 +5,62 @@ WITH recontact_data AS (
   FROM
     datalake_customer_support.tickets AS t
   WHERE
-    DATE(t.ts_solved) = MAKE_DATE({year}, {month}, {day})
+    DATE(t.ts_solved) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
   QUALIFY
     ROW_NUMBER() OVER (PARTITION BY t.id_ticket ORDER BY t.ts_updated DESC) = 1
-),
-without_agg_infos AS (
+)
+,repair_ongoing AS (
+  SELECT
+    rt.id_ticket,
+    MD5(rt.agent_email) AS id_agent,
+    NULL AS csat_score,
+    rt.replies AS replies,
+    IF(rt.reopens > 0, 1, NULL) AS reopens,
+    DATE_DIFF(DAY, DATE(rt.ts_created_local), COALESCE(DATE(rt.ts_solved_local),DATE(rt.ts_closed_local))) AS frt,
+    COALESCE(DATE(rt.ts_solved_local),DATE(rt.ts_closed_local)) AS dt_solved,    
+    rt.ts_updated_local,
+    NULL AS ts_csat_response_submitted
+  FROM
+    datalake_repairs.repair_tickets AS rt
+  WHERE
+    (rt.channel NOT IN ('call', 'whatsapp')
+    AND rt.tags NOT LIKE '%teste_ps_pp_grupo_b_intermediacao_autosservico%'
+    AND rt.tags NOT LIKE '%mvp_fup_iq_intermediacao_autosservico%'
+    AND rt.client_type NOT LIKE '%prestador%'
+    AND rt.tags NOT LIKE '%reembolso_rep_ong%'
+    AND rt.tags NOT LIKE '%reembolso_sem_autorização%'
+    AND rt.tags NOT LIKE '%tarefacx_ongrep_solicitar_reembolso_reparos%'
+    AND rt.tags NOT LIKE '%whatspp_reparos%'
+    OR  rt.tags like '%mvp_fup_iq_intermediacao_autosservico%')
+    AND COALESCE(DATE(rt.ts_solved_local),DATE(rt.ts_closed_local)) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+  UNION ALL
+
+  SELECT
+    rt.id_ticket AS id_ticket,
+    MD5(rt.agent_email) AS id_agent,
+    rt.csat_score,
+    NULL AS replies,
+    NULL AS reopens,
+    NULL AS frt,
+    NULL AS dt_solved,
+    rt.ts_updated_local,
+    rt.ts_csat_response_submitted AS ts_csat_response_submitted
+  FROM
+    datalake_repairs.repair_tickets AS rt
+  WHERE
+    (rt.channel NOT IN ('call', 'whatsapp')
+    AND rt.tags NOT LIKE '%teste_ps_pp_grupo_b_intermediacao_autosservico%'
+    AND rt.tags NOT LIKE '%mvp_fup_iq_intermediacao_autosservico%'
+    AND rt.client_type NOT LIKE '%prestador%'
+    AND rt.tags NOT LIKE '%reembolso_rep_ong%'
+    AND rt.tags NOT LIKE '%reembolso_sem_autorização%'
+    AND rt.tags NOT LIKE '%tarefacx_ongrep_solicitar_reembolso_reparos%'
+    AND rt.tags NOT LIKE '%whatspp_reparos%'
+    OR  rt.tags like '%mvp_fup_iq_intermediacao_autosservico%')
+    AND DATE(rt.ts_csat_response_submitted) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+)
+,without_agg_infos AS (
   SELECT
     t.id_ticket,
     MD5(t.last_analyst_email) AS id_agent,
@@ -21,8 +72,8 @@ without_agg_infos AS (
     csat.first_csat_score AS first_csat_score,
     DATEDIFF(t.ts_solved, t.ts_created) AS attendance_time,
     r.is_recontact_ticket,
-    IF(DATE(t.ts_solved) = MAKE_DATE({year}, {month}, {day}) OR DATE(t.ts_closed) = MAKE_DATE({year}, {month}, {day}), TRUE, FALSE) AS is_received_demand,
-    IF(DATE(t.ts_solved) = MAKE_DATE({year}, {month}, {day}), TRUE, FALSE) AS is_productive_ticket,
+    IF(DATE(t.ts_solved) = DATE('{load_start_date}') OR DATE(t.ts_closed) = DATE('{load_start_date}'), TRUE, FALSE) AS is_received_demand,
+    IF(DATE(t.ts_solved) = DATE('{load_start_date}'), TRUE, FALSE) AS is_productive_ticket,
     CASE
       WHEN ROW_NUMBER() OVER(PARTITION BY MD5(
         COALESCE(CONCAT(rd.id_call, 'call'),
@@ -34,8 +85,9 @@ without_agg_infos AS (
     bmt.is_backlog_in_time,
     bmt.is_backlog_not_in_time,
     IF(bmt.is_backlog_in_time OR bmt.is_backlog_not_in_time, TRUE, FALSE) AS is_backlog,
+    NULL AS frt,
     csat.ts_first_response AS ts_csat_response,
-    DATE(t.ts_updated) AS dt_last_ticket_updated
+    DATE('{load_start_date}') AS dt_reference
   FROM
     datalake_customer_support.tickets t
   LEFT JOIN
@@ -57,11 +109,11 @@ without_agg_infos AS (
 		t.front_or_back <> "undefined"
 		AND dc.team IS NOT NULL
 		AND t.last_analyst_email IS NOT NULL
-		AND DATE(t.ts_updated) = MAKE_DATE({year}, {month}, {day})
+		AND DATE(t.ts_updated) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
   QUALIFY
 		ROW_NUMBER() OVER (PARTITION BY t.id_ticket ORDER BY t.ts_updated DESC) = 1
 
-    UNION ALL
+  UNION ALL
 
   SELECT
     t.id_ticket,
@@ -80,8 +132,9 @@ without_agg_infos AS (
     NULL AS is_backlog_in_time,
     NULL AS is_backlog_not_in_time,
     NULL AS is_backlog,
+    NULL AS frt,
     zes.ts_first_response AS ts_csat_response,
-    CURRENT_DATE - 1 AS dt_last_ticket_updated
+    DATE('{load_start_date}') AS dt_reference
 	FROM
     datalake_survicate.zendesk_email_surveys AS zes
   LEFT JOIN
@@ -93,88 +146,125 @@ without_agg_infos AS (
   WHERE
     zes.id_survey = '2f81495a1a142887'
     AND t.channel = 'whatsapp'
-    AND DATE(zes.ts_first_response) = CURRENT_DATE - 1
-    AND eso.current_payment_method IN ('CASH','CASH_USING_FGTS','FINANCED','FINANCED_USING_FGTS')
+    AND DATE(zes.ts_first_response) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    AND eso.current_payment_method IN
+      (
+        'CASH',
+        'CASH_USING_FGTS',
+        'FINANCED',
+        'FINANCED_USING_FGTS'
+      )
     AND t.last_analyst_email IS NOT NULL
   QUALIFY
     ROW_NUMBER() OVER (PARTITION BY zes.id_ticket ORDER BY zes.ts_first_response DESC) = 1
+
+  UNION ALL
+
+  SELECT
+    ro.id_ticket,
+    ro.id_agent,
+    NULL AS team,
+    NULL AS status,
+    NULL AS resolution_survey,
+    ro.reopens AS reopened_tickets,
+    ro.replies AS replied_tickets,
+    ro.csat_score AS first_csat_score,
+    NULL AS attendance_time,
+    NULL AS is_recontact_ticket,
+    NULL AS is_received_demand,
+    IF(ro.dt_solved = DATE('{load_start_date}'), TRUE, FALSE) AS is_productive_ticket,
+    NULL AS is_first_department_interaction,
+    NULL AS is_backlog_in_time,
+    NULL AS is_backlog_not_in_time,
+    NULL AS is_backlog,
+    ro.frt,
+    ro.ts_csat_response_submitted AS ts_csat_response,
+    DATE('{load_start_date}') AS dt_reference
+  FROM
+    repair_ongoing AS ro
+  WHERE
+    ro.id_agent IS NOT NULL
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY ro.id_ticket ORDER BY ro.ts_updated_local DESC) = 1
 )
 SELECT
-    wa.id_agent || dt_last_ticket_updated AS sk_snapshot,
-    wa.id_agent AS sk_agent,
-    COALESCE(CAST(DATE_FORMAT(wa.dt_last_ticket_updated,'yyyyMMdd') AS BIGINT), -1) AS sk_last_ticket_updated_date,
-    COUNT(id_ticket) AS total_tickets,
-    SUM(wa.reopened_tickets) AS ticket_reopenings,
-    SUM(wa.replied_tickets) AS ticket_responses,
-    SUM(wa.attendance_time) AS total_attendance_time,
-    COUNT_IF(wa.is_recontact_ticket = TRUE) AS total_tickets_recontact,
-    COUNT_IF(wa.is_received_demand = TRUE) AS total_received_demand,
-    COUNT_IF(wa.is_productive_ticket = TRUE) AS total_productivity,
-    COUNT(
-      CASE
-        WHEN
-          wa.first_csat_score IS NOT NULL
-          THEN wa.id_ticket
+  wa.id_agent || dt_reference AS sk_snapshot,
+  wa.id_agent AS sk_agent,
+  COALESCE(CAST(DATE_FORMAT(wa.dt_reference,'yyyyMMdd') AS BIGINT), -1) AS sk_reference_date,
+  COUNT(DISTINCT id_ticket) AS total_tickets,
+  SUM(wa.reopened_tickets) AS ticket_reopenings,
+  SUM(wa.replied_tickets) AS ticket_responses,
+  SUM(wa.attendance_time) AS total_attendance_time,
+  COUNT_IF(wa.is_recontact_ticket = TRUE) AS total_tickets_recontact,
+  COUNT_IF(wa.is_received_demand = TRUE) AS total_received_demand,
+  COUNT_IF(wa.is_productive_ticket = TRUE) AS total_productivity,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.first_csat_score IS NOT NULL
+        THEN wa.id_ticket
+      ELSE NULL
+    END
+  ) AS tickets_with_csat_score,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.first_csat_score IN (4,5)
+        THEN wa.id_ticket
+      ELSE NULL
+    END
+  ) AS tickets_csat_satisfied,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.first_csat_score = 3
+        THEN wa.id_ticket
+      ELSE NULL
+    END
+  ) AS tickets_csat_neutral,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.first_csat_score IN (1,2)
+        THEN wa.id_ticket
+      ELSE NULL
+    END
+  ) AS tickets_csat_dissatisfied,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.resolution_survey IS NOT NULL
+        OR wa.ts_csat_response IS NOT NULL
+        THEN wa.id_ticket
+    END
+  ) AS tickets_with_resolution_answered,
+  COUNT( DISTINCT
+    CASE
+      WHEN
+        wa.resolution_survey = True
+        OR wa.ts_csat_response IS NOT NULL
+        THEN wa.id_ticket
+    END
+  ) AS tickets_with_resolution,
+  COUNT( DISTINCT
+    CASE
+      WHEN wa.status = 'TRANSFERRED'
+        AND wa.is_first_department_interaction = True
+        AND wa.team <> 'Inside Sales'
+        THEN wa.id_ticket
         ELSE NULL
-      END
-    ) AS tickets_with_csat_score,
-    COUNT(
-      CASE
-        WHEN
-          wa.first_csat_score IN (4,5)
-          THEN wa.id_ticket
-        ELSE NULL
-      END
-    ) AS tickets_csat_satisfied,
-    COUNT(
-      CASE
-        WHEN
-          wa.first_csat_score = 3
-          THEN wa.id_ticket
-        ELSE NULL
-      END
-    ) AS tickets_csat_neutral,
-    COUNT(
-      CASE
-        WHEN
-          wa.first_csat_score IN (1,2)
-          THEN wa.id_ticket
-        ELSE NULL
-      END
-    ) AS tickets_csat_dissatisfied,
-    COUNT(
-      CASE
-        WHEN
-          wa.resolution_survey IS NOT NULL
-          OR wa.ts_csat_response IS NOT NULL
-          THEN wa.id_ticket
-      END
-    ) AS tickets_with_resolution_answered,
-    COUNT(
-      CASE
-        WHEN
-          wa.resolution_survey = True
-          OR wa.ts_csat_response IS NOT NULL
-          THEN wa.id_ticket
-      END
-    ) AS tickets_with_resolution,
-    COUNT(
-      CASE
-        WHEN wa.status = 'TRANSFERRED'
-          AND wa.is_first_department_interaction = True
-          AND wa.team <> 'Inside Sales'
-          THEN wa.id_ticket
-          ELSE NULL
-      END
-    ) AS tickets_transferred,
-    COUNT_IF(wa.is_backlog = TRUE) AS tickets_in_backlog,
-    COUNT_IF(wa.is_backlog_in_time = TRUE) AS backlog_within_sla,
-    COUNT_IF(wa.is_backlog_not_in_time = TRUE) AS backlog_with_exceed_sla,
-    YEAR(wa.dt_last_ticket_updated) AS year,
-    MONTH(wa.dt_last_ticket_updated) AS month,
-    DAY(wa.dt_last_ticket_updated) AS day,
-    NOW() AS ts_load
+    END
+  ) AS tickets_transferred,
+  AVG(wa.replied_tickets) AS replies,
+  AVG(wa.frt) AS frt,
+  COUNT_IF(wa.is_backlog = TRUE) AS tickets_in_backlog,
+  COUNT_IF(wa.is_backlog_in_time = TRUE) AS backlog_within_sla,
+  COUNT_IF(wa.is_backlog_not_in_time = TRUE) AS backlog_with_exceed_sla,
+  YEAR(wa.dt_reference) AS year,
+  MONTH(wa.dt_reference) AS month,
+  DAY(wa.dt_reference) AS day,
+  NOW() AS ts_load
 FROM
-    without_agg_infos AS wa
+  without_agg_infos AS wa
 GROUP BY
   ALL
--- dags/support_and_service/enrich_static_files_ss/queries/enrich/robbyson_indicators.sql
+++ dags/support_and_service/enrich_static_files_ss/queries/enrich/robbyson_indicators.sql
@@ -2,60 +2,65 @@ WITH union_line_indicators AS (
   SELECT
     EXPLODE(MAP(
       "CSAT", MAP(
-        "O CSAT é a porcentagem de clientes satisfeitos nas pesquisas de satisfação (CSAT) que enviamos após o atendimento.//
-        Este indicador reflete a qualidade do atendimento prestado, baseando-se nas respostas dos clientes.//
-        A fórmula de calculo basea-se nos fatores: (tickets_csat_satisfied / tickets_with_csat_score).",
+        "O CSAT é a porcentagem de clientes satisfeitos nas pesquisas de satisfação (CSAT) que enviamos após o atendimento.
+        Este indicador reflete a qualidade do atendimento prestado, baseando-se nas respostas dos clientes.
+        A fórmula de calculo basea-se nos fatores: (tickets_csat_satisfied [Factor 01] / tickets_with_csat_score [Factor 0] ).",
         ARRAY("tickets_with_csat_score", "tickets_csat_satisfied")
       ),
       "DSAT", MAP(
-        "O DSAT é o percentual de clientes insatisfeitos nas pesquisas de satisfação (CSAT) que enviamos após o atendimento.//
-        Este indicador destaca a proporção de clientes que não ficaram satisfeitos com o serviço recebido.//
-        A fórmula de calculo basea-se nos fatores: (tickets_csat_dissatisfied / tickets_with_csat_score).",
+        "O DSAT é o percentual de clientes insatisfeitos nas pesquisas de satisfação (CSAT) que enviamos após o atendimento.
+        Este indicador destaca a proporção de clientes que não ficaram satisfeitos com o serviço recebido.
+        A fórmula de calculo basea-se nos fatores: (tickets_csat_dissatisfied [Factor 01]  / tickets_with_csat_score [Factor 0] ).",
         ARRAY("tickets_with_csat_score", "tickets_csat_dissatisfied")
       ),
       "Resolution Rate", MAP(
-        "Índice de resolução no qual os clientes classificam em nossa pesquisa de satisfação (se o problema foi resolvido ou não).//
-        A fórmula utilizada é baseda nos fatores (tickets_with_resolution / total_tickets).",
+        "Índice de resolução no qual os clientes classificam em nossa pesquisa de satisfação (se o problema foi resolvido ou não).
+        A fórmula utilizada é baseda nos fatores (tickets_with_resolution [Factor 01] / total_tickets[Factor 0]).",
         ARRAY("total_tickets", "tickets_with_resolution")
       ),
       "Recontato", MAP(
         "O objetivo é saber quantos clientes identificados (com sk_user) entram em contato mais que uma vez em um determinado período
-        A fórmula de calculo basea-se nos fatores: (total_tickets_recontact / total_tickets).",
+        A fórmula de calculo basea-se nos fatores: (total_tickets_recontact [Factor 01] / total_tickets[Factor 0]).",
         ARRAY("total_tickets", "total_tickets_recontact")
       ),
       "Transferência", MAP(
         "Proporção de atendimentos que geram transferências entre níveis de atendimento.
-        A fórmula de calculo basea-se nos fatores: (tickets_transferred / total_tickets).",
+        A fórmula de calculo basea-se nos fatores: (tickets_transferred [Factor 01] / total_tickets[Factor 0]).",
         ARRAY("total_tickets", "tickets_transferred")
       ),
       "Reopen", MAP(
         "% de tickets que sofreram reopen (foram 'encerrados' e reabertos pelos clientes em até 2 dias após o encerramento).
-        A fórmula de calculo basea-se nos fatores: (ticket_reopenings / total_tickets).",
+        A fórmula de calculo basea-se nos fatores: (ticket_reopenings [Factor 01] / total_tickets[Factor 0]).",
         ARRAY("total_tickets", "ticket_reopenings")
       ),
       "First Reply", MAP(
         "Primeira resposta do analista durante o atendimento de chat/e-mail
-        A fórmula de calculo basea-se nos fatores: (ticket_responses / total_tickets).",
+        A fórmula de calculo basea-se nos fatores: (ticket_responses [Factor 01] / total_tickets[Factor 0]).",
         ARRAY("total_tickets", "ticket_responses")
       ),
       "Backlog Fora do prazo", MAP(
-        "São os tickets com status diferente de closed e solved, ou seja, em aberto, e fora do tempo estipulado de atendimento. //
-        Para o reporte do indicador, utilizamos sempre a quantidade de backlog do último dia do período reportado, ou seja, no último dia//
-        do mês para cada mês, D-1 para o período vigente, e o domingo para a semana. / Utiliza base do metabase porém fazemos ajustes no Databricks.//
-        A fórmula de calculo basea-se nos fatores: (backlog_with_exceed_sla, total_tickets).",
+        "São os tickets com status diferente de closed e solved, ou seja, em aberto, e fora do tempo estipulado de atendimento.
+        Para o reporte do indicador, utilizamos sempre a quantidade de backlog do último dia do período reportado, ou seja, no último dia
+        do mês para cada mês, D-1 para o período vigente, e o domingo para a semana. / Utiliza base do metabase porém fazemos ajustes no Databricks.
+        A fórmula de calculo basea-se nos fatores: (backlog_with_exceed_sla [Factor 01] , total_tickets) [Factor 0] .",
         ARRAY("total_tickets", "backlog_with_exceed_sla")
       ),
       "TMA", MAP(
-        "Tempo médio de duração do atendimento. A fórmula de calculo basea-se nos fatores: (total_attendance_time, total_tickets).",
+        "Tempo médio de duração do atendimento. A fórmula de calculo basea-se nos fatores: (total_attendance_time [Factor 01], total_tickets [Factor 0] ).",
         ARRAY("total_tickets", "total_attendance_time")
       ),
       "Produtividade", MAP(
-        "Demandas resolvidas do nível de atendimento. A fórmula de calculo basea-se nos fatores: (total_productivity, total_tickets).",
-        ARRAY("total_tickets", "total_productivity")
+        "Total de tickets de atendimento resolvido pelo analista. A fórmula de calculo basea-se no somatório do fator: (total_productivity [Factor 0]).",
+        ARRAY("total_productivity")
       ),
       "Demanda Recebida", MAP(
-        "O volume de demanda que estamos recebend.",
+        "O volume de demanda que estamos recebend. A fórmula de calculo basea-se no somatório do fator: (total_received_demand [Factor 0]).",
         ARRAY("total_received_demand")
+      ),
+      "FRT", MAP(
+        "O FRT médio (Full Resolution Time) calcula o tempo total desde o início do suporte na frente até o fim, considerando o tempo de backoffice
+        se houver um tíquete de retorno relacionado. A fórmula de calculo basea-se na média do fator: (frt [Factor 0]).",
+        ARRAY("frt")
       )
     )) AS (name, attributes),
     "support_and_services" AS context
