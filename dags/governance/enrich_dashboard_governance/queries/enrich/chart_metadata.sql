7b8e421bdc4bec3e5f19b409d51df1b42d2b7503
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2025-02-13 14:32:19 -0300

    [DG-2105] Migrate enrich_dashboard_governance DAG Builder (#17781)

--- dags/governance/enrich_dashboard_governance/queries/enrich/chart_metadata.sql
+++ dags/governance/enrich_dashboard_governance/queries/enrich/chart_metadata.sql
@@ -1,187 +1,195 @@
-with metabase_last_view as (
-  select
-    id_model,
-    max(ts_viewed) as ts_last_view
-  from
-    datalake_metabase_clean.view_log
-  group by
-    id_model
+WITH metabase_last_view AS (
+    SELECT
+        id_model,
+        MAX(ts_viewed) AS ts_last_view
+    FROM
+        datalake_metabase_clean.view_log
+    GROUP BY
+        id_model
 ),
-window_chart as (
-  select
-    RANK() over (
-      partition by id
-      order by
-        ts_updated desc
-    ) as row_number,
-    chart.id as id_chart,
-    chart.id_user_creator as id_owner,
-    chart.id_database,
-    chart.id_table,
-    chart.id_collection,
-    chart.name as title,
-    chart.description,
-    chart.display as type,
-    chart.is_archived,
-    chart.ts_updated
-  from
-    datalake_metabase_clean.report_card as chart
+window_chart AS (
+    SELECT
+        RANK() OVER (PARTITION BY id ORDER BY ts_updated DESC) AS row_number,
+        chart.id AS id_chart,
+        chart.id_user_creator AS id_owner,
+        chart.id_database,
+        chart.id_table,
+        chart.id_collection,
+        chart.name AS title,
+        chart.description,
+        chart.display AS type,
+        chart.is_archived,
+        chart.ts_updated
+    FROM
+        datalake_metabase_clean.report_card AS chart
 ),
-metabase_chart as (
-  select
-    chart.id_chart,
-    chart.id_owner,
-    chart.id_database,
-    chart.id_table,
-    chart.id_collection,
-    chart.title,
-    chart.description,
-    chart.type,
-    chart.is_archived,
-    if(
-      lv.id_model is not null,
-      lv.ts_last_view,
-      chart.ts_updated
-    ) as ts_last_view
-  from
-    window_chart as chart
-    left join metabase_last_view as lv on lv.id_model = chart.id_chart
-  where
-    chart.row_number = 1
+metabase_chart AS (
+    SELECT
+        chart.id_chart,
+        chart.id_owner,
+        chart.id_database,
+        chart.id_table,
+        chart.id_collection,
+        chart.title,
+        chart.description,
+        chart.type,
+        chart.is_archived,
+        IF(
+          lv.id_model IS NOT NULL,
+          lv.ts_last_view,
+          chart.ts_updated
+        ) AS ts_last_view
+    FROM
+        window_chart AS chart
+    LEFT JOIN
+        metabase_last_view AS lv
+            ON lv.id_model = chart.id_chart
+    WHERE
+        chart.row_number = 1
 ),
-metabase_database as (
-  select
-    id as id_database,
-    name as name_database
-  from
-    datalake_metabase_clean.metabase_database
-  where
-    name = "all-data"
-  group by
-    id,
-    name
+metabase_database AS (
+    SELECT
+        id AS id_database,
+        name AS name_database
+    FROM
+        datalake_metabase_clean.metabase_database
+    WHERE
+        name = "all-data"
+    GROUP BY
+        id,
+        name
 ),
-metabase_table as (
-  select
-    id as id_table,
-    id_database,
-    name as name_table,
-    schema as hive_schema
-  from
-    datalake_metabase_clean.metabase_table
-  group by
-    id,
-    id_database,
-    name,
-    schema
+metabase_table AS (
+    SELECT
+        id AS id_table,
+        id_database,
+        name AS name_table,
+        schema AS hive_schema
+    FROM
+        datalake_metabase_clean.metabase_table
+    GROUP BY
+        id,
+        id_database,
+        name,
+        schema
 ),
-metabase_user as (
-  select
-    id as id_user,
-    email
-  from
-    datalake_metabase_clean.core_user
-  group by
-    id,
-    email
+metabase_user AS (
+    SELECT
+        id AS id_user,
+        email
+    FROM
+        datalake_metabase_clean.core_user
+    GROUP BY
+        id,
+        email
 ),
 non_personal_location_collection(
-  select
-    id as id_collection,
-    name as name_collection
-  from
-    datalake_metabase_clean.collection
-  where
-    id_user_personal_owner is null
+    SELECT
+        id AS id_collection,
+        name AS name_collection
+    FROM
+        datalake_metabase_clean.collection
+    WHERE
+        id_user_personal_owner IS NULL
 ),
-flaten_location_collection as (
-  select
-    id as id_collection,
-    name as name_collection,
-    location as location_collection,
-    split(location, '/') as array_location,
-    explode(split(location, '/')) as dir_collection
-  from
-    datalake_metabase_clean.collection
-  where
-    id_user_personal_owner is null
-    and location != '/'
+flatten_location_collection AS (
+    SELECT
+        id AS id_collection,
+        name AS name_collection,
+        location AS location_collection,
+        SPLIT(location, '/') AS array_location,
+        EXPLODE(SPLIT(location, '/')) AS dir_collection
+    FROM
+        datalake_metabase_clean.collection
+    WHERE
+        id_user_personal_owner IS NULL
+        AND location != '/'
 ),
-joined_location_collection as (
-  select
-    fc.id_collection,
-    fc.name_collection,
-    fc.array_location,
-    fc.dir_collection,
-    mc.name_collection as dir_name_collection
-  from
-    flaten_location_collection as fc
-    join non_personal_location_collection as mc on fc.dir_collection = mc.id_collection
+joined_location_collection AS (
+    SELECT
+        fc.id_collection,
+        fc.name_collection,
+        fc.array_location,
+        fc.dir_collection,
+        mc.name_collection AS dir_name_collection
+    FROM
+        flatten_location_collection AS fc
+    JOIN
+        non_personal_location_collection AS mc
+            ON fc.dir_collection = mc.id_collection
 ),
-metabase_collection as (
-  select
-    id_collection,
-    name_collection,
-    array_location,
-    concat_ws(
-      "/",
-      collect_list(dir_name_collection),
-      name_collection
-    ) as readable_location
-  from
-    joined_location_collection
-  group by
-    id_collection,
-    name_collection,
-    array_location
-  union
-  select
-    id as id_collection,
-    name as name_collection,
-    null as array_location,
-    name as readable_location
-  from
-    datalake_metabase_clean.collection
-  where
-    id_user_personal_owner is null
-    and location = "/"
+metabase_collection AS (
+    SELECT
+        id_collection,
+        name_collection,
+        array_location,
+        concat_ws(
+          "/",
+          collect_list(dir_name_collection),
+          name_collection
+        ) AS readable_location
+    FROM
+        joined_location_collection
+    GROUP BY
+        id_collection,
+        name_collection,
+        array_location
+    UNION
+    SELECT
+        id AS id_collection,
+        name AS name_collection,
+        null AS array_location,
+        name AS readable_location
+    FROM
+        datalake_metabase_clean.collection
+    WHERE
+        id_user_personal_owner IS NULL
+        AND location = "/"
 )
-select
-  "metabase" as platform,
-  concat("/", metabase_collection.readable_location, "/") as chart_path,
-  chart.id_chart as chart_id,
-  chart.type,
-  chart.title,
-  chart.description as chart_description,
-  chart.ts_last_view as last_view,
-  concat(
-    "https://metabase.quintoandar.com.br/card/",
-    chart.id_chart
-  ) as chart_url,
-  if(
-    chart.id_table is not null,
-    concat(
-      "hive.",
-      metabase_table.hive_schema,
-      ".",
-      metabase_table.name_table
-    ),
-    null
-  ) as dataset,
-  metabase_user.email as ownership,
-  case
-    when is_archived is true then 'DEPRECATED'
-    when datediff(current_timestamp(), chart.ts_last_view) > 90 then 'DEPRECATED'
-    else 'ACTIVE'
-  end as status,
-  cast(null as string) as domain,
-  current_timestamp() as ts_ingested,
-  year(current_timestamp()) as year,
-  month(current_timestamp()) as month,
-  day(current_timestamp()) as day
-from
-  metabase_chart as chart
-  join metabase_database on chart.id_database = metabase_database.id_database
-  left join metabase_user on chart.id_owner = metabase_user.id_user
-  left join metabase_table on chart.id_table = metabase_table.id_table
-  left join metabase_collection on chart.id_collection = metabase_collection.id_collection
+SELECT
+    "metabase" AS platform,
+    CONCAT("/", metabase_collection.readable_location, "/") AS chart_path,
+    chart.id_chart AS chart_id,
+    chart.type,
+    chart.title,
+    chart.description AS chart_description,
+    chart.ts_last_view AS last_view,
+    CONCAT(
+        "https://metabase.quintoandar.com.br/card/",
+        chart.id_chart
+    ) AS chart_url,
+    IF(
+        chart.id_table IS NOT NULL,
+        CONCAT(
+          "hive.",
+          metabase_table.hive_schema,
+          ".",
+          metabase_table.name_table
+        ),
+        NULL
+    ) AS dataset,
+    metabase_user.email AS ownership,
+    CASE
+        WHEN is_archived IS TRUE THEN 'DEPRECATED'
+        WHEN DATEDIFF(CURRENT_TIMESTAMP(), chart.ts_last_view) > 90 THEN 'DEPRECATED'
+        ELSE 'ACTIVE'
+    END AS status,
+    CAST(NULL AS string) AS domain,
+    CURRENT_TIMESTAMP() AS ts_ingested,
+    YEAR(CURRENT_TIMESTAMP()) AS year,
+    MONTH(CURRENT_TIMESTAMP()) AS month,
+    DAY(CURRENT_TIMESTAMP()) AS day
+FROM
+    metabase_chart AS chart
+JOIN
+    metabase_database
+        ON chart.id_database = metabase_database.id_database
+LEFT JOIN
+    metabase_user
+        ON chart.id_owner = metabase_user.id_user
+LEFT JOIN
+    metabase_table
+        ON chart.id_table = metabase_table.id_table
+LEFT JOIN
+    metabase_collection
+        ON chart.id_collection = metabase_collection.id_collection
--- dags/governance/enrich_dashboard_governance/queries/enrich/dashboard_metadata.sql
+++ dags/governance/enrich_dashboard_governance/queries/enrich/dashboard_metadata.sql
@@ -1,174 +1,177 @@
-with
-metabase_last_view as (
-  select
-    id_model,
-    count(id_model) as views,
-    max(ts_viewed) as ts_last_view
-  from
-    datalake_metabase_clean.view_log
-  where
-    date(ts_viewed) <= date(format_string('%d-%d-%d', {year}, {month}, {day}))
-  group by
-    id_model
+WITH metabase_last_view AS (
+    SELECT
+        id_model,
+        COUNT(id_model) AS views,
+        MAX(ts_viewed) AS ts_last_view
+    FROM
+        datalake_metabase_clean.view_log
+    WHERE
+        DATE(ts_viewed) <= DATE(format_string('%d-%d-%d', {year}, {month}, {day}))
+    GROUP BY
+        id_model
 ),
-metabase_dashboard_last_update as (
-  select
-    RANK() over (
-      partition by id
-      order by
-        ts_updated desc
-    ) as row_number,
-    CAST(md.id as STRING) as id_dashboard,
-    md.id_user_creator as id_owner,
-    md.id_collection,
-    md.name as title,
-    md.description,
-    md.id_collection,
-    md.is_archived
-  from
-    datalake_metabase_clean.report_dashboard md
-  where
-    date(ts_updated) <= date(format_string('%d-%d-%d', {year}, {month}, {day}))
+metabase_dashboard_last_update AS (
+    SELECT
+        RANK() OVER (PARTITION BY id ORDER BY ts_updated DESC) AS row_number,
+        CAST(md.id AS STRING) AS id_dashboard,
+        md.id_user_creator AS id_owner,
+        md.id_collection,
+        md.name AS title,
+        md.description,
+        md.id_collection,
+        md.is_archived
+    FROM
+        datalake_metabase_clean.report_dashboard AS md
+    WHERE
+        DATE(ts_updated) <= DATE(format_string('%d-%d-%d', {year}, {month}, {day}))
 ),
-metabase_dashboards as (
-  select
-    mdlu.id_dashboard,
-    mdlu.id_owner,
-    mdlu.id_collection,
-    mdlu.title,
-    mdlu.description,
-    mdlu.is_archived,
-    mdlu.id_collection
-  from
-    metabase_dashboard_last_update mdlu
-  where
-    mdlu.row_number == 1
+metabase_dashboards AS (
+    SELECT
+        mdlu.id_dashboard,
+        mdlu.id_owner,
+        mdlu.id_collection,
+        mdlu.title,
+        mdlu.description,
+        mdlu.is_archived,
+        mdlu.id_collection
+    FROM
+        metabase_dashboard_last_update AS mdlu
+    WHERE
+        mdlu.row_number == 1
 ),
-metabase_user_last_update as (
-  select
-    RANK() over (
-      partition by id
-      order by
-        ts_updated desc
-    ) as row_number,
-    id as id_user,
-    email
-  from
-    datalake_metabase_clean.core_user
+metabase_user_last_update AS (
+    SELECT
+        RANK() OVER (PARTITION BY id ORDER BY ts_updated DESC) AS row_number,
+        id AS id_user,
+        email
+    FROM
+        datalake_metabase_clean.core_user
 ),
-metabase_user as (
-select
-  mulu.id_user,
-  mulu.email
-from
-  metabase_user_last_update mulu
-where
-  mulu.row_number == 1
+metabase_user AS (
+    SELECT
+        mulu.id_user,
+        mulu.email
+    FROM
+        metabase_user_last_update AS mulu
+    WHERE
+        mulu.row_number == 1
 ),
-metabase_collections_filtered as (
-  select
-    id as id_collection,
-    name as name_collection,
-    location
-  from
-    datalake_metabase_clean.collection
-  where
-    id_user_personal_owner is null
+metabase_collections_filtered AS (
+    SELECT
+        id AS id_collection,
+        name AS name_collection,
+        location
+    FROM
+        datalake_metabase_clean.collection
+    WHERE
+        id_user_personal_owner IS NULL
 ),
-metabase_collections_exploded as (
-  select
-    id_collection,
-    name_collection,
-    explode(split(trim('/' from location), '/')) as dir_collection_id
-  from
-    metabase_collections_filtered
+metabase_collections_exploded AS (
+    SELECT
+        id_collection,
+        name_collection,
+        EXPLODE(SPLIT(TRIM('/' FROM location), '/')) AS dir_collection_id
+    FROM
+        metabase_collections_filtered
 ),
-metabase_collections as (
-  select
-    mce.id_collection,
-    mce.name_collection,
-    concat_ws(
-      "/",
-      collect_list(mcf.name_collection),
-      mce.name_collection
-    ) as readable_location
-  from
-    metabase_collections_exploded as mce
-    join metabase_collections_filtered as mcf on mce.dir_collection_id = mcf.id_collection
-  group by mce.id_collection, mce.name_collection
+metabase_collections AS (
+    SELECT
+        mce.id_collection,
+        mce.name_collection,
+        CONCAT_WS(
+            "/",
+            COLLECT_LIST(mcf.name_collection),
+            mce.name_collection
+        ) AS readable_location
+    FROM
+        metabase_collections_exploded AS mce
+    JOIN
+        metabase_collections_filtered AS mcf
+            ON mce.dir_collection_id = mcf.id_collection
+    GROUP BY
+        mce.id_collection, mce.name_collection
 ),
-metabase_dash_card as (
-  select
-    CAST(id_dashboard as STRING) as id_dashboard,
-    collect_list(CAST(id_card as STRING)) as ids_charts
-  from
-    datalake_metabase_clean.report_dashboard_card
-  where
-    id_card is not null and
-    date(ts_updated) <= date(format_string('%d-%d-%d', {year}, {month}, {day}))
-  group by
-    id_dashboard
+metabase_dash_card AS (
+    SELECT
+        CAST(id_dashboard AS STRING) AS id_dashboard,
+        COLLECT_LIST(CAST(id_card AS STRING)) AS ids_charts
+    FROM
+        datalake_metabase_clean.report_dashboard_card
+    WHERE
+        id_card IS NOT NULL
+        AND DATE(ts_updated) <= DATE(FORMAT_STRING('%d-%d-%d', {year}, {month}, {day}))
+    GROUP BY
+        id_dashboard
 ),
-metabase_dashboard_enrich as (
-    select
-      "metabase" as platform,
-      md.id_dashboard,
-      mc.readable_location as dashboard_path,
-      md.title,
-      md.description,
-      lv.views,
-      mu.email as ownership,
-      CASE
-        WHEN mc.readable_location like 'Fintech%' THEN 'Fintech'
-        WHEN mc.readable_location like 'Cross%' THEN 'Cross'
-        WHEN mc.readable_location like 'For Rent%' THEN 'For Rent'
-        WHEN mc.readable_location like 'For Sale%' THEN 'For Sale'
-        WHEN mc.readable_location like 'Growth%' THEN 'Growth'
-        WHEN mc.readable_location like 'International%' THEN 'International'
-        WHEN mc.readable_location like 'Rede%' THEN 'Rede'
-        WHEN mc.readable_location like 'BedRock%' THEN 'BedRock'
-        WHEN mc.readable_location like 'Tech Platform%' THEN 'Tech Platform'
-        WHEN mc.readable_location like 'Support and Services%' THEN 'Support and Services'
-        WHEN mc.readable_location like 'People%' THEN 'People'
-        ELSE Null
-      END as domain,
-      case
-        when md.is_archived is true then 'DEPRECATED'
-        when datediff(date(format_string('%d-%d-%d', {year}, {month}, {day})), lv.ts_last_view) > 90 then 'DEPRECATED'
-        else 'ACTIVE'
-      end as status,
-      mdc.ids_charts,
-      lv.ts_last_view as last_view,
-      concat(
-        "https://metabase.quintoandar.com.br/dashboard/",
-        md.id_dashboard
-      ) as dashboard_url,
-      date(format_string('%d-%d-%d', {year}, {month}, {day})) as dt_updated,
-      {year} as year,
-      {month} as month,
-      {day} as day
-    from
-      metabase_dashboards md
-      join metabase_user mu on mu.id_user = md.id_owner
-      join metabase_dash_card mdc on mdc.id_dashboard = md.id_dashboard
-      join metabase_last_view lv on lv.id_model = md.id_dashboard
-      join metabase_collections mc on md.id_collection = mc.id_collection
+metabase_dashboard_enrich AS (
+    SELECT
+        "metabase" AS platform,
+        md.id_dashboard,
+        mc.readable_location AS dashboard_path,
+        md.title,
+        md.description,
+        lv.views,
+        mu.email AS ownership,
+        CASE
+            WHEN mc.readable_location LIKE 'Fintech%' THEN 'Fintech'
+            WHEN mc.readable_location LIKE 'Cross%' THEN 'Cross'
+            WHEN mc.readable_location LIKE 'For Rent%' THEN 'For Rent'
+            WHEN mc.readable_location LIKE 'For Sale%' THEN 'For Sale'
+            WHEN mc.readable_location LIKE 'Growth%' THEN 'Growth'
+            WHEN mc.readable_location LIKE 'International%' THEN 'International'
+            WHEN mc.readable_location LIKE 'Rede%' THEN 'Rede'
+            WHEN mc.readable_location LIKE 'BedRock%' THEN 'BedRock'
+            WHEN mc.readable_location LIKE 'Tech Platform%' THEN 'Tech Platform'
+            WHEN mc.readable_location LIKE 'Support and Services%' THEN 'Support and Services'
+            WHEN mc.readable_location LIKE 'People%' THEN 'People'
+            ELSE NULL
+        END AS domain,
+        CASE
+            WHEN md.is_archived IS TRUE THEN 'DEPRECATED'
+            WHEN DATEDIFF(DATE(format_string('%d-%d-%d', {year}, {month}, {day})), lv.ts_last_view) > 90 THEN 'DEPRECATED'
+            ELSE 'ACTIVE'
+        END AS status,
+        mdc.ids_charts,
+        lv.ts_last_view AS last_view,
+        CONCAT(
+            "https://metabase.quintoandar.com.br/dashboard/",
+            md.id_dashboard
+        ) AS dashboard_url,
+        DATE(format_string('%d-%d-%d', {year}, {month}, {day})) AS dt_updated,
+        {year} AS year,
+        {month} AS month,
+        {day} AS day
+    FROM
+        metabase_dashboards AS md
+    JOIN
+        metabase_user AS mu
+            ON mu.id_user = md.id_owner
+    JOIN
+        metabase_dash_card AS mdc
+            ON mdc.id_dashboard = md.id_dashboard
+    JOIN
+        metabase_last_view AS lv
+            ON lv.id_model = md.id_dashboard
+    JOIN
+        metabase_collections AS mc
+            ON md.id_collection = mc.id_collection
 )
-select
-  platform,
-  id_dashboard,
-  dashboard_path,
-  title,
-  description,
-  views,
-  ownership,
-  domain,
-  status,
-  ids_charts,
-  last_view,
-  dashboard_url,
-  dt_updated,
-  year,
-  month,
-  day
-from metabase_dashboard_enrich
+SELECT
+    platform,
+    id_dashboard,
+    dashboard_path,
+    title,
+    description,
+    views,
+    ownership,
+    domain,
+    status,
+    ids_charts,
+    last_view,
+    dashboard_url,
+    dt_updated,
+    year,
+    month,
+    day
+FROM
+    metabase_dashboard_enrich
