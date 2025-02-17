1b4bacaf38046a7f89d82db137625cacb69dbcfc
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-02-17 09:17:50 -0300

    Remove hefesto_test DAG (#17810)

--- dags/support_and_service/hefesto_test/queries/clean/account.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    id AS id_account,
-    account_sid AS id_account_twilio,
-    workspace_sid AS id_workspace_twilio,
-    workflow_sid AS id_workflow_twilio
-FROM
-    datalake_hefesto_test_raw.account
--- dags/support_and_service/hefesto_test/queries/clean/queue_type.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id AS id_queue_type,
-    name,
-    description
-FROM
-    datalake_hefesto_test_raw.queue_type
