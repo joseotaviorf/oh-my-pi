d59e421f3da97df28693795b68fe25812ec36965
Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
2025-02-17 16:33:22 -0300

    [DFORSALE-8614] Change in primary source of name and email information (Partner to Person) - ciq_users (#17674)
    * [DFORSALE-8614] Change in primary source of name and email information (Person)
    
    * [DFORSALE-8614] Change in primary source of name and email information (Person)

--- dags/agents/enrich_ebdb_agents/queries/enrich/ciq_users.sql
+++ dags/agents/enrich_ebdb_agents/queries/enrich/ciq_users.sql
@@ -1,9 +1,25 @@
+WITH person_data AS (
+  SELECT
+    p.uuid_person,
+    ci.id_person,
+    p.person_name AS name,
+    MAX(CASE WHEN ci.priority = 'PRIMARY' AND ci.category = 'EMAIL' THEN ci.contact_info END) AS primary_email,
+    MAX(CASE WHEN ci.priority = 'SECONDARY' AND ci.category = 'EMAIL' THEN ci.contact_info END) AS secondary_email
+  FROM
+    datalake_person_clean.person as p
+  LEFT JOIN
+    datalake_person_clean.contact_info AS ci
+      ON p.id = ci.id_person
+  GROUP BY
+    1,2,3 
+)
 SELECT
     p.id AS id_partner,
     p.uuid_company,
+    u.uuid_person,
     paa.id_user,
-    p.name,
-    p.email,
+    COALESCE(pd.name, p.name) AS name,
+    COALESCE(pd.primary_email, pd.secondary_email, p.email) AS email,
     paa.status,
     CASE 
         WHEN ROW_NUMBER() OVER (PARTITION BY p.id ORDER BY ure.ts_revision DESC) = 1 THEN TRUE
@@ -20,5 +36,11 @@ LEFT JOIN
 LEFT JOIN 
   datalake_ebdb_user.user_revision_entity AS ure
     ON paa.rev = ure.id
+LEFT JOIN
+  datalake_ebdb_user.user AS u
+    ON paa.id_user = u.id
+LEFT JOIN
+  person_data AS pd
+    ON pd.uuid_person = u.uuid_person
 WHERE
   p.type = 'AUTONOMOUS_AGENT'
