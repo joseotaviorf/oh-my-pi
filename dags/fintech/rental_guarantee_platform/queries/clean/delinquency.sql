996b072f65befd27722573b96ed319d931b1c06e
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-02-18 16:26:24 -0300

    AEFF-2551 add new delinquency descriptions (#17848)
    * AEFF-2551 add new delinquency descriptions
    
    * AEFF-2551 add new delinquency descriptions

--- dags/fintech/rental_guarantee_platform/queries/clean/delinquency.sql
+++ dags/fintech/rental_guarantee_platform/queries/clean/delinquency.sql
@@ -5,6 +5,25 @@ SELECT
     type AS id_type,
     userinsert AS id_user_insert,
     userupdate AS id_user_update,
+    CASE
+      WHEN id_status = 0 THEN 'REGISTERED'
+      WHEN id_status = 1 THEN 'RECOVERING' 
+      WHEN id_status = 2 THEN 'PROGRESS' 
+      WHEN id_status = 3 THEN 'FINISHED' 
+      WHEN id_status = 4 THEN 'UNDER_AGREEMENT' 
+      WHEN id_status = 5 THEN 'REQUESTED_AGREEMENT' 
+      WHEN id_status = 6 THEN 'ACTIVE_FALSE' 
+      WHEN id_status = 7 THEN 'FORGIVEN' 
+    END AS status,
+    CASE
+      WHEN id_type = 0 THEN 'SIGNATURE'
+      WHEN id_type = 1 THEN 'GUARANTEE'
+      WHEN id_type = 2 THEN 'TERMINATION'
+      WHEN id_type = 3 THEN 'BILLING'
+      WHEN id_type = 4 THEN 'RENEWAL'
+      WHEN id_type = 5 THEN 'RENEWAL_MONTH'
+      WHEN id_type = 6 THEN 'ACTIVATION_PARTIAL_LINK'
+    END AS type,
     subcategory,
     source,
     value,
