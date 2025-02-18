0b7b7d1d5d0abb6fad22394477d5b413e3c412ea
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-02-18 18:42:08 -0300

    [DFFRS-1558] Update quinturk clean tables (#17854)
    * Add: extract cpf, name and id_proposal from task
    
    * Update: metadata
    
    * Update: extract result data
    
    * Update: update task completion metadata
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/support_and_service/quinturk/queries/clean/task.sql
+++ dags/support_and_service/quinturk/queries/clean/task.sql
@@ -1,4 +1,5 @@
-SELECT
+WITH extract_quinturk_task_data AS (
+  SELECT
     id,
     project_id AS id_project,
     updated_by_id AS id_updated_by,
@@ -15,9 +16,41 @@ SELECT
     updated_at AS ts_updated,
     year,
     month,
-    day
-FROM
+    day,
+    from_json(
+      data,
+      'STRUCT<
+      cpf: STRING,
+      name: STRING,
+      id_proposal: STRING
+    >'
+    ) AS extracted_data
+  FROM
     datalake_quinturk_raw.task
+)
+SELECT
+  id,
+  id_project,
+  id_updated_by,
+  id_file_upload,
+  id_inner,
+  extracted_data.id_proposal,
+  total_annotations,
+  cancelled_annotations,
+  total_predictions,
+  overlap,
+  data,
+  extracted_data.cpf AS proponent_cpf,
+  extracted_data.name AS proponent_name,
+  meta,
+  is_labeled,
+  ts_created,
+  ts_updated,
+  year,
+  month,
+  day
+FROM
+  extract_quinturk_task_data
 WHERE
     year = {year}
     AND month = {month}
--- dags/support_and_service/quinturk/queries/clean/task_completion.sql
+++ dags/support_and_service/quinturk/queries/clean/task_completion.sql
@@ -1,4 +1,5 @@
-SELECT
+WITH extract_quinturk_task_completion_result AS (
+  SELECT
     id,
     task_id AS id_task,
     parent_annotation_id AS id_parent_annotation,
@@ -6,6 +7,7 @@ SELECT
     completed_by_id AS id_completed_by,
     last_created_by_id AS id_last_created_by,
     prediction,
+    result,
     lead_time,
     result_count,
     last_action,
@@ -15,9 +17,46 @@ SELECT
     updated_at AS ts_updated,
     year,
     month,
-    day
-FROM
+    day,
+    from_json(
+      result,
+      'ARRAY<STRUCT<
+    id: STRING,
+    type: STRING,
+    origin: STRING,
+    from_name: STRING,
+    value: STRUCT<choices: ARRAY<STRING>, text: ARRAY<STRING>>
+    >>'
+    ) AS extracted_result
+  FROM
     datalake_quinturk_raw.task_completion
+)
+SELECT
+  id,
+  id_task,
+  id_parent_annotation,
+  id_parent_prediction,
+  id_completed_by,
+  id_last_created_by,
+  extracted_result.id AS id_input,
+  extracted_result.type AS input_type,
+  extracted_result.origin AS inpput_origin,
+  extracted_result.from_name AS input_document_name,
+  prediction,
+  result,
+  lead_time,
+  result_count,
+  last_action,
+  extracted_result.value AS input_value,
+  is_ground_truth,
+  was_cancelled,
+  ts_created,
+  ts_updated,
+  year,
+  month,
+  day
+FROM
+  extract_quinturk_task_completion_result
 WHERE
     year = {year}
     AND month = {month}
