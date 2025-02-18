ae303c157359208c54b70040dbda522bee5ca3db
Herivelto Macedo <61527726+heri-macedo@users.noreply.github.com>
2025-02-18 09:53:24 -0300

    [DG] Fix reverse dashboard governance (#17836)

--- dags/governance/reverse_dashboard_governance/spark_jobs/load_data_assets_into_mp.py
+++ dags/governance/reverse_dashboard_governance/spark_jobs/load_data_assets_into_mp.py
@@ -20,9 +20,9 @@ logger = QuintoAndarLogger(JOB_NAME)
 def remove_nulls(row_dict:dict) -> dict:
     """
     Removes null values from a dictionary
-    
+
     :param row_dict: dictionary to be cleaned
-    
+
     :return: dictionary without null values
     """
     return {k: v for k, v in row_dict.items() if v is not None}
@@ -31,15 +31,15 @@ def remove_nulls(row_dict:dict) -> dict:
 def _get_payloads_from_datalake(spark_client:SparkClient, data_asset:str) -> list:
     """
     This function gets the data from the reverse tables and transforms it into a list of payloads
-    
+
     :param spark_client: SparkClient to connect to local spark
     :param data_asset: data asset to be loaded into Metadata Propagator. Options are dashboard, chart, dataset
-    
+
     :return: list of payloads
     """
     DEAFULT_QUERY = "SELECT * FROM reverse_dashboard_governance.{asset}"
     formatted_query = DEAFULT_QUERY.format(asset=data_asset)
-    
+
     df = spark_client.get_records(formatted_query)
     rows = df.rdd.map(lambda row: row.asDict()).collect()
 
@@ -48,11 +48,11 @@ def _get_payloads_from_datalake(spark_client:SparkClient, data_asset:str) -> lis
 def _send_requests(endpoint:str, payloads:list, chunk_size:int=30) -> None:
     """
     Sends requests with payload with 5 retries and breaks the payload into chunks if needed
-    
+
     :param endpoint: endpoint to send the requests. Must have already which asset to send to
     :param payloads: list of payloads to be sent
     :param chunk_size: size of the chunks to break the payloads. Default is 30
-    
+
     :return: None
     """
     session = requests.Session()
@@ -79,7 +79,7 @@ def _send_requests(endpoint:str, payloads:list, chunk_size:int=30) -> None:
                     f"exception={exception}, "
                     f"payload={chunk}"
                 )
-                
+
 
 if __name__ == "__main__":
 
@@ -87,29 +87,31 @@ if __name__ == "__main__":
     all_data_assets = ["dashboard", "chart", "dataset"]
 
     parser.add_argument("environment", help="forno/prod values ")
-    parser.add_argument("--data_assets", type=str, nargs="+",choices=all_data_assets, 
+    parser.add_argument("--data_assets", type=str, nargs="+",choices=all_data_assets,
                         help="list of data assets to be loaded into Metadata Propagator. Options are dashboard, chart, dataset")
 
     args = parser.parse_args()
 
     environment = args.environment
     data_assets = args.data_assets
-    
+    chunk_size = 30
+
     logger.info(
         f"""m=__main__, environment={environment}, source=dashboard_governance,
         """
     )
     error = []
-    
+
     for asset in data_assets:
         logger.info(f"m=__main__, asset={asset}")
         if asset == "dashboard":
             METADATA_PROPAGATOR_PATH = "/dashboard"
         elif asset == "chart":
             METADATA_PROPAGATOR_PATH = "/chart"
+            chunk_size = 100
         elif asset == "dataset":
             METADATA_PROPAGATOR_PATH = "/dataset"
-        
+
         base_dbutils = BaseDBUtils()
         if base_dbutils.get_dbutils() is not None:
             dbutils = base_dbutils.get_dbutils()
@@ -121,16 +123,15 @@ if __name__ == "__main__":
         host = credentials["host"]
         endpoint = f"{host}{METADATA_PROPAGATOR_PATH}"
         logger.info(f"m=__main__, sending data to {endpoint}")
-        
+
         spark_client = SparkClient()
         payloads = _get_payloads_from_datalake(spark_client, asset)
         try:
-            _send_requests(endpoint, payloads)
+            _send_requests(endpoint, payloads, chunk_size)
         except Exception as e:
             error.append(e)
             logger.error(f"m=__main__, error={e}")
-            
+
     if error:
         raise RuntimeError(f"Error sending data to Metadata Propagator: {error}")
-    
-    
+
