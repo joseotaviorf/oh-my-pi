744596235df2080e6da6e317e8325a2bde685505
Isabela Sayuri Matsumoto <isabela.matsumoto@quintoandar.com.br>
2025-02-19 09:34:38 -0300

    [GROW-3536] Enriching Robin hood enrich (#17842)
    * Adding payee and source fields in accounting_entry enrich
    
    * adjusting linegea
    
    * make dependencies-file

--- dags/fintech/enrich_robin_hood/queries/enrich/accounting_entry.sql
+++ dags/fintech/enrich_robin_hood/queries/enrich/accounting_entry.sql
@@ -1,22 +1,30 @@
 SELECT
-    id,
-    id_external,
-    id_source,
-    id_payee,
-    GET_JSON_OBJECT(metadata, '$.lead-id') as id_lead,
-    GET_JSON_OBJECT(metadata, '$.house-id') as id_house,
-    GET_JSON_OBJECT(metadata, '$.contract-id') as id_contract,
-    GET_JSON_OBJECT(metadata, '$.offer-id') as id_offer,
-    cost_center_code,
-    source_bill_item,
-    description,
-    type,
-    locale,
-    due_amount,
-    accrual_year_month,
-    accounting_year_month,
-    dt_occurrence,
-    ts_blocked,
-    ts_created
+    ae.id,
+    ae.id_external,
+    ae.id_source,
+    ae.id_payee,
+    p.id_external AS id_payee_external,
+    GET_JSON_OBJECT(ae.metadata, '$.lead-id') AS id_lead,
+    GET_JSON_OBJECT(ae.metadata, '$.house-id') AS id_house,
+    GET_JSON_OBJECT(ae.metadata, '$.contract-id') AS id_contract,
+    GET_JSON_OBJECT(ae.metadata, '$.offer-id') AS id_offer,
+    ae.cost_center_code,
+    ae.source_bill_item,
+    ae.daescription,
+    ae.type,
+    ae.locale,
+    aes.source_name,
+    ae.due_amount,
+    ae.accrual_year_month,
+    ae.accounting_year_month,
+    ae.dt_occurrence,
+    ae.ts_blocked,
+    ae.ts_created
 FROM
-    datalake_robin_hood_clean.accounting_entry
+    datalake_robin_hood_clean.accounting_entry ae 
+LEFT JOIN 
+    datalake_robin_hood_clean.payee p 
+        ON p.id = ae.id_payee
+LEFT JOIN 
+    datalake_robin_hood_clean.accounting_entry_source aes
+        ON ae.id_source = aes.id
