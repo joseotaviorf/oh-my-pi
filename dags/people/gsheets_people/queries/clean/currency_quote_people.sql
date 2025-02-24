e852ad44de5f9a139bc5125a19e8bce6d6134cfc
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-02-24 15:17:15 -0300

    [PEOPLE] Drop unused gsheets (#17930)
    * drop tables
    
    * delete table

--- dags/people/gsheets_people/queries/clean/currency_quote_people.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    moeda AS currency,
-    FLOAT(cotacao) AS cotation,
-    ts_load
-FROM datalake_gsheets_people_raw.currency_quote_people
--- dags/people/gsheets_people/queries/clean/executive_leadership_list.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    email AS employee_email,
-    etlt AS employee_type,
-    ts_load
-FROM datalake_gsheets_people_raw.executive_leadership_list
