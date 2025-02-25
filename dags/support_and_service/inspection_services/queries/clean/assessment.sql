7893fd4a8bfc1da0005fed77a38d1e49a911ca74
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2025-02-25 09:39:10 -0300

    Revert "Revert "Revert "Revert "[DDSS-4562]: Adding tables to inspect… (#17950)
    * Revert "Revert "Revert "Revert "[DDSS-4562]: Adding tables to inspection cdc …"
    
    This reverts commit 48d47bbf680481025dc53d6dab3201a11e3aeca5.
    
    * Fix clean primary keys

---
+++ dags/support_and_service/inspection_services/queries/clean/assessment.sql
@@ -0,0 +1,20 @@
+SELECT
+    id AS id_assessment,
+    inspection_id AS id_inspection,
+    previous_assessment_id AS id_previous_assessment,
+    key_location,
+    key_location_details,
+    source,
+    house_supplies,
+    scheduling_key_recovery,
+    owner_limit_revision_date AS dt_owner_limit_revision,
+    tenant_limit_revision_date AS dt_tenant_limit_revision,
+    created_at AS ts_created,
+    finished_at AS ts_finished,
+    started_at AS ts_started,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_inspections_raw.assessment
---
+++ dags/support_and_service/inspection_services/queries/clean/contestation.sql
@@ -0,0 +1,17 @@
+SELECT
+    id AS id_contestation,
+    repair_request_id AS id_repair_request,
+    reviewer_id AS id_reviewer,
+    uuid,
+    comment,
+    reason,
+    origin,
+    type,
+    cost,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_inspections_raw.contestation
