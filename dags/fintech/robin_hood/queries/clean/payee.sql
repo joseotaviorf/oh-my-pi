bfe3c226ba4a27ccb438da407be98f3d21a3c2fd
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2025-02-26 16:33:13 -0300

    Removing hard coded anonymization from payee (#17989)

--- dags/fintech/robin_hood/queries/clean/payee.sql
+++ dags/fintech/robin_hood/queries/clean/payee.sql
@@ -5,9 +5,9 @@ select
     external_affiliate_id as id_external_affiliate,
     external_photographer_id as id_external_photographer,
     external_salesman_id as id_external_salesman,
-    sha2(name,256) as name,
-    sha2(email,256) as email,
-    sha2(document_number,256) as document_number,
+    name,
+    email,
+    document_number,
     timestamp(created_at) as ts_created,
     timestamp(updated_at) as ts_updated
 from
