fd9e08d9397146ea54b0aec44cf0d705975f6374
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2025-02-26 09:53:23 -0300

    [AEFR] Update column name from proj_fup_agents gsheets (#17971)
    * change column name
    
    * update yml

--- dags/for_rent/gsheets_for_rent/queries/clean/proj_fup_agents.sql
+++ dags/for_rent/gsheets_for_rent/queries/clean/proj_fup_agents.sql
@@ -1,14 +1,14 @@
 SELECT
-    CAST(tp_id AS BIGINT) AS id_visitor,
-    CAST(imovel_id AS BIGINT) AS id_house,
+    CAST(visitor_id AS BIGINT) AS id_visitor,
+    CAST(id_property AS BIGINT) AS id_house,
     CAST(owner_id AS BIGINT) AS id_owner,
-    CAST(offer_id AS BIGINT) AS id_offer,
+    CAST(id_offer AS BIGINT) AS id_offer,
     CAST(agent_id AS BIGINT) AS id_agent,
     region_code,
     offer_status,
-    type_offer AS offer_type,
+    offer_type,
     fup_agents_type,
     gt_gc,
-    TO_DATE(os_date, 'yyyy-MM-dd') AS dt_offer_sent
+    TO_DATE(offer_sent_date, 'yyyy-MM-dd') AS dt_offer_sent
 FROM
     datalake_gsheets_raw.fup_agents_historico
