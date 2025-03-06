9ce735f80fd9bad27bf2dc9f6964a5424cfc7ee4
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-03-06 14:34:48 -0300

    [AE-AGENTS] Agents EBDB - Remove unused tables [3] (#18050)
    * remove datalake_ebdb_clean.rating_label dependencies
    
    * remove datalake_ebdb_clean.real_estate_agent_rating [2]
    
    * Update dependencies.yaml
    
    * remove unused edbd tables
    
    * Revert "remove datalake_ebdb_clean.real_estate_agent_rating [2]"
    
    This reverts commit 3dcad5167747deab78610bcd9ab7c4f8859db6ed.
    
    * Revert "remove datalake_ebdb_clean.rating_label dependencies 2"
    
    This reverts commit 0c7f3490c71772c9385271ed50c6d7d4996044ac.
    
    * Revert "remove datalake_ebdb_clean.real_estate_agent_rating [1]"
    
    This reverts commit 372adb6c818a9ef4c8b4489b354b3dfcf3f5fa81.
    
    * remove real_estate_agent_rating_rating_label
    
    * fix dependencies
    
    * Update fact_listing_sale_flows.yml
    
    * Reapply "remove datalake_ebdb_clean.real_estate_agent_rating [1]"
    
    This reverts commit 7dadf527b818456aea27a3ec3a486cb8ca8c144b.
    
    * Reapply "remove datalake_ebdb_clean.rating_label dependencies 2"
    
    This reverts commit e2cb061aaeef319f713a451689f42bcbea57d030.
    
    * Reapply "remove datalake_ebdb_clean.real_estate_agent_rating [2]"
    
    This reverts commit aa5c9d4f34a8595f80395eda53fbe418eb84fdd7.

--- dags/for_rent/ebdb/queries/clean/real_estate_agent_rating.sql
+++
@@ -1,8 +0,0 @@
-select
-    id,
-    rating,
-    complementaryinfo as complementary_info,
-    atualizadoem as ts_updated,
-    criadoem as ts_created
-from
-    datalake_ebdb_raw.RealEstateAgentRating
