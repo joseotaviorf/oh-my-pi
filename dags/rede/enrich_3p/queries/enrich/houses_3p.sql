211cc29310db3d6d21b436b420dba7e990a22dfb
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-03-10 08:56:17 -0300

    [RD] Deprecating `enrich_3p` DAG (#17948)
    * deprecating enrich_3p dag
    
    * update dependencies

--- dags/rede/enrich_3p/queries/enrich/houses_3p.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    h.id AS id_house,
-    h.internal_admin_info,
-    partner_3p_supply AS partner,
-    NOW() AS ts_load
-FROM
-    datalake_ebdb_listing.house AS h
-WHERE
-    is_3p_supply
