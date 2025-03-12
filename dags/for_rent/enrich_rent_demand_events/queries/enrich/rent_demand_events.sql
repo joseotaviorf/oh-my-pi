e452bf782bfcf96544123c90e9eb520711194be8
Jonas Mendes Fiorini <jonasfiorini7@gmail.com>
2025-03-12 14:11:37 -0300

    fix missing offer status filter (#18128)

--- dags/for_rent/enrich_rent_demand_events/queries/enrich/rent_demand_events.sql
+++ dags/for_rent/enrich_rent_demand_events/queries/enrich/rent_demand_events.sql
@@ -33,7 +33,7 @@ WITH rent_flow_house_listing AS (
     END AS id_booking,
     CASE
       WHEN rf.id_offer_context REGEXP '[0-1]{{2}}$' THEN NULL
-      WHEN off.ts_first_sent IS NOT NULL OR (off.status = 'Aprovada' AND off.ts_analyzed IS NOT NULL) THEN rf.id_offer_context
+      WHEN off.ts_first_sent IS NOT NULL OR (off.status IN ('Aprovada', 'ACCEPTED') AND off.ts_analyzed IS NOT NULL) THEN rf.id_offer_context
       ELSE NULL
     END AS id_offer,
     /** Conditions to accept an offer:
