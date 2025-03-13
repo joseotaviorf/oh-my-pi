92928af430db944e9fcd72dc556cb11c420c2595
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-03-13 15:33:51 -0300

    [RD-2039-1] Updating Hubspot's `owner` and `enrich_hubspot_management` DAG documentation (#18046)
    * update owner query, owner metadata and dag documentation
    
    * updating dependencies
    
    * updating tables documentation
    
    * Update dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
    
    * removing current_owner cte
    
    ---------
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>

--- dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
+++ dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
@@ -1,32 +1,44 @@
 WITH most_recent_id_user AS (
-    SELECT
-        id_owner,
-        id_user
-    FROM
-        datalake_hubspot.owner_history
-    WHERE
-        id_user IS NOT NULL
-    QUALIFY
-        ROW_NUMBER() OVER (PARTITION BY id_owner ORDER BY ts_updated DESC) = 1
-        AND ROW_NUMBER() OVER (PARTITION BY id_user ORDER BY ts_updated DESC) = 1
+  SELECT
+    oh.id_owner,
+    oh.id_user
+  FROM
+    datalake_hubspot.owner_history AS oh
+  WHERE
+    oh.id_user IS NOT NULL
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY oh.id_owner, oh.id_user ORDER BY oh.ts_updated DESC) = 1
 )
 SELECT
-    oh.id_owner,
-    COALESCE(oh.id_user, mriu.id_user) AS id_user,
-    email,
-    first_name,
-    last_name,
-    teams,
-    is_archived,
-    ts_created,
-    ts_updated,
-    year,
-    month,
-    day
+  oh.id_owner,
+  team.id AS id_current_team,
+  COALESCE(oh.id_user, mriu.id_user) AS id_user,
+  p.uuid_person,
+  team.name AS current_team_name,
+  oh.email,
+  oh.first_name,
+  oh.last_name,
+  SPLIT(oh.email, '@')[0] AS owner_user,
+  oh.teams,
+  oh.is_archived,
+  oh.ts_created,
+  oh.ts_updated,
+  oh.year,
+  oh.month,
+  oh.day
 FROM
-    datalake_hubspot.owner_history AS oh
+  datalake_hubspot.owner_history AS oh
 LEFT JOIN
-    most_recent_id_user AS mriu
-        ON oh.id_owner = mriu.id_owner
+  most_recent_id_user AS mriu
+    ON oh.id_owner = mriu.id_owner
+LEFT JOIN
+  datalake_person_clean.contact_info AS ci
+    ON oh.email = ci.contact_info
+    AND ci.category = 'EMAIL'
+LEFT JOIN
+  datalake_person_clean.person AS p
+    ON p.id = ci.id_person
+LATERAL VIEW
+  POSEXPLODE_OUTER(oh.teams) exploded_teams AS position, team
 QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY oh.id_owner ORDER BY ts_updated DESC) = 1
+  ROW_NUMBER() OVER (PARTITION BY oh.id_owner ORDER BY position DESC) = 1
