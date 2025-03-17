6c180ad600fc058284a2bc35be7490b0b5ffb2f7
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2025-03-17 11:13:10 -0300

    Reverts PR 18126 (#18211)

--- dags/governance/metabase/queries/clean/query.sql
+++ dags/governance/metabase/queries/clean/query.sql
@@ -1,7 +1,6 @@
 SELECT
     query,
     query_hash,
-    to_base64(query_hash) AS query_hash_string,
     average_execution_time AS average_execution_time_in_milliseconds
 FROM
     datalake_metabase_raw.query
