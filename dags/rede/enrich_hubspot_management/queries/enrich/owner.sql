b612bf8580b3c3ce5e15442d89a940409e08fcb9
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-03-19 10:00:42 -0300

    [RD] Removing `most_recent_id_user` from Hubspot Owner's query (#18246)
    * removing most recent user from query
    
    * Update dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
    
    * Update dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>

--- dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
+++ dags/rede/enrich_hubspot_management/queries/enrich/owner.sql
@@ -1,44 +1,49 @@
-WITH most_recent_id_user AS (
+WITH current_owner AS (
   SELECT
     oh.id_owner,
-    oh.id_user
+    oh.id_user,
+    oh.email,
+    oh.first_name,
+    oh.last_name,
+    oh.teams,
+    oh.is_archived,
+    oh.ts_created,
+    oh.ts_updated,
+    oh.year,
+    oh.month,
+    oh.day
   FROM
     datalake_hubspot.owner_history AS oh
-  WHERE
-    oh.id_user IS NOT NULL
   QUALIFY
-    ROW_NUMBER() OVER (PARTITION BY oh.id_owner, oh.id_user ORDER BY oh.ts_updated DESC) = 1
+    ROW_NUMBER() OVER (PARTITION BY oh.id_owner ORDER BY oh.ts_updated DESC) = 1
 )
 SELECT
-  oh.id_owner,
+  co.id_owner,
   team.id AS id_current_team,
-  COALESCE(oh.id_user, mriu.id_user) AS id_user,
+  co.id_user,
   p.uuid_person,
   team.name AS current_team_name,
-  oh.email,
-  oh.first_name,
-  oh.last_name,
-  SPLIT(oh.email, '@')[0] AS owner_user,
-  oh.teams,
-  oh.is_archived,
-  oh.ts_created,
-  oh.ts_updated,
-  oh.year,
-  oh.month,
-  oh.day
+  co.email,
+  co.first_name,
+  co.last_name,
+  SPLIT(co.email, '@')[0] AS owner_user,
+  co.teams,
+  co.is_archived,
+  co.ts_created,
+  co.ts_updated,
+  co.year,
+  co.month,
+  co.day
 FROM
-  datalake_hubspot.owner_history AS oh
-LEFT JOIN
-  most_recent_id_user AS mriu
-    ON oh.id_owner = mriu.id_owner
+  current_owner AS co
 LEFT JOIN
   datalake_person_clean.contact_info AS ci
-    ON oh.email = ci.contact_info
+    ON co.email = ci.contact_info
     AND ci.category = 'EMAIL'
 LEFT JOIN
   datalake_person_clean.person AS p
     ON p.id = ci.id_person
 LATERAL VIEW
-  POSEXPLODE_OUTER(oh.teams) exploded_teams AS position, team
+  POSEXPLODE_OUTER(co.teams) exploded_teams AS position, team
 QUALIFY
-  ROW_NUMBER() OVER (PARTITION BY oh.id_owner ORDER BY position DESC) = 1
+  ROW_NUMBER() OVER (PARTITION BY co.id_owner ORDER BY position DESC) = 1
