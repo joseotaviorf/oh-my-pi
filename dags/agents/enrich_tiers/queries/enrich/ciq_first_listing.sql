d0c264458a33f3634527fe60f1b62a91c3780321
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-03-19 09:26:59 -0300

    [AE-AGENTS] Adding rent listings in ciq_first_listing (#18232)
    * add Rent ciq listing
    
    * fix

--- dags/agents/enrich_tiers/queries/enrich/ciq_first_listing.sql
+++ dags/agents/enrich_tiers/queries/enrich/ciq_first_listing.sql
@@ -10,27 +10,49 @@ WITH filter_bimester AS (
     WHERE
         ad.date BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
 ),
-ciq_first_listing AS (
-    SELECT DISTINCT
+house_listing_consultant AS (
+    SELECT 
         hslc.id_house,
         hslc.id_user,
         hslc.consultant_type,
-        lbc.ts_first_listing,
-        GREATEST(lbc.ts_first_listing, TIMESTAMP(hslc.ts_enrollment_started), TIMESTAMP(hslc.dt_consultant_started)) AS ts_updated
+        hslc.is_last_ciq_on_listing,
+        hslc.ts_enrollment_started,
+        hslc.dt_consultant_started
     FROM
         datalake_big_agent.house_sale_listing_consultant AS hslc
+    UNION
+    SELECT 
+        hrlc.id_house,
+        hrlc.id_user,
+        hrlc.consultant_type,
+        hrlc.is_last_ciq_on_listing,
+        hrlc.ts_enrollment_started,
+        hrlc.dt_consultant_started
+    FROM
+        datalake_big_agent.house_rent_listing_consultant AS hrlc
+),
+ciq_first_listing AS (
+    SELECT DISTINCT
+        hlc.id_house,
+        hlc.id_user,
+        hlc.consultant_type,
+        lbc.business_context,
+        lbc.ts_first_listing,
+        GREATEST(lbc.ts_first_listing, TIMESTAMP(hlc.ts_enrollment_started), TIMESTAMP(hlc.dt_consultant_started)) AS ts_updated
+    FROM
+        house_listing_consultant AS hlc
     JOIN
         datalake_ebdb_listing.listing_business_context AS lbc
-            ON lbc.id_house = hslc.id_house
-            AND lbc.business_context = 'SALE'
+            ON lbc.id_house = hlc.id_house
     WHERE
-        hslc.is_last_ciq_on_listing = True
-        AND hslc.consultant_type IN ('CIQ_FULL', 'CIQ_MANAGER')
+        hlc.is_last_ciq_on_listing = True
+        AND hlc.consultant_type IN ('CIQ_FULL', 'CIQ_MANAGER')
 )
 SELECT
     cfl.id_house,
     cfl.id_user,
     cfl.consultant_type,
+    cfl.business_context,
     cfl.ts_first_listing IS NOT NULL AS has_first_listing,
     cfl.ts_first_listing,
     cfl.ts_updated,
