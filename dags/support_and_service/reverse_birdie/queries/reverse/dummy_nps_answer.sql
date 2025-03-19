8142cf5c4a6205995476e33e8a06a07c192de854
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-03-19 18:41:01 -0300

    Update query dummy, adding score column (#18283)

--- dags/support_and_service/reverse_birdie/queries/reverse/dummy_nps_answer.sql
+++ dags/support_and_service/reverse_birdie/queries/reverse/dummy_nps_answer.sql
@@ -1,20 +1,30 @@
 with main AS (
-    SELECT 
-        uuid() AS sk_nps_answer,
-        CAST(rand() * 1000 AS INT) AS sk_user,
-        uuid() AS uuid_person,
-        CONCAT('Campaign_', CAST(rand() * 100 AS INT)) AS campaign_name,
-        CONCAT('Comment_', CAST(rand() * 1000 AS INT)) AS comment,
-        CASE WHEN rand() < 0.5 THEN 'Positive' ELSE 'Negative' END AS score_category,
-        year(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS year,
-        month(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS month,
-        day(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS day,
-        CURRENT_DATE AS ts_load
-    FROM
-        range(1000)
-    )
-SELECT 
-    *, 
-    MAKE_DATE(year, month, day) AS ts_answer
-FROM
-    main 
+                SELECT 
+                    uuid() AS sk_nps_answer,
+                    CAST(rand() * 1000 AS INT) AS sk_user,
+                    uuid() AS uuid_person,
+                    CONCAT('Campaign_', CAST(rand() * 100 AS INT)) AS campaign_name,
+                    CONCAT('Comment_', CAST(rand() * 1000 AS INT)) AS comment,
+                    rand() AS score,
+                    year(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS year,
+                    month(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS month,
+                    day(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS day,
+                    CURRENT_DATE AS ts_load
+                FROM
+                    range(1000)
+                )
+            SELECT 
+                m.sk_nps_answer,
+                m.sk_user,
+                m.uuid_person,
+                m.campaign_name,
+                m.score,
+                m.comment,
+                CASE WHEN m.score < 0.5 THEN 'Positive' ELSE 'Negative' END AS score_category,
+                m.year,
+                m.month,
+                m.day,
+                MAKE_DATE(m.year, m.month, m.day) AS ts_answer,
+                m.ts_load
+            FROM
+                main AS m
