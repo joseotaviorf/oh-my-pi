655443ae700180354e8cd398c8b15c9b3d1070fe
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-03-19 14:07:52 -0300

    Add year, month and day columns to dummy query (#18263)
    * Add year, month and day columns to dummy query
    
    * Reduce time range for random date column

--- dags/support_and_service/reverse_birdie/queries/reverse/dummy_nps_answer.sql
+++ dags/support_and_service/reverse_birdie/queries/reverse/dummy_nps_answer.sql
@@ -1,10 +1,20 @@
+with main AS (
+    SELECT 
+        uuid() AS sk_nps_answer,
+        CAST(rand() * 1000 AS INT) AS sk_user,
+        uuid() AS uuid_person,
+        CONCAT('Campaign_', CAST(rand() * 100 AS INT)) AS campaign_name,
+        CONCAT('Comment_', CAST(rand() * 1000 AS INT)) AS comment,
+        CASE WHEN rand() < 0.5 THEN 'Positive' ELSE 'Negative' END AS score_category,
+        year(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS year,
+        month(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS month,
+        day(date_format(current_date() - CAST(rand() * 90 AS INT), 'yyyy-MM-dd')) AS day,
+        CURRENT_DATE AS ts_load
+    FROM
+        range(1000)
+    )
 SELECT 
-    uuid() AS sk_nps_answer,
-    uuid() AS uuid_person,
-    CONCAT('Campaign_', CAST(rand() * 100 AS INT)) AS campaign_name,
-    CONCAT('Comment_', CAST(rand() * 1000 AS INT)) AS comment,
-    CASE WHEN rand() < 0.5 THEN 'Positive' ELSE 'Negative' END AS score_category,
-    date_format(current_date() - CAST(rand() * 365 AS INT), 'yyyy-MM-dd') AS ts_answer,
-    CAST(rand() * 1000 AS INT) AS sk_user
+    *, 
+    MAKE_DATE(year, month, day) AS ts_answer
 FROM
-    range(1000)
+    main 
