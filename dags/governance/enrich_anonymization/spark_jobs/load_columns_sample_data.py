afa80e6a42a20c03497ace1c5f50486997446934
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2025-03-20 15:10:25 -0300

    chore: add dag declaration and pyspark to sample data. (#18154)
    * chore: add dag declaration and pyspark to sample data.
    
    * chore: add clean query, metadata files
    
    * chore: change loader to DetalLoader. change the get columns to sample query to get only not sampled columns.
    
    * chore: change DAG to enrich_anonymiation
    
    * chore: fix documentation file and change the lineage to correct table.
    
    * chore: remove clean queries from enrich_anonymization dag
    
    * fx: dag name dir typo
    
    * fix: spark_jobbs name typo
    
    * add scan files
    
    * update declaration file. add scan files
    
    * fix declaration file
    
    * chore: add load_end_date to get columns_to_sample
    
    * lint: fix query identation
    
    * update scan spark_job
    
    * fix query source
    
    * update declaration. update query styles. update pii spark_job
    
    * fix get sample
    
    * fix udf return
    
    * add limit to test job
    
    * remove superset alteration
    
    * remove custom config
    
    * Trigger security score again
    
    ---------
    
    Co-authored-by: herivelto.junior <herivelto.junior@quintoandar.com.br>

---
+++ dags/governance/enrich_anonymization/spark_jobs/load_columns_sample_data.py
@@ -0,0 +1,202 @@
+import ast
+import logging
+from datetime import datetime
+from argparse import ArgumentParser
+
+from pyspark.sql import DataFrame
+from pyspark.errors import AnalysisException
+
+from bietlejuice.clients.db_clients import SparkClient
+from bietlejuice.base.db import DatalakeMetastoreService
+from bietlejuice.base.spark import SparkDataFrameService
+from bietlejuice.loaders.delta_loader import DeltaLoader
+
+
+DELTA_TABLE_NOT_FOUND = "DELTA_TABLE_NOT_FOUND"
+TABLE_OR_VIEW_NOT_FOUND = "TABLE_OR_VIEW_NOT_FOUND"
+INSUFFICIENT_PERMISSIONS = "INSUFFICIENT_PERMISSIONS"
+JOB_NAME = "load_columns_sample_data"
+
+logging.getLogger("py4j").setLevel(logging.ERROR)
+
+
+def load_table(
+    dataframe: DataFrame,
+    environment: str,
+    datalake_bucket: str,
+    schema: str,
+    table_name: str,
+    merge_on: list = []
+) -> None:
+    print("m=load_table,msg='loading table'")
+
+    db_info = DatalakeMetastoreService.get_db_info(environment, schema, datalake_bucket)
+    database_name = db_info["db_enrich_databricks"]
+    database_location = db_info["db_enrich_path"]
+
+    loader = DeltaLoader()
+    loader.load_table(
+        table_name=f"{database_name}.{table_name}",
+        path=f"{database_location}/{table_name}",
+        source_df=dataframe,
+        merge_on=merge_on,
+    )
+
+
+def create_query_to_sample_data(table: list):
+    """
+    Create a query to sample the data.
+
+    :param columns: List of columns to be smapled.
+    :type columns: List
+    :rtype: str
+    """
+    union_query_sample = ""
+
+    dict_table = table.asDict()
+
+    layer = dict_table["layer"]
+    database_name = dict_table["database_name"]
+    table_name = dict_table["table_name"]
+    columns = dict_table["table_columns"]
+
+    for column in columns:
+        column_sample = """
+            (
+            SELECT
+                '{layer}' AS layer,
+                '{database_name}' AS database_name,
+                '{table_name}' AS table_name,
+                '{column_name}' AS column_name,
+                CAST({column_name} AS STRING) AS column_value
+            FROM
+                {database_name}.{table_name}
+            WHERE
+                {column_name} IS NOT NULL
+                OR CAST({column_name} AS STRING) != ''
+            LIMIT 10
+            )
+        """.format(layer=layer, database_name=database_name, table_name=table_name, column_name=column)
+
+    if not union_query_sample:
+        union_query_sample = column_sample
+    else:
+        union_query_sample += f" UNION ALL {column_sample}"
+
+    return f"""
+        WITH sample AS ({union_query_sample})
+        SELECT
+            CONCAT(database_name, ".", table_name, ".", column_name) AS id_entity,
+            layer,
+            database_name,
+            table_name,
+            column_name,
+            array_agg(column_value) AS sample,
+            date_sub(current_timestamp(), 1) AS ts_ingest
+        FROM
+            sample
+        GROUP BY
+            layer,
+            database_name,
+            table_name,
+            column_name
+        """
+
+
+def get_columns_to_sample(spark_client: SparkClient, load_start_date: str, load_end_date: str):
+    """
+    Read documentation_metrics.columns_metastore to identify all datalake tables and sample data.
+
+    :param sparck_client: QuintoAndar class to manage spark session
+    :type spark_client: SparkClient
+    :rtype: PySpark DataFrame
+    """
+    sql = """
+        WITH columns_datalake AS (
+        SELECT
+            CONCAT(database_name, ".", table_name, ".", column_name) AS id_entity,
+            layer,
+            database_name,
+            table_name,
+            column_name
+        FROM
+            datalake_documentation_metrics_clean.columns_metastore
+        WHERE
+            MAKE_DATE(year, month, day) BETWEEN "{load_start_date}" AND "{load_end_date}"
+        ),
+        columns_to_sample AS (
+        SELECT
+            id_entity
+        FROM
+            datalake_anonymization.columns_sample_data
+        )
+        SELECT
+            layer,
+            database_name,
+            table_name,
+            ARRAY_AGG(column_name) AS table_columns
+        FROM
+            columns_datalake AS cd
+                LEFT JOIN columns_to_sample AS cts
+                    ON cd.id_entity = cts.id_entity
+        WHERE
+            cts.id_entity IS NULL
+        GROUP BY
+            layer,
+            database_name,
+            table_name
+    """.format(load_start_date=load_start_date, load_end_date=load_end_date)
+    return spark_client.get_records(sql)
+
+
+def get_sample_data(spark_client: SparkClient, execution_date: datetime, partition_cols: list):
+    sql = create_query_to_sample_data(table)
+    sample_df = spark_client.get_records(sql)
+    return (
+        SparkDataFrameService()
+        .input(sample_df)
+        .create_year_month_day_columns_from_date(execution_date)
+        .optimize_partitions_by_partition_columns(partition_cols)
+        .output()
+    )
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=JOB_NAME)
+    parser.add_argument("env", type=str)
+    parser.add_argument("datalake_bucket", type=str)
+    parser.add_argument("source", type=str)
+    parser.add_argument("table_name", type=str)
+    parser.add_argument("load_start_date", type=str)
+    parser.add_argument("load_end_date", tyoe=str)
+    parser.add_argument("partitions", type=str)
+    parser.add_argument("merge_on", type=list)
+
+    args = parser.parse_args()
+    env = args.env
+    datalake_bucket = args.datalake_bucket
+    source = args.source
+    table_name = args.table_name
+    load_start_date = args.load_start_date
+    load_end_date = args.load_end_date
+    execution_date = datetime.strptime(load_start_date, "%Y-%m-%d")
+    partition_cols = ast.literal_eval(args.partitions)
+    merge_on = args.merge_on
+
+    spark_client = SparkClient()
+
+    df_tables_to_sample = get_columns_to_sample(spark_client, load_start_date, load_end_date)
+
+    list_tables_to_sample = df_tables_to_sample.collect()
+    for table in list_tables_to_sample:
+        entity_id = f"{table.database_name}.{table.table_name}"
+        try:
+            df_sample = get_sample_data(spark_client, table, execution_date, partition_cols)
+            load_table(df_sample, env, datalake_bucket, source, table_name, merge_on)
+        except AnalysisException as exc:
+            error_class = exc.getErrorClass()
+
+            if error_class in [DELTA_TABLE_NOT_FOUND, TABLE_OR_VIEW_NOT_FOUND]:
+                logging.warning(f"Table {entity_id} not found.")
+            elif error_class in [INSUFFICIENT_PERMISSIONS]:
+                logging.warning(f"Insufficient permission to read table {entity_id}.")
---
+++ dags/governance/enrich_anonymization/spark_jobs/load_pii_scan_into_datalake.py
@@ -0,0 +1,318 @@
+import argparse
+import ast
+import json
+from datetime import datetime
+from typing import Dict, Iterator
+
+from presidio_analyzer.nlp_engine import NlpEngineProvider
+from presidio_analyzer import (
+    AnalyzerEngine,
+    BatchAnalyzerEngine,
+    DictAnalyzerResult,
+    RecognizerRegistry,
+)
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import udf, from_json, col, explode_outer, explode, lit
+from pyspark.sql.types import StructType, StringType, StructField, ArrayType, DoubleType
+
+from quintoandar_logger import QuintoAndarLogger
+
+from bietlejuice.base.db import DatalakeMetastoreService
+from bietlejuice.loaders.delta_loader import DeltaLoader
+
+DATABRICKS_SCOPE = "quintoandar"
+JOB_NAME = "load_pii_scan_into_datalake"
+logger = QuintoAndarLogger(JOB_NAME)
+
+RECOGNIZERS_PATH = "prod_conf.yml"
+SCORE_THRESHOLD = 0.6
+
+ENTITIES_LIST = [
+    "BRAZIL_RG",  # Custom entity
+    "BRAZIL_CPF",  # Custom entity
+    "LOCATION",  # Custom entity
+    "CREDIT_CARD",
+    "EMAIL_ADDRESS",
+    "IP_ADDRESS",
+    "LOCATION",
+    "PERSON",
+    "PHONE_NUMBER",
+    "URL",
+    "DATE_TIME",
+    "BRAZIL_CNPJ",
+]
+
+COLUMN_NAME_ENTITIES = [
+    "BRAZIL_CPF",  # Custom entity
+    "PHONE_NUMBER",  # Custom entity
+    "EMAIL_ADDRESS",  # Custom entity
+    "PERSON",  # Custom entity
+    "BIRTH_DATE",  # Custom entity
+    "LOCATION",
+    "BRAZIL_RG",  # Custom entity
+    "BRAZIL_CNPJ",  # Custom entity
+]
+
+LABELS_TO_IGNORE = {
+    "O",
+    "ORG",
+    "ORGANIZATION",
+    "CARDINAL",
+    "EVENT",
+    "LANGUAGE",
+    "LAW",
+    "MONEY",
+    "ORDINAL",
+    "PERCENT",
+    "PRODUCT",
+    "QUANTITY",
+    "WORK_OF_ART",
+    "FAC",
+}
+
+def load_recognizers_from_yaml() -> RecognizerRegistry:
+    """
+    Load custom recognizers from a YAML configuration file and add them to the registry.
+
+    Returns:
+        registry: A RecognizerRegistry instance with the custom recognizers loaded.
+    """
+    registry = RecognizerRegistry()
+    registry.load_predefined_recognizers()
+    registry.add_recognizers_from_yaml(RECOGNIZERS_PATH)
+
+    return registry
+
+def load_nlp_config() -> Dict:
+    """
+    Load the NLP engine configuration.
+
+    Returns:
+        dict: A dictionary containing the NLP engine configuration.
+    """
+    nlp_config = {
+        "nlp_engine_name": "spacy",
+        "models": [{"lang_code": "en", "model_name": "en_core_web_lg"}],
+        "ner_model_configuration": {"labels_to_ignore": LABELS_TO_IGNORE},
+    }
+
+    return nlp_config
+
+def build_batch_analyzer() -> BatchAnalyzerEngine:
+    """
+    Build and return a BatchAnalyzerEngine instance.
+
+    Returns:
+        BatchAnalyzerEngine: An instance of BatchAnalyzerEngine configured with the custom recognizers and NLP engine.
+    """
+    nlp_config = load_nlp_config()
+    registry = load_recognizers_from_yaml()
+    provider = NlpEngineProvider(nlp_configuration=nlp_config)
+
+    nlp_engine = provider.create_engine()
+
+    analyzer = AnalyzerEngine(nlp_engine=nlp_engine, registry=registry)
+    batch_analyzer = BatchAnalyzerEngine(analyzer_engine=analyzer)
+
+    return batch_analyzer
+
+def analyze(df_dict, is_column_name=False) -> Iterator[DictAnalyzerResult]:
+    """
+    Analyze a dictionary of data for PII entities.
+
+    Args:
+        df_dict (dict): The input dictionary containing data to be analyzed.
+        is_column_name (bool): Flag indicating whether to use column name entities for analysis.
+
+    Returns:
+        Iterator[DictAnalyzerResult]: A list of detected PII entities.
+    """
+    analyzer = build_batch_analyzer()
+    entities_list = ENTITIES_LIST
+    if is_column_name:
+        entities_list = COLUMN_NAME_ENTITIES
+
+    return analyzer.analyze_dict(
+        input_dict=df_dict,
+        entities=entities_list,
+        score_threshold=SCORE_THRESHOLD,
+        language="en",
+    )
+
+def analyze_udf(column_name: str, sample: list, is_column_name=False) -> str:
+  dict_to_analyze = {
+    column_name: sample
+  }
+  analyzer_results = list(analyze(dict_to_analyze, is_column_name=is_column_name))
+  row = []
+  for index, value in enumerate(analyzer_results[0].value):
+    row.append({
+        "value": str(value),
+        "recognizer_results": []
+    })
+    results = analyzer_results[0].recognizer_results[index]
+    if not results:
+      row[index]["recognizer_results"].append({"type": "NOT_FOUND", "score": 0.0})
+    for item in results:
+      row[index]["recognizer_results"].append({"type": item.entity_type, "score": str(item.score)})
+  return json.dumps(row)
+
+def load_table(
+    dataframe: DataFrame,
+    environment: str,
+    datalake_bucket: str,
+    schema: str,
+    table_name: str,
+    partition_cols: list
+) -> None:
+    logger.info("m=load_table,msg='loading table'")
+
+    db_info = DatalakeMetastoreService.get_db_info(environment, schema, datalake_bucket)
+    database_name = db_info["db_enrich_databricks"]
+    database_location = db_info["db_enrich_path"]
+
+    loader = DeltaLoader()
+    loader.load_table(
+        table_name=f"{database_name}.{table_name}",
+        path=f"{database_location}/{table_name}",
+        source_df=dataframe,
+        partition_by=partition_cols
+    )
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(JOB_NAME)
+    parser.add_argument("environment", help="forno/prod values")
+    parser.add_argument("datalake_bucket", help="bucket value in forno/prod")
+    parser.add_argument("schema", help="name of the schema of the table to be saved in the data lake")
+    parser.add_argument("table_name", help="name of the table to be saved in the data lake")
+    parser.add_argument("load_start_date", help="timestamp of the ingest to get the sample from")
+    parser.add_argument("partitions", type=str)
+    args = parser.parse_args()
+
+    logger.info(f"m=main,msg='starting job',environment={args.environment},"
+                f"datalake_bucket={args.datalake_bucket},schema={args.schema},"
+                f"table_name={args.table_name},project_ids={args.project_ids}"
+    )
+
+    return args
+
+def get_sample(date_filter) -> DataFrame:
+    """
+    Get a sample of the data from the specified layer from the sampling table
+    generated by the sampling DAG.
+
+    Args:
+        date_filter (str): The execution date to get the sample from.
+
+    Returns:
+        DataFrame: A DataFrame containing the sample data.
+    """
+    logger.info("m=get_sample,msg='getting sample data'")
+    execution_date = datetime.strptime(date_filter, "%Y-%m-%d")
+    year = execution_date.year
+    month = execution_date.month
+    day = execution_date.day
+    query = f"""
+        SELECT
+          layer,
+          id_entity,
+          database_name,
+          table_name,
+          column_name,
+          sample,
+          ARRAY_AGG(column_name) AS list_column_name,
+          {year} AS year,
+          {month} AS month,
+          {day} AS day
+        FROM
+          datalake_anonymization.columns_sample_data
+        WHERE
+          MAKE_DATE(year, month, day) = '{date_filter}'
+        GROUP BY ALL
+        LIMIT 10
+      """
+    return spark.sql(query)
+
+def main():
+    """
+    Main function to load PII scan results into the datalake.
+    """
+    logger.info("m=main,msg='Starting PII scan load into the datalake'")
+    args = parse_args()
+    partition_cols = ast.literal_eval(args.partitions)
+
+    object_schema = StructType([
+        StructField("type", StringType(), True),
+        StructField("score", DoubleType(), True)
+    ])
+
+    recognizer_schema = ArrayType(StructType([
+        StructField("value", StringType(), True),
+        StructField("recognizer_results", ArrayType(object_schema), True)
+    ]), True)
+
+    udf_analyzer = udf(analyze_udf)
+    df = get_sample(date_filter= args.load_start_date)
+
+    df_analysis = (
+        df
+        .withColumn("analysis", udf_analyzer(df["column_name"], df["sample"]))
+        .withColumn("column_name_analysis", udf_analyzer(df["column_name"], df["list_column_name"], lit(True)))
+        .withColumn("analysis_parsed", from_json(
+            col("analysis"),
+            recognizer_schema
+        ))
+        .withColumn("column_analysis_parsed", from_json(
+            col("column_name_analysis"),
+            recognizer_schema
+        ))
+    )
+
+    df_explode = (
+        df_analysis
+        .select(
+            "layer",
+            "id_entity",
+            "database_name",
+            "table_name",
+            "column_name",
+            "analysis_parsed",
+            "column_analysis_parsed",
+            "year",
+            "month",
+            "day"
+        )
+        .withColumn("analysis_exploded", explode(col("analysis_parsed").alias("analysis_exploded")))
+        .withColumn("column_analysis_exploded",
+                    explode(col("column_analysis_parsed").alias("column_analysis_exploded")))
+        .withColumn("parsed_value", col("analysis_exploded.value").alias("value"))
+        .withColumn("recognizer_results",
+                    explode_outer(col("analysis_exploded.recognizer_results")).alias("recognizer_results"))
+        .withColumn("column_parsed_value", col("column_analysis_exploded.value").alias("column_value"))
+        .withColumn("column_recognizer_results",
+                    explode_outer(col("column_analysis_exploded.recognizer_results")).alias(
+                        "column_recognizer_results"))
+        .select(
+            "layer",
+            "id_entity",
+            "database_name",
+            "table_name",
+            "column_name",
+            "parsed_value",
+            col("recognizer_results.type").alias("type"),
+            col("recognizer_results.score").alias("score"),
+            col("column_recognizer_results.type").alias("column_type"),
+            col("column_recognizer_results.score").alias("column_score"),
+            "year",
+            "month",
+            "day"
+        )
+    )
+    load_table(
+        df_explode,
+        args.environment,
+        args.datalake_bucket,
+        args.schema,
+        args.table_name,
+        partition_cols
+    )
