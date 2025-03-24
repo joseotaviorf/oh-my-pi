5806adfdce306584806a9fb1bbb828f9fb8df549
Allan Moura <145588415+allanmoura5A@users.noreply.github.com>
2025-03-24 11:40:20 -0300

    [DFORSALE-8710] Add new amplitude clean - PM login events (#18322)

---
+++ dags/growth/amplitude/queries/clean/440441_home_page_viewed_events.sql
@@ -0,0 +1,26 @@
+SELECT
+    id_amplitude,
+    id_event,
+    id_user,
+    GET_JSON_OBJECT(user_properties, '$.company_id') AS company_id,
+    GET_JSON_OBJECT(user_properties, '$.email') AS email,
+    GET_JSON_OBJECT(user_properties, '$.login_status') AS login_status,
+    GET_JSON_OBJECT(user_properties, '$.source') AS source,
+    GET_JSON_OBJECT(user_properties, '$.product') AS product,
+    GET_JSON_OBJECT(user_properties, '$.role') AS role,
+    language,
+    city,
+    id_device,
+    device_type,
+    os_name,
+    platform,
+    ts_event,
+    year,
+    month,
+    day
+FROM
+    datalake_amplitude_clean_staging.440441_home_page_viewed_events
+WHERE
+    year={}
+    AND month={}
+    AND day={}
