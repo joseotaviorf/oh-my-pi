caa1bddd4248e791aa7f42c0a0e1dd986a32f4e5
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2025-03-25 15:32:26 -0300

    create invoice table (#18359)

---
+++ dags/for_rent/enrich_condo_monitoring/queries/enrich/invoice.sql
@@ -0,0 +1,43 @@
+WITH
+communication_sent AS (
+  SELECT
+    id_invoice,
+    ARRAY_AGG(DISTINCT communication_type) FILTER (WHERE was_delivered) AS communications_sent
+  FROM
+    datalake_condo_monitoring.communication
+  GROUP BY ALL
+),
+invoice_paid AS (
+  SELECT
+    id AS id_invoice,
+    ts_updated AS ts_paid
+  FROM
+    datalake_condominium_payments_clean.invoice_aud
+  WHERE
+    status = 'PAID'
+)
+SELECT
+  i.id AS id_invoice,
+  i.id_contract,
+  i.status,
+  c.communications_sent,
+  CASE
+    WHEN ARRAY_CONTAINS(c.communications_sent, "WARNING") THEN "WARNING"
+    WHEN ARRAY_CONTAINS(c.communications_sent, "REMINDER") THEN "REMINDER"
+    WHEN c.communications_sent = ARRAY()
+      OR c.communications_sent IS NULL THEN "COMMUNICATION_NOT_FOUND"
+  END AS communication_funnel_step_reached,
+  i.value,
+  IF(ip.ts_paid >= DATE_ADD(i.dt_due, -1) OR ip.id_invoice IS NULL, TRUE, FALSE) AS was_reminder_comm_required,
+  IF(ip.ts_paid >= DATE_ADD(i.dt_due, 3) OR ip.id_invoice IS NULL, TRUE, FALSE) AS was_warning_comm_required,
+  i.dt_due,
+  i.ts_created AS ts_issued,
+  ip.ts_paid
+FROM
+  datalake_condominium_payments_clean.invoice AS i
+LEFT JOIN
+  communication_sent AS c
+    ON i.id = c.id_invoice
+LEFT JOIN
+  invoice_paid AS ip
+    ON i.id = ip.id_invoice
