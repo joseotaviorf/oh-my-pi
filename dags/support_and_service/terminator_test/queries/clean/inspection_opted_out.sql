ef05238285515fe9d2bf17a04bbea11f73f0df70
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2025-03-26 11:11:08 -0300

    Fix id termination and adding year month and day to query (#18377)

--- dags/support_and_service/terminator_test/queries/clean/inspection_opted_out.sql
+++ dags/support_and_service/terminator_test/queries/clean/inspection_opted_out.sql
@@ -1,9 +1,12 @@
 SELECT
     id AS id_inspection_opted_out,
-    terminator_id AS id_terminator,
+    termination_id AS id_termination,
     exit_inspection_opt_out AS is_exit_inspection_opt_out,
     reason,
     reason_details,
-    date_opt_out AS dt_opt_out
+    date_opt_out AS dt_opt_out,
+    year,
+    month,
+    day
 FROM
     datalake_terminator_test_raw.inspection_opted_out
