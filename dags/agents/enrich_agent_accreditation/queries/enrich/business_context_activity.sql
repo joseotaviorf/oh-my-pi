aa5e671758cdef9a6970c0b788f05c22917c4ee4
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-03-26 11:20:17 -0300

    [AE-AGENT] Fix duplicates in enrich_agent_accreditation tables (#18376)
    * Update enrich_agent_accreditation_declaration.yml
    
    * Update business_context_activity.sql

--- dags/agents/enrich_agent_accreditation/queries/enrich/business_context_activity.sql
+++ dags/agents/enrich_agent_accreditation/queries/enrich/business_context_activity.sql
@@ -11,6 +11,7 @@ WITH agent_actions AS (
             ac.dt_action
         ) AS total_active_days, 
         FIRST(ac.id_action) OVER (PARTITION BY ac.id_agent ORDER BY ac.ts_revision DESC) IN (2, 7) AS is_currently_inactive,
+        FIRST(ac.id_action_log) OVER (PARTITION BY ac.id_agent ORDER BY ac.ts_revision DESC) = ac.id_action_log AS is_last_action_log,
         ac.ts_revision,
         ac.dt_action,
         COALESCE(LEAD(ac.dt_action) OVER (PARTITION BY ac.id_agent ORDER BY ac.ts_revision), DATE('{load_end_date}')) AS dt_next_action
@@ -34,6 +35,7 @@ grouped_intervals AS (
         ) OVER (PARTITION BY ac.id_agent ORDER BY ac.ts_revision) AS id_group,
         ac.business_context,
         ac.is_currently_inactive,
+        -- Here we need to rebuild this is_last_action_log column, because the filter ac.id_action NOT IN (2, 7) will eliminate the records and generate a new last record.
         FIRST(ac.id_action_log) OVER (PARTITION BY ac.id_agent ORDER BY ac.ts_revision DESC) = ac.id_action_log AS is_last_action_log,
         ac.dt_action,
         ac.dt_next_action
@@ -41,22 +43,41 @@ grouped_intervals AS (
         agent_actions AS ac
     WHERE
         ac.id_action NOT IN (2, 7)
-        AND ac.total_active_days <> 0
+        AND (
+          ac.total_active_days <> 0
+          OR ac.is_last_action_log
+        )
+),
+business_context_activity AS (
+    SELECT
+        XXHASH64(gi.id_agent, gi.business_context, MIN(gi.dt_action)) AS id_business_context_activity,
+        gi.id_agent,
+        gi.id_user,
+        gi.id_group,
+        gi.business_context,
+        DATEDIFF(MAX(gi.dt_next_action), MIN(gi.dt_action)) AS total_active_days,
+        MIN(gi.dt_action) AS dt_started,
+        CASE  
+            WHEN MAX(gi.is_last_action_log) IS TRUE AND MAX(gi.is_currently_inactive) IS FALSE THEN DATE('{load_end_date}')
+            ELSE MAX(gi.dt_next_action)
+        END AS dt_ended
+    FROM 
+        grouped_intervals AS gi
+    GROUP BY ALL
 )
-SELECT
-    XXHASH64(gi.id_agent, gi.business_context, MIN(gi.dt_action)) AS id_business_context_activity,
-    gi.id_agent,
-    gi.id_user,
-    gi.business_context,
-    DATEDIFF(MAX(gi.dt_next_action), MIN(gi.dt_action)) AS total_active_days,
-    MIN(gi.dt_action) AS dt_started,
-    CASE  
-        WHEN MAX(gi.is_last_action_log) IS TRUE AND MAX(gi.is_currently_inactive) IS FALSE THEN DATE('{load_end_date}')
-        ELSE MAX(gi.dt_next_action)
-    END AS dt_ended
+SELECT 
+    bca.id_business_context_activity,
+    bca.id_agent,
+    bca.id_user,
+    bca.business_context,
+    bca.total_active_days,
+    bca.dt_started,
+    IF(
+        LEAD(bca.dt_started) OVER (PARTITION BY id_agent ORDER BY dt_ended) = bca.dt_ended,
+        bca.dt_ended - INTERVAL 1 DAY,
+        bca.dt_ended
+    ) AS dt_ended
 FROM 
-    grouped_intervals AS gi
+    business_context_activity AS bca 
 WHERE
-    DATE(gi.dt_next_action) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-GROUP BY 
-    gi.id_agent, gi.id_user, gi.business_context, gi.id_group
+    bca.dt_ended BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
