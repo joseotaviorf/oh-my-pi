677d368043efee04d664a8dbf99e6fcf3e0e7545
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2025-03-26 16:31:18 -0300

    remove tables that wont be ingested by cdc (#18378)

--- dags/for_rent/condominium_payments_test/queries/clean/condo_monitoring_eligibility.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    id,
-    contract_id AS id_contract,
-    eligibility,
-    created_at AS ts_created,
-    updated_at AS ts_updated
-FROM
-    datalake_condominium_payments_test_raw.condo_monitoring_eligibility
