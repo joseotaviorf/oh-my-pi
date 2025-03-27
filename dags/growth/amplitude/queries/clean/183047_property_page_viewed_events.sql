bccd016db16c292c55e6efdaf4a1b4152fd2c047
Camila Simões <camila.simoes@quintoandar.com.br>
2025-03-27 14:02:14 -0300

    Include field on yml and sql files (#18407)
    Co-authored-by: Camila De Santana Delgado Simões <camila.simoes@BRSPPM-FG7C3VMP6R.local>

--- dags/growth/amplitude/queries/clean/183047_property_page_viewed_events.sql
+++ dags/growth/amplitude/queries/clean/183047_property_page_viewed_events.sql
@@ -16,6 +16,7 @@ SELECT
     amplitude_event_type,
     GET_JSON_OBJECT(user_properties, '$.ab_beakman_owner_house_setup') AS up_ab_beakman_owner_house_setup,
     GET_JSON_OBJECT(event_properties, '$.house_business_contexts') AS ep_business_context,
+    GET_JSON_OBJECT(event_properties, '$.ad_improvements_button_available') AS ep_ad_improvements_button_available,
     city,
     country,
     data,
