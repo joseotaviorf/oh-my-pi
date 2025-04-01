03394893411851f2034e8dfbc0258888d064dbf0
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2025-04-01 11:34:52 -0300

    Adapts DAG reverse_rent_listings_report_load to allow reprocessing (#18406)
    * Update rent_listing_offer_sent
    
    * Update rent_listing_visit_scheduled
    
    * Update rent_listing_traffic
    
    * Update dependencies file
    
    * Remove unnecessary load tables

--- dags/rede/reverse_rent_listings_report_load/queries/reverse/rent_listing_offer_sent.sql
+++ dags/rede/reverse_rent_listings_report_load/queries/reverse/rent_listing_offer_sent.sql
@@ -1,9 +1,20 @@
+WITH house_status AS (
+  SELECT DISTINCT
+    id_house,
+    status,
+    ts_state_started,
+    ts_state_ended
+  FROM
+    datalake_ebdb_listing.lbc_status_version_order
+  WHERE
+    is_last_state_of_day
+)
 SELECT
   UUID() AS id,
   CONCAT(dl.id_house, dd.year, dd.month, dd.day) AS business_id,
-  fl.sk_region AS location_id,
-  IF(rf.sk_company_supply = -1, '1P', rf.sk_company_supply) AS company_uuid,
   dl.id_house AS property_id,
+  fdi.sk_region AS location_id,
+  IF(fdi.sk_company_supply = -1, '1P', fdi.sk_company_supply) AS company_uuid,
   'RENT' AS business_context,
   COUNT(DISTINCT rf.sk_offer) AS offers_sent_count,
   TIMESTAMP(dd.date) AS ts_event,
@@ -16,15 +27,22 @@ INNER JOIN
   dw_rent.dim_house_listing AS dl
     ON dl.sk_house_listing = rf.sk_house_listing
     AND dl.is_last_version = TRUE
-INNER JOIN
-  dw_rent.fact_house_listings AS fl
-    ON fl.sk_house_listing = dl.sk_house_listing
 LEFT JOIN
   dw_public.dim_date AS dd
     ON dd.sk_date = rf.sk_offer_submitted_date
+INNER JOIN
+  house_status AS hs
+    ON hs.id_house = dl.id_house
+    AND MAKE_DATE(dd.year, dd.month, dd.day) BETWEEN DATE(hs.ts_state_started) AND DATE(hs.ts_state_ended)
+INNER JOIN
+  dw_rent.fact_house_listing_daily_infos AS fdi
+    ON fdi.sk_house = dl.id_house
+    AND fdi.year = dd.year
+    AND fdi.month = dd.month
+    AND fdi.day = dd.day
 WHERE
   MAKE_DATE(dd.year, dd.month, dd.day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-  AND dl.status IN ('publicado', 'PUBLISHED')
+  AND hs.status IN ('publicado', 'PUBLISHED')
 GROUP BY
   2, 3, 4, 5, 6, 8, 9, 10, 11
 HAVING
--- dags/rede/reverse_rent_listings_report_load/queries/reverse/rent_listing_traffic.sql
+++ dags/rede/reverse_rent_listings_report_load/queries/reverse/rent_listing_traffic.sql
@@ -1,30 +1,42 @@
+WITH house_status AS (
+  SELECT DISTINCT
+    id_house,
+    status,
+    ts_state_started,
+    ts_state_ended
+  FROM
+    datalake_ebdb_listing.lbc_status_version_order
+  WHERE
+    is_last_state_of_day
+)
 SELECT
   UUID() AS id,
   CONCAT(dl.id_house, dd.year, dd.month, dd.day) AS business_id,
   dl.id_house AS property_id,
-  fl.sk_region AS location_id,
-  IF(di.sk_company_supply = -1, '1P', di.sk_company_supply) AS company_uuid,
+  fdi.sk_region AS location_id,
+  IF(fdi.sk_company_supply = -1, '1P', fdi.sk_company_supply) AS company_uuid,
   'RENT' AS business_context,
-  SUM(di.page_views) AS traffic_count,
+  SUM(fdi.page_views) AS traffic_count,
   TIMESTAMP(dd.date) AS ts_event,
   dd.year,
   dd.month,
   dd.day
 FROM
-  dw_rent.fact_house_listing_daily_infos AS di
+  dw_rent.fact_house_listing_daily_infos AS fdi
 INNER JOIN
   dw_rent.dim_house_listing AS dl
-    ON dl.id_house = di.sk_house
+    ON dl.id_house = fdi.sk_house
     AND dl.is_last_version = TRUE
-INNER JOIN
-  dw_rent.fact_house_listings AS fl
-    ON fl.sk_house_listing = dl.sk_house_listing
 LEFT JOIN
   dw_public.dim_date AS dd
-    ON dd.sk_date = di.sk_date
+    ON dd.sk_date = fdi.sk_date
+INNER JOIN
+  house_status AS hs
+    ON hs.id_house = dl.id_house
+    AND MAKE_DATE(dd.year, dd.month, dd.day) BETWEEN DATE(hs.ts_state_started) AND DATE(hs.ts_state_ended)
 WHERE
   MAKE_DATE(dd.year, dd.month, dd.day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-  AND dl.status IN ('publicado', 'PUBLISHED')
+  AND hs.status IN ('publicado', 'PUBLISHED')
 GROUP BY
   2, 3, 4, 5, 6, 8, 9, 10, 11
 HAVING
