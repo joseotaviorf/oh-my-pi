ae71c34f1ff5f07db9d8198caceb66405cc057e2
pedronogueiraquinto <155736231+pedronogueiraquinto@users.noreply.github.com>
2025-04-07 18:13:42 +0100

    chore: final fix (#18521)
    * fix: fixing timestamp
    
    * fix: fix naming final

--- dags/primitives/search_metrics_service/queries/clean/search_contamination.sql
+++ dags/primitives/search_metrics_service/queries/clean/search_contamination.sql
@@ -1,8 +1,8 @@
 select
     exp_name,
-    execution_date,
-    user_contamination_percentage,
-    abs_contaminated_users,
-    abs_users
+    date,
+    user_contamination_percentange as user_contamination_percentage,
+    users_who_swapped_ab as abs_contaminated_users,
+    number_of_users as abs_users
 from
   datalake_search_metrics_service_raw.search_contamination
