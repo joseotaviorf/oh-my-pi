b389a692ee9a88ce651d81796708d851cdb7cd5c
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-04-07 14:50:45 -0300

    Aeff 2702 (#18523)
    * add neurotech fields
    
    * add column

--- dags/fintech/velo_neurotech_pj/queries/clean/logs_pj_credit_granting.sql
+++ dags/fintech/velo_neurotech_pj/queries/clean/logs_pj_credit_granting.sql
@@ -1,6 +1,8 @@
 SELECT
     codigo_proposta AS id_proposal,
     codigo_operacao AS id_operation,
+    CAST(CAST(prop_num_proposta AS DOUBLE) AS INT) AS proposal_number,
+    TO_TIMESTAMP(REPLACE(instante, '.000', ''), 'yyyy-MM-dd HH:mm:ss') AS ts_operation,
     year,
     month,
     day
