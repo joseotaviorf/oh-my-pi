7ce44c14fe64312a4948bd4712def3df322b8117
Uilson Bonfim <91347079+uilsonbonfim@users.noreply.github.com>
2025-04-09 14:09:21 -0300

    Adding a new column to GSheet (#18558)

--- dags/growth/gsheets_growth/queries/clean/plaquinhas_installation_pp_organic.sql
+++ dags/growth/gsheets_growth/queries/clean/plaquinhas_installation_pp_organic.sql
@@ -3,6 +3,7 @@ SELECT
     CAST(id_house AS INTEGER) AS id_house, 
     client_type, 
     business_context,
+    state, 
     TO_DATE(dt_install, "dd/MM/yy") AS dt_install
 FROM
     datalake_gsheets_raw.plaquinhas_installation_pp_organic
