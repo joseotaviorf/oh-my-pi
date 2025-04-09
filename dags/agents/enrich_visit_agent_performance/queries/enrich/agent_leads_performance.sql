c6c3da0d681a12a0c5c63b235ce47f6a1f57aeb3
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-04-09 11:26:18 -0300

    [AGENTS] Fix total number of leads in agents performance metrics (#18548)
    * fix total_leads
    
    * Update agent_leads_performance.sql

--- dags/agents/enrich_visit_agent_performance/queries/enrich/agent_leads_performance.sql
+++ dags/agents/enrich_visit_agent_performance/queries/enrich/agent_leads_performance.sql
@@ -1,28 +1,32 @@
-WITH all_leads AS (
-    SELECT
-        ags.id_agent,
-        ags.id_lead,
-        DATE(ags.ts_created) AS dt_reference
+WITH rent_flows_touchpoint AS (
+    SELECT DISTINCT
+        rf.id_rent_flow,
+        COALESCE(rf.first_touchpoint = "DIRECT", FALSE) AS has_direct_first_touchpoint
     FROM
-        datalake_visit_agent_performance.agent_schedules AS ags
-    WHERE
-        ags.has_direct_first_touchpoint IS FALSE
-        AND DATE(ags.ts_created) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-    UNION
+        datalake_rent_flows.rent_flows AS rf
+),
+prospect_daily_results AS (
     SELECT
-        ofe.id_agent,
-        ofe.id_lead,
-        DATE(ofe.ts_event) AS dt_reference
+        pdr.id_agent AS id_user,
+        pdr.id_prospect AS id_lead,
+        DATE(pdr.ts_event) AS dt_reference
     FROM
-        datalake_visit_agent_performance.offer_flow_events AS ofe
+        datalake_demand_flows.prospect_daily_results AS pdr
+    LEFT JOIN
+        rent_flows_touchpoint AS rft
+            ON rft.id_rent_flow = pdr.id_rent_flow
     WHERE
-        ofe.has_direct_first_touchpoint IS FALSE
-        AND DATE(ofe.ts_event) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+        pdr.event_name IN ('USER FIRST ACTIVATION', 'USER RECOVERY')
+        AND rft.has_direct_first_touchpoint IS NOT TRUE
+        AND pdr.id_agent IS NOT NULL
+        AND DATE(pdr.ts_event) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    QUALIFY
+        1 = ROW_NUMBER() OVER(PARTITION BY COALESCE(pdr.id_rent_flow, pdr.id_sale_flow), pdr.event_type, pdr.business_context ORDER BY pdr.ts_event ASC)
 )
 SELECT /*+ RANGE_JOIN(ac, 1500) */ 
     XXHASH64(ac.id_agent, ad.date) AS id_agent_performance,
     ac.id_agent,
-    COALESCE(COUNT(DISTINCT al.id_lead), 0) AS total_leads,
+    COALESCE(COUNT(DISTINCT pdr.id_lead), 0) AS total_leads,
     COALESCE(COUNT(DISTINCT ags.id_lead) FILTER (WHERE ags.has_three_or_more_confirmed_visits_same_agent IS TRUE), 0) AS total_leads_with_three_or_more_confirmed_visits,
     COALESCE(COUNT(DISTINCT ags.id_lead), 0) AS total_lead_to_visit_booking,
     COALESCE(COUNT(DISTINCT ags.id_lead) FILTER (WHERE ags.is_visit_booking_by_agent IS TRUE), 0) AS total_lead_to_visit_booking_by_agent,
@@ -69,9 +73,9 @@ LEFT JOIN
         AND ofe.day = ad.day
         AND ofe.has_direct_first_touchpoint IS FALSE
 LEFT JOIN
-    all_leads AS al
-        ON al.id_agent = ac.id_agent
-        AND al.dt_reference = ad.date
+    prospect_daily_results AS pdr
+        ON pdr.id_user = ac.id_user
+        AND pdr.dt_reference = ad.date
 WHERE
     COALESCE(ags.id_agent, ofe.id_agent) IS NOT NULL
     AND ad.date BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
