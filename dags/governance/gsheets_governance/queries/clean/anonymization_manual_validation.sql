0a0f2ea33bc6ec1c21d2fb902401096d8798f922
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2025-04-16 14:16:14 -0300

    [DG-2243] Add new columns to manage exception cases (#18631)
    * chore: add nre columns to manage exception cases.
    
    * chore: add new columns to use in to exception cases

--- dags/governance/gsheets_governance/queries/clean/anonymization_manual_validation.sql
+++ dags/governance/gsheets_governance/queries/clean/anonymization_manual_validation.sql
@@ -1,6 +1,9 @@
 SELECT
     id_entity,
     manual_eval,
-    ts_ingested
+    requester,
+    aproved_by,
+    justification,
+    to_timestamp(ts_ingested) as ts_ingested
 FROM
     datalake_gsheets_raw.anonymization_manual_validation
