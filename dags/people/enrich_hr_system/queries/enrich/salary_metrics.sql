582dcf775f3789fc85af3fe711d7eed7a19d4179
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-04-16 12:47:21 -0300

    [PEOPLE] Update salary metrics (#18540)
    * Update filter to remove future data. Also, add converted salary value to brl
    
    * fix logic to brl salaries
    
    * add metadata
    
    * add dependencies
    
    * remove extra date filtering
    
    * add a lineage
    
    * remove extra file
    
    * add lineage
    
    * remove blank line

--- dags/people/enrich_hr_system/queries/enrich/salary_metrics.sql
+++ dags/people/enrich_hr_system/queries/enrich/salary_metrics.sql
@@ -28,6 +28,8 @@ WITH cte_salaries AS (
     dt_from
   FROM
     datalake_hr_system_clean.salaries
+  WHERE
+    dt_from <= DATE('{load_end_date}')
 ),
 cte_first_salary_and_promotions AS (
   SELECT
@@ -36,6 +38,8 @@ cte_first_salary_and_promotions AS (
     dt_from
   FROM
     datalake_hr_system_clean.salaries
+  WHERE
+    dt_from <= DATE('{load_end_date}')
   QUALIFY ROW_NUMBER() OVER (
       PARTITION BY id_assignment
       ORDER BY
@@ -50,6 +54,7 @@ cte_first_salary_and_promotions AS (
     datalake_hr_system_clean.salaries
   WHERE
     action_code = 'PROMOTION'
+    AND dt_from <= DATE('{load_end_date}')
 ),
 cte_time_between_promotions AS (
   SELECT
@@ -72,7 +77,19 @@ cte_promotion_metrics AS (
     cte_time_between_promotions
   GROUP BY
     id_assignment
+),
+exchange_rates AS (
+  SELECT
+    currency_from,
+    bid_price AS conversion_rate
+  FROM
+    datalake_awesomeapi_currency_rates_clean.currency_closing_rates
+  WHERE
+    currency_to = 'BRL'
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY currency_from ORDER BY dt_created DESC) = 1
 )
+
 SELECT
   cs.id_assignment,
   REPLACE(
@@ -107,6 +124,7 @@ SELECT
     WHERE
       cs.salary_desc_order = 1
   ) AS current_salary_amount,
+  current_salary_amount * IF(cs.currency_code <> 'BRL', er.conversion_rate, 1) AS current_salary_amount_brl,
   MAX(cs.salary_amount) FILTER (
     WHERE
       cs.salary_asc_order = 1
@@ -150,8 +168,12 @@ SELECT
       cs.action_code = 'PROMOTION'
   ) AS dt_first_promotion
 FROM
-  cte_salaries cs
+  cte_salaries AS cs
 LEFT JOIN
-  cte_promotion_metrics AS cpm ON cpm.id_assignment = cs.id_assignment
+  cte_promotion_metrics AS cpm
+    ON cpm.id_assignment = cs.id_assignment
+LEFT JOIN
+  exchange_rates AS er
+    ON er.currency_from = cs.currency_code
 GROUP BY
   cs.id_assignment
