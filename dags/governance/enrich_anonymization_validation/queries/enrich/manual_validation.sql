be2ff9eddbe104db853cac9545c472b5dfc7976a
Augusto T. Santos <GutoSantos@users.noreply.github.com>
2025-04-17 10:31:46 -0300

    chore: add new columns to use in to exception cases (#18641)

--- dags/governance/enrich_anonymization_validation/queries/enrich/manual_validation.sql
+++ dags/governance/enrich_anonymization_validation/queries/enrich/manual_validation.sql
@@ -1,9 +1,15 @@
 SELECT
     id_entity,
     manual_eval,
+    requester,
+    aproved_by,
+    justification,
     ts_ingested,
     YEAR(ts_ingested) AS year,
     MONTH(ts_ingested) AS month,
     DAY(ts_ingested) AS day
 FROM
     datalake_gsheets_clean.anonymization_manual_validation
+WHERE
+    manual_eval is not null
+    or manual_eval != ''
