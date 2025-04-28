327e0c1c2d175a91c848bf401c7541c96f2149db
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2025-04-28 11:05:55 -0300

    added new columns in trato-feito debt table (#18684)

--- dags/fintech/trato_feito/queries/clean/debt.sql
+++ dags/fintech/trato_feito/queries/clean/debt.sql
@@ -7,8 +7,10 @@ SELECT
     original_amount,
     fine_fee_amount,
     discount_amount,
+    write_off AS is_write_off,
     due_date AS dt_due,
     offset_at AS dt_offset,
+    DATE(write_off_at) AS dt_write_off,
     created_at AS ts_created,
     updated_at AS ts_updated
 FROM
