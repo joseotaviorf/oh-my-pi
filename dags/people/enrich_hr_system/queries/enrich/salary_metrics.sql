a87d80a439cbd4cb7c83fa113506776b28d7991d
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-04-29 15:39:44 -0300

    [PEOPLE] Fix duplicity salary metrics (#18682)
    * add cte filtering salary metrics
    
    * fix filter by removing wrong join

--- dags/people/enrich_hr_system/queries/enrich/salary_metrics.sql
+++ dags/people/enrich_hr_system/queries/enrich/salary_metrics.sql
@@ -1,4 +1,5 @@
-WITH cte_salaries AS (
+WITH
+cte_salaries AS (
   SELECT
     id_assignment,
     action_code,
@@ -9,27 +10,32 @@ WITH cte_salaries AS (
     adjustment_percentage,
     currency_code,
     UPPER(action_reason_code) LIKE 'DISSÍDIO%' AS is_collective_wage_adjustment,
-    ROW_NUMBER() OVER (
-      PARTITION BY id_assignment
-      ORDER BY
-        dt_from ASC
-    ) AS salary_asc_order,
-    ROW_NUMBER() OVER (
-      PARTITION BY id_assignment
-      ORDER BY
-        dt_from DESC
-    ) AS salary_desc_order,
-    ROW_NUMBER() OVER (
-      PARTITION BY id_assignment,
-      action_code
-      ORDER BY
-        dt_from ASC
-    ) AS salary_and_action_asc_order,
+    ROW_NUMBER() OVER (PARTITION BY id_assignment ORDER BY dt_from ASC) AS salary_asc_order,
+    ROW_NUMBER() OVER (PARTITION BY id_assignment ORDER BY dt_from DESC) AS salary_desc_order,
+    ROW_NUMBER() OVER (PARTITION BY id_assignment, action_code ORDER BY dt_from ASC) AS salary_and_action_asc_order,
     dt_from
   FROM
     datalake_hr_system_clean.salaries
   WHERE
     dt_from <= DATE('{load_end_date}')
+),
+cte_last_currency AS (
+  SELECT
+    id_assignment,
+    FIRST_VALUE(currency_code)
+      OVER (PARTITION BY id_assignment ORDER BY dt_from DESC) AS last_currency_code
+  FROM
+    cte_salaries
+),
+cte_filtered_salaries AS (
+  SELECT
+    cs.*,
+    lc.last_currency_code
+  FROM
+    cte_salaries cs
+  INNER JOIN
+    cte_last_currency lc ON cs.id_assignment = lc.id_assignment
+    AND cs.currency_code = lc.last_currency_code
 ),
 cte_first_salary_and_promotions AS (
   SELECT
@@ -37,34 +43,23 @@ cte_first_salary_and_promotions AS (
     salary_amount,
     dt_from
   FROM
-    datalake_hr_system_clean.salaries
-  WHERE
-    dt_from <= DATE('{load_end_date}')
-  QUALIFY ROW_NUMBER() OVER (
-      PARTITION BY id_assignment
-      ORDER BY
-        dt_from
-    ) = 1
+    cte_filtered_salaries
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY id_assignment ORDER BY dt_from) = 1
   UNION
   SELECT
     id_assignment,
     salary_amount,
     dt_from
   FROM
-    datalake_hr_system_clean.salaries
+    cte_filtered_salaries
   WHERE
     action_code = 'PROMOTION'
-    AND dt_from <= DATE('{load_end_date}')
 ),
 cte_time_between_promotions AS (
   SELECT
     id_assignment,
     dt_from AS salary_date,
-    LEAD(dt_from) OVER (
-      PARTITION BY id_assignment
-      ORDER BY
-        dt_from
-    ) AS next_promotion_date,
+    LEAD(dt_from) OVER (PARTITION BY id_assignment ORDER BY dt_from) AS next_promotion_date,
     DATEDIFF(MONTH, salary_date, next_promotion_date) AS time_between_movimentations
   FROM
     cte_first_salary_and_promotions
@@ -86,94 +81,35 @@ exchange_rates AS (
     datalake_awesomeapi_currency_rates_clean.currency_closing_rates
   WHERE
     currency_to = 'BRL'
-  QUALIFY
-    ROW_NUMBER() OVER (PARTITION BY currency_from ORDER BY dt_created DESC) = 1
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY currency_from ORDER BY dt_created DESC) = 1
 )
-
 SELECT
-  cs.id_assignment,
-  REPLACE(
-    MAX(cs.dt_from) FILTER (
-      WHERE
-        NOT cs.is_collective_wage_adjustment
-    ),
-    '-',
-    ''
-  ) AS sk_last_increase_date,
-  REPLACE(
-    MIN(cs.dt_from) FILTER (
-      WHERE
-        cs.action_code = 'PROMOTION'
-    ),
-    '-',
-    ''
-  ) AS sk_first_promotion_date,
-  MAX(cs.currency_code) FILTER (
-    WHERE
-      cs.salary_desc_order = 1
-  ) AS currency_code,
-  COUNT(1) FILTER (
-    WHERE
-      cs.action_code = 'PROMOTION'
-  ) AS qnt_promotions,
-  COUNT(1) FILTER (
-    WHERE
-      NOT cs.is_collective_wage_adjustment
-  ) AS qnt_movimentations,
-  MAX(cs.salary_amount) FILTER (
-    WHERE
-      cs.salary_desc_order = 1
-  ) AS current_salary_amount,
-  current_salary_amount * IF(cs.currency_code <> 'BRL', MAX(er.conversion_rate), 1) AS current_salary_amount_brl,
-  MAX(cs.salary_amount) FILTER (
-    WHERE
-      cs.salary_asc_order = 1
-  ) AS first_salary_amount,
-  MAX(cs.salary_amount) FILTER (
-    WHERE
-      cs.salary_desc_order <> 1
-      AND NOT cs.is_collective_wage_adjustment
-  ) AS previous_salary_amount,
-  MAX(cs.salary_range_mid_point) FILTER (
-    WHERE
-      cs.salary_desc_order = 1
-  ) AS salary_reference,
-  MIN(cs.adjustment_amount) FILTER (
-    WHERE
-      NOT cs.is_collective_wage_adjustment
-  ) AS last_salary_increase,
-  MAX(cs.adjustment_percentage) FILTER (
-    WHERE
-      NOT cs.is_collective_wage_adjustment
-  ) AS pct_last_salary_increase,
-  current_salary_amount - first_salary_amount AS range_salary_movement,
-  MIN(cs.salary_amount) FILTER (
-    WHERE
-      cs.action_code = 'PROMOTION'
-      AND salary_and_action_asc_order = 1
-  ) AS first_promotion_salary,
-  MIN(cs.adjustment_amount) FILTER (
-    WHERE
-      cs.action_code = 'PROMOTION'
-      AND salary_and_action_asc_order = 1
-  ) AS nominal_increase_first_promotion,
-  MIN(cs.adjustment_percentage) FILTER (
-    WHERE
-      cs.action_code = 'PROMOTION'
-      AND salary_and_action_asc_order = 1
-  ) AS pct_increase_first_promotion,
+  fs.id_assignment,
+  REPLACE(MAX(fs.dt_from) FILTER (WHERE NOT fs.is_collective_wage_adjustment), '-', '') AS sk_last_increase_date,
+  REPLACE(MIN(fs.dt_from) FILTER (WHERE fs.action_code = 'PROMOTION'), '-', '') AS sk_first_promotion_date,
+  MAX(fs.currency_code) AS currency_code,
+  COUNT(1) FILTER (WHERE fs.action_code = 'PROMOTION') AS qnt_promotions,
+  COUNT(1) FILTER (WHERE NOT fs.is_collective_wage_adjustment) AS qnt_movimentations,
+  MAX(fs.salary_amount) AS current_salary_amount,
+  MAX(fs.salary_amount) * IF(MAX(fs.currency_code) <> 'BRL', MAX(er.conversion_rate), 1) AS current_salary_amount_brl,
+  MAX(fs.salary_amount) FILTER (WHERE fs.salary_asc_order = 1) AS first_salary_amount,
+  MAX(fs.salary_amount) FILTER (WHERE fs.salary_desc_order <> 1 AND NOT fs.is_collective_wage_adjustment) AS previous_salary_amount,
+  MAX(fs.salary_range_mid_point) AS salary_reference,
+  MIN(fs.adjustment_amount) FILTER (WHERE NOT fs.is_collective_wage_adjustment) AS last_salary_increase,
+  MAX(fs.adjustment_percentage) FILTER (WHERE NOT fs.is_collective_wage_adjustment) AS pct_last_salary_increase,
+  MAX(fs.salary_amount) - MAX(fs.salary_amount) FILTER (WHERE fs.salary_asc_order = 1) AS range_salary_movement,
+  MIN(fs.salary_amount) FILTER (WHERE fs.action_code = 'PROMOTION' AND salary_and_action_asc_order = 1) AS first_promotion_salary,
+  MIN(fs.adjustment_amount) FILTER (WHERE fs.action_code = 'PROMOTION' AND salary_and_action_asc_order = 1) AS nominal_increase_first_promotion,
+  MIN(fs.adjustment_percentage) FILTER (WHERE fs.action_code = 'PROMOTION' AND salary_and_action_asc_order = 1) AS pct_increase_first_promotion,
   MAX(cpm.average_time_between_movimentations) AS average_time_between_movimentations,
-  MIN(cs.dt_from) FILTER (
-    WHERE
-      cs.action_code = 'PROMOTION'
-  ) AS dt_first_promotion
+  MIN(fs.dt_from) FILTER (WHERE fs.action_code = 'PROMOTION') AS dt_first_promotion
 FROM
-  cte_salaries AS cs
+  cte_filtered_salaries AS fs
 LEFT JOIN
   cte_promotion_metrics AS cpm
-    ON cpm.id_assignment = cs.id_assignment
+    ON cpm.id_assignment = fs.id_assignment
 LEFT JOIN
   exchange_rates AS er
-    ON er.currency_from = cs.currency_code
+    ON er.currency_from = fs.last_currency_code
 GROUP BY
-  cs.id_assignment, cs.currency_code
+  fs.id_assignment
