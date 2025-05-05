b1cdf7c204389ab39bcd816a39ee3f919325a5d3
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2025-05-05 14:59:19 -0300

    [DDSS-4870]: Adding photo job tables to cdc dag (#18753)

--- dags/for_rent/ebdb/queries/clean/conservation_item_functional.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    CAST(id AS BIGINT) AS id_conservation_item_functional,
-    CAST(itemId AS BIGINT) AS id_item,
-    isFunctional AS functional_answer,
-    comment,
-    CAST(criadoEm AS TIMESTAMP) AS ts_created,
-    CAST(atualizadoEm AS TIMESTAMP) AS ts_updated
-FROM
-    datalake_ebdb_raw.conservationitemfunctional
--- dags/for_rent/ebdb/queries/clean/conservation_item_status.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    CAST(id AS BIGINT) AS id_conservation_status_item,
-    CAST(itemId AS BIGINT) AS id_item,
-    conservation,
-    CAST(criadoEm AS TIMESTAMP) AS ts_created,
-    CAST(atualizadoEm AS TIMESTAMP) AS ts_updated
-FROM
-    datalake_ebdb_raw.conservationitemstatus
