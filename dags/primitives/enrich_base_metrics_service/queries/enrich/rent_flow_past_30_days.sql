ccc3e441cb83b844e15b0f0d3737604f30e6d58d
pedronogueiraquinto <155736231+pedronogueiraquinto@users.noreply.github.com>
2025-05-06 16:30:03 +0100

    Feat adding info to sale rent flow (#18773)
    * fix: fixing timestamp
    
    * feat: Adding recs impressions incremental table
    
    * feat: Adding more info to sale and rent flow tables. This is an intermediate step to add this info to other tables
    
    * feat: Adding more info to sale and rent flow tables. This is an intermediate step to add this info to other tables

--- dags/primitives/enrich_base_metrics_service/queries/enrich/rent_flow_past_30_days.sql
+++ dags/primitives/enrich_base_metrics_service/queries/enrich/rent_flow_past_30_days.sql
@@ -8,14 +8,33 @@ SELECT
     MIN(coalesce(ts_direct_offer_submitted, ts_offer_submitted)) AS ts_offer,
     MIN(ts_contract_signed) AS ts_contract_signed,
     MIN(ts_direct_offer_submitted) AS ts_direct_offer,
-    MIN(ts_offer_submitted) AS ts_offer_submitted
+    MIN(ts_offer_submitted) AS ts_offer_submitted,
+    MIN(ts_offer_approved) AS ts_offer_approved,
+    GREATEST(
+        MIN(ts_visit_completed),
+        MIN(ts_direct_offer_submitted),
+        MIN(ts_offer_submitted),
+        MIN(ts_contract_signed),
+        MIN(ts_offer_approved)
+    ) AS ts_rent_flow_latest_event
 FROM
     datalake_rent_flows.rent_flows
 WHERE
-    ts_rent_flow_event BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
-    AND id_tenant_prospect IS NOT NULL
+    id_tenant_prospect IS NOT NULL
     AND id_house IS NOT NULL
+    -- This clause is more correct than filtering by ts_rent_flow_event
+    AND
+            (
+             ts_visit_completed BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
+             OR
+             ts_direct_offer_submitted BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
+             OR
+             ts_offer_submitted BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
+             OR
+             ts_contract_signed BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
+             OR
+             ts_offer_approved BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_30}) AND DATE('{end_date}')
+            )
 GROUP BY
     id_tenant_prospect,
     id_house
-
