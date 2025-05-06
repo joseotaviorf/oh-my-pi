62e54438fecb0ab7bd58a3c68dbc54b013332d4a
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2025-05-06 08:31:02 -0300

    fixed schemas from fact_accounting_funnel (#18767)

--- dags/fintech/dw_accounting_funnel/queries/dw/fact_accounting_funnel.sql
+++ dags/fintech/dw_accounting_funnel/queries/dw/fact_accounting_funnel.sql
@@ -156,7 +156,7 @@ SELECT
     dt_sap_reference,
     NOW() AS ts_load    
 FROM 
-    quintocred_bank_settlement
+    datalake_sap_accounting_process.quintocred_bank_settlement
 
 UNION ALL 
 
@@ -183,4 +183,4 @@ SELECT
     dt_sap_reference,
     NOW() AS ts_load    
 FROM 
-    for_rent_bank_settlement
+    datalake_sap_accounting_process.for_rent_bank_settlement
