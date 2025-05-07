79d2298558780e093cc883cd4937dbda9c2cfc3c
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-05-07 09:14:44 -0300

    Deleting dw_tracksale (#18779)
    * Remove DW_tracksale dag structure
    
    * Remove all dw_tracksale dependencies from dependencies file

--- dags/support_and_service/dw_tracksale/queries/dw/fact_nps_answer_justifications.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id_answer AS sk_nps_answer,
-    level,
-    justification,
-    current_timestamp AS ts_load
-FROM 
-    (SELECT * FROM datalake_tracksale.answer_justifications
-    UNION ALL
-    SELECT * FROM datalake_casa_mineira_tracksale.answer_justifications) -- we are merging historical data from Casa Mineira's Tracksale account
