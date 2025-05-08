ee1ebe614500b7bb6e3eeaa4c9160f3219f8e9ff
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2025-05-08 12:42:43 -0300

    [DFFRS-1633] Add new events to delinquency_app_events (#18809)
    * [DFFRS-1633] Add new events to delinquency_app_events
    
    * updating metadata
    
    ---------
    
    Co-authored-by: Bianca Garcia Martins <biigarciam@gmail.com>

--- dags/fintech/enrich_collections_quintoandar/queries/enrich/delinquency_app_events.sql
+++ dags/fintech/enrich_collections_quintoandar/queries/enrich/delinquency_app_events.sql
@@ -1,19 +1,23 @@
 WITH
-pending_invoices_events AS (
+overdue_invoices_events AS (
   SELECT
     id_amplitude,
     id_session,
     id_user,
     FROM_JSON(ep_id_contracts, 'array<bigint>') AS id_contract,
     FROM_JSON(ep_id_invoices, 'array<string>') AS id_invoice,
-    device_brand,
+    device_family,
     "pending_invoices_page_viewed" AS event_name,
     "Pending Invoices" AS funnel_step,
+    3 AS level,
+    "Triggers when user accesses the pending invoices page (overdue and upcoming)" AS event_description,
+    FALSE AS is_active,
     year,
     month,
     day,
     ts_event
-  FROM datalake_amplitude_clean.170698_pending_invoices_page_viewed_events
+  FROM
+    datalake_amplitude_clean.170698_pending_invoices_page_viewed_events
   WHERE
     MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
 
@@ -23,150 +27,294 @@ pending_invoices_events AS (
     id_amplitude,
     id_session,
     id_user,
-    ARRAY(ep_id_contract) AS id_contract,
-    FROM_JSON(ep_id_invoices, 'array<string>') AS id_invoice,
-    device_brand,
-    "rm_pending_invoices_page_viewed" AS event_name,
-    "Pending Invoices" AS funnel_step,
+    FROM_JSON(GET_JSON_OBJECT(event_properties, "$.contract_ids"), 'array<bigint>') AS id_contract,
+    FROM_JSON(GET_JSON_OBJECT(event_properties, "$.invoice_ids"), 'array<string>') AS id_invoice,
+    device_family,
+    "overdue_invoices_page_viewed" AS event_name,
+    "Overdue Invoices" AS funnel_step,
+    3 AS level,
+    "Triggers when user accesses the overdue invoices page (new flow)" AS event_description,
+    TRUE AS is_active,
     year,
     month,
     day,
     ts_event
-  FROM datalake_amplitude_clean.170698_rm_pending_invoices_page_viewed_events
+  FROM
+    datalake_amplitude_clean.170698_overdue_invoices_page_viewed_events
   WHERE
     MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-),
-explode_pending_invoices_events AS (
+
+  UNION ALL
+
+  SELECT
+    id_amplitude,
+    id_session,
+    id_user,
+    FROM_JSON(GET_JSON_OBJECT(event_properties, "$.contract_ids"), 'array<bigint>') AS id_contract,
+    FROM_JSON(GET_JSON_OBJECT(event_properties, "$.invoice_ids"), 'array<string>') AS id_invoice,
+    device_family,
+    "overdue_invoices_invoice_pay_total_button_clicked" AS event_name,
+    "Self Service Negotiation" AS funnel_step,
+    4 AS level,
+    "Triggers when user accesses the page to pay the total amount from overdue invoices (new flow)" AS event_description,
+    TRUE AS is_active,
+    year,
+    month,
+    day,
+    ts_event
+  FROM
+    datalake_amplitude_clean.170698_overdue_invoices_invoice_pay_total_button_clicked_events
+  WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  ),
+  explode_overdue_invoices_events AS (
   SELECT
     contract_invoice.id_contract,
     contract_invoice.id_invoice,
     id_amplitude,
     id_session,
     id_user,
-    device_brand,
+    device_family,
     event_name,
     funnel_step,
+    level,
+    event_description,
+    is_active,
     year,
     month,
     day,
     ts_event
-  FROM pending_invoices_events
+  FROM
+    overdue_invoices_events
   LATERAL VIEW EXPLODE(
     ARRAYS_ZIP(
       id_contract,
       id_invoice
     )
   ) AS contract_invoice
-),
-union_all_events (
-  SELECT
-    id_amplitude,
-    id_session,
-    id_user,
-    ep_id_contract AS id_contract,
-    NULL AS id_invoice,
-    device_brand,
-    "contract_page_viewed" AS event_name,
-    "Contract Page" AS funnel_step,
-    "Triggers when user accesses the contract page under my rent" AS event_description,
-    year,
-    month,
-    day,
-    ts_event
-  FROM datalake_amplitude_clean.170698_contract_page_viewed_events
-  WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  )
 
-  UNION ALL
-
-  SELECT
-    id_amplitude,
-    id_session,
-    id_user,
-    ep_id_contract AS id_contract,
-    NULL AS id_invoice,
-    device_brand,
-    "my_rent_pending_invoices_card_view" AS event_name,
-    "My rent - Pending Invoices" AS funnel_step,
-    "Triggers when user accesses the alert about overdue invoices on the 'My Rent' page" AS event_description,
-    year,
-    month,
-    day,
-    ts_event
-  FROM datalake_amplitude_clean.170698_my_rent_pending_invoices_card_view_events
-  WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-
-  UNION ALL
-
-  SELECT
-    id_amplitude,
-    id_session,
-    id_user,
-    id_contract,
-    id_invoice,
-    device_brand,
-    event_name,
-    funnel_step,
-    "Triggers when user accesses the pending invoices page (overdue and upcoming)" AS event_description,
-    year,
-    month,
-    day,
-    ts_event
-  FROM explode_pending_invoices_events
-
-  UNION ALL
-
-  SELECT
-    id_amplitude,
-    id_session,
-    id_user,
-    ep_id_contract AS id_contract,
-    ep_id_invoice AS id_invoice,
-    device_brand,
-    "pending_invoices_invoice_pay_button_clicked" AS event_name,
-    "Self Service Negotiation" AS funnel_step,
-    "Triggers when user accesses the page to pay de pending invoices" AS event_description,
-    year,
-    month,
-    day,
-    ts_event
-  FROM datalake_amplitude_clean.170698_pending_invoices_invoice_pay_button_clicked_events
-  WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-
-  UNION ALL
-
-  SELECT
-    id_amplitude,
-    id_session,
-    id_user,
-    ep_id_contract AS id_contract,
-    ep_id_invoice AS id_invoice,
-    device_brand,
-    "rm_invoice_payment_option_clicked" AS event_name,
-    "Self Service Negotiation" AS funnel_step,
-    "Triggers when user accesses the page to pay de pending invoices" AS event_description,
-    year,
-    month,
-    day,
-    ts_event
-  FROM datalake_amplitude_clean.170698_rm_invoice_payment_option_clicked_events
-  WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-)
 SELECT
   id_amplitude,
   id_session,
   id_user,
-  BIGINT(id_contract) AS id_contract,
-  BIGINT(id_invoice) AS id_invoice,
-  device_brand,
-  event_name,
-  funnel_step,
-  event_description,
+  ep_id_contract AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "contract_page_viewed" AS event_name,
+  "Contract Page" AS funnel_step,
+  1 AS level,
+  "Triggers when user accesses the contract page under my rent" AS event_description,
+  TRUE AS is_active,
   year,
   month,
   day,
   ts_event
-FROM union_all_events
+FROM
+  datalake_amplitude_clean.170698_contract_page_viewed_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "my_rent_pending_invoices_card_view" AS event_name,
+  "My rent - Pending Invoices" AS funnel_step,
+  2 AS level,
+  "Triggers when user accesses the alert about overdue invoices on the 'My Rent' page" AS event_description,
+  TRUE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_my_rent_pending_invoices_card_view_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  id_contract,
+  id_invoice,
+  device_family,
+  event_name,
+  funnel_step,
+  level,
+  event_description,
+  is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM explode_overdue_invoices_events
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "rm_pending_invoices_page_viewed" AS event_name,
+  "Pending Invoices" AS funnel_step,
+  3 AS level,
+  "Triggers when user accesses the pending invoices page (overdue and upcoming)" AS event_description,
+  FALSE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_rm_pending_invoices_page_viewed_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  ep_id_invoice AS id_invoice,
+  device_family,
+  "pending_invoices_invoice_pay_button_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  4 AS level,
+  "Triggers when user accesses the page to pay an individual pending invoice" AS event_description,
+  FALSE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_pending_invoices_invoice_pay_button_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  ep_id_invoice AS id_invoice,
+  device_family,
+  "overdue_invoices_invoice_pay_button_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  4 AS level,
+  "Triggers when user accesses the page to pay an individual overdue invoice (new flow)" AS event_description,
+  TRUE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_overdue_invoices_invoice_pay_button_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  NULL AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "pending_invoices_pay_total_cta_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  4 AS level,
+  "Triggers when user accesses the page to pay the total amount from pending invoices" AS event_description,
+  FALSE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_pending_invoices_pay_total_cta_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  ep_id_invoice AS id_invoice,
+  device_family,
+  "rm_invoice_payment_option_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  5 AS level,
+  "Triggers when user accesses the page to choose the payment method for an individual pending invoice" AS event_description,
+  FALSE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_rm_invoice_payment_option_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "pending_invoices_payment_option_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  5 AS level,
+  "Triggers when user accesses the page to choose the payment method for an individual pending invoice" AS event_description,
+  TRUE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_pending_invoices_payment_option_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+
+UNION ALL
+
+SELECT
+  id_amplitude,
+  id_session,
+  id_user,
+  ep_id_contract AS id_contract,
+  NULL AS id_invoice,
+  device_family,
+  "pi_negotiation_payment_option_clicked" AS event_name,
+  "Self Service Negotiation" AS funnel_step,
+  5 AS level,
+  "Triggers when user accesses the page to choose the payment method for all overdue invoices (new flow)" AS event_description,
+  TRUE AS is_active,
+  year,
+  month,
+  day,
+  ts_event
+FROM
+  datalake_amplitude_clean.170698_pi_negotiation_payment_option_clicked_events
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
