cca6e9d8aba06dd6d5936610b1f83dc218c4f641
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-05-13 16:06:07 -0300

    [DFCA-210] Update update arquivo_confidencial and deprecate arquivo_confidencial_integration_report (#18855)
    * Update: update arquivo_confidencial and deprecate arquivo_confidencial_integration_report
    
    * Update: dependencies
    
    * Update: SLA monitoring
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/arquivo_confidencial/queries/clean/integration_report.sql
+++ dags/fintech/arquivo_confidencial/queries/clean/integration_report.sql
@@ -12,7 +12,3 @@ SELECT
     day
 FROM
     datalake_arquivo_confidencial_raw.integration_report
-WHERE
-    year = {year}
-    AND month = {month}
-    AND day = {day}
--- dags/fintech/arquivo_confidencial/queries/clean/integration_report_aud.sql
+++ dags/fintech/arquivo_confidencial/queries/clean/integration_report_aud.sql
@@ -13,7 +13,3 @@ SELECT
     day
 FROM
     datalake_arquivo_confidencial_raw.integration_report_aud
-WHERE
-    year = {year}
-    AND month = {month}
-    AND day = {day}
