81b8a01c08364b2b303c86357c2fb119610df09c
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-05-13 14:04:16 -0300

    entry model migration from access_type to unified_model (#18845)
    * entry model migration from access_type to unified_model
    
    * dependencies update
    
    * lineage update
    
    * syntax adjustment

--- dags/for_sale/enrich_sale_listings_lenses/queries/enrich/availability_lens.sql
+++ dags/for_sale/enrich_sale_listings_lenses/queries/enrich/availability_lens.sql
@@ -1,7 +1,7 @@
 WITH available_hours AS (
-  SELECT 
+  SELECT
     id_house,
-    CASE 
+    CASE
       WHEN week_available_hours = 0 THEN '[0]'
       WHEN week_available_hours BETWEEN 1 AND 9 THEN '[1-9]'
       WHEN week_available_hours BETWEEN 10 AND 29 THEN '[10-29]'
@@ -10,124 +10,105 @@ WITH available_hours AS (
     END AS week_available_hours_bins,
     dt_schedule_started,
     DATE_ADD(dt_schedule_ended, -1) AS dt_schedule_ended
-  FROM 
-    datalake_sale_available_booking_hours.weekly_available_booking_hours  
+  FROM
+    datalake_sale_available_booking_hours.weekly_available_booking_hours
 ),
 key_location_by_day AS (
   SELECT
-    a.id,
-    a.id_authorization,
-    a.id_house,
-    a.rev,
-    a_type.name AS key_location,
-    rev.ts_revision AS ts_key_location_started
-  FROM
-    datalake_ebdb_clean.access_type_aud AS a
-  LEFT JOIN
-    datalake_ebdb_clean.access_authorization_type AS a_type
-      ON a.id_authorization = a_type.id
-  LEFT JOIN
-    datalake_ebdb_user.user_revision_entity AS rev
-      ON a.rev = rev.id
-  QUALIFY 
-    ROW_NUMBER() OVER (PARTITION BY id_house, DATE(ts_revision) ORDER BY rev DESC) = 1
-),
-key_location_aux AS (
-  SELECT 
-    id,
     id_house,
     key_location,
-    ts_key_location_started
+    ts_entrance_started AS ts_key_location_started
   FROM
-    key_location_by_day
-  QUALIFY 
-    LAG(id_authorization) OVER (PARTITION BY id_house ORDER BY rev) IS DISTINCT FROM id_authorization
+    datalake_ebdb_listing.house_entrance_history
+  WHERE
+    is_last_status_of_day
+  QUALIFY
+    LAG(key_location) OVER (PARTITION BY id_house ORDER BY ts_entrance_started) IS DISTINCT FROM key_location
 ),
 key_location AS (
-  SELECT 
-    id,
+  SELECT
     id_house,
     key_location,
     ts_key_location_started,
     LEAD(ts_key_location_started) OVER (PARTITION BY id_house ORDER BY ts_key_location_started) AS ts_key_location_ended
   FROM
-    key_location_aux
+    key_location_by_day
 ),
 visits_canceled AS (
-  SELECT 
+  SELECT
     dt_booking AS date,
     b.id_house,
     COUNT(CASE WHEN bc.cancelled_by = 'Owner' THEN b.id END) AS vbs_canceled_by_owner,
     COUNT(CASE WHEN bc.cancelled_by != 'Owner' THEN b.id END) AS vbs_canceled_by_other
   FROM
     datalake_ebdb_clean.booking AS b
-  LEFT JOIN 
+  LEFT JOIN
     datalake_booking.booking_cancellation AS bc
       ON b.id = bc.id_booking
-  WHERE 
+  WHERE
     b.business_context = 'SALE'
     AND b.ts_created IS NOT NULL
-  GROUP BY 
+  GROUP BY
     1, 2
 ),
 visits_completed AS (
-  SELECT 
+  SELECT
     TO_DATE(CAST(dt_booking AS TIMESTAMP) + FLOOR((slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
     id_house,
     COUNT(DISTINCT id) AS visits_completed
   FROM
     datalake_ebdb_clean.booking AS b
-  WHERE 
+  WHERE
     business_context = 'SALE'
     AND ts_created IS NOT NULL
-    AND visit_fup IN ('NaoGostou', 'Talvez', 'VaiNegociar', 'VisitouSozinho') 
-  GROUP BY 
+    AND visit_fup IN ('NaoGostou', 'Talvez', 'VaiNegociar', 'VisitouSozinho')
+  GROUP BY
     1, 2
 ),
 visits_unauthorized_entry AS (
-  SELECT 
+  SELECT
     TO_DATE(CAST(dt_booking AS TIMESTAMP) + FLOOR((slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
     id_house,
     COUNT(DISTINCT id) AS visits_unauthorized_entry
   FROM
     datalake_ebdb_clean.booking AS b
-  WHERE 
+  WHERE
     business_context = 'SALE'
     AND ts_created IS NOT NULL
     AND visit_fup = 'EntradaNaoAutorizada'
-  GROUP BY 
+  GROUP BY
     1, 2
 ),
 suspected_unavailability_listings_aux AS (
-  SELECT 
+  SELECT
     l_aud.id_house,
     DATE(r.ts_revision) AS date
   FROM
     datalake_ebdb_clean.suspected_unavailability_listings_aud AS l_aud
-  INNER JOIN 
+  INNER JOIN
     datalake_ebdb_clean.suspected_unavailability_listings AS l
       ON l_aud.id_house = l.id_house
   INNER JOIN
     datalake_ebdb_user.user_revision_entity AS r
       ON l_aud.rev = r.id
-  QUALIFY 
+  QUALIFY
     LAST(r.ts_revision) OVER (PARTITION BY l_aud.id_house, DATE(r.ts_revision)) = r.ts_revision
     AND l_aud.is_confirmed IS FALSE
-    AND l.is_confirmed IS FALSE 
+    AND l.is_confirmed IS FALSE
 ),
 suspected_unavailability_listings AS (
-  SELECT 
+  SELECT
     id_house,
     1 AS contact_attempts_from_suspicious_listings,
     date
   FROM
     suspected_unavailability_listings_aux
-  WHERE 
+  WHERE
     date <= DATE_SUB(CURRENT_DATE, 3)
   GROUP BY
     1, 2, 3
 ),
-rent_contracts AS ( 
+rent_contracts AS (
     SELECT
     id AS id_contract,
     id_house,
@@ -135,7 +116,7 @@ rent_contracts AS (
     LEAST(TO_DATE(ts_analyst_annulment_input), dt_termination) AS dt_contract_ended
   FROM
     datalake_ebdb_contract.contract
-  WHERE 
+  WHERE
     status_closing = 'ContratoAssinado'
     AND is_canceled IS FALSE
 ),
@@ -173,7 +154,7 @@ status_changes AS (
   SELECT
     id_house,
     id_region,
-    CASE 
+    CASE
       WHEN status = 'PUBLISHED' THEN status
       ELSE 'UNPUBLISHED'
     END AS status,
@@ -183,19 +164,19 @@ status_changes AS (
     status_changes_aux
 ),
 sale_listings_timeline AS (
-  SELECT 
+  SELECT
     lbc.id_house,
     d.date
-  FROM 
+  FROM
     datalake_ebdb_clean.listing_business_context AS lbc
-  JOIN 
+  JOIN
     datalake_quintoandar.aux_date AS d
-      ON d.date BETWEEN TO_DATE(lbc.ts_first_publication) AND CURRENT_DATE 
-  WHERE 
+      ON d.date BETWEEN TO_DATE(lbc.ts_first_publication) AND CURRENT_DATE
+  WHERE
     lbc.business_context = 'SALE'
 ),
 dataset AS (
-  SELECT 
+  SELECT
     t.id_house,
     sc.id_region,
     sc.status,
@@ -208,41 +189,41 @@ dataset AS (
     rc.id_contract IS NOT NULL AS has_active_rental_contract,
     COUNT_IF(rc.id_contract IS NOT NULL) OVER (PARTITION BY t.id_house) > 0 AS has_house_been_rented,
     t.date
-  FROM 
+  FROM
     sale_listings_timeline AS t
-  LEFT JOIN 
+  LEFT JOIN
     visits_completed AS vc
       ON t.id_house = vc.id_house
       AND t.date = vc.date
-  LEFT JOIN 
-    visits_canceled AS cl 
+  LEFT JOIN
+    visits_canceled AS cl
       ON t.id_house = cl.id_house
       AND t.date = cl.date
-  LEFT JOIN 
-    visits_unauthorized_entry AS vu 
+  LEFT JOIN
+    visits_unauthorized_entry AS vu
       ON t.id_house = vu.id_house
       AND t.date = vu.date
-  LEFT JOIN 
+  LEFT JOIN
     suspected_unavailability_listings AS sul
       ON t.id_house = sul.id_house
       AND t.date = sul.date
-  LEFT JOIN 
-    available_hours AS ah 
+  LEFT JOIN
+    available_hours AS ah
       ON t.id_house = ah.id_house
       AND t.date BETWEEN ah.dt_schedule_started AND COALESCE(ah.dt_schedule_ended, CURRENT_DATE())
-  LEFT JOIN 
+  LEFT JOIN
     key_location AS kl
-      ON t.id_house = kl.id_house 
+      ON t.id_house = kl.id_house
       AND t.date BETWEEN kl.ts_key_location_started AND COALESCE(kl.ts_key_location_ended, CURRENT_DATE())
-  LEFT JOIN 
-    rent_contracts AS rc 
+  LEFT JOIN
+    rent_contracts AS rc
       ON t.id_house = rc.id_house
       AND t.date BETWEEN rc.dt_contract_started AND COALESCE(rc.dt_contract_ended, CURRENT_DATE())
-  LEFT JOIN 
-    status_changes AS sc 
+  LEFT JOIN
+    status_changes AS sc
       ON t.id_house = sc.id_house
       AND t.date BETWEEN sc.ts_status_started AND COALESCE(sc.ts_status_ended, CURRENT_DATE())
-  WHERE 
+  WHERE
     sc.status IS NOT NULL
 ),
 business_logic AS (
@@ -250,25 +231,25 @@ business_logic AS (
     id_house,
     id_region,
     status,
-    CASE 
+    CASE
       WHEN key_location = 'OwnerPresent' AND visits_canceled_by_owner_last_45_days >= 1 THEN -10000
       WHEN key_location = 'OwnerPresent' AND visits_canceled_by_owner_last_45_days = 0 THEN -2000
       ELSE 0
     END AS key_location_score,
-    CASE 
+    CASE
       WHEN has_active_rental_contract = TRUE THEN -1000
       ELSE 0
     END AS has_active_rental_contract_score,
     IF(visits_canceled_by_owner_last_45_days >= 1, -3000, 0) AS cancel_by_owner_score,
     visits_unauthorized_entry_last_45_days,
     CASE
-      WHEN visits_unauthorized_entry_last_45_days >= 1 AND visits_completed_last_45_days = 0 THEN -100000 
+      WHEN visits_unauthorized_entry_last_45_days >= 1 AND visits_completed_last_45_days = 0 THEN -100000
       WHEN visits_unauthorized_entry_last_45_days >= 1 THEN -5000
-      ELSE 0 
+      ELSE 0
     END AS cancel_by_unauthorized_entry_score,
-    CASE 
+    CASE
       WHEN contact_attempts_from_suspicious_listings_last_45_days >= 1 THEN -3500
-      ELSE 0 
+      ELSE 0
     END AS suspicious_listing_contact_score,
     week_available_hours_bins,
     CASE week_available_hours_bins
@@ -278,43 +259,43 @@ business_logic AS (
       WHEN '[30-49]' THEN -500
       ELSE 0
     END AS week_available_hours_score,
-    CASE 
+    CASE
       WHEN key_location = 'OwnerPresent' AND visits_canceled_by_owner_last_45_days >= 1 THEN 'and its key location is Owner Present.'
-      WHEN key_location = 'OwnerPresent' AND visits_canceled_by_owner_last_45_days = 0 THEN 'and its key location is ' || key_location 
+      WHEN key_location = 'OwnerPresent' AND visits_canceled_by_owner_last_45_days = 0 THEN 'and its key location is ' || key_location
       WHEN key_location IS NULL OR key_location = 'None' THEN 'and the key location is unknown '
       ELSE 'and its key location is ' || key_location
     END AS key_location_score_disclaimer,
-    CASE 
+    CASE
       WHEN has_active_rental_contract = TRUE THEN 'has an active rental contract '
       ELSE 'doesnt have an active contract '
     END AS has_active_rental_contract_score_disclaimer,
-    CASE 
+    CASE
       WHEN has_house_been_rented = TRUE THEN 'has house been rented'
       ELSE ''
-    END AS has_house_been_rented_score_disclaimer, 
+    END AS has_house_been_rented_score_disclaimer,
     visits_canceled_by_owner_last_45_days,
     CASE
       WHEN visits_canceled_by_owner_last_45_days= 0 THEN 'no visits canceled by the owner in the last 45 days, '
-      WHEN visits_canceled_by_owner_last_45_days BETWEEN 1 AND 3 THEN CONCAT('has ', CAST(visits_canceled_by_owner_last_45_days AS STRING), ' visit canceled by the owner in the last 45 days, ') 
+      WHEN visits_canceled_by_owner_last_45_days BETWEEN 1 AND 3 THEN CONCAT('has ', CAST(visits_canceled_by_owner_last_45_days AS STRING), ' visit canceled by the owner in the last 45 days, ')
       WHEN visits_canceled_by_owner_last_45_days > 3 THEN 'more than 3 visits canceled by the owner in the last 45 days, '
     END AS cancel_by_owner_score_disclaimer,
-    CASE 
+    CASE
       WHEN contact_attempts_from_suspicious_listings_last_45_days >= 1 THEN 'not has confirmed in our whatsapp message about availability of the house, '
       ELSE ''
-    END AS suspicious_listing_contact_score_disclaimer, 
+    END AS suspicious_listing_contact_score_disclaimer,
     CASE week_available_hours_bins
       WHEN '[0]' THEN 'The listing has no available hours this week, '
       WHEN '[1-9]' THEN 'The listing has between 1 and 9 available hours this week, '
-      WHEN '[10-29]' THEN 'The listing has between 10 and 29 available hours this week, ' 
-      WHEN '[30-49]' THEN 'The listing has between 30 and 49 available hours this week, ' 
-      WHEN '[>=50]' THEN 'The listing has over 49 available hours this week, '  
+      WHEN '[10-29]' THEN 'The listing has between 10 and 29 available hours this week, '
+      WHEN '[30-49]' THEN 'The listing has between 30 and 49 available hours this week, '
+      WHEN '[>=50]' THEN 'The listing has over 49 available hours this week, '
     END AS week_available_hours_score_disclaimer,
     date
   FROM
     dataset
 ),
 score AS (
-  SELECT 
+  SELECT
     id_house,
     id_region,
     status,
@@ -333,28 +314,28 @@ score AS (
     CASE LEAST(week_available_hours_score, cancel_by_owner_score, has_active_rental_contract_score, cancel_by_unauthorized_entry_score,suspicious_listing_contact_score, key_location_score)
       WHEN 0 THEN 'Everything looks Great.'
       WHEN week_available_hours_score THEN 'The house could have more available hours to visit. Currently, it has ' || week_available_hours_bins || ' available hours.'
-      WHEN cancel_by_owner_score THEN 'The main reason is because the owner canceled ' || visits_canceled_by_owner_last_45_days || 'visits booked in the last 45 days.' 
+      WHEN cancel_by_owner_score THEN 'The main reason is because the owner canceled ' || visits_canceled_by_owner_last_45_days || 'visits booked in the last 45 days.'
       WHEN has_active_rental_contract_score THEN 'We know that having a tenant in the house is worse for VB2VC Conversion.'
-      WHEN cancel_by_unauthorized_entry_score THEN 'The main reason is because the house had ' || visits_unauthorized_entry_last_45_days || ' unauthorized entries in the last 45 days.' 
+      WHEN cancel_by_unauthorized_entry_score THEN 'The main reason is because the house had ' || visits_unauthorized_entry_last_45_days || ' unauthorized entries in the last 45 days.'
       WHEN suspicious_listing_contact_score THEN 'The main reason is because the owner did not reply to our whatsapp message about the house availability'
       WHEN key_location_score THEN 'The owners provided key location is worse for VB2VC Conversion.'
     END AS main_detractor,
     SIZE(
       FILTER(
-        ARRAY(week_available_hours_score, cancel_by_owner_score, has_active_rental_contract_score, cancel_by_unauthorized_entry_score, suspicious_listing_contact_score, key_location_score), 
+        ARRAY(week_available_hours_score, cancel_by_owner_score, has_active_rental_contract_score, cancel_by_unauthorized_entry_score, suspicious_listing_contact_score, key_location_score),
         x -> x == ARRAY_MIN(ARRAY(week_available_hours_score, cancel_by_owner_score, has_active_rental_contract_score, cancel_by_unauthorized_entry_score, suspicious_listing_contact_score, key_location_score))
       )
-    ) > 1 AS multi_detractors, 
+    ) > 1 AS multi_detractors,
     week_available_hours_score_disclaimer || cancel_by_owner_score_disclaimer || has_active_rental_contract_score_disclaimer || suspicious_listing_contact_score || key_location_score_disclaimer AS drill_down,
     date
-  FROM 
+  FROM
     business_logic
 ),
 create_tiers AS (
-  SELECT 
-    id_house, 
+  SELECT
+    id_house,
     id_region,
-    status, 
+    status,
     availability_score,
     key_location_score,
     week_available_hours_score,
@@ -362,7 +343,7 @@ create_tiers AS (
     has_active_rental_contract_score,
     cancel_by_unauthorized_entry_score,
     suspicious_listing_contact_score,
-    CASE 
+    CASE
       WHEN status = 'UNPUBLISHED' THEN 'DISCARD'
       WHEN availability_score < -100000 THEN 'A1'
       WHEN availability_score BETWEEN -1500 AND 0 THEN 'A5'
@@ -371,19 +352,19 @@ create_tiers AS (
       ELSE 'A2'
     END AS tier,
     IF(
-      multi_detractors, 
+      multi_detractors,
       'This listing has multiple detractors with equal weight for the score, see the drill down.',
       main_detractor) AS tier_disclaimer,
-    drill_down AS tier_drill_down, 
+    drill_down AS tier_drill_down,
     date AS ts_tier_started
   FROM
     score
 ),
 grouping_tiers AS (
-  SELECT 
-    id_house, 
+  SELECT
+    id_house,
     id_region,
-    status, 
+    status,
     availability_score,
     key_location_score,
     week_available_hours_score,
@@ -397,7 +378,7 @@ grouping_tiers AS (
     ts_tier_started
   FROM
     create_tiers
-  QUALIFY 
+  QUALIFY
     ts_tier_started = MIN(IF(status = 'PUBLISHED', ts_tier_started, NULL)) OVER (PARTITION BY id_house)
     OR tier != LAG(tier) OVER (PARTITION BY id_house ORDER BY ts_tier_started)
 ),
@@ -414,7 +395,7 @@ tier_status AS (
     has_active_rental_contract_score,
     cancel_by_unauthorized_entry_score,
     suspicious_listing_contact_score,
-    CASE 
+    CASE
       WHEN tier = 'A5' THEN 'Great Availability'
       WHEN tier = 'A4' THEN 'Good Availability'
       WHEN tier = 'A3' THEN 'Standard Availability'
@@ -448,7 +429,7 @@ aux AS (
     ts_tier_ended
   FROM
     tier_status
-  WHERE 
+  WHERE
     tier != 'DISCARD'
 )
 SELECT
