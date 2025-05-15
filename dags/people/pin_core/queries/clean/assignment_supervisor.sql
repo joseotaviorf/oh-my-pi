1e9b3a61d46a20eb95c641ff4b243ea4bafb9e81
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-05-15 13:58:49 -0300

    [PEOPLE] Add new columns to PIN tables (#18924)
    * add dt_effective to assignment_supervisor
    
    * update metadata

--- dags/people/pin_core/queries/clean/assignment_supervisor.sql
+++ dags/people/pin_core/queries/clean/assignment_supervisor.sql
@@ -13,6 +13,8 @@ SELECT
   CAST(object_version_number AS INT) AS object_version_number,
   TO_DATE(freeze_start_date) AS dt_freeze_started,
   TO_DATE(freeze_until_date) AS dt_freeze_ended,
+  TO_DATE(effective_start_date) AS dt_effective_started,
+  TO_DATE(effective_end_date) AS dt_effective_ended,
   TO_TIMESTAMP(creation_date) AS ts_created,
   TO_TIMESTAMP(last_update_date) AS ts_updated,
   NOW() AS ts_load,
