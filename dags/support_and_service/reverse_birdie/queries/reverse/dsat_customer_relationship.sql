cedca513b9d081e706a50bb7812d5d4d36928ca2
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-05-21 15:57:42 -0300

    Back query to previous pattern (#18984)
    * Back query to old pattern
    
    * Remove duplicated tickets from final list
    
    * Update dependencies file

--- dags/support_and_service/reverse_birdie/queries/reverse/dsat_customer_relationship.sql
+++ dags/support_and_service/reverse_birdie/queries/reverse/dsat_customer_relationship.sql
@@ -1,127 +1,197 @@
-WITH dados_offers as (
+WITH base_answers AS (
+SELECT DISTINCT
+  rc.id_response,
+  rc.ts_collected AS response_date,
+  sr.response_url,
+  PARSE_URL(sr.response_url,'QUERY', 'agent') AS email_agent,
+  PARSE_URL(sr.response_url,'QUERY', 'dt_disparo') AS dt_disparo,
+  CASE
+    WHEN response_url LIKE '%t_id=%7B%7B%' THEN
+      regexp_extract(response_url, 't_id=%7B%7B([0-9]+)%7D%7D', 1)
+    WHEN response_url LIKE '%t_id={{%' THEN
+      regexp_extract(response_url, 't_id=([0-9]+)', 1)
+    ELSE
+      PARSE_URL(sr.response_url, 'QUERY', 't_id')
+  END AS ticket_id,
+  ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] AS pergunta_carinhas,
+  CASE
+    WHEN ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] = 'Extremely happy' then 5
+    WHEN ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] = 'Happy' then 4
+    WHEN ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] = 'Neutral' then 3
+    WHEN ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] = 'Unsatisfied' then 2
+    WHEN ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729626')[0] = 'Extremely unsatisfied' then 1
+    ELSE NULL
+  END AS csat_score,
+  ARRAY_JOIN(ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729629'),' | ') AS resolution,
+  ARRAY_JOIN(ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729630'),' | ') AS need_help,
+  ARRAY_JOIN(ARRAY_AGG(CASE WHEN rc.answer_content IS NOT NULL THEN rc.answer_content END) FILTER(WHERE rc.id_question = '2729628'),' | ') AS comment
+  FROM
+    datalake_survicate.response_content AS rc
+  LEFT JOIN datalake_survicate.survey_responses AS sr
+    ON sr.id_response = rc.id_response
+  LEFT JOIN datalake_survicate.survey_questions AS sq
+    ON sq.id_question = rc.id_question
+  WHERE (sq.id_survey = '2f81495a1a142887') 
+    GROUP BY 1,2,3
+),
+dados_tickets AS (
   SELECT distinct
-    sa.sk_offer,
-    date_format(sa.ts_sale_agreement_signed, 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'') AS ccv_signed_date,
+    em.id_ticket,
+    em.id_user,
+    an.email AS agent_email,
+    CASE 
+      WHEN an.email LIKE '%webhelpbr.com.br' THEN 'webhelp'
+      WHEN an.email LIKE '%quintoandar.com.br' THEN 'quintoandar'
+      ELSE NULL
+    END AS agent_company,
+    em.department,
+    date(em.ts_ticket_started) AS date_started,
+    tkt.sk_sale_offer AS offer_id,
     CASE
-      WHEN payment_method in ('CASH','CASH_USING_FGTS') THEN 'CASH'
-      WHEN payment_method in ('FINANCED','FINANCED_USING_FGTS') THEN 'FINANCED'
+      WHEN GET_JSON_OBJECT(em.custom_fields, '$["[RC] Qual tipo de cliente?"]') = 'sl_rc_' then 'seller'
+      WHEN GET_JSON_OBJECT(em.custom_fields, '$["[RC] Qual tipo de cliente?"]') = 'by_rc_' then 'buyer'
+      ELSE NULL
+    END AS tipo_cliente
+  FROM datalake_customer_support.email em
+  LEFT JOIN dw_customer_support.fact_tickets tkt
+    ON CAST(em.id_ticket AS BIGINT) = tkt.sk_ticket
+  LEFT JOIN dw_customer_support.dim_analyst an
+    ON tkt.sk_first_analyst= an.sk_analyst
+  WHERE 
+    em.channel = 'whatsapp'
+  GROUP BY 
+    1,2,3,5,6,7,8
+  ),
+franchise_last_pre_analysis AS (
+  SELECT 
+    sk_offer,
+    franchise_name
+  FROM 
+    dw_atta.fact_pre_analysis_proposal_flow AS fpp
+  LEFT JOIN dw_atta.dim_franchise_atta AS dfr 
+    ON fpp.sk_franchise = dfr.sk_franchise
+  QUALIFY
+    row_number() over(PARTITION BY sk_offer ORDER BY ts_last_updated DESC) = 1
+),
+dados_offers AS (
+  SELECT DISTINCT
+    sa.sk_offer,
+    DATE(sa.ts_sale_agreement_signed) AS ccv_signed_date,
+    sa.is_ccv_canceled,
+    sa.payment_model,
+    CREDIT_MODEL,
+    CASE
+      WHEN payment_method in ('CASH','CASH_USING_FGTS') 
+      THEN 'CASH'
+      WHEN payment_method in ('FINANCED','FINANCED_USING_FGTS') 
+      THEN 'FINANCED'
       ELSE NULL
     END AS payment_method,
     CASE 
-      WHEN payment_method in ('FINANCED','FINANCED_USING_FGTS') AND CREDIT_MODEL IN ('UNDEFINED','ATTA') THEN 1
-      ELSE NULL
+      WHEN payment_method in ('FINANCED','FINANCED_USING_FGTS') 
+      AND CREDIT_MODEL IN ('UNDEFINED','ATTA') 
+      THEN TRUE
+      ELSE false
     END AS credit_model_flag,
-      dr.city_group,
-      CREDIT_MODEL,
-      sa.financing_bank,
-      sa.seller_dilligence_status as share_risco,
-      sa.HAS_USED_FGTS_IN_PAYMENT as has_used_fgts_in_payment
+    dr.city_group,
+    sa.financing_bank
   FROM dw_sale.dim_sale_agreement sa
   LEFT JOIN dw_sale.fact_offers AS fo
     ON sa.sk_offer = fo.sk_offer
   LEFT JOIN dw_public.dim_region AS dr
     ON fo.sk_region = dr.sk_region
-),
-franchise_last_pre_analysis AS (
-    SELECT 
-      sk_offer,
-      franchise_name
-    FROM 
-      dw_atta.fact_pre_analysis_proposal_flow AS fpp
-    LEFT JOIN dw_atta.dim_franchise_atta AS dfr 
-      ON fpp.sk_franchise = dfr.sk_franchise
-    QUALIFY
-      row_number() over (PARTITION BY sk_offer ORDER BY ts_last_updated DESC) = 1
-) SELECT 
-  'RC' as csat_campanha,
-  zes.response_uuid AS feedback_id,
-  date_format(ftc.ts_last_response, 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'') AS posted_at,
-  ft.sk_ticket AS ticket_id,
-  ft.sk_user as author_id,
-  da.email AS agent_email,
-  da.agent_organization AS agent_company,
-  ft.sk_sale_offer AS offer_id,
-  dt.customer_type,
-  concat(ft.sk_sale_offer, '_', dt.customer_type_tag) as account_id,
+)
+SELECT
+  'RC' AS csat_campanha,
+  csat.id_response AS feedback_id,
+  date_format(csat.response_date, 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'') AS posted_at,
+  csat.ticket_id AS id_ticket,
+  tkt.id_user AS author_id,
+  tkt.agent_email,
+  tkt.agent_company,
+  tkt.offer_id,
+  tkt.tipo_cliente AS customer_type,
+  concat(tkt.offer_id, CASE WHEN tkt.tipo_cliente ='seller' THEN '_seller' ELSE '_buyer' END) AS account_id,
   offer.payment_method,
-  offer.credit_model_flag as internal_vendors_flag,
-  CASE
-    WHEN zes.csat_score >= 4 THEN 'promoter'
-    WHEN zes.csat_score <=2 THEN 'detractor'
-    ELSE 'passive'
+  offer.credit_model_flag AS internal_vendors_flag,
+  CASE WHEN csat.csat_score>= 4 THEN 'promoter'
+    WHEN csat_score <=2 then 'detractor' 
+    ELSE 'passive' 
   END AS csat_score_category,
-  zes.csat_score AS rating,
-  zes.user_comment AS comment,
+  csat.csat_score AS rating,
   CASE 
-    WHEN zes.user_comment IS NULL THEN 'No Signal | Nenhum comentário deixado pelo usuário.'
-    WHEN zes.csat_score IN (1, 2) THEN 'Insatisfeito: ' || CAST(zes.csat_score AS STRING) || ' - ' || zes.user_comment 
-    WHEN zes.csat_score IN (4, 5) THEN 'Satisfeito: ' || CAST(zes.csat_score AS STRING) || ' - ' || zes.user_comment
-    ELSE 'Neutro: ' || CAST(zes.csat_score AS STRING) || ' - ' || zes.csat_score
+    WHEN csat.comment IS NULL 
+    THEN 'No Signal | Nenhum comentário deixado pelo usuário.'
+    WHEN csat.csat_score IN (1, 2) 
+    THEN 'Insatisfeito: ' || CAST(csat.csat_score AS STRING) || ' - ' || csat.comment 
+    WHEN csat.csat_score IN (4, 5) 
+    THEN 'Satisfeito: ' || CAST(csat.csat_score AS STRING) || ' - ' || csat.comment
+    ELSE 'Neutro: ' || CAST(csat.csat_score AS STRING) || ' - ' || csat.comment
   END AS text,
-  CASE
-    WHEN fran.franchise_name = 'QuintoAndar'
-    AND offer.payment_method in ('FINANCED','FINANCED_USING_FGTS') 
-    AND offer.CREDIT_MODEL IN('UNDEFINED','ATTA')    
-    THEN TRUE
-    ELSE FALSE
+  CASE 
+    WHEN fran.franchise_name = 'QuintoAndar' 
+    AND offer.payment_model in ('FINANCED', 'FINANCED_USING_FGTS') 
+    AND offer.CREDIT_MODEL in ('UNDEFINED', 'ATTA')
+    THEN TRUE 
+    ELSE FALSE 
   END AS is_corban,
   offer.city_group,
-  fran.franchise_name as adjusted_franchise_name,
-  offer.share_risco,
-  offer.has_used_fgts_in_payment,
+  fran.franchise_name AS adjusted_franchise_name,
+  a.seller_dilligence_status AS share_risco,
+  a.HAS_USED_FGTS_IN_PAYMENT AS has_used_fgts_in_payment,
   offer.financing_bank,
-  COALESCE (ftc.is_solved, false) AS resolution,
-  dtk.group_name as department,
-  year(ftc.ts_last_response) AS year,
-  month(ftc.ts_last_response) AS month,
-  day(ftc.ts_last_response) AS day,
+  CASE 
+    WHEN csat.resolution='Sim' 
+    THEN TRUE 
+    ELSE FALSE 
+  END AS resolution,
+  dim.group_name AS department,
+  year(csat.response_date) AS year,
+  month(csat.response_date) AS month,
+  day(csat.response_date) AS day,
   NOW() AS ts_load
-FROM dw_customer_support.fact_tickets AS ft 
-LEFT JOIN dw_customer_support.dim_analyst AS da
-  ON da.sk_analyst = ft.sk_last_analyst
-LEFT JOIN dw_customer_support.dim_taxonomy AS dt 
-  ON dt.sk_taxonomy = ft.sk_taxonomy 
-LEFT JOIN dw_satisfaction_rating.fact_ticket_csat AS ftc
-  ON ftc.sk_ticket = ft.sk_ticket
-LEFT JOIN dw_customer_support.dim_ticket AS dtk
-  ON dtk.sk_ticket = ft.sk_ticket
+FROM base_answers AS csat
+LEFT JOIN dados_tickets AS tkt
+  ON csat.ticket_id = tkt.id_ticket
 LEFT JOIN dados_offers AS offer
-  ON offer.sk_offer = ft.sk_sale_offer
-LEFT JOIN franchise_last_pre_analysis as fran
+  ON offer.sk_offer = tkt.offer_id
+LEFT JOIN dw_customer_support.dim_ticket AS dim
+  ON tkt.id_ticket = CAST(dim.sk_ticket AS STRING)
+LEFT JOIN franchise_last_pre_analysis AS fran
   ON fran.sk_offer = offer.sk_offer
-LEFT JOIN datalake_survicate.zendesk_email_surveys AS zes
-  ON CAST(zes.id_ticket AS BIGINT) = ft.sk_ticket
+LEFT JOIN dw_sale.dim_sale_agreement AS a 
+  ON a.sk_offer = offer.sk_offer
 WHERE 
-dtk.channel = 'whatsapp'
-AND DATE(ftc.ts_last_response) >= DATE('{load_start_date}')
-AND ft.sk_ticket NOT IN ('82683066',
-'82497086',
-'82494776',
-'82248542',
-'81944018',
-'81943831',
-'81941902',
-'81876390',
-'81870665',
-'81869979',
-'81804253',
-'81628610',
-'75376578',
-'75966095',
-'76167278',
-'76590026',
-'79111487',
-'79147953',
-'79448132',
-'79479820',
-'79832975',
-'80480862',
-'80539921',
-'80549040',
-'80549802',
-'80562966',
-'80563216',
-'80570523',
-'80571241',
-'80571390',
-'81886440')
+  DATE(csat.response_date) >= DATE('{load_start_date}')
+  AND csat.csat_score IS NOT NULL
+  AND ticket_id not in ('82683066',
+  '82497086',
+  '82494776',
+  '82248542',
+  '81944018',
+  '81943831',
+  '81941902',
+  '81876390',
+  '81870665',
+  '81869979',
+  '81804253',
+  '81628610',
+  '75376578',
+  '75966095',
+  '76167278',
+  '76590026',
+  '79111487',
+  '79147953',
+  '79448132',
+  '79479820',
+  '79832975',
+  '80480862',
+  '80539921',
+  '80549040',
+  '80549802',
+  '80562966',
+  '80563216',
+  '80570523',
+  '80571241',
+  '80571390',
+  '81886440')
