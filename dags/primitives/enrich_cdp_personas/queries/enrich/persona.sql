0e2b4cacc34131dcae14b10ab8eed526ac5b64d3
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-05-21 15:52:39 -0300

    Adding BP to Persona main table (#19003)

--- dags/primitives/enrich_cdp_personas/queries/enrich/persona.sql
+++ dags/primitives/enrich_cdp_personas/queries/enrich/persona.sql
@@ -23,6 +23,17 @@ UNION ALL
 SELECT
     id_user,
     uuid_person,
+    'BUYER_PROSPECT' AS persona,
+    is_active,
+    ts_first_event,
+    ts_last_event,
+    NOW() AS ts_load
+FROM
+    datalake_cdp_personas.buyer_prospect
+UNION ALL
+SELECT
+    id_user,
+    uuid_person,
     'OWNER' AS persona,
     is_active,
     ts_first_event,
