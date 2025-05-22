0391223f13b4ed58fd3792729af2b2b2d2f78643
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-05-22 10:06:15 -0300

    [DBP-878] Refactor: Update org_chart to remove outdated sources and apply improvements (#18998)
    * update source tables
    
    * update lineage
    
    * add first set of data qualities
    
    * add dependencies

--- dags/people/enrich_people_public/queries/enrich/org_chart.sql
+++ dags/people/enrich_people_public/queries/enrich/org_chart.sql
@@ -1,104 +1,101 @@
-WITH manager_cte AS (
-  SELECT DISTINCT
+WITH 
+manager_cte AS (
+  SELECT
     id_period_of_service,
     id_assignment,
     id_person,
     id_manager_assignment
   FROM
-    datalake_hr_system.managers
+    datalake_pin.managers_history
   WHERE
-    dt_effective_start <= current_date
-    AND manager_type = 'LINE_MANAGER'
+    dt_effective_started <= DATE(CURRENT_DATE)
   QUALIFY
-    dt_effective_start = MAX(dt_effective_start)
-    OVER (PARTITION BY id_assignment)
+    ROW_NUMBER() OVER (PARTITION BY id_assignment ORDER BY dt_effective_started DESC) = 1
 ),
 assignment_cte AS (
-  SELECT DISTINCT
-    assignments.id_person,
-    assignments.id_assignment,
-    assignments.assignment_number,
-    assignments.assignment_name,
-    assignments.business_unit_name,
-    assignments.id_cost_center,
-    assignments.cost_center_name,
-    assignments.assignment_status_type,
-    assignments.dt_effective_start,
-    assignments.dt_effective_end,
-    organizations.business,
-    organizations.product,
-    organizations.vertical,
-    organizations.vice_presidency,
-    organizations.directorate,
-    organizations.subdirectorate,
-    manager_cte.id_manager_assignment,
-    manager_assignment.id_person AS id_manager_person,
-    employee_manager.full_name AS manager_name,
-    employee_manager.work_email AS manager_email
+  SELECT
+    a.id_person,
+    a.id_assignment,
+    a.assignment_number,
+    a.assignment_name,
+    a.business_unit_name,
+    a.id_cost_center,
+    a.cost_center_name,
+    a.assignment_status_type,
+    a.dt_effective_start,
+    a.dt_effective_end,
+    o.business,
+    o.product,
+    o.vertical,
+    o.vice_presidency,
+    o.directorate,
+    o.subdirectorate,
+    m.id_manager_assignment,
+    em.id_person AS id_manager_person,
+    em.full_name AS manager_name,
+    em.work_email AS manager_email,
+    jf.name_job_family AS position_class
   FROM
-    datalake_hr_system.assignments
+    datalake_hr_system.assignments AS a
   LEFT JOIN
-    datalake_hr_system_clean.organizations
-      ON organizations.id_organization = assignments.id_cost_center
+    datalake_hr_system_clean.organizations AS o
+      ON o.id_organization = a.id_cost_center
   LEFT JOIN
-    manager_cte
-      ON manager_cte.id_assignment = assignments.id_assignment
+    manager_cte AS m 
+      ON m.id_assignment = a.id_assignment
   LEFT JOIN
-    datalake_hr_system.assignments AS manager_assignment
-      ON manager_assignment.id_assignment = manager_cte.id_manager_assignment
+    datalake_hr_system.employee_ids AS em
+      ON em.id_assignment = m.id_manager_assignment
+  LEFT JOIN 
+    datalake_hr_system_clean.jobs AS j
+      ON j.id_job = a.id_job
   LEFT JOIN
-    datalake_hr_system.employee_ids AS employee_manager
-      ON employee_manager.id_assignment = manager_assignment.id_assignment
+    datalake_hr_system_clean.job_families AS jf
+      ON jf.id_job_family = j.id_job_family
   WHERE
-      assignments.dt_effective_start < current_date()
-      AND assignments.assignment_type IN ('E', 'C')
+    a.dt_effective_start < DATE(CURRENT_DATE)
+    AND a.assignment_type IN ('E', 'C')
   QUALIFY
-    assignments.dt_effective_start = MAX(assignments.dt_effective_start)
-    OVER (PARTITION BY assignments.id_assignment)
+    ROW_NUMBER() OVER (PARTITION BY a.id_assignment ORDER BY a.dt_effective_start DESC) = 1
 )
 
 SELECT
-  employee.assignment_number,
-  employee.full_name AS full_name,
-  employee.work_email,
-  assignment_cte.manager_name,
-  assignment_cte.manager_email,
-  UPPER(assignment_cte.assignment_name) AS assignment_name,
-  pc.position_class AS job_class,
-  assignment_cte.assignment_status_type,
-  assignment_cte.business_unit_name,
-  assignment_cte.cost_center_name,
-  assignment_cte.business,
-  assignment_cte.product,
-  assignment_cte.vertical,
-  assignment_cte.vice_presidency,
-  assignment_cte.directorate,
-  assignment_cte.subdirectorate,
-  sheets.line,
-  sheets.chapter,
-  sheets.line_leader,
-  sheets.team_leader,
-  sheets.team_1 AS product_and_tech_team_1,
-  sheets.team_2 AS product_and_tech_team_2,
-  sheets.team_3 AS product_and_tech_team_3,
-  sheets.team_4 AS product_and_tech_team_4,
-  sheets.team_5 AS product_and_tech_team_5,
-  sheets.team_6 AS product_and_tech_team_6,
-  sheets.team_7 AS product_and_tech_team_7,
-  sheets.team_8 AS product_and_tech_team_8,
-  sheets.team_9 AS product_and_tech_team_9,
-  sheets.team_10 AS product_and_tech_team_10,
+  e.assignment_number,
+  e.full_name AS full_name,
+  e.work_email,
+  a.manager_name,
+  a.manager_email,
+  UPPER(a.assignment_name) AS assignment_name,
+  a.position_class AS job_class,
+  a.assignment_status_type,
+  a.business_unit_name,
+  a.cost_center_name,
+  a.business,
+  a.product,
+  a.vertical,
+  a.vice_presidency,
+  a.directorate,
+  a.subdirectorate,
+  s.line,
+  s.chapter,
+  s.line_leader,
+  s.team_leader,
+  s.team_1 AS product_and_tech_team_1,
+  s.team_2 AS product_and_tech_team_2,
+  s.team_3 AS product_and_tech_team_3,
+  s.team_4 AS product_and_tech_team_4,
+  s.team_5 AS product_and_tech_team_5,
+  s.team_6 AS product_and_tech_team_6,
+  s.team_7 AS product_and_tech_team_7,
+  s.team_8 AS product_and_tech_team_8,
+  s.team_9 AS product_and_tech_team_9,
+  s.team_10 AS product_and_tech_team_10,
   NOW() AS ts_load
 FROM
-  datalake_hr_system.employee_ids AS employee
+  datalake_hr_system.employee_ids AS e
+INNER JOIN
+  assignment_cte AS a
+    ON e.id_assignment = a.id_assignment
 LEFT JOIN
-  assignment_cte
-    ON employee.id_assignment = assignment_cte.id_assignment
-LEFT JOIN
-  datalake_gsheets_people_clean.position_class pc
-    ON UPPER(pc.position) = UPPER(assignment_cte.assignment_name)
-LEFT JOIN
-  datalake_gsheets_people_clean.team_formation_product_tech sheets
-    ON sheets.assignment_number = employee.assignment_number
-WHERE
-  employee.assignment_number NOT LIKE 'P%'
+  datalake_gsheets_people_clean.team_formation_product_tech AS s
+    ON s.assignment_number = e.assignment_number
