ff3041637675ea0ab59dc7776c3f0352934f3aa9
William Angelo <39243586+williamangelo@users.noreply.github.com>
2025-05-26 17:14:06 -0300

    [DECONVXP] Create `enrich_chatbot` (#18874)
    * feat: new enrich chatbot
    
    * fix: table name
    
    * fix: table name
    
    * feat: wall-e data on enrich chatbot
    
    * feat: read 3 months of data
    
    * fix: dependencies file
    
    * docs: update lineage file
    
    * fix: query syntax

---
+++ dags/conversational_xp/enrich_chatbot/queries/enrich/support_sessions.sql
@@ -0,0 +1,62 @@
+WITH orchestrator_sessions AS (
+  SELECT DISTINCT
+    s.id AS id_session,
+    s.id_sauron_session,
+    s.id_user,
+    m.channel,
+    s.ts_created
+  FROM
+    datalake_copilot_service_clean.session AS s
+  INNER JOIN
+    datalake_copilot_service_clean.message AS m
+      ON m.id_session = s.id
+  WHERE
+    s.ts_created BETWEEN DATE('{load_start_date}') - INTERVAL 3 MONTH AND DATE('{load_end_date}')
+    AND m.channel = 'IN_APP_SUPPORT_CHAT'
+),
+escalated_sessions AS (
+  SELECT DISTINCT
+    cs.id_ticket,
+    cs.id_session
+  FROM
+    datalake_customer_support.tickets AS cs
+  INNER JOIN
+    orchestrator_sessions AS bs
+      ON cs.id_session = bs.id_sauron_session
+  WHERE
+    cs.front_or_back = 'front'
+    AND cs.ticket_origin IN ('call in app', 'whatsapp', 'chat in app')
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY cs.id_session ORDER BY cs.ts_updated DESC) = 1
+),
+sessions AS (
+  SELECT
+    bs.id_session,
+    bs.id_sauron_session,
+    es.id_ticket,
+    bs.id_user,
+    es.id_session IS NOT NULL AS is_escalation,
+    bs.ts_created,
+    ss.ts_updated AS ts_finished
+  FROM
+    orchestrator_sessions AS bs
+  LEFT JOIN
+    datalake_sauron_clean.session AS ss
+      ON ss.id = bs.id_sauron_session
+      AND ss.status = 'expired'
+  LEFT JOIN
+    escalated_sessions AS es
+      ON es.id_session = bs.id_sauron_session
+)
+SELECT
+  s.id_session,
+  s.id_sauron_session,
+  s.id_ticket,
+  s.id_user,
+  s.is_escalation,
+  TIMESTAMPDIFF(SECOND, s.ts_created, s.ts_finished) AS session_time_sec,
+  s.ts_created,
+  s.ts_finished,
+  LAG(s.ts_created) OVER(PARTITION BY s.id_user ORDER BY s.ts_created) AS ts_previous_session
+FROM
+  sessions AS s
