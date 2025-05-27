1f2416acb66b30a37fddd91484933b69b43b23f0
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-05-27 16:59:44 -0300

    [CDP] Time window on Tenant Prospect base (#19031)
    * [CDP] Time window on Tenant Prospect base
    
    * adjusting description
    
    * bool col
    
    * revert
    
    * fix

--- dags/primitives/enrich_cdp_personas/queries/enrich/tenant_prospect.sql
+++ dags/primitives/enrich_cdp_personas/queries/enrich/tenant_prospect.sql
@@ -1,13 +1,22 @@
-SELECT
-    rde.id_tenant_prospect AS id_user,
-    u.uuid_person,
-    CAST(NULL AS BOOLEAN) AS is_active,
-    MIN(rde.ts_event) AS ts_first_event,
-    MAX(rde.ts_event) AS ts_last_event,
+WITH base AS (
+    SELECT
+        rde.id_tenant_prospect AS id_user,
+        u.uuid_person,
+        MIN(rde.ts_event) AS ts_first_event,
+        MAX(rde.ts_event) AS ts_last_event
+    FROM
+        datalake_rent_demand_events.rent_demand_events AS rde
+    LEFT JOIN
+        datalake_ebdb_clean.user AS u
+            ON u.id = rde.id_tenant_prospect
+    GROUP BY 1, 2
+)
+SELECT 
+    id_user,
+    uuid_person,
+    IF(DATEDIFF(NOW(), ts_last_event) <= 30, TRUE, FALSE) AS is_active,
+    ts_first_event,
+    ts_last_event,
     NOW() AS ts_load
 FROM
-    datalake_rent_demand_events.rent_demand_events AS rde
-LEFT JOIN
-    datalake_ebdb_clean.user AS u
-        ON u.id = rde.id_tenant_prospect
-GROUP BY 1, 2, 3, 6
+    base
