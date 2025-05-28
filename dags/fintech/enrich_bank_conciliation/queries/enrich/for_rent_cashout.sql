def1e5f19ca83f897fae4bab740143f88846bf36
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2025-05-28 15:46:58 -0300

    Reversed workaround for company_use missing in SAP (#19099)

--- dags/fintech/enrich_bank_conciliation/queries/enrich/for_rent_cashout.sql
+++ dags/fintech/enrich_bank_conciliation/queries/enrich/for_rent_cashout.sql
@@ -1,13 +1,4 @@
-WITH de_para_company_use AS (
-    SELECT DISTINCT
-        UPPER(REPLACE(reference_1, '|', '!')) AS company_use,
-        id_business_entity,
-        id_finance_entity
-    FROM 
-        datalake_accounting_funnel.payment_platforms
-),
-
-francesinha AS (
+WITH francesinha AS (
     SELECT
         UPPER(REPLACE(company_use, '|', '!')) AS company_use,
         our_number,
@@ -33,8 +24,6 @@ francesinha AS (
 
 cap AS (
     SELECT
-        id_business_entity,
-        id_finance_entity,
         dt_paid,
         UPPER(REPLACE(reference_1, '|', '!')) AS company_use,
         payment_status,
@@ -51,24 +40,18 @@ cap AS (
     AND
         (CAST(SPLIT(reference_5, ':')[0] AS INT) IN (1, 2, 7, 10, 13) OR reference_5 IS NULL)
     GROUP BY
-        1, 2, 3, 4, 5, 6, 7, 8
+        1, 2, 3, 4, 5, 6
 ),
 
 sap AS (
-    SELECT DISTINCT
-        l.id_business_entity,
-        l.id_finance_entity,
-        dp.company_use,
-        l.account_number,
-        l.dt_reference AS dt_paid,
-        l.dt_tax,
-        ROUND(SUM(l.debit_credit), 2) AS paid_amount
+    SELECT
+        UPPER(id_external_payment) AS company_use,
+        account_number,
+        dt_reference AS dt_paid,
+        dt_tax,
+        ROUND(SUM(debit_credit), 2) AS paid_amount
     FROM
-        datalake_accounting_funnel.ledger AS l
-    LEFT JOIN 
-        de_para_company_use AS dp
-            ON l.id_business_entity = dp.id_business_entity
-            AND l.id_finance_entity = dp.id_finance_entity
+        datalake_accounting_funnel.ledger
     WHERE
         (
             dt_reference >= DATE('2024-01-01')
@@ -81,7 +64,7 @@ sap AS (
         )
 
     GROUP BY
-        1, 2, 3, 4, 5, 6
+        1, 2, 3, 4
 ),
 
 cap_sap AS (
