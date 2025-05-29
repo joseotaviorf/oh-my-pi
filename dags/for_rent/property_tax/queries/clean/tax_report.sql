d576c1767c439500ac53ebe340522dacb347b4a2
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2025-05-29 09:10:03 -0300

    [AEFR-5918] Migrate property_tax DAG to use CDC (#19029)
    * remove property_tax dag and replace with the test
    
    * delete dag from conflict
    
    * update metadata
    
    * fix declaration

--- dags/for_rent/property_tax/queries/clean/tax_report.sql
+++ dags/for_rent/property_tax/queries/clean/tax_report.sql
@@ -4,15 +4,8 @@ SELECT
     house_external_id AS id_house_ebdb,
     status,
     last_year_amount,
-    created_at AS ts_created,
-    updated_at AS ts_updated,
     year_tax_report,
-    year,
-    month,
-    day
-FROM 
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
     datalake_property_tax_raw.tax_report
-WHERE
-    year = {year}
-    AND month = {month}
-    AND day = {day}
