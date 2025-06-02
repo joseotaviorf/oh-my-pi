48050c0acd8b4c60260ac363c9442681fb3a6984
pedronogueiraquinto <155736231+pedronogueiraquinto@users.noreply.github.com>
2025-06-02 13:51:03 +0100

    Chore adding new exp 1 (#19142)
    * fix: fixing timestamp
    
    * feat: Adding recs impressions incremental table
    
    * chore: changing recs impressions to full load so that its easier to re process
    
    * chore: adding missing filters to minimize data processing
    
    * fix: removing duplicate recset ids that lead to having duplicate variants
    
    * chore: adding new exp

--- dags/primitives/enrich_base_metrics_service/queries/enrich/experiment_config.sql
+++ dags/primitives/enrich_base_metrics_service/queries/enrich/experiment_config.sql
@@ -228,6 +228,27 @@ FROM VALUES
                                                 )
                                 )
         )
+    ),
+    -- New carrousels exp
+    (
+        "ab_beakman_native_house_recommendation_hls_experiment",
+        named_struct(
+            'begin_date', DATE('2025-05-30'),
+            'end_date', NULL,
+            'variants', to_json(
+                named_struct(
+                    '0', 'baseline',
+                    '1', 'treatment'
+                )
+            ),
+            'filters', NULL,
+            'metrics', to_json(
+                                    named_struct(
+                                                    'default_metrics', true,
+                                                    'experiment_metrics', array()
+                                                )
+                                )
+        )
     )
 
 AS experiment_config(experiment_name, config)
