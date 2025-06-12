db52014c828f2d29246cc9b760cfc10709cf2133
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-06-12 12:14:51 -0300

    [CDP] Adding user's last event (#19281)
    * tp update
    
    * personas
    
    * fix bp

--- dags/primitives/enrich_cdp_personas/queries/enrich/buyer_prospect.sql
+++ dags/primitives/enrich_cdp_personas/queries/enrich/buyer_prospect.sql
@@ -1,16 +1,48 @@
+WITH base AS (
+    SELECT
+        sde.id_buyer AS id_user,
+        u.uuid_person,
+        MIN(sde.ts_event) AS ts_first_event,
+        MAX(sde.ts_event) AS ts_last_event
+    FROM
+        datalake_sale_demand_events.sale_demand_events AS sde
+    LEFT JOIN
+        datalake_ebdb_clean.user AS u
+            ON u.id = sde.id_buyer
+    WHERE
+        sde.id_buyer IS NOT NULL
+        AND sk_event_type <> 6 -- SALE_AGREEMENT_SIGNED
+    GROUP BY 1, 2
+),
+latest_event AS (
+    SELECT
+        id_buyer AS id_user,
+        CASE
+            WHEN sk_event_type IN (1, 2, 7) THEN 'VISIT'
+            WHEN sk_event_type IN (3, 4, 8) THEN 'OFFER'
+            WHEN sk_event_type = 5 THEN 'CLOSING'
+            ELSE NULL
+        END AS journey_step
+    FROM
+        datalake_sale_demand_events.sale_demand_events AS sde
+    JOIN
+        base AS b
+            ON b.id_user = sde.id_buyer
+    WHERE
+        b.ts_last_event = sde.ts_event
+        AND sde.sk_event_type <> 6
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY sde.id_buyer ORDER BY sde.ts_event DESC) = 1
+)
 SELECT
-    sde.id_buyer AS id_user,
-    u.uuid_person,
+    b.id_user,
+    b.uuid_person,
+    le.journey_step,
     CAST(NULL AS BOOLEAN) AS is_active,
-    MIN(sde.ts_event) AS ts_first_event,
-    MAX(sde.ts_event) AS ts_last_event,
-    NOW() AS ts_load
+    b.ts_first_event,
+    b.ts_last_event
 FROM
-    datalake_sale_demand_events.sale_demand_events AS sde
-LEFT JOIN
-    datalake_ebdb_clean.user AS u
-        ON u.id = sde.id_buyer
-WHERE
-    sde.id_buyer IS NOT NULL
-    AND sk_event_type <> 6 -- SALE_AGREEMENT_SIGNED
-GROUP BY 1, 2, 3, 6
+    base AS b
+JOIN
+    latest_event AS le
+        ON b.id_user = le.id_user
--- dags/primitives/enrich_cdp_personas/queries/enrich/tenant_prospect.sql
+++ dags/primitives/enrich_cdp_personas/queries/enrich/tenant_prospect.sql
@@ -10,13 +10,37 @@ WITH base AS (
         datalake_ebdb_clean.user AS u
             ON u.id = rde.id_tenant_prospect
     GROUP BY 1, 2
+),
+latest_event AS (
+    SELECT
+        id_tenant_prospect AS id_user,
+        CASE
+            WHEN id_event_type IN (1, 2, 11, 12, 13, 14) THEN 'VISIT'
+            WHEN id_event_type IN (3, 4) THEN 'OFFER'
+            WHEN id_event_type IN (5, 6, 7, 8) THEN 'PROPOSAL'
+            WHEN id_event_type IN (9, 10) THEN 'CONTRACT'
+            ELSE NULL
+        END AS journey_step
+    FROM
+        datalake_rent_demand_events.rent_demand_events AS rde
+    JOIN
+        base AS b
+            ON b.id_user = rde.id_tenant_prospect
+    WHERE
+        b.ts_last_event = rde.ts_event 
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY rde.id_tenant_prospect ORDER BY rde.ts_event DESC) = 1
 )
 SELECT 
-    id_user,
-    uuid_person,
-    IF(DATEDIFF(NOW(), ts_last_event) <= 30, TRUE, FALSE) AS is_active,
-    ts_first_event,
-    ts_last_event,
+    b.id_user,
+    b.uuid_person,
+    le.journey_step,
+    IF(DATEDIFF(NOW(), b.ts_last_event) <= 30, TRUE, FALSE) AS is_active,
+    b.ts_first_event,
+    b.ts_last_event,
     NOW() AS ts_load
 FROM
-    base
+    base AS b
+JOIN
+    latest_event AS le
+        ON le.id_user = b.id_user
