b20f0b675fcbca3921f3f6eeefbd7c44e706e642
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-06-12 13:40:49 -0300

    Add new klefki table for cdc (#19288)
    * Add new klefki table for cdc
    
    * Update schema name

---
+++ dags/support_and_service/klefki_test/queries/clean/key_delivery_location.sql
@@ -0,0 +1,19 @@
+SELECT
+	id,
+	contract_id as id_contract,
+	property_id as id_property,
+	user_id as id_user,
+	location,
+	responsible_person_name,
+	responsible_person_phone,
+	created_at as ts_created,
+	updated_at as ts_updated,
+	year,
+	month,
+	day
+FROM
+	datalake_klefki_test_raw.key_delivery_location
+WHERE
+	year = {year}
+	AND month = {month}
+	AND day = {day}
