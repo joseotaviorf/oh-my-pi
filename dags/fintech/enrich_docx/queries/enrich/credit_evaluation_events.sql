6b336701d721f2f99fb072397ca8a31b9ef592c6
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-06-16 17:30:08 -0300

    [DFCA-215] Update: credit_evaluation_events pipeline (#19316)
    * Update: credit_evaluation_events pipeline
    
    * Update: dependencies
    
    * Update: get_credit_evaluation_source cte
    
    * Update sql
    
    * Update events
    
    * remove %sql
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/enrich_docx/queries/enrich/credit_evaluation_events.sql
+++ dags/fintech/enrich_docx/queries/enrich/credit_evaluation_events.sql
@@ -1,7 +1,89 @@
-WITH get_credit_evaluation AS (
-  SELECT
-    DISTINCT ce.id AS id_credit_evaluation,
+WITH get_credit_evaluation_source AS (
+SELECT
+   MIN(DATE_TRUNC('day', ce.ts_created)) AS day,
+   ce.id,
+   CASE
+     WHEN
+       MIN(cep.ts_created) IS NULL
+       OR MIN(ce.ts_updated) < MIN(cep.ts_created)
+     THEN
+       'PRE_OFFER_PASSPORT_FLOW'
+     ELSE 'POS_OFFER_PASSPORT_FLOW'
+   END AS credit_evaluation_source
+ FROM
+   datalake_docx_clean.credit_evaluation ce
+     LEFT JOIN datalake_docx_clean.credit_evaluation_aud cea
+       ON cea.scope = 'HOUSE'
+       AND cea.id_group = ce.id_group
+       AND cea.id_city = ce.id_city
+     LEFT JOIN datalake_docx_clean.credit_evaluation cep
+       ON cep.id = cea.id_credit_evaluation
+ WHERE
+   ce.scope = 'CITY'
+ GROUP BY
+   ce.id
+),
+get_credit_passport_events AS (
+  SELECT DISTINCT
+    NULL AS id_proposal,
+    ce.id AS id_credit_evaluation,
+    ce.id_group,
+    ce.id_city,
+    NULL AS id_house,
+    ce.id_user,
+    ce.scope,
+    ce.result AS credit_result,
+    ce.reason AS credit_result_reason,
+    ce.status AS credit_evaluation_status,
+    ce.limit_value AS user_requested_value,
+    ce.pre_approved_limit AS user_pre_approved_limit,
+    ces.credit_evaluation_source,
+    NULL AS is_value_within_pre_approved_limit,
+    NULL AS is_passport_missing,
+    TRUE AS is_credit_passport,
+    CASE
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status = 'PRE_REJECTED' THEN 'EVALUATION_NEGATIVE'
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status IN ('PRE_APPROVED', 'PRE_APPROVED_WITH_GUARANTEE') THEN 'EVALUATION_POSITIVE'
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status = 'BYPASSED' THEN 'BYPASSED'
+    END AS event_name,
+    CASE
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status = 'PRE_REJECTED' THEN ms.ts_created
+    END AS ts_evaluation_negative,
+    CASE
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status IN ('PRE_APPROVED', 'PRE_APPROVED_WITH_GUARANTEE') THEN ms.ts_created
+    END AS ts_evaluation_positive,
+    CASE
+      WHEN ms.input_source_type = 'ANALYSIS_REQUEST'
+      AND ms.status = 'BYPASSED' THEN ms.ts_created
+    END AS ts_bypassed,
+    ce.ts_created AS ts_credit_evaluation_created,
+    NULL AS ts_credit_analysis_approved,
+    NULL AS ts_docs_analysis,
+    NULL AS ts_docs_resend,
+    NULL AS ts_stand_by,
+    ce.ts_expires AS ts_expired
+  FROM
+    datalake_docx_clean.credit_evaluation AS ce
+  LEFT JOIN
+    datalake_docx_clean.machine_state AS ms
+      ON ms.id_credit_evaluation = ce.id
+  LEFT JOIN
+    get_credit_evaluation_source AS ces
+      ON ces.id = ce.id
+  WHERE
+    ms.type = 'TENANT_GROUP_CREDIT' AND
+    ce.scope = 'CITY' AND
+    ms.status IN ('PRE_APPROVED', 'PRE_APPROVED_WITH_GUARANTEE', 'PRE_REJECTED', 'BYPASSED')
+),
+get_credit_evaluation_proposal_events AS (
+  SELECT DISTINCT
     ce.id_proposal,
+    ce.id AS id_credit_evaluation,
     ce.id_group,
     ce.id_city,
     ce.id_house,
@@ -10,328 +92,155 @@ WITH get_credit_evaluation AS (
     ce.result AS credit_result,
     ce.reason AS credit_result_reason,
     ce.status AS credit_evaluation_status,
-    'POS_OFFER_PASSPORT_FLOW' AS credit_evaluation_source,
-    ga.status AS group_authorization_status,
+    ce.limit_value AS user_requested_value,
+    ce.pre_approved_limit AS user_pre_approved_limit,
+    'PROPOSAL_FLOW' AS credit_evaluation_source,
     IF(
-      get_json_object(ga.credit_result, '$.reason') = 'VALUE_WITHIN_PRE_APPROVED_LIMIT',
-      TRUE,
-      FALSE
+      get_json_object(ce.early_result, '$[0].reason') = 'VALUE_WITHIN_PRE_APPROVED_LIMIT', TRUE, FALSE
     ) AS is_value_within_pre_approved_limit,
-    ce.ts_created AS ts_credit_evaluation_created
-  FROM
-    datalake_docx_clean.credit_evaluation AS ce
-    LEFT JOIN datalake_docx_clean.group_authorization AS ga ON ce.id_group = ga.id_group
-    AND ce.id_house = ga.id_house
-  WHERE
-    ga.requestor_type = 'TRANSACT_FLOW'
-  UNION ALL
-  SELECT
-    id AS id_credit_evaluation,
-    id_proposal,
-    id_group,
-    id_city,
-    id_house,
-    id_user,
-    scope,
-    result AS credit_result,
-    reason AS credit_result_reason,
-    status AS credit_evaluation_status,
-    IF(scope = 'CITY', 'PRE_OFFER_PASSPORT_FLOW', 'NORMAL_FLOW') AS credit_evaluation_source,
-    NULL AS group_authorization_status,
-    NULL AS is_value_within_pre_approved_limit,
-    ts_created AS ts_credit_evaluation_created
-  from
-    datalake_docx_clean.credit_evaluation
-  WHERE
-    scope <> 'HOUSE'
-    OR scope IS NULL
-),
-get_credit_evaluation_events AS (
-  SELECT
-    IF(
-      ms.input_source_type = 'CREDIT_EVALUATION',
-      ms.id_input_source,
-      ms.id_credit_evaluation
-    ) AS id_credit_evaluation,
-    ms.id_context_external AS id_proposal,
-    ms.id_group,
-    ms.id_city,
+    False AS is_credit_passport,
+    IF(ce.status = 'ON_HOLD' AND ce.early_result IS NULL, TRUE, FALSE) AS is_passport_missing,
     CASE
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'PERSON_UUID'
-      AND ms.status = 'PRE_EVALUATION_STARTED' THEN 'PRE_EVALUATION_STARTED'
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status = 'PRE_REJECTED' THEN 'PRE_REJECTED'
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status IN ('PRE_APPROVED', 'PRE_APPROVED_WITH_GUARANTEE') THEN 'PRE_APPROVED'
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status = 'BYPASSED' THEN 'BYPASSED'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_EVALUATION', 'OFFER', 'TENANT')
-      AND ms.status IN ('EVALUATION_STARTED', 'NOT_SENT') THEN 'EVALUATION_STARTED'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type = 'CREDIT_ANALYSIS'
-      AND ms.status IN (
-        'EVALUATION_POSITIVE',
-        'EVALUATION_POSITIVE_WITH_GUARANTEE'
-      ) THEN 'EVALUATION_POSITIVE'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
-      AND ms.status = 'APPROVED' THEN 'CREDIT_ANALYSIS_APPROVED'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
-      AND ms.status = 'EVALUATION_NEGATIVE' THEN 'EVALUATION_NEGATIVE'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('TENANT', 'ANALYST')
-      AND ms.status = 'DOCS_ANALYSIS' THEN 'DOCS_ANALYSIS'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('ANALYST', 'EMAIL_VALIDATION_SERVICE')
-      AND ms.status = 'DOCS_RESEND' THEN 'DOCS_RESEND'
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type = 'ANALYST'
-      AND ms.status = 'STAND_BY' THEN 'STAND_BY'
+      WHEN
+        ms.input_source_type = 'CREDIT_ANALYSIS'
+        AND ms.status IN ('EVALUATION_POSITIVE', 'EVALUATION_POSITIVE_WITH_GUARANTEE')
+      THEN
+        'EVALUATION_POSITIVE'
+      WHEN
+        ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
+        AND ms.status = 'APPROVED'
+      THEN
+        'CREDIT_ANALYSIS_APPROVED'
+      WHEN
+        ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
+        AND ms.status = 'EVALUATION_NEGATIVE'
+      THEN
+        'EVALUATION_NEGATIVE'
+      WHEN
+        ms.input_source_type IN ('TENANT', 'ANALYST')
+        AND ms.status = 'DOCS_ANALYSIS'
+      THEN
+        'DOCS_ANALYSIS'
+      WHEN
+        ms.input_source_type IN ('ANALYST', 'EMAIL_VALIDATION_SERVICE')
+        AND ms.status = 'DOCS_RESEND'
+      THEN
+        'DOCS_RESEND'
+      WHEN
+        ms.input_source_type = 'ANALYST'
+        AND ms.status = 'STAND_BY'
+      THEN
+        'STAND_BY'
+      WHEN
+        get_json_object(ce.early_result, '$[0].reason') IN (
+          'CLEAR_NO',
+          'BLOCKLIST',
+          'PENDING_INVOICES',
+          'SCOPE_NOT_EVALUATED'
+        )
+      THEN
+        'EVALUATION_NEGATIVE'
+      WHEN
+        get_json_object(ce.early_result, '$[0].reason') IN ('VALUE_EXCEEDS_CREDIT_LIMIT', 'VALUE_WITHIN_PRE_APPROVED_LIMIT')
+      THEN
+        'SUFFICIENT_CREDIT_LIMIT'
     END AS event_name,
+    NULL AS ts_bypassed,
+    ce.ts_created AS ts_credit_evaluation_created,
     CASE
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'PERSON_UUID'
-      AND ms.status = 'PRE_EVALUATION_STARTED' THEN ms.ts_created
-    END AS ts_pre_evaluation_started,
-    CASE
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status = 'PRE_REJECTED' THEN ms.ts_created
-    END AS ts_pre_rejected,
-    CASE
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status IN ('PRE_APPROVED', 'PRE_APPROVED_WITH_GUARANTEE') THEN ms.ts_created
-    END AS ts_pre_approved,
-    CASE
-      WHEN ms.type = 'TENANT_GROUP_CREDIT'
-      AND ms.input_source_type = 'ANALYSIS_REQUEST'
-      AND ms.status = 'BYPASSED' THEN ms.ts_created
-    END AS ts_bypassed,
-    CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_EVALUATION', 'OFFER', 'TENANT')
-      AND ms.status IN ('EVALUATION_STARTED', 'NOT_SENT') THEN ms.ts_created
-    END AS ts_evaluation_started,
-    CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type = 'CREDIT_ANALYSIS'
-      AND ms.status IN (
-        'EVALUATION_POSITIVE',
-        'EVALUATION_POSITIVE_WITH_GUARANTEE'
-      ) THEN ms.ts_created
+      WHEN
+        ms.input_source_type = 'CREDIT_ANALYSIS'
+        AND ms.status IN ('EVALUATION_POSITIVE', 'EVALUATION_POSITIVE_WITH_GUARANTEE')
+      THEN
+        ms.ts_created
     END AS ts_evaluation_positive,
     CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
-      AND ms.status = 'APPROVED' THEN ms.ts_created
+      WHEN
+        ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
+        AND ms.status = 'APPROVED'
+      THEN
+        ms.ts_created
     END AS ts_credit_analysis_approved,
     CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
-      AND ms.status = 'EVALUATION_NEGATIVE' THEN ms.ts_created
-    END AS ts_evaluation_negative,
-    CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('OFFER', 'TENANT')
-      AND ms.status = 'NOT_SENT' THEN ms.ts_created
-    END AS ts_state_machine_start,
-    CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('TENANT', 'ANALYST')
-      AND ms.status = 'DOCS_ANALYSIS' THEN ms.ts_created
+      WHEN
+        ms.input_source_type IN ('TENANT', 'ANALYST')
+        AND ms.status = 'DOCS_ANALYSIS'
+      THEN
+        ms.ts_created
     END AS ts_docs_analysis,
     CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type IN ('ANALYST', 'EMAIL_VALIDATION_SERVICE')
-      AND ms.status = 'DOCS_RESEND' THEN ms.ts_created
+      WHEN
+        ms.input_source_type IN ('ANALYST', 'EMAIL_VALIDATION_SERVICE')
+        AND ms.status = 'DOCS_RESEND'
+      THEN
+        ms.ts_created
     END AS ts_docs_resend,
     CASE
-      WHEN ms.type = 'TENANT'
-      AND ms.input_source_type = 'ANALYST'
-      AND ms.status = 'STAND_BY' THEN ms.ts_created
+      WHEN
+        ms.input_source_type = 'ANALYST'
+        AND ms.status = 'STAND_BY'
+      THEN
+        ms.ts_created
     END AS ts_stand_by,
-    IF(
-      ms.input_source_type IN ('CREDIT_EVALUATION', 'OFFER', 'TENANT')
-      AND ms.status = 'NOT_SENT',
-      TRUE,
-      FALSE
-    ) AS is_not_sent,
-    ms.ts_created
+    CASE
+      WHEN
+        ms.input_source_type IN ('CREDIT_ANALYSIS', 'ANALYST')
+        AND ms.status = 'EVALUATION_NEGATIVE'
+      THEN
+        ms.ts_created
+    END AS ts_evaluation_negative,
+    ce.ts_expires AS ts_expired
   FROM
-    datalake_docx_clean.machine_state AS ms
+    datalake_docx_clean.credit_evaluation AS ce
+      LEFT JOIN datalake_docx_clean.machine_state AS ms
+        ON ms.id_context_external = ce.id_proposal
   WHERE
-    ms.input_source_type != 'RECOVERY'
-),
-handle_evaluation_negative AS (
-  SELECT
-    ce.id_credit_evaluation,
-    ce.id_proposal,
-    ce.id_group,
-    ce.id_city,
-    ce.id_house,
-    ce.id_user,
-    ce.scope,
-    ce.credit_evaluation_status,
-    ce.credit_evaluation_source,
-    ce.group_authorization_status,
-    IF(
-      cev.is_not_sent = TRUE
-      AND ce.is_value_within_pre_approved_limit = FALSE
-      AND ce.credit_evaluation_status IN ('ON_HOLD', 'FAILED', 'CANCELLED'),
+    ms.type = 'TENANT'
+    AND ms.status IN (
       'EVALUATION_NEGATIVE',
-      cev.event_name
-    ) AS event_name,
-    ce.is_value_within_pre_approved_limit,
-    cev.ts_pre_evaluation_started,
-    cev.ts_pre_rejected,
-    cev.ts_pre_approved,
-    cev.ts_bypassed,
-    cev.ts_state_machine_start,
-    cev.ts_evaluation_started,
-    cev.ts_evaluation_positive,
-    IF(
-      cev.is_not_sent = TRUE,
-      cev.ts_state_machine_start,
-      cev.ts_evaluation_negative
-    ) AS ts_evaluation_negative,
-    cev.ts_credit_analysis_approved,
-    cev.ts_docs_analysis,
-    cev.ts_docs_resend,
-    cev.ts_stand_by,
-    ce.ts_credit_evaluation_created
-  FROM
-    get_credit_evaluation AS ce
-    LEFT JOIN get_credit_evaluation_events AS cev ON cev.id_credit_evaluation = ce.id_credit_evaluation
-    OR cev.id_proposal = ce.id_proposal
-  WHERE
-    cev.event_name IS NOT NULL
-    AND cev.is_not_sent = TRUE
+      'APPROVED',
+      'EVALUATION_POSITIVE',
+      'EVALUATION_POSITIVE_WITH_GUARANTEE',
+      'DOCS_ANALYSIS',
+      'DOCS_RESEND',
+      'STAND_BY',
+      'NOT_SENT'
+    )
+    AND (ce.scope = 'HOUSE' OR ce.scope IS NULL)
+    AND ce.status <> 'FAILED'
 ),
-join_credit_evaluation_events AS (
-  SELECT
-    ce.id_credit_evaluation,
-    ce.id_proposal,
-    ce.id_group,
-    ce.id_city,
-    ce.id_house,
-    ce.id_user,
-    ce.scope,
-    ce.credit_evaluation_status,
-    ce.credit_evaluation_source,
-    ce.group_authorization_status,
-    cev.event_name,
-    ce.is_value_within_pre_approved_limit,
-    cev.ts_pre_evaluation_started,
-    cev.ts_pre_rejected,
-    cev.ts_pre_approved,
-    cev.ts_bypassed,
-    cev.ts_state_machine_start,
-    cev.ts_evaluation_started,
-    cev.ts_evaluation_positive,
-    cev.ts_evaluation_negative,
-    cev.ts_credit_analysis_approved,
-    cev.ts_docs_analysis,
-    cev.ts_docs_resend,
-    cev.ts_stand_by,
-    ce.ts_credit_evaluation_created
-  FROM
-    get_credit_evaluation AS ce
-    LEFT JOIN get_credit_evaluation_events AS cev ON cev.id_credit_evaluation = ce.id_credit_evaluation
-    OR cev.id_proposal = ce.id_proposal
-  WHERE
-    cev.event_name IS NOT NULL
-),
-union_all AS (
-  SELECT
-    id_credit_evaluation,
-    id_proposal,
-    id_group,
-    id_city,
-    id_house,
-    id_user,
-    scope,
-    credit_evaluation_status,
-    credit_evaluation_source,
-    group_authorization_status,
-    event_name,
-    is_value_within_pre_approved_limit,
-    ts_pre_evaluation_started,
-    ts_pre_rejected,
-    ts_pre_approved,
-    ts_bypassed,
-    ts_state_machine_start,
-    ts_evaluation_started,
-    ts_evaluation_positive,
-    ts_evaluation_negative,
-    ts_credit_analysis_approved,
-    ts_docs_analysis,
-    ts_docs_resend,
-    ts_stand_by,
-    ts_credit_evaluation_created
-  FROM
-    join_credit_evaluation_events
-  UNION ALL
-  SELECT
-    id_credit_evaluation,
-    id_proposal,
-    id_group,
-    id_city,
-    id_house,
-    id_user,
-    scope,
-    credit_evaluation_status,
-    credit_evaluation_source,
-    group_authorization_status,
-    event_name,
-    is_value_within_pre_approved_limit,
-    ts_pre_evaluation_started,
-    ts_pre_rejected,
-    ts_pre_approved,
-    ts_bypassed,
-    ts_state_machine_start,
-    ts_evaluation_started,
-    ts_evaluation_positive,
-    ts_evaluation_negative,
-    ts_credit_analysis_approved,
-    ts_docs_analysis,
-    ts_docs_resend,
-    ts_stand_by,
-    ts_credit_evaluation_created
-  FROM
-    handle_evaluation_negative
+union_credit_evaluations AS (
+SELECT * FROM get_credit_passport_events
+UNION ALL
+SELECT * FROM get_credit_evaluation_proposal_events
 )
 SELECT
-    id_credit_evaluation,
-    id_proposal,
-    id_group,
-    id_city,
-    id_house,
-    id_user,
-    scope,
-    credit_evaluation_status,
-    credit_evaluation_source,
-    group_authorization_status,
-    event_name,
-    is_value_within_pre_approved_limit,
-    ts_pre_evaluation_started,
-    ts_pre_rejected,
-    ts_pre_approved,
-    ts_bypassed,
-    ts_state_machine_start,
-    ts_evaluation_started,
-    ts_evaluation_positive,
-    ts_evaluation_negative,
-    ts_credit_analysis_approved,
-    ts_docs_analysis,
-    ts_docs_resend,
-    ts_stand_by,
-    ts_credit_evaluation_created
+  id_proposal,
+  id_credit_evaluation,
+  id_group,
+  id_city,
+  id_house,
+  id_user,
+  scope,
+  credit_result,
+  credit_result_reason,
+  credit_evaluation_status,
+  credit_evaluation_source,
+  user_requested_value,
+  user_pre_approved_limit,
+  event_name,
+  is_value_within_pre_approved_limit,
+  is_passport_missing,
+  is_credit_passport,
+  ts_credit_evaluation_created,
+  ts_bypassed,
+  ts_evaluation_positive,
+  ts_evaluation_negative,
+  ts_credit_analysis_approved,
+  ts_docs_analysis,
+  ts_docs_resend,
+  ts_stand_by,
+  ts_expired
 FROM
-  union_all
+  union_credit_evaluations
+WHERE event_name IS NOT NULL
