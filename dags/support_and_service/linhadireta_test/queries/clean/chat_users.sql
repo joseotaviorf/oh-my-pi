acae5ed2e8a4c400fe6eaa0b7ddd1e67d3d88f7e
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2025-06-16 09:59:15 -0300

    Replace old ingestion format dag with cdc format (#19292)

--- dags/support_and_service/linhadireta_test/queries/clean/chat_users.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id,
-    chat_id as id_chat,
-    user_id as id_user
-FROM
-    datalake_linhadireta_test_raw.chat_users
--- dags/support_and_service/linhadireta_test/queries/clean/user_chats.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id,
-    user_id as id_user,
-    chat_id as id_chat
-FROM
-    datalake_linhadireta_test_raw.user_chats
