16ed4724c9739e0247e1da34a6116d6c16f55131
William Angelo <39243586+williamangelo@users.noreply.github.com>
2025-06-24 10:53:24 -0300

    feat: saruman to cdc (#19399)

--- dags/support_and_service/saruman/queries/clean/session.sql
+++ dags/support_and_service/saruman/queries/clean/session.sql
@@ -12,5 +12,3 @@ SELECT
     day
 FROM
     datalake_saruman_raw.session
-WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
--- dags/support_and_service/saruman/queries/clean/session_history.sql
+++ dags/support_and_service/saruman/queries/clean/session_history.sql
@@ -9,5 +9,3 @@ SELECT
     day
 FROM
     datalake_saruman_raw.session_history
-WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
