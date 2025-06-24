e6160fc85d5d0ea580a24bf792b073406c78dfcb
Rodrigo Lima Guedes <69510567+guedes2411@users.noreply.github.com>
2025-06-24 18:27:10 -0300

    [DEFF] Added billing_amount and dt_billing in for_rent_cashin_collections (#19409)
    * Added billing_amount and dt_billing in for_rent_cashin_collections
    
    * added billing_status column
    
    * added limit dates to trato_feito and retsuko ctes
    
    * update lineage

--- dags/fintech/enrich_bank_conciliation/queries/enrich/for_rent_cashin_collections.sql
+++ dags/fintech/enrich_bank_conciliation/queries/enrich/for_rent_cashin_collections.sql
@@ -133,6 +133,34 @@ trato_feito AS (
             ON b.id_external = aci.id_external
 ),
 
+seu_barriga_sap AS (
+    SELECT
+        id_external AS id_invoice,
+        id_original_invoice_external,
+        UPPER(payment_company_use_number) AS company_use,
+        payment_status,
+        status,
+        paid_via,
+        reason,
+        paid_amount AS amount,
+        dd.next_brz_fintech_business_day AS dt_paid
+    FROM
+        datalake_retsuko.invoice
+    LEFT JOIN
+        dw_public.dim_date dd
+            ON invoice.ts_paid = dd.date
+    WHERE
+        payment_company_use_number IS NOT NULL
+        AND TRIM(payment_company_use_number) != ''
+        AND lower(paid_via) IN ('cnab', 'checkout-boleto', 'cyber-boleto')
+        AND due_amount <= 0
+        AND payment_status != 'canceled'
+        AND status != 'canceled'
+        AND country_code = 'BR'
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY id_invoice, payment_company_use_number ORDER BY ts_created DESC) = 1
+),
+
 df_all AS (
     SELECT DISTINCT
         our_number
@@ -154,9 +182,18 @@ df AS (
     SELECT DISTINCT
         cs.our_number AS id_our_number,
         f.amount AS bank_amount,
+        COALESCE(sbs.amount, tf.amount) AS billing_amount,
         vc.amount AS checkout_amount,
         s.amount AS sap_amount,
         IF(f.our_number IS NULL, 'not recorded', 'ok') AS status_bank,
+        CASE
+            WHEN f.amount = COALESCE(tf.amount, sbs.amount) AND f.dt_paid = DATE(COALESCE(sbs.dt_paid, tf.dt_paid)) THEN 'ok'
+            WHEN f.amount != COALESCE(tf.amount, sbs.amount) AND f.dt_paid != DATE(COALESCE(sbs.dt_paid, tf.dt_paid)) THEN 'recorded on the wrong date and value'
+            WHEN f.amount != COALESCE(tf.amount, sbs.amount) AND f.dt_paid = DATE(COALESCE(sbs.dt_paid, tf.dt_paid)) THEN 'recorded on the wrong value'
+            WHEN f.amount = COALESCE(tf.amount, sbs.amount) AND f.dt_paid != DATE(COALESCE(sbs.dt_paid, tf.dt_paid)) THEN 'recorded on the wrong date'
+            WHEN vc.our_number IS NULL THEN 'not recorded'
+            ELSE 'not ok'
+        END AS status_billing,
         CASE
             WHEN f.amount = vc.amount AND f.dt_paid = DATE(vc.dt_paid) THEN 'ok'
             WHEN f.amount != vc.amount AND f.dt_paid != DATE(vc.dt_paid) THEN 'recorded on the wrong date and value'
@@ -174,6 +211,7 @@ df AS (
             ELSE 'not ok'
         END AS status_sap,
         f.dt_paid AS dt_bank_paid,
+        COALESCE(sbs.dt_paid, tf.dt_paid) AS dt_billing_paid,
         vc.dt_paid AS dt_checkout_paid,
         s.dt_paid AS dt_sap_paid
     FROM
@@ -187,6 +225,9 @@ df AS (
     LEFT JOIN 
         trato_feito tf
             ON tf.our_number = cs.our_number
+    LEFT JOIN 
+        seu_barriga_sap sbs
+            ON (sbs.company_use = cs.our_number) OR (sbs.id_invoice = tf.id_invoice) 
     LEFT JOIN
         sap s
             ON (cs.our_number = s.our_number) OR (tf.id_invoice = s.id_finance_entity)
@@ -196,20 +237,25 @@ df AS (
         (
             DATE(f.dt_paid) >= '2024-01-01' OR
             DATE(vc.dt_paid) >= '2024-01-01' OR
-            DATE(s.dt_paid) >= '2024-01-01'
+            DATE(s.dt_paid) >= '2024-01-01' OR 
+            DATE(sbs.dt_paid) >= '2024-01-01' OR
+            DATE(tf.dt_paid) >= '2024-01-01'
     )
 )
 
 SELECT
     id_our_number,
     bank_amount,
+    billing_amount,
     checkout_amount,
     sap_amount,
     status_bank,
+    status_billing,
     status_checkout,
     status_sap,
-    IF(status_bank = 'ok' AND status_sap = 'ok', TRUE, FALSE) AS is_reconciled,
+    IF(status_bank = 'ok' AND status_sap = 'ok' AND status_checkout = 'ok' AND status_billing = 'ok', TRUE, FALSE) AS is_reconciled,
     dt_bank_paid,
+    dt_billing_paid,
     dt_checkout_paid,
     dt_sap_paid
 FROM
