8a947b36f141839a52c5190e197d4bdc0552fa92
Marcelo Farinaro <marcelo.farinaro@quintoandar.com.br>
2025-06-30 10:00:18 -0300

    Fix-timeout-enrch-access (#19320)
    * adjusting the query
    
    * removing comments
    
    * Update dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
    
    * Update dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
    
    * Update dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>

--- dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
+++ dags/tech_platform/enrich_access_logs/queries/enrich/access_logs.sql
@@ -1,46 +1,96 @@
-WITH istio AS (
-    SELECT *
+WITH opa_filtered AS (
+    SELECT
+        id_decision,
+        id_trace,
+        ts_event,
+        id_request,
+        request_real_ip,
+        request_source_principal,
+        request_destination_principal,
+        request_parameterized_path,
+        request_required_roles,
+        request_method,
+        request_path,
+        result_http_allowed,
+        result_http_status,
+        principal_user_email,
+        id_main_principal_user,
+        uuid_person_principal_user,
+        principal_user_idp,
+        principal_user_issuer,
+        id_principal_user_impersonated_by,
+        principal_service,
+        principal_user_provided_roles,
+        principal_service_provided_roles,
+        app,
+        year,
+        month,
+        day,
+        hour
+    FROM
+        datalake_access_logs_clean.opa
+    WHERE
+        MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
+),
+istio_filtered AS (
+    SELECT
+        id_request,
+        ts_event,
+        request_traceparent,
+        request_x_forwarded_for,
+        request_user_agent,
+        request_duration_ms,
+        response_flags,
+        response_code,
+        app,
+        year,
+        month,
+        day,
+        hour
     FROM
         datalake_access_logs_clean.istio
     WHERE
-        MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
+         MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
+    QUALIFY
+        ROW_NUMBER() OVER (PARTITION BY id_request, app ORDER BY ts_event DESC) = 1 
 )
-SELECT 
+SELECT
     opa.id_decision,
     opa.id_trace,
     opa.ts_event as ts_decision,
-    istio.ts_event as ts_mesh_request, 
-    istio.request_traceparent, 
+    istio.ts_event as ts_mesh_request,
+    istio.request_traceparent,
     istio.request_x_forwarded_for,
     opa.request_real_ip,
-    istio.request_user_agent, 
-    istio.request_duration_ms, 
-    istio.response_flags, 
+    istio.request_user_agent,
+    istio.request_duration_ms,
+    istio.response_flags,
     istio.response_code,
-    opa.request_source_principal, 
-    opa.request_destination_principal, 
-    opa.request_parameterized_path, 
+    opa.request_source_principal,
+    opa.request_destination_principal,
+    opa.request_parameterized_path,
     opa.request_required_roles,
     opa.request_method,
     opa.request_path,
-    opa.result_http_allowed, 
-    result_http_status, 
-    opa.principal_user_email, 
+    opa.result_http_allowed,
+    opa.result_http_status,
+    opa.principal_user_email,
     opa.id_main_principal_user,
     opa.uuid_person_principal_user,
     opa.principal_user_idp,
-    opa.principal_user_issuer, 
-    opa.id_principal_user_impersonated_by,  
+    opa.principal_user_issuer,
+    opa.id_principal_user_impersonated_by,
     opa.principal_service,
-    opa.principal_user_provided_roles, 
-    opa.principal_service_provided_roles, 
+    opa.principal_user_provided_roles,
+    opa.principal_service_provided_roles,
     opa.app,
-    opa.year, 
-    opa.month, 
-    opa.day, 
+    opa.year,
+    opa.month,
+    opa.day,
     opa.hour
-FROM datalake_access_logs_clean.opa as opa
-LEFT JOIN istio on opa.id_request = istio.id_request AND opa.app = istio.app
-WHERE
-    MAKE_DATE(opa.year, opa.month, opa.day) BETWEEN '{load_start_date}' AND '{load_end_date}'
-QUALIFY ROW_NUMBER() OVER (PARTITION BY opa.id_decision ORDER BY istio.ts_event DESC) = 1
+FROM
+    opa_filtered as opa
+LEFT JOIN
+    istio_filtered as istio
+    ON opa.id_request = istio.id_request
+    AND opa.app = istio.app
