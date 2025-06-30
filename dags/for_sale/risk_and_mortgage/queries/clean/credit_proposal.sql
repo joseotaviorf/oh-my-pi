c11f10108c9c15e28342f8bcb8e627c045a310ea
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2025-06-30 10:26:12 -0300

    [DFORSALE-8962] Swap between original and test risk_and_mortgage DAGs (#19460)
    * Update risk_and_mortgage DAG
    
    * Remove risk_and_mortgage_test DAG

--- dags/for_sale/risk_and_mortgage/queries/clean/credit_proposal.sql
+++ dags/for_sale/risk_and_mortgage/queries/clean/credit_proposal.sql
@@ -1,19 +1,17 @@
 SELECT
-  id,
-  credit_proposal_id AS id_credit_proposal,
-  credit_letter_document_id AS id_credit_letter_document,
-  offer_pre_analysis_id AS id_offer_pre_analysis,
-  bank,
-  credit_letter_file_name,
-  status,
-  accepted AS is_accepted,
-  credit_start_date AS dt_credit_started,
-  created_at AS ts_created,
-  updated_at AS ts_updated,
-  year,
-  month,
-  day
+    id,
+    credit_proposal_id AS id_credit_proposal,
+    credit_letter_document_id AS id_credit_letter_document,
+    offer_pre_analysis_id AS id_offer_pre_analysis,
+    bank,
+    credit_letter_file_name,
+    status,
+    accepted AS is_accepted,
+    credit_start_date AS dt_credit_started,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
 FROM
-  datalake_risk_and_mortgage_raw.credit_proposal
-WHERE
-  MAKE_DATE(year,month,day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    datalake_risk_and_mortgage_raw.credit_proposal
