280f1784bb53953a80f2bf0a7be2c1f12c3327c0
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-07-01 10:49:03 -0300

    turn off visit_cancellation_details from dag (#19470)
    * turn off visit_cancellation_details from dag
    
    * dependencies update

--- dags/for_sale/enrich_visit/queries/enrich/visit_cancellation_unified.sql
+++ dags/for_sale/enrich_visit/queries/enrich/visit_cancellation_unified.sql
@@ -11,6 +11,7 @@ new_model AS (
     datalake_ebdb_clean.visit_status_log
   WHERE
     event_type IN ("VISIT_REQUEST_CANCELED", "VISIT_CANCELED")
+    AND DATE(ts_created) >= '2025-01-01'
 ),
 old_model AS (
   SELECT
@@ -22,16 +23,23 @@ old_model AS (
     ts_updated
   FROM
     datalake_ebdb_clean.visit_cancellation_details
+  WHERE
+    DATE(ts_created) < '2025-01-01'
 )
 SELECT
-  COALESCE(new_model.id_visit, old_model.id_visit) AS id_visit,
-  COALESCE(new_model.channel, old_model.channel) AS channel,
-  COALESCE(new_model.on_behalf_of, old_model.on_behalf_of) AS on_behalf_of,
-  COALESCE(new_model.reason, old_model.reason) AS reason,
-  COALESCE(new_model.ts_created, old_model.ts_created) AS ts_created,
-  COALESCE(new_model.ts_updated, old_model.ts_updated) AS ts_updated
-FROM
-  new_model
-FULL OUTER JOIN
-  old_model
-    ON new_model.id_visit = old_model.id_visit
+  id_visit,
+  channel,
+  on_behalf_of,
+  reason,
+  ts_created,
+  ts_updated
+FROM new_model
+UNION ALL
+SELECT
+  id_visit,
+  channel,
+  on_behalf_of,
+  reason,
+  ts_created,
+  ts_updated
+FROM old_model
