9baa84842ef10f13a869f1ab5d4aaf35d3336a4e
Rafael Guedes <104824314+rjguedes8@users.noreply.github.com>
2025-07-01 09:40:00 +0100

    fix: cast as date (#19478)

--- dags/conversational_xp/enrich_concierge_ranking_metrics/queries/enrich/ranking_metrics.sql
+++ dags/conversational_xp/enrich_concierge_ranking_metrics/queries/enrich/ranking_metrics.sql
@@ -28,7 +28,7 @@ base as (
   FROM datalake_house_listing_search_clean.recommendation_user_use_case ruuc
   LEFT JOIN datalake_house_listing_search_clean.recommendation_use_case ruc
     on ruuc.id=ruc.id_user_use_case
-    and ruc.ts_created = DATE_SUB('{start_date}', 4)
+    and CAST(ruc.ts_created AS DATE) = DATE_SUB('{start_date}', 4)
   LEFT JOIN clicked_properties cp 
     on cp.house_id = ruc.id_listing
     and cp.recommendation_id=ruuc.id_recommendation
@@ -38,7 +38,7 @@ base as (
     ruuc.id_recommendation is not null
     AND ruc.id_listing is not null
     AND ruuc.use_case = 'FEED_CONCIERGE'
-    AND ruuc.ts_created = DATE_SUB('{start_date}', 4)
+    AND CAST(ruuc.ts_created AS DATE) = DATE_SUB('{start_date}', 4)
 ),
 
 metrics AS (
