96ba3505c45d1dc3935b546a633b7aba5c250998
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-07-01 15:59:24 -0300

    Add: REPLACE to remove '%' and fix CAST (#19498)
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/gsheets_fintech/queries/clean/expected_evers.sql
+++ dags/fintech/gsheets_fintech/queries/clean/expected_evers.sql
@@ -2,12 +2,12 @@ SELECT
     Chave AS model_key,
     Modelo AS model_name,
     risk_category_canon AS risk_category_canon,
-    CAST(Ever30mob3_EXPECTED AS DECIMAL(10,2)) AS ever_30_mob3_expected,
-    CAST(Ever30mob3_UPPER_LIMIT AS DECIMAL(10,2)) AS ever_30_mob3_upper_limit,
-    CAST(Ever30mob3_low_performance AS DECIMAL(10,2)) AS ever_30_mob3_low_performance,
-    CAST(Ever60mob6_EXPECTED AS DECIMAL(10,2)) AS ever_60_mob6_expected,
-    CAST(Ever60mob6_UPPER_LIMIT AS DECIMAL(10,2)) AS ever_60_mob6_upper_limit,
-    Ever60mob6_low_performance AS ever_60_mob6_low_performance,
+    CAST(REPLACE(Ever30mob3_EXPECTED, '%', '') AS DECIMAL(10,2)) AS ever_30_mob3_expected,
+    CAST(REPLACE(Ever30mob3_UPPER_LIMIT, '%', '') AS DECIMAL(10,2)) AS ever_30_mob3_upper_limit,
+    CAST(REPLACE(Ever30mob3_low_performance, '%', '') AS DECIMAL(10,2)) AS ever_30_mob3_low_performance,
+    CAST(REPLACE(Ever60mob6_EXPECTED, '%', '') AS DECIMAL(10,2)) AS ever_60_mob6_expected,
+    CAST(REPLACE(Ever60mob6_UPPER_LIMIT, '%', '') AS DECIMAL(10,2)) AS ever_60_mob6_upper_limit,
+    CAST(REPLACE(Ever60mob6_low_performance, '%', '') AS DECIMAL(10, 2)) AS ever_60_mob6_low_performance,
     risk_category_agroup AS risk_category_group
 FROM
     datalake_gsheets_raw.expected_evers
