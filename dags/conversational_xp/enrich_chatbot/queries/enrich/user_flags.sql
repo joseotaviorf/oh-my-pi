ab5a5b47717d95a60ad1b975fdb3a104be46f8e0
William Angelo <39243586+williamangelo@users.noreply.github.com>
2025-07-02 08:42:51 -0300

    feat: update wall-e flag (#19500)

--- dags/conversational_xp/enrich_chatbot/queries/enrich/user_flags.sql
+++ dags/conversational_xp/enrich_chatbot/queries/enrich/user_flags.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT
   CAST(id_user AS BIGINT) AS id_user,
   MAX(
     CAST(
-      GET_JSON_OBJECT(user_properties, '$.ab_beakman_conversational_platform_wall_e_experiment')
+      GET_JSON_OBJECT(user_properties, '$.ab_beakman_conversational_platform_wall_e_enabled')
       AS INTEGER
     )
    ) AS wall_e_rollout_flag
@@ -12,5 +12,5 @@ WHERE
   MAKE_DATE(year, month, day) >= DATE('{load_start_date}')
   AND id_app = 170698
   AND id_user IS NOT NULL
-  AND GET_JSON_OBJECT(user_properties, '$.ab_beakman_conversational_platform_wall_e_experiment') IS NOT NULL
+  AND GET_JSON_OBJECT(user_properties, '$.ab_beakman_conversational_platform_wall_e_enabled') IS NOT NULL
 GROUP BY 1
