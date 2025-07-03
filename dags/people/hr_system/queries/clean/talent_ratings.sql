3dff6df0426766daf45285253ed1cd8ea4b27857
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-07-03 12:19:11 -0300

    [PEOPLE] Remove unused tables from Oracle HCM API (#19429)
    * remove unused dags
    
    * remove unused enrich tables

--- dags/people/hr_system/queries/clean/talent_ratings.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    ProfileId AS id_profile,
-    PersonId AS id_person,
-    ProfileCode AS profile_code,
-    nBoxCellAssignments AS n_box_cell_assignments,
-    performanceRatings AS performance_ratings,
-    userDefinedRatingSections AS user_defined_rating_sections,
-    ts_load
-FROM datalake_hr_system_raw.talent_ratings
