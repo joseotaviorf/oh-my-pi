732695280a34b9d3bb9cf818f47a01c678b92042
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-07-07 18:28:30 -0300

    Update clean kong (#19583)
    * Update kong_logs table metadata
    
    * Update kong_logs table file

--- dags/cross/kong/queries/clean/kong_logs.sql
+++ dags/cross/kong/queries/clean/kong_logs.sql
@@ -1,13 +1,13 @@
-SELECT
-  app,
+with main as (SELECT
+  CAST(GET_JSON_OBJECT(kubernetes, '$.pod_labels') AS STRING) AS pod_labels,
   cluster_name,
-  container_name,
-  env,
+  CAST(GET_JSON_OBJECT(kubernetes, '$.container_name') AS STRING) AS container_name,
   message,
-  namespace,
-  pod_name,
+  CAST(GET_JSON_OBJECT(kubernetes, '$.pod_namespace') AS STRING) AS namespace,
+  CAST(GET_JSON_OBJECT(kubernetes, '$.pod_namespace') AS STRING) AS env,
+  CAST(GET_JSON_OBJECT(kubernetes, '$.pod_name') AS STRING) AS pod_name,
   stream,
-  time AS ts_event,
+  timestamp as ts_event,
   year,
   month,
   day,
@@ -16,3 +16,20 @@ FROM
   datalake_kong_raw.kong_logs
 WHERE
   MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  )
+SELECT 
+  CAST(GET_JSON_OBJECT(pod_labels, '$.app') AS STRING) AS app,
+  cluster_name,
+  container_name,
+  env,
+  message, 
+  namespace,
+  pod_name,
+  stream,
+  ts_event,
+  year,
+  month,
+  day,
+  hour
+FROM 
+  main
