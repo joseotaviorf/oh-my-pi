1f605c565b842f62b35de53ffb770229cdf6073c
Kleber Yuji Inoue <53949782+kleberyuji@users.noreply.github.com>
2025-07-07 15:29:43 -0300

    add is_last_event in pro_owner_history table (#19577)

--- dags/for_rent/enrich_pro_owners/queries/enrich/pro_owner_history.sql
+++ dags/for_rent/enrich_pro_owners/queries/enrich/pro_owner_history.sql
@@ -6,10 +6,10 @@ WITH aud_ts AS (
     LAG(aud.is_active) OVER (PARTITION BY COALESCE(um.id_user, aud.id_user) ORDER BY aud.rev, aud.id DESC) AS previous_status,
     aud.is_active,
     TIMESTAMP(FROM_UNIXTIME(ure.ts_revision/1000)) AS ts_event
-  FROM 
+  FROM
     datalake_ebdb_clean.user_pro_owner_aud AS aud
-  JOIN 
-    datalake_ebdb_clean.user_revision_entity AS ure 
+  JOIN
+    datalake_ebdb_clean.user_revision_entity AS ure
       ON aud.rev = ure.id
   LEFT JOIN
     datalake_ebdb_user.user_merge AS um
@@ -17,26 +17,26 @@ WITH aud_ts AS (
 ),
 
 pro_owner_dates AS (
-  SELECT 
+  SELECT
     aud.id_owner,
     aud.is_active,
     aud.ts_event AS ts_pro_owner_started,
     LEAD(aud.ts_event) OVER (PARTITION BY aud.id_owner ORDER BY aud.ts_event) AS ts_pro_owner_ended
-  FROM 
+  FROM
     aud_ts AS aud
-  WHERE 
+  WHERE
       aud.previous_status != aud.is_active
       OR aud.previous_status IS NULL
 ),
 
 account_manager_dates AS (
-  SELECT 
+  SELECT
     aud.id_owner,
     aud.id_account_manager,
     aud.is_active,
     aud.ts_event AS ts_account_manager_started,
     LEAD(aud.ts_event) OVER (PARTITION BY aud.id_owner ORDER BY aud.ts_event) AS ts_account_manager_ended
-  FROM 
+  FROM
     aud_ts AS aud
   WHERE
     (aud.id_previous_account_manager != aud.id_account_manager
@@ -46,31 +46,32 @@ account_manager_dates AS (
 
 SELECT
   at.id_owner,
-  CASE 
+  CASE
     WHEN at.is_active = False THEN NULL
     ELSE at.id_account_manager
   END AS id_account_manager,
   at.is_active AS is_pro_owner,
   CASE
-    WHEN at.is_active = False 
+    WHEN at.is_active = False
       OR at.id_account_manager IS NULL THEN False
     ELSE True
   END AS is_expert,
+  ROW_NUMBER() OVER (PARTITION BY at.id_owner ORDER BY at.ts_event DESC) = 1 AS is_last_event,
   at.ts_event,
   pod.ts_pro_owner_started,
   pod.ts_pro_owner_ended,
   am.ts_account_manager_started,
   am.ts_account_manager_ended
-FROM 
+FROM
   aud_ts AS at
-LEFT JOIN 
+LEFT JOIN
   pro_owner_dates AS pod
     ON at.id_owner = pod.id_owner
     AND at.ts_event >= pod.ts_pro_owner_started
     AND at.ts_event BETWEEN pod.ts_pro_owner_started AND COALESCE(pod.ts_pro_owner_ended, CURRENT_TIMESTAMP())
     AND at.is_active = pod.is_active
     AND pod.is_active = True
-LEFT JOIN 
+LEFT JOIN
   account_manager_dates AS am
     ON at.id_owner = am.id_owner
     AND at.id_account_manager = am.id_account_manager
