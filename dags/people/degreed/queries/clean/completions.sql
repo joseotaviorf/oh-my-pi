32e8111478292d1395ee95f9363e2e591c58491d
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-07-10 10:29:45 -0300

    [PEOPLE] Fixes bugs in Degreed DAG (#19585)
    * delete accomplishments table
    
    * fix domain, spelling error and add schedule
    
    * delete accomplishments declaration

--- dags/people/degreed/queries/clean/completions.sql
+++ dags/people/degreed/queries/clean/completions.sql
@@ -1,16 +1,16 @@
 SELECT
     id,
-    attributtes.employee_id AS email_employee,
-    attributtes,
+    attributes.employee_id AS email_employee,
+    attributes,
     included,
     relationships,
-    CAST(attributtes.points_earned AS FLOAT) AS points_earned,
-    CAST(attributtes.rating AS INT) AS rating,
-    CAST(attributtes.is_verified AS BOOLEAN) AS is_verified,
-    TO_DATE(attributtes.completed_at) AS dt_completed,
-    TO_TIMESTAMP(attributtes.added_at) AS ts_added,
+    CAST(attributes.points_earned AS FLOAT) AS points_earned,
+    CAST(attributes.rating AS INT) AS rating,
+    CAST(attributes.is_verified AS BOOLEAN) AS is_verified,
+    TO_DATE(attributes.completed_at) AS dt_completed,
+    TO_TIMESTAMP(attributes.added_at) AS ts_added,
     NOW() AS ts_load
 FROM 
     datalake_degreed_raw.completions
 WHERE 
-    DATE(attributtes.added_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    DATE(attributes.added_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
--- dags/people/degreed/queries/clean/logins.sql
+++ dags/people/degreed/queries/clean/logins.sql
@@ -2,9 +2,9 @@ SELECT
     id,
     relationships[0].user.data.id AS id_user,
     employee_id AS email_employee,
-    TO_TIMESTAMP(attributtes.logged_in_at) AS ts_logged,
+    TO_TIMESTAMP(attributes.logged_in_at) AS ts_logged,
     NOW() AS ts_load
 FROM 
     datalake_degreed_raw.logins
 WHERE 
-    DATE(attributtes.logged_in_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+    DATE(attributes.logged_in_at) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
