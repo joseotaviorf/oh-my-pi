0e8ab0c8ea2060f784c0ee9fe48e323578fc1f34
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-07-16 17:59:50 -0300

    [3P] Signup CDC ingestion (#19636)
    * create signup's ingestion dag
    
    * add table description
    
    * add 3P Partners in metadata file schema
    
    ---------
    
    Co-authored-by: Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>

---
+++ dags/3p_partners/signup/queries/clean/user_registration_history.sql
@@ -0,0 +1,14 @@
+SELECT
+    id,
+    requester_person_id AS uuid_person_requester,
+    target_person_uuid AS uuid_person_target,
+    operation,
+    reason,
+    addition_comment,
+    channel,
+    executed_at AS ts_executed,
+    year,
+    month,
+    day
+FROM
+    datalake_signup_raw.user_registration_history
