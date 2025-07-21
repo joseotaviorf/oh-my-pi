d8830fd26f1d33d5a8ef7ccd49744fec1aec7b49
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-07-21 15:40:10 -0300

    [DBP-947] Add new tables to pin_core DAG (#19683)
    * add query and metadata
    
    * add ident
    
    * update domain
    
    * fix declaration
    
    * rename domain

---
+++ dags/people/pin_core/queries/clean/grade_ladder_translation.sql
@@ -0,0 +1,19 @@
+SELECT
+    grade_ladder_id AS id_grade_ladder,
+    business_group_id AS id_business_group,
+    name,
+    language,
+    source_lang AS source_language,
+    created_by,
+    last_updated_by AS updated_by,
+    CAST(object_version_number AS INT) AS object_version_number,
+    TO_DATE(effective_start_date) AS dt_effective_started,
+    TO_DATE(effective_end_date) AS dt_effective_ended,
+    TO_TIMESTAMP(creation_date) AS ts_created,
+    TO_TIMESTAMP(last_update_date) AS ts_updated,
+    NOW() AS ts_load,
+    year,
+    month,
+    day
+FROM
+    datalake_pin_core_raw.per_grade_ladders_f_tl
