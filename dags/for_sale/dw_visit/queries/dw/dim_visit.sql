e2cf3d187c02d153582f607364f52b5c2166cfde
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-07-28 10:29:38 -0300

    add rent_flow (#19767)

--- dags/for_sale/dw_visit/queries/dw/dim_visit.sql
+++ dags/for_sale/dw_visit/queries/dw/dim_visit.sql
@@ -1,3 +1,21 @@
+WITH
+  visit_rent_flow AS (
+    SELECT
+      frde.sk_visit,
+      LOWER(dret.abbreviation) AS event_code,
+      'RENT' AS business_context,
+      MIN(frde.ts_event) AS ts_first_event,
+      MAX(frde.ts_event) AS ts_last_event
+    FROM
+      dw_rent.fact_rent_demand_events AS frde
+    INNER JOIN
+      dw_rent.dim_rent_event_type AS dret
+        ON dret.sk_event_type = frde.sk_event_type
+    WHERE
+      LOWER(dret.abbreviation) IN ('os','oa','cs')
+      AND ts_event::date >= '2024-01-01'
+    GROUP BY 1, 2, 3
+  )
 SELECT -- [ODS] This table was migrated from ODS flow and needs a future refactoring to remove castings and renaming
   v.id_visit AS sk_visit,
   v.id_visit AS id_visit,
@@ -36,6 +54,12 @@ SELECT -- [ODS] This table was migrated from ODS flow and needs a future refacto
   v.has_fup_collected AS has_visit_fup_collected,
   v.has_finisher_status AS has_visit_finisher_status,
   v.has_more_one_agent AS has_visit_more_one_agent,
+  IF(os.ts_first_event IS NOT NULL, TRUE, FALSE) AS has_offer_submitted,
+  IF(oa.ts_first_event IS NOT NULL, TRUE, FALSE) AS has_offer_accepted,
+  IF(cs.ts_first_event IS NOT NULL, TRUE, FALSE) AS has_contract_signed,
+  IF(v.ts_created < os.ts_first_event, TRUE, FALSE) AS has_offer_submitted_after_visit_creation,
+  IF(v.ts_created < oa.ts_first_event, TRUE, FALSE) AS has_offer_accepted_after_visit_creation,
+  IF(v.ts_created < cs.ts_first_event, TRUE, FALSE) AS has_contract_signed_after_visit_creation,
   v.ts_created AS dt_created,
   v.ts_updated AS dt_updated,
   v.ts_visit,
@@ -50,9 +74,27 @@ SELECT -- [ODS] This table was migrated from ODS flow and needs a future refacto
   v.ts_visit_fup_collected,
   v.ts_visit_stalled,
   v.ts_first_visit,
+  os.ts_first_event AS ts_first_offer_submitted,
+  oa.ts_first_event AS ts_first_offer_accepted,
+  cs.ts_first_event AS ts_first_contract_signed,
   NOW() AS ts_load
 FROM
   datalake_visit.visits AS v
 LEFT JOIN
   datalake_sale_visit_hubs.sale_visit_hubs AS sv
     ON sv.id_booking = v.id_visit
+LEFT JOIN
+  visit_rent_flow AS os
+    ON v.id_visit = os.sk_visit
+    AND v.business_context = os.business_context
+    AND os.event_code = 'os'
+LEFT JOIN
+  visit_rent_flow AS oa
+    ON v.id_visit = oa.sk_visit
+    AND v.business_context = oa.business_context
+    AND oa.event_code = 'oa'
+LEFT JOIN
+  visit_rent_flow AS cs
+    ON v.id_visit = cs.sk_visit
+    AND v.business_context = cs.business_context
+    AND cs.event_code = 'cs'
