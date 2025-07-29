ff83876096cde80a8c3b0b0aa934eee142a712be
Karina Duran Munhos <63967857+KarinaDuran@users.noreply.github.com>
2025-07-29 17:59:32 -0300

    Fix: Docpilot extraction processing (#19757)
    * Fixing the error join issue
    
    * Fixing identation
    
    * Update dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
    
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
    
    * Update dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
    
    Co-authored-by: luana-silvia <93656129+luana-silvia@users.noreply.github.com>
    
    * Update dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
    
    Co-authored-by: luana-silvia <93656129+luana-silvia@users.noreply.github.com>
    
    * Update dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
    
    Co-authored-by: luana-silvia <93656129+luana-silvia@users.noreply.github.com>
    
    * Add the new columns and update old in .yml and add a median for incomes.
    
    * update Fintech name
    
    ---------
    
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
    Co-authored-by: luana-silvia <93656129+luana-silvia@users.noreply.github.com>

--- dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
+++ dags/fintech/enrich_docpilot/queries/enrich/income_extraction_processing.sql
@@ -1,9 +1,16 @@
 WITH extract_errors AS (
   SELECT
+  *,
+  LOWER(regexp_replace(llm_bank_name, '[^A-Za-z0-9\s]+', '')) AS llm_normalized_bank_name
+FROM (
+  SELECT
     id_external,
     get_json_object(to_json(errors), "$.type") as error_type,
     get_json_object(to_json(errors), "$.detail.document_path") as file_path,
-    get_json_object(to_json(errors), "$.detail.detected_layout") as detected_layout
+    get_json_object(to_json(errors), "$.detail.detected_layout") AS detected_layout,
+    get_json_object(to_json(errors), "$.detail.reference_date") AS error_reference_date,
+    get_json_object(to_json(errors), "$.detail.identification_result.bank_name") AS llm_bank_name,
+    get_json_object(to_json(errors), "$.detail.identification_result.document_type") AS llm_document_type
   FROM (
     SELECT
       id_external,
@@ -14,15 +21,37 @@ WITH extract_errors AS (
             type:string,
             detail: Struct<
               document_path:string,
-              detected_layout:string
+              detected_layout:string,
+              reference_date:string,
+              identification_result: Struct<
+                bank_name:string,
+                document_type:string
+              >
             >
           >
-        >")) as errors
+        >"
+        )) as errors
     FROM datalake_docpilot_clean.income_extraction
+    )
   )
 ),
+processing_errors AS (
+    SELECT
+    id_external,
+    MAX(processing_error) as processing_errors
+    FROM(
+      SELECT
+      id_external,
+      CASE WHEN error_type IN ('INVALID_DATE_RANGE', 'LESS_THAN_RECOMMENDED_PERIOD')
+            THEN error_type
+            ELSE NULL
+            END AS processing_error
+      FROM extract_errors
+    )
+    GROUP BY id_external
+),
 extract_json_data AS (
-  SELECT
+    SELECT
     id_external,
     external_source,
     processing_result,
@@ -38,17 +67,17 @@ extract_json_data AS (
     GET_JSON_OBJECT(extracted_data, '$.committed_income') AS committed_income,
     FROM_JSON(
       GET_JSON_OBJECT(extracted_data, '$.documents'),
-      'ARRAY<STRUCT<data:STRUCT<end_date:STRING, start_date:STRING, date:STRING>, path:STRING, metadata_analysis:STRUCT<is_suspicious:BOOLEAN, dates_divergent:BOOLEAN>>>'
+      'ARRAY<STRUCT<data:STRUCT<end_date:STRING, start_date:STRING, date:STRING, statement_type:String>, path:STRING, metadata_analysis:STRUCT<is_suspicious:BOOLEAN, dates_divergent:BOOLEAN>>>'
     ) AS documents,
     ts_created,
     ts_updated
   FROM
     datalake_docpilot_clean.income_extraction
 ),
-explode_documents AS (
+analysis_data AS(
   SELECT
+  DISTINCT
     id_external,
-    document.path AS file_path,
     external_source,
     processing_result,
     documents_type,
@@ -58,62 +87,96 @@ explode_documents AS (
     committed_income,
     ARRAY_SIZE(extract_errors) AS number_of_errors,
     ARRAY_SIZE(all_documents) AS number_of_processed_documents,
+    ts_created,
+    ts_updated
+    FROM
+    extract_json_data
+),
+explode_documents AS (
+   SELECT
+    id_external,
+    document.path AS file_path,
     document.data.date AS document_date,
     document.data.end_date AS document_end_date,
     document.data.start_date AS document_start_date,
+    document.data.statement_type as statement_type,
     document.metadata_analysis.is_suspicious AS is_suspicious,
-    document.metadata_analysis.dates_divergent AS dates_divergent,
-    ts_created,
-    ts_updated
+    document.metadata_analysis.dates_divergent AS dates_divergent
   FROM
     extract_json_data
     LATERAL VIEW OUTER EXPLODE(documents) AS document
-),
-join_document_erros AS (
+), join_document_error AS(
   SELECT
-    ed.id_external,
-    ed.file_path,
-    ed.external_source,
-    ed.processing_result,
-    ed.documents_type,
-    ed.reference_date,
-    ed.net_income,
-    ed.gross_income,
-    ed.committed_income,
-    ed.number_of_errors,
-    ed.number_of_processed_documents,
+    COALESCE(ed.id_external, er.id_external) as id_external,
+    COALESCE(ed.file_path, er.file_path) AS file_path,
+    ed.statement_type,
     ed.document_date,
     ed.document_end_date,
     ed.document_start_date,
     er.error_type,
+    pe.processing_errors,
     er.file_path AS error_file_path,
     er.detected_layout,
+    er.llm_bank_name,
+    er.llm_document_type,
+    er.llm_normalized_bank_name,
     ed.is_suspicious,
-    ed.dates_divergent,
-    ed.ts_created,
-    ed.ts_updated
+    ed.dates_divergent
   FROM explode_documents AS ed
-  LEFT JOIN
+  FULL OUTER JOIN
     extract_errors AS er
-    ON (
+    ON
     ed.id_external = er.id_external
-    AND (ed.file_path = er.file_path OR
-    er.file_path IS NULL OR ed.file_path IS NULL)
-    )
+    AND
+    ed.file_path = er.file_path
+    LEFT JOIN processing_errors pe
+    ON ed.id_external = pe.id_external
+), join_all_data(
+SELECT
+    ad.id_external,
+    jd.file_path,
+    ad.external_source,
+    ad.processing_result,
+    ad.documents_type,
+    jd.statement_type,
+    ad.reference_date,
+    ad.net_income,
+    ad.gross_income,
+    ad.committed_income,
+    ad.number_of_errors,
+    ad.number_of_processed_documents,
+    jd.document_date,
+    jd.document_end_date,
+    jd.document_start_date,
+    jd.error_type,
+    jd.processing_errors,
+    jd.error_file_path,
+    jd.detected_layout,
+    jd.llm_bank_name,
+    jd.llm_document_type,
+    jd.llm_normalized_bank_name,
+    jd.is_suspicious,
+    jd.dates_divergent,
+    ad.ts_created,
+    ad.ts_updated
+  FROM analysis_data ad
+  LEFT JOIN join_document_error jd
+  ON ad.id_external = jd.id_external
+  WHERE file_path IS NOT NULL
 ),
 explode_net_income AS (
   SELECT
     id_external,
     EXPLODE(from_json(net_income, 'MAP<STRING, DECIMAL>')) AS (date, net_income)
   FROM
-    join_document_erros
+    join_all_data
 ),
 explode_gross_income AS (
   SELECT
     id_external,
     EXPLODE(from_json(gross_income, 'MAP<STRING, DECIMAL>')) AS (date, gross_income)
   FROM
-    join_document_erros
+    join_all_data
 ),
 explode_committed_income AS (
   SELECT
@@ -122,7 +185,7 @@ explode_committed_income AS (
       from_json(committed_income, 'MAP<STRING, DECIMAL>')
     ) AS (date, committed_income)
   FROM
-    join_document_erros
+    join_all_data
 ),
 get_incomes AS (
   SELECT
@@ -150,6 +213,16 @@ avg_incomes AS (
     get_incomes
   GROUP BY id_external
 ),
+median_incomes AS (
+  SELECT
+    id_external,
+    ROUND(MEDIAN(net_income), 2) AS median_net_income,
+    ROUND(MEDIAN(gross_income), 2) AS median_gross_income,
+    ROUND(MEDIAN(committed_income), 2) AS median_committed_income
+  FROM
+    get_incomes
+  GROUP BY id_external
+),
 user_folder AS (
   SELECT
     DISTINCT id_folder,
@@ -167,11 +240,16 @@ transform_income_data AS (
     jd.external_source,
     jd.processing_result,
     jd.documents_type,
-    jd.number_of_errors AS number_of_documents_with_errors,
+    jd.statement_type,
+    jd.number_of_errors,
     jd.number_of_processed_documents,
     jd.error_type,
+    jd.processing_errors,
     jd.error_file_path,
     jd.detected_layout,
+    jd.llm_bank_name,
+    jd.llm_document_type,
+    jd.llm_normalized_bank_name,
     gi.date AS reference_period,
     gi.net_income,
     gi.gross_income,
@@ -179,6 +257,9 @@ transform_income_data AS (
     av.avg_net_income AS avg_net_income,
     av.avg_gross_income AS avg_gross_income,
     av.avg_committed_income AS avg_committed_income,
+    mi.median_net_income,
+    mi.median_gross_income,
+    mi.median_committed_income,
     jd.is_suspicious,
     jd.dates_divergent AS is_dates_divergent,
     jd.reference_date,
@@ -189,7 +270,7 @@ transform_income_data AS (
     jd.ts_updated,
     NOW() AS ts_load
   FROM
-    join_document_erros AS jd
+    join_all_data AS jd
     LEFT JOIN datalake_docx_clean.document AS d ON jd.id_external = d.id
     LEFT JOIN user_folder AS uf ON (
       d.id_folder = uf.id_folder
@@ -198,12 +279,15 @@ transform_income_data AS (
     LEFT JOIN get_incomes AS gi
       ON gi.id_external = jd.id_external
       AND (
-        gi.date = jd.document_start_date OR
-        gi.date = jd.document_date OR
-        gi.date = jd.document_date OR
-        jd.document_date IS NULL)
+        (gi.date >= jd.document_start_date
+        AND gi.date < jd.document_end_date)
+        OR
+        gi.date = jd.document_date )
+        AND jd.error_type IS NULL
     LEFT JOIN avg_incomes AS av
       ON av.id_external = jd.id_external
+    LEFT JOIN median_incomes AS mi
+      ON mi.id_external = jd.id_external
 ),
 get_user AS (
   SELECT
@@ -223,11 +307,16 @@ SELECT DISTINCT
     id.external_source,
     id.processing_result,
     id.documents_type,
+    id.statement_type,
+    id.llm_normalized_bank_name,
     id.number_of_processed_documents,
-    id.number_of_documents_with_errors,
+    id.number_of_errors,
     id.error_type,
+    id.processing_errors,
     id.error_file_path,
     id.detected_layout,
+    id.llm_bank_name,
+    id.llm_document_type,
     TO_DATE(id.reference_date) AS reference_date,
     TO_DATE(id.reference_period) AS reference_period,
     id.net_income,
@@ -236,6 +325,9 @@ SELECT DISTINCT
     id.avg_net_income,
     id.avg_gross_income,
     id.avg_committed_income,
+    id.median_net_income,
+    id.median_gross_income,
+    id.median_committed_income,
     IF(u.id_user IS NOT NULL, TRUE, FALSE) AS is_main_user,
     id.is_suspicious,
     id.is_dates_divergent,
