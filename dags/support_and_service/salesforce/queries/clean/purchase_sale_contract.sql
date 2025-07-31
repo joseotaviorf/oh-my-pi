c4c6247b5dbe355740cc6223ffabe7f7060cca63
Thiago Bueno <35686851+tbuenos@users.noreply.github.com>
2025-07-31 12:11:37 -0300

    [DFORSALE-9071] - SalesForce - Update fields clean table purchase_sale_contract (#19827)
    * DFORSALE-9071 Update fields raw table purchase_sale_contract
    
    * DFORSALE-9071 Update fields raw table purchase_sale_contract
    
    * DFORSALE-9071 Remove last , before FROM
    
    * DFORSALE-9071 - Fix: remove field 'attributes'
    
    * DFORSALE-9071 Updated new fields in clean layer
    
    * DFORSALE-9071 Adjust line name and description CRN
    
    * Update dags/support_and_service/salesforce/metadata/clean/purchase_sale_contract.yml
    
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
    
    * Update dags/support_and_service/salesforce/metadata/clean/purchase_sale_contract.yml
    
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
    
    * Update dags/support_and_service/salesforce/metadata/clean/purchase_sale_contract.yml
    
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
    
    * DFORSALE-9071 - Adjusting field name
    
    ---------
    
    Co-authored-by: Thiago Bueno <thiago.bueno@BRSPPM-GGH2XX6MJ4.local>
    Co-authored-by: Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>

--- dags/support_and_service/salesforce/queries/clean/purchase_sale_contract.sql
+++ dags/support_and_service/salesforce/queries/clean/purchase_sale_contract.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT    
   Id,
   OwnerId AS id_owner,
   CreatedById AS id_created_by,
@@ -17,6 +17,44 @@ SELECT
   ZendeskUrl__c AS zendesk_url,
   AgentEmail__c AS agent_email,
   CRNStatus__c AS crn_status,
+  CCVStatus__c AS ccv_status,
+  HouseChattelMortgage__c AS house_chattel_mortgage,
+  HouseState__c AS house_state,
+  ITBIStatus__c AS itbi_status,
+  PaymentMethod__c AS payment_method,
+  SimplifiedStage__c AS simplified_stage,
+  SubscriptionModelBuyer__c AS subscription_model_buyer,
+  SubscriptionModelSeller__c AS subscription_model_seller,
+  CRI_Protocol__c AS cri_protocol,
+  CRNName__c AS crn_name,
+  AlienationStatus__c AS alienation_status,
+  BypassRescissionValidation__c AS bypass_rescission_validation,  
+  CRI_HaveDemandNote__c AS cri_have_demand_note,
+  CRNPartner__c AS crn_partner,
+  Escrivao__c AS escrivao,
+  HaveUnpaidAlienation__c AS have_unpaid_alienation,
+  HouseOccupant__c AS house_occupant,
+  PreemptiveRight__c AS preemptive_right,
+  ProofPaymentReceived__c AS proof_payment_received,
+  RequestType__c AS request_type,
+  RequestedBy__c AS requested_by,
+  RescissionStatus__c AS rescission_status,
+  FS_AVE_ProtocolCCV__c AS fs_ave_protocol_ccv,
+  FS_CRINameCCV__c AS fs_cri_name_ccv,
+  FS_EndorsementStatusCCV__c AS fs_endorsement_status_ccv,
+  FS_IDCartorioCCV__c AS fs_cartorio_ccv_id,
+  FS_ReasonNonConversionCCV__c AS fs_reason_non_conversion_ccv, 
+  Address__c AS address,
+  FS_ResponsibleAssistantCCV__c AS fs_responsible_assistant_ccv,
+  LTBookkeepingDoneForm__c AS lt_bookkeeping_done_form,
+  FS_ReasonsLTOverrunRegisterCCV__c AS fs_reasons_lt_overrun_register_ccv,
+  FS_ReasonsLTOverrunCCV__c AS fs_reasons_lt_overrun_ccv,
+  DealMakerName__c AS deal_maker_name,
+  HubName__c AS hub_name,
+  LongTermPayment__c AS long_term_payment,
+  Faixa_de_Dias_desde_CCV__c AS range_of_days_since_ccv,
+  ThreadId__c AS thread_id,
+  HaveDemandNoteReason__c AS have_demand_note_reason,
   CASE
     WHEN IsDeleted = 'true' THEN TRUE
     WHEN IsDeleted = 'false' THEN FALSE
@@ -50,11 +88,56 @@ SELECT
     WHEN KeysReceived__c = 'True' THEN TRUE
     WHEN KeysReceived__c = 'False' THEN FALSE
   END AS is_key_received,
+  CASE
+    WHEN ContinueWithPartnerRegistry__c = 'Sim' THEN TRUE
+    WHEN ContinueWithPartnerRegistry__c = 'Não' THEN FALSE
+  END AS is_continue_with_partner_registry,
+  CASE
+    WHEN HouseRegistryMustBeUpdated__c = 'true' THEN TRUE
+    WHEN HouseRegistryMustBeUpdated__c = 'false' THEN FALSE
+  END AS is_house_registry_must_be_updated,
+  CASE
+    WHEN IsSuspended__c = 'true' THEN TRUE
+    WHEN IsSuspended__c = 'false' THEN FALSE
+  END AS is_suspended,
+  CASE
+    WHEN IsAssigned__c = 'true' THEN TRUE
+    WHEN IsAssigned__c = 'false' THEN FALSE
+  END AS is_assigned,
+  CASE
+    WHEN FS_BalancePaymentMade__c = 'Sim' THEN TRUE
+    WHEN FS_BalancePaymentMade__c = 'Não' THEN FALSE
+  END AS is_balance_payment_made,
+  CASE
+    WHEN ShareCommunityPortal__c = 'true' THEN TRUE
+    WHEN ShareCommunityPortal__c = 'false' THEN FALSE
+  END AS is_share_community_portal,
+  CASE
+    WHEN ListViewUser__c = 'true' THEN TRUE
+    WHEN ListViewUser__c = 'false' THEN FALSE
+  END AS is_list_view_user,
+  CASE
+    WHEN HouseRegistryMustBeUpdatedForm__c = 'Sim' THEN TRUE
+    WHEN HouseRegistryMustBeUpdatedForm__c = 'Não' THEN FALSE
+  END AS is_house_registry_must_be_updated_form,
+  CASE
+    WHEN ConcomitantRegistration__c = 'Sim' THEN TRUE
+    WHEN ConcomitantRegistration__c = 'Não' THEN FALSE
+  END AS is_concomitant_registration,
+  CASE
+    WHEN LeaseComplaintSent__c = 'Sim' THEN TRUE
+    WHEN LeaseComplaintSent__c = 'Não' THEN FALSE
+  END AS is_lease_complaint_sent,
   CAST(DownPaymentDeadline__c AS DATE) AS dt_down_payment_deadline,
   CAST(FS_BY_KeyDeliveryDate__c AS DATE) AS dt_key_delivery_buyer,
   CAST(FS_SL_KeyDeliveryDate__c AS DATE) AS dt_key_delivery_seller,
   CAST(KeyDeliveryDate__c AS DATE) AS dt_key_delivery,
   CAST(dt_updated AS DATE) AS dt_updated,
+  CAST(SubscriptionSchedulingDate__c AS DATE) AS dt_subscription_scheduling,
+  CAST(BookkeepingDoneDateCCV__c AS DATE) AS dt_bookkeeping_done_ccv,
+  CAST(CRIDoneDateCCV__c AS DATE) AS dt_cri_done_ccv,
+  CAST(FS_Endorsement_RequestDateCCV__c AS DATE) AS dt_fs_endorsement_request_ccv,
+  CAST(FS_CRI_RequestDateCCV__c AS DATE) AS dt_fs_cri_request_ccv,
   CAST(CreatedDate AS TIMESTAMP) AS ts_created,
   CAST(LastModifiedDate AS TIMESTAMP) AS ts_last_modified,
   CAST(SystemModstamp AS TIMESTAMP) AS ts_system_mod,
@@ -62,9 +145,11 @@ SELECT
   CAST(LastViewedDate AS TIMESTAMP) AS ts_last_viewed,
   CAST(LastReferencedDate AS TIMESTAMP) AS ts_last_referenced,
   CAST(CCVSignedAt__c AS TIMESTAMP) AS ts_ccv_signed,
+  CAST(CCVTerminationCreatedAt__c AS TIMESTAMP) AS ts_ccv_termination,
+  CAST(DDAcceptedDate__c AS TIMESTAMP) AS ts_dd_accepted,
   year,
   month,
-  day
+  day 
 FROM 
   datalake_salesforce_raw.purchase_sale_contract
 WHERE
