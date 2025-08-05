53fbbf4d4797d64d88907d0cd2a563c665a7bcb4
Luciane Lopes <lucianesl@users.noreply.github.com>
2025-08-05 10:17:01 +0100

    fix: istio dag struct (#19864)

--- dags/tech_platform/istio/spark_jobs/istio_logs_load.py
+++ dags/tech_platform/istio/spark_jobs/istio_logs_load.py
@@ -4,7 +4,8 @@ from dateutil import parser
 
 from quintoandar_logger import QuintoAndarLogger
 from bietlejuice.loaders.delta_loader import DeltaLoader
-from pyspark.sql.functions import split, when, lit, element_at, col, year, month, dayofmonth, hour, to_timestamp
+from pyspark.sql.functions import split, when, lit, element_at, col, year, month, dayofmonth, hour, to_timestamp, from_json
+from pyspark.sql.types import StructType, StructField, StringType, LongType
 from bietlejuice.services.configuration_service import ConfigurationService
 from bietlejuice.base.spark import (
     spark
@@ -20,35 +21,37 @@ def clean_cf(df):
     Transform extract only the necessary columns.
     """
 
-    ts = to_timestamp(col("start_time"))
-    traceparent = col("traceparent")
-    
+    df = df.withColumn("data", from_json(col("message"), get_istio_schema()))
+    ts = to_timestamp(col("timestamp"))
+    ts_event = to_timestamp(col("data.start_time"))
+    traceparent = col("data.traceparent")
+
     trace_id = when(
-        traceparent.isNotNull() & 
+        traceparent.isNotNull() &
         traceparent.contains("-"),
         element_at(split(traceparent, "-"), 2)
     ).otherwise(lit(None))
 
     df = df.select(
-               ts.alias("ts_event"), 
+               ts_event.alias("ts_event"),
                trace_id.alias("id_trace"),
                traceparent.alias("request_traceparent"),
-               col("namespace").alias("namespace"),
-               col("pod_name").alias("pod_name"),
-               col("method").alias("request_method"),
-               col("user_agent").alias("request_user_agent"),
-               col("path").alias("request_path"),
-               col("duration").alias("request_duration_ms"),
-               col("x_forwarded_for").alias("request_x_forwarded_for"),
-               col("response_flags").alias("response_flags"),
-               col("response_code").alias("response_code"),
-               col("request_id").alias("id_request"),
+               col("namespace"),
+               col("pod_name"),
+               col("data.method").alias("request_method"),
+               col("data.user_agent").alias("request_user_agent"),
+               col("data.path").alias("request_path"),
+               col("data.duration").alias("request_duration_ms"),
+               col("data.x_forwarded_for").alias("request_x_forwarded_for"),
+               col("data.response_flags").alias("response_flags"),
+               col("data.response_code").alias("response_code"),
+               col("data.request_id").alias("id_request"),
                col("app"),
                year(ts).alias("year"),
                month(ts).alias("month"),
                dayofmonth(ts).alias("day"),
                hour(ts).alias("hour")
-              ).where(col("start_time").isNotNull())
+              ).where(col("data.start_time").isNotNull())
 
     return df
 
@@ -104,6 +107,33 @@ def main():
         partition_by=args.partition_cols,
     )
 
+def get_istio_schema():
+    return StructType([
+        StructField('response_code', LongType(), True),
+        StructField('upstream_service_time', StringType(), True),
+        StructField('method', StringType(), True),
+        StructField('traceparent', StringType(), True),
+        StructField('downstream_remote_address', StringType(), True),
+        StructField('upstream_local_address', StringType(), True),
+        StructField('bytes_sent', LongType(), True),
+        StructField('user_agent', StringType(), True),
+        StructField('x_forwarded_for', StringType(), True),
+        StructField('path', StringType(), True),
+        StructField('upstream_host', StringType(), True),
+        StructField('requested_server_name', StringType(), True),
+        StructField('request_id', StringType(), True),
+        StructField('protocol', StringType(), True),
+        StructField('route_name', StringType(), True),
+        StructField('upstream_transport_failure_reason', StringType(), True),
+        StructField('downstream_local_address', StringType(), True),
+        StructField('bytes_received', LongType(), True),
+        StructField('start_time', StringType(), True),
+        StructField('response_flags', StringType(), True),
+        StructField('upstream_cluster', StringType(), True),
+        StructField('authority', StringType(), True),
+        StructField('istio_policy_status', StringType(), True),
+        StructField('duration', LongType(), True)
+    ])
 
 if __name__ == "__main__":
     main()
