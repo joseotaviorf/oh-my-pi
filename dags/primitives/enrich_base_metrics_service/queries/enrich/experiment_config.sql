1ecb3be36213d9cf406cf260497105612a6a0fc9
pedronogueiraquinto <155736231+pedronogueiraquinto@users.noreply.github.com>
2025-08-06 18:14:40 +0100

    feat: Adding concierge exp (#19881)
    * fix: fixing timestamp
    
    * feat: Adding recs impressions incremental table
    
    * chore: changing recs impressions to full load so that its easier to re process
    
    * chore: adding missing filters to minimize data processing
    
    * fix: removing duplicate recset ids that lead to having duplicate variants
    
    * feat: adding tof global metrics
    
    * chore: adding new exp
    
    * chore: adding new exp
    
    * chore: adding new exp

--- dags/primitives/enrich_base_metrics_service/queries/enrich/experiment_config.sql
+++ dags/primitives/enrich_base_metrics_service/queries/enrich/experiment_config.sql
@@ -122,6 +122,27 @@ FROM VALUES
                                                 )
                                 )
         )
+    ),
+    -- Concierge VS Secretaria experiment
+    (
+        "ab_beakman_search_services_concierge_v1",
+        named_struct(
+            'begin_date', DATE('2025-08-08'),
+            'end_date', NULL,
+            'variants', to_json(
+                named_struct(
+                    '0', 'baseline',
+                    '1', 'treatment'
+                )
+            ),
+            'filters', NULL,
+            'metrics', to_json(
+                                    named_struct(
+                                                    'default_metrics', true,
+                                                    'experiment_metrics', array()
+                                                )
+                                )
+        )
     )
 
 AS experiment_config(experiment_name, config)
