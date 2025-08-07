2ed0f4269167e223a91ad9c48906b660fabc2633
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-08-07 13:32:33 -0300

    Creating new visit DAG (#19769)
    * creating new visit tables
    
    * remove partitions
    
    * adjusting dag name
    
    * test dag
    
    * metadata
    
    * metadata
    
    * descriptions
    
    * fix
    
    * fix
    
    * dbutils
    
    * fix dq
    
    * remove zorderby

---
+++ dags/for_sale/ebdb_visit_fast_lane/queries/clean/visit_status_log.sql
@@ -0,0 +1,15 @@
+SELECT
+    id AS id_visit_status_log,
+    visit_id AS id_visit,
+    schedule_id AS id_schedule,
+    author_user_identifier AS id_author_user,
+    author_user_type,
+    author_user_role,
+    on_behalf_of,
+    reason,
+    channel,
+    event_type,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_ebdb_test_raw.visitstatuslog
