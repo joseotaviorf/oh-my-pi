a8f933825fe5f45904446c39274b47ce912ac6a1
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2025-08-11 15:22:36 -0300

    [DFCA] Update columns in datalake_rental_guarantee_clean (#19919)
    * Add: new columns to datalake_rental_guarantee_clean.charge and datalake_rental_guarantee_clean.charge_aud
    
    * Add descriptions to table and column
    
    * Add: domain
    
    ---------
    
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/rental_guarantee/queries/clean/charge.sql
+++ dags/fintech/rental_guarantee/queries/clean/charge.sql
@@ -1,6 +1,7 @@
 SELECT
     id,
     guarantee_id AS id_guarantee,
+    checkout_order_id AS id_checkout_order,
     cancellation_reason,
     card_token,
     emv,
@@ -10,6 +11,8 @@ SELECT
     status AS charge_status,
     type AS charge_type,
     refund_amount,
+    external_source,
+    checkout_order_hash,
     created_at AS ts_created,
     updated_at AS ts_updated,
     year,
