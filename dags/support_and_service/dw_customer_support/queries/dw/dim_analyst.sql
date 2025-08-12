64dd9854231326389ef7950e1d0a044ef4bf70f5
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-08-12 13:42:50 -0300

    Fix duplicated record from analyst list (#19941)
    * Fix duplicated record from analyst list
    
    * Fix line break
    
    * Fix again

--- dags/support_and_service/dw_customer_support/queries/dw/dim_analyst.sql
+++ dags/support_and_service/dw_customer_support/queries/dw/dim_analyst.sql
@@ -1,27 +1,47 @@
-SELECT
-    MD5(a.email) AS sk_analyst,
+WITH clean_email AS (
+    SELECT 
+        CASE
+            WHEN LENGTH(SPLIT(a.email, '\\.')[SIZE(SPLIT(a.email, '\\.')) - 1]) = 1
+                AND SPLIT(a.email, '\\.')[SIZE(SPLIT(a.email, '\\.')) - 1] LIKE 'b%'
+            THEN CONCAT(a.email, 'r')
+            ELSE a.email
+        END AS email,
+        a.name AS full_name,
+        a.phone,
+        CASE
+            WHEN a.organization = 'atn' THEN 'atento'
+            WHEN a.organization = 'quintoandar.com' THEN 'quintoandar'
+            WHEN a.organization = 'webhelpbr' THEN 'webhelp'
+            WHEN a.organization = 'contractors' THEN 'webhelp'
+            ELSE a.organization
+        END AS agent_organization,
+        DATE(a.ts_created) AS dt_agent_start
+    FROM
+        datalake_support_users.analysts AS a
+)
+SELECT 
+    MD5(ce.email) AS sk_analyst,
     u.id AS sk_user,
     u.uuid_person,
     a.id_agent_twilio AS sk_agent_twilio,
-    a.name AS full_name,
-    a.email,
-    a.phone,
-    CASE
-        WHEN a.organization = 'atn' THEN 'atento'
-        WHEN a.organization = 'quintoandar.com' THEN 'quintoandar'
-        WHEN a.organization = 'webhelpbr' THEN 'webhelp'
-        WHEN a.organization = 'contractors' THEN 'webhelp'
-        ELSE a.organization
-    END agent_organization,
+    ce.full_name,
+    ce.email,
+    ce.phone,
+    ce.agent_organization,
     ac.manager AS agent_manager,
-    DATE(a.ts_created) AS dt_agent_start,
+    ce.dt_agent_start,
     NOW() AS ts_load
-FROM
+FROM 
+    clean_email AS ce
+LEFT JOIN 
     datalake_support_users.analysts AS a
-LEFT JOIN
+        ON a.email = ce.email
+LEFT JOIN 
     datalake_gsheets_clean.agents_control AS ac
-        ON LOWER(a.email) = LOWER(ac.email)
-LEFT JOIN
+        ON LOWER(ce.email) = LOWER(ac.email)
+LEFT JOIN 
     datalake_ebdb_user.user AS u
-        ON u.email = a.email
+        ON u.email = ce.email
         AND u.country_code = 'BR'
+QUALIFY 
+    ROW_NUMBER () OVER (PARTITION BY ce.email ORDER BY dt_agent_start ASC) = 1
