a259fdbf2e5c5a4a6870b5c80c2ba53fe7cc1c07
William Angelo <39243586+williamangelo@users.noreply.github.com>
2025-08-12 10:24:12 -0300

    fix: worng time window on clean queries (#19936)

--- dags/conversational_xp/langfuse/queries/clean/scores.sql
+++ dags/conversational_xp/langfuse/queries/clean/scores.sql
@@ -24,16 +24,6 @@ SELECT
 FROM
     datalake_langfuse_raw.scores
 WHERE
-    MAKE_TIMESTAMP(year, month, day, hour, 0, 0) BETWEEN TIMESTAMP('{load_start_date}') - INTERVAL 2 HOUR 
-    AND TIMESTAMP('{load_end_date}')
-    -- removes sessions with multiple tags (BUG from late July/2025)
-    AND (
-        (
-            sessionId IS NOT NULL 
-            AND sessionID NOT IN (
-                SELECT DISTINCT sessionId FROM datalake_langfuse_raw.traces WHERE cardinality(tags) > 1
-            )
-        ) OR sessionId IS NULL
-    )
+    MAKE_TIMESTAMP(year, month, day, hour, 0, 0) >= TIMESTAMP('{load_start_date}')
 QUALIFY
     ROW_NUMBER() OVER (PARTITION BY id_score ORDER BY ts_updated DESC) = 1
--- dags/conversational_xp/langfuse/queries/clean/traces.sql
+++ dags/conversational_xp/langfuse/queries/clean/traces.sql
@@ -27,7 +27,6 @@ SELECT
 FROM
     datalake_langfuse_raw.traces
 WHERE
-    MAKE_TIMESTAMP(year, month, day, hour, 0, 0) BETWEEN TIMESTAMP('{load_start_date}') - INTERVAL 2 HOUR 
-    AND TIMESTAMP('{load_end_date}')
+    MAKE_TIMESTAMP(year, month, day, hour, 0, 0) >= TIMESTAMP('{load_start_date}')
 QUALIFY
     ROW_NUMBER() OVER (PARTITION BY id_trace ORDER BY ts_updated DESC) = 1
