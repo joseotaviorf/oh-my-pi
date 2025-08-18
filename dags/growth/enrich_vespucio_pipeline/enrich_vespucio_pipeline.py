a9d9407c146ded50ec515af9431bc160e5d05293
Gabriel Mota <63614154+gfmota@users.noreply.github.com>
2025-08-18 11:24:13 -0300

    Revert "chore: revert image step input (#19915)" (#19995)
    This reverts commit e7c5aa55fa880d66b4ba390acdabb6d22f968d8a.

--- dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
+++ dags/growth/enrich_vespucio_pipeline/enrich_vespucio_pipeline.py
@@ -436,7 +436,8 @@ images_and_relations_tasks = [
     create_task(
         entry_point="core_images_step",
         parameters=[
-            f"--input_merged_houses={Tables.merge_step_houses}",
+            f"--input_clustered_houses={Tables.cluster_step_houses}",
+            f"--input_extracted_houses={Tables.extract_step_houses}",
             f"--input_kodak_photo_invalid_source={Tables.kodak_photo_invalid_source}",
             f"--input_kodak_photo={Tables.kodak_photo}",
             "--overwrite_schema",
@@ -704,8 +705,9 @@ address_tasks[-1] >> cluster_task
 cluster_task >> source_predict_task
 source_predict_task >> merge_task
 
-extract_step_task >> predict_and_join_task[0]
-merge_task >> images_and_relations_tasks
+extract_step_task >> images_and_relations_tasks[0]
+cluster_task >> images_and_relations_tasks[0]
+merge_task >> images_and_relations_tasks[1]
 
 images_and_relations_tasks >> predict_and_join_task[0]
 chain(*predict_and_join_task)
