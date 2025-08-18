a085adf502e7b1f408c670f47c6f61f552f73aba
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-08-18 10:52:57 -0300

    [Incident Guild] Creating new incidents table (#19977)
    * add new columns
    
    * add incidents table
    
    * Update dependencies.yaml
    
    * Update jira_alert_actions.sql
    
    * Update enrich_opsgenie_declaration.yml
    
    * add alert ID
    
    * Update dags/governance/enrich_opsgenie/queries/enrich/alerts.sql
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
    
    * update cluster
    
    * add is_card_filled
    
    ---------
    
    Co-authored-by: Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>

--- dags/governance/enrich_opsgenie/queries/enrich/acordamentos.sql
+++ dags/governance/enrich_opsgenie/queries/enrich/acordamentos.sql
@@ -1,49 +1,40 @@
-WITH
-extracted_log AS (
-  SELECT
-    l.id_alert,
-    SUBSTRING_INDEX(SUBSTRING_INDEX(l.log, 'key: [', -1), ']', 1) AS id_issue_jira
-  FROM
-    datalake_jira_ops_clean.alert_log AS l
-  WHERE
-    l.log LIKE '%key: [DEI-%]'
-),
-alerts AS (
+WITH alerts AS (
   SELECT
     a.id_alert,
-    element_at(tags, 1) AS dag_name,
+    a.id_issue_jira,
+    a.dag_name,
     a.is_acknowledged,
-    a.ts_created,
-    LAG(element_at(a.tags, 1)) OVER (ORDER BY a.ts_created ASC) AS previous_dag
+    a.acknowledged_by,
+    a.has_call_notification_made,
+    a.has_email_notification_sent,
+    a.ts_created_local_tz,
+    LAG(a.dag_name) OVER (ORDER BY a.ts_created_local_tz ASC) AS previous_dag
   FROM
-    datalake_jira_ops_clean.alerts AS a
+    datalake_opsgenie.alerts AS a
 ),
 ordered_alerts AS (
   SELECT
     a.id_alert,
-    el.id_issue_jira,
+    a.id_issue_jira,
     a.dag_name,
-    ja.acknowledged_by,
+    a.acknowledged_by,
     a.is_acknowledged,
-    ja.is_call_notification_sent AS has_call_notification_made,
-    ja.is_email_notification_sent AS has_email_notification_sent,
-    a.ts_created,
+    a.has_call_notification_made,
+    a.has_email_notification_sent,
+    a.ts_created_local_tz,
     a.previous_dag,
-    SUM(CASE WHEN a.dag_name != a.previous_dag THEN 1 ELSE 0 END)
-    OVER (ORDER BY a.ts_created ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS dag_alert_group
+    SUM(
+      CASE 
+        WHEN a.dag_name != a.previous_dag THEN 1 
+        ELSE 0 
+      END
+    ) OVER (
+      ORDER BY a.ts_created_local_tz ASC 
+      ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+    ) AS dag_alert_group
   FROM
     alerts AS a
-  INNER JOIN
-    datalake_jira_ops_clean.alert_log AS l
-      ON a.id_alert = l.id_alert
-  LEFT JOIN
-    extracted_log AS el
-      ON a.id_alert = el.id_alert
-  LEFT JOIN
-    datalake_opsgenie.jira_alert_actions AS ja
-      ON a.id_alert = ja.id_alert
-  GROUP BY
-    ALL
+  GROUP BY ALL
 ),
 get_acordamentos AS (
   SELECT
@@ -54,20 +45,18 @@ get_acordamentos AS (
     MAX(oa.is_acknowledged) AS is_acknowledged,
     MAX(oa.has_email_notification_sent) AS has_email_notification_sent,
     MAX(oa.has_call_notification_made) AS has_call_notification_made,
-    MIN(FROM_UTC_TIMESTAMP(oa.ts_created, 'America/Sao_Paulo')) AS ts_first_alert,
-    MAX(FROM_UTC_TIMESTAMP(oa.ts_created, 'America/Sao_Paulo')) AS ts_last_alert
+    MIN(oa.ts_created_local_tz) AS ts_first_alert,
+    MAX(oa.ts_created_local_tz) AS ts_last_alert
   FROM
     ordered_alerts AS oa
   WHERE
     oa.dag_name IS NOT NULL
-      AND REGEXP_REPLACE(oa.dag_name, '\\s+', '') != ''
-  GROUP BY
-    ALL
+  GROUP BY ALL
 )
 SELECT
   id_incident,
   id_issue_jira,
-  REGEXP_REPLACE(dag_name, '^\\[', '') AS dag_name,
+  dag_name,
   acknowledged_by,
   is_acknowledged,
   has_email_notification_sent,
