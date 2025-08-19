04d2e67782b1bd9840951b6fa683ed42cd26d49f
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-08-19 15:45:00 -0300

    [CRMG-2210] Adjusting Visit Core Model logic (#19983)
    * Creating Core layer
    
    * adjusting test of layerEnum
    
    * changing enrich_core_visit to use core concept
    
    * tests
    
    * dependencies fix
    
    * fix code
    
    * db pattern regex
    
    * fix regex
    
    * creating factory for core
    
    * add core factory ref
    
    * comments
    
    * creating core_query_delta_workflow
    
    * missing import
    
    * replacing for core workflow
    
    * replacing for core workflow
    
    * lint
    
    * fixing folder path
    
    * dependencies
    
    * unstandard dags
    
    * metadata_file_service updates
    
    * table storage format
    
    * moving to new path dedicated
    
    * spark table storage
    
    * renaming path
    
    * out of pattern and deps
    
    * undo
    
    * adjusting generate_metadata file
    
    * validate_dags_dependencies and metadatafile_service files
    
    * test_table_storage_descriptor_enum
    
    * renaming dag to core_visit
    
    * fixing dependencies and tests
    
    * removing dag from out of pattern
    
    * adding more logic in core model query
    
    * more columns
    
    * manual modifications
    
    * enrich_transactional_entitieS
    
    * removing
    
    * descriptions
    
    * description
    
    * data quality tests
    
    * more descriptions
    
    * removing extra comma
    
    * z order fix

--- dags/core/core_visit/queries/core/visit.sql
+++ dags/core/core_visit/queries/core/visit.sql
@@ -1,4 +1,4 @@
-WITH vsl AS (
+WITH vsl_last_event AS (
     SELECT
         id_visit,
         event_type AS last_event_type
@@ -6,23 +6,53 @@ WITH vsl AS (
         datalake_ebdb_test_clean.visit_status_log
     QUALIFY
         ROW_NUMBER() OVER (PARTITION BY id_visit ORDER BY ts_created DESC) = 1
+),
+all_events AS (
+    SELECT
+        id_visit,
+        MAX(CASE WHEN event_type IN ('VISIT_REQUEST_CANCELED', 'VISIT_CANCELED') THEN reason END) AS cancellation_reason,
+        MAX(CASE WHEN event_type = 'VISIT_REQUESTED' THEN ts_created END) AS ts_visit_requested,
+        MAX(CASE WHEN event_type = 'VISIT_CONFIRMED' THEN ts_created END) AS ts_visit_confirmed,
+        MAX(CASE WHEN event_type = 'VISIT_DONE' THEN ts_created END) AS ts_visit_done,
+        MAX(CASE WHEN event_type IN ('VISIT_REQUEST_CANCELED', 'VISIT_CANCELED') THEN ts_created END) AS ts_visit_canceled,
+        MAX(CASE WHEN event_type = 'VISIT_UNSUCCESSFUL' THEN ts_created END) AS ts_visit_unsuccessful
+    FROM
+        datalake_ebdb_test_clean.visit_status_log
+    GROUP BY 1
 )
 SELECT
-    v.id,
+    SHA2(CONCAT_WS("||", 'VISIT', v.id), 256) AS sk_core_visit,
+    v.id AS id_visit,
     v.id_house,
     v.id_visitor,
+    h.id_user AS id_owner,
     v.id_agent,
     v.code,
     v.status,
     v.computed_status,
+    v.type,
     v.business_context,
     vsl.last_event_type,
+    ae.cancellation_reason,
+    v.is_fixed_agent,
     v.dt_visit,
-    v.dt_request,
+    v.ts_visit,
+    ae.ts_visit_requested,
+    ae.ts_visit_confirmed,
+    ae.ts_visit_done,
+    ae.ts_visit_canceled,
+    ae.ts_visit_unsuccessful,
     v.ts_created,
-    v.ts_updated
+    v.ts_updated,
+    NOW() AS ts_load
 FROM
     datalake_ebdb_test_clean.visit AS v
 LEFT JOIN
-    vsl
+    vsl_last_event AS vsl
         ON vsl.id_visit = v.id
+LEFT JOIN
+    all_events AS ae
+        ON ae.id_visit = v.id
+LEFT JOIN
+    datalake_ebdb_clean.house AS h
+        ON h.id = v.id_house
