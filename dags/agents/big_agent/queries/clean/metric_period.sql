3fa5be35350346a2b445822b64dcdce57d90a303
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-08-19 15:00:08 -0300

    [AGENTS] Add metric_period table to BigAgent (#20014)

---
+++ dags/agents/big_agent/queries/clean/metric_period.sql
@@ -0,0 +1,14 @@
+SELECT
+    id, 
+    metric, 
+    invalidated_by, 
+    status, 
+    DATE(init_date) AS dt_init,
+    DATE(end_date) AS dt_end,
+    TIMESTAMP(created_at) AS ts_created,
+    TIMESTAMP(updated_at) AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_big_agent_raw.metric_period
