4d207024aff1be7c5dc6801324a11ab78c649029
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2025-08-20 11:20:08 -0300

    [Incident Guild] Creating new incidents table (#20001)
    * Update jira_alert_actions.sql
    
    * Update jira_alert_actions.sql
    
    * Update jira_alert_actions.sql
    
    * revert
    
    * add merge_on
    
    * Update alerts.sql
    
    * Update jira_alert_actions.sql
    
    * fix
    
    * Update alerts.sql

---
+++ dags/governance/enrich_opsgenie/queries/enrich/incidents.sql
@@ -0,0 +1,60 @@
+SELECT
+  ji.id_issue AS id_issue_jira,
+  oa.id_alert,
+  ji.summary AS dag_name,
+  pl.line_name AS dag_owner,
+  ji.incident_owner,
+  pl.layer,
+  ji.current_status,
+  ji.incident_category,
+  ji.root_cause_resolution AS incident_root_cause_resolution,
+  ji.incident_status AS oncall_incident_status,
+  ji.issue_description,
+  ji.assignee AS user_assigned,
+  SPLIT(oa.acknowledged_by, '@')[0] AS user_acknowledged,
+  (ji.assignee IS NOT NULL) AS is_assigned,
+  oa.is_acknowledged,
+  ji.root_cause_resolution = 'Not an incident' AS is_not_an_incident,
+  (ji.ts_resolved IS NULL) AS is_open,
+  (ji.ts_resolved IS NOT NULL) AS is_resolved,
+  CASE
+    WHEN ji.incident_owner <> 'AE All'
+      AND ji.incident_owner IS NOT NULL
+      AND ji.incident_status <> 'Under investigation' -- on call status
+      AND ji.assignee IS NOT NULL
+      AND ji.incident_category IS NOT NULL
+      AND ji.root_cause_resolution <> 'Discovery'
+      THEN TRUE
+    ELSE FALSE
+  END AS is_card_filled,
+  oa.has_notification_sent,
+  oa.has_call_notification_made AS has_call_notification_sent,
+  oa.has_email_notification_sent,
+  ji.root_cause_resolution IS NOT NULL AS has_root_cause_filled,
+  ji.incident_category IS NOT NULL AS has_incident_category_filled,
+  ji.is_deleted,
+  ji.is_deleted IS True AND DATE(ji.ts_created) >= DATE('2025-06-01') AS is_deleted_duplicate,
+  ji.dt_deleted,
+  ji.dt_started,
+  ji.ts_created,
+  ji.ts_resolved,
+  TIMESTAMP(ji.ts_updated) AS ts_updated,
+  NOW() AS ts_load,
+  aux.quarter,
+  aux.year,
+  aux.month,
+  aux.day
+FROM
+  datalake_jira.issues AS ji
+LEFT JOIN
+  datalake_opsgenie.alerts AS oa
+    ON ji.id_issue = oa.id_issue_jira
+LEFT JOIN
+  datalake_pipeline.dag AS pl
+    ON ji.summary = pl.id_dag
+LEFT JOIN
+  datalake_quintoandar.aux_date AS aux
+    ON DATE(ji.ts_created) = aux.date
+WHERE
+  ji.id_project = '11446'
+  AND GREATEST(DATE(ji.ts_updated), ji.dt_deleted, oa.ts_updated_local_tz) BETWEEN DATE("{load_start_date}") AND DATE("{load_end_date}")
--- dags/governance/enrich_opsgenie/queries/enrich/jira_alert_actions.sql
+++ dags/governance/enrich_opsgenie/queries/enrich/jira_alert_actions.sql
@@ -1,48 +1,47 @@
-WITH get_alert_actions AS (
+WITH alerts_updated AS (
+  SELECT
+    al.id_alert,
+    al.dt_load,
+    al.year,
+    al.month,
+    al.day
+  FROM
+    datalake_jira_ops_clean.alert_log AS al
+  WHERE
+    MAKE_DATE(al.year, al.month, al.day) BETWEEN DATE("{load_start_date}") AND DATE("{load_end_date}")
+),
+alert_log AS (
+  SELECT
+    al.id_alert,
+    al.log_type,
+    al.log,
+    al.owner,
+    al.ts_log,
+    au.dt_load,
+    au.year,
+    au.month,
+    au.day
+  FROM
+    datalake_jira_ops_clean.alert_log AS al
+  JOIN
+    alerts_updated AS au
+      ON al.id_alert = au.id_alert
+),
+get_alert_actions AS (
   SELECT
     id_alert,
     log_type,
     owner,
     TRIM(SPLIT(SPLIT(log, 'via') [0], 'Alert ') [1]) AS action,
     TRIM(
-      SPLIT(SPLIT(SPLIT(log, 'via') [1], '\\[') [0], '\\.') [0]
+      SPLIT(SPLIT(SPLIT(log, 'via') [1], '\\\[') [0], '\\\.') [0]
     ) AS direction,
     ts_log
   FROM
-    datalake_jira_ops_clean.alert_log
+    alert_log
   WHERE
     log LIKE "Alert%"
-  GROUP BY
-    ALL
-),
-get_alert_notification AS (
-  SELECT
-    id_alert,
-    log_type,
-    owner AS log_owner,
-    log,
-    REGEXP_EXTRACT(log, 'Rule\\[([^\\]]+)\\]', 1) AS rule_name,
-    REGEXP_EXTRACT(log, 'Rule\\[[^\\]]+\\]\\[([^\\]]+)\\]', 1) AS rule_action,
-    REGEXP_EXTRACT(
-      log,
-      'Rule\\[[^\\]]+\\]\\[[^\\]]+\\]\\[([^\\]]+)\\]',
-      1
-    ) AS rule_step,
-    CASE
-      WHEN log RLIKE '->\\s*Sent' THEN 'Sent'
-      WHEN log RLIKE '->\\s*Received' THEN 'Received'
-      WHEN log RLIKE '->\\s*An aggregated' THEN 'Aggregated'
-      WHEN log RLIKE '->\\s*\\[.*\\]\\s*notification.*submitted' THEN 'Submitted'
-      ELSE NULL
-    END AS direction,
-    REGEXP_EXTRACT(log, '\\[?(email|sms|voice)\\]?', 1) AS notification_type,
-    ts_log
-  FROM
-    datalake_jira_ops_clean.alert_log
-  WHERE
-    log LIKE "%notification%"
-  GROUP BY
-    ALL
+  GROUP BY ALL
 ),
 filter_alert_actions AS (
   SELECT
@@ -52,26 +51,141 @@ filter_alert_actions AS (
     MIN(ts_log) FILTER(WHERE action = 'acknowledged') AS ts_acknowledged,
     MIN(ts_log) FILTER(WHERE action = 'closed') AS ts_closed
   FROM
-    get_alert_actions
+    get_alert_actions AS gaa
   WHERE
     action IN ('acknowledged', 'closed')
   GROUP BY ALL
+),
+get_alert_notification AS (
+  SELECT
+    id_alert,
+    log_type,
+    owner AS log_owner,
+    log,
+    REGEXP_EXTRACT(log, 'Rule\\\[([^\\\]]+)\\\]', 1) AS rule_name,
+    REGEXP_EXTRACT(log, 'Rule\\\[[^\\\]]+\\\]\\\[([^\\\]]+)\\\]', 1) AS rule_action,
+    REGEXP_EXTRACT(
+      log,
+      'Rule\\\[[^\\\]]+\\\]\\\[[^\\\]]+\\\]\\\[([^\\\]]+)\\\]',
+      1
+    ) AS rule_step,
+    CASE
+      WHEN log RLIKE '->\\\s*Sent' THEN 'Sent'
+      WHEN log RLIKE '->\\\s*Received' THEN 'Received'
+      WHEN log RLIKE '->\\\s*An aggregated' THEN 'Aggregated'
+      WHEN log RLIKE '->\\\s*\\\[.*\\\]\\\s*notification.*submitted' THEN 'Submitted'
+      ELSE NULL
+    END AS direction,
+    REGEXP_EXTRACT(log, '\\\[?(email|sms|voice)\\\]?', 1) AS notification_type,
+    ts_log
+  FROM
+    alert_log
+  WHERE
+    log LIKE "%notification%"
+  GROUP BY ALL
+),
+get_alert_out_of_rotation AS (
+  SELECT
+    id_alert,
+    TRUE AS is_out_of_rotation,
+    MIN(ts_log) FILTER(WHERE REGEXP_LIKE(log, '(?i)^Will close alert automatically.*Auto-Close out-of-rotation time') IS TRUE) AS ts_out_of_rotation_identified,
+    MIN(ts_log) FILTER(WHERE REGEXP_LIKE(log, '(?i)^Alert closed via system.*Auto-Close out-of-rotation time') IS TRUE) AS ts_out_of_rotation_closed
+  FROM
+    alert_log
+  WHERE
+    log LIKE '%[Auto-Close out-of-rotation time]%'    
+  GROUP BY ALL
+),
+get_alert_dei_automation AS (
+  SELECT
+    l.id_alert,
+    IF(
+      REGEXP_EXTRACT(l.log, 'ExecutionStatus\\\[([^\\\]]+)\\\]', 1) = 'succeed',
+      SUBSTRING_INDEX(SUBSTRING_INDEX(l.log, 'key: [', -1), ']', 1),
+      NULL
+    ) AS id_issue_jira,
+    NULLIF(REGEXP_EXTRACT(l.log, 'ExecutionStatus\\\[([^\\\]]+)\\\]', 1), '') AS card_creation_status,
+    REGEXP_EXTRACT(l.log, 'ExecutionStatus\\\[([^\\\]]+)\\\]', 1) = "failed" AS is_card_creation_failed
+  FROM
+    alert_log AS l
+  WHERE
+    REGEXP_LIKE(l.log, 'AE - DEI Automation')
+),
+get_alert_dei_automation_exception_1 AS (
+  SELECT
+    l.id_alert,
+    NULLIF(REGEXP_EXTRACT(l.log, '\\\{{[^=]+=(DEI-[0-9]+)\\\}}', 1), '') AS id_issue_jira
+  FROM
+    alert_log AS l
+  WHERE
+    REGEXP_LIKE(l.log, '\\\{{issueKey')
+),
+get_alert_dei_automation_exception_2 AS (
+  SELECT
+    l.id_alert,
+    NULLIF(SUBSTRING_INDEX(SUBSTRING_INDEX(l.log, 'key: [', -1), ']', 1), '') AS id_issue_jira
+  FROM
+    alert_log AS l
+  WHERE
+    l.log LIKE '%key: [DEI-%]'
+),
+filter_dei_automation AS (
+  SELECT
+    gad.id_alert,
+    CASE 
+      WHEN gad.id_issue_jira RLIKE '^DEI-[0-9]+$' THEN gad.id_issue_jira
+      ELSE COALESCE(exception_1.id_issue_jira, exception_2.id_issue_jira)
+    END AS id_issue_jira,
+    gad.card_creation_status,
+    gad.is_card_creation_failed
+  FROM
+    get_alert_dei_automation AS gad
+  LEFT JOIN
+    get_alert_dei_automation_exception_1 AS exception_1
+      ON exception_1.id_alert = gad.id_alert
+  LEFT JOIN
+    get_alert_dei_automation_exception_2 AS exception_2
+      ON exception_2.id_alert = gad.id_alert
+  QUALIFY
+    1 = ROW_NUMBER() OVER (
+        PARTITION BY gad.id_alert
+        ORDER BY CAST(gad.is_card_creation_failed AS SMALLINT)
+    )
 )
 SELECT
-  aa.id_alert,
+  a.id_alert,
+  fda.id_issue_jira,
   aa.closed_by,
   aa.acknowledged_by,
+  fda.card_creation_status,
   MIN(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'voice') IS NOT NULL AS is_call_notification_sent,
   MIN(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'email') IS NOT NULL AS is_email_notification_sent,
+  COALESCE(aor.is_out_of_rotation, FALSE) AS is_out_of_rotation,
+  COALESCE(fda.is_card_creation_failed, FALSE) AS is_card_creation_failed,
   aa.ts_acknowledged,
   aa.ts_closed,
+  aor.ts_out_of_rotation_identified,
+  aor.ts_out_of_rotation_closed,
   MIN(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'voice') AS ts_first_call_notification_sent,
   MAX(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'voice') AS ts_last_call_notification_sent,
   MIN(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'email') AS ts_first_email_notification_sent,
-  MAX(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'email') AS ts_last_email_notification_sent
+  MAX(an.ts_log) FILTER(WHERE an.direction = 'Sent' AND an.notification_type = 'email') AS ts_last_email_notification_sent,
+  a.dt_load AS dt_updated,
+  a.year,
+  a.month,
+  a.day
 FROM
+  alert_log AS a
+LEFT JOIN
   filter_alert_actions AS aa
+    ON a.id_alert = aa.id_alert
 LEFT JOIN
   get_alert_notification AS an
     ON aa.id_alert = an.id_alert
+LEFT JOIN
+  get_alert_out_of_rotation AS aor
+    ON a.id_alert = aor.id_alert
+LEFT JOIN
+  filter_dei_automation AS fda
+    ON a.id_alert = fda.id_alert
 GROUP BY ALL
