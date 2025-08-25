2a3edb02ee0d2670e43fa4293cae8eb9fb687248
aravinii <69015398+aravinii@users.noreply.github.com>
2025-08-25 10:59:14 -0300

    [GDE-69] Including ctwaClid and extra params to lead_origin (#20061)
    * adding click to whatsapp params in the lead origin
    
    * adding UTM fields
    
    * last details
    
    * Update dependencies.yaml
    
    * Apply suggestion from @Andre-L-Melo
    
    * Apply suggestion from @Andre-L-Melo
    
    * Apply suggestion from @Andre-L-Melo
    
    * Apply suggestion from @Andre-L-Melo
    
    * Apply suggestion from @Andre-L-Melo
    
    * Apply suggestion from @Andre-L-Melo
    
    ---------
    
    Co-authored-by: henriquedepaulo <henrique.paulo@quintoandar.com.br>
    Co-authored-by: André Melo <47090012+Andre-L-Melo@users.noreply.github.com>

--- dags/growth/enrich_supply_leads/queries/enrich/lead_origin.sql
+++ dags/growth/enrich_supply_leads/queries/enrich/lead_origin.sql
@@ -23,6 +23,7 @@ WITH lead_origin_rene_descartes AS (
     CAST(GET_JSON_OBJECT(amd.acquisition_campaign, '$.taskId') AS STRING) AS id_task,
     CAST(GET_JSON_OBJECT(amd.acquisition_campaign, '$.gclid') AS STRING) AS gclid,
     CAST(GET_JSON_OBJECT(amd.acquisition_campaign, '$.fbclid') AS STRING) AS fbclid,
+    CAST(GET_JSON_OBJECT(amd.acquisition_campaign, '$.ctwaClid') AS STRING) AS ctwa_clid,
     amd.ts_created AS ts_event
   FROM
     datalake_rene_descartes_clean.house_lead AS hl
@@ -65,11 +66,13 @@ lead_origin_amplitude AS (
     DATE(ts_event) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
   QUALIFY ROW_NUMBER() OVER(PARTITION BY COALESCE(formfield_lead_uuid, id_firestore, id_lead) ORDER BY ts_event) = 1 -- Works if formfield_lead_uuid is a NULL
 ),
-
 phone_number AS (
   SELECT DISTINCT
     id_task,
-    twilio_phone_number as quinto_andar_phone_number
+    twilio_phone_number as quinto_andar_phone_number,
+    id_source_ctwa,
+    url_source_ctwa,
+    type_source_ctwa
   FROM
     datalake_customer_support.chats
   UNION ALL
@@ -78,20 +81,36 @@ phone_number AS (
     CASE
       WHEN direction = 'inbound' THEN to_phone_number
       WHEN direction = 'outbound' THEN from_phone_number
-    END AS quinto_andar_phone_number
+    END AS quinto_andar_phone_number,
+    NULL AS id_source_ctwa,
+    NULL AS url_source_ctwa,
+    NULL AS type_source_ctwa
   FROM
     datalake_customer_support.calls
+),
+click_to_whatsapp_campaigns AS (
+  SELECT 
+    id_ad,
+    utm_campaign,
+    utm_term,
+    utm_content,
+    origin AS utm_source,
+    'whatsapp' AS utm_medium
+  FROM 
+    datalake_growth_media_platform.facebook_metrics
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY id_ad ORDER BY dt_cost DESC) = 1
 ),
 mid_table AS (
   SELECT
     r.id_lead AS id_lead,
     r.id_lead_ebdb AS id_lead_ebdb,
     COALESCE(r.affiliate_type, atf.first_affiliate_type) AS affiliate_type,
-    COALESCE(IF(r.campaign == '', NULL, r.campaign), a.campaign, a2.campaign) AS campaign,
-    COALESCE(IF(r.medium == '', NULL, r.medium), a.medium, a2.medium) AS medium,
-    COALESCE(IF(r.source == '', NULL, r.source), a.source, a2.source) AS source,
-    COALESCE(IF(r.content == '', NULL, r.content), a.content, a2.content) AS content,
-    COALESCE(IF(r.term == '', NULL, r.term), a.term, a2.term) AS term,
+    COALESCE(IF(r.campaign == '', NULL, r.campaign), a.campaign, a2.campaign, ctwac.utm_campaign) AS campaign,
+    COALESCE(IF(r.medium == '', NULL, r.medium), a.medium, a2.medium, ctwac.utm_medium) AS medium,
+    COALESCE(IF(r.source == '', NULL, r.source), a.source, a2.source, ctwac.utm_source) AS source,
+    COALESCE(IF(r.content == '', NULL, r.content), a.content, a2.content, ctwac.utm_content) AS content,
+    COALESCE(IF(r.term == '', NULL, r.term), a.term, a2.term, ctwac.utm_term) AS term,
     r.ops_agent,
     r.ops_partner,
     r.application,
@@ -106,13 +125,17 @@ mid_table AS (
     r.id_task,
     r.gclid,
     r.fbclid,
+    r.ctwa_clid,
     pn.quinto_andar_phone_number,
+    pn.id_source_ctwa,
+    pn.url_source_ctwa,
+    pn.type_source_ctwa,
     -- We're tracking how database is the source of our UTMs
-    NVL2(r.campaign, 'rene_descartes', NVL2(a.campaign, 'amplitude', NVL2(a2.campaign, 'amplitude', 'lost_tracking'))) AS database_tracking_campaign,
-    NVL2(r.medium, 'rene_descartes', NVL2(a.medium, 'amplitude', NVL2(a2.medium, 'amplitude', 'lost_tracking'))) AS database_tracking_medium,
-    NVL2(r.source, 'rene_descartes', NVL2(a.source, 'amplitude', NVL2(a2.source, 'amplitude', 'lost_tracking'))) AS database_tracking_source,
-    NVL2(r.content, 'rene_descartes', NVL2(a.content, 'amplitude', NVL2(a2.content, 'amplitude', 'lost_tracking'))) AS database_tracking_content,
-    NVL2(r.term, 'rene_descartes', NVL2(a.term, 'amplitude', NVL2(a2.term, 'amplitude', 'lost_tracking'))) AS database_tracking_term,
+    NVL2(r.campaign, 'rene_descartes', NVL2(a.campaign, 'amplitude', NVL2(a2.campaign, 'amplitude', NVL2(ctwac.utm_campaign, 'facebook_api', 'lost_tracking')))) AS database_tracking_campaign,
+    NVL2(r.medium, 'rene_descartes', NVL2(a.medium, 'amplitude', NVL2(a2.medium, 'amplitude', NVL2(ctwac.utm_medium, 'facebook_api', 'lost_tracking')))) AS database_tracking_medium,
+    NVL2(r.source, 'rene_descartes', NVL2(a.source, 'amplitude', NVL2(a2.source, 'amplitude', NVL2(ctwac.utm_source, 'facebook_api', 'lost_tracking')))) AS database_tracking_source,
+    NVL2(r.content, 'rene_descartes', NVL2(a.content, 'amplitude', NVL2(a2.content, 'amplitude', NVL2(ctwac.utm_content, 'facebook_api', 'lost_tracking')))) AS database_tracking_content,
+    NVL2(r.term, 'rene_descartes', NVL2(a.term, 'amplitude', NVL2(a2.term, 'amplitude', NVL2(ctwac.utm_term, 'facebook_api', 'lost_tracking')))) AS database_tracking_term,
     COALESCE(r.ts_event, a.ts_event) AS ts_event
   FROM
     lead_origin_rene_descartes AS r
@@ -128,6 +151,9 @@ mid_table AS (
   LEFT JOIN
     phone_number AS pn
       ON (pn.id_task = r.id_task)
+  LEFT JOIN
+    click_to_whatsapp_campaigns AS ctwac
+      ON ctwac.id_ad = pn.id_source_ctwa
 )
 -- Hard rules
 SELECT DISTINCT
@@ -158,8 +184,12 @@ SELECT DISTINCT
   SF_NORMALIZE_STRING(detailed_route) AS detailed_route,
   gclid,
   fbclid,
+  ctwa_clid,
   id_task,
   REGEXP_EXTRACT(quinto_andar_phone_number, '[0-9]+', 0) AS quinto_andar_phone_number,
+  id_source_ctwa,
+  url_source_ctwa,
+  type_source_ctwa,
   original_lead,
   database_tracking_campaign,
   database_tracking_medium,
