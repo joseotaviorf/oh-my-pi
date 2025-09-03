a32fdc8cdfc735de166d7611a5eebcb9a3616063
Caio Francisco <42449297+caiofgf@users.noreply.github.com>
2025-09-03 14:42:26 -0300

    feat(SEOGB-1719): enrich gsc classifieds data with IBGE cities (#20114)
    * feat: enrich gsc classifieds data with IBGE cities
    
    * fix: fix table name
    
    * fix: fix query
    
    * fix: fix param name
    
    * fix: fix query
    
    * fix: fix table reference
    
    * fix: fix field name
    
    * fix: fix field name
    
    * chore: reorder columns
    
    * Update dags/growth/enrich_google_search_console_classified/queries/enrich/gsc_keyword_region_attributes.sql
    
    * Update dags/growth/enrich_google_search_console_classified/queries/enrich/gsc_keyword_region_attributes.sql
    
    * Update dags/growth/enrich_google_search_console_classified/metadata/enrich/gsc_keywords.yml
    
    * Update dags/growth/enrich_google_search_console_classified/queries/enrich/gsc_keyword_region_attributes.sql
    
    * Update dags/growth/enrich_google_search_console_classified/metadata/enrich/gsc_keyword_region_attributes.yml
    
    * Update dags/growth/enrich_google_search_console_classified/metadata/enrich/gsc_keyword_region_attributes.yml
    
    * fix: add date limiter condition
    
    ---------
    
    Co-authored-by: André Melo <47090012+Andre-L-Melo@users.noreply.github.com>

---
+++ dags/growth/enrich_google_search_console_classified/queries/enrich/gsc_keyword_region_attributes.sql
@@ -0,0 +1,92 @@
+WITH cities AS (
+  SELECT 
+    DISTINCT LOWER(municipio_clean) AS city_name
+  FROM
+    datalake_seo_keywords_clusters.ibge_cities
+  WHERE
+    municipio_clean IS NOT NULL
+    AND municipio_clean != ' '
+),
+general_city_level_enrichment AS (
+  SELECT
+    keyword,
+    keyword_clean,
+    CASE
+      WHEN CONTAINS(c.city_name, ' ') THEN city_name
+      WHEN ARRAY_CONTAINS(SPLIT(keyword_clean, ' '), LOWER(c.city_name)) THEN city_name
+      WHEN CONTAINS(keyword_clean, 'sao paulo') THEN COALESCE(c.city_name, 'sao paulo')
+      WHEN RLIKE(keyword_clean, r'.*(\bsp\b).*') THEN COALESCE(c.city_name, 'sao paulo')
+      WHEN RLIKE(keyword_clean, r'.*(\brj\b).*') THEN COALESCE(c.city_name, 'rio de janeiro')
+      WHEN CONTAINS(keyword_clean, ' bh') THEN COALESCE(c.city_name, 'belo horizonte')
+      WHEN CONTAINS(keyword_clean, ' sjc') THEN COALESCE(c.city_name, 'sao jose dos campos')
+      WHEN CONTAINS(keyword_clean, ' santo andre') THEN COALESCE(c.city_name, 'santo andre')
+      WHEN CONTAINS(keyword_clean, ' sao caetano') THEN COALESCE(c.city_name, 'sao caetano do sul')
+      WHEN CONTAINS(keyword_clean, ' goiania') THEN COALESCE(c.city_name, 'goiania')
+      WHEN CONTAINS(keyword_clean, ' nova iguacu') THEN COALESCE(c.city_name, 'nova iguacu')
+      WHEN CONTAINS(keyword_clean, ' sao goncalo') THEN COALESCE(c.city_name, 'sao goncalo')
+      WHEN CONTAINS(keyword_clean, ' carapicuiba') THEN COALESCE(c.city_name, 'carapicuiba')
+      WHEN CONTAINS(keyword_clean, ' sbc') THEN COALESCE(c.city_name, 'sao bernardo do campo')
+      WHEN CONTAINS(keyword_clean, ' sbo') THEN COALESCE(c.city_name, 'santa barbara do oeste')
+      WHEN CONTAINS(keyword_clean, ' vcp') THEN COALESCE(c.city_name, 'campinas')
+      WHEN CONTAINS(keyword_clean, ' vix') THEN COALESCE(c.city_name, 'vitória')
+      WHEN CONTAINS(keyword_clean, ' gru') THEN COALESCE(c.city_name, 'guarulhos')
+      WHEN CONTAINS(keyword_clean, ' sao bernardo do campo') THEN COALESCE(c.city_name, 'sao bernardo do campo')
+      ELSE COALESCE(c.city_name, '')
+    END AS match_ibge_city,
+    page,
+    structure,
+    country,
+    position,
+    impressions,
+    clicks,
+    ctr,
+    site_url,
+    posimp,
+    device,
+    domain,
+    is_branded,
+    dt_created,
+    year,
+    month,
+    day
+  FROM
+    datalake_google_search_console_classified.gsc_keywords AS kfp
+  LEFT JOIN 
+    cities AS c
+      ON kfp.country = "bra"
+      AND kfp.domain IN ("imovelweb", "wimoveis")
+      AND CHARINDEX(LOWER(c.city_name), LOWER(kfp.keyword_clean)) > 0
+  WHERE
+    kfp.dt_created BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  QUALIFY ROW_NUMBER() OVER(
+    PARTITION BY dt_created, keyword_clean, page, device
+    ORDER BY LENGTH(match_ibge_city) DESC, CHARINDEX(match_ibge_city, keyword_clean)
+    DESC
+  ) = 1
+)
+SELECT 
+  keyword,
+  keyword_clean,
+  match_ibge_city,
+  page,
+  structure,
+  site_url,
+  device,
+  domain,
+  country,
+  position,
+  impressions,
+  clicks,
+  ctr,
+  posimp,
+  is_branded,
+  CASE
+    WHEN match_ibge_city != '' THEN 1
+    ELSE 0
+  END AS has_mention_to_city,
+  dt_created,
+  year,
+  month,
+  day
+FROM
+  general_city_level_enrichment
---
+++ dags/growth/enrich_google_search_console_classified/queries/enrich/gsc_keywords.sql
@@ -0,0 +1,57 @@
+WITH base_gsc AS (
+  SELECT
+    query as keyword,
+    site_url,
+    device,
+    page,
+    country,
+    ctr,
+    clicks,
+    position,
+    impressions,
+    posimp,
+    domain,
+    struct AS structure,
+    is_branded,
+    dt_created,
+    year,
+    month,
+    day
+  FROM
+    datalake_google_search_console_classified.seo_gsc_data
+  WHERE
+    DATE(dt_created) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+)
+SELECT
+  keyword,
+  -- Replaces the UDF SF_SET_ALPHANUMERIC_LOWER
+  LOWER(
+    REGEXP_REPLACE(
+      TRANSLATE(
+        keyword,
+        'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇ',
+        'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC'
+      ),
+      '[^a-zA-Z0-9 ]',
+      ''
+    )
+  ) AS keyword_clean,
+  page,
+  structure,
+  site_url,
+  device,
+  domain,
+  country,
+  ctr,
+  clicks,
+  position,
+  impressions,
+  posimp,
+  is_branded,
+  MAKE_DATE(YEAR(dt_created), MONTH(dt_created), 01) AS dt_created,
+  year,
+  month,
+  day
+FROM
+  base_gsc
+GROUP BY ALL
