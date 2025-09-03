a90ec1c7ed283b36709d4f89eff35031cc302b03
Gian Carvalho <81176900+giancarvalho@users.noreply.github.com>
2025-09-03 15:22:15 -0300

    feat(SPY-464): add house reprocessing dag (#19935)
    * feat: add house reprocessing dag
    
    * chore: add dag to out of pattern list
    
    * chore: adapt to pattern
    
    * chore: regenerated dependency file
    
    * fix: query access
    
    * chore: add test commit
    
    * chore: deleted state
    
    * chore: delete temporarly region
    
    * fix: syntax issue
    
    * chore: change table name
    
    * chore: add missing fields
    
    * fix: sale price
    
    * chore: style
    
    * chore: revert deleted file
    
    * chore: modify conn id

---
+++ dags/growth/reverse_house_portfolio_reprocessing/queries/reverse/ebdb_houses_formatted.sql
@@ -0,0 +1,57 @@
+-- Query to extract formatted house data from EBDB for portfolio reprocessing
+SELECT DISTINCT
+    i.id AS property_id,
+    i.dataCriacao AS created_at,
+    'house' AS event_type,
+    TO_JSON(NAMED_STRUCT(
+        'zipcode', imovel.cep,
+        'state', estado.nome,
+        'city', imovel.cidade,
+        'neighbourhood', imovel.bairro,
+        'street', imovel.endereco,
+        'number', imovel.numero,
+        'complement', imovel.complemento,
+        'region', regiao.nome,
+        'lat', imovel.lat,
+        'lng', imovel.lng,
+        'regionSlug', regiao.slug
+    )) AS address,
+    TO_JSON(NAMED_STRUCT(
+        'ownerId', person.id,
+        'personUUId', person.uuid_person,
+        'name', person.person_name,
+        'phoneNumber', contact_info.contact_info
+    )) AS owner,
+    TO_JSON(NAMED_STRUCT(
+        'area', imovel.areaTotal,
+        'iptu', imovel.iptu,
+        'forRent', imovel.forRent,
+        'forSale', imovel.forSale,
+        'rentValue', imovel.aluguel,
+        'saleValue', imovel.salePrice,
+        'condominium', imovel.condominio,
+        'numberRooms', imovel.numeroQuartos,
+        'numberSuites', imovel.numeroSuites,
+        'numberBathrooms', imovel.numeroBanheiros,
+        'houseType', imovel.tipo
+    )) AS property_info
+FROM
+    datalake_ebdb_raw.imovel AS imovel
+LEFT JOIN
+    datalake_ebdb_raw.estado AS estado
+        ON imovel.estado_id = estado.id
+LEFT JOIN
+    datalake_ebdb_raw.regiao AS regiao
+        ON imovel.regiao_id = regiao.id
+LEFT JOIN
+    datalake_person_clean.person AS person
+        ON imovel.usuario_id = person.id
+LEFT JOIN
+    datalake_person_clean.contact_info AS contact_info
+        ON person.id = contact_info.id_person
+WHERE
+    imovel.id IS NOT NULL
+    AND (
+        imovel.endereco IS NOT NULL
+        OR (imovel.lat IS NOT NULL AND imovel.lng IS NOT NULL)
+    )
