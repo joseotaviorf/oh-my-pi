609e4a66807900d82ecf73b59872ec4a3f1ea2e7
André Melo <47090012+Andre-L-Melo@users.noreply.github.com>
2025-09-08 11:31:18 -0300

    feat(ADB-728): Create Table Zap Imoveis Latest (#20279)
    * ADB-728: create sql file with incremental reading
    
    * ADB-728: add zap_imoveis_latest to the declaration file
    
    * ADB-728: add inner dependencies to the declaration file
    
    * ADB-728: format declaration file and update dag documentation
    
    * ADB-728: update cluster spark_version to 16.4x-scala2.12
    
    * ADB-728: create zap_imoveis_latest metadata file
    
    * ADB-728: update the zap_imoveis table documentation
    
    * ADB-728: segment extraction_type for each table
    
    * ADB-728: fix zap_imoveis_latest metadata file

---
+++ dags/cross/enrich_crawler_zap_imoveis/queries/enrich/zap_imoveis_latest.sql
@@ -0,0 +1,42 @@
+SELECT
+    id_house_platform,
+    id_house,
+    platform,
+    country,
+    state,
+    address_city,
+    neighborhood,
+    street,
+    street_number,
+    zip_code,
+    latitude,
+    longitude,
+    listing_name,
+    total_area,
+    amenities,
+    bathrooms,
+    floors,
+    parking_spaces,
+    suites,
+    unit_type,
+    usage_type,
+    advertiser_name,
+    advertiser_phone,
+    condo_fee,
+    iptu,
+    price_rental,
+    price_sale,
+    price_m2_rental,
+    price_m2_sale,
+    is_for_rent,
+    is_for_sale,
+    is_hybrid,
+    ts_created::TIMESTAMP AS ts_created,
+    ts_updated::TIMESTAMP AS ts_updated
+FROM
+    datalake_crawler_zap_imoveis.zap_imoveis
+WHERE
+    ts_updated IS NOT NULL
+    AND MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY id_house ORDER BY ts_updated DESC) = 1
