0a941fe8589d3bdc37fb9094641048233fc1c09d
Gabriel Berger <120596108+gab-berger@users.noreply.github.com>
2025-09-10 12:48:28 -0300

    change ratings ingestion type (#20318)

--- dags/people/workable_redshift/queries/clean/ratings.sql
+++ dags/people/workable_redshift/queries/clean/ratings.sql
@@ -17,14 +17,5 @@ SELECT
     TO_TIMESTAMP(updated_at) AS ts_updated,
     NOW() AS ts_load,
 
-    -- partitions
-    year,
-    month,
-    day
-
 FROM
     datalake_workable_redshift_raw.ratings
-WHERE 
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-QUALIFY 
-    updated_at = MAX(updated_at) OVER (PARTITION BY id)
