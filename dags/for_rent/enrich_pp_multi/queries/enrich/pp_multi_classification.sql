902c89e1a2f7731473a7835c200f1e4591f39066
Gustavo Henrique Brunelli <gustavo@brunelli.net.br>
2025-09-10 09:36:54 -0300

    add visits stats from last month to pp_multi_classification (#20299)

--- dags/for_rent/enrich_pp_multi/queries/enrich/pp_multi_classification.sql
+++ dags/for_rent/enrich_pp_multi/queries/enrich/pp_multi_classification.sql
@@ -1,100 +1,77 @@
-WITH yesterday_stats AS (
+WITH visits_last_month AS (
   SELECT
     id_owner,
-    COUNT(*) AS listing_count,
-    SUM(
-      CASE
-        WHEN status_change_reason = 'RENTED' THEN 1
-        ELSE 0
-      END
-    ) AS rented_listing_count
+    COUNT(DISTINCT IF(id_event_type = 1, id_event, NULL)) AS visits_booked,
+    COUNT(DISTINCT IF(id_event_type = 2, id_event, NULL)) AS visits_completed
   FROM
-    datalake_rental_historical_follow_up.house_listings_daily_info
+    datalake_rent_demand_events.rent_demand_events
   WHERE
-    MAKE_DATE(year, month, day) = DATE('{load_start_date}')
+    ts_event >= DATE('{load_start_date}') - INTERVAL '1' MONTH
   GROUP BY ALL
 ),
-two_year_stats AS (
+
+ongoing_houses_stats AS (
   SELECT
     id_owner,
     day,
     month,
     year,
-    COUNT(1) AS daily_listing_count,
-    SUM(
-      CASE
-        WHEN status_change_reason = 'RENTED' THEN 1
-        ELSE 0
-      END
-    ) AS daily_rented_listing_count
+    COUNT(DISTINCT IF(status_history IN ('alugado', 'publicado', 'suspenso', 'SUSPENDED', 'PUBLISHED'), id_house, NULL)) AS ongoing_houses
   FROM
     datalake_rental_historical_follow_up.house_listings_daily_info
-  WHERE
-    MAKE_DATE(year, month, day) >= (DATE('{load_start_date}') - INTERVAL '2' YEAR)
   GROUP BY ALL
 ),
+
+yesterday_stats AS (
+  SELECT
+    ohs.id_owner,
+    ohs.ongoing_houses
+  FROM
+    ongoing_houses_stats AS ohs
+  WHERE
+    MAKE_DATE(ohs.year, ohs.month, ohs.day) = DATE('{load_start_date}')
+  GROUP BY ALL
+),
+
 two_year_max AS (
   SELECT
-    id_owner,
-    MAX(daily_listing_count) AS max_listing_count,
-    MAX(daily_rented_listing_count) AS max_rented_listing_count
+    ohs.id_owner,
+    MAX(ohs.ongoing_houses) AS max_ongoing_houses
   FROM
-    two_year_stats
-  GROUP BY
-    id_owner
-),
-lifetime_stats AS (
-  SELECT
-    id_owner,
-    day,
-    month,
-    year,
-    COUNT(1) AS daily_listing_count,
-    SUM(
-      CASE
-        WHEN status_change_reason = 'RENTED' THEN 1
-        ELSE 0
-      END
-    ) AS daily_rented_listing_count
-  FROM
-    datalake_rental_historical_follow_up.house_listings_daily_info
+    ongoing_houses_stats AS ohs
+  WHERE
+    MAKE_DATE(ohs.year, ohs.month, ohs.day) = DATE('{load_start_date}') - INTERVAL '2' YEAR
   GROUP BY ALL
 ),
+
+
 lifetime_max AS (
   SELECT
-    id_owner,
-    MAX(daily_listing_count) AS max_listing_count,
-    MAX(daily_rented_listing_count) AS max_rented_listing_count
+    ohs.id_owner,
+    MAX(ohs.ongoing_houses) AS max_ongoing_houses
   FROM
-    lifetime_stats
-  GROUP BY
-    id_owner
+    ongoing_houses_stats AS ohs
+  GROUP BY ALL
 )
+
 SELECT
   ys.id_owner,
   CASE
     WHEN
-      (
-        ys.listing_count >= 5
-        AND ys.rented_listing_count >= 1
-      )
+      ys.ongoing_houses >= 5
     THEN 
       'ACTIVE'
     WHEN 
-      (
-        tym.max_listing_count >= 5
-        AND tym.max_rented_listing_count >= 1
-      )
+      tym.max_ongoing_houses >= 5
     THEN
       'POTENTIAL'
     ELSE 'LIFETIME'
   END AS pp_multi_classification,
-  ys.listing_count AS current_listings,
-  ys.rented_listing_count AS current_rented_listings,
-  tym.max_listing_count AS max_listing_in_two_years,
-  tym.max_rented_listing_count AS max_rented_listing_in_two_years,
-  lm.max_listing_count AS max_listing_in_lifetime,
-  lm.max_rented_listing_count AS max_rented_listing_in_lifetime
+  ys.ongoing_houses AS ongoing_houses,
+  tym.max_ongoing_houses AS max_ongoing_houses_in_two_years,
+  lm.max_ongoing_houses AS max_ongoing_houses_in_lifetime,
+  vlm.visits_booked AS visits_booked_last_month,
+  vlm.visits_completed AS visits_completed_last_month
 FROM
   yesterday_stats AS ys
 JOIN
@@ -103,6 +80,8 @@ JOIN
 JOIN 
   lifetime_max AS lm
     ON ys.id_owner = lm.id_owner
+JOIN
+  visits_last_month AS vlm
+    ON ys.id_owner = vlm.id_owner
 WHERE
-  lm.max_listing_count >= 5
-  AND lm.max_rented_listing_count >= 1
+  lm.max_ongoing_houses >= 5
