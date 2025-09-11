b524f50f7de11209e6bd6af2a93245d99f3204dc
Eduardo  Soares <eduardorochasoares@users.noreply.github.com>
2025-09-11 14:31:55 -0300

    feat(entities): create a termination fast lane dag (#20349)
    * create a termination fast lane dag

--- dags/primitives/enrich_transactional_entities/queries/enrich/entities.sql
+++ dags/primitives/enrich_transactional_entities/queries/enrich/entities.sql
@@ -182,7 +182,7 @@ termination AS (
       END AS is_active,
       ts_created,
       ts_updated
-    FROM datalake_terminator_clean.termination
+    FROM datalake_terminator_test_clean.termination
   )
   SELECT
         tb.id_entity,
