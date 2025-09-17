5c924ad7bd0d94a7ee93054d4f6fcdbc134122e7
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-09-17 16:31:08 -0300

    feat: add orignal invoice (#20439)

--- dags/fintech/billing/queries/clean/invoice.sql
+++ dags/fintech/billing/queries/clean/invoice.sql
@@ -1,8 +1,27 @@
+WITH 
+original_invoice_base AS (
 SELECT
-	id,
+  id,
+  regexp_replace(
+    regexp_replace(payload, '^"|"$', ''),
+    '\\\\"', '"'
+  ) AS unescaped_payload
+FROM 
+	datalake_billing_raw.invoice
+),
+original_invoice AS (
+SELECT
+  id,
+  get_json_object(unescaped_payload, '$.original-id') as id_original
+FROM 
+  original_invoice_base
+)
+SELECT
+	i.id,
 	account_id AS id_account,
 	contract_id AS id_contract, 
 	source_invoice_id AS id_source_invoice,
+	o.id_original,
 	account_type,
 	status,
 	sub_status,
@@ -15,4 +34,7 @@ SELECT
 	created_at AS ts_created,
 	updated_at AS ts_updated
 FROM 
-	datalake_billing_raw.invoice
+	datalake_billing_raw.invoice i 
+LEFT JOIN
+  original_invoice o
+  ON i.id = o.id
