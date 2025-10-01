bd2005dce5f9bcfae27beb4e1608c35179e6e11f
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-10-01 12:29:20 -0300

    remove visit_origin ingestion (#20621)

--- dags/for_sale/ebdb_visit/queries/clean/visit_origin.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    atualizadoEm AS ts_updated,
-    criadoEm AS ts_created,
-    descricao AS description,
-    isApp AS is_app,
-    nome AS name
-FROM
-    datalake_ebdb_raw.VisitaOrigem
