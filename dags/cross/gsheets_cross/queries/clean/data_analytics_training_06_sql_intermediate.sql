e51ae580b11e107e8f0a12e37bbe292bbdd380b3
gustavohalencar <90861839+gustavohalencar@users.noreply.github.com>
2025-10-07 14:47:24 -0300

    Terminating the ingestion of Analytics Training data (#20716)
    * Terminating the ingestion of Analytics Training data
    
    * Deleting clean sql files

--- dags/cross/gsheets_cross/queries/clean/data_analytics_training_06_sql_intermediate.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    email,
-    points,
-    line,
-    question_1,
-    TO_TIMESTAMP(date, "dd/MM/yyyy HH:mm:ss") AS ts_answered,
-    ts_load
-FROM
-    datalake_gsheets_raw.data_analytics_training_06_sql_intermediate
--- dags/cross/gsheets_cross/queries/clean/data_analytics_training_10_basic_statistical_concepts.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    email,
-    points,
-    line,
-    question_1,
-    TO_TIMESTAMP(date, "dd/MM/yyyy HH:mm:ss") AS ts_answered,
-    ts_load
-FROM
-    datalake_gsheets_raw.data_analytics_training_10_basic_statistical_concepts
