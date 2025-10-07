d61b67f39c4145532e23944007341b725effa759
William Angelo <39243586+williamangelo@users.noreply.github.com>
2025-10-07 17:15:29 -0300

    fix: new whatsapp tabkle (#20747)

--- dags/support_and_service/help_center_api/queries/clean/whatsapp_migration_strategy.sql
+++ dags/support_and_service/help_center_api/queries/clean/whatsapp_migration_strategy.sql
@@ -5,5 +5,3 @@ SELECT
   updated_at AS ts_updated
 FROM
   datalake_help_center_api_raw.t_whatsapp_migration_strategy
-WHERE
-  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
