23b8483c2a14d4529d31869597a25957eb1cc36f
luana-silvia <93656129+luana-silvia@users.noreply.github.com>
2025-10-08 16:13:35 -0300

    fix(sonia_journey_log): fixing user id, proposal deduplicate and (#20759)
    group_name_full

--- dags/fintech/enrich_sonia_ht/queries/enrich/sonia_journey_log.sql
+++ dags/fintech/enrich_sonia_ht/queries/enrich/sonia_journey_log.sql
@@ -1,7 +1,8 @@
-WITH hightouch_data as (
+WITH hightouch_data AS (
 SELECT
   log.row_instance_id,
-  log.row_id AS user_id,
+  log.row_id,
+  COALESCE(get_json_object(syc.fields, '$.id_user'), log.row_id) AS user_id,
   log.timestamp,
   runs.started_at,
   runs.finished_at,
@@ -13,12 +14,13 @@ SELECT
     WHEN meta.node_name = 'CONTROLE AUDIT' THEN 'Control'
    END AS group_name,
   get_json_object(syc.fields, '$.id_proposal') AS proposal_id,
-  get_json_object(syc.fields, '$.is_bypass') AS is_bypass,
-  get_json_object(syc.fields, '$.is_credit_passport') AS is_credit_passport,
-  get_json_object(syc.fields, '$.is_free_guarantee_offered') AS is_free_guarantee_offered,
-  get_json_object(syc.fields, '$.is_light_approval') AS is_light_approval,
-  get_json_object(syc.fields, '$.is_only_one_active_offer') AS is_only_one_active_offer,
-  get_json_object(syc.fields, '$.is_single_tenant') AS is_single_tenant
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_bypass') AS BOOLEAN), FALSE) AS is_bypass,
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_credit_passport') AS BOOLEAN), FALSE) AS is_credit_passport,
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_free_guarantee_offered') AS BOOLEAN), TRUE) AS is_free_guarantee_offered,
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_light_approval') AS BOOLEAN), TRUE) AS is_light_approval,
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_only_one_active_offer') AS BOOLEAN), TRUE) AS is_only_one_active_offer,
+  COALESCE(CAST(get_json_object(syc.fields, '$.is_single_tenant') AS BOOLEAN), TRUE) AS is_single_tenant,
+  CONCAT(get_json_object(syc.fields, '$.id_proposal'), COALESCE(CAST(get_json_object(syc.fields, '$.is_only_one_active_offer') AS BOOLEAN), TRUE)) AS proposal_active_offer
 FROM hightouch_planner.journey_log_93d2e2b5d82c486db22aa5209dddf354 AS log
 LEFT JOIN hightouch_planner.journey_metadata_93d2e2b5d82c486db22aa5209dddf354 AS meta
   ON log.to_node_id = meta.node_id
@@ -36,18 +38,19 @@ WHERE log.timestamp >= '2025-09-24'
 rn_count AS (
   SELECT
     *,
-    ROW_NUMBER() OVER (PARTITION BY proposal_id ORDER BY IF(group_name = 'Out of Experiment', 1, IF(group_name = 'Test', 2, 3))) AS rn_latest
+    ROW_NUMBER() OVER (PARTITION BY proposal_active_offer ORDER BY IF(group_name = 'Out of Experiment', 1, IF(group_name = 'Test', 2, 3))) AS rn_latest
   FROM hightouch_data
 ),
 groups_agg AS (
   SELECT
-    proposal_id,
+    proposal_active_offer,
     ARRAY_JOIN(ARRAY_SORT(ARRAY_DISTINCT(ARRAY_AGG(group_name))), ', ') AS all_groups
   FROM hightouch_data
-  GROUP BY proposal_id
+  GROUP BY proposal_active_offer
 )
 SELECT
   rc.row_instance_id,
+  rc.row_id,
   rc.user_id,
   rc.proposal_id,
   rc.timestamp,
@@ -56,24 +59,25 @@ SELECT
   rc.sync_id,
   rc.sync_run_id,
   CASE
-    WHEN ga.all_groups IN ('Out of Experiment', 'Control', 'Test') THEN all_groups
+    WHEN ga.all_groups IN ('Out of Experiment', 'Control', 'Test') THEN ga.all_groups
     WHEN ga.all_groups LIKE '%Out of Experiment%' THEN 'Error - Out of Experiment'
     WHEN ga.all_groups LIKE '%Test%' THEN 'Test'
     WHEN ga.all_groups LIKE '%Control%' THEN 'Control'
   END AS group_name,
   CASE
-    WHEN ga.all_groups IN ('Out of Experiment', 'Control', 'Test') THEN all_groups
-    WHEN ga.all_groups LIKE '%Out of Experiment%' THEN 'Error - Out of Experiment'
-    WHEN ga.all_groups LIKE '%Test%' THEN 'Error - Test'
-    WHEN ga.all_groups LIKE '%Control%' THEN 'Error - Control'
+    WHEN ga.all_groups IN ('Out of Experiment', 'Control', 'Test') THEN ga.all_groups
+    WHEN ga.all_groups LIKE '%Out of Experiment%' AND ga.all_groups LIKE '%Test%' THEN 'Error - Assigned Test and Out of Experiment'
+    WHEN ga.all_groups LIKE '%Out of Experiment%' AND ga.all_groups LIKE '%Control%' THEN 'Error - Assigned Control and Out of Experiment'
+    WHEN ga.all_groups LIKE '%Test%' THEN 'Error - Assigned Control and Test'
   END AS group_name_full,
-  COALESCE(CAST(rc.is_bypass AS BOOLEAN), FALSE) AS is_bypass,
-  COALESCE(CAST(rc.is_credit_passport AS BOOLEAN), FALSE) AS is_credit_passport,
-  COALESCE(CAST(rc.is_free_guarantee_offered AS BOOLEAN), TRUE) AS is_free_guarantee_offered,
-  COALESCE(CAST(rc.is_light_approval AS BOOLEAN), TRUE) AS is_light_approval,
-  COALESCE(CAST(rc.is_only_one_active_offer AS BOOLEAN), TRUE) AS is_only_one_active_offer,
-  COALESCE(CAST(rc.is_single_tenant AS BOOLEAN), TRUE) AS is_single_tenant
+  rc.is_bypass,
+  rc.is_credit_passport,
+  rc.is_free_guarantee_offered,
+  rc.is_light_approval,
+  rc.is_only_one_active_offer,
+  rc.is_single_tenant,
+  rc.proposal_active_offer
 FROM rn_count AS rc
 JOIN groups_agg AS ga
-  ON rc.proposal_id = ga.proposal_id
+  ON rc.proposal_active_offer = ga.proposal_active_offer
 WHERE rc.rn_latest = 1
