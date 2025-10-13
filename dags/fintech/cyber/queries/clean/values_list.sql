c5d60b4b26db65a507796e48ffcc4b6baba3cf70
Bianca Garcia Martins <biigarciam@gmail.com>
2025-10-13 16:27:55 +0200

    Revert "[RFP-4577] Cyber: Add 2 tables to Cyber: lov and lov_vals (#20802)" (#20812)
    This reverts commit 21463bd41c34a28179d42e5680672b1a93926bf5.

--- dags/fintech/cyber/queries/clean/values_list.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    LSID AS id_value,
-    LSCODE AS value_code,
-    LSDESC AS value_description,
-    LSDFLT AS default_flag
-FROM
-    datalake_cyber_raw.lov_vals
--- dags/fintech/cyber/queries/clean/values_list_catalog.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    LVID AS id_value,
-    LVFIELD AS field_with_predefined_values,
-    LVLENGTH AS max_length,
-    LVNAME AS name,
-    LVTYPE AS field_data_type
-FROM
-    datalake_cyber_raw.lov
