e84f8c902b7fe0ab5e5f27f711cdd5b3ccb3a0f6
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2025-10-13 09:49:18 -0300

    [RFP-3115] Fill queue timeline gaps (#20806)
    * Fill queue timeline gaps
    
    * update query

--- dags/fintech/enrich_cyber/queries/enrich/queue_timeline_2.sql
+++ dags/fintech/enrich_cyber/queries/enrich/queue_timeline_2.sql
@@ -1,6 +1,82 @@
-WITH unpivot_table AS (
+WITH deduplicate_queues AS (
+            SELECT
+                queue,
+                queue_type,
+                queue_name
+            FROM datalake_cyber.queue_decision_tree
+            QUALIFY ROW_NUMBER() OVER(PARTITION BY queue, queue_type ORDER BY level DESC) = 1
+        ),
+        base_union AS (
+            SELECT
+                id_contract_external,
+                1 AS priority,
+                segmentation_queue,
+                segementation_queue_description,
+                ts_last_update_segmentation_queue,
+                agreement_queue,
+                agreement_queue_description,
+                ts_last_update_agreement_queue,
+                eviction_queue,
+                eviction_queue_description,
+                ts_last_update_eviction_queue,
+                ts_last_activity
+            FROM
+                datalake_cyber.contracts
+
+            UNION ALL
+
+            SELECT
+                SPLIT(id_contract,r'\.')[0] AS id_contract_external,
+                2 AS priority,
+                segmentation_queue,
+                qdts.queue_name AS segementation_queue_description,
+                dt_reference AS ts_last_update_segmentation_queue,
+                agreement_queue,
+                qdta.queue_name AS agreement_queue_description,
+                dt_reference AS ts_last_update_agreement_queue,
+                eviction_queue,
+                qdte.queue_name AS eviction_queue_description,
+                dt_reference AS ts_last_update_eviction_queue,
+                NULL AS ts_last_activity
+            FROM
+                datalake_cyber.fallback_queue fq
+            LEFT JOIN deduplicate_queues AS qdta
+                ON fq.agreement_queue = qdta.queue AND qdta.queue_type = 'Acordo'
+            LEFT JOIN deduplicate_queues AS qdts
+                ON fq.segmentation_queue = qdts.queue AND qdts.queue_type = 'Segmentação'
+            LEFT JOIN deduplicate_queues AS qdte
+                ON fq.eviction_queue = qdte.queue AND qdte.queue_type = 'Eviction'
+            WHERE dt_reference >= DATE('2025-05-29') AND dt_reference <= DATE('2025-07-15')
+
+            UNION ALL
+
+            SELECT
+                SPLIT(id_contract,r'\.')[0] AS id_contract_external,
+                3 AS priority,
+                segmentation_queue,
+                qdts.queue_name AS segementation_queue_description,
+                ts_distribution AS ts_last_update_segmentation_queue,
+                agreement_queue,
+                qdta.queue_name AS agreement_queue_description,
+                ts_distribution AS ts_last_update_agreement_queue,
+                eviction_queue,
+                qdte.queue_name AS eviction_queue_description,
+                ts_distribution AS ts_last_update_eviction_queue,
+                NULL AS ts_last_activity
+            FROM
+                datalake_cyber_clean.history_contract_distribution h
+            LEFT JOIN deduplicate_queues AS qdta
+                ON h.agreement_queue = qdta.queue AND qdta.queue_type = 'Acordo'
+            LEFT JOIN deduplicate_queues AS qdts
+                ON h.segmentation_queue = qdts.queue AND qdts.queue_type = 'Segmentação'
+            LEFT JOIN deduplicate_queues AS qdte
+                ON h.eviction_queue = qdte.queue AND qdte.queue_type = 'Eviction'
+            WHERE DATE(ts_distribution) >= DATE('2025-05-29') AND DATE(ts_distribution) <= DATE('2025-07-15')
+        ),
+        unpivot_table AS (
     SELECT
         id_contract_external AS id_contract,
+        priority,
         queue_type,
         CASE
             WHEN queue_type = 'agreement' THEN agreement_queue
@@ -14,7 +90,7 @@ WITH unpivot_table AS (
         END AS queue_description,
         dt_updated,
         ts_last_activity
-    FROM datalake_cyber.contracts
+    FROM base_union
     UNPIVOT
     (dt_updated FOR queue_type IN (ts_last_update_agreement_queue AS `agreement`,
                             ts_last_update_segmentation_queue AS `segmentation`,
@@ -26,7 +102,7 @@ deduplicate_records AS (
     SELECT
         *
     FROM unpivot_table
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY id_contract, queue_type, dt_updated ORDER BY ts_last_activity DESC) = 1
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY id_contract, queue_type, dt_updated ORDER BY ts_last_activity DESC, priority) = 1
     ORDER BY dt_updated
 ),
 range_date_explode AS (
