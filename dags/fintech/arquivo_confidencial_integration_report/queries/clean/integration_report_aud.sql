e3440b080ae0a60b3a2673cbea1f4a18d25718d9
luana-silvia <93656129+luana-silvia@users.noreply.github.com>
2025-10-14 13:14:50 -0300

    [CRF-417] feat: extract Serasa and Boa Vista scores from attributes before hashing for Superset usage (#20835)
    * adding serasa and bvs scores
    
    * adding domain in yml
    
    * adding descriptions
    
    * changing order

--- dags/fintech/arquivo_confidencial_integration_report/queries/clean/integration_report_aud.sql
+++ dags/fintech/arquivo_confidencial_integration_report/queries/clean/integration_report_aud.sql
@@ -7,6 +7,36 @@ SELECT
     integration_provider,
     raw_data,
     attributes,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) IN ('SERASA_REPORT_HVA4', 'SERASA_REPORT_HSPA')
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.reports[0].score.score') AS INT)
+      WHEN TRIM(UPPER(integration_provider)) IN ('SERASA_SCORE_HSPI','SERASA_SCORE_HSPN')
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.score') AS INT)
+    END AS score_serasa,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'SERASA_REPORT_HVA4'
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.reports[0].score.score') AS INT)
+    END AS hva4_serasa,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'SERASA_REPORT_HSPA'
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.reports[0].score.score') AS INT)
+    END AS hspa_serasa,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'SERASA_SCORE_HSPI'
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.score') AS INT)
+    END AS hspi_serasa,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'SERASA_SCORE_HSPN'
+        THEN CAST(GET_JSON_OBJECT(attributes, '$.score') AS INT)
+    END AS hspn_serasa,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'SERASA_REPORT_HSPA'
+        THEN GET_JSON_OBJECT(attributes, '$.reports[0].negativeData.pefin.summary.count')
+    END AS serasa_pefin_count,
+    CASE
+      WHEN TRIM(UPPER(integration_provider)) = 'BOAVISTA_SCORE_P6'
+        THEN GET_JSON_OBJECT(attributes, '$.score_p6')
+    END AS bvs_score,
     created_at AS ts_created,
     CAST(year AS INT) AS year,
     CAST(month AS INT) AS month,
