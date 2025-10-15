8084595d2fecedc3a8bc16cb2b8816973daf756e
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2025-10-15 11:45:58 -0300

    Updating fact_ai_agents_interaction (#20851)
    * Updating fact_ai_agents_interaction
    
    * change alias

--- dags/fintech/dw_collection_ai_agents/queries/dw/fact_ai_agents_interaction.sql
+++ dags/fintech/dw_collection_ai_agents/queries/dw/fact_ai_agents_interaction.sql
@@ -1,11 +1,10 @@
 SELECT DISTINCT
     m.id_session,
     m.id_sauron_session,
-    m.id_external,
+    m.id_langfuse_session AS id_external,
     m.id_ticket,
     m.id_user,
     m.bot,
-    m.status,
     m.first_queue,
     m.last_queue,
     s.name,
@@ -19,7 +18,7 @@ SELECT DISTINCT
         WHEN m.bot = 'matthew' THEN 'Matthew in Whatsapp'
         ELSE 'Wall-e'
     END AS ai_agent_source,
-    m.is_escalation,
+    m.is_escalated AS is_escalation,
     DATE(m.ts_created) AS dt_session_created,
     m.ts_created,
     m.ts_updated
@@ -27,6 +26,6 @@ FROM
     datalake_chatbot.sessions as m
 LEFT JOIN
     datalake_langfuse_clean.scores s
-        ON s.id_session = m.id_external
+        ON s.id_session = m.id_langfuse_session
         AND s.name = 'SessionContainsMatthewAgentEvaluator'
 WHERE (bot = 'matthew' OR (bot = 'wall-e'))
