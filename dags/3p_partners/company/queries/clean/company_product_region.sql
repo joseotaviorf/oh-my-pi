4070516c11e746a0f33b29e88aa01b699fe2e5af
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-10-16 16:04:45 -0300

    Update company_product_region.sql (#20875)

--- dags/3p_partners/company/queries/clean/company_product_region.sql
+++ dags/3p_partners/company/queries/clean/company_product_region.sql
@@ -3,7 +3,7 @@ SELECT
     company_id AS id_company,
     product_id AS id_product,
     region_external_id AS id_region,
-    purspose,
+    purpose,
     version,
     created_at AS ts_created,
     updated_at AS ts_updated,
