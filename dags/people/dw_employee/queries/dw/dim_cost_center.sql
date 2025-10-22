345d44884a2af707af712e5f60518e4dd8ff48c7
Gabriel Berger <120596108+gab-berger@users.noreply.github.com>
2025-10-22 14:40:28 -0300

    Dbp 1070/data quality dim cost center (#20899)
    * change status to is_active
    
    * add custom data quality check to cost center code

--- dags/people/dw_employee/queries/dw/dim_cost_center.sql
+++ dags/people/dw_employee/queries/dw/dim_cost_center.sql
@@ -16,7 +16,7 @@ SELECT
   COALESCE(o.owner_leadership_layer_2_name, '-1') AS owner_leadership_layer_2_name,
   COALESCE(o.owner_leadership_layer_3_name, '-1') AS owner_leadership_layer_3_name,
   COALESCE(o.headcount_type, '-1') AS headcount_type,
-  o.status,
+  COALESCE(o.status = 'A', '-1') AS is_active,
   o.dt_effective_start,
   o.dt_effective_end,
   NOW () AS ts_load
