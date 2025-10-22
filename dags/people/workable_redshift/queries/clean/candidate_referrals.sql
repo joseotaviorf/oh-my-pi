330da60426736e16808a20cb24f14c1dd00983bf
Isabella Araujo <45436397+araujoic@users.noreply.github.com>
2025-10-22 12:21:10 -0300

    [PEOPLE] Delete workable_redshift DAG (#20924)
    * delete workable redshift
    
    * remove enrich workable
    
    * update dependencies

--- dags/people/workable_redshift/queries/clean/candidate_referrals.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  id,
-  candidate_id AS id_candidate,
-  payload,
-  created_at AS ts_created,
-  updated_at AS ts_updated,
-  NOW() AS ts_load
-FROM
-  datalake_workable_redshift_raw.candidate_referrals
--- dags/people/workable_redshift/queries/clean/departments.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  id,
-  parent_id AS id_parent,
-  name,
-  created_at AS ts_created,
-  updated_at AS ts_updated,
-  NOW() AS ts_load
-FROM
-  datalake_workable_redshift_raw.departments
