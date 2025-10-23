c1702a4369c9a169b9a17d5709c50bfee01a18cd
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2025-10-23 11:18:43 -0300

    [DFORSALE-9182] Creating some test table in ebdb_listing_fast_lane DAG (#20876)
    * Create query and metadata, also update declaration
    
    * Create fast lane for house_listing_relation and AUD
    
    * Update z order
    
    * Add data quality for lbc_aud
    
    * Add data qualities and update declaration
    
    * Create new cluster

---
+++ dags/for_sale/ebdb_listing_fast_lane/queries/clean/house_listing_relation.sql
@@ -0,0 +1,12 @@
+SELECT
+    id,
+    imovelId AS id_house,
+    listingBusinessContextId AS id_listing_business_context,
+    relatedId AS id_related,
+    relatedAs AS related_as,
+    sourceType AS source_type,
+    imovelOriginated AS house_origin,
+    criadoEm AS ts_created,
+    atualizadoEm AS ts_updated
+FROM
+    datalake_ebdb_test_raw.ImovelListingRelation
---
+++ dags/for_sale/ebdb_listing_fast_lane/queries/clean/house_listing_relation_aud.sql
@@ -0,0 +1,20 @@
+SELECT
+    id AS id_house_listing_relation,
+    imovelId AS id_house,
+    listingBusinessContextId AS id_listing_business_context,
+    relatedId AS id_related,
+    REV AS rev,
+    REVTYPE AS rev_type,
+    sourceType AS source_type,
+    relatedAs AS related_as,
+    imovelOriginated AS house_origin,
+    imovelId_MOD AS mod_id_house,
+    listingBusinessContextId_MOD AS mod_id_listing_business_context,
+    sourceType_MOD AS mod_source_type,
+    relatedId_MOD AS mod_id_related,
+    relatedAs_MOD AS mod_related_as,
+    imovelOriginated_MOD AS mod_house_origin,
+    criadoEm AS ts_created,
+    atualizadoEm AS ts_updated
+FROM
+    datalake_ebdb_test_raw.ImovelListingRelation_AUD
