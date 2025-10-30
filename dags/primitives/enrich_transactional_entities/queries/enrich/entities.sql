4eb0a6022ecd575bb94a10f9591af5009333adcc
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-10-30 10:57:02 -0300

    [CRMG-2345] Adding invoice entity (#20868)
    * [CRMG-2345] Adding invoice entity
    
    * manual modifications
    
    * adjusting invoice logic and adding account table
    
    * fixing entity
    
    * adjusting query
    
    * metadata

--- dags/primitives/enrich_transactional_entities/queries/enrich/entities.sql
+++ dags/primitives/enrich_transactional_entities/queries/enrich/entities.sql
@@ -738,6 +738,46 @@ repair AS (
     WHERE
         entity = 'FR_REPAIR_OFFBOARDING'
 ),
+invoice AS (
+    SELECT
+        i.id AS id_entity,
+        cc.id_house,
+        CASE
+            WHEN ai.type = 'tenant' THEN COALESCE(ai.id_main_user, cc.id_tenant)
+            WHEN ai.type = 'landlord' THEN COALESCE(ai.id_main_user, cc.id_owner)
+        END AS id_user,
+        cc.id_contract,
+        'FR_INVOICE' AS entity,
+        CASE
+            WHEN ai.type = 'tenant' THEN 'TENANT'
+            WHEN ai.type = 'landlord' THEN 'OWNER'
+        END AS persona,
+        'RENT' AS business_context,
+        TO_JSON(
+            STRUCT(
+                i.status AS status,
+                i.ts_created AS when
+            )
+        ) AS properties,
+        CASE
+            WHEN i.status = 'open' THEN TRUE
+            WHEN i.status IN ('divergent-payment', 'not-payable', 'written-down', 'canceled', 'paid') THEN FALSE
+            ELSE NULL
+        END AS is_active,
+        i.ts_created,
+        i.ts_retsuko_updated AS ts_updated
+    FROM
+        datalake_retsuko_test_clean.invoice AS i
+    LEFT JOIN
+      datalake_retsuko_test_clean.account AS ai
+        ON i.id_account = ai.id
+    LEFT JOIN
+        datalake_retsuko_test_clean.contract AS c
+            ON c.id = i.id_contract
+    JOIN
+        core_contract.contract AS cc
+            ON c.id_external = cc.id_contract
+)
 base AS (
     SELECT
         {sk_entity} AS sk_entity,
@@ -898,6 +938,22 @@ base AS (
         ts_updated
     FROM
         repair
+    UNION ALL
+    SELECT
+        {sk_entity} AS sk_entity,
+        id_entity,
+        id_house,
+        id_contract,
+        id_user,
+        entity,
+        persona,
+        business_context,
+        properties,
+        is_active,
+        ts_created,
+        ts_updated
+    FROM
+        invoice
 )
 SELECT
     b.sk_entity,
