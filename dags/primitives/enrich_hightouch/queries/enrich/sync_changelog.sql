45dfa555b458b599a4ca1365645152520cd3d700
Eduardo  Soares <eduardorochasoares@users.noreply.github.com>
2025-11-03 10:29:07 -0300

    feat: create the incremental ingestion of hightouch sync_changelog table (#21084)
    * feat: create the incremental ingestion of hightouch sync_changelog table
    
    * updated metadat
    
    * updated query

---
+++ dags/primitives/enrich_hightouch/queries/enrich/sync_changelog.sql
@@ -0,0 +1,30 @@
+WITH filtered_runs AS (
+  SELECT
+    id_sync,
+    id_sync_run,
+    ts_started,
+    ts_finished,
+    year,
+    month,
+    day
+  FROM
+    datalake_hightouch.sync_runs
+  WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+)
+SELECT
+  r.id_sync,
+  r.id_sync_run,
+  r.ts_started,
+  r.ts_finished,
+  r.year,
+  r.month,
+  r.day,
+  c.row_id,
+  c.op_type,
+  c.status,
+  c.failure_reason
+FROM filtered_runs AS r
+INNER JOIN hightouch_audit.sync_changelog AS c
+  ON r.id_sync = c.sync_id
+  AND r.id_sync_run = c.sync_run_id;
