c3090221915d1afe686fb8652c9f339d298fc797
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-11-03 09:59:54 -0300

    VSTS-3001-create_post_visit_agent (#21049)
    * create post_visit_agent model
    
    * update the metadata

--- dags/for_sale/enrich_visit/queries/enrich/post_visit_agent_unified.sql
+++ dags/for_sale/enrich_visit/queries/enrich/post_visit_agent_unified.sql
@@ -13,14 +13,21 @@ SELECT
     b.id_visit,
     b.id AS id_schedule,
     b.visit_fup AS reason,
-    b.visit_fup_comment AS comment,
+    CASE
+      WHEN b.visit_fup = 'VaiNegociar' THEN 'VISIT_DONE'
+      WHEN b.visit_fup IN ('EntradaNaoAutorizada', 'NaoCompareceu') THEN 'VISIT_UNSUCCESSFUL'
+      ELSE 'ERROR'
+    END AS event_type,
     show_demand.has_attended AS has_demand_attended,
     show_agent.has_attended AS has_agent_attended,
     show_tenant_living.has_attended AS has_tenant_living_attended,
     show_supply.has_attended AS has_supply_attended,
-    b.ts_visit_fup AS ts_visit_unsuccessful
+    b.ts_visit_fup AS ts_post_visit_agent
 FROM
-    datalake_ebdb_clean.booking b
+    datalake_ebdb_clean.booking AS b
+LEFT JOIN
+    datalake_ebdb_clean.visit AS v
+        ON b.id_visit = v.id
 LEFT JOIN
     visit_show AS show_demand
         ON show_demand.id_booking = b.id
@@ -38,7 +45,8 @@ LEFT JOIN
         ON show_supply.id_booking = b.id
         AND show_supply.type = 'Landlord'
 WHERE
-    b.visit_fup IN ('EntradaNaoAutorizada', 'NaoCompareceu')
+    b.visit_fup IS NOT NULL
     AND b.type = 'Visita'
+    AND v.ts_created::DATE >= '2020-01-01'
 QUALIFY
     ROW_NUMBER() OVER(PARTITION BY b.id_visit ORDER BY b.id DESC) = 1
