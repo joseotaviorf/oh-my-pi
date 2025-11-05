5adffaa6beba53dfce7e9d9b5d3a2890a80833d6
Gui Sacramento <71848596+GuiSacramento@users.noreply.github.com>
2025-11-05 16:41:08 -0300

    fix: new funnel rule (#21141)

--- dags/fintech/enrich_sap_accounting_process/queries/enrich/retsuko_transactional.sql
+++ dags/fintech/enrich_sap_accounting_process/queries/enrich/retsuko_transactional.sql
@@ -492,25 +492,25 @@ errors_base AS (
         r.accounting_name,
         r.accrual_year_month,
         MIN(CASE
-        WHEN (sl_hash.hash IS NOT NULL OR sl_entry.hash IS NOT NULL) THEN 'success'
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND (se.id_finance_entity IS NULL OR se.status = 'failed' OR se.failed_reason IS NOT NULL) THEN 'source failure'
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND (sg.id_feature IS NULL OR sg.sync_sap_job_status = 'error' OR sg.webhook_error IS NOT NULL) THEN 'gateway failure'
+        WHEN sl_hash.hash IS NOT NULL THEN 'success'
+        WHEN sl_hash.hash IS NULL AND (se.id_finance_entity IS NULL OR se.status = 'failed' OR se.failed_reason IS NOT NULL) THEN 'source failure'
+        WHEN sl_hash.hash IS NULL AND (sg.id_feature IS NULL OR sg.sync_sap_job_status = 'error' OR sg.webhook_error IS NOT NULL) THEN 'gateway failure'
         ELSE 'unknown failure'
         END) AS accounting_process_status,
         MIN(CASE
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND se.id_finance_entity IS NULL THEN 'source not found'
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND se.status = 'failed' THEN se.failed_reason
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND sg.id_feature IS NULL THEN 'gateway not found'
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND sg.sync_sap_job_status = 'error' THEN sg.webhook_error
-        WHEN (sl_hash.hash IS NULL AND sl_entry.hash IS NULL) AND se.id_finance_entity IS NOT NULL AND sg.id_feature IS NOT NULL THEN 'sap not found'
+        WHEN sl_hash.hash IS NULL AND se.id_finance_entity IS NULL THEN 'source not found'
+        WHEN sl_hash.hash IS NULL AND se.status = 'failed' THEN se.failed_reason
+        WHEN sl_hash.hash IS NULL AND sg.id_feature IS NULL THEN 'gateway not found'
+        WHEN sl_hash.hash IS NULL AND sg.sync_sap_job_status = 'error' THEN sg.webhook_error
+        WHEN sl_hash.hash IS NULL AND se.id_finance_entity IS NOT NULL AND sg.id_feature IS NOT NULL THEN 'sap not found'
         ELSE NULL
         END) AS error_description,
-        MIN(IF(sl_hash.hash IS NULL AND sl_entry.hash IS NULL, FALSE, TRUE)) AS is_completeness,
+        MIN(IF(sl_hash.hash IS NULL, FALSE, TRUE)) AS is_completeness,
         CAST(r.source_amount AS DECIMAL(12,2)) AS source_amount,
-        CAST(SUM(COALESCE(sl_hash.debit_credit, sl_entry.debit_credit, 0)) AS DECIMAL(12,2)) AS sap_amount,
+        CAST(SUM(COALESCE(sl_hash.debit_credit, 0)) AS DECIMAL(12,2)) AS sap_amount,
         MAX(r.dt_source_trigger) AS dt_source_trigger,
-        MAX(COALESCE(sl_hash.dt_sap_created, sl_entry.dt_sap_created)) AS dt_sap_created,
-        MAX(COALESCE(sl_hash.dt_sap_reference, sl_entry.dt_sap_reference)) AS dt_sap_reference
+        MAX(sl_hash.dt_sap_created) AS dt_sap_created,
+        MAX(sl_hash.dt_sap_reference) AS dt_sap_reference
     FROM
         retsuko AS r
     LEFT JOIN
@@ -522,9 +522,6 @@ errors_base AS (
     LEFT JOIN
         sap AS sl_hash
             ON sl_hash.hash = sg.hash AND r.account_number = sl_hash.account_number
-    LEFT JOIN
-        sap AS sl_entry
-            ON sl_entry.id_finance_entity_entry = r.id_finance_entity_entry AND r.account_number = sl_entry.account_number
     GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 12
 ),
 
