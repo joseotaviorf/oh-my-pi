cd185fbe0517246ffdcf07da70d22448f6592ee8
rodrigorochedo <96083546+rodrigorochedo@users.noreply.github.com>
2025-11-11 17:56:28 -0300

    Revert "add merge on clause to ebdb agents dag declaration (#21203)" (#21229)
    This reverts commit 902466b3ee16646c1f041b88f12c1f2f1cb7737f.

--- dags/agents/enrich_ebdb_agents/queries/enrich/preferred_property_agent_program_eligibility.sql
+++ dags/agents/enrich_ebdb_agents/queries/enrich/preferred_property_agent_program_eligibility.sql
@@ -18,7 +18,6 @@ WITH revised_agent_data_programs_aud AS (
       AND DATE(r.ts_revision) <= DATE('{load_end_date}')
 )
 SELECT
-  XXHASH64(id_agent_data, ts_revision) AS id_snapshot,
   id,
   id_agent_data AS id_agent,
   program,
