1d36c5cdd66be54b660ce57c0f5508acdd5fbe75
Marcio de Lucas <97989464+quintomarcio@users.noreply.github.com>
2025-11-11 09:16:15 -0300

    Create `photo_session` View for Transactional Entities (#21093)

---
+++ dags/primitives/enrich_entities_views/queries/enrich/photo_session.sql
@@ -0,0 +1,89 @@
+WITH photo_session_exploded AS (
+  SELECT
+    pj.id AS id_entity,
+    pj.id_house,
+    pj.id AS id_photographer,
+    COALESCE(u.id, h.id_user) AS id_owner,
+    'PHOTO_SESSION' AS entity,
+    EXPLODE(
+      CASE
+          WHEN h.is_for_rent AND h.is_for_sale THEN ARRAY('RENT','SALE')
+          WHEN h.is_for_rent THEN ARRAY('RENT')
+          WHEN h.is_for_sale THEN ARRAY('SALE')
+          ELSE ARRAY(NULL)
+      END
+    ) AS business_context,
+    pj.status AS status,
+    CASE
+      WHEN upper(trim(pj.status)) IN ('PUBLICADO', 'CANCELADO', 'COMPLETADO', 'FOTOSTIRADAS') THEN FALSE
+      ELSE TRUE
+    END AS is_active,
+    pj.ts_created,
+    pj.ts_updated
+  FROM
+    datalake_ebdb_clean.photographer_job AS pj
+  LEFT JOIN
+      datalake_ebdb_clean.house_listing_relation AS hl
+          ON hl.id = pj.id_house
+          AND hl.related_as = 'PROPERTY_OWNER'
+  LEFT JOIN
+    datalake_ebdb_clean.user AS u
+      ON pj.id_photographer_data = u.id_photographer_data
+      AND (u.id = hl.id_related
+          OR u.uuid_person = hl.id_related)
+  LEFT JOIN
+    datalake_ebdb_clean.house AS h
+      ON pj.id_house = h.id
+  WHERE
+    YEAR(pj.ts_created) >= 2025
+),
+photo_session_base AS (
+  SELECT
+      id_entity,
+      id_house,
+      NULL::STRING AS id_contract,
+      id_owner,
+      id_photographer,
+      entity,
+      business_context,
+      TO_JSON(
+          STRUCT(
+              status AS status,
+              ts_created AS when
+          )
+      ) AS properties,
+      is_active,
+      ts_created,
+      ts_updated
+  FROM
+      photo_session_exploded
+)
+SELECT
+    id_entity,
+    id_house,
+    id_contract,
+    id_owner AS id_user,
+    entity,
+    'OWNER' AS persona,
+    business_context,
+    properties,
+    is_active,
+    ts_created,
+    ts_updated
+FROM
+    photo_session_base
+UNION ALL
+SELECT
+    id_entity,
+    id_house,
+    id_contract,
+    id_photographer AS id_user,
+    entity,
+    'AGENT_PHOTOGRAPHER' AS persona,
+    business_context,
+    properties,
+    is_active,
+    ts_created,
+    ts_updated
+FROM
+    photo_session_base
