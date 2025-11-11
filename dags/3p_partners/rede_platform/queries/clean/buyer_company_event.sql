64c7e6ba13e78b57a14ff7ca43a084c40f1e2803
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-11-11 13:31:43 -0300

    add application_source (#21209)

--- dags/3p_partners/rede_platform/queries/clean/buyer_company_event.sql
+++ dags/3p_partners/rede_platform/queries/clean/buyer_company_event.sql
@@ -3,6 +3,7 @@ SELECT
     event_entity_id AS id_event_entity,
     company_uuid AS uuid_company,
     person_uuid AS uuid_person,
+    application_source,
     event_entity_name,
     event_type,
     trigger_actor,
