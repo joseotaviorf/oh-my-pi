22cabef06c1e0616622081cd312ac8383e4bf438
Bianca Garcia Martins <biigarciam@gmail.com>
2025-11-13 18:48:33 -0300

    fix fields (#21283)

--- dags/fintech/gsheets_fintech/queries/clean/collections_for_sale_journey_routine.sql
+++ dags/fintech/gsheets_fintech/queries/clean/collections_for_sale_journey_routine.sql
@@ -12,25 +12,30 @@ SELECT
         ELSE NULL
     END AS is_cancellation_with_brokerage,
     CASE
-        WHEN LOWER(NULLIF(É_3p_leads_, '')) = 'sim' THEN TRUE
-        WHEN LOWER(NULLIF(É_3p_leads_, '')) = 'não' THEN FALSE
+        WHEN LOWER(NULLIF(`É 3p leads?`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`É 3p leads?`, '')) = 'não' THEN FALSE
         ELSE NULL
     END AS is_3p_leads,
     CASE
-        WHEN LOWER(NULLIF(É_mercado_primário_, '')) = 'sim' THEN TRUE
-        WHEN LOWER(NULLIF(É_mercado_primário_, '')) = 'não' THEN FALSE
+        WHEN LOWER(NULLIF(`É mercado primário?`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`É mercado primário?`, '')) = 'não' THEN FALSE
         ELSE NULL
     END AS is_primary_market,
     CASE
-        WHEN LOWER(NULLIF(É_minha_casa_minha_vida_, '')) = 'sim' THEN TRUE
-        WHEN LOWER(NULLIF(É_minha_casa_minha_vida_, '')) = 'não' THEN FALSE
+        WHEN LOWER(NULLIF(`É minha casa minha vida?`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`É minha casa minha vida?`, '')) = 'não' THEN FALSE
         ELSE NULL
     END AS is_my_house_my_life,
     CASE
-        WHEN LOWER(NULLIF(É_perda_reconhecida_, '')) = 'sim' THEN TRUE
-        WHEN LOWER(NULLIF(É_perda_reconhecida_, '')) = 'não' THEN FALSE
+        WHEN LOWER(NULLIF(`É perda reconhecida?`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`É perda reconhecida?`, '')) = 'não' THEN FALSE
         ELSE NULL
     END AS is_recognized_loss,
+    CASE
+        WHEN LOWER(NULLIF(`CCV é apartir de 06/01/2025?`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`CCV é apartir de 06/01/2025?`, '')) = 'não' THEN FALSE
+        ELSE NULL
+    END AS `is_ccv_from_2025_01_06`,
     CASE
         WHEN LOWER(NULLIF(Recomenda_WO, '')) = 'sim' THEN TRUE
         WHEN LOWER(NULLIF(Recomenda_WO, '')) = 'não' THEN FALSE
@@ -42,49 +47,49 @@ SELECT
         ELSE NULL
     END AS is_settled,
     CASE
-        WHEN LOWER(NULLIF(Notificação_Extrajudicial, '')) = 'sim' THEN TRUE
-        WHEN LOWER(NULLIF(Notificação_Extrajudicial, '')) = 'não' THEN FALSE
+        WHEN LOWER(NULLIF(`Notificação_Extrajudicial`, '')) = 'sim' THEN TRUE
+        WHEN LOWER(NULLIF(`Notificação_Extrajudicial`, '')) = 'não' THEN FALSE
         ELSE NULL
     END AS has_extrajudicial_notification,
     NULLIF(Status_da_offer, '') AS offer_status,
-    NULLIF(Status_cobrança, '') AS collection_status,
+    NULLIF(`Status_cobrança`, '') AS collection_status,
     NULLIF(Tempo_na_base, '') AS time_in_base,
-    NULLIF(Tempo_desde_a_última_interação, '') AS time_since_last_interaction,
-    NULLIF(Aging_desde_a_última_interação, '') AS aging_since_last_interaction,
-    NULLIF(Possível_WO__H1_2025_, '') AS possible_wo_h1_2025,
+    NULLIF(`Tempo_desde_a_última_interação`, '') AS time_since_last_interaction,
+    NULLIF(`Aging_desde_a_última_interação`, '') AS aging_since_last_interaction,
+    NULLIF(`Possível_WO__H1_2025_`, '') AS possible_wo_h1_2025,
     NULLIF(Status, '') AS status,
     NULLIF(Status_da_offer_2, '') AS offer_status_2,
-    NULLIF(Tipo_pagamento, '') AS payment_type,
+    NULLIF(`Tipo_pagamento`, '') AS payment_type,
     NULLIF(Produto, '') AS product,
     NULLIF(Partner___Rede, '') AS partner_network,
     NULLIF(Tag_Vendas, '') AS sales_tag,
     NULLIF(Vendedor, '') AS seller,
     NULLIF(Telefone, '') AS phone,
     NULLIF(E_mail, '') AS email,
-    NULLIF(Endereço, '') AS address,
+    NULLIF(`Endereço`, '') AS address,
     NULLIF(Analista, '') AS analyst,
     NULLIF(Status_Detalhado, '') AS detailed_status,
-    NULLIF(Status_da_cobrança_2, '') AS collection_status_2,
-    NULLIF(Motivo_da_cobrança, '') AS collection_reason,
-    NULLIF(Ligação, '') AS call_record,
+    NULLIF(`Status_da_cobrança_2`, '') AS collection_status_2,
+    NULLIF(`Motivo_da_cobrança`, '') AS collection_reason,
+    NULLIF(`Ligação`, '') AS call_record,
     NULLIF(Ticket_Zendesk, '') AS zendesk_ticket,
     NULLIF(Condicionante, '') AS condition,
     NULLIF(Demais_contatos, '') AS other_contacts,
     CAST(NULLIF(Valor_de_corretagem, '') AS DOUBLE) AS brokerage_amount,
     CAST(NULLIF(Saldo_recebido, '') AS DOUBLE) AS balance_received,
     CAST(NULLIF(Saldo_a_receber, '') AS DOUBLE) AS balance_to_receive,
-    CAST(NULLIF(Nº_de_tentativas_de_contato, '') AS INT) AS contact_attempts_count,
+    CAST(NULLIF(`Nº_de_tentativas_de_contato`, '') AS INT) AS contact_attempts_count,
     DATE(NULLIF(Data_entrada, ''), 'd/M/y') AS dt_entry,
     DATE(NULLIF(Data_CCV_assinado, ''), 'd/M/y') AS dt_ccv_signed,
     DATE(NULLIF(Data_CRI, ''), 'd/M/y') AS dt_cri,
-    DATE(NULLIF(Data_diligência, ''), 'd/M/y') AS dt_diligence,
+    DATE(NULLIF(`Data_diligência`, ''), 'd/M/y') AS dt_diligence,
     DATE(NULLIF(Data_do_cancelamento, ''), 'd/M/y') AS dt_cancellation,
     DATE(NULLIF(Data_resgate_da_offer, ''), 'd/M/y') AS dt_offer_rescue,
     DATE(NULLIF(Data_financiamento, ''), 'd/M/y') AS dt_financing,
-    DATE(NULLIF(Data_início_cobrança, ''), 'd/M/y') AS dt_collection_start,
-    DATE(NULLIF(Data_da_quitação, ''), 'd/M/y') AS dt_settlement,
-    DATE(NULLIF(Data_Notificação_EJ, ''), 'd/M/y') AS dt_extrajudicial_notification,
-    DATE(NULLIF(Data_Condicionante, ''), 'd/M/y') AS dt_condition,
+    DATE(NULLIF(`Data_início_cobrança`, ''), 'd/M/y') AS dt_collection_start,
+    DATE(NULLIF(`Data_da_quitação`, ''), 'd/M/y') AS dt_settlement,
+    DATE(NULLIF(`Data_Notificação_EJ`, ''), 'd/M/y') AS dt_extrajudicial_notification,
+    DATE(NULLIF(`Data_Condicionante`, ''), 'd/M/y') AS dt_condition,
     DATE(NULLIF(Data_primeiro_contato, ''), 'd/M/y') AS dt_first_contact,
     DATE(NULLIF(Data_Prevista_de_Contato, ''), 'd/M/y') AS dt_expected_contact,
     NOW() AS ts_load
