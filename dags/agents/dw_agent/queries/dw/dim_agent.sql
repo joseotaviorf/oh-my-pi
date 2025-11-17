bdb817d9c53946a1ec9a7cd5a98847da317cb93e
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-11-17 13:28:07 -0300

    [3P] `dim_agent` upgrade (#21305)
    * update dim agent
    
    * update dependencies-file
    
    * remove duplicated active column

--- dags/agents/dw_agent/queries/dw/dim_agent.sql
+++ dags/agents/dw_agent/queries/dw/dim_agent.sql
@@ -1,56 +1,62 @@
 SELECT
-    du.dados_agente_id AS sk_agent,
-    du.id AS id_user,
-    du.nome AS name,
-    du.cpf AS cpf,
-    du.rg AS rg,
-    du.sexo AS sex,
-    du.email,
-    du.email_alternativo AS alternative_email,
-    du.telefone_principal AS main_phone_number,
-    du.endereco AS address,
-    du.numero AS number,
-    du.complemento AS complement,
-    du.bairro AS neighborhood,
-    du.cep,
-    du.cidade AS city,
-    du.estado_nome AS state_name,
-    du.estado_abreviacao AS state_abbreviation,
-    du.country_code,
-    wd.contract_name AS work_contract_name,
-    NULLIF(wd.3p_partner, '') AS rede_partner,
-    at.types AS agent_type,
-    du.dadosagente_perfil AS agent_profile,
-    du.dadosagente_numero_creci AS agent_creci,
-    BOOLEAN(du.active) AS is_user_active,
-    BOOLEAN(du.bloqueado) AS is_blocked,
-    BOOLEAN(du.dadosafiliado_ativo) AS is_affiliate_active,
-    BOOLEAN(du.dadosagente_ativo) AS is_agent_active,
-    BOOLEAN(du.dadosfotografo_ativo) AS is_photographer_active,
-    BOOLEAN(du.inquilino) AS is_tenant,
-    du.is_sale_agent,
-    du.is_rent_agent,
-    tqc.id_agent IS NOT NULL AND tqc.is_currently_active AS is_tqc_3p_agent,
-    wd.is_3p_contract AS is_rede_agent,
-    du.data_nascimento AS dt_birth,
-    du.criado_em AS ts_created,
-    du.atualizado_em AS ts_updated,
-    NOW() AS ts_load
+  ad.id AS sk_agent,
+  COALESCE(p.sk_person, -1) AS sk_person,
+  COALESCE(u.id, -1) AS sk_user,
+  COALESCE(c.sk_company, -1) AS sk_company,
+  u.id AS id_user,
+  ad.code AS agent_code,
+  ad.profile AS agent_profile,
+  at.types AS agent_type,
+  ad.agent_type AS agent_type_main,
+  REGEXP_REPLACE(wc.contract_name, '[\\[\\]]', '') AS contract_name,
+  ad.creci_number,
+  ad.is_active AS is_agent_active,
+  u.is_active AS is_user_active,
+  u.is_blocked,
+  ad.agent_type = 'CORRETOR_REDE' AS is_3p_agent,
+  ead.is_rent_agent,
+  ead.is_sale_agent,
+  af.is_active AS is_affiliate_active,
+  ph.is_active AS is_photographer_active,
+  u.is_tenant,
+  ad.is_passive_lead_receiver,
+  ad.is_service_link_active,
+  mp.status = 'ACTIVE' AS has_3p_member_profile_active,
+  mp.status IN ('PENDING CONFIRMATION', 'PENDING INVITATION') AS has_3p_member_profile_pending,
+  ad.ts_created,
+  ad.ts_updated,
+  NOW() AS ts_load
 FROM
-    dw_public.dim_user AS du
-JOIN
-    datalake_ebdb_clean.agent_data AS ad
-        ON du.dados_agente_id = ad.id
+  datalake_ebdb_clean.agent_data AS ad
 LEFT JOIN
-    datalake_ebdb_work_contract.work_contract AS wd
-        ON wd.id = ad.id_work_contract
+  datalake_ebdb_user.agent_data AS ead
+    ON ad.id = ead.id
 LEFT JOIN
-    datalake_ebdb_clean.agent_data_types AS at 
-        ON at.id_agent_data = du.dados_agente_id
+  datalake_ebdb_user.user AS u
+    ON ad.id = u.id_agent
 LEFT JOIN
-    datalake_gsheets_clean.agents_3p_tqc AS tqc
-        ON tqc.id_agent = du.dados_agente_id
-WHERE
-    dados_agente_id IS NOT NULL
+  datalake_ebdb_user.affiliate_data AS af
+    ON af.id = u.id_affiliates
+LEFT JOIN
+  datalake_ebdb_clean.photographer_data AS ph
+    ON ph.id = u.id_photographer_data
+LEFT JOIN
+  datalake_ebdb_clean.agent_data_types AS at
+    ON at.id_agent_data = ad.id
+LEFT JOIN
+  datalake_ebdb_clean.work_contract AS wc
+    ON wc.id = ad.id_work_contract
+LEFT JOIN
+  datalake_person.person_sks AS p
+    ON p.uuid_person = u.uuid_person
+LEFT JOIN
+  datalake_company.company_sks AS c
+    ON ad.uuid_company = c.uuid_company
+LEFT JOIN
+  datalake_company_clean.member_profile AS mp
+    ON p.uuid_person = mp.uuid_person
+    AND c.id_company = mp.id_company
+    AND mp.id_profile = 17 -- 3P agent profile in company service
+    AND mp.id_product = 27 -- 3P real estate product in company service
 QUALIFY -- There are extremely few duplicate rows on agent_data_types (12/16595 at the moment of writing). This is to get rid of them.
-    ROW_NUMBER() OVER(PARTITION BY du.dados_agente_id ORDER BY at.types) = 1
+  ROW_NUMBER() OVER(PARTITION BY ad.id ORDER BY at.types) = 1
