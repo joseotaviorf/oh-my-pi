205fde269646566ab7c653e629670df312fc9d93
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-11-17 16:26:44 -0300

    VSTS-3001-fix_dim_visit_funnel (#21314)
    * fix dim_visit_funnel
    
    * fix fact_visit_schedules

--- dags/for_sale/dw_visit/queries/dw/dim_visit_funnel.sql
+++ dags/for_sale/dw_visit/queries/dw/dim_visit_funnel.sql
@@ -1,50 +1,53 @@
 WITH visit_rent_flow AS (
-  SELECT
-    frde.sk_visit,
-    LOWER(dret.abbreviation) AS event_code,
-    'RENT' AS business_context,
-    MIN(frde.ts_event) AS ts_first_event,
-    MAX(frde.ts_event) AS ts_last_event
-  FROM dw_rent.fact_rent_demand_events AS frde
-  INNER JOIN dw_rent.dim_rent_event_type AS dret
-    ON dret.sk_event_type = frde.sk_event_type
-  WHERE
-    LOWER(dret.abbreviation) IN ('os', 'oa', 'cs') AND NOT sk_visit IS NULL
-  GROUP BY
-    1,
-    2,
-    3
-), visit_sale_flow AS (
-  SELECT
-    sk_visit,
-    IF(LOWER(dset.abbreviation) = 'ccv', 'cs', LOWER(dset.abbreviation)) AS event_code,
-    'SALE' AS business_context,
-    MIN(fsde.ts_event) AS ts_first_event,
-    MAX(fsde.ts_event) AS ts_last_event
-  FROM dw_sale.fact_sale_demand_event AS fsde
-  INNER JOIN dw_sale.dim_sale_event_type AS dset
-    ON dset.sk_event_type = fsde.sk_event_type
-  WHERE
-    LOWER(dset.abbreviation) IN ('os', 'oa', 'ccv') AND NOT sk_visit IS NULL
-  GROUP BY
-    1,
-    2,
-    3
+    SELECT
+        frde.sk_visit,
+        LOWER(dret.abbreviation) AS event_code,
+        'RENT' AS business_context,
+        MIN(frde.ts_event) AS ts_first_event,
+        MAX(frde.ts_event) AS ts_last_event
+    FROM
+        dw_rent.fact_rent_demand_events AS frde
+    INNER JOIN
+        dw_rent.dim_rent_event_type AS dret
+            ON dret.sk_event_type = frde.sk_event_type
+    WHERE
+        LOWER(dret.abbreviation) IN ('os','oa','cs')
+        AND sk_visit IS NOT NULL
+    GROUP BY 1, 2, 3
+),
+visit_sale_flow AS (
+    SELECT
+        fsde.sk_visit,
+        IF(LOWER(dset.abbreviation) = 'ccv', 'cs', LOWER(dset.abbreviation)) AS event_code,
+        'SALE' AS business_context,
+        MIN(fsde.ts_event) AS ts_first_event,
+        MAX(fsde.ts_event) AS ts_last_event
+    FROM
+        dw_sale.fact_sale_demand_event AS fsde
+    INNER JOIN
+        dw_sale.dim_sale_event_type AS dset
+            ON dset.sk_event_type = fsde.sk_event_type
+    WHERE
+        LOWER(dset.abbreviation) IN ('os', 'oa', 'ccv')
+        AND sk_visit IS NOT NULL
+    GROUP BY 1, 2, 3
 )
 SELECT
-  UNHEX(MD5(sk_visit || event_code || business_context)) AS sk_visit_funnel,
-  sk_visit,
-  event_code,
-  business_context,
-  ts_first_event,
-  ts_last_event
-FROM visit_rent_flow
+    MD5(sk_visit || event_code || business_context) AS sk_visit_funnel,
+    sk_visit,
+    event_code,
+    business_context,
+    ts_first_event,
+    ts_last_event
+FROM
+    visit_rent_flow
 UNION ALL
 SELECT
-  UNHEX(MD5(sk_visit || event_code || business_context)) AS sk_visit_funnel,
-  sk_visit,
-  event_code,
-  business_context,
-  ts_first_event,
-  ts_last_event
-FROM visit_sale_flow
+    MD5(sk_visit || event_code || business_context) AS sk_visit_funnel,
+    sk_visit,
+    event_code,
+    business_context,
+    ts_first_event,
+    ts_last_event
+FROM
+    visit_sale_flow
--- dags/for_sale/dw_visit/queries/dw/fact_visit_schedules.sql
+++ dags/for_sale/dw_visit/queries/dw/fact_visit_schedules.sql
@@ -1,69 +1,76 @@
 SELECT
-  es.id_schedule AS sk_schedule,
-  es.id_visit AS sk_visit,
-  es.id_region AS sk_region,
-  es.id_visitor AS sk_visitor,
-  es.id_owner AS sk_owner,
-  es.id_house AS sk_house,
-  bc.sk_business_context,
-  vm.sk_visit_model,
-  db.sk_behavior_type,
-  es.id_business_unit AS sk_business_unit,
-  es.id_company_supply AS sk_company_supply,
-  es.id_company_demand AS sk_company_demand,
-  es.id_agent AS sk_agent,
-  es.id_user_agent AS sk_user_agent,
-  es.id_user_en AS sk_user_en,
-  es.id_fixed_agent AS sk_fixed_agent,
-  ppa.id_house_listing_relation AS sk_ppa_relation,
-  es.id_user_creation AS sk_author_creator,
-  es.id_user_cancelation AS sk_author_cancelation,
-  es.visit_code,
-  es.id_offer AS sk_offer,
-  dim_heh.sk_house_entrance,
-  es.id_succeed_schedule AS sk_succeed_schedule,
-  sk_origin_type,
-  es.days_visit_cancelled_to_visit,
-  es.days_visit_booked_to_visit,
-  es.days_visit_booked_to_cancelled,
-  es.days_visit_booked_to_visit_completed,
-  es.hours_booking_to_offer,
-  es.hours_visit_to_offer,
-  CASE WHEN es.is_hub_flow IS TRUE THEN 1 ELSE 0 END AS is_hub_flow,
-  CASE WHEN es.is_house_rented IS TRUE THEN 1 ELSE 0 END AS is_house_rented,
-  CASE WHEN es.has_tenant_living IS TRUE THEN 1 ELSE 0 END AS has_tenant_living,
-  1 AS is_booking,
-  CASE WHEN NOT es.ts_schedule_rescheduled IS NULL THEN 1 ELSE 0 END AS is_reschedule,
-  CASE WHEN NOT es.ts_schedule_confirmed IS NULL THEN 1 ELSE 0 END AS is_confirmed,
-  CASE WHEN NOT es.ts_schedule_completed IS NULL THEN 1 ELSE 0 END AS is_completed,
-  CASE WHEN NOT es.ts_schedule_canceled IS NULL THEN 1 ELSE 0 END AS is_canceled,
-  CASE WHEN NOT es.ts_schedule_unsuccessful IS NULL THEN 1 ELSE 0 END AS is_unsuccessful,
-  COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_created, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_created,
-  COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_confirmed, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_confirmed,
-  COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_completed, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_completed,
-  COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_canceled, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_canceled,
-  COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_unsuccessful, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_unsuccessful,
-  ts_schedule_created,
-  ts_schedule_confirmed,
-  ts_schedule_completed,
-  ts_schedule_canceled,
-  ts_schedule_unsuccessful,
-  ts_schedule_rescheduled,
-  CURRENT_TIMESTAMP() AS ts_load
-FROM datalake_visit.visit_schedules AS es
-LEFT JOIN dw_visit.dim_origin_type AS dot
-  ON es.schedule_origin = dot.origin_name
-LEFT JOIN dw_visit.dim_business_context AS bc
-  ON es.business_context = bc.business_context
-LEFT JOIN dw_visit.dim_visit_model AS vm
-  ON es.visit_model = vm.visit_model
-LEFT JOIN dw_visit.dim_behavior AS db
-  ON es.behavior = db.behavior_type
-LEFT JOIN dw_house.dim_house_entrance_history AS dim_heh
-  ON es.id_house = dim_heh.sk_house
-  AND es.ts_schedule_created >= dim_heh.ts_entrance_started
-  AND es.ts_schedule_created < COALESCE(dim_heh.ts_entrance_ended, CURRENT_TIMESTAMP())
-LEFT JOIN datalake_ebdb_agents.preferred_property_agent_relation_history AS ppa
-  ON ppa.id_house = es.id_house
-  AND es.business_context = ppa.business_context
-  AND ts_schedule_created BETWEEN ppa.ts_relation_started AND COALESCE(ppa.ts_relation_ended, CURRENT_TIMESTAMP())
+    es.id_schedule AS sk_schedule,
+    es.id_visit AS sk_visit,
+    es.id_region AS sk_region,
+    es.id_visitor AS sk_visitor,
+    es.id_owner AS sk_owner,
+    es.id_house AS sk_house,
+    bc.sk_business_context,
+    vm.sk_visit_model,
+    db.sk_behavior_type,
+    es.id_business_unit AS sk_business_unit,
+    es.id_company_supply AS sk_company_supply,
+    es.id_company_demand AS sk_company_demand,
+    es.id_agent AS sk_agent,
+    es.id_user_agent AS sk_user_agent,
+    es.id_user_en AS sk_user_en,
+    es.id_fixed_agent AS sk_fixed_agent,
+    ppa.id_house_listing_relation AS sk_ppa_relation,
+    es.id_user_creation AS sk_author_creator,
+    es.id_user_cancelation AS sk_author_cancelation,
+    es.visit_code,
+    es.id_offer AS sk_offer,
+    dim_heh.sk_house_entrance,
+    es.id_succeed_schedule AS sk_succeed_schedule,
+    sk_origin_type,
+    es.days_visit_cancelled_to_visit,
+    es.days_visit_booked_to_visit,
+    es.days_visit_booked_to_cancelled,
+    es.days_visit_booked_to_visit_completed,
+    es.hours_booking_to_offer,
+    es.hours_visit_to_offer,
+    CASE WHEN es.is_hub_flow IS TRUE THEN 1 ELSE 0 END AS is_hub_flow,
+    CASE WHEN es.is_house_rented IS TRUE THEN 1 ELSE 0 END AS is_house_rented,
+    CASE WHEN es.has_tenant_living IS TRUE THEN 1 ELSE 0 END AS has_tenant_living,
+    1 AS is_booking,
+    CASE WHEN es.ts_schedule_rescheduled IS NOT NULL THEN 1 ELSE 0 END AS is_reschedule,
+    CASE WHEN es.ts_schedule_confirmed IS NOT NULL THEN 1 ELSE 0 END AS is_confirmed,
+    CASE WHEN es.ts_schedule_completed IS NOT NULL THEN 1 ELSE 0 END AS is_completed,
+    CASE WHEN es.ts_schedule_canceled IS NOT NULL THEN 1 ELSE 0 END AS is_canceled,
+    CASE WHEN es.ts_schedule_unsuccessful IS NOT NULL THEN 1 ELSE 0 END AS is_unsuccessful,
+    COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_created, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_created,
+    COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_confirmed, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_confirmed,
+    COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_completed, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_completed,
+    COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_canceled, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_canceled,
+    COALESCE(CAST(REPLACE(SUBSTRING(ts_schedule_unsuccessful, 1, 10), '-', '') AS BIGINT), -1) AS sk_schedule_unsuccessful,
+    ts_schedule_created,
+    ts_schedule_confirmed,
+    ts_schedule_completed,
+    ts_schedule_canceled,
+    ts_schedule_unsuccessful,
+    ts_schedule_rescheduled,
+    CURRENT_TIMESTAMP() AS ts_load
+FROM
+    datalake_visit.visit_schedules AS es
+LEFT JOIN
+    dw_visit.dim_origin_type AS dot
+        ON es.schedule_origin = dot.origin_name
+LEFT JOIN
+    dw_visit.dim_business_context AS bc
+        ON es.business_context = bc.business_context
+LEFT JOIN
+    dw_visit.dim_visit_model AS vm
+        ON es.visit_model = vm.visit_model
+LEFT JOIN
+    dw_visit.dim_behavior AS db
+        ON es.behavior = db.behavior_type
+LEFT JOIN
+    dw_house.dim_house_entrance_history AS dim_heh
+        ON es.id_house = dim_heh.sk_house
+        AND es.ts_schedule_created >= dim_heh.ts_entrance_started
+        AND es.ts_schedule_created < COALESCE(dim_heh.ts_entrance_ended, CURRENT_TIMESTAMP())
+LEFT JOIN
+    datalake_ebdb_agents.preferred_property_agent_relation_history AS ppa
+        ON ppa.id_house = es.id_house
+        AND es.business_context = ppa.business_context
+        AND es.ts_schedule_created BETWEEN ppa.ts_relation_started AND COALESCE(ppa.ts_relation_ended, CURRENT_TIMESTAMP())
