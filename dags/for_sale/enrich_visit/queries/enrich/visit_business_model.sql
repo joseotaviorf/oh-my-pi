dae53f2d4d3a5a683d5ceb35b530fce93a41b0dd
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2025-11-17 18:22:29 -0300

    update visit business model (#21320)

--- dags/for_sale/enrich_visit/queries/enrich/visit_business_model.sql
+++ dags/for_sale/enrich_visit/queries/enrich/visit_business_model.sql
@@ -1,18 +1,36 @@
+WITH consolidated_visits AS (
+  SELECT
+    v.id AS id_visit,
+    bm.id AS id_visit_business_model,
+    COALESCE(bm.business_model, v.business_model) AS business_model,
+    bm.ts_created AS ts_business_model_created,
+    v.ts_created AS ts_visit_created,
+    bm.ts_updated AS ts_business_model_updated,
+    v.ts_updated AS ts_visit_updated
+  FROM
+    datalake_ebdb_clean.visit AS v
+  LEFT JOIN
+    datalake_ebdb_clean.visit_business_model AS bm
+      ON v.id = bm.id_visit
+)
 SELECT
-  bm.id,
-  vt.id_booking,
-  bm.id_visit,
+  cv.id_visit,
+  cv.id_visit_business_model,
   vt.uuid_company,
-  bm.business_model,
-  bm.business_model LIKE '%3P_SUPPLY%' AS is_3p_supply,
-  bm.business_model LIKE '%3P_DEMAND%' AS is_3p_demand,
-  bm.business_model LIKE '%3P_LEAD_GEN%' AS is_3p_lead_gen,
-  bm.business_model LIKE '%3P%' AS has_3p_access_control,
-  bm.ts_created,
-  bm.ts_updated
+  cv.business_model,
+  cv.business_model LIKE '%3P_SUPPLY%' AS is_3p_supply,
+  cv.business_model LIKE '%3P_DEMAND%' AS is_3p_demand,
+  cv.business_model LIKE '%3P_LEAD_GEN%' AS is_3p_lead_gen,
+  cv.business_model LIKE '%3P%' AS has_3p_access_control,
+  cv.ts_business_model_created,
+  cv.ts_visit_created,
+  cv.ts_business_model_updated,
+  cv.ts_visit_updated
 FROM
-  datalake_ebdb_clean.visit_business_model AS bm
+  consolidated_visits AS cv
 LEFT JOIN
   datalake_ebdb_clean.visitor AS vt
-    ON bm.id_visit = vt.id_visit
+    ON cv.id_visit = vt.id_visit
       AND vt.type = 'Agent'
+QUALIFY
+  ROW_NUMBER() OVER (PARTITION BY vt.id_visit ORDER BY vt.ts_updated DESC) = 1
