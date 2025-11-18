fa882f4652fb0379d9d77f97d095b00ad309b97e
Yiyi Ji <69869418+yiyiji@users.noreply.github.com>
2025-11-18 17:16:00 +0000

    Feat (SEARCH-1713) Add Recs click trough rate metric (#21213)
    * add recs click trough rate metric
    
    * add metric to .md file
    
    * add dependencies
    
    * add table partitions
    
    * chore: transpile SQL files to Databricks syntax [skip ci]
    
    * fix punctuation
    
    * undo homesbot incremental date pattern
    
    ---------
    
    Co-authored-by: YIYI JI <yiyi.ji@quintoandar.com.br>
    Co-authored-by: Homes <homesbot@quintoandar.com.br>

---
+++ dags/conversational_xp/enrich_find_health_metrics/queries/enrich/metric_recs_click_through_rate.sql
@@ -0,0 +1,17 @@
+SELECT
+  GET_JSON_OBJECT(ids, '$.id_recset_fix') AS id_recset_fix,
+  CAST(GET_JSON_OBJECT(ids, '$.id_house') AS INT) AS id_house,
+  GET_JSON_OBJECT(dimensions, '$.business_context') AS business_context,
+  GET_JSON_OBJECT(dimensions, '$.city') AS house_city,
+  GET_JSON_OBJECT(dimensions, '$.platform') AS platform,
+  GET_JSON_OBJECT(dimensions, '$.position') AS listing_position_in_recset,
+  GET_JSON_OBJECT(dimensions, '$.showcase') AS showcase,
+  GET_JSON_OBJECT(dimensions, '$.listing_age') AS listing_age,
+  CASE WHEN GET_JSON_OBJECT(metrics, '$.click') = '1' THEN TRUE ELSE FALSE END AS listing_clicked,
+  ts_event AS ts_recommendation_event,
+  year,
+  month,
+  day
+FROM datalake_search.recs_impressions_processed
+WHERE
+  WHERE MAKE_DATE(year, month, day) BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_7}) AND DATE('{end_date}')
