734f1cc98ee57eec347a055fd27d0f89eb79fca1
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2025-11-18 18:24:05 -0300

    VSTS-3143-createfs_exp_dag (#21306)
    * create enrich_for_sale_experiment DAG
    
    * update dependencies
    
    * update null columns creation

---
+++ dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_1_1_visits_boosting_agents.sql
@@ -0,0 +1,24 @@
+SELECT
+    id_visitor AS identifier,
+    1 AS id_neotribe,
+    1 AS id_experiment,
+    'VISITS XP' AS name_neotribe,
+    'visits_boosting_agents' as name_experiment,
+    'VISITOR' as identifier_type,
+    business_context,
+    CASE
+        WHEN RIGHT(id_visitor, 2) >= 50 AND RIGHT(id_visitor, 2) <= 99 THEN 'CONTROL'
+        WHEN RIGHT(id_visitor, 2) >= 00 AND RIGHT(id_visitor, 2) <= 49 THEN 'TREATMENT'
+        ELSE 'ERROR'
+    END AS test_group,
+    CAST(NULL AS STRING) AS documentation_link,
+    'Os BP são identificados de acordo com as visitas marcadas e divididos de acordo com o final do id' AS additional_information,
+    DATE('2025-07-13') AS dt_started,
+    CAST(NULL AS DATE) AS dt_ended,
+    MIN(ts_created::DATE) AS dt_identifier_started
+FROM
+    datalake_ebdb_clean.visit
+WHERE
+    ts_created::DATE >= '2025-11-14'
+    AND business_context = 'RENT'
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12
