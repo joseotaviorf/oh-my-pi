6d334bc847d6c52b4d4820fe8f608edb3b28b95b
isa-caruso <161630736+isa-caruso@users.noreply.github.com>
2025-11-25 10:32:13 -0300

    [FPF-5412] Fix: correct comma in payment block (#21385)

--- dags/fintech/retsuko/queries/clean/payment_block.sql
+++ dags/fintech/retsuko/queries/clean/payment_block.sql
@@ -2,8 +2,8 @@ SELECT
     id,
     external_id AS id_external,
     contract_id AS id_contract,
-    activated_by_audit_id AS id_activated_by_audit
-    deactivated_by_audit_id AS id_deactivated_by_audit
+    activated_by_audit_id AS id_activated_by_audit,
+    deactivated_by_audit_id AS id_deactivated_by_audit,
     reason,
     description,
     status,
