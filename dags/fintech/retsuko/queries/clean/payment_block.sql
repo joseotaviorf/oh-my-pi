e580986843dd366f6de5b1aa7924293c92e55d9c
isa-caruso <161630736+isa-caruso@users.noreply.github.com>
2025-11-25 09:34:06 -0300

    [FPF-5412] Feat: add new table to retsuko (#21378)

---
+++ dags/fintech/retsuko/queries/clean/payment_block.sql
@@ -0,0 +1,14 @@
+SELECT
+    id,
+    external_id AS id_external,
+    contract_id AS id_contract,
+    activated_by_audit_id AS id_activated_by_audit
+    deactivated_by_audit_id AS id_deactivated_by_audit
+    reason,
+    description,
+    status,
+    timestamp(activated_at) AS ts_activated,
+    timestamp(deactivated_at) AS ts_deactivated,
+    timestamp(synced_at) AS ts_synced
+FROM
+    datalake_retsuko_raw.payment_block
