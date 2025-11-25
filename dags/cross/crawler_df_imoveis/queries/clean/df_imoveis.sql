6547513824461595ad02a838ac31eecb6654f359
Mateus Affonso Ferreira <69057688+MateusAffer@users.noreply.github.com>
2025-11-25 08:13:20 -0300

    df_imoveis dag raw and clean (#21331)

---
+++ dags/cross/crawler_df_imoveis/queries/clean/df_imoveis.sql
@@ -0,0 +1,22 @@
+SELECT
+  id, 
+  origin, 
+  address, 
+  city, 
+  geolocation, 
+  name AS listing_name, 
+  description AS listing_description, 
+  crawl_metadata, 
+  house_info, 
+  type, 
+  image_urls, 
+  advertiser, 
+  price, 
+  date_info, 
+  year, 
+  month,
+  day
+FROM
+  datalake_crawler_df_imoveis_raw.df_imoveis
+WHERE
+  MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
