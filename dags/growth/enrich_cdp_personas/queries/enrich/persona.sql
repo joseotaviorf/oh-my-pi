e1051b8e41d4375c80e5af44f4e4cfe1fe9ad337
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2025-11-27 16:20:39 -0300

    [CRMG-2432] Creating AGENT_PHOTOGRAPHER persona (#21420)

--- dags/growth/enrich_cdp_personas/queries/enrich/persona.sql
+++ dags/growth/enrich_cdp_personas/queries/enrich/persona.sql
@@ -81,3 +81,15 @@ SELECT
     NOW() AS ts_load
 FROM
     datalake_cdp_personas.agent_broker
+UNION ALL
+SELECT
+    id_user,
+    uuid_person,
+    'AGENT_PHOTOGRAPHER' AS persona,
+    NULL AS journey_step,
+    is_active,
+    ts_first_event,
+    ts_last_event,
+    NOW() AS ts_load
+FROM
+    datalake_cdp_personas.agent_photographer
