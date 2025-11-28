220d7cb052e64304766cd6d56fcf78bbe0dd638d
Bianca Garcia Martins <biigarciam@gmail.com>
2025-11-28 12:09:09 -0300

    Revert "[RFP-4903] Add original_agency_distribution field to fact_overdue_por…" (#21459)
    This reverts commit f64ca539e5f90102e50eec1a727c7e17dfeae4a6.

--- dags/fintech/enrich_cyber/queries/enrich/agency_timeline.sql
+++ dags/fintech/enrich_cyber/queries/enrich/agency_timeline.sql
@@ -1,31 +1,16 @@
-WITH
-expand_interval AS (
-    SELECT
-        cad.id_contract,
-        cad.id_agency,
-        cad.main_agency_name,
-        EXPLODE(
-            SEQUENCE(
-                cad.dt_start_interval,
-                GREATEST(cad.dt_start_interval, IF(cad.dt_end_interval = LAST_DAY(CURRENT_DATE), CURRENT_DATE, cad.dt_end_interval))
-            )
-        ) AS dt_reference
-    FROM
-        datalake_cyber.contract_agency_distribution AS cad
-    WHERE
-        cad.creditor = 'QuintoAndar'
-        AND cad.dt_end_interval IS NOT NULL
-        AND cad.dt_start_interval <= cad.dt_end_interval
-)
 SELECT
-  ei.id_contract,
-  ei.id_agency,
-  ei.main_agency_name,
-  hcd.agency AS original_agency_distribution,
-  ei.dt_reference
-FROM expand_interval AS ei
-  LEFT JOIN datalake_cyber_clean.contracts AS c
-    ON ei.id_contract = c.id_contract_external
-LEFT JOIN datalake_cyber_clean.history_contract_distribution As hcd
-  ON c.id_contract = hcd.id_contract
-    AND ei.dt_reference = hcd.ts_distribution
+    id_contract,
+    id_agency,
+    main_agency_name,
+    EXPLODE(
+        SEQUENCE(
+            dt_start_interval,
+            GREATEST(dt_start_interval, IF(dt_end_interval = LAST_DAY(CURRENT_DATE), CURRENT_DATE, dt_end_interval))
+        )
+    ) AS dt_reference
+FROM
+    datalake_cyber.contract_agency_distribution
+WHERE
+    creditor = 'QuintoAndar'
+    AND dt_end_interval IS NOT NULL
+    AND dt_start_interval <= dt_end_interval
