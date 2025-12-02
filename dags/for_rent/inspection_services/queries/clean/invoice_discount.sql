f2d164c3f0ebc2eb9dc4a1c685f577f3f254586c
Gustavo Huster <119461813+GustavoHuster@users.noreply.github.com>
2025-12-02 09:50:02 -0300

    invoice discount table ingestion (#21480)

---
+++ dags/for_rent/inspection_services/queries/clean/invoice_discount.sql
@@ -0,0 +1,29 @@
+SELECT
+    discount_uuid AS uuid_discount,
+    contract_id AS id_contract,
+    inspection_uuid AS uuid_inspection,
+    checkpoint_id AS id_checkpoint,
+    user_id AS id_user,
+    person_uuid AS uuid_person,
+    discount_type,
+    inspection_cost,
+    disputed_inspection_cost,
+    discount_value,
+    discount_hash,
+    discount_value_type,
+    tenant_approval AS has_tenant_approval,
+    landlord_approval AS has_landlord_approval,
+    landlord_repair_request AS has_landlord_repair_request,
+    is_eviction,
+    is_spoc,
+    person_blocked,
+    person_blocked_reason,
+    discount_accepted AS is_discount_accepted,
+    discount_reviewed_value,
+    discount_acceptance_by,
+    discount_accepted_at AS dt_discount_accepted,
+    created_at AS ts_created,
+    updated_at AS ts_updated
+FROM
+    datalake_inspection_services_raw.invoice_discount
+
