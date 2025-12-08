e843c55b84b983943418b8278dde6941a2825dab
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2025-12-08 10:47:46 -0300

    Feat: Add sk_task to chat messages and bot interaction messages (#21548)
    * Update fact_chat_messages adding bot message context
    
    * Update fact_chat_message metadata adding new bot context and task cols
    
    * Update dependencies file
    
    * Move message_index to last cte

--- dags/support_and_service/dw_customer_support/queries/dw/fact_chat_messages.sql
+++ dags/support_and_service/dw_customer_support/queries/dw/fact_chat_messages.sql
@@ -1,162 +1,262 @@
-WITH messages AS (
+WITH whatsapp_messages AS 
+(
   SELECT DISTINCT
-    id_channel,
-    GET_JSON_OBJECT(event_payload, "$.MessageSid") AS id_message,
-    REPLACE(from_phone_number, 'whatsapp:', '') AS user_sender,
+    ce.id_channel,
+    ce.id AS id_message,
+    c.id_session AS id_sauron_session,
+    REPLACE(ce.from_phone_number, 'whatsapp:', '') AS user_sender,
+    ce.message_body AS message,
+    ce.ts_created,
     CASE
-      WHEN from_phone_number = 'system' THEN 'Bot'
-      WHEN from_phone_number LIKE '%whatsapp%' THEN 'User'
-      WHEN REPLACE(REPLACE(from_phone_number,'_2E', '.'), '_40', '@')  LIKE '%@%' THEN 'Analyst'
-      WHEN from_phone_number LIKE '%@%' THEN 'Analyst'
-      ELSE NULL
-    END AS user_type,
-    message_body AS message,
-    ts_created
+        WHEN from_phone_number = 'system' THEN 'Bot'
+        WHEN from_phone_number LIKE '%whatsapp%' THEN 'User'
+        WHEN REPLACE(REPLACE(from_phone_number,'_2E', '.'), '_40', '@')  LIKE '%@%' THEN 'Analyst'
+        WHEN from_phone_number LIKE '%@%' THEN 'Analyst'
+        ELSE NULL
+      END AS user_type
   FROM
-    datalake_quinto_messenger_clean.channel_event
+    datalake_quinto_messenger_clean.channel_event AS ce
+  LEFT JOIN
+    datalake_quinto_messenger_clean.channel AS c
+      ON c.id_channel = ce.id_channel
   WHERE
-    ( -- Condition for being on or after the start date
-      year > YEAR(DATE('{load_start_date}')) OR
-      (year = YEAR(DATE('{load_start_date}')) AND month > MONTH(DATE('{load_start_date}'))) OR
-      (year = YEAR(DATE('{load_start_date}')) AND month = MONTH(DATE('{load_start_date}')) AND day >= DAY(DATE('{load_start_date}')))
-    ) AND (-- Condition for being on or before the end date
-      year < YEAR(DATE('{load_end_date}')) OR
-      (year = YEAR(DATE('{load_end_date}')) AND month < MONTH(DATE('{load_end_date}'))) OR
-      (year = YEAR(DATE('{load_end_date}')) AND month = MONTH(DATE('{load_end_date}')) AND day <= DAY(DATE('{load_end_date}')))
-    )
-  QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY id_channel, id_message ORDER BY ts_created DESC) = 1
-  UNION ALL
+    MAKE_DATE(ce.year, ce.month, ce.day) >= '{load_start_date}'
+),
+inapp_messages AS (
   SELECT DISTINCT
-    id_channel,
-    id_message,
-    REPLACE(REPLACE(id_user_external,'_2E', '.'), '_40', '@') AS user_sender,
+    icm.id_channel,
+    icm.id_message,
+    MAX(c.id_session) AS id_sauron_session,
+    REPLACE(REPLACE(icm.id_user_external,'_2E', '.'), '_40', '@') AS user_sender,
+    icm.message,
+    icm.ts_created,
     CASE
       WHEN id_user_external = 'system' THEN 'Bot'
       WHEN REPLACE(REPLACE(id_user_external,'_2E', '.'), '_40', '@')  LIKE '%@%' THEN 'Analyst'
       WHEN id_user_external IS NOT NULL THEN 'User'
       ELSE NULL
-    END AS user_type,
+    END AS user_type
+  FROM
+    datalake_internal_chat_clean.internal_chat_messages AS icm
+  LEFT JOIN
+    datalake_quinto_messenger_clean.chat AS c
+      ON c.id_channel = icm.id_channel
+  WHERE
+    MAKE_DATE(icm.year, icm.month, icm.day) >= '{load_start_date}'
+  GROUP BY ALL
+),
+messages AS (
+  SELECT
+    id_channel,
+    id_message,
+    id_sauron_session,
+    user_sender,
     message,
+    user_type,
     ts_created
   FROM
-    datalake_internal_chat_clean.internal_chat_messages
-  WHERE
-    ( -- Condition for being on or after the start date
-      year > YEAR(DATE('{load_start_date}')) OR
-      (year = YEAR(DATE('{load_start_date}')) AND month > MONTH(DATE('{load_start_date}'))) OR
-      (year = YEAR(DATE('{load_start_date}')) AND month = MONTH(DATE('{load_start_date}')) AND day >= DAY(DATE('{load_start_date}')))
-    ) AND (-- Condition for being on or before the end date
-      year < YEAR(DATE('{load_end_date}')) OR
-      (year = YEAR(DATE('{load_end_date}')) AND month < MONTH(DATE('{load_end_date}'))) OR
-      (year = YEAR(DATE('{load_end_date}')) AND month = MONTH(DATE('{load_end_date}')) AND day <= DAY(DATE('{load_end_date}')))
-    )
-  QUALIFY
-    ROW_NUMBER() OVER(PARTITION BY id_channel, id_message ORDER BY ts_created DESC) = 1
+    whatsapp_messages
+  UNION ALL
+  SELECT
+    id_channel,
+    id_message,
+    id_sauron_session,
+    user_sender,
+    message,
+    user_type,
+    ts_created
+  FROM
+    inapp_messages
 ),
-twilio_data AS (
+messages_w_users AS (
   SELECT DISTINCT
-    m.id_channel,
-    COALESCE(ch.id_session, cht.id_session) AS id_session,
-    COALESCE(t1.id_task, t2.id_task) AS id_task,
     m.id_message,
-    REPLACE(m.user_sender, '+', '') AS user_sender,
-    m.user_type,
+    m.id_channel,
+    m.id_sauron_session,
+    CASE
+      WHEN m.user_sender = 'system' THEN -1
+      WHEN STARTSWITH(m.user_sender, '+') THEN NULL
+      WHEN s.user_data:["user_id"] IS NOT NULL 
+        AND m.user_type = 'User' THEN s.user_data:["user_id"]
+      WHEN u1.id IS NOT NULL THEN u1.id
+    END AS id_user,
+    s.source as origin,
     m.message,
+    m.user_type,
     m.ts_created
   FROM
     messages AS m
   LEFT JOIN
-    datalake_quinto_messenger_clean.channel AS ch
-      ON ch.id_channel = m.id_channel
-      AND ch.ts_created >= '{load_start_date}' - INTERVAL 60 DAY
-  LEFT JOIN
-    datalake_quinto_messenger_clean.chat AS cht
-      ON cht.id_channel = m.id_channel
-      AND cht.ts_created >= '{load_start_date}' - INTERVAL 60 DAY
-  LEFT JOIN
-    datalake_quinto_messenger_clean.task AS t1
-      ON t1.id_channel = ch.id_channel
-      AND t1.ts_created >= '{load_start_date}' - INTERVAL 60 DAY
-  LEFT JOIN
-    datalake_quinto_messenger_clean.task AS t2
-      ON t2.id_chat = cht.id_chat
-      AND t2.ts_created >= '{load_start_date}' - INTERVAL 60 DAY
-),
-messages_with_users AS (
-  SELECT DISTINCT
-    td.id_channel AS sk_channel,
-    MAX(COALESCE(td.id_session, ss.id)) AS sk_session,
-    td.id_message AS sk_message,
-    MAX(
-      CASE
-        WHEN COALESCE(ss.user_data:['user_id'], td.user_sender) = 'system' THEN -1
-        WHEN STARTSWITH(COALESCE(u1.id, u2.id, td.user_sender), '55')
-          AND LENGTH(COALESCE(u1.id, u2.id, td.user_sender)) = 12 THEN -1
-        WHEN CONTAINS(COALESCE(u1.id, u2.id, td.user_sender), '@') THEN -1
-        ELSE COALESCE(u1.id, u2.id, TRY_CAST(td.user_sender AS BIGINT), -1)
-      END
-    ) AS sk_user_sender,
-    td.user_type,
-    td.message,
-    td.ts_created
-  FROM
-    twilio_data AS td
-  LEFT JOIN
-    datalake_sauron_clean.session AS ss
-      ON td.id_task = ss.source_identity
-      AND ss.ts_created >= DATE('{load_start_date}') - INTERVAL 1 YEAR
+    datalake_sauron_clean.session AS s
+      ON s.id = m.id_sauron_session
   LEFT JOIN
     datalake_ebdb_user.user AS u1
-      ON u1.email = td.user_sender
-      AND CONTAINS(td.user_sender, '@')
+      ON u1.email = m.user_sender
+      AND CONTAINS(m.user_sender, '@')
       AND u1.country_code = 'BR'
-  LEFT JOIN
-    datalake_ebdb_user.user AS u2
-      ON REPLACE(u2.main_phone, '+', '') = td.user_sender
-      AND u2.country_code = 'BR'
-  GROUP BY ALL
+  WHERE
+    s.source_environment = 'QuintoandarSupport:OFFBOARDING'
 ),
-origin_infos AS (
-  SELECT 
-    id_session, 
-    origin
-  FROM 
-    datalake_customer_support.chats
-  WHERE 
-    ts_created >= DATE('{load_start_date}') - INTERVAL 1 YEAR
-    AND id_session IS NOT NULL
-),
-messages_with_origins AS (
-  SELECT DISTINCT
-  mwu.sk_channel,
-  mwu.sk_session,
-  mwu.sk_message,
-  mwu.sk_user_sender,
-  oi.origin,
-  mwu.user_type,
-  mwu.message,
-  mwu.ts_created
-FROM
-  messages_with_users AS mwu
+messages_w_tasks AS (
+  SELECT
+    mw.id_channel,
+    mw.id_message,
+    mw.id_sauron_session AS id_session,
+    mw.id_user,
+    mw.origin,
+    mw.message,
+    mw.user_type,
+    mw.ts_created,
+    CASE
+      WHEN LAG(mw.ts_created) OVER(PARTITION BY mw.id_sauron_session ORDER BY mw.ts_created) IS NOT NULL
+        AND LAG(mw.user_type) OVER(PARTITION BY mw.id_sauron_session ORDER BY mw.ts_created) <> mw.user_type
+          THEN DATE_DIFF(SECOND, LAG(mw.ts_created) OVER (PARTITION BY mw.id_sauron_session ORDER BY mw.ts_created), mw.ts_created)
+      ELSE NULL
+    END AS reply_time,
+    c.id_task,
+    c.ts_created as ts_task_twilio_created 
+FROM 
+  messages_w_users AS mw
 LEFT JOIN 
-  origin_infos AS oi
-  ON mwu.sk_session = oi.id_session
+  datalake_customer_support.chats AS c
+    ON c.id_channel = mw.id_channel
+    AND c.id_session = mw.id_sauron_session
+    AND mw.ts_created >= c.ts_created
+WHERE
+  MAKE_DATE(year, month, day) >= '{load_start_date}'
+),
+ai_without_spoc AS (
+  SELECT DISTINCT
+  c.id_channel,
+  m.id_message,
+  c.id_session,
+  c.id_user,
+  c.origin,
+  m.message,
+  CASE WHEN m.role LIKE 'HUMAN' THEN 'User'
+    WHEN m.role LIKE 'ANALYST' THEN 'Analyst'
+    WHEN m.role LIKE 'SYSTEM' THEN 'Bot'
+  ELSE m.role
+  END AS user_type,
+  m.conversation_type,
+  m.reply_time,
+  m.ts_created,
+  NULL AS id_task,
+  NULL AS ts_task_twilio_created 
+FROM 
+  datalake_chatbot.messages AS m
+LEFT JOIN  
+  datalake_customer_support.chats AS c
+    ON c.id_session = m.id_sauron_session
+    AND m.conversation_type = 'HUMAN-AI'
+WHERE
+  MAKE_DATE(year, month, day) >= '{load_start_date}'
+),
+human_without_spoc AS (
+  SELECT 
+  c.id_channel,
+  m.id_message,
+  c.id_session,
+  c.id_user,
+  c.origin,
+  m.message,
+  CASE WHEN m.role LIKE 'HUMAN' THEN 'User'
+    WHEN m.role LIKE 'ANALYST' THEN 'Analyst'
+    WHEN m.role LIKE 'SYSTEM' THEN 'Bot'
+  ELSE m.role
+  END AS user_type,
+  m.conversation_type,
+  m.reply_time,
+  m.ts_created,
+  c.id_task,
+  c.ts_created as ts_task_twilio_created 
+FROM 
+  datalake_chatbot.messages AS m
+LEFT JOIN 
+  datalake_customer_support.chats AS c
+    ON c.id_session = m.id_sauron_session
+    AND m.ts_created >= c.ts_created
+WHERE
+  MAKE_DATE(year, month, day) >= '{load_start_date}'
+QUALIFY
+  ROW_NUMBER() OVER (PARTITION BY m.id_message ORDER BY c.ts_created DESC) = 1
+), 
+all_without_spoc AS (
+  SELECT 
+    id_channel,
+    id_message,
+    id_session,
+    id_user,
+    origin,
+    message,
+    user_type,
+    reply_time,
+    ts_created,
+    id_task,
+    ts_task_twilio_created 
+  FROM 
+    ai_without_spoc 
+  UNION ALL 
+  SELECT 
+    id_channel,
+    id_message,
+    id_session,
+    id_user,
+    origin,
+    message,
+    user_type,
+    reply_time,
+    ts_created,
+    id_task,
+    ts_task_twilio_created 
+  FROM
+   human_without_spoc
+), 
+all_messages_with_tasks AS (
+  SELECT
+    id_channel, 
+    id_message,
+    id_session,
+    id_user,
+    origin,
+    message,
+    user_type,
+    reply_time,
+    ts_created,
+    id_task,
+    ts_task_twilio_created
+  FROM 
+    messages_w_tasks
+  UNION ALL
+  SELECT
+    id_channel, 
+    id_message,
+    id_session,
+    id_user,
+    origin,
+    message,
+    user_type,
+    message_index,
+    reply_time,
+    ts_created,
+    id_task,
+    ts_task_twilio_created
+  FROM 
+    all_without_spoc 
 )
-SELECT DISTINCT
-  sk_channel,
-  sk_session,
-  sk_message,
-  sk_user_sender,
+SELECT 
+  id_channel AS sk_channel, 
+  MD5(id_message) AS sk_message,
+  id_task AS sk_task,
+  id_session AS sk_session,
+  id_user AS sk_user_sender,
   origin,
   user_type,
   message,
-  CASE
-    WHEN LAG(ts_created) OVER (PARTITION BY sk_channel ORDER BY ts_created) IS NOT NULL
-      THEN DATEDIFF(SECOND, LAG(ts_created) OVER (PARTITION BY sk_channel ORDER BY ts_created), ts_created)
-    ELSE NULL
-  END AS reply_time,
+  ROW_NUMBER() OVER(PARTITION BY id_session ORDER BY ts_created) AS message_index,
+  reply_time,
   ts_created,
+  ts_task_twilio_created,
   NOW() AS ts_load
-FROM
- messages_with_origins
+FROM 
+  all_messages_with_tasks
