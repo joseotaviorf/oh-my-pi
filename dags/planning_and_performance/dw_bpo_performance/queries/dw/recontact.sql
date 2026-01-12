1a33e2dea62192e511452e2458c334132d6a5fa3
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2026-01-12 11:06:34 -0300

    Update Recontact query  (#21726)
    * Update recontact file
    
    * Update recontact metadata file
    
    * Add missing cols to recontact metadata file

--- dags/planning_and_performance/dw_bpo_performance/queries/dw/recontact.sql
+++ dags/planning_and_performance/dw_bpo_performance/queries/dw/recontact.sql
@@ -13,17 +13,21 @@ WITH time_metrics AS (
 front_tickets_list AS (
   SELECT DISTINCT
     ft.sk_ticket,
-    LAG(ft.sk_ticket) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS sk_ticket_previous_contact,
-    LEAD(ft.sk_ticket) OVER(PARTITION BY ft.sk_user ORDER BY ft.ts_created) AS sk_ticket_contact_later,
     ft.sk_user,
     ft.sk_main_department,
     ft.sk_taxonomy,
     da.agent_organization,
-    da.email,
+    COALESCE(da.email, da2.email) AS email,
+    ft.channel,
+    CASE
+      WHEN ft.ticket_origin = 'call inapp' THEN UPPER(ft.direction)
+      WHEN ft.ticket_origin IN ('call inbound', 'chat5a') THEN 'INBOUND'
+      WHEN ft.ticket_origin = 'call outbound' THEN 'OUTBOUND'
+      ELSE UPPER(ft.direction)
+    END AS refined_direction,
     dd.team,
     dd.department,
     dd.journey_step,
-    ft.channel,
     ft.ticket_origin,
     dt.theme,
     dt.theme_detail,
@@ -31,17 +35,7 @@ front_tickets_list AS (
     tm.total_minutes_handling_time,
     tm.total_minutes_queue_time,
     tm.total_minutes_talk_time,
-    CASE
-      WHEN LAG(ft.sk_ticket) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) IS NOT NULL THEN 1
-      ELSE 0
-    END AS recontact_flag,
-    LAG(ft.channel) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS previous_contact_channel,
-    LAG(dt.theme) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS previous_contact_taxonomy,
-    LAG(da.email) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS previous_contact_email,
-    LAG(ftc.last_csat_score) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS previous_contact_csat,
-    DATE_DIFF(DATE(ft.ts_created), LAG(DATE(ft.ts_created)) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created)) AS days_since_last_contact,
-    LAG(ft.ts_created) OVER(PARTITION BY ft.sk_user, dd.team ORDER BY ft.ts_created) AS ts_started_previous_contact,
-    DATE_SUB(ft.ts_created, 3) AS search_window_from,
+    DATE_ADD(DAY, -3, ft.ts_created) AS search_window_from,
     ft.ts_created AS ts_started
   FROM
     dw_customer_support.fact_tickets AS ft
@@ -58,87 +52,177 @@ front_tickets_list AS (
     dw_customer_support.dim_taxonomy AS dt
       ON ft.sk_taxonomy = dt.sk_taxonomy
   LEFT JOIN
-    dw_customer_support.dim_analyst AS da
+    dw_customer_support.dim_analyst as da
       ON ft.sk_last_analyst = da.sk_analyst
-      OR ft.sk_last_analyst = da.sk_agent_twilio
+  LEFT JOIN
+    dw_customer_support.dim_analyst as da2
+      ON ft.sk_last_analyst = da2.sk_agent_twilio
   WHERE
-    ft.sk_user <> -1
+    ft.sk_user  <> -1
     AND dd.area = 'CX'
     AND ft.front_or_back = 'front'
-    AND ( (ft.channel = 'chat' AND ft.direction = 'inbound')
-        OR (ft.ticket_origin IN ('call inbound', 'call in app')) )
+    AND ft.channel IN ('call', 'chat')
 ),
 recontact_check AS (
   SELECT
     rc.sk_ticket,
-    channel,
-    ticket_origin,
-    sk_main_department,
-    sk_taxonomy,
-    sk_user,
-    agent_organization,
-    team,
-    journey_step,
-    search_window_from,
+    rc.channel,
+    rc.ticket_origin,
+    rc.sk_main_department,
+    rc.sk_taxonomy,
+    rc.sk_user,
+    rc.agent_organization,
+    rc.team,
+    rc.journey_step,
+    rc.search_window_from,
     rc.ts_started AS search_window_until,
-    days_since_last_contact,
-    theme,
-    theme_detail,
+    DATE_DIFF(
+      DAY,
+      CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team ORDER BY rc.ts_started)
+           THEN LAG(DATE(rc.ts_started)) OVER(PARTITION BY rc.sk_user, rc.team ORDER BY rc.ts_started)
+           ELSE NULL END, 
+      DATE(rc.ts_started)
+    ) AS days_since_last_contact,
+    rc.theme,
+    rc.theme_detail,
     CASE
-      WHEN rc.days_since_last_contact <= 3 AND rc.team IN ('CX Expert') THEN recontact_flag
-      WHEN rc.days_since_last_contact <= 1 AND rc.team IN ('Rental Manager', 'Rental Manager Gold') THEN recontact_flag
+      WHEN DATE_DIFF(DAY, LAG(DATE(rc.ts_started)) OVER(PARTITION BY rc.sk_user, rc.team ORDER BY rc.ts_started), DATE(rc.ts_started)) <= 3 
+      AND rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team ORDER BY rc.ts_started)
+      THEN 1
       ELSE 0
     END AS recontact_flag,
-    sk_ticket_previous_contact,
-    ts_started_previous_contact,
-    previous_contact_channel,
-    0 AS flag_last_completion_reason_idled,
-    previous_contact_taxonomy,
-    csat_score,
-    previous_contact_csat,
-    total_minutes_handling_time,
-    total_minutes_queue_time,
-    total_minutes_talk_time,
-    email,
-    previous_contact_email,
-    department,
-    sk_ticket_contact_later,
-    ts_started
+    CASE
+      WHEN rc.theme is null then 0
+      WHEN DATE_DIFF(DAY, LAG(DATE(rc.ts_started)) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started), DATE(rc.ts_started)) <= 3 
+      AND LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) != rc.ts_started 
+      THEN 1
+      ELSE 0
+    END AS recontact_flag_taxo,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.sk_ticket) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS sk_ticket_previous_contact,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user,rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS ts_started_previous_contact,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.channel) OVER(PARTITION BY rc.sk_user,rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS previous_contact_channel,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.theme) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS previous_contact_taxonomy,
+    rc.csat_score,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.csat_score) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS previous_contact_csat,
+    rc.total_minutes_handling_time,
+    rc.total_minutes_queue_time,
+    rc.total_minutes_talk_time,
+    rc.email,
+    CASE WHEN rc.ts_started != LAG(rc.ts_started) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) THEN LAG(rc.email) OVER(PARTITION BY rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started) ELSE NULL END AS previous_contact_email,
+    rc.department,
+    LEAD(rc.sk_ticket) OVER(PARTITION BY rc.sk_user ORDER BY rc.ts_started) AS sk_ticket_contact_later,
+    LAG(rc.total_minutes_handling_time) OVER(PARTITION BY rc.sk_user ORDER BY rc.ts_started) AS total_minutes_handling_time_previous_contact,
+    rc.refined_direction,
+    CAST(rc.sk_user AS STRING) || '-' ||
+    CASE
+      WHEN rc.department IN (
+        'CX Mudança [FRONT] [POS]',
+        'CX Parceiros Compra e Venda [FRONT]',
+        'CX Parceiros [FRONT] [PRE]',
+        'CX Parceiros da Portaria [FRONT] [PRE]',
+        'CX Propostas [FRONT] [PRE]',
+        'CX Visitas [FRONT] [PRE]',
+        'Consultores imobiliários 5A',
+        'CX Pagamentos [FRONT] [POS]',
+        'CX Reparos [FRONT] [POS]',
+        'CX Rescisão [FRONT] [POS]'
+      ) THEN 'front'
+    END AS unificador_front,
+    rc.ts_started
   FROM
     front_tickets_list AS rc
   WHERE
-    rc.ts_started BETWEEN DATE('{load_start_date}') - INTERVAL '45' DAY AND DATE('{load_end_date}')
-    AND department NOT LIKE '%[CNX]%'
-    AND team IN ('CX Expert', 'Rental Manager', 'Rental Manager Gold')
+    ts_started BETWEEN DATE_TRUNC('MONTH', DATE('{load_start_date}')) - INTERVAL '6' MONTH AND DATE('{load_end_date}')
+    AND refined_direction = 'INBOUND'
+  QUALIFY 
+    ROW_NUMBER() OVER(PARTITION BY rc.sk_ticket, rc.sk_user, rc.team, rc.theme ORDER BY rc.ts_started DESC) = 1
+),
+demand_back_FRC AS (
+  SELECT DISTINCT
+    ft.ts_created AS ts_started,
+    ft.ts_solved,
+    ft.sk_ticket,
+    ft.sk_user,
+    dd.department AS department_back,
+    dd.team AS team_back,
+    CASE
+      WHEN ft.channel = 'email' THEN 'email'
+      ELSE ft.channel
+    END AS channel,
+    CAST(ft.sk_user AS STRING) || '-' ||
+    CASE
+      WHEN dd.department IN(
+        'CX Partners Tarefas [PRE] [BACK]',
+        'CX Propostas Tarefas [PRE] [BACK]',
+        'Aditivos [REP] [POS] [BACK]',
+        'Entrada no imóvel [ONB] [POS] [BACK]',
+        'CX Pagamentos Ativo [POS] [BACK] [PAY]',
+        'Alteração de dados bancários [BACK]')
+      THEN 'front'
+    END AS unificador_back
+  FROM
+    dw_customer_support.fact_tickets AS ft
+  INNER JOIN
+    dw_customer_support.dim_department AS dd
+      ON ft.sk_main_department = dd.sk_department
+  WHERE
+    ft.sk_user  <> -1
+    AND dd.area = 'CX'
+    AND dd.department IN(
+      'CX Partners Tarefas [PRE] [BACK]',
+      'CX Propostas Tarefas [PRE] [BACK]',
+      'Aditivos [REP] [POS] [BACK]',
+      'Entrada no imóvel [ONB] [POS] [BACK]',
+      'CX Pagamentos Ativo [POS] [BACK] [PAY]',
+      'Alteração de dados bancários [BACK]')
+    AND dd.front_or_back = 'back'
+    AND ft.ts_created >= DATE('{load_start_date}') - INTERVAL '6' month
 )
 SELECT
   rc.sk_ticket,
   rc.sk_ticket_previous_contact AS sk_ticket_first_contact,
-  rc.sk_ticket_contact_later as sk_ticket_contact_later,
+  rc.sk_ticket_contact_later,
   rc.sk_user,
   rc.channel,
+  rc.department,
   rc.team,
-  rc.agent_organization AS organization,
   rc.email,
-  rc.previous_contact_taxonomy AS macrotaxo_first_contact,
-  rc.theme as macrotaxo_last_contact,
+  rc.agent_organization as organization,
   rc.theme_detail,
+  rc.previous_contact_taxonomy AS macrotaxo_first_contact,
+  rc.theme AS macrotaxo_last_contact,
   rc.previous_contact_email AS email_first_contact,
   rc.total_minutes_handling_time,
+  rc.total_minutes_handling_time_previous_contact,
   rc.total_minutes_queue_time,
   rc.previous_contact_csat AS csat_first_contact,
   rc.csat_score,
-  CASE
-    WHEN rc.days_since_last_contact <= 0 THEN rc.recontact_flag
-    ELSE 0
-  END AS recontact_flag,
+  ARRAY_JOIN(
+    ARRAY_AGG(
+      CASE
+        WHEN rc.ts_started >= DATE_TRUNC('hour', db.ts_started)
+          AND (rc.ts_started <= DATE_TRUNC('hour', db.ts_solved)
+          OR db.ts_solved IS NULL)
+        THEN CAST(db.sk_ticket AS STRING)
+      ELSE NULL
+    END), '') AS ticket_back,
+  recontact_flag,
+  recontact_flag_taxo,
   rc.search_window_from,
   rc.search_window_until,
   rc.ts_started_previous_contact AS ts_started_first_contact,
-  rc.ts_started as ts_started,
-  YEAR(CURRENT_DATE - 1) AS year,
-  MONTH(CURRENT_DATE - 1) AS month,
-  DAY(CURRENT_DATE - 1) AS day,
+  rc.ts_started,
+  refined_direction,
+  YEAR(rc.ts_started) AS year,
+  MONTH(rc.ts_started) AS month,
+  DAY(rc.ts_started) AS day,
   NOW() AS ts_load
 FROM
   recontact_check AS rc
+LEFT JOIN
+  demand_back_FRC AS db
+    ON rc.unificador_front = db.unificador_back
+WHERE
+  department NOT LIKE '%[WH]%' --caixas da WebHelp agora são identificadas assim
+  AND department NOT LIKE '%[CNX]%'
+GROUP BY ALL
