ca016ed37d231741ec6bd943383adae850e954ef
Conrado Fabri <63262503+dataconra@users.noreply.github.com>
2026-01-14 17:27:16 -0300

    Adding amplitude views for consorcio (#21642)
    * adding amplitude views for consorcio
    
    * Update 170698_consorcio_cta_clicked_events.sql
    
    * Update 170698_consorcio_lead_generated_events.sql
    
    * Update 170698_consorcio_page_viewed_events.sql

---
+++ dags/growth/amplitude_subpartitioned/queries/clean/170698_consorcio_page_viewed_events.sql
@@ -0,0 +1,36 @@
+SELECT
+    id_amplitude,
+    id_user,
+    id_device,
+    id_event,
+    id_app,
+    id_session,
+    event_type,
+    city,
+    country,
+    NULLIF(GET_JSON_OBJECT(event_properties, '$.customer_journey'),'') as ep_costumer_journey,
+    NULLIF(GET_JSON_OBJECT(event_properties, '$.referrer'),'') as ep_referrer,
+    NULLIF(GET_JSON_OBJECT(event_properties, '$.ub_page_name'),'') as ep_ub_page_name,
+    NULLIF(GET_JSON_OBJECT(event_properties, '$.platform'),'') as ep_platform,
+    NULLIF(PARSE_URL(GET_JSON_OBJECT(event_properties, '$.uri'),'QUERY', 'utm_source'),'') as ep_utm_source,
+    NULLIF(PARSE_URL(GET_JSON_OBJECT(event_properties, '$.uri'),'QUERY', 'utm_medium'),'') as ep_utm_medium,
+    NULLIF(PARSE_URL(GET_JSON_OBJECT(event_properties, '$.uri'),'QUERY', 'utm_campaign'),'') as ep_utm_campaign,
+    NULLIF(GET_JSON_OBJECT(user_properties, '$.utm_source'),'') as up_utm_source,
+    NULLIF(GET_JSON_OBJECT(user_properties, '$.utm_medium'),'') as up_utm_medium,
+    NULLIF(GET_JSON_OBJECT(user_properties, '$.utm_campaign'),'') as up_utm_campaign,
+    NULLIF(GET_JSON_OBJECT(user_properties, '$.ab_beakman_consorcio_new_landing_page'), '') as up_ab_beakman_consorcio_new_landing_page,
+    event_properties,
+    user_properties,
+    ts_event,
+    ts_server_uploaded,
+    ts_client_event,
+    ts_client_uploaded,
+    ts_processed,
+    year,
+    month,
+    day
+FROM
+    datalake_amplitude_clean.events
+WHERE
+    id_app = '170698' AND event_type = 'consorcio.page_viewed'
+    AND MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
