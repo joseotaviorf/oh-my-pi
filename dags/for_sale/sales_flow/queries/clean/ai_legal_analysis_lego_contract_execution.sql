859f7c7a44d5d79d33c9fb1727350f66348dc5f1
Thiago Bueno <35686851+tbuenos@users.noreply.github.com>
2026-01-26 17:22:06 -0300

    TJS-5307 - Fix query clean (#21865)
    Co-authored-by: Thiago Bueno <thiago.bueno@BRSPPM-GGH2XX6MJ4.local>

--- dags/for_sale/sales_flow/queries/clean/ai_legal_analysis_lego_contract_execution.sql
+++ dags/for_sale/sales_flow/queries/clean/ai_legal_analysis_lego_contract_execution.sql
@@ -1,10 +1,8 @@
 SELECT
     id,
     sales_flow_id AS id_sales_flow,
-    ccv_id AS id_ccv,
     legocontract_execution_id AS id_legocontract_execution,
     person_id AS id_person,
-    signed_document_token,
     payload_fields_hash,
     legalops_saved_fields,
     created_at AS ts_created,
