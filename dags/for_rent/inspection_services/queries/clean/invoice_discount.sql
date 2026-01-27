6ef6fbee8855310bca3bb9d541a3e1bbf7f9548d
Gustavo Huster <119461813+GustavoHuster@users.noreply.github.com>
2026-01-27 10:12:38 -0300

    Removing id_user from invoice_discount (#21877)
    * removing id_user
    
    * update metadata
    
    * dependencies updated

--- dags/for_rent/inspection_services/queries/clean/invoice_discount.sql
+++ dags/for_rent/inspection_services/queries/clean/invoice_discount.sql
@@ -3,7 +3,6 @@ SELECT
     contract_id AS id_contract,
     inspection_uuid AS uuid_inspection,
     checkpoint_id AS id_checkpoint,
-    CAST(user_id AS BIGINT) AS id_user,
     person_uuid AS uuid_person,
     discount_type,
     inspection_cost,
