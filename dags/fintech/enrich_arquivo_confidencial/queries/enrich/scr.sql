fa6b3c3255dc5dcbb436ec6a391ea54e749529c5
Marcos Mota <94015949+marcosmota@users.noreply.github.com>
2026-01-27 15:23:24 -0300

    fix(CRF-1398): fix SCR data extraction in enrich SQL query (#21852)
    * Refactor SCR data extraction in enrich SQL query
    
    - Updated the extraction logic to handle cases where SCR attributes may be null, introducing a new column `has_scr_attributes`.
    - Changed the source of the JSON data from `raw_data:analysis_output:scr.scr_data` to `attributes:analysis_output:scr.scr_data`.
    - Renamed the `mobs` CTE to `mob_scr` for clarity and adjusted the row numbering logic accordingly.
    - Enhanced the final selection to include the `has_scr_attributes` column and corrected the value assignment for `dat.value`.
    
    * feat: Enhance SCR metadata by adding `has_scr_attributes` field
    
    * refactor: SCR query to improve data handling
    
    * fix: Adjust value calculation in SCR query to improve accuracy
    
    * fix: Correct domain casing in SCR metadata configuration

--- dags/fintech/enrich_arquivo_confidencial/queries/enrich/scr.sql
+++ dags/fintech/enrich_arquivo_confidencial/queries/enrich/scr.sql
@@ -1,80 +1,85 @@
 WITH scr_data AS (
     SELECT
-    id,
-    cpf,
-    `rev_end`,
-    explode_outer(FROM_JSON(raw_data:analysis_output:scr.scr_data,
-        'array<
-        struct<
-            source:string,
-            indirect_risk:double,
-            reference_date:string,
-            operation_count:double,
-            operation_items:array<struct<value:double,
-            domain:string,
-            modality:string,
-            submodality:string,
-            domain_group:string,
-            modality_description:string,
-            submodality_description:string,
-            linked_to_foreign_currency:string>>,
-            assumed_coobligation:string,
-            receive_coobligation:string,
-            start_relationship_date:string,
-            subjudice_operations_count:string,
-            subjudice_operations_value:string,
-            financial_institution_count:string ,
-            disagreement_operations_count:string,
-            disagreement_operations_value:string
-        >
-        >')
-    ) scr_data,
-    aud.ts_created,
-    rev.ts_created as ts_updated
-  FROM datalake_arquivo_confidencial_clean.integration_report_aud AS aud
-  LEFT JOIN datalake_arquivo_confidencial_clean.rev_info AS rev
-    ON rev.rev = aud.rev
-  WHERE integration_provider = 'QI_TECH_SCR'
-    AND aud.ts_created >= DATE('2022-09-01')
-
+        id,
+        cpf,
+        `rev_end`,
+        CASE
+            WHEN aud.attributes:analysis_output:scr.scr_data IS NULL THEN 0
+            ELSE 1
+        END AS has_scr_attributes,
+        explode_outer(FROM_JSON(attributes:analysis_output:scr.scr_data,
+            'array<
+            struct<
+                source:string,
+                indirect_risk:double,
+                reference_date:string,
+                operation_count:double,
+                operation_items:array<struct<value:double,
+                domain:string,
+                modality:string,
+                submodality:string,
+                domain_group:string,
+                modality_description:string,
+                submodality_description:string,
+                linked_to_foreign_currency:string>>,
+                assumed_coobligation:string,
+                receive_coobligation:string,
+                start_relationship_date:string,
+                subjudice_operations_count:string,
+                subjudice_operations_value:string,
+                financial_institution_count:string,
+                disagreement_operations_count:string,
+                disagreement_operations_value:string
+            >
+            >')
+        ) AS scr_data,
+        aud.ts_created,
+        rev.ts_created AS ts_updated
+    FROM
+        datalake_arquivo_confidencial_clean.integration_report_aud AS aud
+    LEFT JOIN
+        datalake_arquivo_confidencial_clean.rev_info AS rev
+            ON rev.rev = aud.rev
+    WHERE
+        integration_provider = 'QI_TECH_SCR'
+        AND aud.ts_created >= DATE('2022-09-01')
 ),
 mobs AS (
-  SELECT
-  distinct
-    *,
-    ROW_NUMBER() OVER (PARTITION BY cpf, ts_created ORDER BY scr_data.reference_date DESC) mob
-  FROM scr_data
+    SELECT
+        *,
+        ROW_NUMBER() OVER (PARTITION BY cpf, ts_created ORDER BY scr_data.reference_date DESC) AS mob
+    FROM
+        scr_data
 ),
 next_up AS (
   SELECT
     *,
-    LEAD(ts_updated) OVER (PARTITION BY cpf, mob ORDER BY ts_created) ts_next_updated
-  FROM mobs
+    LEAD(ts_updated) OVER (PARTITION BY cpf, mob ORDER BY ts_created) AS ts_next_updated
+  FROM
+    mobs
 ),
 operation_items_exploded AS (
-  SELECT
-    id,
-    cpf,
-    `rev_end`,
-    mob,
-    scr_data.reference_date,
-    scr_data.financial_institution_count,
-    scr_data.start_relationship_date,
-    explode_outer(scr_data.operation_items) AS dat,
-    ts_created,
-    ts_updated,
-    ts_next_updated
-FROM next_up
+    SELECT
+        id,
+        cpf,
+        `rev_end`,
+        mob,
+        has_scr_attributes,
+        scr_data.reference_date,
+        scr_data.financial_institution_count,
+        scr_data.start_relationship_date,
+        explode_outer(scr_data.operation_items) AS dat,
+        ts_created,
+        ts_updated,
+        ts_next_updated
+    FROM
+        next_up
 )
-
-SELECT
-distinct
+SELECT DISTINCT
     id,
     cpf,
     `rev_end`,
     mob,
-    reference_date,
-    0.01*dat.value AS `value`,
     dat.domain,
     dat.modality,
     dat.submodality,
@@ -82,9 +87,13 @@ distinct
     dat.modality_description,
     dat.submodality_description,
     dat.linked_to_foreign_currency,
+    0.01*dat.value AS `value`,
     financial_institution_count,
+    has_scr_attributes,
+    reference_date,
     start_relationship_date,
     ts_created,
     ts_updated,
     ts_next_updated
-FROM operation_items_exploded
+FROM
+    operation_items_exploded
