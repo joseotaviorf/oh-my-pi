b48a0f81e04b073e3bc8da3e32348ab2eb4f648c
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2026-01-27 15:57:11 -0300

    Add analyst email masking (#21882)
    * Add area filter for CX service
    
    * Update both aec and atento_test process for filtering analyst email

--- dags/planning_and_performance/reverse_atento_test/spark_jobs/load_reverse_atento.py
+++ dags/planning_and_performance/reverse_atento_test/spark_jobs/load_reverse_atento.py
@@ -6,14 +6,32 @@ from pyspark.sql import functions as F
 
 JOB_NAME = "load_reverse_atento"
 
-
 def cast_void_columns_to_string(df):
-    """Cast void/null type columns to string to avoid parquet write errors."""
+    """
+    Cast void/null type columns to string to avoid parquet write errors.
+    Args:
+        df: A dataframe to cast void/null type columns to string.
+    Returns:
+        A dataframe with void/null type columns cast to string.
+    """
     for field in df.schema.fields:
         if isinstance(field.dataType, NullType):
             df = df.withColumn(field.name, F.col(field.name).cast("string"))
     return df
 
+def create_regex_email_filter(organization_filters):
+    """
+    Create a regex pattern to filter emails from the organization filters.
+    Args:
+        organization_filters: A string containing the organization names.
+    Returns:
+        A regex pattern to filter emails from the organization filters.
+    """
+    orgs = [org.strip().strip("'") for org in organization_filters.strip("()").split(",")]
+    patterns = [f"@{org.lower()}.com.br|@{org.lower()}.com" for org in orgs]
+    regex_pattern = "(" + "|".join(patterns) + ")$"
+    return regex_pattern
+  
 if __name__ == "__main__":
     parser = ArgumentParser(description=JOB_NAME)
 
@@ -91,12 +109,19 @@ if __name__ == "__main__":
                         AND day={execution_date.day}
                 """
             )
-            # Check if any organization columns exist and apply filter to all of them
+
+            # get all columns with "organization" in the name
             organization_columns = [col for col in df.columns if "organization" in col.lower()]
 
             if table_name == 'tickets_perspective':
+                # for this table, we only need to filter by the last agent organization
                 organization_columns = ['last_agent_organization']
+                agent_filters = ['first_agent_email']
+
+            if table_name == 'segments_perspective': 
+                agent_filters = ['last_agent_email']
             
+            # keep only rows with organization in the organization_filters
             if organization_columns:
                 logger.info(f"m=Filtering by organization, columns={organization_columns}, organization_filters={organization_filters}")
                 filter_conditions = " OR ".join([f"{col} IN {organization_filters}" for col in organization_columns])
@@ -105,15 +130,32 @@ if __name__ == "__main__":
             else:
                 logger.info("m=No organization columns found, skipping filter")
             
+            # transform external agent emails from each BPO as non identifiable organization
+            if agent_filters:
+                logger.info(f"m=Transforming agent email, columns={agent_filters}, organization_filters={organization_filters}")
+                regex_pattern = create_regex_email_filter(organization_filters)
+                # overwrite value if it doesn't match any organization email
+                for column in agent_filters:
+                    df = df.withColumn(
+                        column,
+                        F.when(
+                            F.col(column).rlike(regex_pattern),
+                            F.col(column)
+                        ).otherwise(F.lit("Other Organization Analyst"))
+                    )
+                logger.info("m=Agent email transformed successfully")
+            else:
+                logger.info("m=No agent filters found, skipping transformation")
+
             if df.isEmpty():
                 logger.info(f"m=Empty {schema}.{table_name} for execution_date={execution_date_str}!")
             else:
-                # Extract year, month, day for partitioned path
+                # extract year, month, day for partitioned path
                 year = execution_date.strftime("%Y")
                 month = execution_date.strftime("%m")
                 day = execution_date.strftime("%d")
                 
-                # Create partitioned S3 path
+                # create partitioned S3 path
                 s3_path = f"s3a://{bucket}/{partner_name.lower()}/{table_name}/year={year}/month={month}/day={day}/"
                 file_name = f"{table_name}_{year}_{month}_{day}.parquet"
                 
