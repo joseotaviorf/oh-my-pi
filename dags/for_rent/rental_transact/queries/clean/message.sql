68a5b1d96e0d9a038d9de2271d3b75c963b61e40
Gustavo Henrique Brunelli <gustavo@brunelli.net.br>
2026-01-27 09:53:16 -0300

    fix clean message clean layer (#21876)
    Co-authored-by: Gustavo Brunelli <gustavo.brunelli@quintoandar.com.br>

--- dags/for_rent/rental_transact/queries/clean/message.sql
+++ dags/for_rent/rental_transact/queries/clean/message.sql
@@ -1,9 +1,14 @@
-SELECT 
-	id,
-	incremental_id AS id_incremental,
-	destination,
-	headers,
-	payload,
-	creation_datetime AS ts_created
-FROM 
-	datalake_rental_transact_raw.message
+SELECT
+  	id,
+  	incremental_id AS id_incremental,
+  	destination,
+  	headers,
+  	payload,
+  	CASE
+  	  	WHEN published = 0 THEN FALSE
+  	  	ELSE TRUE
+  	END as is_published,
+  	message_partition,
+  	TIMESTAMP_MILLIS(creation_time) AS ts_created
+FROM
+  	datalake_rental_transact_raw.message
