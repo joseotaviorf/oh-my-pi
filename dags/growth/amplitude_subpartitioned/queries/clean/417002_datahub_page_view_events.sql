fc28c8ce24b5e5c4028eb923818f1114041cdf5a
Vitor Carneiro <viitor.augusto@gmail.com>
2026-02-02 11:48:23 -0300

    Dplt 71/feat/clean/create page view events table (#21928)
    * create page view events table and drop unnecessary columns
    
    * metadata review after table changes and creation of page view events

---
+++ dags/growth/amplitude_subpartitioned/queries/clean/417002_datahub_page_view_events.sql
@@ -0,0 +1,24 @@
+SELECT
+    id_amplitude,
+    id_app,
+    id_event,
+    id_session,
+    id_inserted,
+    id_user,
+    city,
+    country,
+    event_type,
+    GET_JSON_OBJECT(event_properties, '$.title') AS page_title,
+    GET_JSON_OBJECT(event_properties, '$.path') AS page_path,
+    GET_JSON_OBJECT(event_properties, '$.prevPathname') AS previous_path,
+    GET_JSON_OBJECT(event_properties, '$.url') AS page_url,
+    GET_JSON_OBJECT(event_properties, '$.search') AS url_query_parameter,
+    DATE(ts_event) AS dt_event,
+    year,
+    month,
+    day
+FROM
+    datalake_amplitude_clean.events
+WHERE
+    id_app = '417002' AND event_type = 'Page View'
+    AND MAKE_DATE(year, month, day) BETWEEN '{load_start_date}' AND '{load_end_date}'
