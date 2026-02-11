f43f8691317982bb811c40392582c33d97df5a64
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2026-02-11 18:41:44 -0300

    Revert "[3P] Update Brokers Supply Processor data ingestion" (#22090)
    * Revert "[3P] Update Brokers Supply Processor data ingestion (#22070)"
    
    This reverts commit 2a48af1cc833f04ead6b2f93a18347be1eca7080.
    
    * Update analytic_event.sql

---
+++ dags/3p_partners/brokers_supply_processor/queries/clean/analytic_event.sql
@@ -0,0 +1,16 @@
+SELECT
+    id,
+    tracking_id AS id_tracking,
+    event_uuid AS uuid_event,
+    lead_uuid AS uuid_lead,
+    event_type,
+    event_properties,
+    TRUE AS has_3p_access_control,
+    event_date AS ts_event,
+    created_at AS ts_created,
+    updated_at AS ts_updated,
+    year,
+    month,
+    day
+FROM
+    datalake_brokers_supply_processor_raw.analytic_event
