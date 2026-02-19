51eaeac37996a23d8a48ce0e8db0894aa0e20950
Andressa Bataglin <andressaapio@hotmail.com>
2026-02-19 16:56:22 -0300

    fix: Update date format in fraud contracts SQL query to MM/dd/yyyy (#22171)
    * fix: Update date format in fraud contracts SQL query to MM/dd/yyyy
    
    * fix: Correct date format in fraud contracts SQL query to m/d/yyyy

--- dags/fintech/gsheets_fintech/queries/clean/fraud_contracts_for_rent.sql
+++ dags/fintech/gsheets_fintech/queries/clean/fraud_contracts_for_rent.sql
@@ -4,6 +4,6 @@ SELECT
     line,
     conclusion_status,
     source,
-    to_timestamp(data_da_investigacao, 'dd/MM/yyyy') as dt_investigation
+    to_timestamp(data_da_investigacao, 'm/d/yyyy') as dt_investigation
 FROM
     datalake_gsheets_raw.fraud_contracts_for_rent
