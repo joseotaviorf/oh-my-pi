d631dbc9b9099e9b4aeae032765a0faacf37fec2
Thiago Bueno <35686851+tbuenos@users.noreply.github.com>
2026-02-25 19:27:21 -0300

    Revert "TJS-4438 - Import new gsheets for Performance ForSale (#22222)" (#22229)
    This reverts commit c21e5a2c35338d3de59b9b48103a75c3baf50e15.

--- dags/for_sale/gsheets_for_sale/queries/clean/performance_fs_hc_faturado.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    division,
-    status AS hc_status,
-    hc AS hc_value,
-    month AS dt_reference
-FROM
-    datalake_gsheets_raw.performance_fs_hc_faturado
--- dags/for_sale/gsheets_for_sale/queries/clean/performance_fs_hc_realizado.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    division,
-    hc AS hc_value,
-    data AS dt_reference
-FROM
-    datalake_gsheets_raw.performance_fs_hc_realizado
