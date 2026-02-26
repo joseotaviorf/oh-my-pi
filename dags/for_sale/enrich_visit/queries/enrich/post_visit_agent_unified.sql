06294d8c8bafd34cf64de949e3fde9580d901f57
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-02-26 10:37:56 -0300

    enrich visit_fup from vsl (#22231)

--- dags/for_sale/enrich_visit/queries/enrich/post_visit_agent_unified.sql
+++ dags/for_sale/enrich_visit/queries/enrich/post_visit_agent_unified.sql
@@ -1,5 +1,4 @@
-WITH
-visit_show AS (
+WITH visit_show AS (
     SELECT
         id_booking,
         type,
@@ -8,23 +7,44 @@ visit_show AS (
         datalake_ebdb_clean.visitor
     QUALIFY
         ROW_NUMBER() OVER(PARTITION BY id_booking, type ORDER BY ts_created DESC) = 1
+),
+visit_status_log AS ( -- This is to handle the case where the visit_fup is not in the booking table (missing data from visit finalization rollout) so the visit finalization is enriched temporarily from visit_status_log table.
+    SELECT
+        id_visit,
+        id_schedule,
+        CASE
+            WHEN event_type = 'VISIT_DONE' THEN 'VaiNegociar'
+            WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('DEMAND_DID_NOT_ATTEND_VISIT', 'AGENT_DID_NOT_ATTEND_VISIT', 'SUPPLY_DID_NOT_ATTEND_VISIT') THEN 'NaoCompareceu'
+            WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('ACCESS_TO_HOUSE_NOT_AUTHORIZED', 'HOUSE_KEYS_NOT_AVAILABLE', 'HOUSE_NO_LONGER_AVAILABLE_FOR_RENT', 'TENANT_LIVING_DID_NOT_ALLOW_VISIT', 'HOUSE_NO_LONGER_AVAILABLE_FOR_SALE') THEN 'EntradaNaoAutorizada'
+        END AS visit_fup,
+        ts_created AS ts_visit_fup
+    FROM
+        datalake_ebdb_clean.visit_status_log
+    WHERE
+        ts_created::DATE >= '2025-01-01'
+        AND event_type IN ('VISIT_DONE', 'VISIT_UNSUCCESSFUL')
+    QUALIFY
+        ROW_NUMBER() OVER(PARTITION BY id_visit ORDER BY id_visit_status_log DESC) = 1
 )
 SELECT
     b.id_visit,
     b.id AS id_schedule,
-    b.visit_fup AS reason,
+    COALESCE(b.visit_fup, vsl.visit_fup) AS reason,
     CASE
-      WHEN b.visit_fup = 'VaiNegociar' THEN 'VISIT_DONE'
-      WHEN b.visit_fup IN ('EntradaNaoAutorizada', 'NaoCompareceu') THEN 'VISIT_UNSUCCESSFUL'
-      ELSE 'ERROR'
+        WHEN COALESCE(b.visit_fup, vsl.visit_fup) = 'VaiNegociar' THEN 'VISIT_DONE'
+        WHEN COALESCE(b.visit_fup, vsl.visit_fup) IN ('EntradaNaoAutorizada', 'NaoCompareceu') THEN 'VISIT_UNSUCCESSFUL'
+        ELSE 'ERROR'
     END AS event_type,
     show_demand.has_attended AS has_demand_attended,
     show_agent.has_attended AS has_agent_attended,
     show_tenant_living.has_attended AS has_tenant_living_attended,
     show_supply.has_attended AS has_supply_attended,
-    b.ts_visit_fup AS ts_post_visit_agent
+    COALESCE(b.ts_visit_fup, vsl.ts_visit_fup) AS ts_post_visit_agent
 FROM
     datalake_ebdb_clean.booking AS b
+LEFT JOIN
+    visit_status_log AS vsl
+        ON b.id = vsl.id_schedule
 LEFT JOIN
     datalake_ebdb_clean.visit AS v
         ON b.id_visit = v.id
@@ -45,7 +65,7 @@ LEFT JOIN
         ON show_supply.id_booking = b.id
         AND show_supply.type = 'Landlord'
 WHERE
-    b.visit_fup IS NOT NULL
+    (b.visit_fup IS NOT NULL OR vsl.visit_fup IS NOT NULL)
     AND b.type = 'Visita'
     AND v.ts_created::DATE >= '2020-01-01'
 QUALIFY
