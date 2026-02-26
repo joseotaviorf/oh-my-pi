31a12fc8e35420faa0db73022e50dbbae0dcdcfc
Gustavo Huster <119461813+GustavoHuster@users.noreply.github.com>
2026-02-26 09:02:57 -0300

    metric rent retention deprecated (#22196)

--- dags/for_rent/metric_rent__retention/queries/metric/ongoing_terminations_overdue_stock.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    DATE(ts_snapshot) AS dt_reference_day,
-    COUNT_IF(is_overdue_stock) AS qtd_overdue_stock,
-    COUNT(is_overdue_stock) AS qtd_ongoing_terminations,
-    ROUND(100.00 * COUNT_IF(is_overdue_stock)/COUNT(is_overdue_stock), 2) AS percent_overdue_stock
-FROM 
-    dw_retention.fact_ongoing_terminations_snapshots
-GROUP BY 
-  1
