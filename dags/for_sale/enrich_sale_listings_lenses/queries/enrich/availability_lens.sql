7a37b23bf3fd5e59698c9ccab4b79a9afe3cc0b6
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-03-02 16:47:37 -0300

    enrich visit_fup in lenses (#22283)

--- dags/for_sale/enrich_sale_listings_lenses/queries/enrich/availability_lens.sql
+++ dags/for_sale/enrich_sale_listings_lenses/queries/enrich/availability_lens.sql
@@ -51,31 +51,55 @@ visits_canceled AS (
   GROUP BY
     1, 2
 ),
+visit_fup_vsl AS ( -- This is to handle the case where the visit_fup is not in the booking table (missing data from visit finalization rollout) so the visit finalization is enriched temporarily from visit_status_log table.
+  SELECT
+    id_visit,
+    id_schedule,
+    CASE
+      WHEN event_type = 'VISIT_DONE' THEN 'VaiNegociar'
+      WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('DEMAND_DID_NOT_ATTEND_VISIT', 'AGENT_DID_NOT_ATTEND_VISIT', 'SUPPLY_DID_NOT_ATTEND_VISIT') THEN 'NaoCompareceu'
+      WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('ACCESS_TO_HOUSE_NOT_AUTHORIZED', 'HOUSE_KEYS_NOT_AVAILABLE', 'HOUSE_NO_LONGER_AVAILABLE_FOR_RENT', 'TENANT_LIVING_DID_NOT_ALLOW_VISIT', 'HOUSE_NO_LONGER_AVAILABLE_FOR_SALE') THEN 'EntradaNaoAutorizada'
+    END AS visit_fup,
+    ts_created AS ts_visit_fup
+  FROM
+    datalake_ebdb_clean.visit_status_log
+  WHERE
+    ts_created::DATE >= '2025-01-01'
+    AND event_type IN ('VISIT_DONE', 'VISIT_UNSUCCESSFUL')
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY id_visit ORDER BY id_visit_status_log DESC) = 1
+),
 visits_completed AS (
   SELECT
-    TO_DATE(CAST(dt_booking AS TIMESTAMP) + FLOOR((slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
-    id_house,
-    COUNT(DISTINCT id) AS visits_completed
+    TO_DATE(CAST(b.dt_booking AS TIMESTAMP) + FLOOR((b.slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(b.slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
+    b.id_house,
+    COUNT(DISTINCT b.id) AS visits_completed
   FROM
     datalake_ebdb_clean.booking AS b
+  LEFT JOIN
+    visit_fup_vsl AS fup_vsl
+      ON b.id = fup_vsl.id_schedule
   WHERE
-    business_context = 'SALE'
-    AND ts_created IS NOT NULL
-    AND visit_fup IN ('NaoGostou', 'Talvez', 'VaiNegociar', 'VisitouSozinho')
+    b.business_context = 'SALE'
+    AND b.ts_created IS NOT NULL
+    AND COALESCE(b.visit_fup, fup_vsl.visit_fup) IN ('NaoGostou', 'Talvez', 'VaiNegociar', 'VisitouSozinho')
   GROUP BY
     1, 2
 ),
 visits_unauthorized_entry AS (
   SELECT
-    TO_DATE(CAST(dt_booking AS TIMESTAMP) + FLOOR((slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
-    id_house,
-    COUNT(DISTINCT id) AS visits_unauthorized_entry
+    TO_DATE(CAST(b.dt_booking AS TIMESTAMP) + FLOOR((b.slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(b.slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS date,
+    b.id_house,
+    COUNT(DISTINCT b.id) AS visits_unauthorized_entry
   FROM
     datalake_ebdb_clean.booking AS b
+  LEFT JOIN
+    visit_fup_vsl AS fup_vsl
+      ON b.id = fup_vsl.id_schedule
   WHERE
-    business_context = 'SALE'
-    AND ts_created IS NOT NULL
-    AND visit_fup = 'EntradaNaoAutorizada'
+    b.business_context = 'SALE'
+    AND b.ts_created IS NOT NULL
+    AND COALESCE(b.visit_fup, fup_vsl.visit_fup) = 'EntradaNaoAutorizada'
   GROUP BY
     1, 2
 ),
--- dags/for_sale/enrich_sale_listings_lenses/queries/enrich/demand_lens.sql
+++ dags/for_sale/enrich_sale_listings_lenses/queries/enrich/demand_lens.sql
@@ -1,15 +1,36 @@
-WITH bookings AS (
+WITH visit_fup_vsl AS ( -- This is to handle the case where the visit_fup is not in the booking table (missing data from visit finalization rollout) so the visit finalization is enriched temporarily from visit_status_log table.
   SELECT
-    id,
-    id_house,
-    visit_fup,
-    TO_DATE(ts_created) AS dt_booking,
-    TO_DATE(CAST(dt_booking AS TIMESTAMP) + FLOOR((slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS dt_visit_completed
+    id_visit,
+    id_schedule,
+    CASE
+      WHEN event_type = 'VISIT_DONE' THEN 'VaiNegociar'
+      WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('DEMAND_DID_NOT_ATTEND_VISIT', 'AGENT_DID_NOT_ATTEND_VISIT', 'SUPPLY_DID_NOT_ATTEND_VISIT') THEN 'NaoCompareceu'
+      WHEN event_type = 'VISIT_UNSUCCESSFUL' AND reason IN ('ACCESS_TO_HOUSE_NOT_AUTHORIZED', 'HOUSE_KEYS_NOT_AVAILABLE', 'HOUSE_NO_LONGER_AVAILABLE_FOR_RENT', 'TENANT_LIVING_DID_NOT_ALLOW_VISIT', 'HOUSE_NO_LONGER_AVAILABLE_FOR_SALE') THEN 'EntradaNaoAutorizada'
+    END AS visit_fup,
+    ts_created AS ts_visit_fup
   FROM
-    datalake_ebdb_clean.booking
+    datalake_ebdb_clean.visit_status_log
   WHERE
-    business_context = 'SALE'
-    AND ts_created IS NOT NULL
+    ts_created::DATE >= '2025-01-01'
+    AND event_type IN ('VISIT_DONE', 'VISIT_UNSUCCESSFUL')
+  QUALIFY
+    ROW_NUMBER() OVER(PARTITION BY id_visit ORDER BY id_visit_status_log DESC) = 1
+),
+bookings AS (
+  SELECT
+    b.id,
+    b.id_house,
+    COALESCE(b.visit_fup, fup_vsl.visit_fup) AS visit_fup,
+    TO_DATE(b.ts_created) AS dt_booking,
+    TO_DATE(CAST(b.dt_booking AS TIMESTAMP) + FLOOR((b.slot_day * 15 / 60)+8) * INTERVAL 1 HOURS + ABS(b.slot_day * 15 % 60) * INTERVAL 1 MINUTES) AS dt_visit_completed
+  FROM
+    datalake_ebdb_clean.booking AS b
+  LEFT JOIN
+    visit_fup_vsl AS fup_vsl
+      ON b.id = fup_vsl.id_schedule
+  WHERE
+    b.business_context = 'SALE'
+    AND b.ts_created IS NOT NULL
 ),
 offers AS (
   SELECT
