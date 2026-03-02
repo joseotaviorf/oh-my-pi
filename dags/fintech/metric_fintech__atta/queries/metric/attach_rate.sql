5cc4f949fd389f39645ad408820884967a8c2d32
Thiago Bueno <35686851+tbuenos@users.noreply.github.com>
2026-03-02 14:14:15 -0300

    TJS-5608 - Remove metric layer atta (#22268)
    * TJS-5608 - Remove metric layer atta
    
    * TJS-5608 - Remove metric layer atta
    
    ---------
    
    Co-authored-by: Thiago Bueno <thiago.bueno@BRSPPM-GGH2XX6MJ4.local>

--- dags/fintech/metric_fintech__atta/queries/metric/attach_rate.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-      DATE_TRUNC('month', TO_DATE(CAST(fcf.sk_credit_analysis_ended_date AS string), 'yyyyMMdd')) AS credit_analysis_ended_month,
-      COUNT(DISTINCT CASE WHEN (dsa.credit_model = 'ATTA') THEN fcf.sk_offer ELSE NULL END)*1.00 / COUNT(DISTINCT fcf.sk_offer) AS attach_rate
-FROM dw_sale.fact_closing_flows fcf
-LEFT JOIN dw_sale.dim_sale_agreement AS dsa
-      ON fcf.sk_offer = dsa.sk_offer WHERE 1=1 AND dsa.credit_model IN ('ATTA','EXTERNAL','UNDEFINED') AND dsa.payment_method LIKE '%FINANCED%' AND (NOT (dsa.is_ccv_canceled ) OR (dsa.is_ccv_canceled ) IS NULL)
-AND fcf.sk_credit_analysis_ended_date <> -1
-GROUP BY 1 ORDER BY 1 DESC
--- dags/fintech/metric_fintech__atta/queries/metric/pre_analysis.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    date_trunc('day',ts_registration) AS dt_pre_analysis,
-    COUNT(DISTINCT sk_offer) AS offer,
-    COUNT(DISTINCT sk_pre_analysis) AS pre_analysis
-
-FROM
-  dw_atta.fact_pre_analysis_proposal_flow
-WHERE ts_registration IS NOT NULL
-GROUP BY 1
