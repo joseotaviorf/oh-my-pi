37fbb7d4f409cc6e68aec81ec4323fc7b2dd169d
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-03-03 14:14:46 -0300

    VSTS-3446-add_exp_1_3 (#22295)
    * add visits_confirmation_agent experiment
    
    * update lineage
    
    * update inner dependencies

--- dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_visitor_identifier.sql
+++ dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_visitor_identifier.sql
@@ -12,9 +12,26 @@ SELECT
     additional_information,
     dt_identifier_started,
     dt_identifier_ended
-  FROM
+FROM
     datalake_for_sale_experiment.exp_1_1_visits_boosting_agents
-  UNION ALL
+UNION ALL
+SELECT
+    CONCAT_WS('_', id_neotribe, id_experiment, identifier_bc) AS id_element_exp,
+    identifier,
+    id_neotribe,
+    id_experiment,
+    name_neotribe,
+    name_experiment,
+    identifier_type,
+    business_context,
+    test_group,
+    documentation_link,
+    additional_information,
+    dt_identifier_started,
+    dt_identifier_ended
+FROM
+    datalake_for_sale_experiment.exp_1_2_visits_boosting_agents_area_increased_to_800m
+UNION ALL
   SELECT
     CONCAT_WS('_', id_neotribe, id_experiment, identifier_bc) AS id_element_exp,
     identifier,
@@ -30,4 +47,4 @@ SELECT
     dt_identifier_started,
     dt_identifier_ended
   FROM
-    datalake_for_sale_experiment.exp_1_2_visits_boosting_agents_area_increased_to_800m
+    datalake_for_sale_experiment.exp_1_3_visits_confirmation_agent
