ebba7c4e54fd06687aaa37924c590d05fdc9150f
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-03-04 10:31:31 -0300

    add experiment visits_confirmation_agent_jan13th_onward (#22312)

---
+++ dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_1_4_visits_confirmation_agent_jan13th_onward.sql
@@ -0,0 +1,42 @@
+SELECT
+    CONCAT_WS('_', v.id_visitor, v.business_context) AS identifier_bc,
+    v.id_visitor AS identifier,
+    1 AS id_neotribe,
+    4 AS id_experiment,
+    'VISITS XP' AS name_neotribe,
+    'visits_confirmation_agent_jan13th_onward' as name_experiment,
+    'VISITOR' as identifier_type,
+    v.business_context,
+    CASE
+      WHEN RIGHT(NULLIF(u.main_phone, ''), 3) >= 500 THEN 'CONTROL'
+      WHEN RIGHT(NULLIF(u.main_phone, ''), 3) <= 499 THEN 'TREATMENT'
+      ELSE 'ERROR'
+    END AS test_group,
+    CAST(NULL AS STRING) AS documentation_link,
+    'Os TP/BP são identificados de acordo com o final do seu telefone.' AS additional_information,
+    MIN(v.ts_created::DATE) AS dt_identifier_started,
+    MAX(v.ts_created::DATE) AS dt_identifier_ended
+FROM
+    datalake_ebdb_clean.visit AS v
+LEFT JOIN
+    datalake_ebdb_clean.user AS u
+        ON v.id_visitor = u.id
+WHERE
+    DATE(v.ts_created) >= DATE('2026-01-13')
+    AND (
+    (DATE(v.ts_created) >= DATE('2025-12-10')
+    AND (RIGHT(NULLIF(u.main_phone, ''), 3) >= 950 OR RIGHT(NULLIF(u.main_phone, ''), 3) <= 049))
+    OR
+    (DATE(v.ts_created) >= DATE('2026-01-19') AND v.business_context='RENT'
+    AND (RIGHT(NULLIF(u.main_phone, ''), 3) >= 900 OR RIGHT(NULLIF(u.main_phone, ''), 3) <= 099))
+    OR
+    (DATE(v.ts_created) >= DATE('2026-01-20') AND v.business_context='RENT'
+    AND (RIGHT(NULLIF(u.main_phone, ''), 3) >= 850 OR RIGHT(NULLIF(u.main_phone, ''), 3) <= 149))
+    OR
+    (DATE(v.ts_created) >= DATE('2026-01-21') AND v.business_context='RENT'
+    AND (RIGHT(NULLIF(u.main_phone, ''), 3) >= 800 OR RIGHT(NULLIF(u.main_phone, ''), 3) <= 199))
+    OR
+    (DATE(v.ts_created) >= DATE('2026-02-04') AND v.business_context='RENT'
+    AND (RIGHT(NULLIF(u.main_phone, ''), 3) >= 500 OR RIGHT(NULLIF(u.main_phone, ''), 3) <= 499))
+    )
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11
--- dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_visitor_identifier.sql
+++ dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_visitor_identifier.sql
@@ -32,7 +32,7 @@ SELECT
 FROM
     datalake_for_sale_experiment.exp_1_2_visits_boosting_agents_area_increased_to_800m
 UNION ALL
-  SELECT
+SELECT
     CONCAT_WS('_', id_neotribe, id_experiment, identifier_bc) AS id_element_exp,
     identifier,
     id_neotribe,
@@ -46,5 +46,22 @@ UNION ALL
     additional_information,
     dt_identifier_started,
     dt_identifier_ended
-  FROM
+FROM
     datalake_for_sale_experiment.exp_1_3_visits_confirmation_agent
+UNION ALL
+SELECT
+    CONCAT_WS('_', id_neotribe, id_experiment, identifier_bc) AS id_element_exp,
+    identifier,
+    id_neotribe,
+    id_experiment,
+    name_neotribe,
+    name_experiment,
+    identifier_type,
+    business_context,
+    test_group,
+    documentation_link,
+    additional_information,
+    dt_identifier_started,
+    dt_identifier_ended
+FROM
+    datalake_for_sale_experiment.exp_1_4_visits_confirmation_agent_jan13th_onward
