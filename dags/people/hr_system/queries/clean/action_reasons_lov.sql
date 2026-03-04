f57f610343829e96a2c6ec6ec90c25ad0808ebef
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-03-04 14:36:55 -0300

    remove action_reasons_lov (#22330)

--- dags/people/hr_system/queries/clean/action_reasons_lov.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    ActionReasonId AS id_action_reason,
-    ActionId AS id_action,
-    ActionReasonCode AS action_reason_code,
-    ActionReason AS action_reason,
-    to_date(StartDate) AS dt_start,
-    to_date(EndDate) AS dt_end,
-    ts_load
-FROM datalake_hr_system_raw.action_reasons_lov
