cd99efaf0263b692fe5a534d0f8352e2ee21af44
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-03-09 14:22:46 -0300

    [DBP-12100] Add dim_absence_type dimension to dw_employee_details (#22392)

---
+++ dags/people/dw_employee_details/queries/dw/dim_absence_type.sql
@@ -0,0 +1,23 @@
+SELECT
+    b.id_absence_type AS sk_absence_type,
+    tl.name AS absence_type,
+    b.maximum_duration AS max_days_allowed,
+    IF(tl.name = 'Licença Sem Vencimentos', FALSE, TRUE) AS is_paid_leave,
+    tl.name IN (
+        'Afastamento Temporário por Acidente de Trabalho',
+        'Afastamento Temporário por Doença',
+        'Licença Parental - Responsável 1',
+        'Licença Parental - Responsável 2'
+    ) AS is_performa_protected,
+    NOW() AS ts_load
+FROM
+    datalake_pin_absence_clean.type AS b
+INNER JOIN
+    datalake_pin_absence_clean.type_translation AS tl
+        ON b.id_absence_type = tl.id_absence_type
+WHERE
+    tl.language = 'US'
+    AND b.dt_effective_started <= CURRENT_DATE()
+    AND b.dt_effective_ended >= CURRENT_DATE()
+    AND tl.dt_effective_started <= CURRENT_DATE()
+    AND tl.dt_effective_ended >= CURRENT_DATE()
