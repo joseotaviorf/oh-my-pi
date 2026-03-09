d92e15c32ed8b3fa05d0079fbdc9a7be35b01d64
Yiyi Ji <69869418+yiyiji@users.noreply.github.com>
2026-03-09 11:04:51 +0000

    FIX missing indirect visits on concierge_demand table  (#22298)
    * change to union
    
    * partition
    
    * join with concierge_contact and not message
    
    * adjust concierge prospect
    
    * fix parenthesis
    
    ---------
    
    Co-authored-by: YIYI JI <yiyi.ji@quintoandar.com.br>

--- dags/conversational_xp/enrich_concierge_health_metrics/queries/enrich/concierge_demand.sql
+++ dags/conversational_xp/enrich_concierge_health_metrics/queries/enrich/concierge_demand.sql
@@ -1,11 +1,65 @@
+WITH visits AS (
+    SELECT
+        id_user,
+        id_house,
+        id_visit,
+        id_phone_session,
+        visit_code,
+        concierge_flow_type,
+        'direct' AS concierge_vb_type,
+        visit_request_channel,
+        visit_event_type,
+        is_direct_visit_booked AS is_visit_booked,
+        is_visit_completed,
+        is_offer_submitted,
+        is_offer_accepted, 
+        is_contract_signed,
+        business_context,
+        NULL AS days_msg2vb,
+        ts_concierge_contact,
+        ts_visit_event_created AS ts_concierge_visit_event,
+        ts_visit_created,
+        year,
+        month,
+        day
+    FROM datalake_search.concierge_direct_vb
+
+    UNION ALL
+
+    SELECT
+        id_user,
+        id_house,
+        id_visit,
+        id_phone_session,
+        visit_code,
+        concierge_flow_type,
+        'indirect' AS concierge_vb_type,
+        visit_request_channel,
+        NULL AS visit_event_type,
+        is_indirect_visit_booked AS is_visit_booked,
+        is_visit_completed,
+        is_offer_submitted,
+        is_offer_accepted, 
+        is_contract_signed,
+        business_context,
+        days_msg2vb,
+        ts_concierge_contact,
+        NULL AS ts_concierge_visit_event,
+        ts_visit_created,
+        year,
+        month,
+        day
+    FROM datalake_search.concierge_indirect_vb
+)
+
 SELECT DISTINCT
-    COALESCE(m.id_user, d.id_user, i.id_user) AS id_user,
+    COALESCE(m.id_user, v.id_user) AS id_user,
     m.id_copilot_session,
     m.user_phone,
     m.id_phone_session,
-    COALESCE(d.id_house, i.id_house) AS id_house_of_vb,
-    COALESCE(d.id_visit, i.id_visit) AS id_visit,
-    COALESCE(d.visit_code, i.visit_code) AS visit_code,
+    v.id_house AS id_house_of_vb,
+    v.id_visit, 
+    v.visit_code,
     COALESCE(m.concierge_flow, 'Unknown') AS concierge_flow,
     COALESCE(m.concierge_flow_type, 'Unknown') AS concierge_flow_type,
     m.has_human_reply,
@@ -24,45 +78,35 @@ SELECT DISTINCT
             OR p.prospect_event_type = 'prospect_churn' 
             OR (p.prospect_event_type <> 'prospect_churn' AND DATE(p.ts_prospect_event) = DATE(m.ts_message_sent))
         )
-        AND (
-            COALESCE(d.is_direct_visit_booked, FALSE) 
-            OR COALESCE(i.is_indirect_visit_booked, FALSE) 
-        )
+        AND COALESCE(v.is_visit_booked, FALSE)
     AS is_concierge_prospect, -- Users who became a prospect by initiating a RENT/SALE flow by booking a visit through concierge (direct or indirect).
-    COALESCE(d.business_context, i.business_context, p.business_context) AS business_context,
-    CASE
-        WHEN d.is_direct_visit_booked THEN 'direct'
-        WHEN i.is_indirect_visit_booked THEN 'indirect'
-        ELSE 'no booking'
-    END AS concierge_vb_type,
-    COALESCE(d.visit_request_channel, i.visit_request_channel) AS visit_request_channel,
-    d.visit_event_type AS concierge_visit_event,
-    COALESCE(d.is_direct_visit_booked, i.is_indirect_visit_booked) AS is_visit_booked,
-    COALESCE(d.is_visit_completed, i.is_visit_completed) AS is_visit_completed,
-    COALESCE(d.is_offer_submitted, i.is_offer_submitted) AS is_offer_submitted,
-    COALESCE(d.is_offer_accepted, i.is_offer_accepted) AS is_offer_accepted,
-    COALESCE(d.is_contract_signed, i.is_contract_signed) AS is_contract_signed,
-    i.days_msg2vb AS days_msg2indirect_vb,
+    COALESCE(v.business_context, p.business_context) AS business_context,
+    COALESCE(v.concierge_vb_type, 'no booking') AS concierge_vb_type,
+    v.visit_request_channel,
+    v.visit_event_type AS concierge_visit_event,
+    v.is_visit_booked,
+    v.is_visit_completed,
+    v.is_offer_submitted,
+    v.is_offer_accepted,
+    v.is_contract_signed,
+    v.days_msg2vb AS days_msg2indirect_vb,
     m.ts_first_outbound_contact,
     m.ts_first_inbound_contact,
     m.ts_first_concierge_contact,
     m.ts_concierge_contact,
     p.ts_prospect_event,
-    d.ts_visit_event_created AS ts_concierge_visit_event,
-    COALESCE(d.ts_visit_created, i.ts_visit_created) AS ts_visit_created,
-    COALESCE(m.year, YEAR(d.ts_visit_created), YEAR(i.ts_visit_created)) AS year,
-    COALESCE(m.month, MONTH(d.ts_visit_created), MONTH(i.ts_visit_created)) AS month,
-    COALESCE(m.day, DAY(d.ts_visit_created), DAY(i.ts_visit_created)) AS day
+    v.ts_concierge_visit_event,
+    v.ts_visit_created,
+    COALESCE(m.year, v.year) AS year,
+    COALESCE(m.month, v.month) AS month,
+    COALESCE(m.day, v.day) AS day
 FROM datalake_search.concierge_messages m
 LEFT JOIN datalake_search.concierge_prospects_aux p
     ON m.id_user = p.id_user
     AND m.ts_concierge_contact = p.ts_concierge_contact
-FULL JOIN datalake_search.concierge_direct_vb d
-    ON m.id_phone_session = d.id_phone_session
-    AND m.ts_message_sent = d.ts_message_sent
-    AND m.concierge_flow_type = d.concierge_flow_type
-FULL JOIN datalake_search.concierge_indirect_vb i
-    ON m.id_phone_session = i.id_phone_session
-    AND m.ts_message_sent = i.ts_message_sent
-    AND m.concierge_flow_type = i.concierge_flow_type
-WHERE COALESCE(MAKE_DATE(m.year, m.month, m.day), MAKE_DATE(d.year, d.month, d.day), MAKE_DATE(i.year, i.month, i.day)) BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_7}) AND DATE('{end_date}')
+FULL JOIN visits v
+    ON m.id_phone_session = v.id_phone_session
+    AND m.ts_concierge_contact = v.ts_concierge_contact
+    AND m.concierge_flow_type = v.concierge_flow_type
+WHERE MAKE_DATE(m.year, m.month, m.day) BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_7}) AND DATE('{end_date}')
+    OR MAKE_DATE(v.year, v.month, v.day) BETWEEN DATE_SUB(DATE('{start_date}'), {days_past_7}) AND DATE('{end_date}')
