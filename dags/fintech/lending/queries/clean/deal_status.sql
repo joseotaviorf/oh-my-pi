54463c2dac001cd7338b4ffac53f4dadfc911b3a
Isabella Joviano <67592547+isabellavjs@users.noreply.github.com>
2026-03-09 16:56:55 -0300

    fix(lending): use med_general cluster and align clean tables metadata (#22402)
    Co-authored-by: isabella.santos <isabella.santos@quintoandar.com.br>

--- dags/fintech/lending/queries/clean/deal_status.sql
+++ dags/fintech/lending/queries/clean/deal_status.sql
@@ -2,7 +2,6 @@ SELECT
     id,
     deal_id AS id_deal,
     status,
-    created_at AS ts_created,
-    updated_at AS ts_updated
-FROM 
+    created_at AS ts_created
+FROM
     datalake_lending_raw.deal_status
--- dags/fintech/lending/queries/clean/installment_status.sql
+++ dags/fintech/lending/queries/clean/installment_status.sql
@@ -2,7 +2,6 @@ SELECT
     id,
     installment_id AS id_installment,
     status,
-    created_at AS ts_created,
-    updated_at AS ts_updated
-FROM 
+    created_at AS ts_created
+FROM
     datalake_lending_raw.installment_status
