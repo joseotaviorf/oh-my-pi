59041cd9e3ebe5b4aa1839c292bfc79d27411362
João Pires <joao.pires@quintoandar.com>
2026-03-12 10:01:15 +0000

    Find XP delete OS abtest (#22446)

--- dags/conversational_xp/enrich_base_metrics_service/queries/enrich/experiment_config.sql
+++ dags/conversational_xp/enrich_base_metrics_service/queries/enrich/experiment_config.sql
@@ -285,30 +285,6 @@ FROM VALUES
                                 ),
            'running', True
         )
-    ),
-
-    -- Open search versus Elastic search
-    (
-        "ab_beakman_search_services_open_search_find",
-        named_struct(
-            'begin_date', DATE('2026-02-26'),
-            'end_date', NULL,
-            'variants', to_json(
-                named_struct(
-                    '0', 'baseline',
-                    '1', 'treatment'
-                )
-            ),
-            'filters', NULL,
-            'metrics', to_json(
-                                    named_struct(
-                                                    'default_metrics', true,
-                                                    'experiment_metrics', array()
-                                                )
-                                ),
-           'running', True
-        )
     )
-
 
 AS experiment_config(experiment_name, config)
