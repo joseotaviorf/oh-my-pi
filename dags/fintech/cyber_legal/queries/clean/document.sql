f4d2a6a93e4e542d968fe156aa70b1689189ab53
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2026-03-18 15:53:39 -0300

    Change document table to incremental load (#22537)
    * Change document table to incremental load
    
    * fix metadata

--- dags/fintech/cyber_legal/queries/clean/document.sql
+++ dags/fintech/cyber_legal/queries/clean/document.sql
@@ -1,13 +1,18 @@
 SELECT
-     DOCOLLID AS id_document_uploader,
-     DOID AS id_document,
-     DOACCT AS account,
-     DOACCTG AS document_group,
-     DODESC AS document_description,
-     base64(DOFILE) AS file_blob,
-     DONAFILE AS file_name,
-     DOSTATUS AS status,
-     DOLSTDT AS dt_last_update,
-     DOADDDT AS dt_document_added,
+    DOCOLLID AS id_document_uploader,
+    DOID AS id_document,
+    DOACCT AS account,
+    DOACCTG AS document_group,
+    DODESC AS document_description,
+    base64(DOFILE) AS file_blob,
+    DONAFILE AS file_name,
+    DOSTATUS AS status,
+    DOLSTDT AS dt_last_update,
+    DOADDDT AS dt_document_added,
+    year,
+    month,
+    day,
     NOW() AS ts_load
 FROM datalake_cyber_legal_raw.document
+WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
