f2ca916616c27c27dc3734838f08b4bdf6f13eb5
fabiosoeiro <73497288+fabiosoeiro@users.noreply.github.com>
2026-03-23 10:08:03 -0300

    Fix campaign cluster data type (#22584)

--- dags/growth/gsheets_growth/queries/clean/supply_campaign_cluster.sql
+++ dags/growth/gsheets_growth/queries/clean/supply_campaign_cluster.sql
@@ -1,11 +1,11 @@
 SELECT
-    CAST(NULLIF(campaign_strategy_intent, '') AS BIGINT) AS campaign_strategy_intent,
-    CAST(NULLIF(campaign_business_context, '') AS BIGINT) AS campaign_business_context,
+    CAST(NULLIF(campaign_strategy_intent, '') AS STRING) AS campaign_strategy_intent,
+    CAST(NULLIF(campaign_business_context, '') AS STRING) AS campaign_business_context,
     CAST(NULLIF(source,'') AS STRING) AS source,
     CAST(NULLIF(medium,'') AS STRING) AS medium,
-    CAST(NULLIF(behavior_type, '') AS DATE) AS behavior_type,
-    CAST(NULLIF(funnel_side, '') AS DATE) AS funnel_side,
-    CAST(NULLIF(campaign_landing_page, '') AS DATE) AS campaign_landing_page,
-    CAST(NULLIF(campaign_cluster, '') AS DATE) AS campaign_cluster
+    CAST(NULLIF(behavior_type, '') AS STRING) AS behavior_type,
+    CAST(NULLIF(funnel_side, '') AS STRING) AS funnel_side,
+    CAST(NULLIF(campaign_landing_page, '') AS STRING) AS campaign_landing_page,
+    CAST(NULLIF(campaign_cluster, '') AS STRING) AS campaign_cluster
 FROM
     datalake_gsheets_raw.supply_campaign_cluster
