1f9d259d7ad0ad51ac5d064b6cdbeb919fa4eff5
Vitor Musachio <62345546+VitorMusachio@users.noreply.github.com>
2026-03-24 10:22:52 -0300

    [RD-3156] Update `hubspot_brokers_history` granularity (#22569)
    * update hubspot brokers
    
    * add custom data quality alert

--- dags/3p_partners/enrich_brokers/queries/enrich/hubspot_brokers_history.sql
+++ dags/3p_partners/enrich_brokers/queries/enrich/hubspot_brokers_history.sql
@@ -10,28 +10,55 @@ WITH brokers_members AS (
       AND NOT(c.is_merged_into_other_company)
   QUALIFY
     ROW_NUMBER() OVER(PARTITION BY c.cnpj ORDER BY c.ts_updated DESC) = 1
+),
+broker_history AS (
+  SELECT
+    cb.sk_broker,
+    ch.id_company AS id_hubspot_company,
+    ch.id_hubspot_owner,
+    ch.cnpj,
+    CASE
+      WHEN ch.sale_lead_status = 'Membro' THEN 'ACTIVE'
+      ELSE 'INACTIVE'
+    END AS broker_status,
+    ch.company_cluster,
+    ch.cluster_performance,
+    ch.ts_updated,
+    TRUE AS has_3p_access_control,
+    LAG(
+      CASE
+        WHEN ch.sale_lead_status = 'Membro' THEN 'ACTIVE'
+        ELSE 'INACTIVE'
+      END
+    ) OVER (PARTITION BY cb.sk_broker ORDER BY ch.ts_updated) AS prev_broker_status,
+    LAG(ch.company_cluster) OVER (PARTITION BY cb.sk_broker ORDER BY ch.ts_updated) AS prev_company_cluster,
+    LAG(ch.cluster_performance) OVER (PARTITION BY cb.sk_broker ORDER BY ch.ts_updated) AS prev_cluster_performance
+  FROM
+    datalake_hubspot.company_history AS ch
+  INNER JOIN
+    brokers_members AS bm
+      ON ch.id_company = bm.id_company
+  INNER JOIN
+    core_brokers.brokers AS cb
+      ON bm.cnpj = cb.cnpj
 )
 SELECT
-  cb.sk_broker,
-  ch.id_company,
-  ch.id_hubspot_owner,
-  CASE
-    WHEN ch.sale_lead_status = 'Membro' THEN 'ACTIVE'
-    ELSE 'INACTIVE'
-  END AS broker_status,
-  ch.company_cluster,
-  ch.cluster_performance,
-  ROW_NUMBER() OVER(PARTITION BY cb.sk_broker ORDER BY ch.ts_updated DESC) = 1 AS is_current,
-  TRUE AS has_3p_access_control,
-  ch.ts_updated,
+  bh.sk_broker,
+  bh.id_hubspot_company,
+  bh.id_hubspot_owner,
+  bh.cnpj,
+  bh.broker_status,
+  bh.company_cluster,
+  bh.cluster_performance,
+  ROW_NUMBER() OVER(PARTITION BY bh.id_hubspot_company ORDER BY bh.ts_updated DESC) AS broker_version,
+  ROW_NUMBER() OVER(PARTITION BY bh.id_hubspot_company ORDER BY bh.ts_updated DESC) = 1 AS is_current,
+  bh.has_3p_access_control,
+  bh.ts_updated,
   CURRENT_TIMESTAMP() AS ts_load
 FROM
-  datalake_hubspot.company_history AS ch
-INNER JOIN
-  brokers_members AS bm
-    ON ch.id_company = bm.id_company
-INNER JOIN
-  core_brokers.brokers AS cb
-    ON bm.cnpj = cb.cnpj
+  broker_history AS bh
 WHERE
-  ch.ts_updated >= '{load_start_date}'
+  bh.broker_status != bh.prev_broker_status
+  OR bh.company_cluster != bh.prev_company_cluster
+  OR bh.cluster_performance != bh.prev_cluster_performance
+  OR bh.prev_broker_status IS NULL
