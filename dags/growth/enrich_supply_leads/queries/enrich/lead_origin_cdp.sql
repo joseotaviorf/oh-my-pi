118cbd6720e6048960b479f4a84a826afc3621e4
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2026-03-26 18:07:44 -0300

    Adding back fallback for Amplitude and Inbound Leads (#22701)

--- dags/growth/enrich_supply_leads/queries/enrich/lead_origin_cdp.sql
+++ dags/growth/enrich_supply_leads/queries/enrich/lead_origin_cdp.sql
@@ -71,6 +71,24 @@ affiliate_type_fix AS (
     lo_base.affiliate_type IS NULL
     AND lo_fix.affiliate_type IS NOT NULL
 ),
+lead_origin_amplitude AS (
+  SELECT
+    COALESCE(formfield_lead_uuid, id_firestore) AS id_lead,
+    id_lead AS id_lead_ebdb,
+    IF(utm_campaign == '', '-1', utm_campaign) AS campaign,
+    IF(utm_medium == '', '-1', utm_medium) AS medium,
+    IF(utm_source == '', '-1', utm_source) AS source,
+    IF(utm_content == '', '-1', utm_content) AS content,
+    IF(utm_term == '', '-1', utm_term) AS term,
+    IF(city == '', '-1', city) AS city,
+    IF(platform == '', '-1', platform) AS platform,
+    ts_event
+  FROM
+    datalake_amplitude_lead.lead_origin AS lo
+  WHERE
+    DATE(ts_event) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+  QUALIFY ROW_NUMBER() OVER(PARTITION BY COALESCE(formfield_lead_uuid, id_firestore, id_lead) ORDER BY ts_event) = 1
+),
 inbound_leads AS (
   SELECT 
     ia.id_lead_ebdb,
@@ -99,13 +117,16 @@ mid_table AS (
     r.id_lead AS id_lead,
     r.id_lead_ebdb AS id_lead_ebdb,
     COALESCE(r.affiliate_type, atf.first_affiliate_type) AS affiliate_type,
-    cdp.egw_utm_campaign AS campaign,
+    COALESCE(cdp.egw_utm_campaign, a.campaign, a2.campaign, ia.utm_campaign) AS campaign,
     COALESCE(
       cdp.egw_utm_medium,
       CASE
         WHEN cdp.egw_referrer_domain IN ('www.google.com', 'www.google.com.br', 'www.bing.com',
           'br.search.yahoo.com', 'r.search.yahoo.com', 'search.brave.com', 'duckduckgo.com') THEN 'seo'
-      END
+      END,
+      a.medium,
+      a2.medium,
+      ia.utm_medium
     ) AS medium,
     COALESCE(
       cdp.egw_utm_source,
@@ -115,18 +136,21 @@ mid_table AS (
         WHEN cdp.egw_referrer_domain IN ('br.search.yahoo.com', 'r.search.yahoo.com') THEN 'yahoo'
         WHEN cdp.egw_referrer_domain = 'search.brave.com' THEN 'brave'
         WHEN cdp.egw_referrer_domain = 'duckduckgo.com' THEN 'duckduckgo'
-      END
+      END,
+      a.source,
+      a2.source,
+      ia.utm_source
     ) AS source,
-    cdp.egw_utm_content AS content,
-    cdp.egw_utm_term AS term,
+    COALESCE(cdp.egw_utm_content, a.content, a2.content, ia.utm_content) AS content,
+    COALESCE(cdp.egw_utm_term, a.term, a2.term, ia.utm_term) AS term,
     r.ops_agent,
     r.ops_partner,
     r.application,
-    r.city,
+    COALESCE(r.city, a.city, a2.city) AS city,
     r.landing_page,
     r.ops_approach,
     r.ops_contact_medium,
-    r.platform,
+    COALESCE(IF(r.platform == '', NULL, r.platform), a.platform, a2.platform) AS platform,
     r.lead_type,
     r.detailed_route,
     r.original_lead,
@@ -140,11 +164,11 @@ mid_table AS (
     ia.id_source_ctwa,
     ia.url_source_ctwa,
     ia.type_source_ctwa,
-    NVL2(cdp.egw_utm_campaign, 'cdp', 'lost_tracking') AS database_tracking_campaign,
-    NVL2(cdp.egw_utm_medium, 'cdp', NVL2(cdp.egw_referrer_domain, 'cdp_referrer', 'lost_tracking')) AS database_tracking_medium,
-    NVL2(cdp.egw_utm_source, 'cdp', NVL2(cdp.egw_referrer_domain, 'cdp_referrer', 'lost_tracking')) AS database_tracking_source,
-    NVL2(cdp.egw_utm_content, 'cdp', 'lost_tracking') AS database_tracking_content,
-    NVL2(cdp.egw_utm_term, 'cdp', 'lost_tracking') AS database_tracking_term,
+    NVL2(cdp.egw_utm_campaign, 'cdp', NVL2(a.campaign, 'amplitude', NVL2(a2.campaign, 'amplitude', NVL2(ia.utm_campaign, 'facebook_api', 'lost_tracking')))) AS database_tracking_campaign,
+    NVL2(cdp.egw_utm_medium, 'cdp', NVL2(cdp.egw_referrer_domain, 'cdp_referrer', NVL2(a.medium, 'amplitude', NVL2(a2.medium, 'amplitude', NVL2(ia.utm_medium, 'facebook_api', 'lost_tracking'))))) AS database_tracking_medium,
+    NVL2(cdp.egw_utm_source, 'cdp', NVL2(cdp.egw_referrer_domain, 'cdp_referrer', NVL2(a.source, 'amplitude', NVL2(a2.source, 'amplitude', NVL2(ia.utm_source, 'facebook_api', 'lost_tracking'))))) AS database_tracking_source,
+    NVL2(cdp.egw_utm_content, 'cdp', NVL2(a.content, 'amplitude', NVL2(a2.content, 'amplitude', NVL2(ia.utm_content, 'facebook_api', 'lost_tracking')))) AS database_tracking_content,
+    NVL2(cdp.egw_utm_term, 'cdp', NVL2(a.term, 'amplitude', NVL2(a2.term, 'amplitude', NVL2(ia.utm_term, 'facebook_api', 'lost_tracking')))) AS database_tracking_term,
     NVL2(r.ctwa_clid, 'rene_descartes', NVL2(ia.ctwa_clid, 'sauron', 'lost_tracking')) AS database_tracking_ctwa,
     r.ts_event
   FROM
@@ -152,6 +176,12 @@ mid_table AS (
   LEFT JOIN
     cdp_utms AS cdp
       ON r.id_lead_ebdb = cdp.id_lead_ebdb
+  LEFT JOIN
+    lead_origin_amplitude AS a
+      ON (a.id_lead = r.id_lead)
+  LEFT JOIN
+    lead_origin_amplitude AS a2
+      ON (r.id_lead_ebdb = a2.id_lead_ebdb)
   LEFT JOIN
     affiliate_type_fix AS atf
       ON (r.id_lead = atf.id_lead)
