22a48c953b344529184ccc35c780c52f54b6bfeb
Juliana Hocihara <62107514+juhocihara@users.noreply.github.com>
2026-04-06 09:46:50 -0300

    Update cyber legal expenses (#22805)

--- dags/fintech/cyber_legal/queries/clean/case_expense.sql
+++ dags/fintech/cyber_legal/queries/clean/case_expense.sql
@@ -25,5 +25,10 @@ SELECT
     EXDTRELCLI AS dt_recovered,
     EXDTRELAGN AS dt_reimbursed,
     EXDTUPD AS ts_updated,
+    year,
+    month,
+    day,
     NOW() AS ts_load
 FROM datalake_cyber_legal_raw.caexpns
+WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
