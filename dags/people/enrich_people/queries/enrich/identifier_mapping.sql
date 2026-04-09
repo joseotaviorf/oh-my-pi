6156d402e9fade653ef33e77cfd7cd3222b268ef
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-04-09 08:54:46 -0300

    DBP-1295 | add employment cycle fields to identifier_mapping (#22872)

--- dags/people/enrich_people/queries/enrich/identifier_mapping.sql
+++ dags/people/enrich_people/queries/enrich/identifier_mapping.sql
@@ -77,10 +77,109 @@ test_users AS (
     WHERE
         type_external_identifier = 'ID_ONDA1'
         AND dt_ended IS NULL
+),
+latest_periods_of_service AS (
+    SELECT
+        id_period_of_service,
+        id_person,
+        dt_started,
+        ts_updated,
+        year,
+        month,
+        day
+    FROM
+        datalake_pin_core_clean.periods_of_service
+    WHERE
+        dt_started IS NOT NULL
+    QUALIFY
+        ROW_NUMBER() OVER (
+            PARTITION BY id_period_of_service
+            ORDER BY
+                ts_updated DESC,
+                year DESC,
+                month DESC,
+                day DESC
+        ) = 1
+),
+transfer_continuation_periods AS (
+    SELECT
+        aa_next.id_period_of_service
+    FROM
+        datalake_pin_core_clean.all_assignments AS aa
+    INNER JOIN
+        datalake_pin_core_clean.all_assignments AS aa_next
+            ON aa_next.id_person = aa.id_person
+            AND aa_next.assignment_sequence = aa.assignment_sequence + 1
+    WHERE
+        aa.assignment_status_type = 'INACTIVE'
+        AND aa.action_code = 'GLB_TRANSFER'
+    QUALIFY
+        ROW_NUMBER() OVER (
+            PARTITION BY aa_next.id_period_of_service
+            ORDER BY aa.dt_effective_started ASC
+        ) = 1
+),
+period_cycle_base AS (
+    SELECT
+        ps.id_person,
+        ps.id_period_of_service,
+        ps.dt_started,
+        tcp.id_period_of_service IS NOT NULL AS is_transfer_continuation
+    FROM
+        latest_periods_of_service AS ps
+    LEFT JOIN
+        transfer_continuation_periods AS tcp
+            ON ps.id_period_of_service = tcp.id_period_of_service
+),
+period_cycle_flags AS (
+    SELECT
+        id_person,
+        id_period_of_service,
+        dt_started,
+        is_transfer_continuation,
+        ROW_NUMBER() OVER (
+            PARTITION BY id_person
+            ORDER BY dt_started ASC, id_period_of_service ASC
+        ) AS period_sequence
+    FROM
+        period_cycle_base
+),
+period_cycle_groups AS (
+    SELECT
+        id_person,
+        id_period_of_service,
+        dt_started,
+        SUM(
+            CASE
+                WHEN period_sequence = 1 THEN 1
+                WHEN is_transfer_continuation THEN 0
+                ELSE 1
+            END
+        ) OVER (
+            PARTITION BY id_person
+            ORDER BY dt_started ASC, id_period_of_service ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS employment_cycle_group
+    FROM
+        period_cycle_flags
+),
+period_continuous_employment_cycles AS (
+    SELECT
+        id_person,
+        id_period_of_service,
+        MIN(dt_started) OVER (
+            PARTITION BY id_person, employment_cycle_group
+        ) AS dt_original_hired,
+        MIN(id_period_of_service) OVER (
+            PARTITION BY id_person, employment_cycle_group
+        ) AS id_continuous_employment_cycle
+    FROM
+        period_cycle_groups
 )
 SELECT
     ca.id_assignment,
     ca.id_period_of_service,
+    pcec.id_continuous_employment_cycle,
     ca.id_person,
     COALESCE(wr.registration, lr.legacy_registration) AS legacy_registration,
     ca.assignment_number,
@@ -118,6 +217,7 @@ SELECT
             pp.dt_started DESC NULLS LAST
     ) = 1 AS is_person_latest_assignment,
     ca.dt_projected_started,
+    pcec.dt_original_hired,
     pp.dt_started,
     pp.dt_actual_termination,
     pp.dt_notified_termination,
@@ -148,3 +248,7 @@ LEFT JOIN
 LEFT JOIN
     datalake_pin_core_clean.periods_of_service AS pp
         ON pp.id_period_of_service = ca.id_period_of_service
+LEFT JOIN
+    period_continuous_employment_cycles AS pcec
+        ON pcec.id_person = ca.id_person
+        AND pcec.id_period_of_service = ca.id_period_of_service
