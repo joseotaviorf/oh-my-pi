60032da9c582c8bc18b54e9766d656da71cbf2e3
alexandreg1 <73649686+alexandreg1@users.noreply.github.com>
2026-04-22 14:49:24 -0300

    updating columns to isaias full process (#23129)

--- dags/conversational_xp/enrich_chatbot/queries/enrich/isaias_conversational_flow.sql
+++ dags/conversational_xp/enrich_chatbot/queries/enrich/isaias_conversational_flow.sql
@@ -1,35 +1,70 @@
 SELECT
-  t.id_session AS id_langfuse_session,
-  MAX(o.name = 'UserNameQualification.post_clarification' AND GET_JSON_OBJECT(o.output, '$.user_name.first_name') IS NOT NULL) AS has_user_name_success,
-  MAX(o.name = 'PropertyAddress.post_clarification' AND CAST(GET_JSON_OBJECT(o.output, '$.property_address_verified') AS BOOLEAN) = TRUE) AS has_property_address_success,
-  MAX(o.name = 'PropertyAddressConfirmation.post_clarification' AND (CAST(GET_JSON_OBJECT(o.output, '$.location_covered') AS BOOLEAN) = TRUE OR CAST(GET_JSON_OBJECT(o.output, '$.location_covered_for_rent') AS BOOLEAN) = TRUE OR CAST(GET_JSON_OBJECT(o.output, '$.location_covered_for_sale') AS BOOLEAN) = TRUE)) AS has_property_address_confirmation_success,
-  MAX(o.name = 'PropertyCity.disqualification' OR o.name = 'PropertyAddressConfirmation.disqualification') AS has_property_address_confirmation_disqualification,
-  MAX(o.name = 'PropertyType.disqualification') AS has_property_type_disqualification,
-  MAX(o.name = 'PropertyType.post_clarification') AS has_property_type_success,
-  MAX(o.name = 'PropertyAvailability.disqualification') AS has_property_availability_disqualification,
-  MAX(o.name = 'PropertyAvailability.post_clarification' AND CAST(GET_JSON_OBJECT(o.output, '$.is_available') AS BOOLEAN) = TRUE) AS has_property_availability_success,
-  MAX(GET_JSON_OBJECT(o.input, '$.lead_id') IS NOT NULL AND o.name = 'RetrieveProspect.clarification') AS has_retrieved_lead,
-  MAX(CASE WHEN GET_JSON_OBJECT(o.input, '$.lead_id') IS NOT NULL AND o.name = 'RetrieveProspect.clarification' THEN GET_JSON_OBJECT(o.input, '$.lead_id') END) AS id_lead_retrieved
+  trc.id_session AS id_langfuse_session,
+  MAX(
+    CASE
+      WHEN obs.name = 'RetrieveProspect.clarification'
+        AND GET_JSON_OBJECT(obs.input, '$.lead_id') IS NOT NULL
+        THEN GET_JSON_OBJECT(obs.input, '$.lead_id')
+    END
+  ) AS id_lead_retrieved,
+  BOOL_OR(obs.name = 'UserNameQualification.post_clarification' AND GET_JSON_OBJECT(obs.output, '$.user_name.first_name') IS NOT NULL) AS has_user_name,
+  BOOL_OR(obs.name = 'AddressCollectorAgent.clarification' AND GET_JSON_OBJECT(obs.output, '$.is_address_confirmed') = 'true') AS has_property_address,
+  BOOL_OR(obs.name = 'ListingType.post_clarification' AND (GET_JSON_OBJECT(obs.output, '$.property_intent.for_rent') = 'true' OR GET_JSON_OBJECT(obs.output, '$.property_intent.for_sale') = 'true')) AS has_business_context,
+  BOOL_OR(obs.name = 'PropertyVacancy.post_clarification' AND GET_JSON_OBJECT(obs.output, '$.is_available') = 'true') AS has_property_vacancy,
+  BOOL_OR(obs.name = 'PropertyDetailsNode.post_clarification') AS has_property_details,
+  BOOL_OR(obs.name = 'PropertyNumbersNode.post_clarification') AS has_property_features,
+  BOOL_OR(obs.name = 'CostsCondo.post_clarification') AS has_property_condo,
+  BOOL_OR(obs.name = 'CostsIPTU.post_clarification') AS has_property_iptu,
+  BOOL_OR(obs.name = 'DraftConfirmation.post_clarification') AS has_property_features_confirmation,
+  BOOL_OR(
+    obs.name IN ('RentPricingSuggestion.post_clarification', 'SalePricingSuggestion.post_clarification')
+    AND (
+      GET_JSON_OBJECT(obs.output, '$.pricing.chosen_rent_price') IS NOT NULL
+      OR GET_JSON_OBJECT(obs.output, '$.pricing.chosen_sale_price') IS NOT NULL
+    )
+  ) AS has_pricing_information,
+  BOOL_OR(obs.name = 'ListAvailablePhotoTimeNode.post_clarification' AND GET_JSON_OBJECT(obs.output, '$.photo_schedule_date') IS NOT NULL) AS has_photo_selected,
+  BOOL_OR(obs.name = 'Submission.post_clarification' AND GET_JSON_OBJECT(obs.output, '$.photo_session_successfully_scheduled') = 'true') AS has_submission,
+  BOOL_OR(obs.name = 'escalate_when_qualified') AS has_escalated_qualified,
+  BOOL_OR(obs.name = 'FAQAgentInputState') AS has_faq_agent_interaction,
+  BOOL_OR(obs.name = 'escalation_node') AS has_escalated_non_qualified,
+  BOOL_OR(obs.name = 'EntryAccessModelNode.post_clarification') AS has_entry_model,
+  BOOL_OR(
+    obs.name = 'EntryAccessModelNode.post_clarification'
+    AND GET_JSON_OBJECT(obs.output, '$.entry_access_model_details.access_model_type') IN ('FRONT_DOOR', 'PASSWORD', 'KEYS_WITH_AGENT')
+  ) AS has_easy_entry_model,
+  BOOL_OR(obs.name = 'PhotoSessionSchedulingAuth.clarification') AS has_auth_request,
+  BOOL_OR(obs.name = 'RetrieveProspect.clarification' AND GET_JSON_OBJECT(obs.input, '$.lead_id') IS NOT NULL) AS has_retrieved_lead,
+  BOOL_OR(GET_JSON_OBJECT(obs.output, '$.is_draft_enabled') = 'true') AS is_full_process,
+  MIN(trc.ts_created) AS ts_session
 FROM
-  datalake_langfuse_clean.observations o
+  datalake_langfuse_clean.observations AS obs
 INNER JOIN
-  datalake_langfuse_clean.traces t 
-    ON o.id_trace = t.id_trace
-WHERE 
-  o.ts_started >= '{load_start_date}'
-  AND t.environment = 'prod' 
-  AND o.name IN (
+  datalake_langfuse_clean.traces AS trc
+    ON obs.id_trace = trc.id_trace
+WHERE
+  obs.ts_started >= TIMESTAMP('{load_start_date}')
+  AND trc.environment = 'prod'
+  AND obs.name IN (
+    'orchestrator_init',
     'UserNameQualification.post_clarification',
-    'PropertyAddress.post_clarification',
-    'PropertyAddressConfirmation.post_clarification',
-    'PropertyCity.disqualification',
-    'PropertyAddressConfirmation.disqualification',
-    'PropertyType.disqualification',
-    'PropertyType.post_clarification',
-    'PropertyAvailability.disqualification',
-    'PropertyAvailability.post_clarification',
-    'escalation_node',
+    'AddressCollectorAgent.clarification',
+    'ListingType.post_clarification',
+    'PropertyVacancy.post_clarification',
+    'PropertyDetailsNode.post_clarification',
+    'PropertyNumbersNode.post_clarification',
+    'CostsCondo.post_clarification',
+    'CostsIPTU.post_clarification',
+    'DraftConfirmation.post_clarification',
+    'RentPricingSuggestion.post_clarification',
+    'SalePricingSuggestion.post_clarification',
+    'ListAvailablePhotoTimeNode.post_clarification',
+    'Submission.post_clarification',
     'escalate_when_qualified',
+    'FAQAgentInputState',
+    'escalation_node',
+    'EntryAccessModelNode.post_clarification',
+    'PhotoSessionSchedulingAuth.clarification',
     'RetrieveProspect.clarification'
   )
 GROUP BY 1
