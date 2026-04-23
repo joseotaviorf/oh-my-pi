b2332b1ee2bd5f476ca2b9e34f5214ae78ad062c
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-04-23 17:33:56 -0300

    feat(dbp-1313): add Oitchau punches incremental ingestion to oitchau_api (#23154)

---
+++ dags/people/oitchau_api/queries/clean/punches.sql
@@ -0,0 +1,42 @@
+SELECT
+    id AS id_punch,
+    uuid AS id_punch_key,
+    CAST(createdBy.id AS BIGINT) AS id_created_by,
+    createdBy.uuid AS id_created_by_profile,
+    createdBy.externalId AS id_created_by_external,
+    CAST(employee.id AS BIGINT) AS id_employee,
+    employee.uuid AS id_employee_profile,
+    employee.externalId AS id_employee_external,
+    locationId AS id_location,
+    `key` AS punch_key,
+    timezone AS timezone_name,
+    `type` AS punch_type,
+    status AS punch_status,
+    reason AS punch_reason,
+    source AS punch_source,
+    validation AS validation_status,
+    lateReason AS late_reason,
+    comment AS punch_comment,
+    minutes AS duration_minutes,
+    createdBy.cpf AS created_by_national_tax_number,
+    createdBy.fullName AS created_by_full_name,
+    createdBy.matricula AS created_by_registration_code,
+    createdBy.pis AS created_by_pis_number,
+    employee.cpf AS employee_national_tax_number,
+    employee.fullName AS employee_full_name,
+    employee.matricula AS employee_registration_code,
+    employee.pis AS employee_pis_number,
+    manual AS is_manual_adjustment,
+    customLocation AS is_custom_location,
+    forceValidated AS is_force_validated,
+    CAST(`date` AS DATE) AS dt_punched,
+    `time` AS punch_time_local,
+    ts_load,
+    year,
+    month,
+    day
+FROM
+    datalake_oitchau_raw.punches
+WHERE
+    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}')
+        AND DATE('{load_end_date}')
