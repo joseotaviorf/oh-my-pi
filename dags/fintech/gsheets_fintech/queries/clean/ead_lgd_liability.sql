9ed638fc86dbb480bdc8ce7cc54ba0e2e29ba29c
Marcos Mota <94015949+marcosmota@users.noreply.github.com>
2026-04-29 09:23:16 -0300

    feat: enhance ead_lgd_liability metadata and SQL query with additional date references and exposure at default coefficient (#23204)

--- dags/fintech/gsheets_fintech/queries/clean/ead_lgd_liability.sql
+++ dags/fintech/gsheets_fintech/queries/clean/ead_lgd_liability.sql
@@ -1,9 +1,12 @@
 SELECT
   TO_DATE(initial_dt_month_reference, 'yyyy-MM-dd') AS initial_dt_month_reference,
   TO_DATE(last_dt_month_reference, 'yyyy-MM-dd') AS last_dt_month_reference,
+  TO_DATE(initial_dt_start_reference, 'yyyy-MM-dd') AS initial_dt_start_reference,
+  TO_DATE(last_dt_start_reference, 'yyyy-MM-dd') AS last_dt_start_reference,
   IF(LOWER(TRIM(is_guarantee_paid)) = 'true', TRUE, FALSE) AS is_guarantee_paid,
   CAST(NULLIF(TRIM(delay_range_start), '') AS INT) AS delay_range_start,
   CAST(NULLIF(TRIM(delay_range_end), '') AS INT) AS delay_range_end,
+  CAST(NULLIF(TRIM(exposure_at_default), '') AS DOUBLE) AS exposure_at_default,
   CAST(NULLIF(TRIM(loss_given_default), '') AS DOUBLE) AS loss_given_default,
   NULLIF(TRIM(ead_lgd_model_version), '') AS ead_lgd_model_version
 FROM
