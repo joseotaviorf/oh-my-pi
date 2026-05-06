4dd22104c49d2d8808bd9987c8ea47cc2bdeb99a
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-05-06 12:23:01 -0300

    VSTS-4197-create-entryaccess-logs-ingestion (#23360)
    * create entryaccess ingestion
    
    * rename clean table
    
    * fix entryaccess name
    
    * update metadata

---
+++ dags/cross/request_logging/queries/clean/entryaccess.sql
@@ -0,0 +1,16 @@
+SELECT
+    GET_JSON_OBJECT(request, '$.request_id') AS id_request,
+    trace_id AS id_trace,
+    GET_JSON_OBJECT(application_payload, '$.domain_info.house_id') AS id_house,
+    CAST(GET_JSON_OBJECT(rpc_protocol, '$.http.status_code') AS BIGINT) AS status_code,
+    GET_JSON_OBJECT(application_payload, '$.tool_name') AS tool,
+    GET_JSON_OBJECT(application_payload, '$.platform_info.chat_bot_name') AS host,
+    timestamp AS ts_request,
+    year,
+    month,
+    day,
+    hour
+FROM
+    datalake_request_logging_raw.entryaccess
+QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY id_request ORDER BY ts_request DESC) = 1
