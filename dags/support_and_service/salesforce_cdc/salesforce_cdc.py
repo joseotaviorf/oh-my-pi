66333952ce157edbcfe202e82cd42a767168de8b
Luisa Rodrigues <91343743+luisarodriguees@users.noreply.github.com>
2026-05-06 16:50:08 -0300

    Revert "fix: task name (#23403)" (#23406)
    This reverts commit 7aeee86e36bfb2e08ac06c180c4fe606ae322604.

--- dags/support_and_service/salesforce_cdc/salesforce_cdc.py
+++ dags/support_and_service/salesforce_cdc/salesforce_cdc.py
@@ -280,7 +280,7 @@ with DAG(
             )
             (
                 execute_job_cluster
-                >> airflow_gate_task
+                >> boto3_ga te_task
                 >> check_appflow_status_task
                 >> decide_appflow_status_task
                 >> raw_task
