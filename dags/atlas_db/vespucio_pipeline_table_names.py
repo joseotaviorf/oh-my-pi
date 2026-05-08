11cc9a995da0a605a2b8df26df2202e787fde898
Gabriel Mota <63614154+gfmota@users.noreply.github.com>
2026-05-08 14:08:44 -0300

    Revert "feat(ADB-1110): vespucio address parser integration (#23197)" (#23459)
    This reverts commit 9dc345637ab09982f7d1c96168ebf6f7fa6851a4.

--- dags/atlas_db/vespucio_pipeline_table_names.py
+++ dags/atlas_db/vespucio_pipeline_table_names.py
@@ -29,7 +29,6 @@ class Tables:
     stage_step_houses = "vespucio_pipeline_delta.stage_step_houses"
     address_details_hash = "vespucio_pipeline_delta.address_details_hash"
     address_details_hasher_link = "vespucio_pipeline_delta.address_details_hasher_link"
-    staged_parsed_complements = "vespucio_pipeline_delta.staged_parsed_complements"
     source_adapter_step_condos = "vespucio_pipeline_delta.source_adapter_step_condos"
     source_adapter_step_houses = "vespucio_pipeline_delta.source_adapter_step_houses"
     extract_step_condos = "vespucio_pipeline_delta.extract_step_condos"
