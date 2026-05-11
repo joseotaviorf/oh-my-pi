8c7245a4b0b430087491f896579582bbc940cdbe
Gabriel Rodrigues <66857715+GabriellRodrigues@users.noreply.github.com>
2026-05-11 20:01:32 -0300

    Adjusting the “if” condition (#23519)

--- bietlejuice/base/sst/core/utils/transforms.py
+++ bietlejuice/base/sst/core/utils/transforms.py
@@ -93,7 +93,7 @@ def apply_schema_remaps(
         if column_name not in df.columns or not accept_new_cols:
             continue
 
-        elif right_type == "null" and accept_new_cols:
+        elif right_type is None and accept_new_cols:
             expr = F.col(column_name)
 
         elif left_type == right_type:
@@ -104,7 +104,7 @@ def apply_schema_remaps(
             expr = remap_struct_expr(column_name, right_type)
 
         # Missing value or unable to convert it
-        elif left_type == "null":
+        elif left_type is None:
             expr = F.lit(None).cast(right_type)
         elif left_type == "string" and right_type == "boolean":
             expr = cast_string_to_boolean(column_name)
