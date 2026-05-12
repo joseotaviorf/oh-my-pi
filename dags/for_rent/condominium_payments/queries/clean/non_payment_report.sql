0807bed8f2ac12d9e8a62c0588417304f6588176
Rafaela Araújo <rafaela.araujo@quintoandar.com.br>
2026-05-12 13:28:26 -0300

    feat(PCBP-1899): add payment sync fields (#23527)
    * feat(non_payment_report): add payment sync fields
    
    * fix(non_payment_report): add required metadata fields

--- dags/for_rent/condominium_payments/queries/clean/non_payment_report.sql
+++ dags/for_rent/condominium_payments/queries/clean/non_payment_report.sql
@@ -1,9 +1,11 @@
 SELECT
     CAST(id AS BIGINT) AS id,
     CAST(contract_id AS BIGINT) AS id_contract,
+    third_party_bill_uuid AS uuid_third_party_bill,
     expense_type,
     created_by,
     notification_source,
+    rental_management_sync_initiated AS is_rental_management_sync_initiated,
     TO_TIMESTAMP(created_at) AS ts_created,
     TO_TIMESTAMP(updated_at) AS ts_updated
 FROM
