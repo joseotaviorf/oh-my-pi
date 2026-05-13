7b651752f902297664241534c4c0051351df41ef
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2026-05-13 15:47:14 -0300

    refactor(aarede-389): hub services member profile and daily allocation (#23608)
    * chore: refactor member_profile tables
    
    * fix: dedup member data

--- dags/agents/enrich_hub_services/queries/enrich/member_hub_allocation.sql
+++ dags/agents/enrich_hub_services/queries/enrich/member_hub_allocation.sql
@@ -1,72 +1,51 @@
-WITH member_profile AS (
-    SELECT
-        mp.id_member_relationship,
-        mp.id_member_profile,
-        mp.id_user,
-        mp.id_parent_member_profile,
-        mp.id_parent_user,
-        mp.id_business_unit,
-        mp.profile,
-        mp.ts_relationship_started,
-        CASE
-            WHEN mp.ts_relationship_ended IS NULL THEN mp.ts_load
-            ELSE mp.ts_relationship_ended - INTERVAL 1 DAY 
-        END AS ts_relationship_ended
-    FROM
-        datalake_hub_services.member_profile AS mp
-)
 SELECT
+    mp.id_member_relationship,
     mp.id_member_profile,
     mp.id_user,
-    u.id_main_user AS id_main_user,
-    u.id_agent,
-    u.uuid_person,
-    ad.uuid_company,
+    mp.id_main_user,
+    mp.id_agent,
+    mp.uuid_person,
+    mp.uuid_company,
     mp.id_parent_member_profile,
     mp.id_parent_user,
-    u_parent.id_main_user AS id_parent_main_user,
-    u_parent.id_agent AS id_parent_agent,
-    u_parent.uuid_person AS uuid_parent_person,
     mp.id_business_unit,
-    bu.hub_name,
-    bu.city_group,
-    bu.city_name,
-    bu.short_region_name,
-    bu.lead_types,
-    bu.business_context,
+    mp.id_parent_main_user,
+    mp.id_parent_agent,
+    mp.uuid_parent_person,
+    mp.id_region,
     mp.profile,
-    ad.agent_type,
-    u.name AS user_name,
-    u.email AS user_email,
-    u.phone_number AS user_phone_number,
-    u.secondary_phone_number AS user_secondary_phone_number,
-    u.cpf AS user_cpf,
-    u_parent.name AS user_parent_name,
-    u_parent.email AS user_parent_email,
-    u_parent.phone_number AS user_parent_phone_number,
-    u_parent.secondary_phone_number AS user_parent_secondary_phone_number,
-    u_parent.cpf AS user_parent_cpf,
+    mp.agent_type,
+    mp.hub_name,
+    mp.city_group,
+    mp.city_name,
+    mp.short_region_name,
+    mp.lead_types,
+    mp.business_context,
+    mp.name AS user_name,
+    mp.email AS user_email,
+    mp.phone_number AS user_phone_number,
+    mp.secondary_phone_number AS user_secondary_phone_number,
+    mp.cpf AS user_cpf,
+    mp.parent_name AS parent_name,
+    mp.parent_email AS parent_email,
+    mp.parent_phone_number AS parent_phone_number,
+    mp.parent_secondary_phone_number,
+    mp.parent_cpf AS parent_cpf,
+    mp.is_member_active_in_period AS is_active,
     aux_date.date AS dt_reference,
     aux_date.year,
     aux_date.month,
     aux_date.day
 FROM
-    member_profile AS mp
+    datalake_hub_services.member_profile AS mp
 JOIN
     datalake_quintoandar.aux_date
-        ON aux_date.date BETWEEN DATE(mp.ts_relationship_started) AND DATE(mp.ts_relationship_ended)
-LEFT JOIN
-    datalake_hub_services.users AS u
-        ON u.id_user = mp.id_user
-LEFT JOIN
-    datalake_hub_services.users AS u_parent
-        ON u_parent.id_user = mp.id_parent_user
-LEFT JOIN
-    datalake_ebdb_clean.agent_data AS ad
-        ON ad.id = u.id_agent
-LEFT JOIN
-    datalake_hub_services.business_unit AS bu
-        ON bu.id_business_unit = mp.id_business_unit
-        AND bu.is_last_region_associated IS TRUE
+        ON aux_date.date BETWEEN DATE(mp.ts_relationship_started) 
+            AND DATE(mp.ts_relationship_ended)
 WHERE
     MAKE_DATE(aux_date.year, aux_date.month, aux_date.day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
+QUALIFY     
+    1 = ROW_NUMBER() OVER(
+        PARTITION BY mp.id_member_profile, aux_date.date
+        ORDER BY mp.ts_relationship_started DESC, mp.ts_relationship_ended DESC
+    )
--- dags/agents/enrich_hub_services/queries/enrich/member_profile.sql
+++ dags/agents/enrich_hub_services/queries/enrich/member_profile.sql
@@ -1,10 +1,12 @@
 WITH member_profile_aud AS (
     SELECT DISTINCT
+        XXHASH64(mpa.id, mpa.id_business_unit, COALESCE(mpa.id_parent_member_profile, -1), mpa.rev) AS id_revision,
+        XXHASH64(mpa.id, mpa.id_business_unit, COALESCE(mpa.id_parent_member_profile, -1)) AS id_member_relationship,
         mpa.id,
         mpa.id_user,
         COALESCE(mpa.id_parent_member_profile, -1) AS id_parent_member_profile,
         mpa.id_business_unit,
-        mpa.rev_end,
+        mpa.rev,
         mpa.profile,
         ROW_NUMBER() OVER(PARTITION BY mpa.id ORDER BY mpa.ts_updated DESC) = 1 AS is_last_update,
         mpa.is_active,
@@ -18,49 +20,98 @@ WITH member_profile_aud AS (
     FROM 
         datalake_hub_services_clean.member_profile_aud AS mpa
     LEFT JOIN 
-        datalake_hub_services_clean.rev_info rev 
+        datalake_hub_services_clean.rev_info AS rev 
             ON rev.id = mpa.rev_end
             AND rev.ts_created >= mpa.ts_created
 ),
-member_profile_ended AS (
+member_profile_last_user AS (
     SELECT
         mpa.id,
-        mpa.id_parent_member_profile,
-        LAST(mpa.id_user) AS id_user,
-        LAST(mpa.is_active) AS is_active,
-        FIRST(mpa.ts_relationship_started) AS ts_relationship_started,
-        LAST(mpa.ts_relationship_ended) AS ts_relationship_ended,
-        LAST(mpa.ts_updated) AS ts_updated
+        mpa.id_user,
+        u.id_main_user,
+        u.id_agent,
+        u.uuid_person,
+        u.name,
+        u.email,
+        u.phone_number,
+        u.secondary_phone_number,
+        u.cpf
+    FROM 
+        datalake_hub_services_clean.member_profile AS mpa
+    JOIN
+        datalake_hub_services.users AS u
+            ON u.id_user = mpa.id_user
+),
+relationship_ended AS (
+    SELECT
+        mpa.id_revision,
+        CASE
+            WHEN mpa.ts_relationship_ended IS NULL AND mpa.is_active IS FALSE THEN mpa.ts_updated
+            WHEN mpa.ts_relationship_ended IS NULL AND mpa.is_active IS TRUE THEN NOW()
+            ELSE mpa.ts_relationship_ended
+        END AS ts_relationship_ended
     FROM
         member_profile_aud AS mpa
-    GROUP BY 1, 2
-    ORDER BY ts_updated
 )
-SELECT DISTINCT
-    XXHASH64(mpa.id, mpa.id_business_unit, mpa.id_parent_member_profile) AS id_member_relationship,
+SELECT
+    mpa.id_revision,
+    mpa.id_member_relationship,
     mpa.id AS id_member_profile,
-    mpe.id_user,
+    mpu.id_user,
+    mpu.id_main_user,
+    mpu.id_agent,
+    mpu.uuid_person,
+    ad.uuid_company,
     mpa.id_parent_member_profile,
-    mpa_parent.id_user AS id_parent_user,
+    mpu_parent.id_user AS id_parent_user,
+    mpu_parent.id_main_user AS id_parent_main_user,
+    mpu_parent.id_agent AS id_parent_agent,
+    mpu_parent.uuid_person AS uuid_parent_person,
     mpa.id_business_unit,
+    bu.id_region,
     mpa.profile,
-    CASE
-        WHEN mpe.ts_relationship_ended IS NULL THEN mpe.is_active
-        ELSE FALSE
-    END AS is_active,
-    mpe.ts_relationship_started,
-    CASE
-        WHEN mpe.ts_relationship_ended IS NULL AND mpe.is_active IS FALSE THEN mpe.ts_updated
-        ELSE mpe.ts_relationship_ended
-    END AS ts_relationship_ended,
+    ad.agent_type,
+    bu.hub_name,
+    bu.city_group,
+    bu.city_name,
+    bu.short_region_name,
+    bu.lead_types,
+    bu.business_context,
+    mpu.name,
+    mpu.email,
+    mpu.phone_number,
+    mpu.secondary_phone_number,
+    mpu.cpf,
+    mpu_parent.name AS parent_name,
+    mpu_parent.email AS parent_email,
+    mpu_parent.phone_number AS parent_phone_number,
+    mpu_parent.secondary_phone_number AS parent_secondary_phone_number,
+    mpu_parent.cpf AS parent_cpf,
+    mpa.is_active AS is_member_active_in_period,
+    ROW_NUMBER() OVER(PARTITION BY mpa.id_member_relationship ORDER BY mpa.ts_updated DESC) = 1 AS is_latest_relationship_state,
+    mpa.ts_relationship_started,
+    mre.ts_relationship_ended,
     NOW() AS ts_load
 FROM 
     member_profile_aud AS mpa
+JOIN 
+    relationship_ended AS mre
+        ON mre.id_revision = mpa.id_revision
 LEFT JOIN
-    member_profile_aud AS mpa_parent
-        ON mpa_parent.id = mpa.id_parent_member_profile
-        AND mpa_parent.is_last_update IS TRUE
-JOIN
-    member_profile_ended AS mpe
-        ON mpe.id = mpa.id
-        AND mpe.id_parent_member_profile = mpa.id_parent_member_profile
+    member_profile_last_user AS mpu
+        ON mpu.id = mpa.id
+LEFT JOIN
+    member_profile_last_user AS mpu_parent
+        ON mpu_parent.id = mpa.id_parent_member_profile
+LEFT JOIN
+    datalake_ebdb_clean.agent_data AS ad
+        ON ad.id = mpu.id_agent
+LEFT JOIN
+    datalake_hub_services.business_unit AS bu
+        ON bu.id_business_unit = mpa.id_business_unit
+        AND bu.ts_region_association_created <= mre.ts_relationship_ended
+QUALIFY     
+    1 = ROW_NUMBER() OVER(
+        PARTITION BY mpa.id_revision 
+        ORDER BY mre.ts_relationship_ended DESC, bu.ts_business_unit_updated DESC
+    )
