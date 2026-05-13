9a9b978f47804e492969169562bffe2172779ff2
Leonardo Soares <leonardo.soares@quintoandar.com.br>
2026-05-13 01:25:03 -0300

    feat(spark): add backward-compatible re-export of SparkContext for legacy jobs (#23585)

--- packages/bietlejuice-runtime/src/bietlejuice/base/spark/__init__.py
+++ packages/bietlejuice-runtime/src/bietlejuice/base/spark/__init__.py
@@ -3,6 +3,7 @@ from bietlejuice.base.spark.spark_dataframe_service import SparkDataFrameService
 from bietlejuice.base.spark.spark_table_storage_format import SparkTableStorageFormat
 
 spark = BaseSparkContext.spark  # backward-compat re-export for legacy spark jobs
+sc = BaseSparkContext.sc  # backward-compat re-export for legacy spark jobs
 
 __all__ = [
     "BaseDBUtils",
@@ -10,4 +11,5 @@ __all__ = [
     "SparkDataFrameService",
     "SparkTableStorageFormat",
     "spark",
+    "sc",
 ]
