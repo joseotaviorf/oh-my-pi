15715a4ac453bf7e59a086796fdfd64a16f5904e
Gabriel Berger <120596108+gab-berger@users.noreply.github.com>
2026-05-18 17:55:19 -0300

    feat(dbp-1411): denormalize hrbp name and email into dim_cost_center (#23711)
    * feat(dbp-1411): denormalize hrbp name and email into dim_cost_center
    
    * refactor(dbp-1411): use is_person_latest_assignment for hrbp dedup
    
    * refactor(dbp-1411): join hrbp by assignment and drop sk_business_partner
    
    * fix(dbp-1411): restore sk_business_partner on dim_cost_center
    
    * fix(dbp-1411): remove unsupported personal_data_classification from metadata

--- dags/people/dw_organization/queries/dw/dim_cost_center.sql
+++ dags/people/dw_organization/queries/dw/dim_cost_center.sql
@@ -1,9 +1,21 @@
+WITH hrbp_info AS (
+    SELECT
+        im.id_assignment,
+        im.person_number,
+        im.name,
+        im.work_email
+    FROM
+        datalake_people.identifier_mapping AS im
+)
 SELECT
     h.sk_cost_center_version,
     h.cost_center_code,
     h.id_organization,
     h.sk_business_partner_assignment,
     h.sk_business_partner,
+    hrbp.person_number AS hrbp_person_number,
+    hrbp.name AS hrbp_name,
+    hrbp.work_email AS hrbp_work_email,
     h.cost_center_name,
     h.business,
     h.product,
@@ -25,3 +37,6 @@ SELECT
     NOW() AS ts_load
 FROM
     datalake_people.cost_center_history AS h
+LEFT JOIN
+    hrbp_info AS hrbp
+    ON hrbp.id_assignment = h.sk_business_partner_assignment
