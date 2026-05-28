b72e13c15be1ff26c601f01803ad215b6243bf9d
Eduardo  Soares <eduardorochasoares@users.noreply.github.com>
2026-05-28 16:24:02 -0300

    feat(dw_employee_details): migrate to EMR cluster preset (#24066)

--- dags/people/dw_employee_details/queries/dw/dim_contact.sql
+++ dags/people/dw_employee_details/queries/dw/dim_contact.sql
@@ -27,173 +27,219 @@ contact_versions AS (
 ),
 personal_email_at_version AS (
     SELECT
-        cv.id_person,
-        cv.dt_effective_started,
-        ea.email_address AS personal_email
-    FROM
-        contact_versions AS cv
-    INNER JOIN
-        datalake_pin_core_clean.email_address AS ea
-            ON cv.id_person = ea.id_person
-            AND ea.email_type = 'H1'
-            AND ea.dt_started <= cv.dt_effective_started
-            AND (ea.dt_ended >= cv.dt_effective_started OR ea.dt_ended IS NULL)
-    QUALIFY
-        ROW_NUMBER() OVER (
-            PARTITION BY
-                cv.id_person,
-                cv.dt_effective_started
-            ORDER BY
-                ea.dt_started DESC
-        ) = 1
+        id_person,
+        dt_effective_started,
+        personal_email
+    FROM (
+        SELECT
+            cv.id_person,
+            cv.dt_effective_started,
+            ea.email_address AS personal_email,
+            ROW_NUMBER() OVER (
+                PARTITION BY cv.id_person, cv.dt_effective_started
+                ORDER BY ea.dt_started DESC
+            ) AS _rn
+        FROM
+            contact_versions AS cv
+        INNER JOIN
+            datalake_pin_core_clean.email_address AS ea
+                ON cv.id_person = ea.id_person
+                AND ea.email_type = 'H1'
+                AND ea.dt_started <= cv.dt_effective_started
+                AND (ea.dt_ended >= cv.dt_effective_started OR ea.dt_ended IS NULL)
+    )
+    WHERE _rn = 1
 ),
 phone_at_version AS (
     SELECT
-        cv.id_person,
-        cv.dt_effective_started,
-        p.country_code_number,
-        p.area_code,
-        p.phone_number
-    FROM
-        contact_versions AS cv
-    INNER JOIN
-        datalake_pin_core_clean.phone AS p
-            ON cv.id_primary_phone = p.id_phone
-            AND p.dt_started <= cv.dt_effective_started
-            AND (p.dt_ended >= cv.dt_effective_started OR p.dt_ended IS NULL)
-    QUALIFY
-        ROW_NUMBER() OVER (
-            PARTITION BY
-                cv.id_person,
-                cv.dt_effective_started
-            ORDER BY
-                p.dt_started DESC
-        ) = 1
+        id_person,
+        dt_effective_started,
+        country_code_number,
+        area_code,
+        phone_number
+    FROM (
+        SELECT
+            cv.id_person,
+            cv.dt_effective_started,
+            p.country_code_number,
+            p.area_code,
+            p.phone_number,
+            ROW_NUMBER() OVER (
+                PARTITION BY cv.id_person, cv.dt_effective_started
+                ORDER BY p.dt_started DESC
+            ) AS _rn
+        FROM
+            contact_versions AS cv
+        INNER JOIN
+            datalake_pin_core_clean.phone AS p
+                ON cv.id_primary_phone = p.id_phone
+                AND p.dt_started <= cv.dt_effective_started
+                AND (p.dt_ended >= cv.dt_effective_started OR p.dt_ended IS NULL)
+    )
+    WHERE _rn = 1
 ),
 github_at_version AS (
     SELECT
-        cv.id_person,
-        cv.dt_effective_started,
-        pl.github_username
-    FROM
-        contact_versions AS cv
-    INNER JOIN
-        datalake_pin_core_clean.people_legislative AS pl
-            ON cv.id_person = pl.id_person
-            AND pl.legislation_code = 'BR'
-            AND pl.dt_effective_started <= cv.dt_effective_started
-            AND (pl.dt_effective_ended >= cv.dt_effective_started OR pl.dt_effective_ended IS NULL)
-    QUALIFY
-        ROW_NUMBER() OVER (
-            PARTITION BY
-                cv.id_person,
-                cv.dt_effective_started
-            ORDER BY
-                pl.dt_effective_started DESC
-        ) = 1
+        id_person,
+        dt_effective_started,
+        github_username
+    FROM (
+        SELECT
+            cv.id_person,
+            cv.dt_effective_started,
+            pl.github_username,
+            ROW_NUMBER() OVER (
+                PARTITION BY cv.id_person, cv.dt_effective_started
+                ORDER BY pl.dt_effective_started DESC
+            ) AS _rn
+        FROM
+            contact_versions AS cv
+        INNER JOIN
+            datalake_pin_core_clean.people_legislative AS pl
+                ON cv.id_person = pl.id_person
+                AND pl.legislation_code = 'BR'
+                AND pl.dt_effective_started <= cv.dt_effective_started
+                AND (pl.dt_effective_ended >= cv.dt_effective_started OR pl.dt_effective_ended IS NULL)
+    )
+    WHERE _rn = 1
 ),
 address_at_version AS (
     SELECT
-        cv.id_person,
-        cv.dt_effective_started,
-        a.street,
-        a.number AS address_number,
-        a.complement AS address_complement,
-        a.neighborhood AS address_district,
-        a.postal_code AS address_zip_code,
-        a.town_or_city AS address_city,
-        a.state AS address_state,
-        a.country_code AS address_country
-    FROM
-        contact_versions AS cv
-    INNER JOIN
-        datalake_pin_core_clean.address AS a
-            ON cv.id_mailing_address = a.id_address
-            AND a.dt_effective_started <= cv.dt_effective_started
-            AND (a.dt_effective_ended >= cv.dt_effective_started OR a.dt_effective_ended IS NULL)
-    QUALIFY
-        ROW_NUMBER() OVER (
-            PARTITION BY
-                cv.id_person,
-                cv.dt_effective_started
-            ORDER BY
-                a.dt_effective_started DESC
-        ) = 1
-)
-SELECT
-    MD5(CONCAT_WS('|', CAST(cv.id_person AS STRING), CAST(cv.dt_effective_started AS STRING))) AS sk_contact_version,
-    cv.person_number,
-    pe.personal_email,
-    pv.country_code_number AS phone_country_code,
-    pv.area_code AS phone_area_code,
-    pv.phone_number AS phone_number,
-    CONCAT_WS(' ', pv.country_code_number, pv.area_code, pv.phone_number) AS full_phone_number,
-    gh.github_username,
-    av.street AS address_street,
-    av.address_number,
-    av.address_complement,
-    av.address_district,
-    av.address_zip_code,
-    av.address_city,
-    av.address_state,
-    av.address_country,
-    CONCAT_WS(
-        ', ',
-        av.street,
-        CAST(av.address_number AS STRING),
+        id_person,
+        dt_effective_started,
+        street,
+        address_number,
+        address_complement,
+        address_district,
+        address_zip_code,
+        address_city,
+        address_state,
+        address_country
+    FROM (
+        SELECT
+            cv.id_person,
+            cv.dt_effective_started,
+            a.street,
+            a.number AS address_number,
+            a.complement AS address_complement,
+            a.neighborhood AS address_district,
+            a.postal_code AS address_zip_code,
+            a.town_or_city AS address_city,
+            a.state AS address_state,
+            a.country_code AS address_country,
+            ROW_NUMBER() OVER (
+                PARTITION BY cv.id_person, cv.dt_effective_started
+                ORDER BY a.dt_effective_started DESC
+            ) AS _rn
+        FROM
+            contact_versions AS cv
+        INNER JOIN
+            datalake_pin_core_clean.address AS a
+                ON cv.id_mailing_address = a.id_address
+                AND a.dt_effective_started <= cv.dt_effective_started
+                AND (a.dt_effective_ended >= cv.dt_effective_started OR a.dt_effective_ended IS NULL)
+    )
+    WHERE _rn = 1
+),
+final_joined AS (
+    SELECT
+        MD5(CONCAT_WS('|', CAST(cv.id_person AS STRING), CAST(cv.dt_effective_started AS STRING))) AS sk_contact_version,
+        cv.person_number,
+        pe.personal_email,
+        pv.country_code_number AS phone_country_code,
+        pv.area_code AS phone_area_code,
+        pv.phone_number AS phone_number,
+        CONCAT_WS(' ', pv.country_code_number, pv.area_code, pv.phone_number) AS full_phone_number,
+        gh.github_username,
+        av.street AS address_street,
+        av.address_number,
         av.address_complement,
         av.address_district,
         av.address_zip_code,
         av.address_city,
         av.address_state,
-        av.address_country
-    ) AS full_address,
-    cv.dt_effective_started AS dt_valid_from,
-    COALESCE(
-        LEAD(cv.dt_effective_started) OVER (
-            PARTITION BY
-                cv.id_person
-            ORDER BY
-                cv.dt_effective_started
-        ) - INTERVAL '1 DAY',
-        DATE('9999-12-31')
-    ) AS dt_valid_to,
-    (
-        cv.dt_effective_started <= CURRENT_DATE
-        AND COALESCE(
+        av.address_country,
+        CONCAT_WS(
+            ', ',
+            av.street,
+            CAST(av.address_number AS STRING),
+            av.address_complement,
+            av.address_district,
+            av.address_zip_code,
+            av.address_city,
+            av.address_state,
+            av.address_country
+        ) AS full_address,
+        cv.dt_effective_started AS dt_valid_from,
+        COALESCE(
             LEAD(cv.dt_effective_started) OVER (
-                PARTITION BY
-                    cv.id_person
-                ORDER BY
-                    cv.dt_effective_started
+                PARTITION BY cv.id_person
+                ORDER BY cv.dt_effective_started
             ) - INTERVAL '1 DAY',
             DATE('9999-12-31')
-        ) >= CURRENT_DATE
-    ) AS is_current,
-    NOW() AS ts_load
-FROM
-    contact_versions AS cv
-LEFT JOIN
-    personal_email_at_version AS pe
-        ON cv.id_person = pe.id_person
-        AND cv.dt_effective_started = pe.dt_effective_started
-LEFT JOIN
-    phone_at_version AS pv
-        ON cv.id_person = pv.id_person
-        AND cv.dt_effective_started = pv.dt_effective_started
-LEFT JOIN
-    github_at_version AS gh
-        ON cv.id_person = gh.id_person
-        AND cv.dt_effective_started = gh.dt_effective_started
-LEFT JOIN
-    address_at_version AS av
-        ON cv.id_person = av.id_person
-        AND cv.dt_effective_started = av.dt_effective_started
-QUALIFY
-    ROW_NUMBER() OVER (
-        PARTITION BY
-            cv.id_person,
-            cv.dt_effective_started
-        ORDER BY
-            cv.dt_effective_ended DESC NULLS LAST
-    ) = 1
+        ) AS dt_valid_to,
+        (
+            cv.dt_effective_started <= CURRENT_DATE
+            AND COALESCE(
+                LEAD(cv.dt_effective_started) OVER (
+                    PARTITION BY cv.id_person
+                    ORDER BY cv.dt_effective_started
+                ) - INTERVAL '1 DAY',
+                DATE('9999-12-31')
+            ) >= CURRENT_DATE
+        ) AS is_current,
+        NOW() AS ts_load,
+        cv.id_person AS _id_person,
+        cv.dt_effective_started AS _dt_effective_started,
+        cv.dt_effective_ended AS _dt_effective_ended
+    FROM
+        contact_versions AS cv
+    LEFT JOIN
+        personal_email_at_version AS pe
+            ON cv.id_person = pe.id_person
+            AND cv.dt_effective_started = pe.dt_effective_started
+    LEFT JOIN
+        phone_at_version AS pv
+            ON cv.id_person = pv.id_person
+            AND cv.dt_effective_started = pv.dt_effective_started
+    LEFT JOIN
+        github_at_version AS gh
+            ON cv.id_person = gh.id_person
+            AND cv.dt_effective_started = gh.dt_effective_started
+    LEFT JOIN
+        address_at_version AS av
+            ON cv.id_person = av.id_person
+            AND cv.dt_effective_started = av.dt_effective_started
+)
+SELECT
+    sk_contact_version,
+    person_number,
+    personal_email,
+    phone_country_code,
+    phone_area_code,
+    phone_number,
+    full_phone_number,
+    github_username,
+    address_street,
+    address_number,
+    address_complement,
+    address_district,
+    address_zip_code,
+    address_city,
+    address_state,
+    address_country,
+    full_address,
+    dt_valid_from,
+    dt_valid_to,
+    is_current,
+    ts_load
+FROM (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY _id_person, _dt_effective_started
+            ORDER BY _dt_effective_ended DESC NULLS LAST
+        ) AS _rn
+    FROM final_joined
+)
+WHERE _rn = 1
