049f5ae6d3181364d3b4c3424ecc3d68cc6a15bd
Guilherme Muzzi da Rocha <101020688+gui-muzzi@users.noreply.github.com>
2026-05-28 09:27:22 -0300

    add visits triangulation experiment (#24045)

---
+++ dags/for_sale/enrich_for_sale_experiment/queries/enrich/exp_1_16_visits_triangulation_owner.sql
@@ -0,0 +1,29 @@
+SELECT
+    CONCAT_WS('_', h.id_user, v.business_context) AS identifier_bc,
+    h.id_user AS identifier,
+    1 AS id_neotribe,
+    16 AS id_experiment,
+    'VISITS XP' AS name_neotribe,
+    'visits_triangulation_owner' as name_experiment,
+    'OWNER' as identifier_type,
+    v.business_context,
+    CASE
+      WHEN RIGHT(NULLIF(u.main_phone, ''), 1) IN (1,3,4,6,9) THEN 'CONTROL'
+      WHEN RIGHT(NULLIF(u.main_phone, ''), 1) IN (0,2,5,7,8) THEN 'TREATMENT'
+      ELSE 'ERROR'
+    END AS test_group,
+    CAST(NULL AS STRING) AS documentation_link,
+    'Os Owners são identificados de acordo com o dígito final do seu telefone.' AS additional_information,
+    MIN(v.ts_created::DATE) AS dt_identifier_started,
+    MAX(v.ts_created::DATE) AS dt_identifier_ended
+FROM
+    datalake_ebdb_clean.visit AS v
+LEFT JOIN
+    datalake_ebdb_clean.house AS h
+        ON v.id_house = h.id
+LEFT JOIN
+    datalake_ebdb_clean.user AS u
+        ON h.id_user = u.id
+WHERE
+    DATE(v.ts_created) >= DATE('2026-05-27')
+GROUP BY 1,2,3,4,5,6,7,8,9,10,11
