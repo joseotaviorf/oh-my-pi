b7d1818bd637e8b5116535bdc2e59ca0cea7a536
João Gabriel <jgabsfernandes@gmail.com>
2026-06-01 16:16:13 -0300

    chore: add ebdb clean for listing_quality_analysis_request (#24246)

---
+++ dags/growth/ebdb_listing_quality/queries/clean/listing_quality_analysis_request.sql
@@ -0,0 +1,7 @@
+SELECT id,
+       house_id AS id_house,
+       analysis_requested,
+       listing_quality_id AS id_listing_quality,
+       criadoem AS ts_created,
+       atualizadoem AS ts_updated
+FROM datalake_ebdb_raw.listingqualityanalysisrequest
