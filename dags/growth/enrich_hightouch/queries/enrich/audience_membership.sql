0c406e62c856e77bdc06706fb0660cc6f4fba914
Isadora Gallerani <isadora.gallerani@quintoandar.com.br>
2026-06-11 17:20:58 -0300

    [CRMG-2818] Adjusting HighTouch pipelines after SQL Warehouse deprecation (#24472)
    * [CRMG-2818] Adjusting HighTouch pipelines after SQL Warehouse deprecation
    
    * fix dependencies

--- dags/growth/enrich_hightouch/queries/enrich/audience_membership.sql
+++ dags/growth/enrich_hightouch/queries/enrich/audience_membership.sql
@@ -7,22 +7,6 @@ SELECT
     year,
     month,
     day
-FROM
-    datalake_hightouch_logs_clean.audience_membership_databricks
-WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-
-UNION ALL
-
-SELECT
-    id_audience,
-    id_row,
-    ht_event_type,
-    ht_split_group,
-    ts_ht,
-    year,
-    month,
-    day
 
 FROM
     datalake_hightouch_logs_clean.audience_membership_trino
--- dags/growth/enrich_hightouch/queries/enrich/sync_runs.sql
+++ dags/growth/enrich_hightouch/queries/enrich/sync_runs.sql
@@ -1,46 +1,12 @@
 SELECT
     id_sync,
     id_sync_run,
-     id_model,
-    primary_key,
-    destination,
-    model_name,
-    error,
-    status,
-
-    num_planned_add,
-    num_planned_change,
-    num_planned_remove,
-    num_attempted_add,
-    num_attempted_change,
-    num_attempted_remove,
-    num_succeeded_add,
-    num_succeeded_change,
-    num_succeeded_remove,
-    num_failed_add,
-    num_failed_change,
-    num_failed_remove,
-
-    ts_started,
-    ts_finished,
-    year,
-    month,
-    day
-FROM datalake_hightouch_logs_clean.sync_runs_databricks
-WHERE
-    MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
-UNION ALL
-
-SELECT
-    id_sync,
-    id_sync_run,
     id_model,
     primary_key,
     destination,
     model_name,
     error,
     status,
-
     num_planned_add,
     num_planned_change,
     num_planned_remove,
@@ -53,13 +19,12 @@ SELECT
     num_failed_add,
     num_failed_change,
     num_failed_remove,
-
     ts_started,
     ts_finished,
     year,
     month,
     day
-
-FROM datalake_hightouch_logs_clean.sync_runs_trino
+FROM
+    datalake_hightouch_logs_clean.sync_runs_trino
 WHERE
     MAKE_DATE(year, month, day) BETWEEN DATE('{load_start_date}') AND DATE('{load_end_date}')
