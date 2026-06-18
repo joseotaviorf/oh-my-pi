c7ab65f67743890cb467d81e470c9778ff4fc4b8
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-06-18 15:26:08 -0300

    chore(reverse_reports): remove column_mapping_delta_test_pin Forno smoke export (#24836)
    The column_mapping_mode smoke test has been validated since April; drop the
    Forno-only GSheet export and its reverse query.

--- dags/people/reverse_reports/queries/reverse/column_mapping_delta_test_pin.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    1 AS `test_value(PER_ORGANIZATION_UNIT_DFF=Global Data Elements)`,
-    YEAR(DATE('{load_start_date}')) AS year,
-    MONTH(DATE('{load_start_date}')) AS month,
-    DAY(DATE('{load_start_date}')) AS day
