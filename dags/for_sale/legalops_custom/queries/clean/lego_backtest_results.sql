33f4748f0a1486b35fa5bbf39857f3542b862439
Helder Rodrigues <helder.garjunior@outlook.com>
2026-06-25 13:10:56 -0300

    feat(TJS-7624): Adding lego backtest results clean table (#25507)
    * Adde lego backtest results clean table
    
    * lint

---
+++ dags/for_sale/legalops_custom/queries/clean/lego_backtest_results.sql
@@ -0,0 +1,16 @@
+SELECT 
+    backtest_id AS id_backtest,
+    backtest_run_id AS id_run_backtest,
+    sales_flow_id AS id_sales_flow,
+    validation_id,
+    assessment_target,
+    validation_status,
+    assessment_status,
+    assessment_consolidated_status,
+    backtest_concordance,
+    contract_created_at AS ts_analysis_start,
+    contract_updated_at AS ts_analysis_end
+FROM datalake_legalops_raw.lego_backtest_results 
+WHERE 
+  backtest_id = 'unified_benchmark.week_20260302' AND
+  status = 'DONE'
