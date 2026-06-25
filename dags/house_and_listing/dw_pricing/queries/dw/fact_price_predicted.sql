9ea0ac07811b69c75f3d0b695f13fa2e7b03200a
Bruna Fernandes Prates <38513162+brunafernandesp@users.noreply.github.com>
2026-06-25 10:12:10 -0300

    [OWXP-3106] Delete prediction modeling tables (enrich, fact and dim) (#23814)
    * Delete enrich listing_prediction_changes
    
    * Delete DW prediction modeling
    
    * Update dependencies file
    
    * Remove reverted files from rebase

--- dags/house_and_listing/dw_pricing/queries/dw/fact_price_predicted.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    id_prediction_change AS sk_price_predicted,
-    id_house AS sk_house,
-    ts_calculator_result_started AS ts_price_predicted_started,
-    ts_calculator_result_ended AS ts_price_predicted_ended,
-    NOW() AS ts_load
-FROM
-    datalake_ebdb_pricing.listing_prediction_changes
