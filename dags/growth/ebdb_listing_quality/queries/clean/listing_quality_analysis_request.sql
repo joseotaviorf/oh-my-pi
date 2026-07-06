7588063b94c7c5589011ec9bb778acff98ad4595
Gabriel Salla <gabriel.c.salla@gmail.com>
2026-07-06 14:35:02 -0300

    add uploaded_by column to listing_quality_analysis_request (#25844)

--- dags/growth/ebdb_listing_quality/queries/clean/listing_quality_analysis_request.sql
+++ dags/growth/ebdb_listing_quality/queries/clean/listing_quality_analysis_request.sql
@@ -3,5 +3,6 @@ SELECT id,
        analysis_requested,
        listing_quality_id AS id_listing_quality,
        criadoem AS ts_created,
-       atualizadoem AS ts_updated
+       atualizadoem AS ts_updated,
+       uploaded_by
 FROM datalake_ebdb_raw.listingqualityanalysisrequest
