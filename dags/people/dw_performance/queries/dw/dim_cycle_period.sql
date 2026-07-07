5ae7a74b26d0db3624f33cb40f282a75a2477ef3
Gabriel Berger <120596108+gab-berger@users.noreply.github.com>
2026-07-07 12:56:26 -0300

    fix(dbp-1637): scope dim_cycle_period rolling windows to released cycles only (#25896)

--- dags/people/dw_performance/queries/dw/dim_cycle_period.sql
+++ dags/people/dw_performance/queries/dw/dim_cycle_period.sql
@@ -1,92 +1,4 @@
 WITH
-distinct_cycles AS (
-  SELECT DISTINCT
-    meeting_type,
-    meeting_year,
-    reference_period
-  FROM
-    dw_performance.dim_committee_meeting
-  WHERE
-    meeting_year IS NOT NULL
-),
-cycles_with_start AS (
-  SELECT
-    meeting_type,
-    meeting_year,
-    reference_period,
-    MAKE_DATE(
-      meeting_year,
-      CASE
-        WHEN reference_period IS NULL THEN 1
-        WHEN reference_period = 'Q1' THEN 1
-        WHEN reference_period = 'Q2' THEN 4
-        WHEN reference_period = 'Q3' THEN 7
-        WHEN reference_period = 'Q4' THEN 10
-        WHEN reference_period = 'H1' THEN 1
-        WHEN reference_period = 'H2' THEN 7
-      END,
-      1
-    ) AS dt_valid_from
-  FROM
-    distinct_cycles
-),
-distinct_starts AS (
-  -- Different reference_period values can share a start month (e.g. Q1 and H1).
-  SELECT DISTINCT
-    meeting_type,
-    dt_valid_from
-  FROM
-    cycles_with_start
-),
-distinct_starts_with_next AS (
-  SELECT
-    meeting_type,
-    dt_valid_from,
-    LEAD(dt_valid_from) OVER (
-      PARTITION BY meeting_type
-      ORDER BY dt_valid_from
-    ) AS dt_next_valid_from
-  FROM
-    distinct_starts
-),
-cycles_with_window AS (
-  SELECT
-    cs.meeting_type,
-    cs.meeting_year,
-    cs.reference_period,
-    cs.dt_valid_from,
-    dswn.dt_next_valid_from
-  FROM
-    cycles_with_start AS cs
-  LEFT JOIN
-    distinct_starts_with_next AS dswn
-      ON dswn.meeting_type = cs.meeting_type
-      AND dswn.dt_valid_from = cs.dt_valid_from
-),
-review_periods AS (
-  SELECT
-    MD5(CONCAT(
-      meeting_type,
-      CAST(meeting_year AS STRING),
-      COALESCE(reference_period, '-1')
-    )) AS sk_cycle_period,
-    CASE
-      WHEN meeting_type = 'performance_calibration'
-        THEN CONCAT('Performa ', CAST(meeting_year - 1 AS STRING))
-      WHEN meeting_type = 'talent_review'
-        THEN CONCAT('Talent Review ', CAST(meeting_year AS STRING), ' ', reference_period)
-    END AS cycle_name,
-    meeting_type,
-    meeting_year,
-    reference_period,
-    dt_valid_from,
-    CASE
-      WHEN dt_next_valid_from IS NULL THEN DATE'9999-12-31'
-      ELSE DATE_ADD(dt_next_valid_from, -1)
-    END AS dt_valid_to
-  FROM
-    cycles_with_window
-),
 released_cycles AS (
   -- Curated release allow-list, updated via PR. Cycles not listed here are not released.
   SELECT
@@ -102,49 +14,132 @@ released_cycles AS (
       ('Talent Review 2025 H2')
   ) AS released (cycle_name)
 ),
-review_periods_with_release AS (
+distinct_cycles AS (
+  SELECT DISTINCT
+    meeting_type,
+    meeting_year,
+    reference_period
+  FROM
+    dw_performance.dim_committee_meeting
+  WHERE
+    meeting_year IS NOT NULL
+),
+cycles_with_start AS (
   SELECT
-    rp.sk_cycle_period,
-    rp.cycle_name,
-    rp.meeting_type,
-    rp.meeting_year,
-    rp.reference_period,
-    rp.dt_valid_from,
-    rp.dt_valid_to,
+    meeting_type,
+    meeting_year,
+    reference_period,
+    MD5(CONCAT(
+      meeting_type,
+      CAST(meeting_year AS STRING),
+      COALESCE(reference_period, '-1')
+    )) AS sk_cycle_period,
+    CASE
+      WHEN meeting_type = 'performance_calibration'
+        THEN CONCAT('Performa ', CAST(meeting_year - 1 AS STRING))
+      WHEN meeting_type = 'talent_review'
+        THEN CONCAT('Talent Review ', CAST(meeting_year AS STRING), ' ', reference_period)
+    END AS cycle_name,
+    MAKE_DATE(
+      meeting_year,
+      CASE
+        WHEN reference_period IS NULL THEN 1
+        WHEN reference_period = 'Q1' THEN 1
+        WHEN reference_period = 'Q2' THEN 4
+        WHEN reference_period = 'Q3' THEN 7
+        WHEN reference_period = 'Q4' THEN 10
+        WHEN reference_period = 'H1' THEN 1
+        WHEN reference_period = 'H2' THEN 7
+      END,
+      1
+    ) AS dt_start
+  FROM
+    distinct_cycles
+),
+cycles_with_release AS (
+  SELECT
+    cs.sk_cycle_period,
+    cs.cycle_name,
+    cs.meeting_type,
+    cs.meeting_year,
+    cs.reference_period,
+    cs.dt_start,
     rc.cycle_name IS NOT NULL AS is_released
   FROM
-    review_periods AS rp
+    cycles_with_start AS cs
   LEFT JOIN
     released_cycles AS rc
-      ON rc.cycle_name = rp.cycle_name
+      ON rc.cycle_name = cs.cycle_name
+),
+released_starts AS (
+  -- Different reference_period values can share a start month (e.g. Q1 and H1).
+  SELECT DISTINCT
+    meeting_type,
+    dt_start
+  FROM
+    cycles_with_release
+  WHERE
+    is_released
+),
+released_starts_with_next AS (
+  SELECT
+    meeting_type,
+    dt_start,
+    LEAD(dt_start) OVER (
+      PARTITION BY meeting_type
+      ORDER BY dt_start
+    ) AS dt_next_start
+  FROM
+    released_starts
+),
+review_periods AS (
+  SELECT
+    cwr.sk_cycle_period,
+    cwr.cycle_name,
+    cwr.meeting_type,
+    cwr.meeting_year,
+    cwr.reference_period,
+    cwr.is_released,
+    CASE WHEN cwr.is_released THEN cwr.dt_start END AS dt_valid_from,
+    CASE
+      WHEN NOT cwr.is_released THEN NULL
+      WHEN rswn.dt_next_start IS NULL THEN DATE'9999-12-31'
+      ELSE DATE_ADD(rswn.dt_next_start, -1)
+    END AS dt_valid_to
+  FROM
+    cycles_with_release AS cwr
+  LEFT JOIN
+    released_starts_with_next AS rswn
+      ON rswn.meeting_type = cwr.meeting_type
+      AND rswn.dt_start = cwr.dt_start
 ),
 latest_released_cycle AS (
   SELECT
     meeting_type,
     MAX(dt_valid_from) AS dt_latest_released_valid_from
   FROM
-    review_periods_with_release
+    review_periods
   WHERE
     is_released
   GROUP BY
     meeting_type
 )
 SELECT
-  rpr.sk_cycle_period,
-  rpr.cycle_name,
-  rpr.meeting_type,
-  rpr.meeting_year,
-  rpr.reference_period,
+  rp.sk_cycle_period,
+  rp.cycle_name,
+  rp.meeting_type,
+  rp.meeting_year,
+  rp.reference_period,
   (
-    rpr.is_released
-    AND rpr.dt_valid_from = lrc.dt_latest_released_valid_from
+    rp.is_released
+    AND rp.dt_valid_from = lrc.dt_latest_released_valid_from
   ) AS is_current,
-  rpr.is_released,
-  rpr.dt_valid_from,
-  rpr.dt_valid_to,
+  rp.is_released,
+  rp.dt_valid_from,
+  rp.dt_valid_to,
   NOW() AS ts_load
 FROM
-  review_periods_with_release AS rpr
+  review_periods AS rp
 LEFT JOIN
   latest_released_cycle AS lrc
-    ON lrc.meeting_type = rpr.meeting_type
+    ON lrc.meeting_type = rp.meeting_type
