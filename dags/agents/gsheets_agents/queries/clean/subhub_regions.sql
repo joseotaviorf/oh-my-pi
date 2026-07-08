cc842dad7973aa26ef255e91e20905857980f90e
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2026-07-08 15:15:31 -0300

    feat(aarede-436): ingest subhub_regions mapping from Google Sheets (#25974)

---
+++ dags/agents/gsheets_agents/queries/clean/subhub_regions.sql
@@ -0,0 +1,10 @@
+SELECT
+    CAST(id_do_bairro AS INTEGER) AS id_region,
+    CAST(cidade AS STRING) AS city,
+    CAST(praca AS STRING) AS city_group,
+    CAST(bairro AS STRING) AS region,
+    CAST(hub AS STRING) AS hub_name,
+    CAST(subhub AS STRING) AS subhub_name,
+    NOW() AS ts_load
+FROM
+    datalake_gsheets_raw.subhub_regions
