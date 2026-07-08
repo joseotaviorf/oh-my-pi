3afed61db4069e0c7431a6ac6b523f4b541c4704
Anne Macedo <31035092+AnneMacedo@users.noreply.github.com>
2026-07-08 11:40:43 -0300

    chore(aarede-431): remove ciq commissioning gsheets pipelines (#25930)

--- dags/cross/gsheets_static/queries/clean/rent_comission_for_strategic_region.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    CAST(id AS BIGINT) AS id_region,
-    city_name,
-    neighborhood_name,
-    concat_city_neighborhood,
-    CAST(strategic_region AS BOOLEAN) AS is_strategic_region,
-    NOW() AS ts_load
-FROM
-    datalake_gsheets_raw.rent_comission_for_strategic_region
