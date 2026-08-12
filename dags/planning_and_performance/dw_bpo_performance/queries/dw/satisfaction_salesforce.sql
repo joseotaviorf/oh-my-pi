f5d14a74cde00857473a10167aa027d5e431a354
aleffroes <alef.vieira@quintoandar.com.br>
2026-08-12 10:13:52 -0300

    Adjust satisfaction sf (#27419)
    * adjust-satisfaction-sf
    
    * adjust-satisfaction-sf

--- dags/planning_and_performance/dw_bpo_performance/queries/dw/satisfaction_salesforce.sql
+++ dags/planning_and_performance/dw_bpo_performance/queries/dw/satisfaction_salesforce.sql
@@ -1,36 +1,86 @@
 WITH 
 
-csat AS ( 
-    SELECT DISTINCT  
-        sk_case, 
-        fa.ts_submitted,
-        fa.sk_answer,
-        satisfaction_score,
-        respondent_comments, 
-        fa.is_solved,
-        sk_account,
-        type__c,
-        CASE 
-            WHEN type__c = 'Tenant' THEN 'Inquilino' 
-            WHEN type__c IN ('Owner','Landlord') THEN 'Proprietário' 
-            ELSE 'Sem Definição de Partes' 
-        END AS client_type_csat,
-        ROW_NUMBER() OVER (
-            PARTITION BY sk_case, (CASE 
-            WHEN type__c = 'Tenant' THEN 'Inquilino' 
-            WHEN type__c IN ('Owner','Landlord') THEN 'Proprietário' 
-            ELSE 'Sem Definição de Partes' 
-        END) 
-            ORDER BY fa.ts_submitted ASC
-        ) as rn
-    FROM dw_satisfaction_rating.fact_answer as fa
-    LEFT JOIN dw_satisfaction_rating.dim_answer as da on da.sk_answer = fa.sk_answer
-    LEFT JOIN datalake_salesforce_clean.events_case_member as member_csat on member_csat.account__c = fa.sk_account
-        AND type__c IN ('Owner','Tenant','Landlord') AND fa.sk_case = member_csat.case__c 
-    WHERE satisfaction_score IS NOT NULL AND sk_case IS NOT NULL
-    AND fa.ts_submitted BETWEEN DATE('{load_start_date}') - INTERVAL 3 DAYS AND DATE('{load_end_date}') 
+
+deletados as  (SELECT 
+
+id_record,
+event_type
+
+FROM datalake_salesforce_clean.events_case 
+WHERE event_type IN ('DELETE')
+
 ),
 
+base_csat AS (
+    SELECT DISTINCT  
+        fa.sk_case, 
+        fa.ts_submitted,
+        fa.sk_answer,
+        fa.satisfaction_score,
+        da.respondent_comments, 
+        fa.sk_account,
+        fa.is_solved,
+        member_csat.type__c,
+        fa.sk_trigger,
+        CONCAT(
+            fa.sk_case,
+            COALESCE(NULLIF(fa.sk_trigger, '-1'), NULLIF(fa.sk_account, '-1'), NULLIF(fa.sk_case, '-1'))
+        ) AS key_join,
+        CASE 
+            WHEN member_csat.type__c = 'Tenant' THEN 'Inquilino' 
+            WHEN member_csat.type__c IN ('Owner','Landlord') THEN 'Proprietário' 
+            ELSE 'Sem Definição de Partes' 
+        END AS client_type_csat
+    FROM dw_satisfaction_rating.fact_answer AS fa
+    LEFT JOIN dw_satisfaction_rating.dim_answer AS da 
+        ON da.sk_answer = fa.sk_answer
+    LEFT JOIN datalake_salesforce_clean.events_case_member AS member_csat 
+        ON member_csat.account__c = fa.sk_account 
+       AND fa.sk_case = member_csat.case__c
+       AND member_csat.type__c IN ('Owner','Tenant','Landlord')
+    WHERE fa.sk_case IS NOT NULL
+    AND fa.ts_submitted BETWEEN DATE('{load_start_date}') - INTERVAL 3 DAYS AND DATE('{load_end_date}')  
+),
+
+CSAT AS (
+    SELECT 
+        key_join,
+        ts_submitted,
+        sk_answer,
+        sk_case,
+        sk_trigger,
+        type__c,
+        sk_account,
+        client_type_csat,
+        
+        -- Busca o satisfaction_score preenchido mais recente no Spark SQL
+        FIRST_VALUE(satisfaction_score) OVER (
+            PARTITION BY key_join 
+            ORDER BY CASE WHEN satisfaction_score IS NOT NULL THEN 1 ELSE 2 END, ts_submitted DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS satisfaction_score,
+
+        -- Busca o comentário do satisfaction_score preenchido mais recente
+        FIRST_VALUE(respondent_comments) OVER (
+            PARTITION BY key_join 
+            ORDER BY CASE WHEN satisfaction_score IS NOT NULL THEN 1 ELSE 2 END, ts_submitted DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS respondent_comments,
+
+        -- Busca o status is_solved preenchido mais recente
+        FIRST_VALUE(is_solved) OVER (
+            PARTITION BY key_join 
+            ORDER BY CASE WHEN is_solved IS NOT NULL THEN 1 ELSE 2 END, ts_submitted DESC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS is_solved,
+
+        -- Marca a linha mais recente da chave
+        ROW_NUMBER() OVER (
+            PARTITION BY key_join 
+            ORDER BY ts_submitted DESC
+        ) AS rn
+    FROM base_csat
+),
 fact_request as (
     SELECT 
         sk_case,
@@ -51,7 +101,7 @@ events_case as (
         c.status,
         c.id_owner,
         c.type,
-        fr.record_type_name, -- Herdado do join com fact_request
+        fr.record_type_name,
         id_parent,
         event_type,
         last_modified_date,
@@ -63,23 +113,61 @@ events_case as (
     WHERE id_record IN (SELECT DISTINCT sk_case FROM csat)
 ), 
 
-solved_date as (
+events_user as (
+SELECT 
+id_record,
+email,
+ROW_NUMBER() OVER (PARTITION BY id_record ORDER BY committed_at DESC) as rn_user
+FROM datalake_salesforce_clean.events_user
+),
+
+status_historico AS (
     SELECT 
-        cast(case_number as int) as case_number, 
-        status,   
-        CAST(last_modified_date AS TIMESTAMP) - INTERVAL 3 HOURS as ts_event, 
-        ROW_NUMBER() OVER (
+        CAST(case_number AS INT) AS case_number, 
+        status,
+        id_owner,
+        -- No Spark, a subtração de horas é feita via INTERVAL
+        CAST(last_modified_date AS TIMESTAMP) - INTERVAL 3 HOURS AS ts_event,
+        ROW_NUMBER() OVER (PARTITION BY CAST(case_number AS INT) ORDER BY CAST(last_modified_date AS TIMESTAMP) DESC) AS rn,
+        -- Pega o próximo status que o caso assumiu cronologicamente
+        LEAD(status) OVER (
             PARTITION BY case_number 
-            ORDER BY CAST(last_modified_date AS TIMESTAMP) - INTERVAL 3 HOURS ASC
-        ) rn
-    FROM events_case AS E
+            ORDER BY CAST(last_modified_date AS TIMESTAMP) ASC
+        ) AS proximo_status
+    FROM events_case
+),
+
+solved_date AS (
+    SELECT 
+        case_number,
+        status,
+        ts_event,
+        eu.email,
+        ROW_NUMBER() OVER (PARTITION BY case_number ORDER BY ts_event ASC) AS rn
+    FROM status_historico
+    LEFT JOIN events_user as eu on eu.id_record = status_historico.id_owner and rn_user = 1  
     WHERE status = 'Solved'
+),
+
+solved_final AS (
+SELECT DISTINCT
+    c.case_number,
+    CASE WHEN h.proximo_status IS NULL OR h.proximo_status IN ('Closed','Solved') THEN s.ts_event END as ts_solved,
+    c.closed_date,
+    CASE WHEN h.proximo_status IS NULL OR h.proximo_status IN ('Closed','Solved') THEN s.email END as agent_solved,
+    CAST(c.created_date AS TIMESTAMP) - INTERVAL 3 HOURS as ts_created,
+    h.proximo_status,
+  ROW_NUMBER() OVER (PARTITION BY c.case_number ORDER BY MAX(c.last_modified_date) DESC) rn
+
+FROM events_case as c
+LEFT JOIN solved_date as s on s.case_number = CAST(c.case_number as INT) and s.rn = 1
+LEFT JOIN status_historico as h on h.case_number = CAST(c.case_number as INT) and h.rn = 1
+GROUP BY 1,2,3,4,5,6
 ),
 
 first_open as (
     SELECT 
         cast(case_number as int) as case_number, 
-        -- Spark SQL: Conversão direta para TIMESTAMP
         MIN(CAST(last_modified_date AS TIMESTAMP)) AS dt_first_open
     FROM events_case AS E
     WHERE status not in ('SelfService','Solved','Closed')
@@ -122,15 +210,6 @@ repair_infiltracao AS (
     GROUP BY c.id_parent
 ),
 
-events_user as (
-SELECT 
-id_record,
-email,
-ROW_NUMBER() OVER (PARTITION BY id_record ORDER BY committed_at DESC) as rn_user
-FROM datalake_salesforce_clean.events_user
-),
-
-
 csat_final as (
     SELECT DISTINCT
         csat.sk_answer as sk_answer_csat,
@@ -145,23 +224,22 @@ csat_final as (
         cast(c.case_number AS INT) as case_number,
         record_type_name,
         CAST(c.created_date AS TIMESTAMP) - INTERVAL 3 HOURS as ts_created, 
-        sd.ts_event as ts_solved,
+        sd.ts_solved as ts_solved,
         CAST(c.closed_date AS TIMESTAMP) - INTERVAL 3 HOURS as ts_closed,
         c.status,
         f.dt_first_open,
         c.type as criticidade,
-        COALESCE(sf.email_analista, u.email) as last_agent_email,
-        CASE WHEN COALESCE(sf.email_analista, u.email) LIKE '%webhelp%' THEN 'webhelp'
-             WHEN COALESCE(sf.email_analista, u.email) LIKE '%concentrix%' THEN 'webhelp'
-             WHEN COALESCE(sf.email_analista, u.email) LIKE '%atento%' THEN 'atento'
-             WHEN COALESCE(sf.email_analista, u.email) LIKE '%aec%' THEN 'aec'
-             WHEN COALESCE(sf.email_analista, u.email) LIKE '%quintoandar%' THEN 'quintoandar'
+        COALESCE(sf.email_analista, sd.agent_solved,u.email) as last_agent_email,
+        CASE WHEN COALESCE(sf.email_analista, sd.agent_solved,u.email) LIKE '%webhelp%' THEN 'webhelp'
+             WHEN COALESCE(sf.email_analista, sd.agent_solved,u.email) LIKE '%concentrix%' THEN 'webhelp'
+             WHEN COALESCE(sf.email_analista, sd.agent_solved,u.email) LIKE '%atento%' THEN 'atento'
+             WHEN COALESCE(sf.email_analista, sd.agent_solved,u.email) LIKE '%aec%' THEN 'aec'
+             WHEN COALESCE(sf.email_analista, sd.agent_solved,u.email) LIKE '%quintoandar%' THEN 'quintoandar'
              END as last_agent_organization,
         dc.ts_termination_requested,
         dc.dt_start,
         CASE
             WHEN dc.dt_start IS NULL THEN NULL
-            -- Spark SQL: datediff(fim, inicio)
             WHEN datediff(CAST(c.created_date AS TIMESTAMP), dc.dt_start) <= 40 THEN 'ONB'
             ELSE 'ONG'
         END AS contract_journey,
@@ -172,7 +250,6 @@ csat_final as (
         COALESCE(ri.flag_infiltracao, FALSE) AS is_infiltracao,
         CASE
             WHEN f.dt_first_open IS NOT NULL
-            -- Spark SQL: date_add(data, dias)
             THEN date_add(f.dt_first_open, 1)
         END AS dt_max_fr,
         CASE
@@ -186,7 +263,7 @@ csat_final as (
 
     FROM csat as csat 
     LEFT JOIN events_case as c on c.id_record = csat.sk_case and rn_case = 1 
-    LEFT JOIN solved_date as sd on CAST(sd.case_number AS int) = CAST(c.case_number AS int) and sd.rn = 1
+    LEFT JOIN solved_final as sd on CAST(sd.case_number AS int) = CAST(c.case_number AS int) and sd.rn = 1
     LEFT JOIN first_open AS f ON CAST(f.case_number AS INT) = CAST(c.case_number AS INT)
     LEFT JOIN events_user as u on u.id_record = c.id_owner and u.rn_user = 1
     LEFT JOIN dw_rent.dim_contract dc ON TRY_CAST(c.sk_contract AS BIGINT) = dc.sk_contract
@@ -195,7 +272,8 @@ csat_final as (
     LEFT JOIN sandbox.analistas_reparos_sf as sf on cast(sf.case_number as int) = cast(c.case_number as int)
     LEFT JOIN repair_infiltracao ri ON ri.id_case_pai = c.id_record
   
-    WHERE  CSAT.rn = 1 
+    WHERE  c.id_record NOT IN (SELECT id_record FROM deletados)
+AND (CSAT.rn = 1)
 
     GROUP BY 1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29
 ),
@@ -250,3 +328,4 @@ SELECT
     DAY(CURRENT_DATE) AS day,
     NOW() AS ts_load
 FROM csat_final_ajustado
+
