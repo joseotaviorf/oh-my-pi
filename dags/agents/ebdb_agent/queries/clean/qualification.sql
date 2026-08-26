3ad0401818f35ccec1eb207664a36e6a8321de2a
Rafaela Escobar <rafaela.escobar@quintoandar.com.br>
2026-08-26 15:43:44 -0300

    feat(APLAT-2829): add Qualification rejection audit columns to datalake clean (#28019)
    * feat(APLAT-2829): add Qualification rejection audit columns to datalake clean
    
    * feat(APLAT-2829): add Qualification rejection audit columns to datalake clean
    
    * feat(APLAT-2829): add Qualification rejection audit columns to datalake clean

--- dags/agents/ebdb_agent/queries/clean/qualification.sql
+++ dags/agents/ebdb_agent/queries/clean/qualification.sql
@@ -2,9 +2,12 @@ SELECT
     id,
     agent_prospect_id AS id_agent_prospect,
     qualification_template_id AS id_qualification_template,
+    author_identifier AS id_author_identifier,
     qualification_uuid AS uuid_qualification,
     reason,
     state,
+    author_type,
+    rejected_at AS ts_rejected,
     created_at AS ts_created,
     updated_at AS ts_updated
 FROM
