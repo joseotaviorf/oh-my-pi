226219b60cfbd1590542cbbc435936efa8a50169
Gabriel Salla <gabriel.c.salla@gmail.com>
2026-09-01 15:17:27 -0300

    add id_device column to rene_descartes.lead_intent (#28223)

--- dags/growth/rene_descartes/queries/clean/lead_intent.sql
+++ dags/growth/rene_descartes/queries/clean/lead_intent.sql
@@ -1,7 +1,8 @@
 SELECT
     id_intent,
     contact_id AS id_contact,
-    house_lead_id as id_house_lead,
+    house_lead_id AS id_house_lead,
+    device_id AS id_device,
     origin,
     supply_source,
     detailed_route,
