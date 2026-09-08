4a7e98c64636070fe05d22d27415a405c24144f7
Isabella Araujo <45436397+isa-carol@users.noreply.github.com>
2026-09-08 11:06:05 -0300

    feat(dbp-1921): migrate Greenhouse Audit Log to EMR (#28392)
    * fix(dbp-1921): read inconsistent Greenhouse JSON safely
    
    * feat(dbp-1921): run Greenhouse Audit Log on EMR

--- dags/people/greenhouse_audit_log/queries/clean/events.sql
+++ dags/people/greenhouse_audit_log/queries/clean/events.sql
@@ -1,17 +1,8 @@
 SELECT
   -- ids
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.organization_id'),
-    CAST(organization_id AS STRING)
-  ) AS id_organization,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.request.id'),
-    CAST(request.id AS STRING)
-  ) AS id_request,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.performer.id'),
-    CAST(performer.id AS STRING)
-  ) AS id_performer,
+  GET_JSON_OBJECT(raw_payload, '$.organization_id') AS id_organization,
+  GET_JSON_OBJECT(raw_payload, '$.request.id') AS id_request,
+  GET_JSON_OBJECT(raw_payload, '$.performer.id') AS id_performer,
   CAST(GET_JSON_OBJECT(raw_payload, '$.event.target_id') AS STRING) AS id_event_target,
   FROM_JSON(
     GET_JSON_OBJECT(raw_payload, '$.event.meta.close_reason_id'),
@@ -22,28 +13,13 @@ SELECT
     'array<string>'
   )[1] AS id_close_reason_after_event,
   -- non-metrics
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.request.type'),
-    request.type
-  ) AS request_type,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.performer.type'),
-    performer.type
-  ) AS performer_type,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.performer.ip_address'),
-    performer.ip_address
-  ) AS performer_ip_address,
+  GET_JSON_OBJECT(raw_payload, '$.request.type') AS request_type,
+  GET_JSON_OBJECT(raw_payload, '$.performer.type') AS performer_type,
+  GET_JSON_OBJECT(raw_payload, '$.performer.ip_address') AS performer_ip_address,
   GET_JSON_OBJECT(raw_payload, '$.performer.meta.name') AS performer_name,
   GET_JSON_OBJECT(raw_payload, '$.performer.meta.username') AS performer_username,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.event.type'),
-    event.type
-  ) AS event_type,
-  COALESCE(
-    GET_JSON_OBJECT(raw_payload, '$.event.target_type'),
-    event.target_type
-  ) AS event_target_type,
+  GET_JSON_OBJECT(raw_payload, '$.event.type') AS event_type,
+  GET_JSON_OBJECT(raw_payload, '$.event.target_type') AS event_target_type,
   GET_JSON_OBJECT(raw_payload, '$.event.meta') AS event_meta,
   -- dates
   CAST(
@@ -59,13 +35,7 @@ SELECT
     )[1] AS DATE
   ) AS dt_closed_after_event,
   -- timestamp
-  CAST(
-    COALESCE(
-      GET_JSON_OBJECT(raw_payload, '$.event_time'),
-      event_time
-    )
-    AS TIMESTAMP
-  ) AS ts_event,
+  CAST(GET_JSON_OBJECT(raw_payload, '$.event_time') AS TIMESTAMP) AS ts_event,
   NOW() AS ts_load,
   -- partitions
   year,
