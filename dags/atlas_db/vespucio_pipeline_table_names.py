0d8b0efc476ebd997b1b494e389d1df364c1c6d7
humberto-tondinelli <37297479+humberto-tondinelli@users.noreply.github.com>
2026-09-10 12:14:17 -0300

    Revert "chore: remove vespucio photo duplication step (#28416)" (#28528)
    This reverts commit 9c6e97609293dfa899427f925c27e6a7535643a8.

--- dags/atlas_db/vespucio_pipeline_table_names.py
+++ dags/atlas_db/vespucio_pipeline_table_names.py
@@ -121,6 +121,9 @@ class Tables:
     artifacts_publish_checkpoint = (
         "vespucio_pipeline_delta.artifacts_publish_checkpoint"
     )
+    kodak_photo_duplication_sent = (
+        "vespucio_pipeline_delta.kodak_photo_duplication_sent"
+    )
 
     zordominium_compounds = "zordominium_vespucio_plugin.zordominium_official_condos"
 
