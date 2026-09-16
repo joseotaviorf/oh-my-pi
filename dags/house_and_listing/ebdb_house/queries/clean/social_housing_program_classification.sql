6a6bac66944d6b132aeecf1d90e69477b439aa37
Matheus Lopes <mlopes.matheus@gmail.com>
2026-09-16 17:56:36 -0300

    Revert "feat(ebdb_house): add SocialHousingProgramClassification clean layer …" (#28830)
    This reverts commit f407e4b985d2866099f29a2b50661d31d3e827dd.

--- dags/house_and_listing/ebdb_house/queries/clean/social_housing_program_classification.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    id,
-    imovel_id AS id_house,
-    social_housing_program_id AS id_social_housing_program,
-    status,
-    created_at AS ts_created,
-    updated_at AS ts_updated
-FROM
-    datalake_ebdb_raw.SocialHousingProgramClassification
