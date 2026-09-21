3cd400f8bfeb9140ac68d59a35816a3a1ee50b5c
Gabriel Berger <120596108+gab-berger@users.noreply.github.com>
2026-09-21 12:39:15 -0300

    feat(dbp-2154): ingest H2.26 talent review sheet (#28952)

---
+++ dags/people/gsheets_people_static/queries/clean/talent_review_h2_26.sql
@@ -0,0 +1,20 @@
+SELECT
+    NULLIF(
+        NULLIF(TRIM(review.matricula_pk_assignment_number), ''),
+        '-'
+    ) AS assignment_number,
+    NULLIF(NULLIF(TRIM(review.nome), ''), '-') AS name,
+    NULLIF(NULLIF(TRIM(review.email), ''), '-') AS work_email,
+    NULLIF(NULLIF(TRIM(review.lider), ''), '-') AS manager_name,
+    NULLIF(NULLIF(TRIM(review.avaliador), ''), '-') AS evaluator_name,
+    NULLIF(NULLIF(TRIM(review.tr_potencial), ''), '-') AS potential,
+    NULLIF(NULLIF(TRIM(review.tr_prontidao), ''), '-') AS readiness,
+    NULLIF(NULLIF(TRIM(review.tr_criticidade), ''), '-') AS criticality,
+    NULLIF(NULLIF(TRIM(review.tr_risco_de_perda), ''), '-') AS risk_of_loss,
+    NULLIF(
+        NULLIF(TRIM(review.data_de_atualizacao_review_month), ''),
+        '-'
+    ) AS review_month,
+    review.ts_load
+FROM
+    datalake_gsheets_people_raw.talent_review_h2_26 AS review
