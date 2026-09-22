5bbeca99f4880ceee3bebf70de44868f93550c65
Helder Rodrigues <helder.garjunior@outlook.com>
2026-09-22 09:10:47 -0300

    Revert "feat(tjs-9807): add request_source to lego_analysis_results (#28953)" (#29022)
    This reverts commit 1dc1d28bb79af274fdf4862f59e813616b3eed07.

--- dags/for_sale/legalops_custom/queries/clean/lego_analysis_results.sql
+++ dags/for_sale/legalops_custom/queries/clean/lego_analysis_results.sql
@@ -3,7 +3,6 @@ WITH base AS (
     contract_analysis_job_id AS id_contract_analysis_job,
     sales_flow_id AS id_sales_flow,
     CAST(created_at AS DATE) AS analysis_date,
-    GET_JSON_OBJECT(analysis_metadata, '$.request_source') AS request_source,
     analysis_started_at AS ts_analysis_started,
     analysis_ended_at AS ts_analysis_end,
     full_analysis
@@ -18,7 +17,6 @@ WITH base AS (
     id_contract_analysis_job,
     id_sales_flow,
     analysis_date,
-    request_source,
     ts_analysis_started,
     ts_analysis_end,
     FROM_JSON(
@@ -31,7 +29,6 @@ WITH base AS (
     id_contract_analysis_job,
     id_sales_flow,
     analysis_date,
-    request_source,
     ts_analysis_started,
     ts_analysis_end,
     vr AS validation_result
@@ -43,7 +40,6 @@ SELECT
   id_contract_analysis_job,
   id_sales_flow,
   analysis_date,
-  request_source,
   assessment.validation_id AS validation_id,
   assessment.assessment_target AS assessment_name,
   assessment.assessment_status AS assessment_status,
